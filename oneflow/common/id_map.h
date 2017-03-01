95e8e3f7aab940581ecfba9dadcad012db7d4db0
willzhang4a58 <willzhang4a58@gmail.com>
2017-03-01 04:59:46 -0500

    id map init

--- oneflow/common/id_map.h
+++ oneflow/common/id_map.h
@@ -1,14 +1,32 @@
 #ifndef ONEFLOW_COMMON_ID_MAP_H_
 #define ONEFLOW_COMMON_ID_MAP_H_
 
+#include "common/util.h"
+#include "job/resource.pb.h"
+
 namespace oneflow {
 
-using DeviceId = int32_t;
 using DeviceLogicalId = int32_t;
-using DeviceLocalId = int32_t;
 using DevicePhysicalId = int32_t;
 
 using MachineId = int32_t;
+
+using ThreadGlobalId = int32_t;
+using ThreadLocalId = int32_t;
+
+class IDMap {
+ public:
+  DISALLOW_COPY_AND_MOVE(IDMap);
+  IDMap() = default;
+  ~IDMap() = default;
+
+  void Init(const Resource& resource) {
+    // TODO
+  }
+
+ private:
+
+};
 
 } // namespace oneflow
 
