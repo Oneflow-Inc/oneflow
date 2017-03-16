7446ac339d052b42ec5aa766c06b91b062377ebc
willzhang4a58 <willzhang4a58@gmail.com>
2017-03-16 02:07:10 -0400

    save work

--- oneflow/graph/boxing_transformer_graph.cpp
+++ oneflow/graph/boxing_transformer_graph.cpp
@@ -4,7 +4,14 @@ namespace oneflow {
 
 void BoxingTransfmGraph::FwBuildGraph() {
   auto boxing_task_node = of_dynamic_cast<const BoxingTaskNode*>(task_node());
-  // TODO
+  std::unordered_map<ChainNode*, std::vector<TaskEdge*>> chain2task_edges;
+  std::vector<std::pair<ChainNode*, ChainNode*>> chain_pairs;
+  // Set chain2tasks, chain_pairs
+  for (Edge* base_in_edge : boxing_task_node()->in_edges()) {
+    auto in_edge = of_dynamic_cast<TaskEdge*> (base_in_edge);
+
+  }
+
 }
 
 } // namespace oneflow
