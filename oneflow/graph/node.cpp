b5c6d028bd46a8e156c7c292431ad9f4a9815360
willzhang4a58 <willzhang4a58@gmail.com>
2017-03-16 07:53:03 -0400

    template node,edge

--- oneflow/graph/node.cpp
+++ oneflow/graph/node.cpp
@@ -3,52 +3,9 @@
 
 namespace oneflow {
 
-void Node::Init() {
+int32_t NewNodeId() {
   static int32_t node_id_cnt = 0;
-  node_id_ = node_id_cnt++;
-}
-  
-void Node::DisconnectAllEdges() {
-  for (Edge* edge : in_edges_) {
-    DisConnect(edge);
-  }
-  for (Edge* edge : out_edges_) {
-    DisConnect(edge);
-  }
-  CHECK(in_edges_.empty());
-  CHECK(out_edges_.empty());
-}
-
-bool Node::HasSuccessor(const Node* succ_node) const {
-  for (Edge* edge : out_edges_) {
-    if (edge->dst_node() == succ_node) {
-      return true;
-    }
-  }
-  return false;
-}
-
-bool Node::HasPredecessor(const Node* pred_node) const {
-  for (Edge* edge : in_edges_) {
-    if (edge->src_node() == pred_node) {
-      return true;
-    }
-  }
-  return false;
-}
-
-void DisConnect(Edge* edge) {
-  edge->src_node_->out_edges_.erase(edge);
-  edge->dst_node_->in_edges_.erase(edge);
-  edge->src_node_ = nullptr;
-  edge->dst_node_ = nullptr;
-}
-
-void Connect(Node* src_node, Edge* edge, Node* dst_node) {
-  src_node->out_edges_.insert(edge);
-  dst_node->in_edges_.insert(edge);
-  edge->src_node_ = src_node;
-  edge->dst_node_ = dst_node;
+  return node_id_cnt++;
 }
 
 } // namespace oneflow
--- oneflow/graph/node.h
+++ oneflow/graph/node.h
@@ -9,69 +9,120 @@
 
 namespace oneflow {
 
-class Node;
+template<typename DerivedEdge>
+void Connect(typename DerivedEdge::NodeType* src_node,
+             DerivedEdge* edge,
+             typename DerivedEdge::NodeType* dst_node) {
+  src_node->out_edges_.insert(edge);
+  dst_node->in_edges_.insert(edge);
+  edge->src_node_ = src_node;
+  edge->dst_node_ = dst_node;
+}
 
+template<typename DerivedEdge>
+void DisConnect(DerivedEdge* edge) {
+  edge->src_node_->out_edges_.erase(edge);
+  edge->dst_node_->in_edges_.erase(edge);
+  edge->src_node_ = nullptr;
+  edge->dst_node_ = nullptr;
+}
+
+int32_t NewNodeId();
+
+template<typename DerivedEdge>
 class Edge {
  public:
   DISALLOW_COPY_AND_MOVE(Edge);
   Edge() = default;
   virtual ~Edge() = default;
+  
+  using NodeType = typename DerivedEdge::NodeType;
+  using EdgeType = typename NodeType::EdgeType;
+  static_assert(std::is_same<DerivedEdge, EdgeType>::value, "");
 
   virtual void Init() {
     src_node_ = nullptr;
     dst_node_ = nullptr;
   }
 
-  Node* src_node() const { return src_node_; }
-  Node* dst_node() const { return dst_node_; }
+  NodeType* src_node() const { return src_node_; }
+  NodeType* dst_node() const { return dst_node_; }
 
  private:
-  // only follows can write the src_node_ and dst_node_
-  friend void Connect(Node* src_node, Edge* edge, Node* dst_node);
-  friend void DisConnect(Edge* edge);
+  friend void Connect<EdgeType>(NodeType* src_node,
+                                EdgeType* edge,
+                                NodeType* dst_node);
+  friend void DisConnect<EdgeType>(EdgeType* edge);
   
-  Node* src_node_;
-  Node* dst_node_;
+  NodeType* src_node_;
+  NodeType* dst_node_;
 
 };
 
+
+template<typename DerivedNode>
 class Node {
  public:
   DISALLOW_COPY_AND_MOVE(Node);
   Node() = default;
   virtual ~Node() = default;
   
-  virtual void Init();
+  using EdgeType = typename DerivedNode::EdgeType;
+  using NodeType = typename EdgeType::NodeType;
+  static_assert(std::is_same<DerivedNode, NodeType>::value, "");
+
+  virtual void Init() {
+    node_id_ = NewNodeId();
+  }
 
   int32_t node_id() const { return node_id_; }
 
-  const std::unordered_set<Edge*>& in_edges() const {
+  const std::unordered_set<EdgeType*>& in_edges() const {
     return in_edges_;
   }
-  const std::unordered_set<Edge*>& out_edges() const {
+  const std::unordered_set<EdgeType*>& out_edges() const {
     return out_edges_;
   }
 
-  void DisconnectAllEdges();
-
-  bool HasSuccessor(const Node* succ_node) const;
-
-  bool HasPredecessor(const Node* pred_node) const;
-
-  virtual bool IsEmptyIn() const {
-    return in_edges_.empty();
+  void DisconnectAllEdges() {
+    for (EdgeType* edge : in_edges_) {
+      DisConnect(edge);
+    }
+    for (EdgeType* edge : out_edges_) {
+      DisConnect(edge);
+    }
+    CHECK(in_edges_.empty());
+    CHECK(out_edges_.empty());
   }
-  virtual bool IsEmptyOut() const {
-    return out_edges_.empty();
+
+  bool HasSuccessor(const NodeType* succ_node) const {
+    for (EdgeType* edge : out_edges_) {
+      if (edge->dst_node() == succ_node) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  bool HasPredecessor(const Node* pred_node) const {
+    for (EdgeType* edge : in_edges_) {
+      if (edge->src_node() == pred_node) {
+        return true;
+      }
+    }
+    return false;
   }
 
  private:
-  friend void Connect(Node* src_node, Edge* edge, Node* dst_node);
-  friend void DisConnect(Edge* edge);
+  friend void Connect<EdgeType>(NodeType* src_node,
+                                EdgeType* edge,
+                                NodeType* dst_node);
+  friend void DisConnect<EdgeType>(EdgeType* edge);
+
   int32_t node_id_;
 
-  std::unordered_set<Edge*> in_edges_;
-  std::unordered_set<Edge*> out_edges_;
+  std::unordered_set<EdgeType*> in_edges_;
+  std::unordered_set<EdgeType*> out_edges_;
 
 };
 
