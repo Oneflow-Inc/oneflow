b4ac0f17c6227f492608d58b717664ad27af80ca
xswang <2012wxs@gmail.com>
2017-03-21 02:18:27 -0400

    update: split findgrpc and findprotoc

--- cmake/FindGRPC.cmake
+++ cmake/FindGRPC.cmake
@@ -1,4 +1,4 @@
-function(PROTOBUF_GENERATE_CPP SRCS HDRS ROOT_DIR)
+function(GRPC_GENERATE_CPP SRCS HDRS ROOT_DIR)
   if(NOT ARGN)
     message(SEND_ERROR "Error: RELATIVE_PROTOBUF_GENERATE_CPP() called without any proto files")
     return()
@@ -12,9 +12,7 @@ function(PROTOBUF_GENERATE_CPP SRCS HDRS ROOT_DIR)
     get_filename_component(FIL_DIR ${ABS_FIL} PATH)
     file(RELATIVE_PATH REL_DIR ${ROOT_DIR} ${FIL_DIR})
 
-    list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.cc")
     list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.grpc.pb.cc")
-    list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h")
     list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.grpc.pb.h")
 
     #MESSAGE(STATUS "ROOT_DIR = ${ROOT_DIR}; ABS_FIL = ${ABS_FIL}")
@@ -23,14 +21,8 @@ function(PROTOBUF_GENERATE_CPP SRCS HDRS ROOT_DIR)
     #MESSAGE(STATUS "REL_DIR = ${REL_DIR}")
     #MESSAGE(STATUS "PROJECT_SOURCE_DIR = ${PROJECT_SOURCE_DIR}")
     add_custom_command(
-      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.cc"
-             "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.grpc.pb.cc"
-             "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h"
+      OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.grpc.pb.cc"
              "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.grpc.pb.h"
-      COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
-      ARGS "--proto_path=${PROJECT_SOURCE_DIR}/${REL_DIR}/"
-           "--cpp_out=${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/" 
-           "${ABS_FIL}"
       COMMAND ${PROTOBUF_PROTOC_EXECUTABLE}
       ARGS "--proto_path=${PROJECT_SOURCE_DIR}/${REL_DIR}/"
            "--grpc_out=${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/"
--- cmake/FindPROTOC.cmake
+++ cmake/FindPROTOC.cmake
@@ -7,29 +7,33 @@ function(PROTOBUF_GENERATE_CPP SRCS HDRS ROOT_DIR)
   set(${SRCS})
   set(${HDRS})
   foreach(FIL ${ARGN})
-    MESSAGE(STATUS "FIL = ${FIL}")
     set(ABS_FIL ${ROOT_DIR}/${FIL})
-    MESSAGE(STATUS "ABS_FIL = ${ABS_FIL}")
     get_filename_component(FIL_WE ${FIL} NAME_WE)
     get_filename_component(FIL_DIR ${ABS_FIL} PATH)
-    MESSAGE(STATUS "FIL_DIR = ${FIL_DIR}")
     file(RELATIVE_PATH REL_DIR ${ROOT_DIR} ${FIL_DIR})
 
     list(APPEND ${SRCS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.cc")
     list(APPEND ${HDRS} "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h")
 
+    #MESSAGE(STATUS "ROOT_DIR = ${ROOT_DIR}; ABS_FIL = ${ABS_FIL}")
+    #MESSAGE(STATUS "Protobuf_INCLUDE_DIRS = ${Protobuf_INCLUDE_DIRS}")
+    #MESSAGE(STATUS "CMAKE_CURRENT_BINARY_DIR = ${CMAKE_CURRENT_BINARY_DIR}")
+    #MESSAGE(STATUS "REL_DIR = ${REL_DIR}")
+    #MESSAGE(STATUS "PROJECT_SOURCE_DIR = ${PROJECT_SOURCE_DIR}")
     add_custom_command(
       OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.cc"
              "${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/${FIL_WE}.pb.h"
       COMMAND  ${PROTOBUF_PROTOC_EXECUTABLE}
-      ARGS --cpp_out  ${CMAKE_CURRENT_BINARY_DIR} -I ${ROOT_DIR} ${ABS_FIL} -I ${Protobuf_INCLUDE_DIRS} -I ${PROJECT_SOURCE_DIR}/oneflow
+      ARGS "--proto_path=${PROJECT_SOURCE_DIR}/${REL_DIR}/"
+           "--cpp_out=${CMAKE_CURRENT_BINARY_DIR}/${REL_DIR}/" 
+           "${ABS_FIL}"
       DEPENDS ${ABS_FIL}
       COMMENT "Running C++ protocol buffer compiler on ${FIL}"
-      VERBATIM )
+      VERBATIM 
+      )
   endforeach()
 
   set_source_files_properties(${${SRCS}} ${${HDRS}} PROPERTIES GENERATED TRUE)
   set(${SRCS} ${${SRCS}} PARENT_SCOPE)
   set(${HDRS} ${${HDRS}} PARENT_SCOPE)
 endfunction()
-
