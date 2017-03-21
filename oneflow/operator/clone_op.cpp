e1113ba77ed451852e9c16fdd7009001dbc87b90
willzhang4a58 <willzhang4a58@gmail.com>
2017-03-21 05:11:59 -0400

    clone op with different output_blob_name

--- oneflow/operator/clone_op.cpp
+++ oneflow/operator/clone_op.cpp
@@ -10,26 +10,27 @@ void CloneOp::Init(const OperatorConf& op_conf) {
   mutable_pb_op_conf().reset(cnf);
   
   RegisterInputBlobName(cnf->lbn());
-  RegisterInputDiffBlobName(cnf->lbn());
+  RegisterInputDiffBlobName(GenDiffBlobName(cnf->lbn()));
   for (int32_t i = 0; i < cnf->clone_num(); ++i) {
-    RegisterOutputBlobName(cnf->lbn());
-    RegisterOutputDiffBlobName(cnf->lbn());
+    std::string obn = cnf->lbn() + "/" + std::to_string(i);
+    RegisterOutputBlobName(obn);
+    RegisterOutputDiffBlobName(GenDiffBlobName(obn));
   }
 }
 
 std::string CloneOp::ibn2lbn(const std::string& input_blob_name) const {
   return input_blob_name;
 }
-std::string CloneOp::obn2lbn(const std::string& output_blob_name) const {
-  return output_blob_name;
-}
-
 std::string CloneOp::idbn2lbn(const std::string input_diff_blob_name) const {
-  return input_diff_blob_name;
+  return GenUnDiffBlobName(input_diff_blob_name);
 }
 
+std::string CloneOp::obn2lbn(const std::string& output_blob_name) const {
+  size_t slash_pos = output_blob_name.rfind('/');
+  return output_blob_name.substr(0, slash_pos);
+}
 std::string CloneOp::odbn2lbn(const std::string output_diff_blob_name) const {
-  return output_diff_blob_name;
+  return obn2lbn(GenUnDiffBlobName(output_diff_blob_name));
 }
 
 } // namespace oneflow
