10f41e80fc573b3e366dd6cca27e7f895e889b2e
willzhang4a58 <willzhang4a58@gmail.com>
2017-03-22 03:26:56 -0400

    delete blob_desc

--- oneflow/path/path_builder.h
+++ oneflow/path/path_builder.h
@@ -1,14 +1,21 @@
 #ifndef ONEFLOW_PATH_PATH_BUILDER_H_
 #define ONEFLOW_PATH_PATH_BUILDER_H_
 
-#include "graph/transformer_graph.h"
+#include "common/util.h"
+#include "graph/transfm_graph.h"
 
 namespace oneflow {
 
 class PathBuilder {
  public:
+  DISALLOW_COPY_AND_MOVE(PathBuilder);
+  PathBuilder() = default;
+  ~PathBuilder() = default;
 
  private:
+  std::shared_ptr<TaskGraph> task_graph_;
+  std::vector<std::unique_ptr<TransfmGraph>> transfm_graph_vec_;
+
 };
 
 } // namespace oneflow
