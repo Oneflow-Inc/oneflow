1ad8ffad6afc085c100cd704b10901206537de14
willzhang4a58 <willzhang4a58@gmail.com>
2017-03-22 02:13:07 -0400

    add todo for register_desc

--- oneflow/graph/register_desc.h
+++ oneflow/graph/register_desc.h
@@ -1,23 +1,54 @@
 #ifndef ONEFLOW_GRAPH_REGISTER_DESC_H_
 #define ONEFLOW_GRAPH_REGISTER_DESC_H_
 
+#include "common/util.h"
+
 namespace oneflow {
 
 class RegisterDesc {
  public:
-  void Add(const std::string& pbn) {}
-  void Add(const std::string& pbn, const std::string& lbn) {}
+  DISALLOW_COPY_AND_MOVE(RegisterDesc);
+  RegisterDesc() = default;
+  ~RegisterDesc() = default;
 
-  virtual void Init();
+  void Add(const std::string& pbn) {
+    LOG(FATAL) << "TODO";
+  }
+  void Add(const std::string& pbn, const std::string& lbn) {
+    LOG(FATAL) << "TODO";
+  }
+
+  virtual void Init() {
+    LOG(FATAL) << "TODO";
+  }
 
  private:
+
 };
 
 // Contiguous
-class ContigRegistDesc : public RegisterDesc {
+class ContigRegistDesc final : public RegisterDesc {
+ public:
+  DISALLOW_COPY_AND_MOVE(ContigRegistDesc);
+  ContigRegistDesc() = default;
+  ~ContigRegistDesc() = default;
+
+  void Init() override {
+    LOG(FATAL) << "TODO";
+  }
+
 };
 
-class DisContigRegistDesc : public RegisterDesc {
+class DisContigRegistDesc final : public RegisterDesc {
+ public:
+  DISALLOW_COPY_AND_MOVE(DisContigRegistDesc);
+  DisContigRegistDesc() = default;
+  ~DisContigRegistDesc() = default;
+  
+  void Init() override {
+    LOG(FATAL) << "TODO";
+  }
+
 };
 
 } // namespace oneflow
