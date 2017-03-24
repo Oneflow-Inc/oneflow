b82f13cd7b2bd69e8f91a440a58410f29252010a
xswang <2012wxs@gmail.com>
2017-03-24 03:30:33 -0400

    update: refactor

---
+++ oneflow/proto/proto_io.cpp
@@ -0,0 +1,69 @@
+#include <stdint.h>
+#include <fcntl.h>
+
+#include <algorithm>
+#include <fstream>  // NOLINT(readability/streams)
+#include <string>
+#include <vector>
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/text_format.h>
+#include "proto_io.h"
+
+const int kProtoReadBytesLimit = INT_MAX;  // Max size of 2 GB minus 1 byte.
+
+namespace oneflow {
+
+using google::protobuf::io::FileInputStream;
+using google::protobuf::io::FileOutputStream;
+using google::protobuf::io::ZeroCopyInputStream;
+using google::protobuf::io::CodedInputStream;
+using google::protobuf::io::ZeroCopyOutputStream;
+using google::protobuf::io::CodedOutputStream;
+using google::protobuf::Message;
+
+bool ParseProtoFromString(const std::string& str, Message* proto) {
+  bool success = google::protobuf::TextFormat::ParseFromString(str, proto);
+  return success;
+}
+void PrintProtoToString(const Message& proto, std::string* str) {
+  CHECK(google::protobuf::TextFormat::PrintToString(proto, str));
+}
+bool ReadProtoFromTextFile(const char* filename, Message* proto) {
+  int fd = open(filename, O_RDONLY);
+  CHECK_NE(fd, -1) << "File not found: " << filename;
+  FileInputStream* input = new FileInputStream(fd);
+  bool success = google::protobuf::TextFormat::Parse(input, proto);
+  delete input;
+  close(fd);
+  return success;
+}
+
+void WriteProtoToTextFile(const Message& proto, const char* filename) {
+  int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  FileOutputStream* output = new FileOutputStream(fd);
+  CHECK(google::protobuf::TextFormat::Print(proto, output));
+  delete output;
+  close(fd);
+}
+
+bool ReadProtoFromBinaryFile(const char* filename, Message* proto) {
+  int fd = open(filename, O_RDONLY | O_BINARY);
+  CHECK_NE(fd, -1) << "File not found: " << filename;
+  ZeroCopyInputStream* raw_input = new FileInputStream(fd);
+  CodedInputStream* coded_input = new CodedInputStream(raw_input);
+  coded_input->SetTotalBytesLimit(kProtoReadBytesLimit, 536870912);
+
+  bool success = proto->ParseFromCodedStream(coded_input);
+
+  delete coded_input;
+  delete raw_input;
+  close(fd);
+  return success;
+}
+
+void WriteProtoToBinaryFile(const Message& proto, const char* filename) {
+  std::fstream output(filename, std::ios::out | std::ios::trunc | std::ios::binary);
+  CHECK(proto.SerializeToOstream(&output));
+}
+}  // namespace oneflow
