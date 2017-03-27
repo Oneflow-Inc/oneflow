365bdf114e433770ba6cd68a061e1f9031915b18
xswang <2012wxs@gmail.com>
2017-03-26 23:48:20 -0400

    fix: pipe dag

--- oneflow/dag/pipe_dag.cpp
+++ oneflow/dag/pipe_dag.cpp
@@ -17,7 +17,7 @@ PipeDag<Dtype>::PipeDag(
     std::shared_ptr<StageDag<Dtype>> stage_dag,
     PathType path_type,
     const std::string& name) :
-  Dag(path_type, name),
+  Dag<EnvelopeMeta, PipeMeta>(path_type, name),
   segment_dag_(segment_dag),
   stage_dag_(stage_dag) {
   Build();
