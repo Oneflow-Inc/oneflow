0cc8bd6293c5a85acb4ebc6d1d6fa457748e33ce
willzhang4a58 <willzhang4a58@gmail.com>
2017-04-10 02:10:56 -0400

    model load/save op

---
+++ oneflow/operator/model_load_op.h
@@ -0,0 +1,27 @@
+#ifndef ONEFLOW_OPERATOR_MODEL_LOAD_OP_H_
+#define ONEFLOW_OPERATOR_MODEL_LOAD_OP_H_
+
+#include "operator/operator.h"
+
+namespace oneflow {
+
+class ModelLoadOp final : public SysOperator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(ModelLoadOp);
+  ModelLoadOp() default;
+  ~ModelLoadOp() = default;
+
+  void Init(const OperatorConf& op_conf) override {
+    mut_op_name() = op_conf.name();
+    
+    CHECK(op_conf.has_model_load_op_conf());
+    auto cnf = new ModelLoadOp(op_conf.model_load_op_conf());
+    mut_pb_op_conf().reset(cnf);
+  }
+
+ private:
+};
+
+} // namespace oneflow
+
+#endif // ONEFLOW_OPERATOR_MODEL_LOAD_OP_H_
