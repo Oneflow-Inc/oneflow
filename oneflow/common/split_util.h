e5d04b9767b0eef5ece15c0a782abd9f387ab7fd
xswang <2012wxs@gmail.com>
2017-04-17 04:59:57 -0400

    rm

--- oneflow/common/split_util.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COMMON_SPLIT_UTIL_H_
-#define _COMMON_SPLIT_UTIL_H_
-#include <vector>
-#include <cstdint>
-namespace oneflow {
-void GetDimOfEachSplit(
-  int64_t whole_dim, int64_t num_split, std::vector<int64_t> *split_dim);
-}  // namespace oneflow
-#endif  // _COMMON_SPLIT_UTIL_H_
