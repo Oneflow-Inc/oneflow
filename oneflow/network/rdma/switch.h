3e2a0e6b885ea330fc433f8c67ea55f52f4476c1
syshangguan <shgshy@gmail.com>
2017-04-18 21:47:00 +0800

    add: switch.h

---
+++ oneflow/network/rdma/switch.h
@@ -0,0 +1,11 @@
+#ifndef ONEFLOW_NETWORK_RDMA_SYSTEM_COMPATIBLE_H_
+#define ONEFLOW_NETWORK_RDMA_SYSTEM_COMPATIBLE_H_
+
+#ifdef WIN32
+#include "windows/common.h"
+#else
+#include "linux/common.h"
+#endif
+
+
+#endif // ONEFLOW_NETWORK_RDMA_SYSTEM_COMPATIBLE_H_
