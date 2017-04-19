a42664813183817395446f0b329e25422aabd967
syshangguan <shgshy@gmail.com>
2017-04-20 01:26:35 +0800

    update format

--- oneflow/network/rdma/connection_pool.cpp
+++ oneflow/network/rdma/connection_pool.cpp
@@ -2,15 +2,13 @@
 
 namespace oneflow {
 
-void ConnectionPool::AddConnection(int32_t peer_rank, Connection* conn)
-{
+void ConnectionPool::AddConnection(int32_t peer_rank, Connection* conn) {
     conn->BuildConnection(peer_rank);
     connection_dict_.insert({ peer_rank, conn });
     connNum++;
 }
 
-void ConnectionPool::CleanConnection(int32_t peer_rank)
-{
+void ConnectionPool::CleanConnection(int32_t peer_rank) {
     Connection* conn = GetConnection(peer_rank);
     if (conn != nullptr) {
         conn->DestroyConnection();
@@ -19,8 +17,7 @@ void ConnectionPool::CleanConnection(int32_t peer_rank)
     }
 }
 
-Connection* ConnectionPool::GetConnection(peer_rank) const
-{
+Connection* ConnectionPool::GetConnection(peer_rank) const {
     auto conn_it = connection_dict_.find(peer_rank);
     if (conn_it != connection_dict_.end()) {
         Connection* conn = conn_it->second;
