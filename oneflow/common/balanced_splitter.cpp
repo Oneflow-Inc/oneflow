418455c38f43795e8b24c4a66e5664890bb42df6
willzhang4a58 <willzhang4a58@gmail.com>
2017-04-22 01:58:27 -0400

    balanced_splitter

--- oneflow/common/balanced_splitter.cpp
+++ oneflow/common/balanced_splitter.cpp
@@ -3,20 +3,12 @@
 
 namespace oneflow {
 
-void BalancedSplitter::Init(int64_t total_num, int64_t split_num) {
-  CHECK_GE(total_num, split_num);
-  CHECK_GT(split_num, 0);
-  minimum_guarantee_ = total_num / split_num;
-  threshold_ = total_num % split_num;
+BalancedSplitter::BalancedSplitter(int64_t total_num, int64_t split_num) {
+  TODO();
 }
 
-int64_t BalancedSplitter::At(int64_t idx) const {
-  CHECK_GE(idx, 0);
-  if (idx < threshold_) {
-    return minimum_guarantee_ + 1;
-  } else {
-    return minimum_guarantee_;
-  }
+Range BalancedSplitter::At(int64_t idx) const {
+  TODO();
 }
 
 } // namespace oneflow
