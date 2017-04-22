a0b6136fc408797e073441bb517b1e8dad8946e0
willzhang4a58 <willzhang4a58@gmail.com>
2017-04-22 05:35:53 -0400

    add log

--- oneflow/job/compiler.cpp
+++ oneflow/job/compiler.cpp
@@ -14,10 +14,13 @@ DEFINE_string(job_conf_filepath, "", "");
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   google::ParseCommandLineFlags(&argc, &argv, true);
+  LOG(INFO) << "Compiler Starting Up...";
+  LOG(INFO) << "Read JobConf...";
   JobConf job_conf;
   ParseProtoFromTextFile(FLAGS_job_conf_filepath, &job_conf);
   JobDesc::Singleton().InitFromJobConf(job_conf);
   IDMgr::Singleton().InitFromResource(JobDesc::Singleton().resource());
   TaskGraphMgr::Singleton().Init();
+  LOG(INFO) << "Compiler Shutting Down...";
   return 0;
 }
