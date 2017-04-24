8b67f9ac2ff162e724b99be6e43120d6d12fb5ca
syshangguan <shgshy@gmail.com>
2017-04-24 18:45:07 +0800

    add implementation of linux/connection

--- oneflow/network/rdma/linux/connection.h
+++ oneflow/network/rdma/linux/connection.h
@@ -9,15 +9,28 @@
 
 namespace oneflow{
 
+extern const int BUFFER_SIZE;
+
+struct Context {
+    struct ibv_context *ctx;
+    struct ibv_pd *pd;
+    struct ibv_cq *cq;
+    struct ibv_comp_channel *comp_channel;
+
+    pthread_t cq_poller_thread;
+}
+
 class Connection {
 public:
     Connection();
     ~Connection();
-    Connection(int32_t peer_rank);
+    Connection(uint64_t peer_machine_id);
+    void BuildConnection(uint64_t peer_machine_id);
 
 private:
-    int32_t peer_rank_ = { -1 };
+    uint64_t peer_machine_id_;
     
+    struct rdma_cm_id* id_;
     struct ibv_qp* queue_pair_;
 
     struct ibv_mr* recv_mr_;
@@ -25,13 +38,16 @@ private:
 
     char* recv_region_;
     char* send_region_;
+    
+    struct Context *s_ctx;
 
     // map peer_rank to rdma_cm_id
-    
     void BuildConnection(struct rdma_cm_id* id);
     void BuildContext(struct ibv_context* verbs);
     void BuildQPAttr(struct ibv_qp_init_attr* queue_pair_attr);
     void BuildParams(struct rdma_comm_param* params);
+    DestroyConnection();
+    
     // void RegisterMemory();
     // void PostReceiver();
     // void 
