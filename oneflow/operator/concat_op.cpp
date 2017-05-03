3405ad81edb6674bfa06931133d94975779d997f
willzhang4a58 <willzhang4a58@gmail.com>
2017-05-03 16:15:11 +0800

    fix bug in concat enroll blob

--- oneflow/operator/concat_op.cpp
+++ oneflow/operator/concat_op.cpp
@@ -9,8 +9,8 @@ void ConcatOp::InitFromOpConf(const OperatorConf& op_conf) {
 
   for (int i = 0; i < op_conf.concat_conf().in_size(); ++i) {
     std::string ibn = "in_" + std::to_string(i);
-    EnrollInputBn(ibn);
     CHECK(ibn2lbn_.emplace(ibn, op_conf.concat_conf().in(i)).second);
+    EnrollInputBn(ibn);
   }
   EnrollOutputBn("out");
 }
