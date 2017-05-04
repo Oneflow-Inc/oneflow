01787ff260613ebf5b6ec5882aec84065f9b90b2
chengcheng <472491134@qq.com>
2017-05-04 13:30:10 +0800

    use lvalue reference avoid copy constructor

--- oneflow/operator/pooling_op.cpp
+++ oneflow/operator/pooling_op.cpp
@@ -27,18 +27,19 @@ void PoolingOp::InferShape4FwBlobs(
   Shape* output_shape_ptr = GetShapePtr4BnInOp(SoleObn());
   Shape* input_shape_ptr = GetShapePtr4BnInOp(SoleIbn());
   const PoolingOpConf& pooling_conf = op_conf().pooling_conf();
-  auto pad_vec = pooling_conf.pad();
-  auto kernel_vec = pooling_conf.kernel_size();
-  auto stride_vec = pooling_conf.stride();
+  const auto& pad_vec = pooling_conf.pad();
+  const auto& kernel_size_vec = pooling_conf.kernel_size();
+  const auto& stride_vec = pooling_conf.stride();
   std::vector<int64_t> output_shape_dim_vec = {
       input_shape_ptr->At(0),
       input_shape_ptr->At(1)};
   CHECK_EQ(input_shape_ptr->NumAxes() - 2, pad_vec.size());
-  CHECK_EQ(pad_vec.size(), kernel_vec.size());
+  CHECK_EQ(pad_vec.size(), kernel_size_vec.size());
   CHECK_EQ(pad_vec.size(), stride_vec.size());
   for (size_t i = 0; i < pad_vec.size(); i++) {
-    int64_t temp = input_shape_ptr->At(i + 2) + 2 * pad_vec[i] - kernel_vec[i];
-    output_shape_dim_vec.push_back(temp / stride_vec[i] + 1);
+    int64_t input_val = input_shape_ptr->At(i + 2);
+    output_shape_dim_vec.push_back(
+      (input_val + 2 * pad_vec[i] - kernel_size_vec[i]) / stride_vec[i] + 1);
   }
   *output_shape_ptr = Shape(output_shape_dim_vec);
   CHECK_EQ(data_tmp_bns().size(), 1);
