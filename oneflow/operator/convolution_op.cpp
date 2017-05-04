d7671118b81a298102a8fd4dd383a7b6f3770401
Wind5 <1163643073@qq.com>
2017-05-04 13:57:19 +0800

    concat, softmax, conv

--- oneflow/operator/convolution_op.cpp
+++ oneflow/operator/convolution_op.cpp
@@ -27,38 +27,31 @@ void ConvolutionOp::InferShape4FwBlobs(
     ParallelPolicy policy,
     uint64_t parallel_id,
     uint64_t parallel_num) const {
-  TODO();
-  /*
   Shape* input_shape_ptr = GetShapePtr4BnInOp(SoleIbn());
   Shape* output_shape_ptr = GetShapePtr4BnInOp(SoleObn());
   Shape* colbuf_shape_ptr = GetShapePtr4BnInOp(data_tmp_bns().at(0));
   auto conv_conf = op_conf().convolution_conf();
-  auto pad = CheckDimPara4CnnOrPooling(conv_conf.pad(), 
-                                       conv_conf.pad_h(), 
-                                       conv_conf.pad_w());
-  auto kernel = CheckDimPara4CnnOrPooling(conv_conf.kernel_size(), 
-                                          conv_conf.kernel_h(),
-                                          conv_conf.kernel_w());
-  auto stride = CheckDimPara4CnnOrPooling(conv_conf.stride(), 
-                                          conv_conf.stride_h(),
-                                          conv_conf.stride_w());
   int64_t batch_size = input_shape_ptr->At(0);
   int64_t c_i = input_shape_ptr->At(1);
-  int64_t h_i = input_shape_ptr->At(2);
-  int64_t w_i = input_shape_ptr->At(3);
   int64_t c_o = conv_conf.out_num();
-  int64_t h_o = (h_i + 2 * pad.first - kernel.first) / stride.first + 1;
-  int64_t w_o = (w_i + 2 * pad.second - kernel.second) / stride.second + 1;
-  *output_shape_ptr = Shape(({batch_size, c_o, h_o, w_o}));
-  *colbuf_shape_ptr = Shape({batch_size, h_o * w_o, 
-                             c_i * kernel.first * kernel.second}); 
+  int64_t kernel_size = 1;
+  int64_t output_size = 1;
+  std::vector<int64_t> output_shape_vec = {batch_size, c_o};
+  for (int64_t i = 0; i < input_shape_ptr->NumAxes() - 2; ++i) {
+    int64_t len = (input_shape_ptr->At(i + 2) + 2 * conv_conf.pad(i) - 
+                  conv_conf.kernel_size(i)) / conv_conf.stride(i) + 1; 
+    output_shape_vec.push_back(len);
+    kernel_size *= conv_conf.kernel_size(i);
+    output_size *= len;
+  }
+  *output_shape_ptr = Shape(output_shape_vec);
+  *colbuf_shape_ptr = Shape({batch_size, output_size, c_i * kernel_size}); 
   Shape* weight = GetShapePtr4BnInOp(model_bns().at(0));
   Shape* bias  = GetShapePtr4BnInOp(model_bns().at(1));
   Shape* biasmult_shape_ptr = GetShapePtr4BnInOp(model_tmp_bns().at(0));
-  *weight = Shape({c_o, c_i * kernel.first * kernel.second});
+  *weight = Shape({c_o, c_i * kernel_size});
   *bias = Shape({c_o});
-  *biasmult_shape_ptr = Shape({h_o * w_o});
-  */
+  *biasmult_shape_ptr = Shape({output_size});
 }
 
 REGISTER_OP(OperatorConf::kConvolutionConf, ConvolutionOp);
