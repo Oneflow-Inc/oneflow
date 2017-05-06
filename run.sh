62783ca3c6f65e2c57e6c6699711aa306d9af41d
willzhang4a58 <willzhang4a58@gmail.com>
2017-05-06 16:19:51 +0800

    add option 4 run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 rm -rf ./log ./core.*
 mkdir log
 GLOG_logtostderr=1 GLOG_log_dir=./log ./compiler -job_conf_filepath=../prototxt/job_conf.prototxt
-for dot_file in `ls ./log/*.dot`
-do
-  #echo "process ${dot_file}"
-  dot -Tpng -O $dot_file
-done
+
+if [ $1 = "--withdot" ]
+then
+  for dot_file in `ls ./log/*.dot`
+  do
+    #echo "process ${dot_file}"
+    dot -Tpng -O $dot_file
+  done
+fi
