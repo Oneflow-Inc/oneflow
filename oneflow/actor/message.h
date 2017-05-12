3b63814b5b3e2d418567e300e512b085e65f7542
chengcheng <472491134@qq.com>
2017-05-12 19:32:51 +0800

    message.h

---
+++ oneflow/actor/message.h
@@ -0,0 +1,16 @@
+#ifndef ONEFLOW_ACTOR_MESSAGE_H_
+#define ONEFLOW_ACTOR_MESSAGE_H_
+
+#include <stdint.h>
+
+namespace oneflow {
+
+struct Message {
+  uint64_t from_actor_id;
+  uint64_t to_actor_id;
+  uint64_t register_id;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_ACTOR_MESSAGE_H_
