c10bcbd932fc2a24cfeced7e9774544bc9bc2cf4
LeGend-AI <legend-ai@outlook.com>
2017-05-13 17:08:56 +0800

    block queue when empty

--- oneflow/common/blocking_queue.cpp
+++ oneflow/common/blocking_queue.cpp
@@ -4,7 +4,7 @@ namespace oneflow {
 
 bool BlockingQueue::Write(const Message& msg) {
   std::unique_lock<std::mutex> lck(mtx_);
-  write_cond_.wait(lck);
+  write_cond_.wait(lck, []() { return is_closed_ == true; });
   CHECK_NE(is_closed_, false);
   msgs_.emplace(msg);
   read_cond_.notify_one();
@@ -13,7 +13,8 @@ bool BlockingQueue::Write(const Message& msg) {
 
 bool BlockingQueue::Read(Message* msg) {
   std::unique_lock<std::mutex> lck(mtx_);
-  read_cond_.wait(lck);
+  read_cond_.wait(lck, [this](){ 
+      return !this->msgs_.empty() || is_closed == true });
   CHECK_NE(is_closed_, false);
   *msg = msgs_.front();
   msgs_.pop();
