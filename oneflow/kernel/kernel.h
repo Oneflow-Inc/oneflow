792ab2a58d71257220b9d80831c31095762a36e9
chengcheng <472491134@qq.com>
2017-05-13 10:55:49 +0800

    kernel.h

---
+++ oneflow/kernel/kernel.h
@@ -0,0 +1,27 @@
+#ifndef ONEFLOW_KERNEL_KERNEL_H_
+#define ONEFLOW_KERNEL_KERNEL_H_
+
+#include <memory>
+#include <functional>
+#include "operator/operator.h"
+#include "operator/operator.pb.h"
+
+namespace oneflow {
+
+class Kernel {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(Kernel);
+  Kernel() = default;
+  ~Kernel() = default;
+
+  virtual void InitFromOpProto(const OperatorProto& op_proto) = 0;
+  virtual void Forward(std::function<void()>) = 0;
+  virtual void Backward(std::function<void()>) = 0;
+
+ private:
+  std::unique_ptr<Operator> op_;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_KERNEL_KERNEL_H_
