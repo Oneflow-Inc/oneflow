fdf00cc0a76c2ef44183991608262e64c3942d47
Wind5 <1163643073@qq.com>
2017-05-13 16:58:41 +0800

    bug fix

--- oneflow/memory/memory_manager.h
+++ oneflow/memory/memory_manager.h
@@ -1,5 +1,5 @@
-#ifndef ONEFLOW_JOB_ID_MANAGER_H_
-#define ONEFLOW_JOB_ID_MANAGER_H_
+#ifndef ONEFLOW_MEMORY_MEMORY_MANAGER_H_
+#define ONEFLOW_MEMORY_MEMORY_MANAGER_H_
 
 #include "cuda.h"
 #include "common/util.h"
@@ -9,14 +9,13 @@ namespace oneflow {
 enum class MemoryType {
   kHostPageableMemory = 0,
   kHostPinnedMemory,
-  kGPUDeviceMemory
+  kDeviceGPUMemory
 };
 struct MemoryCase {
   MemoryType type;
   int32_t id;
 };
 
-template<typename Dptr>
 class MemoryMgr final {
  public:
   OF_DISALLOW_COPY_AND_MOVE(MemoryMgr);
@@ -28,38 +27,39 @@ class MemoryMgr final {
     return obj;
   }
 
-  std::pair<Dptr*, std::function<void(Dptr*)>> AllocateMem(
+  std::pair<void*, std::function<void(void*)>> AllocateMem(
       MemoryCase mem_cas,std::size_t size) {
     switch(mem_cas.type) {
       case MemoryType::kHostPageableMemory : {
-                                               dptr = (Dptr*) malloc(size);
+                                               dptr = (void*) malloc(size);
+                                               CHECK_NE(dptr, NULL);
                                                break;
                                              }
       case MemoryType::kHostPinnedMemory : {
                                             CHECK_EQ(cudaMallocHost(&dptr, size), 0);
                                              break;
                                            }
-      case MemoryType::kGPUDeviceMemory : {
+      case MemoryType::kDeviceGPUMemory : {
                                             CHECK_EQ(cudaSetDevice(mem_cas.id), 0);
                                             CHECK_EQ(cudaMalloc(&dptr, size), 0);
                                             break;
                                           }
     }
-    return std::make_pair(dptr, std::bind(&MemoryMgr::DeallocateMem, this, _1, mem_cas));
+    return {dptr, std::bind(&MemoryMgr::DeallocateMem, this, _1, mem_cas)};
   }
 
  private:
-  void DeallocateMem(Dptr* dptr, MemoryCase mem_cas) {
+  void DeallocateMem(void* dptr, MemoryCase mem_cas) {
     switch(mem_cas.type) {
       case MemoryType::kHostPageableMemory : {
-                                               dptr = free(dptr);
+                                               free(dptr);
                                                break;
                                              }
       case MemoryType::kHostPinnedMemory : {
                                              CHECK_EQ(cudaFreeHost(&dptr), 0);
                                              break;
                                            }
-      case MemoryType::kGPUDeviceMemory : {
+      case MemoryType::kDeviceGPUMemory : {
                                             CHECK_EQ(cudaSetDevice(mem_cas.id), 0);
                                             CHECK_EQ(cudaFree(&dptr), 0);
                                             break;
