c7b3c7fc63046d6911696accd02d1f4df8a9c5cb
xswang <2012wxs@gmail.com>
2017-05-15 15:13:06 +0800

    add: Serivice Method

--- oneflow/distributed_runtime/grpc_init_service_impl.h
+++ oneflow/distributed_runtime/grpc_init_service_impl.h
@@ -44,26 +44,45 @@ class InitService GRPC_FINAL {
 
     };
 
-   class Stub GRPC_FINAL : public StubInterface {
-     public:
-       Stub(const std::shared_ptr<::grpc::ChannelInterface>& channel);
-       ::grpc::Status exchange_machine_id(::grpc::ClientContext* context,
+    class Stub GRPC_FINAL : public StubInterface {
+      public:
+        Stub(const std::shared_ptr<::grpc::ChannelInterface>& channel);
+        ::grpc::Status exchange_machine_id(::grpc::ClientContext* context,
                                           const ::oneflow::Node& request,
                                           ::oneflow::Node* response) GRPC_OVERRIDE;
-       ::grpc::Status exchange_memory_desc(::grpc::ClientContext* context,
+        ::grpc::Status exchange_memory_desc(::grpc::ClientContext* context,
                                           const ::oneflow::mem_desc& request,
                                           ::oneflow::mem_desc* response) GRPC_OVERRIDE;
 
-     private:
-       std::shared_ptr<::grpc::ChannelInterface> channel_;
-       const ::grpc::RpcMethod rpcmethod_exchange_machine_id_;
-       const ::grpc::RpcMethod rpcmethod_exchange_memory_desc_;
-   } 
+      private:
+        std::shared_ptr<::grpc::ChannelInterface> channel_;
+        const ::grpc::RpcMethod rpcmethod_exchange_machine_id_;
+        const ::grpc::RpcMethod rpcmethod_exchange_memory_desc_;
+    }; 
 
-   static std::unique_str<Stub> NewStub(
-       const std::shared_ptr< ::grpc::ChannelInterface>& channel,
-       const ::grpc::StubOptions& options = ::grpc::StubOptions());
+    static std::unique_str<Stub> NewStub(
+        const std::shared_ptr< ::grpc::ChannelInterface>& channel,
+        const ::grpc::StubOptions& options = ::grpc::StubOptions());
   
+    class Service : public ::grpc::Service {
+      public:
+        Service();
+        virtual ~Service();
+
+        void Requestexchange_machine_id(::grpc::ServerContext* context, ::oneflow::    Node* request,
+            ::grpc::ServerAsyncResponseWriter< ::oneflow::Node>* response,
+            ::grpc::CompletionQueue* new_call_cq,
+            ::grpc::ServerCompletionQueue* notification_cq, void *tag) {
+          ::grpc::Service::RequestAsyncUnary(0, context, request, response, new_call_cq, notification_cq, tag);
+        }
+
+        void Requestexchange_memory_desc(::grpc::ServerContext* context, ::oneflow::mem_desc* request,
+            ::grpc::ServerAsyncResponseWriter< ::oneflow::mem_desc>* response, ::grpc::CompletionQueue* new_call_cq,
+            ::grpc::ServerCompletionQueue* notification_cq, void *tag) {
+          ::grpc::Service::RequestAsyncUnary(1, context, request, response, new_call_cq, notification_cq, tag);
+        }
+    };
+
 
 
 };
