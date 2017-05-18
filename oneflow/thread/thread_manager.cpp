8063ad0a47b631f4a8bb89cf476f3e8459554038
LeGend-AI <legend-ai@outlook.com>
2017-05-18 18:31:02 +0800

    use range-based for-loop

--- oneflow/thread/thread_manager.cpp
+++ oneflow/thread/thread_manager.cpp
@@ -9,17 +9,17 @@ Channel<ActorMsg>* ThreadMgr::GetMsgChanFromThrdLocId(uint64_t thrd_loc_id) {
 }
 
 void ThreadMgr::InitFromProto(const PbRpf<TaskProto>& tasks) {
-  for (auto it = tasks.begin(); it != tasks.end(); ++it) {
-    if (it->machine_id() != RuntimeInfo::Singleton().this_machine_id()) {
+  for (auto task : tasks) {
+    if (task.machine_id() != RuntimeInfo::Singleton().this_machine_id()) {
       continue;
     }
-    if (thrd_loc_id2thread_.find(it->thrd_local_id()) == thrd_loc_id2thread_.end()) {
+    if (thrd_loc_id2thread_.find(task.thrd_local_id()) == thrd_loc_id2thread_.end()) {
       auto thrd = of_make_unique<Thread>();
-      thrd->set_thrd_loc_id(it->thrd_local_id());
-      CHECK(thrd_loc_id2thread_.emplace(it->thrd_local_id(), std::move(thrd)).second);
+      thrd->set_thrd_loc_id(task.thrd_local_id());
+      CHECK(thrd_loc_id2thread_.emplace(task.thrd_local_id(), std::move(thrd)).second);
     }
-    auto& thrd = thrd_loc_id2thread_.at(it->thrd_local_id());
-    thrd->AddActor(*it);
+    auto& thrd = thrd_loc_id2thread_.at(task.thrd_local_id());
+    thrd->AddActor(task);
   }
 }
 
