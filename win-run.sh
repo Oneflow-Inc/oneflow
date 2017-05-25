26d0b16daf28b13995dbf8538e8725e3085f44b8
chengcheng <472491134@qq.com>
2017-05-25 17:29:53 +0800

    add win-run.sh in windows

---
+++ win-run.sh
@@ -0,0 +1,28 @@
+# begin
+
+set -e
+rm -rf ./log ./log.tar.gz ./core.*
+
+# compiler
+mkdir "log"
+mkdir "log/dot"
+mkdir "log/dot/data"
+mkdir "log/dot/model"
+mkdir "log/dot/exec"
+GLOG_logtostderr=0 GLOG_log_dir=./log ./Debug/compiler.exe \
+    -job_conf_filepath=../prototxt/multi_machine_job.prototxt \
+    -elf_filepath=./elf
+
+if [ "$1" = "--withdot" ]
+then
+  for dot_file in `find . -name *.dot`
+  do
+    echo "process $dot_file"
+    dot -Tpng -O $dot_file && rm $dot_file
+  done
+fi
+
+#GLOG_logtostderr=1 GLOG_log_dir=./log ./elf_runner -elf_filepath=./elf -this_machine_name="centos-0"
+
+# end
+tar zcf log.tar.gz log/
