db7a4931021674d0312ccaab4ca1d91e0d73217c
jiyuan <jiyuan>
2017-05-28 11:16:08 +0800

    works on windows

--- cmake/third_party.cmake
+++ cmake/third_party.cmake
@@ -20,6 +20,13 @@ set(oneflow_third_party_libs
     ${GOOGLETEST_STATIC_LIBRARIES}
     ${PROTOBUF_STATIC_LIBRARIES}
     ${GRPC_STATIC_LIBRARIES}
+    ${tensorflow_STATIC_LIBRARIES}
+    ${gif_STATIC_LIBRARIES}
+    ${farmhash_STATIC_LIBRARIES}
+    ${highwayhash_STATIC_LIBRARIES}
+    ${JPEG_STATIC_LIBRARIES}
+    ${PNG_STATIC_LIBRARIES}
+    ${JSONCPP_STATIC_LIBRARIES}
 )
 
 if(WIN32)
@@ -41,7 +48,21 @@ set(oneflow_third_party_dependencies
   protobuf_copy_binary_to_destination
   grpc_copy_headers_to_destination
   grpc_copy_libs_to_destination
-  tensorflow
+  tensorflow_copy_headers_to_destination
+  tensorflow_copy_libs_to_destination
+  gif_copy_headers_to_destination
+  gif_copy_libs_to_destination
+  farmhash_copy_headers_to_destination
+  farmhash_copy_libs_to_destination
+  highwayhash_copy_headers_to_destination
+  highwayhash_copy_libs_to_destination
+  jpeg_copy_headers_to_destination
+  jpeg_copy_libs_to_destination
+  png_copy_headers_to_destination
+  png_copy_libs_to_destination
+  jsoncpp_copy_headers_to_destination
+  jsoncpp_copy_libs_to_destination
+  eigen_copy_headers_dir
 )
 
 include_directories(
@@ -51,4 +72,12 @@ include_directories(
     ${GOOGLETEST_INCLUDE_DIR}
     ${PROTOBUF_INCLUDE_DIR}
     ${GRPC_INCLUDE_DIR}
+    ${TENSORFLOW_INCLUDE_DIR}
+    ${GIF_INCLUDE_DIR}
+    ${FARMHASH_INCLUDE_DIR}
+    ${HIGHWAYHASH_INCLUDE_DIR}
+    ${JPEG_INCLUDE_DIR}
+    ${PNG_INCLUDE_DIR}
+    ${JSONCPP_INCLUDE_DIR}
+    ${EIGEN_INCLUDE_DIRS}
 )
