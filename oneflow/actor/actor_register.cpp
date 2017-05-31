1373a4e33b4ef8c10a8d54b996036fb55f53bc8e
chengcheng <472491134@qq.com>
2017-05-31 12:17:31 +0800

    actor register

---
+++ oneflow/actor/actor_register.cpp
@@ -0,0 +1,39 @@
+#include "actor/actor_register.h"
+#include "glog/logging.h"
+
+namespace oneflow {
+
+namespace {
+
+struct pair_hash {
+  std::size_t operator () (const std::pair<int, bool> &p) const {
+    auto h1 = std::hash<int>{}(p.first);
+    auto h2 = std::hash<bool>{}(p.second);
+    return (h1 << 1) | h2;
+  }
+};
+
+using ActorTypePair = std::pair<TaskType, bool>;
+using ActorCreatorMap = HashMap<ActorTypePair, std::function<Actor*()>, pair_hash>;
+
+ActorCreatorMap& ActorType2Creator() {
+  static ActorCreatorMap obj;
+  return obj;
+}
+
+}
+
+void AddActorCreator(TaskType task_type, bool is_forward,
+                     std::function<Actor*()> creator) {
+  CHECK(ActorType2Creator().emplace(std::make_pair(task_type, is_forward), creator).second);
+}
+
+std::shared_ptr<Actor> ConstructActor(const TaskProto& task_proto) {
+  ActorTypePair actor_type_pair = std::make_pair(task_proto.type(),
+                                                 task_proto.is_forward());
+  std::shared_ptr<Actor> ret(ActorType2Creator().at(actor_type_pair)());
+  ret->Init(task_proto);
+  return ret;
+}
+
+}  // namespace oneflow
---
+++ oneflow/actor/actor_register.h
@@ -0,0 +1,26 @@
+#ifndef ONEFLOW_ACTOR_ACTOR_REGISTER_H_
+#define ONEFLOW_ACTOR_ACTOR_REGISTER_H_
+
+#include "actor/actor.h"
+#include "common/task.pb.h"
+
+namespace oneflow {
+
+std::shared_ptr<Actor> ConstructActor(const TaskProto&);
+
+void AddActorCreator(TaskType task_type, bool is_forward, 
+                     std::function<Actor*()> creator);
+
+template<TaskType task_type, bool is_forward, typename ActorType>
+struct ActorRegister {
+  ActorRegister() {
+    AddActorCreator(task_type, is_forward, []() { return new ActorType });
+  }
+};
+
+#define REGISTER_ACTOR(TaskType, IsForward, ActorType) \
+  static ActorRegister<TaskType, IsForward, ActorType) g_##ActorType##_register_var;
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_ACTOR_ACTOR_REGISTER_H_
