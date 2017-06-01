b66dfacaf12ab176df34318ff3686deb53d0588e
LeGend-AI <legend-ai@outlook.com>
2017-06-01 18:04:28 +0800

    implement RegstMgr and RtRgstDesc

--- oneflow/register/register_manager.cpp
+++ oneflow/register/register_manager.cpp
@@ -2,11 +2,23 @@
 
 namespace oneflow {
 
-void NewRegsts(const RegstDescProto& regst_desc_proto,
+void RegstMgr::NewRegsts(const RegstDescProto& regst_desc_proto,
                std::function<void(Regst*)> OneRegstDone) {
   // One RegstDesc means Multi Regst
   // All Regst has a shared_ptr point to the same RtRegstDesc obj
   // Call OneRegstDone for each regst
+  std::shared_ptr<RtRegstDesc> runtime_regst_desc(new RtRegstDesc(regst_desc_proto));
+  for (size_t i = 0; i < regst_desc_proto.register_num(); ++i) {
+    Regst* regst = new Regst;
+    regst->regst_desc_ = runtime_regst_desc;
+    regst->regst_id_ = i;
+    for (const auto& pair : regst_desc_proto.lbn2shape()) {
+      Shape* shape_ptr = runtime_regst_desc->GetShapePtrFromLbn(pair.first);
+      regst->lbn2blob_.emplace(pair.first, 
+          of_make_unique<Blob>(new char[shape_ptr->elem_cnt()], shape_ptr));
+    }
+    OneRegstDone(regst);
+  }
 }
 
 }
--- oneflow/register/runtime_register_desc.h
+++ oneflow/register/runtime_register_desc.h
@@ -2,6 +2,8 @@
 #define ONEFLOW_REGISTER_RUNTIME_REGISTER_DESC_H_
 
 #include "common/util.h"
+#include "common/protobuf.h"
+#include "common/shape.h"
 #include "memory/memory_case.pb.h"
 #include "register/register_desc.pb.h"
 
@@ -13,9 +15,31 @@ class RtRegstDesc {
   RtRegstDesc() = delete;
   ~RtRegstDesc() = default;
 
-  RtRegstDesc(const RegstDescProto&) { TODO(); }
+  RtRegstDesc(const RegstDescProto& regst_desc_proto) {
+    regst_desc_id_ = regst_desc_proto.regst_desc_id();
+    producer_task_id_ = regst_desc_proto.producer_task_id();
+    register_num_ = regst_desc_proto.register_num();
 
-  // TODO: Add Getter
+    const auto& subscriber = regst_desc_proto.subscriber_task_id();
+    subscribers_task_id_ = std::vector<uint64_t>(subscriber.begin(), subscriber.end());
+
+    for (const auto& pair : regst_desc_proto.lbn2shape()) {
+      lbn2shape_.emplace(pair.first, of_make_unique<Shape>(pair.second));
+    }
+    mem_case_ = regst_desc_proto.mem_case();
+  }
+
+  uint64_t regst_desc_id() const { return regst_desc_id_; }
+  uint64_t producer_task_id() const { return producer_task_id_; }
+  std::vector<uint64_t>& subscribers_task_id() { 
+    return subscribers_task_id_;
+  }
+  int64_t register_num() const { return register_num_; }
+  MemoryCase mem_case() const { return mem_case_; }
+
+  Shape* GetShapePtrFromLbn(const std::string& lbn) {
+    return lbn2shape_.at(lbn).get();
+  }
 
  private:
   uint64_t regst_desc_id_;
