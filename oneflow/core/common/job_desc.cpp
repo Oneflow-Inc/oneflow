eeb3c45c5ab788e50452ff833239388a5ea491cb
willzhang4a58 <willzhang4a58@gmail.com>
2017-06-07 13:39:01 +0800

    model_load_snapshot_path and model_save_snapshots_path

--- oneflow/core/common/job_desc.cpp
+++ oneflow/core/common/job_desc.cpp
@@ -8,8 +8,8 @@ void JobDesc::InitFromJobConf(const JobConf& conf) {
   ParseProtoFromTextFile(conf.train_dlnet_conf_filepath(), &train_dlnet_conf_);
   ParseProtoFromTextFile(conf.resource_filepath(), &resource_);
   ParseProtoFromTextFile(conf.strategy_filepath(), &strategy_);
-  md_load_machine_ = conf.model_load_machine();
-  md_save_machine_ = conf.model_save_machine();
+  md_load_snapshot_path_ = conf.model_load_snapshot_path();
+  md_save_snapshots_path_ = conf.model_save_snapshots_path();
   batch_size_ = conf.batch_size();
   piece_size_ = conf.piece_size();
   is_train_ = conf.is_train();
@@ -21,8 +21,8 @@ void JobDesc::InitFromProto(const JobDescProto& proto) {
   train_dlnet_conf_ = proto.train_dlnet_conf();
   resource_ = proto.resource();
   strategy_ = proto.strategy();
-  md_load_machine_ = proto.model_load_machine();
-  md_save_machine_ = proto.model_save_machine();
+  md_load_snapshot_path_ = proto.model_load_snapshot_path();
+  md_save_snapshots_path_ = proto.model_save_snapshots_path();
   batch_size_ = proto.batch_size();
   piece_size_ = proto.piece_size();
   is_train_ = proto.is_train();
@@ -33,8 +33,8 @@ void JobDesc::ToProto(JobDescProto* proto) const {
   *(proto->mutable_train_dlnet_conf()) = train_dlnet_conf_;
   *(proto->mutable_resource()) = resource_;
   *(proto->mutable_strategy()) = strategy_;
-  *(proto->mutable_model_load_machine()) = md_load_machine_;
-  *(proto->mutable_model_save_machine()) = md_save_machine_;
+  *(proto->mutable_model_load_snapshot_path()) = md_load_snapshot_path_;
+  *(proto->mutable_model_save_snapshots_path()) = md_save_snapshots_path_;
   proto->set_batch_size(batch_size_);
   proto->set_piece_size(piece_size_);
   proto->set_is_train(is_train_);
--- oneflow/core/common/job_desc.h
+++ oneflow/core/common/job_desc.h
@@ -28,8 +28,8 @@ class JobDesc final {
   const FloatingPointType& floating_point_type() const { 
     return floating_point_type_; 
   }
-  const std::string& md_load_machine() { return md_load_machine_; }
-  const std::string& md_save_machine() { return md_save_machine_; }
+  const std::string& md_load_snapshot_path() { return md_load_snapshot_path_; }
+  const std::string& md_save_snapshots_path() { return md_save_snapshots_path_; }
   bool is_train() const { return is_train_; }
 
   uint32_t piece_size() const { return piece_size_; }
@@ -41,8 +41,8 @@ class JobDesc final {
   DLNetConf train_dlnet_conf_;
   Resource resource_;
   Strategy strategy_;
-  std::string md_load_machine_;
-  std::string md_save_machine_;
+  std::string md_load_snapshot_path_;
+  std::string md_save_snapshots_path_;
   uint32_t batch_size_;
   uint32_t piece_size_;
   bool is_train_;
