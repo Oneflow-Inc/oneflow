aba46b3e104c01e79f1ab80fa72c9eee2ae60e64
LeGend-AI <legend-ai@outlook.com>
2017-06-08 15:27:01 +0800

    implement CopyCommNetActor

--- oneflow/core/actor/copy_comm_net_actor.cpp
+++ oneflow/core/actor/copy_comm_net_actor.cpp
@@ -1,15 +1,36 @@
 #include "oneflow/core/actor/copy_comm_net_actor.h"
 #include "oneflow/core/actor/actor_registry.h"
+#include "oneflow/core/register/local_register_warpper.h"
 
 namespace oneflow {
 
 void CopyCommNetActor::Init(const TaskProto& task_proto) {
-  TODO();
+  Actor::Init(task_proto);
 }
 
-void CopyCommNetActor::ProcessMsg(const ActorMsg& actor_msg,
+void CopyCommNetActor::ProcessMsg(const ActorMsg& msg,
                                   const ThreadContext&) {
-  TODO();
+  KernelContext kernel_ctx;
+  if (TryOneReadDone(msg.regst_warpper()->regst_raw_ptr()) != 0) {
+    waiting_in_regst_.push(std::move(msg.regst_warpper()));
+  }
+  if (!waiting_in_regst_.empty() && IsWriteReady()) {
+    WardKernel(kernel_ctx, [this](uint64_t regst_desc_id) -> std::shared_ptr<RegstWarpper> {
+      Regst* regst = GetCurWriteableRegst(regst_desc_id);
+      if (regst == nullptr) {
+        CHECK_EQ(regst_desc_id, waiting_in_regst_.front()->regst_desc_id());
+        return waiting_in_regst_.front();
+      } else {
+        return std::make_shared<LocalRegstWarpper> (regst);
+      }
+    });
+    CurWriteDone();
+    std::shared_ptr<RegstWarpper> regst = waiting_in_regst_.front();
+    ActorMsgBus::Singleton().SendMsg(ActorMsg::BuildMsgForRegstWriter(
+          regst->producer_actor_id(),
+          regst->regst_raw_ptr()));
+    waiting_in_regst_.pop();
+  }
 }
 
 REGISTER_ACTOR(kCopyCommNetTask, true, CopyCommNetActor);
