8ebad377ad1ac852d305e540cc2cd61a1dc4cab5
LeGend-AI <legend-ai@outlook.com>
2017-06-10 15:30:09 +0800

    ModelDiffAccOp

---
+++ oneflow/core/operator/model_diff_accumulate_op.cpp
@@ -0,0 +1,23 @@
+#include "oneflow/core/operator/model_diff_accumulate_op.h"
+#include "glog/logging.h"
+#include "oneflow/core/operator/operator_manager.h"
+
+namespace oneflow {
+
+void ModelDiffAccOp::InitFromOpConf(const OperatorConf& op_conf) {
+  CHECK(op_conf.has_model_update_conf());
+  mut_op_conf() = op_conf;
+
+  model_diff_num_ = op_conf.model_diff_acc_conf().model_diff_num();
+  
+  EnrollInputBn("model_diff", false);
+  EnrollOutputBn("model_diff_acc", false);
+}
+
+const PbMessage& ModelUpdateOp::GetSpecialConf() const {
+  return op_conf().model_update_conf();
+}
+
+REGISTER_OP(OperatorConf::kModelDiffAccConf, ModelDiffAccOp);
+
+}
