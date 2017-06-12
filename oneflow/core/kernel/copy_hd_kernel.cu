0a3addc634596d74dfdda1d01c8b50a28138279a
syshangguan <shgshy@gmail.com>
2017-06-12 18:40:38 +0800

    add CHECK and abstraction

--- oneflow/core/kernel/copy_hd_kernel.cu
+++ oneflow/core/kernel/copy_hd_kernel.cu
@@ -4,6 +4,30 @@
 
 namespace oneflow {
 
+namespace {
+
+void CopyH2DAsync(Blob* in_blob, Blob* out_blob,
+                  const KernelContext& ctx, size_t type_size) {
+  CHECK_EQ(cudaMemcpyAsync(out_blob->mut_dptr(),
+                           in_blob->dptr(),
+                           in_blob->shape().elem_cnt() * type_size,
+                           cudaMemcpyHostToDevice,
+                           *(ctx.cuda_stream)),
+           cudaSuccess);
+}
+
+void CopyD2HAsync(Blob* in_blob, Blob* out_blob,
+                  const KernelContext& ctx, size_t type_size) {
+  CHECK_EQ(cudaMemcpyAsync(out_blob->mut_dptr(),
+                           in_blob->dptr(),
+                           in_blob->shape().elem_cnt() * type_size,
+                           cudaMemcpyHostToDevice,
+                           *(ctx.cuda_stream)),
+           cudaSuccess);
+}
+
+}  // namespace
+
 template<typename floating_point_type>
 void CopyHdKernel<DeviceType::kGPU, floating_point_type>::Forward(
     const KernelContext& ctx,
@@ -12,19 +36,14 @@ void CopyHdKernel<DeviceType::kGPU, floating_point_type>::Forward(
   Blob* in_blob = BnInOp2BlobPtr(ibn);
   const std::string& obn = op()->SoleObn();
   Blob* out_blob = BnInOp2BlobPtr(obn);
+  size_t type_size = sizeof(floating_point_type);
 
   const CopyHdOpConf& copy_hd_conf = op()->op_conf().copy_hd_conf();
 
   if (copy_hd_conf.type() == copy_hd_conf.H2D) {
-    cudaMemcpyAsync(out_blob->mut_dptr(), in_blob->dptr(),
-                    in_blob->shape().elem_cnt()*sizeof(floating_point_type),
-                    cudaMemcpyHostToDevice,
-                    *(ctx.cuda_stream));
+    CopyH2DAsync(in_blob, out_blob, ctx, type_size);
   } else {
-    cudaMemcpyAsync(out_blob->mut_dptr(), in_blob->dptr(),
-                    in_blob->shape().elem_cnt()*sizeof(floating_point_type),
-                    cudaMemcpyDeviceToHost,
-                    *(ctx.cuda_stream));
+    CopyD2HAsync(in_blob, out_blob, ctx, type_size);
   }
 }
 
@@ -36,19 +55,14 @@ void CopyHdKernel<DeviceType::kGPU, floating_point_type>::Backward(
   Blob* in_blob = BnInOp2BlobPtr(odbn);
   const std::string& idbn = op()->SoleIdbn();
   Blob* out_blob = BnInOp2BlobPtr(idbn);
+  size_t type_size = sizeof(floating_point_type);
 
   const CopyHdOpConf& copy_hd_conf = op()->op_conf().copy_hd_conf();
 
   if (copy_hd_conf.type() == copy_hd_conf.H2D) {
-    cudaMemcpyAsync(out_blob->mut_dptr(), in_blob->dptr(),
-                    in_blob->shape().elem_cnt()*sizeof(floating_point_type),
-                    cudaMemcpyHostToDevice,
-                    *(ctx.cuda_stream));
+    CopyH2DAsync(in_blob, out_blob, ctx, type_size);
   } else {
-    cudaMemcpyAsync(out_blob->mut_dptr(), in_blob->dptr(),
-                    in_blob->shape().elem_cnt()*sizeof(floating_point_type),
-                    cudaMemcpyDeviceToHost,
-                    *(ctx.cuda_stream));
+    CopyD2HAsync(in_blob, out_blob, ctx, type_size);
   }
 }
 
