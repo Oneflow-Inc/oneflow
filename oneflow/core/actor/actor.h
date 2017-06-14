2059b1a3ee6afff075f739a77b74621c3580c9c2
willzhang4a58 <willzhang4a58@gmail.com>
2017-06-14 17:17:52 +0800

    delete device_ctx getter

--- oneflow/core/actor/actor.h
+++ oneflow/core/actor/actor.h
@@ -43,7 +43,6 @@ class Actor {
     return name2regst_desc_id_.at(name);
   }
 
-  const DeviceCtx* device_ctx() const { return device_ctx_.get(); }
   std::unique_ptr<DeviceCtx>& mut_device_ctx() { return device_ctx_; }
   KernelCtx GenDefaultKernelCtx() const;
 
