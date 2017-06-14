e896219683cc0d8e1fd8b5e8d553e69fd6857c90
xswang <2012wxs@gmail.com>
2017-06-14 16:20:45 +0800

    update: cpplint.py

---
+++ oneflow/core/distributed_runtime/grpc_master_service_impl.h
@@ -0,0 +1,78 @@
+#ifndef ONEFLOW_CORE_DISTRIBUTED_RUNTIME_GRPC_MASTER_SERVICE_IMPL_H_
+#define ONEFLOW_CORE_DISTRIBUTED_RUNTIME_GRPC_MASTER_SERVICE_IMPL_H_
+
+#include <grpc++/impl/codegen/async_stream.h>
+#include <grpc++/impl/codegen/async_unary_call.h>
+#include <grpc++/impl/codegen/proto_utils.h>
+#include <grpc++/impl/codegen/rpc_method.h>
+#include <grpc++/impl/codegen/service_type.h>
+#include <grpc++/impl/codegen/status.h>
+#include <grpc++/impl/codegen/stub_options.h>
+#include <grpc++/impl/codegen/sync_stream.h>
+
+#include <memory>
+
+#include "distributed_runtime/grpc_serialization_traits.h"
+#include "distributed_runtime/master_service.pb.h"
+
+// OF_GRPC_ALLOW_UNLIMITED_MESSAGE_SIZE(oneflow::SendGraphRequest);
+
+namespace grpc {
+class CompletionQueue;
+class Channel;
+class RpcService;
+class ServerCompletionQueue;
+class ServerContext;
+}
+
+namespace oneflow {
+
+enum class GrpcMasterMethod {
+  kSendGraph
+};
+
+static const int kGrpcNumMasterMethods =
+  static_cast<int> (GrpcMasterMethod::kSendGraph) + 1;
+
+const char* GrpcMasterMethodName(GrpcMasterMethod id);
+
+namespace grpc {
+
+class MasterService GRPC_FINAL {
+ public:
+  class StubInterface {
+   public:
+    virtual ~StubInterface() {}
+    virtual ::grpc::Status SendGraph(::grpc::ClientContext* context,
+                                     const SendGraphRequest& request,
+                                     SendGraphResponse* response) = 0;
+  };  // Stubinterface
+
+  class Stub GRPC_FINAL : public StubInterface {
+   public:
+    Stub(const std::shared_ptr<::grpc::ChannelInterface>& channel);
+    ::grpc::Status SendGraphSync(::grpc::ClientContext* context,
+                                 const SendGraphRequest& request,
+                                 SendGraphResponse* response) GRPC_OVERRIDE;
+   private:
+    std::shared_ptr<::grpc::ChannelInterface> channel_;
+    const ::grpc::RpcMethod rpcmethod_SendGraph_;
+  };  // Stub
+
+  static std::unique_ptr<Stub> NewStub(
+      const std::shared_ptr<::grpc::ChannelInterface>& channel);
+
+  class AsyncService : public ::grpc::Service {
+   public:
+    AsyncService();
+    virtual ~AsyncService();
+    using ::grpc::Service::RequestAsyncUnary;
+  };  // Asyncservice
+};  // Masterservice
+
+}  // namespace grpc
+
+}  // namespace oneflow
+
+
+#endif  // ONEFLOW_CORE_DISTRIBUTED_RUNTIME_GRPC_MASTER_SERVICE_IMPL_H_
