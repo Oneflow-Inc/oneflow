e46b1632fd008dfce4d4cffe5319d78276ab9722
xswang <2012wxs@gmail.com>
2017-06-14 21:24:55 +0800

    add: grpc_remote_master.h for client Sync call

---
+++ oneflow/core/distributed_runtime/grpc_remote_master.h
@@ -0,0 +1,35 @@
+#ifndef ONEFLOW_CORE_DISTRIBUTED_RUNTIME_GRPC_REMOTE_MASTER_H_
+#define ONEFLOW_CORE_DISTRIBUTED_RUNTIME_GRPC_REMOTE_MASTER_H_
+
+#include <memory>
+
+#include "grpc++/grpc++.h"
+
+#include "oneflow/core/distributed_runtime/grpc_master_service_impl.h"
+#include "oneflow/core/distributed_runtime/master_service.pb.h"
+#include "tensorflow/core/lib/core/status.h"
+
+namespace oneflow {
+
+class GrpcRemoteMaster {
+ public:
+  explicit GrpcRemoteMaster(std::shared_ptr<::grpc::Channel> client_channel)
+      : stub_(grpc::MasterService::NewStub(client_channel)) {}
+
+  ~GrpcRemoteMaster() override {}
+
+  Status SendGraphSync(const SendGraphRequest* request,
+                       SendGraphResponse* response) override {
+    ::grpc::ClientContext ctx;
+    ctx.set_fail_fast(false);
+    return FromGrpcStatus(stub_->SendGraphSync(&ctx, *request, response));
+  }
+
+ private:
+  std::unique_ptr<grpc::MasterService::Stub> stub_;
+};  // Grpcremotemaster
+
+}  // namespace oneflow
+
+
+#endif  // ONEFLOW_CORE_DISTRIBUTED_RUNTIME_GRPC_REMOTE_MASTER_H_
