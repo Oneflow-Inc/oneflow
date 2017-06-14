9ad7128cbf96e2906d0564ab4d96b27b3e0ad39a
xswang <2012wxs@gmail.com>
2017-06-14 16:23:07 +0800

    update: cpplint

---
+++ oneflow/core/distributed_runtime/grpc_master_service_impl.cpp
@@ -0,0 +1,58 @@
+#include "distributed_runtime/grpc_master_service_impl.h"
+
+#include "grpc++/impl/codegen/async_stream.h"
+#include "grpc++/impl/codegen/async_unary_call.h"
+#include "grpc++/impl/codegen/channel_interface.h"
+#include "grpc++/impl/codegen/client_unary_call.h"
+#include "grpc++/impl/codegen/method_handler_impl.h"
+#include "grpc++/impl/codegen/rpc_service_method.h"
+#include "grpc++/impl/codegen/service_type.h"
+#include "grpc++/impl/codegen/sync_stream.h"
+
+namespace oneflow {
+
+const char* GrpcMasterMethodName(GrpcMasterMethod id) {
+  switch (id) {
+    case GrpcMasterMethod::kSendGraph:
+      return "/oneflow.MasterService/SendGraph";
+  }
+}  // GrpcMasterMethodName
+
+namespace grpc {
+
+std::unique_ptr<MasterService::Stub> MasterService::NewStub(
+    const std::shared_ptr<::grpc::ChannelInterface>& channel) {
+  std::unique_ptr<MasterService::Stub> stub(new MasterService::Stub(channel));
+  return stub;
+}
+
+MasterService::Stub::Stub(
+    const std::shared_ptr<::grpc::ChannelInterface>& channel)
+  : channel_(channel),
+    rpcmethod_SendGraph_(GrpcMasterMethodName(static_cast<GrpcMasterMethod>(0)),
+                              ::grpc::RpcMethod::NORMAL_RPC, channel) {}
+
+::grpc::Status MasterService::Stub::SendGraphSync(
+    ::grpc::ClientContext* context, const SendGraphRequest& request,
+    SendGraphResponse* response) {
+  return ::grpc::BlockingUnaryCall(channel_.get(), rpcmethod_SendGraph_,
+                                   context, request, response);
+}
+
+MasterService::AsyncService::AsyncService() {
+  for (int i = 0; i < kGrpcNumMasterMethods; ++i) {
+    AddMethod(new ::grpc::RpcServiceMethod(
+          GrpcMasterMethodName(static_cast<GrpcMasterMethod>(i)),
+          ::grpc::RpcMethod::NORMAL_RPC,
+          nullptr));
+    ::grpc::Service::MarkMethodAsync(i);
+  }
+}
+
+MasterService::AsyncService::~AsyncService() {}
+
+}  // namespace grpc
+
+}  // namespace oneflow
+
+
