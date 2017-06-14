8be741c71f80b99053fb82edcc4abcf4ea1b4d26
chengcheng <472491134@qq.com>
2017-06-14 14:26:13 +0800

    model save actor

--- oneflow/core/actor/model_save_comp_actor.cpp
+++ oneflow/core/actor/model_save_comp_actor.cpp
@@ -5,11 +5,27 @@ namespace oneflow {
 
 // need review
 void MdSaveCompActor::Init(const TaskProto& task_proto) {
-  TODO();
+  CompActor::Init(task_proto);
+  model_regst_desc_id_ = RegstDescId4Name("model");
 }
 
 void MdSaveCompActor::ProcessMsg(const ActorMsg& actor_msg, const ThreadContext&) {
-  TODO();
+  if (actor_msg.msg_type() == ActorMsgType::kCmdMsg) {
+    CHECK(actor_msg.actor_cmd() == ActorCmd::kStop);
+    TODO();
+  } else if (actor_msg.msg_type() == ActorMsgType::kRegstMsg) {
+    auto regst_warpper = actor_msg.regst_warpper();
+    AsyncWardKernel(
+        [&](uint64_t regst_desc_id) -> std::shared_ptr<RegstWarpper> {
+      CHECK_EQ(regst_desc_id, model_regst_desc_id_);
+      return regst_warpper;
+    });
+    ActorMsgBus::Singleton().SendMsg(ActorMsg::BuildMsgForRegstWriter(
+        regst_warpper->producer_actor_id(),
+        regst_warpper->regst_raw_ptr()));
+  } else {
+    UNEXPECTED_RUN();
+  }
 }
 
 REGISTER_ACTOR(kMdSaveCompTask, true, MdSaveCompActor);
--- oneflow/core/actor/model_save_comp_actor.h
+++ oneflow/core/actor/model_save_comp_actor.h
@@ -1,12 +1,12 @@
 #ifndef ONEFLOW_CORE_ACTOR_MODEL_SAVE_COMP_ACTOR_H_
 #define ONEFLOW_CORE_ACTOR_MODEL_SAVE_COMP_ACTOR_H_
 
-#include "oneflow/core/actor/actor.h"
+#include "oneflow/core/actor/compute_actor.h"
 
 namespace oneflow {
 
-class MdSaveCompActor final : public Actor {
-public:
+class MdSaveCompActor final : public CompActor {
+ public:
   OF_DISALLOW_COPY_AND_MOVE(MdSaveCompActor);
   MdSaveCompActor() = default;
   ~MdSaveCompActor() = default;
@@ -14,8 +14,8 @@ public:
   void Init(const TaskProto&) override;
   void ProcessMsg(const ActorMsg&, const ThreadContext&) override;
 
-private:
-
+ private:
+  uint64_t model_regst_desc_id_;
 };
 
 }  // namespace oneflow
