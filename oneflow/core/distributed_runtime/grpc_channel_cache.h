bf6fc0007a5b8be841331a1459e5d282567b3b42
xswang <2012wxs@gmail.com>
2017-06-15 17:15:49 +0800

    update: create channel for one ip:port

--- oneflow/core/distributed_runtime/grpc_channel_cache.h
+++ oneflow/core/distributed_runtime/grpc_channel_cache.h
@@ -6,16 +6,26 @@
 
 #include "grpc++/grpc++.h"
 
+#include "oneflow/core/proto/cluster_config.pb.h"
+
+using ::grpc::Channel;
+using ::grpc::Server;
+using ::grpc::ServerBuilder;
+using ::grpc::ClientContext;
+
 namespace oneflow {
 
 class GrpcChannelCache {
  public:
-  GrpcChannelCache();
-  ~GrpcChannelCache(); 
+  GrpcChannelCache(oneflow::ClusterSpec cluster_spec);
+  ~GrpcChannelCache();
 
   void CreateChannelCache();
+  std::shared_ptr<::grpc::Channel> FindChannel(std::string& address);
 
   std::map<std::string, std::shared_ptr<::grpc::Channel>> channel_map_;
+
+  oneflow::ClusterSpec cluster_spec_;
 };
 
 
