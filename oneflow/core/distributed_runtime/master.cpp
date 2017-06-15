4456027f7b488f059167b9368966b191b376318d
xswang <2012wxs@gmail.com>
2017-06-15 12:14:25 +0800

    add: master.* for server to process request

---
+++ oneflow/core/distributed_runtime/master.cpp
@@ -0,0 +1,42 @@
+#include "oneflow/core/distributed_runtime/master.h"
+
+namespace oneflow {
+
+Master::Master(GrpcChannelCache* channel_cache) 
+  : channel_cache_(channel_cache) {}
+
+Master::~Master() {}
+
+void Master::SendGraph(SendGraphRequest* request,
+                       SendGraphResponse* response) {
+  /*
+  GraphCompile graph_compile;
+  graph_compile->compile();
+  */
+  oneflow::SendTaskGraphRequest task_request;
+  oneflow::SendTaskGraphResponse task_response;
+  Barrier();
+  for(auto& channel : channel_cache_->channel_map_) {
+    std::unique_ptr<::grpc::ServerCompletionQueue> cq_;
+    //remote_worker_ = new GrpcRemoteWorker(channel.second, cq_.get());
+    //remote_worker_->SendTaskGraphSync(task_request, task_response);
+  }
+}
+
+void Master::Barrier() {
+  oneflow::CheckStatusRequest request;
+  oneflow::CheckStatusResponse response;
+  int32_t worker_num = 0;
+  for(auto& channel : channel_cache_->channel_map_) {
+    std::unique_ptr<::grpc::ServerCompletionQueue> cq_;
+    //remote_worker_ = new GrpcRemoteWorker(channel.second, cq_.get());
+    //remote_worker_->CheckStatusSync(request, response);
+    worker_num += response.status();
+  }
+  if(worker_num != channel_cache_->channel_map_.size()) return;
+}
+
+}
+
+
+
---
+++ oneflow/core/distributed_runtime/master.h
@@ -0,0 +1,28 @@
+#ifndef ONEFLOW_CORE_DISTRIBUTED_RUNTIME_MASTER_H_
+#define ONEFLOW_CORE_DISTRIBUTED_RUNTIME_MASTER_H_
+
+#include "oneflow/core/distributed_runtime/worker.pb.h"
+#include "oneflow/core/distributed_runtime/master_service.pb.h"
+//#include "oneflow/core/distributed_runtime/grpc_remote_worker.h"
+#include "oneflow/core/distributed_runtime/grpc_channel_cache.h"
+
+namespace oneflow {
+
+class Master {
+ public:
+  Master(GrpcChannelCache* channel_cache);
+  ~Master();
+
+  void SendGraph(SendGraphRequest* request,
+                 SendGraphResponse* response);
+  void Barrier();
+
+ private:
+  //GrpcRemoteWorker* remote_worker_;
+  GrpcChannelCache* channel_cache_;
+
+};  // Master
+
+}  // oneflow
+
+#endif  // ONEFLOW_CORE_DISTRIBUTED_RUNTIME_MASTER_H_
