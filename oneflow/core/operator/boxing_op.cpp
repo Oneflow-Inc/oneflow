9d0af0d9d986bebdbc4c54193a956dc7d387b672
kingsmad <grinchcoder@gmail.com>
2017-06-17 13:24:28 +0800

    add infer shape function to boxing_op

--- oneflow/core/operator/boxing_op.cpp
+++ oneflow/core/operator/boxing_op.cpp
@@ -37,15 +37,21 @@ void BoxingOp::InferShape4FwBlobs(
     uint64_t parallel_id,
     uint64_t parallel_num) const {
   auto boxing_conf = op_conf().boxing_conf();
-  // concat input blob shape to middle blob shape
-  int32_t concat_axis = boxing_conf.concat_box().axis();
-  CHECK(concat_axis == 0 || concat_axis == 1);
+  auto in_box_case = boxing_conf.in_box_case();
   std::vector<int64_t> data_tmp_blob_shape_vec =
     GetShapePtr4BnInOp(input_bns().at(0))->dim_vec();
+
+  // if it is a concat-box, concat input blob shape to middle blob shape
+  // otherwise only check all boxes are in the same shape.
+  int32_t concat_axis = 0;
+  if (in_box_case == BoxingOpConf::kConcatBox) {
+    concat_axis = boxing_conf.concat_box().axis();
+    CHECK(concat_axis == 0 || concat_axis == 1);
+  }
   for (size_t ib_idx = 1; ib_idx < input_bns().size(); ++ib_idx) {
     auto ib_shape_vec = GetShapePtr4BnInOp(input_bns().at(ib_idx))->dim_vec();
     for (size_t i = 0; i < ib_shape_vec.size(); ++i) {
-      if (i == concat_axis) {
+      if (in_box_case == BoxingOpConf::kConcatBox && i == concat_axis) {
         data_tmp_blob_shape_vec[i] += ib_shape_vec[i];
       } else {
         CHECK_EQ(data_tmp_blob_shape_vec[i], ib_shape_vec[i]);
