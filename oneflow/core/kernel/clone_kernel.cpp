ab767b628fa8589d6791ed52a379b416302ef254
duduscript <duduscript@outlook.com>
2017-06-19 19:36:04 +0800

    fix clonekernel some problem

--- oneflow/core/kernel/clone_kernel.cpp
+++ oneflow/core/kernel/clone_kernel.cpp
@@ -1,72 +1,79 @@
 #include "oneflow/core/kernel/clone_kernel.h"
 #include <string>
 #include <cstring>
+#include "oneflow/core/common/cblas.h"
 
 namespace oneflow {
 
 namespace {
 template<typename T>
-cublasStatus_t cublas_axpy(const int N,
-                           const T *alpha,
-                           const T *x, int incx,
-                                 T *y, int incy){
-//LOG
+void cblas_axpy(const int N,
+                const T alpha,
+                const T *x, const int incx,
+                T *y, const int incy) {
+  LOG(FATAL) << "floating_point_type should be float or dounle";
 }
 
 template<>
-cublasStatus_t cublas_axpy<float>(const int N,
-                                  const float *alpha,
-                                  const float *x, int incx,
-                                        float *y, int incy){
-  return cublasSaxpy(handle, n, alpha, x, incx, y, incy);
+void cblas_axpy<float>(const int N,
+                       const float alpha,
+                       const float *x, const int incx,
+                       float *y, const int incy) {
+  cblas_saxpy(N, alpha, x, incx, y, incy);
 }
 
 template<>
-cublasStatus_t cublas_axpy<double>(const int N,
-                                   const double *alpha,
-                                   const double *x, int incx,
-                                         double *y, int incy){
-  return cublasDaxpy(handle, n, alpha, x, incx, y, incy);
+void cblas_axpy<double>(const int N,
+                        const double alpha,
+                        const double *x, const int incx,
+                        double *y, const int incy) {
+  cblas_daxpy(N, alpha, x, incx, y, incy);
 }
 
 }// namespace
 
 template<typename floating_point_type>
 void CloneKernel<DeviceType::kCPU, floating_point_type>::Forward(
-  const KernelContext&,
-  std::function<Blob*(const std::string&)> bn_in_op2blob_ptr) const {
-  Blob* in_blob = bn_in_op2blob_ptr(op()->SoleIbn());
-  const vector<std::string>& obns = op()->output_bns();
+  const KernelCtx& ctx,
+  std::function<Blob*(const std::string&)> BnInOp2BlobPtr) const {
+  Blob* in_blob = BnInOp2BlobPtr(op()->SoleIbn());
+  const std::vector<std::string>& obns = op()->output_bns();
   for(auto& obn : obns) {
-    Blob* out_blob = bn_in_op2blob_ptr(obn);
-    memcpy(out_blob->mut_dptr(), in_blob->dptr(),
-           in_blob->shape().elem_cnt()*sizeof(floating_point_type));
+    Blob* out_blob = BnInOp2BlobPtr(obn);
+    auto cpu_stream_func = [&]() {
+      memcpy(out_blob->mut_dptr(),
+             in_blob->dptr(),
+             in_blob->shape().elem_cnt()*sizeof(floating_point_type));
+    };
+    ctx.device_ctx->cpu_stream()->Send(cpu_stream_func);
   }
 }
 
 template<typename floating_point_type>
 void CloneKernel<DeviceType::kCPU, floating_point_type>::Backward(
-    const KernelContext&,
-    std::function<Blob*(const std::string&)> bn_in_op2blob_ptr) const {
-  Blob* in_blob = bn_in_op2blob_ptr(op()->SoleIbn());
-  memset(in_blob->dptr(), 0, in_blob->shape().elem_cnt()*sizeof(floating_point_type));
-  const vector<std::string>& obns = op()->output_bns();
-  const floating_point_type* alpha = new floating_point_type(1.0);
-  for(auto& obn : obns) {
-    Blob* out_blob = bn_in_op2blob_ptr(obn);
-    //async?
-    cblas_axpy<floating_point_type>(in_blob->shape().elem_cnt(), alpha,
-                                    static_cast<floating_point_type*>(out_blob->dptr()), 1,
-                                    static_cast<floating_point_type*>(in_blob->mut_dptr()), 1);
-    /*
-    for(int i=0;i!=in_blob->shape().elem_cnt();++i){
-      in_dptr[i] += out_dptr[i];
-    }
-    */
+    const KernelCtx& ctx,
+    std::function<Blob*(const std::string&)> BnInOp2BlobPtr) const {
+  Blob* in_blob = BnInOp2BlobPtr(op()->SoleIdbn());
+  const std::vector<std::string>& odbns = op()->output_diff_bns();
+  if (odbns.size() == 0) return;
+  auto cpu_stream_memcpy = [&]() {
+    memcpy(in_blob->mut_dptr(),
+           BnInOp2BlobPtr(odbns[0])->dptr(),
+           in_blob->shape().elem_cnt() * sizeof(floating_point_type));
+  };
+  ctx.device_ctx->cpu_stream()->Send(cpu_stream_memcpy);
+  for(int i=1; i!=odbns.size(); ++i) {
+    Blob* out_blob = BnInOp2BlobPtr(odbns[i]);
+    auto cpu_stream_axpy = [&]() {
+      cblas_axpy<floating_point_type>(in_blob->shape().elem_cnt(), 1.0,
+                                      reinterpret_cast<const floating_point_type*>(out_blob->dptr()), 1,
+                                      reinterpret_cast<floating_point_type*>(in_blob->mut_dptr()), 1);
+    };
+    ctx.device_ctx->cpu_stream()->Send(cpu_stream_axpy);
   }
 }
 
 INSTANTIATE_CPU_KERNEL_CLASS(CloneKernel);
-REGISTER_KERNEL(OperatorConf::kCloneConf, CloneKernel);
+REGISTER_CPU_KERNEL(OperatorConf::kCloneConf, CloneKernel);
 
 }  // namespace oneflow
