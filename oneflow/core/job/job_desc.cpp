b8705463c24d02ed2c9cf70287a273cafafd1055
willzhang4a58 <willzhang4a58@gmail.com>
2017-06-20 13:40:44 +0800

    num of piece in batch

--- oneflow/core/job/job_desc.cpp
+++ oneflow/core/job/job_desc.cpp
@@ -11,9 +11,12 @@ void JobDesc::InitFromJobConf(const JobConf& conf) {
   md_load_snapshot_path_ = conf.model_load_snapshot_path();
   md_save_snapshots_path_ = conf.model_save_snapshots_path();
   piece_size_ = conf.piece_size();
+  num_of_pieces_in_batch_ = conf.num_of_pieces_in_batch();
   is_train_ = conf.is_train();
   floating_point_type_ = conf.floating_point_type();
   num_of_batches_in_snapshot_ = conf.num_of_batches_in_snapshot();
+  staleness_ = conf.staleness();
+  total_batch_num_ = conf.total_batch_num();
 }
 
 void JobDesc::InitFromProto(const JobDescProto& proto) {
@@ -24,9 +27,12 @@ void JobDesc::InitFromProto(const JobDescProto& proto) {
   md_load_snapshot_path_ = proto.model_load_snapshot_path();
   md_save_snapshots_path_ = proto.model_save_snapshots_path();
   piece_size_ = proto.piece_size();
+  num_of_pieces_in_batch_ = proto.num_of_pieces_in_batch();
   is_train_ = proto.is_train();
   floating_point_type_ = proto.floating_point_type();
   num_of_batches_in_snapshot_ = proto.num_of_batches_in_snapshot();
+  staleness_ = proto.staleness();
+  total_batch_num_ = proto.total_batch_num();
 }
 
 void JobDesc::ToProto(JobDescProto* proto) const {
@@ -36,9 +42,12 @@ void JobDesc::ToProto(JobDescProto* proto) const {
   *(proto->mutable_model_load_snapshot_path()) = md_load_snapshot_path_;
   *(proto->mutable_model_save_snapshots_path()) = md_save_snapshots_path_;
   proto->set_piece_size(piece_size_);
+  proto->set_num_of_pieces_in_batch(num_of_pieces_in_batch_);
   proto->set_is_train(is_train_);
   proto->set_floating_point_type(floating_point_type_);
   proto->set_num_of_batches_in_snapshot(num_of_batches_in_snapshot_);
+  proto->set_staleness(staleness_);
+  proto->set_total_batch_num(total_batch_num_);
 }
 
 } // namespace oneflow
