02cf23812aa2fb25cba166d13f8a33be1eb36f31
willzhang4a58 <willzhang4a58@gmail.com>
2017-06-20 16:18:59 +0800

    char* -> void*

--- oneflow/core/register/blob.h
+++ oneflow/core/register/blob.h
@@ -9,16 +9,16 @@ namespace oneflow {
 class Blob {
  public:
   OF_DISALLOW_COPY_AND_MOVE(Blob);
-  Blob(char* dptr, const Shape* shape) : dptr_(dptr), shape_(shape) {}
+  Blob(void* dptr, const Shape* shape) : dptr_(dptr), shape_(shape) {}
   ~Blob() {}
 
-  const char* dptr() const { return dptr_; }
+  const void* dptr() const { return dptr_; }
   const Shape& shape() const { return *shape_; }
   
-  char* mut_dptr() { return dptr_; }
+  void* mut_dptr() { return dptr_; }
 
  private:
-  char* dptr_ ;
+  void* dptr_ ;
   const Shape* shape_;
 };
 
