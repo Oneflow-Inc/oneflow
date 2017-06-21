c19cb23e4260b52d8b940f2f83a61469accdae02
xswang <2012wxs@gmail.com>
2017-06-21 09:24:15 +0800

    update:

--- oneflow/core/distributed_runtime/grpc_remote_master.h
+++ oneflow/core/distributed_runtime/grpc_remote_master.h
@@ -23,7 +23,6 @@ class GrpcRemoteMaster {
   ::tensorflow::Status SendGraph(const SendGraphRequest* request,
                        SendGraphResponse* response) {
     ::grpc::ClientContext ctx;
-    std::cout<<"request = "<<request->tmp()<<std::endl;
     return FromGrpcStatus(stub_->SendGraph(&ctx, *request, response));
   }
 
