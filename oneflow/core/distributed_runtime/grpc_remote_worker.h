7e0deadbd1847330d0804e5938daa6e8866715bd
xswang <2012wxs@gmail.com>
2017-06-22 14:12:28 +0800

    fix: bug for std::bad_alloc

--- oneflow/core/distributed_runtime/grpc_remote_worker.h
+++ oneflow/core/distributed_runtime/grpc_remote_worker.h
@@ -25,30 +25,30 @@ class GrpcRemoteWorker {
     ~GrpcRemoteWorker() {}
 
     ::tensorflow::Status GetStatus(const GetStatusRequest* request,
-                   GetStatusResponse* response) {
+                                   GetStatusResponse* response) {
       ::grpc::ClientContext ctx;
       return FromGrpcStatus(stub_->GetStatus(&ctx, *request, response));
     }
 
-    ::tensorflow::Status GetMachineDesc(GetMachineDescRequest* request,
+    ::tensorflow::Status GetMachineDesc(const GetMachineDescRequest* request,
                         GetMachineDescResponse* response) {
       ::grpc::ClientContext ctx;
       return FromGrpcStatus(stub_->GetMachineDesc(&ctx, *request, response));
     }
 
-    ::tensorflow::Status GetMemoryDesc(GetMemoryDescRequest* request,
+    ::tensorflow::Status GetMemoryDesc(const GetMemoryDescRequest* request,
                        GetMemoryDescResponse* response) {
       ::grpc::ClientContext ctx;
       return FromGrpcStatus(stub_->GetMemoryDesc(&ctx, *request, response)); 
     }
 
-    ::tensorflow::Status SendTaskGraph(SendTaskGraphRequest* request,
+    ::tensorflow::Status SendTaskGraph(const SendTaskGraphRequest* request,
                        SendTaskGraphResponse* response) {
       ::grpc::ClientContext ctx;
       return FromGrpcStatus(stub_->SendTaskGraph(&ctx, *request, response));
     }
 
-    void SendMessageAsync(SendMessageRequest* request,
+    void SendMessageAsync(const SendMessageRequest* request,
                           SendMessageResponse* response,
                           StatusCallback done) {
       IssueRequest(request, response, sendmessage_, std::move(done));
@@ -81,10 +81,10 @@ class GrpcRemoteWorker {
   template <class RequestMessage, class ResponseMessage>
   class RPCState final : public GrpcClientCQTag {
    public:
-    RPCState(::grpc::ChannelInterface* channel, ::grpc::CompletionQueue* cq, 
+    RPCState(::grpc::ChannelInterface* channel, ::grpc::CompletionQueue* cq,
              const ::grpc::RpcMethod& method, const RequestMessage& request,
              StatusCallback done)
-      : reader_(channel, cq, method, context_, request),
+      : reader_(channel, cq, method, InitContext(), request),
         done_(std::move(done)) {}
 
     ~RPCState() {}
@@ -93,30 +93,36 @@ class GrpcRemoteWorker {
       reader_.Finish(response, &status_, this);
     }
 
-    void OnCompleted(bool ok) {
+    void OnCompleted(bool ok) override {
       if (ok) {
         done_(FromGrpcStatus(status_));
         delete this;
       }
     }
 
-    private:
-     ::grpc::ClientContext* context_;
-     ::grpc::ClientAsyncResponseReader<ResponseMessage> reader_;
-     ::grpc::Status status_;
-     StatusCallback done_;
+   private:
+    ::grpc::ClientContext context_;
+    ::grpc::ClientAsyncResponseReader<ResponseMessage> reader_;
+    ::grpc::Status status_;
+    StatusCallback done_;
+
+    ::grpc::ClientContext* InitContext() {
+      return &context_;
+    }
   };  // class RPCState
 
   template <class RequestMessage, class ResponseMessage>
   void IssueRequest(const RequestMessage* request, ResponseMessage* response,
                     const ::grpc::RpcMethod& method, StatusCallback done) {
     auto state = new RPCState<RequestMessage, ResponseMessage>(
-                   channel_.get(), cq_, method, *request, std::move(done));
+      channel_.get(), cq_, method, *request, std::move(done));
     state->StartRPC(response);
-  }//IssueRequest
+  }  // IssueRequest
 
   ::grpc::RpcMethod Method(GrpcWorkerMethod id) {
-    return ::grpc::RpcMethod(GrpcWorkerMethodName(id), ::grpc::RpcMethod::NORMAL_RPC, channel_);
+    return ::grpc::RpcMethod(GrpcWorkerMethodName(id), 
+                             ::grpc::RpcMethod::NORMAL_RPC, 
+                             channel_);
   }
 
   std::shared_ptr<::grpc::Channel> channel_;
@@ -124,9 +130,8 @@ class GrpcRemoteWorker {
 
   const ::grpc::RpcMethod sendmessage_;
   const ::grpc::RpcMethod readdata_;
-};//class GrpcRemoteWorker
+};  // class GrpcRemoteWorker
 
+}  // namespace oneflow
 
-}//namespace oneflow
-
-#endif /* !GRPC_REMOTE_WORKER_H */
+#endif  // ONEFLOW_CORE_DISTRIBUTED_RUNTIME_GRPC_REMOTE_WORKER_H_
