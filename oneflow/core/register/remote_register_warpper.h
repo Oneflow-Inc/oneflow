ca474ebc24347a87612bda56e88b2e2a3659d20c
LeGend-AI <legend-ai@outlook.com>
2017-06-24 17:04:42 +0800

    remote regst warpper

--- oneflow/core/register/remote_register_warpper.h
+++ oneflow/core/register/remote_register_warpper.h
@@ -2,6 +2,7 @@
 #define ONEFLOW_CORE_REGISTER_REMOTE_REGISTER_WARPPER_H_
 
 #include "oneflow/core/register/register_warpper.h"
+#include "oneflow/core/job/keyword.h"
 
 namespace oneflow {
 
@@ -12,17 +13,41 @@ class RemoteRegstWarpper final : public RegstWarpper {
   ~RemoteRegstWarpper() = default;
 
   RemoteRegstWarpper(Regst* regst) {
-    TODO();
+    regst_ = regst;
+    baled_blob_ = regst_->GetBlobPtrFromLbn(kBaledBlobName);
+    piece_id_ = regst_->piece_id();
+    model_version_id_ = regst_->model_version_id();
+    regst_desc_id_ = regst_->regst_desc_id();
+    producer_actor_id_ = regst_->producer_actor_id();
   }
 
-  Blob* GetBlobPtrFromLbn(const std::string& lbn) override { TODO(); }
-  int64_t piece_id() const override { TODO(); }
-  int64_t model_version_id() const override { TODO(); }
-  int64_t regst_desc_id() const override { TODO(); }
-  int64_t producer_actor_id() const override { TODO(); }
-  Regst* regst_raw_ptr() const override { TODO(); }
+  Blob* GetBlobPtrFromLbn(const std::string& lbn) override {
+    CHECK_EQ(kBaledBlobName, lbn);
+    return baled_blob_;
+  }
+  int64_t piece_id() const override {
+    return piece_id_;
+  }
+  int64_t model_version_id() const override {
+    return model_version_id_;
+  }
+  int64_t regst_desc_id() const override {
+    return regst_desc_id_;
+  }
+  int64_t producer_actor_id() const override {
+    return producer_actor_id_;
+  }
+  Regst* regst_raw_ptr() const override {
+    return regst_;
+  }
 
  private:
+  int64_t piece_id_;
+  int64_t model_version_id_;
+  int64_t regst_desc_id_;
+  int64_t producer_actor_id_;
+  Regst* regst_;
+  Blob* baled_blob_;
 
 };
 
