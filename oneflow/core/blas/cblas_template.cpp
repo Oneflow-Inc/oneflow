7541ad8eae64e53ceb4348dfe5f29b718585a5f7
syshangguan <shgshy@gmail.com>
2017-06-27 01:40:28 +0800

    add cblas_template

--- oneflow/core/blas/cblas_template.cpp
+++ oneflow/core/blas/cblas_template.cpp
@@ -2,6 +2,95 @@
 
 namespace oneflow {
 
+// level 1 vector and vector
+// dot product
+template<>
+void cblas_dot<float>(
+    const int N, const float* X, const int incX,
+    const float* Y, const int incY) {
+  cblas_sdot(N, X, incX, Y, incY);
+}
+
+template<>
+void cblas_dot<double>(
+    const int N, const double* X, const int incX,
+    const double* Y, const int incY) {
+  cblas_ddot(N, X, incX, Y, incY);
+}
+
+// swap x and y
+template<>
+void cblas_swap<float>(
+    const int N, float* X, const int incX, float* Y, const int incY) {
+  cblas_sswap(N, X, incX, Y, incY);
+}
+
+template<>
+void cblas_swap<double>(
+    const int N, double* X, const int incX, double* Y, const int incY) {
+  cblas_dswap(N, X, incX, Y, incY);
+}
+
+// copy x into y
+template<>
+void cblas_copy<float>(
+    const int N, const float* X, const int incX, float* Y, const int incY) {
+  cblas_scopy(N, X, incX, Y, incY);
+}
+
+template<>
+void cblas_copy<double>(
+    const int N, const double* X, const int incX, double* Y, const int incY) {
+  cblas_dcopy(N, X, incX, Y, incY);
+}
+
+// y = a*x + y
+template<>
+void cblas_axpy<float>(
+    const int N, const float alpha, const float* X, const int incX,
+    float* Y, const int incY) {
+  cblas_saxpy(N, alpha, X, incX, Y, incY);
+}
+
+template<>
+void cblas_axpy<double>(
+    const int N, const double alpha, const double* X, const int incX,
+    double* Y, const int incY) {
+  cblas_daxpy(N, alpha, X, incX, Y, incY);
+}
+
+// x = a*x
+template<>
+void cblas_scal<float>(
+    const int N, const float alpha, float* X, const int incX) {
+  cblas_sscal(N, alpha, X, incX);
+}
+
+template<>
+void cblas_scal<double>(
+    const int N, const double alpha, double* X, const int incX) {
+  cblas_dscal(N, alpha, X, incX);
+}
+
+// level 2 matrix and vector
+// matrix vector multiply
+template<>
+void cblas_gemv<float>(
+    const enum CBLAS_TRANSPOSE TransA, const int M, const int N,
+    const float alpha, const float* A, const int lda, const float* X,
+    const int incX, const float beta, float* Y, const int incY) {
+  cblas_sgemv(CblasRowMajor, TransA, M, N, alpha, A, lda, X, incX, beta, Y, incY);
+}
+
+template<>
+void cblas_gemv<double>(
+    const enum CBLAS_TRANSPOSE TransA, const int M, const int N,
+    const double alpha, const double* A, const int lda, const double* X,
+    const int incX, const double beta, double* Y, const int incY) {
+  cblas_dgemv(CblasRowMajor, TransA, M, N, alpha, A, lda, X, incX, beta, Y, incY);
+}
+
+// matrix matrix multiply
 template<>
 void cblas_gemm<float>(
     const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB,
@@ -22,22 +111,5 @@ void cblas_gemm<double>(
               beta, C, ldc);
 }
 
-template<>
-void cblas_axpy<float>(
-    const int N,
-    const float alpha,
-    const float *x, const int incx,
-    float *y, const int incy) {
-  cblas_saxpy(N, alpha, x, incx, y, incy);
-}
-
-template<>
-void cblas_axpy<double>(
-    const int N,
-    const double alpha,
-    const double *x, const int incx,
-    double *y, const int incy) {
-  cblas_daxpy(N, alpha, x, incx, y, incy);
-}
 
 }  // namespace oneflow
--- oneflow/core/blas/cblas_template.h
+++ oneflow/core/blas/cblas_template.h
@@ -7,6 +7,54 @@ extern "C" {
 
 namespace oneflow {
 
+// level 1 vector and vector
+// dot product
+template<typename floating_point_type>
+void cblas_dot(
+    const int N, 
+    const floating_point_type* X, const int incX,
+    const floating_point_type* Y, const int incY);
+
+// swap x and y
+template<typename floating_point_type>
+void cblas_swap(
+    const int N,
+    floating_point_type* X, const int incX,
+    floating_point_type* Y, const int incY);
+
+// copy x into y
+template<typename floating_point_type>
+void cblas_copy(
+    const int N,
+    const floating_point_type* X, const int incX,
+    floating_point_type* Y, const int incY);
+
+// y = a*x + y
+template<typename floating_point_type>
+void cblas_axpy(
+    const int N,
+    const floating_point_type alpha,
+    const floating_point_type* X, const int incX,
+    floating_point_type* Y, const int incY);
+
+// x = a*x
+template<typename floating_point_type>
+void cblas_scal(
+    const int N,
+    const floating_point_type alpha,
+    floating_point_type* X, const int incX);
+
+// level 2 matrix and vector
+// matrix vector multiply
+template<typename floating_point_type>
+void cblas_gemv(
+    const enum CBLAS_TRANSPOSE TransA, const int M, const int N,
+    const floating_point_type alpha, const floating_point_type* A,
+    const int lda, const floating_point_type* X, const int incX,
+    const floating_point_type beta, floating_point_type* Y, const int incY);
+
+// level 3 matrix and matrix
+// matrix matrix multiply
 template<typename floating_point_type>
 void cblas_gemm(
     const enum CBLAS_TRANSPOSE TransA, const enum CBLAS_TRANSPOSE TransB,
@@ -14,13 +62,6 @@ void cblas_gemm(
     const floating_point_type* A, const int lda,
     const floating_point_type* B, const int ldb, const floating_point_type beta,
     floating_point_type* C, const int ldc);
-
-template<typename floating_point_type>
-void cblas_axpy(
-    const int N,
-    const floating_point_type alpha,
-    const floating_point_type *x, const int incx,
-    floating_point_type *y, const int incy);
 
 } // namespace oneflow
 
