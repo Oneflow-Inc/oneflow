b3f76f9838e174338f3d8a15a137b458cd53bc4f
kingsmad <grinchcoder@gmail.com>
2017-06-27 12:21:35 +0800

    fixed cblas template

--- oneflow/core/blas/kernel_util.cpp
+++ oneflow/core/blas/kernel_util.cpp
@@ -3,64 +3,38 @@
 
 namespace oneflow {
 
-template<>
-void KernelUtil<DeviceType::kCPU, float>::BlasAxpy(
-    const KernelCtx& ctx, 
-    const int N, const float alpha, 
-    const float *X, const int incX, 
-    float *Y, const int incY) {
-  cblas_axpy(N, alpha, X, incX, Y, incY);
-}
+template<typename floating_point_type> 
+class KernelUtil<DeviceType::kCPU, floating_point_type> final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(KernelUtil);
+  KernelUtil() = default;
+  ~KernelUtil() = default;
 
-template<>
-void KernelUtil<DeviceType::kCPU, double>::BlasAxpy(
-    const KernelCtx& ctx, 
-    const int N, const double alpha, 
-    const double *X, const int incX, 
-    double *Y, const int incY) {
-  cblas_axpy(N, alpha, X, incX, Y, incY);
-}
+  static void Memcpy(const KernelCtx& ctx, 
+     void* dst, const void* src, size_t sz) {
+    memcpy(dst, src, sz);
+  }
 
-template<> 
-void KernelUtil<DeviceType::kCPU, double>::BlasScal(
-    const KernelCtx& ctx,
-    const int n, const double alpha,
-    double* x, int incx) {
-  cblas_scal(n, alpha, x, incx);
-}
+  static void Memset(const KernelCtx& ctx, void* dst, const char value,
+      size_t sz) {
+    memset(dst, value, sz);
+  }
 
-template<> 
-void KernelUtil<DeviceType::kCPU, float>::BlasScal(
-    const KernelCtx& ctx,
-    const int n, const float alpha,
-    float* x, int incx) {
-  cblas_scal(n, alpha, x, incx);
-}
 
-template<>
-void KernelUtil<DeviceType::kCPU, float>::Memcpy(
-    const KernelCtx& ctx, void* dst,
-    const void* src, size_t sz) {
-  memcpy(dst, src, sz);
-}
+  static void BlasAxpy(const KernelCtx& ctx, const int N,
+      const floating_point_type alpha,
+      const floating_point_type* X, const int incX,
+      floating_point_type *Y, const int incY) {
+    cblas_axpy(N, alpha, X, incX, Y, incY);
+  }
 
-template<>
-void KernelUtil<DeviceType::kCPU, double>::Memcpy(
-    const KernelCtx& ctx, void* dst,
-    const void* src, size_t sz) {
-  memcpy(dst, src, sz);
-}
+  static void BlasScal(const KernelCtx& ctx, const int n,
+      const floating_point_type alpha, floating_point_type* x, int incx) {
+    cblas_scal(n, alpha, x, incx);
+  }
+};
 
-template<>
-void KernelUtil<DeviceType::kCPU, float>::Memset(
-    const KernelCtx& ctx, void* dst, const char value, size_t sz) {
-  memset(dst, value, sz);
-}
-
-template<>
-void KernelUtil<DeviceType::kCPU, double>::Memset(
-    const KernelCtx& ctx, void* dst, const char value, size_t sz) {
-  memset(dst, value, sz);
-}
+template class KernelUtil<DeviceType::kCPU, float>;
+template class KernelUtil<DeviceType::kCPU, double>;
 
 }  //  namespace oneflow
