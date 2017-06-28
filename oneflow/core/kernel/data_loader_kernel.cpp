d709807a39fcf1b7a7c00ac512d9016ac8f636dd
willzhang4a58 <willzhang4a58@gmail.com>
2017-06-28 15:13:57 +0800

    init data loader kernel

---
+++ oneflow/core/kernel/data_loader_kernel.cpp
@@ -0,0 +1,40 @@
+#include "oneflow/core/kernel/data_loader_kernel.h"
+#include "oneflow/core/job/runtime_context.h"
+
+namespace oneflow {
+
+template<typename floating_point_type>
+class DataLoaderKernel<DeviceType::kCPU, floating_point_type> final : public Kernel {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(DataLoaderKernel);
+  DataLoaderKernel() = default;
+  ~DataLoaderKernel() = default;
+
+  void Forward(const KernelCtx&,
+               std::function<Blob*(const std::string&)>) const override;
+  void Backward(const KernelCtx&,
+                std::function<Blob*(const std::string&)>) const override {
+    UNEXPECTED_RUN();
+  }
+
+};
+
+template<typename floating_point_type>
+void DataLoaderKernel<DeviceType::kCPU, floating_point_type>::Forward(
+    const KernelCtx& kernel_ctx,
+    std::function<Blob*(const std::string&)> BnInOp2BlobPtr) const {
+  PersistentCircularLineReader* reader = RuntimeCtx::Singleton().GetDataReader();
+  if (reader == nullptr) {
+    std::string data_dir = op()->GetStringFromSpecialConf("data_dir");
+    int64_t parallel_id = reinterpret_cast<int64_t>(kernel_ctx.other);
+    std::string file_path = data_dir + "part-" + std::to_string(parallel_id);
+    RuntimeCtx::Singleton().InitDataReader(file_path);
+    reader = RuntimeCtx::Singleton().GetDataReader();
+  }
+  TODO();
+}
+
+INSTANTIATE_CPU_KERNEL_CLASS(DataLoaderKernel);
+REGISTER_CPU_KERNEL(OperatorConf::kDataLoaderConf, DataLoaderKernel)
+  
+} // namespace oneflow
