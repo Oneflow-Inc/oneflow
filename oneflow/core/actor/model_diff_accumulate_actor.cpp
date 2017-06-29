a7d95ec253a315c87e23e81f8d76b56f4d513265
LeGend-AI <legend-ai@outlook.com>
2017-06-29 16:18:28 +0800

    alignment

--- oneflow/core/actor/model_diff_accumulate_actor.cpp
+++ oneflow/core/actor/model_diff_accumulate_actor.cpp
@@ -2,8 +2,9 @@
 
 namespace oneflow {
 
-void MdDiffAccActor::Init(const TaskProto& task_proto, const ThreadCtx& thread_ctx) {
-  Actor::Init(task_proto, thread_ctx);
+void MdDiffAccActor::Init(const TaskProto& task_proto,
+                          const ThreadCtx& thread_ctx) {
+  CompActor::Init(task_proto, thread_ctx);
   if (thread_ctx.cpu_stream) {
     clear_kernel_ = KernelMgr::Singleton().GetKernelFromOpName("cpu_clear");
     mut_device_ctx().reset(new CpuDeviceCtx(thread_ctx.cpu_stream));
@@ -49,40 +50,39 @@ int MdDiffAccActor::HandleMdDiffAccWhenNoReadableRegstMsg(const ActorMsg& msg) {
 }
 
 void MdDiffAccActor::TryWardKernelAndSendMsg() {
-  if (!waiting_in_regst_.empty() && IsWriteReady()) {
-    std::shared_ptr<RegstWarpper> regst_wp = waiting_in_regst_.front();
-    CHECK_EQ(regst_wp->piece_id(), expected_piece_id());
-    KernelCtx ctx = GenDefaultKernelCtx();
-    ForEachCurWriteableRegst([&](Regst* regst) {
-      auto diff_cnt = model_diff_acc_cnt_.find(regst);
-      if (diff_cnt->second != JobDesc::Singleton().num_of_piece_in_batch()) {
-        return;
-      }
-      clear_kernel_->Forward(ctx,
-          [&](const std::string& bn_in_op) {
-        const std::string& lbn = clear_kernel_->Lbn4BnInOp(bn_in_op); 
-        return regst->GetBlobPtrFromLbn(lbn);
-      });
-      diff_cnt->second = 0;
-    });
-    AsyncWardKernel(ctx,
-        [this](uint64_t regst_desc_id) -> std::shared_ptr<RegstWarpper> {
-      Regst* regst = GetCurWriteableRegst(regst_desc_id);
-      if (regst == nullptr) {
-        CHECK_EQ(regst_desc_id, waiting_in_regst_.front()->regst_desc_id());
-        return waiting_in_regst_.front();
-      } else {
-        return std::make_shared<LocalRegstWarpper> (regst);
-      }
-    });
-    ForEachCurWriteableRegst([this, &regst_wp](Regst* regst) {
-      regst->set_piece_id(regst_wp->piece_id());
-      ++ model_diff_acc_cnt_[regst];
-    });
-    AsyncSendReadableRegstMsg();
-    AsyncSendRegstMsgToProducer(regst_wp);
-    waiting_in_regst_.pop();
+  if (waiting_in_regst_.empty() || !IsWriteReady()) {
+    return;
   }
+  std::shared_ptr<RegstWarpper> regst_wp = waiting_in_regst_.front();
+  CHECK_EQ(regst_wp->piece_id(), expected_piece_id());
+  KernelCtx ctx = GenDefaultKernelCtx();
+  ForEachCurWriteableRegst([&](Regst* regst) {
+    auto diff_cnt = model_diff_acc_cnt_.find(regst);
+    if (diff_cnt->second != JobDesc::Singleton().num_of_piece_in_batch()) {
+      return;
+    }
+    clear_kernel_->Forward(ctx, [&](const std::string& bn_in_op) {
+      const std::string& lbn = clear_kernel_->Lbn4BnInOp(bn_in_op);
+      return regst->GetBlobPtrFromLbn(lbn);
+    });
+    diff_cnt->second = 0;
+  });
+  AsyncWardKernel(ctx, [this](uint64_t regst_desc_id) -> std::shared_ptr<RegstWarpper> {
+    Regst* regst = GetCurWriteableRegst(regst_desc_id);
+    if (regst == nullptr) {
+      CHECK_EQ(regst_desc_id, waiting_in_regst_.front()->regst_desc_id());
+      return waiting_in_regst_.front();
+    } else {
+      return std::make_shared<LocalRegstWarpper> (regst);
+    }
+  });
+  ForEachCurWriteableRegst([this, &regst_wp](Regst* regst) {
+    regst->set_piece_id(regst_wp->piece_id());
+    ++model_diff_acc_cnt_[regst];
+  });
+  AsyncSendReadableRegstMsg();
+  AsyncSendRegstMsgToProducer(regst_wp);
+  waiting_in_regst_.pop();
 }
 
 }  // namespace oneflow
