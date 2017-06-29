3cb2c870bd1a72fe899db38bae4e5a78018385e7
Liu Guo <legend-ai@outlook.com>
2017-06-29 16:34:35 +0800

    model diff accumulate actor (#161)
    * refine copy actor
    
    * using unix path separator
    
    * separate Init for CopyHdActor and CopyCommNetActor
    
    * refine CopyHdActor with different stages
    
    * refine CopyCommNetActor with different stages
    
    * remove useless code
    
    * remove useless code
    
    * using cuda stream in thread context
    
    * using OF_SET_MSG_HANDLE
    
    * remove useless code
    
    * remote regst warpper
    
    * fix blob ptr in RemoteRegstWarpper
    
    * fix memory leak
    
    * add model diff accumulate actor
    
    * implement model diff accumulate actor
    
    * fix style
    
    * alignment

---
+++ oneflow/core/actor/model_diff_accumulate_actor.h
@@ -0,0 +1,30 @@
+#ifndef ONEFLOW_CORE_ACTOR_MODEL_DIFF_ACCUMULATE_ACTOR_H_
+#define ONEFLOW_CORE_ACTOR_MODEL_DIFF_ACCUMULATE_ACTOR_H_
+
+#include "oneflow/core/actor/compute_actor.h"
+
+namespace oneflow {
+
+class MdDiffAccActor final : public CompActor {
+public:
+  OF_DISALLOW_COPY_AND_MOVE(MdDiffAccActor);
+  MdDiffAccActor() = default;
+  ~MdDiffAccActor() = default;
+
+  void Init(const TaskProto&, const ThreadCtx&) override;
+
+private:
+  int HandleMdDiffAcc(const ActorMsg&);
+  int HandleMdDiffAccWhenNoReadableRegstMsg(const ActorMsg&);
+
+  void TryWardKernelAndSendMsg();
+
+  std::queue<std::shared_ptr<RegstWarpper>> waiting_in_regst_;
+  const Kernel* clear_kernel_;
+  CudaStreamHandle cuda_handle_;
+  HashMap<Regst*, int32_t> model_diff_acc_cnt_;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_ACTOR_COPY_HD_ACTOR_H_
