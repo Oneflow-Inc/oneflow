8528fa1c803d321794dc2d9515871991d2b161a3
syshangguan <shgshy@gmail.com>
2017-07-01 03:18:56 +0800

    update include path

--- oneflow/core/network/rdma/agency.h
+++ oneflow/core/network/rdma/agency.h
@@ -1,10 +1,10 @@
-#ifndef ONEFLOW_NETWORK_RDMA_AGENCY_H_
-#define ONEFLOW_NETWORK_RDMA_AGENCY_H_
+#ifndef ONEFLOW_CORE_NETWORK_RDMA_AGENCY_H_
+#define ONEFLOW_CORE_NETWORK_RDMA_AGENCY_H_
 
 #ifdef WIN32
-#include "network/rdma/windows/interface.h"
+#include "oneflow/core/network/rdma/windows/interface.h"
 #else
-#include "network/rdma/linux/interface.h"
+#include "oneflow/core/network/rdma/linux/interface.h"
 #endif
 
 #endif  // ONEFLOW_NETWORK_RDMA_AGENCY_H_
