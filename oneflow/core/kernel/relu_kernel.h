2c48055ff023b45b47742766eb5b9fccc0ef86f4
chengtbf <472491134@qq.com>
2017-07-01 15:40:59 +0800

    relu_kernel, circle_read_line resolve warning

---
+++ oneflow/core/kernel/relu_kernel.h
@@ -0,0 +1,36 @@
+#ifndef ONEFLOW_CORE_KERNEL_RELU_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_RELU_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename FloatingPointType>
+class ReluKernel final : public Kernel {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(ReluKernel);
+  ReluKernel() = default;
+  ~ReluKernel() = default;
+
+  void Forward(const KernelCtx&,
+               std::function<Blob*(const std::string&)>) const override;
+  void Backward(const KernelCtx&,
+                std::function<Blob*(const std::string&)>) const override;
+};
+
+template<DeviceType device_type, typename FloatingPointType>
+class ReluKernelUtil final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(ReluKernelUtil);
+  ReluKernelUtil() = delete;
+
+  static void Forward(const KernelCtx& ctx, const int64_t n,
+                      const FloatingPointType* in, FloatingPointType* out);
+  static void Backward(const KernelCtx& ctx, const int64_t n,
+                       const FloatingPointType* out_diff,
+                       const FloatingPointType* in, FloatingPointType* in_diff);
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_RELU_KERNEL_H_
