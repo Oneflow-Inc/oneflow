a7b6d5ddbbfd49365ae7f46f56477ce1c674d428
kingsmad <grinchcoder@gmail.com>
2017-07-02 17:23:57 +0800

    remove middle blobs of boxing op in most cases

--- oneflow/core/operator/boxing_op_test.cpp
+++ oneflow/core/operator/boxing_op_test.cpp
@@ -26,7 +26,7 @@ TEST(BoxingOp, box_4_10x5x6x6) {
       {boxing_op->input_bns()[1], new Shape(input_shape_vec2)},
       {boxing_op->input_bns()[2], new Shape(input_shape_vec2)},
       {boxing_op->input_bns()[3], new Shape(input_shape_vec1)},
-      {boxing_op->SoleDtbn(), new Shape},
+      {"middle", new Shape},
       {boxing_op->output_bns()[0], new Shape},
       {boxing_op->output_bns()[1], new Shape},
       {boxing_op->output_bns()[2], new Shape},
@@ -39,10 +39,6 @@ TEST(BoxingOp, box_4_10x5x6x6) {
   boxing_op->InferShape4FwBlobs(fp, kModelParallel, 0, 1);
 
   // test results
-  // data_tmp_shape is {10, 17, 6, 6}, and the 17 = 5 + 4 + 4 + 4
-  Shape* data_tmp_shape_ptr = bn2shape_ptr.at(boxing_op->SoleDtbn());
-  std::vector<int64_t> data_temp_shape_vec = {10, 17, 6, 6};
-  ASSERT_EQ(*data_tmp_shape_ptr, Shape(data_temp_shape_vec));
   // output_shape should be:
   // out1 {4, 17, 6, 6}
   // out2 {3, 17, 6, 6}
@@ -54,7 +50,7 @@ TEST(BoxingOp, box_4_10x5x6x6) {
     ASSERT_EQ(*output_shape_ptr, Shape(output_shape_vec));
   }
 
-  // Test add_box shape function
+  // Test add clone box shape function
   boxing_conf->set_in_num(3);
   boxing_conf->set_out_num(1);
   boxing_conf->mutable_add_box();
@@ -66,9 +62,33 @@ TEST(BoxingOp, box_4_10x5x6x6) {
 
   // test results
   // output shape should be the same as input
+  for (const std::string& bn : boxing_op->output_bns()) {
+    Shape* output_shape_ptr = bn2shape_ptr.at(bn);
+    ASSERT_EQ(*output_shape_ptr, Shape(input_shape_vec2));
+  }
+
+  // Test concat clone shape function, this box has data_tmp_shape
+  boxing_conf->set_in_num(4);
+  boxing_conf->set_out_num(1);
+  boxing_conf->mutable_concat_box()->set_axis(1);
+  boxing_conf->mutable_clone_box();
+  boxing_op = OpMgr::Singleton()->ConstructOp(op_conf);
+
+  // do infer shape
+  boxing_op->InferShape4FwBlobs(fp, kModelParallel, 0, 1);
+
+  // data_tmp_shape is {10, 17, 6, 6}, and the 17 = 4 + 4 + 4 + 5
+  Shape* data_tmp_shape_ptr = bn2shape_ptr.at(boxing_op->SoleDtbn());
+  std::vector<int64_t> data_temp_shape_vec = {10, 17, 6, 6};
+  ASSERT_EQ(*data_tmp_shape_ptr, Shape(data_temp_shape_vec));
+
+  // test results
+  // output shape should be the same as data_tmp_shape
   data_tmp_shape_ptr = bn2shape_ptr.at(boxing_op->SoleDtbn());
-  Shape* output_shape_ptr = bn2shape_ptr.at(boxing_op->output_bns()[0]);
-  ASSERT_EQ(*output_shape_ptr, Shape(input_shape_vec2));
+  for (const std::string& bn : boxing_op->output_bns()) {
+    Shape* output_shape_ptr = bn2shape_ptr.at(bn);
+    ASSERT_EQ(*output_shape_ptr, *data_tmp_shape_ptr);
+  }
 }
 
 }  // namespace oneflow
