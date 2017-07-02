e4ae6e26af73c9826969d635d7ca747da3f79494
LeGend-AI <legend-ai@outlook.com>
2017-07-02 16:06:33 +0800

    implement KernelTestCommon

---
+++ oneflow/core/kernel/kernel_test_common.cu
@@ -0,0 +1,91 @@
+#include <random>
+#include "oneflow/core/device/cuda_device_context.h"
+#include "oneflow/core/kernel/kernel_test_common.h"
+
+namespace oneflow {
+
+namespace test {
+
+template<typename FloatingPointType>
+class KernelTestCommon<DeviceType::kGPU, FloatingPointType> final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(KernelTestCommon);
+  KernelTestCommon() = delete;
+
+  static Blob* CreateBlobWithVector(const std::vector<int64_t>& dim_vec,
+                                    FloatingPointType* data_vec) {
+    void* dptr;
+    Shape* shape = new Shape(dim_vec);
+    size_t dptr_size = shape->elem_cnt() * sizeof(FloatingPointType);
+    CHECK_EQ(cudaMalloc(&dptr, dptr_size), cudaSuccess);
+    CHECK_EQ(cudaMemcpy(dptr, data_vec, dptr_size, cudaMemcpyHostToDevice),
+             cudaSuccess);
+    return new Blob(dptr, shape);
+  }
+
+  static Blob* CreateBlobWithSameValue(const std::vector<int64_t>& dim_vec,
+                                       FloatingPointType value) {
+    Shape* shape = new Shape(dim_vec);
+    FloatingPointType* data_vec = new FloatingPointType[shape->elem_cnt()];
+    std::fill(data_vec, data_vec + shape->elem_cnt(), value);
+    return KernelTestCommon<DeviceType::kGPU,
+                            FloatingPointType>::CreateBlobWithVector(dim_vec,
+                                                                     data_vec);
+  }
+
+  static Blob* CreateBlobWithRandomValue(const std::vector<int64_t>& dim_vec) {
+    Shape* shape = new Shape(dim_vec);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<FloatingPointType> dis(0, 10);
+    FloatingPointType* data_vec = new FloatingPointType[shape->elem_cnt()];
+    for (int64_t i = 0; i != shape->elem_cnt(); ++i) { data_vec[i] = dis(gen); }
+    return KernelTestCommon<DeviceType::kGPU,
+                            FloatingPointType>::CreateBlobWithVector(dim_vec,
+                                                                     data_vec);
+  }
+
+  static void BuildKernelCtx(KernelCtx* ctx) {
+    cudaStream_t* cuda_stream = new cudaStream_t;
+    cublasHandle_t* cublas_handle = new cublasHandle_t;
+    CHECK_EQ(cudaStreamCreate(cuda_stream), cudaSuccess);
+    CHECK_EQ(cublasCreate(cublas_handle), CUBLAS_STATUS_SUCCESS);
+    CHECK_EQ(cublasSetStream(*cublas_handle, *cuda_stream),
+             CUBLAS_STATUS_SUCCESS);
+    ctx->device_ctx = new CudaDeviceCtx(cuda_stream, cublas_handle, nullptr);
+  }
+
+  static void SyncStream(KernelCtx* ctx) {
+    CHECK_EQ(cudaStreamSynchronize(ctx->device_ctx->cuda_stream()),
+             cudaSuccess);
+  }
+
+  static void BlobCmp(Blob* lhs, Blob* rhs) {
+    FloatingPointType* dptr;
+    size_t dptr_size = lhs->shape().elem_cnt() * sizeof(FloatingPointType);
+    cudaMallocHost(&dptr, dptr_size);
+    memset(dptr, 0, dptr_size);
+    Blob* copy_lhs = KernelTestCommon<DeviceType::kCPU, FloatingPointType>::
+        CreateBlobWithVector(lhs->shape().dim_vec(), dptr);
+    Blob* copy_rhs = KernelTestCommon<DeviceType::kCPU, FloatingPointType>::
+        CreateBlobWithVector(rhs->shape().dim_vec(), dptr);
+    cudaMemcpy(copy_lhs->mut_dptr(), lhs->dptr(), dptr_size,
+               cudaMemcpyDeviceToHost);
+    cudaMemcpy(copy_rhs->mut_dptr(), rhs->dptr(), dptr_size,
+               cudaMemcpyDeviceToHost);
+
+    KernelTestCommon<DeviceType::kCPU, FloatingPointType>::BlobCmp(copy_lhs,
+                                                                   copy_rhs);
+  }
+
+  static void CheckResult(
+      std::function<Blob*(const std::string&)> BnInOp2BlobPtr,
+      const std::string& check, const std::string& expected) {
+    KernelTestCommon<DeviceType::kGPU, FloatingPointType>::BlobCmp(
+        BnInOp2BlobPtr(check), BnInOp2BlobPtr(expected));
+  }
+};
+INSTANTIATE_GPU_KERNEL_TEST_COMMON_CLASS(KernelTestCommon);
+
+}  // namespace test
+}  // namespace oneflow
