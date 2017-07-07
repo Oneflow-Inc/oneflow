4a55fdcd45a06c4a4f14ceecf776e3acfc34d7d5
duduscript <duduscript@outlook.com>
2017-07-07 20:01:06 +0800

    fix bug

--- oneflow/core/kernel/model_update_kernel.cpp
+++ oneflow/core/kernel/model_update_kernel.cpp
@@ -9,10 +9,11 @@ void MdUpdateKernel<device_type, FloatingPointType>::Backward(
   Blob* model_blob = BnInOp2BlobPtr("model");
   Blob* model_diffs_blob = BnInOp2BlobPtr("model_diffs");
   float learn_rate = op()->op_conf().model_update_conf().learn_rate();
+  std::cout << learn_rate << std::endl;
   KernelUtil<device_type, FloatingPointType>::BlasAxpy(
       ctx, model_blob->shape().elem_cnt(), -learn_rate,
-      static_cast<const FloatingPointType*>(model_blob->dptr()), 1,
-      static_cast<FloatingPointType*>(model_diffs_blob->mut_dptr()), 1);
+      static_cast<const FloatingPointType*>(model_diffs_blob->dptr()), 1,
+      static_cast<FloatingPointType*>(model_blob->mut_dptr()), 1);
 }
 
 INSTANTIATE_KERNEL_CLASS(MdUpdateKernel);
