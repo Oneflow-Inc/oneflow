d0f2a0e10b782cce68b53df3b088ee8f31875623
duduscript <duduscript@outlook.com>
2017-07-08 14:00:43 +0800

    function problem

--- oneflow/core/kernel/model_update_kernel.cpp
+++ oneflow/core/kernel/model_update_kernel.cpp
@@ -3,7 +3,7 @@
 namespace oneflow {
 
 template<DeviceType device_type, typename FloatingPointType>
-void MdUpdateKernel<device_type, FloatingPointType>::Backward(
+void MdUpdateKernel<device_type, FloatingPointType>::Forward(
     const KernelCtx& ctx,
     std::function<Blob*(const std::string&)> BnInOp2BlobPtr) const {
   Blob* model_blob = BnInOp2BlobPtr("model");
@@ -11,7 +11,7 @@ void MdUpdateKernel<device_type, FloatingPointType>::Backward(
   float learning_rate = op()->op_conf().model_update_conf().learning_rate();
   float alpha = -learning_rate / JobDesc::Singleton()->total_piece_num();
 
-  // model = model - learning_rate * model * model_diff
+  // model = model - alpha * model_diff
   KernelUtil<device_type, FloatingPointType>::BlasAxpy(
       ctx, model_blob->shape().elem_cnt(), alpha,
       static_cast<const FloatingPointType*>(model_diffs_blob->dptr()), 1,
