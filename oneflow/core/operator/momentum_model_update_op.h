670193ba276c167d1101784633c909694d5b2494
duduscript <duduscript@outlook.com>
2017-07-13 11:39:46 +0800

    add momentum op

---
+++ oneflow/core/operator/momentum_model_update_op.h
@@ -0,0 +1,28 @@
+#ifndef ONEFLOW_CORE_OPERATOR_MOMENTUM_MODEL_UPDATE_OP_H_
+#define ONEFLOW_CORE_OPERATOR_MOMENTUM_MODEL_UPDATE_OP_H_
+
+#include "oneflow/core/operator/operator_manager.h"
+
+namespace oneflow {
+
+class MomentumModelUpdateOp final : public SysOperator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(MomentumModelUpdateOp);
+  MomentumModelUpdateOp() = default;
+  ~MomentumModelUpdateOp() = default;
+
+  void InitFromOpConf(const OperatorConf& op_conf) override;
+  const PbMessage& GetSpecialConf() const override;
+
+ private:
+  std::string ibn2lbn(const std::string& input_bn) const override {
+    return kPackedBlobName;
+  }
+  std::string obn2lbn(const std::string& output_bn) const override {
+    return kPackedBlobName;
+  }
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_MOMENTUM_MODEL_UPDATE_OP_H_
