46905e17fb084fbcb951a64f1c8e4bc71c1397e5
syshangguan <shgshy@gmail.com>
2017-07-15 05:37:48 +0800

    add pooling_kernel

--- oneflow/core/kernel/pooling_kernel.cpp
+++ oneflow/core/kernel/pooling_kernel.cpp
@@ -2,25 +2,62 @@
 
 namespace oneflow {
 
+namespace {
+
+template<FloatingPointType>
+std::pair<size_t, FloatingPointType> GetMaxValue(
+    FloatingPointType* dptr, size_t pad_height, size_t pad_weight) {
+  
+}
+
+template<FloatingPointType>
+std::pair<size_t, FloatingPointType> GetAveValue() {
+  
+}
+
+template<FloatingPointType>
+std::pair<size_t, FloatingPointType> GetStoValue();
+
+}  // namespace
+
+template<typename FloatingPointType>
+void PoolingKernel<DeviceType::kCPU, FloatingPointType>::InitFromOpProto(
+    const OperatorProto& op_proto) {
+  Kernel::InitFromOpProto(op_proto);
+
+  switch(op()->op_conf().pooling_conf().pool()) {
+    case PoolingOpConf::MAX:
+      PoolingMethodFunc_ = &GetMaxValue;
+      break;
+    case PoolingOpConf::AVE:
+      PoolingMethodFunc_ = &GetAveValue;
+      break;
+    case PoolingOpConf::STOCHASTIC:
+      PoolingMethodFunc_ = &GetStoValue;
+      break;
+    default:
+      UNEXPECTED_RUN();
+  }
+}
+
 template<typename FloatingPointType>
 void PoolingKernel<DeviceType::kCPU, FloatingPointType>::Forward(
-    const KernelCtx&,
-    std::function<Blob*(const std::string&)> bn_in_op2blob_ptr) const {
-  Blob* in_blob = bn_in_op2blob_ptr("in");
-  Blob* out_blob = bn_in_op2blob_ptr("out");
-  PoolingOpConf_PoolMethod pool_method = op()->op_conf().pooling_conf().pool();
-  switch (pool_method) {
-    case PoolingOpConf_PoolMethod_MAX: break;
-    case PoolingOpConf_PoolMethod_AVE: break;
-    case PoolingOpConf_PoolMethod_STOCHASTIC: break;
-  }
+    const KernelCtx& ctx,
+    std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* in_blob = BnInOp2Blob("in");
+  Blob* out_blob = BnInOp2Blob("out");
+  Blob* data_tmp = BnInOp2Blob("idx");
+    
 }
 
 template<typename FloatingPointType>
 void PoolingKernel<DeviceType::kCPU, FloatingPointType>::Backward(
     const KernelCtx&,
     std::function<Blob*(const std::string&)> bn_in_op2blob_ptr) const {
-  TODO();
+  const Blob* out_diff_blob = BnInOp2Blob("out_diff");
+  Blob* in_diff_blob = BnInOp2Blob("in_diff");
+  Blob* data_temp = BnInOp2Blob("idx");
+
 }
 
 INSTANTIATE_CPU_KERNEL_CLASS(PoolingKernel);
--- oneflow/core/kernel/pooling_kernel.h
+++ oneflow/core/kernel/pooling_kernel.h
@@ -6,23 +6,7 @@
 namespace oneflow {
 
 template<DeviceType device_type, typename FloatingPointType>
-class PoolingKernel;
-
-template<typename FloatingPointType>
-class PoolingKernel<DeviceType::kCPU, FloatingPointType> final : public Kernel {
- public:
-  OF_DISALLOW_COPY_AND_MOVE(PoolingKernel);
-  PoolingKernel() = default;
-  ~PoolingKernel() = default;
-
-  void Forward(const KernelCtx&,
-               std::function<Blob*(const std::string&)>) const override;
-  void Backward(const KernelCtx&,
-                std::function<Blob*(const std::string&)>) const override;
-};
-
-template<typename FloatingPointType>
-class PoolingKernel<DeviceType::kGPU, FloatingPointType> final : public Kernel {
+class PoolingKernel final : public Kernel {
  public:
   OF_DISALLOW_COPY_AND_MOVE(PoolingKernel);
   PoolingKernel() = default;
