283e1ae8169d20865dbe20fb051c332955f0bfae
duduscript <duduscript@outlook.com>
2017-07-15 17:04:38 +0800

    RMSProp Model Update Kernel (#202)
    * basic model update
    
    * spell mistake
    
    * fix bug
    
    * remove a log line
    
    * add model_update_kernel_test
    
    * fix some problem in model_update
    
    * function problem
    
    * judge isfinite and change function
    
    * fix batch problem
    
    * remove model update test
    
    * model_update_kernel_test
    
    * change a name
    
    * add two function in kernel_util
    
    * add a function in kernel_util
    
    * remove functions in kernel_util
    
    * add op in conf
    
    * conflict
    
    * RMSProp model update kernel
    
    * remove conflict
    
    * fix comment error
    
    * add const
    
    * conflict
    
    * conflict
    
    * add operator
    
    * fix some problem in rmsprop model update kernel

---
+++ oneflow/core/kernel/rmsprop_model_update_kernel.cpp
@@ -0,0 +1,70 @@
+#include "oneflow/core/kernel/rmsprop_model_update_kernel.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename FloatingPointType>
+void RMSPropMdUpdateKernel<device_type, FloatingPointType>::Forward(
+    const KernelCtx& ctx,
+    std::function<Blob*(const std::string&)> BnInOp2BlobPtr) const {
+  Blob* model_blob = BnInOp2BlobPtr("model");
+  Blob* model_diffs_blob = BnInOp2BlobPtr("model_diffs");
+  Blob* mean_square_blob = BnInOp2BlobPtr("mean_square");
+  float learning_rate =
+      op()->op_conf().rmsprop_model_update_conf().learning_rate();
+  float decay_rate = op()->op_conf().rmsprop_model_update_conf().decay_rate();
+  float epsilon = op()->op_conf().rmsprop_model_update_conf().epsilon();
+  float alpha = learning_rate / JobDesc::Singleton()->batch_size();
+  CHECK(std::isfinite(alpha));
+
+  RMSPropMdUpdateKernelUtil<device_type, FloatingPointType>::UpdateMeanSquare(
+      ctx, mean_square_blob->shape().elem_cnt(),
+      static_cast<FloatingPointType>(decay_rate),
+      static_cast<FloatingPointType*>(mean_square_blob->mut_dptr()),
+      static_cast<const FloatingPointType*>(model_diffs_blob->dptr()));
+
+  RMSPropMdUpdateKernelUtil<device_type, FloatingPointType>::UpdateModel(
+      ctx, model_blob->shape().elem_cnt(),
+      static_cast<FloatingPointType*>(model_blob->mut_dptr()),
+      static_cast<const FloatingPointType*>(model_diffs_blob->dptr()),
+      static_cast<const FloatingPointType*>(mean_square_blob->dptr()),
+      static_cast<FloatingPointType>(epsilon),
+      static_cast<FloatingPointType>(alpha));
+}
+
+template<typename FloatingPointType>
+class RMSPropMdUpdateKernelUtil<DeviceType::kCPU, FloatingPointType> final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(RMSPropMdUpdateKernelUtil);
+  RMSPropMdUpdateKernelUtil() = delete;
+
+  static void UpdateMeanSquare(const KernelCtx& ctx, const int64_t n,
+                               const FloatingPointType decay_rate,
+                               FloatingPointType* mean_square,
+                               const FloatingPointType* model_diff) {
+    ctx.device_ctx->cpu_stream()->SendWork([=]() {
+      for (int64_t i = 0; i < n; ++i) {
+        mean_square[i] = decay_rate * mean_square[i]
+                         + (1 - decay_rate) * model_diff[i] * model_diff[i];
+      }
+    });
+  }
+
+  static void UpdateModel(const KernelCtx& ctx, const int64_t n,
+                          FloatingPointType* model,
+                          const FloatingPointType* model_diff,
+                          const FloatingPointType* mean_square,
+                          const FloatingPointType epsilon,
+                          const FloatingPointType alpha) {
+    ctx.device_ctx->cpu_stream()->SendWork([=]() {
+      for (int64_t i = 0; i < n; ++i) {
+        model[i] -= alpha * model_diff[i] / (std::sqrt(mean_square[i]) + epsilon);
+      }
+    });
+  }
+};
+
+INSTANTIATE_CPU_KERNEL_UTIL_CLASS(RMSPropMdUpdateKernelUtil);
+INSTANTIATE_KERNEL_CLASS(RMSPropMdUpdateKernel);
+REGISTER_KERNEL(OperatorConf::kRmspropModelUpdateConf, RMSPropMdUpdateKernel);
+
+}  // namespace oneflow
