4751560a4abe32a8d3e9ecd3bfc71c6ea5274f9e
duduscript <duduscript@outlook.com>
2017-07-15 16:01:34 +0800

    add operator

---
+++ oneflow/core/operator/rmsprop_model_update_op.cpp
@@ -0,0 +1,20 @@
+#include "oneflow/core/operator/rmsprop_model_update_op.h"
+
+namespace oneflow {
+
+void RMSPropModelUpdateOp::InitFromOpConf(const OperatorConf& op_conf) {
+  CHECK(op_conf.has_rmsprop_model_update_conf());
+  mut_op_conf() = op_conf;
+
+  EnrollInputBn("model_diffs", false);
+  EnrollModelTmpBn("mean_square");
+  EnrollOutputBn("model", false);
+}
+
+const PbMessage& RMSPropModelUpdateOp::GetSpecialConf() const {
+  return op_conf().rmsprop_model_update_conf();
+}
+
+REGISTER_OP(OperatorConf::kRmspropModelUpdateConf, RMSPropModelUpdateOp);
+
+}  // namespace oneflow
