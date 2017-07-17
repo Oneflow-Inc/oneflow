9321a4034d93ee0cb147a704373079d7ad05dfe2
chengtbf <472491134@qq.com>
2017-07-17 19:08:39 +0800

    softmaxWithLossOp (#210)
    * softmaxWithLossOp
    
    * remove normalization mode

--- oneflow/core/operator/softmax_loss_op.h
+++ oneflow/core/operator/softmax_loss_op.h
@@ -1,18 +1,19 @@
-#ifndef ONEFLOW_CORE_OPERATOR_SOFTMAX_OP_H_
-#define ONEFLOW_CORE_OPERATOR_SOFTMAX_OP_H_
+#ifndef ONEFLOW_CORE_OPERATOR_SOFTMAX_LOSS_OP_H_
+#define ONEFLOW_CORE_OPERATOR_SOFTMAX_LOSS_OP_H_
 
 #include "oneflow/core/operator/operator_manager.h"
 
 namespace oneflow {
 
-class SoftmaxOp final : public UserOperator {
+class SoftmaxLossOp final : public UserOperator {
  public:
-  OF_DISALLOW_COPY_AND_MOVE(SoftmaxOp);
-  SoftmaxOp() = default;
-  ~SoftmaxOp() = default;
+  OF_DISALLOW_COPY_AND_MOVE(SoftmaxLossOp);
+  SoftmaxLossOp() = default;
+  ~SoftmaxLossOp() = default;
 
   void InitFromOpConf(const OperatorConf& op_conf) override;
   const PbMessage& GetSpecialConf() const override;
+  bool IsLossOp() const override { return true; }
 
   void InferShape4FwBlobs(
       std::function<Shape*(const std::string&)> GetShapePtr4BnInOp,
@@ -24,4 +25,4 @@ class SoftmaxOp final : public UserOperator {
 
 }  // namespace oneflow
 
-#endif  // ONEFLOW_CORE_OPERATOR_SOFTMAX_OP_H_
+#endif  // ONEFLOW_CORE_OPERATOR_SOFTMAX_LOSS_OP_H_
