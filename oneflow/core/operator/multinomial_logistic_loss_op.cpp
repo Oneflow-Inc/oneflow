074c023f6e3793da7d8b6f8196377d13c1b09d84
chengtbf <472491134@qq.com>
2017-07-18 14:22:06 +0800

    delete useless code for input_diff_blob infer shape (#213)

--- oneflow/core/operator/multinomial_logistic_loss_op.cpp
+++ oneflow/core/operator/multinomial_logistic_loss_op.cpp
@@ -21,12 +21,6 @@ void MultinomialLogisticLossOp::InferShape4FwBlobs(
     ParallelPolicy policy, int64_t parallel_id, int64_t parallel_num) const {
   *GetShapePtr4BnInOp(SoleObn()) = Shape({1});
   *GetShapePtr4BnInOp(SoleDtbn()) = Shape({1});
-  for (size_t i = 0; i < input_diff_bns().size(); ++i) {
-    Shape* input_diff_shape_ptr = GetShapePtr4BnInOp(input_diff_bns().at(i));
-    if (input_diff_shape_ptr != nullptr) {
-      *input_diff_shape_ptr = *GetShapePtr4BnInOp(input_bns().at(i));
-    }
-  }
 }
 
 REGISTER_OP(OperatorConf::kMultinomialLogisticLossConf,
