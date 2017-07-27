0f5f2a5bc247e19cb2418bfbe57e60c40c8835e6
willzhang4a58 <willzhang4a58@gmail.com>
2017-07-27 12:06:25 +0800

    fix bug in comp_actor, set correct policy and parallel_num

--- single_machine_test.sh
+++ single_machine_test.sh
@@ -7,7 +7,7 @@ GLOG_logtostderr=0 GLOG_log_dir=./log ./compiler \
     -job_conf_filepath="../prototxt/single_machine_job.prototxt" \
     -plan_filepath="./plan" \
 
-GLOG_logtostderr=0 GLOG_log_dir=./log GLOG_logbuflevel=-1 valgrind --tool=memcheck ./runtime \
+GLOG_logtostderr=0 GLOG_log_dir=./log GLOG_logbuflevel=-1 ./runtime \
     -plan_filepath="./plan" \
     -this_machine_name="centos-0"
 
