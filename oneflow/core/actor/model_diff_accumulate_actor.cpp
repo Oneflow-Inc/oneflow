779f6ce0d2aa85c9e5bddced563b20445f092e7b
Will Zhang <willzhang_bit@163.com>
2017-07-28 17:23:49 +0800

    remove meaningless bp task (#242)
    * debug
    
    * debug

--- oneflow/core/actor/model_diff_accumulate_actor.cpp
+++ oneflow/core/actor/model_diff_accumulate_actor.cpp
@@ -28,8 +28,13 @@ int MdDiffAccActor::HandlerNormal(const ActorMsg& msg) {
   } else if (msg.msg_type() == ActorMsgType::kRegstMsg) {
     if (TryUpdtStateAsProducedRegst(msg.regst_wrapper()->regst_raw_ptr())
         != 0) {
+      auto regst_wp = msg.regst_wrapper();
       mut_num_of_read_empty() = 0;
-      waiting_in_regst_.push(msg.regst_wrapper());
+      waiting_in_regst_.push(regst_wp);
+      VLOG(4) << "model diff accumulate actor " << actor_id() << " "
+              << "receive readable regst " << regst_wp->regst_raw_ptr() << ", "
+              << "regst_desc_id:" << regst_wp->regst_desc_id() << ", "
+              << "current num_of_read_empty:" << num_of_read_empty();
     }
     ActUntilFail();
   } else {
