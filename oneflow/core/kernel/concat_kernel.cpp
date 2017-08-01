42dc9bcda4f0c418790aa15694ce2d409c501a79
bubuface1987 <bydu1987@gmail.com>
2017-08-01 20:38:21 +0800

    modify variable name and add comment

--- oneflow/core/kernel/concat_kernel.cpp
+++ oneflow/core/kernel/concat_kernel.cpp
@@ -4,11 +4,19 @@ namespace oneflow {
 
 namespace {
 
+// Calculates the addr of the next concat point in blob. Ex:
+// For a 2-dimension matrix with columns and rows, assume the concat operation
+// is on the row dimension, so each row will be concated/extended. The
+// start_addr is the address of element(0,0), concat_idx indicates the index of
+// the current row to be concated. concat_axis_dim is the row length/dimension.
+// offset is the concat position in each row. The elem_cnt is 1 for 2-dimension
+// matrix and is calculated by Count(concat_axis, NumAxes()) for higher
+// dimension matrix.
 template<typename FloatingPointType>
-FloatingPointType* CalcCopyAddr(FloatingPointType* start_addr, int64_t cp_times,
-                                int64_t axis_dim, int64_t offset,
-                                int64_t elem_cnt) {
-  return start_addr + (cp_times * axis_dim + offset) * elem_cnt;
+FloatingPointType* NextConcatAddr(FloatingPointType* start_addr,
+                                  int64_t concat_idx, int64_t concat_axis_dim,
+                                  int64_t offset, int64_t elem_cnt) {
+  return start_addr + (concat_idx * concat_axis_dim + offset) * elem_cnt;
 }
 
 }  // namespace
@@ -32,16 +40,23 @@ void ConcatKernel<device_type, FloatingPointType>::ConcatKernelWork(
     concat_num_each_blob = out_blob->shape().Count(0, concat_axis);
   }
   const int64_t out_concat_axis_dim = out_blob->shape().At(concat_axis);
-  FloatingPointType* out_blob_dptr = out_blob->mut_dptr<FloatingPointType>();
+  FloatingPointType* out_blob_mut_dptr =
+      out_blob->mut_dptr<FloatingPointType>();
   int64_t offset_concat_axis = 0;
-  cudaMemcpyKind kind = cudaMemcpyKind::cudaMemcpyHostToHost;
-  if (device_type == DeviceType::kGPU) {
+  cudaMemcpyKind kind;
+  if (device_type == DeviceType::kCPU) {
+    kind = cudaMemcpyKind::cudaMemcpyHostToHost;
+  } else if (device_type == DeviceType::kGPU) {
     kind = cudaMemcpyKind::cudaMemcpyDeviceToDevice;
+  } else {
+    LOG(FATAL) << "device type has not been set";
+    return;
   }
 
   for (const std::string& in_bn : in_bns) {
-    const Blob* in_blob = BnInOp2BlobPtr(in_bn);
-    FloatingPointType* in_blob_dptr = in_blob->mut_dptr<FloatingPointType>();
+    Blob* in_blob = BnInOp2BlobPtr(in_bn);
+    FloatingPointType* in_blob_mut_dptr =
+        in_blob->mut_dptr<FloatingPointType>();
     const int64_t in_concat_axis_dim = in_blob->shape().At(concat_axis);
     const int64_t cp_sz =
         in_concat_axis_dim * concat_element_cnt * sizeof(FloatingPointType);
@@ -49,10 +64,11 @@ void ConcatKernel<device_type, FloatingPointType>::ConcatKernelWork(
     for (int64_t concat_idx = 0; concat_idx < concat_num_each_blob;
          ++concat_idx) {
       FloatingPointType* out_cp_adr =
-          CalcCopyAddr(out_blob_dptr, concat_idx, out_concat_axis_dim,
-                       offset_concat_axis, concat_element_cnt);
-      FloatingPointType* in_cp_adr = CalcCopyAddr(
-          in_blob_dptr, concat_idx, in_concat_axis_dim, 0, concat_element_cnt);
+          NextConcatAddr(out_blob_mut_dptr, concat_idx, out_concat_axis_dim,
+                         offset_concat_axis, concat_element_cnt);
+      FloatingPointType* in_cp_adr =
+          NextConcatAddr(in_blob_mut_dptr, concat_idx, in_concat_axis_dim, 0,
+                         concat_element_cnt);
       copy_func(ctx, in_cp_adr, out_cp_adr, cp_sz, kind);
     }
 
