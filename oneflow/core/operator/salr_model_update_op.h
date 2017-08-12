671db1bf78c29a3f63c5d673c359b16fee4e4fa0
duduscript <duduscript@outlook.com>
2017-08-12 15:10:20 +0800

    salr_model_update_op

---
+++ oneflow/core/operator/salr_model_update_op.h
@@ -0,0 +1,35 @@
+#ifndef ONEFLOW_CORE_OPERATOR_SALR_MODEL_UPDATE_OP_H_
+#define ONEFLOW_CORE_OPERATOR_SALR_MODEL_UPDATE_OP_H_
+
+#include "oneflow/core/operator/operator_manager.h"
+
+namespace oneflow {
+
+class SALRModelUpdateOp final : public SysOperator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SALRModelUpdateOp);
+  SALRModelUpdateOp() = default;
+  ~SALRModelUpdateOp() = default;
+
+  void InitFromOpConf(const OperatorConf& op_conf) override;
+  const PbMessage& GetSpecialConf() const override;
+  void InferShape4FwBlobs(
+      std::function<Shape*(const std::string&)> GetShapePtr4BnInOp,
+      ParallelPolicy policy, int64_t parallel_id,
+      int64_t parallel_num) const override;
+
+ private:
+  std::string ibn2lbn(const std::string& input_bn) const override {
+    return kPackedBlobName;
+  }
+  std::string obn2lbn(const std::string& output_bn) const override {
+    return kPackedBlobName;
+  }
+  std::string mtbn2lbn(const std::string& output_bn) const override {
+    return kPackedBlobName;
+  }
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_SALR_MODEL_UPDATE_OP_H_
