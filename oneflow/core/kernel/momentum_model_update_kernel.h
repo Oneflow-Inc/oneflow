d83ec51d710cbc736952195e801c3863b9a37cf5
duduscript <duduscript@outlook.com>
2017-08-12 13:59:31 +0800

    fix version problem

--- oneflow/core/kernel/momentum_model_update_kernel.h
+++ oneflow/core/kernel/momentum_model_update_kernel.h
@@ -1,12 +1,12 @@
 #ifndef ONEFLOW_CORE_KERNEL_MOMENTUM_MODEL_UPDATE_KERNEL_H_
 #define ONEFLOW_CORE_KERNEL_MOMENTUM_MODEL_UPDATE_KERNEL_H_
 
-#include "oneflow/core/kernel/kernel_manager.h"
+#include "oneflow/core/kernel/model_update_kernel.h"
 
 namespace oneflow {
 
 template<DeviceType device_type, typename FloatingPointType>
-class MomentumMdUpdateKernel final : public Kernel {
+class MomentumMdUpdateKernel final : public ModelUpdtKernel {
  public:
   OF_DISALLOW_COPY_AND_MOVE(MomentumMdUpdateKernel);
   MomentumMdUpdateKernel() = default;
@@ -15,10 +15,9 @@ class MomentumMdUpdateKernel final : public Kernel {
   void Forward(const KernelCtx&,
                std::function<Blob*(const std::string&)>) const override;
 
-  void Backward(const KernelCtx&,
-                std::function<Blob*(const std::string&)>) const override {
-    UNEXPECTED_RUN();
-  }
+  void InitDataTmpBlobs(
+      const KernelCtx& ctx,
+      std::function<Blob*(const std::string&)>) const override;
 
  private:
 };
