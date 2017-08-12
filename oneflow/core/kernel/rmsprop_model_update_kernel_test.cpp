5a19582d407d02b628f5ae72c03b4fd0e3b749bc
duduscript <duduscript@outlook.com>
2017-08-12 14:02:12 +0800

    Fix Rmsprop Model Update (#260)
    * fix implementation inconformite with formula
    
    * fix bug -- innerproduct bias shape
    
    * add fillconf for rmsprop
    
    * remove fix
    
    * fix rmsprop problem
    
    * fix a bug in momentum model update kernel
    
    * RMSPROP change fill method
    
    * remove change in momentum
    
    * fix version problem

--- oneflow/core/kernel/rmsprop_model_update_kernel_test.cpp
+++ oneflow/core/kernel/rmsprop_model_update_kernel_test.cpp
@@ -8,15 +8,13 @@ namespace test {
 namespace {
 
 template<DeviceType device_type, typename FloatingPointType>
-Kernel* BuildRMSPropMdUpdateKernel(float learning_rate, float decay_rate,
-                                   float epsilon) {
+Kernel* BuildRMSPropMdUpdateKernel(float learning_rate, float decay_rate) {
   OperatorConf op_conf;
   op_conf.set_name("rmsprop_model_update_test");
   RMSPropModelUpdateOpConf* rmsprop_md_update_conf =
       op_conf.mutable_rmsprop_mdupdt_conf();
   rmsprop_md_update_conf->set_learning_rate(learning_rate);
   rmsprop_md_update_conf->set_decay_rate(decay_rate);
-  rmsprop_md_update_conf->set_epsilon(epsilon);
   auto rmsprop_md_update_op = ConstructOp(op_conf);
   OperatorProto op_proto;
   rmsprop_md_update_op->ToProto(&op_proto);
@@ -34,19 +32,18 @@ void InitJobDesc(int32_t piece_size, int32_t num_of_pieces_in_batch) {
 }
 
 template<DeviceType device_type, typename FloatingPointType>
-std::function<Blob*(const std::string&)> BuildBnInOp2BlobPtr() {
+std::function<Blob*(const std::string&)> BuildBnInOp2BlobPtr(
+    std::vector<int64_t>& dim_vec) {
   using KTCommon = KernelTestCommon<device_type, FloatingPointType>;
 
-  std::vector<int64_t> dim_vec = {1, 3, 2};
-
   auto bn2blob_ptr = new HashMap<std::string, Blob*>;
-  (*bn2blob_ptr)["model"] = KTCommon::CreateBlobWithSameValue(dim_vec, 3);
-  (*bn2blob_ptr)["mean_square"] = KTCommon::CreateBlobWithSameValue(dim_vec, 2);
+  (*bn2blob_ptr)["model"] = KTCommon::CreateBlobWithSameValue(dim_vec, 2);
+  (*bn2blob_ptr)["mean_square"] = KTCommon::CreateBlobWithSameValue(dim_vec, 0);
   (*bn2blob_ptr)["model_diffs"] = KTCommon::CreateBlobWithSameValue(dim_vec, 2);
   (*bn2blob_ptr)["model_expected"] =
-      KTCommon::CreateBlobWithSameValue(dim_vec, 2);
+      KTCommon::CreateBlobWithSameValue(dim_vec, 0);
   (*bn2blob_ptr)["mean_square_expected"] =
-      KTCommon::CreateBlobWithSameValue(dim_vec, 3);
+      KTCommon::CreateBlobWithSameValue(dim_vec, 4);
   return [bn2blob_ptr](const std::string& bn) { return bn2blob_ptr->at(bn); };
 }
 
@@ -55,18 +52,22 @@ void TestRMSPropMdUpdateKernel() {
   using KTCommon = KernelTestCommon<device_type, FloatingPointType>;
   KernelCtx ctx;
   KTCommon::BuildKernelCtx(&ctx);
+  ctx.other = new int64_t(1);
 
+  std::vector<int64_t> dim_vec = {1, 3, 2};
   const float learning_rate = {2.0f};
-  const float decay_rate = {0.5f};
-  const float epsilon = {1.0f};
-  auto BnInOp2BlobPtr = BuildBnInOp2BlobPtr<device_type, FloatingPointType>();
+  const float decay_rate = 1.0f / 2;
+  auto BnInOp2BlobPtr =
+      BuildBnInOp2BlobPtr<device_type, FloatingPointType>(dim_vec);
   auto rmsprop_md_update_kernel =
-      BuildRMSPropMdUpdateKernel<device_type, FloatingPointType>(
-          learning_rate, decay_rate, epsilon);
+      BuildRMSPropMdUpdateKernel<device_type, FloatingPointType>(learning_rate,
+                                                                 decay_rate);
   int32_t piece_size = 1;
   int32_t num_of_pieces_in_batch = 2;
   InitJobDesc(piece_size, num_of_pieces_in_batch);
 
+  rmsprop_md_update_kernel->Forward(ctx, BnInOp2BlobPtr);
+  ctx.other = new int64_t(2);
   rmsprop_md_update_kernel->Forward(ctx, BnInOp2BlobPtr);
   KTCommon::SyncStream(&ctx);
 
