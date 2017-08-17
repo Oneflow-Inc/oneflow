c7f3740df544aec07422b80f7ab11f0352f5f981
jiyuan <jiyuan@WIN-ONEFLOW2>
2017-08-17 15:35:28 +0800

    add copy_comm_net kernel

---
+++ oneflow/core/kernel/copy_comm_net_kernel.h
@@ -0,0 +1,23 @@
+#ifndef ONEFLOW_CORE_KERNEL_COPY_COMM_NET_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_COPY_COMM_NET_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel_manager.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename FloatingPointType>
+class CopyCommNetKernel final : public Kernel {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(CopyCommNetKernel);
+  CopyCommNetKernel() = default;
+  ~CopyCommNetKernel() = default;
+
+  void Forward(const KernelCtx&,
+               std::function<Blob*(const std::string&)>) const override;
+  void Backward(const KernelCtx&,
+                std::function<Blob*(const std::string&)>) const override;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_COPY_COMM_NET_KERNEL_H_
