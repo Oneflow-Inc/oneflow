a0e47e709511affd1a25f9749b5187f89a6f9ae4
Xinqi <lixinqi0703106@163.com>
2017-08-17 15:48:59 +0800

    SccVisitor: strongly connected component visitor

---
+++ oneflow/core/schedule/scc_visitor.h
@@ -0,0 +1,105 @@
+/**
+ * Copyright 2017 Xinqi Li
+ */
+#ifndef ONEFLOW_CORE_SCHEDULE_SCC_VISITOR_H_
+#define ONEFLOW_CORE_SCHEDULE_SCC_VISITOR_H_
+
+#include <limits.h>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+#include "oneflow/core/common/util.h"
+#include "oneflow/core/job/plan.pb.h"
+#include "oneflow/core/schedule/snode.h"
+#include "oneflow/core/schedule/util.h"
+
+namespace oneflow {
+namespace schedule {
+
+//	strongly connected component visitor
+template<typename NodeType>
+class SccVisitor {
+ public:
+  typedef std::function<void(NodeType)> NodeVisitor;
+  typedef std::function<void(const std::list<NodeType>&)> ComponentVisitor;
+  typedef std::function<void(NodeType, const NodeVisitor&)> ForEachNode;
+
+  OF_DISALLOW_COPY_AND_MOVE(SccVisitor);
+  SccVisitor(const ForEachNode& foreach_next) : foreach_next_(foreach_next) {}
+  ~SccVisitor() = default;
+
+  uint32_t operator()(NodeType start,
+                      const ComponentVisitor& component_visitor) {
+    Reset(component_visitor);
+    Walk(start);
+    return component_cnt_;
+  }
+
+  template<template<class, class...> class C, typename... Args>
+  uint32_t operator()(const C<NodeType, Args...>& starts,
+                      const ComponentVisitor& component_visitor) {
+    Reset(component_visitor);
+    for (NodeType start : starts) { Walk(start); }
+    return component_cnt_;
+  }
+
+ private:
+  void Reset(const ComponentVisitor& component_visitor) {
+    component_visitor_ = component_visitor;
+    node2index_.clear();
+    node2low_.clear();
+    stack_.clear();
+    index_ = 0u;
+    component_cnt_ = 0u;
+  }
+
+  void Walk(NodeType node) {
+    if (node2index_[node]) { return; }
+    node2index_[node] = node2low_[node] = ++index_;
+    stack_.push_front(node);
+    node2is_on_stack_[node] = true;
+    foreach_next_(node, [&](NodeType next) {
+      if (!node2index_[next]) {
+        Walk(next);
+        node2low_[node] = std::min(node2low_[node], node2low_[next]);
+      } else if (node2is_on_stack_[next]) {
+        node2low_[node] = std::min(node2low_[node], node2low_[next]);
+      }
+    });
+    if (node2index_[node] == node2low_[node]) {
+      std::list<NodeType> scc;
+      NodeType w;
+      do {
+        w = stack_.front();
+        stack_.pop_front();
+        node2is_on_stack_[w] = false;
+        scc.push_front(w);
+      } while (w != node);
+      component_visitor_(scc);
+      ++component_cnt_;
+    }
+  }
+  ComponentVisitor component_visitor_;
+  ForEachNode foreach_next_;
+  std::unordered_map<NodeType, uint32_t> node2index_;
+  std::unordered_map<NodeType, uint32_t> node2low_;
+  std::unordered_map<NodeType, bool> node2is_on_stack_;
+  std::list<NodeType> stack_;
+  uint32_t index_ = 0u;
+  uint32_t component_cnt_ = 0u;
+};
+
+}  // namespace schedule
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_SCHEDULE_SCC_VISITOR_H_
---
+++ oneflow/core/schedule/scc_visitor_test.cpp
@@ -0,0 +1,90 @@
+#include "oneflow/core/schedule/scc_visitor.h"
+namespace oneflow {
+namespace schedule {
+
+TEST(SccVisitor, self_loop) {
+  std::unordered_map<int, int> data{
+      {1, 1},
+  };
+  auto foreach_next = [&](int x, const std::function<void(int)>& cb) {
+    cb(data[x]);
+  };
+  SccVisitor<int> scc(foreach_next);
+  uint32_t cnt = scc(1, [&](const std::list<int>& l) {
+    ASSERT_TRUE(l.size() == 1);
+    ASSERT_TRUE(l.front() == 1);
+  });
+  ASSERT_TRUE(cnt == 1);
+}
+
+TEST(SccVisitor, loop) {
+  std::unordered_map<int, int> data{
+      {1, 2}, {2, 3}, {3, 1},
+  };
+  auto foreach_next = [&](int x, const std::function<void(int)>& cb) {
+    cb(data[x]);
+  };
+  SccVisitor<int> scc(foreach_next);
+  uint32_t cnt = scc(1, [&](const std::list<int>& l) {
+    ASSERT_TRUE(l.size() == 3);
+    ASSERT_TRUE(l.front() == 1);
+  });
+  ASSERT_TRUE(cnt == 1);
+}
+
+TEST(SccVisitor, nested_loop) {
+  std::unordered_map<int, std::list<int>> data{{1, {2}},    {2, {3}}, {3, {4}},
+                                               {4, {2, 5}}, {5, {6}}, {6, {1}}};
+  auto foreach_next = [&](int x, const std::function<void(int)>& cb) {
+    for (auto itt = data[x].begin(); itt != data[x].end(); itt++) {
+      int n = *itt;
+      cb(n);
+    }
+  };
+  SccVisitor<int> scc(foreach_next);
+  uint32_t cnt = scc(1, [&](const std::list<int>& l) {
+    ASSERT_TRUE(l.size() == 6);
+    ASSERT_TRUE(l.front() == 1);
+  });
+  ASSERT_TRUE(cnt == 1);
+}
+
+TEST(SccVisitor, forest) {
+  std::unordered_map<int, int> data{
+      {1, 2}, {2, 3}, {3, 1}, {4, 5}, {5, 6}, {6, 4},
+  };
+  auto foreach_next = [&](int x, const std::function<void(int)>& cb) {
+    cb(data[x]);
+  };
+  SccVisitor<int> scc(foreach_next);
+  std::list<int> starts{1, 4};
+  uint32_t cnt = scc(starts, [&](const std::list<int>& l) {
+    ASSERT_TRUE(l.size() == 3);
+    ASSERT_TRUE(l.front() == 1 || l.front() == 4);
+  });
+  ASSERT_TRUE(cnt == 2);
+}
+
+TEST(SccVisitor, wikipedia_tarjan_demo) {
+  std::unordered_map<int, std::list<int>> data{
+      {1, {2}},    {2, {3}},    {3, {1}}, {4, {2, 3, 5}},
+      {5, {4, 6}}, {6, {3, 7}}, {7, {6}}, {8, {5, 7, 8}}};
+  auto foreach_next = [&](int x, const std::function<void(int)>& cb) {
+    for (auto itt = data[x].begin(); itt != data[x].end(); itt++) {
+      int n = *itt;
+      cb(n);
+    }
+  };
+  SccVisitor<int> scc(foreach_next);
+  std::list<int> nodes{1, 2, 3, 4, 5, 6, 7, 8};
+  uint32_t cnt = scc(nodes, [&](const std::list<int>& l) {
+    if (l.front() == 1) { ASSERT_TRUE(l.size() == 3); }
+    if (l.front() == 4) { ASSERT_TRUE(l.size() == 2); }
+    if (l.front() == 6) { ASSERT_TRUE(l.size() == 2); }
+    if (l.front() == 8) { ASSERT_TRUE(l.size() == 1); }
+  });
+  ASSERT_TRUE(cnt == 4);
+}
+
+}  // namespace schedule
+}  // namespace oneflow
