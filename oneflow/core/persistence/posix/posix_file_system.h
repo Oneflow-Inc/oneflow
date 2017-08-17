e4b6836a21fa17d25f527a4c9959a1c6bea1b79c
chengtbf <472491134@qq.com>
2017-08-17 20:33:53 +0800

    Dev file_system (#280)
    * add file_system interface
    
    * add windows and posix file system interface
    
    * implement some method in filesystem
    
    * remove function StatusToString and fix for review
    
    * add stauts item for file
    
    * add dir status
    
    * add error code to Status
    
    * add platform and win fs implement
    
    * add #define for different fs
    
    * delete Sync() in WriteableFile
    
    * fix bug about platform define

---
+++ oneflow/core/persistence/posix/posix_file_system.h
@@ -0,0 +1,54 @@
+#ifndef ONEFLOW_CORE_PERSISTENCE_POSIX_POSIX_FILE_SYSTEM_H_
+#define ONEFLOW_CORE_PERSISTENCE_POSIX_POSIX_FILE_SYSTEM_H_
+
+#include "oneflow/core/persistence/file_system.h"
+
+#ifdef PLATFORM_POSIX
+
+namespace oneflow {
+
+namespace fs {
+
+class PosixFileSystem final : public FileSystem {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(PosixFileSystem);
+  PosixFileSystem() = default;
+  ~PosixFileSystem() = default;
+
+  Status NewRandomAccessFile(
+      const std::string& fname,
+      std::unique_ptr<RandomAccessFile>* result) override;
+
+  Status NewWritableFile(const std::string& fname,
+                         std::unique_ptr<WritableFile>* result) override;
+
+  Status NewAppendableFile(const std::string& fname,
+                           std::unique_ptr<WritableFile>* result) override;
+
+  Status FileExists(const std::string& fname) override;
+
+  Status GetChildren(const std::string& dir,
+                     std::vector<std::string>* result) override;
+
+  Status DeleteFile(const std::string& fname) override;
+
+  Status CreateDir(const std::string& dirname) override;
+
+  Status DeleteDir(const std::string& dirname) override;
+
+  Status GetFileSize(const std::string& fname, uint64_t* file_size) override;
+
+  Status RenameFile(const std::string& src, const std::string& target) override;
+
+  Status IsDirectory(const std::string& fname) override;
+
+ private:
+};
+
+}  // namespace fs
+
+}  // namespace oneflow
+
+#endif  // PLATFORM_POSIX
+
+#endif  // ONEFLOW_CORE_PERSISTENCE_POSIX_POSIX_FILE_SYSTEM_H_
