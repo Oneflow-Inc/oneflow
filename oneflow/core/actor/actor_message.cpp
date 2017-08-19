9563f465f9b7f8ac5547768c41a018a09b6246a1
jiyuan <jiyuan@WIN-ONEFLOW2>
2017-08-19 09:13:20 +0800

    fix missing piece_id of network msg from producer

--- oneflow/core/actor/actor_message.cpp
+++ oneflow/core/actor/actor_message.cpp
@@ -20,6 +20,7 @@ ActorMsg ActorMsg::BuildReadableRegstMsg(int64_t writer_actor_id,
   msg.dst_actor_id_ = reader_actor_id;
   msg.msg_type_ = ActorMsgType::kRegstMsg;
   msg.regst_ = regst_raw_ptr;
+  msg.piece_id_ = regst_raw_ptr->piece_id();
   // if (IDMgr::Singleton()->MachineId4ActorId(reader_actor_id)
   //    == RuntimeCtx::Singleton()->this_machine_id()) {
   //  msg.regst_ = regst_raw_ptr;
