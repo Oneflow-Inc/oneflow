9b540d0c7ca4c905c07fa6a0ce02a4cbc5d00c50
chengtbf <472491134@qq.com>
2017-08-23 18:46:48 +0800

    refine loss op

--- oneflow/core/operator/multinomial_logistic_loss_op.cpp
+++ oneflow/core/operator/multinomial_logistic_loss_op.cpp
@@ -1,29 +1,48 @@
-//#include "oneflow/core/operator/multinomial_logistic_loss_op.h"
-//
-// namespace oneflow {
-//
-// void MultinomialLogisticLossOp::InitFromOpConf(const OperatorConf& op_conf) {
-//  CHECK(op_conf.has_multinomial_logistic_loss_conf());
-//  mut_op_conf() = op_conf;
-//
-//  EnrollInputBn("prediction");
-//  EnrollInputBn("label", false);
-//  EnrollOutputBn("loss", false);
-//  EnrollDataTmpBn("loss_buffer");
-//}
-//
-// const PbMessage& MultinomialLogisticLossOp::GetSpecialConf() const {
-//  return op_conf().multinomial_logistic_loss_conf();
-//}
-//
-// void MultinomialLogisticLossOp::InferBlobDesc4FwBlobs(
-//    std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
-//    ParallelPolicy policy, int64_t parallel_id, int64_t parallel_num) const {
-//  GetBlobDesc4BnInOp("loss")->mut_shape() = Shape({1});
-//  GetBlobDesc4BnInOp("loss_buffer")->mut_shape() = Shape({1});
-//}
-//
-// REGISTER_OP(OperatorConf::kMultinomialLogisticLossConf,
-//            MultinomialLogisticLossOp);
-//
-//}  // namespace oneflow
+#include "oneflow/core/operator/multinomial_logistic_loss_op.h"
+
+namespace oneflow {
+
+void MultinomialLogisticLossOp::InitFromOpConf(const OperatorConf& op_conf) {
+  CHECK(op_conf.has_multinomial_logistic_loss_conf());
+  mut_op_conf() = op_conf;
+
+  EnrollInputBn("prediction");
+  EnrollInputBn("label", false);
+  EnrollOutputBn("loss", false);
+  EnrollDataTmpBn("loss_buffer");
+}
+
+const PbMessage& MultinomialLogisticLossOp::GetSpecialConf() const {
+  return op_conf().multinomial_logistic_loss_conf();
+}
+
+void MultinomialLogisticLossOp::InferBlobDesc4FwBlobs(
+    std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
+    ParallelPolicy policy, int64_t parallel_id, int64_t parallel_num) const {
+  const MultinomialLogisticLossOpConf conf =
+      op_conf().multinomial_logistic_loss_conf();
+  const BlobDesc* pred_blob_desc = GetBlobDesc4BnInOp("prediction");
+  CHECK_EQ(pred_blob_desc->data_type(), conf.prediction().data_type());
+  CHECK_EQ(conf.prediction().data_type(),
+           JobDesc::Singleton()->default_data_type());
+  CHECK_EQ(conf.loss().data_type(), JobDesc::Singleton()->default_data_type());
+  const BlobDesc* label_blob_desc = GetBlobDesc4BnInOp("label");
+  CHECK_EQ(pred_blob_desc->has_data_id(), label_blob_desc->has_data_id());
+  // CHECK label data type is int
+  CHECK(IsIntegral(label_blob_desc->data_type()));
+  // loss
+  BlobDesc* loss_blob_desc = GetBlobDesc4BnInOp("loss");
+  loss_blob_desc->mut_shape() = Shape({1});
+  loss_blob_desc->set_data_type(conf.loss().data_type());
+  loss_blob_desc->set_has_data_id(pred_blob_desc->has_data_id());
+  // loss_buffer
+  BlobDesc* loss_buffer_blob_desc = GetBlobDesc4BnInOp("loss");
+  loss_buffer_blob_desc->mut_shape() = Shape({1});
+  loss_buffer_blob_desc->set_data_type(conf.loss().data_type());
+  loss_buffer_blob_desc->set_has_data_id(false);
+}
+
+REGISTER_OP(OperatorConf::kMultinomialLogisticLossConf,
+            MultinomialLogisticLossOp);
+
+}  // namespace oneflow
