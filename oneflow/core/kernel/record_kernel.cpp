176c522ea4298aa0b6cc135de45ee83a2cea4020
willzhang4a58 <willzhang4a58@gmail.com>
2017-08-24 17:45:51 +0800

    record

--- oneflow/core/kernel/record_kernel.cpp
+++ oneflow/core/kernel/record_kernel.cpp
@@ -1,13 +1,14 @@
-//#include "oneflow/core/kernel/record_kernel.h"
-//
-// namespace oneflow {
-//
-// template<typename FloatingPointType>
-// void RecordKernel<DeviceType::kCPU, FloatingPointType>::Forward(
-//    const KernelCtx& kernel_ctx,
-//    std::function<Blob*(const std::string&)> BnInOp2BlobPtr) const {}
-//
-// INSTANTIATE_CPU_KERNEL_CLASS(RecordKernel);
-// REGISTER_CPU_KERNEL(OperatorConf::kRecordConf, RecordKernel);
-//
-//}  // namespace oneflow
+#include "oneflow/core/kernel/record_kernel.h"
+
+namespace oneflow {
+
+void RecordKernel::Forward(
+    const KernelCtx& kernel_ctx,
+    std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  // TODO
+}
+
+COMMAND(AddKernelCreator(OperatorConf::kRecordConf, DeviceType::kCPU,
+                         [](const OperatorConf&) { return new RecordKernel; }));
+
+}  // namespace oneflow
