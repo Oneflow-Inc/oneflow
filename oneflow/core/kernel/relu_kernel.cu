cb081f839d39ed53d55da6319e8ef8d9460b93e1
Yi Zhu <619828575@qq.com>
2017-08-25 16:14:05 +0800

    Update model_save and relu kernel (#295)
    * updt relu operator
    
    * update relu and model_save operator
    
    * add check between input blob desc and op config for relu operator
    
    * update model_save and relu kernel
    
    * refine model_save and relu kernel

--- oneflow/core/kernel/relu_kernel.cu
+++ oneflow/core/kernel/relu_kernel.cu
@@ -1,49 +1,46 @@
-//#include "oneflow/core/device/cuda_util.h"
-//#include "oneflow/core/kernel/relu_kernel.h"
-//
-// namespace oneflow {
-//
-// namespace {
-//
-// template<typename FloatingPointType>
-//__global__ void ReluForwardGpu(const int64_t n, const FloatingPointType* in,
-//                               FloatingPointType* out) {
-//  CUDA_1D_KERNEL_LOOP(i, n) { out[i] = in[i] > 0 ? in[i] : 0; }
-//}
-//
-// template<typename FloatingPointType>
-//__global__ void ReluBackwardGpu(const int64_t n,
-//                                const FloatingPointType* out_diff,
-//                                const FloatingPointType* in,
-//                                FloatingPointType* in_diff) {
-//  CUDA_1D_KERNEL_LOOP(i, n) { in_diff[i] = in[i] > 0 ? out_diff[i] : 0; }
-//}
-//
-//}  // namespace
-//
-// template<typename FloatingPointType>
-// class ReluKernelUtil<DeviceType::kGPU, FloatingPointType> final {
-// public:
-//  OF_DISALLOW_COPY_AND_MOVE(ReluKernelUtil);
-//  ReluKernelUtil() = delete;
-//
-//  static void Forward(const KernelCtx& ctx, const int64_t n,
-//                      const FloatingPointType* in, FloatingPointType* out) {
-//    ReluForwardGpu<FloatingPointType>
-//        <<<BlocksNum4ThreadsNum(n), kCudaThreadsNumPerBlock, 0,
-//           ctx.device_ctx->cuda_stream()>>>(n, in, out);
-//  }
-//
-//  static void Backward(const KernelCtx& ctx, const int64_t n,
-//                       const FloatingPointType* out_diff,
-//                       const FloatingPointType* in,
-//                       FloatingPointType* in_diff) {
-//    ReluBackwardGpu<FloatingPointType>
-//        <<<BlocksNum4ThreadsNum(n), kCudaThreadsNumPerBlock, 0,
-//           ctx.device_ctx->cuda_stream()>>>(n, out_diff, in, in_diff);
-//  }
-//};
-//
-// INSTANTIATE_GPU_KERNEL_UTIL_CLASS(ReluKernelUtil);
-//
-//}  // namespace oneflow
+#include "oneflow/core/device/cuda_util.h"
+#include "oneflow/core/kernel/relu_kernel.h"
+
+namespace oneflow {
+
+namespace {
+
+template<typename T>
+__global__ void ReluForwardGpu(const int64_t n, const T* in, T* out) {
+  CUDA_1D_KERNEL_LOOP(i, n) { out[i] = in[i] > 0 ? in[i] : 0; }
+}
+
+template<typename T>
+__global__ void ReluBackwardGpu(const int64_t n, const T* out_diff, const T* in,
+                                T* in_diff) {
+  CUDA_1D_KERNEL_LOOP(i, n) { in_diff[i] = in[i] > 0 ? out_diff[i] : 0; }
+}
+
+}  // namespace
+
+template<typename T>
+class ReluKernelUtil<DeviceType::kGPU, T> final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(ReluKernelUtil);
+  ReluKernelUtil() = delete;
+
+  static void Forward(const KernelCtx& ctx, const int64_t n, const T* in,
+                      T* out) {
+    ReluForwardGpu<T><<<BlocksNum4ThreadsNum(n), kCudaThreadsNumPerBlock, 0,
+                        ctx.device_ctx->cuda_stream()>>>(n, in, out);
+  }
+
+  static void Backward(const KernelCtx& ctx, const int64_t n, const T* out_diff,
+                       const T* in, T* in_diff) {
+    ReluBackwardGpu<T>
+        <<<BlocksNum4ThreadsNum(n), kCudaThreadsNumPerBlock, 0,
+           ctx.device_ctx->cuda_stream()>>>(n, out_diff, in, in_diff);
+  }
+};
+
+#define MACRO_PAIR(type_cpp, type_proto) \
+  template class ReluKernelUtil<DeviceType::kGPU, type_cpp>;
+ARITHMETIC_DATA_TYPE_PAIR()
+#undef MACRO_PAIR()
+
+}  // namespace oneflow
