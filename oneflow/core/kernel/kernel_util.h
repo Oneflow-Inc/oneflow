b9d17094b0f8f91bbe0220fcb90ae96e32e90ba3
willzhang4a58 <willzhang4a58@gmail.com>
2017-08-27 14:54:07 +0800

    refine kernel util

--- oneflow/core/kernel/kernel_util.h
+++ oneflow/core/kernel/kernel_util.h
@@ -14,7 +14,7 @@
 
 namespace oneflow {
 
-template<DeviceType device_type, typename FloatingPointType>
+template<DeviceType device_type, typename T>
 class KernelUtil final {
  public:
   OF_DISALLOW_COPY_AND_MOVE(KernelUtil);
@@ -22,82 +22,67 @@ class KernelUtil final {
 
   // level 1 vector and vector
   // dot product
-  static void BlasDot(DeviceCtx* ctx, const int n, const FloatingPointType* x,
-                      const int incx, const FloatingPointType* y,
-                      const int incy, FloatingPointType* result);
+  static void BlasDot(DeviceCtx* ctx, const int n, const T* x, const int incx,
+                      const T* y, const int incy, T* result);
 
   // swap x and y
-  static void BlasSwap(DeviceCtx* ctx, const int n, FloatingPointType* x,
-                       const int incx, FloatingPointType* y, const int incy);
+  static void BlasSwap(DeviceCtx* ctx, const int n, T* x, const int incx, T* y,
+                       const int incy);
 
   // copy x into y
-  static void BlasCopy(DeviceCtx* ctx, const int n, const FloatingPointType* x,
-                       const int incx, FloatingPointType* y, const int incy);
+  static void BlasCopy(DeviceCtx* ctx, const int n, const T* x, const int incx,
+                       T* y, const int incy);
 
   // y = a*x + y
-  static void BlasAxpy(DeviceCtx* ctx, const int n,
-                       const FloatingPointType alpha,
-                       const FloatingPointType* x, const int incx,
-                       FloatingPointType* y, const int incy);
+  static void BlasAxpy(DeviceCtx* ctx, const int n, const T alpha, const T* x,
+                       const int incx, T* y, const int incy);
 
   // x = a*x
-  static void BlasScal(DeviceCtx* ctx, const int n,
-                       const FloatingPointType alpha, FloatingPointType* x,
+  static void BlasScal(DeviceCtx* ctx, const int n, const T alpha, T* x,
                        const int incx);
   // max(x)
   // no Template specialization for gpu
-  static void Max(DeviceCtx* ctx, const int64_t n, const FloatingPointType* x,
-                  FloatingPointType* max_ptr);
+  static void Max(DeviceCtx* ctx, const int64_t n, const T* x, T* max_ptr);
 
   // max(x)
   // temp_storage is for gpu parallel
-  static void Max(DeviceCtx* ctx, const int64_t n, const FloatingPointType* x,
-                  FloatingPointType* max_ptr, FloatingPointType* temp_storage,
-                  size_t temp_storage_bytes);
+  static void Max(DeviceCtx* ctx, const int64_t n, const T* x, T* max_ptr,
+                  T* temp_storage, size_t temp_storage_bytes);
 
   // y = exp(x)
-  static void Exp(DeviceCtx* ctx, const int64_t n, const FloatingPointType* x,
-                  FloatingPointType* y);
+  static void Exp(DeviceCtx* ctx, const int64_t n, const T* x, T* y);
 
   // sum(x)
   // no Template specialization for gpu
-  static void Sum(DeviceCtx* ctx, const int64_t n, const FloatingPointType* x,
-                  FloatingPointType* sum_ptr);
+  static void Sum(DeviceCtx* ctx, const int64_t n, const T* x, T* sum_ptr);
 
   // sum(x)
   // temp_storage is for gpu parallel
-  static void Sum(DeviceCtx* ctx, const int64_t n, const FloatingPointType* x,
-                  FloatingPointType* sum_ptr, FloatingPointType* temp_storage,
-                  size_t temp_storage_bytes);
+  static void Sum(DeviceCtx* ctx, const int64_t n, const T* x, T* sum_ptr,
+                  T* temp_storage, size_t temp_storage_bytes);
 
   // x = x / a
-  static void Div(DeviceCtx* ctx, const int64_t n, FloatingPointType* x,
-                  const FloatingPointType* alpha_ptr);
+  static void Div(DeviceCtx* ctx, const int64_t n, T* x, const T* alpha_ptr);
 
   // element-wise multiplication
   // z[i] = x[i] * y[i]
-  static void Mul(DeviceCtx* ctx, const int64_t n, const FloatingPointType* x,
-                  const FloatingPointType* y, FloatingPointType* z);
+  static void Mul(DeviceCtx* ctx, const int64_t n, const T* x, const T* y,
+                  T* z);
 
   // level 2 matrix and vector
   // matrix vector multiply
   static void BlasGemv(DeviceCtx* ctx, const enum CBLAS_TRANSPOSE trans, int m,
-                       int n, const FloatingPointType alpha,
-                       const FloatingPointType* a, int lda,
-                       const FloatingPointType* x, const int incx,
-                       const FloatingPointType beta, FloatingPointType* y,
-                       const int incy);
+                       int n, const T alpha, const T* a, int lda, const T* x,
+                       const int incx, const T beta, T* y, const int incy);
 
   // level 3 matrix and matrix
   // matrix matrix multiply
   static void BlasGemm(DeviceCtx* ctx, const enum CBLAS_ORDER order,
                        const enum CBLAS_TRANSPOSE trans_a,
                        const enum CBLAS_TRANSPOSE trans_b, const int m,
-                       const int n, const int k, const FloatingPointType alpha,
-                       const FloatingPointType* a, const int lda,
-                       const FloatingPointType* b, const int ldb,
-                       const FloatingPointType beta, FloatingPointType* c,
-                       const int ldc);
+                       const int n, const int k, const T alpha, const T* a,
+                       const int lda, const T* b, const int ldb, const T beta,
+                       T* c, const int ldc);
 
   // Generate random number of specific distribution
   static void Fill(const FillConf& fill_conf, uint32_t random_seed, Blob* blob);
