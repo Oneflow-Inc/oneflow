2f480db336c01659095e1ef8a6f55632dcc7c2e8
duduscript <duduscript@outlook.com>
2017-08-28 00:05:31 +0800

    momentum

--- oneflow/core/operator/momentum_model_update_op.cpp
+++ oneflow/core/operator/momentum_model_update_op.cpp
@@ -1,26 +1,31 @@
-//#include "oneflow/core/operator/momentum_model_update_op.h"
-//
-// namespace oneflow {
-//
-// void MomentumModelUpdateOp::InitFromOpConf(const OperatorConf& op_conf) {
-//  CHECK(op_conf.has_momentum_mdupdt_conf());
-//  mut_op_conf() = op_conf;
-//
-//  EnrollInputBn("model_diffs", false);
-//  EnrollDataTmpBn("momentum");
-//  EnrollOutputBn("model", false);
-//}
-//
-// const PbMessage& MomentumModelUpdateOp::GetSpecialConf() const {
-//  return op_conf().momentum_mdupdt_conf();
-//}
-//
-// void MomentumModelUpdateOp::InferBlobDesc4FwBlobs(
-//    std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
-//    ParallelPolicy policy, int64_t parallel_id, int64_t parallel_num) const {
-//  TODO();
-//}
-//
-// REGISTER_OP(OperatorConf::kMomentumMdupdtConf, MomentumModelUpdateOp);
-//
-//}  // namespace oneflow
+#include "oneflow/core/operator/momentum_model_update_op.h"
+
+namespace oneflow {
+
+void MomentumModelUpdateOp::InitFromOpConf() {
+  CHECK(op_conf().has_momentum_mdupdt_conf());
+
+  EnrollInputBn("model_diffs", false);
+  EnrollDataTmpBn("momentum");
+  EnrollOutputBn("model", false);
+}
+
+const PbMessage& MomentumModelUpdateOp::GetSpecialConf() const {
+  return op_conf().momentum_mdupdt_conf();
+}
+
+void MomentumModelUpdateOp::InferBlobDesc4FwBlobs(
+    std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
+    ParallelPolicy policy, int64_t parallel_id, int64_t parallel_num) {
+  const BlobDesc* md_diff_blob_desc = GetBlobDesc4BnInOp("model_diffs");
+
+  // momentum
+  BlobDesc* momentum_blob_desc = GetBlobDesc4BnInOp("momentum");
+  momentum_blob_desc->mut_shape() = md_diff_blob_desc->shape();
+  momentum_blob_desc->set_data_type(md_diff_blob_desc->data_type());
+  momentum_blob_desc->set_has_data_id(md_diff_blob_desc->has_data_id());
+}
+
+REGISTER_OP(OperatorConf::kMomentumMdupdtConf, MomentumModelUpdateOp);
+
+}  // namespace oneflow
