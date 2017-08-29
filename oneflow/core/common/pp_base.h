fa729790387f19ec23a617f4581c4a0e3d49cbb2
chengtbf <472491134@qq.com>
2017-08-29 13:37:12 +0800

    int32 to int

--- oneflow/core/common/pp_base.h
+++ oneflow/core/common/pp_base.h
@@ -5,7 +5,8 @@
 #define OF_PP_STRINGIZE_I(text) #text
 
 #define OF_PP_CAT(a, b) OF_PP_CAT_I(a, b)
-#define OF_PP_CAT_I(a, b) a##b
+#define OF_PP_CAT_I(a, b) OF_PP_CAT_II(_, a##b)
+#define OF_PP_CAT_II(a, b) b
 
 #define OF_PP_SEQ_HEAD(seq) OF_PP_PAIR_FIRST(OF_PP_SEQ_TO_PAIR(seq))
 #define OF_PP_SEQ_TAIL(seq) OF_PP_PAIR_SECOND(OF_PP_SEQ_TO_PAIR(seq))
@@ -17,8 +18,8 @@
 #define OF_PP_PAIR_FIRST(t) OF_PP_FIRST_ARG t
 #define OF_PP_PAIR_SECOND(t) OF_PP_SECOND_ARG t
 
-#define OF_PP_FIRST_ARG(x, args...) x
-#define OF_PP_SECOND_ARG(x, y, args...) y
+#define OF_PP_FIRST_ARG(x, ...) x
+#define OF_PP_SECOND_ARG(x, y, ...) y
 
 #define OF_PP_MAKE_TUPLE(...) (__VA_ARGS__)
 #define OF_PP_MAKE_TUPLE_SEQ(...) (OF_PP_MAKE_TUPLE(__VA_ARGS__))
