2fa28188431df72e5989dda995d107190ca89b0d
XIE Xuan <xiexuanx2@gmail.com>
2017-08-31 02:31:31 +0800

    FIRST_ARG -> PAIR_FIRST()

--- oneflow/core/kernel/accumulate_kernel_test.cpp
+++ oneflow/core/kernel/accumulate_kernel_test.cpp
@@ -71,7 +71,7 @@ void TestAccumulateKernel() {
 }  // namespace test
 
 TEST(AccumulateKernel, accumulate) {
-#define MAKE_ENTRY(x, y) test::TestAccumulateKernel<x, OF_PP_FIRST_ARG y>();
+#define MAKE_ENTRY(x, y) test::TestAccumulateKernel<x, OF_PP_PAIR_FIRST(y)>();
   OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(MAKE_ENTRY, DEVICE_TYPE_SEQ,
                                    FLOATING_DATA_TYPE_SEQ)
 }
