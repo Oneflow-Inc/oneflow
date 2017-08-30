ba6f3769217386d1674ba0a272332074321b9e17
duduscript <duduscript@outlook.com>
2017-08-30 15:17:44 +0800

    fix name problem

--- oneflow/core/kernel/normal_model_update_kernel_test.cpp
+++ oneflow/core/kernel/normal_model_update_kernel_test.cpp
@@ -31,21 +31,21 @@ void InitJobDesc(int32_t piece_size, int32_t num_of_pieces_in_batch) {
 
 template<DeviceType device_type, typename T>
 std::function<Blob*(const std::string&)> BuildBnInOp2Blob() {
-  using KTCommon = KTCommon<device_type, T>;
+  using KTC = KTCommon<device_type, T>;
 
   BlobDesc* blob_desc =
       new BlobDesc(Shape({1, 3, 2}), GetDataType<T>::val, false);
 
   auto bn2blob = new HashMap<std::string, Blob*>;
-  (*bn2blob)["model"] = KTCommon::CreateBlobWithSameVal(blob_desc, 2);
-  (*bn2blob)["model_diffs"] = KTCommon::CreateBlobWithSameVal(blob_desc, 2);
-  (*bn2blob)["model_expected"] = KTCommon::CreateBlobWithSameVal(blob_desc, 1);
+  (*bn2blob)["model"] = KTC::CreateBlobWithSameVal(blob_desc, 2);
+  (*bn2blob)["model_diffs"] = KTC::CreateBlobWithSameVal(blob_desc, 2);
+  (*bn2blob)["model_expected"] = KTC::CreateBlobWithSameVal(blob_desc, 1);
   return [bn2blob](const std::string& bn) { return bn2blob->at(bn); };
 }
 
 template<DeviceType device_type, typename T>
 void TestMdUpdateKernel() {
-  using KTCommon = KTCommon<device_type, T>;
+  using KTC = KTCommon<device_type, T>;
   KernelCtx ctx;
   BuildKernelCtx<device_type>(&ctx);
 
@@ -59,7 +59,7 @@ void TestMdUpdateKernel() {
   model_update_kernel->Forward(ctx, BnInOp2BlobPtr);
   SyncStream<device_type>(&ctx);
 
-  KTCommon::CheckResult(BnInOp2BlobPtr, "model", "model_expected");
+  KTC::CheckResult(BnInOp2BlobPtr, "model", "model_expected");
 }
 
 }  // namespace
@@ -67,11 +67,10 @@ void TestMdUpdateKernel() {
 }  // namespace test
 
 TEST(MdUpdateKernel, model_update) {
-#define FLOATINGPOINTTYPE (double)(float)
-#define DEVICETYPE (DeviceType::kCPU)(DeviceType::kGPU)
-#define TESTMdUpdateKernel(x, y) test::TestMdUpdateKernel<x, y>();
-  FOR_EACH_TUPLE(TESTMdUpdateKernel,
-                 OF_PP_INTERNAL_SEQ_PRODUCT(DEVICETYPE, FLOATINGPOINTTYPE));
+#define MAKE_ENTRY(device_type, type_pair) \
+  test::TestMdUpdateKernel<device_type, OF_PP_FIRST_ARG type_pair>();
+  OF_PP_FOR_EACH_TUPLE(MAKE_ENTRY, OF_PP_INTERNAL_SEQ_PRODUCT(
+                                       DEVICE_TYPE_SEQ, FLOATING_DATA_TYPE_SEQ))
 }
 
 }  // namespace oneflow
