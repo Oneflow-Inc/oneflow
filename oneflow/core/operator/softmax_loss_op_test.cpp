bda4c5472491a9cba2bfdeca20277501bef63d51
chengtbf <472491134@qq.com>
2017-09-02 10:35:41 +0800

    softmax loss op test

--- oneflow/core/operator/softmax_loss_op_test.cpp
+++ oneflow/core/operator/softmax_loss_op_test.cpp
@@ -1,30 +1,42 @@
-//#include "oneflow/core/operator/softmax_loss_op.h"
-//
-// namespace oneflow {
-//
-// TEST(SoftmaxLossOp, softmax_loss_3x5) {
-//  // create softmax_loss_op
-//  OperatorConf op_conf;
-//  op_conf.set_name("softmax_loss_test");
-//  op_conf.mutable_softmax_loss_conf()->set_prediction(
-//      "softmax_loss/prediction");
-//  op_conf.mutable_softmax_loss_conf()->set_label("softmax_loss/label");
-//  op_conf.mutable_softmax_loss_conf()->set_loss("softmax_loss/loss");
-//  auto softmax_loss_op = ConstructOp(op_conf);
-//  HashMap<std::string, Shape*> bn2shape_ptr{{"prediction", new Shape({3, 5})},
-//                                            {"label", new Shape({3})},
-//                                            {"prob", new Shape},
-//                                            {"tmp_1D", new Shape},
-//                                            {"loss", new Shape}};
-//  auto fp = [&bn2shape_ptr](const std::string& bn) {
-//    return bn2shape_ptr.at(bn);
-//  };
-//  // infershape
-//  softmax_loss_op->InferBlobDesc4FwBlobs(fp, kDataParallel, 0, 1);
-//  // test
-//  ASSERT_EQ(*fp("loss"), Shape({1}));
-//  ASSERT_EQ(*fp("prob"), Shape({3, 5}));
-//  ASSERT_EQ(*fp("tmp_1D"), Shape({3}));
-//}
-//
-//}  // namespace oneflow
+#include "oneflow/core/operator/softmax_loss_op.h"
+
+namespace oneflow {
+
+TEST(SoftmaxLossOp, softmax_loss_3x5) {
+  // create softmax_loss_op
+  OperatorConf op_conf;
+  op_conf.set_name("softmax_loss_test");
+  op_conf.mutable_softmax_loss_conf()->mutable_prediction()->set_name(
+      "softmax_loss/prediction");
+  op_conf.mutable_softmax_loss_conf()->mutable_prediction()->set_data_type(
+      DataType::kFloat);
+  op_conf.mutable_softmax_loss_conf()->mutable_label()->set_name(
+      "softmax_loss/label");
+  op_conf.mutable_softmax_loss_conf()->mutable_label()->set_data_type(
+      DataType::kFloat);
+  op_conf.mutable_softmax_loss_conf()->mutable_loss()->set_name(
+      "softmax_loss/loss");
+  op_conf.mutable_softmax_loss_conf()->mutable_loss()->set_data_type(
+      DataType::kInt32);
+  auto softmax_loss_op = ConstructOp(op_conf);
+  JobConf job_conf;
+  job_conf.set_default_data_type(DataType::kFloat);
+  JobDesc::Singleton()->InitFromJobConf(job_conf);
+  HashMap<std::string, BlobDesc*> bn2blob_desc_map{
+      {"prediction", new BlobDesc(Shape({3, 5}), DataType::kFloat, false)},
+      {"label", new BlobDesc(Shape({3}), DataType::kInt32, false)},
+      {"prob", new BlobDesc},
+      {"tmp_1D", new BlobDesc},
+      {"loss", new BlobDesc}};
+  auto fp = [&bn2blob_desc_map](const std::string& bn) {
+    return bn2blob_desc_map.at(bn);
+  };
+  // infershape
+  softmax_loss_op->InferBlobDesc4FwBlobs(fp, kDataParallel, 0, 1);
+  // test
+  ASSERT_EQ(*fp("loss"), BlobDesc(Shape({1}), DataType::kFloat, false));
+  ASSERT_EQ(*fp("prob"), BlobDesc(Shape({3, 5}), DataType::kFloat, false));
+  ASSERT_EQ(*fp("tmp_1D"), BlobDesc(Shape({3}), DataType::kFloat, false));
+}
+
+}  // namespace oneflow
