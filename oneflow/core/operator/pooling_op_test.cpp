f8746b7d83729c63bcfbd19b89358f3301e3e372
duduscript <duduscript@outlook.com>
2017-09-03 11:17:47 +0800

    PoolingOpTest (#329)

--- oneflow/core/operator/pooling_op_test.cpp
+++ oneflow/core/operator/pooling_op_test.cpp
@@ -1,49 +1,53 @@
-//#include "oneflow/core/operator/pooling_op.h"
-//
-// namespace oneflow {
-//
-// TEST(PoolingOp, pool_100x64x11x11) {
-//  // create pooling_op with input shape 100x64x11x11
-//  // PoolMethod = MAX
-//  // pad_h = pad_w = 1
-//  // kernel_h = hernel_w = 3
-//  // stride_h = stride_w = 2
-//  OperatorConf op_conf;
-//  op_conf.set_name("pooling_test");
-//  PoolingOpConf* pooling_conf = op_conf.mutable_pooling_conf();
-//  pooling_conf->set_in("pooling_in");
-//  pooling_conf->set_out("pooling_out");
-//  pooling_conf->set_pool(PoolingOpConf::kMax);
-//  pooling_conf->set_pad_h(1);
-//  pooling_conf->set_pad_w(1);
-//  pooling_conf->set_kernel_size_h(2);
-//  pooling_conf->set_kernel_size_w(2);
-//  pooling_conf->set_stride_h(2);
-//  pooling_conf->set_stride_w(2);
-//  auto pooling_op = ConstructOp(op_conf);
-//  std::vector<int64_t> input_shape_vec = {100, 64, 11, 11};
-//  HashMap<std::string, Shape*> bn2shape_ptr{
-//      {pooling_op->SoleIbn(), new Shape(input_shape_vec)},
-//      {pooling_op->SoleObn(), new Shape},
-//      {pooling_op->SoleDtbn(), new Shape}};
-//  auto fp = [&bn2shape_ptr](const std::string& bn) {
-//    return bn2shape_ptr.at(bn);
-//  };
-//  // do infer shape
-//  pooling_op->InferBlobDesc4FwBlobs(fp, kDataParallel, 0, 1);
-//  // test
-//  Shape* output_shape_ptr = bn2shape_ptr.at(pooling_op->SoleObn());
-//  Shape* data_tmp_shape_ptr = bn2shape_ptr.at(pooling_op->SoleDtbn());
-//  // n * c * h_o * w_o
-//  // where h_o = (h_i + 2 * pad_h - kernel_h) / stride_h + 1 and w_o likewise.
-//  // n = 100
-//  // c = 64
-//  // h_o = (11 + 2 * 1 - 3) / 2 + 1 = 6
-//  // w_o = (11 + 2 * 1 - 3) / 2 + 1 = 6
-//  std::vector<int64_t> output_shape_vec = {100, 64, 6, 6};
-//  ASSERT_EQ(*output_shape_ptr, Shape(output_shape_vec));
-//  ASSERT_EQ(*data_tmp_shape_ptr, *output_shape_ptr);
-//  ASSERT_NE(data_tmp_shape_ptr, output_shape_ptr);
-//}
-//
-//}  // namespace oneflow
+#include "oneflow/core/operator/pooling_op.h"
+
+namespace oneflow {
+
+template<typename T>
+std::shared_ptr<Operator> GetTestPoolingOp() {
+  JobConf job_conf;
+  job_conf.set_default_data_type(DataType::kFloat);
+  JobDesc::Singleton()->InitFromJobConf(job_conf);
+  OperatorConf op_conf;
+  op_conf.set_name("pooling_test");
+  op_conf.mutable_pooling_conf()->mutable_in()->set_name("pooling_in");
+  op_conf.mutable_pooling_conf()->mutable_in()->set_data_type(
+      GetDataType<T>::val);
+  op_conf.mutable_pooling_conf()->mutable_out()->set_name("pooling_out");
+  op_conf.mutable_pooling_conf()->mutable_out()->set_data_type(
+      GetDataType<T>::val);
+  op_conf.mutable_pooling_conf()->set_pool(PoolingOpConf_PoolMethod_kMax);
+  op_conf.mutable_pooling_conf()->set_pad_h(1);
+  op_conf.mutable_pooling_conf()->set_pad_w(1);
+  op_conf.mutable_pooling_conf()->set_kernel_size_h(2);
+  op_conf.mutable_pooling_conf()->set_kernel_size_w(2);
+  op_conf.mutable_pooling_conf()->set_stride_h(2);
+  op_conf.mutable_pooling_conf()->set_stride_w(2);
+  return ConstructOp(op_conf);
+}
+
+template<typename T>
+void TestPoolingOp(ParallelPolicy policy, bool has_data_id) {
+  auto pooling_op = GetTestPoolingOp<T>();
+  HashMap<std::string, BlobDesc*> bn2blob_desc_map{
+      {"in", new BlobDesc(Shape({100, 64, 11, 11}), GetDataType<T>::val,
+                          has_data_id)},
+      {"out", new BlobDesc},
+      {"idx", new BlobDesc}};
+  auto Bn2BlobDescFunc = [&bn2blob_desc_map](const std::string& bn) {
+    return bn2blob_desc_map.at(bn);
+  };
+  pooling_op->InferBlobDesc4FwBlobs(Bn2BlobDescFunc, policy, 0, 1);
+  ASSERT_EQ(*Bn2BlobDescFunc("out"),
+            BlobDesc(Shape({100, 64, 6, 6}), GetDataType<T>::val, has_data_id));
+  ASSERT_EQ(*Bn2BlobDescFunc("idx"),
+            BlobDesc(Shape({100, 64, 6, 6}), DataType::kUInt32, false));
+}
+
+TEST(PoolingOp, pooling) {
+#define MAKE_ENTRY(data_type, policy, has_data_id) \
+  TestPoolingOp<OF_PP_PAIR_FIRST(data_type)>(policy, has_data_id);
+  OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(MAKE_ENTRY, FLOATING_DATA_TYPE_SEQ,
+                                   PARALLEL_POLICY_SEQ, BOOL_SEQ)
+}
+
+}  // namespace oneflow
