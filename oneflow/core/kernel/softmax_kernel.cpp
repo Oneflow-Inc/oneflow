2bff704bbfba0a09b49a594cfe73bcb1f022b5d3
chengtbf <472491134@qq.com>
2017-09-03 19:22:58 +0800

    softmax kernel register (#335)
    * softmax kernel register
    
    * softmax kernel regst

--- oneflow/core/kernel/softmax_kernel.cpp
+++ oneflow/core/kernel/softmax_kernel.cpp
@@ -81,4 +81,20 @@ class SoftmaxKernelUtil<DeviceType::kCPU, T> final {
   }
 };
 
+Kernel* CreateSoftmaxKernel(const OpContext& op_ctx) {
+  static const HashMap<std::string, std::function<Kernel*()>>
+      data_type2creator = {
+#define SOFTMAX_KERNEL_ENTRY(device_type, data_type_pair)                     \
+  {GetHashKey(device_type, OF_PP_PAIR_SECOND(data_type_pair)), []() {         \
+     return new SoftmaxKernel<device_type, OF_PP_PAIR_FIRST(data_type_pair)>; \
+   }},
+          OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(
+              SOFTMAX_KERNEL_ENTRY, DEVICE_TYPE_SEQ, FLOATING_DATA_TYPE_SEQ)};
+
+  return data_type2creator.at(
+      GetHashKey(op_ctx.device_type(), op_ctx.bn_in_op2data_type().at("in")))();
+}
+
+COMMAND(AddKernelCreator(OperatorConf::kSoftmaxConf, CreateSoftmaxKernel));
+
 }  // namespace oneflow
