9c7215fb67f50eeba6179d5538a5e7f6fd0ff2fe
duduscript <duduscript@outlook.com>
2017-09-03 18:40:34 +0800

    pooling and convolution kernel register

--- oneflow/core/kernel/convolution_kernel.cpp
+++ oneflow/core/kernel/convolution_kernel.cpp
@@ -289,16 +289,23 @@ void ConvolutionKernel<device_type, T>::InitModelTmpBlobs(
 namespace {
 
 Kernel* CreateConvolutionKenrel(const OpContext& op_ctx) {
-  static const HashMap<OpContext, std::function<Kernel*()>> data_type2creator = {
-#define CONVOLUTION_KERNEL_ENTRY(type_cpp, type_proto) \
-  {type_proto, []() { return new ConvolutionKernel<device_type, type_cpp>; }},
-      OF_PP_FOR_EACH_TUPLE(CONVOLUTION_KERNEL_ENTRY, FLOATING_DATA_TYPE_SEQ)};
-  return data_type2creator.at(op_ctx)();
+  static const HashMap<std::string, std::function<Kernel*()>>
+      data_type2creator = {
+#define CONVOLUTION_KERNEL_ENTRY(device_type, data_type_pair)          \
+  {GetHashKey(device_type, OF_PP_PAIR_SECOND(data_type_pair)), []() {  \
+     return new ConvolutionKernel<device_type,                         \
+                                  OF_PP_PAIR_FIRST(data_type_pair)>(); \
+   }},
+          OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(CONVOLUTION_KERNEL_ENTRY,
+                                           DEVICE_TYPE_SEQ,
+                                           FLOATING_DATA_TYPE_SEQ)};
+  return data_type2creator.at(
+      GetHashKey(op_ctx.device_type(), op_ctx.bn_in_op2data_type().at("in")))();
 }
+
+}  // namespace
 
 COMMAND(AddKernelCreator(OperatorConf::kConvolutionConf,
                          CreateConvolutionKenrel))
-
-}  // namespace
 
 }  // namespace oneflow
