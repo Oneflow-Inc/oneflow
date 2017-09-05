af4d9360fe0925811f3769966092ed0959681831
Will Zhang <willzhang_bit@163.com>
2017-09-05 15:33:33 +0800

    file system (#341)
    * add file_system interface
    
    * add windows and posix file system interface
    
    * implement some method in filesystem
    
    * remove function StatusToString and fix for review
    
    * add stauts item for file
    
    * add dir status
    
    * add error code to Status
    
    * add platform and win fs implement
    
    * add #define for different fs
    
    * update posix header
    
    * delete Sync() in WriteableFile
    
    * fix bug about platform define
    
    * posix file system implementation and test
    
    * delete sync method in PosixFileSystem
    
    * remove individual posix test
    
    * Baic file read and write test
    
    * add directory operation test
    
    * DIR test updt
    
    * fix macro definition bug of win32/linux filesystem
    
    * add windows file system and test project
    
    * add hadoop interface
    
    * add appendable file test
    
    * hadoop cpp
    
    * convert cmake

---
+++ oneflow/core/persistence/hadoop/hadoop_file_system.h
@@ -0,0 +1,59 @@
+#ifndef ONEFLOW_CORE_PERSISTENCE_HADOOP_HADOOP_FILE_SYSTEM_H_
+#define ONEFLOW_CORE_PERSISTENCE_HADOOP_HADOOP_FILE_SYSTEM_H_
+
+#include "oneflow/core/persistence/file_system.h"
+
+extern "C" {
+struct hdfs_internal;
+typedef hdfs_internal* hdfsFS;
+}
+
+namespace oneflow {
+
+namespace fs {
+
+class LibHDFS;
+
+class HadoopFileSystem final : public FileSystem {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(HadoopFileSystem);
+  HadoopFileSystem() = default;
+  ~HadoopFileSystem() = default;
+
+  Status NewRandomAccessFile(
+      const std::string& fname,
+      std::unique_ptr<RandomAccessFile>* result) override;
+
+  Status NewWritableFile(const std::string& fname,
+                         std::unique_ptr<WritableFile>* result) override;
+
+  Status NewAppendableFile(const std::string& fname,
+                           std::unique_ptr<WritableFile>* result) override;
+
+  Status FileExists(const std::string& fname) override;
+
+  Status GetChildren(const std::string& dir,
+                     std::vector<std::string>* result) override;
+
+  Status DeleteFile(const std::string& fname) override;
+
+  Status CreateDir(const std::string& dirname) override;
+
+  Status DeleteDir(const std::string& dirname) override;
+
+  Status GetFileSize(const std::string& fname, uint64_t* file_size) override;
+
+  Status RenameFile(const std::string& src, const std::string& target) override;
+
+  Status IsDirectory(const std::string& fname) override;
+
+ private:
+  Status Connect(std::string fname, hdfsFS* fs);
+  LibHDFS* hdfs_;
+};
+
+}  // namespace fs
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_PERSISTENCE_HADOOP_HADOOP_FILE_SYSTEM_H_
--- oneflow/core/persistence/windows/windows_file_system.cpp
+++ oneflow/core/persistence/windows/windows_file_system.cpp
@@ -1,9 +1,8 @@
 #include "oneflow/core/persistence/windows/windows_file_system.h"
-#include <Windows.h>
 
 #ifdef PLATFORM_WINDOWS
 
-#undef DeleteFile
+#include <Shlwapi.h>
 
 namespace oneflow {
 
@@ -75,27 +74,25 @@ class WindowsRandomAccessFile : public RandomAccessFile {
   }
 
   Status Read(uint64_t offset, size_t n, char* result) const override {
-    Status s;
     char* dst = result;
-    while (n > 0 && s == Status::OK) {
+    while (n > 0) {
       SSIZE_T r = pread(hfile_, dst, n, offset);
       if (r > 0) {
         offset += r;
         dst += r;
         n -= r;
       } else if (r == 0) {
-        s = Status::OUT_OF_RANGE;
         PLOG(WARNING) << "Read file warning for : " + filename_
                              + " Read fewer bytes than requested";
+        return Status::OUT_OF_RANGE;
       } else if (errno == EINTR || errno == EAGAIN) {
         // Retry
       } else {
-        s = ErrnoToStatus(errno);
         PLOG(WARNING) << "Read file warning for : " + filename_;
+        return ErrnoToStatus(errno);
       }
     }
-    if (s != Status::OK) {}
-    return s;
+    return Status::OK;
   }
 };
 
@@ -151,6 +148,209 @@ class WindowsWritableFile : public WritableFile {
 };
 
 }  // namespace
+
+Status WindowsFileSystem::NewRandomAccessFile(
+    const std::string& fname, std::unique_ptr<RandomAccessFile>* result) {
+  std::string translated_fname = TranslateName(fname);
+  std::wstring ws_translated_fname = Utf8ToWideChar(translated_fname);
+  result->reset();
+
+  // Open the file for read-only random access
+  // Open in async mode which makes Windows allow more parallelism even
+  // if we need to do sync I/O on top of it.
+  DWORD file_flags = FILE_ATTRIBUTE_READONLY | FILE_FLAG_OVERLAPPED;
+  // Shared access is necessary for tests to pass
+  // almost all tests would work with a possible exception of fault_injection.
+  DWORD share_mode = FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE;
+
+  HANDLE hfile =
+      ::CreateFileW(ws_translated_fname.c_str(), GENERIC_READ, share_mode, NULL,
+                    OPEN_EXISTING, file_flags, NULL);
+
+  if (INVALID_HANDLE_VALUE == hfile) {
+    std::string context = "NewRandomAccessFile failed to Create/Open: " + fname;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+
+  result->reset(new WindowsRandomAccessFile(translated_fname, hfile));
+  return Status::OK;
+}
+
+Status WindowsFileSystem::NewWritableFile(
+    const std::string& fname, std::unique_ptr<WritableFile>* result) {
+  std::string translated_fname = TranslateName(fname);
+  std::wstring ws_translated_fname = Utf8ToWideChar(translated_fname);
+  result->reset();
+
+  DWORD share_mode = FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE;
+  HANDLE hfile =
+      ::CreateFileW(ws_translated_fname.c_str(), GENERIC_WRITE, share_mode,
+                    NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+  if (INVALID_HANDLE_VALUE == hfile) {
+    std::string context = "Failed to create a NewWriteableFile: " + fname;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+
+  result->reset(new WindowsWritableFile(translated_fname, hfile));
+  return Status::OK;
+}
+
+Status WindowsFileSystem::NewAppendableFile(
+    const std::string& fname, std::unique_ptr<WritableFile>* result) {
+  std::string translated_fname = TranslateName(fname);
+  std::wstring ws_translated_fname = Utf8ToWideChar(translated_fname);
+  result->reset();
+
+  DWORD share_mode = FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE;
+  HANDLE hfile =
+      ::CreateFileW(ws_translated_fname.c_str(), GENERIC_WRITE, share_mode,
+                    NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+  if (INVALID_HANDLE_VALUE == hfile) {
+    std::string context = "Failed to create a NewAppendableFile: " + fname;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+
+  UniqueCloseHandlePtr file_guard(hfile, CloseHandleFunc);
+
+  DWORD file_ptr = ::SetFilePointer(hfile, NULL, NULL, FILE_END);
+  if (INVALID_SET_FILE_POINTER == file_ptr) {
+    std::string context = "Failed to create a NewAppendableFile: " + fname;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+
+  result->reset(new WindowsWritableFile(translated_fname, hfile));
+  file_guard.release();
+
+  return Status::OK;
+}
+
+Status WindowsFileSystem::FileExists(const std::string& fname) {
+  constexpr int kOk = 0;
+  if (_access(TranslateName(fname).c_str(), kOk) == 0) { return Status::OK; }
+  LOG(WARNING) << fname << " not found";
+  return Status::NOT_FOUND;
+}
+
+Status WindowsFileSystem::GetChildren(const std::string& dir,
+                                      std::vector<std::string>* result) {
+  std::string translated_dir = TranslateName(dir);
+  std::wstring ws_translated_dir = Utf8ToWideChar(translated_dir);
+  result->clear();
+
+  std::wstring pattern = ws_translated_dir;
+  if (!pattern.empty() && pattern.back() != '\\' && pattern.back() != '/') {
+    pattern += L"\\*";
+  } else {
+    pattern += L'*';
+  }
+
+  WIN32_FIND_DATAW find_data;
+  HANDLE find_handle = ::FindFirstFileW(pattern.c_str(), &find_data);
+  if (find_handle == INVALID_HANDLE_VALUE) {
+    std::string context = "FindFirstFile failed for: " + translated_dir;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+
+  do {
+    std::string file_name = WideCharToUtf8(find_data.cFileName);
+    if (file_name != "." && file_name != "..") { result->push_back(file_name); }
+  } while (::FindNextFileW(find_handle, &find_data));
+
+  if (!::FindClose(find_handle)) {
+    std::string context = "FindClose failed for: " + translated_dir;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+
+  return Status::OK;
+}
+
+Status WindowsFileSystem::DeleteFile(const std::string& fname) {
+  std::wstring file_name = Utf8ToWideChar(fname);
+  if (_wunlink(file_name.c_str()) != 0) {
+    std::string context = "Failed to delete a file: " + fname;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+  return Status::OK;
+}
+
+Status WindowsFileSystem::CreateDir(const std::string& name) {
+  std::wstring ws_name = Utf8ToWideChar(name);
+  if (_wmkdir(ws_name.c_str()) != 0) {
+    std::string context = "Failed to create a directory: " + name;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+  return Status::OK;
+}
+
+Status WindowsFileSystem::DeleteDir(const std::string& name) {
+  std::wstring ws_name = Utf8ToWideChar(name);
+  if (_wrmdir(ws_name.c_str()) != 0) {
+    std::string context = "Failed to remove a directory: " + name;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+  return Status::OK;
+}
+
+Status WindowsFileSystem::GetFileSize(const std::string& fname,
+                                      uint64_t* size) {
+  std::string translated_fname = TranslateName(fname);
+  std::wstring ws_translated_dir = Utf8ToWideChar(translated_fname);
+  WIN32_FILE_ATTRIBUTE_DATA attrs;
+  if (TRUE
+      == ::GetFileAttributesExW(ws_translated_dir.c_str(),
+                                GetFileExInfoStandard, &attrs)) {
+    ULARGE_INTEGER file_size;
+    file_size.HighPart = attrs.nFileSizeHigh;
+    file_size.LowPart = attrs.nFileSizeLow;
+    *size = file_size.QuadPart;
+  } else {
+    std::string context = "Can not get size for: " + fname;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+  return Status::OK;
+}
+
+Status WindowsFileSystem::RenameFile(const std::string& src,
+                                     const std::string& target) {
+  // rename() is not capable of replacing the existing file as on Linux
+  // so use OS API directly
+  std::wstring ws_translated_src = Utf8ToWideChar(TranslateName(src));
+  std::wstring ws_translated_target = Utf8ToWideChar(TranslateName(target));
+  if (!::MoveFileExW(ws_translated_src.c_str(), ws_translated_target.c_str(),
+                     MOVEFILE_REPLACE_EXISTING)) {
+    std::string context = "Failed to rename: " + src + " to: " + target;
+    LOG(WARNING) << context;
+    return ErrnoToStatus(::GetLastError());
+  }
+  return Status::OK;
+}
+
+Status WindowsFileSystem::IsDirectory(const std::string& fname) {
+  struct _stat sbuf;
+  std::wstring ws_translated_fname = Utf8ToWideChar(TranslateName(fname));
+  if (_wstat(ws_translated_fname.c_str(), &sbuf) != 0) {
+    LOG(WARNING) << fname;
+    return ErrnoToStatus(::GetLastError());
+  } else if (PathIsDirectoryW(ws_translated_fname.c_str())) {
+    return Status::OK;
+  } else {
+    std::string context = fname + " not a directory";
+    LOG(WARNING) << context;
+    return Status::FAILED_PRECONDITION;
+  }
+}
 
 }  // namespace fs
 
