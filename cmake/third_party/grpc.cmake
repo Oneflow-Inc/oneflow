f60d24818b8fc54dfe87a409fac4e47040cedf85
willzhang4a58 <willzhang4a58@gmail.com>
2017-09-14 16:42:48 +0800

    auto generate grpc.pb

--- cmake/third_party/grpc.cmake
+++ cmake/third_party/grpc.cmake
@@ -2,27 +2,28 @@ include (ExternalProject)
 
 set(GRPC_INCLUDE_DIR ${THIRD_PARTY_DIR}/grpc/include)
 set(GRPC_LIBRARY_DIR ${THIRD_PARTY_DIR}/grpc/lib)
+set(GRPC_BINARY_DIR  ${THIRD_PARTY_DIR}/grpc/bin)
 
 set(GRPC_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/grpc/src/grpc/include)
-set(GRPC_URL https://github.com/yuanms2/grpc.git)
-set(GRPC_TAG e0db46e140405f0f94f03c9a55b302e39a514c48)
+set(GRPC_URL https://github.com/grpc/grpc.git)
+set(GRPC_BUILD ${CMAKE_CURRENT_BINARY_DIR}/grpc/src/grpc)
+set(GRPC_TAG 781fd6f6ea03645a520cd5c675da67ab61f87e4b)
 
 if(WIN32)
-    set(GRPC_BUILD_LIBRARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/grpc/src/grpc/${CMAKE_BUILD_TYPE})
+  set(GRPC_BUILD_LIBRARY_DIR ${GRPC_BUILD}/${CMAKE_BUILD_TYPE})
     set(GRPC_LIBRARY_NAMES gpr.lib
       grpc_unsecure.lib
       grpc++_unsecure.lib)
-elseif(APPLE AND ("${CMAKE_GENERATOR}" STREQUAL "Xcode"))
-    set(GRPC_BUILD_LIBRARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/grpc/src/grpc/${CMAKE_BUILD_TYPE})
-    set(GRPC_LIBRARY_NAMES libgpr.a
-      libgrpc_unsecure.a
-      libgrpc++_unsecure.a)
+    set(GRPC_CPP_PLUGIN_NAME grpc_cpp_plugin.exe)
 else()
-    set(GRPC_BUILD_LIBRARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/grpc/src/grpc)
+  set(GRPC_BUILD_LIBRARY_DIR ${GRPC_BUILD})
     set(GRPC_LIBRARY_NAMES libgpr.a
       libgrpc_unsecure.a
       libgrpc++_unsecure.a)
+    set(GRPC_CPP_PLUGIN_NAME grpc_cpp_plugin)
 endif()
+
+set(GRPC_CPP_PLUGIN_PATH ${GRPC_BINARY_DIR}/${GRPC_CPP_PLUGIN_NAME})
 
 foreach(LIBRARY_NAME ${GRPC_LIBRARY_NAMES})
     list(APPEND GRPC_STATIC_LIBRARIES ${GRPC_LIBRARY_DIR}/${LIBRARY_NAME})
@@ -39,16 +40,23 @@ ExternalProject_Add(grpc
     UPDATE_COMMAND ""
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND ""
+    BUILD_COMMAND ${CMAKE_COMMAND} --build . --config Release --target grpc++_unsecure 
+    COMMAND ${CMAKE_COMMAND} --build . --config Release --target grpc_cpp_plugin
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_CURRENT_SOURCE_DIR}/cmake/patches/grpc/CMakeLists.txt ${GRPC_BUILD}
     CMAKE_CACHE_ARGS
-        -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-        -DCMAKE_CXX_FLAGS_DEBUG:STRING=${CMAKE_CXX_FLAGS_DEBUG}
-        -DCMAKE_CXX_FLAGS_RELEASE:STRING=${CMAKE_CXX_FLAGS_RELEASE}
-        -DCMAKE_C_FLAGS_DEBUG:STRING=${CMAKE_C_FLAGS_DEBUG}
-        -DCMAKE_C_FLAGS_RELEASE:STRING=${CMAKE_C_FLAGS_RELEASE}
+        -DCMAKE_BUILD_TYPE:STRING=Release
         -DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF
         -DPROTOBUF_INCLUDE_DIRS:STRING=${PROTOBUF_SRC_DIR}
         -DPROTOBUF_LIBRARIES:STRING=${protobuf_STATIC_LIBRARIES}
         -DZLIB_ROOT:STRING=${ZLIB_INSTALL}
+        -DgRPC_SSL_PROVIDER:STRING=NONE
+)
+
+ExternalProject_Add_Step(grpc copy_rand
+    COMMAND ${CMAKE_COMMAND} -E copy
+    ${CMAKE_SOURCE_DIR}/cmake/patches/grpc/rand.h ${GRPC_BUILD}/include/openssl/rand.h
+    DEPENDEES patch
+    DEPENDERS build
 )
 
 add_custom_target(grpc_create_header_dir
@@ -66,5 +74,13 @@ add_custom_target(grpc_create_library_dir
 add_custom_target(grpc_copy_libs_to_destination
   COMMAND ${CMAKE_COMMAND} -E copy_if_different ${GRPC_BUILD_STATIC_LIBRARIES} ${GRPC_LIBRARY_DIR}
   DEPENDS grpc_create_library_dir)
+
+add_custom_target(grpc_create_binary_dir
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${GRPC_BINARY_DIR}
+  DEPENDS grpc)
+
+add_custom_target(grpc_copy_binary_to_destination
+  COMMAND ${CMAKE_COMMAND} -E copy_if_different ${GRPC_BUILD_LIBRARY_DIR}/${GRPC_CPP_PLUGIN_NAME} ${GRPC_CPP_PLUGIN_PATH}
+  DEPENDS grpc_create_binary_dir)
 
 endif(BUILD_THIRD_PARTY)
