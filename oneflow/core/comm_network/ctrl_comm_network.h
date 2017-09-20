85d6a2ec0f79f25429f8bfb2160eff909f922b76
willzhang4a58 <willzhang4a58@gmail.com>
2017-09-20 17:41:26 +0800

    trylock, notifydone, wait

--- oneflow/core/comm_network/ctrl_comm_network.h
+++ oneflow/core/comm_network/ctrl_comm_network.h
@@ -18,21 +18,9 @@ class CtrlCommNet final {
   void Barrier(const std::string& barrier_name);
   void Barrier(const std::string& barrier_name, int32_t barrier_num);
 
-  // 0 : locked
-  // 1 : done
-  // 2 : doing
-  int32_t TryLock(const std::string& name) {
-    // TODO
-    static HashSet<std::string> locked_name;
-    if (locked_name.find(name) != locked_name.end()) {
-      return 1;
-    } else {
-      locked_name.insert(name);
-      return 0;
-    }
-  }
-  void NotifyDone(const std::string& name) {}
-  void WaitForLockDone(const std::string& name) {}
+  TryLockResult TryLock(const std::string& name);
+  void NotifyDone(const std::string& name);
+  void WaitUntilDone(const std::string& name);
 
  private:
   CtrlCommNet() = default;
@@ -47,18 +35,18 @@ class CtrlCommNet final {
 
 #define OF_BARRIER() CtrlCommNet::Singleton()->Barrier(FILE_LINE_STR)
 
-#define OF_ONCE_GUARD(name, ...)                                \
-  do {                                                          \
-    int32_t lock_ret = CtrlCommNet::Singleton()->TryLock(name); \
-    if (lock_ret == 0) {                                        \
-      __VA_ARGS__;                                              \
-      CtrlCommNet::Singleton()->NotifyDone(name);               \
-    } else if (lock_ret == 1) {                                 \
-    } else if (lock_ret == 2) {                                 \
-      CtrlCommNet::Singleton()->WaitForLockDone(name);          \
-    } else {                                                    \
-      UNEXPECTED_RUN();                                         \
-    }                                                           \
+#define OF_ONCE_GUARD(name, ...)                                      \
+  do {                                                                \
+    TryLockResult lock_ret = CtrlCommNet::Singleton()->TryLock(name); \
+    if (lock_ret == TryLockResult::kLocked) {                         \
+      __VA_ARGS__;                                                    \
+      CtrlCommNet::Singleton()->NotifyDone(name);                     \
+    } else if (lock_ret == TryLockResult::kDone) {                    \
+    } else if (lock_ret == TryLockResult::kDoing) {                   \
+      CtrlCommNet::Singleton()->WaitUntilDone(name);                  \
+    } else {                                                          \
+      UNEXPECTED_RUN();                                               \
+    }                                                                 \
   } while (0)
 
 }  // namespace oneflow
--- oneflow/core/comm_network/ctrl_server.cpp
+++ oneflow/core/comm_network/ctrl_server.cpp
@@ -91,4 +91,56 @@ void CtrlServer::BarrierHandler(CtrlCallIf* call) {
   ENQUEUE_REQUEST(Barrier);
 }
 
+void CtrlServer::TryLockHandler(CtrlCallIf* call) {
+  using TryLockCtrlCall = CtrlCall<TryLockRequest, TryLockResponse>;
+  auto try_lock_call = static_cast<TryLockCtrlCall*>(call);
+  const std::string& lock_name = try_lock_call->request().name();
+  auto name2lock_status_it = name2lock_status_.find(lock_name);
+  if (name2lock_status_it == name2lock_status_.end()) {
+    try_lock_call->mut_response()->set_result(TryLockResult::kLocked);
+    auto waiting_until_done_calls = new std::list<CtrlCallIf*>;
+    CHECK(
+        name2lock_status_.emplace(lock_name, waiting_until_done_calls).second);
+  } else {
+    if (name2lock_status_it->second) {
+      try_lock_call->mut_response()->set_result(TryLockResult::kDoing);
+    } else {
+      try_lock_call->mut_response()->set_result(TryLockResult::kDone);
+    }
+  }
+  call->SendResponse();
+  ENQUEUE_REQUEST(TryLock);
+}
+
+void CtrlServer::NotifyDoneHandler(CtrlCallIf* call) {
+  using NotifyDoneCtrlCall = CtrlCall<NotifyDoneRequest, NotifyDoneResponse>;
+  auto notify_done_call = static_cast<NotifyDoneCtrlCall*>(call);
+  const std::string& lock_name = notify_done_call->request().name();
+  auto name2lock_status_it = name2lock_status_.find(lock_name);
+  auto waiting_calls =
+      static_cast<std::list<CtrlCallIf*>*>(name2lock_status_it->second);
+  for (CtrlCallIf* waiting_call : *waiting_calls) {
+    waiting_call->SendResponse();
+  }
+  delete waiting_calls;
+  name2lock_status_it->second = nullptr;
+  call->SendResponse();
+  ENQUEUE_REQUEST(NotifyDone);
+}
+
+void CtrlServer::WaitUntilDoneHandler(CtrlCallIf* call) {
+  using WaitUntilDoneCtrlCall =
+      CtrlCall<WaitUntilDoneRequest, WaitUntilDoneResponse>;
+  auto wait_until_done_call = static_cast<WaitUntilDoneCtrlCall*>(call);
+  const std::string& lock_name = wait_until_done_call->request().name();
+  void* lock_status = name2lock_status_.at(lock_name);
+  if (lock_status) {
+    auto waiting_calls = static_cast<std::list<CtrlCallIf*>*>(lock_status);
+    waiting_calls->push_back(call);
+  } else {
+    call->SendResponse();
+  }
+  ENQUEUE_REQUEST(WaitUntilDone);
+}
+
 }  // namespace oneflow
