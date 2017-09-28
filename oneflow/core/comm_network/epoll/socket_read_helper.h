66634c7e0b574d1b3ff0f50844d5b2871bbea96f
willzhang4a58 <willzhang4a58@gmail.com>
2017-09-28 16:56:56 +0800

    read helper init
    Former-commit-id: 61ce791b32c71895be838f0540b0b6f0c0e90592

--- oneflow/core/comm_network/epoll/socket_read_helper.h
+++ oneflow/core/comm_network/epoll/socket_read_helper.h
@@ -1,6 +1,7 @@
 #ifndef ONEFLOW_CORE_COMM_NETWORK_EPOLL_SOCKET_READ_HELPER_H_
 #define ONEFLOW_CORE_COMM_NETWORK_EPOLL_SOCKET_READ_HELPER_H_
 
+#include "oneflow/core/comm_network/epoll/socket_message.h"
 #include "oneflow/core/device/cpu_stream.h"
 
 #ifdef PLATFORM_POSIX
@@ -11,13 +12,33 @@ class SocketReadHelper final {
  public:
   OF_DISALLOW_COPY_AND_MOVE(SocketReadHelper);
   SocketReadHelper() = delete;
-  ~SocketReadHelper() { TODO(); }
+  ~SocketReadHelper();
 
-  SocketReadHelper(int sockfd, CpuStream* cpu_stream) { TODO(); }
+  SocketReadHelper(int sockfd, CpuStream* cpu_stream);
 
-  void NotifyMeSocketReadable() { TODO(); }
+  void NotifyMeSocketReadable();
 
  private:
+  void ReadUntilSocketNotReadable();
+
+  bool MsgHeadReadHandle();
+  bool MsgBodyReadHandle();
+
+  bool DoCurRead(bool (SocketReadHelper::*set_cur_read_done)());
+  bool SetStatusWhenMsgHeadDone();
+  bool SetStatusWhenMsgBodyDone();
+
+#define MAKE_ENTRY(x, y) bool SetStatusWhen##x##MsgHeadDone();
+  OF_PP_FOR_EACH_TUPLE(MAKE_ENTRY, SOCKET_MSG_TYPE_SEQ);
+#undef MAKE_ENTRY
+
+  int sockfd_;
+  CpuStream* cpu_stream_;
+
+  SocketMsg cur_msg_;
+  bool (SocketReadHelper::*cur_read_handle_)();
+  char* read_ptr_;
+  size_t read_size_;
 };
 
 }  // namespace oneflow
