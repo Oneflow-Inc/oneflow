7388fbcf8de94b6d64ebd1c3a7117e5b35e47bb8
willzhang4a58 <willzhang4a58@gmail.com>
2017-09-30 17:25:33 +0800

    run_scheduler on mutil machine

--- examples/mnist/train_mnist.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-rm -rf ./train_log ./core.* ./snapshots
-
-GLOG_logtostderr=0 GLOG_log_dir=./train_log GLOG_v=0 GLOG_logbuflevel=-1 ./scheduler \
-    -job_conf_filepath="./train_job.prototxt" \
-    -this_machine_name="centos-0"
