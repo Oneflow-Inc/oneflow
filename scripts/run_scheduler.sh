93f8e14f0d798651d84710336f319d1faee99329
willzhang4a58 <willzhang4a58@gmail.com>
2017-10-16 13:24:36 +0800

    scheduler.sh
    Former-commit-id: e09a67fedac3b770ad520f67fb64935d0e71ffbc

--- scripts/run_scheduler.sh
+++ scripts/run_scheduler.sh
@@ -3,7 +3,7 @@ set -x
 
 declare -a hosts=("192.168.1.11" "192.168.1.13")
 
-SCHEDULER_CMD='GLOG_logtostderr=0 GLOG_log_dir=./log GLOG_v=0 GLOG_logbuflevel=-1 nohup ./scheduler -job_conf_filepath=./job.prototxt'
+SCHEDULER_CMD='GLOG_logtostderr=0 GLOG_log_dir=./log GLOG_v=0 GLOG_logbuflevel=-1 nohup ./scheduler.sh -job_conf_filepath=./job.prototxt'
 
 set +e
 for host in "${hosts[@]}"
@@ -16,6 +16,6 @@ set -e
 for host in "${hosts[@]}"
 do
   ssh $USER@$host 'rm -rf ~/oneflow_temp/*'
-  scp ./compiler ./runtime ./scheduler ./*.prototxt $USER@$host:~/oneflow_temp
-  ssh $USER@$host "cd ~/oneflow_temp; $SCHEDULER_CMD -this_machine_name=$host &"
+  scp ./scheduler.sh ./*.prototxt $USER@$host:~/oneflow_temp
+  ssh $USER@$host "cd ~/oneflow_temp; $SCHEDULER_CMD -this_machine_name=$host 1>scheduler.sh.log 2>&1 </dev/null &"
 done
