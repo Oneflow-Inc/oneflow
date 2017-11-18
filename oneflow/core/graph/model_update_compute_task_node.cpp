63f6acdcbc8f774f9761ce14f762f1b8d73e7851
duduscript <duduscript@outlook.com>
2017-11-18 20:18:51 +0800

    model_update

--- oneflow/core/graph/model_update_compute_task_node.cpp
+++ oneflow/core/graph/model_update_compute_task_node.cpp
@@ -1,20 +1,22 @@
 #include "oneflow/core/graph/model_update_compute_task_node.h"
+#include "oneflow/core/graph/backward_compute_task_node.h"
+#include "oneflow/core/graph/chain_node.h"
+#include "oneflow/core/graph/forward_compute_task_node.h"
+#include "oneflow/core/graph/model_save_compute_task_node.h"
 #include "oneflow/core/operator/operator.h"
 
 namespace oneflow {
 
 void MdUpdtCompTaskNode::ProduceAllRegstsAndBindEdges() {
-  ProduceRegst("model_tmp", 1, 1);
-  ProduceRegst("model", 3, kMaxRegisterNum);
-  auto model_regst = GetProduceRegst("model");
-  auto model_tmp_regst = GetProduceRegst("model_tmp");
+  auto model_regst = ProduceRegst("model_tmp", 1, 1);
+  auto model_tmp_regst = ProduceRegst("model", 3, kMaxRegisterNum);
   for (TaskEdge* out_edge : out_edges()) {
     TaskNode* src_node = out_edge->src_node();
-    if (dynamic_cast<ForwardChainNode*>(src_node)) {
+    if (dynamic_cast<ForwardCompTaskNode*>(src_node) != nullptr) {
       out_edge->AddRegst("model", model_regst);
       out_edge->AddRegst("model_tmp", model_tmp_regst);
-    } else if (dynamic_cast<BackwardChainNode*>(src_node)
-               || dynamic_cast<MdSaveChainNode*>(src_node)) {
+    } else if (dynamic_cast<BackwardCompTaskNode*>(src_node) != nullptr
+               || dynamic_cast<MdSaveCompTaskNode*>(src_node) != nullptr) {
       out_edge->AddRegst("model", model_regst);
     } else {
       UNEXPECTED_RUN();
@@ -26,18 +28,17 @@ void MdUpdtCompTaskNode::ConsumeAllRegsts() {
   ConsumeRegst("model_diff_acc", SoleInEdge()->GetSoleRegst());
 }
 
-void MdUpdtCompTaskNode::Build() {
+void MdUpdtCompTaskNode::BuildExecGphAndRegst() {
   ExecNode* node = mut_exec_gph().NewNode();
   node->mut_op() = chain_node()->SoleOp();
-  auto model_diff_acc_regst = SoleInEdge->GetSoleRegst();
+  auto model_diff_acc_regst = SoleInEdge()->GetSoleRegst();
   node->BindBnInOpAndRegst("model_diffs", model_diff_acc_regst);
   auto model_regst = GetProducedRegst("model");
-  node->BindBnInOpAndRegst(exec->op()->SoleObn(), model_regst);
-  ProduceRegst("data_tmp", 1, 1);
-  auto data_tmp_regst = GetProducedRegst("data_tmp");
+  node->BindBnInOpAndRegst(node->op()->SoleObn(), model_regst);
+  auto data_tmp_regst = ProduceRegst("data_tmp", 1, 1);
   for (const std::string dtbn : node->op()->data_tmp_bns()) {
     const std::string& lbn = node->op()->Lbn4BnInOp(dtbn);
-    data_tmp_regst->EnrollDataTmpBn(lbn);
+    data_tmp_regst->AddLbn(lbn);
     node->BindBnInOpAndRegst(dtbn, data_tmp_regst);
   }
 }
