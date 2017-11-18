d0e6e09222e2b15fb3826484c7076d9f9746d2ec
duduscript <duduscript@outlook.com>
2017-11-18 20:27:39 +0800

    remove fix in boxingop

--- oneflow/core/graph/model_update_compute_task_node.h
+++ oneflow/core/graph/model_update_compute_task_node.h
@@ -12,8 +12,6 @@ class MdUpdtCompTaskNode final : public CompTaskNode {
   ~MdUpdtCompTaskNode() = default;
 
   void ProduceAllRegstsAndBindEdges() override;
-  void ConsumeAllRegsts() override;
-  void Build() override;
 
   void set_random_seed(uint32_t val) { random_seed_ = val; }
   TaskType GetTaskType() const override { return TaskType::kMdUpdt; }
