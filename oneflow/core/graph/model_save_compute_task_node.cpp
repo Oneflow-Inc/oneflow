18ae2cd8d288ec27901124c1026fe699f7bc4a3e
duduscript <duduscript@outlook.com>
2017-11-18 20:47:20 +0800

    model_save

--- oneflow/core/graph/model_save_compute_task_node.cpp
+++ oneflow/core/graph/model_save_compute_task_node.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/graph/model_save_compute_task_node.h"
+#include "oneflow/core/graph/chain_node.h"
 
 namespace oneflow {
 
@@ -8,9 +9,9 @@ void MdSaveCompTaskNode::ConsumeAllRegsts() {
   ConsumeRegst("model", SoleInEdge()->GetSoleRegst());
 }
 
-void MdSaveCompTaskNode::Build() {
+void MdSaveCompTaskNode::BuildExecGphAndRegst() {
   ExecNode* node = mut_exec_gph().NewNode();
-  node->mut_op() = ChainNode()->SoleOp();
+  node->mut_op() = chain_node()->SoleOp();
   for (const std::string& ibn : node->op()->input_bns()) {
     node->BindBnInOpAndRegst(ibn, SoleInEdge()->GetSoleRegst());
   }
