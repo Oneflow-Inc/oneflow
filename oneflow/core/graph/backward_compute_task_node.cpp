19001cf19fb2985878738582c3c4e2caacb22c96
zyeric <zyeric@github.com>
2017-11-20 01:29:53 +0800

    remove useless head file

--- oneflow/core/graph/backward_compute_task_node.cpp
+++ oneflow/core/graph/backward_compute_task_node.cpp
@@ -1,10 +1,5 @@
 #include "oneflow/core/graph/backward_compute_task_node.h"
-#include "oneflow/core/graph/boxing_task_node.h"
-#include "oneflow/core/graph/chain_graph.h"
 #include "oneflow/core/graph/chain_node.h"
-#include "oneflow/core/graph/forward_compute_task_node.h"
-#include "oneflow/core/graph/model_diff_accumulate_compute_task_node.h"
-#include "oneflow/core/graph/model_update_compute_task_node.h"
 
 namespace oneflow {
 
--- oneflow/core/graph/forward_compute_task_node.cpp
+++ oneflow/core/graph/forward_compute_task_node.cpp
@@ -1,7 +1,5 @@
 #include "oneflow/core/graph/forward_compute_task_node.h"
-#include "oneflow/core/graph/backward_compute_task_node.h"
 #include "oneflow/core/graph/chain_node.h"
-#include "oneflow/core/graph/model_update_compute_task_node.h"
 
 namespace oneflow {
 
