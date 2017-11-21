32d30b691fa6c116c7ea2031cf9e2497314262a5
zyeric <zyeric@github.com>
2017-11-21 18:50:39 +0800

    updt implementation & fix bugs

--- oneflow/core/graph/source_compute_task_node.cpp
+++ oneflow/core/graph/source_compute_task_node.cpp
@@ -4,6 +4,7 @@
 namespace oneflow {
 
 void SourceCompTaskNode::ProduceAllRegstsAndBindEdges() {
+  ProduceRegst("data_tmp", 1, 1);
   auto out_regst = ProduceRegst("out", 1, kMaxRegisterNum);
   SoleOutEdge()->AddRegst("out", out_regst);
 }
@@ -12,11 +13,19 @@ void SourceCompTaskNode::ConsumeAllRegsts() {}
 
 void SourceCompTaskNode::BuildExecGphAndRegst() {
   auto out_regst = GetProducedRegst("out");
+  auto data_tmp_regst = GetProducedRegst("data_tmp");
   ExecNode* node = mut_exec_gph().NewNode();
   node->mut_op() = chain_node()->SoleOp();
+  auto data_output_lbns = chain_node()->data_output_lbns();
   for (const std::string& obn : node->op()->output_bns()) {
-    out_regst->AddLbn(node->op()->Lbn4BnInOp(obn));
-    node->BindBnInOpAndRegst(obn, out_regst);
+    auto lbn = node->op()->Lbn4BnInOp(obn);
+    if (data_output_lbns.find(lbn) == data_output_lbns.end()) {
+      data_tmp_regst->AddLbn(lbn);
+      node->BindBnInOpAndRegst(obn, data_tmp_regst);
+    } else {
+      out_regst->AddLbn(lbn);
+      node->BindBnInOpAndRegst(obn, out_regst);
+    }
   }
   node->op()->InferBlobDescs(node->GetBlobDesc4BnInOpFunc(), parallel_ctx());
 }
