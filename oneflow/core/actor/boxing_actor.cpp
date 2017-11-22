dcbe58b658f69cd17494cff380492e25362af06c
willzhang4a58 <willzhang4a58@gmail.com>
2017-11-22 15:56:51 +0800

    remove set_num_of_remaining_eord

--- oneflow/core/actor/boxing_actor.cpp
+++ oneflow/core/actor/boxing_actor.cpp
@@ -4,8 +4,6 @@
 namespace oneflow {
 
 void BoxingActor::VirtualActorInit(const TaskProto& task_proto) {
-  int num_of_consumed_regsts = task_proto.consumed_regst_desc_id().size();
-  set_num_of_remaining_eord(num_of_consumed_regsts);
   mut_device_ctx().reset(new CpuDeviceCtx());
   OF_SET_MSG_HANDLER(&BoxingActor::HandlerNormal);
 }
