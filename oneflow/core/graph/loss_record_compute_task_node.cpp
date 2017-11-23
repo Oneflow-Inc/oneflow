47d0d9d08a03c8c30f44acc06feec2eac1f9cea1
chengtbf <472491134@qq.com>
2017-11-23 16:27:27 +0800

    loss record

--- oneflow/core/graph/loss_record_compute_task_node.cpp
+++ oneflow/core/graph/loss_record_compute_task_node.cpp
@@ -1,8 +1,23 @@
 #include "oneflow/core/graph/loss_record_compute_task_node.h"
+#include "oneflow/core/graph/chain_node.h"
 
 namespace oneflow {
 
 void LossRecordCompTaskNode::ProduceAllRegstsAndBindEdges() {}
+
+void LossRecordCompTaskNode::ConsumeAllRegsts() {
+  ConsumeRegst("in", SoleInEdge()->GetSoleRegst());
+}
+
+void LossRecordCompTaskNode::BuildExecGphAndRegst() {
+  auto in_regst = GetConsumedRegst("in");
+  ExecNode* exec_node = mut_exec_gph().NewNode();
+  exec_node->mut_op() = chain_node()->SoleOp();
+  auto op = exec_node->op();
+  CHECK(op->IsRecordOp());
+  exec_node->BindBnInOpAndRegst(op->SoleIbn(), in_regst);
+  op->InferBlobDescs(exec_node->GetBlobDesc4BnInOpFunc(), parallel_ctx());
+}
 
 void LossRecordCompTaskNode::FixThrdId() {
   set_thrd_id(IDMgr::Singleton()->AllocatePersistenceThrdId(machine_id()));
