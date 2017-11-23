652a778ecbe447211b24b7ba30f5e46586f55809
spwlyzx <spwlyzx@gmail.com>
2017-11-23 21:53:57 +0800

    add clone_op necessary virtual function

--- oneflow/core/operator/clone_op.h
+++ oneflow/core/operator/clone_op.h
@@ -24,6 +24,10 @@ class CloneOp final : public Operator {
   std::string obn2lbn(const std::string& output_bn) const override {
     return op_name() + "/" + output_bn;
   }
+  void VirtualGenKernelConf(
+      std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+      const ParallelContext* parallel_ctx,
+      KernelConf* kernel_conf) const override;
 };
 
 }  // namespace oneflow
