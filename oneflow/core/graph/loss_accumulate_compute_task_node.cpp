2db68268f4283d980b4422236c3ead9c8edef97a
chengtbf <472491134@qq.com>
2017-11-23 19:57:19 +0800

    fix review

--- oneflow/core/graph/loss_accumulate_compute_task_node.cpp
+++ oneflow/core/graph/loss_accumulate_compute_task_node.cpp
@@ -4,15 +4,15 @@
 namespace oneflow {
 
 void LossAccCompTaskNode::BuildExecGphAndRegst() {
-  std::shared_ptr<RegstDesc> in_regst = GetConsumedRegst("one");
+  std::shared_ptr<RegstDesc> one_regst = GetConsumedRegst("one");
   std::shared_ptr<RegstDesc> acc_regst = GetProducedRegst("acc");
   ExecNode* exec_node = mut_exec_gph().NewNode();
   exec_node->mut_op() = chain_node()->SoleOp();
   std::shared_ptr<const Operator> op = exec_node->op();
-  exec_node->BindBnInOpAndRegst(op->SoleIbn(), in_regst);
+  exec_node->BindBnInOpAndRegst(op->SoleIbn(), one_regst);
   acc_regst->AddLbn(op->Lbn4BnInOp(op->SoleObn()));
   exec_node->BindBnInOpAndRegst(op->SoleObn(), acc_regst);
-  op->InferBlobDescs(exec_node->GetBlobDesc4BnInOpFunc(), parallel_ctx());
+  acc_regst->CopyBlobDescFrom(one_regst.get());
 }
 
 }  // namespace oneflow
