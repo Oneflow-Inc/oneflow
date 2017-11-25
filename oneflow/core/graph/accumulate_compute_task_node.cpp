2ff117f7ad8c0e05d6ae593aea0096812e6adb9e
chengtbf <472491134@qq.com>
2017-11-25 16:47:27 +0800

    Dev cc loss acc (#406)
    * acc compute task node
    
    * loss acc
    
    * loss acc
    
    * delete auto
    
    * fix review
    
    * change order
    
    * delete wrong add lbn in acc node
    
    * move build exec graph from loss acc to acc
    
    
    Former-commit-id: 4d504c448e238ca9387880341d58aca3d81b8ed3

--- oneflow/core/graph/accumulate_compute_task_node.cpp
+++ oneflow/core/graph/accumulate_compute_task_node.cpp
@@ -1,9 +1,26 @@
+#include "oneflow/core/graph/chain_node.h"
 #include "oneflow/core/graph/loss_accumulate_compute_task_node.h"
 
 namespace oneflow {
 
 void AccCompTaskNode::ProduceAllRegstsAndBindEdges() {
-  ProduceRegst("acc", 1, kMaxRegisterNum);
+  auto acc_regst = ProduceRegst("acc", 1, kMaxRegisterNum);
+  SoleOutEdge()->AddRegst("acc", acc_regst);
+}
+
+void AccCompTaskNode::ConsumeAllRegsts() {
+  ConsumeRegst("one", SoleInEdge()->GetSoleRegst());
+}
+
+void AccCompTaskNode::BuildExecGphAndRegst() {
+  std::shared_ptr<RegstDesc> one_regst = GetConsumedRegst("one");
+  std::shared_ptr<RegstDesc> acc_regst = GetProducedRegst("acc");
+  acc_regst->CopyBlobDescFrom(one_regst.get());
+  std::shared_ptr<const Operator> op = chain_node()->SoleOp();
+  ExecNode* exec_node = mut_exec_gph().NewNode();
+  exec_node->mut_op() = op;
+  exec_node->BindBnInOpAndRegst(op->SoleIbn(), one_regst);
+  exec_node->BindBnInOpAndRegst(op->SoleObn(), acc_regst);
 }
 
 }  // namespace oneflow
