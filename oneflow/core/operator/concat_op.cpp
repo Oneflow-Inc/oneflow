ad51965c1c23e1da6dabc8dc0f5aa5da7aa3ef16
zyeric <zyeric@github.com>
2017-11-26 16:59:17 +0800

    infer useful info in concat op

--- oneflow/core/operator/concat_op.cpp
+++ oneflow/core/operator/concat_op.cpp
@@ -46,8 +46,64 @@ void ConcatOp::InferBlobDescs(
 void ConcatOp::VirtualGenKernelConf(
     std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
+  const ConcatOpConf& concat_op_conf = op_conf().concat_conf();
+  ConcatKernelConf* concat_kernel_conf = kernel_conf->mutable_concat_conf();
+
   DataType dtype = GetBlobDesc4BnInOp(input_bns().at(0))->data_type();
-  kernel_conf->mutable_concat_conf()->set_data_type(dtype);
+  concat_kernel_conf->set_data_type(dtype);
+  const size_t elem_size = GetSizeOfDataType(dtype);
+
+  const int64_t& concat_axis = concat_op_conf.axis();
+
+  const BlobDesc* out_blob = GetBlobDesc4BnInOp(kernel_conf->output_bns(0));
+  const BlobDesc* out_diff_blob =
+      GetBlobDesc4BnInOp(kernel_conf->output_diff_bns(0));
+
+  concat_kernel_conf->set_fw_concat_element_cnt(1);
+  if ((concat_axis != (out_blob->shape().NumAxes() - 1))
+      && (concat_axis != -1)) {
+    concat_kernel_conf->set_fw_concat_element_cnt(
+        out_blob->shape().Count(concat_axis + 1));
+  }
+  concat_kernel_conf->set_bw_concat_element_cnt(1);
+  if ((concat_axis != (out_diff_blob->shape().NumAxes() - 1))
+      && (concat_axis != -1)) {
+    concat_kernel_conf->set_bw_concat_element_cnt(
+        out_diff_blob->shape().Count(concat_axis + 1));
+  }
+
+  concat_kernel_conf->set_fw_concat_num_each_blob(1);
+  if ((concat_axis != (-out_blob->shape().NumAxes())) && (concat_axis != 0)) {
+    concat_kernel_conf->set_fw_concat_num_each_blob(
+        out_blob->shape().Count(0, concat_axis));
+  }
+  concat_kernel_conf->set_bw_concat_num_each_blob(1);
+  if ((concat_axis != (-out_diff_blob->shape().NumAxes()))
+      && (concat_axis != 0)) {
+    concat_kernel_conf->set_bw_concat_num_each_blob(
+        out_diff_blob->shape().Count(0, concat_axis));
+  }
+
+  size_t index = 0;
+  for (const std::string& ibn : kernel_conf->input_bns()) {
+    const BlobDesc* in_blob = GetBlobDesc4BnInOp(ibn);
+    const int64_t in_concat_axis_dim = in_blob->shape().At(concat_axis);
+    const int64_t cp_sz = in_concat_axis_dim
+                          * concat_kernel_conf->fw_concat_element_cnt()
+                          * elem_size;
+    concat_kernel_conf->set_fw_cp_szs(index, cp_sz);
+    index++;
+  }
+  index = 0;
+  for (const std::string& idbn : kernel_conf->input_diff_bns()) {
+    const BlobDesc* in_diff_blob = GetBlobDesc4BnInOp(idbn);
+    const int64_t in_concat_axis_dim = in_diff_blob->shape().At(concat_axis);
+    const int64_t cp_sz = in_concat_axis_dim
+                          * concat_kernel_conf->bw_concat_element_cnt()
+                          * elem_size;
+    concat_kernel_conf->set_bw_cp_szs(index, cp_sz);
+    index++;
+  }
 }
 
 REGISTER_OP(OperatorConf::kConcatConf, ConcatOp);
