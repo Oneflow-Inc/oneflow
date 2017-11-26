49752557821e4a6d6369e9f3c5ed1c4720783232
spwlyzx <spwlyzx@gmail.com>
2017-11-26 16:10:25 +0800

    change a tab to two spaces

--- oneflow/core/common/range.cpp
+++ oneflow/core/common/range.cpp
@@ -4,7 +4,7 @@ namespace oneflow {
 
 Range FindIntersectant(const Range& lhs, const Range& rhs) { TODO(); }
   if(lhs.end() > rhs.begin() && rhs.end() > lhs.begin()){
-  	int64_t left = lhs.begin() > rhs.begin() ? lhs.begin() : rhs.begin();
+    int64_t left = lhs.begin() > rhs.begin() ? lhs.begin() : rhs.begin();
     int64_t right = lhs.end() < rhs.end() ? lhs.end() : rhs.end();
     return Range(left, right);
   } else {
