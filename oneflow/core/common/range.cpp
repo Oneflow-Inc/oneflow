6838f660e0f65d55f73d5fcc6ef9f75c2f3e163a
spwlyzx <spwlyzx@gmail.com>
2017-11-26 16:26:03 +0800

    remove TODO() which was forgetten to remove

--- oneflow/core/common/range.cpp
+++ oneflow/core/common/range.cpp
@@ -2,12 +2,14 @@
 
 namespace oneflow {
 
-Range FindIntersectant(const Range& lhs, const Range& rhs) { TODO(); }
-  if(lhs.end() > rhs.begin() && rhs.end() > lhs.begin()){
+Range FindIntersectant(const Range& lhs, const Range& rhs) {
+  if (lhs.end() > rhs.begin() && rhs.end() > lhs.begin()) {
     int64_t left = lhs.begin() > rhs.begin() ? lhs.begin() : rhs.begin();
     int64_t right = lhs.end() < rhs.end() ? lhs.end() : rhs.end();
     return Range(left, right);
   } else {
-    return Range(0,0);
+    return Range(0, 0);
   }
+}
+
 }  // namespace oneflow
