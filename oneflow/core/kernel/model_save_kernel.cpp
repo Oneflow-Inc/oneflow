683ff8f150c951736fc06a8de193d3207bf13b6c
cheng cheng <472491134@qq.com>
2017-11-28 19:25:35 +0800

    change var name

--- oneflow/core/kernel/model_save_kernel.cpp
+++ oneflow/core/kernel/model_save_kernel.cpp
@@ -14,7 +14,8 @@ void ModelSaveKernel::Forward(
     const KernelCtx& kernel_ctx,
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   Snapshot* snapshot = static_cast<Snapshot*>(kernel_ctx.other);
-  const size_t elem_size = GetSizeOfDataType(this->kernel_conf().data_type());
+  const size_t byte_size_of_data_field =
+      GetSizeOfDataType(this->kernel_conf().data_type());
   for (const std::string& ibn : kernel_conf().input_bns()) {
     const std::string& lbn = Lbn4BnInOp(ibn);
     Blob* blob_ptr = BnInOp2Blob(ibn);
@@ -22,7 +23,7 @@ void ModelSaveKernel::Forward(
       std::unique_ptr<PersistentOutStream> out_stream =
           snapshot->GetOutStream(lbn, part_id_);
       out_stream->Write(blob_ptr->dptr<char>(),
-                        blob_ptr->shape().elem_cnt() * elem_size);
+                        blob_ptr->shape().elem_cnt() * byte_size_of_data_field);
     }
     snapshot->OnePartDone(lbn, part_id_, part_num_);
   }
