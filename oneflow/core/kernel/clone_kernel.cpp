0e8732b8edc81fa7afcd619575a5b19e4804ad18
spwlyzx <spwlyzx@gmail.com>
2017-11-28 16:50:53 +0800

    change the copy of total content to only data content

--- oneflow/core/kernel/clone_kernel.cpp
+++ oneflow/core/kernel/clone_kernel.cpp
@@ -29,9 +29,9 @@ void CloneKernel<device_type, T>::BackwardDataContent(
   if (odbns.size() == 0) return;
   Blob* in_diff_blob = BnInOp2Blob(this->kernel_conf().input_diff_bns(0));
   const Blob* out_diff_blob_0 = BnInOp2Blob(odbns[0]);
-  Memcpy<device_type>(ctx.device_ctx, in_diff_blob->mut_memory_ptr(),
-                      out_diff_blob_0->memory_ptr(),
-                      out_diff_blob_0->TotalByteSize());
+  Memcpy<device_type>(ctx.device_ctx, in_diff_blob->mut_dptr(),
+                      out_diff_blob_0->dptr(),
+                      out_diff_blob_0->ByteSizeOfDataContentField());
   for (size_t i = 1; i != odbns.size(); ++i) {
     const Blob* out_diff_blob = BnInOp2Blob(odbns[i]);
     CloneKernelUtil<device_type, T>::AdditionAssign(
@@ -68,13 +68,12 @@ OF_PP_FOR_EACH_TUPLE(DEFINE_NONFLOAT_CLONE_KERNEL_UTIL,
 
 namespace {
 
+Kernel* CreateCloneKernel(DeviceType dev_type, const KernelConf& kernel_conf) {
+  static const HashMap<std::string, std::function<Kernel*()>> creators = {
 #define CLONE_KERNEL_ENTRY(device_type, data_type_pair)                       \
   {GetHashKey(device_type, OF_PP_PAIR_SECOND(data_type_pair)), []() {         \
      return new CloneKernel<device_type, OF_PP_PAIR_FIRST(data_type_pair)>(); \
    }},
-
-Kernel* CreateCloneKernel(DeviceType dev_type, const KernelConf& kernel_conf) {
-  static const HashMap<std::string, std::function<Kernel*()>> creators = {
       OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(CLONE_KERNEL_ENTRY, DEVICE_TYPE_SEQ,
                                        FLOATING_DATA_TYPE_SEQ)};
   return creators.at(GetHashKey(dev_type, kernel_conf.data_type()))();
