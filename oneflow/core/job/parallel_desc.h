832e1666cc038e31ca46cebdf06f3c3bbc1333fa
chengtbf <472491134@qq.com>
2017-11-30 12:54:49 +0800

    model save kernel (#419)
    * model save kernel
    
    * model_save_op
    
    * command
    
    * refine model save kernel
    
    * add func for partid and num , refine model save kernel interface
    
    * rename func
    
    * change var name
    
    * ByteSizeOfDataContentField
    
    
    Former-commit-id: ede9a8e6146bcda05190a138c5f8c03bf7d72464

--- oneflow/core/job/parallel_desc.h
+++ oneflow/core/job/parallel_desc.h
@@ -44,6 +44,9 @@ class ParallelDesc {
   int64_t parallel_num_;
 };
 
+std::tuple<int32_t, int32_t> GetPartIdAndPartNumFromParallelCtx(
+    const ParallelContext* parallel_ctx);
+
 }  // namespace oneflow
 
 #endif  // ONEFLOW_CORE_JOB_PARALLEL_DESC_H_
