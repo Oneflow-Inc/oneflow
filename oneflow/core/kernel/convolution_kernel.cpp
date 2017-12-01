153dc661dc67f06c130623366f6817439d153e14
syshangguan <shgshy@gmail.com>
2017-12-01 15:06:58 +0800

    reset convolution kernel

--- oneflow/core/kernel/convolution_kernel.cpp
+++ oneflow/core/kernel/convolution_kernel.cpp
@@ -271,7 +271,7 @@ void ConvolutionKernel<device_type, T>::InitModelBlobsWithDir(
 
 template<DeviceType device_type, typename T>
 void ConvolutionKernel<device_type, T>::InitModelTmpBlobs(
-    const KernelCtx& ctx, const ParallelContext& parallel_ctx,
+    const KernelCtx& ctx, const ParallelContext* parallel_ctx,
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   if (this->op_conf().convolution_conf().has_bias_term()) {
     FillConf bias_multiplier_fill_conf;
@@ -281,26 +281,7 @@ void ConvolutionKernel<device_type, T>::InitModelTmpBlobs(
   }
 }
 
-namespace {
-
-Kernel* CreateConvolutionKernel(DeviceType dev_type,
-                                const KernelConf& kernel_conf) {
-  static const HashMap<std::string, std::function<Kernel*()>> creators = {
-#define CONVOLUTION_KERNEL_ENTRY(device_type, data_type_pair)          \
-  {GetHashKey(device_type, OF_PP_PAIR_SECOND(data_type_pair)), []() {  \
-     return new ConvolutionKernel<device_type,                         \
-                                  OF_PP_PAIR_FIRST(data_type_pair)>(); \
-   }},
-      OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(
-          CONVOLUTION_KERNEL_ENTRY, DEVICE_TYPE_SEQ, FLOATING_DATA_TYPE_SEQ)};
-  CHECK(kernel_conf.has_convolution_conf());
-  return creators.at(
-      GetHashKey(dev_type, kernel_conf.convolution_conf().data_type()))();
-}
-
-}  // namespace
-
-COMMAND(AddKernelCreator(OperatorConf::kConvolutionConf,
-                         CreateConvolutionKernel));
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kConvolutionConf, ConvolutionKernel,
+                           FLOATING_DATA_TYPE_SEQ);
 
 }  // namespace oneflow
