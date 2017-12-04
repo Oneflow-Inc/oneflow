629c77b4fe1ba77cda5506986819318da5440754
willzhang4a58 <willzhang4a58@gmail.com>
2017-12-04 14:00:25 +0800

    use default kernel creator for model update kernel
    Former-commit-id: d496b81176cd68271b97ab09c9fd572bce4f200c

--- oneflow/core/kernel/momentum_model_update_kernel.cpp
+++ oneflow/core/kernel/momentum_model_update_kernel.cpp
@@ -31,24 +31,7 @@ void MomentumMdUpdateKernel<device_type, T>::Forward(
       momentum_blob->dptr<T>(), 1, model_blob->mut_dptr<T>(), 1);
 }
 
-namespace {
-
-Kernel* CreateMomentumMdUpdateKernel(DeviceType device_type,
-                                     const KernelConf& kernel_conf) {
-  static const HashMap<std::string, std::function<Kernel*()>> creators = {
-#define MODEL_UPDATE_KERNEL_ENTRY(device_type, data_type_pair)              \
-  {GetHashKey(device_type, OF_PP_PAIR_SECOND(data_type_pair)), []() {       \
-     return new MomentumMdUpdateKernel<device_type,                         \
-                                       OF_PP_PAIR_FIRST(data_type_pair)>(); \
-   }},
-      OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(
-          MODEL_UPDATE_KERNEL_ENTRY, DEVICE_TYPE_SEQ, FLOATING_DATA_TYPE_SEQ)};
-  return creators.at(GetHashKey(device_type, kernel_conf.data_type()))();
-}
-
-}  // namespace
-
-COMMAND(AddKernelCreator(OperatorConf::kMomentumMdupdtConf,
-                         CreateMomentumMdUpdateKernel))
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kMomentumMdupdtConf,
+                           MomentumMdUpdateKernel, FLOATING_DATA_TYPE_SEQ);
 
 }  // namespace oneflow
