2dfaa94d59ca04c0279ebaa90af72165fd53a4d0
willzhang4a58 <willzhang4a58@gmail.com>
2017-12-05 15:14:53 +0800

    update train example

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 set -e
 set -x
 
-declare -a hosts=("192.168.1.11" "192.168.1.13")
+declare -a hosts=("192.168.1.11")
 
-ONEFLOW_CMD='GLOG_logtostderr=0 GLOG_log_dir=./log GLOG_v=0 GLOG_logbuflevel=-1 nohup ./oneflow.run -job_conf_filepath=./job.prototxt'
+ONEFLOW_CMD='GLOG_logtostderr=0 GLOG_log_dir=./log GLOG_v=0 GLOG_logbuflevel=-1 nohup ./oneflow -job_conf_filepath=./job.prototxt'
 
 set +e
 for host in "${hosts[@]}"
@@ -15,6 +15,6 @@ set -e
 for host in "${hosts[@]}"
 do
   ssh $USER@$host 'rm -rf ~/oneflow_temp/*'
-  scp ./oneflow.run ./*.prototxt $USER@$host:~/oneflow_temp
-  ssh $USER@$host "cd ~/oneflow_temp; $ONEFLOW_CMD -this_machine_name=$host 1>oneflow.run.log 2>&1 </dev/null &"
+  scp ./oneflow ./*.prototxt $USER@$host:~/oneflow_temp
+  ssh $USER@$host "cd ~/oneflow_temp; $ONEFLOW_CMD -this_machine_name=$host 1>/dev/null 2>&1 </dev/null &"
 done
