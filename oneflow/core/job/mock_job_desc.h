65dca636fa9d6b96dca34697da5b14c9a7ab22f2
duduscript <duduscript@outlook.com>
2017-12-05 19:53:27 +0800

    Add mockjobdesc

---
+++ oneflow/core/job/mock_job_desc.h
@@ -0,0 +1,24 @@
+#include <gmock/gmock.h>
+#include "oneflow/core/common/data_type.h"
+#include "oneflow/core/job/job_desc.h"
+
+namespace oneflow {
+
+template<typename T>
+class MockJobDesc : public JobDesc {
+ public:
+  MockJobDesc() = default;
+  MOCK_METHOD0(SizeOfOneDataId, int32_t());
+  MOCK_METHOD0(DefaultDataType, DataType());
+};
+
+template<typename T>
+void InitJobDescSingleton(MockJobDesc<T>& mock_job_desc) {
+  EXPECT_CALL(mock_job_desc, SizeOfOneDataId())
+      .WillRepeatedly(testing::Return(8));
+  EXPECT_CALL(mock_job_desc, DefaultDataType())
+      .WillRepeatedly(testing::Return(GetDataType<T>::val));
+  *(JobDesc::SingletonPPtr()) = &mock_job_desc;
+}
+
+}  // namespace oneflow
