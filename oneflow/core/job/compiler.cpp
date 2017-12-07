3fac22688e597af1ee64f6280e54c8cce2138b37
ShawnXuan <xiexuanx2@gmail.com>
2017-12-07 12:37:16 +0800

    Dev xuan plan2 dot (#438)
    * xuan test only
    
    * remove unused run.sh
    
    * refine Plan2Dot

--- oneflow/core/job/compiler.cpp
+++ oneflow/core/job/compiler.cpp
@@ -4,8 +4,55 @@ namespace oneflow {
 
 namespace {
 
-void ToDotFile(const Plan& plan, const std::string& filepath) {}
+std::map<TaskType, std::pair<std::string, std::string>> task_type2dot_type = {
+    {kInvalid, {"plaintext", ""}},   {kForward, {"house", ""}},
+    {kBackward, {"invhouse", ""}},   {kSource, {"folder", ""}},
+    {kLoss, {"ellipse", ""}},        {kLossAcc, {"ellipse", "filled"}},
+    {kLossPrint, {"signature", ""}}, {kMdUpdt, {"box", "rounded"}},
+    {kMdSave, {"folder", "filled"}}, {kMdDiffAcc, {"box", "rounded,filled"}},
+    {kCopyHd, {"note", ""}},         {kCopyCommNet, {"tab", ""}},
+    {kBoxing, {"box3d", ""}},        {kPrint, {"signature", ""}},
+};
 
+void ToDotFile(const Plan& plan, const std::string& filepath) {
+  PersistentOutStream out_stream(LocalFS(), filepath);
+  out_stream << "digraph {\n";
+  HashSet<int64_t> regst_desc_ids;
+  for (const TaskProto& task_proto : plan.task()) {
+    out_stream << "task" << std::to_string(task_proto.task_id()) << "[label=\""
+               << std::to_string(task_proto.task_id())
+               << "\\nthrd_id:" << std::to_string(task_proto.thrd_id())
+               << "\\nparallel_id:"
+               << std::to_string(task_proto.parallel_ctx().parallel_id())
+               << "\", shape="
+               << task_type2dot_type[task_proto.task_type()].first;
+    if (task_type2dot_type[task_proto.task_type()].second != "")
+      out_stream << ", style=\""
+                 << task_type2dot_type[task_proto.task_type()].second << "\"";
+    out_stream << "];\n";
+    for (const auto& pair : task_proto.produced_regst_desc()) {
+      regst_desc_ids.insert(pair.second.regst_desc_id());
+    }
+  }
+  for (const int64_t regst_task_id : regst_desc_ids) {
+    out_stream << "regst_desc" << std::to_string(regst_task_id) << "[label=\""
+               << std::to_string(regst_task_id) << "\", shape=box];\n";
+  }
+  for (const TaskProto& task_proto : plan.task()) {
+    for (const auto& pair : task_proto.produced_regst_desc()) {
+      out_stream << "task" << std::to_string(task_proto.task_id())
+                 << "->regst_desc"
+                 << std::to_string(pair.second.regst_desc_id()) << "[label=\""
+                 << pair.first << "\"];\n";
+    }
+    for (const auto& pair : task_proto.consumed_regst_desc_id()) {
+      out_stream << "regst_desc" << std::to_string(pair.second) << "->task"
+                 << std::to_string(task_proto.task_id()) << "[label=\""
+                 << pair.first << "\"];\n";
+    }
+  }
+  out_stream << "}\n";
+}
 }  // namespace
 
 Plan Compiler::Compile() {
