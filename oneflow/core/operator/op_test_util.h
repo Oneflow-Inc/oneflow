335a49a06f02290e2da027fec5dab7cd3f6df7c1
duduscript <duduscript@outlook.com>
2017-12-08 20:19:48 +0800

    change op_test_util API

--- oneflow/core/operator/op_test_util.h
+++ oneflow/core/operator/op_test_util.h
@@ -12,7 +12,7 @@ std::shared_ptr<Operator> CreateCloneOp(int out_num) {
 
 void GenBn2BlobDescMap(HashMap<std::string, BlobDesc*>& bn2blobdesc_map,
                        const std::vector<std::string>& ibns,
-                       const std::vector<std::string>& obns,
+                       const std::vector<std::string>& other_bns,
                        const std::vector<std::vector<int64_t>>& in_shapes,
                        const DataType& data_type, bool has_data_id) {
   CHECK_EQ(ibns.size(), in_shapes.size());
@@ -20,7 +20,7 @@ void GenBn2BlobDescMap(HashMap<std::string, BlobDesc*>& bn2blobdesc_map,
     bn2blobdesc_map[ibns.at(i)] =
         new BlobDesc(Shape(in_shapes.at(i)), data_type, has_data_id);
   }
-  for (const std::string& obn : obns) { bn2blobdesc_map[obn] = new BlobDesc; }
+  for (const std::string& bn : other_bns) { bn2blobdesc_map[bn] = new BlobDesc; }
 }
 
 }  // namespace oneflow
