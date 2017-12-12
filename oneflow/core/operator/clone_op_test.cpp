baf58fc8494f93f6c7d82f4d9288416a0b7dd80b
duduscript <duduscript@outlook.com>
2017-12-12 18:29:36 +0800

    remove changes

--- oneflow/core/operator/clone_op_test.cpp
+++ oneflow/core/operator/clone_op_test.cpp
@@ -1,44 +1,30 @@
-#include "oneflow/core/job/mock_job_desc.h"
 #include "oneflow/core/operator/clone_op.h"
-#include "oneflow/core/operator/op_test_util.h"
 
 namespace oneflow {
 
 template<typename T, bool has_data_id>
-void DoCloneOpTest(const int out_num,
-                   const std::vector<std::vector<int64_t>>& in_shapes,
-                   const std::vector<std::string>& ibns,
-                   const std::vector<std::string>& obns,
-                   const std::vector<std::string>& other_bns) {
-  auto clone_op = CreateCloneOp(out_num);
+void TestCloneOp() {
+  OperatorConf op_conf;
+  op_conf.set_name("clone_test");
+  op_conf.mutable_clone_conf()->set_out_num(3);
+  op_conf.mutable_clone_conf()->set_lbn("clone_lbn");
+  op_conf.mutable_clone_conf()->set_data_type(GetDataType<T>::val);
+  auto clone_op = ConstructOp(op_conf);
   HashMap<std::string, BlobDesc*> bn2blobdesc_map;
-  auto bn2blobdesc_func =
-      ConstructBn2BlobDescFunc(bn2blobdesc_map, ibns, obns, other_bns,
-                               in_shapes, GetDataType<T>::val, has_data_id);
-  clone_op->InferBlobDescs(bn2blobdesc_func, nullptr);
-
+  bn2blobdesc_map[clone_op->SoleIbn()] =
+      new BlobDesc(Shape({4, 3}), GetDataType<T>::val, has_data_id);
+  for (const std::string& obn : clone_op->output_bns()) {
+    bn2blobdesc_map[obn] = new BlobDesc;
+  }
+  auto bn2blobdesc_func = [&](const std::string& bn) {
+    return bn2blobdesc_map.at(bn);
+  };
+  clone_op->InferBlobDescs(bn2blobdesc_func, kDataParallel, 3, 10);
   const BlobDesc* in_blob_desc = bn2blobdesc_map.at(clone_op->SoleIbn());
   for (const std::string& obn : clone_op->output_bns()) {
     const BlobDesc* out_blob_desc = bn2blobdesc_map.at(obn);
     ASSERT_TRUE(*in_blob_desc == *out_blob_desc);
   }
-}
-
-template<typename T, bool has_data_id>
-void TestCloneOp() {
-  test::MockJobDesc mock_job_desc;
-  test::InitJobDescSingleton(&mock_job_desc, 8, GetDataType<T>::val);
-
-  int out_num = 3;
-  std::vector<std::vector<int64_t>> in_shapes = {{3, 4}};
-  std::vector<std::string> ibns = {"in"};
-  std::vector<std::string> obns = {"out_0", "out_1", "out_2"};
-  std::vector<std::string> other_bns = {};
-  DoCloneOpTest<T, has_data_id>(out_num, in_shapes, ibns, obns, other_bns);
-
-  out_num = 1;
-  obns = {"out_0"};
-  DoCloneOpTest<T, has_data_id>(out_num, in_shapes, ibns, obns, other_bns);
 }
 
 TEST(CloneOp, infer_blob_desc) {
