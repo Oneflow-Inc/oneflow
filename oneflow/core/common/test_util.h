3cd9638f600b109f7a356223f44011b4a5dc1c63
duduscript <duduscript@outlook.com>
2017-12-12 19:27:30 +0800

    test_util

---
+++ oneflow/core/common/test_util.h
@@ -0,0 +1,11 @@
+#ifndef ONEFLOW_CORE_COMMON_TEST_UTIL_H_
+#define ONEFLOW_CORE_COMMON_TEST_UTIL_H_
+
+#ifndef final
+#define final
+#endif
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#endif  // ONEFLOW_CORE_COMMON_TEST_UTIL_H_
