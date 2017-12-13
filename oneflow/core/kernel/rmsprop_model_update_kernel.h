abe9bbeeeaab3ed845162c18f46a98fa561c2204
willzhang4a58 <willzhang4a58@gmail.com>
2017-12-13 19:57:57 +0800

    refine rmsprop

--- oneflow/core/kernel/rmsprop_model_update_kernel.h
+++ oneflow/core/kernel/rmsprop_model_update_kernel.h
@@ -1,19 +1,21 @@
 #ifndef ONEFLOW_CORE_KERNEL_RMSPROP_MODEL_UPDATE_KERNEL_H_
 #define ONEFLOW_CORE_KERNEL_RMSPROP_MODEL_UPDATE_KERNEL_H_
 
-#include "oneflow/core/kernel/kernel.h"
+#include "oneflow/core/kernel/model_update_kernel.h"
 
 namespace oneflow {
 
 template<DeviceType device_type, typename T>
-class RMSPropMdUpdateKernel final : public KernelIf<device_type> {
+class RMSPropMdUpdateKernel final : public MdUpdateKernel<device_type> {
  public:
   OF_DISALLOW_COPY_AND_MOVE(RMSPropMdUpdateKernel);
   RMSPropMdUpdateKernel() = default;
   ~RMSPropMdUpdateKernel() = default;
 
-  void Forward(const KernelCtx&,
-               std::function<Blob*(const std::string&)>) const override;
+ private:
+  void UpdateModel(
+      DeviceCtx* ctx, const Blob* pre_model_blob, int64_t next_model_vid,
+      std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
 };
 
 template<DeviceType device_type, typename T>
@@ -22,11 +24,12 @@ class RMSPropMdUpdateKernelUtil final {
   // alpha = (1 - decay_rate) / batch_size ^ 2
   // mean_square = alpha * model_diff_acc ^ 2 + decay_rate * mean_square
   // learning_rate = learning_rate / batch_size
-  // model -= learning_rate * model_diff_acc / sqrt(mean_square + epsilon)
-  static void UpdateModel(const KernelCtx& ctx, const int64_t n, const T alpha,
+  // model = pre_model - learning_rate * model_diff_acc / sqrt(mean_square +
+  // epsilon)
+  static void UpdateModel(DeviceCtx*, const int64_t n, const T alpha,
                           const T learning_rate, const T decay_rate,
-                          const T epsilon, T* model, T* mean_square,
-                          const T* model_diff_acc);
+                          const T epsilon, const T* pre_model, T* model,
+                          T* mean_square, const T* model_diff_acc);
 };
 
 }  // namespace oneflow
