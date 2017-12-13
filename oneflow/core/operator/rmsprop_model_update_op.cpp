576b80e7fded4e5fead801770efd0207738cc5b3
willzhang4a58 <willzhang4a58@gmail.com>
2017-12-13 19:30:36 +0800

    model update kernel
    Former-commit-id: e422372af87ba360d9d2b276206034610a3e276d

--- oneflow/core/operator/rmsprop_model_update_op.cpp
+++ oneflow/core/operator/rmsprop_model_update_op.cpp
@@ -14,7 +14,7 @@ const PbMessage& RMSPropModelUpdateOp::GetSpecialConf() const {
 
 void RMSPropModelUpdateOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
-    const ParallelContext* parallel_ctx) {
+    const ParallelContext* parallel_ctx) const {
   const BlobDesc* model_blob_desc = GetBlobDesc4BnInOp("model");
   CHECK_EQ(model_blob_desc->data_type(),
            JobDesc::Singleton()->DefaultDataType());
