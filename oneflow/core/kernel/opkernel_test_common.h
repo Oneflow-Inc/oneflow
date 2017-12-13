f644b3d87b2e95ab2223507ba90d2eb60974bb51
duduscript <duduscript@outlook.com>
2017-12-13 18:50:35 +0800

    add test_util.h to opkernel_test_common

--- oneflow/core/kernel/opkernel_test_common.h
+++ oneflow/core/kernel/opkernel_test_common.h
@@ -1,6 +1,7 @@
 #ifndef ONEFLOW_CORE_KERNEL_OPKERNEL_TEST_COMMON_H_
 #define ONEFLOW_CORE_KERNEL_OPKERNEL_TEST_COMMON_H_
 
+#include "oneflow/core/common/test_util.h"
 #include "oneflow/core/job/resource.pb.h"
 #include "oneflow/core/kernel/kernel_context.h"
 #include "oneflow/core/operator/op_conf.pb.h"
