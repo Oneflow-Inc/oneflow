e422372af87ba360d9d2b276206034610a3e276d
willzhang4a58 <willzhang4a58@gmail.com>
2017-12-13 19:30:36 +0800

    model update kernel

--- oneflow/core/kernel/normal_model_update_kernel.cpp
+++ oneflow/core/kernel/normal_model_update_kernel.cpp
@@ -3,8 +3,8 @@
 namespace oneflow {
 
 template<DeviceType device_type, typename T>
-void NormalMdUpdateKernel<device_type, T>::Forward(
-    const KernelCtx& ctx,
+void NormalMdUpdateKernel<device_type, T>::UpdateModel(
+    DeviceCtx* ctx, const Blob* pre_model_blob, int64_t next_model_vid,
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* model_diff_acc_blob = BnInOp2Blob("model_diff_acc");
   Blob* model_blob = BnInOp2Blob("model");
@@ -12,10 +12,14 @@ void NormalMdUpdateKernel<device_type, T>::Forward(
   float alpha = learning_rate / JobDesc::Singleton()->BatchSize();
   CHECK(std::isfinite(alpha));
 
+  if (pre_model_blob != model_blob) {
+    model_blob->CopyDataContentFrom<device_type>(ctx, pre_model_blob);
+  }
+
   // model = model - alpha * model_diff
-  KernelUtil<device_type, T>::Axpy(
-      ctx.device_ctx, model_blob->shape().elem_cnt(), -alpha,
-      model_diff_acc_blob->dptr<T>(), 1, model_blob->mut_dptr<T>(), 1);
+  KernelUtil<device_type, T>::Axpy(ctx, model_blob->shape().elem_cnt(), -alpha,
+                                   model_diff_acc_blob->dptr<T>(), 1,
+                                   model_blob->mut_dptr<T>(), 1);
 }
 
 ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kNormalMdupdtConf,
