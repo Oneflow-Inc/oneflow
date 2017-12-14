f773d90fbb9f4bc63a4eb7dd5112a5c45ef873a6
ShawnXuan <xiexuanx2@gmail.com>
2017-12-14 17:39:09 +0800

    op and kernel test OK

--- oneflow/core/operator/softmax_op_test.cpp
+++ oneflow/core/operator/softmax_op_test.cpp
@@ -6,25 +6,29 @@ namespace oneflow {
 
 template<typename T, bool has_data_id>
 void TestSoftmaxOp() {
+  // mock JobDesc
   test::MockJobDesc mock_job_desc;
-  test::InitJobDescSingleton(&mock_job_desc, 8, GetDataType<T>::val);
+  test::InitJobDescSingleton(&mock_job_desc);
+  EXPECT_CALL(mock_job_desc, DefaultDataType())
+      .WillRepeatedly(testing::Return(GetDataType<T>::val));
 
   std::vector<std::vector<int64_t>> in_shapes = {{3, 5}};
   std::vector<std::string> ibns = {"in"};
   std::vector<std::string> obns = {"out"};
   std::vector<std::string> other_bns = {"tmp"};
-  // create softmax_op
-  OperatorConf op_conf;
-  op_conf.set_name("softmax_test");
-  op_conf.mutable_softmax_conf()->set_in("softmax/in");
-  op_conf.mutable_softmax_conf()->set_out("softmax/out");
-  auto softmax_op = ConstructOp(op_conf);
 
   // Construct bn2BlobDesc function
   HashMap<std::string, BlobDesc*> bn2blobdesc_map;
   auto bn2blobdesc_func =
       ConstructBn2BlobDescFunc(bn2blobdesc_map, ibns, obns, other_bns,
                                in_shapes, GetDataType<T>::val, has_data_id);
+
+  // create softmax_op
+  OperatorConf op_conf;
+  op_conf.set_name("softmax_test");
+  op_conf.mutable_softmax_conf()->set_in("softmax/in");
+  op_conf.mutable_softmax_conf()->set_out("softmax/out");
+  auto softmax_op = ConstructOp(op_conf);
 
   // infershape
   softmax_op->InferBlobDescs(bn2blobdesc_func, nullptr);
