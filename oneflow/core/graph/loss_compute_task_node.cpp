a7114f1c8c3282a3d66cefc2a26b69c7f1e30c6d
willzhang4a58 <willzhang4a58@gmail.com>
2017-12-23 17:22:01 +0800

    softmax loss without sum
    Former-commit-id: 4bf6e93d11468e2eefc88f5b3f7042b8458ed2d0

--- oneflow/core/graph/loss_compute_task_node.cpp
+++ oneflow/core/graph/loss_compute_task_node.cpp
@@ -22,29 +22,49 @@ void LossCompTaskNode::ConsumeAllRegsts() {
 }
 
 void LossCompTaskNode::BuildExecGphAndRegst() {
+  // regst
   std::shared_ptr<RegstDesc> in_regst = GetConsumedRegst("in");
   std::shared_ptr<RegstDesc> loss_regst = GetProducedRegst("loss");
   std::shared_ptr<RegstDesc> in_diff_regst = GetProducedRegst("in_diff");
   std::shared_ptr<RegstDesc> data_tmp_regst = GetProducedRegst("data_tmp");
-  std::shared_ptr<const Operator> op = chain_node()->SoleOp();
-  CHECK(op->IsLossOp());
-  ExecNode* node = mut_exec_gph().NewNode();
-  node->mut_op() = op;
-  for (const std::string& ibn : op->input_bns()) {
-    node->BindBnInOpAndRegst(ibn, in_regst);
+  // op
+  std::shared_ptr<const Operator> loss_op = chain_node()->SoleOp();
+  CHECK(loss_op->IsLossOp());
+  const std::string& loss_lbn = loss_op->Lbn4BnInOp("loss");
+  OperatorConf sum_op_conf;
+  sum_op_conf.set_name("sum_op_" + NewUniqueId());
+  sum_op_conf.mutable_sum_conf()->set_in(loss_lbn);
+  sum_op_conf.mutable_sum_conf()->set_out("out");
+  sum_op_conf.mutable_sum_conf()->set_axis(0);
+  std::shared_ptr<const Operator> sum_op = ConstructOp(sum_op_conf);
+  // exec gph
+  ExecNode* loss_node = mut_exec_gph().NewNode();
+  loss_node->mut_op() = loss_op;
+  ExecNode* sum_node = mut_exec_gph().NewNode();
+  sum_node->mut_op() = sum_op;
+  Connect(loss_node, mut_exec_gph().NewEdge(), sum_node);
+  // bind
+  for (const std::string& ibn : loss_op->input_bns()) {
+    loss_node->BindBnInOpAndRegst(ibn, in_regst);
   }
-  loss_regst->AddLbn(op->Lbn4BnInOp(op->SoleObn()));
-  node->BindBnInOpAndRegst(op->SoleObn(), loss_regst);
-  for (const std::string& idbn : op->input_diff_bns()) {
-    in_diff_regst->AddLbn(op->Lbn4BnInOp(idbn));
-    node->BindBnInOpAndRegst(idbn, in_diff_regst);
+  for (const std::string& obn : loss_op->output_bns()) {
+    data_tmp_regst->AddLbn(loss_op->Lbn4BnInOp(obn));
+    loss_node->BindBnInOpAndRegst(obn, data_tmp_regst);
   }
+  for (const std::string& idbn : loss_op->input_diff_bns()) {
+    in_diff_regst->AddLbn(loss_op->Lbn4BnInOp(idbn));
+    loss_node->BindBnInOpAndRegst(idbn, in_diff_regst);
+  }
+  for (const std::string& dtbn : loss_op->data_tmp_bns()) {
+    data_tmp_regst->AddLbn(loss_op->Lbn4BnInOp(dtbn));
+    loss_node->BindBnInOpAndRegst(dtbn, data_tmp_regst);
+  }
+  sum_node->BindBnInOpAndRegst(sum_op->SoleIbn(), data_tmp_regst);
+  loss_regst->AddLbn(sum_op->Lbn4BnInOp(sum_op->SoleObn()));
+  sum_node->BindBnInOpAndRegst(sum_op->SoleObn(), loss_regst);
+  loss_op->InferBlobDescs(loss_node->GetBlobDesc4BnInOpFunc(), parallel_ctx());
+  sum_op->InferBlobDescs(sum_node->GetBlobDesc4BnInOpFunc(), parallel_ctx());
   in_diff_regst->CopyBlobDescWithoutAddLbn(in_regst.get());
-  for (const std::string& dtbn : op->data_tmp_bns()) {
-    data_tmp_regst->AddLbn(op->Lbn4BnInOp(dtbn));
-    node->BindBnInOpAndRegst(dtbn, data_tmp_regst);
-  }
-  op->InferBlobDescs(node->GetBlobDesc4BnInOpFunc(), parallel_ctx());
 }
 
 }  // namespace oneflow
