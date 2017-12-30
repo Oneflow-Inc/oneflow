91b31f5a548adb08ca1af320f0f06691221b2ce1
Wind5 <1163643073@qq.com>
2017-12-30 17:23:38 +0800

    rnn_boxing

---
+++ oneflow/core/actor/rnn_boxing_actor.h
@@ -0,0 +1,31 @@
+#ifndef ONEFLOW_CORE_ACTOR_BOXING_ACTOR_H_
+#define ONEFLOW_CORE_ACTOR_BOXING_ACTOR_H_
+
+#include "oneflow/core/actor/actor.h"
+
+namespace oneflow {
+
+class BoxingActor final : public Actor {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BoxingActor);
+  BoxingActor() = default;
+  ~BoxingActor() = default;
+
+  void VirtualActorInit(const TaskProto&) override;
+
+ private:
+  int HandlerNormal(const ActorMsg&) override;
+
+  void Act() override;
+  bool IsReadReady() override;
+  bool IsReadAlwaysUnReadyFromNow() override;
+  void AsyncReturnAllReadableRegst() override;
+
+  bool is_eord_;
+  HashMap<int64_t, std::queue<Regst*>> readable_regst_;
+  int64_t readable_regst_cnt_;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_ACTOR_BOXING_ACTOR_H_
