0614ef6bf3089ee35288a3e967b02c51e8fec8f4
cheng cheng <472491134@qq.com>
2018-01-17 13:36:10 +0800

    abstract new func for succ/pred chain node on edge

--- oneflow/core/graph/compute_task_node.cpp
+++ oneflow/core/graph/compute_task_node.cpp
@@ -3,31 +3,34 @@
 
 namespace oneflow {
 
+namespace {
+
+const ChainNode* ChainNodeOnEdge(
+    TaskEdge* edge, TaskNode* (TaskEdge::*GetNode)() const,
+    const std::unordered_set<TaskEdge*>& (TaskNode::*GetEdges)() const) {
+  CompTaskNode* target_node = nullptr;
+  do {
+    TaskNode* tmp_node = (edge->*GetNode)();
+    edge = *((tmp_node->*GetEdges)().begin());
+    target_node = dynamic_cast<CompTaskNode*>(tmp_node);
+  } while (!target_node && edge);
+  if (target_node) { return target_node->chain_node(); }
+  return nullptr;
+}
+
+}  // namespace
+
 void CompTaskNode::ToProto(TaskProto* task_proto) {
   TaskNode::ToProto(task_proto);
   *(task_proto->mutable_parallel_ctx()) = parallel_ctx_;
 }
 
 const ChainNode* CompTaskNode::SuccChainNodeOnEdge(TaskEdge* edge) {
-  CompTaskNode* succ_comp_node = nullptr;
-  do {
-    TaskNode* dst_node = edge->dst_node();
-    edge = *(dst_node->out_edges().begin());
-    succ_comp_node = dynamic_cast<CompTaskNode*>(dst_node);
-  } while (!succ_comp_node && edge);
-  if (succ_comp_node) { return succ_comp_node->chain_node(); }
-  return nullptr;
+  return ChainNodeOnEdge(edge, &TaskEdge::dst_node, &TaskNode::out_edges);
 }
 
 const ChainNode* CompTaskNode::PredChainNodeOnEdge(TaskEdge* edge) {
-  CompTaskNode* pred_comp_node = nullptr;
-  do {
-    TaskNode* src_node = edge->src_node();
-    edge = *(src_node->in_edges().begin());
-    pred_comp_node = dynamic_cast<CompTaskNode*>(src_node);
-  } while (!pred_comp_node && edge);
-  if (pred_comp_node) { return pred_comp_node->chain_node(); }
-  return nullptr;
+  return ChainNodeOnEdge(edge, &TaskEdge::src_node, &TaskNode::in_edges);
 }
 
 void SortByParallelId(std::vector<CompTaskNode*>* node_vec) {
