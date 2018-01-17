44834604bcd34fce2a807044c8ab7307cafd447d
zyeric <zyeric@github.com>
2018-01-17 20:06:04 +0800

    remove useless UNEXPECTED_RUN

--- oneflow/core/graph/backward_compute_task_node.h
+++ oneflow/core/graph/backward_compute_task_node.h
@@ -18,7 +18,7 @@ class BackwardCompTaskNode : public CompTaskNode {
 
  protected:
   virtual void VirtualBuildExecGphAndBindOutDiffRegst() { UNEXPECTED_RUN(); }
-  virtual void VirtualBuildActivationDiffRegst() { UNEXPECTED_RUN(); }
+  virtual void VirtualBuildActivationDiffRegst() {}
   virtual void VirtualBuildInDiffRegst() { UNEXPECTED_RUN(); }
   virtual void VirtualProduceInDiffAndBindEdge(TaskEdge* edge) {
     UNEXPECTED_RUN();
@@ -26,12 +26,12 @@ class BackwardCompTaskNode : public CompTaskNode {
   virtual void VirtualProduceRegstOnRecurrentEdge(TaskEdge* edge) {
     UNEXPECTED_RUN();
   }
-  virtual void VirtualProduceActivationDiff() { UNEXPECTED_RUN(); }
-  virtual void VirtualConsumeActivation(TaskEdge* edge) { UNEXPECTED_RUN(); }
+  virtual void VirtualProduceActivationDiff() {}
+  virtual void VirtualConsumeActivation(TaskEdge* edge) {}
   virtual void VirtualConsumeDiffRegst(TaskEdge* edge) { UNEXPECTED_RUN(); }
   virtual void VirtualConsumeInRegst() { UNEXPECTED_RUN(); };
-  virtual void VirtualInferBlobDescInActivationDiff() { UNEXPECTED_RUN(); }
-  virtual void VirtualInferBlobDescInHiddenDiff() { UNEXPECTED_RUN(); }
+  virtual void VirtualInferBlobDescInActivationDiff() {}
+  virtual void VirtualInferBlobDescInHiddenDiff() {}
 
  private:
   void BindModelDiffRegst();
