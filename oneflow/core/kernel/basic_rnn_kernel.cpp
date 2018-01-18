a87ea3f92b002129cbf14f5de68b39fe1c43842c
duduscript <duduscript@outlook.com>
2018-01-19 00:49:12 +0800

    Fix bug and add comment

--- oneflow/core/kernel/basic_rnn_kernel.cpp
+++ oneflow/core/kernel/basic_rnn_kernel.cpp
@@ -6,30 +6,28 @@ template<DeviceType device_type, typename T>
 void BasicRnnKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx,
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* in_blob = BnInOp2Blob("in");
-  const Blob* in_ip_op_weight_blob = BnInOp2Blob("in_ip_op_weight");
   Blob* in_ip_op_out_blob = BnInOp2Blob("in_ip_op_out");
   const Blob* ht_1_blob = this->GetHiddenBlob(BnInOp2Blob);
-  const Blob* hidden_ip_op_weight_blob = BnInOp2Blob("hidden_ip_op_weight");
   Blob* hidden_ip_op_out_blob = BnInOp2Blob("hidden_ip_op_out");
   Blob* plus_op_out_blob = BnInOp2Blob("plus_op_out");
-  Blob* f_op_out_blob = BnInOp2Blob("f_op_out");
   Blob* ht_blob = BnInOp2Blob("ht");
-  Blob* out_blob = BnInOp2Blob("out");
 
   // in_ip_op_out = in * in_ip_op_weight
   KernelUtil<device_type, T>::BlasMatrixMatrix(
       ctx, CblasNoTrans, CblasTrans, static_cast<T>(1), static_cast<T>(0),
-      in_blob, in_ip_op_weight_blob, in_ip_op_out_blob);
+      BnInOp2Blob("in"), BnInOp2Blob("in_ip_op_weight"), in_ip_op_out_blob);
+
   // hidden_ip_op_out = ht_1 * hidden_ip_op_weight
   KernelUtil<device_type, T>::BlasMatrixMatrix(
       ctx, CblasNoTrans, CblasNoTrans, static_cast<T>(1), static_cast<T>(0),
-      ht_1_blob, hidden_ip_op_weight_blob, hidden_ip_op_out_blob);
+      ht_1_blob, BnInOp2Blob("hidden_ip_op_weight"), hidden_ip_op_out_blob);
+
   // plus_op_out = in_ip_op_out + hidden_ip_op_out
   BasicRnnKernelUtil<device_type, T>::Add(
       ctx.device_ctx, plus_op_out_blob->shape().elem_cnt(),
       in_ip_op_out_blob->dptr<T>(), hidden_ip_op_out_blob->dptr<T>(),
       plus_op_out_blob->mut_dptr<T>());
+
   if (this->op_conf().recurrent_conf().has_bias_term()) {
     const Blob* bias_blob = BnInOp2Blob("bias");
     const Blob* bias_mul_blob = BnInOp2Blob("bias_multiplier");
@@ -38,68 +36,74 @@ void BasicRnnKernel<device_type, T>::ForwardDataContent(
         ctx, CblasNoTrans, CblasNoTrans, static_cast<T>(1), static_cast<T>(1),
         bias_mul_blob, bias_blob, plus_op_out_blob);
   }
-  // f_op_out = tanh(plus_op_out)
+  // ht = tanh(plus_op_out)
   BasicRnnKernelUtil<device_type, T>::Tanh(
-      ctx.device_ctx, f_op_out_blob->shape().elem_cnt(),
-      plus_op_out_blob->dptr<T>(), f_op_out_blob->mut_dptr<T>());
-  // ht = f_op_out
-  ht_blob->CopyDataContentFrom<device_type>(ctx.device_ctx, f_op_out_blob);
-  // out = f_op_out
-  out_blob->CopyDataContentFrom<device_type>(ctx.device_ctx, f_op_out_blob);
+      ctx.device_ctx, ht_blob->shape().elem_cnt(), plus_op_out_blob->dptr<T>(),
+      ht_blob->mut_dptr<T>());
+  // rec_ht = ht
+  BnInOp2Blob("rec_ht")->CopyDataContentFrom<device_type>(ctx.device_ctx,
+                                                          ht_blob);
 }
 
 template<DeviceType device_type, typename T>
 void BasicRnnKernel<device_type, T>::ForwardDataId(
     const KernelCtx& ctx,
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  BnInOp2Blob("ht")->CopyDataIdFrom<device_type>(ctx.device_ctx,
-                                                 BnInOp2Blob("in"));
+  BnInOp2Blob("rec_ht")->CopyDataIdFrom<device_type>(ctx.device_ctx,
+                                                     BnInOp2Blob("in"));
 }
 
 template<DeviceType device_type, typename T>
 void BasicRnnKernel<device_type, T>::BackwardDataContent(
     const KernelCtx& ctx,
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  Blob* in_diff_blob = BnInOp2Blob("in_diff");
   Blob* ht_diff_blob = BnInOp2Blob("ht_diff");
   const Blob* ht_blob = BnInOp2Blob("ht");
   const Blob* ht_1_blob = this->GetHiddenBlob(BnInOp2Blob);
-  const Blob* in_blob = BnInOp2Blob("in");
   Blob* in_ip_op_weight_diff_blob = BnInOp2Blob("in_ip_op_weight_diff");
-  Blob* hidden_ip_op_weight_diff_blob = BnInOp2Blob("hidden_ip_op_weight_diff");
-  Blob* plus_op_out_blob = BnInOp2Blob("plus_op_out");
+  // reuse memory
+  Blob* diff_sum_blob = BnInOp2Blob("plus_op_out");
+  Blob* plus_op_out_diff_blob = BnInOp2Blob("plus_op_out");
 
-  Blob* plus_op_out_diff_blob = plus_op_out_blob;
-  if (this->op_conf().recurrent_conf().has_bias_term()) {
-    plus_op_out_diff_blob = BnInOp2Blob("bias_diff_blob");
-  }
-  Blob* diff_sum_blob = BnInOp2Blob("f_op_out");
-  const Blob* out_diff_blob = BnInOp2Blob("out_diff");
-  // diff_sum = ht_diff + out_diff
+  // diff_sum = ht_diff + rec_ht_diff
   BasicRnnKernelUtil<device_type, T>::Add(
       ctx.device_ctx, ht_diff_blob->shape().elem_cnt(), ht_diff_blob->dptr<T>(),
-      out_diff_blob->dptr<T>(), diff_sum_blob->mut_dptr<T>());
+      BnInOp2Blob("rec_ht_diff")->dptr<T>(), diff_sum_blob->mut_dptr<T>());
+
   // plus_op_out_diff = (1 - ht^2) .* ht_diff
   BasicRnnKernelUtil<device_type, T>::ComputePlusOutDiff(
       ctx.device_ctx, ht_blob->shape().elem_cnt(), ht_blob->dptr<T>(),
       diff_sum_blob->dptr<T>(), plus_op_out_diff_blob->mut_dptr<T>());
+
   // hidden_ip_op_weight_diff = plus_op_out_diff * ht_1
   KernelUtil<device_type, T>::BlasMatrixMatrix(
       ctx, CblasNoTrans, CblasTrans, static_cast<T>(1), static_cast<T>(0),
-      plus_op_out_diff_blob, ht_1_blob, hidden_ip_op_weight_diff_blob);
+      plus_op_out_diff_blob, ht_1_blob,
+      BnInOp2Blob("hidden_ip_op_weight_diff"));
+
   // in_ip_op_weight_diff = plus_op_out_diff * in
   KernelUtil<device_type, T>::BlasMatrixMatrix(
       ctx, CblasNoTrans, CblasTrans, static_cast<T>(1), static_cast<T>(0),
-      plus_op_out_diff_blob, in_blob, in_ip_op_weight_diff_blob);
+      plus_op_out_diff_blob, BnInOp2Blob("in"), in_ip_op_weight_diff_blob);
+
   // in_diff = plus_op_out_diff * in_ip_op_weight_diff
   KernelUtil<device_type, T>::BlasMatrixMatrix(
       ctx, CblasNoTrans, CblasNoTrans, static_cast<T>(1), static_cast<T>(0),
-      plus_op_out_diff_blob, in_ip_op_weight_diff_blob, in_diff_blob);
-  if (this->Ish0Model() && out_diff_blob->col_id() == 0) {
-    Blob* h0_diff_blob = BnInOp2Blob("h0_diff");
+      plus_op_out_diff_blob, in_ip_op_weight_diff_blob, BnInOp2Blob("in_diff"));
+
+  if (this->op_conf().recurrent_conf().has_bias_term()) {
+    // bias_diff = plus_op_out_diff * bias_multiplier
+    KernelUtil<device_type, T>::BlasMatrixMatrix(
+        ctx, CblasTrans, CblasNoTrans, static_cast<T>(1), static_cast<T>(0),
+        BnInOp2Blob("bias_multiplier"), plus_op_out_diff_blob,
+        BnInOp2Blob("bias_diff"));
+  }
+  if (this->Ish0Model() && BnInOp2Blob("rec_ht_diff")->col_id() == 0) {
+    // ho_diff = plus_op_out_diff * hidden_ip_op_weight
     KernelUtil<device_type, T>::BlasMatrixMatrix(
         ctx, CblasNoTrans, CblasNoTrans, static_cast<T>(0), static_cast<T>(0),
-        plus_op_out_diff_blob, hidden_ip_op_weight_diff_blob, h0_diff_blob);
+        plus_op_out_diff_blob, BnInOp2Blob("hidden_ip_op_weight"),
+        BnInOp2Blob("h0_diff"));
   }
 }
 
