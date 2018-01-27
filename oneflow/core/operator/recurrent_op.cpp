9f91bf78cd0fa88e9e45ad918d640d2a06494f9c
duduscript <duduscript@outlook.com>
2018-01-27 15:07:09 +0800

    clang format

--- oneflow/core/operator/recurrent_op.cpp
+++ oneflow/core/operator/recurrent_op.cpp
@@ -11,10 +11,9 @@ void InferBasicRnnCellBlobDesc(
   const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
   int64_t embedding_size = in_blob_desc->shape().Count(1);
   int64_t data_num = in_blob_desc->shape().At(0);
-  *GetBlobDesc4BnInOp("plus_op_out") = 
-      BlobDesc(Shape({data_num, hidden_size}),
-               JobDesc::Singleton()->DefaultDataType(), false, true,
-               in_blob_desc->max_col_num());
+  *GetBlobDesc4BnInOp("plus_op_out") = BlobDesc(
+      Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
+      false, true, in_blob_desc->max_col_num());
 
   *GetBlobDesc4BnInOp("i2h_weight") =
       BlobDesc(Shape({hidden_size, embedding_size}));
