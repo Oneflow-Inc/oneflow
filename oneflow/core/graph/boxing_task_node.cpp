131947ed3d4600e8b703d9d853dd4ead891d5327
Yi Zhu <619828575@qq.com>
2018-01-29 17:19:55 +0800

    Fix shared model bugs (#539)
    * fix compile bugs
    
    * fix bug
    
    * rollback changes to op
    
    * rollback correctly
    
    * add interface to fix bug
    
    * fix model_updt_actor bug
    
    * fix boxing bug
    
    * refine code
    
    * refine code
    
    * fix bug of init model
    
    * fix bug
    
    * fix names
    
    * replace HasSameLbns with HasSameBlobDescs
    
    * fix bug

--- oneflow/core/graph/boxing_task_node.cpp
+++ oneflow/core/graph/boxing_task_node.cpp
@@ -189,7 +189,10 @@ void BoxingTaskNode::BuildWithChainPair(
       auto regst = sorted_out_edges[i].edge->GetSoleRegst();
       const std::string& obn = node->op()->output_bns().at(i);
       if (lbn == kPackedBlobName) {
-        regst->CopyBlobDescFrom(sorted_in_edges[0].edge->GetSoleRegst().get());
+        RegstDesc* in_regst = sorted_in_edges[0].edge->GetSoleRegst().get();
+        if (!regst->HasSameBlobDescs(in_regst)) {
+          regst->CopyBlobDescFrom(in_regst);
+        }
       } else {
         regst->AddLbn(lbn);
       }
