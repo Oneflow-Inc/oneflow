49b7671a9888f5a291122cd2ef1d42a13301e691
cheng cheng <472491134@qq.com>
2018-02-02 19:54:47 +0800

    fix bug

--- oneflow/core/operator/reshape_op.cpp
+++ oneflow/core/operator/reshape_op.cpp
@@ -17,11 +17,12 @@ void ReshapeOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
-  *out_blob_desc = *GetBlobDesc4BnInOp("in");
+  BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
+  *out_blob_desc = *in_blob_desc;
 
   const ReshapeOpConf& conf = op_conf().reshape_conf();
   std::vector<int64_t> dim_vec(1 + conf.shape().dim_size());
-  dim_vec[0] = JobDesc::Singleton()->SinglePieceSize();
+  dim_vec[0] = in_blob_desc->shape().At(0);
   FOR_RANGE(size_t, i, 1, dim_vec.size()) {
     dim_vec[i] = conf.shape().dim(i - 1);
   }
