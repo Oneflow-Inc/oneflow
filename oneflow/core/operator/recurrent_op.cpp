38742944f1f2e68606c3422eb6919f731ae0279d
duduscript <duduscript@outlook.com>
2018-02-03 12:11:41 +0800

    BasicRnnOp refactor (#549)
    * add l1 l2 regularization
    
    * fix some problem
    
    * Fix bug
    
    * fix op API
    
    * Fix a bug in op
    
    * add l1l2
    
    * remove regularization_diff_blob
    
    * remove sign in kernel_util
    
    * fix naming problem and add l1l2 function
    
    * simplify code
    
    * remove else
    
    * update modelupdate API
    
    * Fix bug
    
    * ~
    
    * fix bug
    
    * add basic_rnn_op
    
    * recurrent op
    
    * UNEXPECT_RUN
    
    * change config
    
    * remove recurrentopconf
    
    
    Former-commit-id: 3a83164a355f5a5ba21cd1d29d01d8876ebe32de

--- oneflow/core/operator/recurrent_op.cpp
+++ oneflow/core/operator/recurrent_op.cpp
@@ -3,72 +3,36 @@
 
 namespace oneflow {
 
-namespace {
-
-void InferBasicRnnCellBlobDesc(
-    std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
-    int32_t hidden_size) {
-  const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
-  int64_t embedding_size = in_blob_desc->shape().Count(1);
-  int64_t data_num = in_blob_desc->shape().At(0);
-  *GetBlobDesc4BnInOp("plus_op_out") =
-      BlobDesc(Shape({embedding_size, hidden_size}),
-               JobDesc::Singleton()->DefaultDataType(), false, true,
-               in_blob_desc->max_col_num());
-  *GetBlobDesc4BnInOp("i2h_weight") =
-      BlobDesc(Shape({hidden_size, embedding_size}));
-  *GetBlobDesc4BnInOp("h2h_weight") =
-      BlobDesc(Shape({hidden_size, hidden_size}));
-  *GetBlobDesc4BnInOp("bias") = BlobDesc(Shape({1, hidden_size}));
-  *GetBlobDesc4BnInOp("bias_multiplier") = BlobDesc(Shape({data_num, 1}));
-}
-
-}  //  namespace
-
 void RecurrentOp::InitFromOpConf() {
-  CHECK(op_conf().has_recurrent_conf());
-  const RecurrentOpConf& conf = op_conf().recurrent_conf();
   EnrollInputBn("in");
   EnrollInputBn("rec_in");
-  if (!conf.init_hidden().empty()) {
-    CHECK(!conf.has_init_hidden_initializer());
+  if (!GetStringFromSpecialConf("init_hidden").empty()) {
+    CHECK(!GetBoolFromSpecialConf("has_init_hidden_initializer"));
     EnrollInputBn("h0");
   } else {
     EnrollModelBn("h0");
   }
   EnrollOutputBn("out");
   EnrollOutputBn("rec_out");
-
-  if (conf.rnn_type_case() == RecurrentOpConf::kBasicRnnCell) {
-    EnrollDataTmpBn("plus_op_out");
-    EnrollModelBn("i2h_weight");
-    EnrollModelBn("h2h_weight");
-    EnrollModelBn("bias");
-    EnrollModelTmpBn("bias_multiplier");
-  } else if (conf.rnn_type_case() == RecurrentOpConf::kBasicLstmCell) {
-    TODO();
-  } else {
-    UNEXPECTED_RUN();
-  }
+  VirtualInitFromOpConf();
 }
 
-const PbMessage& RecurrentOp::GetSpecialConf() const {
-  return op_conf().recurrent_conf();
+int32_t RecurrentOp::MaxModelSplitNum() const {
+  return GetInt32FromSpecialConf("hidden_size");
 }
 
 void RecurrentOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
-  const RecurrentOpConf& conf = op_conf().recurrent_conf();
   const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
   DataType data_type = JobDesc::Singleton()->DefaultDataType();
   CHECK_EQ(in_blob_desc->data_type(), data_type);
   CHECK_EQ(in_blob_desc->shape().NumAxes(), 2);
   CHECK_EQ(in_blob_desc->has_col_num_field(), true);
   int64_t data_num = in_blob_desc->shape().At(0);
-  int32_t hidden_size = conf.hidden_size();
+  int32_t hidden_size = GetInt32FromSpecialConf("hidden_size");
   Shape h0_shape = Shape({data_num, hidden_size});
-  if (!conf.init_hidden().empty()) {
+  if (!GetStringFromSpecialConf("init_hidden").empty()) {
     const BlobDesc* h0_blob_desc = GetBlobDesc4BnInOp("h0");
     CHECK_EQ(h0_blob_desc->data_type(), data_type);
     CHECK_EQ(h0_blob_desc->shape(), h0_shape);
@@ -89,24 +53,16 @@ void RecurrentOp::InferBlobDescs(
   // recurrent_out
   *GetBlobDesc4BnInOp("rec_out") = out_blob_desc;
 
-  if (op_conf().recurrent_conf().rnn_type_case()
-      == RecurrentOpConf::kBasicRnnCell) {
-    InferBasicRnnCellBlobDesc(GetBlobDesc4BnInOp, hidden_size);
-  } else if (op_conf().recurrent_conf().rnn_type_case()
-             == RecurrentOpConf::kBasicLstmCell) {
-    TODO();
-  } else {
-    UNEXPECTED_RUN();
-  }
+  VirtualInferBlobDescs(GetBlobDesc4BnInOp, parallel_ctx);
 }
 
 std::string RecurrentOp::ibn2lbn(const std::string& input_bn) const {
   if (input_bn == "rec_in") {
     return obn2lbn("rec_out");
   } else if (input_bn == "h0") {
-    return op_conf().recurrent_conf().init_hidden();
+    return GetStringFromSpecialConf("init_hidden");
   } else if (input_bn == "in") {
-    return op_conf().recurrent_conf().in();
+    return GetStringFromSpecialConf("in");
   } else {
     UNEXPECTED_RUN();
     return "";
@@ -115,15 +71,13 @@ std::string RecurrentOp::ibn2lbn(const std::string& input_bn) const {
 
 std::string RecurrentOp::obn2lbn(const std::string& output_bn) const {
   if (output_bn == "out") {
-    return op_name() + "/" + op_conf().recurrent_conf().out();
+    return op_name() + "/" + GetStringFromSpecialConf("out");
   } else if (output_bn == "rec_out") {
-    return op_name() + "/rec_" + op_conf().recurrent_conf().out();
+    return op_name() + "/rec_" + GetStringFromSpecialConf("out");
   } else {
     UNEXPECTED_RUN();
     return "";
   }
 }
-
-REGISTER_OP(OperatorConf::kRecurrentConf, RecurrentOp);
 
 }  // namespace oneflow
