615c43b79be56edb854cfb8e90d8934cb3e8b9d1
duduscript <duduscript@outlook.com>
2018-02-06 20:07:13 +0800

    data_type

--- oneflow/core/common/data_type.h
+++ oneflow/core/common/data_type.h
@@ -11,31 +11,19 @@ namespace oneflow {
   OF_PP_MAKE_TUPLE_SEQ(float, DataType::kFloat) \
   OF_PP_MAKE_TUPLE_SEQ(double, DataType::kDouble)
 
-#define SIGNED_INT_DATA_TYPE_SEQ                  \
-  OF_PP_MAKE_TUPLE_SEQ(int8_t, DataType::kInt8)   \
-  OF_PP_MAKE_TUPLE_SEQ(int16_t, DataType::kInt16) \
-  OF_PP_MAKE_TUPLE_SEQ(int32_t, DataType::kInt32) \
-  OF_PP_MAKE_TUPLE_SEQ(int64_t, DataType::kInt64)
-
-#define UNSIGNED_INT_DATA_TYPE_SEQ                  \
-  OF_PP_MAKE_TUPLE_SEQ(uint8_t, DataType::kUInt8)   \
-  OF_PP_MAKE_TUPLE_SEQ(uint16_t, DataType::kUInt16) \
-  OF_PP_MAKE_TUPLE_SEQ(uint32_t, DataType::kUInt32) \
-  OF_PP_MAKE_TUPLE_SEQ(uint64_t, DataType::kUInt64)
+#define SIGNED_INT_DATA_TYPE_SEQ                \
+  OF_PP_MAKE_TUPLE_SEQ(int8_t, DataType::kInt8) \
+  OF_PP_MAKE_TUPLE_SEQ(int32_t, DataType::kInt32)
 
 #define CHAR_DATA_TYPE_SEQ OF_PP_MAKE_TUPLE_SEQ(char, DataType::kChar)
 
-#define INT_DATA_TYPE_SEQ  \
-  SIGNED_INT_DATA_TYPE_SEQ \
-  UNSIGNED_INT_DATA_TYPE_SEQ
+#define INT_DATA_TYPE_SEQ SIGNED_INT_DATA_TYPE_SEQ
 
 #define ARITHMETIC_DATA_TYPE_SEQ \
   FLOATING_DATA_TYPE_SEQ         \
   INT_DATA_TYPE_SEQ
 
-#define ALL_DATA_TYPE_SEQ  \
-  ARITHMETIC_DATA_TYPE_SEQ \
-  CHAR_DATA_TYPE_SEQ
+#define ALL_DATA_TYPE_SEQ ARITHMETIC_DATA_TYPE_SEQ CHAR_DATA_TYPE_SEQ
 
 bool IsIntegral(DataType data_type);
 bool IsFloatingPoint(DataType data_type);
