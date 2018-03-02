66a8944efa932d424a6b9dbfdef7a3278f4b1927
jackalcooper <jackalcooper@gmail.com>
2018-03-02 15:14:07 +0800

    Sum implementation with Axpy

--- oneflow/core/kernel/eltwise_kernel.cpp
+++ oneflow/core/kernel/eltwise_kernel.cpp
@@ -8,29 +8,23 @@ void EltwiseKernel<device_type, T>::ForwardDataContent(
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const EltwiseOpConf& eltwise_conf = this->op_conf().eltwise_conf();
   Blob* out_blob = BnInOp2Blob("out");
-  Memset<device_type>(ctx.device_ctx, out_blob->mut_dptr(), 0, out_blob->ByteSizeOfDataContentField());
-  Blob* tmp_blob = BnInOp2Blob("tmp");
-  Memset<device_type>(ctx.device_ctx, tmp_blob->mut_dptr(), 0, out_blob->ByteSizeOfDataContentField());
+  Memset<device_type>(ctx.device_ctx, out_blob->mut_dptr(), 0,
+                      out_blob->ByteSizeOfDataContentField());
   switch (eltwise_conf.operation()) {
     case EltwiseOpConf_EltwiseOp_SUM:
       // out = sum(in1, in2, in3....)
       for (int i = 0; i < eltwise_conf.in_size(); ++i) {
         std::string ibn = "in_" + std::to_string(i);
         const Blob* in_blob = BnInOp2Blob(ibn);
-        KernelUtil<device_type, T>::Sum(
-            ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
-            out_blob->mut_dptr<T>(), tmp_blob->mut_dptr<T>(),
-            tmp_blob->ByteSizeOfDataContentField());
+        KernelUtil<device_type, T>::Axpy(
+            ctx.device_ctx, in_blob->shape().elem_cnt(), 1.0f,
+            in_blob->dptr<T>(), 1, out_blob->mut_dptr<T>(), 1);
       }
     case EltwiseOpConf_EltwiseOp_MAX:
       // out = max(in1, in2, in3....)
       for (int i = 0; i < eltwise_conf.in_size(); ++i) {
         std::string ibn = "in_" + std::to_string(i);
         const Blob* in_blob = BnInOp2Blob(ibn);
-        KernelUtil<device_type, T>::Max(
-            ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
-            out_blob->mut_dptr<T>(), tmp_blob->mut_dptr<T>(),
-            tmp_blob->ByteSizeOfDataContentField());
       };
     default: break;
   }
