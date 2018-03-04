d8c672534e21b10352c96dbb96f9d1e8c85ca049
syshangguan <shgshy@gmail.com>
2018-03-04 22:34:33 +0800

    conv_kernel

--- oneflow/core/kernel/conv_kernel.cu
+++ oneflow/core/kernel/conv_kernel.cu
@@ -13,10 +13,24 @@ void CudnnConvKernel<T>::VirtualKernelInit(const ParallelContext*) {
   Shape out_shape(this->kernel_conf().conv_conf().out());
   Shape weight_shape(this->kernel_conf().conv_conf().weight());
 
-  // TODO()
-  in_desc_ = new CudnnTensorDesc(CudnnDataType<T>::val, in_shape);
-  out_desc_ = new CudnnTensorDesc(CudnnDataType<T>::val, out_shape);
-  filter_desc_ =
+  std::vector<int> stride_of_in_tensor(kernel_dim_size, 1);
+  std::vector<int> stride_of_out_tensor(kernel_dim_size, 1);
+  for (int32_t i = kernel_dim_size + 2 - 1; i > 0; --i) {
+    for (int32_t j = kernel_dim_size + 2 - 2; j >= 0; --j) {
+      stride_of_in_tensor[j] *= in_shape.At(i);
+      stride_of_out_tensor[j] *= out_shape.At(i);
+    }
+  }
+  std::vector<int> in_dim(in_shape.dim_vec().begin(),
+                              in_shape.dim_vec().end());
+  std::vector<int> out_dim(out_shape.dim_vec().begin(),
+                               out_shape.dim_vec().end());
+
+  this->in_desc_ =
+      new CudnnTensorDesc(CudnnDataType<T>::val, in_dim, stride_of_in_tensor);
+  this->out_desc_ =
+      new CudnnTensorDesc(CudnnDataType<T>::val, out_dim, stride_of_out_tensor);
+  this->filter_desc_ =
       new CudnnFilterDesc(CudnnDataType<T>::val, weight_shape,
                           this->GetStringFromCustomizedOpConf("data_format"));
   conv_desc_ = new CudnnConvDesc(
@@ -29,8 +43,8 @@ void CudnnConvKernel<T>::VirtualKernelInit(const ParallelContext*) {
       this->GetStringFromCustomizedOpConf("padding"));
 
   if (this->GetBoolFromCustomizedOpConf("use_bias")) {
-    Shape bias_shape(this->kernel_conf().conv_conf().bias());
-    this->bias_desc_ = new CudnnTensorDesc(CudnnDataType<T>::val, bias_shape);
+    int32_t filters = this->GetInt32FromCustomizedOpConf("filters");
+    this->bias_desc_ = new CudnnTensorDesc(CudnnDataType<T>::val, 1, &filters);
   }
 }
 
@@ -45,20 +59,20 @@ void CudnnConvKernel<T>::ForwardDataContent(
 
   cudnnConvolutionFwdAlgo_t cudnn_fwd_algo =
       static_cast<cudnnConvolutionFwdAlgo_t>(
-          kernel_conf().conv_conf().cudnn_fwd_algo());
+          this->kernel_conf().conv_conf().cudnn_fwd_algo());
   CudaCheck(cudnnConvolutionForward(
-      ctx.device_ctx->cudnn_handle(), CudnnDataType<T>::one, in_desc_->Get(),
-      in_blob->dptr<T>(), filter_desc_->Get(), weight_blob->dptr<T>(),
-      conv_desc_->Get(), cudnn_fwd_algo, cudnn_workspace->mut_dptr<T>(),
-      cudnn_workspace->shape().At(0), CudnnDataType<T>::zero, out_desc_->Get(),
-      out_blob->mut_dptr<T>()));
+      ctx.device_ctx->cudnn_handle(), CudnnDataType<T>::one,
+      this->in_desc_->Get(), in_blob->dptr<T>(), this->filter_desc_->Get(),
+      weight_blob->dptr<T>(), this->conv_desc_->Get(), cudnn_fwd_algo,
+      cudnn_workspace->mut_dptr<T>(), cudnn_workspace->shape().At(0),
+      CudnnDataType<T>::zero, this->out_desc_->Get(), out_blob->mut_dptr<T>()));
 
   if (this->GetBoolFromCustomizedOpConf<T>("use_bias")) {
     const Blob* bias_blob = BnInOp2Blob("bias");
     CudaCheck(cudnnAddTensor(ctx.device_ctx->cudnn_handle(),
-                             CudnnDataType<T>::one, bias_desc_->Get(),
+                             CudnnDataType<T>::one, this->bias_desc_->Get(),
                              bias_blob->dptr<T>(), CudnnDataType<T>::one,
-                             out_desc_->Get(), out_blob->mut_dptr<T>()));
+                             this->out_desc_->Get(), out_blob->mut_dptr<T>()));
   }
 }
 
@@ -75,9 +89,9 @@ void CudnnConvKernel<T>::BackwardDataContent(
                              bias_diff_blob->ByteSizeOfDataContentField());
 
     CudaCheck(cudnnConvolutionBackwardBias(
-        ctx.device_ctx->cudnn_handle(), CudnnDataType<T>::one, out_desc_->Get(),
-        out_diff_blob->dptr<T>(), CudnnDataType<T>::one, bias_desc_->Get(),
-        bias_diff_blob->mut_dptr<T>()));
+        ctx.device_ctx->cudnn_handle(), CudnnDataType<T>::one,
+        this->out_desc_->Get(), out_diff_blob->dptr<T>(), CudnnDataType<T>::one,
+        this->bias_desc_->Get(), bias_diff_blob->mut_dptr<T>()));
   }
 
   // compute weight_diff
@@ -92,11 +106,12 @@ void CudnnConvKernel<T>::BackwardDataContent(
           kernel_conf().conv_conf().cudnn_bwd_filter_algo());
 
   CudaCheck(cudnnConvolutionBackwardFilter(
-      ctx.device_ctx->cudnn_handle(), CudnnDataType<T>::one, in_desc_->Get(),
-      in_blob->dptr<T>(), out_desc_->Get(), out_diff_blob->dptr<T>(),
-      conv_desc_->Get(), cudnn_bwd_filter_algo, cudnn_workspace->mut_dptr<T>(),
-      cudnn_workspace->shape().At(0), CudnnDataType<T>::one,
-      filter_desc_->Get(), weight_diff_blob->mut_dptr<T>()));
+      ctx.device_ctx->cudnn_handle(), CudnnDataType<T>::one,
+      this->in_desc_->Get(), in_blob->dptr<T>(), this->out_desc_->Get(),
+      out_diff_blob->dptr<T>(), this->conv_desc_->Get(), cudnn_bwd_filter_algo,
+      cudnn_workspace->mut_dptr<T>(), cudnn_workspace->shape().At(0),
+      CudnnDataType<T>::one, this->filter_desc_->Get(),
+      weight_diff_blob->mut_dptr<T>()));
 
   // compute in_diff
   const Blob* weight_blob = BnInOp2Blob("weight");
@@ -111,10 +126,11 @@ void CudnnConvKernel<T>::BackwardDataContent(
 
   CudaCheck(cudnnConvolutionBackwardData(
       ctx.device_ctx->cudnn_handle(), CudnnDataType<T>::one,
-      filter_desc_->Get(), weight_blob->dptr<T>(), out_desc_->Get(),
-      out_diff_blob->dptr<T>(), conv_desc_->Get(), cudnn_bwd_data_algo,
+      this->filter_desc_->Get(), weight_blob->dptr<T>(), this->out_desc_->Get(),
+      out_diff_blob->dptr<T>(), this->conv_desc_->Get(), cudnn_bwd_data_algo,
       cudnn_workspace->mut_dptr<T>(), cudnn_workspace->shape().At(0),
-      CudnnDataType<T>::zero, in_desc_->Get(), in_diff_blob->mut_dptr<T>()));
+      CudnnDataType<T>::zero, this->in_desc_->Get(),
+      in_diff_blob->mut_dptr<T>()));
 }
 
 #define INSTANTIATE_CONV_KERNEL(type_cpp, type_proto) \
