9055cd81045e8c99afbd0185d4ab569adc4ae18f
jackalcooper <jackalcooper@gmail.com>
2018-03-05 18:00:38 +0800

    apply suggestions by zhuyi

--- oneflow/core/operator/eltwise_op.cpp
+++ oneflow/core/operator/eltwise_op.cpp
@@ -11,6 +11,7 @@ void EltwiseOp::InitFromOpConf() {
   }
   EnrollOutputBn("out");
   EnrollOutputBn("tmp");
+  EnrollOutputBn("mask");
 }
 
 const PbMessage& EltwiseOp::GetCustomizedConf() const {
@@ -23,19 +24,11 @@ void EltwiseOp::InferBlobDescs(
   const BlobDesc* in_0_blob_desc = GetBlobDesc4BnInOp(input_bns().at(0));
   std::vector<int64_t> out_dim_vec = in_0_blob_desc->shape().dim_vec();
   for (size_t i = 1; i < input_bns().size(); ++i) {
-    const BlobDesc* in_i_blob_desc = GetBlobDesc4BnInOp(input_bns().at(i));
-    CHECK_EQ(in_i_blob_desc->data_type(), in_0_blob_desc->data_type());
-    CHECK_EQ(in_i_blob_desc->has_data_id_field(),
-             in_0_blob_desc->has_data_id_field());
+    CHECK(*in_i_blob_desc == *GetBlobDesc4BnInOp(input_bns().at(i)));
   }
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
   *out_blob_desc = *in_0_blob_desc;
   out_blob_desc->mut_shape() = Shape(out_dim_vec);
-  // this blob is for GPU implementation
-  BlobDesc* tmp_blob_desc = GetBlobDesc4BnInOp("tmp");
-  *tmp_blob_desc = *in_0_blob_desc;
-  tmp_blob_desc->mut_shape() = Shape(out_dim_vec);
-  tmp_blob_desc->set_has_data_id_field(false);
   // this blob is for mask recording in blob index of a max value
   BlobDesc* mask_blob_desc = GetBlobDesc4BnInOp("mask");
   *mask_blob_desc = *in_0_blob_desc;
