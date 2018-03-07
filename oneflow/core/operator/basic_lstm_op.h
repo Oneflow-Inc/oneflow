62f07c57b81a79b53fc0599e8276bb8c54c61d96
Dungu <dungu1995@outlook.com>
2018-03-07 09:49:58 +0800

    lstm op.h

---
+++ oneflow/core/operator/basic_lstm_op.h
@@ -0,0 +1,24 @@
+#ifdef ONEFLOW_CORE_OPERATOR_BASIC_LSTM_OP_H_
+#define ONEFLOW_CORE_OPERATOR_BASIC_LSTM_OP_H_
+
+#inlcude "oneflow/core/operator/recurrent_op.h"
+
+namespace oneflow {
+
+class BasicLstmOp final : public RecurrentOp {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BasicLstmOp);
+  BasicLstmOp() = default;
+  ~BasicLstmOp() = default;
+  const PbMesssage& GetCustomizedConf() const override;
+
+ private:
+  void VirtualInitFromConf();
+  void VirtualInferBlobDescs(
+      std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
+      const ParallelContext* parallel_ctx) const;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_BASIC_LSTM_OP_H_
