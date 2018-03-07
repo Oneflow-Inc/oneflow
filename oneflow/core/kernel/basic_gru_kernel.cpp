f771c50e20b619c4850f072707e532f336b48700
clackhan <hanbinbin17@mails.ucas.ac.cn>
2018-03-07 16:21:37 +0800

    change Gru.h

--- oneflow/core/kernel/basic_gru_kernel.cpp
+++ oneflow/core/kernel/basic_gru_kernel.cpp
@@ -115,13 +115,13 @@ void BasicGruKernel<device_type, T>::ForwardDataContent(
   KernelUtil<device_type, T>::Axpy(
       ctx.device_ctx,
       static_cast<T>(reset_mul_candidate_hidden_blob->shape().At(0)),
-      static_cast<T>(-1), reset_mul_candidate_hidden_blob->dptr<T>(), static_cast<T>(1),
-      plus_op_out_blob->mut_dptr<T>(), static_cast<T>(1));
+      static_cast<T>(-1), reset_mul_candidate_hidden_blob->dptr<T>(),
+      static_cast<T>(1), plus_op_out_blob->mut_dptr<T>(), static_cast<T>(1));
   // plus_op_out += candidate_hidden
   KernelUtil<device_type, T>::Axpy(
       ctx.device_ctx, static_cast<T>(candidate_hidden_out_blob->shape().At(0)),
-      static_cast<T>(1), candidate_hidden_out_blob->dptr<T>(), static_cast<T>(1),
-      plus_op_out_blob->mut_dptr<T>(), static_cast<T>(1));
+      static_cast<T>(1), candidate_hidden_out_blob->dptr<T>(),
+      static_cast<T>(1), plus_op_out_blob->mut_dptr<T>(), static_cast<T>(1));
 
   // rec_out = out
   BnInOp2Blob("rec_out")->CopyDataContentFrom(ctx.device_ctx, out_blob);
