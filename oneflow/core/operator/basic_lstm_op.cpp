6baf057e084b5bbca429e6310bd2f250dc0dd907
Dungu <dungu1995@outlook.com>
2018-03-07 16:10:51 +0800

    deal with some bugs

--- oneflow/core/operator/basic_lstm_op.cpp
+++ oneflow/core/operator/basic_lstm_op.cpp
@@ -7,58 +7,57 @@ const PbMessage& BasicLstmOp::GetCustomizedConf() const {
 }
 
 void BasicLstmOp::VirtualInitFromOpConf() {
-	EnrollDataTmp("f_gate_out");	
-	EnrollDataTmp("i_gate_out");
-	EnrollDataTmp("o_gate_out");
-	EnrollDataTmp("c_gate_out");
+  EnrollDataTmpBn("f_gate_out");
+  EnrollDataTmpBn("i_gate_out");
+  EnrollDataTmpBn("o_gate_out");
+  EnrollDataTmpBn("c_gate_out");
 
-	EnrollModelBn("i2h_f_weight");
-	EnrollModelBn("h2h_f_weight");
-	EnrollModelBn("i2h_i_weight");
-	EnrollModelBn("h2h_i_weight");
-	EnrollModelBn("i2h_o_weight");
-	EnrollModelBn("h2h_o_weight");
-	EnrollModelBn("i2h_c_weight");
-	EnrollModelBn("h2h_c_weight");
+  EnrollModelBn("i2h_f_weight");
+  EnrollModelBn("h2h_f_weight");
+  EnrollModelBn("i2h_i_weight");
+  EnrollModelBn("h2h_i_weight");
+  EnrollModelBn("i2h_o_weight");
+  EnrollModelBn("h2h_o_weight");
+  EnrollModelBn("i2h_c_weight");
+  EnrollModelBn("h2h_c_weight");
 }
 
 void BasicLstmOp::VirtualInferBlobDescs(
-		std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
-		const ParalleContext* parallel_ctx) const {
-	int32_t hidden_size = GetBlobDesc4BnInOp("out")->shape().At(1);
-	const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
-	int64_t embedding_zie = in_blob_desc->shape().Count(1);
-	int64_t data_num = in_blob_desc->shape().At(0);
+    std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx) const {
+  int32_t hidden_size = GetBlobDesc4BnInOp("out")->shape().At(1);
+  const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
+  int64_t embedding_zie = in_blob_desc->shape().Count(1);
+  int64_t data_num = in_blob_desc->shape().At(0);
 
-	*GetBlobDesc4BnInOp("f_gate_out") = BlobDesc(
-			Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
-			false, true, in_blob_desc->max_col_num());
-	*GetBlobDesc4BnInOp("i_gate_out") = BlobDesc(
-			Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
-			false, true, in_blob_desc->max_col_num());
-	*GetBlobDesc4BnInOp("o_gate_out") = BlobDesc(
-			Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
-			false, true, in_blob_desc->max_col_num());
-	*GetBlobDesc4BnInOp("c_gate_out") = BlobDesc(
-			Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
-			false, true, in_blob_desc->max_col_num());
+  *GetBlobDesc4BnInOp("f_gate_out") = BlobDesc(
+      Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
+      false, true, in_blob_desc->max_col_num());
+  *GetBlobDesc4BnInOp("i_gate_out") = BlobDesc(
+      Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
+      false, true, in_blob_desc->max_col_num());
+  *GetBlobDesc4BnInOp("o_gate_out") = BlobDesc(
+      Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
+      false, true, in_blob_desc->max_col_num());
+  *GetBlobDesc4BnInOp("c_gate_out") = BlobDesc(
+      Shape({data_num, hidden_size}), JobDesc::Singleton()->DefaultDataType(),
+      false, true, in_blob_desc->max_col_num());
 
-	*GetBlobDesc4BnInOp("i2h_f_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-	*GetBlobDesc4BnInOp("h2h_f_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-	*GetBlobDesc4BnInOp("i2h_i_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-	*GetBlobDesc4BnInOp("h2h_i_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-	*GetBlobDesc4BnInOp("i2h_o_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-	*GetBlobDesc4BnInOp("h2h_o_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-	*GetBlobDesc4BnInOp("i2h_c_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-	*GetBlobDesc4BnInOp("h2h_c_weight") = 
-			BlobDesc(Shape({hidden_size, embedding_zie}));
-
-			}
+  *GetBlobDesc4BnInOp("i2h_f_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+  *GetBlobDesc4BnInOp("h2h_f_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+  *GetBlobDesc4BnInOp("i2h_i_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+  *GetBlobDesc4BnInOp("h2h_i_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+  *GetBlobDesc4BnInOp("i2h_o_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+  *GetBlobDesc4BnInOp("h2h_o_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+  *GetBlobDesc4BnInOp("i2h_c_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+  *GetBlobDesc4BnInOp("h2h_c_weight") =
+      BlobDesc(Shape({hidden_size, embedding_zie}));
+}
 }  // namespace oneflow
