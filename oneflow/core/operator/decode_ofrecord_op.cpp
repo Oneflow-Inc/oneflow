7ac4d93a9d294a4dafee9e1ff702a255dace38e0
cheng cheng <472491134@qq.com>
2018-03-09 19:04:03 +0800

    decode op

--- oneflow/core/operator/decode_ofrecord_op.cpp
+++ oneflow/core/operator/decode_ofrecord_op.cpp
@@ -5,7 +5,7 @@ namespace oneflow {
 void DecodeOfrecordOp::InitFromOpConf() {
   CHECK(op_conf().has_decode_ofrecord_conf());
   EnrollInputBn("in", false);
-  for(int32_t i = 0; i < op_conf().decode_ofrecord_conf().blobs_size(); ++i) {
+  for (int32_t i = 0; i < op_conf().decode_ofrecord_conf().blobs_size(); ++i) {
     EnrollOutputBn("out_" + std::to_string(i), false);
   }
 }
@@ -20,21 +20,29 @@ void DecodeOfrecordOp::InferBlobDescs(
   const DecodeOfrecordOpConf& conf = op_conf().decode_ofrecord_conf();
   FOR_RANGE(size_t, i, 0, output_bns().size()) {
     BlobDesc* out_blob_desc = GetBlobDesc4BnInOp(output_bns().at(i));
-    std::vector<int64_t> dim_vec(1 + conf.blobs(i).shape().dim_size());
+    const BlobConf& blob_conf = conf.blobs(i);
+    std::vector<int64_t> dim_vec(1 + blob_conf.shape().dim_size());
     dim_vec[0] = JobDesc::Singleton()->SinglePieceSize();
     FOR_RANGE(size_t, j, 1, dim_vec.size()) {
-      dim_vec[j] = conf.blobs(i).shape().dim(j - 1);
+      dim_vec[j] = blob_conf.shape().dim(j - 1);
     }
     out_blob_desc->mut_shape() = Shape(dim_vec);
-    out_blob_desc->set_data_type(conf.blobs(i).data_type());
+    out_blob_desc->set_data_type(blob_conf.data_type());
+    out_blob_desc->set_has_data_id_field(JobDesc::Singleton()->SizeOfOneDataId()
+                                         > 0);
+    out_blob_desc->set_has_col_num_field(blob_conf.max_sequence_size() > 1);
+    out_blob_desc->set_max_col_num(blob_conf.max_sequence_size());
   }
 }
 
 std::string DecodeOfrecordOp::obn2lbn(const std::string& output_bn) const {
   CHECK(output_bn.substr(0, 4) == "out_");
-  return op_name() + "/" + op_conf().decode_ofrecord_conf().blobs(std::stoi(output_bn.substr(4))).name();
+  return op_name() + "/"
+         + op_conf()
+               .decode_ofrecord_conf()
+               .blobs(std::stoi(output_bn.substr(4)))
+               .name();
 }
-
 
 REGISTER_OP(OperatorConf::kDecodeOfrecordConf, DecodeOfrecordOp);
 
