c56943b9f61ca6cd8ed11fa0d0e322dd6289544e
cheng cheng <472491134@qq.com>
2018-03-09 14:16:37 +0800

    remove op_context in softmax

--- oneflow/core/operator/softmax_op.cpp
+++ oneflow/core/operator/softmax_op.cpp
@@ -2,14 +2,6 @@
 
 namespace oneflow {
 
-struct SoftmaxOpCtx : public OpContext {
-  int32_t axis;
-  int64_t transpose_rows;
-  int64_t transpose_cols;
-  bool need_transpose;
-  PbRf<int32_t> perm;
-};
-
 void SoftmaxOp::InitFromOpConf() {
   CHECK(op_conf().has_softmax_conf());
 
@@ -27,34 +19,24 @@ const PbMessage& SoftmaxOp::GetCustomizedConf() const {
 
 void SoftmaxOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
-    const ParallelContext* parallel_ctx, DeviceType device_type,
-    std::function<void(OpContext*)> EnrollOpContext) const {
+    const ParallelContext* parallel_ctx) const {
   // in
   const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
+  Shape in_shape = in_blob_desc->shape();
   // out
   *GetBlobDesc4BnInOp("out") = *in_blob_desc;
   int32_t axis = op_conf().softmax_conf().axis();
-  int32_t dims = in_blob_desc->shape().NumAxes();
+  int32_t dims = in_shape.NumAxes();
   if (axis < 0) { axis += dims; }
+  CHECK_GE(dims, 2);
   CHECK_GE(axis, 0);
   CHECK_LT(axis, dims);
-  Shape in_shape = in_blob_desc->shape();
-  int64_t transpose_cols = in_shape.At(axis);
-  int64_t transpose_rows = in_shape.elem_cnt() / transpose_cols;
   // 1D blob store tmp calculate result
   BlobDesc* tmp_blob_desc = GetBlobDesc4BnInOp("softmax_num");
-  tmp_blob_desc->mut_shape() = Shape({transpose_rows});
+  tmp_blob_desc->mut_shape() = Shape({in_shape.elem_cnt() / in_shape.At(axis)});
   tmp_blob_desc->set_data_type(in_blob_desc->data_type());
-  SoftmaxOpCtx* op_ctx = new SoftmaxOpCtx;
-  EnrollOpContext(op_ctx);
-  op_ctx->axis = axis;
-  op_ctx->transpose_rows = transpose_rows;
-  op_ctx->transpose_cols = transpose_cols;
-  if (axis == dims - 1) {
-    op_ctx->need_transpose = false;
-  } else {
+  if (axis != dims - 1) {
     // transpose blob
-    op_ctx->need_transpose = true;
     BlobDesc* transpose_blob_desc = GetBlobDesc4BnInOp("transpose_in");
     transpose_blob_desc->mut_shape() = in_shape;
     transpose_blob_desc->mut_shape().Set(axis, in_shape.At(dims - 1));
@@ -62,24 +44,33 @@ void SoftmaxOp::InferBlobDescs(
     transpose_blob_desc->set_data_type(in_blob_desc->data_type());
     *GetBlobDesc4BnInOp("transpose_out") = *transpose_blob_desc;
     *GetBlobDesc4BnInOp("transpose_out_diff") = *transpose_blob_desc;
-    op_ctx->perm.Reserve(dims);
-    for (size_t i = 0; i < dims; ++i) { op_ctx->perm.Add(i); }
-    op_ctx->perm[axis] = dims - 1;
-    op_ctx->perm[dims - 1] = axis;
   }
 }
 
 void SoftmaxOp::VirtualGenKernelConf(
     std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-    const ParallelContext* parallel_ctx, const OpContext* op_ctx,
-    KernelConf* kernel_conf) const {
+    const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
   auto conf = kernel_conf->mutable_softmax_conf();
-  auto softmax_ctx = static_cast<const SoftmaxOpCtx*>(op_ctx);
-  conf->set_axis(softmax_ctx->axis);
-  conf->set_transpose_rows(softmax_ctx->transpose_rows);
-  conf->set_transpose_cols(softmax_ctx->transpose_cols);
-  conf->set_need_transpose(softmax_ctx->need_transpose);
-  *(conf->mutable_perm()) = softmax_ctx->perm;
+  const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
+  Shape in_shape = in_blob_desc->shape();
+  int32_t axis = op_conf().softmax_conf().axis();
+  int32_t dims = in_blob_desc->shape().NumAxes();
+  if (axis < 0) { axis += dims; }
+  int64_t transpose_cols = in_shape.At(axis);
+  int64_t transpose_rows = in_shape.elem_cnt() / transpose_cols;
+  conf->set_axis(axis);
+  conf->set_transpose_rows(transpose_rows);
+  conf->set_transpose_cols(transpose_cols);
+  if (axis == dims - 1) {
+    conf->set_need_transpose(false);
+  } else {
+    conf->set_need_transpose(true);
+    PbRf<int32_t>* perm = conf->mutable_perm();
+    perm->Reserve(dims);
+    for (size_t i = 0; i < dims; ++i) { perm->Add(i); }
+    (*perm)[axis] = dims - 1;
+    (*perm)[dims - 1] = axis;
+  }
 }
 
 REGISTER_OP(OperatorConf::kSoftmaxConf, SoftmaxOp);
