f0f90e272e2a5d9321c0711dd318eafcfd6c1d4a
chengtbf <472491134@qq.com>
2018-03-10 17:15:53 +0800

    decode op (#618)

---
+++ oneflow/core/operator/decode_ofrecord_op.h
@@ -0,0 +1,31 @@
+#ifndef ONEFLOW_CORE_OPERATOR_DECODE_OFRECORD_OP_H_
+#define ONEFLOW_CORE_OPERATOR_DECODE_OFRECORD_OP_H_
+
+#include "oneflow/core/operator/operator.h"
+
+namespace oneflow {
+
+class DecodeOFRecordOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(DecodeOFRecordOp);
+  DecodeOFRecordOp() = default;
+  ~DecodeOFRecordOp() = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  bool IsDecodeOp() const override { return true; }
+
+  void InferBlobDescs(
+      std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
+      const ParallelContext* parallel_ctx) const override;
+
+ private:
+  std::string ibn2lbn(const std::string& input_bn) const override {
+    return kPackedBlobName;
+  }
+  std::string obn2lbn(const std::string& output_bn) const override;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_DECODE_OFRECORD_OP_H_
