774010d0a251437f0f75c0e224bdf3b26a5719e0
jackalcooper <jackalcooper@gmail.com>
2018-03-11 13:58:26 +0800

    rename out_dim_vec to in_0_dim_vec and declare const

--- oneflow/core/operator/elementwise_op.cpp
+++ oneflow/core/operator/elementwise_op.cpp
@@ -11,13 +11,13 @@ void ElementwiseOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
   const BlobDesc* in_0_blob_desc = GetBlobDesc4BnInOp(input_bns().at(0));
-  std::vector<int64_t> out_dim_vec = in_0_blob_desc->shape().dim_vec();
+  const std::vector<int64_t> in_0_dim_vec = in_0_blob_desc->shape().dim_vec();
   for (size_t i = 1; i < input_bns().size(); ++i) {
     CHECK(*in_0_blob_desc == *GetBlobDesc4BnInOp(input_bns().at(i)));
   }
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
   *out_blob_desc = *in_0_blob_desc;
-  out_blob_desc->mut_shape() = Shape(out_dim_vec);
+  out_blob_desc->mut_shape() = Shape(in_0_dim_vec);
 }
 
 }  // namespace oneflow
