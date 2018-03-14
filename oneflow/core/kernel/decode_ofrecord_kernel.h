fe3cc8f5b7f9db74a7296631914eadaca6379736
Yi Zhu <619828575@qq.com>
2018-03-14 17:37:45 +0800

    Decode actor impl (#628)

---
+++ oneflow/core/kernel/decode_ofrecord_kernel.h
@@ -0,0 +1,17 @@
+#ifndef ONEFLOW_CORE_KERNEL_DECODE_OFRECORD_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_DECODE_OFRECORD_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+#include "oneflow/core/register/register.h"
+
+namespace oneflow {
+
+struct DecodeStatus {
+  Regst* in_regst_;
+  int32_t cur_col_id_;
+  int32_t max_col_id_;
+};
+
+}  // oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_DECODE_OFRECORD_KERNEL_H_
