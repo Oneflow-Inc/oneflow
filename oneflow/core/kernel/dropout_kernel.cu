8c529d57ff80ff8edbc0cee6341d802d4832afc2
Li Xinqi <lixinqi2010@gmail.com>
2018-03-15 18:23:36 +0800

    dropout op (#623)
    Former-commit-id: fb2ba3306d8a8ac94a185803bf4e0be3fb5851ae

---
+++ oneflow/core/kernel/dropout_kernel.cu
@@ -0,0 +1,36 @@
+#include "oneflow/core/kernel/dropout_kernel.h"
+#include "oneflow/core/kernel/kernel_util.h"
+#include "oneflow/core/common/data_type.h"
+#include "oneflow/core/common/preprocessor.h"
+
+namespace oneflow {
+
+namespace {
+
+template<typename T>
+__global__ void MaskAndScaleGpu(const int64_t n, double threshold, double scale,
+                                const T* x, const float* random_mask, T* y) {
+  CUDA_1D_KERNEL_LOOP(i, n) {
+    y[i] = x[i] * (random_mask[i] > threshold) * scale;
+  }
+}
+
+}  // namespace
+
+template<typename T>
+struct DropoutKernelUtil<DeviceType::kGPU, T> final {
+  static void MaskAndScale(DeviceCtx* ctx, const int64_t n, double threshold,
+                           double scale, const T* x, const float* random_mask,
+                           T* y) {
+    MaskAndScaleGpu<T>
+        <<<BlocksNum4ThreadsNum(n), kCudaThreadsNumPerBlock, 0,
+           ctx->cuda_stream()>>>(n, threshold, scale, x, random_mask, y);
+  }
+};
+
+#define INITIATE_DROPOUT_KERNEL_UTIL(T, type_proto) \
+  template struct DropoutKernelUtil<DeviceType::kGPU, T>;
+
+OF_PP_FOR_EACH_TUPLE(INITIATE_DROPOUT_KERNEL_UTIL, ARITHMETIC_DATA_TYPE_SEQ);
+
+}  // namespace oneflow
