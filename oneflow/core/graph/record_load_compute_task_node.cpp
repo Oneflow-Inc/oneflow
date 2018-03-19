b4fc8166b7ed5e626621ed8322624ddd405b8f7c
cheng cheng <472491134@qq.com>
2018-03-19 19:49:18 +0800

    reduce code

--- oneflow/core/graph/record_load_compute_task_node.cpp
+++ oneflow/core/graph/record_load_compute_task_node.cpp
@@ -30,8 +30,7 @@ void RecordLoadCompTaskNode::ToProto(TaskProto* task_proto) {
   int32_t zero_count =
       std::max<int32_t>(part_name_suffix_length - num.length(), 0);
   task_proto->set_data_path(JoinPath(
-      data_dir,
-      std::string(part_name_prefix + std::string(zero_count, '0') + num)));
+      data_dir, part_name_prefix + std::string(zero_count, '0') + num));
 }
 
 }  // namespace oneflow
