594f7c5fac65d47c36d1f6dcfee2913569d6b0f3
spwlyzx <spwlyzx@gmail.com>
2018-03-19 17:28:21 +0800

    compile

--- oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
+++ oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
@@ -14,8 +14,9 @@ std::string GenTokensMsgKey(int64_t machine_id) {
 }  // namespace
 
 void IBVerbsCommNet::Init(const Plan& plan) {
+  IBVerbsCommNet* comm_net = new IBVerbsCommNet(plan);
   CommNet::Singleton()->set_comm_network_ptr(new IBVerbsCommNet(plan));
-  endpoint_manager_.reset(new EndpointManager());
+  comm_net->endpoint_manager_.reset(new EndpointManager());
 }
 
 const void* IBVerbsCommNet::RegisterMemory(void* mem_ptr, size_t byte_size) {
@@ -60,9 +61,7 @@ void IBVerbsCommNet::SendActorMsg(int64_t dst_machine_id, const ActorMsg& msg) {
   endpoint_manager_->SendActorMsg(dst_machine_id, msg);
 }
 
-IBVerbsCommNet::IBVerbsCommNet(const Plan& plan) {
-  GenConnectionInfo(plan);
-}
+IBVerbsCommNet::IBVerbsCommNet(const Plan& plan) { GenConnectionInfo(plan); }
 
 void IBVerbsCommNet::DoRead(void* read_id, int64_t src_machine_id,
                             const void* src_token, const void* dst_token) {
