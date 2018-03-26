690e2aea62e28dddacbeb330284b465aeb03ba17
willzhang4a58 <willzhang4a58@gmail.com>
2018-03-26 16:07:37 +0800

    template cblas

--- oneflow/core/blas/cblas_template.h
+++ oneflow/core/blas/cblas_template.h
@@ -1,59 +1,38 @@
 #ifndef ONEFLOW_CORE_BLAS_CBLAS_TEMPLATE_H_
 #define ONEFLOW_CORE_BLAS_CBLAS_TEMPLATE_H_
 
+#include <type_traits>
+#include <utility>
 #include "oneflow/core/blas/cblas.h"
 
 namespace oneflow {
 
-// level 1 vector and vector
-// dot product
-template<typename FloatingPointType>
-FloatingPointType cblas_dot(const int n, const FloatingPointType* x,
-                            const int incx, const FloatingPointType* y,
-                            const int incy);
+#define CBLAS_TEMPLATE(name)                                               \
+  template<typename T, typename... Args>                                   \
+  auto cblas_##name(Args&&... args)                                        \
+      ->typename std::enable_if<std::is_same<T, float>::value,             \
+                                decltype(cblas_##s##name(                  \
+                                    std::forward<Args>(args)...))>::type { \
+    return cblas_##s##name(std::forward<Args>(args)...);                   \
+  }                                                                        \
+  template<typename T, typename... Args>                                   \
+  auto cblas_##name(Args&&... args)                                        \
+      ->typename std::enable_if<std::is_same<T, double>::value,            \
+                                decltype(cblas_##d##name(                  \
+                                    std::forward<Args>(args)...))>::type { \
+    return cblas_##d##name(std::forward<Args>(args)...);                   \
+  }
 
-// swap x and y
-template<typename FloatingPointType>
-void cblas_swap(const int n, FloatingPointType* x, const int incx,
-                FloatingPointType* y, const int incy);
+CBLAS_TEMPLATE(dot);
+CBLAS_TEMPLATE(swap);
+CBLAS_TEMPLATE(copy);
+CBLAS_TEMPLATE(axpy);
+CBLAS_TEMPLATE(scal);
+CBLAS_TEMPLATE(gemv);
+CBLAS_TEMPLATE(gemm);
 
-// copy x into y
-template<typename FloatingPointType>
-void cblas_copy(const int n, const FloatingPointType* x, const int incx,
-                FloatingPointType* y, const int incy);
+#undef CBLAS_TEMPLATE
 
-// y = a*x + y
-template<typename FloatingPointType>
-void cblas_axpy(const int n, const FloatingPointType alpha,
-                const FloatingPointType* x, const int incx,
-                FloatingPointType* y, const int incy);
-
-// x = a*x
-template<typename FloatingPointType>
-void cblas_scal(const int n, const FloatingPointType alpha,
-                FloatingPointType* x, const int incx);
-
-// level 2 matrix and vector
-// matrix vector multiply
-template<typename FloatingPointType>
-void cblas_gemv(const enum CBLAS_ORDER order,
-                const enum CBLAS_TRANSPOSE trans_a, const int m, const int n,
-                const FloatingPointType alpha, const FloatingPointType* a,
-                const int lda, const FloatingPointType* x, const int incx,
-                const FloatingPointType beta, FloatingPointType* y,
-                const int incy);
-
-// level 3 matrix and matrix
-// matrix matrix multiply
-template<typename FloatingPointType>
-void cblas_gemm(const enum CBLAS_ORDER order,
-                const enum CBLAS_TRANSPOSE trans_a,
-                const enum CBLAS_TRANSPOSE trans_b, const int m, const int n,
-                const int k, const FloatingPointType alpha,
-                const FloatingPointType* a, const int lda,
-                const FloatingPointType* b, const int ldb,
-                const FloatingPointType beta, FloatingPointType* c,
-                const int ldc);
 }  // namespace oneflow
 
 #endif  // ONEFLOW_CORE_BLAS_CBLAS_TEMPLATE_H_
