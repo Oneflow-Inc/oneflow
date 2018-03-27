27ab7ad5680d40d6cf83293372274764dd8c4629
Yi Zhu <619828575@qq.com>
2018-03-27 19:34:11 +0800

    Fix bugs related to Alexnet (#691)
    Former-commit-id: 6a82d21eaffbf6451f782fa3348b1be6b3da87ea

--- oneflow/core/kernel/local_response_normalization_kernel.h
+++ oneflow/core/kernel/local_response_normalization_kernel.h
@@ -24,31 +24,35 @@ class CudnnLRNDesc final {
 #endif  // WITH_CUDA
 
 template<DeviceType device_type, typename T>
-class LocalResponseNormalizationKernel final : public KernelIf<device_type> {
+class LocalResponseNormalizationKernel;
+
+template<typename T>
+class LocalResponseNormalizationKernel<DeviceType::kCPU, T> final
+    : public KernelIf<DeviceType::kCPU> {
  public:
   OF_DISALLOW_COPY_AND_MOVE(LocalResponseNormalizationKernel);
   LocalResponseNormalizationKernel() = default;
   ~LocalResponseNormalizationKernel() = default;
 
  private:
-  void VirtualKernelInit(const ParallelContext*) override {
-#ifdef WITH_CUDA
-    const PbRf<int64_t>& shape = GetPbRfFromPbMessage<int64_t>(
-        GetValFromPbMessage<const PbMessage&>(
-            this->kernel_conf().local_response_normalization_conf(), "batch"),
-        "dim");
-    std::vector<int> dims(shape.begin(), shape.end());
-    std::vector<int> strides{dims[1] * dims[2] * dims[3], 1, dims[2] * dims[3],
-                             dims[3]};
-    batch_desc_.reset(new CudnnTensorDesc(GetDataType<T>::val, 4, dims.data(),
-                                          strides.data()));
-    const LocalResponseNormalizationOpConf& op_conf =
-        this->op_conf().local_response_normalization_conf();
-    normalize_desc_.reset(new CudnnLRNDesc(op_conf.depth_radius(),
-                                           op_conf.alpha(), op_conf.beta(),
-                                           op_conf.bias()));
-#endif  // WITH_CUDA
-  }
+  void ForwardDataContent(
+      const KernelCtx&,
+      std::function<Blob*(const std::string&)>) const override;
+  void BackwardDataContent(
+      const KernelCtx&,
+      std::function<Blob*(const std::string&)>) const override;
+};
+
+template<typename T>
+class LocalResponseNormalizationKernel<DeviceType::kGPU, T> final
+    : public KernelIf<DeviceType::kGPU> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(LocalResponseNormalizationKernel);
+  LocalResponseNormalizationKernel() = default;
+  ~LocalResponseNormalizationKernel() = default;
+
+ private:
+  void VirtualKernelInit(const ParallelContext*) override;
   void ForwardDataContent(
       const KernelCtx&,
       std::function<Blob*(const std::string&)>) const override;
