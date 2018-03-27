f4fba3f339dabd4e43f9d42d4a0f347e8fc2d5f3
zyeric <619828575@qq.com>
2018-03-28 00:25:57 +0800

    make alexnet runnable, while bugs exist: 1) axpy in boxing fails 2) cudnncallback may fail at different places

--- oneflow/core/kernel/boxing_kernel.cpp
+++ oneflow/core/kernel/boxing_kernel.cpp
@@ -24,9 +24,14 @@ void CalcSumOfBlobs(DeviceCtx* ctx,
                            src_blob_0->ByteSizeOfDataContentField());
   FOR_RANGE(size_t, i, 1, src_bns.size()) {
     Blob* src_blob_i = BnInOp2Blob(src_bns.Get(i));
-    KernelUtil<DeviceType::kCPU, T>::Axpy(ctx, dst_blob->shape().elem_cnt(),
-                                          1.0, src_blob_i->dptr<T>(), 1,
-                                          dst_blob->mut_dptr<T>(), 1);
+    const T* src_ptr = src_blob_i->dptr<T>();
+    T* dst_ptr = dst_blob->mut_dptr<T>();
+    FOR_RANGE(size_t, j, 0, dst_blob->shape().elem_cnt()) {
+      *(dst_ptr + j) = *(src_ptr + j);
+    }
+    // KernelUtil<DeviceType::kCPU, T>::Axpy(ctx, dst_blob->shape().elem_cnt(),
+    //                                       1.0, src_blob_i->dptr<T>(), 1,
+    //                                       dst_blob->mut_dptr<T>(), 1);
   }
 }
 
