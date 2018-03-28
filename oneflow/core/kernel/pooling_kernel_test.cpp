ed4ba5f9511dedf0c87c18de279c5d626122474d
jackal <jackalcooper@gmail.com>
2018-03-28 15:33:26 +0800

    3D max

--- oneflow/core/kernel/pooling_kernel_test.cpp
+++ oneflow/core/kernel/pooling_kernel_test.cpp
@@ -97,6 +97,51 @@ OpKernelTestCase* MaxPooling2DTestCase(const std::string& job_type,
 }
 
 template<DeviceType device_type, typename T>
+OpKernelTestCase* MaxPooling3DTestCase(const std::string& job_type,
+                                       const std::string& forward_or_backward) {
+  OpKernelTestCase* pooling_test_case = new OpKernelTestCase();
+  pooling_test_case->mut_job_conf()->set_default_data_type(GetDataType<T>::val);
+  pooling_test_case->set_is_train(job_type == "train");
+  pooling_test_case->set_is_forward(forward_or_backward == "forward");
+  pooling_test_case->set_device_type(device_type);
+  MaxPooling3DOpConf* pooling_conf =
+      pooling_test_case->mut_op_conf()->mutable_max_pooling_3d_conf();
+  pooling_conf->set_padding("SAME");
+  pooling_conf->add_pool_size(1);
+  pooling_conf->add_pool_size(3);
+  pooling_conf->add_pool_size(3);
+  pooling_conf->add_strides(1);
+  pooling_conf->add_strides(2);
+  pooling_conf->add_strides(2);
+  pooling_conf->set_data_format("channels_first");
+
+  using KTC = KTCommon<device_type, T>;
+  BlobDesc* in_blob_desc = new BlobDesc(Shape({1, 1, 1, 5, 5}),
+                                        GetDataType<T>::val, false, false, 1);
+  BlobDesc* out_blob_desc = new BlobDesc(Shape({1, 1, 1, 3, 3}),
+                                         GetDataType<T>::val, false, false, 1);
+  pooling_test_case->InitBlob(
+      "in",
+      KTC::CreateBlobWithSpecifiedVal(
+          in_blob_desc, {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13,
+                         14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}));
+  pooling_test_case->InitBlob(
+      GenDiffBn("out"), KTC::CreateBlobWithSpecifiedVal(
+                            out_blob_desc, {7, 9, 10, 17, 19, 20, 22, 24, 25}));
+  pooling_test_case->ForwardCheckBlob(
+      "out", device_type,
+      KTC::CreateBlobWithSpecifiedVal(out_blob_desc,
+                                      {7, 9, 10, 17, 19, 20, 22, 24, 25}));
+  pooling_test_case->BackwardCheckBlob(
+      GenDiffBn("in"), device_type,
+      KTC::CreateBlobWithSpecifiedVal(
+          in_blob_desc, {0, 0, 0, 0,  0, 0,  7,  0, 9,  10, 0,  0, 0,
+                         0, 0, 0, 17, 0, 19, 20, 0, 22, 0,  24, 25}));
+
+  return pooling_test_case;
+}
+
+template<DeviceType device_type, typename T>
 OpKernelTestCase* AveragePoolingTestCase(
     const std::string& job_type, const std::string& forward_or_backward) {
   OpKernelTestCase* pooling_test_case = new OpKernelTestCase();
@@ -171,6 +216,9 @@ TEST_CPU_ONLY_OPKERNEL(MaxPooling1DTestCase, ARITHMETIC_DATA_TYPE_SEQ,
                        (train)(predict), (forward)(backward));
 
 TEST_CPU_ONLY_OPKERNEL(MaxPooling2DTestCase, ARITHMETIC_DATA_TYPE_SEQ,
+                       (train)(predict), (forward)(backward));
+
+TEST_CPU_ONLY_OPKERNEL(MaxPooling3DTestCase, ARITHMETIC_DATA_TYPE_SEQ,
                        (train)(predict), (forward)(backward));
 
 TEST_CPU_ONLY_OPKERNEL(AveragePoolingTestCase, FLOATING_DATA_TYPE_SEQ,
