e90b9c57ea12a8b1b9ac3ae61dba9ba1241ec2a6
Xinqi Li <lixinqi0703106@163.com>
2018-03-28 11:49:02 +0800

    dropout kernel test cases is passed

--- oneflow/core/kernel/dropout_kernel_test.cpp
+++ oneflow/core/kernel/dropout_kernel_test.cpp
@@ -36,7 +36,7 @@ std::vector<T> Dropout(const std::vector<T>& input,
                        const std::vector<float>& random_mask, float rate) {
   std::vector<T> output(input);
   FOR_RANGE(int, i, 0, output.size()) {
-    output.at(i) *= (random_mask.at(i) > rate);
+    output.at(i) *= (random_mask.at(i) > rate) / (1.f - rate);
   }
   return output;
 }
@@ -47,7 +47,8 @@ template<DeviceType device_type, typename T>
 OpKernelTestCase* DropoutTestCase(const std::string& job_type,
                                   const std::string& fw_or_bw) {
   OpKernelTestCase* dropout_test_case = new OpKernelTestCase();
-  dropout_test_case->set_is_train(job_type == "train");
+  bool is_train = (job_type == "train");
+  dropout_test_case->set_is_train(is_train);
   dropout_test_case->set_is_forward(fw_or_bw == "forward");
   dropout_test_case->set_device_type(device_type);
   auto* dropout_conf = dropout_test_case->mut_op_conf()->mutable_dropout_conf();
@@ -59,7 +60,9 @@ OpKernelTestCase* DropoutTestCase(const std::string& job_type,
   using KTC = KTCommon<device_type, T>;
   BlobDesc* blob_desc =
       new BlobDesc(Shape({1, 8}), GetDataType<T>::val, false, false, 1);
-  std::vector<T> input_data{1, -1, -2, 2, 0, 5, -10, 100};
+  BlobDesc* random_mask_blob_desc =
+      new BlobDesc(Shape({1, 8}), GetDataType<float>::val, false, false, 1);
+  std::vector<T> input_data{1, -1, -2, 2, 30, 5, -10, 100};
   std::vector<T> output_diff_data{-8, 7, -6, 5, -4, 3, -2, 1};
   std::vector<float> random_mask(input_data.size());
   RngUniform<device_type>(&random_mask, seed);
@@ -68,20 +71,27 @@ OpKernelTestCase* DropoutTestCase(const std::string& job_type,
   dropout_test_case->InitBlob(
       GenDiffBn("out"),
       KTC::CreateBlobWithSpecifiedVal(blob_desc, output_diff_data));
+  if (is_train) {
+    dropout_test_case->ForwardCheckBlob(
+        "random_mask", device_type,
+        KTCommon<device_type, float>::CreateBlobWithSpecifiedVal(
+            random_mask_blob_desc, random_mask));
+  }
   dropout_test_case->ForwardCheckBlob(
       "out", device_type,
       KTC::CreateBlobWithSpecifiedVal(
-          blob_desc, Dropout<device_type, T>(input_data, random_mask, rate)));
+          blob_desc, Dropout<device_type, T>(input_data, random_mask,
+                                             (is_train ? rate : 0.f))));
   dropout_test_case->BackwardCheckBlob(
       GenDiffBn("in"), device_type,
       KTC::CreateBlobWithSpecifiedVal(
-          blob_desc,
-          Dropout<device_type, T>(output_diff_data, random_mask, rate)));
+          blob_desc, Dropout<device_type, T>(output_diff_data, random_mask,
+                                             (is_train ? rate : 0.f))));
   return dropout_test_case;
 }
 
-TEST_CPU_ONLY_OPKERNEL(DropoutTestCase, ARITHMETIC_DATA_TYPE_SEQ,
-                       (train)(predict), (forward)(backward));
+TEST_CPU_AND_GPU_OPKERNEL(DropoutTestCase, ARITHMETIC_DATA_TYPE_SEQ,
+                          (train)(predict), (forward)(backward));
 
 }  // namespace test
 
