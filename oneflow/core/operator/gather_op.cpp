2c06816326b34aa915e1ab5a2c4c71f41cfc2562
Dounm <niuchong_dnm@163.com>
2018-03-29 20:07:13 +0800

    fix(gather_op.cpp): move max_col_num of out_blob to 1

--- oneflow/core/operator/gather_op.cpp
+++ oneflow/core/operator/gather_op.cpp
@@ -15,7 +15,10 @@ const PbMessage& GatherOp::GetSpecialConf() const {
 void GatherOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
-  *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
+  BlobDesc* out = GetBlobDesc4BnInOp("out");
+  *out = *GetBlobDesc4BnInOp("in");
+  out->set_max_col_num(1);
+  CHECK(out->has_col_num_field());
 }
 
 REGISTER_OP(OperatorConf::kGatherConf, GatherOp);
