62af31dc55f58f8a67bafaaf0b185e0a2b36dcce
jackal <jackalcooper@gmail.com>
2018-03-30 14:34:28 +0800

    new values

--- oneflow/core/kernel/softmax_kernel_test.cpp
+++ oneflow/core/kernel/softmax_kernel_test.cpp
@@ -14,18 +14,22 @@ void SoftmaxTestCase(OpKernelTestCase<device_type>* softmax_test_case,
 
   BlobDesc* blob_desc =
       new BlobDesc(Shape({2, 4}), GetDataType<T>::value, false, false, 1);
-  softmax_test_case->template InitBlob<T>("in", blob_desc,
-                                          {1, 2, 3, 4, 0, 0, 0, 0});
-  softmax_test_case->template InitBlob<T>(GenDiffBn("out"), blob_desc,
-                                          {0.2f, 1, 2, 3, -4, 3, -2, 1});
+  softmax_test_case->template InitBlob<T>(
+      "in", blob_desc,
+      {-1.2797170877f, 12.1243171692f, -3.2357401848f, -2.3464746475f,
+       5.7763452530f, 4.3293132782f, -4.2348613739f, -12.9131784439f});
+  softmax_test_case->template InitBlob<T>(
+      GenDiffBn("out"), blob_desc,
+      {-11.4311561584f, -7.3028049469f, -12.8807067871f, -7.8707337379f,
+       3.6359558105f, 3.3263847828f, -12.1905670166f, 4.5639686584f});
   softmax_test_case->template ForwardCheckBlob<T>(
       "out", blob_desc,
-      {0.0320586f, 0.0871443f, 0.2368828f, 0.6439143f, 0.25f, 0.25f, 0.25f,
-       0.25f});
+      {0.0000015090f, 0.9999977350f, 0.0000002134f, 0.0000005193f,
+       0.8095117807f, 0.1904518455f, 0.0000363422f, 0.0000000062f});
   softmax_test_case->template BackwardCheckBlob<T>(
       GenDiffBn("in"), blob_desc,
-      {-0.0737048f, -0.1306350f, -0.1182198f, 0.3225595f, -0.875f, 0.875f,
-       -0.375f, 0.375f});
+      {-0.0000062298f, 0.0007545450f, -0.0001190367f, -0.0000294919f,
+       4.8193175346f, -4.7620084137f, -0.0573007332f, 0.0000006110f});
 }
 
 TEST_CPU_AND_GPU_OPKERNEL(SoftmaxTestCase, FLOATING_DATA_TYPE_SEQ,
