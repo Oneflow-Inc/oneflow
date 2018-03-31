61bfb95b4a95937c2326eb5614a5806db677492e
Li Xinqi <lixinqi2010@gmail.com>
2018-03-31 16:12:22 +0800

    fc test (#724)

--- oneflow/core/kernel/opkernel_test_case.cpp
+++ oneflow/core/kernel/opkernel_test_case.cpp
@@ -280,21 +280,20 @@ void OpKernelTestCase<device_type>::Run() {
   auto op = ConstructOp(op_conf_);
   auto BnInOp2BlobDesc = MakeGetterBnInOp2BlobDesc();
   op->InferBlobDescs(BnInOp2BlobDesc, &parallel_ctx_, device_type);
-  std::list<bool> is_forward_launch_types;
-  if (Global<JobDesc>::Get()->IsPredict() || is_forward_) {
-    is_forward_launch_types = {true};
-  } else {
-    is_forward_launch_types = {true, false};
-  }
   auto BnInOp2Blob = MakeGetterBnInOp2Blob();
-  for (bool is_forward : is_forward_launch_types) {
+  auto Launch = [&](bool is_forward) {
     KernelConf kernel_conf;
     op->GenKernelConf(BnInOp2BlobDesc, is_forward, device_type, &parallel_ctx_,
                       &kernel_conf, nullptr);
     auto kernel = ConstructKernel(&parallel_ctx_, kernel_conf);
     kernel->Launch(kernel_ctx_, BnInOp2Blob);
+    SyncStream(&kernel_ctx_);
+  };
+  Launch(this);
+  if (Global<JobDesc>::Get()->IsTrain() && !is_forward_) {
+    initiation_before_backward_();
+    Launch(false);
   }
-  SyncStream(&kernel_ctx_);
   AssertAfterRun();
 }
 
