e7ac8738c690ee79b69335ff62ca92b4000a9230
zhiyuan <dssgsra@gmail.com>
2018-04-04 12:30:46 +0800

    kernelifwithmodel

--- oneflow/core/kernel/normalization_kernel.h
+++ oneflow/core/kernel/normalization_kernel.h
@@ -6,30 +6,27 @@
 namespace oneflow {
 
 template<DeviceType device_type, typename T>
-class NormalizationKernel final : public KernelIf<device_type> {
+class NormalizationKernel final : public KernelIfWithModel<device_type, T> {
  public:
   OF_DISALLOW_COPY_AND_MOVE(NormalizationKernel);
   NormalizationKernel() = default;
   ~NormalizationKernel() = default;
 
  private:
+  void InitModelBlobsWithRandomSeed(
+      DeviceCtx* ctx, std::mt19937* random_seed_gen,
+      std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
   void InitModelBlobsWithDir(
       DeviceCtx* ctx, int32_t part_id, int32_t part_num,
       const std::string& model_load_dir,
       std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
-  void InitModelBlobsWithRandomSeed(
-      DeviceCtx* ctx, std::mt19937* random_seed_gen,
-      std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+  void ForwardDataContent(
+      const KernelCtx&,
+      std::function<Blob*(const std::string&)>) const override;
+  void BackwardDataContent(
+      const KernelCtx&,
+      std::function<Blob*(const std::string&)>) const override;
 
-  void CalcMeanAndVariance(const KernelCtx&,
-                           const std::function<Blob*(const std::string&)>&,
-                           const Blob* in_blob) const;
-  void UpdateMovingMeanAndMovingVariance(
-      const KernelCtx&, const std::function<Blob*(const std::string&)>&) const;
-  void Normalize(const KernelCtx&,
-                 const std::function<Blob*(const std::string&)>&,
-                 const Blob* mean_blob, const Blob* variance_blob,
-                 const Blob* in_blob, Blob* out_blob) const;
   void CalcAboutGammaDiff(const KernelCtx&,
                           const std::function<Blob*(const std::string&)>,
                           const Blob* out_diff_blob,
@@ -41,13 +38,15 @@ class NormalizationKernel final : public KernelIf<device_type> {
   void CalcInDiff(const KernelCtx&,
                   const std::function<Blob*(const std::string&)>,
                   const Blob* out_diff_blob, Blob* in_diff_blob) const;
-
-  void ForwardDataContent(
-      const KernelCtx&,
-      std::function<Blob*(const std::string&)>) const override;
-  void BackwardDataContent(
-      const KernelCtx&,
-      std::function<Blob*(const std::string&)>) const override;
+  void Normalize(const KernelCtx&,
+                 const std::function<Blob*(const std::string&)>&,
+                 const Blob* mean_blob, const Blob* variance_blob,
+                 const Blob* in_blob, Blob* out_blob) const;
+  void CalcMeanAndVariance(const KernelCtx&,
+                           const std::function<Blob*(const std::string&)>&,
+                           const Blob* in_blob) const;
+  void UpdateMovingMeanAndMovingVariance(
+      const KernelCtx&, const std::function<Blob*(const std::string&)>&) const;
 };
 
 }  // namespace oneflow
