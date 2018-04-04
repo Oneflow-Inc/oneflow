aa871e96e0b6c1a4a947b9afc779fc122bbaf3c9
dssgsra <dssgsra@gmail.com>
2018-04-04 11:55:19 +0800

    get tmp size for reduce sum (#755)
    Former-commit-id: c16f859753a5b6bb8452a8067c7c7a055a6f90ab

--- oneflow/core/kernel/kernel_util.cu
+++ oneflow/core/kernel/kernel_util.cu
@@ -56,6 +56,20 @@ void Memset<DeviceType::kGPU>(DeviceCtx* ctx, void* dst, const char value,
   CudaCheck(cudaMemsetAsync(dst, value, sz, ctx->cuda_stream()));
 }
 
+#define MAKE_CUB_DEVICE_REDUCE_SWITCH_ENTRY(func_name, T) \
+  cub::DeviceReduce::func_name<T*, T*>
+DEFINE_STATIC_SWITCH_FUNC(cudaError_t, Sum, MAKE_CUB_DEVICE_REDUCE_SWITCH_ENTRY,
+                          MAKE_DATA_TYPE_CTRV_SEQ(FLOATING_DATA_TYPE_SEQ));
+
+size_t GetTmpSizeForReduceSum(DataType data_type, int64_t sum_elem_num) {
+  size_t tmp_storage_size;
+  SwitchSum(SwitchCase(data_type), nullptr, tmp_storage_size, nullptr, nullptr,
+            sum_elem_num);
+  return tmp_storage_size;
+}
+
+#undef MAKE_CUB_DEVICE_REDUCE_SWITCH_ENTRY
+
 #define KU_IF_METHOD                     \
   template<typename T, typename Derived> \
   void GpuKernelUtilIf<T, Derived>::
