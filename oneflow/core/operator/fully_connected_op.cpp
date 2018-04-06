a29c16b00ecc25babce4ff841b1026deeff0903d
spwlyzx <spwlyzx@gmail.com>
2018-04-06 15:51:02 +0800

    some minor changes

--- oneflow/core/operator/fully_connected_op.cpp
+++ oneflow/core/operator/fully_connected_op.cpp
@@ -4,11 +4,13 @@
 namespace oneflow {
 
 void FullyConnectedOp::InitFromOpConf() {
+  CHECK(op_conf().has_fully_connected_conf());
+
   EnrollInputBn("in");
   EnrollOutputBn("out");
   EnrollModelBn("weight");
 
-  if (GetValFromCustomizedConf<bool>("use_bias")) {
+  if (op_conf().fully_connected_conf().use_bias()) {
     EnrollModelBn("bias");
     EnrollModelTmpBn("bias_multiplier");
   }
@@ -26,10 +28,11 @@ void FullyConnectedOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
   // useful vars
+  const FullyConnectedOpConf& conf = op_conf().fully_connected_conf();
   const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
   CHECK_EQ(in_blob_desc->data_type(),
            Global<JobDesc>::Get()->DefaultDataType());
-  int32_t units = op_conf().fully_connected_conf().units();
+  int32_t units = conf.units();
   if (parallel_ctx->policy() == kModelParallel) {
     BalancedSplitter splitter(units, parallel_ctx->parallel_num());
     units = splitter.At(parallel_ctx->parallel_id()).size();
@@ -38,8 +41,7 @@ void FullyConnectedOp::InferBlobDescs(
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
   *out_blob_desc = *in_blob_desc;
   out_blob_desc->mut_shape() = Shape({in_blob_desc->shape().At(0), units});
-  if (op_conf().fully_connected_conf().activation()
-          != ActivationType::kNoActivation
+  if (conf.activation() != ActivationType::kNoActivation
       && Global<JobDesc>::Get()->IsTrain()) {
     GetBlobDesc4BnInOp("activation_buf")->mut_shape() =
         Shape({in_blob_desc->shape().At(0), units});
@@ -49,7 +51,7 @@ void FullyConnectedOp::InferBlobDescs(
   GetBlobDesc4BnInOp("weight")->mut_shape() =
       Shape({units, in_blob_desc->shape().Count(1)});
 
-  if (op_conf().fully_connected_conf().use_bias()) {
+  if (conf.use_bias()) {
     // bias
     GetBlobDesc4BnInOp("bias")->mut_shape() = Shape({1, units});
 
