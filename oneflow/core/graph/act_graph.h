59afb8f5dfee80e909befa97bbd77db5578e4e96
Li Xinqi <lixinqi2010@gmail.com>
2018-04-10 12:31:43 +0800

    optimize memory utilization (#787)
    Former-commit-id: e793e17493884fa3429cdd389791f2c8e1a50ad0

--- oneflow/core/graph/act_graph.h
+++ oneflow/core/graph/act_graph.h
@@ -77,13 +77,18 @@ class ActGraph final : public Graph<ActNode, ActEdge> {
   const TaskProto& GetTaskProto(int64_t actor_id) const {
     return *task_id2task_proto_.at(actor_id);
   }
+  const HashMap<int64_t, int64_t>& actor_id2act_cnt() const {
+    return actor_id2act_cnt_;
+  }
+  const HashMap<int64_t, double>& actor_id2total_act_time() const {
+    return actor_id2total_act_time_;
+  }
   const std::list<const ActNode*>& Nodes4Depth(int64_t depth) const {
     return depth2nodes_.at(depth);
   }
   const ActNode* ProducerNode4RegstUid(const std::string& regst_uid) const {
     return regst_uid2producer_node_.at(regst_uid);
   }
-
   const std::list<const ActNode*>& ConsumerNodes4RegstUid(
       const std::string& regst_uid) const {
     return regst_uid2consumer_nodes_.at(regst_uid);
@@ -97,6 +102,7 @@ class ActGraph final : public Graph<ActNode, ActEdge> {
   void InitEdges();
   void InitDepth();
   void InitTaskId2TaskProto();
+  void InitActorStatistics();
   void ForEachDepthRangeRegstUids(
       const std::function<void(const Range& range,
                                const std::list<std::string>& regst_uids)>&
@@ -114,6 +120,8 @@ class ActGraph final : public Graph<ActNode, ActEdge> {
   HashMap<std::string, ActNode*> regst_uid2producer_node_;
   HashMap<std::string, std::list<const ActNode*>> regst_uid2consumer_nodes_;
   HashMap<int64_t, std::list<const ActNode*>> depth2nodes_;
+  HashMap<int64_t, int64_t> actor_id2act_cnt_;
+  HashMap<int64_t, double> actor_id2total_act_time_;
 };
 
 }  // namespace oneflow
