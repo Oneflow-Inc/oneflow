925609c24a66561ef5049f333bfacd9abf9fe98e
leaves-zwx <kunta0932@gmail.com>
2018-04-12 22:02:57 +0800

    update rmsprop model update test

--- oneflow/core/kernel/rmsprop_model_update_kernel_test.cpp
+++ oneflow/core/kernel/rmsprop_model_update_kernel_test.cpp
@@ -5,6 +5,44 @@ namespace oneflow {
 
 namespace test {
 
+template<DeviceType device_type, typename T>
+void RmspropModelUpdateTestCase(OpKernelTestCase* mdupdt_test_case,
+                              const std::string& job_type,
+                              const std::string& fw_or_bw) {
+  mdupdt_test_case->set_is_train(job_type == "train");
+  mdupdt_test_case->set_is_forward(fw_or_bw == "forward");
+  NormalModelUpdateOpConf* normal_mdupdt_conf =
+      mdupdt_test_case->mut_op_conf()->mutable_normal_mdupdt_conf();
+  normal_mdupdt_conf->set_in_num(1);
+  NormalModelUpdateOpUserConf* user_conf =
+      normal_mdupdt_conf->mutable_user_conf();
+  user_conf->set_learning_rate(2.0f);
+  RMSPropModelUpdateConf* rmsprop_conf = user_conf->mutable_rmsprop_conf();
+  rmsprop_conf->set_decay_rate(1.0f / 2);
+  rmsprop_conf->set_epsilon(3.0f);
+
+  mdupdt_test_case->InitJobConf([](JobConf* job_conf) {
+    job_conf->set_single_piece_size(1);
+    job_conf->set_data_part_num(1);
+    job_conf->mutable_train_conf()->set_num_of_pieces_in_batch(2);
+  });
+
+  DataType data_type = GetDataType<T>::value;
+  Shape shape({1, 3, 2});
+  BlobDesc* blob_desc = new BlobDesc(shape, data_type, false, false, 1);
+  
+  mdupdt_test_case->template InitBlob<T>("model", blob_desc, std::vector<T>(shape.elem_cnt(), 2));
+  mdupdt_test_case->template InitBlob<T>("mean_square", blob_desc, std::vector<T>(shape.elem_cnt(), 0));
+  mdupdt_test_case->template InitBlob<T>("packed_blob", blob_desc,
+                                         std::vector<T>(shape.elem_cnt(), 2));
+  mdupdt_test_case->template ForwardCheckBlob<T>(
+      "model", blob_desc, std::vector<T>(shape.elem_cnt(), 1));
+
+  auto* other = new std::tuple<int64_t, const Blob*>(
+      -1, mdupdt_test_case->mut_bn_in_op2blob()->at("packed_blob"));
+  mdupdt_test_case->mut_kernel_ctx()->other = other;
+}
+
 namespace {
 
 template<DeviceType device_type, typename T>
