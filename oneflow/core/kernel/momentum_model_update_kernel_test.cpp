9f66831022d3b2e53579001a5f28663952d678b4
leaves-zwx <kunta0932@gmail.com>
2018-04-13 17:54:22 +0800

    Pass momentum model update test

--- oneflow/core/kernel/momentum_model_update_kernel_test.cpp
+++ oneflow/core/kernel/momentum_model_update_kernel_test.cpp
@@ -1,83 +1,98 @@
-#include "oneflow/core/kernel/momentum_model_update_kernel.h"
-#include "oneflow/core/kernel/kernel_test_common.h"
+#include "oneflow/core/kernel/opkernel_test_case.h"
 
 namespace oneflow {
 
 namespace test {
 
-namespace {
-
 template<DeviceType device_type, typename T>
-Kernel* BuildMomentumMdUpdateKernel(float learning_rate, float beta) {
-  OperatorConf op_conf;
-  op_conf.set_name("momentum_model_update_test");
-  MomentumModelUpdateOpConf* momentum_md_update_conf =
-      op_conf.mutable_momentum_mdupdt_conf();
-  momentum_md_update_conf->set_learning_rate(learning_rate);
-  momentum_md_update_conf->set_beta(beta);
-  auto momentum_md_update_op = ConstructOp(op_conf);
-  OperatorProto op_proto;
-  momentum_md_update_op->ToProto(&op_proto);
-  auto momentum_md_update_kernel = new MomentumMdUpdateKernel<device_type, T>();
-  momentum_md_update_kernel->InitFromOpProto(op_proto);
-  return momentum_md_update_kernel;
+void MomentumModelUpdateTestCase(OpKernelTestCase* mdupdt_test_case,
+                                 const std::string& job_type,
+                                 const std::string& fw_or_bw) {
+  mdupdt_test_case->set_is_train(job_type == "train");
+  mdupdt_test_case->set_is_forward(fw_or_bw == "forward");
+  NormalModelUpdateOpConf* normal_mdupdt_conf =
+      mdupdt_test_case->mut_op_conf()->mutable_normal_mdupdt_conf();
+  normal_mdupdt_conf->set_in_num(2);
+  NormalModelUpdateOpUserConf* user_conf =
+      normal_mdupdt_conf->mutable_user_conf();
+  user_conf->set_learning_rate(0.5f);
+  MomentumModelUpdateConf* momentum_conf = user_conf->mutable_momentum_conf();
+  momentum_conf->set_beta(0.5f);
+
+  DataType data_type = GetDataType<T>::value;
+  mdupdt_test_case->InitJobConf([data_type](JobConf* job_conf) {
+    job_conf->set_single_piece_size(1);
+    job_conf->set_data_part_num(1);
+    job_conf->mutable_train_conf()->set_num_of_pieces_in_batch(2);
+    job_conf->set_default_data_type(data_type);
+  });
+
+  Shape shape({1, 3, 2});
+  BlobDesc* blob_desc = new BlobDesc(shape, data_type, false, false, 1);
+  mdupdt_test_case->template InitBlob<T>("in_0", blob_desc,
+                                         std::vector<T>(shape.elem_cnt(), 1));
+  mdupdt_test_case->template InitBlob<T>("in_1", blob_desc,
+                                         std::vector<T>(shape.elem_cnt(), 3));
+  mdupdt_test_case->template InitBlob<T>("momentum", blob_desc,
+                                         std::vector<T>(shape.elem_cnt(), 4));
+  mdupdt_test_case->template InitBlob<T>("packed_blob", blob_desc,
+                                         std::vector<T>(shape.elem_cnt(), 2));
+  auto* other = new std::tuple<int64_t, const Blob*>(
+      -1, mdupdt_test_case->mut_bn_in_op2blob()->at("packed_blob"));
+  mdupdt_test_case->mut_kernel_ctx()->other = other;
+
+  mdupdt_test_case->template ForwardCheckBlob<T>(
+      "model", blob_desc, std::vector<T>(shape.elem_cnt(), 3));
+  mdupdt_test_case->template ForwardCheckBlob<T>(
+      "momentum", blob_desc, std::vector<T>(shape.elem_cnt(), 1), false);
 }
 
-void InitJobDesc(int32_t piece_size, int32_t num_of_pieces_in_batch) {
-  JobConf job_conf;
-  job_conf.set_piece_size(piece_size);
-  auto train_conf = job_conf.mutable_train_conf();
-  train_conf->set_num_of_pieces_in_batch(num_of_pieces_in_batch);
-  Global<JobDesc>::Get()->InitFromJobConf(job_conf);
+TEST_CPU_AND_GPU_OPKERNEL(MomentumModelUpdateTestCase, FLOATING_DATA_TYPE_SEQ,
+                          (train), (forward));
+
+DiffKernelImplTestCase* DiffMomentumModelUpdateKernelImpl(
+    const std::string& job_type, const std::string& fw_or_bw,
+    const std::string& cpp_type) {
+  auto* mdupdt_test_case =
+      new DiffKernelImplTestCase(job_type == "train", fw_or_bw == "forward",
+                                 DataType4CppTypeString(cpp_type));
+  mdupdt_test_case->InitJobConf([](JobConf* job_conf) {
+    job_conf->set_single_piece_size(1);
+    job_conf->set_data_part_num(1);
+    job_conf->mutable_train_conf()->set_num_of_pieces_in_batch(1);
+  });
+  mdupdt_test_case->set_initiate_kernel_ctx(
+      [mdupdt_test_case](
+          const std::function<Blob*(const std::string&)>& Blob4BnInOp) {
+        auto* other = new std::tuple<int64_t, const Blob*>(
+            -1, Blob4BnInOp("packed_blob"));
+        mdupdt_test_case->mut_kernel_ctx()->other = other;
+      });
+  NormalModelUpdateOpConf* normal_mdupdt_conf =
+      mdupdt_test_case->mut_op_conf()->mutable_normal_mdupdt_conf();
+  normal_mdupdt_conf->set_in_num(1);
+  NormalModelUpdateOpUserConf* user_conf =
+      normal_mdupdt_conf->mutable_user_conf();
+  user_conf->set_learning_rate(2.0f);
+  MomentumModelUpdateConf* momentum_conf = user_conf->mutable_momentum_conf();
+  momentum_conf->set_beta(0.8f);
+
+  mdupdt_test_case->SetBlobNames({"in_0", "packed_blob"}, {"model", "momentum"},
+                                 {}, {});
+  mdupdt_test_case->SetInputBlobDesc("in_0", Shape({1, 3, 2}),
+                                     DataType4CppTypeString(cpp_type));
+  mdupdt_test_case->SetInputBlobDesc("packed_blob", Shape({1, 3, 2}),
+                                     DataType4CppTypeString(cpp_type));
+  mdupdt_test_case->SetInputBlobDesc("model", Shape({1, 3, 2}),
+                                     DataType4CppTypeString(cpp_type));
+  mdupdt_test_case->SetInputBlobDesc("momentum", Shape({1, 3, 2}),
+                                     DataType4CppTypeString(cpp_type));
+  return mdupdt_test_case;
 }
 
-template<DeviceType device_type, typename T>
-std::function<Blob*(const std::string&)> BuildBnInOp2Blob() {
-  using KTC = KTCommon<device_type, T>;
-
-  BlobDesc* blob_desc =
-      new BlobDesc(Shape({1, 3, 2}), GetDataType<T>::value, false);
-
-  auto bn2blob = new HashMap<std::string, Blob*>;
-  (*bn2blob)["model"] = KTC::CreateBlobWithSameVal(blob_desc, 2);
-  (*bn2blob)["momentum"] = KTC::CreateBlobWithSameVal(blob_desc, 4);
-  (*bn2blob)["model_diffs"] = KTC::CreateBlobWithSameVal(blob_desc, 4);
-  (*bn2blob)["model_expected"] = KTC::CreateBlobWithSameVal(blob_desc, 3);
-  (*bn2blob)["momentum_expected"] = KTC::CreateBlobWithSameVal(blob_desc, 1);
-  return [bn2blob](const std::string& bn) { return bn2blob->at(bn); };
-}
-
-template<DeviceType device_type, typename T>
-void TestMomentumMdUpdateKernel() {
-  using KTC = KTCommon<device_type, T>;
-  KernelCtx ctx;
-  BuildKernelCtx<device_type>(&ctx);
-
-  const float learning_rate = {0.5f};
-  const float beta = {0.5f};
-  auto BnInOp2Blob = BuildBnInOp2Blob<device_type, T>();
-  auto momentum_md_update_kernel =
-      BuildMomentumMdUpdateKernel<device_type, T>(learning_rate, beta);
-  int32_t piece_size = 1;
-  int32_t num_of_pieces_in_batch = 2;
-  InitJobDesc(piece_size, num_of_pieces_in_batch);
-
-  momentum_md_update_kernel->Forward(ctx, BnInOp2Blob);
-  SyncStream<device_type>(&ctx);
-
-  KTC::CheckResult(BnInOp2Blob, "momentum", "momentum_expected");
-  KTC::CheckResult(BnInOp2Blob, "model", "model_expected");
-}
-
-}  // namespace
+TEST_DIFF_KERNEL_IMPL(DiffMomentumModelUpdateKernelImpl, (train), (forward),
+                      OF_PP_SEQ_MAP(OF_PP_PAIR_FIRST, FLOATING_DATA_TYPE_SEQ));
 
 }  // namespace test
-
-TEST(MomentumMdUpdateKernel, model_update) {
-#define MAKE_ENTRY(device_type, type_pair) \
-  test::TestMomentumMdUpdateKernel<device_type, OF_PP_PAIR_FIRST(type_pair)>();
-  OF_PP_FOR_EACH_TUPLE(MAKE_ENTRY, OF_PP_INTERNAL_SEQ_PRODUCT(
-                                       DEVICE_TYPE_SEQ, FLOATING_DATA_TYPE_SEQ))
-}
 
 }  // namespace oneflow
