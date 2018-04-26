da8b235cadc65c2cd6730d4a7daa86b934b5e9db
Niu Chong <niuchong_dnm@163.com>
2018-04-26 16:56:35 +0800

    feat: implement DecodeComputeTaskNode (#837)
    Former-commit-id: 59ffa1f481d07ab50058d4c6349e8970450c9c12

--- oneflow/core/graph/decode_compute_task_node.cpp
+++ oneflow/core/graph/decode_compute_task_node.cpp
@@ -1,12 +1,24 @@
 #include "oneflow/core/graph/decode_compute_task_node.h"
+#include "oneflow/core/graph/task_graph.h"
 #include "oneflow/core/graph/logical_node.h"
 
 namespace oneflow {
 
 void DecodeCompTaskNode::ProduceAllRegstsAndBindEdges() {
   ProduceRegst("data_tmp", 1, 1);
-  std::shared_ptr<RegstDesc> out_regst = ProduceRegst("out");
-  for (TaskEdge* edge : out_edges()) { edge->AddRegst("out", out_regst); }
+  ProduceRegst("boxing_out");
+  ProduceRegst("121_out");
+  for (TaskEdge* edge : out_edges()) {
+    const LogicalNode* succ_logical = GetOneSuccLogicalNodeOnEdge(edge);
+    BldSubTskGphMthd mthd = GetMthdForBldSubTskGph(logical_node(), succ_logical);
+    if (mthd == &TaskGraph::BldSubTskGphByBoxing) {
+      BindEdgeWithProducedRegst(edge, "boxing_out");
+    } else if (mthd == &TaskGraph::BldSubTskGphByOneToOne) {
+      BindEdgeWithProducedRegst(edge, "121_out");
+    } else {
+      UNIMPLEMENTED();
+    }
+  }
 }
 
 void DecodeCompTaskNode::ConsumeAllRegsts() {
@@ -18,27 +30,29 @@ void DecodeCompTaskNode::ConsumeAllRegsts() {
 }
 
 void DecodeCompTaskNode::BuildExecGphAndRegst() {
-  std::shared_ptr<RegstDesc> out_regst = GetProducedRegst("out");
+  std::shared_ptr<RegstDesc> out_regst_boxing = GetProducedRegst("boxing_out");
+  std::shared_ptr<RegstDesc> out_regst_121 = GetProducedRegst("121_out");
   std::shared_ptr<RegstDesc> data_tmp_regst = GetProducedRegst("data_tmp");
   ExecNode* node = mut_exec_gph().NewNode();
   node->mut_op() = logical_node()->SoleOp();
-  // const auto& data_output_lbns = logical_node()->data_output_lbns();
-  // for (const std::string& obn : node->op()->output_bns()) {
-  //  const std::string& lbn = node->op()->Lbn4BnInOp(obn);
-  //  if (data_output_lbns.find(lbn) == data_output_lbns.end()) {
-  //    data_tmp_regst->AddLbn(lbn);
-  //    node->BindBnWithRegst(obn, data_tmp_regst);
-  //  } else {
-  //    out_regst->AddLbn(lbn);
-  //    node->BindBnWithRegst(obn, out_regst);
-  //  }
-  //}
-  // for (const std::string& dtbn : node->op()->data_tmp_bns()) {
-  //  const std::string& lbn = node->op()->Lbn4BnInOp(dtbn);
-  //  data_tmp_regst->AddLbn(lbn);
-  //  node->BindBnWithRegst(dtbn, data_tmp_regst);
-  //}
-  // node->InferBlobDescs(parallel_ctx(), device_type());
+
+  const HashSet<LogicalBlobId>& lbi_boxing = logical_node()->lbi_boxing();
+  const HashSet<LogicalBlobId>& lbi_121 = logical_node()->lbi_121();
+  for (const std::string& obn : node->op()->output_bns()) {
+    const LogicalBlobId& lbi = node->op()->BnInOp2Lbi(obn);
+    if (lbi_boxing.find(lbi) != lbi_boxing.end()) {
+      out_regst_boxing->AddLbi(lbi);
+      node->BindBnWithRegst(obn, out_regst_boxing);
+    } else if (lbi_121.find(lbi) != lbi_121.end()) {
+      out_regst_121->AddLbi(lbi);
+      node->BindBnWithRegst(obn, out_regst_121);
+    } else {
+      data_tmp_regst->AddLbi(lbi);
+      node->BindBnWithRegst(obn, data_tmp_regst);
+    }
+  }
+  node->AddBnToRegstAndBindIt(&Operator::data_tmp_bns, data_tmp_regst);
+  node->InferBlobDescs(parallel_ctx());
 }
 
 }  // namespace oneflow
