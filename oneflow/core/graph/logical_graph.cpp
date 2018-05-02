595fbc3cd162ff46cadb4af3fd477c135c7a2c82
Dounm <niuchong_dnm@163.com>
2018-05-02 17:25:21 +0800

    fix: skip clone_op when ConnectFwToBw()

--- oneflow/core/graph/logical_graph.cpp
+++ oneflow/core/graph/logical_graph.cpp
@@ -506,6 +506,7 @@ void LogicalGraph::BuildRecordLoadStruct() {
 
 void LogicalGraph::ConnectFwToBw() {
   ForEachLogicalNode<BackwardLogicalNode>([this](BackwardLogicalNode* bw_node) {
+    if (bw_node->SoleOp()->op_conf().has_clone_conf()) { return; }
     Connect<LogicalNode>(bw_node->fw_node(), NewEdge(), bw_node);
   });
 }
