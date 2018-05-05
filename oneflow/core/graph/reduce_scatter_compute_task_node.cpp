6b0b66e3b4e83a07ee637d27e75e2dd71ec5786b
zyeric <619828575@qq.com>
2018-05-05 10:34:28 +0800

    impl reduce scatter tasknode

--- oneflow/core/graph/reduce_scatter_compute_task_node.cpp
+++ oneflow/core/graph/reduce_scatter_compute_task_node.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/graph/reduce_scatter_compute_task_node.h"
+#include "oneflow/core/graph/logical_node.h"
 
 namespace oneflow {
 
@@ -6,8 +7,21 @@ void ReduceScatterCompTaskNode::ProduceAllRegstsAndBindEdges() {
   int64_t index = 0;
   for (TaskEdge* edge : out_edges()) {
     std::string out_regst_name = "out_" + std::to_string(index);
-    edge->AddRegst(out_regst_name, ProduceRegst(out_regst_name));
+    std::shared_ptr<RegstDesc> out_regst = ProduceRegst(out_regst_name);
+    edge->AddRegst(out_regst_name, out_regst);
     ++index;
+
+    TaskNode* dst_node = edge->dst_node();
+    if (dst_node->GetTaskType() == TaskType::kReduceAdd) {
+      CompTaskNode* reduce_add_node = static_cast<CompTaskNode*>(dst_node);
+      if (this->parallel_id() == reduce_add_node->parallel_id()
+          && device_type() == DeviceType::kGPU) {
+        MemoryCase* mem_case = out_regst.get()->mut_mem_case();
+        mem_case->Clear();
+        mem_case->mutable_device_cuda_mem()->set_device_id(
+            Global<IDMgr>::Get()->GetGpuDevPhyIdFromThrdId(thrd_id()));
+      }
+    }
   }
 }
 
@@ -16,11 +30,22 @@ void ReduceScatterCompTaskNode::ConsumeAllRegsts() {
 }
 
 void ReduceScatterCompTaskNode::InitProducedRegstMemCase(MemoryCase* mem_case) {
-  // TODO
+  mem_case->mutable_host_mem();
+  if (device_type() == DeviceType::kGPU) { mem_case->mutable_host_mem()->set_used_by_device(true); }
 }
 
 void ReduceScatterCompTaskNode::BuildExecGphAndRegst() {
-  // TODO
+  ExecNode* node = mut_exec_gph().NewNode();
+  std::shared_ptr<Operator> reduce_scatter_op = this->logical_node()->SoleOp();
+  node->mut_op() = reduce_scatter_op;
+  node->BindBnWithRegst(reduce_scatter_op->SoleIbn(), GetSoleConsumedRegst("in"));
+  FOR_RANGE(size_t, i, 0, reduce_scatter_op->output_bns().size()) {
+    std::shared_ptr<RegstDesc> out_regst = GetProducedRegst("out_" + std::to_string(i));
+    const std::string& obn = reduce_scatter_op->output_bns().Get(i);
+    out_regst->AddLbi(reduce_scatter_op->BnInOp2Lbi(obn));
+    node->BindBnWithRegst(obn, out_regst);
+  }
+  node->InferBlobDescs(parallel_ctx());
 }
 
 }  // namespace oneflow
