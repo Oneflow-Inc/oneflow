d7481e86a6a5482f99fa39f807c76243bbb0c5d1
zyeric <619828575@qq.com>
2018-05-07 19:56:58 +0800

    add naive actor

---
+++ oneflow/core/actor/naive_actor.cpp
@@ -0,0 +1,19 @@
+#include "oneflow/core/actor/naive_actor.h"
+
+namespace oneflow {
+
+void NaiveActor::Act() {
+  AsyncLaunchKernel(GenDefaultKernelCtx(),
+                    [&](int64_t regst_desc_id) -> Regst* { return nullptr; });
+  int64_t piece_id = GetNaiveFirstCurReadable()->piece_id();
+  AsyncSendRegstMsgToConsumer([&](Regst* regst) {
+    regst->set_piece_id(piece_id);
+    return true;
+  });
+}
+
+REGISTER_ACTOR(TaskType::kReduceScatter, NaiveActor);
+REGISTER_ACTOR(TaskType::kReduceAdd, NaiveActor);
+REGISTER_ACTOR(TaskType::kReduceGather, NaiveActor);
+
+}  // namespace oneflow
