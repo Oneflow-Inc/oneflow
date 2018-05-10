bb98e5711ad4dbf879a9313ed5ab2c3c31d9a148
zyeric <619828575@qq.com>
2018-05-10 19:16:16 +0800

    refine code

--- oneflow/core/actor/naive_actor.cpp
+++ oneflow/core/actor/naive_actor.cpp
@@ -3,7 +3,6 @@
 namespace oneflow {
 
 void NaiveActor::Act() {
-  LOG(INFO) << actor_id() << "  launch kernel";
   AsyncLaunchKernel(GenDefaultKernelCtx());
   int64_t piece_id = GetNaiveFirstCurReadable()->piece_id();
   AsyncSendRegstMsgToConsumer([&](Regst* regst) {
