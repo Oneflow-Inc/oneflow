12b4e61b9d4ab1a40948da51fb53c9f89edac2b1
leaves-zwx <kunta0932@gmail.com>
2018-05-18 15:18:36 +0800

    Rename model tmp to const model (#878)

--- oneflow/core/actor/normal_backward_compute_actor.cpp
+++ oneflow/core/actor/normal_backward_compute_actor.cpp
@@ -6,8 +6,8 @@ void NormalBackwardCompActor::VirtualCompActorInit(const TaskProto& task_proto) 
   b121_out_regst_desc_id_ = Name2SoleRegstDescId("boxing_out");
   if (b121_out_regst_desc_id_ == -1) { b121_out_regst_desc_id_ = Name2SoleRegstDescId("121_out"); }
   model_regst_desc_id_ = Name2SoleRegstDescId("model");
-  model_tmp_regst_desc_id_ = Name2SoleRegstDescId("model_tmp");
-  model_tmp_regst_ = nullptr;
+  const_model_regst_desc_id_ = Name2SoleRegstDescId("const_model");
+  const_model_regst_ = nullptr;
   const_buf_regst_desc_id_ = Name2SoleRegstDescId("const_buf");
   const_buf_regst_ = nullptr;
   staleness_ = -1;
@@ -20,9 +20,9 @@ void NormalBackwardCompActor::ForEachCurCustomizedReadableRegst(
     CHECK_EQ(model_regst_queue_.empty(), false);
     handler(model_regst_queue_.front());
   }
-  if (model_tmp_regst_desc_id_ != -1) {
-    CHECK(model_tmp_regst_);
-    handler(model_tmp_regst_);
+  if (const_model_regst_desc_id_ != -1) {
+    CHECK(const_model_regst_);
+    handler(const_model_regst_);
   }
   if (const_buf_regst_desc_id_ != -1) {
     CHECK(const_buf_regst_);
@@ -40,9 +40,9 @@ void NormalBackwardCompActor::NormalProcessCustomizedReadableRegstMsg(const Acto
   if (msg.regst()->regst_desc_id() == model_regst_desc_id_) {
     if (staleness_ == -1) { staleness_ = msg.regst()->regst_desc()->register_num() - 1; }
     model_regst_queue_.push(msg.regst());
-  } else if (msg.regst()->regst_desc_id() == model_tmp_regst_desc_id_) {
-    CHECK(model_tmp_regst_ == nullptr);
-    model_tmp_regst_ = msg.regst();
+  } else if (msg.regst()->regst_desc_id() == const_model_regst_desc_id_) {
+    CHECK(const_model_regst_ == nullptr);
+    const_model_regst_ = msg.regst();
   } else if (msg.regst()->regst_desc_id() == const_buf_regst_desc_id_) {
     CHECK(const_buf_regst_ == nullptr);
     const_buf_regst_ = msg.regst();
@@ -57,8 +57,8 @@ void NormalBackwardCompActor::Act() {
   AsyncLaunchKernel(GenDefaultKernelCtx(), [this](int64_t regst_desc_id) -> Regst* {
     if (regst_desc_id == model_regst_desc_id_) {
       return model_regst_queue_.front();
-    } else if (regst_desc_id == model_tmp_regst_desc_id_) {
-      return model_tmp_regst_;
+    } else if (regst_desc_id == const_model_regst_desc_id_) {
+      return const_model_regst_;
     } else if (regst_desc_id == const_buf_regst_desc_id_) {
       return const_buf_regst_;
     } else {
@@ -85,7 +85,7 @@ bool NormalBackwardCompActor::IsCustomizedReadReady() {
     int64_t expected_model_vid = GetNaiveCurReadable(b121_out_regst_desc_id_)->model_version_id();
     CHECK_EQ(expected_model_vid, model_regst_queue_.front()->model_version_id());
   }
-  if (model_tmp_regst_desc_id_ != -1 && model_tmp_regst_ == nullptr) { return false; }
+  if (const_model_regst_desc_id_ != -1 && const_model_regst_ == nullptr) { return false; }
   if (const_buf_regst_desc_id_ != -1 && const_buf_regst_ == nullptr) { return false; }
   return true;
 }
@@ -95,9 +95,9 @@ void NormalBackwardCompActor::AsyncReturnAllCustomizedReadableRegst() {
     AsyncSendRegstMsgToProducer(model_regst_queue_.front());
     model_regst_queue_.pop();
   }
-  if (model_tmp_regst_) {
-    AsyncSendRegstMsgToProducer(model_tmp_regst_);
-    model_tmp_regst_ = nullptr;
+  if (const_model_regst_) {
+    AsyncSendRegstMsgToProducer(const_model_regst_);
+    const_model_regst_ = nullptr;
   }
   if (const_buf_regst_) {
     AsyncSendRegstMsgToProducer(const_buf_regst_);
