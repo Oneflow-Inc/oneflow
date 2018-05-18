bb1c1ec2bba86088bcafa355b689b94742c8d999
Li Xinqi <lixinqi2010@gmail.com>
2018-05-18 17:53:33 +0800

    fast softmax kernel (#871)

--- oneflow/core/operator/sparse_softmax_cross_entropy_loss_op.cpp
+++ oneflow/core/operator/sparse_softmax_cross_entropy_loss_op.cpp
@@ -2,7 +2,10 @@
 
 namespace oneflow {
 
-void SparseSoftmaxCrossEntropyLossOp::VirtualInitFromOpConf() { EnrollDataTmpBn("prob"); }
+void SparseSoftmaxCrossEntropyLossOp::VirtualInitFromOpConf() {
+  EnrollDataTmpBn("prob");
+  EnrollConstBufBn("sum_multiplier");
+}
 
 const PbMessage& SparseSoftmaxCrossEntropyLossOp::GetCustomizedConf() const {
   return op_conf().sparse_softmax_cross_entropy_loss_conf();
@@ -17,10 +20,15 @@ void SparseSoftmaxCrossEntropyLossOp::VirtualInferBlobDescs(
     std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
   const BlobDesc* pred_blob_desc = GetBlobDesc4BnInOp("prediction");
+  CHECK_EQ(pred_blob_desc->shape().NumAxes(), 2);
   // prob
   BlobDesc* prob_blob_desc = GetBlobDesc4BnInOp("prob");
   prob_blob_desc->mut_shape() = Shape(pred_blob_desc->shape());
   prob_blob_desc->set_data_type(pred_blob_desc->data_type());
+
+  BlobDesc* sum_multiplier_blob_desc = GetBlobDesc4BnInOp("sum_multiplier");
+  sum_multiplier_blob_desc->mut_shape() = Shape({pred_blob_desc->shape().At(1)});
+  sum_multiplier_blob_desc->set_data_type(pred_blob_desc->data_type());
 }
 
 REGISTER_OP(OperatorConf::kSparseSoftmaxCrossEntropyLossConf, SparseSoftmaxCrossEntropyLossOp);
