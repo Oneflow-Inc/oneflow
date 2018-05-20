720e268701a334c6b99134221957eb1ab483624b
jiyuan <yuan.ms2@gmail.com>
2018-05-20 16:01:11 +0800

    remove useless code

--- oneflow/core/device/cuda_device_context.cpp
+++ oneflow/core/device/cuda_device_context.cpp
@@ -5,20 +5,7 @@ namespace oneflow {
 
 #ifdef WITH_CUDA
 
-namespace {
-
-void CUDART_CB CudaCallBackHandle(cudaStream_t, cudaError_t status, void* void_ptr) {
-  CudaCheck(status);
-  auto callback_ptr = static_cast<std::function<void()>*>(void_ptr);
-  (*callback_ptr)();
-  delete callback_ptr;
-}
-
-}  // namespace
-
 void CudaDeviceCtx::AddCallBack(std::function<void()> callback_stack) const {
-  // auto callback_heap = new std::function<void()>(callback_stack);
-  // CudaCheck(cudaStreamAddCallback(cuda_stream(), &CudaCallBackHandle, callback_heap, 0));
   CudaEventCB event_cb;
   CudaCheck(cudaEventCreateWithFlags(&(event_cb.event), cudaEventDisableTiming));
   event_cb.callback = callback_stack;
