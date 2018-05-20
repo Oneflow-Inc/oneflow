c65a98709f6e195e251b2f61f2ef808579241ee5
Xinqi Li <lixinqi0703106@163.com>
2018-05-20 23:44:16 +0800

    A BnInOp2Blob wrapper is better than GetOutDiffBlob

--- oneflow/core/kernel/fully_connected_kernel.cpp
+++ oneflow/core/kernel/fully_connected_kernel.cpp
@@ -29,7 +29,7 @@ template<DeviceType device_type, typename T>
 void FullyConnectedKernel<device_type, T>::BackwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* in_blob = BnInOp2Blob("in");
-  const Blob* out_diff_blob = this->GetOutDiffBlob(BnInOp2Blob);
+  const Blob* out_diff_blob = BnInOp2Blob("out_diff");
 
   Blob* in_diff_blob = BnInOp2Blob("in_diff");
 
