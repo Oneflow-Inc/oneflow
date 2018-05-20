dc6fb26e825fff16689488c4c8a545ce8b5ec0d2
Xinqi Li <lixinqi0703106@163.com>
2018-05-20 22:09:17 +0800

    fix curand init bug

--- oneflow/core/kernel/random_generator.cpp
+++ oneflow/core/kernel/random_generator.cpp
@@ -5,10 +5,8 @@ namespace oneflow {
 
 RandomGenerator::RandomGenerator(int64_t seed) : mt19937_generator_(seed) {
 #ifdef WITH_CUDA
-  if (curandCreateGenerator(&curand_generator_, CURAND_RNG_PSEUDO_DEFAULT)
-      != CURAND_STATUS_SUCCESS) {
-    CudaCheck(curandSetPseudoRandomGeneratorSeed(curand_generator_, GetCurTime()));
-  }
+  CudaCheck(curandCreateGenerator(&curand_generator_, CURAND_RNG_PSEUDO_DEFAULT));
+  CudaCheck(curandSetPseudoRandomGeneratorSeed(curand_generator_, seed));
 #endif
 }
 
