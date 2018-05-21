5e687ebf9d4e987525145d7486c5ad68fb2cc6da
jiyuan <yuan.ms2@gmail.com>
2018-05-21 14:34:37 +0800

    fix missing cudaEventRecord issue

--- oneflow/core/device/cuda_device_context.cpp
+++ oneflow/core/device/cuda_device_context.cpp
@@ -7,7 +7,10 @@ namespace oneflow {
 
 void CudaDeviceCtx::AddCallBack(std::function<void()> callback_stack) const {
   CudaEventCB event_cb;
-  CudaCheck(cudaEventCreateWithFlags(&(event_cb.event), cudaEventDisableTiming));
+  // Using cudaEventDisableTiming has better performance but cost more CPUs
+  // CudaCheck(cudaEventCreateWithFlags(&(event_cb.event), cudaEventDisableTiming));
+  CudaCheck(cudaEventCreateWithFlags(&(event_cb.event), cudaEventBlockingSync));
+  CudaCheck(cudaEventRecord(event_cb.event, cuda_stream()));
   event_cb.callback = callback_stack;
   static_cast<GpuThread*>(cur_thread_)->AddEventCb(event_cb);
 }
