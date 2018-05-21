d0bfbdf09296970ff3f455122766555755bf0090
Xinqi Li <lixinqi0703106@163.com>
2018-05-21 16:40:14 +0800

    refactor softmax with RowMax RowSum

--- oneflow/core/kernel/kernel_util.h
+++ oneflow/core/kernel/kernel_util.h
@@ -105,6 +105,14 @@ struct CpuKernelUtilIf {
   static void CopyColsRegion(DeviceCtx* ctx, const int64_t row_num, const int64_t col_num,
                              const T* x, const int64_t x_col_offset, const int64_t x_lda, T* y,
                              const int64_t y_col_offset, const int64_t y_lda);
+  static void RowMax(DeviceCtx* ctx, const int64_t row_num, const int64_t col_num, const T* x, T* y,
+                     void* temp_storage, const size_t temp_storage_bytes) {
+    TODO();
+  }
+  static void RowSum(DeviceCtx* ctx, const int64_t row_num, const int64_t col_num, const T* x, T* y,
+                     void* temp_storage, const size_t temp_storage_bytes) {
+    TODO();
+  }
   static void Transpose(DeviceCtx* ctx, const int32_t num_axis, const Shape& x_shape,
                         const Shape& y_shape, const PbRf<int32_t>& permutation,
                         const int64_t elem_cnt, const T* x, T* y);
@@ -180,6 +188,14 @@ struct GpuKernelUtilIf {
   static void CopyColsRegion(DeviceCtx* ctx, const int64_t row_num, const int64_t col_num,
                              const T* x, const int64_t x_col_offset, const int64_t x_lda, T* y,
                              const int64_t y_col_offset, const int64_t y_lda);
+  static void RowMax(DeviceCtx* ctx, const int64_t row_num, const int64_t col_num, const T* x, T* y,
+                     void* temp_storage, const size_t temp_storage_bytes) {
+    TODO();
+  }
+  static void RowSum(DeviceCtx* ctx, const int64_t row_num, const int64_t col_num, const T* x, T* y,
+                     void* temp_storage, const size_t temp_storage_bytes) {
+    TODO();
+  }
   static void Transpose(DeviceCtx* ctx, const int32_t num_axis, const Shape& x_shape,
                         const Shape& y_shape, const PbRf<int32_t>& permutation,
                         const int64_t elem_cnt, const T* x, T* y);
--- oneflow/core/kernel/softmax_kernel.h
+++ oneflow/core/kernel/softmax_kernel.h
@@ -13,8 +13,6 @@ class SoftmaxKernel final : public KernelIf<device_type> {
   ~SoftmaxKernel() = default;
 
  private:
-  void InitConstBufBlobs(DeviceCtx*,
-                         std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
   void ForwardDataContent(const KernelCtx&,
                           std::function<Blob*(const std::string&)>) const override;
   void BackwardDataContent(const KernelCtx&,
@@ -23,13 +21,6 @@ class SoftmaxKernel final : public KernelIf<device_type> {
 
 template<DeviceType device_type, typename T>
 struct SoftmaxKernelUtil {
-  // n = number of data sample
-  // w = number of (input/output) neuron
-  static void ForwardMax(DeviceCtx* ctx, const int64_t n, const int64_t w, const T* out, T* tmp);
-
-  static void RowSum(DeviceCtx* ctx, const int64_t n, const int64_t w, const T* matrix, T* sum_vec,
-                     const T* sum_multiplier);
-
   // matrix[i][j] -= vector[i]
   // matrix shape = n*w, vector shape = n
   static void Sub(DeviceCtx* ctx, const int64_t n, const int64_t w, T* matrix, const T* vector);
@@ -41,19 +32,19 @@ struct SoftmaxKernelUtil {
 
 template<DeviceType device_type, typename T>
 void SoftmaxComputeProb(DeviceCtx* ctx, const int64_t n, const int64_t w, const T* in, T* tmp,
-                        T* prob, const T* sum_multiplier) {
+                        T* prob, void* temp_storage, const size_t temp_storage_bytes) {
   // copy in blob to prob blob
   KernelUtil<device_type, T>::Copy(ctx, n * w, in, 1, prob, 1);
   // max | calculate max of every sample vector prob[i], store in tmp[i]
   //       the prob[i] now is store the data of in[i]
-  SoftmaxKernelUtil<device_type, T>::ForwardMax(ctx, n, w, prob, tmp);
+  KernelUtil<device_type, T>::RowMax(ctx, n, w, prob, tmp, temp_storage, temp_storage_bytes);
   // sub | every element of prob blob subract the max value of the same sample
   SoftmaxKernelUtil<device_type, T>::Sub(ctx, n, w, prob, tmp);
   // exp | exponentiation every element
   KernelUtil<device_type, T>::Exp(ctx, n * w, prob, prob);
   // sum | calculate sum of every sample vector prob[i], store in tmp[i]
   //       the prob[i] now is store the tmp data after exp
-  SoftmaxKernelUtil<device_type, T>::RowSum(ctx, n, w, prob, tmp, sum_multiplier);
+  KernelUtil<device_type, T>::RowSum(ctx, n, w, prob, tmp, temp_storage, temp_storage_bytes);
   // div | every element of prob[i] divided by the data of tmp[i] (the sum
   // value)
   SoftmaxKernelUtil<device_type, T>::Div(ctx, n, w, prob, tmp);
