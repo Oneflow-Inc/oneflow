3d5767b16c25412dcb246cefc3bbc5cd971dc606
Yi Zhu <619828575@qq.com>
2018-05-29 13:07:35 +0800

    fix: fix bug when elem in model < split num (#911)
    * fix bug when elem cnt in model < split num
    
    * add checker
    
    
    Former-commit-id: c6ac67d538bec94843c761ca07cf351ddb175576

--- oneflow/core/kernel/reduce_gather_kernel.cpp
+++ oneflow/core/kernel/reduce_gather_kernel.cpp
@@ -8,11 +8,13 @@ void ReduceGatherKernel::ForwardDataContent(
   char* dst_cur_dptr = out_blob->mut_dptr<char>();
   for (const std::string& ibn : this->op_attribute().input_bns()) {
     Blob* in_blob = BnInOp2Blob(ibn);
+    if (!in_blob) { break; }
     size_t in_byte_size = in_blob->ByteSizeOfDataContentField();
     AutoMemcpy(ctx.device_ctx, dst_cur_dptr, in_blob->dptr<char>(), in_byte_size,
                in_blob->mem_case(), out_blob->mem_case());
     dst_cur_dptr += in_byte_size;
   }
+  CHECK_EQ(dst_cur_dptr - out_blob->mut_dptr<char>(), out_blob->ByteSizeOfDataContentField());
 }
 
 REGISTER_KERNEL(OperatorConf::kReduceGatherConf, ReduceGatherKernel);
