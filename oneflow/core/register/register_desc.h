0728d877eddcad4b16be81587087694d1ea9fab6
zhangwenxiao <258127960@qq.com>
2018-06-14 16:17:01 +0800

    disable record load sharing

--- oneflow/core/register/register_desc.h
+++ oneflow/core/register/register_desc.h
@@ -46,7 +46,6 @@ class RegstDesc final {
   const MemoryCase& mem_case() const { return mem_case_; }
   MemoryCase* mut_mem_case() { return &mem_case_; }
   void set_enable_mem_sharing(bool enable_mem_sharing) { enable_mem_sharing_ = enable_mem_sharing; }
-  bool enable_mem_sharing() { return enable_mem_sharing_; }
 
   RegstDescTypeProto* mut_regst_desc_type() { return &regst_desc_type_; }
 
