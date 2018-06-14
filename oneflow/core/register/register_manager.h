41e1cf8c9ad337a3d840266cc2a129122da2862f
xumingkai <13703635183@163.com>
2018-06-14 18:53:37 +0800

    shard and unshared registers

--- oneflow/core/register/register_manager.h
+++ oneflow/core/register/register_manager.h
@@ -25,6 +25,8 @@ class RegstMgr final {
   explicit RegstMgr(const Plan& plan);
   explicit RegstMgr(const std::list<const RegstDescProto*>& regst_protos);
   void InitFromRegstProtoList(const std::list<const RegstDescProto*>& regst_protos);
+  void InitFromUnsharedRegstProtoList(const std::list<const RegstDescProto*>& regst_protos);
+  void InitFromSharedRegstProtoList(const std::list<const RegstDescProto*>& regst_protos);
 
   HashMap<int64_t, std::unique_ptr<const RtRegstDesc>> regst_desc_id2rt_regst_desc_;
   HashMap<int64_t, char*> regst_desc_id2mem_ptr_;
