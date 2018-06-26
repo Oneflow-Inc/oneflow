f9a2f1da2b1c969939831656a11a1d8ba0c91bb6
jiyuan <yuan.ms2@gmail.com>
2018-06-26 08:44:59 +0800

    finish record loader op

--- oneflow/core/operator/record_loader_op.cpp
+++ oneflow/core/operator/record_loader_op.cpp
@@ -2,9 +2,16 @@
 
 namespace oneflow {
 
-void RecordLoaderOp::InitFromOpConf() {}
+void RecordLoaderOp::InitFromOpConf() {
+  CHECK(op_conf().has_record_loader_conf());
+  EnrollOutputBn("out");
+}
 
 void RecordLoaderOp::InferBlobDescs(std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
-                                    const ParallelContext* parallel_ctx) const {}
+                                    const ParallelContext* parallel_ctx) const {
+  BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
+  const RecordLoaderOpConf& conf = op_conf().record_loader_conf();
+  out_blob_desc->mut_shape() = Shape({conf.piece_size_in_each_loader()});
+}
 
 }  // namespace oneflow
