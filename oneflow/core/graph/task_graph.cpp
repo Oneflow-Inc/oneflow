0f97226f2684f2d2f41a9b34505a29b7978ae440
Li Xinqi <lixinqi2010@gmail.com>
2018-06-29 22:16:39 +0800

    Dev dfs topo sorted by dinstance to sink (#980)
    * dfs topo sorted by distance to sink
    
    * extract DfsTopoForEachNode from DfsTopoForEachNodeSortByDistanceToSink
    
    * refine DfsTopoForEachNodeSortByDistanceToSink
    
    
    Former-commit-id: 81d74e6b3db6728e175a13d0d2f73e4624e277db

--- oneflow/core/graph/task_graph.cpp
+++ oneflow/core/graph/task_graph.cpp
@@ -126,7 +126,7 @@ void TaskGraph::AcyclicTopoForEachNode(std::function<void(TaskNode* node)> handl
       handler(const_cast<TaskNode*>(node_on_out_edge));
     });
   };
-  TopoForEachNode(starts, ForEachInNode, ForEachOutNode, handler);
+  DfsTopoForEachNodeSortByDistanceToSink(starts, ForEachInNode, ForEachOutNode, handler);
 }
 
 void TaskGraph::SetAreaIdForNewNodes(const LogicalNode* src_logical,
