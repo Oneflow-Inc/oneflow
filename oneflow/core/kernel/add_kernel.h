2bc41677721a315066ea088511b3fc0b34966afe
cheng cheng <472491134@qq.com>
2018-07-02 16:54:33 +0800

    activation after add op, remove cwise kernel

--- oneflow/core/kernel/add_kernel.h
+++ oneflow/core/kernel/add_kernel.h
@@ -1,12 +1,14 @@
 #ifndef ONEFLOW_CORE_KERNEL_ADD_KERNEL_H_
 #define ONEFLOW_CORE_KERNEL_ADD_KERNEL_H_
 
-#include "oneflow/core/kernel/cwise_kernel.h"
+#include "oneflow/core/kernel/kernel.h"
 
 namespace oneflow {
 
 template<DeviceType device_type, typename T>
-class AddKernel final : public CWiseKernel<device_type> {
+class AddKernel final
+    : public std::conditional<IsFloating<T>::value, KernelIfWithActivation<device_type, T>,
+                              KernelIf<device_type>>::type {
  public:
   OF_DISALLOW_COPY_AND_MOVE(AddKernel);
   AddKernel() = default;
