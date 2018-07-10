faf69401b610ff5bffc529d7a32fb4b5c4ee7164
Xinqi <lixinqi0703106@163.com>
2018-07-10 12:54:41 +0800

    kCudaMemAllocAlignSize

--- oneflow/core/common/util.h
+++ oneflow/core/common/util.h
@@ -179,6 +179,7 @@ inline double GetCurTime() {
 }
 
 const size_t kCudaAlignSize = 8;
+const size_t kCudaMemAllocAlignSize = 256;
 inline size_t RoundUp(size_t n, size_t align) { return (n + align - 1) / align * align; }
 
 size_t GetAvailableCpuMemSize();
