6d9663c7b324450219894fb8496fcfdbb7d2e12a
Niu Chong <niuchong_dnm@163.com>
2018-07-13 23:30:24 +0800

    feat: refactor reduce struct to control network order, avoiding network contention (#994)
    * feat: avoid net contention by adding ctrl edge in ReduceStruct
    
    * refine(task_graph.h/cpp): refine AddCtrlEdgeInReduceStruct()
    
    * fix(graph/task_graph.cpp): fix the bug of machine order
    
    * fix(graph/task_graph.cpp): do not add ctrl edge with reduce scatter
    
    * feat: add ReduceGlobalAddCompActor
    
    * fix: fix the bug of reduce_global_actor/kernel
    
    * chore: remove used vim .swp file
    
    * fix(graph/task_graph.cpp): fix the bug of sorting copycomment when build reduce ctrl edge
    
    * fix(graph/task_graph.h/cpp): add CtrlEdge for ReduceGather
    
    * revert: remove the ReduceGlobalAddCompActor from this PR
    
    * feat: add use_ordered_allreduce_in_mdupdt in OtherConf

--- oneflow/core/job/compiler.cpp
+++ oneflow/core/job/compiler.cpp
@@ -60,6 +60,9 @@ Plan Compiler::DoCompile() {
   task_gph->ForEachNode(std::bind(&TaskNode::EraseEmptyProducedRegst, _1));
   task_gph->ForEachNode(std::bind(&TaskNode::ClearOutOfDateConsumedRegst, _1));
   task_gph->AddOrderingCtrlEdgeInSameChain();
+  if (Global<JobDesc>::Get()->other_conf().use_ordered_allreduce_in_mdupdt()) {
+    task_gph->AddCtrlEdgeInReduceStruct();
+  }
   Plan plan;
   task_gph->ForEachNode([&](TaskNode* task_node) {
     if (task_node->IsMeaningLess()) { return; }
