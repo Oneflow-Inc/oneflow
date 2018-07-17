6d7b58294d02c1b202159895f391e32a6636e7a1
jackal <jackalcooper@gmail.com>
2018-07-17 17:05:46 +0800

    InferBlobDescs

--- oneflow/core/operator/roi_pooling_op.cpp
+++ oneflow/core/operator/roi_pooling_op.cpp
@@ -13,10 +13,24 @@ const PbMessage& RoIPoolingOp::GetCustomizedConf() const { return op_conf().roi_
 
 void RoIPoolingOp::InferBlobDescs(std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
                                   const ParallelContext* parallel_ctx) const {
-  TODO();  // check shape of in (feat. map)
-  TODO();  // check shape of rois
-  TODO();  // set shape of out, [batch num, roi num, channel num, pooled_h, pooled_w, c]
-  TODO();  // set shape of argmax
+  // in
+  const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
+  CHECK_EQ(in_blob_desc->shape().NumAxes(), 4);
+  // rois
+  const BlobDesc* rois_blob_desc = GetBlobDesc4BnInOp("rois");
+  CHECK_EQ(rois_blob_desc->shape().NumAxes(), 3);
+  CHECK_EQ(rois_blob_desc->shape().At(0), in_blob_desc->shape().At(0));
+  CHECK_EQ(rois_blob_desc->shape().At(2), 4);
+  // out
+  BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
+  out_blob_desc->mut_shape() = Shape(
+      {in_blob_desc->shape().At(0), rois_blob_desc->shape().At(1), in_blob_desc->shape().At(1),
+       op_conf().roi_pooling_conf().pooled_h(), op_conf().roi_pooling_conf().pooled_w()});
+  out_blob_desc->set_data_type(in_blob_desc->data_type());
+  // argmax
+  BlobDesc* argmax_blob_desc = GetBlobDesc4BnInOp("argmax");
+  argmax_blob_desc->mut_shape() = out_blob_desc->shape();
+  argmax_blob_desc->set_data_type(DataType::kInt32);
 }
 
 REGISTER_OP(OperatorConf::kRoiPoolingConf, RoIPoolingOp);
