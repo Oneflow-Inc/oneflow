8e933f1bbdd993f983080f323bdbb115b1341df4
jackal <jackalcooper@gmail.com>
2018-07-17 16:05:04 +0800

    add interface and todo

---
+++ oneflow/core/operator/roi_pooling_op.cpp
@@ -0,0 +1,24 @@
+#include "oneflow/core/operator/roi_pooling_op.h"
+
+namespace oneflow {
+
+void RoIPoolingOp::InitFromOpConf() {
+  EnrollInputBn("in");
+  EnrollInputBn("rois");
+  EnrollOutputBn("out");
+  EnrollDataTmpBn("argmax");
+}
+
+const PbMessage& RoIPoolingOp::GetCustomizedConf() const { return op_conf().roi_pooling_conf(); }
+
+void RoIPoolingOp::InferBlobDescs(std::function<BlobDesc*(const std::string)> GetBlobDesc4BnInOp,
+                                  const ParallelContext* parallel_ctx) const {
+  // TODO check shape of in (feat. map)
+  // TODO check shape of rois
+  // TODO set shape of out
+  // TODO set shape of argmax
+}
+
+REGISTER_OP(OperatorConf::kRoiPoolingConf, RoIPoolingOp);
+
+}  // namespace oneflow
