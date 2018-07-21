2a2661c9e5cdc6b7d14b8866d4dafa0f949a3b47
ScXfjiang <sc.xfjiang@gmail.com>
2018-07-21 11:04:09 +0800

    naive version of smooth_l1_loss

--- oneflow/core/kernel/smooth_l1_loss_kernel.cpp
+++ oneflow/core/kernel/smooth_l1_loss_kernel.cpp
@@ -1,19 +1,81 @@
 #include "oneflow/core/kernel/smooth_l1_loss_kernel.h"
-#include "oneflow/core/kernel/sparse_cross_entropy_loss_kernel.h"
-#include "oneflow/core/kernel/softmax_kernel.h"
+#include "oneflow/core/register/blob_desc.h"
 
 namespace oneflow {
 
 template<DeviceType device_type, typename PredType, typename LabelType>
-void SmoothL1LossKernel<device_type, PredType, LabelType>::
-    VirtualLossForwardDataContent(const KernelCtx& ctx,
-                                  std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  TODO();
+void SmoothL1LossKernel<device_type, PredType, LabelType>::VirtualLossForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* prediction = BnInOp2Blob("prediction");
+  const Blob* label = BnInOp2Blob("label");
+  const Blob* inside_weights = BnInOp2Blob("inside_weights");
+  const Blob* outside_weights = BnInOp2Blob("outside_weights");
+  Blob* const_all_one = BnInOp2Blob("const_all_one");
+  Blob* loss_buf = BnInOp2Blob("loss_buf");
+  Blob* loss = BnInOp2Blob("loss");
+  Blob* pred_diff_blob = BnInOp2Blob(GenDiffBn("prediction"));
+  auto kernel_conf = this->kernel_conf();
+  const float beta = kernel_conf.op_attribute().op_conf().smooth_l1_loss_conf().beta();
+  const float scale = kernel_conf.op_attribute().op_conf().smooth_l1_loss_conf().scale();
+  int64_t N = BnInOp2Blob("prediction")->shape().At(0);
+  int64_t D = BnInOp2Blob("prediction")->shape().Count(1);
+
+  if (const_all_one != nullptr) {
+    Memset<device_type>(ctx.device_ctx, const_all_one->mut_dptr<PredType>(), 1,
+                        const_all_one->TotalByteSize());
+  }
+
+  SmoothL1LossKernelUtil<device_type, PredType, LabelType>::Forward(
+      ctx.device_ctx, N, D, prediction->dptr<PredType>(), label->dptr<LabelType>(),
+      inside_weights->dptr<int8_t>(), outside_weights->dptr<int8_t>(),
+      const_all_one->dptr<PredType>(), beta, scale, loss_buf->mut_dptr<PredType>(),
+      loss->mut_dptr<PredType>());
+
+  SmoothL1LossKernelUtil<device_type, PredType, LabelType>::Backward(
+      ctx.device_ctx, N, D, prediction->dptr<PredType>(), label->dptr<LabelType>(),
+      inside_weights->dptr<int8_t>(), outside_weights->dptr<int8_t>(), beta, scale,
+      pred_diff_blob->mut_dptr<PredType>());
 }
 
+template<typename PredType, typename LabelType>
+struct SmoothL1LossKernelUtil<DeviceType::kCPU, PredType, LabelType> {
+  static void Forward(DeviceCtx* ctx, const int64_t N, const int64_t D, const PredType* prediction,
+                      const LabelType* label, const int8_t* inside_weights,
+                      const int8_t* outside_weights, const PredType* const_all_one,
+                      const float beta, const float scale, PredType* loss_buf, PredType* loss) {
+    int64_t elem_cnt = N * D;
+    for (int i = 0; i < elem_cnt; i++) {
+      PredType x = inside_weights[i] * (prediction[i] - label[i]);
+      PredType abs_x = abs(x);
+      if (abs_x < beta) {
+        loss_buf[i] = 0.5 * x * x / beta;
+      } else {
+        loss_buf[i] = abs_x - 0.5 * beta;
+      }
+      loss_buf[i] *= scale / elem_cnt * outside_weights[i];
+    }
+    KernelUtil<DeviceType::kCPU, PredType>::Dot(ctx, N * D, loss_buf, 1, const_all_one, 1, loss);
+  }
+  static void Backward(DeviceCtx* ctx, const int64_t N, const int64_t D, const PredType* prediction,
+                       const LabelType* label, const int8_t* inside_weights,
+                       const int8_t* outside_weights, const float beta, const float scale,
+                       PredType* in_diff) {
+    int64_t elem_cnt = N * D;
+    for (int i = 0; i < elem_cnt; i++) {
+      PredType x = inside_weights[i] * (prediction[i] - label[i]);
+      PredType abs_x = abs(x);
+      if (abs_x < beta) {
+        in_diff[i] = x / beta;
+      } else {
+        in_diff[i] = x > 0 ? 1 : -1;
+      }
+      in_diff[i] *= scale / elem_cnt * outside_weights[i];
+    }
+  }
+};
+
 template<DeviceType device_type, typename PredType, typename LabelType>
-const LossKernelConf&
-SmoothL1LossKernel<device_type, PredType, LabelType>::GetLossKernelConf(
+const LossKernelConf& SmoothL1LossKernel<device_type, PredType, LabelType>::GetLossKernelConf(
     const KernelConf& kernel_conf) const {
   return kernel_conf.smooth_l1_loss_conf().loss_conf();
 }
@@ -22,19 +84,17 @@ namespace {
 
 Kernel* CreateSmoothL1LossKernel(const KernelConf& kernel_conf) {
   static const HashMap<std::string, std::function<Kernel*()>> creators = {
-#define SMOOTH_L1_LOSS_KERNEL_ENTRY(device_type, pred_type_pair,                \
-                                                       label_type_pair)                            \
+#define SMOOTH_L1_LOSS_KERNEL_ENTRY(device_type, pred_type_pair, label_type_pair)                  \
   {GetHashKey(device_type, OF_PP_PAIR_SECOND(pred_type_pair), OF_PP_PAIR_SECOND(label_type_pair)), \
    []() {                                                                                          \
-     return new SmoothL1LossKernel<device_type, OF_PP_PAIR_FIRST(pred_type_pair), \
-                                                    OF_PP_PAIR_FIRST(label_type_pair)>();          \
+     return new SmoothL1LossKernel<device_type, OF_PP_PAIR_FIRST(pred_type_pair),                  \
+                                   OF_PP_PAIR_FIRST(label_type_pair)>();                           \
    }},
-      OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(SMOOTH_L1_LOSS_KERNEL_ENTRY,
-                                       DEVICE_TYPE_SEQ, FLOATING_DATA_TYPE_SEQ, INT_DATA_TYPE_SEQ)};
-  return creators.at(
-      GetHashKey(kernel_conf.op_attribute().op_conf().device_type(),
-                 kernel_conf.smooth_l1_loss_conf().loss_conf().prediction_type(),
-                 kernel_conf.smooth_l1_loss_conf().loss_conf().label_type()))();
+      OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(SMOOTH_L1_LOSS_KERNEL_ENTRY, DEVICE_TYPE_SEQ,
+                                       FLOATING_DATA_TYPE_SEQ, INT_DATA_TYPE_SEQ)};
+  return creators.at(GetHashKey(kernel_conf.op_attribute().op_conf().device_type(),
+                                kernel_conf.smooth_l1_loss_conf().loss_conf().prediction_type(),
+                                kernel_conf.smooth_l1_loss_conf().loss_conf().label_type()))();
 }
 
 }  // namespace
--- oneflow/core/kernel/smooth_l1_loss_kernel.cu
+++ oneflow/core/kernel/smooth_l1_loss_kernel.cu
@@ -5,43 +5,66 @@ namespace oneflow {
 namespace {
 
 template<typename PredType, typename LabelType>
-__global__ void SmoothL1LossForward(const int64_t N, const int64_t D,
-                                                          const PredType* predict,
-                                                          const LabelType* target,
-                                                          const int64_t* inside_weights,
-                                                          const int64_t* outside_weights,
-                                                          PredType* loss) {
-  TODO();
+__global__ void SmoothL1LossForward(const int64_t N, const int64_t D, const PredType* prediction,
+                                    const LabelType* label, const int8_t* inside_weights,
+                                    const int8_t* outside_weights, const float beta,
+                                    const float scale, PredType* loss_buf) {
+  int64_t elem_cnt = N * D;
+  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
+    PredType x = inside_weights[i] * (prediction[i] - label[i]);
+    PredType abs_x = abs(x);
+    if (abs_x < beta) {
+      loss_buf[i] = 0.5 * x * x / beta;
+    } else {
+      loss_buf[i] = abs_x - 0.5 * beta;
+    }
+    loss_buf[i] *= scale / elem_cnt * outside_weights[i];
+  }
 }
 
 template<typename PredType, typename LabelType>
-__global__ void SmoothL1LossBackward(const int64_t N, const int64_t D,
-                                                         const PredType* predict,
-                                                         const LabelType* target,
-                                                         const int64_t* inside_weights,
-                                                         const int64_t* outside_weights,
-                                                         PredType* in_diff) {
-  TODO();
+__global__ void SmoothL1LossBackward(const int64_t N, const int64_t D, const PredType* prediction,
+                                     const LabelType* label, const int8_t* inside_weights,
+                                     const int8_t* outside_weights, const float beta,
+                                     const float scale, PredType* in_diff) {
+  int64_t elem_cnt = N * D;
+  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
+    PredType x = inside_weights[i] * (prediction[i] - label[i]);
+    PredType abs_x = abs(x);
+    if (abs_x < beta) {
+      in_diff[i] = x / beta;
+    } else {
+      in_diff[i] = x > 0 ? 1 : -1;
+    }
+    in_diff[i] *= scale / elem_cnt * outside_weights[i];
+  }
 }
 
 }  // namespace
 
 template<typename PredType, typename LabelType>
 struct SmoothL1LossKernelUtil<DeviceType::kGPU, PredType, LabelType> {
-  static void Forward(DeviceCtx* ctx, const int64_t N, const int64_t D, const PredType* predict, const LabelType* target, const int64_t* inside_weights, const int64_t* outside_weights,
-    PredType* loss) {
+  static void Forward(DeviceCtx* ctx, const int64_t N, const int64_t D, const PredType* prediction,
+                      const LabelType* label, const int8_t* inside_weights,
+                      const int8_t* outside_weights, const PredType* const_all_one,
+                      const float beta, const float scale, PredType* loss_buf, PredType* loss) {
     SmoothL1LossForward<PredType>
-      <<<BlocksNum4ThreadsNum(n), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(N, D, predict, target, loss);
+        <<<BlocksNum4ThreadsNum(N * D), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            N, D, prediction, label, inside_weights, outside_weights, beta, scale, loss_buf);
+    KernelUtil<DeviceType::kGPU, PredType>::Dot(ctx, N * D, loss_buf, 1, const_all_one, 1, loss);
   }
-  static void Backward(DeviceCtx* ctx, const int64_t n, const int64_t w, const PredType* predict ,const LabelType* target, const int64_t* inside_weights, const int64_t* outside_weights,
-                          PredType* in_diff) {
+  static void Backward(DeviceCtx* ctx, const int64_t N, const int64_t D, const PredType* prediction,
+                       const LabelType* label, const int8_t* inside_weights,
+                       const int8_t* outside_weights, const float beta, const float scale,
+                       PredType* in_diff) {
     SmoothL1LossBackward<PredType>
-    <<<BlocksNum4ThreadsNum(n), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(N, D, predict, target, in_diff);
+        <<<BlocksNum4ThreadsNum(N * D), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            N, D, prediction, label, inside_weights, outside_weights, beta, scale, in_diff);
   }
 };
 
-#define MAKE_ENTRY(data_type_pair, label_type_pair)        \
-  template struct SmoothL1LossKernelUtil< \
-      DeviceType::kGPU, OF_PP_PAIR_FIRST(data_type_pair), OF_PP_PAIR_FIRST(label_type_pair)>;
+#define MAKE_ENTRY(data_type_pair, label_type_pair)                                          \
+  template struct SmoothL1LossKernelUtil<DeviceType::kGPU, OF_PP_PAIR_FIRST(data_type_pair), \
+                                         OF_PP_PAIR_FIRST(label_type_pair)>;
 OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(MAKE_ENTRY, FLOATING_DATA_TYPE_SEQ, INT_DATA_TYPE_SEQ)
 }  // namespace oneflow
