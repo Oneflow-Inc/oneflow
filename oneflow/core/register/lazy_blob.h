f1a24c8cb002ae895140be7cfb78dbaebf5eeef5
Xinqi <lixinqi0703106@163.com>
2018-07-22 22:14:28 +0800

    profile lazy evaluation

--- oneflow/core/register/lazy_blob.h
+++ oneflow/core/register/lazy_blob.h
@@ -12,6 +12,14 @@ namespace oneflow {
   for (LazyBlobVarBuilder<T> var(std::make_unique<LazyBlobGraph>()); var.Touch() == 0; \
        var.graph().Evaluate())
 
+#if defined(_MSC_VER)
+#define ALWAYS_INLINE __forceinline inline
+#elif defined(__GNUC__) || defined(__GNUG__) || defined(__clang__)
+#define ALWAYS_INLINE __attribute__((always_inline)) inline
+#else
+#define ALWAYS_INLINE inline
+#endif
+
 class Slice final {
  public:
   Slice(int64_t x);
@@ -81,6 +89,7 @@ class LazyBlobGraph final : public Graph<LazyBlobNode, LazyBlobEdge> {
 template<typename DerivedT>
 class LazyBlobIf : public LazyBlobNode {
  public:
+  const static bool is_element_wise = false;
   OF_DISALLOW_COPY_AND_MOVE(LazyBlobIf);
   virtual ~LazyBlobIf() = default;
 
@@ -129,6 +138,7 @@ template<typename T>
 class VarLazyBlob final : public LazyBlobIf<VarLazyBlob<T>> {
  public:
   typedef T dtype;
+  const static bool is_element_wise = true;
 
   OF_DISALLOW_COPY_AND_MOVE(VarLazyBlob);
   VarLazyBlob(LazyBlobGraph* graph, Blob* backend_blob)
@@ -159,19 +169,19 @@ class VarLazyBlob final : public LazyBlobIf<VarLazyBlob<T>> {
     value_lazy_blob_node_->Evaluate(this->mut_backend_blob());
   }
 
-  inline dtype operator()(int64_t dim0) const { return dptr_[dim0]; }
-  inline dtype operator()(int64_t dim0, int64_t dim1) const {
+  ALWAYS_INLINE dtype At(int64_t dim0) const { return dptr_[dim0]; }
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1) const {
     return dptr_[dim0 * dim0_next_dim_count_ + dim1];
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2) const {
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2) const {
     return dptr_[dim0 * dim0_next_dim_count_ + dim1 * dim1_next_dim_count_ + dim2];
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
     return dptr_[dim0 * dim0_next_dim_count_ + dim1 * dim1_next_dim_count_
                  + dim2 * dim2_next_dim_count_ + dim3];
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
-                          int64_t dim4) const {
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
+                         int64_t dim4) const {
     return dptr_[dim0 * dim0_next_dim_count_ + dim1 * dim1_next_dim_count_
                  + dim2 * dim2_next_dim_count_ + dim3 * dim3_next_dim_count_ + dim4];
   }
@@ -195,6 +205,7 @@ class UnaryExpresionLazyBlob final : public LazyBlobIf<UnaryExpresionLazyBlob<Co
  public:
   using T = typename XT::dtype;
   typedef decltype(CoreFunc<T>::Invoke(*(const T*)nullptr)) dtype;
+  const static bool is_element_wise = true;
 
   OF_DISALLOW_COPY_AND_MOVE(UnaryExpresionLazyBlob);
   explicit UnaryExpresionLazyBlob(const XT& x)
@@ -202,32 +213,40 @@ class UnaryExpresionLazyBlob final : public LazyBlobIf<UnaryExpresionLazyBlob<Co
                                                          GetDataType<dtype>::value),
         x_(x) {}
 
-  inline dtype operator()(int64_t dim0) const { return CoreFunc<T>::Invoke(x_(dim0)); }
-  inline dtype operator()(int64_t dim0, int64_t dim1) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1));
+  ALWAYS_INLINE dtype At(int64_t dim0) const { return CoreFunc<T>::Invoke(x_.At(dim0)); }
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1));
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1, dim2));
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1, dim2));
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1, dim2, dim3));
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1, dim2, dim3));
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
-                          int64_t dim4) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1, dim2, dim3, dim4));
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
+                         int64_t dim4) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1, dim2, dim3, dim4));
+  }
+
+  virtual typename std::enable_if<XT::is_element_wise>::type Evaluate(
+      Blob* output_blob) const override {
+    int64_t elem_cnt = this->shape().elem_cnt();
+    T* dptr = output_blob->mut_dptr<T>();
+    FOR_RANGE(int64_t, i, 0, elem_cnt) { dptr[i] = CoreFunc<T>::Invoke(x_.At(i)); }
   }
 
  private:
   const XT& x_;
 };
 
-template<template<typename> class CoreFunc, typename XT, typename YT = XT,
+template<template<typename> class CoreFunc, typename XT, typename YT,
          typename = typename std::enable_if<
              std::is_same<typename XT::dtype, typename YT::dtype>::value>::type>
 class BinaryExpresionLazyBlob final : public LazyBlobIf<BinaryExpresionLazyBlob<CoreFunc, XT, YT>> {
  public:
   using T = typename XT::dtype;
   typedef decltype(CoreFunc<T>::Invoke(*(const T*)nullptr, *(const T*)nullptr)) dtype;
+  const static bool is_element_wise = true;
 
   OF_DISALLOW_COPY_AND_MOVE(BinaryExpresionLazyBlob);
   BinaryExpresionLazyBlob(const XT& x, const YT& y)
@@ -238,20 +257,35 @@ class BinaryExpresionLazyBlob final : public LazyBlobIf<BinaryExpresionLazyBlob<
     CHECK(x.shape() == y.shape());
   }
 
-  inline dtype operator()(int64_t dim0) const { return CoreFunc<T>::Invoke(x_(dim0), y_(dim0)); }
-  inline dtype operator()(int64_t dim0, int64_t dim1) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1), y_(dim0, dim1));
+  static ALWAYS_INLINE typename XT::dtype CoreInvoke(const XT& x, int64_t i) { return x.At(i); }
+
+  ALWAYS_INLINE dtype At(int64_t dim0) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0), y_.At(dim0));
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1, dim2), y_(dim0, dim1, dim2));
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1), y_.At(dim0, dim1));
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1, dim2, dim3), y_(dim0, dim1, dim2, dim3));
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1, dim2), y_.At(dim0, dim1, dim2));
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
-                          int64_t dim4) const {
-    return CoreFunc<T>::Invoke(x_(dim0, dim1, dim2, dim3, dim4), y_(dim0, dim1, dim2, dim3, dim4));
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1, dim2, dim3), y_.At(dim0, dim1, dim2, dim3));
   }
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
+                         int64_t dim4) const {
+    return CoreFunc<T>::Invoke(x_.At(dim0, dim1, dim2, dim3, dim4),
+                               y_.At(dim0, dim1, dim2, dim3, dim4));
+  }
+
+  virtual typename std::enable_if<XT::is_element_wise && YT::is_element_wise>::type Evaluate(
+      Blob* output_blob) const override {
+    int64_t elem_cnt = this->shape().elem_cnt();
+    T* dptr = output_blob->mut_dptr<T>();
+    FOR_RANGE(int64_t, i, 0, elem_cnt) { dptr[i] = CoreFunc<T>::Invoke(x_.At(i), y_.At(i)); }
+  }
+
+  ALWAYS_INLINE const XT& x() { return x_; }
+  ALWAYS_INLINE const YT& y() { return y_; }
 
  private:
   const XT& x_;
@@ -276,20 +310,20 @@ class BroadcastLazyBlob final : public LazyBlobIf<BroadcastLazyBlob<XT>> {
     CheckShape(x.shape(), shape);
   }
 
-  inline dtype operator()(int64_t dim0) const { return x_(dim0 % dim0_size_); }
-  inline dtype operator()(int64_t dim0, int64_t dim1) const {
-    return x_(dim0 % dim0_size_, dim1 % dim1_size_);
+  ALWAYS_INLINE dtype At(int64_t dim0) const { return x_.At(dim0 % dim0_size_); }
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1) const {
+    return x_.At(dim0 % dim0_size_, dim1 % dim1_size_);
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2) const {
-    return x_(dim0 % dim0_size_, dim1 % dim1_size_, dim2 % dim2_size_);
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2) const {
+    return x_.At(dim0 % dim0_size_, dim1 % dim1_size_, dim2 % dim2_size_);
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
-    return x_(dim0 % dim0_size_, dim1 % dim1_size_, dim2 % dim2_size_, dim3 % dim3_size_);
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3) const {
+    return x_.At(dim0 % dim0_size_, dim1 % dim1_size_, dim2 % dim2_size_, dim3 % dim3_size_);
   }
-  inline dtype operator()(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
-                          int64_t dim4) const {
-    return x_(dim0 % dim0_size_, dim1 % dim1_size_, dim2 % dim2_size_, dim3 % dim3_size_,
-              dim4 % dim4_size_);
+  ALWAYS_INLINE dtype At(int64_t dim0, int64_t dim1, int64_t dim2, int64_t dim3,
+                         int64_t dim4) const {
+    return x_.At(dim0 % dim0_size_, dim1 % dim1_size_, dim2 % dim2_size_, dim3 % dim3_size_,
+                 dim4 % dim4_size_);
   }
 
  private:
@@ -352,23 +386,30 @@ class LazyBlobVarBuilder final {
   OF_PP_MAKE_TUPLE_SEQ(LogicalAnd, &&, bool) \
   OF_PP_MAKE_TUPLE_SEQ(LogicalOr, &&, bool)
 
-#define DECLARE_LAZY_BLOB_BINARY_CORE(name, op, ret_type)                  \
-  template<typename T>                                                     \
-  struct LazyBlobCore##name final {                                        \
-    static inline ret_type Invoke(const T x, const T y) { return x op y; } \
+#define DECLARE_LAZY_BLOB_BINARY_CORE(name, op, ret_type)                         \
+  template<typename T>                                                            \
+  struct LazyBlobCore##name final {                                               \
+    static ALWAYS_INLINE ret_type Invoke(const T x, const T y) { return x op y; } \
   };
 OF_PP_FOR_EACH_TUPLE(DECLARE_LAZY_BLOB_BINARY_CORE, LAZY_BLOB_BINARY_CORE_OP_FUNC_SEQ);
+#undef DECLARE_LAZY_BLOB_BINARY_CORE
 
 #define LAZY_BLOB_UNARY_CORE_OP_FUNC_SEQ \
   OF_PP_MAKE_TUPLE_SEQ(Negative, -, T)   \
   OF_PP_MAKE_TUPLE_SEQ(LogicalNot, !, bool)
 
-#define DECLARE_LAZY_BLOB_UNARY_CORE(name, op, ret_type)      \
-  template<typename T>                                        \
-  struct LazyBlobCore##name final {                           \
-    static inline ret_type Invoke(const T x) { return op x; } \
+template<typename T>
+ALWAYS_INLINE T Add(const T x, const T y) {
+  return x + y;
+}
+
+#define DECLARE_LAZY_BLOB_UNARY_CORE(name, op, ret_type)             \
+  template<typename T>                                               \
+  struct LazyBlobCore##name final {                                  \
+    static ALWAYS_INLINE ret_type Invoke(const T x) { return op x; } \
   };
 OF_PP_FOR_EACH_TUPLE(DECLARE_LAZY_BLOB_UNARY_CORE, LAZY_BLOB_UNARY_CORE_OP_FUNC_SEQ);
+#undef DECLARE_LAZY_BLOB_UNARY_CORE
 
 template<template<typename> class LazyBlobCoreFunc, typename XT, typename YT = XT>
 typename std::enable_if<std::is_base_of<LazyBlobNode, XT>::value
@@ -384,7 +425,7 @@ BuildBinaryLazyBlob(XT& x, YT& y) {
   Connect(dynamic_cast<LazyBlobNode*>(&x), graph->NewEdge(), base_ret);
   Connect(dynamic_cast<LazyBlobNode*>(&y), graph->NewEdge(), base_ret);
   if (x.backend_blob() == nullptr) { CHECK_EQ(x.out_edges().size(), 1); }
-  if (x.backend_blob() == nullptr) { CHECK_EQ(y.out_edges().size(), 1); }
+  if (y.backend_blob() == nullptr) { CHECK_EQ(y.out_edges().size(), 1); }
   return *ret;
 }
 
@@ -399,6 +440,7 @@ BuildBinaryLazyBlob(XT& x, YT& y) {
     return BuildBinaryLazyBlob<LazyBlobCore##name, XT, YT>(x, y);                     \
   }
 OF_PP_FOR_EACH_TUPLE(OVERLOAD_BINARY_LAZY_BLOB_OP_FUNC, LAZY_BLOB_BINARY_CORE_OP_FUNC_SEQ);
+#undef OVERLOAD_BINARY_LAZY_BLOB_OP_FUNC
 
 template<template<typename> class LazyBlobCoreFunc, typename XT>
 typename std::enable_if<std::is_base_of<LazyBlobNode, XT>::value,
@@ -421,6 +463,7 @@ BuildUnaryLazyBlob(XT& x) {
     return BuildUnaryLazyBlob<LazyBlobCore##name, XT>(x);                             \
   }
 OF_PP_FOR_EACH_TUPLE(OVERLOAD_UNARY_LAZY_BLOB_OP_FUNC, LAZY_BLOB_UNARY_CORE_OP_FUNC_SEQ);
+#undef OVERLOAD_UNARY_LAZY_BLOB_OP_FUNC
 
 //  implementations
 
@@ -431,7 +474,7 @@ void LazyBlobIf<DerivedT>::Evaluate1(Blob* output_blob) const {
   DT* dptr = output_blob->mut_dptr<DT>();
   const DerivedT* this_ptr = dynamic_cast<const DerivedT*>(this);
   int64_t dim0_size = shape().At(0);
-  FOR_RANGE(int64_t, i, 0, dim0_size) { dptr[i] = (*this_ptr)(i); }
+  FOR_RANGE(int64_t, i, 0, dim0_size) { dptr[i] = this_ptr->At(i); }
 }
 template<typename DerivedT>
 void LazyBlobIf<DerivedT>::Evaluate2(Blob* output_blob) const {
@@ -444,7 +487,7 @@ void LazyBlobIf<DerivedT>::Evaluate2(Blob* output_blob) const {
   int64_t dim0_next_dim_count = shape().Count(1);
   FOR_RANGE(int64_t, i, 0, dim0_size) {
     DT* dptr_i = dptr + i * dim0_next_dim_count;
-    FOR_RANGE(int64_t, j, 0, dim1_size) { dptr_i[j] = (*this_ptr)(i, j); }
+    FOR_RANGE(int64_t, j, 0, dim1_size) { dptr_i[j] = this_ptr->At(i, j); }
   }
 }
 template<typename DerivedT>
@@ -462,7 +505,7 @@ void LazyBlobIf<DerivedT>::Evaluate3(Blob* output_blob) const {
     DT* dptr_i = dptr + i * dim0_next_dim_count;
     FOR_RANGE(int64_t, j, 0, dim1_size) {
       DT* dptr_j = dptr_i + j * dim1_next_dim_count;
-      FOR_RANGE(int64_t, k, 0, dim2_size) { dptr_j[k] = (*this_ptr)(i, j, k); }
+      FOR_RANGE(int64_t, k, 0, dim2_size) { dptr_j[k] = this_ptr->At(i, j, k); }
     }
   }
 }
@@ -485,7 +528,7 @@ void LazyBlobIf<DerivedT>::Evaluate4(Blob* output_blob) const {
       DT* dptr_j = dptr_i + j * dim1_next_dim_count;
       FOR_RANGE(int64_t, k, 0, dim2_size) {
         DT* dptr_k = dptr_j + k * dim2_next_dim_count;
-        FOR_RANGE(int64_t, s, 0, dim3_size) { dptr_k[s] = (*this_ptr)(i, j, k, s); }
+        FOR_RANGE(int64_t, s, 0, dim3_size) { dptr_k[s] = this_ptr->At(i, j, k, s); }
       }
     }
   }
@@ -513,7 +556,7 @@ void LazyBlobIf<DerivedT>::Evaluate5(Blob* output_blob) const {
         DT* dptr_k = dptr_j + k * dim2_next_dim_count;
         FOR_RANGE(int64_t, s, 0, dim3_size) {
           DT* dptr_s = dptr_k + s * dim3_next_dim_count;
-          FOR_RANGE(int64_t, t, 0, dim4_size) { dptr_s[t] = (*this_ptr)(i, j, k, s, t); }
+          FOR_RANGE(int64_t, t, 0, dim4_size) { dptr_s[t] = this_ptr->At(i, j, k, s, t); }
         }
       }
     }
