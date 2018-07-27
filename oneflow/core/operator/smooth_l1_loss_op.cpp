f448a42d78e0ba440a82a32c65aec76d3b3944ad
ScXfjiang <sc.xfjiang@gmail.com>
2018-07-27 16:06:26 +0800

    refine smooth l1 loss

--- oneflow/core/operator/smooth_l1_loss_op.cpp
+++ oneflow/core/operator/smooth_l1_loss_op.cpp
@@ -5,8 +5,6 @@ namespace oneflow {
 void SmoothL1LossOp::VirtualInitFromOpConf() {
   EnrollInputBn("inside_weights");
   EnrollInputBn("outside_weights");
-  EnrollDataTmpBn("loss_buf");
-  EnrollConstBufBn("const_all_one");
 }
 
 const PbMessage& SmoothL1LossOp::GetCustomizedConf() const {
@@ -30,17 +28,10 @@ void SmoothL1LossOp::VirtualInferBlobDescs(
   CHECK_EQ(pred_blob_desc->shape(), outside_weights_blob_desc->shape());
   CHECK_GE(pred_blob_desc->shape().NumAxes(), 2);
 
-  BlobDesc* loss_buf = GetBlobDesc4BnInOp("loss_buf");
-  loss_buf->mut_shape() = Shape(pred_blob_desc->shape());
-  loss_buf->set_data_type(pred_blob_desc->data_type());
-
-  BlobDesc* const_all_one = GetBlobDesc4BnInOp("const_all_one");
-  const_all_one->mut_shape() = Shape(pred_blob_desc->shape());
-  const_all_one->set_data_type(DataType::kInt8);
-
   BlobDesc* loss_blob_desc = GetBlobDesc4BnInOp("loss");
-  loss_blob_desc->mut_shape() = Shape({1});
+  loss_blob_desc->mut_shape() = Shape({pred_blob_desc->shape()});
   loss_blob_desc->set_data_type(pred_blob_desc->data_type());
+  loss_blob_desc->set_has_data_id_field(pred_blob_desc->has_data_id_field());
 
   BlobDesc* prediction_diff_blob_desc = GetBlobDesc4BnInOp("prediction_diff");
   prediction_diff_blob_desc->mut_shape() = Shape(pred_blob_desc->shape());
