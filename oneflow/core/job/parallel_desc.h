874dff3e3f2c847a629500352256d48c6d27a57e
Jinhui Yuan <yuan.ms2@gmail.com>
2018-07-27 11:26:25 +0800

    Add safe check (#1055)
    * remove staleness
    
    * ensure balanced placement for data and model parallel
    
    * add device_num_of_each_machine

--- oneflow/core/job/parallel_desc.h
+++ oneflow/core/job/parallel_desc.h
@@ -24,6 +24,7 @@ class ParallelDesc {
     return machine_id2sorted_dev_phy_ids_.at(machine_id);
   }
   int64_t parallel_num() const { return parallel_num_; }
+  int64_t device_num_of_each_machine() const { return device_num_of_each_machine_; }
 
   // Setters
   void set_policy(ParallelPolicy val) { policy_ = val; }
@@ -38,12 +39,14 @@ class ParallelDesc {
 
  private:
   void ClearUp();
+  void CheckValidity();
 
   DeviceType device_type_;
   ParallelPolicy policy_;
   std::vector<int64_t> sorted_machine_ids_;
   HashMap<int64_t, std::vector<int64_t>> machine_id2sorted_dev_phy_ids_;
   int64_t parallel_num_;
+  int64_t device_num_of_each_machine_;
 };
 
 std::tuple<int32_t, int32_t> GetPartIdAndPartNumFromParallelCtx(
