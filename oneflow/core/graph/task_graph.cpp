0b92af2e57cb2c9b5446f099cb457efcc7986933
jiyuan <yuan.ms2@gmail.com>
2018-07-28 13:41:13 +0800

    use BfsTopo instead. DfsTopo causes issues in ReduceStruct

--- oneflow/core/graph/task_graph.cpp
+++ oneflow/core/graph/task_graph.cpp
@@ -274,7 +274,8 @@ void TaskGraph::AcyclicTopoForEachNode(std::function<void(TaskNode* node)> handl
       handler(const_cast<TaskNode*>(node_on_out_edge));
     });
   };
-  DfsTopoForEachNodeSortByDistanceToSink(starts, ForEachInNode, ForEachOutNode, handler);
+  // DfsTopoForEachNodeSortByDistanceToSink(starts, ForEachInNode, ForEachOutNode, handler);
+  TopoForEachNode(starts, ForEachInNode, ForEachOutNode, handler);
 }
 
 void TaskGraph::SetAreaIdForNewNodes(const LogicalNode* src_logical,
