b84da061d84baf2635f6c051817e40b96b91edca
Dounm <niuchong_dnm@163.com>
2018-08-01 20:00:02 +0800

    fix(reduce_scatter_compute_task_node): fix the bug of set produced out regst name

--- oneflow/core/graph/reduce_scatter_compute_task_node.cpp
+++ oneflow/core/graph/reduce_scatter_compute_task_node.cpp
@@ -5,17 +5,21 @@ namespace oneflow {
 
 void ReduceScatterCompTaskNode::ProduceAllRegstsAndBindEdges() {
   int64_t machine_num = logical_node()->parallel_desc()->sorted_machine_ids().size();
-  int64_t i = 0;
+  int64_t dev_num_of_each_machine = logical_node()->parallel_desc()->device_num_of_each_machine();
+  std::vector<int64_t> edge_index4dst_dev(dev_num_of_each_machine, 0);
   for (TaskEdge* edge : out_edges()) {
     std::vector<CompTaskNode*> comp_task_nodes = GetSuccCompTaskNodesOnEdge(edge);
     CHECK_EQ(comp_task_nodes.size(), 1);
-    int64_t dst_dev_index_of_this_machine = i / machine_num;
-    int64_t edge_index_of_this_dst_dev = i % machine_num;
+    int64_t parallel_id = comp_task_nodes.front()->parallel_ctx()->parallel_id();
+    int64_t dst_dev_index_of_this_machine = parallel_id % dev_num_of_each_machine;
+    int64_t edge_index_of_this_dst_dev = edge_index4dst_dev.at(dst_dev_index_of_this_machine);
+    edge_index4dst_dev.at(dst_dev_index_of_this_machine) += 1;
+    CHECK_LT(edge_index4dst_dev.at(dst_dev_index_of_this_machine), machine_num);
+
     std::string out_regst_name = "out_" + std::to_string(dst_dev_index_of_this_machine) + "_"
                                  + std::to_string(edge_index_of_this_dst_dev);
     std::shared_ptr<RegstDesc> out_regst = ProduceRegst(out_regst_name, false);
     edge->AddRegst(out_regst_name, out_regst);
-    i += 1;
   }
 }
 
