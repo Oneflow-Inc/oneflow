ed46b7c2eba038427c64e7aed126290b28a8beb8
chengtbf <472491134@qq.com>
2018-08-02 16:25:14 +0800

    refine activation and remove RemoveBackwardAdd (#1076)
    * half impl
    
    * refine activation
    
    * kernel if with activation
    
    * remove virtual of infer if

--- oneflow/core/graph/logical_graph.h
+++ oneflow/core/graph/logical_graph.h
@@ -49,10 +49,6 @@ class LogicalGraph final : public Graph<LogicalNode, LogicalEdge> {
   void NaiveBuildBwStruct();
   void AddBackwardClone();
   void AddOneBackwardClone(const BackwardCloneInfo& clone_info);
-  void MoveBackwardActivations();
-  void RemoveBackwardAdd();
-  void CollectBackwardB121CloneInfos(HashMap<LogicalNode*, LogicalNode*>* bw_add_node2pre_node);
-  void RemoveOneBackwardAdd(const std::pair<LogicalNode*, LogicalNode*>& bw_add_node_and_pre);
 
   void MergeEdge();
   void SetNodeDataLbi();
