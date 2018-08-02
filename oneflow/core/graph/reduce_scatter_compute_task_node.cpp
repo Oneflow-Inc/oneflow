82d622a10010811da3d15c121543bdaa7d852a40
Dounm <niuchong_dnm@163.com>
2018-08-02 12:13:04 +0800

    refactor(reduce_scatter_comp_task_node): rename the produced out regst

--- oneflow/core/graph/reduce_scatter_compute_task_node.cpp
+++ oneflow/core/graph/reduce_scatter_compute_task_node.cpp
@@ -6,18 +6,22 @@ namespace oneflow {
 void ReduceScatterCompTaskNode::ProduceAllRegstsAndBindEdges() {
   int64_t machine_num = logical_node()->parallel_desc()->sorted_machine_ids().size();
   int64_t dev_num_of_each_machine = logical_node()->parallel_desc()->device_num_of_each_machine();
+  CHECK_EQ(machine_num * dev_num_of_each_machine, parallel_ctx()->parallel_num());
+
   std::vector<int64_t> edge_index4dst_dev(dev_num_of_each_machine, 0);
   for (TaskEdge* edge : out_edges()) {
     std::vector<CompTaskNode*> comp_task_nodes = GetSuccCompTaskNodesOnEdge(edge);
     CHECK_EQ(comp_task_nodes.size(), 1);
+
     int64_t parallel_id = comp_task_nodes.front()->parallel_ctx()->parallel_id();
     int64_t dst_dev_index_of_this_machine = parallel_id % dev_num_of_each_machine;
     int64_t edge_index_of_this_dst_dev = edge_index4dst_dev.at(dst_dev_index_of_this_machine);
     edge_index4dst_dev.at(dst_dev_index_of_this_machine) += 1;
     CHECK_LT(edge_index4dst_dev.at(dst_dev_index_of_this_machine), machine_num);
 
-    std::string out_regst_name = "out_" + std::to_string(dst_dev_index_of_this_machine) + "_"
-                                 + std::to_string(edge_index_of_this_dst_dev);
+    int64_t out_edge_index =
+        edge_index_of_this_dst_dev * dev_num_of_each_machine + dst_dev_index_of_this_machine;
+    std::string out_regst_name = "out_" + std::to_string(out_edge_index);
     std::shared_ptr<RegstDesc> out_regst = ProduceRegst(out_regst_name, false);
     edge->AddRegst(out_regst_name, out_regst);
   }
@@ -38,19 +42,13 @@ void ReduceScatterCompTaskNode::BuildExecGphAndRegst() {
   node->mut_op() = reduce_scatter_op;
   node->BindBnWithRegst(reduce_scatter_op->SoleIbn(), GetSoleConsumedRegst("in"));
 
-  int64_t dev_num_of_each_machine = logical_node()->parallel_desc()->device_num_of_each_machine();
   FOR_RANGE(size_t, i, 0, reduce_scatter_op->output_bns().size()) {
-    int64_t dst_dev_index_of_this_machine = i % dev_num_of_each_machine;
-    int64_t edge_index_of_this_dst_dev = i / dev_num_of_each_machine;
-    std::shared_ptr<RegstDesc> out_regst =
-        GetProducedRegst("out_" + std::to_string(dst_dev_index_of_this_machine) + "_"
-                         + std::to_string(edge_index_of_this_dst_dev));
-    CHECK(out_regst.get());
+    std::shared_ptr<RegstDesc> out_regst = GetProducedRegst("out_" + std::to_string(i));
+    CHECK(out_regst.get() != nullptr);
     const std::string& obn = reduce_scatter_op->output_bns().Get(i);
     out_regst->AddLbi(reduce_scatter_op->BnInOp2Lbi(obn));
     node->BindBnWithRegst(obn, out_regst);
   }
-
   node->InferBlobDescs(parallel_ctx());
 }
 
