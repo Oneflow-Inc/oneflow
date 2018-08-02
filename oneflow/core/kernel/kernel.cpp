dc9952ad8ce140750732f716442b40fc6c691425
jiyuan <yuan.ms2@gmail.com>
2018-08-02 16:13:24 +0800

    make opaque header mutual exclusive

--- oneflow/core/kernel/kernel.cpp
+++ oneflow/core/kernel/kernel.cpp
@@ -50,9 +50,12 @@ void Kernel::Forward(const KernelCtx& ctx,
     Blob* out_blob = BnInOp2Blob(obns[0]);
     PostForwardActivation(ctx, out_blob);
   }
-  if (kernel_conf_.need_do_packed_header()) { ForwardPackedHeader(ctx, BnInOp2Blob); }
-  if (kernel_conf_.need_do_data_id()) { ForwardDataId(ctx, BnInOp2Blob); }
-  if (kernel_conf_.need_do_col_num()) { ForwardColNum(ctx, BnInOp2Blob); }
+  if (kernel_conf_.need_do_packed_header()) {
+    ForwardPackedHeader(ctx, BnInOp2Blob);
+  } else {
+    if (kernel_conf_.need_do_data_id()) { ForwardDataId(ctx, BnInOp2Blob); }
+    if (kernel_conf_.need_do_col_num()) { ForwardColNum(ctx, BnInOp2Blob); }
+  }
 }
 
 void Kernel::Backward(const KernelCtx& ctx,
