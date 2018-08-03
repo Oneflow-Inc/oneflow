204bd73a0ef2e25499105c7e4cf9b4f28c511fad
jiyuan <yuanjh03@gmail.com>
2018-08-03 09:38:14 +0800

    fix infer in_diff blob desc of add op

--- oneflow/core/operator/add_op.cpp
+++ oneflow/core/operator/add_op.cpp
@@ -3,13 +3,24 @@
 namespace oneflow {
 
 void AddOp::VirtualInitFromOpConf() { CHECK(op_conf().has_add_conf()); }
+/*
 void AddOp::VirtualInferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                                   const ParallelContext* parallel_ctx) const {
+
   int64_t mem_shared_id = std::stol(NewUniqueId());
+  // std::vector<std::string> idbns = input_diff_bns();
+
+  LOG(INFO) << "op_name:" << op_name();
+  // LOG(INFO) << "add_op:" << idbns.size();
   for (size_t i = 0; i < input_diff_bns().size(); ++i) {
-    GetBlobDesc4BnInOp(input_diff_bns().Get(i))->set_mem_shared_id(mem_shared_id);
+    std::string idbn = input_diff_bns().Get(i);
+    LOG(INFO) << idbn;
+    BlobDesc* in_diff = GetBlobDesc4BnInOp(idbn);
+    in_diff->set_mem_shared_id(mem_shared_id);
+    // GetBlobDesc4BnInOp(input_diff_bns().Get(i))->set_mem_shared_id(mem_shared_id);
   }
 }
+*/
 const PbMessage& AddOp::GetCustomizedConf() const { return op_conf().add_conf(); }
 
 REGISTER_OP(OperatorConf::kAddConf, AddOp);
