c527e9fa26b7d5b742abf2649d04b2c4effb5299
guo-ran <360112263@qq.com>
2018-08-09 10:38:29 +0800

    use scoreslice

--- oneflow/core/kernel/proposal_target_kernel.cpp
+++ oneflow/core/kernel/proposal_target_kernel.cpp
@@ -1,68 +1,14 @@
 #include "oneflow/core/kernel/proposal_target_kernel.h"
 #include "oneflow/core/kernel/kernel_util.h"
-#include "oneflow/core/kernel/faster_rcnn_util.h"
 
 namespace oneflow {
 
 namespace {
 
 template<typename T>
-void RoisNearestGtAndMaxIou(const int64_t rois_num, const int64_t gt_num, const T* rpn_rois_ptr,
-                            const T* gt_boxes_ptr, int32_t* roi_nearest_gt_index_ptr,
-                            T* roi_max_overlap_ptr) {
-  const BBox<T>* roi_box = BBox<T>::Cast(rpn_rois_ptr);
-  const BBox<T>* gt_box = BBox<T>::Cast(gt_boxes_ptr);
-  FOR_RANGE(int64_t, i, 0, rois_num) {
-    float maxIou = 0;
-    int64_t maxIouIdx = 0;
-    FOR_RANGE(int64_t, j, 0, gt_num) {
-      float iou = roi_box[j].InterOverUnion(&gt_box[j]);
-      if (iou > maxIou) {
-        maxIou = iou;
-        maxIouIdx = j;
-      }
-    }
-    roi_nearest_gt_index_ptr[i] = maxIouIdx;
-    roi_max_overlap_ptr[i] = maxIou;
-  }
-}
-
-template<typename T>
-ScoredBBoxSlice<T> ForegroundChoice(const ProposalTargetOpConf& conf, const ScoredBBoxSlice<T> rois_slice, int64_t& fg_sample_size) {
-  const int64_t fg_thresh = conf.fg_thresh();
-  int64_t fg_end_index = rois_slice.FindByThreshold(conf.fg_thresh());
-  ScoredBBoxSlice<T> fg_rois_slice = rois_slice.Slice(0, fg_end_index);
-  const int64_t num_roi_per_image = conf.num_roi_per_image();
-  const float fg_fraction = conf.fg_fraction();
-  fg_sample_size = std::min(fg_end_index + 1, (int64_t)std::round(num_roi_per_image * fg_fraction));
-  if(fg_sample_size < fg_end_index + 1){
-      fg_rois_slice.Shuffle();
-      fg_rois_slice.Truncate(fg_sample_size);
-  }
-  return fg_rois_slice;
-}
-
-template<typename T>
-ScoredBBoxSlice<T> BackgroundChoice(const ProposalTargetOpConf& conf, const ScoredBBoxSlice<T> rois_slice, const int64_t fg_sample_size) {
-  const int64_t bg_thresh_hi = conf.bg_thresh_hi();
-  const int64_t bg_thresh_lo = conf.bg_thresh_lo();
-  CHECK_GT(bg_thresh_hi, bg_thresh_lo);
-  int64_t bg_start_index = rois_slice.FindByThreshold(conf.bg_thresh_hi());
-  int64_t bg_end_index = rois_slice.FindByThreshold(conf.bg_thresh_lo());
-  ScoredBBoxSlice<T> bg_rois_slice = rois_slice.Slice(bg_start_index, bg_end_index);
-  const int64_t num_roi_per_image = conf.num_roi_per_image();
-  const int64_t bg_sample_size =
-      std::min(bg_end_index - bg_start_index + 1, num_roi_per_image - fg_sample_size);
-    if(bg_sample_size < bg_end_index - bg_start_index + 1){
-      bg_rois_slice.Shuffle();
-      bg_rois_slice.Truncate(bg_sample_size);
-    }
-  return bg_rois_slice;
-}
-
-template<typename T>
-void ComputeTargetAndWriteOut(const int32_t* roi_index_ptr, const int32_t* roi_nearest_gt_index_ptr,
-                              const T* rpn_rois_ptr, const T* gt_boxes_ptr,
+void ComputeTargetAndWriteOut(const ScoredBBoxSlice<T>& fg_slice,
+                              const ScoredBBoxSlice<T>& bg_slice,
+                              const int32_t* roi_nearest_gt_index_ptr, const T* gt_boxes_ptr,
                               const int32_t* gt_labels_ptr, T* rois_ptr, int32_t* labels_ptr,
                               T* bbox_targets_ptr, T* inside_weights_ptr, T* outside_weights_ptr) {
   TODO();
@@ -105,14 +51,74 @@ void ProposalTargetKernel<T>::ForwardDataContent(
 
     RoisNearestGtAndMaxIou(roi_num, gt_num, rpn_rois_ptr, gt_boxes_ptr, roi_nearest_gt_index_ptr,
                            roi_max_overlap_ptr);
-    ScoredBBoxSlice<T> rois_slice(roi_num, rpn_rois_ptr, roi_max_overlap_ptr, rois_index_ptr); 
+    ScoredBBoxSlice<T> rois_slice(roi_num, rpn_rois_ptr, roi_max_overlap_ptr, rois_index_ptr);
     rois_slice.DescSortByScore();
     int64_t fg_sample_size = 0;
-    ScoredBBoxSlice<T> fg_slice = ForegroundChoice(conf, rois_slice, fd_sample_size);
-    ScoredBBoxSlice<T> bg_slice = BackgroundChoice(conf, rois_slice, fd_sample_size);
-    ComputeTargetAndWriteOut(roi_nearest_gt_index_ptr, gt_boxes_ptr, gt_labels_ptr, rois_ptr, labels_ptr, bbox_targets_ptr,
+    ScoredBBoxSlice<T> fg_slice = ForegroundChoice(conf, rois_slice, fg_sample_size);
+    ScoredBBoxSlice<T> bg_slice = BackgroundChoice(conf, rois_slice, fg_sample_size);
+    ComputeTargetAndWriteOut(fg_slice, bg_slice, roi_nearest_gt_index_ptr, gt_boxes_ptr,
+                             gt_labels_ptr, rois_ptr, labels_ptr, bbox_targets_ptr,
                              inside_weights_ptr, outside_weights_ptr);
   }
+}
+
+template<typename T>
+void ProposalTargetKernel<T>::RoisNearestGtAndMaxIou(const int64_t rois_num, const int64_t gt_num,
+                                                     const T* rpn_rois_ptr, const T* gt_boxes_ptr,
+                                                     int32_t* roi_nearest_gt_index_ptr,
+                                                     T* roi_max_overlap_ptr) const {
+  const BBox<T>* roi_box = BBox<T>::Cast(rpn_rois_ptr);
+  const BBox<T>* gt_box = BBox<T>::Cast(gt_boxes_ptr);
+  FOR_RANGE(int64_t, i, 0, rois_num) {
+    float maxIou = 0;
+    int64_t maxIouIdx = 0;
+    FOR_RANGE(int64_t, j, 0, gt_num) {
+      float iou = roi_box[j].InterOverUnion(&gt_box[j]);
+      if (iou > maxIou) {
+        maxIou = iou;
+        maxIouIdx = j;
+      }
+    }
+    roi_nearest_gt_index_ptr[i] = maxIouIdx;
+    roi_max_overlap_ptr[i] = maxIou;
+  }
+}
+
+template<typename T>
+ScoredBBoxSlice<T> ProposalTargetKernel<T>::ForegroundChoice(const ProposalTargetOpConf& conf,
+                                                             ScoredBBoxSlice<T>& rois_slice,
+                                                             int64_t& fg_sample_size) const {
+  const float fg_thresh = conf.fg_thresh();
+  int64_t fg_end_index = rois_slice.FindByThreshold(fg_thresh);
+  ScoredBBoxSlice<T> fg_rois_slice = rois_slice.Slice(0, fg_end_index);
+  const int64_t num_roi_per_image = conf.num_roi_per_image();
+  const float fg_fraction = conf.fg_fraction();
+  fg_sample_size = std::min(fg_end_index + 1, (int64_t)std::round(num_roi_per_image * fg_fraction));
+  if (fg_sample_size < fg_end_index + 1) {
+    fg_rois_slice.Shuffle();
+    fg_rois_slice.Truncate(fg_sample_size);
+  }
+  return fg_rois_slice;
+}
+
+template<typename T>
+ScoredBBoxSlice<T> ProposalTargetKernel<T>::BackgroundChoice(const ProposalTargetOpConf& conf,
+                                                             ScoredBBoxSlice<T>& rois_slice,
+                                                             const int64_t fg_sample_size) const {
+  const int64_t bg_thresh_hi = conf.bg_thresh_hi();
+  const int64_t bg_thresh_lo = conf.bg_thresh_lo();
+  CHECK_GT(bg_thresh_hi, bg_thresh_lo);
+  int64_t bg_start_index = rois_slice.FindByThreshold(conf.bg_thresh_hi());
+  int64_t bg_end_index = rois_slice.FindByThreshold(conf.bg_thresh_lo());
+  ScoredBBoxSlice<T> bg_rois_slice = rois_slice.Slice(bg_start_index, bg_end_index);
+  const int64_t num_roi_per_image = conf.num_roi_per_image();
+  const int64_t bg_sample_size =
+      std::min(bg_end_index - bg_start_index + 1, num_roi_per_image - fg_sample_size);
+  if (bg_sample_size < bg_end_index - bg_start_index + 1) {
+    bg_rois_slice.Shuffle();
+    bg_rois_slice.Truncate(bg_sample_size);
+  }
+  return bg_rois_slice;
 }
 
 ADD_CPU_DEFAULT_KERNEL_CREATOR(OperatorConf::kProposalTargetConf, ProposalTargetKernel,
--- oneflow/core/kernel/proposal_target_kernel.h
+++ oneflow/core/kernel/proposal_target_kernel.h
@@ -2,6 +2,7 @@
 #define ONEFLOW_CORE_KERNEL_PROPOSAL_TARGET_KERNEL_H_
 
 #include "oneflow/core/kernel/kernel.h"
+#include "oneflow/core/kernel/faster_rcnn_util.h"
 
 namespace oneflow {
 template<typename T>
@@ -14,6 +15,15 @@ class ProposalTargetKernel final : public KernelIf<DeviceType::kCPU> {
  private:
   void ForwardDataContent(const KernelCtx&,
                           std::function<Blob*(const std::string&)>) const override;
+  void RoisNearestGtAndMaxIou(const int64_t rois_num, const int64_t gt_num, const T* rpn_rois_ptr,
+                              const T* gt_boxes_ptr, int32_t* roi_nearest_gt_index_ptr,
+                              T* roi_max_overlap_ptr) const;
+  ScoredBBoxSlice<T> ForegroundChoice(const ProposalTargetOpConf& conf,
+                                      ScoredBBoxSlice<T>& rois_slice,
+                                      int64_t& fg_sample_size) const;
+  ScoredBBoxSlice<T> BackgroundChoice(const ProposalTargetOpConf& conf,
+                                      ScoredBBoxSlice<T>& rois_slice,
+                                      const int64_t fg_sample_size) const;
 };
 
 }  // namespace oneflow
