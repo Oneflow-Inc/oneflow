7a8b61b68b67d0f7e410759bfd4c43d0e3f42b50
Dounm <niuchong_dnm@163.com>
2018-08-10 18:10:40 +0800

    feat: update reduce_local_add actor to support inplace kernel

--- oneflow/core/actor/reduce_local_add_compute_actor.cpp
+++ oneflow/core/actor/reduce_local_add_compute_actor.cpp
@@ -12,15 +12,27 @@ void ReduceLocalAddCompActor::VirtualCompActorInit(const TaskProto& proto) {
                         .op_conf()
                         .reduce_local_add_conf()
                         .out_num();
+  int64_t parallel_num = parallel_ctx()->parallel_num();
+  int64_t parallel_id = parallel_ctx()->parallel_id();
+  int64_t dev_num_of_each_machine = parallel_num / out_num;
+
   out_blob_init_status_.resize(out_num, false);
+  int64_t inplace_in_blob_id = parallel_id % dev_num_of_each_machine;
+  for (int64_t i = 0; i < out_num; ++i) {
+    inplace_blob_ids_.emplace(inplace_in_blob_id);
+    inplace_in_blob_id += dev_num_of_each_machine;
+  }
+  CHECK_LT(inplace_in_blob_id, parallel_num);
 }
 
 void ReduceLocalAddCompActor::SetKernelCtxOther(void** other) {
-  int64_t in_bn_id = InBnId4RegstDescId(cur_processed_regst_desc_id());
   int64_t dev_num_of_each_machine = parallel_ctx()->parallel_num() / out_blob_init_status_.size();
+  int64_t in_bn_id = InBnId4RegstDescId(cur_processed_regst_desc_id());
   cur_out_blob_id_ = in_bn_id / dev_num_of_each_machine;
-  other_val_ =
-      std::make_tuple(in_bn_id, cur_out_blob_id_, out_blob_init_status_.at(cur_out_blob_id_));
+  bool is_inited = out_blob_init_status_.at(cur_out_blob_id_);
+  bool is_inplace_in_bn_id = inplace_blob_ids_.find(in_bn_id) != inplace_blob_ids_.end();
+
+  other_val_ = std::make_tuple(in_bn_id, cur_out_blob_id_, is_inited, is_inplace_in_bn_id);
   *other = static_cast<void*>(&other_val_);
 }
 
