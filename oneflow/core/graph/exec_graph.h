64ecc59e3e444df2c52e93d7b76e7f4ee018a40a
Niu Chong <niuchong_dnm@163.com>
2018-08-12 20:38:14 +0800

    feat: enable mem sharing in ReduceStruct (#1097)
    * feat(register_desc): add mem_shared_offset
    
    * feat(regst_desc): set the default val of mem_shared_offset as -1
    
    * feat(register_manager): add offset in register_manager
    
    * feat: add EnableMemSharingInReduceStruct(), set the mem_shared_id/offset of reduce regst
    
    * feat(task_graph): AddCtrlEdge4MemSharingInOneReduce()
    
    * feat: set mem_shared of regst produced by reduce/copy task nodes as true
    
    * refactor(copy_task_node): regsts produced by CopyH2D and consumed by reduce_task are able to share mem, others stays unable
    
    * fix(copy_task_node): not use SoleOutEdge() for copy_task_node
    
    * fix(task_graph): fix the compile bug
    
    * feat: set min/max regst_num of regst produced by reduce/copy task_node as 1
    
    * feat: support mem_shared_offset in improver
    
    * fix(copy_task_node): set max_regst_num of copy node succeed reduce nodes as 1
    
    * refactor(task_graph): remove FindSuccReduceTaskNode() from member function, just as lambda function
    
    * refactor(task_graph): refine EnableMemSharingInOneReduce()
    
    * fix(reduce_scatter_task_node): fix the bug of wrong out_regst_name when parallel_num==machine_num
    
    * refine: refine due to comment
    
    
    Former-commit-id: 25ddb8339b64ba30913158312bd7287856f78431

--- oneflow/core/graph/exec_graph.h
+++ oneflow/core/graph/exec_graph.h
@@ -42,6 +42,7 @@ class ExecNode final : public Node<ExecNode, ExecEdge> {
 
   std::shared_ptr<const Operator> op() const { return op_; }
   std::shared_ptr<const Operator>& mut_op() { return op_; }
+  RegstDesc* RegstDesc4BnInOp(const std::string& bn) { return bn_in_op2regst_.at(bn).lock().get(); }
 
   void BindBnWithRegst(const std::string& bn, std::weak_ptr<RegstDesc>);
   void BindBnsWithRegst(const PbRpf<std::string>& (Operator::*bns_getter)() const,
