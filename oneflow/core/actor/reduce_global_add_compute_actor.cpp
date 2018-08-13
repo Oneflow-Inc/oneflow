ee46733114467be63bcfc5f0a5234442740d2989
Niu Chong <niuchong_dnm@163.com>
2018-08-13 21:08:49 +0800

    feat: update reduce kernel to support inplace compute (#1105)
    * feat(reduce_scatter_kernel): reduce scatter kernel is as before for cpu, and do nothing for gpu
    
    * feat(reduce_local_add_kernel): update reduce_local_add kernel for in-place on GPU
    
    * feat: update reduce_local_add actor to support inplace kernel
    
    * feat: add support for inplace in reduce_global_add/gather
    
    * fix: use ibn.substr(3) to specify inplace in_blob other than in_bn_id
    
    
    Former-commit-id: e15fea94be152f1006b7203e6e1d918356fa4c3d

--- oneflow/core/actor/reduce_global_add_compute_actor.cpp
+++ oneflow/core/actor/reduce_global_add_compute_actor.cpp
@@ -2,9 +2,19 @@
 
 namespace oneflow {
 
+void ReduceGlobalAddCompActor::VirtualCompActorInit(const TaskProto& proto) {
+  InputWiseCompActor::Init(proto);
+  for (const auto& pair : exec_kernel_vec().at(0).bn_in_op2regst_desc_id) {
+    CHECK(regst_desc_id2bn_in_op_.emplace(pair.second, pair.first).second);
+  }
+}
+
 void ReduceGlobalAddCompActor::SetKernelCtxOther(void** other) {
-  other_val_.first = InBnId4RegstDescId(cur_processed_regst_desc_id());
-  other_val_.second = processed_regst_desc_id_cnt() == 0;
+  const std::string& ibn = regst_desc_id2bn_in_op_.at(cur_processed_regst_desc_id());
+  bool is_inited = processed_regst_desc_id_cnt() != 0;
+  bool is_inplace_in_blob = oneflow_cast<int64_t>(ibn.substr(3)) == parallel_ctx()->parallel_id();
+
+  other_val_ = std::make_tuple(ibn, is_inited, is_inplace_in_blob);
   *other = static_cast<void*>(&other_val_);
 }
 
