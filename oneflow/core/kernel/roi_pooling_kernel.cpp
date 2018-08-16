8a414164d5874e0ef68f419a79399a3e22edf409
jackalcooper <jackalcooper@gmail.com>
2018-08-16 11:56:47 +0800

    Roi Resize super class (#1065)
    * add roi align files
    
    * naive version of RoI Align
    
    * fix roi align op
    
    * remote +1 in roi size
    
    * faster_rcnn_util
    
    * bugfix: base_anchors shape should be (ratio_size, scale_size)
    
    * code style fix
    
    * refine
    
    * bug fix
      score index
    
    * format
    
    * add roi resize superclass
    
    * add virtual for deconstruct
    
    * explicit template rather than putting it in .h
    
    * fix merge

--- oneflow/core/kernel/roi_pooling_kernel.cpp
+++ oneflow/core/kernel/roi_pooling_kernel.cpp
@@ -31,36 +31,6 @@ void RoIPoolingKernel<device_type, T>::BackwardDataContent(
       ctx, this->op_conf().roi_pooling_conf(), out_diff_blob, rois_blob, argmax_blob, in_diff_blob);
 }
 
-template<DeviceType device_type, typename T>
-void RoIPoolingKernel<device_type, T>::ForwardDataId(
-    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* in_blob = BnInOp2Blob("in");
-  const Blob* rois_blob = BnInOp2Blob("rois");
-  Blob* out_blob = BnInOp2Blob("out");
-  int32_t roi_num = rois_blob->shape().At(1);
-  size_t size_of_one_data_id = Global<JobDesc>::Get()->SizeOfOneDataId();
-  FOR_RANGE(int64_t, n, 0, in_blob->shape().At(0)) {
-    int32_t n_roi_num = n * roi_num;
-    FOR_RANGE(int64_t, r, 0, roi_num) {
-      Memcpy<device_type>(ctx.device_ctx, out_blob->mut_data_id(n_roi_num + r), in_blob->data_id(n),
-                          size_of_one_data_id);
-    }
-  }
-}
-
-template<DeviceType device_type, typename T>
-void RoIPoolingKernel<device_type, T>::ForwardColNum(
-    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* in_blob = BnInOp2Blob("in");
-  const Blob* rois_blob = BnInOp2Blob("rois");
-  Blob* out_blob = BnInOp2Blob("out");
-  int32_t roi_num = rois_blob->shape().At(1);
-  FOR_RANGE(int64_t, n, 0, in_blob->shape().At(0)) {
-    int32_t col_num = in_blob->col_num(n);
-    FOR_RANGE(int64_t, r, 0, roi_num) { out_blob->set_col_num(n * roi_num + r, col_num); }
-  }
-}
-
 template<typename T>
 class RoIPoolingKernelUtil<DeviceType::kCPU, T> final {
  public:
