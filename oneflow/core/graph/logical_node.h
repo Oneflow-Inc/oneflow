be213666bb6253a7d183fa23470780b5129eab15
Li Xinqi <lixinqi2010@gmail.com>
2018-08-19 19:44:00 +0800

    Dev trainable false (#1132)
    * backpropogate model_diff only if is trainable
    
    * bugfix: consume bw task node only if trainable
    
    * bugfix: connect md_updt and bw_node when bw_node is not null
    
    * bugfix: md_updt enter HandlerNormal only if there is model to train
    
    * set all op trainable = false when predicting

--- oneflow/core/graph/logical_node.h
+++ oneflow/core/graph/logical_node.h
@@ -131,8 +131,6 @@ class ForwardLogicalNode : public LogicalNode {
 
   BackwardLogicalNode* NewBackwardNode();
 
-  void SetBwNode(BackwardLogicalNode* bw_node) { bw_node_ = bw_node; }
-
  protected:
   virtual BackwardLogicalNode* NewCorrectBackwardNode() = 0;
 
