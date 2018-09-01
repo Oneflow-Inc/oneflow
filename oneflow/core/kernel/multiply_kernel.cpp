5410e8d392534808c5a4df9f9e02eddf5ef6b646
clackhan <hanbinbin17@mails.ucas.ac.cn>
2018-09-01 16:00:28 +0800

    fix multiply kernel code style

--- oneflow/core/kernel/multiply_kernel.cpp
+++ oneflow/core/kernel/multiply_kernel.cpp
@@ -6,29 +6,31 @@ namespace oneflow {
 template<DeviceType device_type, typename T>
 void MultiplyKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* in_blob = BnInOp2Blob("in");
-  const Blob* weight_blob = BnInOp2Blob("weight");
+  const Blob* in_0_blob = BnInOp2Blob("in_0");
+  const Blob* in_1_blob = BnInOp2Blob("in_1");
   Blob* out_blob = BnInOp2Blob("out");
-  // out = in .* weight
-  KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
-                                  weight_blob->dptr<T>(), out_blob->mut_dptr<T>());
+  // out = in_0 .* in_1
+  KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_0_blob->shape().elem_cnt(),
+                                  in_0_blob->dptr<T>(), in_1_blob->dptr<T>(),
+                                  out_blob->mut_dptr<T>());
 }
 
 template<DeviceType device_type, typename T>
 void MultiplyKernel<device_type, T>::BackwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* in_blob = BnInOp2Blob("in");
-  Blob* in_diff_blob = BnInOp2Blob("in_diff");
   const Blob* out_diff_blob = BnInOp2Blob("out_diff");
-  const Blob* weight_blob = BnInOp2Blob("weight");
-  Blob* weight_diff_blob = BnInOp2Blob("weight_diff");
-  // weight_diff = out_diff * in
-  KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
-                                  out_diff_blob->dptr<T>(), weight_diff_blob->mut_dptr<T>());
-  // in_diff = out_diff * weight
-  KernelUtil<device_type, T>::Mul(ctx.device_ctx, weight_blob->shape().elem_cnt(),
-                                  weight_blob->dptr<T>(), out_diff_blob->dptr<T>(),
-                                  in_diff_blob->mut_dptr<T>());
+  const Blob* in_0_blob = BnInOp2Blob("in_0");
+  Blob* in_0_diff_blob = BnInOp2Blob("in_0_diff");
+  const Blob* in_1_blob = BnInOp2Blob("in_1");
+  Blob* in_1_diff_blob = BnInOp2Blob("in_1_diff");
+  // in_1_diff = out_diff * in_0
+  KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_0_blob->shape().elem_cnt(),
+                                  in_0_blob->dptr<T>(), out_diff_blob->dptr<T>(),
+                                  in_1_diff_blob->mut_dptr<T>());
+  // in_0_diff = out_diff * in_1
+  KernelUtil<device_type, T>::Mul(ctx.device_ctx, in_1_blob->shape().elem_cnt(),
+                                  in_1_blob->dptr<T>(), out_diff_blob->dptr<T>(),
+                                  in_0_diff_blob->mut_dptr<T>());
 }
 
 template<DeviceType device_type, typename T>
--- oneflow/core/operator/multiply_op.cpp
+++ oneflow/core/operator/multiply_op.cpp
@@ -4,8 +4,8 @@ namespace oneflow {
 
 void MultiplyOp::InitFromOpConf() {
   CHECK(op_conf().has_multiply_conf());
-  EnrollInputBn("in");
-  EnrollInputBn("weight");
+  EnrollInputBn("in_0");
+  EnrollInputBn("in_1");
   EnrollOutputBn("out");
 }
 
@@ -13,13 +13,13 @@ const PbMessage& MultiplyOp::GetCustomizedConf() const { return op_conf().multip
 
 void MultiplyOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                                 const ParallelContext* parallel_ctx) const {
-  BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
-  BlobDesc* weight_blob_desc = GetBlobDesc4BnInOp("weight");
-  CHECK_EQ(in_blob_desc->data_type(), Global<JobDesc>::Get()->DefaultDataType());
-  CHECK_EQ(in_blob_desc->shape(), weight_blob_desc->shape());
+  BlobDesc* in_0_blob_desc = GetBlobDesc4BnInOp("in_0");
+  BlobDesc* in_1_blob_desc = GetBlobDesc4BnInOp("in_1");
+  CHECK_EQ(in_0_blob_desc->data_type(), Global<JobDesc>::Get()->DefaultDataType());
+  CHECK_EQ(in_0_blob_desc->shape(), in_1_blob_desc->shape());
   // out
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
-  *out_blob_desc = *in_blob_desc;
+  *out_blob_desc = *in_0_blob_desc;
 }
 REGISTER_OP(OperatorConf::kMultiplyConf, MultiplyOp);
 
