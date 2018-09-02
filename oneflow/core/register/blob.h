ae7a0820a663f0b86c508826d802e7ad9984925f
Xinqi <lixinqi0703106@163.com>
2018-09-02 16:40:46 +0800

    no blob coping gdb function

--- oneflow/core/register/blob.h
+++ oneflow/core/register/blob.h
@@ -75,7 +75,6 @@ class Blob final {
   void set_max_col_id(int32_t val);
   bool IsColValid() const { return col_id() <= max_col_id(); }
   const MemoryCase& mem_case() const;
-  const Regst* regst() const { return regst_; }
 
  private:
   template<typename T>
