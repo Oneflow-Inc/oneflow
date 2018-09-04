8100cf843d0d73e7e6473e5d0f9746dc3d8d72ca
binbinHan <han_binbin@163.com>
2018-09-04 15:14:16 +0800

    Dev matmul dot multiply (#1189)
    * add matmul & dot & multiply
    
    * optimize dot kernel
    
    * fix multiply kernel code style
    
    * optimize matmul kernel
    
    
    Former-commit-id: 6ab4006f20dee5bfc2b00b2ca50cfcefeedc6a04

---
+++ oneflow/core/kernel/multiply_kernel.h
@@ -0,0 +1,24 @@
+#ifndef ONEFLOW_CORE_KERNEL_MULTIPLY_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_MULTIPLY_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+class MultiplyKernel final : public KernelIfWithModel<device_type, T> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(MultiplyKernel);
+  MultiplyKernel() = default;
+  ~MultiplyKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx&,
+                          std::function<Blob*(const std::string&)>) const override;
+  void BackwardDataContent(const KernelCtx&,
+                           std::function<Blob*(const std::string&)>) const override;
+  const PbMessage& GetCustomizedOpConf() const override;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOE_CORE_KERNEL_MULTIPLY_KERNEL_H_
