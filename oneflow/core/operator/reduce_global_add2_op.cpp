4c7eb30b48c89d60767544c19d765fc3016cba7c
jiyuan <yuan.ms2@gmail.com>
2018-09-06 11:27:08 +0800

    complete ReduceGlobalAdd2 op

--- oneflow/core/operator/reduce_global_add2_op.cpp
+++ oneflow/core/operator/reduce_global_add2_op.cpp
@@ -3,13 +3,11 @@
 namespace oneflow {
 
 void ReduceGlobalAdd2Op::InitFromOpConf() {
-  /*
   CHECK(op_conf().has_reduce_global_add2_conf());
-  for (int64_t parallel_id : op_conf().reduce_global_add2_conf().in_parallel_ids()) {
-    EnrollInputBn("in_" + std::to_string(parallel_id), false);
+  FOR_RANGE(int32_t, i, 0, op_conf().reduce_global_add2_conf().in_num()) {
+    EnrollInputBn("in_" + std::to_string(i), false);
   }
   EnrollOutputBn("out", false);
-  */
 }
 
 const PbMessage& ReduceGlobalAdd2Op::GetCustomizedConf() const {
@@ -26,15 +24,13 @@ LogicalBlobId ReduceGlobalAdd2Op::obn2lbi(const std::string& output_bn) const {
 void ReduceGlobalAdd2Op::InferBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
-  /*
-  int32_t in_num = op_conf().reduce_global_add2_conf().in_parallel_ids_size();
+  int32_t in_num = op_conf().reduce_global_add2_conf().in_num();
   CHECK_GE(in_num, 2);
   BlobDesc* first_in_blob = GetBlobDesc4BnInOp(input_bns().Get(0));
   *GetBlobDesc4BnInOp(SoleObn()) = *first_in_blob;
   for (int32_t i = 1; i < in_num; ++i) {
     CHECK(*first_in_blob == *GetBlobDesc4BnInOp(input_bns().Get(i)));
   }
-  */
 }
 
 REGISTER_OP(OperatorConf::kReduceGlobalAdd2Conf, ReduceGlobalAdd2Op);
