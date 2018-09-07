ba0d7fc2e7d06819e5bc30a75a7537cd82d97c27
jiyuan <yuan.ms2@gmail.com>
2018-09-07 09:06:41 +0800

    fix reduce_global_add actor bug

--- oneflow/core/actor/reduce_global_add_compute_actor.cpp
+++ oneflow/core/actor/reduce_global_add_compute_actor.cpp
@@ -8,12 +8,13 @@ void ReduceGlobalAddCompActor::VirtualCompActorInit(const TaskProto& proto) {
 
 void ReduceGlobalAddCompActor::SetKernelCtxOther(void** other) {
   int64_t in_bn_id = InBnId4RegstDescId(cur_processed_regst_desc_id());
+  int64_t machine_num =
+      parallel_ctx()->parallel_num() / parallel_ctx()->device_num_of_each_machine();
   bool do_local_reduce_scatter =
-      parallel_ctx()->parallel_num() / parallel_ctx()->device_num_of_each_machine() > 1
-      && parallel_ctx()->device_num_of_each_machine() > 1;
+      machine_num > 1 && parallel_ctx()->device_num_of_each_machine() > 1;
   int64_t machine_id_if_do_local_reduce =
       do_local_reduce_scatter
-          ? (parallel_ctx()->parallel_num() / parallel_ctx()->device_num_of_each_machine())
+          ? (parallel_ctx()->parallel_id() / parallel_ctx()->device_num_of_each_machine())
           : parallel_ctx()->parallel_id();
   bool is_inited = EnableInplace() ? true : processed_regst_desc_id_cnt() != 0;
   bool is_inplace_in_blob = EnableInplace() ? (in_bn_id == machine_id_if_do_local_reduce) : false;
--- oneflow/core/graph/task_node.cpp
+++ oneflow/core/graph/task_node.cpp
@@ -366,7 +366,7 @@ std::map<TaskType, std::string> task_type2color = {
     {kDecode, "1"},        {kLoss, "4"},           {kLossAcc, "5"},         {kLossPrint, "1"},
     {kNormalMdUpdt, "6"},  {kMdSave, "1"},         {kMdDiffAcc, "7"},       {kCopyHd, "8"},
     {kCopyCommNet, "9"},   {kBoxing, "10"},        {kPrint, "1"},           {kReduceConcat, "2"},
-    {kReduceSplit, "2"},   {kAccuracy, "4"},       {kAccuracyPrint, "1"},   {kAccuracyAcc, "5"},
-    {kReduceScatter, "2"}, {kReduceLocalAdd, "2"}, {kReduceGlobalAdd, "2"}, {kReduceGather, "2"}};
+    {kReduceScatter, "2"}, {kReduceLocalAdd, "2"}, {kReduceGlobalAdd, "2"}, {kReduceGather, "2"},
+    {kReduceSplit, "2"},   {kAccuracy, "4"},       {kAccuracyPrint, "1"},   {kAccuracyAcc, "5"}};
 
 }  // namespace oneflow
