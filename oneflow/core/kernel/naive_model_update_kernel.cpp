11408363e2a7cf16d101610d84f7a7d355e87a92
Shiyuan Shang-Guan <shgshy@gmail.com>
2018-09-15 18:17:12 +0800

    separate model for update (#1232)
    * make each blob of the packed blob be updated separately in the ModelUpdate
    
    * make blob descs in regst be consistent in bw->md_diff_acc->shared_md_diff_add->md_update->fw
    
    * copy lbi2blob_descs from model
    
    * add shared_model_diff_add kernel
    
    * refine model_update actor and kernel
    
    * rm useless TODO
    
    * add shared_model_diff_add kernel
    
    * refine code

--- oneflow/core/kernel/naive_model_update_kernel.cpp
+++ oneflow/core/kernel/naive_model_update_kernel.cpp
@@ -5,25 +5,24 @@ namespace oneflow {
 
 template<DeviceType device_type, typename T>
 void NaiveMdUpdateKernel<device_type, T>::UpdateModel(
-    DeviceCtx* ctx, int64_t batch_size, T learning_rate, T l1, T l2, const Blob* pre_model_blob,
-    const Blob* model_diff_blob, int64_t next_model_vid,
+    DeviceCtx* ctx, int64_t batch_size, T learning_rate, T l1, T l2, int64_t next_model_vid,
     std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* model_diff_blob = BnInOp2Blob("model_diff");
   Blob* model_blob = BnInOp2Blob("model");
-  if (pre_model_blob != model_blob) { model_blob->CopyDataContentFrom(ctx, pre_model_blob); }
   // model = model - alpha * model_diff
   NaiveMdUpdateKernelUtil<device_type, T>::UpdateModel(
       ctx, model_blob->shape().elem_cnt(), batch_size, learning_rate, l1, l2,
-      model_diff_blob->dptr<T>(), pre_model_blob->dptr<T>(), model_blob->mut_dptr<T>());
+      model_diff_blob->dptr<T>(), model_blob->mut_dptr<T>());
 }
 
 template<typename T>
 class NaiveMdUpdateKernelUtil<DeviceType::kCPU, T> final {
  public:
   static void UpdateModel(DeviceCtx*, const int64_t n, int64_t batch_size, T learning_rate, T l1,
-                          T l2, const T* model_diff, const T* pre_model, T* model) {
+                          T l2, const T* model_diff, T* model) {
     for (int64_t i = 0; i != n; ++i) {
-      T reg_diff = RegularizeDiff(model_diff[i], batch_size, l1, l2, pre_model[i]);
-      model[i] = pre_model[i] - learning_rate * reg_diff;
+      T reg_diff = RegularizeDiff(model_diff[i], batch_size, l1, l2, model[i]);
+      model[i] = model[i] - learning_rate * reg_diff;
     }
   }
 };
