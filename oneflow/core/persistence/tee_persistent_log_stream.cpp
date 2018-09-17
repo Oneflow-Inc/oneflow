d76513b37fa1bfdc4923c2b697b5a8352e6086e5
Jinhui Yuan <yuan.ms2@gmail.com>
2018-09-17 15:08:23 +0800

    Refine runtime (#1108)
    * only master machine saves plan and has event logger
    
    * separate Data, Persistence, Cache, Log FileSystem config
    
    * refine
    
    * only specify data and snapshot path conf
    
    * forbit multiple machines use localfs as snapshot fs
    
    * networkfs as localfs
    
    * refine
    
    * Store log to snapshot (#1109)
    
    * use machine id, drop machine name
    
    * ensure setting machine id
    
    * allow save snapshot to localfs for distributed training (#1113)
    
    * Snapshot to master (#1116)
    
    * allow save snapshot to localfs for distributed training
    
    * fix mdSave to master for model parallel
    
    * fix review comment issues
    
    * add sanity check for machine id
    
    * rm useless comments
    
    * update example
    
    * Dev refine runtime add log stream mgr (#1142)
    
    * add LogStreamMgr
    
    * refine and refactor OutStream=>LogStream
    
    * bugfix
    
    * use LogStreamMgr to write graph, dot, plan, profile and proto
    
    * refine
    
    * simplify, remove LogStreamMgr (#1243)
    
    * simplify, remove LogStreamMgr
    
    * TeePersistentLogStream add static factory (#1244)

---
+++ oneflow/core/persistence/tee_persistent_log_stream.cpp
@@ -0,0 +1,39 @@
+#include "oneflow/core/persistence/tee_persistent_log_stream.h"
+#include "oneflow/core/common/str_util.h"
+#include <google/protobuf/text_format.h>
+
+namespace oneflow {
+
+TeePersistentLogStream::TeePersistentLogStream(const std::string& path) {
+  destinations_.emplace_back(LocalFS(), LogDir());
+  branches_.reserve(destinations_.size());
+  for (const auto& destination : destinations_) {
+    branches_.emplace_back(std::make_unique<PersistentOutStream>(
+        destination.mut_file_system(), JoinPath(destination.base_dir(), path)));
+  }
+}
+
+TeePersistentLogStream::~TeePersistentLogStream() { Flush(); }
+
+std::unique_ptr<TeePersistentLogStream> TeePersistentLogStream::Create(const std::string& path) {
+  auto stream_ptr = new TeePersistentLogStream(path);
+  return std::unique_ptr<TeePersistentLogStream>(stream_ptr);
+}
+
+void TeePersistentLogStream::Flush() {
+  for (const auto& branch : branches_) { branch->Flush(); }
+};
+
+void TeePersistentLogStream::Write(const char* s, size_t n) {
+  for (const auto& branch : branches_) { branch->Write(s, n); }
+};
+
+void TeePersistentLogStream::Write(const std::string& str) { this->Write(str.data(), str.size()); }
+
+void TeePersistentLogStream::Write(const PbMessage& proto) {
+  std::string output;
+  google::protobuf::TextFormat::PrintToString(proto, &output);
+  this->Write(output);
+}
+
+}  // namespace oneflow
