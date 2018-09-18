ebb56d2e995a93af1dfe8139ffb4893b58b907f5
ScXfjiang <sc.xfjiang@gmail.com>
2018-09-18 12:23:57 +0800

    delete infer has_instance_num in compiler, set all blobs has_instance_num = true except for packed blobs

--- oneflow/core/graph/loss_compute_task_node.cpp
+++ oneflow/core/graph/loss_compute_task_node.cpp
@@ -60,17 +60,9 @@ void LossCompTaskNode::BuildRegstWhenTraining() {
   data_tmp_regst->AddLbi(loss_op->BnInOp2Lbi("loss"));
   loss_node->BindBnWithRegst("loss", data_tmp_regst);
   loss_node->InferBlobDescs(parallel_ctx());
-  // copy prediction_blob_desc to prediction_diff_blob_desc and set instance_num if necessary
-  bool has_instance_num = false;
   for (std::shared_ptr<RegstDesc> regst : GetConsumedRegst("in")) {
     out_regst->CopyBlobDescWithoutAddLbi(regst.get());
-    const BlobDesc* label_blob_desc = regst->GetBlobDesc(loss_op->BnInOp2Lbi("label"));
-    if (label_blob_desc && label_blob_desc->has_instance_num_field()) { has_instance_num = true; }
   }
-  BlobDesc* prediction_diff_blob_desc =
-      out_regst->MutBlobDesc(loss_op->BnInOp2Lbi(GenDiffBn("prediction")));
-  prediction_diff_blob_desc->set_has_instance_num_field(has_instance_num);
-
   std::shared_ptr<const Operator> sum_op = op_vec[1];
   ExecNode* sum_node = mut_exec_gph().NewNode();
   sum_node->mut_op() = sum_op;
