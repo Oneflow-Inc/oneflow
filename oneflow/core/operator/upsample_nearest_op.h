b66e1b3a09a66c224ea7b0204a9522234e7a26e6
Shenghang Cai <jackalcooper@gmail.com>
2018-09-18 17:00:32 +0800

    add upsample_nearest_op

---
+++ oneflow/core/operator/upsample_nearest_op.h
@@ -0,0 +1,27 @@
+#ifndef ONEFLOW_CORE_OPERATOR_UPSAMPLE_NEAREST_OP_H_
+#define ONEFLOW_CORE_OPERATOR_UPSAMPLE_NEAREST_OP_H_
+
+#include "oneflow/core/operator/operator.h"
+
+namespace oneflow {
+
+class UpsampleNearestOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(UpsampleNearestOp);
+  UpsampleNearestOp() = default;
+  ~UpsampleNearestOp() = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  bool IsElemWiseOp() const override { return true; }
+  bool NeedInBlobWhenBackward() const override { return false; }
+
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override;
+
+ private:
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_UPSAMPLE_NEAREST_OP_H_
