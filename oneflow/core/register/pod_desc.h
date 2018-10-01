1c29eb42febbe68b5681ae8ef0213e7e8dc3a56c
Li Xinqi <lixinqi2010@gmail.com>
2018-10-01 19:09:46 +0800

    Dev pod desc (#1268)
    * available instance num
    
    * import shape.proto
    
    * PodProto
    
    * rename message
    
    * union pod is useless
    
    * PodPtr
    
    * rename: PodPtr::get() => PodPtr::Get()
    
    * BlobDescProto.pod
    
    * mv register_desc.time_shape into another pr
    
    * pod_helper.h
    
    * FieldAlignedByteSize
    
    * pod_desc
    
    * PodDesc copy constructor
    
    * BlobDesc::body_shape_pod_desc_
    
    * add BlobDesc::opaque_header_pod_desc_
    
    * align_shift => alignment
    
    * default alignment
    
    * add field Blob::header_pod_ptr_
    
    * rename AlignedFieldPodProto => FieldPodProto
    
    * bugfix
    
    * check
    
    * FieldId
    
    * simplify RtBlobDesc
    
    * simplify Blob
    
    * ShapedPod => TensorPod
    
    * refine ComputePackedBlobDesc
    
    
    Former-commit-id: 8800da932878813705e4dc8d8dd2b216039c5d5c

---
+++ oneflow/core/register/pod_desc.h
@@ -0,0 +1,155 @@
+#ifndef ONEFLOW_CORE_REGISTER_POD_DESC_H_
+#define ONEFLOW_CORE_REGISTER_POD_DESC_H_
+
+#include "oneflow/core/common/util.h"
+#include "oneflow/core/common/data_type.h"
+#include "oneflow/core/common/protobuf.h"
+#include "oneflow/core/common/shape.h"
+#include "oneflow/core/register/pod.pb.h"
+
+namespace std {
+
+template<>
+struct hash<oneflow::FieldId> {
+  size_t operator()(const oneflow::FieldId& field_id) const {
+    if (field_id.has_key()) { return std::hash<int>()(field_id.key()); }
+    if (field_id.has_lbi()) { return std::hash<oneflow::LogicalBlobId>()(field_id.lbi()); }
+    UNIMPLEMENTED();
+  }
+};
+
+}  // namespace std
+
+namespace oneflow {
+
+FieldId NewFieldId(FieldKey key);
+FieldId NewFieldId(const LogicalBlobId& lbi);
+inline bool operator==(const FieldId& lhs, const FieldId& rhs) {
+  PbMd message_diff;
+  return message_diff.Equivalent(lhs, rhs);
+}
+
+class PodDesc {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(PodDesc);
+  PodDesc() = default;
+  virtual ~PodDesc() = default;
+
+  template<typename T>
+  const T& Cast() const;
+  template<typename T>
+  T* MutCast();
+
+  virtual size_t ByteSize() const = 0;
+  virtual void ToProto(PodProto* pod_proto) const = 0;
+  virtual std::unique_ptr<PodDesc> Clone() const = 0;
+  virtual bool operator==(const PodDesc& rhs) const = 0;
+  bool operator!=(const PodDesc& rhs) const { return !(*this == rhs); }
+};
+
+class TensorPodDesc final : public PodDesc {
+ public:
+  TensorPodDesc() = default;
+  TensorPodDesc(const Shape& shape, DataType data_type) : shape_(shape), data_type_(data_type) {}
+  explicit TensorPodDesc(const TensorPodProto& shape_pod_proto);
+  explicit TensorPodDesc(const TensorPodDesc& shape_pod);
+  ~TensorPodDesc() = default;
+  const Shape& shape() const { return shape_; }
+  DataType data_type() const { return data_type_; }
+  Shape* mut_shape() { return &shape_; }
+  void set_data_type(DataType data_type) { data_type_ = data_type; }
+
+  void InitFromProto(const TensorPodProto& shape_pod);
+
+  size_t ByteSize() const override;
+  void ToProto(PodProto* pod_proto) const override;
+  std::unique_ptr<PodDesc> Clone() const override { return std::make_unique<TensorPodDesc>(*this); }
+  bool operator==(const PodDesc& rhs) const override;
+
+ private:
+  Shape shape_;
+  DataType data_type_;
+};
+
+class FieldPodDesc;
+
+class StructPodDesc final : public PodDesc {
+ public:
+  StructPodDesc() = default;
+  explicit StructPodDesc(const StructPodProto&);
+  explicit StructPodDesc(const StructPodDesc&);
+  ~StructPodDesc() = default;
+
+  StructPodDesc* MutStructField(const FieldId& field_id);
+  const PodDesc& Field(FieldKey field_key) const { return Field(NewFieldId(field_key)); }
+  const PodDesc& Field(const FieldId& field_id) const;
+  void AddField(FieldKey field_key, const PodDesc& pod_desc);
+  void AddField(const FieldId& field_id, const PodDesc& pod_desc);
+  size_t ByteSize() const override;
+  void InitFromProto(const StructPodProto& struct_pod);
+
+  bool HasField(FieldKey field_key) const { return HasField(NewFieldId(field_key)); }
+  bool HasField(const FieldId& field_id) const;
+  StructPodDesc& operator=(const StructPodDesc&);
+  std::unique_ptr<PodDesc> Clone() const override { return std::make_unique<StructPodDesc>(*this); }
+  void ToProto(PodProto* pod_proto) const override { ToProto(pod_proto->mutable_struct_pod()); }
+  void ToProto(StructPodProto* pod_proto) const;
+  StructPodDesc* MutStructField(const FieldId& field_id, int32_t default_alignment);
+  void AddField(const FieldId& field_id, const PodDesc& pod_desc, size_t alignment);
+  bool operator==(const PodDesc& rhs) const override;
+  size_t ByteOffset4Field(const FieldId& field_name) const;
+
+ private:
+  void Clear();
+  PodDesc* MutExistedField(const FieldId& field_id);
+  void AddField(std::unique_ptr<FieldPodDesc>&& field);
+  void AddField(const FieldId& field_id, std::unique_ptr<PodDesc>&& field);
+  void AddField(const FieldId& field_id, std::unique_ptr<PodDesc>&& field, size_t alignment);
+
+  std::vector<std::unique_ptr<FieldPodDesc>> fields_;
+  HashMap<FieldId, int32_t> field_id2field_idx_;
+};
+
+class FieldPodDesc final : public PodDesc {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(FieldPodDesc);
+  ~FieldPodDesc() = default;
+
+ private:
+  friend class StructPodDesc;
+  FieldPodDesc(const FieldId& field_id, std::unique_ptr<PodDesc>&& pod, size_t alignment)
+      : PodDesc(), field_id_(field_id), pod_(std::move(pod)), alignment_(alignment) {}
+  explicit FieldPodDesc(const FieldPodProto& field_pod_proto);
+
+  size_t ByteSize() const override;
+  void ToProto(PodProto* pod_proto) const override { UNIMPLEMENTED(); }
+  std::unique_ptr<PodDesc> Clone() const override { UNIMPLEMENTED(); }
+  void ToProto(FieldPodProto* field_proto) const;
+  bool operator==(const PodDesc& rhs) const override;
+
+  const PodDesc& pod() const { return *pod_; }
+  const FieldId& field_id() const { return field_id_; }
+  PodDesc* mut_pod() { return pod_.get(); }
+
+  FieldId field_id_;
+  std::unique_ptr<PodDesc> pod_;
+  size_t alignment_;
+};
+
+template<typename T>
+const T& PodDesc::Cast() const {
+  static_assert(std::is_same<T, TensorPodDesc>::value || std::is_same<T, StructPodDesc>::value,
+                "only TensorPodDesc and StructPodDesc supported");
+  return *dynamic_cast<T*>(this);
+}
+
+template<typename T>
+T* PodDesc::MutCast() {
+  static_assert(std::is_same<T, TensorPodDesc>::value || std::is_same<T, StructPodDesc>::value,
+                "only TensorPodDesc and StructPodDesc supported");
+  return dynamic_cast<T*>(this);
+}
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_REGISTER_POD_DESC_H_
