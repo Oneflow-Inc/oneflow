8800da932878813705e4dc8d8dd2b216039c5d5c
Li Xinqi <lixinqi2010@gmail.com>
2018-10-01 19:09:46 +0800

    Dev pod desc (#1268)
    * available instance num
    
    * import shape.proto
    
    * PodProto
    
    * rename message
    
    * union pod is useless
    
    * PodPtr
    
    * rename: PodPtr::get() => PodPtr::Get()
    
    * BlobDescProto.pod
    
    * mv register_desc.time_shape into another pr
    
    * pod_helper.h
    
    * FieldAlignedByteSize
    
    * pod_desc
    
    * PodDesc copy constructor
    
    * BlobDesc::body_shape_pod_desc_
    
    * add BlobDesc::opaque_header_pod_desc_
    
    * align_shift => alignment
    
    * default alignment
    
    * add field Blob::header_pod_ptr_
    
    * rename AlignedFieldPodProto => FieldPodProto
    
    * bugfix
    
    * check
    
    * FieldId
    
    * simplify RtBlobDesc
    
    * simplify Blob
    
    * ShapedPod => TensorPod
    
    * refine ComputePackedBlobDesc

--- oneflow/core/register/blob.h
+++ oneflow/core/register/blob.h
@@ -9,6 +9,7 @@
 #include "oneflow/core/persistence/persistent_in_stream.h"
 #include "oneflow/core/record/record.pb.h"
 #include "oneflow/core/record/record_io.h"
+#include "oneflow/core/register/pod_ptr.h"
 
 namespace oneflow {
 
@@ -87,6 +88,8 @@ class Blob final {
   void set_max_col_id(int32_t val);
   bool IsColValid() const { return col_id() <= max_col_id(); }
   const MemoryCase& mem_case() const;
+  const PodPtr* header_pod_ptr() const { return &header_pod_ptr_; }
+  PodPtr* header_pod_ptr() { return &header_pod_ptr_; }
 
  private:
   int64_t GetDptrOffset(int32_t index) const { return 0; }
@@ -114,6 +117,7 @@ class Blob final {
   void* dptr_;
   const RtBlobDesc* blob_desc_;
   Regst* regst_;
+  PodPtr header_pod_ptr_;
 };
 
 template<typename RecordType>
