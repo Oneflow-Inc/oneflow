40de0a971c2df5e24bfda0f8ed75ee6f69264f7b
Shenghang Cai <jackalcooper@gmail.com>
2018-10-02 17:36:50 +0800

    refactor roi align

--- oneflow/core/kernel/roi_align_kernel.cu
+++ oneflow/core/kernel/roi_align_kernel.cu
@@ -89,7 +89,7 @@ __device__ bool BilinearInterpolateDiff(const T bin_diff_avg, const int64_t heig
 template<typename T>
 __global__ void RoIAlignForward(const int64_t nthreads, const T* in_dptr, const float spatial_scale,
                                 const int32_t sampling_ratio, const int64_t channel_num,
-                                const int64_t height, const int64_t width, const int64_t roi_num,
+                                const int64_t height, const int64_t width,
                                 const int64_t pooled_height, const int64_t pooled_width,
                                 const T* rois_dptr, T* out_dptr) {
   const int64_t pooled_area = pooled_height * pooled_width;
@@ -98,13 +98,13 @@ __global__ void RoIAlignForward(const int64_t nthreads, const T* in_dptr, const 
     const int64_t h = (index / pooled_width) % pooled_height;
     const int64_t w = index % pooled_width;
     const int64_t c = (index / pooled_area) % channel_num;
-    const int64_t r = (index / channel_pooled_area) % roi_num;
-    const int64_t n = index / channel_pooled_area / roi_num;
-    const T* offset_rois_dptr = rois_dptr + (n * roi_num + r) * 4;
-    const T roi_start_w = offset_rois_dptr[0] * spatial_scale;
-    const T roi_start_h = offset_rois_dptr[1] * spatial_scale;
-    const T roi_end_w = offset_rois_dptr[2] * spatial_scale;
-    const T roi_end_h = offset_rois_dptr[3] * spatial_scale;
+    const int64_t r = index / channel_pooled_area;
+    const T* offset_rois_dptr = rois_dptr + r * 4;
+    const int64_t n = static_cast<int64_t>(offset_rois_dptr[0]);
+    const T roi_start_w = offset_rois_dptr[1] * spatial_scale;
+    const T roi_start_h = offset_rois_dptr[2] * spatial_scale;
+    const T roi_end_w = offset_rois_dptr[3] * spatial_scale;
+    const T roi_end_h = offset_rois_dptr[4] * spatial_scale;
     const T roi_height = max(roi_end_h - roi_start_h, static_cast<T>(1.0));
     const T roi_width = max(roi_end_w - roi_start_w, static_cast<T>(1.0));
     const T bin_height = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
@@ -134,22 +134,21 @@ template<typename T>
 __global__ void RoIAlignBackward(const int64_t nthreads, const T* out_diff_dptr,
                                  const float spatial_scale, const int32_t sampling_ratio,
                                  const int64_t channel_num, const int64_t height,
-                                 const int64_t width, const int64_t roi_num,
-                                 const int64_t pooled_height, const int64_t pooled_width,
-                                 const T* rois_dptr, T* in_diff_dptr) {
+                                 const int64_t width, const int64_t pooled_height,
+                                 const int64_t pooled_width, const T* rois_dptr, T* in_diff_dptr) {
   const int64_t pooled_area = pooled_height * pooled_width;
   const int64_t channel_pooled_area = channel_num * pooled_height * pooled_width;
   CUDA_1D_KERNEL_LOOP(index, nthreads) {
     const int64_t h = (index / pooled_width) % pooled_height;
     const int64_t w = index % pooled_width;
     const int64_t c = (index / pooled_area) % channel_num;
-    const int64_t r = (index / channel_pooled_area) % roi_num;
-    const int64_t n = index / channel_pooled_area / roi_num;
-    const T* offset_rois_dptr = rois_dptr + (n * roi_num + r) * 4;
-    const T roi_start_w = offset_rois_dptr[0] * spatial_scale;
-    const T roi_start_h = offset_rois_dptr[1] * spatial_scale;
-    const T roi_end_w = offset_rois_dptr[2] * spatial_scale;
-    const T roi_end_h = offset_rois_dptr[3] * spatial_scale;
+    const int64_t r = index / channel_pooled_area;
+    const T* offset_rois_dptr = rois_dptr + r * 4;
+    const int64_t n = static_cast<int64_t>(offset_rois_dptr[0]);
+    const T roi_start_w = offset_rois_dptr[1] * spatial_scale;
+    const T roi_start_h = offset_rois_dptr[2] * spatial_scale;
+    const T roi_end_w = offset_rois_dptr[3] * spatial_scale;
+    const T roi_end_h = offset_rois_dptr[4] * spatial_scale;
     const T roi_height = max(roi_end_h - roi_start_h, static_cast<T>(1.0));
     const T roi_width = max(roi_end_w - roi_start_w, static_cast<T>(1.0));
     const T bin_height = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
@@ -201,10 +200,9 @@ struct RoIAlignKernelUtil<DeviceType::kGPU, T> {
     const int64_t elem_cnt = out_blob->shape().elem_cnt();
     RoIAlignForward<T><<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0,
                          ctx.device_ctx->cuda_stream()>>>(
-        elem_cnt, in_blob->dptr<T>(), conf.spatial_scale(), conf.sampling_ratio(),
-        in_blob->shape().At(1), in_blob->shape().At(2), in_blob->shape().At(3),
-        rois_blob->shape().At(1), conf.pooled_h(), conf.pooled_w(), rois_blob->dptr<T>(),
-        out_blob->mut_dptr<T>());
+        elem_cnt, out_blob->dptr<T>(), conf.spatial_scale(), conf.sampling_ratio(),
+        in_blob->shape().At(1), in_blob->shape().At(2), in_blob->shape().At(3), conf.pooled_h(),
+        conf.pooled_w(), rois_blob->dptr<T>(), out_blob->mut_dptr<T>());
   }
 
   static void Backward(const KernelCtx& ctx, const RoIAlignOpConf& conf, const Blob* out_diff_blob,
@@ -214,8 +212,7 @@ struct RoIAlignKernelUtil<DeviceType::kGPU, T> {
                           ctx.device_ctx->cuda_stream()>>>(
         elem_cnt, out_diff_blob->dptr<T>(), conf.spatial_scale(), conf.sampling_ratio(),
         in_diff_blob->shape().At(1), in_diff_blob->shape().At(2), in_diff_blob->shape().At(3),
-        rois_blob->shape().At(1), conf.pooled_h(), conf.pooled_w(), rois_blob->dptr<T>(),
-        in_diff_blob->mut_dptr<T>());
+        conf.pooled_h(), conf.pooled_w(), rois_blob->dptr<T>(), in_diff_blob->mut_dptr<T>());
   }
 };
 
