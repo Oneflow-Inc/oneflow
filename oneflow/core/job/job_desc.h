a1b44c051053f3731dccd5302b73f7c2350ca05f
Shiyuan Shang-Guan <shgshy@gmail.com>
2018-10-03 16:27:32 +0800

    fix decode_random and refine synthetic_data (#1278)
    * fix decode_random and refine synthetic_data
    
    * add example
    
    * initialize only once

--- oneflow/core/job/job_desc.h
+++ oneflow/core/job/job_desc.h
@@ -25,7 +25,6 @@ class JobDesc final {
   DataType DefaultDataType() const { return job_conf_.other().default_data_type(); }
   size_t SizeOfOneDataId() const { return job_conf_.other().max_data_id_length() * sizeof(char); }
   bool use_rdma() const { return job_conf_.other().use_rdma(); }
-  bool use_synthetic_data() const { return job_conf_.other().use_synthetic_data(); }
   bool EnableCudnn() const { return job_conf_.other().enable_cudnn(); }
   int64_t TotalMachineNum() const { return job_conf_.resource().machine().size(); }
   int32_t CpuDeviceNum() const { return job_conf_.resource().cpu_device_num(); }
