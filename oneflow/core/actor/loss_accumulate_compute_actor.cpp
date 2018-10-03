efdf7508a5d208dc76532967f2031b2abb50ede3
Juncheng <liujuncheng1022@gmail.com>
2018-10-03 11:19:31 +0800

    infer max_acc_count by time shape

--- oneflow/core/actor/loss_accumulate_compute_actor.cpp
+++ oneflow/core/actor/loss_accumulate_compute_actor.cpp
@@ -2,6 +2,19 @@
 
 namespace oneflow {
 
+void LossAccCompActor::VirtualCompActorInit(const TaskProto& proto) {
+  const Shape& one_time_shape = Global<RegstMgr>::Get()
+                                    ->RegstDesc4RegstDescId(Name2SoleRegstDescId("one"))
+                                    .data_regst_time_shape();
+  const Shape& acc_time_shape = Global<RegstMgr>::Get()
+                                    ->RegstDesc4RegstDescId(Name2SoleRegstDescId("acc"))
+                                    .data_regst_time_shape();
+  CHECK_GE(one_time_shape.elem_cnt(), acc_time_shape.elem_cnt());
+  AccumulateCompActor::Init(
+      proto, static_cast<int32_t>(one_time_shape.elem_cnt() / acc_time_shape.elem_cnt()),
+      ColIdOrder::kAscending);
+}
+
 REGISTER_ACTOR(TaskType::kLossAcc, LossAccCompActor);
 
 }  // namespace oneflow
