c9e5b1fbfd92ef54243a2799ce20256f51d220d3
Juncheng <liujuncheng1022@gmail.com>
2018-10-03 17:45:49 +0800

    BK[P]

--- oneflow/core/actor/loss_accumulate_compute_actor.cpp
+++ oneflow/core/actor/loss_accumulate_compute_actor.cpp
@@ -6,12 +6,12 @@ void LossAccCompActor::VirtualCompActorInit(const TaskProto& proto) {
   const Shape& one_time_shape = Global<RegstMgr>::Get()
                                     ->RegstDesc4RegstDescId(Name2SoleRegstDescId("one"))
                                     .data_regst_time_shape();
-  const Shape& acc_time_shape = Global<RegstMgr>::Get()
-                                    ->RegstDesc4RegstDescId(Name2SoleRegstDescId("acc"))
-                                    .data_regst_time_shape();
-  CHECK_GE(one_time_shape.elem_cnt(), acc_time_shape.elem_cnt());
+  CHECK_GE(one_time_shape.NumAxes(), 2);
+  CHECK_EQ(one_time_shape.At(0), Global<JobDesc>::Get()->TotalBatchNum());
+  CHECK_EQ(one_time_shape.At(1), Global<JobDesc>::Get()->NumOfPiecesInBatch());
   AccumulateCompActor::Init(
-      proto, static_cast<int32_t>(one_time_shape.elem_cnt() / acc_time_shape.elem_cnt()),
+      proto,
+      static_cast<int32_t>(one_time_shape.Count(2) * Global<JobDesc>::Get()->PieceNumOfPrintLoss()),
       ColIdOrder::kAscending);
 }
 
