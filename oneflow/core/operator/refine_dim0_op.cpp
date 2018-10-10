6a71e8c60a758feddb4cacdaf5d72f64e71566ee
leaves-zwx <kunta0932@gmail.com>
2018-10-10 13:48:27 +0800

    fix

--- oneflow/core/operator/refine_dim0_op.cpp
+++ oneflow/core/operator/refine_dim0_op.cpp
@@ -14,8 +14,6 @@ void RefineDim0Op::InferBlobDescs(std::function<BlobDesc*(const std::string&)> G
                                   const ParallelContext* parallel_ctx) const {
   const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
-  *out_blob_desc = *in_blob_desc;
-
   std::vector<int64_t> shape_vec;
   std::vector<int64_t> inner_shape_vec;
   const RefineDim0OpConf& conf = op_conf().refine_dim0_conf();
@@ -24,21 +22,24 @@ void RefineDim0Op::InferBlobDescs(std::function<BlobDesc*(const std::string&)> G
       int64_t shrink_axis = conf.shrink_conf().axis();
       CHECK_GE(shrink_axis, 1);
       CHECK(in_blob_desc->has_dim0_inner_shape());
-      CHECK(!out_blob_desc->has_dim1_valid_num_field());
+      CHECK(!in_blob_desc->has_dim1_valid_num_field());
       const Shape& shape = in_blob_desc->shape();
       const Shape& inner_shape = in_blob_desc->dim0_inner_shape();
-      shape_vec.reserve(shape.dim_vec().size() + inner_shape.NumAxes() - shrink_axis);
       inner_shape_vec.reserve(shrink_axis);
-      FOR_RANGE(int64_t, axis, shrink_axis, inner_shape.NumAxes()) {
-        shape_vec.emplace_back(inner_shape.At(axis));
-        if (axis == 1) {
-          out_blob_desc->set_has_dim1_valid_num_field(true);
-          out_blob_desc->set_has_dim0_valid_num_field(false);
-        }
-      }
-      shape_vec.insert(shape_vec.end(), shape.dim_vec().begin(), shape.dim_vec().end());
       FOR_RANGE(int64_t, axis, 0, shrink_axis) {
         inner_shape_vec.emplace_back(inner_shape.At(axis));
+      }
+      shape_vec.reserve(shape.dim_vec().size() + inner_shape.NumAxes() - shrink_axis);
+      FOR_RANGE(int64_t, axis, shrink_axis, inner_shape.NumAxes()) {
+        shape_vec.emplace_back(inner_shape.At(axis));
+      }
+      shape_vec.insert(shape_vec.end(), ++shape.dim_vec().begin(), shape.dim_vec().end());
+      if (shrink_axis == 1) {
+        out_blob_desc->set_has_dim1_valid_num_field(in_blob_desc->has_dim0_valid_num_field());
+        out_blob_desc->set_has_dim0_valid_num_field(false);
+      } else {
+        out_blob_desc->set_has_dim1_valid_num_field(false);
+        out_blob_desc->set_has_dim0_valid_num_field(in_blob_desc->has_dim0_valid_num_field());
       }
       break;
     }
@@ -46,28 +47,44 @@ void RefineDim0Op::InferBlobDescs(std::function<BlobDesc*(const std::string&)> G
       int64_t extend_axis = conf.extend_conf().axis();
       CHECK_GE(extend_axis, 1);
       const Shape& shape = in_blob_desc->shape();
+      bool has_inner_shape = false;
       if (in_blob_desc->has_dim0_inner_shape()) {
         CHECK(!in_blob_desc->has_dim1_valid_num_field());
         const Shape& inner_shape = in_blob_desc->dim0_inner_shape();
         inner_shape_vec.reserve(inner_shape.NumAxes() + extend_axis);
-        FOR_RANGE(int64_t, axis, 0, inner_shape.NumAxes()) {
-          inner_shape_vec.emplace_back(inner_shape.At(axis));
-        }
+        inner_shape_vec.insert(inner_shape_vec.begin(), inner_shape.dim_vec().begin(),
+                               inner_shape.dim_vec().end());
+        has_inner_shape = true;
       } else {
         inner_shape_vec.reserve(1 + extend_axis);
         inner_shape_vec.emplace_back(shape.At(0));
       }
+      int64_t extend_dims = 1;
+      FOR_RANGE(int64_t, axis, 1, extend_axis + 1) {
+        int64_t dim = shape.At(axis);
+        inner_shape_vec.emplace_back(dim);
+        extend_dims *= dim;
+      }
       shape_vec.reserve(shape.dim_vec().size() - extend_axis);
-      FOR_RANGE(int64_t, axis, extend_axis, shape.NumAxes()) {
+      shape_vec.emplace_back(shape.At(0) * extend_dims);
+      FOR_RANGE(int64_t, axis, extend_axis + 1, shape.NumAxes()) {
         shape_vec.emplace_back(shape.At(axis));
       }
-      FOR_RANGE(int64_t, axis, 1, extend_axis + 1) { inner_shape_vec.emplace_back(shape.At(axis)); }
+      if (has_inner_shape) {
+        out_blob_desc->set_has_dim1_valid_num_field(false);
+        out_blob_desc->set_has_dim0_valid_num_field(in_blob_desc->has_dim0_valid_num_field());
+      } else {
+        out_blob_desc->set_has_dim1_valid_num_field(false);
+        out_blob_desc->set_has_dim0_valid_num_field(in_blob_desc->has_dim1_valid_num_field());
+      }
       break;
     }
     default: { UNIMPLEMENTED(); }
   }
   out_blob_desc->mut_shape() = Shape(shape_vec);
-  out_blob_desc->mut_dim0_inner_shape() = Shape(inner_shape_vec);
+  if (inner_shape_vec.size() > 1) {
+    out_blob_desc->mut_dim0_inner_shape() = Shape(inner_shape_vec);
+  }
 }
 
 REGISTER_OP(OperatorConf::kRefineDim0Conf, RefineDim0Op);
