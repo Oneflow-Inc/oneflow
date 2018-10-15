92ab2124048e381f416be9a55b503d4a3cece7e9
ScXfjiang <sc.xfjiang@gmail.com>
2018-10-15 18:43:50 +0800

    little refine in actor

--- oneflow/core/actor/actor.cpp
+++ oneflow/core/actor/actor.cpp
@@ -377,12 +377,11 @@ void Actor::AsyncLaunchKernel(const KernelCtx& kernel_ctx,
       Regst* regst = GetNaiveCurWriteable(regst_desc_id_it->second);
       if (regst == nullptr) { regst = GetNaiveCurReadable(regst_desc_id_it->second); }
       if (regst == nullptr) { regst = Regst4RegstDescId(regst_desc_id_it->second); }
-      if (regst == nullptr) {
-        return nullptr;
-      } else {
+      if (regst != nullptr) {
         const LogicalBlobId& lbi = ek.kernel->BnInOp2Lbi(bn_in_op);
         return regst->GetBlobByLbi(lbi);
       }
+      return nullptr;
     });
   }
 }
