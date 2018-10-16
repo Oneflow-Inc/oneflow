7e43e51b431e11c3e0f1af02303983eb4d9f8d66
ShawnXuan <xiexuanx2@gmail.com>
2018-10-16 22:44:57 +0800

    add blob_dump_kernel from print_kernel

---
+++ oneflow/core/kernel/blob_dump_kernel.cpp
@@ -0,0 +1,84 @@
+#include "oneflow/core/kernel/blob_dump_kernel.h"
+#include "oneflow/core/job/runtime_context.h"
+#include "oneflow/core/record/ofrecord_encoder.h"
+
+namespace oneflow {
+
+void BlobDumpKernel::VirtualKernelInit(const ParallelContext* parallel_ctx) {
+  const auto& conf = op_conf().blob_dump_conf();
+  const std::string& root_path = conf.dump_dir();
+  OfCallOnce(root_path, SnapshotFS(), &fs::FileSystem::RecursivelyCreateDir);
+  int32_t part_name_suffix_length = conf.part_name_suffix_length();
+  std::string num = std::to_string(parallel_ctx->parallel_id());
+  int32_t zero_count = std::max(part_name_suffix_length - static_cast<int32_t>(num.length()), 0);
+  std::string file_path =
+      JoinPath(root_path, conf.part_name_prefix() + std::string(zero_count, '0') + num);
+  out_stream_.reset(new PersistentOutStream(SnapshotFS(), file_path));
+}
+
+void BlobDumpKernel::Forward(const KernelCtx& ctx,
+                             std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  if (HasEmptyShapeBlob(this->op_attribute().input_bns(), BnInOp2Blob)) { return; }
+  auto GetBlob = [&](int64_t blob_id) -> Blob* {
+    return BnInOp2Blob("in_" + std::to_string(blob_id));
+  };
+  const auto& conf = op_conf().blob_dump_conf();
+  int32_t total_blob_num = op_attribute().input_bns_size() + op_attribute().pb_input_bns_size();
+  int64_t min_record_num = MaxVal<int64_t>();
+  bool has_data_id_field = false;
+  bool has_col_num_field = false;
+  // loop for min record_num
+  FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
+    const Blob* cur_blob = GetBlob(blob_id);
+    min_record_num = std::min(cur_blob->shape().At(0), min_record_num);
+    has_data_id_field = has_data_id_field | cur_blob->has_data_id_field();
+    has_col_num_field = has_col_num_field | cur_blob->has_col_num_field();
+  }
+  if (has_col_num_field) { TODO(); }
+  FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
+    const Blob* cur_blob = GetBlob(blob_id);
+    CHECK_EQ(cur_blob->has_data_id_field(), has_data_id_field);
+    CHECK_EQ(cur_blob->has_col_num_field(), has_col_num_field);
+  }
+
+  OFRecord record;
+  FOR_RANGE(int64_t, record_id, 0, min_record_num) {
+    record.clear_feature();
+    if (has_data_id_field) {
+      const char* data_id_str = nullptr;
+      FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
+        const Blob* cur_blob = GetBlob(blob_id);
+        int64_t hidden_col_num = cur_blob->shape().At(0) / min_record_num;
+        FOR_RANGE(int32_t, col, 0, hidden_col_num) {
+          if (data_id_str) {
+            CHECK_STREQ(data_id_str, cur_blob->data_id(record_id * hidden_col_num + col));
+          } else {
+            data_id_str = cur_blob->data_id(record_id * hidden_col_num + col);
+          }
+        }
+      }
+      if (*data_id_str == '\0') { break; }
+      OFRecordEncoderIf::EncodeOneDataId(ctx.device_ctx, data_id_str, record);
+    }
+    FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
+      const Blob* cur_blob = GetBlob(blob_id);
+      const PrintRecordConf& cur_dump_conf = conf.in(blob_id);
+      std::string field_name = cur_dump_conf.lbn();
+      if (cur_dump_conf.has_name()) { field_name = cur_dump_conf.name(); }
+      CHECK(record.feature().find(field_name) == record.feature().end())
+          << "Field " << field_name << " found repeatedly in OfRecord";
+      int64_t one_col_elem_num =
+          cur_blob->shape().Count(1) * cur_blob->shape().At(0) / min_record_num;
+      Feature& feature = (*(record.mutable_feature()))[field_name];
+      GetOFRecordEncoder(cur_dump_conf.encode_case().encode_case(), cur_blob->data_type())
+          ->EncodeOneCol(ctx.device_ctx, cur_blob, record_id * one_col_elem_num, feature,
+                         field_name, one_col_elem_num);
+    }
+    *out_stream_ << record;
+  }
+  out_stream_->Flush();
+}
+
+REGISTER_KERNEL(OperatorConf::kBlobDumpConf, BlobDumpKernel);
+
+}  // namespace oneflow
