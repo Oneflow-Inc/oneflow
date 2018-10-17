4da5fd0e2526179ad1a0804cb395c1b64fc7bb68
ShawnXuan <xiexuanx2@gmail.com>
2018-10-17 09:50:28 +0800

    update forward func

--- oneflow/core/kernel/blob_dump_kernel.cpp
+++ oneflow/core/kernel/blob_dump_kernel.cpp
@@ -24,58 +24,21 @@ void BlobDumpKernel::Forward(const KernelCtx& ctx,
   };
   const auto& conf = op_conf().blob_dump_conf();
   int32_t total_blob_num = op_attribute().input_bns_size() + op_attribute().pb_input_bns_size();
-  int64_t min_record_num = MaxVal<int64_t>();
-  bool has_data_id_field = false;
-  bool has_col_num_field = false;
-  // loop for min record_num
-  FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
-    const Blob* cur_blob = GetBlob(blob_id);
-    min_record_num = std::min(cur_blob->shape().At(0), min_record_num);
-    has_data_id_field = has_data_id_field | cur_blob->has_data_id_field();
-    has_col_num_field = has_col_num_field | cur_blob->has_col_num_field();
-  }
-  if (has_col_num_field) { TODO(); }
-  FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
-    const Blob* cur_blob = GetBlob(blob_id);
-    CHECK_EQ(cur_blob->has_data_id_field(), has_data_id_field);
-    CHECK_EQ(cur_blob->has_col_num_field(), has_col_num_field);
-  }
 
   OFRecord record;
-  FOR_RANGE(int64_t, record_id, 0, min_record_num) {
-    record.clear_feature();
-    if (has_data_id_field) {
-      const char* data_id_str = nullptr;
-      FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
-        const Blob* cur_blob = GetBlob(blob_id);
-        int64_t hidden_col_num = cur_blob->shape().At(0) / min_record_num;
-        FOR_RANGE(int32_t, col, 0, hidden_col_num) {
-          if (data_id_str) {
-            CHECK_STREQ(data_id_str, cur_blob->data_id(record_id * hidden_col_num + col));
-          } else {
-            data_id_str = cur_blob->data_id(record_id * hidden_col_num + col);
-          }
-        }
-      }
-      if (*data_id_str == '\0') { break; }
-      OFRecordEncoderIf::EncodeOneDataId(ctx.device_ctx, data_id_str, record);
-    }
-    FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
-      const Blob* cur_blob = GetBlob(blob_id);
-      const PrintRecordConf& cur_dump_conf = conf.in(blob_id);
-      std::string field_name = cur_dump_conf.lbn();
-      if (cur_dump_conf.has_name()) { field_name = cur_dump_conf.name(); }
-      CHECK(record.feature().find(field_name) == record.feature().end())
-          << "Field " << field_name << " found repeatedly in OfRecord";
-      int64_t one_col_elem_num =
-          cur_blob->shape().Count(1) * cur_blob->shape().At(0) / min_record_num;
-      Feature& feature = (*(record.mutable_feature()))[field_name];
-      GetOFRecordEncoder(cur_dump_conf.encode_case().encode_case(), cur_blob->data_type())
-          ->EncodeOneCol(ctx.device_ctx, cur_blob, record_id * one_col_elem_num, feature,
-                         field_name, one_col_elem_num);
-    }
-    *out_stream_ << record;
+  FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
+    const Blob* cur_blob = GetBlob(blob_id);
+    const PrintRecordConf& cur_dump_conf = conf.in(blob_id);
+    std::string field_name = cur_dump_conf.lbn();
+    if (cur_dump_conf.has_name()) { field_name = cur_dump_conf.name(); }
+    CHECK(record.feature().find(field_name) == record.feature().end())
+        << "Field " << field_name << " found repeatedly in OfRecord";
+    int64_t one_col_elem_num = cur_blob->shape().elem_cnt();
+    Feature& feature = (*(record.mutable_feature()))[field_name];
+    GetOFRecordEncoder(cur_dump_conf.encode_case().encode_case(), cur_blob->data_type())
+        ->EncodeOneCol(ctx.device_ctx, cur_blob, 0, feature, field_name, one_col_elem_num);
   }
+  *out_stream_ << record;
   out_stream_->Flush();
 }
 
