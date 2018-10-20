1a332524a0d557823c2bf8cc7d3cf958a106796c
leaves-zwx <kunta0932@gmail.com>
2018-10-20 16:44:39 +0800

    moidfy in and out blob_desc

--- oneflow/core/operator/proposal_target_op.cpp
+++ oneflow/core/operator/proposal_target_op.cpp
@@ -6,11 +6,11 @@ void ProposalTargetOp::InitFromOpConf() {
   CHECK_EQ(this->device_type(), DeviceType::kCPU);
   CHECK(op_conf().has_proposal_target_conf());
   // Enroll input
-  EnrollInputBn("rpn_rois", false);
-  EnrollPbInputBn("gt_boxes");
-  EnrollPbInputBn("gt_labels");
+  EnrollInputBn("rois", false);
+  EnrollInputBn("gt_boxes", false);
+  EnrollInputBn("gt_labels", false);
   // Enroll output
-  EnrollOutputBn("rois", false);
+  EnrollOutputBn("out_rois", false);
   EnrollOutputBn("labels", false);
   EnrollOutputBn("bbox_targets", false);
   EnrollOutputBn("bbox_inside_weights", false);
@@ -29,38 +29,46 @@ void ProposalTargetOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
   const ProposalTargetOpConf& conf = op_conf().proposal_target_conf();
-  // TODO: Check conf
-  // input: rpn_rois (n, r, 4) T
-  const BlobDesc* rpn_rois_blob_desc = GetBlobDesc4BnInOp("rpn_rois");
-  // input: gt_boxes (n) FloatList16 (r * 4)
+  // input: rois (r, 5) T
+  const BlobDesc* rois_blob_desc = GetBlobDesc4BnInOp("rois");
+  // input: gt_boxes (n, g, 4)
   const BlobDesc* gt_boxes_blob_desc = GetBlobDesc4BnInOp("gt_boxes");
-  // input: gt_labels (n) Int32List16 (r)
+  // input: gt_labels (n, g)
   const BlobDesc* gt_labels_blob_desc = GetBlobDesc4BnInOp("gt_labels");
-  int64_t image_num = rpn_rois_blob_desc->shape().At(0);
-  CHECK_EQ(image_num, gt_boxes_blob_desc->shape().At(0));
-  CHECK_EQ(image_num, gt_labels_blob_desc->shape().At(0));
-  int64_t rois_num = rpn_rois_blob_desc->shape().At(1);
-  int64_t output_num = conf.num_rois_per_image();
-  int64_t class_num = conf.num_classes();
-  int64_t max_gt_boxes_num = conf.max_gt_boxes_num();
-  DataType data_type = rpn_rois_blob_desc->data_type();
-  // output: rois (n, output_num, 4) T
-  BlobDesc* rois_blob_desc = GetBlobDesc4BnInOp("rois");
-  rois_blob_desc->mut_shape() = Shape({image_num, output_num, 4});
-  rois_blob_desc->set_data_type(data_type);
-  rois_blob_desc->set_has_data_id_field(rpn_rois_blob_desc->has_data_id_field());
-  // output: labels (n * output_num) int32_t
+  CHECK_EQ(rois_blob_desc->data_type(), gt_boxes_blob_desc->data_type());
+  CHECK(gt_boxes_blob_desc->has_dim1_valid_num_field());
+  CHECK(gt_labels_blob_desc->has_dim1_valid_num_field());
+  const int64_t num_images = gt_boxes_blob_desc->shape().At(0);
+  CHECK_EQ(gt_labels_blob_desc->shape().At(0), num_images);
+  const int64_t num_rois = rois_blob_desc->shape().At(0);
+  //const int64_t num_sampled_rois = conf.num_sampled_rois_per_image();
+  const int64_t total_num_sampled_rois = conf.num_sampled_rois_per_image() * num_images;
+  CHECK_GE(total_num_sampled_rois, num_rois);
+  const int64_t num_classes = conf.num_classes();
+
+  // output: rois (total_num_sampled_rois, 5) T
+  BlobDesc* out_rois_blob_desc = GetBlobDesc4BnInOp("out_rois");
+  out_rois_blob_desc->mut_shape() = Shape({total_num_sampled_rois, 5});
+  out_rois_blob_desc->set_data_type(rois_blob_desc->data_type());
+  out_rois_blob_desc->mut_dim0_inner_shape() = Shape({1, total_num_sampled_rois});
+  out_rois_blob_desc->set_has_dim0_valid_num_field(true);
+  // output: labels (total_num_sampled_rois) int32_t
   BlobDesc* labels_blob_desc = GetBlobDesc4BnInOp("labels");
-  labels_blob_desc->mut_shape() = Shape({image_num * output_num});
+  labels_blob_desc->mut_shape() = Shape({total_num_sampled_rois});
   labels_blob_desc->set_data_type(DataType::kInt32);
-  // output: bbox_targets (n * output_num, class_num * 4) T
+  labels_blob_desc->mut_dim0_inner_shape() = Shape({1, total_num_sampled_rois});
+  labels_blob_desc->set_has_dim0_valid_num_field(true);
+  // output: bbox_targets (total_num_sampled_rois, num_classes * 4) T
   BlobDesc* bbox_targets_blob_desc = GetBlobDesc4BnInOp("bbox_targets");
-  bbox_targets_blob_desc->mut_shape() = Shape({image_num * output_num, class_num * 4});
-  bbox_targets_blob_desc->set_data_type(data_type);
-  // output: bbox_targets (n * output_num, class_num * 4) T
+  bbox_targets_blob_desc->mut_shape() = Shape({total_num_sampled_rois, num_classes * 4});
+  bbox_targets_blob_desc->set_data_type(rois_blob_desc->data_type());
+  bbox_targets_blob_desc->mut_dim0_inner_shape() = Shape({1, total_num_sampled_rois});
+  bbox_targets_blob_desc->set_has_dim0_valid_num_field(true);
+  // output: bbox_inside_weights (total_num_sampled_rois, num_classes * 4) T
   *GetBlobDesc4BnInOp("bbox_inside_weights") = *bbox_targets_blob_desc;
-  // output: bbox_targets (n * output_num, class_num * 4) T
+  // output: bbox_outside_weights (total_num_sampled_rois, num_classes * 4) T
   *GetBlobDesc4BnInOp("bbox_outside_weights") = *bbox_targets_blob_desc;
+
   // data tmp: boxes_index (rois_num + max_gt_boxes_num) int32_t
   BlobDesc* boxes_index_blob_desc = GetBlobDesc4BnInOp("boxes_index");
   boxes_index_blob_desc->mut_shape() = Shape({rois_num + max_gt_boxes_num});
