aaa3719525acd956439435e678410c18ff669927
Li Xinqi <lixinqi2010@gmail.com>
2018-10-21 20:16:52 +0800

    Dev record idx in device piece (#1358)
    * record_idx_in_device_piece
    
    * let define test blob support record_idx_in_device_piece
    
    * call ForwardRecordIdxInDevicePiece in Foward
    
    
    Former-commit-id: 6028dfad3c6d72add6c21ad31df883129446c039

--- oneflow/core/register/runtime_blob_desc.h
+++ oneflow/core/register/runtime_blob_desc.h
@@ -28,6 +28,7 @@ class RtBlobDesc {
   bool has_dim0_valid_num_field() const;
   bool has_dim1_valid_num_field() const;
   bool has_dim2_valid_num_field() const;
+  bool has_record_idx_in_device_piece_field() const;
   const StructPodDesc& header_pod_desc() const { return header_pod_desc_; }
 
   int32_t max_col_num() const { return blob_desc_proto_.header().max_col_num(); }
@@ -41,6 +42,7 @@ class RtBlobDesc {
   size_t ByteSizeOfDim0ValidNumField() const;
   size_t ByteSizeOfDim1ValidNumField() const;
   size_t ByteSizeOfDim2ValidNumField() const;
+  size_t ByteSizeOfRecordIdxInDevicePieceField() const;
   size_t ByteSizeOfDataContentField() const;
 
   bool operator==(const RtBlobDesc& rhs) const;
