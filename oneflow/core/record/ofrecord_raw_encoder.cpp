8de00531d51ee59d69a506d2ea11317ce6b67826
Xinqi <lixinqi0703106@163.com>
2018-10-22 22:19:07 +0800

    elem_num

--- oneflow/core/record/ofrecord_raw_encoder.cpp
+++ oneflow/core/record/ofrecord_raw_encoder.cpp
@@ -7,12 +7,12 @@ void OFRecordEncoderImpl<EncodeCase::kRaw, T>::EncodeOneCol(DeviceCtx* ctx, cons
                                                             int64_t in_offset, Feature& feature,
                                                             const std::string& field_name,
                                                             int64_t one_col_elem_num) const {
+  const auto& shape = in_blob->shape();
   CHECK(in_blob->dim2_valid_num_ptr() == nullptr);
-  CHECK_EQ(one_col_elem_num, in_blob->shape().Count(1));
+  CHECK_EQ(one_col_elem_num, shape.Count(1));
   CHECK_EQ(in_offset % one_col_elem_num, 0);
   int64_t dim0_idx = in_offset / one_col_elem_num;
-  int64_t dim1_elem_num = in_blob->shape().NumAxes() == 1 ? 1 : in_blob->shape().Count(2);
-  int64_t elem_num = in_blob->dim1_valid_num(dim0_idx) * dim1_elem_num;
+  int64_t elem_num = shape.NumAxes() == 1 ? 1 : in_blob->dim1_valid_num(dim0_idx) * shape.Count(2);
 
   const T* in_dptr = in_blob->dptr<T>() + in_offset;
   DataType data_type = GetDataType<T>();
