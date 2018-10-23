8fcfeca74fa649766415ce0fba52f9777a73549a
Juncheng <liujuncheng1022@gmail.com>
2018-10-23 16:07:07 +0800

    Dev faster rcnn mask target (#1367)
    * mask_target_op
    
    * record_idx
    
    * refine
    
    * rle util

---
+++ oneflow/core/kernel/mask_target_kernel.h
@@ -0,0 +1,43 @@
+#ifndef ONEFLOW_CORE_KERNEL_MASK_TARGET_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_MASK_TARGET_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+#include "oneflow/core/kernel/kernel_context.h"
+#include "oneflow/core/record/coco.pb.h"
+#include "oneflow/core/kernel/bbox_util.h"
+
+namespace oneflow {
+
+template<typename T>
+class MaskTargetKernel final : public KernelIf<DeviceType::kCPU> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(MaskTargetKernel);
+  MaskTargetKernel() = default;
+  ~MaskTargetKernel() override = default;
+
+ private:
+  using RoiBBox = BBoxImpl<T, BBoxCategory::kIndexCorner>;
+  using SegmBBox = BBoxImpl<float, BBoxCategory::kCorner>;
+  void ForwardDataContent(const KernelCtx&,
+                          std::function<Blob*(const std::string&)>) const override;
+  void ForwardDim0ValidNum(const KernelCtx& ctx,
+                           std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+  void ForwardRecordIdxInDevicePiece(
+      const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+  void InitConstBufBlobs(DeviceCtx*,
+                         std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+
+  void ParseSegmPolygonLists(const Blob* gt_segms,
+                             std::vector<std::vector<PolygonList>>* segms) const;
+  void ComputeSegmBBoxes(const std::vector<std::vector<PolygonList>>& segms, Blob* bboxes) const;
+  void Segm2BBox(const PolygonList& segm, SegmBBox* bbox) const;
+  void Segm2Mask(const PolygonList& segm, const RoiBBox& fg_roi, size_t mask_h, size_t mask_w,
+                 T* mask) const;
+  void Polygon2BBox(const FloatList& polygon, SegmBBox* bbox) const;
+  size_t GetMaxOverlapIndex(const RoiBBox& fg_roi, const SegmBBox* gt_bboxs,
+                            size_t gt_bboxs_num) const;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_MASK_TARGET_KERNEL_H_
--- oneflow/core/kernel/rle_util.h
+++ oneflow/core/kernel/rle_util.h
@@ -6,7 +6,13 @@
 
 namespace oneflow {
 
-size_t RleEncode(uint32_t* buf, const uint8_t* mask, size_t h, size_t w);
-}
+namespace RleUtil {
+
+size_t Encode(uint32_t* buf, const uint8_t* mask, size_t h, size_t w);
+void PolygonXy2ColMajorMask(const double* xy, size_t num_xy, size_t h, size_t w, uint8_t* mask);
+
+}  // namespace RleUtil
+
+}  // namespace oneflow
 
 #endif  // ONEFLOW_CORE_KERNEL_RLE_H_
