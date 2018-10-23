9b0e6bb57c928ee1c8acccabc2cb53de50697022
leaves-zwx <kunta0932@gmail.com>
2018-10-23 15:47:48 +0800

    add record idx for fpn_collect (#1368)

--- oneflow/core/kernel/fpn_collect_kernel.h
+++ oneflow/core/kernel/fpn_collect_kernel.h
@@ -21,6 +21,8 @@ class FpnCollectKernel final : public KernelIf<DeviceType::kCPU> {
                           std::function<Blob*(const std::string&)>) const override;
   void ForwardDim0ValidNum(const KernelCtx& ctx,
                            std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+  void ForwardRecordIdxInDevicePiece(
+      const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
 };
 
 }  // namespace oneflow
