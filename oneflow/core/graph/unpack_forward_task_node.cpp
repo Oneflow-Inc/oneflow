bd67d3e53a115e112885a8327561b856918ccc4b
Juncheng <liujuncheng1022@gmail.com>
2018-10-27 19:15:03 +0800

    unpack

--- oneflow/core/graph/unpack_forward_task_node.cpp
+++ oneflow/core/graph/unpack_forward_task_node.cpp
@@ -1,5 +1,6 @@
 #include "oneflow/core/graph/unpack_forward_task_node.h"
 #include "oneflow/core/graph/logical_node.h"
+#include "oneflow/core/operator/unpack_op.h"
 
 namespace oneflow {
 
@@ -32,9 +33,10 @@ void UnpackForwardCompTaskNode::InferProducedDataRegstTimeShape() {
   std::shared_ptr<RegstDesc> out_regst = GetProducedRegst("out");
   std::vector<int64_t> time_shape_dim_vec(in_regst->data_regst_time_shape()->dim_vec());
 
-  std::shared_ptr<const Operator> op = logical_node()->SoleOp();
+  const UnpackOp* op = dynamic_cast<UnpackOp*>(logical_node()->SoleOp().get());
+  CHECK(op != nullptr);
   int64_t in_piece_size = in_regst->GetBlobDesc(op->BnInOp2Lbi("in"))->shape().At(0);
-  int64_t unpack_num = op->op_conf().unpack_conf().unpack_num();
+  int64_t unpack_num = op->GetUnpackNum(*parallel_ctx());
   CHECK_EQ(0, in_piece_size % unpack_num);
   time_shape_dim_vec.push_back(unpack_num);
   *out_regst->mut_data_regst_time_shape() = std::make_shared<Shape>(std::move(time_shape_dim_vec));
