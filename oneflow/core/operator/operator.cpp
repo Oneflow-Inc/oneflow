3332365f89eb0e9351ce45f9f912e27a27d407fc
Li Xinqi <lixinqi2010@gmail.com>
2018-10-29 17:15:34 +0800

    Dev op only cpu supported (#1430)
    * NeedBackwardIfBlobEmpty (#1387)
    
    * IsOpOnlyCpuSupported
    
    * refine REGISTER_OP_CREATOR
    
    * refine REGISTER_CLASS_WITH_ARGS
    
    
    Former-commit-id: 54ea7debb6e273ae5cb6570463b4e47fb023180b

--- oneflow/core/operator/operator.cpp
+++ oneflow/core/operator/operator.cpp
@@ -454,8 +454,15 @@ std::pair<std::string, int32_t> GenUnRepeatedBn(const std::string& bn) {
   return std::make_pair(prefix, idx);
 }
 
+bool IsOpOnlyCpuSupported(OperatorConf::OpTypeCase op_type_case) {
+  return *std::unique_ptr<OnlyCpuSupportPredicator>(NewObj<OnlyCpuSupportPredicator>(op_type_case));
+}
+
 std::shared_ptr<Operator> ConstructOp(const OperatorConf& op_conf) {
   Operator* rptr = NewObj<Operator>(op_conf.op_type_case(), op_conf);
+  if (IsOpOnlyCpuSupported(op_conf.op_type_case())) {
+    CHECK_EQ(op_conf.device_type(), DeviceType::kCPU);
+  }
   rptr->InitFromOpConf(op_conf);
   return std::shared_ptr<Operator>(rptr);
 }
