03cc0d9958f5d6c618b3b33420edddb86f8f6d01
Juncheng <liujuncheng1022@gmail.com>
2018-10-29 16:19:22 +0800

    CHECK time_shape

--- oneflow/core/actor/repeat_forward_compute_actor.cpp
+++ oneflow/core/actor/repeat_forward_compute_actor.cpp
@@ -3,13 +3,17 @@
 namespace oneflow {
 
 void RepeatForwardCompActor::VirtualCompActorInit(const TaskProto& proto) {
-  CHECK_EQ(proto.exec_sequence().exec_node().size(), 1);
-  const KernelConf& kernel_conf = proto.exec_sequence().exec_node().Get(0).kernel_conf();
-  CHECK(kernel_conf.op_attribute().op_conf().has_repeat_conf());
+  const Shape& in_time_shape = Global<RegstMgr>::Get()
+                                   ->RegstDesc4RegstDescId(Name2SoleRegstDescId("in"))
+                                   .data_regst_time_shape();
   const Shape& out_time_shape = Global<RegstMgr>::Get()
                                     ->RegstDesc4RegstDescId(Name2SoleRegstDescId("out"))
                                     .data_regst_time_shape();
   CHECK_GE(out_time_shape.NumAxes(), 1);
+  CHECK_EQ(in_time_shape.NumAxes() + 1, out_time_shape.NumAxes());
+  FOR_RANGE(int64_t, i, 0, in_time_shape.NumAxes()) {
+    CHECK_EQ(in_time_shape.At(i), out_time_shape.At(i));
+  }
   repeat_num_ = out_time_shape.At(out_time_shape.NumAxes() - 1);
   repeat_count_ = 0;
   cur_piece_id_ = 0;
