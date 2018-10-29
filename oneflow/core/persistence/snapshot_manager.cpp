ded52c7532771e9ed95a2794316c07d6d12de0a8
Juncheng <liujuncheng1022@gmail.com>
2018-10-29 12:30:04 +0800

    revert snapshot manager

--- oneflow/core/persistence/snapshot_manager.cpp
+++ oneflow/core/persistence/snapshot_manager.cpp
@@ -9,7 +9,7 @@ SnapshotMgr::SnapshotMgr(const Plan& plan) {
     model_save_snapshots_path_ = Global<JobDesc>::Get()->MdSaveSnapshotsPath();
     OfCallOnce(model_save_snapshots_path_, SnapshotFS(), &fs::FileSystem::MakeEmptyDir);
   }
-  std::string load_path = Global<JobDesc>::Get()->MdLoadSnapshotPath();
+  const std::string& load_path = Global<JobDesc>::Get()->MdLoadSnapshotPath();
   if (load_path != "") { readable_snapshot_.reset(new Snapshot(load_path)); }
   total_mbn_num_ = plan.total_mbn_num();
 }
