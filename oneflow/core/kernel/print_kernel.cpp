d9ae91980fa5fa0b675a31b973cb3ff9cfa1c389
Juncheng <liujuncheng1022@gmail.com>
2018-11-01 12:18:02 +0800

    Refine print op (#1442)
    * refine
    
    * format
    
    * refine
    
    * refine
    
    * refine
    
    * refine
    
    * refine
    
    * refine
    
    * fix
    
    * revert print op
    
    * CHECK record_id_in_device_piece

--- oneflow/core/kernel/print_kernel.cpp
+++ oneflow/core/kernel/print_kernel.cpp
@@ -4,68 +4,6 @@
 
 namespace oneflow {
 
-struct RecordOffsets {
-  HashMap<int64_t, std::vector<int64_t>> blob_id2offsets;
-};
-
-namespace {
-
-void SplitBns(PbRpf<std::string>* bns_with_record_ids, PbRpf<std::string>* bns_without_record_ids,
-              const std::function<Blob*(const std::string&)> BnInOp2Blob,
-              const PbRpf<std::string>& bns) {
-  for (const auto& bn : bns) {
-    if (BnInOp2Blob(bn)->has_record_id_in_device_piece_field()) {
-      *bns_with_record_ids->Add() = bn;
-    } else {
-      *bns_without_record_ids->Add() = bn;
-    }
-  }
-}
-
-void CheckSameDim0Size(const PbRpf<std::string>& bns,
-                       const std::function<Blob*(const std::string&)> BnInOp2Blob) {
-  FOR_RANGE(int32_t, i, 1, bns.size()) {
-    CHECK_EQ(BnInOp2Blob(bns.Get(0))->shape().At(0), BnInOp2Blob(bns.Get(i))->shape().At(0));
-  }
-}
-
-void CheckRecordIdInDevicePieceIsValid(const Blob* blob, size_t max_size) {
-  FOR_RANGE(int64_t, i, 0, blob->shape().At(0)) {
-    CHECK_LT(blob->record_id_in_device_piece(i), max_size);
-  }
-}
-
-void CheckRecordIds(const PbRpf<std::string>& bns_with_record_ids,
-                    const PbRpf<std::string>& bns_without_record_ids,
-                    const std::function<Blob*(const std::string&)> BnInOp2Blob) {
-  CHECK(!(bns_with_record_ids.empty() && bns_without_record_ids.empty()));
-  if (bns_without_record_ids.size() > 0 && bns_with_record_ids.size() > 0) {
-    size_t max_size = BnInOp2Blob(bns_without_record_ids.Get(0))->shape().At(0);
-    CheckRecordIdInDevicePieceIsValid(BnInOp2Blob(bns_with_record_ids.Get(0)), max_size);
-  }
-}
-
-void InitRecordOffsets(std::map<int64_t, RecordOffsets>* record_id2record_offsets,
-                       const std::function<Blob*(const std::string&)> BnInOp2Blob,
-                       const PbRpf<std::string>& bns) {
-  PbRpf<std::string> bns_with_record_ids;
-  PbRpf<std::string> bns_without_record_ids;
-  SplitBns(&bns_with_record_ids, &bns_without_record_ids, BnInOp2Blob, bns);
-  CheckSameRecordIdInDevicePiece(bns_with_record_ids, BnInOp2Blob);
-  CheckSameDim0Size(bns_without_record_ids, BnInOp2Blob);
-  CheckRecordIds(bns_with_record_ids, bns_without_record_ids, BnInOp2Blob);
-  FOR_RANGE(int64_t, blob_id, 0, bns.size()) {
-    const Blob* blob = BnInOp2Blob(bns.Get(blob_id));
-    FOR_RANGE(int64_t, i, 0, blob->shape().At(0)) {
-      int64_t record_id = blob->record_id_in_device_piece(i);
-      std::vector<int64_t>* vec = &(*record_id2record_offsets)[record_id].blob_id2offsets[blob_id];
-      vec->push_back(i * blob->shape().Count(1));
-    }
-  }
-}
-
-}  // namespace
-
 void PrintKernel::VirtualKernelInit(const ParallelContext* parallel_ctx) {
   const auto& conf = op_conf().print_conf();
   const std::string& root_path = conf.print_dir();
@@ -80,37 +18,50 @@ void PrintKernel::VirtualKernelInit(const ParallelContext* parallel_ctx) {
 
 void PrintKernel::Forward(const KernelCtx& ctx,
                           std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  if (HasEmptyShapeBlob(this->op_attribute().input_bns(), BnInOp2Blob)) { return; }
-  std::map<int64_t, RecordOffsets> record_id2record_offsets;
-  InitRecordOffsets(&record_id2record_offsets, BnInOp2Blob, this->op_attribute().input_bns());
-  auto GetBlob = [&](int64_t blob_id) -> Blob* {
+  CHECK(!this->op_attribute().input_bns().empty());
+  auto GetBlob = [&](int32_t blob_id) -> Blob* {
     return BnInOp2Blob(this->op_attribute().input_bns(blob_id));
   };
   const auto& conf = op_conf().print_conf();
-  OFRecord record;
-  for (const auto& record_id7record_offsets : record_id2record_offsets) {
-    record.clear_feature();
-    for (const auto& pair : record_id7record_offsets.second.blob_id2offsets) {
-      int64_t blob_id = pair.first;
-      const std::vector<int64_t>& offsets = pair.second;
+  const int32_t blob_num = op_attribute().input_bns().size();
+  const Blob* first_blob = GetBlob(0);
+  const int64_t record_num = first_blob->shape().At(0);
+  bool has_data_id_field = first_blob->has_data_id_field();
+  bool has_col_num_field = first_blob->has_col_num_field();
+  if (has_col_num_field) { TODO(); }
+  FOR_RANGE(int32_t, blob_id, 1, blob_num) {
+    const Blob* cur_blob = GetBlob(blob_id);
+    CHECK_EQ(cur_blob->shape().At(0), record_num);
+    CHECK_EQ(cur_blob->has_data_id_field(), has_data_id_field);
+    CHECK_EQ(cur_blob->has_col_num_field(), has_col_num_field);
+    FOR_RANGE(int32_t, record_id, 0, record_num) {
+      CHECK_EQ(first_blob->record_id_in_device_piece(record_id),
+               cur_blob->record_id_in_device_piece(record_id));
+    }
+  }
+
+  FOR_RANGE(int64_t, record_id, 0, record_num) {
+    OFRecord record;
+    if (has_data_id_field) {
+      const char* data_id_str = first_blob->data_id(record_id);
+      FOR_RANGE(int32_t, blob_id, 1, op_attribute().input_bns().size()) {
+        CHECK_STREQ(data_id_str, GetBlob(blob_id)->data_id(record_id));
+      }
+      if (*data_id_str == '\0') { break; }
+      OFRecordEncoderIf::EncodeOneDataId(ctx.device_ctx, data_id_str, record);
+    }
+    FOR_RANGE(int32_t, blob_id, 0, blob_num) {
       const Blob* cur_blob = GetBlob(blob_id);
       const PrintRecordConf& cur_print_conf = conf.in(blob_id);
-      std::string field_name = cur_print_conf.lbn();
-      if (cur_print_conf.has_name()) { field_name = cur_print_conf.name(); }
+      const std::string field_name =
+          cur_print_conf.has_name() ? cur_print_conf.name() : cur_print_conf.lbn();
       CHECK(record.feature().find(field_name) == record.feature().end())
           << "Field " << field_name << " found repeatedly in OfRecord";
       int64_t one_col_elem_num = cur_blob->shape().Count(1);
       Feature& feature = (*(record.mutable_feature()))[field_name];
-      if (cur_blob->has_record_id_in_device_piece_field()) {
-        GetOFRecordEncoder(cur_print_conf.encode_case().encode_case(), cur_blob->data_type())
-            ->EncodeMultiCol(ctx.device_ctx, cur_blob, offsets, feature, field_name,
-                             one_col_elem_num);
-      } else {
-        CHECK_EQ(offsets.size(), 1);
-        GetOFRecordEncoder(cur_print_conf.encode_case().encode_case(), cur_blob->data_type())
-            ->EncodeOneCol(ctx.device_ctx, cur_blob, offsets.at(0), feature, field_name,
-                           one_col_elem_num);
-      }
+      GetOFRecordEncoder(cur_print_conf.encode_case().encode_case(), cur_blob->data_type())
+          ->EncodeOneCol(ctx.device_ctx, cur_blob, record_id * one_col_elem_num, feature,
+                         field_name, one_col_elem_num);
     }
     *out_stream_ << record;
   }
