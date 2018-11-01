3f78560941d06e1ac6f0ee47e85f5ee9298b7895
Juncheng <liujuncheng1022@gmail.com>
2018-11-01 12:09:05 +0800

    CHECK record_id_in_device_piece

--- oneflow/core/kernel/print_kernel.cpp
+++ oneflow/core/kernel/print_kernel.cpp
@@ -18,26 +18,30 @@ void PrintKernel::VirtualKernelInit(const ParallelContext* parallel_ctx) {
 
 void PrintKernel::Forward(const KernelCtx& ctx,
                           std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  if (HasEmptyShapeBlob(this->op_attribute().input_bns(), BnInOp2Blob)) { return; }
-  auto GetBlob = [&](int64_t blob_id) -> Blob* {
+  CHECK(!this->op_attribute().input_bns().empty());
+  auto GetBlob = [&](int32_t blob_id) -> Blob* {
     return BnInOp2Blob(this->op_attribute().input_bns(blob_id));
   };
   const auto& conf = op_conf().print_conf();
-  int32_t total_blob_num = op_attribute().input_bns().size();
+  const int32_t blob_num = op_attribute().input_bns().size();
   const Blob* first_blob = GetBlob(0);
-  int64_t max_record_num = first_blob->shape().At(0);
+  const int64_t record_num = first_blob->shape().At(0);
   bool has_data_id_field = first_blob->has_data_id_field();
   bool has_col_num_field = first_blob->has_col_num_field();
   if (has_col_num_field) { TODO(); }
-  FOR_RANGE(int32_t, blob_id, 1, total_blob_num) {
+  FOR_RANGE(int32_t, blob_id, 1, blob_num) {
     const Blob* cur_blob = GetBlob(blob_id);
-    CHECK_EQ(cur_blob->shape().At(0), max_record_num);
+    CHECK_EQ(cur_blob->shape().At(0), record_num);
     CHECK_EQ(cur_blob->has_data_id_field(), has_data_id_field);
     CHECK_EQ(cur_blob->has_col_num_field(), has_col_num_field);
+    FOR_RANGE(int32_t, record_id, 0, record_num) {
+      CHECK_EQ(first_blob->record_id_in_device_piece(record_id),
+               cur_blob->record_id_in_device_piece(record_id));
+    }
   }
-  OFRecord record;
-  FOR_RANGE(int64_t, record_id, 0, max_record_num) {
-    record.clear_feature();
+
+  FOR_RANGE(int64_t, record_id, 0, record_num) {
+    OFRecord record;
     if (has_data_id_field) {
       const char* data_id_str = first_blob->data_id(record_id);
       FOR_RANGE(int32_t, blob_id, 1, op_attribute().input_bns().size()) {
@@ -46,11 +50,11 @@ void PrintKernel::Forward(const KernelCtx& ctx,
       if (*data_id_str == '\0') { break; }
       OFRecordEncoderIf::EncodeOneDataId(ctx.device_ctx, data_id_str, record);
     }
-    FOR_RANGE(int32_t, blob_id, 0, total_blob_num) {
+    FOR_RANGE(int32_t, blob_id, 0, blob_num) {
       const Blob* cur_blob = GetBlob(blob_id);
       const PrintRecordConf& cur_print_conf = conf.in(blob_id);
-      std::string field_name = cur_print_conf.lbn();
-      if (cur_print_conf.has_name()) { field_name = cur_print_conf.name(); }
+      const std::string field_name =
+          cur_print_conf.has_name() ? cur_print_conf.name() : cur_print_conf.lbn();
       CHECK(record.feature().find(field_name) == record.feature().end())
           << "Field " << field_name << " found repeatedly in OfRecord";
       int64_t one_col_elem_num = cur_blob->shape().Count(1);
