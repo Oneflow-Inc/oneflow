d0bb18c15765964a6f24afc18a1fa843d77e47f5
Niu Chong <niuchong893184@gmail.com>
2018-11-01 15:06:44 +0800

    feat: add must_run_experimentally in OtherConf (#1437)
    * feat: add must_run_experimentally in OtherConf
    
    * refactor: add ExperimentalRunConf in JobConf
    
    * refactor: remove HasRelayPlacement

--- oneflow/core/job/job_desc.h
+++ oneflow/core/job/job_desc.h
@@ -37,6 +37,7 @@ class JobDesc final {
   bool IsPredict() const { return job_conf_.other().has_predict_conf(); }
   int64_t PieceSize() const { return job_conf_.other().piece_size(); }
   int64_t piece_num_of_experiment_phase() const;
+  bool enable_experiment_run() const;
   float available_zone_mem_ratio() const;
   size_t persistence_buf_byte() const;
   size_t reserved_host_mem_byte() const;
