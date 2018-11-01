812ef187bb28bb72e0637245a36a0fb8d39b51c0
Juncheng <liujuncheng1022@gmail.com>
2018-11-01 14:52:29 +0800

    TargetSize and ExpendTo

--- oneflow/core/record/image_preprocess.h
+++ oneflow/core/record/image_preprocess.h
@@ -50,10 +50,28 @@ class ImagePreprocessImpl<PreprocessCase::kMirror> final : public ImagePreproces
                     std::function<int32_t(void)> NextRandomInt) const override;
 };
 
-#define PREPROCESS_CASE_SEQ                     \
-  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kResize) \
-  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kMirror) \
-  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kCrop)
+template<>
+class ImagePreprocessImpl<PreprocessCase::kTargetResize> final : public ImagePreprocessIf {
+ public:
+ private:
+  void DoPreprocess(cv::Mat* image, const ImagePreprocess& preprocess_conf,
+                    std::function<int32_t(void)> NextRandomInt) const override;
+};
+
+template<>
+class ImagePreprocessImpl<PreprocessCase::kExpendTo> final : public ImagePreprocessIf {
+ public:
+ private:
+  void DoPreprocess(cv::Mat* image, const ImagePreprocess& preprocess_conf,
+                    std::function<int32_t(void)> NextRandomInt) const override;
+};
+
+#define PREPROCESS_CASE_SEQ                           \
+  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kResize)       \
+  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kMirror)       \
+  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kCrop)         \
+  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kTargetResize) \
+  OF_PP_MAKE_TUPLE_SEQ(PreprocessCase::kExpendTo)
 
 ImagePreprocessIf* GetImagePreprocess(PreprocessCase);
 
