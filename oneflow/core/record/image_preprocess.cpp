5d034a39e66092c7dc482126f57134eaeaf58b8e
cheng cheng <472491134@qq.com>
2018-11-06 18:30:16 +0800

    Dev crop with random size (#1468)
    * random size crop proto
    
    * ImagePreprocessImpl::<kCropWithRandomSize>
    
    * clang format
    
    * MaxVal
    
    
    Former-commit-id: c027432320cc0f03248f9165994150fce058f00a

--- oneflow/core/record/image_preprocess.cpp
+++ oneflow/core/record/image_preprocess.cpp
@@ -2,6 +2,15 @@
 
 namespace oneflow {
 
+namespace {
+
+float GetRandomFloatValue(float min, float max, std::function<int32_t(void)> NextRandomInt) {
+  float ratio = static_cast<float>(NextRandomInt()) / static_cast<float>(MaxVal<int32_t>());
+  return (max - min) * ratio + min;
+}
+
+}  // namespace
+
 void ImagePreprocessImpl<PreprocessCase::kResize>::DoPreprocess(
     cv::Mat* image, const ImagePreprocess& preprocess_conf,
     std::function<int32_t(void)> NextRandomInt) const {
@@ -24,13 +33,47 @@ void ImagePreprocessImpl<PreprocessCase::kCrop>::DoPreprocess(
   CHECK_LE(width, image->cols);
   CHECK_LE(height, image->rows);
   if (crop.random_xy()) {
-    x = NextRandomInt() % (image->cols - width);
-    y = NextRandomInt() % (image->rows - height);
+    int32_t x_max = (image->cols - width);
+    int32_t y_max = (image->rows - height);
+    x = x_max > 0 ? (NextRandomInt() % x_max) : x_max;
+    y = y_max > 0 ? (NextRandomInt() % y_max) : y_max;
   } else {
     CHECK_LE(x, image->cols - width);
     CHECK_LE(y, image->rows - height);
   }
   *image = (*image)(cv::Rect(x, y, width, height));
+}
+
+void ImagePreprocessImpl<PreprocessCase::kCropWithRandomSize>::DoPreprocess(
+    cv::Mat* image, const ImagePreprocess& preprocess_conf,
+    std::function<int32_t(void)> NextRandomInt) const {
+  CHECK(preprocess_conf.has_crop_with_random_size());
+  const ImageCropWithRandomSize& conf = preprocess_conf.crop_with_random_size();
+  int32_t max_attempts = conf.max_attempts();
+  float area_min = conf.area_range().min();
+  float area_max = conf.area_range().max();
+  float ratio_min = conf.aspect_ratio_range().min();
+  float ratio_max = conf.aspect_ratio_range().max();
+  CHECK_LE(area_min, area_max);
+  CHECK_GT(area_min, 0.0);
+  CHECK_LE(area_max, 1.0);
+  CHECK_LE(ratio_min, ratio_max);
+  CHECK_GT(ratio_min, 0.0);
+  while (max_attempts--) {
+    float area_size = GetRandomFloatValue(area_min, area_max, NextRandomInt) * image->total();
+    float aspect_ratio = GetRandomFloatValue(ratio_min, ratio_max, NextRandomInt);
+    float height_float = sqrt(area_size / aspect_ratio);
+    int32_t height = static_cast<int32_t>(height_float);
+    int32_t width = static_cast<int32_t>(height_float * aspect_ratio);
+    if (width <= image->cols && height <= image->rows) {
+      int32_t x_max = (image->cols - width);
+      int32_t y_max = (image->rows - height);
+      int32_t x = x_max > 0 ? (NextRandomInt() % x_max) : x_max;
+      int32_t y = y_max > 0 ? (NextRandomInt() % y_max) : y_max;
+      *image = (*image)(cv::Rect(x, y, width, height));
+      return;
+    }
+  }
 }
 
 void ImagePreprocessImpl<PreprocessCase::kMirror>::DoPreprocess(
