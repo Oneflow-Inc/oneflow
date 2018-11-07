dc289c39779838fdcb9de8ad34645711f870d911
Juncheng <liujuncheng1022@gmail.com>
2018-11-07 14:20:14 +0800

    data_id/col_num

--- oneflow/core/operator/clone_op.cpp
+++ oneflow/core/operator/clone_op.cpp
@@ -31,6 +31,8 @@ void CloneOp::VirtualGenKernelConf(
                     kernel_conf->mutable_op_attribute()->mutable_output_diff_bns());
   if (!kernel_conf->is_forward()) {
     const BlobDesc* in_diff_blob_desc = GetBlobDesc4BnInOp(SoleIdbn());
+    if (in_diff_blob_desc->has_data_id_field()) { kernel_conf->set_need_do_data_id(true); }
+    if (in_diff_blob_desc->has_col_num_field()) { kernel_conf->set_need_do_col_num(true); }
     if (in_diff_blob_desc->has_dim0_valid_num_field()) {
       kernel_conf->set_need_do_dim0_valid_num(true);
       kernel_conf->set_can_naive_do_dim0_valid_num(true);
