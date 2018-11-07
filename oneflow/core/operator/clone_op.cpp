fdf995e2fedcda867e17019778abfaa326365827
cheng cheng <472491134@qq.com>
2018-11-07 16:42:22 +0800

    fix code for clone op

--- oneflow/core/operator/clone_op.cpp
+++ oneflow/core/operator/clone_op.cpp
@@ -31,19 +31,23 @@ void CloneOp::VirtualGenKernelConf(
                     kernel_conf->mutable_op_attribute()->mutable_output_diff_bns());
   if (!kernel_conf->is_forward()) {
     const BlobDesc* in_diff_blob_desc = GetBlobDesc4BnInOp(SoleIdbn());
-    if (in_diff_blob_desc->has_data_id_field()) { kernel_conf->set_need_do_data_id(true); }
-    if (in_diff_blob_desc->has_col_num_field()) { kernel_conf->set_need_do_col_num(true); }
-    if (in_diff_blob_desc->has_dim0_valid_num_field()) {
+    if (in_diff_blob_desc->HasField<FieldKey::kDataId>()) {
+      kernel_conf->set_need_do_col_num(true);
+    }
+    if (in_diff_blob_desc->HasField<FieldKey::kColNum>()) {
+      kernel_conf->set_need_do_col_num(true);
+    }
+    if (in_diff_blob_desc->HasField<FieldKey::kDim0ValidNum>()) {
       kernel_conf->set_need_do_dim0_valid_num(true);
       kernel_conf->set_can_naive_do_dim0_valid_num(true);
     }
-    if (in_diff_blob_desc->has_dim1_valid_num_field()) {
+    if (in_diff_blob_desc->HasField<FieldKey::kDim1ValidNum>()) {
       kernel_conf->set_need_do_dim1_valid_num(true);
     }
-    if (in_diff_blob_desc->has_dim2_valid_num_field()) {
+    if (in_diff_blob_desc->HasField<FieldKey::kDim2ValidNum>()) {
       kernel_conf->set_need_do_dim2_valid_num(true);
     }
-    if (in_diff_blob_desc->has_record_id_in_device_piece_field()) {
+    if (in_diff_blob_desc->HasField<FieldKey::kRecordIdInDevicePiece>()) {
       kernel_conf->set_need_do_record_id_in_device_piece(true);
       kernel_conf->set_can_naive_do_record_id_in_device_piece(true);
     }
