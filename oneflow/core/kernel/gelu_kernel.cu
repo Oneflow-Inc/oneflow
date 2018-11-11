b9d7260def1ebfe4e918e0db2dd9c15054baa659
zyeric <619828575@qq.com>
2018-11-11 15:30:08 +0800

    call different funcs for float and double

--- oneflow/core/kernel/gelu_kernel.cu
+++ oneflow/core/kernel/gelu_kernel.cu
@@ -7,14 +7,39 @@ namespace {
 
 template<typename T>
 __global__ void GeluForwardGpu(const int64_t n, const T* x, T* y) {
-  T inv_sqrt2 = sqrt(0.5);
-  CUDA_1D_KERNEL_LOOP(i, n) { y[i] = 0.5 * x[i] * (1.0 + erf(inv_sqrt2 * x[i])); }
+  UNIMPLEMENTED();
 }
 
 template<typename T>
 __global__ void GeluBackwardGpu(const int64_t n, const T* x, const T* dy, T* dx) {
-  T inv_sqrt2 = sqrt(0.5);
-  T coef = sqrt(2.0 / acos(-1.0));
+  UNIMPLEMENTED();
+}
+
+template<>
+__global__ void GeluForwardGpu(const int64_t n, const float* x, float* y) {
+  float inv_sqrt2 = sqrtf(0.5);
+  CUDA_1D_KERNEL_LOOP(i, n) { y[i] = 0.5 * x[i] * (1.0 + erff(inv_sqrt2 * x[i])); }
+}
+
+template<>
+__global__ void GeluBackwardGpu(const int64_t n, const float* x, const float* dy, float* dx) {
+  float inv_sqrt2 = sqrtf(0.5);
+  float coef = sqrtf(2.0 / acosf(-1.0));
+  CUDA_1D_KERNEL_LOOP(i, n) {
+    dx[i] = 0.5 * (1.0 + erff(inv_sqrt2 * x[i]) + x[i] * coef * expf(-x[i] * x[i]));
+  }
+}
+
+template<>
+__global__ void GeluForwardGpu(const int64_t n, const double* x, double* y) {
+  double inv_sqrt2 = sqrt(0.5);
+  CUDA_1D_KERNEL_LOOP(i, n) { y[i] = 0.5 * x[i] * (1.0 + erf(inv_sqrt2 * x[i])); }
+}
+
+template<>
+__global__ void GeluBackwardGpu(const int64_t n, const double* x, const double* dy, double* dx) {
+  double inv_sqrt2 = sqrt(0.5);
+  double coef = sqrt(2.0 / acos(-1.0));
   CUDA_1D_KERNEL_LOOP(i, n) {
     dx[i] = 0.5 * (1.0 + erf(inv_sqrt2 * x[i]) + x[i] * coef * exp(-x[i] * x[i]));
   }
