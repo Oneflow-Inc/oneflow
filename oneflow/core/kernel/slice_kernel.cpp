aed6e2694600b35b46cb9f16e920b3d20f716ecd
leaves-zwx <kunta0932@gmail.com>
2018-11-12 22:26:10 +0800

    Dev bert slice op (#1491)
    * add op_conf
    
    * add slice op impl
    
    * add space kernel impl
    
    * fix
    
    * same semantic as python
    
    * optional start and end
    
    * fix
    
    
    Former-commit-id: e78d19c8622fb36680f61a99abf610dc81e06f1f

---
+++ oneflow/core/kernel/slice_kernel.cpp
@@ -0,0 +1,19 @@
+#include "oneflow/core/kernel/slice_kernel.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+void SliceKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  // TODO();
+}
+
+template<DeviceType device_type, typename T>
+void SliceKernel<device_type, T>::BackwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  // TODO();
+}
+
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kSliceConf, SliceKernel, ARITHMETIC_DATA_TYPE_SEQ);
+
+}  // namespace oneflow
