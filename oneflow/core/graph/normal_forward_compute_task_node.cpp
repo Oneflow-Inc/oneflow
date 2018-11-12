1d9edfdcec0b12c546143a7a3e7160797beff895
Juncheng <liujuncheng1022@gmail.com>
2018-11-12 11:20:14 +0800

    disable mem sharing for VariableOp

--- oneflow/core/graph/normal_forward_compute_task_node.cpp
+++ oneflow/core/graph/normal_forward_compute_task_node.cpp
@@ -1,11 +1,31 @@
 #include "oneflow/core/graph/normal_forward_compute_task_node.h"
 #include "oneflow/core/graph/task_graph.h"
 #include "oneflow/core/graph/logical_node.h"
+#include "oneflow/core/operator/variable_op.h"
 
 namespace oneflow {
 
+namespace {
+
+bool IsVariableOp(const LogicalNode* logical_node) {
+  FOR_EACH(op, logical_node->op_vec()) {
+    if (dynamic_cast<VariableOp*>(op->get()) != nullptr) {
+      CHECK_EQ(logical_node->op_vec().size(), 1);
+      return true;
+    }
+  }
+  return false;
+}
+
+}  // namespace
+
 void NormalForwardCompTaskNode::ProduceAllRegstsAndBindEdges() {
-  ProduceRegst("out", true);
+  // HACK: disable mem sharing for VariableOp
+  if (IsVariableOp(logical_node())) {
+    ProduceRegst("out", false, 1, 1);
+  } else {
+    ProduceRegst("out", true);
+  }
   ProduceRegst("activation", true);
   ProduceRegst("data_tmp", true);
   ProduceRegst("fw_buf", true, 1, 1);
