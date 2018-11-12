052aef6bfe09de3232b50fce31c209c60469a697
leaves-zwx <kunta0932@gmail.com>
2018-11-12 17:50:45 +0800

    fix

--- oneflow/core/kernel/slice_kernel.cpp
+++ oneflow/core/kernel/slice_kernel.cpp
@@ -5,13 +5,13 @@ namespace oneflow {
 template<DeviceType device_type, typename T>
 void SliceKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  TODO();
+  // TODO();
 }
 
 template<DeviceType device_type, typename T>
 void SliceKernel<device_type, T>::BackwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  TODO();
+  // TODO();
 }
 
 ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kSliceConf, SliceKernel, ARITHMETIC_DATA_TYPE_SEQ);
