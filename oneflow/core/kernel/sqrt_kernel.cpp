257baf06361c2413ff280f1d82e6f7dde0798180
zyeric <619828575@qq.com>
2018-11-13 20:19:12 +0800

    fix typo

--- oneflow/core/kernel/sqrt_kernel.cpp
+++ oneflow/core/kernel/sqrt_kernel.cpp
@@ -7,8 +7,8 @@ void SqrtKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   Blob* in_blob = BnInOp2Blob("in");
   Blob* out_blob = BnInOp2Blob("out");
-  KernelUtil<device_type, T>::Square(ctx.device_ctx, in_blob->static_shape().elem_cnt(),
-                                     in_blob->dptr<T>(), out_blob->mut_dptr<T>());
+  KernelUtil<device_type, T>::Sqrt(ctx.device_ctx, in_blob->static_shape().elem_cnt(),
+                                   in_blob->dptr<T>(), out_blob->mut_dptr<T>());
 }
 
 template<DeviceType device_type, typename T>
