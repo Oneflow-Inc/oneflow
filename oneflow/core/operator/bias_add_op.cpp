64c76602aad07c8e3d696544e34af16996a3466e
Juncheng <liujuncheng1022@gmail.com>
2018-11-14 18:25:56 +0800

    b is 1-D blob

--- oneflow/core/operator/bias_add_op.cpp
+++ oneflow/core/operator/bias_add_op.cpp
@@ -17,12 +17,11 @@ void BiasAddOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetB
                                const ParallelContext* parallel_ctx) const {
   const BlobDesc* a_blob_desc = GetBlobDesc4BnInOp("a");
   const BlobDesc* b_blob_desc = GetBlobDesc4BnInOp("b");
-
-  CHECK_EQ(a_blob_desc->shape().At(1), b_blob_desc->shape().At(1));
-  CHECK_EQ(b_blob_desc->shape().At(0), 1);
+  CHECK_EQ(a_blob_desc->shape().NumAxes(), 2);
+  CHECK_EQ(b_blob_desc->shape().NumAxes(), 1);
+  CHECK_EQ(a_blob_desc->shape().At(1), b_blob_desc->shape().At(0));
   CHECK_EQ(a_blob_desc->data_type(), Global<JobDesc>::Get()->DefaultDataType());
   CHECK_EQ(b_blob_desc->data_type(), Global<JobDesc>::Get()->DefaultDataType());
-
   *GetBlobDesc4BnInOp("out") = *a_blob_desc;
   GetBlobDesc4BnInOp("bias_multiplier")->mut_shape() = Shape({a_blob_desc->shape().At(0), 1});
 }
