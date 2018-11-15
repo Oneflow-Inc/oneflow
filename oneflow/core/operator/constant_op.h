e9d8ccbe8f4e7504c3abc21531576f3724a81f13
Juncheng <liujuncheng1022@gmail.com>
2018-11-15 18:23:54 +0800

    constant_op

---
+++ oneflow/core/operator/constant_op.h
@@ -0,0 +1,28 @@
+#ifndef ONEFLOW_CORE_OPERATOR_CONSTANT_OP_H_
+#define ONEFLOW_CORE_OPERATOR_CONSTANT_OP_H_
+
+#include "oneflow/core/operator/operator.h"
+
+namespace oneflow {
+
+class ConstantOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(ConstantOp);
+  ConstantOp() = default;
+  ~ConstantOp() override = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override;
+  bool IsAllOutputConst() const override { return true; }
+
+ private:
+  void VirtualGenKernelConf(std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                            const ParallelContext* parallel_ctx,
+                            KernelConf* kernel_conf) const override;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_CONSTANT_OP_H_
