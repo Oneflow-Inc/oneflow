ead7541421088228ec30ca985b9af00fad6292b6
Xinqi <lixinqi0703106@163.com>
2018-11-15 19:54:26 +0800

    add bw kernel of broadcast_add
    Former-commit-id: 3df066427fbec0f1f44529cb7b8a2cc484641294

--- oneflow/core/kernel/broadcast_add_kernel.cpp
+++ oneflow/core/kernel/broadcast_add_kernel.cpp
@@ -1,6 +1,7 @@
 #include "oneflow/core/kernel/broadcast_add_kernel.h"
 #include "oneflow/core/ndarray/binary_func.h"
 #include "oneflow/core/ndarray/xpu_var_ndarray.h"
+#include "oneflow/core/kernel/broadcast_add_xpu_util.h"
 #include "oneflow/core/common/preprocessor.h"
 
 namespace oneflow {
@@ -13,7 +14,33 @@ void BroadcastAddKernel<device_type, T>::ForwardDataContent(
 
 template<DeviceType device_type, typename T>
 void BroadcastAddKernel<device_type, T>::BackwardDataContent(
-    const KernelCtx&, std::function<Blob*(const std::string&)> BnInOp2Blob) const {}
+    const KernelCtx& kernel_ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* out_diff_blob = BnInOp2Blob("out_diff");
+  Blob* a_diff_blob = BnInOp2Blob("a_diff");
+  Blob* b_diff_blob = BnInOp2Blob("b_diff");
+  Blob* bw_buf_blob = BnInOp2Blob("bw_buf");
+  size_t num_axes = out_diff_blob->shape().NumAxes();
+  if (a_diff_blob) {
+    CHECK_EQ(a_diff_blob->shape().NumAxes(), num_axes);
+    SwitchBackwardInputDiff(SwitchCase(num_axes), kernel_ctx.device_ctx,
+                            XpuVarNdarray<T>(a_diff_blob), XpuVarNdarray<const T>(out_diff_blob),
+                            XpuVarNdarray<T>(bw_buf_blob));
+  }
+  if (b_diff_blob) {
+    CHECK_EQ(b_diff_blob->shape().NumAxes(), num_axes);
+    SwitchBackwardInputDiff(SwitchCase(num_axes), kernel_ctx.device_ctx,
+                            XpuVarNdarray<T>(b_diff_blob), XpuVarNdarray<const T>(out_diff_blob),
+                            XpuVarNdarray<T>(bw_buf_blob));
+  }
+}
+
+template<typename T, int NDIMS>
+struct BroadcastAddKernelUtil<DeviceType::kCPU, T, NDIMS> final {
+  static void BackwardInputDiff(DeviceCtx*, XpuVarNdarray<T>&& in_diff_a,
+                                const XpuVarNdarray<const T>& out_diff, XpuVarNdarray<T>&& bw_buf) {
+    BroadcastAddXpuUtil<T, NDIMS>::BackwardInputDiff(&in_diff_a, out_diff, &bw_buf);
+  }
+};
 
 ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kBroadcastAddConf, BroadcastAddKernel,
                            ARITHMETIC_DATA_TYPE_SEQ);
