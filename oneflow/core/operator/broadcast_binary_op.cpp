676d6c212c51e50d0af1fe56791ed22d4440df7b
Juncheng <liujuncheng1022@gmail.com>
2018-11-15 12:39:45 +0800

    broadcast binary op check data type equal

--- oneflow/core/operator/broadcast_binary_op.cpp
+++ oneflow/core/operator/broadcast_binary_op.cpp
@@ -22,6 +22,7 @@ void BroadcastBinaryOp::InferBlobDescs(
     const ParallelContext* parallel_ctx) const {
   const BlobDesc* a_blob_desc = GetBlobDesc4BnInOp("a");
   const BlobDesc* b_blob_desc = GetBlobDesc4BnInOp("b");
+  CHECK_EQ(a_blob_desc->data_type(), b_blob_desc->data_type());
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
   if (IsScalarBlob(a_blob_desc)) {
     *out_blob_desc = *b_blob_desc;
