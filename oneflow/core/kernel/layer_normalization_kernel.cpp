bfcc34bc23274644218db92367d6c65658a4315f
Dounm <niuchong893184@gmail.com>
2018-11-15 12:53:45 +0800

    feat: add LayerNormOp/Kernel without kernel implement

---
+++ oneflow/core/kernel/layer_normalization_kernel.cpp
@@ -0,0 +1,31 @@
+#include "oneflow/core/kernel/layer_normalization_kernel.h"
+#include "oneflow/core/kernel/kernel_util.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+void LayerNormKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx&, std::function<Blob*(const std::string&)>) const {
+  // TODO
+}
+
+template<DeviceType device_type, typename T>
+void LayerNormKernel<device_type, T>::BackwardDataContent(
+    const KernelCtx&, std::function<Blob*(const std::string&)>) const {
+  // TODO
+}
+
+template<DeviceType device_type, typename T>
+void LayerNormKernel<device_type, T>::InitModelBlobsWithRandomSeed(
+    DeviceCtx*, std::mt19937*, std::function<Blob*(const std::string&)>) const {
+  // TODO
+}
+
+template<DeviceType device_type, typename T>
+void LayerNormKernel<device_type, T>::InitModelBlobsWithDir(
+    DeviceCtx*, int32_t part_id, int32_t part_num, const std::string& model_load_dir,
+    std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  // TODO
+}
+
+}  // namespace oneflow
