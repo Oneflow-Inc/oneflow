97be2c1c623f16cc40a88a0f112530b7448c3f56
Juncheng <liujuncheng1022@gmail.com>
2018-11-20 15:31:50 +0800

    fix before_dim_size

--- oneflow/core/kernel/kernel_util.cpp
+++ oneflow/core/kernel/kernel_util.cpp
@@ -163,7 +163,7 @@ void RangeInitializer(const RangeInitializerConfT& initializer_conf, uint32_t ra
                            : initializer_conf.axis();
   CHECK_GE(axis, 0);
   CHECK_LT(axis, blob->shape().NumAxes());
-  int64_t before_dim_size = blob->shape().Count(0, axis - 1);
+  int64_t before_dim_size = blob->shape().Count(0, axis);
   int64_t index_dim_size = blob->shape().At(axis);
   int64_t after_dim_size = blob->shape().Count(axis + 1);
   RangeInitializer<T>(before_dim_size, index_dim_size, after_dim_size,
