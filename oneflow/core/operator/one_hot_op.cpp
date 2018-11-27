b44aa553add4b43174ee0948ef7a692efe935862
Juncheng <liujuncheng1022@gmail.com>
2018-11-27 12:32:52 +0800

    refine

--- oneflow/core/operator/one_hot_op.cpp
+++ oneflow/core/operator/one_hot_op.cpp
@@ -12,15 +12,15 @@ const PbMessage &OneHotOp::GetCustomizedConf() const { return op_conf().one_hot_
 
 void OneHotOp::InferBlobDescs(std::function<BlobDesc *(const std::string &)> GetBlobDesc4BnInOp,
                               const ParallelContext *parallel_ctx) const {
-  const OneHotOpConf &conf = op_conf().one_hot_conf();
+  const OneHotOpConf& conf = op_conf().one_hot_conf();
   const int64_t depth = conf.depth();
   const DataType data_type =
       conf.has_data_type() ? conf.data_type() : Global<JobDesc>::Get()->DefaultDataType();
   CHECK_GT(depth, 0);
-  const BlobDesc *indices = GetBlobDesc4BnInOp("indices");
+  const BlobDesc* indices = GetBlobDesc4BnInOp("indices");
   CHECK(IsIntegralDataType(indices->data_type()));
   CHECK_GT(indices->shape().NumAxes(), 0);
-  BlobDesc *out = GetBlobDesc4BnInOp("out");
+  BlobDesc* out = GetBlobDesc4BnInOp("out");
   *out = *indices;
   out->set_data_type(data_type);
   std::vector<int64_t> dim_vec = indices->shape().dim_vec();
