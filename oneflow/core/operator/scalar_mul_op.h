50551e55cef0da26f686f62a393523177c2c0ee4
Dounm <niuchong893184@gmail.com>
2018-11-29 18:35:35 +0800

    fix: fix typo of include file in scalar_add_op/scalar_mul_op
    Former-commit-id: 765da6a13627277cce716e7517b48dc81818f551

--- oneflow/core/operator/scalar_mul_op.h
+++ oneflow/core/operator/scalar_mul_op.h
@@ -1,7 +1,7 @@
 #ifndef ONEFLOW_CORE_OPERATOR_SCALAR_MUL_OP_H_
 #define ONEFLOW_CORE_OPERATOR_SCALAR_MUL_OP_H_
 
-#include "oneflow/core/operator/cwise_op.h"
+#include "oneflow/core/operator/operator.h"
 
 namespace oneflow {
 
