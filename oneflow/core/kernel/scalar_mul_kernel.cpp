0a4a2924df674020b67f2668e7a0ccdf5f0f9b74
Niu Chong <niuchong893184@gmail.com>
2018-11-29 18:13:41 +0800

    Dev nc scalar (#1543)
    * feat: add ScalarAddOp and ScalarMulOp
    
    * feat: add ScalarAddKernel and ScalarMulKernel
    
    * fix: ScalarAddOp/ScalarMulOp not inheri from CWiseOp
    
    * fix: fix code style
    
    
    Former-commit-id: eb19edbb392182508a898e744c48c60d8a187852

---
+++ oneflow/core/kernel/scalar_mul_kernel.cpp
@@ -0,0 +1,49 @@
+#include "oneflow/core/kernel/scalar_mul_kernel.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+void ScalarMulKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* in_blob = BnInOp2Blob("in");
+  Blob* out_blob = BnInOp2Blob("out");
+  Memcpy<device_type>(ctx.device_ctx, out_blob->mut_dptr<T>(), in_blob->dptr<T>(),
+                      out_blob->ByteSizeOfDataContentField());
+  T scalar_operand = 0;
+  const auto& conf = this->op_conf().scalar_mul_conf();
+  if (IsIntegral<T>::value) {
+    CHECK(conf.has_int_operand());
+    scalar_operand = static_cast<T>(conf.int_operand());
+  } else if (IsFloating<T>::value) {
+    CHECK(conf.has_float_operand());
+    scalar_operand = static_cast<T>(conf.float_operand());
+  } else {
+    UNIMPLEMENTED();
+  }
+  KernelUtil<device_type, T>::Scal(ctx.device_ctx, out_blob->shape().elem_cnt(), scalar_operand,
+                                   out_blob->mut_dptr<T>(), 1);
+}
+
+template<DeviceType device_type, typename T>
+void ScalarMulKernel<device_type, T>::BackwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* out_diff_blob = BnInOp2Blob(GenDiffBn("out"));
+  Blob* in_diff_blob = BnInOp2Blob(GenDiffBn("in"));
+  Memcpy<device_type>(ctx.device_ctx, in_diff_blob->mut_dptr<T>(), out_diff_blob->dptr<T>(),
+                      out_diff_blob->ByteSizeOfDataContentField());
+  T scalar_operand = 0;
+  const auto& conf = this->op_conf().scalar_add_conf();
+  if (IsIntegral<T>::value) {
+    CHECK(conf.has_int_operand());
+    scalar_operand = static_cast<T>(conf.int_operand());
+  } else if (IsFloating<T>::value) {
+    CHECK(conf.has_float_operand());
+    scalar_operand = static_cast<T>(conf.float_operand());
+  } else {
+    UNIMPLEMENTED();
+  }
+  KernelUtil<device_type, T>::Scal(ctx.device_ctx, in_diff_blob->shape().elem_cnt(), scalar_operand,
+                                   in_diff_blob->mut_dptr<T>(), 1);
+}
+
+}  // namespace oneflow
--- oneflow/core/operator/scalar_add_op.h
+++ oneflow/core/operator/scalar_add_op.h
@@ -5,12 +5,16 @@
 
 namespace oneflow {
 
-class ScalarAddOp final : public CWiseOp {
+class ScalarAddOp final : public Operator {
  public:
   OF_DISALLOW_COPY_AND_MOVE(ScalarAddOp);
   ScalarAddOp() = default;
   ~ScalarAddOp() = default;
 
+  void InitFromOpConf() override;
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override;
+  bool IsElemWiseOp() const override { return true; }
   const PbMessage& GetCustomizedConf() const override { return op_conf().scalar_add_conf(); }
   bool NeedInBlobWhenBackward() const override { return false; }
   bool NeedOutBlobWhenBackward() const override { return false; }
