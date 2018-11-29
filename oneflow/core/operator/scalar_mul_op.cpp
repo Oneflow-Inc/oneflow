cec180191b4b14ebc7ca39d036044a8c51bfc356
Dounm <niuchong893184@gmail.com>
2018-11-29 16:23:01 +0800

    fix: ScalarAddOp/ScalarMulOp not inheri from CWiseOp

--- oneflow/core/operator/scalar_mul_op.cpp
+++ oneflow/core/operator/scalar_mul_op.cpp
@@ -2,6 +2,16 @@
 
 namespace oneflow {
 
+void ScalarMulOp::InitFromOpConf() {
+  EnrollInputBn("in");
+  EnrollOutputBn("out");
+}
+
+void ScalarMulOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const {
+  *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
+}
+
 REGISTER_OP(OperatorConf::kScalarMulConf, ScalarMulOp);
 
 }  // namespace oneflow
--- oneflow/core/operator/scalar_mul_op.h
+++ oneflow/core/operator/scalar_mul_op.h
@@ -11,6 +11,10 @@ class ScalarMulOp final : public CWiseOp {
   ScalarMulOp() = default;
   ~ScalarMulOp() = default;
 
+  void InitFromOpConf() override;
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override;
+  bool IsElemWiseOp() const override { return true; }
   const PbMessage& GetCustomizedConf() const override { return op_conf().scalar_mul_conf(); }
   bool NeedInBlobWhenBackward() const override { return false; }
   bool NeedOutBlobWhenBackward() const override { return false; }
