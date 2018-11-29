eb19edbb392182508a898e744c48c60d8a187852
Niu Chong <niuchong893184@gmail.com>
2018-11-29 18:13:41 +0800

    Dev nc scalar (#1543)
    * feat: add ScalarAddOp and ScalarMulOp
    
    * feat: add ScalarAddKernel and ScalarMulKernel
    
    * fix: ScalarAddOp/ScalarMulOp not inheri from CWiseOp
    
    * fix: fix code style

--- oneflow/core/operator/scalar_mul_op.h
+++ oneflow/core/operator/scalar_mul_op.h
@@ -5,12 +5,16 @@
 
 namespace oneflow {
 
-class ScalarMulOp final : public CWiseOp {
+class ScalarMulOp final : public Operator {
  public:
   OF_DISALLOW_COPY_AND_MOVE(ScalarMulOp);
   ScalarMulOp() = default;
   ~ScalarMulOp() = default;
 
+  void InitFromOpConf() override;
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override;
+  bool IsElemWiseOp() const override { return true; }
   const PbMessage& GetCustomizedConf() const override { return op_conf().scalar_mul_conf(); }
   bool NeedInBlobWhenBackward() const override { return false; }
   bool NeedOutBlobWhenBackward() const override { return false; }
