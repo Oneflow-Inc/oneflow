d516fcf869be18a2cf3eb8e8027f2d59f91efc13
Juncheng <liujuncheng1022@gmail.com>
2018-11-30 15:50:32 +0800

    op

--- oneflow/core/operator/sparse_cross_entropy_op.cpp
+++ oneflow/core/operator/sparse_cross_entropy_op.cpp
@@ -4,7 +4,6 @@ namespace oneflow {
 
 void SparseCrossEntropyOp::InitFromOpConf() {
   CHECK(op_conf().has_sparse_cross_entropy_conf());
-
   EnrollInputBn("prediction");
   EnrollInputBn("label", false);
   EnrollOutputBn("out");
@@ -19,19 +18,23 @@ void SparseCrossEntropyOp::InferBlobDescs(
     const ParallelContext* parallel_ctx, std::function<void(OpContext*)> EnrollOpCtx) const {
   const BlobDesc* pred_blob_desc = GetBlobDesc4BnInOp("prediction");
   const BlobDesc* label_blob_desc = GetBlobDesc4BnInOp("label");
+  CHECK(IsIntegralDataType(label_blob_desc->data_type()));
+  CHECK(IsFloatingDataType(pred_blob_desc->data_type()));
   CHECK_EQ(pred_blob_desc->has_data_id_field(), label_blob_desc->has_data_id_field());
   CHECK_EQ(pred_blob_desc->has_dim0_valid_num_field(), label_blob_desc->has_dim0_valid_num_field());
   CHECK_EQ(pred_blob_desc->has_dim0_inner_shape(), label_blob_desc->has_dim0_inner_shape());
   if (pred_blob_desc->has_dim0_inner_shape()) {
     CHECK_EQ(pred_blob_desc->dim0_inner_shape().At(0), 1);
+    CHECK_EQ(pred_blob_desc->dim0_inner_shape(), label_blob_desc->dim0_inner_shape());
   }
-  CHECK_GE(pred_blob_desc->shape().NumAxes(), 2);
-
-  // out
+  CHECK_GT(label_blob_desc->shape().NumAxes(), 0);
+  CHECK_EQ(pred_blob_desc->shape().NumAxes(), label_blob_desc->shape().NumAxes() + 1);
+  FOR_RANGE(int64_t, i, 0, label_blob_desc->shape().NumAxes()) {
+    CHECK_EQ(pred_blob_desc->shape().At(i), label_blob_desc->shape().At(i));
+  }
   BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
   *out_blob_desc = *pred_blob_desc;
-  out_blob_desc->mut_shape() = Shape({pred_blob_desc->shape().At(0)});
-  out_blob_desc->set_data_type(pred_blob_desc->data_type());
+  out_blob_desc->mut_shape() = label_blob_desc->shape();
 }
 
 REGISTER_OP(OperatorConf::kSparseCrossEntropyConf, SparseCrossEntropyOp);
