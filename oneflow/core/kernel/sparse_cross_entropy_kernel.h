4b09b4332dd70c5621fc8ab82bda3dd89d0fed3d
Juncheng <liujuncheng1022@gmail.com>
2018-12-03 20:36:54 +0800

    Dev sparse cross entropy (#1550)
    * op for sparse cross _entropy
    
    * modify op_conf for sparse cross entropy
    
    * saprse cross entropy kernel
    
    * op
    
    * SparseCrossEntropyKernelUtil
    
    * refine
    
    
    Former-commit-id: c7914bc54927eb34ab9ef77a1ad9e48f17f1d1a1

---
+++ oneflow/core/kernel/sparse_cross_entropy_kernel.h
@@ -0,0 +1,24 @@
+#ifndef ONEFLOW_CORE_KERNEL_SPARSE_CROSS_ENTROPY_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_SPARSE_CROSS_ENTROPY_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+class SparseCrossEntropyKernel final : public KernelIf<device_type> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SparseCrossEntropyKernel);
+  SparseCrossEntropyKernel() = default;
+  ~SparseCrossEntropyKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx&,
+                          std::function<Blob*(const std::string&)>) const override;
+  void BackwardDataContent(const KernelCtx&,
+                           std::function<Blob*(const std::string&)>) const override;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_SPARSE_CROSS_ENTROPY_KERNEL_H_
