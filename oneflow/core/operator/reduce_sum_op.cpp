81eb3ddcc3432dc424690f3aa6623b581d9e550a
jackalcooper <jackalcooper@gmail.com>
2018-12-04 20:31:20 +0800

    refactoring reduce sum (#1554)
    * refactoring reduce sum
    
    * also use shape and dptr when bw
    
    * add resize when keepdims
    
    * address reviews
    
    * move functions to Anonymous namespace
    
    * address reviews
    
    * remove auto
    
    * replace find
    
    * rename keepdims
    
    
    Former-commit-id: 32f0e867a5e9fe47556328a19bb1bc04725ad253

--- oneflow/core/operator/reduce_sum_op.cpp
+++ oneflow/core/operator/reduce_sum_op.cpp
@@ -2,6 +2,36 @@
 #include "oneflow/core/kernel/kernel_util.h"
 
 namespace oneflow {
+namespace {
+std::vector<int64_t> KeepDims(const std::vector<int64_t> dim_vec,
+                              const std::vector<int64_t> axis_vec) {
+  std::vector<int64_t> ret = dim_vec;
+  for (const auto& axis : axis_vec) { ret[axis] = 1; }
+  return ret;
+}
+
+std::vector<int64_t> DropDims(const std::vector<int64_t> dim_vec,
+                              const std::vector<int64_t> axis_vec) {
+  std::vector<int64_t> ret;
+  std::vector<int32_t> dim2is_reduced(dim_vec.size());
+  for (const auto& axis : axis_vec) { dim2is_reduced[axis] = 1; }
+  FOR_RANGE(int64_t, i, 0, dim_vec.size()) {
+    if (dim2is_reduced[i] != 1) { ret.push_back(dim_vec[i]); }
+  }
+  if (ret.empty()) { ret.push_back(1); }
+  return ret;
+}
+
+std::vector<int64_t> ShiftAxisIfNegative(std::vector<int64_t> axis_vec, const int64_t num_axes) {
+  FOR_RANGE(size_t, i, 0, axis_vec.size()) {
+    if (axis_vec[i] < 0) { axis_vec[i] += num_axes; }
+    CHECK_LT(axis_vec[i], num_axes);
+    CHECK_GE(axis_vec[i], 0);
+  }
+  return axis_vec;
+}
+
+}  // namespace
 
 void ReduceSumOp::InitFromOpConf() {
   EnrollInputBn("in");
@@ -13,44 +43,52 @@ const PbMessage& ReduceSumOp::GetCustomizedConf() const { return op_conf().reduc
 
 void ReduceSumOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                                  const ParallelContext*) const {
+  const ReduceSumOpConf& conf = op_conf().reduce_sum_conf();
   const BlobDesc* in_blob = GetBlobDesc4BnInOp("in");
-  std::vector<int64_t> out_dim_vec = {1};
-  if (op_conf().reduce_sum_conf().has_axis()) {
-    out_dim_vec = in_blob->shape().dim_vec();
-    int32_t axis = GetCorrectAxis(GetBlobDesc4BnInOp);
-    if (op_conf().reduce_sum_conf().keepdims() == true) {
-      out_dim_vec[axis] = 1;
+  *GetBlobDesc4BnInOp("fw_tmp") = *in_blob;
+  std::vector<int64_t> out_dim_vec;
+  if (conf.axis().empty()) {
+    if (conf.keep_dims() == true) {
+      out_dim_vec.resize(in_blob->shape().NumAxes());
+      std::fill(out_dim_vec.begin(), out_dim_vec.end(), 1);
     } else {
-      out_dim_vec.erase(out_dim_vec.begin() + axis);
+      out_dim_vec = {1};
     }
-    if (out_dim_vec.empty()) { out_dim_vec.push_back(1); }
   } else {
-    BlobDesc* fw_tmp_blob = GetBlobDesc4BnInOp("fw_tmp");
-    fw_tmp_blob->mut_shape() = Shape({static_cast<int64_t>(
-        GetTmpSizeForReduceSum(in_blob->data_type(), in_blob->shape().elem_cnt()))});
-    fw_tmp_blob->set_data_type(DataType::kChar);
+    const PbRf<int32_t>& axis_repeated = conf.axis();
+    std::vector<int64_t> axis_vec = {axis_repeated.begin(), axis_repeated.end()};
+    axis_vec = ShiftAxisIfNegative(axis_vec, in_blob->shape().NumAxes());
+    std::sort(axis_vec.begin(), axis_vec.end());
+    CHECK(std::unique(axis_vec.begin(), axis_vec.end()) == axis_vec.end())
+        << "duplicate found in axis";
+    if (conf.keep_dims() == true) {
+      out_dim_vec = KeepDims(in_blob->shape().dim_vec(), axis_vec);
+    } else {
+      out_dim_vec = DropDims(in_blob->shape().dim_vec(), axis_vec);
+    }
   }
+  CHECK(!out_dim_vec.empty());
   BlobDesc* out_blob = GetBlobDesc4BnInOp("out");
-  if (op_conf().reduce_sum_conf().has_axis() && GetCorrectAxis(GetBlobDesc4BnInOp) > 0) {
-    *out_blob = *in_blob;
-  } else {
-    out_blob->set_data_type(in_blob->data_type());
-  }
+  out_blob->set_data_type(in_blob->data_type());
   out_blob->mut_shape() = Shape(out_dim_vec);
 }
 
 void ReduceSumOp::VirtualGenKernelConf(
     std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp, const ParallelContext*,
     KernelConf* kernel_conf) const {
-  if (op_conf().reduce_sum_conf().has_axis() == false) { return; }
-  kernel_conf->mutable_reduce_sum_conf()->set_axis(GetCorrectAxis(GetBlobDesc4BnInOp));
-}
-
-int32_t ReduceSumOp::GetCorrectAxis(
-    std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp) const {
-  int32_t axis = op_conf().reduce_sum_conf().axis();
-  if (axis < 0) { axis += GetBlobDesc4BnInOp("in")->shape().NumAxes(); }
-  return axis;
+  const ReduceSumOpConf& conf = op_conf().reduce_sum_conf();
+  const BlobDesc* in_blob = GetBlobDesc4BnInOp("in");
+  std::vector<int64_t> kept_dims;
+  if (conf.axis().empty()) {
+    kept_dims.resize(in_blob->shape().NumAxes());
+    std::fill(kept_dims.begin(), kept_dims.end(), 1);
+  } else {
+    const PbRf<int32_t>& axis_repeated = op_conf().reduce_sum_conf().axis();
+    std::vector<int64_t> axis_vec = {axis_repeated.begin(), axis_repeated.end()};
+    kept_dims = KeepDims(in_blob->shape().dim_vec(),
+                         ShiftAxisIfNegative(axis_vec, in_blob->shape().NumAxes()));
+  }
+  Shape(kept_dims).ToProto(kernel_conf->mutable_reduce_sum_conf()->mutable_kept_dims_shape());
 }
 
 REGISTER_OP(OperatorConf::kReduceSumConf, ReduceSumOp);
