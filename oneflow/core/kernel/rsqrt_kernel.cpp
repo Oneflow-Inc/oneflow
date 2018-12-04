21fb66cdca3315d8a85f9934a2725bb39ade0e81
ScXfjiang <sc.xfjiang@gmail.com>
2018-12-04 11:06:11 +0800

    static_shape() -> shape()

--- oneflow/core/kernel/rsqrt_kernel.cpp
+++ oneflow/core/kernel/rsqrt_kernel.cpp
@@ -8,7 +8,7 @@ void RsqrtKernel<device_type, T>::ForwardDataContent(
   Blob* in_blob = BnInOp2Blob("in");
   Blob* out_blob = BnInOp2Blob("out");
   KernelUtil<device_type, T>::Rsqrt(
-      ctx.device_ctx, in_blob->static_shape().elem_cnt(), in_blob->dptr<T>(),
+      ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
       this->kernel_conf().op_attribute().op_conf().rsqrt_conf().epsilon(), out_blob->mut_dptr<T>());
 }
 
@@ -18,11 +18,11 @@ void RsqrtKernel<device_type, T>::BackwardDataContent(
   Blob* out_blob = BnInOp2Blob("out");
   Blob* out_diff_blob = BnInOp2Blob("out_diff");
   Blob* in_diff_blob = BnInOp2Blob("in_diff");
-  KernelUtil<device_type, T>::Powx(ctx.device_ctx, out_blob->static_shape().elem_cnt(),
+  KernelUtil<device_type, T>::Powx(ctx.device_ctx, out_blob->shape().elem_cnt(),
                                    out_blob->dptr<T>(), 3, in_diff_blob->mut_dptr<T>());
-  KernelUtil<device_type, T>::Scal(ctx.device_ctx, out_blob->static_shape().elem_cnt(),
+  KernelUtil<device_type, T>::Scal(ctx.device_ctx, out_blob->shape().elem_cnt(),
                                    static_cast<T>(-0.5), in_diff_blob->mut_dptr<T>(), 1);
-  KernelUtil<device_type, T>::Mul(ctx.device_ctx, out_blob->static_shape().elem_cnt(),
+  KernelUtil<device_type, T>::Mul(ctx.device_ctx, out_blob->shape().elem_cnt(),
                                   out_diff_blob->dptr<T>(), in_diff_blob->dptr<T>(),
                                   in_diff_blob->mut_dptr<T>());
 }
