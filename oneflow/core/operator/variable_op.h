1dafe47396cdef87f3567c156dea4e2ddd6f394a
Xinqi <lixinqi0703106@163.com>
2018-12-10 16:47:46 +0800

    VariableLogicalNode
    Former-commit-id: cfceb001d977ed8724a519c947568756d683574d

--- oneflow/core/operator/variable_op.h
+++ oneflow/core/operator/variable_op.h
@@ -2,6 +2,7 @@
 #define ONEFLOW_CORE_OPERATOR_VARIABLE_OP_H_
 
 #include "oneflow/core/operator/operator.h"
+#include "oneflow/core/graph/logical_node.h"
 
 namespace oneflow {
 
@@ -13,6 +14,7 @@ class VariableOp final : public Operator {
 
   void InitFromOpConf() override;
   const PbMessage& GetCustomizedConf() const override;
+  LogicalNode* NewProperLogicalNode() override { return new VariableLogicalNode; }
   bool NeedInBlobWhenBackward() const override { return false; }
   bool NeedOutBlobWhenBackward() const override { return false; }
   void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
