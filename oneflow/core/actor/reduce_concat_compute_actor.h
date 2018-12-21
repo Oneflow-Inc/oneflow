cf98dd810e1b27f6a19270fc9619f52aa4cfa554
Li Xinqi <lixinqi2010@gmail.com>
2018-12-22 00:20:30 +0800

    Dev bert cuda event sync (#1581)
    * cudaSetDevice in actor poller threads
    
    * ReduceConcatCompActor ; NaiveActor

--- oneflow/core/actor/reduce_concat_compute_actor.h
+++ oneflow/core/actor/reduce_concat_compute_actor.h
@@ -1,21 +1,15 @@
 #ifndef ONEFLOW_CORE_ACTOR_REDUCE_CONCAT_COMPUTE_ACTOR_H_
 #define ONEFLOW_CORE_ACTOR_REDUCE_CONCAT_COMPUTE_ACTOR_H_
 
-#include "oneflow/core/actor/input_wise_compute_actor.h"
+#include "oneflow/core/actor/naive_actor.h"
 
 namespace oneflow {
 
-class ReduceConcatCompActor final : public InputWiseCompActor {
+class ReduceConcatCompActor final : public NaiveActor {
  public:
   OF_DISALLOW_COPY_AND_MOVE(ReduceConcatCompActor);
   ReduceConcatCompActor() = default;
   ~ReduceConcatCompActor() = default;
-
- private:
-  void VirtualCompActorInit(const TaskProto& proto) override;
-  void SetKernelCtxOther(void** other) override;
-
-  std::pair<int64_t, bool> other_val_;
 };
 
 }  // namespace oneflow
