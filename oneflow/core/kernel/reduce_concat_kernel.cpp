f9030def3552c7e9c9b79961f9aff6346d993b2d
Li Xinqi <lixinqi2010@gmail.com>
2018-12-22 00:20:30 +0800

    Dev bert cuda event sync (#1581)
    * cudaSetDevice in actor poller threads
    
    * ReduceConcatCompActor ; NaiveActor
    
    
    Former-commit-id: cf98dd810e1b27f6a19270fc9619f52aa4cfa554

--- oneflow/core/kernel/reduce_concat_kernel.cpp
+++ oneflow/core/kernel/reduce_concat_kernel.cpp
@@ -5,16 +5,15 @@ namespace oneflow {
 template<DeviceType device_type>
 void ReduceConcatKernel<device_type>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const auto* other_val = static_cast<std::pair<int64_t, bool>*>(ctx.other);
-  int64_t in_bn_id = other_val->first;
-  bool is_inplace = other_val->second;
-  if (is_inplace) { return; }
+  if (device_type == DeviceType::kGPU && Global<JobDesc>::Get()->enable_mem_sharing()) { return; }
   Blob* out_blob = BnInOp2Blob("out");
   char* dst_cur_dptr = out_blob->mut_dptr<char>();
-  dst_cur_dptr += this->kernel_conf().reduce_concat_conf().data_offset().Get(in_bn_id);
-  Blob* in_blob = BnInOp2Blob(this->op_attribute().input_bns().Get(in_bn_id));
-  size_t in_byte_size = in_blob->ByteSizeOfDataContentField();
-  Memcpy<device_type>(ctx.device_ctx, dst_cur_dptr, in_blob->dptr<char>(), in_byte_size);
+  FOR_RANGE(int, in_bn_id, 0, this->op_attribute().input_bns().size()) {
+    dst_cur_dptr += this->kernel_conf().reduce_concat_conf().data_offset().Get(in_bn_id);
+    Blob* in_blob = BnInOp2Blob(this->op_attribute().input_bns().Get(in_bn_id));
+    size_t in_byte_size = in_blob->ByteSizeOfDataContentField();
+    Memcpy<device_type>(ctx.device_ctx, dst_cur_dptr, in_blob->dptr<char>(), in_byte_size);
+  }
 }
 
 ADD_DEVICE_TYPE_KERNEL_CREATOR(OperatorConf::kReduceConcatConf, ReduceConcatKernel);
