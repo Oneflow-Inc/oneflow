99b0b61985b27858665806eee785286ea981b570
Xinqi <lixinqi0703106@163.com>
2018-12-22 00:17:14 +0800

    ReduceConcatCompActor ; NaiveActor

--- oneflow/core/actor/reduce_concat_compute_actor.cpp
+++ oneflow/core/actor/reduce_concat_compute_actor.cpp
@@ -2,16 +2,6 @@
 
 namespace oneflow {
 
-void ReduceConcatCompActor::VirtualCompActorInit(const TaskProto& proto) {
-  InputWiseCompActor::Init(proto);
-}
-
-void ReduceConcatCompActor::SetKernelCtxOther(void** other) {
-  int64_t in_bn_id = InBnId4RegstDescId(cur_processed_regst_desc_id());
-  other_val_ = std::make_pair(in_bn_id, EnableInplace());
-  *other = static_cast<void*>(&other_val_);
-}
-
 REGISTER_ACTOR(TaskType::kReduceConcat, ReduceConcatCompActor);
 
 }  // namespace oneflow
