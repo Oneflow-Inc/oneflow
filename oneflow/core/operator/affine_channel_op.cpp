54d354d0477298571d4c78cdb3a861e8d043179f
scxfjiang <sc.xfjiang@gmail.com>
2018-12-23 01:47:44 +0800

    ssd merge affine channel (#1584)

--- oneflow/core/operator/affine_channel_op.cpp
+++ oneflow/core/operator/affine_channel_op.cpp
@@ -3,34 +3,32 @@
 namespace oneflow {
 
 void AffineChannelOp::InitFromOpConf() {
-  mut_op_conf()->set_trainable(false);
+  CHECK(op_conf().has_affine_channel_conf());
   EnrollInputBn("in");
   EnrollOutputBn("out");
   EnrollModelBn("scale");
-  EnrollModelBn("bias");
-}
-
-const PbMessage& AffineChannelOp::GetCustomizedConf() const {
-  return op_conf().affine_channel_conf();
+  if (GetValFromCustomizedConf<bool>("use_bias")) { EnrollModelBn("bias"); }
 }
 
 void AffineChannelOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx, std::function<void(OpContext*)> EnrollOpCtx) const {
-  CHECK(op_conf().has_model_load_dir());
   const auto& conf = op_conf().affine_channel_conf();
   const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
-  const DataType in_data_type = in_blob_desc->data_type();
-  CHECK_EQ(in_data_type, Global<JobDesc>::Get()->DefaultDataType());
-  CHECK_GT(conf.channel_axis(), 0);
-  CHECK_LE(conf.channel_axis(), in_blob_desc->shape().NumAxes());
-  // out
+  CHECK_GE(conf.axis(), -in_blob_desc->shape().NumAxes());
+  CHECK_LT(conf.axis(), in_blob_desc->shape().NumAxes());
+  const int32_t axis =
+      conf.axis() >= 0 ? conf.axis() : conf.axis() + in_blob_desc->shape().NumAxes();
+
   *GetBlobDesc4BnInOp("out") = *in_blob_desc;
-  // model
-  BlobDesc model_blob_desc(Shape({in_blob_desc->shape().At(conf.channel_axis())}), in_data_type,
+  BlobDesc model_blob_desc(Shape({in_blob_desc->shape().At(axis)}), in_blob_desc->data_type(),
                            false, false, 1);
   *GetBlobDesc4BnInOp("scale") = model_blob_desc;
-  *GetBlobDesc4BnInOp("bias") = model_blob_desc;
+  if (GetValFromCustomizedConf<bool>("use_bias")) { *GetBlobDesc4BnInOp("bias") = model_blob_desc; }
+}
+
+const PbMessage& AffineChannelOp::GetCustomizedConf() const {
+  return op_conf().affine_channel_conf();
 }
 
 REGISTER_OP(OperatorConf::kAffineChannelConf, AffineChannelOp);
