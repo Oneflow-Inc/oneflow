89ca3609653731b12d09e1e785cb08681a9c2c4b
Shiyuan Shang-Guan <shgshy@gmail.com>
2018-12-26 10:40:57 +0800

    port sparse cross entropy to facenet (#1596)

---
+++ oneflow/core/kernel/sparse_cross_entropy_kernel_util.h
@@ -0,0 +1,20 @@
+#ifndef ONEFLOW_CORE_KERNEL_SPARSE_CROSS_ENTROPY_KERNEL_UTIL_H_
+#define ONEFLOW_CORE_KERNEL_SPARSE_CROSS_ENTROPY_KERNEL_UTIL_H_
+
+#include "oneflow/core/kernel/kernel_util.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T, typename K>
+struct SparseCrossEntropyKernelUtil {
+  static void ComputeEntropy(DeviceCtx* ctx, int64_t num_instances, int64_t num_classes, const T* x,
+                             const K* labels, T* y);
+  static void ComputeDiff(DeviceCtx* ctx, int64_t num_instances, int64_t num_classes, const T* x,
+                          const K* labels, T* dx);
+  static void ComputeDiff(DeviceCtx* ctx, int64_t num_instances, int64_t num_classes, const T* x,
+                          const K* labels, const T* dy, T* dx);
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_SPARSE_CROSS_ENTROPY_KERNEL_UTIL_H_
