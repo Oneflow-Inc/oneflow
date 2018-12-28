26cf1f9399a6907bbc8cb4469284b82c7d35ca0b
Xinqi <lixinqi0703106@163.com>
2018-12-28 21:41:46 +0800

    EnableMemSharingInVariableOp

--- oneflow/core/job/compiler.cpp
+++ oneflow/core/job/compiler.cpp
@@ -113,6 +113,7 @@ Plan Compiler::DoCompile() {
   if (job_desc->IsTrain() && job_desc->enable_mem_sharing()) {
     task_gph->EnableMemSharingInReduceStruct();
     task_gph->EnableMemSharingAfterAllManualSetForMdUpdt();  // must last mem shared manual set
+    task_gph->EnableMemSharingInVariableOp();
   }
   if (job_desc->IsTrain()) { task_gph->AddOrderCtrlEdgeBetweenCopyAndMdUpdt(); }
   if (job_desc->IsTrain()) { task_gph->RmUselessConsumeRelationshipBetweenFwBw(); }
