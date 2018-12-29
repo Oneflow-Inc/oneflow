03aa5588e7b9671e5495df579db5bf2c86c87152
ScXfjiang <sc.xfjiang@gmail.com>
2018-12-29 17:48:15 +0800

    CHECK_LE -> CHECK_EQ

--- oneflow/core/operator/reduce_concat_op.cpp
+++ oneflow/core/operator/reduce_concat_op.cpp
@@ -38,15 +38,18 @@ void ReduceConcatOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)>
 }
 
 void ReduceConcatOp::VirtualGenKernelConf(
-    std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp, const ParallelContext*,
-    KernelConf* kernel_conf) const {
+    std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
   ReduceConcatKernelConf* reduce_concat_conf = kernel_conf->mutable_reduce_concat_conf();
   int64_t offset = 0;
   for (int32_t i = 0; i < op_conf().reduce_concat_conf().in_num(); ++i) {
     reduce_concat_conf->mutable_data_offset()->Add(offset);
     offset += RtBlobDesc(*(GetBlobDesc4BnInOp(input_bns().Get(i)))).ByteSizeOfBlobBody();
   }
-  CHECK_LE(offset, RtBlobDesc(*GetBlobDesc4BnInOp(SoleObn())).ByteSizeOfBlobBody());
+  const int64_t data_type_byte_size =
+      static_cast<int64_t>(GetSizeOfDataType(GetBlobDesc4BnInOp(input_bns().Get(0))->data_type()));
+  CHECK_EQ(RoundUp(offset, parallel_ctx->parallel_num() * data_type_byte_size),
+           RtBlobDesc(*GetBlobDesc4BnInOp(SoleObn())).ByteSizeOfBlobBody());
 }
 
 LogicalBlobId ReduceConcatOp::obn2lbi(const std::string& output_bn) const {
