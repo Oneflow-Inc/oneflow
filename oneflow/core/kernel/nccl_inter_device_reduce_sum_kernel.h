d55e23e41fc17239271f680bf75c05ef0e69f156
Juncheng <liujuncheng1022@gmail.com>
2019-01-02 19:16:05 +0800

    kernel

---
+++ oneflow/core/kernel/nccl_inter_device_reduce_sum_kernel.h
@@ -0,0 +1,32 @@
+#ifndef ONEFLOW_CORE_KERNEL_NCCL_INTER_DEVICE_REDUCE_SUM_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_NCCL_INTER_DEVICE_REDUCE_SUM_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+
+namespace oneflow {
+
+template<DeviceType device_type>
+class NcclInterDeviceReduceSumKernel final : KernelIf<device_type> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(NcclInterDeviceReduceSumKernel);
+  NcclInterDeviceReduceSumKernel() = default;
+  ~NcclInterDeviceReduceSumKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx& ctx,
+                          std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+  void BackwardDataContent(const KernelCtx& ctx,
+                           std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+  const PbMessage& GetCustomizedOpConf() const override {
+    return this->op_conf().nccl_inter_device_reduce_sum_conf();
+  }
+};
+
+template<DeviceType device_type>
+struct NcclInterDeviceReduceSumKernelUtil {
+  static void ReduceSum(DeviceCtx* ctx, Blob* send, Blob* recv);
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_NCCL_INTER_DEVICE_REDUCE_SUM_KERNEL_H_
