7bca110aa1eef08bb697d1045d506d20f1304dbd
cheng cheng <472491134@qq.com>
2019-01-07 16:54:13 +0800

    fix complie bug of upsample kernel

--- oneflow/core/kernel/upsample_nearest_kernel.cpp
+++ oneflow/core/kernel/upsample_nearest_kernel.cpp
@@ -9,9 +9,8 @@ void UpsampleNearestKernel<device_type, T>::ForwardDataContent(
   const Blob* in_blob = BnInOp2Blob("in");
   Blob* out_blob = BnInOp2Blob("out");
   const int32_t scale = this->op_conf().upsample_nearest_conf().scale();
-  ResizeNearestNeighborUtil<device_type, T>::Forward(
-      ctx, 1.f/scale, 1.f/scale,false, in_blob,
-      out_blob);
+  ResizeNearestNeighborUtil<device_type, T>::Forward(ctx, 1.f / scale, 1.f / scale, false, in_blob,
+                                                     out_blob);
 }
 
 template<DeviceType device_type, typename T>
@@ -23,8 +22,8 @@ void UpsampleNearestKernel<device_type, T>::BackwardDataContent(
                       in_diff_blob->ByteSizeOfDataContentField());
   const Blob* out_diff_blob = BnInOp2Blob("out_diff");
   const int32_t scale = this->op_conf().upsample_nearest_conf().scale();
-  UpsampleNearestUtil<device_type, T>::Backward(
-      ctx, 1.f/scale, 1.f/scale, false, out_diff_blob, in_diff_blob);
+  ResizeNearestNeighborUtil<device_type, T>::Backward(ctx, 1.f / scale, 1.f / scale, false,
+                                                      out_diff_blob, in_diff_blob);
 }
 
 template<DeviceType device_type, typename T>
@@ -33,7 +32,8 @@ void UpsampleNearestKernel<device_type, T>::ForwardInstanceShape(
   const int32_t scale = this->op_conf().upsample_nearest_conf().scale();
   const Shape& in_shape = BnInOp2Blob("in")->shape();
   CHECK_EQ(in_shape.NumAxes(), 4);
-  BnInOp2Blob("out")->set_instance_shape(Shape({in_shape.At(1), scale * in_shape.At(2), scale * in_shape.At(3)}));
+  BnInOp2Blob("out")->set_instance_shape(
+      Shape({in_shape.At(1), scale * in_shape.At(2), scale * in_shape.At(3)}));
 }
 
 template<DeviceType device_type, typename T>
@@ -43,9 +43,10 @@ void UpsampleNearestKernel<device_type, T>::BackwardInstanceShape(
   if (!in_diff_blob) { return; }
   const int32_t scale = this->op_conf().upsample_nearest_conf().scale();
   const Shape& out_shape = BnInOp2Blob("out_diff")->shape();
-  CHECK_EQ(out_shape.At(2) % scale, 0)
-  CHECK_EQ(out_shape.At(3) % scale, 0)
-  in_diff_blob->set_instance_shape(Shape({out_shape.At(1), out_shape.At(2) / scale, out_shape.At(3) / scale});
+  CHECK_EQ(out_shape.At(2) % scale, 0);
+  CHECK_EQ(out_shape.At(3) % scale, 0);
+  in_diff_blob->set_instance_shape(
+      Shape({out_shape.At(1), out_shape.At(2) / scale, out_shape.At(3) / scale}));
 }
 
 ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kUpsampleNearestConf, UpsampleNearestKernel,
