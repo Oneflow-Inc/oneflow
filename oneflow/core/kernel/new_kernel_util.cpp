e93c3b6df8fc9866cce0914a424a80f7f0ac5f75
nlqq <snow3s@qq.com>
2019-01-14 17:39:29 +0800

    done

--- oneflow/core/kernel/new_kernel_util.cpp
+++ oneflow/core/kernel/new_kernel_util.cpp
@@ -225,6 +225,36 @@ void CpuInitializeWithDir(DeviceCtx* ctx, int32_t part_id, int32_t part_num,
   PersistentInStream in_stream(SnapshotFS(), file_path, begin_pos);
   in_stream.Read(blob->mut_dptr<char>(), blob_size);
 }
+template<typename T>
+void TransposeOnCpu(DeviceCtx* ctx, const int32_t num_axis, const Shape& x_shape,
+                    const Shape& y_shape, const PbRf<int32_t>& permutation, const int64_t elem_cnt,
+                    const T* x, T* y) {
+  int64_t block_size = 1;
+  int32_t shared_idxs_num = 0;
+  for (int32_t i = num_axis - 1; i >= 0 && permutation[i] == i; --i) {
+    block_size *= y_shape.At(i);
+    ++shared_idxs_num;
+  }
+  if (num_axis < 2 || shared_idxs_num == num_axis) {
+    memcpy(y, x, elem_cnt * sizeof(T));
+    return;
+  }
+  int32_t trans_axis = num_axis - shared_idxs_num;
+  std::vector<int64_t> x_to_y_offset;
+  ComputeOffset(trans_axis, y_shape.dim_vec().data(), permutation.data(), x_to_y_offset);
+  std::vector<int64_t> x_index_digits(trans_axis, 0);
+  int64_t num_blocks = elem_cnt / block_size;
+  FOR_RANGE(int64_t, x_idx, 0, num_blocks) {
+    int64_t y_idx = std::inner_product(x_to_y_offset.cbegin(), x_to_y_offset.cend(),
+                                       x_index_digits.cbegin(), 0);
+    if (block_size == 1) {
+      y[y_idx] = x[x_idx];
+    } else {
+      memcpy(y + block_size * y_idx, x + block_size * x_idx, block_size * sizeof(T));
+    }
+    IncreaseIndex(x_shape.dim_vec().data(), x_index_digits);
+  }
+}
 }  // namespace
 
 template<typename T>
@@ -263,31 +293,7 @@ struct NewKernelUtilIf<DeviceType::kCPU, T, typename std::enable_if<IsFloating<T
   static void Transpose(DeviceCtx* ctx, const int32_t num_axis, const Shape& x_shape,
                         const Shape& y_shape, const PbRf<int32_t>& permutation,
                         const int64_t elem_cnt, const T* x, T* y) {
-    int64_t block_size = 1;
-    int32_t shared_idxs_num = 0;
-    for (int32_t i = num_axis - 1; i >= 0 && permutation[i] == i; --i) {
-      block_size *= y_shape.At(i);
-      ++shared_idxs_num;
-    }
-    if (num_axis < 2 || shared_idxs_num == num_axis) {
-      memcpy(y, x, elem_cnt * sizeof(T));
-      return;
-    }
-    int32_t trans_axis = num_axis - shared_idxs_num;
-    std::vector<int64_t> x_to_y_offset;
-    ComputeOffset(trans_axis, y_shape.dim_vec().data(), permutation.data(), x_to_y_offset);
-    std::vector<int64_t> x_index_digits(trans_axis, 0);
-    int64_t num_blocks = elem_cnt / block_size;
-    FOR_RANGE(int64_t, x_idx, 0, num_blocks) {
-      int64_t y_idx = std::inner_product(x_to_y_offset.cbegin(), x_to_y_offset.cend(),
-                                         x_index_digits.cbegin(), 0);
-      if (block_size == 1) {
-        y[y_idx] = x[x_idx];
-      } else {
-        memcpy(y + block_size * y_idx, x + block_size * x_idx, block_size * sizeof(T));
-      }
-      IncreaseIndex(x_shape.dim_vec().data(), x_index_digits);
-    }
+    TransposeOnCpu<T>(ctx, num_axis, x_shape, y_shape, permutation, elem_cnt, x, y);
   }
 };
 
@@ -319,31 +325,7 @@ struct NewKernelUtilIf<DeviceType::kCPU, T, typename std::enable_if<IsIntegral<T
   static void Transpose(DeviceCtx* ctx, const int32_t num_axis, const Shape& x_shape,
                         const Shape& y_shape, const PbRf<int32_t>& permutation,
                         const int64_t elem_cnt, const T* x, T* y) {
-    int64_t block_size = 1;
-    int32_t shared_idxs_num = 0;
-    for (int32_t i = num_axis - 1; i >= 0 && permutation[i] == i; --i) {
-      block_size *= y_shape.At(i);
-      ++shared_idxs_num;
-    }
-    if (num_axis < 2 || shared_idxs_num == num_axis) {
-      memcpy(y, x, elem_cnt * sizeof(T));
-      return;
-    }
-    int32_t trans_axis = num_axis - shared_idxs_num;
-    std::vector<int64_t> x_to_y_offset;
-    ComputeOffset(trans_axis, y_shape.dim_vec().data(), permutation.data(), x_to_y_offset);
-    std::vector<int64_t> x_index_digits(trans_axis, 0);
-    int64_t num_blocks = elem_cnt / block_size;
-    FOR_RANGE(int64_t, x_idx, 0, num_blocks) {
-      int64_t y_idx = std::inner_product(x_to_y_offset.cbegin(), x_to_y_offset.cend(),
-                                         x_index_digits.cbegin(), 0);
-      if (block_size == 1) {
-        y[y_idx] = x[x_idx];
-      } else {
-        memcpy(y + block_size * y_idx, x + block_size * x_idx, block_size * sizeof(T));
-      }
-      IncreaseIndex(x_shape.dim_vec().data(), x_index_digits);
-    }
+    TransposeOnCpu<T>(ctx, num_axis, x_shape, y_shape, permutation, elem_cnt, x, y);
   }
 };
 
