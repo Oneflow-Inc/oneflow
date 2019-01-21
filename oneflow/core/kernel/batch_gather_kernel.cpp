de2e517302d90bf3fcb6ac6e1ee5e828f3ec16b6
ScXfjiang <sc.xfjiang@gmail.com>
2019-01-21 18:02:07 +0800

    Backup: batch_gather op, pass compile

---
+++ oneflow/core/kernel/batch_gather_kernel.cpp
@@ -0,0 +1,118 @@
+#include "oneflow/core/kernel/batch_gather_kernel.h"
+
+namespace oneflow {
+
+namespace {
+
+Shape GetFlatShape(const Shape& shape, int64_t axis) {
+  CHECK_GT(shape.NumAxes(), 0);
+  CHECK_GE(axis, 0);
+  CHECK_LT(axis, shape.NumAxes());
+  return Shape({shape.Count(0, axis), shape.At(axis), shape.Count(axis + 1)});
+}
+
+template<DeviceType device_type, typename T, typename K>
+void BatchGatherForward(DeviceCtx* ctx, const Blob* in, const Blob* indices, Blob* out) {
+  const int64_t axis = indices->shape().NumAxes() - 1;
+  const Shape flat_out_shape = GetFlatShape(out->shape(), axis);
+  BatchGatherKernelUtil<device_type, T, K>::Forward(ctx, in->dptr<T>(), indices->dptr<K>(),
+                                                    flat_out_shape, out->shape().At(axis),
+                                                    out->mut_dptr<T>());
+}
+
+template<DeviceType device_type, typename T, typename K>
+void BatchGatherBackward(DeviceCtx* ctx, const Blob* out_diff, const Blob* indices, Blob* in_diff) {
+  Memset<device_type>(ctx, in_diff->mut_dptr<T>(), 0, in_diff->ByteSizeOfDataContentField());
+  const int64_t axis = indices->shape().NumAxes() - 1;
+  const Shape flat_out_diff_shape = GetFlatShape(out_diff->shape(), axis);
+  BatchGatherKernelUtil<device_type, T, K>::Backward(
+      ctx, out_diff->dptr<T>(), indices->dptr<K>(), flat_out_diff_shape, out_diff->shape().At(axis),
+      in_diff->mut_dptr<T>());
+}
+
+template<DeviceType device_type, typename T>
+struct BatchGatherSwitchUtil final {
+#define MAKE_BATCH_GATHER_SWITCH_ENTRY(func_name, K) func_name<device_type, T, K>
+#define DEFINE_BATCH_GATHER_STATIC_SWITCH_FUNC(func_name)                    \
+  DEFINE_STATIC_SWITCH_FUNC(void, func_name, MAKE_BATCH_GATHER_SWITCH_ENTRY, \
+                            MAKE_DATA_TYPE_CTRV_SEQ(INT_DATA_TYPE_SEQ));
+  DEFINE_BATCH_GATHER_STATIC_SWITCH_FUNC(BatchGatherForward);
+  DEFINE_BATCH_GATHER_STATIC_SWITCH_FUNC(BatchGatherBackward);
+#undef DEFINE_BATCH_GATHER_STATIC_SWITCH_FUNC
+#undef MAKE_BATCH_GATHER_SWITCH_ENTRY
+};
+
+}  // namespace
+
+template<DeviceType device_type, typename T>
+const PbMessage& BatchGatherKernel<device_type, T>::GetCustomizedOpConf() const {
+  return this->op_conf().batch_gather_conf();
+}
+
+template<DeviceType device_type, typename T>
+void BatchGatherKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  BatchGatherSwitchUtil<device_type, T>::SwitchBatchGatherForward(
+      SwitchCase(BnInOp2Blob("indices")->data_type()), ctx.device_ctx, BnInOp2Blob("in"),
+      BnInOp2Blob("indices"), BnInOp2Blob("out"));
+}
+
+template<DeviceType device_type, typename T>
+void BatchGatherKernel<device_type, T>::BackwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  BatchGatherSwitchUtil<device_type, T>::SwitchBatchGatherBackward(
+      SwitchCase(BnInOp2Blob("indices")->data_type()), ctx.device_ctx,
+      BnInOp2Blob(GenDiffBn("out")), BnInOp2Blob("indices"), BnInOp2Blob(GenDiffBn("in")));
+}
+
+template<typename T, typename K>
+struct BatchGatherKernelUtil<DeviceType::kCPU, T, K> final {
+  static void Forward(DeviceCtx* ctx, const T* in, const K* indices, const Shape& flat_out_shape,
+                      const int64_t gather_dim_size, T* out);
+  static void Backward(DeviceCtx* ctx, const T* out_diff, const K* indices,
+                       const Shape& flat_out_diff_shape, const int64_t gather_dim_size, T* in_diff);
+};
+
+template<typename T, typename K>
+void BatchGatherKernelUtil<DeviceType::kCPU, T, K>::Forward(DeviceCtx* ctx, const T* in,
+                                                            const K* indices,
+                                                            const Shape& flat_out_shape,
+                                                            const int64_t gather_dim_size, T* out) {
+  const int64_t batch_num = flat_out_shape.At(0);
+  const int64_t indices_num = flat_out_shape.At(1);
+  const int64_t instance_dim = flat_out_shape.At(2);
+  FOR_RANGE(int64_t, batch_idx, 0, batch_num) {
+    FOR_RANGE(int64_t, i, 0, indices_num) {
+      const K idx = indices[i];
+      CHECK(idx >= 0 && idx < gather_dim_size);
+      const T* from = in + batch_idx * indices_num * instance_dim + idx * instance_dim;
+      T* to = out + batch_idx * indices_num * instance_dim + i * instance_dim;
+      std::copy(from, from + instance_dim, to);
+    }
+  }
+}
+
+template<typename T, typename K>
+void BatchGatherKernelUtil<DeviceType::kCPU, T, K>::Backward(DeviceCtx* ctx, const T* out_diff,
+                                                             const K* indices,
+                                                             const Shape& flat_out_diff_shape,
+                                                             const int64_t gather_dim_size,
+                                                             T* in_diff) {
+  const int64_t batch_num = flat_out_diff_shape.At(0);
+  const int64_t indices_num = flat_out_diff_shape.At(1);
+  const int64_t instance_dim = flat_out_diff_shape.At(2);
+  FOR_RANGE(int64_t, batch_idx, 0, batch_num) {
+    FOR_RANGE(int64_t, i, 0, indices_num) {
+      const int64_t idx = indices[i];
+      CHECK(idx >= 0 && idx < gather_dim_size);
+      const T* from = out_diff + batch_idx * indices_num * instance_dim + i * instance_dim;
+      T* to = in_diff + batch_idx * indices_num * instance_dim + idx * instance_dim;
+      std::transform(from, from + instance_dim, to, to, std::plus<T>());
+    }
+  }
+}
+
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kBatchGatherConf, BatchGatherKernel,
+                           FLOATING_DATA_TYPE_SEQ);
+
+}  // namespace oneflow
--- oneflow/core/operator/batch_gather_op.cpp
+++ oneflow/core/operator/batch_gather_op.cpp
@@ -13,28 +13,24 @@ const PbMessage& BatchGatherOp::GetCustomizedConf() const { return op_conf().bat
 
 void BatchGatherOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                                    const ParallelContext* parallel_ctx) const {
-  const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
-  CHECK_GT(in_blob_desc->shape().NumAxes(), 0);
-  const BlobDesc* indices_blob_desc = GetBlobDesc4BnInOp("indices");
-  CHECK_GT(indices_blob_desc->shape().NumAxes(), 0);
-  CHECK(IsIntegralDataType(indices_blob_desc->data_type()));
-  const std::vector<int64_t>& in_dim_vec = in_blob_desc->shape().dim_vec();
-  const std::vector<int64_t>& indices_dim_vec = indices_blob_desc->shape().dim_vec();
+  const BlobDesc* in = GetBlobDesc4BnInOp("in");
+  CHECK_GT(in->shape().NumAxes(), 0);
+  const BlobDesc* indices = GetBlobDesc4BnInOp("indices");
+  CHECK_GT(indices->shape().NumAxes(), 0);
+  CHECK(IsIntegralDataType(indices->data_type()));
+  const std::vector<int64_t>& in_dim_vec = in->shape().dim_vec();
+  const std::vector<int64_t>& indices_dim_vec = indices->shape().dim_vec();
   CHECK_LE(indices_dim_vec.size(), in_dim_vec.size());
-
-  // out
-  std::vector<int64_t> out_dim_vec;
-  for (int32_t i = 0; i < indices_dim_vec.size() - 1; i++) {
+  FOR_RANGE(int64_t, i, 0, indices_dim_vec.size() - 1) {
     CHECK_EQ(indices_dim_vec.at(i), in_dim_vec.at(i));
-    out_dim_vec.push_back(indices_dim_vec.at(i));
   }
-  out_dim_vec.push_back(indices_dim_vec.back());
-  for (int32_t i = indices_dim_vec.size(); i < in_dim_vec.size(); i++) {
-    out_dim_vec.push_back(in_dim_vec.at(i));
-  }
-  BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
-  *out_blob_desc = *in_blob_desc;
-  out_blob_desc->mut_shape() = Shape(out_dim_vec);
+  // out
+  std::vector<int64_t> out_dim_vec(indices_dim_vec);
+  out_dim_vec.insert(out_dim_vec.end(), in_dim_vec.cbegin() + indices_dim_vec.size(),
+                     in_dim_vec.cend());
+  BlobDesc* out = GetBlobDesc4BnInOp("out");
+  *out = *in;
+  out->mut_shape() = Shape(out_dim_vec);
 }
 
 REGISTER_OP(OperatorConf::kBatchGatherConf, BatchGatherOp);
