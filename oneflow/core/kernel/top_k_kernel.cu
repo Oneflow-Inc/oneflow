785df185dec97968ac1bfca1ff353ce6bb700e25
ScXfjiang <sc.xfjiang@gmail.com>
2019-01-24 17:30:34 +0800

    refine coding style by Juncheng's suggestions, Bravo

--- oneflow/core/kernel/top_k_kernel.cu
+++ oneflow/core/kernel/top_k_kernel.cu
@@ -1,17 +1,16 @@
+#include "oneflow/core/kernel/top_k_kernel.h"
 #include "oneflow/core/common/data_type.h"
 #include "oneflow/core/common/util.h"
 #include "oneflow/core/device/cuda_util.h"
 #include "oneflow/core/kernel/kernel_util.h"
 #include "oneflow/core/kernel/kernel_util.cuh"
-#include "oneflow/core/kernel/top_k_kernel.h"
 
 namespace oneflow {
 
 template<typename T>
 struct TopKKernelUtil<DeviceType::kGPU, T> {
-  static void Forward(const T* in_ptr, const bool sorted, const int32_t instance_num,
-                      const int32_t instance_size, const int32_t k, int32_t* fw_buf,
-                      int32_t* out_ptr) {
+  static void Forward(const T* in, const int32_t instance_num, const int32_t instance_size,
+                      const int32_t k, const bool sorted, int32_t* fw_buf, int32_t* out) {
     UNIMPLEMENTED();
   }
 };
--- oneflow/core/kernel/top_k_kernel.h
+++ oneflow/core/kernel/top_k_kernel.h
@@ -20,9 +20,8 @@ class TopKKernel final : public KernelIf<device_type> {
 
 template<DeviceType device_type, typename T>
 struct TopKKernelUtil {
-  static void Forward(const T* in_ptr, const bool sotred, const int32_t instance_num,
-                      const int32_t instance_size, const int32_t k, int32_t* fw_buf,
-                      int32_t* out_ptr);
+  static void Forward(const T* in, const int32_t instance_num, const int32_t instance_size,
+                      const int32_t k, const bool sorted, int32_t* fw_buf, int32_t* out);
 };
 
 }  // namespace oneflow
