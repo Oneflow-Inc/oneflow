405e91d071fe08cd35a494fd516708d6075ba128
Xinqi <lixinqi0703106@163.com>
2019-01-31 17:07:22 +0800

    rm LogicalBlobParallelConf

--- oneflow/core/job/logical_blob_parallel_desc.cpp
+++ oneflow/core/job/logical_blob_parallel_desc.cpp
@@ -3,32 +3,29 @@
 
 namespace oneflow {
 
-int64_t LogicalBlobParallelDesc::parallel_num() const {
-  CHECK_GT(parallel_num_, 0);
-  return parallel_num_;
+bool operator==(const LogicalBlobParallelDesc& lhs, const LogicalBlobParallelDesc& rhs) {
+  return PbMd().Equivalent(lhs, rhs);
+}
+
+bool operator!=(const LogicalBlobParallelDesc& lhs, const LogicalBlobParallelDesc& rhs) {
+  return !(lhs == rhs);
 }
 
 //  S -> S
 //  P -> C
 //  C -> P
-LogicalBlobParallelDesc LogicalBlobParallelDesc::DualLbpd() const {
-  LogicalBlobParallelDesc ret(*this);
-  if (has_split_parallel()) {
+LogicalBlobParallelDesc GetDualLbpd(const LogicalBlobParallelDesc& lbpd) {
+  LogicalBlobParallelDesc ret(lbpd);
+  if (lbpd.has_split_parallel()) {
     //  do nothing
-  } else if (has_clone_parallel()) {
+  } else if (lbpd.has_clone_parallel()) {
     ret.mutable_partial_sum_parallel();
-  } else if (has_partial_sum_parallel()) {
+  } else if (lbpd.has_partial_sum_parallel()) {
     ret.mutable_clone_parallel();
   } else {
     UNIMPLEMENTED();
   }
   return ret;
-}
-
-bool LogicalBlobParallelDesc::operator==(const LogicalBlobParallelDesc& rhs) const {
-  PbMd message_diff;
-  return parallel_num_ == rhs.parallel_num_
-         && message_diff.Equivalent(lb_parallel_conf_, rhs.lb_parallel_conf_);
 }
 
 }  // namespace oneflow
