17fae768f90f5f9b7deb9117afffc37503b8516f
Xinqi <lixinqi0703106@163.com>
2019-02-17 09:30:49 +0800

    only SplitParallel and BroadcastParallel can be user customized

--- oneflow/core/operator/sbp_parallel_cast_op.cpp
+++ oneflow/core/operator/sbp_parallel_cast_op.cpp
@@ -4,6 +4,18 @@ namespace oneflow {
 
 namespace {
 
+SbpParallel GetSbpParallel(const SbpParallelCastOpConf& conf) {
+  SbpParallel ret;
+  if (conf.has_split_parallel()) {
+    *ret.mutable_split_parallel() = conf.split_parallel();
+  } else if (conf.has_broadcast_parallel()) {
+    *ret.mutable_broadcast_parallel() = conf.broadcast_parallel();
+  } else {
+    UNIMPLEMENTED();
+  }
+  return ret;
+}
+
 class SbpParallelCastOpParallelSignature final : public OpParallelSignature {
  public:
   OF_DISALLOW_COPY_AND_MOVE(SbpParallelCastOpParallelSignature);
@@ -16,8 +28,8 @@ class SbpParallelCastOpParallelSignature final : public OpParallelSignature {
   const OpParallelMatchResult GetMatchResult(
       const std::function<const SbpInferHint&(const std::string&)>& SbpInferHint4BnInOp,
       const ParallelContext* parallel_ctx) const override {
-    const auto& sbp_parallel_conf = op().op_conf().sbp_parallel_cast_conf().sbp();
-    if (SbpInferHint4BnInOp("in").sbp_parallel() == sbp_parallel_conf
+    const auto& configured_sbp_parallel = GetSbpParallel(op().op_conf().sbp_parallel_cast_conf());
+    if (SbpInferHint4BnInOp("in").sbp_parallel() == configured_sbp_parallel
         && parallel_ctx->parallel_num() != SbpInferHint4BnInOp("in").parallel_num()) {
       return MakeOpParallelMatchParallelNumError(parallel_ctx->parallel_num(),
                                                  SbpInferHint4BnInOp("in").parallel_num());
@@ -28,9 +40,9 @@ class SbpParallelCastOpParallelSignature final : public OpParallelSignature {
   void GenerateSignature(
       const std::function<const SbpInferHint&(const std::string&)>& SbpInferHint4BnInOp,
       HashMap<std::string, SbpParallel>* bn2sbp) const override {
-    const auto& sbp_parallel_conf = op().op_conf().sbp_parallel_cast_conf().sbp();
-    (*bn2sbp)["in"] = sbp_parallel_conf;
-    (*bn2sbp)["out"] = sbp_parallel_conf;
+    const auto& sbp_parallel = GetSbpParallel(op().op_conf().sbp_parallel_cast_conf());
+    (*bn2sbp)["in"] = sbp_parallel;
+    (*bn2sbp)["out"] = sbp_parallel;
   }
 };
 
