6fc7ac736f2294bf047e7b66d62dd4384469c904
Dounm <niuchong893184@gmail.com>
2019-02-19 16:31:33 +0800

    fix: remove unnecessary interface

--- oneflow/core/operator/float_to_half_op.h
+++ oneflow/core/operator/float_to_half_op.h
@@ -15,17 +15,9 @@ class FloatToHalfOp final : public Operator {
   void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                       const ParallelContext* parallel_ctx) const override;
   const PbMessage& GetCustomizedConf() const override { return op_conf().float_to_half_conf(); }
-  bool IsElemWiseOp() const override { return true; }
   bool NeedInBlobWhenBackward() const override { return false; }
   bool NeedOutBlobWhenBackward() const override { return false; }
   bool IsInputBlobAllowedModelSplit(const std::string& ibn) const override { return true; }
-  void InferOutputBlobModelSplitAxis(
-      std::function<int32_t*(const std::string&)> ModelSplitAxis4BnInOp,
-      std::function<int32_t(const std::string&)> ShapeNumAxes4BnInOp,
-      const ParallelContext* parallel_context) const override {
-    NaiveInferOutputBlobModelSplitAxis(ModelSplitAxis4BnInOp, ShapeNumAxes4BnInOp,
-                                       parallel_context);
-  }
 };
 
 }  // namespace oneflow
