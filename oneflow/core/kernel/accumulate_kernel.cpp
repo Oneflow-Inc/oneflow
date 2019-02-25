381571cec7382e1172f2f8a7add53f9a63a3d01a
Dounm <niuchong893184@gmail.com>
2019-02-25 12:39:16 +0800

    feat: update accumulate kernel to support half

--- oneflow/core/kernel/accumulate_kernel.cpp
+++ oneflow/core/kernel/accumulate_kernel.cpp
@@ -7,10 +7,11 @@ void AccumulateKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* in_blob = BnInOp2Blob("one");
   Blob* out_blob = BnInOp2Blob("acc");
-  KernelUtil<device_type, T>::Axpy(ctx.device_ctx, in_blob->shape().elem_cnt(), OneVal<T>::value,
-                                   in_blob->dptr<T>(), 1, out_blob->mut_dptr<T>(), 1);
+  NewKernelUtil<device_type, T>::Axpy(ctx.device_ctx, in_blob->shape().elem_cnt(), OneVal<T>::value,
+                                      in_blob->dptr<T>(), 1, out_blob->mut_dptr<T>(), 1);
 }
 
-ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kAccumulateConf, AccumulateKernel, FLOATING_DATA_TYPE_SEQ);
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kAccumulateConf, AccumulateKernel,
+                           FLOATING_DATA_TYPE_SEQ FLOAT16_DATA_TYPE_SEQ);
 
 }  // namespace oneflow
