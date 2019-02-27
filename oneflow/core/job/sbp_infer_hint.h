65dc202ba807d3db97a3a6533d8579efe50c200f
Li Xinqi <lixinqi2010@gmail.com>
2019-02-27 13:54:57 +0800

    replace SbpInferHint::parallel_num_ with SbpInferHint::parallel_desc_ (#1671)
    Former-commit-id: a7480782fa5214262bbb5244e8c10216139bb4e2

--- oneflow/core/job/sbp_infer_hint.h
+++ oneflow/core/job/sbp_infer_hint.h
@@ -2,15 +2,16 @@
 #define ONEFLOW_CORE_JOB_SBP_INFER_HINT_H_
 
 #include "oneflow/core/job/sbp_parallel.pb.h"
+#include "oneflow/core/job/parallel_desc.h"
 
 namespace oneflow {
 
 class SbpInferHint final {
  public:
-  SbpInferHint(bool is_model_blob, int64_t parallel_num, int64_t num_axes,
+  SbpInferHint(bool is_model_blob, const ParallelDesc& parallel_desc, int64_t num_axes,
                const SbpParallel& sbp_parallel)
       : is_model_blob_(is_model_blob),
-        parallel_num_(parallel_num),
+        parallel_desc_(&parallel_desc),
         num_axes_(num_axes),
         sbp_parallel_(sbp_parallel) {}
   SbpInferHint(const SbpInferHint&) = default;
@@ -18,7 +19,7 @@ class SbpInferHint final {
 
   // Getters
   bool is_model_blob() const { return is_model_blob_; }
-  int64_t parallel_num() const;
+  int64_t parallel_num() const { return parallel_desc_->parallel_num(); }
   int64_t num_axes() const;
   int64_t split_axis() const;
   bool has_split_axis() const;
@@ -28,10 +29,11 @@ class SbpInferHint final {
   bool is_data_partial_sum() const;
   bool is_data_blob() const { return !is_model_blob(); }
   const SbpParallel& sbp_parallel() const { return sbp_parallel_; }
+  const ParallelDesc& parallel_desc() const { return *parallel_desc_; }
 
  private:
   const bool is_model_blob_;
-  const int64_t parallel_num_;
+  const ParallelDesc* parallel_desc_;
   const int64_t num_axes_;
   const SbpParallel sbp_parallel_;
 };
