3c0f250f5c7311f19a6063984d5eac772f0f3414
scxfjiang <sc.xfjiang@gmail.com>
2019-02-28 21:32:44 +0800

    refactor smooth l1 op (#1677)

--- oneflow/core/kernel/smooth_l1_kernel.cu
+++ oneflow/core/kernel/smooth_l1_kernel.cu
@@ -1,42 +1,36 @@
 #include "oneflow/core/kernel/smooth_l1_kernel.h"
-#include <cub/cub.cuh>
-#include <math.h>
 
 namespace oneflow {
 
 namespace {
 
 template<typename T>
-__global__ void SmoothL1Forward(const int64_t elem_cnt,
-                                    const T* prediction, const T* label, const T* inside_weights,
-                                    const T* outside_weights, const float beta, const float scale,
-                                    T* out) {
+__global__ void SmoothL1Forward(const int64_t elem_cnt, const T* prediction, const T* label,
+                                const float beta, const float scale, T* out) {
   CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
-    T x = inside_weights[i] * (prediction[i] - label[i]);
-    T abs_x = std::abs(x);
+    const T abs_x = std::abs(prediction[i] - label[i]);
     if (abs_x < beta) {
-      out[i] = 0.5 * x * x / beta;
+      out[i] = 0.5 * abs_x * abs_x / beta;
     } else {
       out[i] = abs_x - 0.5 * beta;
     }
-    out[i] *= scale * outside_weights[i];
+    out[i] *= scale;
   }
 }
-      
+
 template<typename T>
-__global__ void SmoothL1Backward(const int64_t elem_cnt,
-                                      const T* prediction, const T* label, const T* inside_weights,
-                                      const T* outside_weights, const float beta, const float scale,
-                                      T* prediction_diff) {
+__global__ void SmoothL1Backward(const int64_t elem_cnt, const T* out_diff, const T* prediction,
+                                 const T* label, const float beta, const float scale,
+                                 T* prediction_diff) {
   CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
-    T x = inside_weights[i] * (prediction[i] - label[i]);
-    T abs_x = std::abs(x);
+    const T x = prediction[i] - label[i];
+    const T abs_x = std::abs(x);
     if (abs_x < beta) {
       prediction_diff[i] = x / beta;
     } else {
       prediction_diff[i] = (x > ZeroVal<T>::value) - (x < ZeroVal<T>::value);
     }
-    prediction_diff[i] *= scale * inside_weights[i] * outside_weights[i];
+    prediction_diff[i] *= scale * out_diff[i];
   }
 }
 
@@ -44,21 +38,18 @@ __global__ void SmoothL1Backward(const int64_t elem_cnt,
 
 template<typename T>
 struct SmoothL1KernelUtil<DeviceType::kGPU, T> {
-  static void Forward(DeviceCtx* ctx, const int64_t elem_cnt,
-                      const T* prediction, const T* label, const T* inside_weights,
-                      const T* outside_weights, const float beta, const float scale, T* out) {
+  static void Forward(DeviceCtx* ctx, const int64_t elem_cnt, const T* prediction, const T* label,
+                      const float beta, const float scale, T* out) {
     SmoothL1Forward<T>
-        <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0,
-           ctx->cuda_stream()>>>(elem_cnt, prediction, label, inside_weights,
-                                 outside_weights, beta, scale, out);
+        <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            elem_cnt, prediction, label, beta, scale, out);
   }
-  static void Backward(DeviceCtx* ctx, const int64_t elem_cnt,
-                       const T* prediction, const T* label, const T* inside_weights,
-                       const T* outside_weights, const float beta, const float scale, T* prediction_diff) {
+  static void Backward(DeviceCtx* ctx, const int64_t elem_cnt, const T* out_diff,
+                       const T* prediction, const T* label, const float beta, const float scale,
+                       T* prediction_diff) {
     SmoothL1Backward<T>
-        <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0,
-           ctx->cuda_stream()>>>(elem_cnt, prediction, label, inside_weights,
-                                 outside_weights, beta, scale, prediction_diff);
+        <<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            elem_cnt, out_diff, prediction, label, beta, scale, prediction_diff);
   }
 };
 
