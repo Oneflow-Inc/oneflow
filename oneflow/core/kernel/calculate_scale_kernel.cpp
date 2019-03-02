1c2d45e50fdd128a4821cd97c7183f5983df8c21
ScXfjiang <sc.xfjiang@gmail.com>
2019-03-02 13:57:34 +0800

    refine coding style

--- oneflow/core/kernel/calculate_scale_kernel.cpp
+++ oneflow/core/kernel/calculate_scale_kernel.cpp
@@ -6,23 +6,19 @@ template<typename T>
 void CalculateScaleKernel<T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* height_blob = BnInOp2Blob("height");
-  const Blob* weight_blob = BnInOp2Blob("weight");
-  Blob* scale_blob = BnInOp2Blob("scale");
+  const int32_t* height = height_blob->dptr<int32_t>();
+  const int32_t* weight = BnInOp2Blob("weight")->dptr<int32_t>();
+  T* scale = BnInOp2Blob("scale")->mut_dptr<T>();
   const auto& conf = this->op_conf().calculate_scale_conf();
   const int32_t target_size = conf.target_size();
   const int32_t max_size = conf.max_size();
-  CHECK_GT(target_size, 0);
-  CHECK_GE(max_size, target_size);
 
-  const int32_t* height = height_blob->dptr<int32_t>();
-  const int32_t* weight = weight_blob->dptr<int32_t>();
-  T* scale = scale_blob->mut_dptr<T>();
   FOR_RANGE(int32_t, i, 0, height_blob->shape().elem_cnt()) {
     const int32_t im_size_min = std::min(height[i], weight[i]);
     const int32_t im_size_max = std::max(height[i], weight[i]);
-    float im_scale = static_cast<float>(target_size) / static_cast<float>(im_size_min);
+    T im_scale = static_cast<T>(target_size) / static_cast<T>(im_size_min);
     if (std::round(im_scale * im_size_max) > max_size) {
-      im_scale = static_cast<float>(max_size) / static_cast<float>(im_size_max);
+      im_scale = static_cast<T>(max_size) / static_cast<T>(im_size_max);
     }
     scale[i] = im_scale;
   }
--- oneflow/core/operator/calculate_scale_op.cpp
+++ oneflow/core/operator/calculate_scale_op.cpp
@@ -12,12 +12,17 @@ void CalculateScaleOp::InitFromOpConf() {
 void CalculateScaleOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
+  const auto& conf = op_conf().calculate_scale_conf();
+  const int32_t target_size = conf.target_size();
+  const int32_t max_size = conf.max_size();
+  CHECK_GT(target_size, 0);
+  CHECK_GE(max_size, target_size);
   const BlobDesc* height = GetBlobDesc4BnInOp("height");
   const BlobDesc* weight = GetBlobDesc4BnInOp("weight");
   CHECK_EQ(height->shape().NumAxes(), 1);
   CHECK_EQ(height->shape(), weight->shape());
   CHECK_EQ(height->data_type(), DataType::kInt32);
-  CHECK_EQ(weight->data_type(), DataType::kInt32);
+  CHECK_EQ(weight->data_type(), weight->data_type());
 
   // scale
   BlobDesc* scale = GetBlobDesc4BnInOp("scale");
