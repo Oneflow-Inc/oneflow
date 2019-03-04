d7c04e73b9886341963bc1383f9a759be2c4d212
liujuncheng <liujuncheng1022@gmail.com>
2019-03-04 14:31:55 +0800

    fb

--- oneflow/core/kernel/reshape_instance_shape_to_dim0_kernel.cpp
+++ oneflow/core/kernel/reshape_instance_shape_to_dim0_kernel.cpp
@@ -3,7 +3,7 @@
 namespace oneflow {
 
 template<DeviceType device_type>
-void ReshapeInstanceShapeToDim0Kernel<device_type>::ForwardDataContent(
+void ReshapeInstanceShapeToDim0Kernel<device_type>::ForwardDim0ValidNum(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* in = BnInOp2Blob("in");
   Blob* out = BnInOp2Blob("out");
@@ -11,14 +11,23 @@ void ReshapeInstanceShapeToDim0Kernel<device_type>::ForwardDataContent(
   CHECK_EQ(in->shape().elem_cnt() % out_instance_shape.elem_cnt(), 0);
   const int64_t out_dim0_valid_num = in->shape().elem_cnt() / out_instance_shape.elem_cnt();
   out->set_dim0_valid_num(0, out_dim0_valid_num);
-  out->CopyDataContentFrom(ctx.device_ctx, in);
 }
 
 template<DeviceType device_type>
-void ReshapeInstanceShapeToDim0Kernel<device_type>::BackwardDataContent(
+void ReshapeInstanceShapeToDim0Kernel<device_type>::ForwardInstanceShape(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+
+}
+
+template<DeviceType device_type>
+void ReshapeInstanceShapeToDim0Kernel<device_type>::BackwardInDiffDim0ValidNum(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+}
+
+template<DeviceType device_type>
+void ReshapeInstanceShapeToDim0Kernel<device_type>::BackwardInstanceShape(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* in = BnInOp2Blob("in");
-  const Blob* out_diff = BnInOp2Blob(GenDiffBn("out"));
   Blob* in_diff = BnInOp2Blob(GenDiffBn("in"));
   in_diff->set_instance_shape(in->instance_shape());
   if (in->has_dim0_valid_num_field()) {
@@ -26,6 +35,22 @@ void ReshapeInstanceShapeToDim0Kernel<device_type>::BackwardDataContent(
     CHECK_EQ(in->dim0_inner_shape().At(0), 1);
     in_diff->set_dim0_valid_num(0, in->dim0_valid_num(0));
   }
+}
+
+template<DeviceType device_type>
+void ReshapeInstanceShapeToDim0Kernel<device_type>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* in = BnInOp2Blob("in");
+  Blob* out = BnInOp2Blob("out");
+  CHECK_EQ(in->shape().elem_cnt(), out->shape().elem_cnt());
+  out->CopyDataContentFrom(ctx.device_ctx, in);
+}
+
+template<DeviceType device_type>
+void ReshapeInstanceShapeToDim0Kernel<device_type>::BackwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* out_diff = BnInOp2Blob(GenDiffBn("out"));
+  Blob* in_diff = BnInOp2Blob(GenDiffBn("in"));
   CHECK_EQ(in_diff->shape().elem_cnt(), out_diff->shape().elem_cnt());
   in_diff->CopyDataContentFrom(ctx.device_ctx, out_diff);
 }
