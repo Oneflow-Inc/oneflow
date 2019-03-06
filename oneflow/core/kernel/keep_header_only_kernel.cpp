96c3800a1445c1aa48d05447de3ad5c5906e8025
Xinqi <lixinqi0703106@163.com>
2019-03-06 18:46:25 +0800

    remain header only

---
+++ oneflow/core/kernel/keep_header_only_kernel.cpp
@@ -0,0 +1,6 @@
+#include "oneflow/core/kernel/keep_header_only_kernel.h"
+
+namespace oneflow {
+
+ADD_DEVICE_TYPE_KERNEL_CREATOR(OperatorConf::kKeepHeaderOnlyConf, KeepHeaderOnlyKernel);
+}
---
+++ oneflow/core/kernel/keep_header_only_kernel.h
@@ -0,0 +1,23 @@
+#ifndef ONEFLOW_CORE_KERNEL_KEEP_HEADER_ONLY_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_KEEP_HEADER_ONLY_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+#include "oneflow/core/kernel/kernel_context.h"
+
+namespace oneflow {
+
+template<DeviceType device_type>
+class KeepHeaderOnlyKernel final : public KernelIf<device_type> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(KeepHeaderOnlyKernel);
+  KeepHeaderOnlyKernel() = default;
+  ~KeepHeaderOnlyKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx&,
+			  std::function<Blob*(const std::string&)>) const override {}
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_KEEP_HEADER_ONLY_KERNEL_H_
