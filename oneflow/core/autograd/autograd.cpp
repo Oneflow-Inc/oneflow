073ca591f62d7f437a574e822a9e3f99ec1838b9
Li Xinqi <lixinqi2010@gmail.com>
2019-03-11 18:06:13 +0800

    handler clone op grad (#1706)

--- oneflow/core/autograd/autograd.cpp
+++ oneflow/core/autograd/autograd.cpp
@@ -1,6 +1,7 @@
 #include "oneflow/core/autograd/autograd.h"
 #include "oneflow/core/graph/op_graph.h"
 #include "oneflow/core/job/job_conf_builder.h"
+#include "oneflow/core/autograd/clone_grad.h"
 
 namespace oneflow {
 
@@ -41,16 +42,22 @@ std::function<bool(OpNode*)> MakePredicatorNeedBackwardOp(const OpGraph& op_grap
   };
 }
 
-std::function<bool(const LogicalBlobId&)> MakePredicatorHasDiff4Lbi(
+std::function<bool(const LogicalBlobId&, const std::string&)> MakePredicatorHasDiff4LbiOpName(
     const OpGraph& op_graph, const std::function<bool(OpNode*)>& NeedBackwardOp) {
-  auto lbis_with_diff = std::make_shared<HashSet<LogicalBlobId>>();
+  auto lbis2ops_with_in_diff = std::make_shared<HashMap<LogicalBlobId, HashSet<std::string>>>();
   op_graph.ForEachEdge([&](OpEdge* edge) {
-    if (NeedBackwardOp(edge->src_node())) {
-      for (const auto& lbi : edge->lbis()) { lbis_with_diff->emplace(lbi); }
+    if (NeedBackwardOp(edge->src_node()) && NeedBackwardOp(edge->dst_node())) {
+      for (const auto& lbi : edge->lbis()) {
+        if (edge->src_node()->op().HasOutDiff4Lbi(lbi)) {
+          (*lbis2ops_with_in_diff)[lbi].emplace(edge->dst_node()->op().op_name());
+        }
+      }
     }
   });
-  return [lbis_with_diff](const LogicalBlobId& lbi) {
-    return lbis_with_diff->find(lbi) != lbis_with_diff->end();
+  return [lbis2ops_with_in_diff](const LogicalBlobId& lbi, const std::string& op_name) {
+    if (lbis2ops_with_in_diff->find(lbi) == lbis2ops_with_in_diff->end()) { return false; }
+    const auto& op_names = lbis2ops_with_in_diff->at(lbi);
+    return op_names.find(op_name) != op_names.end();
   };
 }
 
@@ -67,9 +74,6 @@ JobConf1 AutoGrad(const JobDesc& job_desc) {
   JobConf1 job_conf(job_desc.job_conf());
   OpGraph op_graph(&job_desc);
   auto NeedBackwardOp = MakePredicatorNeedBackwardOp(op_graph);
-  op_graph.ForEachNode([&](OpNode* op_node) {
-    if (NeedBackwardOp(op_node)) { LOG(INFO) << "NeedBackwardOp: " << op_node->op().op_name(); }
-  });
   std::list<OpNode*> start_nodes;
   {
     std::list<OpNode*> loss_nodes;
@@ -89,24 +93,27 @@ JobConf1 AutoGrad(const JobDesc& job_desc) {
       if (NeedBackwardOp(out_node)) { Handler(out_node); }
     });
   };
-  auto HasDiff4Lbi = MakePredicatorHasDiff4Lbi(op_graph, NeedBackwardOp);
-  HashMap<LogicalBlobId, LogicalBlobId> lbi2grad_lbi;
+  auto HasDiff4LbiOpName = MakePredicatorHasDiff4LbiOpName(op_graph, NeedBackwardOp);
+  HashMap<LogicalBlobId, HashMap<std::string, LogicalBlobId>> lbi2op_name2in_diff_lbi;
+  HashMap<LogicalBlobId, LogicalBlobId> lbi2out_diff_lbi;
   JobConfBuilder job_conf_builder(&job_conf);
   op_graph.TopoForEachNode(start_nodes, ForEachOutNode, ForEachInNode, [&](OpNode* op_node) {
+    const auto& op_name = op_node->op().op_name();
     auto DiffLbi4BnInOp = [&](const std::string& bn) -> LogicalBlobId* {
       const auto& input_bns = op_node->op().input_bns();
       const auto& output_bns = op_node->op().output_bns();
       const auto& lbi = op_node->op().BnInOp2Lbi(bn);
       if (std::find(input_bns.begin(), input_bns.end(), bn) != input_bns.end()) {
-        return HasDiff4Lbi(lbi) ? &lbi2grad_lbi[lbi] : nullptr;
+        return HasDiff4LbiOpName(lbi, op_name) ? &lbi2op_name2in_diff_lbi[lbi][op_name] : nullptr;
       } else if (std::find(output_bns.begin(), output_bns.end(), bn) != output_bns.end()) {
-        if (lbi2grad_lbi.find(lbi) == lbi2grad_lbi.end()) { return nullptr; }
-        return &lbi2grad_lbi.at(lbi);
+        if (lbi2out_diff_lbi.find(lbi) == lbi2out_diff_lbi.end()) { return nullptr; }
+        return &lbi2out_diff_lbi.at(lbi);
       } else {
         UNIMPLEMENTED();
       }
     };
     std::vector<OperatorConf> ops;
+    GenerateCloneGradOpIfNeed(op_node->op(), &ops, lbi2op_name2in_diff_lbi, &lbi2out_diff_lbi);
     GenerateBackwardOpConfIf(op_node->op(), &ops, DiffLbi4BnInOp);
     job_conf_builder.AddOps(op_node->parallel_desc().parallel_conf(), ops);
   });
