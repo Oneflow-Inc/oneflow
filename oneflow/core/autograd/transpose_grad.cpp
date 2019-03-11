1e3fbbabc411e8fc41b70657e8ad6e12df3ea11c
Shenghang Cai <jackalcooper@gmail.com>
2019-03-11 18:19:17 +0800

    address review

--- oneflow/core/autograd/transpose_grad.cpp
+++ oneflow/core/autograd/transpose_grad.cpp
@@ -11,17 +11,16 @@ void GenerateBackwardOpConf(
   const TransposeOpConf& conf = op.op_conf().transpose_conf();
   const PbRf<int32_t>& perm = conf.perm();
   if (DiffLbi4BnInOp("in") != nullptr) {
-    OperatorConf transpose_op;
-    transpose_op.set_name(op.op_name() + "_grad");
-    TransposeOpConf* transpose_op_conf = transpose_op.mutable_transpose_conf();
-    transpose_op_conf->set_out("out");
-    transpose_op_conf->set_in(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    PbRf<int32_t>* invert_perm = transpose_op_conf->mutable_perm();
-    invert_perm->Reserve(perm.size());
-    invert_perm->CopyFrom(perm);
+    OperatorConf reverse_transpose_op;
+    reverse_transpose_op.set_name(op.op_name() + "_grad");
+    TransposeOpConf* reverse_transpose_op_conf = reverse_transpose_op.mutable_transpose_conf();
+    reverse_transpose_op_conf->set_out("out");
+    reverse_transpose_op_conf->set_in(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    PbRf<int32_t>* invert_perm = reverse_transpose_op_conf->mutable_perm();
+    *invert_perm = perm;
     FOR_RANGE(size_t, i, 0, perm.size()) { (*invert_perm)[perm[i]] = i; }
-    op_confs->push_back(transpose_op);
-    DiffLbi4BnInOp("in")->set_op_name(transpose_op.name());
+    op_confs->push_back(reverse_transpose_op);
+    DiffLbi4BnInOp("in")->set_op_name(reverse_transpose_op.name());
     DiffLbi4BnInOp("in")->set_blob_name("out");
   }
 }
