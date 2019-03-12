1c6328e4b50c7404c40fbe19dacbc2b302327bda
Shenghang Cai <jackalcooper@gmail.com>
2019-03-12 16:56:18 +0800

    restore conventional relu bw

--- oneflow/core/kernel/relu_kernel.cpp
+++ oneflow/core/kernel/relu_kernel.cpp
@@ -10,6 +10,15 @@ void ReluKernel<device_type, T>::ForwardDataContent(
                                    BnInOp2Blob("y")->mut_dptr<T>());
 }
 
+template<DeviceType device_type, typename T>
+void ReluKernel<device_type, T>::BackwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  Blob* y_blob = BnInOp2Blob("y");
+  KernelUtil<device_type, T>::ReluBackward(
+      ctx.device_ctx, y_blob->shape().elem_cnt(), y_blob->dptr<T>(), y_blob->dptr<T>(),
+      BnInOp2Blob("y_diff")->dptr<T>(), BnInOp2Blob("x_diff")->mut_dptr<T>());
+}
+
 ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReluConf, ReluKernel, FLOATING_DATA_TYPE_SEQ);
 
 }  // namespace oneflow
