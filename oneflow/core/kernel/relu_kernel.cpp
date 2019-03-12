28c0d8f166c2a6f151c0e62b6a9d21e2cb508c18
Shenghang Cai <jackalcooper@gmail.com>
2019-03-12 16:34:46 +0800

    new relu

--- oneflow/core/kernel/relu_kernel.cpp
+++ oneflow/core/kernel/relu_kernel.cpp
@@ -5,18 +5,9 @@ namespace oneflow {
 template<DeviceType device_type, typename T>
 void ReluKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  Blob* in_blob = BnInOp2Blob("in");
-  KernelUtil<device_type, T>::Relu(ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
-                                   BnInOp2Blob("out")->mut_dptr<T>());
-}
-
-template<DeviceType device_type, typename T>
-void ReluKernel<device_type, T>::BackwardDataContent(
-    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  Blob* out_blob = BnInOp2Blob("out");
-  KernelUtil<device_type, T>::ReluBackward(
-      ctx.device_ctx, out_blob->shape().elem_cnt(), out_blob->dptr<T>(), out_blob->dptr<T>(),
-      BnInOp2Blob("out_diff")->dptr<T>(), BnInOp2Blob("in_diff")->mut_dptr<T>());
+  Blob* x_blob = BnInOp2Blob("x");
+  KernelUtil<device_type, T>::Relu(ctx.device_ctx, x_blob->shape().elem_cnt(), x_blob->dptr<T>(),
+                                   BnInOp2Blob("y")->mut_dptr<T>());
 }
 
 ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReluConf, ReluKernel, FLOATING_DATA_TYPE_SEQ);
--- oneflow/core/operator/relu_op.cpp
+++ oneflow/core/operator/relu_op.cpp
@@ -4,15 +4,15 @@ namespace oneflow {
 
 void ReluOp::InitFromOpConf() {
   CHECK(op_conf().has_relu_conf());
-  EnrollInputBn("in");
-  EnrollOutputBn("out");
+  EnrollInputBn("x");
+  EnrollOutputBn("y");
 }
 
 const PbMessage& ReluOp::GetCustomizedConf() const { return op_conf().relu_conf(); }
 
 void ReluOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                             const ParallelContext* parallel_ctx) const {
-  *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
+  *GetBlobDesc4BnInOp("y") = *GetBlobDesc4BnInOp("x");
 }
 
 REGISTER_OP(OperatorConf::kReluConf, ReluOp);
