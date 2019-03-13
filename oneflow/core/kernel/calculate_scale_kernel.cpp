b9cfe35c233ca929e017c0f1239cd3ca5632031f
ScXfjiang <sc.xfjiang@gmail.com>
2019-03-13 17:03:45 +0800

    add height and width to calculate_scale_op outputs

--- oneflow/core/kernel/calculate_scale_kernel.cpp
+++ oneflow/core/kernel/calculate_scale_kernel.cpp
@@ -5,21 +5,25 @@ namespace oneflow {
 template<typename T>
 void CalculateScaleKernel<T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* height_blob = BnInOp2Blob("height");
-  const int32_t* height = height_blob->dptr<int32_t>();
-  const int32_t* width = BnInOp2Blob("width")->dptr<int32_t>();
+  const Blob* origin_height_blob = BnInOp2Blob("origin_height");
+  const int32_t* origin_height = origin_height_blob->dptr<int32_t>();
+  const int32_t* origin_width = BnInOp2Blob("origin_width")->dptr<int32_t>();
   T* scale = BnInOp2Blob("scale")->mut_dptr<T>();
+  int32_t* height = BnInOp2Blob("height")->mut_dptr<int32_t>();
+  int32_t* width = BnInOp2Blob("width")->mut_dptr<int32_t>();
   const auto& conf = this->op_conf().calculate_scale_conf();
   const int32_t target_size = conf.target_size();
   const int32_t max_size = conf.max_size();
 
-  FOR_RANGE(int32_t, i, 0, height_blob->shape().elem_cnt()) {
-    const int32_t im_size_min = std::min(height[i], width[i]);
-    const int32_t im_size_max = std::max(height[i], width[i]);
+  FOR_RANGE(int32_t, i, 0, origin_height_blob->shape().elem_cnt()) {
+    const int32_t im_size_min = std::min(origin_height[i], origin_width[i]);
+    const int32_t im_size_max = std::max(origin_height[i], origin_width[i]);
     scale[i] = static_cast<T>(target_size) / static_cast<T>(im_size_min);
     if (std::round(scale[i] * im_size_max) > max_size) {
       scale[i] = static_cast<T>(max_size) / static_cast<T>(im_size_max);
     }
+    height[i] = static_cast<int32_t>(origin_height[i] * scale[i]);
+    width[i] = static_cast<int32_t>(origin_width[i] * scale[i]);
   }
 }
 
