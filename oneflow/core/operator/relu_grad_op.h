3892b0b4b7b408fe902baaa36bb3fc0666a838f4
jackalcooper <jackalcooper@gmail.com>
2019-03-14 16:15:25 +0800

    new relu (#1709)
    * new relu
    
    * address comment
    
    * restore conventional relu bw
    
    * add left src file
    
    * set IsInputBlobAllowedModelSplit false
    
    * quick fix
    
    * address review

---
+++ oneflow/core/operator/relu_grad_op.h
@@ -0,0 +1,27 @@
+#ifndef ONEFLOW_CORE_OPERATOR_RELU_GRAD_OP_H_
+#define ONEFLOW_CORE_OPERATOR_RELU_GRAD_OP_H_
+
+#include "oneflow/core/operator/operator.h"
+
+namespace oneflow {
+
+class ReluGradOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(ReluGradOp);
+  ReluGradOp() = default;
+  ~ReluGradOp() = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  bool NeedInBlobWhenBackward() const override { return false; }
+
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override;
+
+ private:
+  bool IsInputBlobAllowedModelSplit(const std::string& ibn) const override { return false; }
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_RELU_GRAD_OP_H_
