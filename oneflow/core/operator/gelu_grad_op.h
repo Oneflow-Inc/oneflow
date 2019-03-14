966c521425ec8f292e608f602356a24242575f58
jackalcooper <jackalcooper@gmail.com>
2019-03-14 19:57:16 +0800

    gelu_grad (#1721)
    * gelu_grad
    
    * remove unnecessary
    
    * address review
    
    * remove unnecessary

---
+++ oneflow/core/operator/gelu_grad_op.h
@@ -0,0 +1,25 @@
+#ifndef ONEFLOW_CORE_OPERATOR_GELU_GRAD_OP_H_
+#define ONEFLOW_CORE_OPERATOR_GELU_GRAD_OP_H_
+
+#include "oneflow/core/operator/operator.h"
+
+namespace oneflow {
+
+class GeluGradOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(GeluGradOp);
+  GeluGradOp() = default;
+  ~GeluGradOp() = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override;
+
+ private:
+  bool IsInputBlobAllowedModelSplit(const std::string& ibn) const override { return false; }
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_GELU_GRAD_OP_H_
