33f32d585f2278e0461fb6fdc49f89ef960d4550
Shenghang Cai <jackalcooper@gmail.com>
2019-03-14 16:02:55 +0800

    address review

--- oneflow/core/operator/relu_op.cpp
+++ oneflow/core/operator/relu_op.cpp
@@ -4,15 +4,15 @@ namespace oneflow {
 
 void ReluOp::InitFromOpConf() {
   CHECK(op_conf().has_relu_conf());
-  EnrollInputBn("x");
-  EnrollOutputBn("y");
+  EnrollInputBn("in");
+  EnrollOutputBn("out");
 }
 
 const PbMessage& ReluOp::GetCustomizedConf() const { return op_conf().relu_conf(); }
 
 void ReluOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                             const ParallelContext* parallel_ctx) const {
-  *GetBlobDesc4BnInOp("y") = *GetBlobDesc4BnInOp("x");
+  *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
 }
 
 REGISTER_OP(OperatorConf::kReluConf, ReluOp);
