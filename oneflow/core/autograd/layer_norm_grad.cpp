45f0b7c1e5778203bb7a27e3b1aa5cf0a8928684
liujuncheng <liujuncheng1022@gmail.com>
2019-03-16 16:37:48 +0800

    fix

--- oneflow/core/autograd/layer_norm_grad.cpp
+++ oneflow/core/autograd/layer_norm_grad.cpp
@@ -12,28 +12,30 @@ void GenerateBackwardOpConf(
   if (conf.center()) { CHECK(conf.has_beta()); }
   if (conf.scale()) { CHECK(conf.has_gamma()); }
   LogicalBlobId grad_dy_lbi = *DiffLbi4BnInOp("out");
-  if (DiffLbi4BnInOp("beta") || DiffLbi4BnInOp("gamma")
-      || (conf.has_gamma() && DiffLbi4BnInOp("in"))) {
+  const bool has_beta_diff = conf.has_beta() && DiffLbi4BnInOp("beta");
+  const bool has_gamma_diff = conf.has_gamma() && DiffLbi4BnInOp("gamma");
+  const bool need_scale_out_diff = conf.has_gamma() && DiffLbi4BnInOp("in");
+  if (has_beta_diff || has_gamma_diff || need_scale_out_diff) {
     OperatorConf param_grad_op;
     param_grad_op.set_name(op.op_name() + "_param_grad");
     LayerNormParamGradOpConf* param_grad_conf = param_grad_op.mutable_layer_norm_param_grad_conf();
     param_grad_conf->set_begin_params_axis(conf.begin_params_axis());
     param_grad_conf->set_dy(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    if (DiffLbi4BnInOp("beta")) {
+    if (has_beta_diff) {
       param_grad_conf->set_beta_diff("beta_diff");
       DiffLbi4BnInOp("beta")->set_op_name(param_grad_op.name());
       DiffLbi4BnInOp("beta")->set_blob_name("beta_diff");
     }
-    if (DiffLbi4BnInOp("gamma") || (conf.has_gamma() && DiffLbi4BnInOp("in"))) {
+    if (has_gamma_diff || need_scale_out_diff) {
       param_grad_conf->set_gamma(GenLogicalBlobName(op.BnInOp2Lbi("gamma")));
     }
-    if (DiffLbi4BnInOp("gamma")) {
+    if (has_gamma_diff) {
       param_grad_conf->set_normalized(GenLogicalBlobName(op.BnInOp2Lbi("normalized")));
       param_grad_conf->set_gamma_diff("gamma_diff");
       DiffLbi4BnInOp("gamma")->set_op_name(param_grad_op.name());
       DiffLbi4BnInOp("gamma")->set_blob_name("gamma_diff");
     }
-    if (conf.has_gamma() && DiffLbi4BnInOp("in")) {
+    if (need_scale_out_diff) {
       param_grad_conf->set_dx("dx");
       grad_dy_lbi.set_op_name(param_grad_op.name());
       grad_dy_lbi.set_blob_name("dx");
