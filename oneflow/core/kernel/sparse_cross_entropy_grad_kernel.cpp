e7bfe2c388fc24439d53e5485d6ba21cf79232c2
Cai Shenghang <caishenghang@gmail.com>
2019-03-19 11:59:16 +0800

    check in files

---
+++ oneflow/core/kernel/sparse_cross_entropy_grad_kernel.cpp
@@ -0,0 +1,46 @@
+#include "oneflow/core/kernel/sparse_cross_entropy_kernel_util.h"
+#include "oneflow/core/kernel/sparse_cross_entropy_kernel.h"
+#include "oneflow/core/kernel/sparse_cross_entropy_grad_kernel.h"
+
+namespace oneflow {
+
+namespace {
+
+template<DeviceType device_type, typename T, typename K>
+void Backward(DeviceCtx* ctx, const Blob* prediction, const Blob* label, const Blob* out_diff,
+              Blob* prediction_diff) {
+  const int64_t num_instances = label->shape().elem_cnt();
+  CHECK_EQ(prediction->shape().elem_cnt() % num_instances, 0);
+  const int64_t num_classes = prediction->shape().elem_cnt() / num_instances;
+  Memset<device_type>(ctx, prediction_diff->mut_dptr<T>(), 0,
+                      prediction_diff->ByteSizeOfDataContentField());
+  SparseCrossEntropyKernelUtil<device_type, T, K>::ComputeDiff(
+      ctx, num_instances, num_classes, prediction->dptr<T>(), label->dptr<K>(), out_diff->dptr<T>(),
+      prediction_diff->mut_dptr<T>());
+}
+
+template<DeviceType device_type, typename T>
+struct SparseCrossEntropyUntil final {
+#define MAKE_CROSS_ENTROPY_SWITCH_ENTRY(func_name, K) func_name<device_type, T, K>
+  DEFINE_STATIC_SWITCH_FUNC(void, Backward, MAKE_CROSS_ENTROPY_SWITCH_ENTRY,
+                            MAKE_DATA_TYPE_CTRV_SEQ(INT_DATA_TYPE_SEQ));
+#undef MAKE_CROSS_ENTROPY_SWITCH_ENTRY
+};
+
+}  // namespace
+
+template<DeviceType device_type, typename T>
+void SparseCrossEntropyGradKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* prediction = BnInOp2Blob("prediction");
+  const Blob* label = BnInOp2Blob("label");
+  const Blob* out_diff = BnInOp2Blob("dy");
+  Blob* prediction_diff = BnInOp2Blob("prediction_grad");
+  SparseCrossEntropyUntil<device_type, T>::SwitchBackward(
+      SwitchCase(label->data_type()), ctx.device_ctx, prediction, label, out_diff, prediction_diff);
+}
+
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kSparseCrossEntropyConf, SparseCrossEntropyKernel,
+                           FLOATING_DATA_TYPE_SEQ);
+
+}  // namespace oneflow
---
+++ oneflow/core/operator/sparse_cross_entropy_grad_op.cpp
@@ -0,0 +1,26 @@
+#include "oneflow/core/operator/sparse_cross_entropy_grad_op.h"
+
+namespace oneflow {
+
+void SparseCrossEntropyGradOp::InitFromOpConf() {
+  CHECK(op_conf().has_sparse_cross_entropy_conf());
+  EnrollInputBn("prediction");
+  EnrollInputBn("label");
+  EnrollInputBn("dy");
+  EnrollOutputBn("prediction_grad");
+}
+
+const PbMessage& SparseCrossEntropyGradOp::GetCustomizedConf() const {
+  return op_conf().sparse_cross_entropy_grad_conf();
+}
+
+void SparseCrossEntropyGradOp::InferBlobDescs(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx, int64_t record_piece_size,
+    std::function<void(OpContext*)> EnrollOpCtx) const {
+  *GetBlobDesc4BnInOp("prediction_grad") = *GetBlobDesc4BnInOp("prediction");
+}
+
+REGISTER_OP(OperatorConf::kSparseCrossEntropyGradConf, SparseCrossEntropyGradOp);
+
+}  // namespace oneflow
