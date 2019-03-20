f0fd96dfd8c7cc9653e14562827637718e41798e
Cai Shenghang <caishenghang@gmail.com>
2019-03-20 16:03:45 +0800

    check in files

--- oneflow/core/kernel/broadcast_like_kernel.cpp
+++ oneflow/core/kernel/broadcast_like_kernel.cpp
@@ -7,28 +7,18 @@ template<DeviceType device_type, typename T>
 void BroadcastLikeKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* x = BnInOp2Blob("x");
-  const Blob* like = BnInOp2Blob("like");
   Blob* y = BnInOp2Blob("y");
-  int64_t n = y->shape().elem_cnt();
-  if (x->shape().elem_cnt() == 1) {
-    CHECK_EQ(n, like->shape().elem_cnt());
-    KernelUtil<device_type, T>::Replicate(ctx.device_ctx, n, y->mut_dptr<T>(), x->dptr<T>());
-  } else if (like->shape().elem_cnt() == 1) {
-    CHECK_EQ(n, x->shape().elem_cnt());
-    KernelUtil<device_type, T>::Replicate(ctx.device_ctx, n, y->mut_dptr<T>(), like->dptr<T>());
+  const int64_t n = y->shape().elem_cnt();
+  const int64_t num_axes = y->shape().NumAxes();
+  if (this->op_conf().broadcast_like_conf().has_kept_dims_shape()) {
+    NdarrayUtil<device_type, T>::BroadcastTo(
+        ctx.device_ctx, XpuVarNdarray<T>(y, num_axes),
+        XpuVarNdarray<const T>(Shape(this->op_conf().broadcast_like_conf().kept_dims_shape()),
+                               x->dptr<T>()));
   } else {
-    size_t num_axes = y->shape().NumAxes();
-    if (this->op_conf().broadcast_like_conf().has_kept_dims_shape()) {
-      NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncDiv>(
-          ctx.device_ctx, XpuVarNdarray<T>(y, num_axes),
-          XpuVarNdarray<const T>(Shape(this->op_conf().broadcast_like_conf().kept_dims_shape()),
-                                 x->dptr<T>()),
-          XpuVarNdarray<const T>(like, num_axes));
-    } else {
-      NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncDiv>(
-          ctx.device_ctx, XpuVarNdarray<T>(y, num_axes), XpuVarNdarray<const T>(x, num_axes),
-          XpuVarNdarray<const T>(like, num_axes));
-    }
+    NdarrayUtil<device_type, T>::BroadcastTo(
+        ctx.device_ctx, XpuVarNdarray<T>(y, num_axes),
+        XpuVarNdarray<T>(x, num_axes);
   }
 }
 
--- oneflow/core/operator/broadcast_like_op.cpp
+++ oneflow/core/operator/broadcast_like_op.cpp
@@ -39,28 +39,17 @@ const PbMessage& BroadcastLikeOp::GetCustomizedConf() const {
 void BroadcastLikeOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
-  const BlobDesc* a_blob_desc = GetBlobDesc4BnInOp("x");
-  const BlobDesc* b_blob_desc = GetBlobDesc4BnInOp("like");
-  CHECK_EQ(a_blob_desc->data_type(), b_blob_desc->data_type());
-  BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("y");
-  size_t output_num_axes = std::max(a_blob_desc->shape().NumAxes(), b_blob_desc->shape().NumAxes());
-  if (IsScalarBlob(a_blob_desc)) {
-    *out_blob_desc = *b_blob_desc;
-  } else if (IsScalarBlob(b_blob_desc)) {
-    *out_blob_desc = *a_blob_desc;
-  } else {
-    const auto& a_shape = op_conf().broadcast_like_conf().has_kept_dims_shape()
-                              ? Shape(op_conf().broadcast_like_conf().kept_dims_shape())
-                              : a_blob_desc->shape().CreateLeftExtendedShape(output_num_axes);
-    const auto& b_shape = b_blob_desc->shape().CreateLeftExtendedShape(output_num_axes);
-    *out_blob_desc = *a_blob_desc;
-    Shape out_shape(a_shape);
-    FOR_RANGE(int64_t, i, 0, a_shape.NumAxes()) {
-      CHECK(a_shape.At(i) == 1 || b_shape.At(i) == 1 || a_shape.At(i) == b_shape.At(i));
-      out_shape.Set(i, std::max(a_shape.At(i), b_shape.At(i)));
-    }
-    out_blob_desc->mut_shape() = out_shape;
+  const BlobDesc* x_blob_desc = GetBlobDesc4BnInOp("x");
+  const BlobDesc* like_blob_desc = GetBlobDesc4BnInOp("like");
+  int64_t y_num_axes = std::max(like_blob_desc->shape().NumAxes(), x_blob_desc->shape().NumAxes());
+  const auto& x_shape = op_conf().broadcast_like_conf().has_kept_dims_shape()
+                        ? Shape(op_conf().broadcast_like_conf().kept_dims_shape())
+                        : x_blob_desc->shape().CreateLeftExtendedShape(y_num_axes);
+  const auto& like_shape = like_blob_desc->shape().CreateLeftExtendedShape(y_num_axes);
+  FOR_RANGE(int64_t, i, 0, x_shape.NumAxes()) {
+    CHECK(x_shape.At(i) == 1 || like_shape.At(i) == 1 || x_shape.At(i) == like_shape.At(i));
   }
+  *GetBlobDesc4BnInOp("y") = *like_blob_desc;
 }
 
 void BroadcastLikeOp::GetOpParallelSignatures(
