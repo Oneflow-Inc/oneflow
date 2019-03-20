d28ec0ccaf4e02f763d6c9ae4a13224af443e3d4
Cai Shenghang <caishenghang@gmail.com>
2019-03-20 16:10:45 +0800

    minor fix

--- oneflow/core/kernel/broadcast_like_kernel.cpp
+++ oneflow/core/kernel/broadcast_like_kernel.cpp
@@ -8,7 +8,6 @@ void BroadcastLikeKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* x = BnInOp2Blob("x");
   Blob* y = BnInOp2Blob("y");
-  const int64_t n = y->shape().elem_cnt();
   const int64_t num_axes = y->shape().NumAxes();
   if (this->op_conf().broadcast_like_conf().has_kept_dims_shape()) {
     NdarrayUtil<device_type, T>::BroadcastTo(
@@ -18,7 +17,7 @@ void BroadcastLikeKernel<device_type, T>::ForwardDataContent(
   } else {
     NdarrayUtil<device_type, T>::BroadcastTo(
         ctx.device_ctx, XpuVarNdarray<T>(y, num_axes),
-        XpuVarNdarray<T>(x, num_axes);
+        XpuVarNdarray<const T>(x, num_axes));
   }
 }
 
