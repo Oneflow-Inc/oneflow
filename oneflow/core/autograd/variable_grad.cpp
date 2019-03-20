eef536a23dd9592900a141916741a4e2c707fce5
Shiyuan Shang-Guan <shgshy@gmail.com>
2019-03-20 17:43:04 +0800

    temp

--- oneflow/core/autograd/variable_grad.cpp
+++ oneflow/core/autograd/variable_grad.cpp
@@ -7,28 +7,43 @@ namespace {
 void GenerateBackwardOpConf(
     const Operator& op, std::vector<OperatorConf>* op_confs,
     const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
-  // TODO: support optimizers other than SGD
   const auto& train_conf =
       Global<JobDesc>::Get()->other_conf().predict_conf().tmp_split_fw_bw_train_conf();
   const auto& model_update_conf = train_conf.model_update_conf();
-  if (model_update_conf.has_naive_conf()) {
-    // TODO complete regularization
-    OperatorConf axpy_op;
-    axpy_op.set_name(op.op_name() + "_grad");
-    AxpyOpConf* axpy_op_conf = axpy_op.mutable_axpy_conf();
-    axpy_op_conf->set_x(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    axpy_op_conf->set_y(GenLogicalBlobName(op.BnInOp2Lbi("out")));
-    axpy_op_conf->set_alpha(-train_conf.primary_lr());
-    op_confs->push_back(axpy_op);
-  } else if (model_update_conf.has_momentum_conf()) {
-    TODO();
-  } else if (model_update_conf.has_rmsprop_conf()) {
-    TODO();
-  } else if (model_update_conf.has_adam_conf()) {
-    TODO();
+  OperatorConf mdupdt_op;
+  mdupdt_op.set_name(op.op_name() + "_grad");
+  mdupdt_op.mutable_normal_mdupdt_conf()->set_model_diff(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+  mdupdt_op.mutable_normal_mdupdt_conf()->set_total_instance_num_diff(op.op_name() + '/'
+                                                                      + "total_instance_num");
+  mdupdt_op.mutable_normal_mdupdt_conf()->set_model(GenLogicalBlobName(op.BnInOp2Lbi("out")));
+  if (op.op_conf().variable_conf().ModelName() == "total_instance_num") {
+    mdupdt_op.mutable_normal_mdupdt_conf()->mutable_user_conf()->mutable_naive_conf();
   } else {
-    UNIMPLEMENTED();
+    *(mdupdt_op.mutable_normal_mdupdt_conf()->mutable_user_conf()) =
+        Global<JobDesc>::Get()->other_conf().train_conf().model_update_conf();
   }
+  float primary_lr = Global<JobDesc>::Get()->primary_lr();
+  float secondary_lr = Global<JobDesc>::Get()->secondary_lr();
+  if (secondary_lr < 0) { secondary_lr = primary_lr; }
+  if (op.op_conf().variable_conf().ModelName() == "weight") {
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_learning_rate(primary_lr);
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l1(Global<JobDesc>::Get()->weight_l1());
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l2(Global<JobDesc>::Get()->weight_l2());
+  } else if (op.op_conf().variable_conf().ModelName() == "bias") {
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_learning_rate(secondary_lr);
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l1(Global<JobDesc>::Get()->bias_l1());
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l2(Global<JobDesc>::Get()->bias_l2());
+  } else if (op.op_conf().variable_conf().ModelName() == "total_instance_num") {
+    // we don't treat total_instance_num as model, just use total_instance_num_diff
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_learning_rate(-1.0);
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l1(0);
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l2(0);
+  } else {
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_learning_rate(primary_lr);
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l1(0);
+    mdupdt_op.mutable_normal_mdupdt_conf()->set_l2(0);
+  }
+  op_confs->push_back(mdupdt_op);
 }
 
 }  // namespace
