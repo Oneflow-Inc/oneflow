17c1bdc3b75f9bccbbca9a00681cb6217ae2acc5
Cai Shenghang <caishenghang@gmail.com>
2019-03-22 00:14:49 +0800

    rename b_blob

--- oneflow/core/kernel/broadcast_div_grad_kernel.cpp
+++ oneflow/core/kernel/broadcast_div_grad_kernel.cpp
@@ -6,12 +6,12 @@ namespace oneflow {
 template<DeviceType device_type, typename T>
 void BroadcastDivGradKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* b = BnInOp2Blob("b");
-  const Blob* dy_blob = BnInOp2Blob("dy");
+  const Blob* b_blob = BnInOp2Blob("b");
   const Blob* y_blob = BnInOp2Blob("y");
+  const Blob* dy_blob = BnInOp2Blob("dy");
   Blob* tmp_blob = BnInOp2Blob("temp_storage");
   Blob* b_diff_blob = BnInOp2Blob("db");
-  
+
   const int64_t num_axes = dy_blob->shape().NumAxes();
   XpuVarNdarray<const T> dy(dy_blob, num_axes);
   XpuVarNdarray<const T> const_tmp(dy.shape(), tmp_blob->dptr<T>());
@@ -20,7 +20,7 @@ void BroadcastDivGradKernel<device_type, T>::ForwardDataContent(
 
   NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncDiv>(
       ctx.device_ctx, tmp, XpuVarNdarray<const T>(y_blob, num_axes),
-      XpuVarNdarray<const T>(b, num_axes));
+      XpuVarNdarray<const T>(b_blob, num_axes));
   NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncMul>(
       ctx.device_ctx, tmp, dy, const_tmp);
   NdarrayUtil<device_type, T>::ReduceSum(ctx.device_ctx, XpuVarNdarray<T>(b_diff_blob, num_axes),
