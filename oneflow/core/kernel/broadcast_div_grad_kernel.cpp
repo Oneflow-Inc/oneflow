615f71003db5a24a210457ba8708228e117b78ca
Cai Shenghang <caishenghang@gmail.com>
2019-03-21 23:59:29 +0800

    Create broadcast_div_grad_kernel.cpp

---
+++ oneflow/core/kernel/broadcast_div_grad_kernel.cpp
@@ -0,0 +1,39 @@
+#include "oneflow/core/kernel/broadcast_div_grad_kernel.h"
+#include "oneflow/core/ndarray/ndarray_util.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+void BroadcastDivGradKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* a = BnInOp2Blob("a");
+  const Blob* b = BnInOp2Blob("b");
+  const Blob* out_diff = BnInOp2Blob(GenDiffBn("out"));
+  Blob* tmp = BnInOp2Blob("temp_storage");
+  Blob* b_diff = BnInOp2Blob(GenDiffBn("b"));
+
+  KernelUtil<device_type, T>::Reciprocal(ctx.device_ctx, b->shape().elem_cnt(), b->dptr<T>(),
+                                         tmp->mut_dptr<T>());
+
+  const int64_t num_axes = out_diff->shape().NumAxes();
+  XpuVarNdarray<const T> out_diff_tensor(out_diff, num_axes);
+  Blob* bw_buf_blob = BnInOp2Blob("bw_buf");
+  XpuVarNdarray<const T> const_tmp(out_diff_tensor.shape(), bw_buf_blob->dptr<T>());
+  XpuVarNdarray<T> tmp_tensor(out_diff_tensor.shape(), bw_buf_blob->mut_dptr<T>());
+  if (b_diff) {
+    const Blob* out_blob = BnInOp2Blob("out");
+    NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncDiv>(
+        ctx.device_ctx, tmp_tensor, XpuVarNdarray<const T>(out_blob, num_axes),
+        XpuVarNdarray<const T>(b, num_axes));
+    NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncMul>(
+        ctx.device_ctx, tmp_tensor, out_diff_tensor, const_tmp);
+    NdarrayUtil<device_type, T>::ReduceSum(ctx.device_ctx, XpuVarNdarray<T>(b_diff, num_axes),
+                                           const_tmp, tmp_tensor);
+    NdarrayUtil<device_type, T>::template ImplaceApplyUnary<UnaryFuncMinus>(
+        ctx.device_ctx, XpuVarNdarray<T>(b_diff, num_axes));
+  }
+}
+
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kDenominatorGradConf, BroadcastDivGradKernel,
+                           FLOATING_DATA_TYPE_SEQ);
+}  // namespace oneflow
