037b08760c22c61a900fd3f1b30d0e3f5381cc3e
Cai Shenghang <caishenghang@gmail.com>
2019-03-21 11:36:03 +0800

    check in files

---
+++ oneflow/core/kernel/reduce_sum_like_kernel.cpp
@@ -0,0 +1,21 @@
+#include "oneflow/core/kernel/reduce_sum_like_kernel.h"
+#include "oneflow/core/ndarray/ndarray_util.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+void ReduceSumLikeKernel<device_type, T>::ForwardDataContent(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* x_blob = BnInOp2Blob("x");
+  Blob* y_blob = BnInOp2Blob("y");
+  Blob* fw_tmp_blob = BnInOp2Blob("fw_tmp");
+  NdarrayUtil<device_type, T>::ReduceSum(
+      ctx.device_ctx, XpuVarNdarray<T>(Shape(BnInOp2Blob("like")->shape()), y_blob->mut_dptr<T>()),
+      XpuVarNdarray<const T>(x_blob, x_blob->shape().NumAxes()),
+      XpuVarNdarray<T>(fw_tmp_blob, x_blob->shape().NumAxes()));
+}
+
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReduceSumLikeConf, ReduceSumLikeKernel,
+                           ARITHMETIC_DATA_TYPE_SEQ);
+
+}  // namespace oneflow
