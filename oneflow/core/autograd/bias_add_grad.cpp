eb07b789f7451cef08885dd9e5aa3df63f975ca0
Cai Shenghang <caishenghang@gmail.com>
2019-03-21 16:26:04 +0800

    minor fix

--- oneflow/core/autograd/bias_add_grad.cpp
+++ oneflow/core/autograd/bias_add_grad.cpp
@@ -8,7 +8,6 @@ void GenerateBackwardOpConf(
     const Operator& op, std::vector<OperatorConf>* op_confs,
     const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
   CHECK(op.op_conf().has_bias_add_conf());
-  const MatmulOpConf& conf = op.op_conf().matmul_conf();
   if (DiffLbi4BnInOp("a") != nullptr) {
     *DiffLbi4BnInOp("a") = *DiffLbi4BnInOp("out");
   }
