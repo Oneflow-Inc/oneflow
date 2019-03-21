553f822991f53bfd355fb4a6f5bcc53b7ba1f05e
Cai Shenghang <caishenghang@gmail.com>
2019-03-22 00:05:42 +0800

    rename out_diff

--- oneflow/core/kernel/broadcast_div_grad_kernel.cpp
+++ oneflow/core/kernel/broadcast_div_grad_kernel.cpp
@@ -7,7 +7,7 @@ template<DeviceType device_type, typename T>
 void BroadcastDivGradKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* b = BnInOp2Blob("b");
-  const Blob* out_diff = BnInOp2Blob(GenDiffBn("out"));
+  const Blob* dy_blob = BnInOp2Blob("dy");
   const Blob* y_blob = BnInOp2Blob("y");
   Blob* tmp_blob = BnInOp2Blob("temp_storage");
   Blob* b_diff = BnInOp2Blob(GenDiffBn("b"));
@@ -15,23 +15,23 @@ void BroadcastDivGradKernel<device_type, T>::ForwardDataContent(
   KernelUtil<device_type, T>::Reciprocal(ctx.device_ctx, b->shape().elem_cnt(), b->dptr<T>(),
                                          tmp_blob->mut_dptr<T>());
 
-  const int64_t num_axes = out_diff->shape().NumAxes();
-  XpuVarNdarray<const T> out_diff_tensor(out_diff, num_axes);
-  XpuVarNdarray<const T> const_tmp(out_diff_tensor.shape(), tmp_blob->dptr<T>());
-  XpuVarNdarray<T> tmp(out_diff_tensor.shape(), tmp_blob->mut_dptr<T>());
+  const int64_t num_axes = dy_blob->shape().NumAxes();
+  XpuVarNdarray<const T> out_diff(dy_blob, num_axes);
+  XpuVarNdarray<const T> const_tmp(out_diff.shape(), tmp_blob->dptr<T>());
+  XpuVarNdarray<T> tmp(out_diff.shape(), tmp_blob->mut_dptr<T>());
 
 
   NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncDiv>(
       ctx.device_ctx, tmp, XpuVarNdarray<const T>(y_blob, num_axes),
       XpuVarNdarray<const T>(b, num_axes));
   NdarrayUtil<device_type, T>::template BroadcastApply<BinaryFuncMul>(
-      ctx.device_ctx, tmp, out_diff_tensor, const_tmp);
+      ctx.device_ctx, tmp, out_diff, const_tmp);
   NdarrayUtil<device_type, T>::ReduceSum(ctx.device_ctx, XpuVarNdarray<T>(b_diff, num_axes),
                                          const_tmp, tmp);
   NdarrayUtil<device_type, T>::template ImplaceApplyUnary<UnaryFuncMinus>(
       ctx.device_ctx, XpuVarNdarray<T>(b_diff, num_axes));
 }
 
-ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kDenominatorGradConf, BroadcastDivGradKernel,
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kBroadcastDivGradConf, BroadcastDivGradKernel,
                            FLOATING_DATA_TYPE_SEQ);
 }  // namespace oneflow
