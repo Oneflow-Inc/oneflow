1d2432ed1bf9c5a5edc070aba9a4590d41e0dfe9
liujuncheng <liujuncheng1022@gmail.com>
2019-03-26 16:02:25 +0800

    refine

--- oneflow/core/autograd/conv_grad.cpp
+++ oneflow/core/autograd/conv_grad.cpp
@@ -4,7 +4,7 @@ namespace oneflow {
 
 namespace {
 
-ConvConf GenConvConfFromConvOpConf(const OperatorConf& op_conf) {
+ConvConf ConvConfFromConvOpConf(const OperatorConf& op_conf) {
   int32_t n_dims = 0;
   const PbMessage* msg = nullptr;
   if (op_conf.has_conv_1d_conf()) {
@@ -33,44 +33,50 @@ void GenerateBackwardOpConf(
     const Operator& op, std::vector<OperatorConf>* op_confs,
     const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
   CHECK(op.op_conf().has_conv_1d_conf() || op.op_conf().has_conv_2d_conf()
-        || op.op_conf().has_conv_2d_conf());
-  const ConvConf conv_conf = GenConvConfFromConvOpConf(op.op_conf());
-  if (op.GetValFromCustomizedConf<bool>("use_bias") && DiffLbi4BnInOp("bias") != nullptr) {
-    OperatorConf bias_grad_op;
-    bias_grad_op.set_name("System-AutoGrad-" + op.op_name() + "-BiasGrad");
-    ConvBiasGradOpConf* conf = bias_grad_op.mutable_conv_bias_grad_conf();
-    conf->set_data_format(conv_conf.data_format());
-    conf->set_num_dims(conv_conf.num_dims());
-    conf->set_dy(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    conf->set_bias_diff("bias_diff");
-    op_confs->push_back(bias_grad_op);
-    DiffLbi4BnInOp("bias")->set_op_name(bias_grad_op.name());
-    DiffLbi4BnInOp("bias")->set_blob_name("bias_diff");
+        || op.op_conf().has_conv_3d_conf());
+  const ConvConf conv_conf = ConvConfFromConvOpConf(op.op_conf());
+  const std::string out_diff_lbn = GenLogicalBlobName(*DiffLbi4BnInOp("out"));
+  if (op.GetValFromCustomizedConf<bool>("use_bias")) {
+    LogicalBlobId* bias_diff_lbi = DiffLbi4BnInOp("bias");
+    if (bias_diff_lbi != nullptr) {
+      OperatorConf bias_grad_op;
+      bias_grad_op.set_name("System-AutoGrad-" + op.op_name() + "-BiasGrad");
+      ConvBiasGradOpConf* conf = bias_grad_op.mutable_conv_bias_grad_conf();
+      conf->set_data_format(conv_conf.data_format());
+      conf->set_num_dims(conv_conf.num_dims());
+      conf->set_dy(out_diff_lbn);
+      conf->set_bias_diff("bias_diff");
+      op_confs->push_back(bias_grad_op);
+      bias_diff_lbi->set_op_name(bias_grad_op.name());
+      bias_diff_lbi->set_blob_name(conf->bias_diff());
+    }
   }
-  if (DiffLbi4BnInOp("weight") != nullptr) {
+  LogicalBlobId* filter_diff_lbi = DiffLbi4BnInOp("weight");
+  if (filter_diff_lbi != nullptr) {
     OperatorConf filter_grad_op;
     filter_grad_op.set_name("System-AutoGrad-" + op.op_name() + "-FilterGrad");
     ConvFilterGradOpConf* conf = filter_grad_op.mutable_conv_filter_grad_conf();
     *conf->mutable_conv_conf() = conv_conf;
-    conf->set_dy(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    conf->set_dy(out_diff_lbn);
     conf->set_x(GenLogicalBlobName(op.BnInOp2Lbi("in")));
     conf->set_filter_diff("filter_diff");
     op_confs->push_back(filter_grad_op);
-    DiffLbi4BnInOp("weight")->set_op_name(filter_grad_op.name());
-    DiffLbi4BnInOp("weight")->set_blob_name("filter_diff");
+    filter_diff_lbi->set_op_name(filter_grad_op.name());
+    filter_diff_lbi->set_blob_name(conf->filter_diff());
   }
-  if (DiffLbi4BnInOp("in") != nullptr) {
+  LogicalBlobId* in_diff_lbi = DiffLbi4BnInOp("in");
+  if (in_diff_lbi != nullptr) {
     OperatorConf data_grad_op;
     data_grad_op.set_name("System-AutoGrad-" + op.op_name() + "DataGrad");
     ConvDataGradOpConf* conf = data_grad_op.mutable_conv_data_grad_conf();
     *conf->mutable_conv_conf() = conv_conf;
-    conf->set_dy(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    conf->set_dy(out_diff_lbn);
     conf->set_filter(GenLogicalBlobName(op.BnInOp2Lbi("weight")));
     conf->set_x_like(GenLogicalBlobName(op.BnInOp2Lbi("in")));
-    conf->set_dy("dx");
+    conf->set_dx("dx");
     op_confs->push_back(data_grad_op);
-    DiffLbi4BnInOp("in")->set_op_name(data_grad_op.name());
-    DiffLbi4BnInOp("in")->set_blob_name("dx");
+    in_diff_lbi->set_op_name(data_grad_op.name());
+    in_diff_lbi->set_blob_name(conf->dx());
   }
 }
 
