821225958d4a07d9a22bb65c6f601f101cab4663
Cai Shenghang <caishenghang@gmail.com>
2019-03-26 12:24:04 +0800

    Update fully_connected_grad.cpp

--- oneflow/core/autograd/fully_connected_grad.cpp
+++ oneflow/core/autograd/fully_connected_grad.cpp
@@ -10,20 +10,33 @@ void GenerateBackwardOpConf(
   CHECK(op.op_conf().has_fully_connected_conf());
   OperatorConf fully_connected_conf(op.op_conf());
   const auto& conf = op.op_conf().fully_connected_conf();
+  if (DiffLbi4BnInOp("in") != nullptr) {
+    OperatorConf matmul_in_op;
+    matmul_in_op.set_name(op.op_name() + "_in_grad");
+    MatmulOpConf* matmul_in_op_conf = matmul_in_op.mutable_matmul_conf();
+    matmul_in_op_conf->set_out("out");
+    matmul_in_op_conf->set_a(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    matmul_in_op_conf->set_b(GenLogicalBlobName(op.BnInOp2Lbi("weight")));
+    matmul_in_op_conf->set_transpose_a(false);
+    matmul_in_op_conf->set_transpose_b(false);
+    op_confs->push_back(matmul_in_op);
+    DiffLbi4BnInOp("in")->set_op_name(matmul_in_op.name());
+    DiffLbi4BnInOp("in")->set_blob_name("out");
+  }
   if (DiffLbi4BnInOp("weight") != nullptr) {
-    OperatorConf matmul_op;
-    matmul_op.set_name(op.op_name() + "_weight_grad");
-    MatmulOpConf* matmul_op_conf = matmul_op.mutable_matmul_conf();
-    matmul_op_conf->set_out("out");
-    matmul_op_conf->set_a(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    matmul_op_conf->set_b(GenLogicalBlobName(op.BnInOp2Lbi("in")));
-    matmul_op_conf->set_transpose_a(true);
-    matmul_op_conf->set_transpose_b(false);
-    op_confs->push_back(matmul_op);
-    DiffLbi4BnInOp("weight")->set_op_name(matmul_op.name());
+    OperatorConf matmul_weight_op;
+    matmul_weight_op.set_name(op.op_name() + "_weight_grad");
+    MatmulOpConf* matmul_weight_op_conf = matmul_weight_op.mutable_matmul_conf();
+    matmul_weight_op_conf->set_out("out");
+    matmul_weight_op_conf->set_a(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    matmul_weight_op_conf->set_b(GenLogicalBlobName(op.BnInOp2Lbi("in")));
+    matmul_weight_op_conf->set_transpose_a(true);
+    matmul_weight_op_conf->set_transpose_b(false);
+    op_confs->push_back(matmul_weight_op);
+    DiffLbi4BnInOp("weight")->set_op_name(matmul_weight_op.name());
     DiffLbi4BnInOp("weight")->set_blob_name("out");
   }
-  if (conf.use_bias() && (DiffLbi4BnInOp("in") != nullptr)) {
+  if (conf.use_bias() && (DiffLbi4BnInOp("bias") != nullptr)) {
     OperatorConf reduce_sum_op;
     reduce_sum_op.set_name(op.op_name() + "_bias_grad");
     ReduceSumOpConf* reduce_sum_op_conf = reduce_sum_op.mutable_reduce_sum_conf();
@@ -35,7 +48,6 @@ void GenerateBackwardOpConf(
     DiffLbi4BnInOp("bias")->set_op_name(reduce_sum_op.name());
     DiffLbi4BnInOp("bias")->set_blob_name("out");
   }
-  op_confs->push_back(fully_connected_conf);
 }
 
 }  // namespace
