fb3c307bd5fa214bc0d09c1f6cd90832f8b2a83b
Li Xinqi <lixinqi2010@gmail.com>
2019-03-27 22:19:55 +0800

    Dev auto variable (#1766)
    * auto complete input variable op_conf for conv/layer_norm/fc
    
    * default obn
    
    * fully_connected_autovar
    
    * fix bug in FullyConnectedOp::InitFromOpConf
    
    * fix a bug in FullyConnectedOp::GetOpParallelSignatures
    
    * add tick input for variable
    
    * fix JobDesc::FixTickOpIfExists
    
    * autotick
    
    * fix bugs in tick_op

--- oneflow/core/operator/log_counter_op.h
+++ oneflow/core/operator/log_counter_op.h
@@ -15,6 +15,8 @@ class LogCounterOp final : public Operator {
   void InitFromOpConf() override;
   const PbMessage& GetCustomizedConf() const override;
   virtual LogicalNode* NewProperLogicalNode() { return new PrintLogicalNode; }
+  void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                      const ParallelContext* parallel_ctx) const override {}
 
  private:
   bool IsInputBlobAllowedModelSplit(const std::string& ibn) const override { return true; }
