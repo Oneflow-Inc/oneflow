53fac8f3688f1d632d97ea02f298050fb9f95d71
jackalcooper <jackalcooper@gmail.com>
2019-03-27 17:45:09 +0800

    Dev reduce mean grad (#1760)
    * add reduce_mean_grad
    
    * minor fix
    
    * fmt on server
    
    * #include "oneflow/core/ndarray/ndarray_util.h"
    
    * rename x_num_axes
    
    * rm unnecessary
    
    * fix tmp_storage
    
    * fix header
    
    * fix const blob bug
    
    * #include oneflow/core/ndarray/ndarray_util.h
    
    * minor fix
    
    * fix temp_storage name
    
    * rewrite
    
    * minor fix
    
    * add CreateLeftExtendedShape
    
    * Revert "add CreateLeftExtendedShape"
    
    This reverts commit 253da0138ec643fad43b449f6b9c3ce3f999ca02.
    
    * remove param
    
    * add check
    
    * debug
    
    * minor fix
    
    * Revert "minor fix"
    
    This reverts commit 558bc04640b5f1192295164db4cef720338cfb1a.
    
    * minor fix
    
    * fmt on server
    
    * remove unnecessary check
    
    * address review
    
    * minor fix
    
    * address review

---
+++ oneflow/core/autograd/reduce_mean_grad.cpp
@@ -0,0 +1,47 @@
+#include "oneflow/core/autograd/autograd.h"
+
+namespace oneflow {
+
+namespace {
+
+std::vector<int64_t> KeepDims(const std::vector<int64_t> dim_vec,
+                              const std::vector<int64_t> axis_vec) {
+  std::vector<int64_t> ret = dim_vec;
+  for (const auto& axis : axis_vec) { ret[axis] = 1; }
+  return ret;
+}
+
+std::vector<int64_t> ShiftAxisIfNegative(std::vector<int64_t> axis_vec, const int64_t num_axes) {
+  FOR_RANGE(size_t, i, 0, axis_vec.size()) {
+    if (axis_vec[i] < 0) { axis_vec[i] += num_axes; }
+    CHECK_LT(axis_vec[i], num_axes);
+    CHECK_GE(axis_vec[i], 0);
+  }
+  return axis_vec;
+}
+
+void GenerateBackwardOpConf(
+    const Operator& op, std::vector<OperatorConf>* op_confs,
+    const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
+  CHECK(op.op_conf().has_reduce_mean_conf());
+  if (DiffLbi4BnInOp("in") != nullptr) {
+    OperatorConf reduce_mean_grad_op;
+    reduce_mean_grad_op.set_name(op.op_name() + "_grad");
+    ReduceMeanGradOpConf* reduce_mean_grad_op_conf =
+        reduce_mean_grad_op.mutable_reduce_mean_grad_conf();
+    reduce_mean_grad_op_conf->set_dy(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    reduce_mean_grad_op_conf->set_x(GenLogicalBlobName(op.BnInOp2Lbi("in")));
+    reduce_mean_grad_op_conf->set_dx("dx");
+    const ReduceMeanOpConf& reduce_mean_op_conf = op.op_conf().reduce_mean_conf();
+    reduce_mean_grad_op_conf->mutable_axis()->CopyFrom(reduce_mean_op_conf.axis());
+    op_confs->push_back(reduce_mean_grad_op);
+    DiffLbi4BnInOp("in")->set_op_name(reduce_mean_grad_op.name());
+    DiffLbi4BnInOp("in")->set_blob_name("dx");
+  }
+}
+
+}  // namespace
+
+REGISTER_OP_GRAD(OperatorConf::kReduceMeanConf, &GenerateBackwardOpConf);
+
+}  // namespace oneflow
