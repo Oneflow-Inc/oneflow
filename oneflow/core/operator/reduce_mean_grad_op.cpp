0f42bb90a5ad400e43c3340f9703b1eccc643f55
Cai Shenghang <caishenghang@gmail.com>
2019-03-27 14:14:28 +0800

    rename x_num_axes

--- oneflow/core/operator/reduce_mean_grad_op.cpp
+++ oneflow/core/operator/reduce_mean_grad_op.cpp
@@ -20,11 +20,11 @@ void ReduceMeanGradOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp, const ParallelContext*) const {
   const BlobDesc* dy_blob_desc = GetBlobDesc4BnInOp("dy");
   const BlobDesc* x_blob_desc = GetBlobDesc4BnInOp("x");
-  int64_t y_num_axes = std::max(x_blob_desc->shape().NumAxes(), dy_blob_desc->shape().NumAxes());
+  int64_t x_num_axes = std::max(x_blob_desc->shape().NumAxes(), dy_blob_desc->shape().NumAxes());
   const auto& x_shape = op_conf().reduce_mean_grad_conf().has_kept_dims_shape()
                             ? Shape(op_conf().reduce_mean_grad_conf().kept_dims_shape())
-                            : dy_blob_desc->shape().CreateLeftExtendedShape(y_num_axes);
-  const auto& like_shape = x_blob_desc->shape().CreateLeftExtendedShape(y_num_axes);
+                            : dy_blob_desc->shape().CreateLeftExtendedShape(x_num_axes);
+  const auto& like_shape = x_blob_desc->shape().CreateLeftExtendedShape(x_num_axes);
   FOR_RANGE(int64_t, i, 0, x_shape.NumAxes()) {
     CHECK(x_shape.At(i) == 1 || like_shape.At(i) == 1 || x_shape.At(i) == like_shape.At(i));
   }
