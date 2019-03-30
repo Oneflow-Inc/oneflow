60f820ac650de6a55b7713647caf68cf4a8eb5da
leaves-zwx <kunta0932@gmail.com>
2019-03-30 21:51:22 +0800

    process dim0_valid_num for reduce_mean

--- oneflow/core/operator/reduce_mean_op.cpp
+++ oneflow/core/operator/reduce_mean_op.cpp
@@ -47,8 +47,13 @@ void ReduceMeanOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> G
                                   const ParallelContext*) const {
   const ReduceMeanOpConf& conf = op_conf().reduce_mean_conf();
   const BlobDesc* in_blob = GetBlobDesc4BnInOp("in");
-  *GetBlobDesc4BnInOp("fw_tmp") = *in_blob;
+  // *GetBlobDesc4BnInOp("fw_tmp") = *in_blob;
+  BlobDesc* fw_tmp_blob_desc = GetBlobDesc4BnInOp("fw_tmp");
+  fw_tmp_blob_desc->mut_shape() = in_blob->shape();
+  fw_tmp_blob_desc->set_data_type(in_blob->data_type());
+
   std::vector<int64_t> out_dim_vec;
+  bool reduce_dim0 = false;
   if (conf.axis().empty()) {
     if (conf.keep_dims() == true) {
       out_dim_vec.resize(in_blob->shape().NumAxes());
@@ -56,11 +61,13 @@ void ReduceMeanOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> G
     } else {
       out_dim_vec = {1};
     }
+    reduce_dim0 = true;
   } else {
     const PbRf<int32_t>& axis_repeated = conf.axis();
     std::vector<int64_t> axis_vec = {axis_repeated.begin(), axis_repeated.end()};
     axis_vec = ShiftAxisIfNegative(axis_vec, in_blob->shape().NumAxes());
     std::sort(axis_vec.begin(), axis_vec.end());
+    if (axis_vec.size() > 0 && axis_vec.at(0) == 0) { reduce_dim0 = true; }
     CHECK(std::unique(axis_vec.begin(), axis_vec.end()) == axis_vec.end())
         << "duplicate found in axis";
     if (conf.keep_dims() == true) {
@@ -73,6 +80,7 @@ void ReduceMeanOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> G
   BlobDesc* out_blob = GetBlobDesc4BnInOp("out");
   out_blob->set_data_type(in_blob->data_type());
   out_blob->mut_shape() = Shape(out_dim_vec);
+  if (!reduce_dim0 && in_blob->has_dim0_valid_num_field()) { UNIMPLEMENTED(); }
 }
 
 void ReduceMeanOp::VirtualGenKernelConf(
@@ -91,11 +99,17 @@ void ReduceMeanOp::VirtualGenKernelConf(
                          ShiftAxisIfNegative(axis_vec, in_blob->shape().NumAxes()));
   }
   Shape(kept_dims).ToProto(kernel_conf->mutable_reduce_sum_conf()->mutable_kept_dims_shape());
+  if (in_blob->has_dim0_valid_num_field() && !kernel_conf->is_forward()) {
+    kernel_conf->set_need_do_dim0_valid_num(true);
+  }
 }
 
 void ReduceMeanOp::InferBwBufBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp, const ParallelContext*) const {
-  *GetBlobDesc4BnInOp("bw_tmp") = *GetBlobDesc4BnInOp("out");
+  const BlobDesc* out_diff_blob_desc = GetBlobDesc4BnInOp(GenDiffBn("out"));
+  BlobDesc* bw_tmp_blob_desc = GetBlobDesc4BnInOp("bw_tmp");
+  bw_tmp_blob_desc->mut_shape() = out_diff_blob_desc->shape();
+  bw_tmp_blob_desc->set_data_type(out_diff_blob_desc->data_type());
 }
 
 REGISTER_OP(OperatorConf::kReduceMeanConf, ReduceMeanOp);
