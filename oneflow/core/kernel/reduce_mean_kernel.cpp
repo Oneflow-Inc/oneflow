24077e1bb27cf703721731059727c7a648d12a58
leaves-zwx <kunta0932@gmail.com>
2019-03-31 00:16:53 +0800

    process dim0_valid_num for reduce_mean (#1769)

--- oneflow/core/kernel/reduce_mean_kernel.cpp
+++ oneflow/core/kernel/reduce_mean_kernel.cpp
@@ -10,11 +10,16 @@ void ReduceMeanKernel<device_type, T>::ForwardDataContent(
   const Blob* in_blob = BnInOp2Blob("in");
   Blob* out_blob = BnInOp2Blob("out");
   Blob* fw_tmp_blob = BnInOp2Blob("fw_tmp");
+  const ReduceSumKernelConf& conf = this->kernel_conf().reduce_sum_conf();
+  if (in_blob->has_dim0_valid_num_field() && in_blob->shape().elem_cnt() == 0) {
+    CHECK_EQ(out_blob->shape().elem_cnt(), 1);
+    Memset<device_type>(ctx.device_ctx, out_blob->mut_dptr(), 0,
+                        out_blob->ByteSizeOfDataContentField());
+    return;
+  }
   size_t count = in_blob->shape().elem_cnt() / out_blob->shape().elem_cnt();
   NdarrayUtil<device_type, T>::ReduceSum(
-      ctx.device_ctx,
-      XpuVarNdarray<T>(Shape(this->kernel_conf().reduce_sum_conf().kept_dims_shape()),
-                       out_blob->mut_dptr<T>()),
+      ctx.device_ctx, XpuVarNdarray<T>(Shape(conf.kept_dims_shape()), out_blob->mut_dptr<T>()),
       XpuVarNdarray<const T>(in_blob, in_blob->shape().NumAxes()),
       XpuVarNdarray<T>(fw_tmp_blob, in_blob->shape().NumAxes()));
   KernelUtil<device_type, T>::Div(ctx.device_ctx, out_blob->shape().elem_cnt(),
@@ -27,6 +32,7 @@ void ReduceMeanKernel<device_type, T>::BackwardDataContent(
   const Blob* out_diff_blob = BnInOp2Blob("out_diff");
   Blob* in_diff_blob = BnInOp2Blob("in_diff");
   Blob* bw_tmp_blob = BnInOp2Blob("bw_tmp");
+  if (in_diff_blob->shape().elem_cnt() == 0) { return; }
   size_t count = in_diff_blob->shape().elem_cnt() / out_diff_blob->shape().elem_cnt();
   Memcpy<device_type>(ctx.device_ctx, bw_tmp_blob->mut_dptr(), out_diff_blob->dptr(),
                       out_diff_blob->ByteSizeOfDataContentField());
@@ -36,6 +42,20 @@ void ReduceMeanKernel<device_type, T>::BackwardDataContent(
       ctx.device_ctx, XpuVarNdarray<T>(in_diff_blob, in_diff_blob->shape().NumAxes()),
       XpuVarNdarray<const T>(Shape(this->kernel_conf().reduce_sum_conf().kept_dims_shape()),
                              bw_tmp_blob->dptr<T>()));
+}
+
+template<DeviceType device_type, typename T>
+void ReduceMeanKernel<device_type, T>::ForwardDim0ValidNum(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  UNIMPLEMENTED();
+}
+
+template<DeviceType device_type, typename T>
+void ReduceMeanKernel<device_type, T>::BackwardInDiffDim0ValidNum(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const Blob* in = BnInOp2Blob("in");
+  Blob* in_diff = BnInOp2Blob(GenDiffBn("in"));
+  in_diff->CopyDim0ValidNumFrom(ctx.device_ctx, in);
 }
 
 ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReduceMeanConf, ReduceMeanKernel, FLOATING_DATA_TYPE_SEQ);
