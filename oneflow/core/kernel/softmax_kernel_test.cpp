ec54a419f86d7dfe7616178efbb6fab72024799f
liujuncheng <liujuncheng1022@gmail.com>
2019-04-03 14:25:21 +0800

    revert

--- oneflow/core/kernel/softmax_kernel_test.cpp
+++ oneflow/core/kernel/softmax_kernel_test.cpp
@@ -14,50 +14,30 @@ void SoftmaxTestCase(OpKernelTestCase* softmax_test_case, const std::string& job
   softmax_conf->set_out("test/out");
 
   BlobDesc* blob_desc = new BlobDesc(Shape({2, 4}), GetDataType<T>::value, false, false, 1);
-  softmax_test_case->template InitBlob<T>("in", blob_desc,
-                                          {
-                                              -6.5082378387f,
-                                              6.2422561646f,
-                                              -5.0046253204f,
-                                              5.7873620987f,
-                                              2.6899633408f,
-                                              -0.9748515487f,
-                                              -5.6746006012f,
-                                              -1.2801564932f,
-                                          });
-  softmax_test_case->template InitBlob<T>(GenDiffBn("out"), blob_desc,
-                                          {
-                                              10.5252790451f,
-                                              -1.9936658144f,
-                                              -19.5707550049f,
-                                              -13.2503423691f,
-                                              -12.4993753433f,
-                                              -13.8137903214f,
-                                              15.2945814133f,
-                                              -18.7320747375f,
-                                          });
-  softmax_test_case->template ForwardCheckBlob<T>("out", blob_desc,
-                                                  {
-                                                      0.0000017748f,
-                                                      0.6117962599f,
-                                                      0.0000079827f,
-                                                      0.3881939948f,
-                                                      0.9572006464f,
-                                                      0.0245128646f,
-                                                      0.0002230073f,
-                                                      0.0180634949f,
-                                                  });
-  softmax_test_case->template BackwardCheckBlob<T>(GenDiffBn("in"), blob_desc,
-                                                   {
-                                                       0.0000299735f,
-                                                       2.6734838486f,
-                                                       -0.0001054287f,
-                                                       -2.6734082699f,
-                                                       0.1326740533f,
-                                                       -0.0288224388f,
-                                                       0.0062291645f,
-                                                       -0.1100806221f,
-                                                   });
+  softmax_test_case->template InitBlob<T>(
+      "in", blob_desc,
+      {
+          -6.5082378387f, 6.2422561646f, -5.0046253204f, 5.7873620987f, 2.6899633408f,
+          -0.9748515487f, -5.6746006012f, -1.2801564932f,
+      });
+  softmax_test_case->template InitBlob<T>(
+      GenDiffBn("out"), blob_desc,
+      {
+          10.5252790451f, -1.9936658144f, -19.5707550049f, -13.2503423691f, -12.4993753433f,
+          -13.8137903214f, 15.2945814133f, -18.7320747375f,
+      });
+  softmax_test_case->template ForwardCheckBlob<T>(
+      "out", blob_desc,
+      {
+          0.0000017748f, 0.6117962599f, 0.0000079827f, 0.3881939948f, 0.9572006464f, 0.0245128646f,
+          0.0002230073f, 0.0180634949f,
+      });
+  softmax_test_case->template BackwardCheckBlob<T>(
+      GenDiffBn("in"), blob_desc,
+      {
+          0.0000299735f, 2.6734838486f, -0.0001054287f, -2.6734082699f, 0.1326740533f,
+          -0.0288224388f, 0.0062291645f, -0.1100806221f,
+      });
 }
 
 TEST_CPU_AND_GPU_OPKERNEL(SoftmaxTestCase, FLOATING_DATA_TYPE_SEQ, (train)(predict),
