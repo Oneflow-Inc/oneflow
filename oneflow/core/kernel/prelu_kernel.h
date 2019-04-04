3e7880410d8747db2c0083d212d1ef340c020b51
mir-of <mir-of@outlook.com>
2019-04-04 16:41:18 +0800

    fix prelu kernel

--- oneflow/core/kernel/prelu_kernel.h
+++ oneflow/core/kernel/prelu_kernel.h
@@ -6,7 +6,7 @@
 namespace oneflow {
 
 template<DeviceType device_type, typename T>
-class PReluKernel final : public KernelIf<device_type> {
+class PReluKernel final : public KernelIfWithModel<device_type, T> {
  public:
   OF_DISALLOW_COPY_AND_MOVE(PReluKernel);
   PReluKernel() = default;
