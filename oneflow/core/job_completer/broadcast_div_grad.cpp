7d4ec21a8a563fe97139d28752b8b5d219297433
Shenghang Cai <jackalcooper@gmail.com>
2019-04-13 18:14:25 +0800

    refactor broadcast grad

--- oneflow/core/job_completer/broadcast_div_grad.cpp
+++ oneflow/core/job_completer/broadcast_div_grad.cpp
@@ -29,6 +29,11 @@ void GenerateBackwardOpConf(
       reduce_sum_like_a_op_conf->set_like(GenLogicalBlobName(op.BnInOp2Lbi("a")));
       reduce_sum_like_a_op_conf->set_x(broadcast_div_a.name() + "/out");
       reduce_sum_like_a_op_conf->set_y("y");
+      const std::vector<int64_t> broadcast_axis_vec =
+          LogicalBlobDesc4BnInOp("a").shape().BroadcastAxis(
+              LogicalBlobDesc4BnInOp("out").shape().dim_vec());
+      *reduce_sum_like_a_op_conf->mutable_axis() = {broadcast_axis_vec.begin(),
+                                                    broadcast_axis_vec.end()};
 
       op_confs->push_back(reduce_sum_like_a);
       DiffLbi4BnInOp("a")->set_op_name(reduce_sum_like_a.name());
--- oneflow/core/job_completer/broadcast_mul_grad.cpp
+++ oneflow/core/job_completer/broadcast_mul_grad.cpp
@@ -10,16 +10,17 @@ void GenerateBackwardOpConf(
     const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4BnInOp) {
   CHECK(op.op_conf().has_broadcast_mul_conf());
   if (DiffLbi4BnInOp("a") != nullptr) {
+    OperatorConf broadcast_mul_a;
+    broadcast_mul_a.set_name(op.op_name() + "_grad_a_mul");
+    BroadcastMulOpConf* broadcast_mul_a_op_conf = broadcast_mul_a.mutable_broadcast_mul_conf();
+    broadcast_mul_a_op_conf->set_a(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    broadcast_mul_a_op_conf->set_b(GenLogicalBlobName(op.BnInOp2Lbi("b")));
+    broadcast_mul_a_op_conf->set_out("out");
+    op_confs->push_back(broadcast_mul_a);
     if (LogicalBlobDesc4BnInOp("out").shape() == LogicalBlobDesc4BnInOp("a").shape()) {
-      *DiffLbi4BnInOp("a") = *DiffLbi4BnInOp("out");
+      DiffLbi4BnInOp("a")->set_op_name(broadcast_mul_a.name());
+      DiffLbi4BnInOp("a")->set_blob_name("y");
     } else {
-      OperatorConf broadcast_mul_a;
-      broadcast_mul_a.set_name(op.op_name() + "_grad_a_mul");
-      BroadcastMulOpConf* broadcast_mul_a_op_conf = broadcast_mul_a.mutable_broadcast_mul_conf();
-      broadcast_mul_a_op_conf->set_a(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-      broadcast_mul_a_op_conf->set_b(GenLogicalBlobName(op.BnInOp2Lbi("b")));
-      broadcast_mul_a_op_conf->set_out("out");
-
       OperatorConf reduce_sum_like_a;
       reduce_sum_like_a.set_name(op.op_name() + "_grad_a_reduce");
       ReduceSumLikeOpConf* reduce_sum_like_a_op_conf =
@@ -32,23 +33,23 @@ void GenerateBackwardOpConf(
               LogicalBlobDesc4BnInOp("out").shape().dim_vec());
       *reduce_sum_like_a_op_conf->mutable_axis() = {broadcast_axis_vec.begin(),
                                                     broadcast_axis_vec.end()};
-      op_confs->push_back(broadcast_mul_a);
       op_confs->push_back(reduce_sum_like_a);
       DiffLbi4BnInOp("a")->set_op_name(reduce_sum_like_a.name());
-      DiffLbi4BnInOp("a")->set_blob_name("y");
+      DiffLbi4BnInOp("a")->set_blob_name("out");
     }
   }
   if (DiffLbi4BnInOp("b") != nullptr) {
+    OperatorConf broadcast_mul_b;
+    broadcast_mul_b.set_name(op.op_name() + "_grad_b_mul");
+    BroadcastMulOpConf* broadcast_mul_b_op_conf = broadcast_mul_b.mutable_broadcast_mul_conf();
+    broadcast_mul_b_op_conf->set_a(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    broadcast_mul_b_op_conf->set_b(GenLogicalBlobName(op.BnInOp2Lbi("a")));
+    broadcast_mul_b_op_conf->set_out("out");
+    op_confs->push_back(broadcast_mul_b);
     if (LogicalBlobDesc4BnInOp("out").shape() == LogicalBlobDesc4BnInOp("b").shape()) {
-      *DiffLbi4BnInOp("b") = *DiffLbi4BnInOp("out");
+      DiffLbi4BnInOp("b")->set_op_name(broadcast_mul_b.name());
+      DiffLbi4BnInOp("b")->set_blob_name("out");
     } else {
-      OperatorConf broadcast_mul_b;
-      broadcast_mul_b.set_name(op.op_name() + "_grad_b_mul");
-      BroadcastMulOpConf* broadcast_mul_b_op_conf = broadcast_mul_b.mutable_broadcast_mul_conf();
-      broadcast_mul_b_op_conf->set_a(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-      broadcast_mul_b_op_conf->set_b(GenLogicalBlobName(op.BnInOp2Lbi("a")));
-      broadcast_mul_b_op_conf->set_out("out");
-
       OperatorConf reduce_sum_like_b;
       reduce_sum_like_b.set_name(op.op_name() + "_grad_b_reduce");
       ReduceSumLikeOpConf* reduce_sum_like_b_op_conf =
@@ -61,7 +62,6 @@ void GenerateBackwardOpConf(
               LogicalBlobDesc4BnInOp("out").shape().dim_vec());
       *reduce_sum_like_b_op_conf->mutable_axis() = {broadcast_axis_vec.begin(),
                                                     broadcast_axis_vec.end()};
-      op_confs->push_back(broadcast_mul_b);
       op_confs->push_back(reduce_sum_like_b);
       DiffLbi4BnInOp("b")->set_op_name(reduce_sum_like_b.name());
       DiffLbi4BnInOp("b")->set_blob_name("y");
