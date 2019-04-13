ff8046d8df8e44f8caf3bf57f70b30f79683b8c9
jackalcooper <jackalcooper@gmail.com>
2019-04-13 20:43:23 +0800

    Dev reduce sum like with axis (#1808)
    * naive ipl
    
    * refactor broadcast grad
    
    * fix
    
    * fix lbi
    
    * rename
    
    * left ext
    
    * fix
    
    * fix
    
    * rename

--- oneflow/core/job_completer/broadcast_add_grad.cpp
+++ oneflow/core/job_completer/broadcast_add_grad.cpp
@@ -6,31 +6,54 @@ namespace {
 
 void GenerateBackwardOpConf(
     const Operator& op, std::vector<OperatorConf>* op_confs,
-    const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
+    const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp,
+    const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4BnInOp) {
   CHECK(op.op_conf().has_broadcast_add_conf());
   if (DiffLbi4BnInOp("a") != nullptr) {
-    OperatorConf reduce_sum_like_a_op;
-    reduce_sum_like_a_op.set_name(op.op_name() + "_grad_a");
-    ReduceSumLikeOpConf* reduce_sum_like_a_op_conf =
-        reduce_sum_like_a_op.mutable_reduce_sum_like_conf();
-    reduce_sum_like_a_op_conf->set_x(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    reduce_sum_like_a_op_conf->set_y("y");
-    reduce_sum_like_a_op_conf->set_like(GenLogicalBlobName(op.BnInOp2Lbi("a")));
-    op_confs->push_back(reduce_sum_like_a_op);
-    DiffLbi4BnInOp("a")->set_op_name(reduce_sum_like_a_op.name());
-    DiffLbi4BnInOp("a")->set_blob_name("y");
+    if (LogicalBlobDesc4BnInOp("out").shape() == LogicalBlobDesc4BnInOp("a").shape()) {
+      *DiffLbi4BnInOp("a") = *DiffLbi4BnInOp("out");
+    } else {
+      OperatorConf reduce_sum_like_a_op;
+      reduce_sum_like_a_op.set_name(op.op_name() + "_grad_a");
+      ReduceSumLikeOpConf* reduce_sum_like_a_op_conf =
+          reduce_sum_like_a_op.mutable_reduce_sum_like_conf();
+      reduce_sum_like_a_op_conf->set_x(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+      reduce_sum_like_a_op_conf->set_y("y");
+      reduce_sum_like_a_op_conf->set_like(GenLogicalBlobName(op.BnInOp2Lbi("a")));
+      const std::vector<int64_t>& broadcast_axis_vec =
+          LogicalBlobDesc4BnInOp("a")
+              .shape()
+              .CreateLeftExtendedShape(LogicalBlobDesc4BnInOp("out").shape().NumAxes())
+              .Axes4BroadcastTo(LogicalBlobDesc4BnInOp("out").shape());
+      *reduce_sum_like_a_op_conf->mutable_axis() = {broadcast_axis_vec.begin(),
+                                                    broadcast_axis_vec.end()};
+      op_confs->push_back(reduce_sum_like_a_op);
+      DiffLbi4BnInOp("a")->set_op_name(reduce_sum_like_a_op.name());
+      DiffLbi4BnInOp("a")->set_blob_name("y");
+    }
   }
   if (DiffLbi4BnInOp("b") != nullptr) {
-    OperatorConf reduce_sum_like_b_op;
-    reduce_sum_like_b_op.set_name(op.op_name() + "_grad_b");
-    ReduceSumLikeOpConf* reduce_sum_like_b_op_conf =
-        reduce_sum_like_b_op.mutable_reduce_sum_like_conf();
-    reduce_sum_like_b_op_conf->set_x(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    reduce_sum_like_b_op_conf->set_y("y");
-    reduce_sum_like_b_op_conf->set_like(GenLogicalBlobName(op.BnInOp2Lbi("b")));
-    op_confs->push_back(reduce_sum_like_b_op);
-    DiffLbi4BnInOp("b")->set_op_name(reduce_sum_like_b_op.name());
-    DiffLbi4BnInOp("b")->set_blob_name("y");
+    if (LogicalBlobDesc4BnInOp("out").shape() == LogicalBlobDesc4BnInOp("b").shape()) {
+      *DiffLbi4BnInOp("b") = *DiffLbi4BnInOp("out");
+    } else {
+      OperatorConf reduce_sum_like_b_op;
+      reduce_sum_like_b_op.set_name(op.op_name() + "_grad_b");
+      ReduceSumLikeOpConf* reduce_sum_like_b_op_conf =
+          reduce_sum_like_b_op.mutable_reduce_sum_like_conf();
+      reduce_sum_like_b_op_conf->set_x(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+      reduce_sum_like_b_op_conf->set_y("y");
+      reduce_sum_like_b_op_conf->set_like(GenLogicalBlobName(op.BnInOp2Lbi("b")));
+      const std::vector<int64_t>& broadcast_axis_vec =
+          LogicalBlobDesc4BnInOp("b")
+              .shape()
+              .CreateLeftExtendedShape(LogicalBlobDesc4BnInOp("out").shape().NumAxes())
+              .Axes4BroadcastTo(LogicalBlobDesc4BnInOp("out").shape());
+      *reduce_sum_like_b_op_conf->mutable_axis() = {broadcast_axis_vec.begin(),
+                                                    broadcast_axis_vec.end()};
+      op_confs->push_back(reduce_sum_like_b_op);
+      DiffLbi4BnInOp("b")->set_op_name(reduce_sum_like_b_op.name());
+      DiffLbi4BnInOp("b")->set_blob_name("y");
+    }
   }
 }
 
