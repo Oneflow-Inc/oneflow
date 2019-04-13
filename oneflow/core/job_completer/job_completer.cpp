b413f7e15b6f013c9cfe276a743c34281da1b4f9
jackalcooper <jackalcooper@gmail.com>
2019-04-13 16:10:35 +0800

    Dev ctrl in set name (#1806)
    * check in naive impl
    
    * rename and change if cond
    
    * check in all changes

--- oneflow/core/job_completer/job_completer.cpp
+++ oneflow/core/job_completer/job_completer.cpp
@@ -243,9 +243,9 @@ void FixAndOptimizeDLNet(Job* job) {
   if (!(job_desc->IsPredict()
         && job_desc->other_conf().predict_conf().has_tmp_split_fw_bw_train_conf())) {
     FixTickOpIfExists(job);
+    // ConvertPseudoChainToChain(job);
   }
-  ConvertPseudoChainToChain(job);
-  if (job_desc->IsTrain()) { AddIdentityOpForAllReduceOverlapingUntrainble(job); }
+  // if (job_desc->IsTrain()) { AddIdentityOpForAllReduceOverlapingUntrainble(job); }
 }
 
 void SetOpTimeShape(const OpGraph& op_graph, Job* job) {
@@ -255,7 +255,38 @@ void SetOpTimeShape(const OpGraph& op_graph, Job* job) {
   });
 }
 
-void SetCtrlInOpName(const OpGraph& op_graph, Job* job) { TODO(); }
+void SetCtrlInOpName(const OpGraph& op_graph, Job* job) {
+  auto IsModelUpdateOp = [](const OperatorConf& op_conf) -> bool {
+    const PbMessage& conf = GetMessageInPbMessage(op_conf, op_conf.op_type_case());
+    const auto* user_conf =
+        TryGetMsgPtrFromPbMessage<NormalModelUpdateOpUserConf>(conf, "user_conf");
+    if (user_conf == nullptr) { return false; }
+    return IsClassRegistered<GenerateOptimizerOpConfWrapperStruct>(user_conf->normal_mdupdt_case());
+  };
+  JobBuilder job_builder(job);
+  op_graph.ForEachNode([&](OpNode* op_node) {
+    if (op_node->op().op_conf().has_variable_conf() == false) { return; }
+    if (op_node->out_edges().size() <= 1) { return; }
+    const OperatorConf* model_update_op_conf = nullptr;
+    std::vector<const OperatorConf*> fw_bw_ops;
+    for (OpEdge* edge : op_node->out_edges()) {
+      const auto& op_conf = edge->dst_node()->op().op_conf();
+      if (IsModelUpdateOp(op_conf)) {
+        CHECK(model_update_op_conf == nullptr);
+        model_update_op_conf = &op_conf;
+      } else {
+        // TODO: exclude model save nodes
+        fw_bw_ops.push_back(&op_conf);
+      }
+    }
+    if (model_update_op_conf == nullptr) { return; }
+    OperatorConf mut_md_updt_op_conf(*model_update_op_conf);
+    for (const auto* fw_bw_op : fw_bw_ops) {
+      mut_md_updt_op_conf.add_ctrl_in_op_name(fw_bw_op->name());
+    }
+    job_builder.MutOps({mut_md_updt_op_conf});
+  });
+}
 
 void SetOpTimeShapeAndCtrlInOpName(const OpGraph& op_graph, Job* job) {
   SetOpTimeShape(op_graph, job);
@@ -277,10 +308,10 @@ void JobCompleter::Complete(Job* job) const {
     WithOpGraphAndMutJob(job, &AutoTick);
     // add keep_header_only op
     WithOpGraphAndMutJob(job, &AddKeepHeaderOnlyOp);
+    WithOpGraphAndMutJob(job, &SetOpTimeShapeAndCtrlInOpName);
   }
   // TODO: refine
   FixAndOptimizeDLNet(job);
-  WithOpGraphAndMutJob(job, &SetOpTimeShapeAndCtrlInOpName);
 }
 
 }  // namespace oneflow
