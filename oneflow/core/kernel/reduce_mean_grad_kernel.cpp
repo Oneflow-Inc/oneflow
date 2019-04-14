6380fa2977dbfb098fa0de48d5c94c09a29f300b
Li Xinqi <lixinqi2010@gmail.com>
2019-04-14 22:34:12 +0800

    Dev empty reduced axes (#1809)
    * CreateReducedShapeOrOnesShape
    
    * refine
    
    
    Former-commit-id: 21ac58a1558c988ce25ba4bd39c95f05ac2b5ebc

--- oneflow/core/kernel/reduce_mean_grad_kernel.cpp
+++ oneflow/core/kernel/reduce_mean_grad_kernel.cpp
@@ -18,10 +18,8 @@ void ReduceMeanGradKernel<device_type, T>::ForwardDataContent(
                                   tmp_blob->mut_dptr<T>(), static_cast<T>(count));
   const int64_t num_axes = dx_blob->shape().NumAxes();
   const ReduceMeanGradOpConf& conf = this->op_conf().reduce_mean_grad_conf();
-  const Shape& reduced_shape = conf.reduced_axis().empty()
-                                   ? Shape::Ones(x_blob->shape().NumAxes())
-                                   : x_blob->shape().CreateReducedShape(
-                                         {conf.reduced_axis().begin(), conf.reduced_axis().end()});
+  const Shape& reduced_shape = x_blob->shape().CreateReducedShapeOrOnesShape(
+      {conf.reduced_axis().begin(), conf.reduced_axis().end()});
   NdarrayUtil<device_type, T>::BroadcastTo(
       ctx.device_ctx, XpuVarNdarray<T>(dx_blob, num_axes),
       XpuVarNdarray<const T>(reduced_shape, tmp_blob->dptr<T>()));
