4f1dc928a66ec515f3f8a78f1772c6ad0e2a9ffa
liujuncheng <liujuncheng1022@gmail.com>
2019-04-18 10:33:12 +0800

    fix enable_write_snapshot

--- oneflow/core/job_completer/add_saver.cpp
+++ oneflow/core/job_completer/add_saver.cpp
@@ -3,6 +3,7 @@
 namespace oneflow {
 
 void AddSaver(const OpGraph& op_graph, Job* job_conf) {
+  if (!Global<JobDesc>::Get()->enable_write_snapshot()) { return; }
   const int64_t num_of_batches_in_snapshot =
       job_conf->other().predict_conf().tmp_split_fw_bw_train_conf().num_of_batches_in_snapshot();
   if (Global<JobDesc>::Get()->TotalBatchNum() < num_of_batches_in_snapshot) { return; }
