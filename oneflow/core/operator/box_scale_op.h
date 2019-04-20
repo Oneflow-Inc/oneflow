fa6270ca5d92fbc53a7606348fea46176f3acbb1
ScXfjiang <sc.xfjiang@gmail.com>
2019-04-20 12:50:41 +0800

    box scale op

--- oneflow/core/operator/box_scale_op.h
+++ oneflow/core/operator/box_scale_op.h
@@ -1,18 +1,18 @@
-#ifndef ONEFLOW_CORE_OPERATOR_GROUND_TRUTH_BBOX_OP_H_
-#define ONEFLOW_CORE_OPERATOR_GROUND_TRUTH_BBOX_OP_H_
+#ifndef ONEFLOW_CORE_OPERATOR_BOX_SCALE_OP_H_
+#define ONEFLOW_CORE_OPERATOR_BOX_SCALE_OP_H_
 
 #include "oneflow/core/operator/operator.h"
 
 namespace oneflow {
 
-class GtBboxScaleOp final : public Operator {
+class BoxScaleOp final : public Operator {
  public:
-  OF_DISALLOW_COPY_AND_MOVE(GtBboxScaleOp);
-  GtBboxScaleOp() = default;
-  ~GtBboxScaleOp() = default;
+  OF_DISALLOW_COPY_AND_MOVE(BoxScaleOp);
+  BoxScaleOp() = default;
+  ~BoxScaleOp() = default;
 
   void InitFromOpConf() override;
-  const PbMessage& GetCustomizedConf() const override { return op_conf().gt_bbox_scale_conf(); }
+  const PbMessage& GetCustomizedConf() const override { return op_conf().box_scale_conf(); }
   void InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                       const ParallelContext* parallel_ctx) const override;
 
@@ -24,4 +24,4 @@ class GtBboxScaleOp final : public Operator {
 
 }  // namespace oneflow
 
-#endif  // ONEFLOW_CORE_OPERATOR_GROUND_TRUTH_BBOX_OP_H_
+#endif  // ONEFLOW_CORE_OPERATOR_BOX_SCALE_OP_H_
