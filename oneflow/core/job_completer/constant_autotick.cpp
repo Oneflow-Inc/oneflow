d4da359a3747c92ff10c8cb49b78caa7dad18ff5
Xinqi <lixinqi0703106@163.com>
2019-04-21 19:12:32 +0800

    auto registry for autotick

---
+++ oneflow/core/job_completer/constant_autotick.cpp
@@ -0,0 +1,20 @@
+#include "oneflow/core/job_completer/autotick.h"
+
+namespace oneflow {
+
+class MutConstantOpConTickInputHelper final : public MutOpConTickInputHelper {
+ public:
+  MutConstantOpConTickInputHelper() : MutOpConTickInputHelper() {}
+
+  bool IsTickInputBound() const override { return op_conf().constant_conf().has_tick(); }
+
+  OperatorConf NewTickInputBoundOpConf(const std::string& lbn) const override {
+    OperatorConf ret(op_conf());
+    ret.mutable_constant_conf()->set_tick(lbn);
+    return ret;
+  }
+};
+
+REGISTER_AUTO_TICK(OperatorConf::kConstantConf, MutConstantOpConTickInputHelper);
+
+}  // namespace oneflow
