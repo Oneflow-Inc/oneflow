fa35a8b79f268e111bd8f6e637b4116928b18412
Li Xinqi <lixinqi2010@gmail.com>
2019-04-22 12:40:51 +0800

    set optimizer op's area_id = kMdUpdtArea (#1831)
    Former-commit-id: 8c744804bae9b170e260a8281ab18561c86affe9

--- oneflow/core/operator/adam_model_update_op.h
+++ oneflow/core/operator/adam_model_update_op.h
@@ -2,6 +2,7 @@
 #define ONEFLOW_CORE_OPERATOR_ADAM_MODEL_UPDATE_OP_H_
 
 #include "oneflow/core/operator/normal_model_update_op.h"
+#include "oneflow/core/graph/logical_node.h"
 
 namespace oneflow {
 
@@ -12,6 +13,7 @@ class AdamModelUpdateOp final : public NormalModelUpdtOp {
   ~AdamModelUpdateOp() = default;
 
   const PbMessage& GetCustomizedConf() const override;
+  LogicalNode* NewProperLogicalNode() const override { return new OptimizerLogicalNode; }
 
  private:
   void MdUpdtVirtualInitFromOpConf() override;
--- oneflow/core/operator/lars_model_update_op.h
+++ oneflow/core/operator/lars_model_update_op.h
@@ -2,6 +2,7 @@
 #define ONEFLOW_CORE_OPERATOR_LARS_MODEL_UPDATE_OP_H_
 
 #include "oneflow/core/operator/normal_model_update_op.h"
+#include "oneflow/core/graph/logical_node.h"
 
 namespace oneflow {
 
@@ -12,6 +13,7 @@ class LARSModelUpdateOp final : public NormalModelUpdtOp {
   ~LARSModelUpdateOp() = default;
 
   const PbMessage& GetCustomizedConf() const override;
+  LogicalNode* NewProperLogicalNode() const override { return new OptimizerLogicalNode; }
 
  private:
   void MdUpdtVirtualInitFromOpConf() override;
