5100ce6d9d129bf9608f7e1be2621597f3db1abb
Shenghang Cai <jackalcooper@gmail.com>
2019-04-26 20:22:56 +0800

    fix variable

--- oneflow/core/kernel/reduce_concat_kernel.cpp
+++ oneflow/core/kernel/reduce_concat_kernel.cpp
@@ -7,9 +7,9 @@ void ReduceConcatKernel<device_type>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   if (device_type == DeviceType::kGPU && Global<JobDesc>::Get()->enable_mem_sharing()) { return; }
   Blob* out_blob = BnInOp2Blob("out");
-  char* dst_cur_dptr = out_blob->mut_dptr<char>();
   FOR_RANGE(int, in_bn_id, 0, this->op_attribute().input_bns().size()) {
-    dst_cur_dptr = this->kernel_conf().reduce_concat_conf().data_offset().Get(in_bn_id);
+    char* dst_cur_dptr = out_blob->mut_dptr<char>()
+                         + this->kernel_conf().reduce_concat_conf().data_offset().Get(in_bn_id);
     Blob* in_blob = BnInOp2Blob(this->op_attribute().input_bns().Get(in_bn_id));
     size_t in_byte_size = in_blob->ByteSizeOfDataContentField();
     Memcpy<device_type>(ctx.device_ctx, dst_cur_dptr, in_blob->dptr<char>(), in_byte_size);
