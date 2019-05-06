40b43f3441026a09b34265953b7010be3cbfc501
liujuncheng <liujuncheng1022@gmail.com>
2019-05-06 13:42:46 +0800

    FreezeSbpSignature

---
+++ oneflow/core/job_completer/freeze_sbp_signature.cpp
@@ -0,0 +1,12 @@
+#include "oneflow/core/job_completer/freeze_sbp_signature.h"
+#include "oneflow/core/job/job_builder.h"
+
+namespace oneflow {
+
+void FreezeSbpSignature(const OpGraph& op_graph, Job* job) {
+  op_graph.ForEachNode([&](const OpNode* node){
+    *job->mutable_sbp_conf()->mutable_op_name2sbp_signature_conf()[node->op().op_name()] = node->sbp_signature();
+  });
+}
+
+}  // namespace oneflow
