bcf53d0f23ed001c1da53ff77c816fd8fca82382
Dounm <niuchong893184@gmail.com>
2019-05-06 16:38:34 +0800

    feat: add cublas_tensor_op_math_handle()

--- oneflow/core/device/device_context.h
+++ oneflow/core/device/device_context.h
@@ -14,6 +14,7 @@ class DeviceCtx {
   virtual const cudaStream_t& cuda_stream() const { UNIMPLEMENTED(); }
   virtual const cublasHandle_t& cublas_pmh_handle() const { UNIMPLEMENTED(); }
   virtual const cublasHandle_t& cublas_pmd_handle() const { UNIMPLEMENTED(); }
+  virtual const cublasHandle_t& cublas_tensor_op_math_handle() const { UNIMPLEMENTED(); }
   virtual const cudnnHandle_t& cudnn_handle() const { UNIMPLEMENTED(); }
   virtual const ncclComm_t& nccl_handle() const { UNIMPLEMENTED(); }
 #endif
