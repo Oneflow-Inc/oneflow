00be6201b550c5ebefe39a354289f3155d5abd42
liujuncheng <liujuncheng1022@gmail.com>
2019-05-08 12:12:40 +0800

    identity op

--- oneflow/core/operator/identity_op.cpp
+++ oneflow/core/operator/identity_op.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/operator/identity_op.h"
+#include "oneflow/core/job/sbp_signature_builder.h"
 
 namespace oneflow {
 
@@ -11,5 +12,20 @@ void IdentityOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> Get
                                 const ParallelContext* parallel_ctx) const {
   *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
 }
+
+const PbMessage& IdentityOp::GetCustomizedConf() const { return op_conf().identity_conf(); }
+
+void IdentityOp::GetSbpSignatures(
+    const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  const auto bns = StdVec2PbRpf<std::string>({"in", "out"});
+  SbpSignatureBuilder().Broadcast(bns).Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  SbpSignatureBuilder().PartialSum(bns).Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  FOR_RANGE(int64_t, i, 0, LogicalBlobDesc4Ibn("in").shape().NumAxes()) {
+    SbpSignatureBuilder().Split(bns, i).Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  }
+}
+
+REGISTER_OP(OperatorConf::kIdentityConf, IdentityOp);
 
 }  // namespace oneflow
