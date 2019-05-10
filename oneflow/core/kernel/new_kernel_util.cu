84c642812dbbc8d1a871b658751e41d9b7b197ea
Dounm <niuchong893184@gmail.com>
2019-05-10 16:14:50 +0800

    refactor(new_kernel_util): unify Hgemm with cublas_gemm<T>

--- oneflow/core/kernel/new_kernel_util.cu
+++ oneflow/core/kernel/new_kernel_util.cu
@@ -146,32 +146,16 @@ cublasOperation_t CblasTrans2CublasTrans(CBLAS_TRANSPOSE trans) {
 
 template<typename T>
 static void Gemm(DeviceCtx* ctx, const enum CBLAS_ORDER order, enum CBLAS_TRANSPOSE trans_a,
-                 enum CBLAS_TRANSPOSE trans_b, const int m, const int n, const int k, const T alpha,
-                 const T* a, const T* b, const T beta, T* c) {
+                 enum CBLAS_TRANSPOSE trans_b, const int m, const int n, const int k,
+                 const T* alpha, const T* a, const T* b, const T* beta, T* c) {
   const int lda = (trans_a == CblasNoTrans) ? k : m;
   const int ldb = (trans_b == CblasNoTrans) ? n : k;
   const int ldc = n;
   cublasOperation_t cublas_trans_a = CblasTrans2CublasTrans(trans_a);
   cublasOperation_t cublas_trans_b = CblasTrans2CublasTrans(trans_b);
 
-  cublas_gemm<T>(ctx->cublas_pmh_handle(), cublas_trans_b, cublas_trans_a, n, m, k, &alpha, b, ldb,
-                 a, lda, &beta, c, ldc);
-}
-
-static void HGemm(DeviceCtx* ctx, const enum CBLAS_ORDER order, enum CBLAS_TRANSPOSE trans_a,
-                  enum CBLAS_TRANSPOSE trans_b, const int m, const int n, const int k,
-                  const float16 alpha, const float16* a, const float16* b, const float16 beta,
-                  float16* c) {
-  const int lda = (trans_a == CblasNoTrans) ? k : m;
-  const int ldb = (trans_b == CblasNoTrans) ? n : k;
-  const int ldc = n;
-
-  cublasOperation_t cublas_trans_a = CblasTrans2CublasTrans(trans_a);
-  cublasOperation_t cublas_trans_b = CblasTrans2CublasTrans(trans_b);
-  CudaCheck(cublasHgemm(ctx->cublas_tensor_op_math_handle(), cublas_trans_b, cublas_trans_a, n, m,
-                        k, reinterpret_cast<const half*>(&alpha), reinterpret_cast<const half*>(b),
-                        ldb, reinterpret_cast<const half*>(a), lda,
-                        reinterpret_cast<const half*>(&beta), reinterpret_cast<half*>(c), ldc));
+  cublas_gemm<T>(ctx->cublas_pmh_handle(), cublas_trans_b, cublas_trans_a, n, m, k, alpha, b, ldb,
+                 a, lda, beta, c, ldc);
 }
 
 template<typename T>
@@ -204,17 +188,19 @@ GPU_KU_METHOD BlobGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_
 GPU_KU_METHOD OFGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_TRANSPOSE trans_b,
                      const int m, const int n, const int k, const float alpha, const float* a,
                      const float* b, const float beta, float* c) {
-  Gemm<float>(ctx, CblasRowMajor, trans_a, trans_b, m, n, k, alpha, a, b, beta, c);
+  Gemm<float>(ctx, CblasRowMajor, trans_a, trans_b, m, n, k, &alpha, a, b, &beta, c);
 }
 GPU_KU_METHOD OFGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_TRANSPOSE trans_b,
                      const int m, const int n, const int k, const double alpha, const double* a,
                      const double* b, const double beta, double* c) {
-  Gemm<double>(ctx, CblasRowMajor, trans_a, trans_b, m, n, k, alpha, a, b, beta, c);
+  Gemm<double>(ctx, CblasRowMajor, trans_a, trans_b, m, n, k, &alpha, a, b, &beta, c);
 }
 GPU_KU_METHOD OFGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_TRANSPOSE trans_b,
                      const int m, const int n, const int k, const float16 alpha, const float16* a,
                      const float16* b, const float16 beta, float16* c) {
-  HGemm(ctx, CblasRowMajor, trans_a, trans_b, m, n, k, alpha, a, b, beta, c);
+  Gemm<half>(ctx, CblasRowMajor, trans_a, trans_b, m, n, k, reinterpret_cast<const half*>(&alpha),
+             reinterpret_cast<const half*>(a), reinterpret_cast<const half*>(b),
+             reinterpret_cast<const half*>(&beta), reinterpret_cast<half*>(c));
 }
 
 GPU_KU_METHOD Relu(DeviceCtx* ctx, const int64_t n, const float* x, float* y) {
