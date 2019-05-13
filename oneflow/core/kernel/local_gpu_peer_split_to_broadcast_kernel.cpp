83f2ea52cf31b92a4dc71a694a0d279b6f2e8802
liujuncheng <liujuncheng1022@gmail.com>
2019-05-13 15:06:43 +0800

    LocalGpuPeerSplitToBroadcastKernel

--- oneflow/core/kernel/local_gpu_peer_split_to_broadcast_kernel.cpp
+++ oneflow/core/kernel/local_gpu_peer_split_to_broadcast_kernel.cpp
@@ -6,7 +6,42 @@ namespace oneflow {
 
 template<typename T>
 void LocalGpuPeerSplitToBroadcastKernel<T>::ForwardDataContent(
-    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {}
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  Blob* out = BnInOp2Blob("out");
+  const int64_t in_split_axis = op_conf().local_gpu_peer_split_to_broadcast_conf().in_split_axis();
+  const LocalGpuPeerSplitToBroadcastKernelConf& conf =
+      kernel_conf().local_gpu_peer_split_to_broadcast_conf();
+  FOR_RANGE(int32_t, i, 0, op_attribute().input_bns_size()) {
+    const std::string& ibn = op_attribute().input_bns(i);
+    CHECK_EQ(ibn, GenRepeatedBn("in", i));
+    const Blob* in_i = BnInOp2Blob(ibn);
+    CHECK_EQ(in_i->shape().NumAxes(), out->shape().NumAxes());
+    const RangeProto& range = conf.in_split().range(i);
+    CHECK_LT(range.start(), range.end());
+    CHECK_LE(range.end(), out->shape().At(in_split_axis));
+    FOR_RANGE(int64_t, axis, 0, in_i->shape().NumAxes()) {
+      if (axis == in_split_axis) {
+        CHECK_EQ(in_i->shape().At(axis), range.end() - range.start());
+      } else {
+        CHECK_EQ(in_i->shape().At(axis), out->shape().At(axis));
+      }
+    }
+    if (in_split_axis == 0) {
+      CudaCheck(cudaMemcpyAsync(out->mut_dptr<T>(range.start()), in_i->dptr(),
+                                in_i->ByteSizeOfDataContentField(), cudaMemcpyDefault,
+                                ctx.device_ctx->cuda_stream()));
+    } else {
+      const int64_t rows = out->shape().Count(0, in_split_axis);
+      const int64_t src_pitch = in_i->shape().elem_cnt() / rows * sizeof(T);
+      const void* src_dptr = in_i->dptr();
+      const int64_t dst_pitch = out->shape().elem_cnt() / rows * sizeof(T);
+      void* dst_dptr = out->mut_dptr<char>()
+                       + range.start() * in_i->shape().Count(in_split_axis + 1) * sizeof(T);
+      CudaCheck(cudaMemcpy2DAsync(dst_dptr, dst_pitch, src_dptr, src_pitch, src_pitch, rows,
+                                  cudaMemcpyDefault, ctx.device_ctx->cuda_stream()));
+    }
+  }
+}
 
 ADD_CPU_DEFAULT_KERNEL_CREATOR(OperatorConf::kLocalGpuPeerSplitToBroadcastConf,
                                LocalGpuPeerSplitToBroadcastKernel, ARITHMETIC_DATA_TYPE_SEQ)
--- oneflow/core/operator/local_gpu_peer_split_to_broadcast_op.cpp
+++ oneflow/core/operator/local_gpu_peer_split_to_broadcast_op.cpp
@@ -26,7 +26,7 @@ void LocalGpuPeerSplitToBroadcastOp::InferBlobDescs(
   std::vector<int64_t> parts;
   parts.push_back(in_0->shape().At(in_split_axis));
   FOR_RANGE(int64_t, i, 1, input_bns().size()) {
-    const BlobDesc* in_i = GetBlobDesc4BnInOp(GenRepeatedBn("in", 0));
+    const BlobDesc* in_i = GetBlobDesc4BnInOp(GenRepeatedBn("in", i));
     CHECK_EQ(in_i->data_type(), in_0->data_type());
     CHECK_EQ(in_i->shape().NumAxes(), num_axes);
     FOR_RANGE(int64_t, axis, 0, in_i->shape().NumAxes()) {
@@ -47,6 +47,23 @@ void LocalGpuPeerSplitToBroadcastOp::InferBlobDescs(
 
 LogicalNode* LocalGpuPeerSplitToBroadcastOp::NewProperLogicalNode() const {
   return new LocalGpuPeerBoxingLogicalNode();
+}
+
+void LocalGpuPeerSplitToBroadcastOp::VirtualGenKernelConf(
+    std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
+  LocalGpuPeerSplitToBroadcastKernelConf* conf =
+      kernel_conf->mutable_local_gpu_peer_split_to_broadcast_conf();
+  const int64_t in_split_axis = op_conf().local_gpu_peer_split_to_broadcast_conf().in_split_axis();
+  int64_t start = 0;
+  FOR_RANGE(int64_t, i, 0, input_bns().size()) {
+    const BlobDesc* in_i = GetBlobDesc4BnInOp(GenRepeatedBn("in", i));
+    const int64_t size = in_i->shape().At(in_split_axis);
+    RangeProto* range = conf->mutable_in_split()->mutable_range()->Add();
+    range->set_start(start);
+    range->set_end(start + size);
+    start = range->end();
+  }
 }
 
 REGISTER_OP(OperatorConf::kLocalGpuPeerSplitToBroadcastConf, LocalGpuPeerSplitToBroadcastOp);
