03cd6a039e951db950e9669b6a83d7c8fed2a78d
liujuncheng <liujuncheng1022@gmail.com>
2019-05-16 20:40:05 +0800

    HostMemoryCopier CudaMemoryCopier

--- oneflow/core/register/memory_copier.cpp
+++ oneflow/core/register/memory_copier.cpp
@@ -4,8 +4,8 @@ namespace oneflow {
 
 void BaseMemoryCopier::Copy2D(DeviceCtx* ctx, void* dst, size_t dst_pitch, const void* src,
                               size_t src_pitch, size_t width, size_t height) const {
-  char* dst_ptr = (char*)dst;
-  const char* src_ptr = (const char*)src;
+  unsigned char* dst_ptr = (unsigned char*)dst;
+  const unsigned char* src_ptr = (unsigned char*)src;
   FOR_RANGE(size_t, i, 0, height) {
     Copy1D(ctx, dst_ptr, src_ptr, width);
     dst_ptr += dst_pitch;
@@ -28,5 +28,37 @@ void BaseMemoryCopier::Copy3D(DeviceCtx* ctx, const TensorCopyDesc& desc) const 
 }
 
 void BaseMemoryCopier::CopyND(DeviceCtx* ctx, const TensorCopyDesc& desc) const { UNIMPLEMENTED(); }
+
+void HostMemoryCopier::Copy1D(DeviceCtx* ctx, void* dst, const void* src, size_t count) const {
+  std::memcpy(dst, src, count);
+}
+
+#ifdef WITH_CUDA
+
+void CudaMemoryCopier::Copy1D(DeviceCtx* ctx, void* dst, const void* src, size_t count) const {
+  CudaCheck(cudaMemcpyAsync(dst, src, count, cudaMemcpyDefault, ctx->cuda_stream()));
+}
+
+void CudaMemoryCopier::Copy2D(DeviceCtx* ctx, void* dst, size_t dst_pitch, const void* src,
+                              size_t src_pitch, size_t width, size_t height) const {
+  CudaCheck(cudaMemcpy2DAsync(dst, dst_pitch, src, src_pitch, width, height, cudaMemcpyDefault,
+                              ctx->cuda_stream()));
+}
+
+void CudaMemoryCopier::Copy3D(DeviceCtx* ctx, const TensorCopyDesc& desc) const {
+  CHECK_EQ(desc.NumAxes(), 3);
+  cudaMemcpy3DParms params{};
+  params.srcPos = make_cudaPos(desc.src_pos().at(2), desc.src_pos().at(1), desc.src_pos().at(0));
+  params.srcPtr = make_cudaPitchedPtr(const_cast<void*>(desc.src_ptr()), desc.src_shape().At(2),
+                                      desc.src_shape().At(2), desc.src_shape().At(1));
+  params.dstPos = make_cudaPos(desc.dst_pos().at(2), desc.dst_pos().at(1), desc.dst_pos().at(0));
+  params.dstPtr = make_cudaPitchedPtr(desc.dst_ptr(), desc.dst_shape().At(2),
+                                      desc.dst_shape().At(2), desc.dst_shape().At(1));
+  params.extent = make_cudaExtent(desc.extent().At(2), desc.extent().At(1), desc.extent().At(0));
+  params.kind = cudaMemcpyDefault;
+  CudaCheck(cudaMemcpy3DAsync(&params, ctx->cuda_stream()));
+}
+
+#endif
 
 }  // namespace oneflow
--- oneflow/core/register/memory_copier.h
+++ oneflow/core/register/memory_copier.h
@@ -23,11 +23,38 @@ class BaseMemoryCopier : virtual public MemoryCopier {
   OF_DISALLOW_COPY_AND_MOVE(BaseMemoryCopier);
   ~BaseMemoryCopier() override = default;
 
+ protected:
   void Copy2D(DeviceCtx* ctx, void* dst, size_t dst_pitch, const void* src, size_t src_pitch,
               size_t width, size_t height) const override;
   void Copy3D(DeviceCtx* ctx, const TensorCopyDesc& desc) const override;
   void CopyND(DeviceCtx* ctx, const TensorCopyDesc& desc) const override;
 };
+
+class HostMemoryCopier final : public BaseMemoryCopier {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(HostMemoryCopier);
+  ~HostMemoryCopier() override = default;
+
+ private:
+  void Copy1D(DeviceCtx* ctx, void* dst, const void* src, size_t count) const override;
+};
+
+#ifdef WITH_CUDA
+
+class CudaMemoryCopier final : virtual public MemoryCopier {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(CudaMemoryCopier);
+  ~CudaMemoryCopier() override = default;
+
+ private:
+  void Copy1D(DeviceCtx* ctx, void* dst, const void* src, size_t count) const override;
+  void Copy2D(DeviceCtx* ctx, void* dst, size_t dst_pitch, const void* src, size_t src_pitch,
+              size_t width, size_t height) const override;
+  void Copy3D(DeviceCtx* ctx, const TensorCopyDesc& desc) const override;
+  void CopyND(DeviceCtx* ctx, const TensorCopyDesc& desc) const override;
+};
+
+#endif
 
 }  // namespace oneflow
 
