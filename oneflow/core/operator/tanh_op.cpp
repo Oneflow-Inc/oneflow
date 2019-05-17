7c23d1c3d75f1d8471f1a127419298397fd7004e
jackalcooper <jackalcooper@gmail.com>
2019-05-17 14:39:37 +0800

    New sbp (#1873)
    * add sbps
    
    * add sbps
    
    
    Former-commit-id: 3a861be9fffb6ddec3c75b2eddb0eb62f811e5de

--- oneflow/core/operator/tanh_op.cpp
+++ oneflow/core/operator/tanh_op.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/operator/tanh_op.h"
+#include "oneflow/core/job/sbp_signature_builder.h"
 
 namespace oneflow {
 
@@ -13,6 +14,16 @@ const PbMessage& TanHOp::GetCustomizedConf() const { return op_conf().tanh_conf(
 void TanHOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                             const ParallelContext* parallel_ctx) const {
   *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
+}
+
+void TanHOp::GetSbpSignatures(
+    const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  SbpSignatureBuilder()
+      .Split(input_bns(), 0)
+      .Split(output_bns(), 0)
+      .MakeSplitSignatureListBuilder(LogicalBlobDesc4Ibn("in").shape().NumAxes())
+      .Build(sbp_sig_list);
 }
 
 REGISTER_OP(OperatorConf::kTanhConf, TanHOp);
