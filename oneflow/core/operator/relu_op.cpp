436d5d6cf5ac57af865368d5baaa2712993804e2
Dounm <niuchong893184@gmail.com>
2019-05-24 14:43:57 +0800

    fix(relu_op): add InferHasBatchDim() and GetSbpSigs()

--- oneflow/core/operator/relu_op.cpp
+++ oneflow/core/operator/relu_op.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/operator/relu_op.h"
+#include "oneflow/core/job/sbp_signature_builder.h"
 
 namespace oneflow {
 
@@ -13,6 +14,13 @@ const PbMessage& ReluOp::GetCustomizedConf() const { return op_conf().relu_conf(
 void ReluOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                             const ParallelContext* parallel_ctx) const {
   *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
+}
+
+void ReluOp::GetSbpSignatures(
+    const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  SbpSignatureBuilder().Split("in", 0).Split("out", 0).Build(
+      sbp_sig_list->mutable_sbp_signature()->Add());
 }
 
 REGISTER_OP(OperatorConf::kReluConf, ReluOp);
