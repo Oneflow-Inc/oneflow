2709536ef28872f57f7d10574dc610e865faaae0
Dounm <niuchong893184@gmail.com>
2019-05-25 14:11:00 +0800

    feat(relu_kernel): support half

--- oneflow/core/kernel/relu_grad_kernel.cpp
+++ oneflow/core/kernel/relu_grad_kernel.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/kernel/relu_grad_kernel.h"
+#include "oneflow/core/kernel/new_kernel_util.h"
 
 namespace oneflow {
 
@@ -6,11 +7,12 @@ template<DeviceType device_type, typename T>
 void ReluGradKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   Blob* y_blob = BnInOp2Blob("y");
-  KernelUtil<device_type, T>::ReluBackward(
+  NewKernelUtil<device_type>::ReluBackward(
       ctx.device_ctx, y_blob->shape().elem_cnt(), y_blob->dptr<T>(), y_blob->dptr<T>(),
       BnInOp2Blob("dy")->dptr<T>(), BnInOp2Blob("dx")->mut_dptr<T>());
 }
 
-ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReluGradConf, ReluGradKernel, FLOATING_DATA_TYPE_SEQ);
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReluGradConf, ReluGradKernel,
+                           FLOATING_DATA_TYPE_SEQ FLOAT16_DATA_TYPE_SEQ);
 
 }  // namespace oneflow
--- oneflow/core/kernel/relu_kernel.cpp
+++ oneflow/core/kernel/relu_kernel.cpp
@@ -1,4 +1,5 @@
 #include "oneflow/core/kernel/relu_kernel.h"
+#include "oneflow/core/kernel/new_kernel_util.h"
 
 namespace oneflow {
 
@@ -6,19 +7,11 @@ template<DeviceType device_type, typename T>
 void ReluKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   Blob* in_blob = BnInOp2Blob("in");
-  KernelUtil<device_type, T>::Relu(ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
+  NewKernelUtil<device_type>::Relu(ctx.device_ctx, in_blob->shape().elem_cnt(), in_blob->dptr<T>(),
                                    BnInOp2Blob("out")->mut_dptr<T>());
 }
 
-template<DeviceType device_type, typename T>
-void ReluKernel<device_type, T>::BackwardDataContent(
-    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  Blob* out_blob = BnInOp2Blob("out");
-  KernelUtil<device_type, T>::ReluBackward(
-      ctx.device_ctx, out_blob->shape().elem_cnt(), out_blob->dptr<T>(), out_blob->dptr<T>(),
-      BnInOp2Blob("out_diff")->dptr<T>(), BnInOp2Blob("in_diff")->mut_dptr<T>());
-}
-
-ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReluConf, ReluKernel, FLOATING_DATA_TYPE_SEQ);
+ADD_DEFAULT_KERNEL_CREATOR(OperatorConf::kReluConf, ReluKernel,
+                           FLOATING_DATA_TYPE_SEQ FLOAT16_DATA_TYPE_SEQ);
 
 }  // namespace oneflow
