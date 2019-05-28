cefaaaf1db09821cf6f14d5ece612acc04ebd135
Shenghang Cai <jackalcooper@gmail.com>
2019-05-28 21:27:32 +0800

    fix pooling

--- oneflow/core/kernel/pooling_grad_kernel.cu
+++ oneflow/core/kernel/pooling_grad_kernel.cu
@@ -5,6 +5,26 @@
 
 namespace oneflow {
 
+namespace {
+void InferPoolingParams(const int32_t num_spatial_dims, const PoolingConf& pooling_conf,
+                        const std::vector<int64_t> x_shape_vec, std::vector<int32_t>* pool_size,
+                        std::vector<int32_t>* padding, std::vector<int32_t>* strides) {
+  std::vector<int32_t> pool_size_3d = Get3DVecInOpConf(pooling_conf.pool_size(), num_spatial_dims);
+  std::vector<int32_t> strides_3d = Get3DVecInOpConf(pooling_conf.strides(), num_spatial_dims);
+  std::vector<int64_t> y_shape_vec;
+  std::vector<int32_t> padding_before;
+  std::vector<int32_t> padding_after;
+  Get3DOutputSize(x_shape_vec, pool_size_3d, strides_3d, pooling_conf.padding(), &y_shape_vec,
+                  &padding_before, &padding_after);
+  FOR_RANGE(int, i, 0, num_spatial_dims) {
+    int32_t index_in_3d = i + 3 - num_spatial_dims;
+    pool_size->at(i) = pool_size_3d.at(index_in_3d);
+    padding->at(i) = std::max(padding_before.at(index_in_3d), padding_after.at(index_in_3d));
+    strides->at(i) = strides_3d.at(index_in_3d);
+  }
+}
+}  // namespace
+
 template<typename T>
 struct PoolingGradKernelUtil<DeviceType::kGPU, T> final {
   static void Compute(DeviceCtx* ctx, const PoolingConf& pooling_conf, const Blob* dy_blob,
@@ -19,24 +39,14 @@ struct PoolingGradKernelUtil<DeviceType::kGPU, T> final {
       pooling_mode = CUDNN_POOLING_MAX;
     }
     const int32_t num_spatial_dims = pooling_conf.num_spatial_dims();
-    std::vector<int> padding(pooling_conf.num_spatial_dims());
-    std::vector<int32_t> pool_size =
-        Get3DVecInOpConf(pooling_conf.pool_size(), pooling_conf.num_spatial_dims());
-    std::vector<int32_t> strides =
-        Get3DVecInOpConf(pooling_conf.strides(), pooling_conf.num_spatial_dims());
+    std::vector<int> pool_size(num_spatial_dims);
+    std::vector<int> padding(num_spatial_dims);
+    std::vector<int> strides(num_spatial_dims);
     const Shape& x_shape = x_blob->shape();
-    std::vector<int64_t> x_shape_vec = {GetInDim(x_shape, data_format, 0, num_spatial_dims),
-                                        GetInDim(x_shape, data_format, 1, num_spatial_dims),
-                                        GetInDim(x_shape, data_format, 2, num_spatial_dims)};
-    std::vector<int64_t> y_shape_vec;
-    std::vector<int32_t> padding_before;
-    std::vector<int32_t> padding_after;
-    Get3DOutputSize(x_shape_vec, pool_size, strides, pooling_conf.padding(), &y_shape_vec,
-                    &padding_before, &padding_after);
-    FOR_RANGE(int, i, 0, num_spatial_dims) {
-      int32_t index_in_3d = i + 3 - num_spatial_dims;
-      padding[i] = std::max(padding_before[index_in_3d], padding_after[index_in_3d]);
-    }
+    const std::vector<int64_t> x_shape_vec = {GetInDim(x_shape, data_format, 0, num_spatial_dims),
+                                              GetInDim(x_shape, data_format, 1, num_spatial_dims),
+                                              GetInDim(x_shape, data_format, 2, num_spatial_dims)};
+    InferPoolingParams(num_spatial_dims, pooling_conf, x_shape_vec, &pool_size, &padding, &strides);
     std::unique_ptr<CudnnPoolingDesc> pooling_desc;
     pooling_desc.reset(new CudnnPoolingDesc(pooling_mode, num_spatial_dims, pool_size.data(),
                                             padding.data(), strides.data()));
