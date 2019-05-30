aea8c389a7d122fe10784e005239de1abec09789
Xinqi <lixinqi0703106@163.com>
2019-05-31 01:11:49 +0800

    simplify NdarrayUtil in layer_norm_param_grad_kernel

--- oneflow/core/kernel/layer_norm_param_grad_kernel.cpp
+++ oneflow/core/kernel/layer_norm_param_grad_kernel.cpp
@@ -7,6 +7,9 @@ namespace oneflow {
 template<DeviceType device_type, typename T>
 void LayerNormParamGradKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  using NdUtil = NdarrayUtil<device_type, T>;
+  auto Val = NdUtil::GetValNdarrayBuilder();
+  auto Var = NdUtil::GetVarNdarrayBuilder();
   const LayerNormParamGradOpConf& conf = this->op_conf().layer_norm_param_grad_conf();
   const Blob* dy = BnInOp2Blob("dy");
   if (conf.has_beta_diff()) {
@@ -15,10 +18,8 @@ void LayerNormParamGradKernel<device_type, T>::ForwardDataContent(
     const int64_t m = beta_diff->shape().elem_cnt();
     CHECK_EQ(dy->shape().elem_cnt() % m, 0);
     const int64_t n = dy->shape().elem_cnt() / m;
-    NdarrayUtil<device_type, T>::ReduceSum(ctx.device_ctx,
-                                           XpuVarNdarray<T>({1, m}, beta_diff->mut_dptr<T>()),
-                                           XpuVarNdarray<const T>({n, m}, dy->dptr<T>()),
-                                           XpuVarNdarray<T>({n, m}, reduce_buf->mut_dptr<T>()));
+    NdUtil::ReduceSum(ctx.device_ctx, Var({1, m}, beta_diff->mut_dptr<T>()),
+                      Val({n, m}, dy->dptr<T>()), Var({n, m}, reduce_buf->mut_dptr<T>()));
   }
   if (conf.has_gamma_diff()) {
     const Blob* normalized = BnInOp2Blob("normalized");
@@ -27,14 +28,10 @@ void LayerNormParamGradKernel<device_type, T>::ForwardDataContent(
     const int64_t m = gamma_diff->shape().elem_cnt();
     CHECK_EQ(dy->shape().elem_cnt() % m, 0);
     const int64_t n = dy->shape().elem_cnt() / m;
-    NdarrayUtil<device_type, T>::BroadcastMul(ctx.device_ctx,
-                                              XpuVarNdarray<T>({n, m}, reduce_buf->mut_dptr<T>()),
-                                              XpuVarNdarray<const T>({n, m}, normalized->dptr<T>()),
-                                              XpuVarNdarray<const T>({n, m}, dy->dptr<T>()));
-    NdarrayUtil<device_type, T>::ReduceSum(ctx.device_ctx,
-                                           XpuVarNdarray<T>({1, m}, gamma_diff->mut_dptr<T>()),
-                                           XpuVarNdarray<const T>({n, m}, reduce_buf->dptr<T>()),
-                                           XpuVarNdarray<T>({n, m}, reduce_buf->mut_dptr<T>()));
+    NdUtil::BroadcastMul(ctx.device_ctx, Var({n, m}, reduce_buf->mut_dptr<T>()),
+                         Val({n, m}, normalized->dptr<T>()), Val({n, m}, dy->dptr<T>()));
+    NdUtil::ReduceSum(ctx.device_ctx, Var({1, m}, gamma_diff->mut_dptr<T>()),
+                      Val({n, m}, reduce_buf->dptr<T>()), Var({n, m}, reduce_buf->mut_dptr<T>()));
   }
   if (conf.has_normalized_diff()) {
     Blob* normalized_diff = BnInOp2Blob("normalized_diff");
@@ -43,10 +40,8 @@ void LayerNormParamGradKernel<device_type, T>::ForwardDataContent(
       const int64_t m = gamma->shape().elem_cnt();
       CHECK_EQ(dy->shape().elem_cnt() % m, 0);
       const int64_t n = dy->shape().elem_cnt() / m;
-      NdarrayUtil<device_type, T>::BroadcastMul(
-          ctx.device_ctx, XpuVarNdarray<T>({n, m}, normalized_diff->mut_dptr<T>()),
-          XpuVarNdarray<const T>({n, m}, dy->dptr<T>()),
-          XpuVarNdarray<const T>({1, m}, gamma->dptr<T>()));
+      NdUtil::BroadcastMul(ctx.device_ctx, Var({n, m}, normalized_diff->mut_dptr<T>()),
+                           Val({n, m}, dy->dptr<T>()), Val({1, m}, gamma->dptr<T>()));
     } else {
       normalized_diff->CopyDataContentFrom(ctx.device_ctx, dy);
     }
