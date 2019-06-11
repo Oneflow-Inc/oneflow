cc6c85f32e82f07390a8a1289c7ec6a86bd12b6d
Xinqi <lixinqi0703106@163.com>
2019-06-11 14:20:54 +0800

    TickActor

--- oneflow/core/actor/normal_forward_compute_actor.cpp
+++ oneflow/core/actor/normal_forward_compute_actor.cpp
@@ -238,6 +238,5 @@ REGISTER_ACTOR(TaskType::kLoss, NormalForwardCompActor);
 REGISTER_ACTOR(TaskType::kAccuracy, NormalForwardCompActor);
 REGISTER_ACTOR(TaskType::kOptimizer, NormalForwardCompActor);
 REGISTER_ACTOR(TaskType::kPrint, NormalForwardCompActor);
-REGISTER_ACTOR(TaskType::kTick, NormalForwardCompActor);
 
 }  // namespace oneflow
