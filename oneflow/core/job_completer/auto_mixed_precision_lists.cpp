baf2686810132f983dbe35b5127fd15b6d19da32
Dounm <niuchong893184@gmail.com>
2019-06-12 13:28:48 +0800

    style(auto_mixed_precision_lists.cpp): use AMPList instead of a long HashSet<...>

--- oneflow/core/job_completer/auto_mixed_precision_lists.cpp
+++ oneflow/core/job_completer/auto_mixed_precision_lists.cpp
@@ -2,29 +2,26 @@
 
 namespace oneflow {
 
-const HashSet<OperatorConf::OpTypeCase, OpTypeCaseHash>& AutoMixedPrecisionLists::WhiteList() {
-  static HashSet<OperatorConf::OpTypeCase, OpTypeCaseHash> white_list = {OperatorConf::kMatmulConf,
-                                                                         OperatorConf::kConv2DConf};
+const AMPList& AutoMixedPrecisionLists::WhiteList() {
+  static AMPList white_list = {OperatorConf::kMatmulConf, OperatorConf::kConv2DConf};
   return white_list;
 }
 
-const HashSet<OperatorConf::OpTypeCase, OpTypeCaseHash>& AutoMixedPrecisionLists::BlackList() {
-  static HashSet<OperatorConf::OpTypeCase, OpTypeCaseHash> black_list = {
-      OperatorConf::kMeanConf, OperatorConf::kSoftmaxConf};
+const AMPList& AutoMixedPrecisionLists::BlackList() {
+  static AMPList black_list = {OperatorConf::kMeanConf, OperatorConf::kSoftmaxConf};
   return black_list;
 }
 
-const HashSet<OperatorConf::OpTypeCase, OpTypeCaseHash>& AutoMixedPrecisionLists::GrayList() {
-  static HashSet<OperatorConf::OpTypeCase, OpTypeCaseHash> gray_list = {
-      OperatorConf::kAddConf,          OperatorConf::kAveragePooling1DConf,
-      OperatorConf::kMaxPooling1DConf, OperatorConf::kAveragePooling2DConf,
-      OperatorConf::kMaxPooling2DConf, OperatorConf::kAveragePooling3DConf,
-      OperatorConf::kMaxPooling3DConf, OperatorConf::kBiasAddConf,
-      OperatorConf::kMultiplyConf,     OperatorConf::kSigmoidConf,
-      OperatorConf::kTanhConf,         OperatorConf::kSqrtConf,
-      OperatorConf::kScalarMulConf,    OperatorConf::kScalarAddConf,
-      OperatorConf::kBroadcastAddConf, OperatorConf::kBroadcastSubConf,
-      OperatorConf::kBroadcastMulConf, OperatorConf::kBroadcastDivConf};
+const AMPList& AutoMixedPrecisionLists::GrayList() {
+  static AMPList gray_list = {OperatorConf::kAddConf,          OperatorConf::kAveragePooling1DConf,
+                              OperatorConf::kMaxPooling1DConf, OperatorConf::kAveragePooling2DConf,
+                              OperatorConf::kMaxPooling2DConf, OperatorConf::kAveragePooling3DConf,
+                              OperatorConf::kMaxPooling3DConf, OperatorConf::kBiasAddConf,
+                              OperatorConf::kMultiplyConf,     OperatorConf::kSigmoidConf,
+                              OperatorConf::kTanhConf,         OperatorConf::kSqrtConf,
+                              OperatorConf::kScalarMulConf,    OperatorConf::kScalarAddConf,
+                              OperatorConf::kBroadcastAddConf, OperatorConf::kBroadcastSubConf,
+                              OperatorConf::kBroadcastMulConf, OperatorConf::kBroadcastDivConf};
   return gray_list;
 }
 
