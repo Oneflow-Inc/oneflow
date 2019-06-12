3a68239a57535333f5659e2992309b12b97d05e3
Tsai <caishenghang@oneflow.org>
2019-06-12 14:25:10 +0800

    fix has diff

--- oneflow/core/operator/variable_op.cpp
+++ oneflow/core/operator/variable_op.cpp
@@ -9,7 +9,8 @@ void VariableOp::InitFromOpConf() {
   CHECK(op_conf().has_variable_conf());
   if (op_conf().variable_conf().has_tick()) { EnrollInputBn("tick", false); }
   bool has_diff =
-      Global<JobDesc>::Get()->other_conf().predict_conf().has_tmp_split_fw_bw_train_conf();
+      Global<JobDesc>::Get()->other_conf().predict_conf().has_tmp_split_fw_bw_train_conf()
+      && op_conf().trainable();
   EnrollOutputBn("out", has_diff)->set_is_mutable(true);
   EnrollModelBn(op_conf().variable_conf().model_name());
 }
