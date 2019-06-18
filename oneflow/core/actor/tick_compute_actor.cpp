1973d1a99c7e32878179d18e0a69e22926423b96
Xinqi <lixinqi0703106@163.com>
2019-06-18 17:36:31 +0800

    rename actor: SourceTickActor => SourceTickComputeActor; TickActor => TickComputeActor

---
+++ oneflow/core/actor/tick_compute_actor.cpp
@@ -0,0 +1,19 @@
+#include "oneflow/core/actor/tick_compute_actor.h"
+
+namespace oneflow {
+
+void TickComputeActor::VirtualCompActorInit(const TaskProto& task_proto) {
+  piece_id_ = 0;
+  OF_SET_MSG_HANDLER(&TickComputeActor::HandlerNormal);
+}
+
+void TickComputeActor::VirtualAsyncSendNaiveProducedRegstMsgToConsumer() {
+  HandleProducedNaiveDataRegstToConsumer([&](Regst* out_regst) {
+    out_regst->set_piece_id(piece_id_);
+    return true;
+  });
+}
+
+REGISTER_ACTOR(kTick, TickComputeActor);
+
+}  // namespace oneflow
---
+++ oneflow/core/actor/tick_compute_actor.h
@@ -0,0 +1,24 @@
+#ifndef ONEFLOW_CORE_ACTOR_TICK_COMPUTE_ACTOR_H_
+#define ONEFLOW_CORE_ACTOR_TICK_COMPUTE_ACTOR_H_
+
+#include "oneflow/core/actor/compute_actor.h"
+
+namespace oneflow {
+
+class TickComputeActor final : public CompActor {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(TickComputeActor);
+  TickComputeActor() = default;
+  ~TickComputeActor() = default;
+
+ private:
+  void VirtualCompActorInit(const TaskProto&) override;
+  void Act() override { ++piece_id_; }
+  void VirtualAsyncSendNaiveProducedRegstMsgToConsumer() override;
+
+  int64_t piece_id_;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_ACTOR_TICK_COMPUTE_ACTOR_H_
