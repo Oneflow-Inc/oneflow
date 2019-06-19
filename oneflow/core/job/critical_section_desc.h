f92bee816dbb8abcaba775e2cd28f9652a1660b2
Xinqi <lixinqi0703106@163.com>
2019-06-19 13:01:42 +0800

    rm CriticalSectionId

--- oneflow/core/job/critical_section_desc.h
+++ oneflow/core/job/critical_section_desc.h
@@ -14,23 +14,23 @@ class CriticalSectionDesc final {
   void AddCriticalSection(std::unique_ptr<CriticalSection>&&);
   void Done();
   size_t CriticalSectionNum() const { return critical_sections_.size(); }
-  const CriticalSection& GetCriticalSectionByIndex(int64_t) const;
-  CriticalSection* MutCriticalSectionByIndex(int64_t) const;
-  const std::vector<int64_t>& CriticalSectionIndexes4JobId(int64_t) const;
-  const HashSet<int64_t>& GetIntersectingCriticalSectionIndexes(int64_t) const;
+  const CriticalSection& GetCriticalSection(int64_t) const;
+  CriticalSection* MutCriticalSection(int64_t) const;
+  const std::vector<int64_t>& CriticalSectionIds4JobId(int64_t) const;
+  const HashSet<int64_t>& GetIntersectingCriticalSectionIds(int64_t) const;
 
  private:
   friend class Global<CriticalSectionDesc>;
   CriticalSectionDesc() : inited_(false) {}
-  void UpdateJobId2CriticalSectionIndexes();
-  void UpdateTotalJobCriticalSectionIndexes();
-  void UpdateCriticalSectionIndexes2IntersectingIndexes();
+  void UpdateJobId2CriticalSectionIds();
+  void UpdateTotalJobCriticalSectionIds();
+  void UpdateCriticalSectionIds2IntersectingIds();
 
   bool inited_;
   std::vector<std::unique_ptr<CriticalSection>> critical_sections_;
-  std::vector<std::vector<int64_t>> job_id2critical_section_indexes_;
-  std::vector<int64_t> total_job_critical_section_indexes_;
-  std::vector<HashSet<int64_t>> critical_section_index2intersecting_indexes_;
+  std::vector<std::vector<int64_t>> job_id2critical_section_ids_;
+  std::vector<int64_t> total_job_critical_section_ids_;
+  std::vector<HashSet<int64_t>> critical_section_id2intersecting_ids_;
 };
 
 static const std::string kChannelNameGlobalWaitJobId = "GlobalWaitJobId";
