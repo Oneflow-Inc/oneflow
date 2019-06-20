1eaf94a0cfa7d96d10117b4a2a3d911faf98e5dc
Tsai <caishenghang@oneflow.org>
2019-06-20 19:42:44 +0800

    fix variable

--- oneflow/core/kernel/variable_kernel.cpp
+++ oneflow/core/kernel/variable_kernel.cpp
@@ -7,14 +7,9 @@ void VariableKernel<device_type, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* model_blob = BnInOp2Blob(ModelName());
   Blob* out_blob = BnInOp2Blob("out");
-  if ((this->op_conf().trainable() && *tick_ % Global<JobDesc>::Get()->NumOfPiecesInBatch() == 0)
-      || (this->op_conf().trainable() == false && *tick_ == 0)) {
-    if (this->kernel_conf().variable_conf().is_fw_inplace()) {
-      CHECK_EQ(out_blob->dptr(), model_blob->dptr());
-    } else {
-      CHECK_NE(out_blob->dptr(), model_blob->dptr());
-      out_blob->CopyDataContentFrom(ctx.device_ctx, model_blob);
-    }
+  if (*tick_ == 0) {
+    CHECK_NE(out_blob->dptr(), model_blob->dptr());
+    out_blob->CopyDataContentFrom(ctx.device_ctx, model_blob);
   } else {
     // do nothing
   }
