4786ea512a8d15763f79b863928a346f13ea96e4
Xinqi <lixinqi0703106@163.com>
2019-07-01 22:39:21 +0800

    mv declaration of Oneflow from cpp file to header file
    Former-commit-id: e2aff313b84b21a2bc6dbb810f967f908d2b8eb6

--- oneflow/core/job/oneflow.h
+++ oneflow/core/job/oneflow.h
@@ -1,2 +1,47 @@
+#ifndef ONEFLOW_CORE_JOB_ONEFLOW_H_
+#define ONEFLOW_CORE_JOB_ONEFLOW_H_
+
 #include "oneflow/core/job/job_set.pb.h"
+#include "oneflow/core/job/plan.pb.h"
+#include "oneflow/core/control/ctrl_server.h"
+#include "oneflow/core/job/runtime.h"
+
+namespace oneflow {
+
+class GlobalObjectsScope final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(GlobalObjectsScope);
+  GlobalObjectsScope(const JobSet& job_set);
+  ~GlobalObjectsScope();
+
+ private:
+  std::unique_ptr<CtrlServer> ctrl_server_;
+};
+
+class RuntimeBuffersScope final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(RuntimeBuffersScope);
+  RuntimeBuffersScope();
+  ~RuntimeBuffersScope();
+};
+
+class Oneflow final {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(Oneflow);
+  Oneflow(const oneflow::JobSet& job_set);
+  ~Oneflow();
+
+  void NaiveSequentialRun() const;
+
+ private:
+  std::unique_ptr<GlobalObjectsScope> global_objects_scope_;
+  Plan plan_;
+  std::unique_ptr<Runtime> runtime_;
+  std::unique_ptr<RuntimeBuffersScope> runtime_buffers_scope_;
+};
+
+}  // namespace oneflow
+
 int Main(const oneflow::JobSet& job_set);
+
+#endif  // ONEFLOW_CORE_JOB_ONEFLOW_H_
