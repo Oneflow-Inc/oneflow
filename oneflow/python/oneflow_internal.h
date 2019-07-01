2bd2ea6a590350338433877abb5e2741b2e06dc4
Xinqi <lixinqi0703106@163.com>
2019-07-01 22:59:17 +0800

    api for python
    Former-commit-id: eba5458a23ad9d39fc8053625687ebb1c909b669

--- oneflow/python/oneflow_internal.h
+++ oneflow/python/oneflow_internal.h
@@ -1,13 +1,25 @@
+#include "oneflow/core/job/job_set.pb.h"
 #include "oneflow/core/job/oneflow.h"
 #include "oneflow/core/register/ofblob.h"
 #include "oneflow/core/common/preprocessor.h"
 #include "oneflow/core/job/foreign_callback.h"
 #include "oneflow/core/common/buffer_manager.h"
 
-int RunSerializedJobSet(const oneflow::JobSet& job_set) { return Main(job_set); }
-int InitRunTimeJobSet() { TODO(); }
+void NaiveSequentialRunSerializedJobSet(const oneflow::JobSet& job_set) {
+  using namespace oneflow;
+  CHECK_ISNULL(Global<Oneflow>::Get());
+  Main(job_set);
+}
+
+void InitGlobalOneflowBySerializedJobSet(const oneflow::JobSet& job_set) {
+  using namespace oneflow;
+  CHECK_ISNULL(Global<Oneflow>::Get());
+  Global<Oneflow>::New(job_set);
+}
+
 void LaunchJob(const std::string& job_name, const std::shared_ptr<oneflow::ForeignCallback>& cb) {
   using namespace oneflow;
+  CHECK_NOTNULL(Global<Oneflow>::Get());
   auto* buffer_mgr = Global<BufferMgr<std::shared_ptr<ForeignCallback>>>::Get();
   int64_t job_id = Global<JobName2JobId>::Get()->at(job_name);
   const JobDesc& job_desc = GlobalJobDesc(job_id);
@@ -16,7 +28,12 @@ void LaunchJob(const std::string& job_name, const std::shared_ptr<oneflow::Forei
   buffer_mgr->Get(GetCallbackNotifierBufferName(job_name))->Send(cb);
   Global<BufferMgr<int64_t>>::Get()->Get(kBufferNameGlobalWaitJobId)->Send(job_id);
 }
-int DestroyRunTime() { TODO(); }
+
+void DestroyGlobalOneflow() {
+  using namespace oneflow;
+  CHECK_NOTNULL(Global<Oneflow>::Get());
+  Global<Oneflow>::Delete();
+}
 
 int Ofblob_GetDataType(uint64_t of_blob_ptr) {
   using namespace oneflow;
