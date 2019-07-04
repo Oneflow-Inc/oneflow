72d14994d7708a83b23771a3f699a32c715ea2dd
Xinqi <lixinqi0703106@163.com>
2019-07-04 15:29:03 +0800

    export dtype.*

--- oneflow/python/__init__.py
+++ oneflow/python/__init__.py
@@ -9,3 +9,4 @@ from oneflow.python.framework.compiler import val
 from oneflow.python.framework.compiler import var
 from oneflow.python.framework.compiler import parse_job_as_func_body
 from oneflow.python.framework.inter_user_job import pull
+from oneflow.python.framework.dtype import *
