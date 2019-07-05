ca748468e9723f88829fc7708bfa8111e5ddaf04
Tsai <caishenghang@oneflow.org>
2019-07-05 14:19:00 +0800

    address reviews

--- oneflow/python/framework/ofblob.py
+++ oneflow/python/framework/ofblob.py
@@ -25,28 +25,28 @@ class OfBlob(object):
     @property
     def shape(self):
         num_axes = oneflow_internal.OfBlob_NumAxes(self.of_blob_ptr_)
-        dst_ndarray = np.ndarray(num_axes, dtype=np.int)
+        dst_ndarray = np.ndarray(num_axes, dtype=np.int64)
         oneflow_internal.OfBlob_CopyShapeToNumpy(self.of_blob_ptr_, dst_ndarray)
         return dst_ndarray.tolist()
 
-    @property
-    def size(self):
-        elem_cnt = 1
-        for d in self.shape:
-            elem_cnt *= d
-        return elem_cnt
-
     def numpy(self):
-        dst_ndarray = np.ndarray(self.size, dtype=dtypeInNumpy(self.dtype))
+        dst_ndarray = np.ndarray(self._size, dtype=dtypeInNumpy(self.dtype))
         method_name = oneflow_internal.OfBlob_GetCopyToBufferFuncName(self.of_blob_ptr_)
         copy_method = getattr(oneflow_internal, method_name)
         copy_method(self.of_blob_ptr_, dst_ndarray)
         return dst_ndarray.reshape(self.shape)
 
     def copyFromNumpy(self, src_ndarray):
-        assert(self.size == src_ndarray.size)
+        assert(self._size == src_ndarray.size)
         assert(dtypeInNumpy(self.dtype) == src_ndarray.dtype)
         method_name = oneflow_internal.OfBlob_GetCopyFromBufferFuncName(self.of_blob_ptr_)
         copy_method = getattr(oneflow_internal, method_name)
         copy_method(self.of_blob_ptr_, src_ndarray)
         return
+
+    @property
+    def _size(self):
+        elem_cnt = 1
+        for d in self.shape:
+            elem_cnt *= d
+        return elem_cnt
