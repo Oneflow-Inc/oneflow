48a85ecf2df4d190377b4c522605c99c23c0f167
Xinqi <lixinqi0703106@163.com>
2019-07-05 12:08:01 +0800

    add_custom_target of_expan_hdr
    Former-commit-id: 6f5cabf0af507d213a2c2965304755295eb58b4c

---
+++ oneflow/python/framework/oneflow_internal.e.h
@@ -0,0 +1,42 @@
+#include "oneflow/core/common/preprocessor.h"
+#include "oneflow/core/common/data_type_seq.h"
+
+#define DEFINE_COPIER(T, type_proto)                                                \
+  void OfBlob_CopyToBuffer_##T(uint64_t of_blob_ptr, T* ptr, int64_t len) {         \
+    using namespace oneflow;                                                        \
+    auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);                         \
+    of_blob->AutoMemCopyTo<T>(ptr, len);                                            \
+  }                                                                                 \
+  void OfBlob_CopyFromBuffer_##T(uint64_t of_blob_ptr, const T* ptr, int64_t len) { \
+    using namespace oneflow;                                                        \
+    auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);                         \
+    of_blob->AutoMemCopyFrom<T>(ptr, len);                                          \
+  }
+
+OF_PP_FOR_EACH_TUPLE(DEFINE_COPIER, POD_DATA_TYPE_SEQ);
+
+#undef DEFINE_COPIER
+
+std::string OfBlob_GetCopyToBufferFuncName(uint64_t of_blob_ptr) {
+  using namespace oneflow;
+  static const HashMap<int64_t, std::string> data_type2func_name{
+#define DATA_TYPE_FUNC_NAME_PAIR(type_cpp, type_proto) \
+  {type_proto, "OfBlob_CopyToBuffer_" #type_cpp},
+      OF_PP_FOR_EACH_TUPLE(DATA_TYPE_FUNC_NAME_PAIR, POD_DATA_TYPE_SEQ)
+#undef DATA_TYPE_FUNC_NAME_PAIR
+  };
+  auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);
+  return data_type2func_name.at(of_blob->data_type());
+}
+
+std::string OfBlob_GetCopyFromBufferFuncName(uint64_t of_blob_ptr) {
+  using namespace oneflow;
+  static const HashMap<int64_t, std::string> data_type2func_name{
+#define DATA_TYPE_FUNC_NAME_PAIR(type_cpp, type_proto) \
+  {type_proto, "OfBlob_CopyFromBuffer_" #type_cpp},
+      OF_PP_FOR_EACH_TUPLE(DATA_TYPE_FUNC_NAME_PAIR, POD_DATA_TYPE_SEQ)
+#undef DATA_TYPE_FUNC_NAME_PAIR
+  };
+  auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);
+  return data_type2func_name.at(of_blob->data_type());
+}
--- oneflow/python/framework/oneflow_internal.h
+++ oneflow/python/framework/oneflow_internal.h
@@ -1,10 +1,9 @@
+#include <google/protobuf/text_format.h>
 #include "oneflow/core/job/job_set.pb.h"
 #include "oneflow/core/job/oneflow.h"
 #include "oneflow/core/register/ofblob.h"
-#include "oneflow/core/common/preprocessor.h"
 #include "oneflow/core/job/foreign_callback.h"
 #include "oneflow/core/common/buffer_manager.h"
-#include <google/protobuf/text_format.h>
 
 void NaiveSequentialRunSerializedJobSet(const oneflow::JobSet& job_set) {
   using namespace oneflow;
@@ -61,46 +60,6 @@ void OfBlob_CopyShapeTo(uint64_t of_blob_ptr, int64_t* ptr, int64_t num_axis) {
   auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);
   return of_blob->CopyShapeTo(ptr, num_axis);
 };
-
-#define DEFINE_COPIER(T, type_proto)                                                \
-  void OfBlob_CopyToBuffer_##T(uint64_t of_blob_ptr, T* ptr, int64_t len) {         \
-    using namespace oneflow;                                                        \
-    auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);                         \
-    of_blob->AutoMemCopyTo<T>(ptr, len);                                            \
-  }                                                                                 \
-  void OfBlob_CopyFromBuffer_##T(uint64_t of_blob_ptr, const T* ptr, int64_t len) { \
-    using namespace oneflow;                                                        \
-    auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);                         \
-    of_blob->AutoMemCopyFrom<T>(ptr, len);                                          \
-  }
-
-OF_PP_FOR_EACH_TUPLE(DEFINE_COPIER, POD_DATA_TYPE_SEQ);
-
-#undef DEFINE_COPIER
-
-std::string OfBlob_GetCopyToBufferFuncName(uint64_t of_blob_ptr) {
-  using namespace oneflow;
-  static const HashMap<int64_t, std::string> data_type2func_name{
-#define DATA_TYPE_FUNC_NAME_PAIR(type_cpp, type_proto) \
-  {type_proto, "OfBlob_CopyToBuffer_" #type_cpp},
-      OF_PP_FOR_EACH_TUPLE(DATA_TYPE_FUNC_NAME_PAIR, POD_DATA_TYPE_SEQ)
-#undef DATA_TYPE_FUNC_NAME_PAIR
-  };
-  auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);
-  return data_type2func_name.at(of_blob->data_type());
-}
-
-std::string OfBlob_GetCopyFromBufferFuncName(uint64_t of_blob_ptr) {
-  using namespace oneflow;
-  static const HashMap<int64_t, std::string> data_type2func_name{
-#define DATA_TYPE_FUNC_NAME_PAIR(type_cpp, type_proto) \
-  {type_proto, "OfBlob_CopyFromBuffer_" #type_cpp},
-      OF_PP_FOR_EACH_TUPLE(DATA_TYPE_FUNC_NAME_PAIR, POD_DATA_TYPE_SEQ)
-#undef DATA_TYPE_FUNC_NAME_PAIR
-  };
-  auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);
-  return data_type2func_name.at(of_blob->data_type());
-}
 
 namespace {
 
