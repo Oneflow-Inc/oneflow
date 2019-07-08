68aba740bab4f9522c65d5bf0600ec19621fb56a
jackalcooper <jackalcooper@gmail.com>
2019-07-08 15:13:52 +0800

    add dtype (#1915)
    * add dtype
    
    * add de-import

--- oneflow/python/framework/dtype.py
+++ oneflow/python/framework/dtype.py
@@ -1,3 +1,14 @@
 from __future__ import absolute_import
+import oneflow.core.common.data_type_pb2 as data_type_pb2
 
-# TODO()
+invalid = data_type_pb2.kInvalidDataType
+char = data_type_pb2.kChar
+float = data_type_pb2.kFloat
+double = data_type_pb2.kDouble
+int8 = data_type_pb2.kInt8
+int32 = data_type_pb2.kInt32
+int64 = data_type_pb2.kInt64
+uint8 = data_type_pb2.kUInt8
+
+del absolute_import
+del data_type_pb2
