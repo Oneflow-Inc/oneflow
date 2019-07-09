144bc9a1006906fa9f3887dd709005730cc0ae6b
liujuncheng <liujuncheng1022@gmail.com>
2019-07-09 15:55:24 +0800

    Revert "SetRealtimePriority"
    This reverts commit 4c5acc2c756ae854d56a9ffffbdc09ce27259e67.

--- oneflow/core/thread/gpu_thread.cpp
+++ oneflow/core/thread/gpu_thread.cpp
@@ -1,29 +1,13 @@
 #include "oneflow/core/thread/gpu_thread.h"
 #include "oneflow/core/device/cuda_stream_handle.h"
-#include <pthread.h>
-#include <sched.h>
 
 namespace oneflow {
-
-namespace {
-
-void SetRealtimePriority() {
-  struct sched_param params {};
-  params.sched_priority = sched_get_priority_max(SCHED_FIFO);
-  pthread_t this_thread = pthread_self();
-  if (pthread_setschedparam(this_thread, SCHED_FIFO, &params) != 0) {
-    LOG(WARNING) << "err set realtime proiority";
-  }
-}
-
-}  // namespace
 
 #ifdef WITH_CUDA
 
 GpuThread::GpuThread(int64_t thrd_id, int64_t dev_id) {
   set_thrd_id(thrd_id);
   mut_actor_thread() = std::thread([this, dev_id]() {
-    SetRealtimePriority();
     CudaCheck(cudaSetDevice(dev_id));
     ThreadCtx ctx;
     ctx.g_cuda_stream.reset(new CudaStreamHandle(&cb_event_chan_));
@@ -31,7 +15,6 @@ GpuThread::GpuThread(int64_t thrd_id, int64_t dev_id) {
     PollMsgChannel(ctx);
   });
   cb_event_poller_ = std::thread([this, dev_id]() {
-    SetRealtimePriority();
     CudaCheck(cudaSetDevice(dev_id));
     CudaCBEvent cb_event;
     while (cb_event_chan_.Receive(&cb_event) == kChannelStatusSuccess) {
