480657fe4c630cf0aaf57c06798b37f0b7c38d06
Xinqi <lixinqi0703106@163.com>
2019-07-10 21:35:58 +0800

    config for remote
    Former-commit-id: 135eeb816a54c6904a019058955776a57ac0d398

--- oneflow/python/framework/config_util.py
+++ oneflow/python/framework/config_util.py
@@ -5,21 +5,21 @@ import oneflow.python.framework.decorator_context as decorator_context
 import oneflow.python.framework.oneflow_mode as oneflow_mode
 
 def MakeResourceConfigDecorator(field, field_type):
-    return _MakeConfigDecorator(_AssertIsCompilingMain, lambda job_set: job_set.resource,
-                                field, field_type)
+    return _MakeConfigDecorator(_AssertIsCompilingMain, _UpdateMainDecoratorContext,
+                                lambda job_set: job_set.resource, field, field_type)
 def MakeIOConfigDecorator(field, field_type):
-    return _MakeConfigDecorator(_AssertIsCompilingMain, lambda job_set: job_set.io_conf,
-                                field, field_type)
+    return _MakeConfigDecorator(_AssertIsCompilingMain, _UpdateMainDecoratorContext,
+                                lambda job_set: job_set.io_conf, field, field_type)
 def MakeCppFlagsConfigDecorator(field, field_type):
-    return _MakeConfigDecorator(_AssertIsCompilingMain, lambda job_set: job_set.cpp_flags_conf,
-                                field, field_type)
+    return _MakeConfigDecorator(_AssertIsCompilingMain, _UpdateMainDecoratorContext,
+                                lambda job_set: job_set.cpp_flags_conf, field, field_type)
 def MakeProfilerConfigDecorator(field, field_type):
-    return _MakeConfigDecorator(_AssertIsCompilingMain, lambda job_set: job_set.profile_conf,
-                                field, field_type)
+    return _MakeConfigDecorator(_AssertIsCompilingMain, _UpdateMainDecoratorContext,
+                                lambda job_set: job_set.profile_conf, field, field_type)
 
 def MakeJobOtherConfigDecorator(field, field_type):
-    return _MakeConfigDecorator(_AssertIsCompilingRemote, lambda job_conf: job_conf.other,
-                                field, field_type)
+    return _MakeConfigDecorator(_AssertIsCompilingRemote, _UpdateRemoteDecoratorContext,
+                                lambda job_conf: job_conf.other, field, field_type)
 
 def DefaultConfigJobSet(job_set):
     assert compile_context.IsCompilingMain()
@@ -47,39 +47,53 @@ def _ComposeConfigFunc(config_func, other_config_func):
         other_config_func(job_set_or_job_conf)
     return ComposedConfigFunc
 
-def _UpdateDecorateFuncAndContext(decorated_func, config_func, func):
+def _UpdateDecorateConfigFunc(decorated_func, config_func, func):
     if hasattr(func, '__oneflow_config_func__'):
         decorated_func.__oneflow_config_func__ = \
             _ComposeConfigFunc(config_func, func.__oneflow_config_func__)
     else:
         decorated_func.__oneflow_config_func__ = config_func
+
+def _UpdateMainDecoratorContext(decorated_func, func):
     if decorator_context.main_func == func:
         decorator_context.main_func = decorated_func
     else:
-        assert decorator_context.main_func is None, "only single main func supported"
+        assert decorator_context.main_func is None, "no mutltiply 'main' decorator supported"
 
-def _GenConfigDecorator(config_func):
+def _UpdateRemoteDecoratorContext(decorated_func, func):
+    job_name = func.__name__
+    if decorator_context.job_name2func.get(job_name) == func:
+        decorator_context.job_name2func = decorated_func
+    else:
+        assert job_name not in decorator_context.job_name2func, \
+            "no mutltiply 'remote' decorator supported"
+
+def _GenConfigDecorator(config_func, decorator_ctx_handler):
     def decorator(func):
         def decorated_func(*argv):
             func(*argv)
-
-        _UpdateDecorateFuncAndContext(decorated_func, config_func, func)
+            
         decorated_func.__name__ = func.__name__
+        decorator_ctx_handler(decorated_func, func)
         for x in dir(func):
             if x.startswith('__oneflow_'):
                 setattr(decorated_func, x, getattr(func, x))
+        _UpdateDecorateConfigFunc(decorated_func, config_func, func)
         return decorated_func
     
     return decorator
 
-def _MakeConfigDecorator(ctx_asserter, get_attr_container, field, field_type):
+def _MakeConfigDecorator(ctx_asserter,
+                         decorator_ctx_handler,
+                         get_attr_container,
+                         field, field_type):
     def Func(val):
         assert isinstance(val, field_type), \
             "config field '%s' should be instance of %s, %s given"%(field, field_type, type(val))
         def ConfigFunc(pb_msg):
             ctx_asserter()
             setattr(get_attr_container(pb_msg), field, val)
-        return _GenConfigDecorator(ConfigFunc)
+        return _GenConfigDecorator(ConfigFunc, decorator_ctx_handler)
     return Func
 
 def _DefaultConfigResource(job_set):
