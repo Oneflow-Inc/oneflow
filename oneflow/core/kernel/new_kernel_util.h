12a2747d0b1968dfa905b14526bf1c44c1f72938
Dounm <niuchong893184@gmail.com>
2019-07-11 19:31:21 +0800

    refactor(new_kernel_util.h): remove unused header files

--- oneflow/core/kernel/new_kernel_util.h
+++ oneflow/core/kernel/new_kernel_util.h
@@ -1,17 +1,6 @@
 #ifndef ONEFLOW_CORE_KERNEL_NEW_KERNEL_UTIL_H_
 #define ONEFLOW_CORE_KERNEL_NEW_KERNEL_UTIL_H_
 
-#include "oneflow/core/common/blas.h"
-#include "oneflow/core/common/data_type.h"
-#include "oneflow/core/common/str_util.h"
-#include "oneflow/core/device/cudnn_util.h"
-#include "oneflow/core/job/job_desc.h"
-#include "oneflow/core/job/resource.pb.h"
-#include "oneflow/core/kernel/kernel_context.h"
-#include "oneflow/core/operator/op_conf.pb.h"
-#include "oneflow/core/persistence/snapshot.h"
-#include "oneflow/core/register/blob.h"
-#include "oneflow/core/common/switch_func.h"
 #include "oneflow/core/kernel/util/dnn_interface.h"
 #include "oneflow/core/kernel/util/blas_interface.h"
 #include "oneflow/core/kernel/util/arithemetic_interface.h"
