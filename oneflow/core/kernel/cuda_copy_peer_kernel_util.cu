99bede801f5a4e16bbb1bcf2a885e8b39c4219ac
liujuncheng <liujuncheng1022@gmail.com>
2019-07-12 19:51:04 +0800

    refine

--- oneflow/core/kernel/cuda_copy_peer_kernel_util.cu
+++ oneflow/core/kernel/cuda_copy_peer_kernel_util.cu
@@ -1,8 +1,10 @@
 #include "oneflow/core/kernel/cuda_copy_peer_kernel_util.h"
 #include "oneflow/core/kernel/kernel_util.cuh"
 
-#define N_THREAD 256
-#define N_LOOP 16
+constexpr int32_t NUM_THREAD = 256;
+constexpr int32_t NUM_BLOCK_PER_CHUNK = 16;
+constexpr int32_t BLOCK_SIZE = NUM_THREAD * sizeof(ulong2);
+constexpr int32_t CHUNK_SIZE = BLOCK_SIZE * NUM_BLOCK_PER_CHUNK;
 
 namespace oneflow {
 
@@ -22,51 +24,85 @@ __forceinline__ __device__ void Store(ulong2* p, ulong2& v) {
   // clang-format on
 }
 
-__global__ void ReadKernel(void* buf, const void* src, volatile int32_t* step_mutex, size_t size) {
-  const int32_t step_size = N_THREAD * N_LOOP * sizeof(ulong2);
-  const int32_t n_step = DivUp(size, step_size);
-  const int32_t thread_id = threadIdx.x;
-  if (thread_id == 0) { assert(*step_mutex == 0); }
-  __syncthreads();
-  for (int32_t step = 0; step < n_step; ++step) {
-    int32_t step_offset = step * step_size;
-    ulong2 v;
+__forceinline__ __device__ void FetchStore(ulong2* dst, const ulong2* src) {
+  ulong2 v;
+  Fetch(v, src);
+  Store(dst, v);
+}
+
+__forceinline__ __device__ void FetchStore(void* dst, const void* src) {
+  FetchStore(reinterpret_cast<ulong2*>(dst), reinterpret_cast<const ulong2*>(src));
+}
+
+__forceinline__ __device__ void CopyChunk(void* dst, const void* src) {
 #pragma unroll
-    for (int32_t l = 0; l < N_LOOP; ++l) {
-      const int32_t offset = step_offset + (l * N_THREAD + thread_id) * sizeof(ulong2);
-      if (offset < size) {
-        Fetch(v, reinterpret_cast<const ulong2*>(static_cast<const uint8_t*>(src) + offset));
-        Store(reinterpret_cast<ulong2*>(static_cast<uint8_t*>(buf) + offset), v);
-      }
-    }
-    __syncthreads();
-    __threadfence_system();
-    if (thread_id == 0) { *step_mutex = step + 1; }
+  for (int32_t l = 0; l < NUM_BLOCK_PER_CHUNK; ++l) {
+    FetchStore(dst, src);
+    dst = (unsigned char*)(dst) + BLOCK_SIZE;
+    src = (const unsigned char*)(src) + BLOCK_SIZE;
   }
 }
 
-__global__ void WriteKernel(void* dst, const void* buf, volatile int32_t* step_mutex, size_t size) {
-  const int32_t step_size = N_THREAD * N_LOOP * sizeof(ulong2);
-  const int32_t n_step = DivUp(size, step_size);
-  const int32_t thread_id = threadIdx.x;
+__forceinline__ __device__ void CopyPartialChunk(void* dst, const void* src, int32_t size) {
+  int32_t offset = 0;
+  for (int32_t l = 0; l < NUM_BLOCK_PER_CHUNK; ++l) {
+    if (offset < size) {
+      FetchStore((unsigned char*)(dst) + offset, (const unsigned char*)(src) + offset);
+    }
+    offset += BLOCK_SIZE;
+  }
+}
+
+__forceinline__ __device__ void Send(const void* src, size_t size, int32_t thread_id, void* buf_0,
+                                     void* buf_1, volatile int32_t* send_step,
+                                     volatile int32_t* recv_step) {
+  const int32_t num_chunk = DivUp(size, CHUNK_SIZE);
+  int32_t remaining = size;
+  if (thread_id == 0) {
+    assert(*send_step == 0);
+    assert(*recv_step == 0);
+  }
   __syncthreads();
-  for (int32_t step = 0; step < n_step; ++step) {
+  for (int32_t step = 0; step < num_chunk; ++step) {
     if (thread_id == 0) {
-      const int32_t next_step = step + 1;
-      while (*step_mutex < next_step) {}
+      while (step - *recv_step < 2) {}
+    }
+    __syncthreads();
+    void* buf = step % 2 == 0 ? buf_0 : buf_1;
+    if (remaining >= CHUNK_SIZE) {
+      CopyChunk(buf, src);
+    } else {
+      CopyPartialChunk(buf, src, remaining);
+    }
+    remaining -= CHUNK_SIZE;
+    src = (const unsigned char*)(src) + CHUNK_SIZE;
+    __syncthreads();
+    __threadfence_system();
+    if (thread_id == 0) { *send_step = step + 1; }
+  }
+}
+
+__forceinline__ __device__ void Recv(void* dst, size_t size, int32_t thread_id, const void* buf_0,
+                                     const void* buf_1, volatile int32_t* send_step,
+                                     volatile int32_t* recv_step) {
+  const int32_t num_chunk = DivUp(size, CHUNK_SIZE);
+  int32_t remaining = size;
+  for (int32_t step = 0; step < num_chunk; ++step) {
+    if (thread_id == 0) {
+      while (*send_step <= step) {}
     }
     __syncthreads();
     __threadfence_system();
-    int32_t step_offset = step * step_size;
-    ulong2 v;
-#pragma unroll
-    for (int32_t l = 0; l < N_LOOP; ++l) {
-      const int32_t offset = step_offset + (l * N_THREAD + thread_id) * sizeof(ulong2);
-      if (offset < size) {
-        Fetch(v, reinterpret_cast<const ulong2*>(static_cast<const uint8_t*>(buf) + offset));
-        Store(reinterpret_cast<ulong2*>(static_cast<uint8_t*>(dst) + offset), v);
-      }
+    const void* buf = step % 2 == 0 ? buf_0 : buf_1;
+    if (remaining >= CHUNK_SIZE) {
+      CopyChunk(dst, buf);
+    } else {
+      CopyPartialChunk(dst, buf, remaining);
     }
+    remaining -= CHUNK_SIZE;
+    dst = (unsigned char*)(dst) + CHUNK_SIZE;
+    __syncthreads();
+    if (thread_id == 0) { *recv_step = step + 1; }
   }
 }
 
@@ -74,14 +110,6 @@ __global__ void WriteKernel(void* dst, const void* buf, volatile int32_t* step_m
 
 void CudaCopyPeerKernelUtil::CopyAsync(void* dst, void* buf, const void* src, int32_t* step_mutex,
                                        size_t size, int32_t dst_dev_id, int32_t src_dev_id,
-                                       cudaStream_t read, cudaStream_t write) {
-  int32_t saved_dev_id;
-  CudaCheck(cudaGetDevice(&saved_dev_id));
-  CudaCheck(cudaSetDevice(src_dev_id));
-  ReadKernel<<<1, N_THREAD, 0, read>>>(buf, src, step_mutex, size);
-  CudaCheck(cudaSetDevice(dst_dev_id));
-  WriteKernel<<<1, N_THREAD, 0, write>>>(dst, buf, step_mutex, size);
-  CudaCheck(cudaSetDevice(saved_dev_id));
-}
+                                       cudaStream_t read, cudaStream_t write) {}
 
 }  // namespace oneflow
