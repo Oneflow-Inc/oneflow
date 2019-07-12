cd1dccbb5313e417efd4fa09b430b450973a535b
Niu Chong <niuchong893184@gmail.com>
2019-07-12 13:13:01 +0800

    Dev new kernel util (#1925)
    * refactor(kernel/util): refactor NewKernelUtil and add DnnIf
    
    * refactor(kernel/util): add BlasIf
    
    * refactor(kernel/util): add ArithemeticIf
    
    * refactor(kernel/util): add cuda_kernel_util.*
    
    * refactor: refactor NewKernelUtil
    
    * refactor(kernel/util/xxx_interface): mv XxxIf to interface_base.h to avoid loop including
    
    * refactor(new_kernel_util.h): remove unused header files
    
    * refactor: refactor loop include

---
+++ oneflow/core/kernel/util/host_arithemetic_interface.h
@@ -0,0 +1,21 @@
+#ifndef ONEFLOW_CORE_KERNEL_UTIL_CPU_ARITHEMETIC_INTERFACE_H_
+#define ONEFLOW_CORE_KERNEL_UTIL_CPU_ARITHEMETIC_INTERFACE_H_
+
+#include "oneflow/core/kernel/util/arithemetic_interface.h"
+#include "oneflow/core/register/blob.h"
+
+namespace oneflow {
+
+template<>
+struct ArithemeticIf<DeviceType::kCPU> {
+  static void Transpose(DeviceCtx* ctx, const int32_t num_axis, const Shape& x_shape,
+                        const Shape& y_shape, const PbRf<int32_t>& permutation,
+                        const int64_t elem_cnt, const float* x, float* y);
+  static void Transpose(DeviceCtx* ctx, const int32_t num_axis, const Shape& x_shape,
+                        const Shape& y_shape, const PbRf<int32_t>& permutation,
+                        const int64_t elem_cnt, const double* x, double* y);
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_UTIL_CPU_ARITHEMETIC_INTERFACE_H_
