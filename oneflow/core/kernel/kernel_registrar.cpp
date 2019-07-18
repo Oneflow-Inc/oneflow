a083cff5d8f33e3351452b19d1ed24d50837beef
Dounm <niuchong893184@gmail.com>
2019-07-18 11:12:09 +0800

    chore: remove unused kernel_registrar.cpp

--- oneflow/core/kernel/kernel_registrar.cpp
+++
@@ -1,8 +0,0 @@
-#include "oneflow/core/kernel/kernel_registrar.h"
-#include "oneflow/core/kernel/kernel.h"
-
-namespace oneflow {
-
-namespace kernel_registration {}  // namespace kernel_registration
-
-}  // namespace oneflow
