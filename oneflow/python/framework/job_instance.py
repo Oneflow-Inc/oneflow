f3996d1af29a022dca9644a44d992d38a1f54a04
lixinqi <lixinqi0703106@163.com>
2019-07-18 20:26:18 +0800

    MakeJobInstance
    Former-commit-id: 515dc990bb1730a677d5cc6470417101f715fa4c

--- oneflow/python/framework/job_instance.py
+++ oneflow/python/framework/job_instance.py
@@ -6,29 +6,37 @@ import oneflow.python.framework.ofblob as ofblob
 import oneflow_internal
 
 def MakeUserJobInstance(job_name, finish_cb = None):
-    if finish_cb is None: finish_cb = _DoNothing
-    return JobInstance(job_name, finish_cb = finish_cb)
+    return MakeJobInstance(job_name, finish_cb = finish_cb)
 
 def MakePullJobInstance(job_name, op_name, pull_cb, finish_cb = None):
-    if finish_cb is None: finish_cb = _DoNothing
-    return JobInstance(job_name,
-                       sole_output_op_name_in_user_job = op_name,
-                       pull_cb = pull_cb,
-                       finish_cb = finish_cb);
+    return MakeJobInstance(
+        job_name,
+        sole_output_op_name_in_user_job = op_name,
+        pull_cb = pull_cb,
+        finish_cb = finish_cb)
 
 def MakePushJobInstance(job_name, op_name, push_cb, finish_cb = None):
-    if finish_cb is None: finish_cb = _DoNothing
-    return JobInstance(job_name,
-                       sole_input_op_name_in_user_job = op_name,
-                       push_cb = push_cb,
-                       finish_cb = finish_cb);
+    return MakeJobInstance(
+        job_name,
+        sole_input_op_name_in_user_job = op_name,
+        push_cb = push_cb,
+        finish_cb = finish_cb)
 
 def MakeArgPassJobInstance(job_name, src_op_name, dst_op_name, finish_cb = None):
-    if finish_cb is None: finish_cb = _DoNothing
-    return JobInstance(job_name,
-                       sole_output_op_name_in_user_job = src_op_name,
-                       sole_input_op_name_in_user_job = dst_op_name,
-                       finish_cb = finish_cb);
+    return MakeJobInstance(
+        job_name,
+        sole_output_op_name_in_user_job = src_op_name,
+        sole_input_op_name_in_user_job = dst_op_name,
+        finish_cb = finish_cb)
+
+def MakeJobInstance(*arg, **kw):
+    def _DoNothing():
+        pass
+    if 'finish_cb' not in kw or kw['finish_cb'] is None: kw['finish_cb'] = _DoNothing
+    job_instance = JobInstance(*arg, **kw)
+    global _flying_job_instance
+    _flying_job_instance[id(job_instance)] = job_instance
+    return job_instance
 
 class JobInstance(oneflow_internal.ForeignJobInstance):
     def __init__(self, job_name,
@@ -45,8 +53,6 @@ class JobInstance(oneflow_internal.ForeignJobInstance):
         self.push_cb_ = push_cb
         self.pull_cb_ = pull_cb
         self.finish_cb_ = finish_cb
-        global _flying_job_instance
-        _flying_job_instance[id(self)] = self
 
     def job_name(self): 
         try: return self.job_name_
@@ -91,5 +97,3 @@ class JobInstance(oneflow_internal.ForeignJobInstance):
 # _flying_job_instance prevents job_instance earlier destructation
 _flying_job_instance = {}
 
-def _DoNothing():
-    pass
