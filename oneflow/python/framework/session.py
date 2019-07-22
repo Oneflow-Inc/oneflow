2c4e48aef1e6246aa0e9c0ca0f3a6924a892a1da
cheng cheng <472491134@qq.com>
2019-07-22 14:00:50 +0800

    finish Session.sync() for latest version (#1934)
    * finish Session.sync() for latest version
    
    * add sess.sync() in example.py
    
    * remove note
    
    * add try-catch for post call back of job instance
    
    * refine func name
    
    
    Former-commit-id: 280b52043cd0f79fd8b5707a7c9bc9ca71751756

--- oneflow/python/framework/session.py
+++ oneflow/python/framework/session.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 
+import threading
 import oneflow.core.job.job_set_pb2 as job_set_util
 import oneflow.core.job.job_pb2 as job_util
 import oneflow.python.framework.compiler as compiler
 import oneflow.python.framework.runtime as runtime
+import oneflow.python.framework.runtime_context as runtime_ctx
 import oneflow.python.framework.config_util as config_util
 from oneflow.python.framework.out_remote_blobs_result_box import OutRemoteBlobsResultBox
 
@@ -18,6 +20,10 @@ class Session(object):
         self.job_name2job_func_ = {}
         for job_func in job_funcs: self.job_name2job_func_[job_func.__name__] = job_func
         self.is_running_ = False
+        self.cond_var_ = threading.Condition()
+        self.running_job_cnt_ = 0
+        runtime_ctx.AddJobInstancePreLaunchCallbacks(self._PreLaunchCallback)
+        runtime_ctx.AddJobInstancePostFinishCallbacks(self._PostFinishCallback)
 
     def run(self, job_func, *arg):
         assert self.is_running_
@@ -25,7 +31,7 @@ class Session(object):
         remote_blobs = runtime.LaunchJob(job_func, *arg)
         if remote_blobs is None: return
         return OutRemoteBlobsResultBox().SetResult(remote_blobs).Inited()
-        
+
     def map(self, job_func, feed_data):
         assert self.is_running_
         assert job_func.__name__ in self.job_name2job_func_
@@ -42,11 +48,26 @@ class Session(object):
         assert self.is_running_
         assert job_func.__name__ in self.job_name2job_func_
         runtime.LaunchJob(job_func, *arg)
-        
+
     def sync(self):
         assert self.is_running_
-        TODO()
-    
+        self.cond_var_.acquire()
+        while self.running_job_cnt_ > 0:
+            self.cond_var_.wait()
+        assert self.running_job_cnt_ == 0
+        self.cond_var_.release()
+
+    def _PreLaunchCallback(self, job_instance):
+        self.cond_var_.acquire()
+        self.running_job_cnt_ += 1
+        self.cond_var_.release()
+
+    def _PostFinishCallback(self, job_instance):
+        self.cond_var_.acquire()
+        self.running_job_cnt_ -= 1
+        self.cond_var_.notify()
+        self.cond_var_.release()
+
     def __enter__(self):
         assert self.is_running_ == False
         self.is_running_ = True
@@ -57,3 +78,4 @@ class Session(object):
     def __exit__(self, *args):
         assert self.is_running_ == True
         self.runtime_env_.__exit__()
+
