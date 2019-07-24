f7ab8604eabdcb1ff57b9327cde1e60c50a5d1b1
Tsai <caishenghang@oneflow.org>
2019-07-24 19:52:38 +0800

    fix blob name

--- oneflow/core/operator/assign_op.cpp
+++ oneflow/core/operator/assign_op.cpp
@@ -4,13 +4,13 @@ namespace oneflow {
 
 void AssignOp::InitFromOpConf() {
   CHECK(op_conf().has_assign_conf());
-  EnrollInputBn("x");
-  EnrollInputBn("y", false)->set_is_mutable(true);
+  EnrollInputBn("x", false)->set_is_mutable(true);
+  EnrollInputBn("value");
 }
 
 void AssignOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                               const ParallelContext* parallel_ctx) const {
-  CHECK(*GetBlobDesc4BnInOp("x") == *GetBlobDesc4BnInOp("y"));
+  CHECK(*GetBlobDesc4BnInOp("x") == *GetBlobDesc4BnInOp("value"));
 }
 
 const PbMessage& AssignOp::GetCustomizedConf() const { return op_conf().assign_conf(); }
