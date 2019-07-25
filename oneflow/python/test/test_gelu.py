6ce920e98fbf8562893ea740686a978b74cfe5e2
AHuskie <2101943624@qq.com>
2019-07-25 13:30:53 +0800

    backup

---
+++ oneflow/python/test/test_gelu.py
@@ -0,0 +1,30 @@
+import oneflow as flow
+import numpy as np
+from oneflow.python.keras.activations import gelu
+import torch
+import math
+
+jobs = []
+@flow.append_func_to_list(jobs)
+def GeluJob(x = flow.val((10,))):
+    job_conf = flow.get_cur_job_conf_builder()
+    job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
+    return gelu(x)
+
+config = flow.ConfigProtoBuilder()
+config.gpu_device_num(1)
+
+data = []
+for i in range(5): data.append(np.ones((10,), dtype=np.float32))
+
+with flow.Session(jobs, config) as sess:
+    print("the result of oneflow is:")
+    for x in data:  print(sess.run(GeluJob, x).get())
+
+
+
+def gelu(x):
+    return 0.5 * x * (1 + troch.tanh(math.sqrt(2/math.pi) * (x + 0.044715 * torch.pow(x, 3))))
+x = torch.tensor(data)
+print("the result of pytorch is:")
+print(x)
