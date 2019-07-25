66ec04783bed510242c7f8e29271093ddfd195bf
AHuskie <2101943624@qq.com>
2019-07-25 17:04:41 +0800

    add tanh sigmoid test

---
+++ oneflow/python/test/tanh.py
@@ -0,0 +1,25 @@
+import oneflow as flow
+import numpy as np
+import torch
+import math
+
+jobs = []
+@flow.append_func_to_list(jobs)
+def TanhJob(x = flow.val((10,))):
+    job_conf = flow.get_cur_job_conf_builder()
+    job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
+    return flow.keras.activations.tanh(x)
+
+x = np.array(range(-5,5), dtype=np.float32)
+
+config = flow.ConfigProtoBuilder()
+config.gpu_device_num(1)
+
+with flow.Session(jobs, config) as sess:
+    a = sess.run(TanhJob, x).get()
+
+b = torch.tanh(torch.Tensor(x))
+
+result = np.isclose(np.array(a), b.numpy(), rtol=1e-03, atol=1e-05)
+for i in result:
+    assert i, "the tanh test is wrong!"
