3224e0030331c25222159ed7dcbb9e810433e184
liujuncheng <liujuncheng1022@gmail.com>
2019-07-25 20:43:32 +0800

    other op

--- oneflow/core/kernel/cuda_ring_all_reduce_kernel.cu
+++ oneflow/core/kernel/cuda_ring_all_reduce_kernel.cu
@@ -169,118 +169,24 @@ __global__ void RecvReduceSendGpu(CudaRingAllReduceArg<T> arg) {
 
 template<typename T>
 __global__ void RecvReduceSendCopyGpu(CudaRingAllReduceArg<T> arg) {
-  const int32_t thread_id = threadIdx.x;
   const int32_t block_id = blockIdx.x;
-  T* send = arg.send[block_id];
-  const T* recv = arg.recv[block_id];
-  T* dst = arg.dst[block_id];
-  const T* src = arg.src[block_id];
-  const int64_t num_elem = arg.num_elem[block_id];
-  static_assert(PACK_SIZE % sizeof(T) == 0, "");
-  const int32_t num_elem_per_line = LINE_SIZE / sizeof(T);
-  const int64_t num_line = num_elem / num_elem_per_line;
-  const int32_t warp_id = thread_id / NUM_THREAD_PER_WARP;
-  const int32_t lane_id = thread_id % NUM_THREAD_PER_WARP;
-  ulong2 line_recv[NUM_PACK_PER_LINE_PER_THREAD];
-  ulong2 line_src[NUM_PACK_PER_LINE_PER_THREAD];
-  const int32_t offset = warp_id * NUM_PACK_PER_LINE_PER_WARP + lane_id;
-  ulong2* send_pack_ptr = reinterpret_cast<ulong2*>(send) + offset;
-  ulong2* dst_pack_ptr = reinterpret_cast<ulong2*>(dst) + offset;
-  const ulong2* src_pack_ptr = reinterpret_cast<const ulong2*>(src) + offset;
-  const ulong2* recv_pack_ptr = reinterpret_cast<const ulong2*>(recv) + offset;
-  for (int64_t l = 0; l < num_line; ++l) {
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      FetchFunctor<Pack>()(line_recv[p], recv_pack_ptr + p * NUM_THREAD_PER_WARP);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      FetchFunctor<Pack>()(line_src[p], src_pack_ptr + p * NUM_THREAD_PER_WARP);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      line_recv[p] = PackReduceFunctor<ReduceMethod::kSum, T>()(line_recv[p], line_src[p]);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      StoreFunctor<Pack>()(send_pack_ptr + p * NUM_THREAD_PER_WARP, line_recv[p]);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      StoreFunctor<Pack>()(dst_pack_ptr + p * NUM_THREAD_PER_WARP, line_recv[p]);
-    }
-    send_pack_ptr += NUM_PACK_PER_LINE;
-    dst_pack_ptr += NUM_PACK_PER_LINE;
-    src_pack_ptr += NUM_PACK_PER_LINE;
-    recv_pack_ptr += NUM_PACK_PER_LINE;
-  }
+  AlignedReduceOrCopy<ReduceMethod::kSum, T, true, true, true, true>(
+      arg.num_elem[block_id], arg.recv[block_id], arg.src[block_id], arg.send[block_id],
+      arg.dst[block_id]);
 }
 
 template<typename T>
 __global__ void RecvSendCopyGpu(CudaRingAllReduceArg<T> arg) {
-  const int32_t thread_id = threadIdx.x;
   const int32_t block_id = blockIdx.x;
-  T* send = arg.send[block_id];
-  const T* recv = arg.recv[block_id];
-  T* dst = arg.dst[block_id];
-  const int64_t num_elem = arg.num_elem[block_id];
-  static_assert(PACK_SIZE % sizeof(T) == 0, "");
-  const int32_t num_elem_per_line = LINE_SIZE / sizeof(T);
-  const int64_t num_line = num_elem / num_elem_per_line;
-  const int32_t warp_id = thread_id / NUM_THREAD_PER_WARP;
-  const int32_t lane_id = thread_id % NUM_THREAD_PER_WARP;
-  ulong2 line_recv[NUM_PACK_PER_LINE_PER_THREAD];
-  const int32_t offset = warp_id * NUM_PACK_PER_LINE_PER_WARP + lane_id;
-  ulong2* send_pack_ptr = reinterpret_cast<ulong2*>(send) + offset;
-  ulong2* dst_pack_ptr = reinterpret_cast<ulong2*>(dst) + offset;
-  const ulong2* recv_pack_ptr = reinterpret_cast<const ulong2*>(recv) + offset;
-  for (int64_t l = 0; l < num_line; ++l) {
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      FetchFunctor<Pack>()(line_recv[p], recv_pack_ptr + p * NUM_THREAD_PER_WARP);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      StoreFunctor<Pack>()(send_pack_ptr + p * NUM_THREAD_PER_WARP, line_recv[p]);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      StoreFunctor<Pack>()(dst_pack_ptr + p * NUM_THREAD_PER_WARP, line_recv[p]);
-    }
-    send_pack_ptr += NUM_PACK_PER_LINE;
-    dst_pack_ptr += NUM_PACK_PER_LINE;
-    recv_pack_ptr += NUM_PACK_PER_LINE;
-  }
+  AlignedReduceOrCopy<ReduceMethod::kSum, T, true, false, true, true>(
+      arg.num_elem[block_id], arg.recv[block_id], nullptr, arg.send[block_id], arg.dst[block_id]);
 }
 
 template<typename T>
 __global__ void RecvCopyGpu(CudaRingAllReduceArg<T> arg) {
-  const int32_t thread_id = threadIdx.x;
   const int32_t block_id = blockIdx.x;
-  const T* recv = arg.recv[block_id];
-  T* dst = arg.dst[block_id];
-  const int64_t num_elem = arg.num_elem[block_id];
-  static_assert(PACK_SIZE % sizeof(T) == 0, "");
-  const int32_t num_elem_per_line = LINE_SIZE / sizeof(T);
-  const int64_t num_line = num_elem / num_elem_per_line;
-  const int32_t warp_id = thread_id / NUM_THREAD_PER_WARP;
-  const int32_t lane_id = thread_id % NUM_THREAD_PER_WARP;
-  ulong2 line_recv[NUM_PACK_PER_LINE_PER_THREAD];
-  const int32_t offset = warp_id * NUM_PACK_PER_LINE_PER_WARP + lane_id;
-  ulong2* dst_pack_ptr = reinterpret_cast<ulong2*>(dst) + offset;
-  const ulong2* recv_pack_ptr = reinterpret_cast<const ulong2*>(recv) + offset;
-  for (int64_t l = 0; l < num_line; ++l) {
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      FetchFunctor<Pack>()(line_recv[p], recv_pack_ptr + p * NUM_THREAD_PER_WARP);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      StoreFunctor<Pack>()(dst_pack_ptr + p * NUM_THREAD_PER_WARP, line_recv[p]);
-    }
-    dst_pack_ptr += NUM_PACK_PER_LINE;
-    recv_pack_ptr += NUM_PACK_PER_LINE;
-  }
+  AlignedReduceOrCopy<ReduceMethod::kSum, T, true, false, false, true>(
+      arg.num_elem[block_id], arg.recv[block_id], nullptr, nullptr, arg.dst[block_id]);
 }
 
 }  // namespace
