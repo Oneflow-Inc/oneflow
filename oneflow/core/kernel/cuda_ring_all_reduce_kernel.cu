e81f8162a9b4344c7574be3ca5ba20bf2615a718
liujuncheng <liujuncheng1022@gmail.com>
2019-07-25 20:25:08 +0800

    SendGpu

--- oneflow/core/kernel/cuda_ring_all_reduce_kernel.cu
+++ oneflow/core/kernel/cuda_ring_all_reduce_kernel.cu
@@ -155,32 +155,9 @@ __device__ void AlignedReduceOrCopy(const int64_t num_elem, const T* recv, const
 
 template<typename T>
 __global__ void SendGpu(CudaRingAllReduceArg<T> arg) {
-  const int32_t thread_id = threadIdx.x;
   const int32_t block_id = blockIdx.x;
-  T* send = arg.send[block_id];
-  const T* src = arg.src[block_id];
-  const int64_t num_elem = arg.num_elem[block_id];
-  static_assert(PACK_SIZE % sizeof(T) == 0, "");
-  const int32_t num_elem_per_line = LINE_SIZE / sizeof(T);
-  const int64_t num_line = num_elem / num_elem_per_line;
-  const int32_t warp_id = thread_id / NUM_THREAD_PER_WARP;
-  const int32_t lane_id = thread_id % NUM_THREAD_PER_WARP;
-  ulong2 line[NUM_PACK_PER_LINE_PER_THREAD];
-  const int32_t offset = warp_id * NUM_PACK_PER_LINE_PER_WARP + lane_id;
-  ulong2* send_pack_ptr = reinterpret_cast<ulong2*>(send) + offset;
-  const ulong2* src_pack_ptr = reinterpret_cast<const ulong2*>(src) + offset;
-  for (int64_t l = 0; l < num_line; ++l) {
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      line[p] = *(src_pack_ptr + p * NUM_THREAD_PER_WARP);
-    }
-#pragma unroll
-    for (int32_t p = 0; p < NUM_PACK_PER_LINE_PER_THREAD; ++p) {
-      *(send_pack_ptr + p * NUM_THREAD_PER_WARP) = line[p];
-    }
-    send_pack_ptr += NUM_PACK_PER_LINE;
-    src_pack_ptr += NUM_PACK_PER_LINE;
-  }
+  AlignedReduceOrCopy<ReduceMethod::kSum, T, false, true, true, false>(
+      arg.num_elem[block_id], nullptr, arg.src[block_id], arg.send[block_id], nullptr);
 }
 
 template<typename T>
