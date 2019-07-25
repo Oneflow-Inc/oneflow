a6e245fe76c2881b3254dada66d75fcd7177f4f1
liujuncheng <liujuncheng1022@gmail.com>
2019-07-25 11:01:44 +0800

    try 2 block

--- oneflow/core/kernel/cuda_ring_all_reduce_kernel.cpp
+++ oneflow/core/kernel/cuda_ring_all_reduce_kernel.cpp
@@ -14,21 +14,27 @@ void CudaRingAllReduceKernel<T>::Forward(
   const int64_t step_id = other_ctx.second;
   const MultiRingAllReduceKernelConf& conf = kernel_conf().multi_ring_all_reduce_conf();
   CudaRingAllReduceArg<T> arg{};
-  arg.num_rings = 1;
+  arg.num_rings = 2;
   CHECK_GT(arg.num_rings, 0);
   CHECK_LE(arg.num_rings, CUDA_RING_ALL_REDUCE_MAX_NUM_RINGS);
   const Blob* in = BnInOp2Blob("in");
   Blob* out = BnInOp2Blob("out");
   const int32_t num_step = conf.ring_conf(ring_id).step_conf_size();
+  const Range range = Range(conf.ring_conf(ring_id).step_conf(step_id).data_range());
+
+  arg.num_elem[0] = range.size() / 2;
+  arg.num_elem[1] = range.size() / 2;
 
   Blob* send = BnInOp2Blob(GenRepeatedBn("send", ring_id));
   arg.send[0] = send != nullptr ? send->mut_dptr<T>() : nullptr;
+  arg.send[1] = send != nullptr ? send->mut_dptr<T>() + arg.num_elem[0] : nullptr;
   const Blob* recv = BnInOp2Blob(GenRepeatedBn("recv", ring_id));
   arg.recv[0] = recv != nullptr ? recv->dptr<T>() : nullptr;
-  const Range range = Range(conf.ring_conf(ring_id).step_conf(step_id).data_range());
+  arg.recv[1] = recv != nullptr ? recv->dptr<T>() + arg.num_elem[0] : nullptr;
   arg.src[0] = in != nullptr ? in->dptr<T>() + range.begin() : nullptr;
+  arg.src[1] = in != nullptr ? in->dptr<T>() + range.begin() + arg.num_elem[0] : nullptr;
   arg.dst[0] = out != nullptr ? out->mut_dptr<T>() + range.begin() : nullptr;
-  arg.num_elem[0] = range.size();
+  arg.dst[1] = out != nullptr ? out->mut_dptr<T>() + range.begin() + arg.num_elem[0] : nullptr;
 
   if (step_id == 0) {
     CudaRingAllReduceKernelUtil<T>::Send(ctx.device_ctx, arg);
