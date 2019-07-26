7b302484e03afab2a07a17c2f0fb84ee7cf56b63
AHuskie <2101943624@qq.com>
2019-07-26 14:22:27 +0800

    add op matmul and test matmul

---
+++ oneflow/python/test/matmul.py
@@ -0,0 +1,26 @@
+import oneflow as flow
+import numpy as np
+import torch
+
+config = flow.ConfigProtoBuilder()
+config.gpu_device_num(1)
+flow.init(config)
+
+jobs = []
+@flow.append_func_to_list(jobs)
+def MatmulJob(a = flow.val((4, 5)), b = flow.val((5,4))):
+    job_conf = flow.get_cur_job_conf_builder()
+    job_conf.batch_size(5).data_part_num(1).default_data_type(flow.float)
+    return flow.keras.maths.matmul(a, b)
+
+a = np.arange(1,21).reshape((4,5)).astype(np.float32)
+b = np.arange(1,21).reshape((5,4)).astype(np.float32)
+
+with flow.Session(jobs) as sess:
+    x = sess.run(MatmulJob, a, b).get()
+
+y = torch.matmul(torch.Tensor(a), torch.Tensor(b))
+
+result = np.isclose(np.array(x), y.numpy(), rtol=1e-03, atol=1e-05)
+for i in result.ravel():
+    assert i, "the tanh test is wrong!"
