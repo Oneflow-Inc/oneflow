dbf9c68ca460120e9bb295d6ed7446d47c260d8a
AHuskie <2101943624@qq.com>
2019-07-31 20:10:00 +0800

    modified

--- oneflow/python/test/variable.py
+++ oneflow/python/test/variable.py
@@ -5,8 +5,6 @@ config = flow.ConfigProtoBuilder()
 config.gpu_device_num(1)
 flow.init(config)
 
-jobs = []
-@flow.append_func_to_list(jobs)
 def VariableJob():
     job_conf = flow.get_cur_job_conf_builder()
     job_conf.batch_size(1).data_part_num(1).default_data_type(flow.float)
