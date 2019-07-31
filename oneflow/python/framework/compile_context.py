6e3571e07b38ceed83a48a54e4847da114efebc0
lixinqi <lixinqi0703106@163.com>
2019-07-31 13:22:14 +0800

    FixedPlacementScope/DevicePriorPlacementScope

--- oneflow/python/framework/compile_context.py
+++ oneflow/python/framework/compile_context.py
@@ -10,4 +10,4 @@ job_name2output_remote_blobs = {}
 
 def CurJobAddOp(op_conf):
     cur_job.net.op.add().CopyFrom(op_conf)
-    placement_context.CurPlacementGroupAddOpName(op_conf.name)
+    placement_context.CurPlacementGroupAddOpConf(op_conf)
