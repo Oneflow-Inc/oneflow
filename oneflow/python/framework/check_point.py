9adffa74fdeaa5344741fd1e7e6c721c8c7392d1
AHuskie <37968048+AHuskie@users.noreply.github.com>
2019-08-01 16:13:18 +0800

    modify check_point and add test check_point (#1963)

--- oneflow/python/framework/check_point.py
+++ oneflow/python/framework/check_point.py
@@ -8,10 +8,10 @@ class CheckPoint(object):
     def __init__(self):
         pass
 
-    def restore(save_path=None):
+    def restore(self, save_path=None):
         return CheckPointRestoreStatus(save_path)
 
-    def save(file_prefix=None, session = None):
+    def save(self, file_prefix=None, session = None):
         assert file_prefix == None
         if session == None: session = runtime_ctx.default_session
         session.NoReturnRun(_MakeModelSaveJobFunc())
@@ -20,7 +20,7 @@ class CheckPointRestoreStatus(object):
     def __init__(self, save_path):
         pass
 
-    def initialize_or_restore(session = None):
+    def initialize_or_restore(self, session = None):
         if session == None: session = runtime_ctx.default_session
         session.NoReturnRun(_MakeModelInitJobFunc())
 
@@ -28,12 +28,14 @@ def _MakeModelInitJobFunc():
     def ModelInit():
         pass
     ModelInit.__name__ = runtime_ctx.inter_user_job_info.global_model_init_job_name
-    ModelInit.__oneflow_input_remote_blobs__ = None
+    ModelInit.__oneflow_input_remote_blobs__ = ()
+    ModelInit.__oneflow_output_remote_blobs__ = None
     return ModelInit
     
 def _MakeModelSaveJobFunc():
     def ModelSave():
         pass
     ModelSave.__name__ = runtime_ctx.inter_user_job_info.global_model_save_job_name
-    ModelSave.__oneflow_input_remote_blobs__ = None
+    ModelSave.__oneflow_input_remote_blobs__ = ()
+    ModelSave.__oneflow_output_remote_blobs__ = None
     return ModelSave
