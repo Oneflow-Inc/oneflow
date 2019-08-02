753326be491c30379dc919b39b8c47c89986b534
Tsai <caishenghang@oneflow.org>
2019-08-02 11:34:26 +0800

    template for different Ndim

--- oneflow/core/job_completer/conv_autovar.cpp
+++ oneflow/core/job_completer/conv_autovar.cpp
@@ -4,15 +4,52 @@ namespace oneflow {
 
 namespace {
 
-template<int32_t NDims>
+template<typename T>
+struct GetConfHelper {
+  static T* MuatableConvNdConf(OperatorConf* conv_nd_conf);
+  static T ConvNdConf(const OperatorConf& conv_nd_conf);
+};
+
+template<>
+struct GetConfHelper<Conv1DOpConf> {
+  static Conv1DOpConf* MuatableConvNdConf(OperatorConf* conv_nd_conf) {
+    return conv_nd_conf->mutable_conv_1d_conf();
+  }
+  static Conv1DOpConf ConvNdConf(const OperatorConf& conv_nd_conf) {
+    CHECK(conv_nd_conf.has_conv_1d_conf());
+    return conv_nd_conf.conv_1d_conf();
+  }
+};
+
+template<>
+struct GetConfHelper<Conv2DOpConf> {
+  static Conv2DOpConf* MuatableConvNdConf(OperatorConf* conv_nd_conf) {
+    return conv_nd_conf->mutable_conv_2d_conf();
+  }
+  static Conv2DOpConf ConvNdConf(const OperatorConf& conv_nd_conf) {
+    CHECK(conv_nd_conf.has_conv_2d_conf());
+    return conv_nd_conf.conv_2d_conf();
+  }
+};
+
+template<>
+struct GetConfHelper<Conv3DOpConf> {
+  static Conv3DOpConf* MuatableConvNdConf(OperatorConf* conv_nd_conf) {
+    return conv_nd_conf->mutable_conv_3d_conf();
+  }
+  static Conv3DOpConf ConvNdConf(const OperatorConf& conv_nd_conf) {
+    CHECK(conv_nd_conf.has_conv_3d_conf());
+    return conv_nd_conf.conv_3d_conf();
+  }
+};
+
+template<typename T>
 void GenerateInputVarOpConf(
     const Operator& op, std::vector<OperatorConf>* op_confs,
     const std::function<const BlobDesc&(const std::string&)>& BlobDesc4ModelBn) {
-  CHECK(op.op_conf().has_conv_1d_conf() || op.op_conf().has_conv_2d_conf()
-        || op.op_conf().has_conv_3d_conf());
-  OperatorConf conv_2d_conf(op.op_conf());
-  auto* mut_conf = conv_2d_conf.mutable_conv_2d_conf();
-  const Conv2DOpConf& conf = op.op_conf().conv_2d_conf();
+  const T& conf = GetConfHelper<T>::ConvNdConf(op.op_conf());
+  OperatorConf conv_nd_conf(op.op_conf());
+  T* mut_conf = GetConfHelper<T>::MuatableConvNdConf(&conv_nd_conf);
 
   if (!conf.has_weight()) {
     OperatorConf weight_var_op =
@@ -34,13 +71,13 @@ void GenerateInputVarOpConf(
       mut_conf->set_bias(bias_var_op.name() + "/out");
     }
   }
-  op_confs->push_back(conv_2d_conf);
+  op_confs->push_back(conv_nd_conf);
 }
 
 }  // namespace
 
-REGISTER_OP_INPUT_VAR(OperatorConf::kConv1DConf, &GenerateInputVarOpConf<1>);
-REGISTER_OP_INPUT_VAR(OperatorConf::kConv2DConf, &GenerateInputVarOpConf<2>);
-REGISTER_OP_INPUT_VAR(OperatorConf::kConv3DConf, &GenerateInputVarOpConf<3>);
+REGISTER_OP_INPUT_VAR(OperatorConf::kConv1DConf, (&GenerateInputVarOpConf<Conv1DOpConf>));
+REGISTER_OP_INPUT_VAR(OperatorConf::kConv2DConf, (&GenerateInputVarOpConf<Conv2DOpConf>));
+REGISTER_OP_INPUT_VAR(OperatorConf::kConv3DConf, (&GenerateInputVarOpConf<Conv3DOpConf>));
 
 }  // namespace oneflow
