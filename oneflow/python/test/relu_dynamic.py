c28600380b6995933440f66dcae0526814199879
cheng cheng <472491134@qq.com>
2019-08-05 17:29:55 +0800

    refine test file

--- oneflow/python/test/relu_dynamic.py
+++ oneflow/python/test/relu_dynamic.py
@@ -14,15 +14,15 @@ def ReluJob(x = flow.val(shape = (10,), is_dynamic=True)):
     return flow.keras.activations.relu(x)
 flow.add_job(ReluJob)
 
-with flow.Session() as sess:
-    print(sess.run(ReluJob, np.array([-2, 5, -5, 2], dtype=np.float32)).get())
-    print(sess.run(ReluJob, np.array([2, 0, 0, 5, -7, 9], dtype=np.float32)).get())
-    print(sess.run(ReluJob, np.array([1], dtype=np.float32)).get())
+def TestResult(a, b):
+    result = np.isclose(a, b, rtol=1e-03, atol=1e-05)
+    for i in result:
+        assert i, "the test is wrong!"
 
-    '''
-    output:
-      [0. 5. 0. 2.]
-      [2. 0. 0. 5. 0. 9.]
-      [1.]
-    '''
+with flow.Session() as sess:
+    TestResult(sess.run(ReluJob, np.array(range(10), dtype=np.float32)).get(), np.array(range(10), dtype=np.float32))
+    TestResult(sess.run(ReluJob, np.array([-2, 5, -5, 2], dtype=np.float32)).get(), np.array([0, 5, 0, 2], dtype=np.float32))
+    TestResult(sess.run(ReluJob, np.array([2, 0, 0, 5, -7, 9], dtype=np.float32)).get(), np.array([2, 0, 0, 5, 0, 9], dtype=np.float32))
+    TestResult(sess.run(ReluJob, np.array([1], dtype=np.float32)).get(), np.array([1], dtype=np.float32))
+
 
