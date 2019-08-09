d8dadb9833a82daeab0adb28d08d957567fb9667
ShawnXuan <xiexuanx2@gmail.com>
2019-08-09 23:57:43 +0800

    unlock critical sections as more as possible

--- oneflow/core/kernel/reentrant_lock_kernel.cpp
+++ oneflow/core/kernel/reentrant_lock_kernel.cpp
@@ -2,6 +2,8 @@
 
 namespace oneflow {
 
+std::string ReentrantLockStatus::kEmptyIbn = "reentrant_lock_status_empty_ibn";
+
 void ReentrantLockStatus::Init(const KernelConf& kernel_conf) {
   const auto& conf = kernel_conf.op_attribute().op_conf().reentrant_lock_conf();
   cur_ibn_ = "";
@@ -48,27 +50,32 @@ void ReentrantLockStatus::ReleaseLock(int64_t lock_id, std::queue<int64_t>* unlo
   CHECK_GT(total_acquired_lock_num_, 0);
   --lock_id2acquired_num_.at(lock_id);
   --total_acquired_lock_num_;
-  auto ReleaseRelatedLockId = [&](int64_t related_lock_id) {
-    if (lock_id2queued_request_act_id_.at(related_lock_id).empty()) { return; }
-    if (TryAcquireLock(related_lock_id)) { unlocked_ids->push(related_lock_id); }
-  };
-  ReleaseRelatedLockId(lock_id);
-  for (int64_t id : lock_id2intersecting_lock_ids_.at(lock_id)) { ReleaseRelatedLockId(id); }
+  size_t unlocked_cnt = 0;
+  do {
+    unlocked_cnt = 0;
+    auto ReleaseRelatedLockId = [&](int64_t related_lock_id) {
+      if (lock_id2queued_request_act_id_.at(related_lock_id).empty()) { return; }
+      if (TryAcquireLock(related_lock_id)) {
+        unlocked_ids->push(related_lock_id);
+        ++unlocked_cnt;
+      }
+    };
+    ReleaseRelatedLockId(lock_id);
+    for (int64_t id : lock_id2intersecting_lock_ids_.at(lock_id)) { ReleaseRelatedLockId(id); }
+  } while (unlocked_cnt > 0);
 }
 
 template<typename T>
 void ReentrantLockKernel<T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  auto* status = static_cast<ReentrantLockStatus*>(ctx.other);
-  if (status->cur_unlocked_ids().empty()) {
-    if (status->cur_ibn() == "start") {
-      T lock_id = *BnInOp2Blob("start")->dptr<T>();
-      status->RequestLock(lock_id, status->mut_cur_unlocked_ids());
-    } else if (status->cur_ibn() == "end") {
-      status->ReleaseLock(*BnInOp2Blob("end")->dptr<T>(), status->mut_cur_unlocked_ids());
-    } else {
-      UNIMPLEMENTED();
-    }
+  auto* const status = static_cast<ReentrantLockStatus*>(ctx.other);
+  if (status->cur_ibn() == "start") {
+    T lock_id = *BnInOp2Blob("start")->dptr<T>();
+    status->RequestLock(lock_id, status->mut_cur_unlocked_ids());
+  } else if (status->cur_ibn() == "end") {
+    status->ReleaseLock(*BnInOp2Blob("end")->dptr<T>(), status->mut_cur_unlocked_ids());
+  } else {
+    CHECK_EQ(status->cur_ibn(), ReentrantLockStatus::kEmptyIbn);
   }
   if (status->cur_unlocked_ids().size() > 0) {
     T lock_id = status->cur_unlocked_ids().front();
