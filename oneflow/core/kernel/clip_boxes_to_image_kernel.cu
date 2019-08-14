e237d8fd8a8641073170c0c26d29dc1de20b4134
ScXfjiang <sc.xfjiang@gmail.com>
2019-08-14 17:32:14 +0800

    clip_boxes_to_image op

--- oneflow/core/kernel/clip_boxes_to_image_kernel.cu
+++ oneflow/core/kernel/clip_boxes_to_image_kernel.cu
@@ -4,8 +4,21 @@ namespace oneflow {
 
 template<typename T>
 struct ClipBoxesToImageUtil<DeviceType::kGPU, T> {
-  static void Forward() {
-    // TODO
+  static void Forward(DeviceCtx* ctx, const int32_t num_boxes, const T* boxes_ptr,
+                      const int32_t* image_size_ptr, T* out_ptr) {
+    const int32_t image_height = image_size_ptr[0];
+    const int32_t image_width = image_size_ptr[1];
+    const T TO_REMOVE = 1;
+    FOR_RANGE(int32_t, i, 0, num_boxes) {
+      out_ptr[i * 4] =
+          min(max(boxes_ptr[i * 4], ZeroVal<T>::value), static_cast<T>(image_width) - TO_REMOVE);
+      out_ptr[i * 4 + 1] = min(max(boxes_ptr[i * 4 + 1], ZeroVal<T>::value),
+                               static_cast<T>(image_height) - TO_REMOVE);
+      out_ptr[i * 4 + 2] = min(max(boxes_ptr[i * 4 + 2], ZeroVal<T>::value),
+                               static_cast<T>(image_width) - TO_REMOVE);
+      out_ptr[i * 4 + 3] = min(max(boxes_ptr[i * 4 + 3], ZeroVal<T>::value),
+                               static_cast<T>(image_height) - TO_REMOVE);
+    }
   }
 };
 
