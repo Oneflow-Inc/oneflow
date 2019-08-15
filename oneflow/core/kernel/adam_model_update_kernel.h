85588a6cbc16cc7b7a2e71297b04fd6fd5903b50
ShawnXuan <xiexuanx2@gmail.com>
2019-08-15 16:21:45 +0800

    fix adam (#2015)
    * fix adam
    
    * div batch instance num before update model
    
    
    Former-commit-id: 7ddc929feec201a481d807f52846711c6aa589fd

--- oneflow/core/kernel/adam_model_update_kernel.h
+++ oneflow/core/kernel/adam_model_update_kernel.h
@@ -22,10 +22,9 @@ class AdamMdUpdateKernel final : public NormalMdUpdateKernel<device_type, T> {
 template<DeviceType device_type, typename T>
 class AdamMdUpdateKernelUtil final {
  public:
-  static void UpdateModel(DeviceCtx*, int64_t n, const T* batch_instance_num_ptr, T learning_rate,
-                          T l1, T l2, T beta1, T beta2, T epsilon, bool do_bias_correction,
-                          int64_t next_model_vid, const T* beta1_t, const T* beta2_t, T* model_diff,
-                          T* model, T* m, T* v);
+  static void UpdateModel(DeviceCtx*, int64_t n, T learning_rate, T l1, T l2, T beta1, T beta2,
+                          T epsilon, bool do_bias_correction, int64_t next_model_vid,
+                          const T* beta1_t, const T* beta2_t, T* model_diff, T* model, T* m, T* v);
 };
 
 DECLARE_MDUPDT_KERNEL_CREATOR(Adam);
