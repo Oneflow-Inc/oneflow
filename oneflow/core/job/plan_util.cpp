930d1b298bd9bb2f2099c54ee2b741f5be046923
cheng cheng <472491134@qq.com>
2019-08-26 17:46:47 +0800

    reuse plan to dot file

--- oneflow/core/job/plan_util.cpp
+++ oneflow/core/job/plan_util.cpp
@@ -1,5 +1,7 @@
 #include "oneflow/core/job/plan_util.h"
 #include "oneflow/core/common/str_util.h"
+#include "oneflow/core/graph/task_node.h"
+#include "oneflow/core/persistence/tee_persistent_log_stream.h"
 
 namespace oneflow {
 
@@ -23,6 +25,43 @@ std::function<const TaskProto&(int64_t)> PlanUtil::MakeGetterTaskProto4TaskId(co
     task_id2task_proto->emplace(task_proto.task_id(), &task_proto);
   }
   return [task_id2task_proto](int64_t task_id) { return *task_id2task_proto->at(task_id); };
+}
+
+void PlanUtil::ToDotFile(const Plan& plan, const std::string& filepath) {
+  auto log_stream = TeePersistentLogStream::Create(filepath);
+  log_stream << "digraph {\n";
+  HashSet<int64_t> regst_desc_ids;
+  for (const TaskProto& task_proto : plan.task()) {
+    log_stream << "task" << std::to_string(task_proto.task_id()) << "[label=\""
+               << std::to_string(task_proto.task_id()) << "\\n"
+               << std::to_string(task_proto.machine_id()) << ":"
+               << std::to_string(task_proto.thrd_id()) << ":"
+               << std::to_string(task_proto.parallel_ctx().parallel_id())
+               << "\", shape=ellipse, style=\"rounded,filled\", "
+                  "colorscheme=set312, color="
+               << task_type2color.at(task_proto.task_type()) << "];\n";
+    for (const auto& pair : task_proto.produced_regst_desc()) {
+      regst_desc_ids.insert(pair.second.regst_desc_id());
+    }
+  }
+  for (const int64_t regst_task_id : regst_desc_ids) {
+    log_stream << "regst_desc" << std::to_string(regst_task_id) << "[label=\""
+               << std::to_string(regst_task_id) << "\", shape=box];\n";
+  }
+  for (const TaskProto& task_proto : plan.task()) {
+    for (const auto& pair : task_proto.produced_regst_desc()) {
+      log_stream << "task" << std::to_string(task_proto.task_id()) << "->regst_desc"
+                 << std::to_string(pair.second.regst_desc_id()) << "[label=\"" << pair.first
+                 << "\"];\n";
+    }
+    for (const auto& pair : task_proto.consumed_regst_desc_id()) {
+      for (int64_t regst_desc_id : pair.second.regst_desc_id()) {
+        log_stream << "regst_desc" << std::to_string(regst_desc_id) << "->task"
+                   << std::to_string(task_proto.task_id()) << "[label=\"" << pair.first << "\"];\n";
+      }
+    }
+  }
+  log_stream << "}\n";
 }
 
 }  // namespace oneflow
