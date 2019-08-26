0f34141feba1e044871e57627ffdc259da8b855f
liujuncheng <liujuncheng1022@gmail.com>
2019-08-26 17:59:50 +0800

    sc broadcast

--- oneflow/core/kernel/nccl_tuple_broadcast_kernel.cu
+++ oneflow/core/kernel/nccl_tuple_broadcast_kernel.cu
@@ -24,6 +24,19 @@ void NcclTupleBroadcastKernel::VirtualKernelInit(const ParallelContext* ctx) {
 void NcclTupleBroadcastKernel::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const NcclTupleBroadcastOpConf& conf = this->op_conf().nccl_tuple_broadcast_conf();
+  void* ptr = nullptr;
+  int64_t size = 0;
+  for (const std::string& obn : op_attribute().output_bns()) {
+    Blob* out = BnInOp2Blob(obn);
+    if (ptr == nullptr
+        || reinterpret_cast<uintptr_t>(out->dptr()) < reinterpret_cast<uintptr_t>(ptr)) {
+      ptr = out->mut_dptr();
+    }
+    size += out->ByteSizeOfDataContentField();
+  }
+  NcclCheck(ncclBroadcast(ptr, ptr, size, ncclChar, 0, ctx.device_ctx->nccl_handle(),
+                          ctx.device_ctx->cuda_stream()));
+  return;
   NcclCheck(ncclGroupStart());
   FOR_RANGE(int64_t, i, 0, conf.out_size()) {
     const void* send = conf.root(i) == parallel_ctx_.rank_ctx().rank_id()
