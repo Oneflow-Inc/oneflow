5a48e5b1f4dac34c9ac3ca2f9b5b27500461c5d1
ScXfjiang <sc.xfjiang@gmail.com>
2019-08-29 17:07:47 +0800

    final version

---
+++ oneflow/python/ops/pooling.py
@@ -0,0 +1,125 @@
+from __future__ import absolute_import
+
+import oneflow.python.framework.compile_context as compile_context
+import oneflow.python.framework.remote_blob as remote_blob_util
+import oneflow.python.framework.id_util as id_util
+import oneflow.core.operator.op_conf_pb2 as op_conf_util
+import oneflow.core.register.logical_blob_id_pb2 as logical_blob_id_util
+
+from oneflow.python.oneflow_export import oneflow_export
+
+
+@oneflow_export("pooling.max_pool1d")
+def max_pool1d(input, ksize, strides, padding, data_format="NWC"):
+    # TODO: fix cuDNN bugs in pooling_1d
+    raise NotImplementedError
+
+
+@oneflow_export("pooling.avg_pool1d")
+def avg_pool1d(input, ksize, strides, padding, data_format="NWC"):
+    # TODO: fix cuDNN bugs in pooling_1d
+    raise NotImplementedError
+
+
+@oneflow_export("pooling.max_pool2d")
+def max_pool2d(input, ksize, strides, padding, data_format="NHWC", name=None):
+    op_conf = op_conf_util.OperatorConf()
+    setattr(
+        op_conf, "name", name if name is not None else id_util.UniqueStr("MaxPool2D_")
+    )
+    setattr(op_conf.max_pooling_2d_conf, "in", input.logical_blob_name)
+    setattr(op_conf.max_pooling_2d_conf, "out", "out")
+    op_conf.max_pooling_2d_conf.pool_size[:] = list(ksize)
+    op_conf.max_pooling_2d_conf.strides[:] = list(strides)
+    assert padding in ["valid", "same"]
+    setattr(op_conf.max_pooling_2d_conf, "padding", padding)
+    assert data_format in ["NHWC", "NCHW", "NCHW_VECT_C"]
+    setattr(
+        op_conf.max_pooling_2d_conf,
+        "data_format",
+        "channels_last" if data_format is "NHWC" else "channels_first",
+    )
+    compile_context.CurJobAddOp(op_conf)
+    out_lbi = logical_blob_id_util.LogicalBlobId()
+    setattr(out_lbi, "op_name", op_conf.name)
+    setattr(out_lbi, "blob_name", "out")
+    return remote_blob_util.RemoteBlob(out_lbi)
+
+
+@oneflow_export("pooling.avg_pool2d")
+def avg_pool2d(input, ksize, strides, padding, data_format="NHWC", name=None):
+    op_conf = op_conf_util.OperatorConf()
+    setattr(
+        op_conf,
+        "name",
+        name if name is not None else id_util.UniqueStr("AveragePool2D_"),
+    )
+    setattr(op_conf.average_pooling_2d_conf, "in", input.logical_blob_name)
+    setattr(op_conf.average_pooling_2d_conf, "out", "out")
+    op_conf.average_pooling_2d_conf.pool_size[:] = list(ksize)
+    op_conf.average_pooling_2d_conf.strides[:] = list(strides)
+    assert padding in ["valid", "same"]
+    setattr(op_conf.average_pooling_2d_conf, "padding", padding)
+    assert data_format in ["NHWC", "NCHW", "NCHW_VECT_C"]
+    setattr(
+        op_conf.average_pooling_2d_conf,
+        "data_format",
+        "channels_last" if data_format is "NHWC" else "channels_first",
+    )
+    compile_context.CurJobAddOp(op_conf)
+    out_lbi = logical_blob_id_util.LogicalBlobId()
+    setattr(out_lbi, "op_name", op_conf.name)
+    setattr(out_lbi, "blob_name", "out")
+    return remote_blob_util.RemoteBlob(out_lbi)
+
+
+@oneflow_export("pooling.max_pool3d")
+def max_pool3d(input, ksize, strides, padding, data_format="NDHWC", name=None):
+    op_conf = op_conf_util.OperatorConf()
+    setattr(
+        op_conf, "name", name if name is not None else id_util.UniqueStr("MaxPool3D_")
+    )
+    setattr(op_conf.max_pooling_3d_conf, "in", input.logical_blob_name)
+    setattr(op_conf.max_pooling_3d_conf, "out", "out")
+    op_conf.max_pooling_3d_conf.pool_size[:] = list(ksize)
+    op_conf.max_pooling_3d_conf.strides[:] = list(strides)
+    assert padding in ["valid", "same"]
+    setattr(op_conf.max_pooling_3d_conf, "padding", padding)
+    assert data_format in ["NDHWC", "NCDHW"]
+    setattr(
+        op_conf.max_pooling_3d_conf,
+        "data_format",
+        "channels_last" if data_format is "NDHWC" else "channels_first",
+    )
+    compile_context.CurJobAddOp(op_conf)
+    out_lbi = logical_blob_id_util.LogicalBlobId()
+    setattr(out_lbi, "op_name", op_conf.name)
+    setattr(out_lbi, "blob_name", "out")
+    return remote_blob_util.RemoteBlob(out_lbi)
+
+
+@oneflow_export("pooling.avg_pool3d")
+def avg_pool3d(input, ksize, strides, padding, data_format="NDHWC", name=None):
+    op_conf = op_conf_util.OperatorConf()
+    setattr(
+        op_conf,
+        "name",
+        name if name is not None else id_util.UniqueStr("AveragePool3D_"),
+    )
+    setattr(op_conf.average_pooling_3d_conf, "in", input.logical_blob_name)
+    setattr(op_conf.average_pooling_3d_conf, "out", "out")
+    op_conf.average_pooling_3d_conf.pool_size[:] = list(ksize)
+    op_conf.average_pooling_3d_conf.strides[:] = list(strides)
+    assert padding in ["valid", "same"]
+    setattr(op_conf.average_pooling_3d_conf, "padding", padding)
+    assert data_format in ["NDHWC", "NCDHW"]
+    setattr(
+        op_conf.average_pooling_3d_conf,
+        "data_format",
+        "channels_last" if data_format is "NDHWC" else "channels_first",
+    )
+    compile_context.CurJobAddOp(op_conf)
+    out_lbi = logical_blob_id_util.LogicalBlobId()
+    setattr(out_lbi, "op_name", op_conf.name)
+    setattr(out_lbi, "blob_name", "out")
+    return remote_blob_util.RemoteBlob(out_lbi)
--- oneflow/python/test/pooling_demo.py
+++ oneflow/python/test/pooling_demo.py
@@ -6,78 +6,145 @@ tf.enable_eager_execution()
 assert tf.executing_eagerly()
 
 
-def test_max_pool_2d():
-    x = np.random.random_sample((10, 1000, 1000, 3)).astype(np.float32)
+def test_max_pool2d():
+    input = np.random.random_sample((10, 1000, 1000, 3)).astype(np.float32)
 
     # OneFlow
     config = flow.ConfigProtoBuilder()
     config.gpu_device_num(1)
     flow.init(config)
 
-    def MaxPool2DTestJob(x=flow.input_blob_def((10, 1000, 1000, 3))):
+    def MaxPool2DTestJob(input=flow.input_blob_def((10, 1000, 1000, 3))):
         job_conf = flow.get_cur_job_conf_builder()
         job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
-        return flow.keras.pooling.max_pool_2d(
-            x,
-            pool_size=[10, 10],
-            strides=[10, 10],
-            padding="valid",
-            data_format="channels_last",
+        return flow.pooling.max_pool2d(
+            input, ksize=[10, 10], strides=[10, 10], padding="valid", data_format="NHWC"
         )
 
     flow.add_job(MaxPool2DTestJob)
     with flow.Session() as sess:
-        of_out = sess.run(MaxPool2DTestJob, x).get()
+        of_out = sess.run(MaxPool2DTestJob, input).get()
 
     # TensorFlow
-    tf_max_pooling_2d = tf.keras.layers.MaxPooling2D(
+    tf_max_pooling_2d = tf.layers.MaxPooling2D(
         pool_size=[10, 10],
         strides=[10, 10],
         padding="valid",
         data_format="channels_last",
     )
-    tf_out = tf_max_pooling_2d(tf.Variable(x)).numpy()
+    tf_out = tf_max_pooling_2d(tf.Variable(input)).numpy()
 
     max_diff = np.max(np.abs(of_out - tf_out))
     print("MaxPool2D max diff: " + str(max_diff))
 
 
-def test_average_pool_2d():
-    x = np.random.random_sample((10, 1000, 1000, 3)).astype(np.float32)
+def test_avg_pool2d():
+    input = np.random.random_sample((10, 1000, 1000, 3)).astype(np.float32)
 
     # OneFlow
     config = flow.ConfigProtoBuilder()
     config.gpu_device_num(1)
     flow.init(config)
 
-    def AveragePool2DTestJob(x=flow.input_blob_def((10, 1000, 1000, 3))):
+    def AveragePool2DTestJob(input=flow.input_blob_def((10, 1000, 1000, 3))):
         job_conf = flow.get_cur_job_conf_builder()
         job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
-        return flow.keras.pooling.average_pool_2d(
-            x,
-            pool_size=[10, 10],
-            strides=[10, 10],
-            padding="valid",
-            data_format="channels_last",
+        return flow.pooling.avg_pool2d(
+            input, ksize=[10, 10], strides=[10, 10], padding="valid", data_format="NHWC"
         )
 
     flow.add_job(AveragePool2DTestJob)
     with flow.Session() as sess:
-        of_out = sess.run(AveragePool2DTestJob, x).get()
+        of_out = sess.run(AveragePool2DTestJob, input).get()
 
     # TensorFlow
-    tf_average_pooling_2d = tf.keras.layers.AveragePooling2D(
+    tf_average_pooling_2d = tf.layers.AveragePooling2D(
         pool_size=[10, 10],
         strides=[10, 10],
         padding="valid",
         data_format="channels_last",
     )
-    tf_out = tf_average_pooling_2d(tf.Variable(x)).numpy()
+    tf_out = tf_average_pooling_2d(tf.Variable(input)).numpy()
 
     max_diff = np.max(np.abs(of_out - tf_out))
     print("AveragePool2D max diff: " + str(max_diff))
 
 
+def test_max_pool3d():
+    input = np.random.random_sample((10, 100, 100, 100, 3)).astype(np.float32)
+
+    # OneFlow
+    config = flow.ConfigProtoBuilder()
+    config.gpu_device_num(1)
+    flow.init(config)
+
+    def MaxPool3DTestJob(input=flow.input_blob_def((10, 100, 100, 100, 3))):
+        job_conf = flow.get_cur_job_conf_builder()
+        job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
+        return flow.pooling.max_pool3d(
+            input,
+            ksize=[10, 10, 10],
+            strides=[10, 10, 10],
+            padding="valid",
+            data_format="NDHWC",
+        )
+
+    flow.add_job(MaxPool3DTestJob)
+    with flow.Session() as sess:
+        of_out = sess.run(MaxPool3DTestJob, input).get()
+
+    # TensorFlow
+    tf_max_pooling_3d = tf.layers.MaxPooling3D(
+        pool_size=[10, 10, 10],
+        strides=[10, 10, 10],
+        padding="valid",
+        data_format="channels_last",
+    )
+    tf_out = tf_max_pooling_3d(tf.Variable(input)).numpy()
+
+    max_diff = np.max(np.abs(of_out - tf_out))
+    print("MaxPool3D max diff: " + str(max_diff))
+
+
+def test_avg_pool3d():
+    input = np.random.random_sample((10, 100, 100, 100, 3)).astype(np.float32)
+
+    # OneFlow
+    config = flow.ConfigProtoBuilder()
+    config.gpu_device_num(1)
+    flow.init(config)
+
+    def AveragePool3DTestJob(input=flow.input_blob_def((10, 100, 100, 100, 3))):
+        job_conf = flow.get_cur_job_conf_builder()
+        job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
+        return flow.pooling.avg_pool3d(
+            input,
+            ksize=[10, 10, 10],
+            strides=[10, 10, 10],
+            padding="valid",
+            data_format="NDHWC",
+        )
+
+    flow.add_job(AveragePool3DTestJob)
+    with flow.Session() as sess:
+        of_out = sess.run(AveragePool3DTestJob, input).get()
+
+    # TensorFlow
+    tf_average_pooling_3d = tf.layers.AveragePooling3D(
+        pool_size=[10, 10, 10],
+        strides=[10, 10, 10],
+        padding="valid",
+        data_format="channels_last",
+    )
+    tf_out = tf_average_pooling_3d(tf.Variable(input)).numpy()
+
+    max_diff = np.max(np.abs(of_out - tf_out))
+    print("AveragePool3D max diff: " + str(max_diff))
+
+
+# run one example each time
 if __name__ == "__main__":
-    test_max_pool_2d()
-    test_average_pool_2d()
+    test_max_pool2d()
+    # test_avg_pool2d()
+    # test_max_pool3d()
+    # test_avg_pool3d()
