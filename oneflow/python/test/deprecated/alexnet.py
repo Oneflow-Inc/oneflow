74982dc8677288a9aad8d4ceb34d3176172d7cb6
jackalcooper <jackalcooper@gmail.com>
2019-08-29 17:22:02 +0800

    fix data dir (#2054)
    * fix data dir
    
    * rm model load path

--- oneflow/python/test/deprecated/alexnet.py
+++ oneflow/python/test/deprecated/alexnet.py
@@ -7,8 +7,6 @@ from datetime import datetime
 import argparse
 
 _DATA_DIR = "/dataset/imagenet_227/train/32"
-_SINGLE_PIC_DATA_DIR = '/home/caishenghang/dev/cnns_test/dataset/PNG227/of_record'
-_MODEL_LOAD_DIR = '/home/caishenghang/dev/cnns_test/alexnet_fp16/fp32/of_model'
 _MODEL_SAVE_DIR = "./model_save-{}".format(
     str(datetime.now().strftime('%Y-%m-%d-%H:%M:%S')))
 
@@ -29,9 +27,9 @@ parser.add_argument('-r', '--remote_by_hand', default=False,
 parser.add_argument('-e', '--eval_dir',
                     type=str, default=_DATA_DIR, required=False)
 parser.add_argument('-t', '--train_dir',
-                    type=str, default=_SINGLE_PIC_DATA_DIR, required=False)
+                    type=str, default=_DATA_DIR, required=False)
 parser.add_argument('-load', '--model_load_dir',
-                    type=str, default=_MODEL_LOAD_DIR, required=False)
+                    type=str, default="", required=False)
 parser.add_argument('-save', '--model_save_dir',
                     type=str, default=_MODEL_SAVE_DIR, required=False)
 
