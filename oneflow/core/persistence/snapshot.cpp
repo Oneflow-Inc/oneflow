64b1f7858a260350e421abee343fc98fdafcbe24
liujuncheng <liujuncheng1022@gmail.com>
2019-08-29 20:45:36 +0800

    refine snapshot

--- oneflow/core/persistence/snapshot.cpp
+++ oneflow/core/persistence/snapshot.cpp
@@ -1,5 +1,4 @@
 #include "oneflow/core/common/str_util.h"
-#include "oneflow/core/job/resource_desc.h"
 #include "oneflow/core/operator/operator.h"
 #include "oneflow/core/persistence/snapshot_manager.h"
 
@@ -11,14 +10,13 @@ Snapshot::Snapshot(const std::string& snapshot_root_path) : root_path_(snapshot_
 
 std::unique_ptr<PersistentOutStream> Snapshot::GetOutStream(const LogicalBlobId& lbi) {
   const std::string op_name_dir = JoinPath(root_path_, lbi.op_name());
-  OfCallOnce(op_name_dir, SnapshotFS(), &fs::FileSystem::CreateDir);
+  SnapshotFS()->CreateDir(op_name_dir);
   return std::make_unique<PersistentOutStream>(SnapshotFS(),
                                                JoinPath(op_name_dir, lbi.blob_name()));
 }
 
 void Snapshot::Done() {
-  const std::string snapshot_done_path = JoinPath(root_path_, "snapshot_done");
-  PersistentOutStream out_stream(SnapshotFS(), snapshot_done_path);
+  PersistentOutStream out_stream(SnapshotFS(), JoinPath(root_path_, "snapshot_done"));
 }
 
 }  // namespace oneflow
