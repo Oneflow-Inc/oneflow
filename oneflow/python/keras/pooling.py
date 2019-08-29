ae9ecf60f4d949d76fa5d395a72c691cc68813b7
ScXfjiang <sc.xfjiang@gmail.com>
2019-08-29 13:47:57 +0800

    correct average_pool_2d

--- oneflow/python/keras/pooling.py
+++ oneflow/python/keras/pooling.py
@@ -10,7 +10,9 @@ from oneflow.python.oneflow_export import oneflow_export
 
 
 @oneflow_export("keras.pooling.max_pool_2d")
-def max_pool_2d(x, pool_size, strides=None, padding="valid", data_format="channels_last"):
+def max_pool_2d(
+    x, pool_size, strides=None, padding="valid", data_format="channels_last"
+):
     op_conf = op_conf_util.OperatorConf()
     setattr(op_conf, "name", id_util.UniqueStr("MaxPool2D_"))
     setattr(op_conf.max_pooling_2d_conf, "in", x.logical_blob_name)
@@ -30,6 +32,29 @@ def max_pool_2d(x, pool_size, strides=None, padding="valid", data_format="channe
     setattr(out_lbi, "blob_name", "out")
     return remote_blob_util.RemoteBlob(out_lbi)
 
+
+@oneflow_export("keras.pooling.average_pool_2d")
+def average_pool_2d(
+    x, pool_size, strides=None, padding="valid", data_format="channels_last"
+):
+    op_conf = op_conf_util.OperatorConf()
+    setattr(op_conf, "name", id_util.UniqueStr("AveragePool2D_"))
+    setattr(op_conf.average_pooling_2d_conf, "in", x.logical_blob_name)
+    setattr(op_conf.average_pooling_2d_conf, "out", "out")
+    assert isinstance(pool_size, list)
+    op_conf.average_pooling_2d_conf.pool_size[:] = pool_size
+    if strides == None:
+        op_conf.average_pooling_2d_conf.strides[:] = pool_size
+    else:
+        assert isinstance(strides, list)
+        op_conf.average_pooling_2d_conf.strides[:] = strides
+    setattr(op_conf.average_pooling_2d_conf, "padding", padding)
+    setattr(op_conf.average_pooling_2d_conf, "data_format", data_format)
+    compile_context.CurJobAddOp(op_conf)
+    out_lbi = logical_blob_id_util.LogicalBlobId()
+    setattr(out_lbi, "op_name", op_conf.name)
+    setattr(out_lbi, "blob_name", "out")
+    return remote_blob_util.RemoteBlob(out_lbi)
 
 
 # @oneflow_export("keras.layers.MaxPool1D")
--- oneflow/python/test/pooling_demo.py
+++ oneflow/python/test/pooling_demo.py
@@ -5,6 +5,7 @@ import numpy as np
 tf.enable_eager_execution()
 assert tf.executing_eagerly()
 
+
 def test_max_pool_2d():
     x = np.random.random_sample((10, 1000, 1000, 3)).astype(np.float32)
 
@@ -12,6 +13,7 @@ def test_max_pool_2d():
     config = flow.ConfigProtoBuilder()
     config.gpu_device_num(1)
     flow.init(config)
+
     def MaxPool2DTestJob(x=flow.input_blob_def((10, 1000, 1000, 3))):
         job_conf = flow.get_cur_job_conf_builder()
         job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
@@ -22,18 +24,60 @@ def test_max_pool_2d():
             padding="valid",
             data_format="channels_last",
         )
+
     flow.add_job(MaxPool2DTestJob)
     with flow.Session() as sess:
         of_out = sess.run(MaxPool2DTestJob, x).get()
-    
+
     # TensorFlow
     tf_max_pooling_2d = tf.keras.layers.MaxPooling2D(
-        pool_size=[10, 10], strides=[10, 10], padding="valid", data_format="channels_last"
+        pool_size=[10, 10],
+        strides=[10, 10],
+        padding="valid",
+        data_format="channels_last",
     )
     tf_out = tf_max_pooling_2d(tf.Variable(x)).numpy()
 
     max_diff = np.max(np.abs(of_out - tf_out))
     print("MaxPool2D max diff: " + str(max_diff))
 
+
+def test_average_pool_2d():
+    x = np.random.random_sample((10, 1000, 1000, 3)).astype(np.float32)
+
+    # OneFlow
+    config = flow.ConfigProtoBuilder()
+    config.gpu_device_num(1)
+    flow.init(config)
+
+    def AveragePool2DTestJob(x=flow.input_blob_def((10, 1000, 1000, 3))):
+        job_conf = flow.get_cur_job_conf_builder()
+        job_conf.batch_size(10).data_part_num(1).default_data_type(flow.float)
+        return flow.keras.pooling.average_pool_2d(
+            x,
+            pool_size=[10, 10],
+            strides=[10, 10],
+            padding="valid",
+            data_format="channels_last",
+        )
+
+    flow.add_job(AveragePool2DTestJob)
+    with flow.Session() as sess:
+        of_out = sess.run(AveragePool2DTestJob, x).get()
+
+    # TensorFlow
+    tf_average_pooling_2d = tf.keras.layers.AveragePooling2D(
+        pool_size=[10, 10],
+        strides=[10, 10],
+        padding="valid",
+        data_format="channels_last",
+    )
+    tf_out = tf_average_pooling_2d(tf.Variable(x)).numpy()
+
+    max_diff = np.max(np.abs(of_out - tf_out))
+    print("AveragePool2D max diff: " + str(max_diff))
+
+
 if __name__ == "__main__":
     test_max_pool_2d()
+    test_average_pool_2d()
