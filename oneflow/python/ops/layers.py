b1fe1f6c5f538bb4747fdcfe13f7152a45e236f7
ScXfjiang <sc.xfjiang@gmail.com>
2019-08-30 21:55:11 +0800

    matmul

---
+++ oneflow/python/ops/layers.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+
+import oneflow.python.framework.compile_context as compile_context
+import oneflow.python.framework.remote_blob as remote_blob_util
+import oneflow.python.framework.id_util as id_util
+import oneflow.core.operator.op_conf_pb2 as op_conf_util
+import oneflow.core.register.logical_blob_id_pb2 as logical_blob_id_util
+from oneflow.python.oneflow_export import oneflow_export
+
+
+@oneflow_export("layers.Dense")
+class Dense(object):
+    def __init__(
+        self,
+        units,
+        activation=None,
+        use_bias=True,
+        kernel_initializer=None,
+        bias_initializer=None,
+        kernel_regularizer=None,
+        bias_regularizer=None,
+        activity_regularizer=None,
+        kernel_constraint=None,
+        bias_constraint=None,
+        trainable=True,
+        name=None,
+        **kwargs
+    ):
+        # TODO
+        pass
+
+    def call(self, inputs):
+        # TODO
+        pass
