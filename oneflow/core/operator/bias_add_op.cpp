a9aec0fae43d88e08becdae38c39ca49c1779f85
Shenghang Tsai <jackalcooper@gmail.com>
2019-09-01 16:48:32 +0800

    Revert "Add axis conf to bias_add for any axis channel (#2087)" (#2091)
    This reverts commit 8679ce980ce8570bf927baeab8616ee7b93fac47.

--- oneflow/core/operator/bias_add_op.cpp
+++ oneflow/core/operator/bias_add_op.cpp
@@ -18,18 +18,15 @@ void BiasAddOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetB
                                const ParallelContext* parallel_ctx) const {
   const BlobDesc* a_blob_desc = GetBlobDesc4BnInOp("a");
   const BlobDesc* b_blob_desc = GetBlobDesc4BnInOp("b");
-  const int32_t bias_add_axis = op_conf().bias_add_conf().axis();
 
+  CHECK_EQ(a_blob_desc->shape().NumAxes(), 2);
   CHECK_EQ(b_blob_desc->shape().NumAxes(), 1);
-  CHECK_GE(bias_add_axis, 0);
-  CHECK_LT(bias_add_axis, a_blob_desc->shape().NumAxes());
-  CHECK_EQ(a_blob_desc->shape().At(bias_add_axis), b_blob_desc->shape().At(0));
+  CHECK_EQ(a_blob_desc->shape().At(1), b_blob_desc->shape().At(0));
 
   *GetBlobDesc4BnInOp("out") = *a_blob_desc;
   *GetBlobDesc4BnInOp("bias_multiplier") = *a_blob_desc;
   GetBlobDesc4BnInOp("bias_multiplier")->mut_shape() = Shape({a_blob_desc->shape().At(0), 1});
 }
-
 void BiasAddOp::GetSbpSignatures(
     const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4Ibn,
     SbpSignatureList* sbp_sig_list) const {
