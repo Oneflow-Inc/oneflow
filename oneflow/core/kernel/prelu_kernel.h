e0a2a6df490e253d9f84dffdb74f1d0d7674f9ed
guo ran <360112263@qq.com>
2019-09-01 11:22:27 +0800

    prelu (#2086)
    * prelu
    
    * fix
    
    * fix

--- oneflow/core/kernel/prelu_kernel.h
+++ oneflow/core/kernel/prelu_kernel.h
@@ -15,18 +15,12 @@ class PReluKernel final : public KernelIf<device_type> {
  private:
   void ForwardDataContent(const KernelCtx&,
                           std::function<Blob*(const std::string&)>) const override;
-  void BackwardDataContent(const KernelCtx&,
-                           std::function<Blob*(const std::string&)>) const override;
 };
 
 template<DeviceType device_type, typename T>
 struct PReluKernelUtil {
   static void Forward(const KernelCtx& ctx, const PReluOpConf& conf, const Blob* in_blob,
                       const Blob* alpha_blob, Blob* out_blob);
-  static void Backward(const KernelCtx& ctx, const PReluOpConf& conf,
-                       const PbRf<int32_t>& permutation, const Blob* in_blob,
-                       const Blob* alpha_blob, const Blob* out_diff_blob, Blob* bw_buf_blob,
-                       Blob* in_diff_blob, Blob* alpha_diff_blob);
 };
 
 }  // namespace oneflow
