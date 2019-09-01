2021b9fee48e689f1fbb03649d7bd2ee7a7a0b93
Tsai <caishenghang@oneflow.org>
2019-09-01 17:20:24 +0800

    fix default data type

--- oneflow/core/job_completer/set_default_variable_conf.cpp
+++ oneflow/core/job_completer/set_default_variable_conf.cpp
@@ -9,8 +9,12 @@ void SetDefaultVariableConf(const OpGraph& op_graph, JobBuilder* job_builder) {
   op_graph.ForEachNode([&](OpNode* op_node) {
     if (op_node->op().op_conf().has_variable_conf()) {
       OperatorConf variable_op_conf(op_node->op().op_conf());
-      variable_op_conf.mutable_variable_conf()->set_data_type(
-          job_builder->job().job_conf().default_data_type());
+      CHECK(job_builder->job().job_conf().has_default_data_type()
+            || variable_op_conf.variable_conf().has_data_type());
+      if (variable_op_conf.variable_conf().has_data_type() == false) {
+        variable_op_conf.mutable_variable_conf()->set_data_type(
+            job_builder->job().job_conf().default_data_type());
+      }
       CHECK(job_builder->job().job_conf().has_default_initializer_conf()
             || variable_op_conf.variable_conf().has_initializer());
       if (variable_op_conf.variable_conf().has_initializer() == false) {
