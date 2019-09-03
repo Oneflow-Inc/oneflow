06f48a25cafec141ebf44d90b7214140704a6a6d
liujuncheng <liujuncheng1022@gmail.com>
2019-09-03 11:13:26 +0800

    ModelSaveKernel

--- oneflow/core/kernel/model_save_kernel.cpp
+++ oneflow/core/kernel/model_save_kernel.cpp
@@ -30,12 +30,13 @@ class ModelSaveKernel final : public KernelIf<DeviceType::kCPU> {
 
 void ModelSaveKernel::Forward(const KernelCtx& ctx,
                               std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const ModelSaveOpConf& conf = this->op_conf().model_save_conf();
   const Blob* path_blob = BnInOp2Blob("path");
   const std::string path(path_blob->dptr<char>(), path_blob->shape().elem_cnt());
   SnapshotWriter writer(path);
-  FOR_RANGE(int64_t, i, 0, op_attribute().input_bns().size()) {
+  FOR_RANGE(int64_t, i, 0, conf.in_size()) {
     const Blob* in_i = BnInOp2Blob(GenRepeatedBn("in", i));
-    writer.Write(op_conf().model_save_conf().key(i), in_i);
+    writer.Write(conf.key(i), in_i);
   }
   writer.Close();
 }
