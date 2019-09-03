d1d35a0bd99226ec13eb6e2f4f10c4bc97da8b9b
leaves-zwx <kunta0932@gmail.com>
2019-09-03 16:05:57 +0800

    minor

--- oneflow/python/test/deprecated/variable_scope.py
+++ oneflow/python/test/deprecated/variable_scope.py
@@ -6,13 +6,13 @@ config.gpu_device_num(1)
 of.init(config)
 
 
-def variable_scope_test_job_1(a=of.input_blob_def((1, 3, 2, 2))):
+def variable_scope_test_job_1(a=of.input_blob_def((1, 3, 6, 6))):
     job_conf = of.get_cur_job_conf_builder()
     job_conf.batch_size(1).data_part_num(1).default_data_type(of.float)
     with of.deprecated.variable_scope("job1_scope1"):
         convw = of.get_variable(
             "conv_weight",
-            shape=(5, 3, 2, 2),
+            shape=(5, 3, 3, 3),
             dtype=a.dtype,
             initializer=of.random_uniform_initializer(),
             trainable=True,
@@ -22,12 +22,14 @@ def variable_scope_test_job_1(a=of.input_blob_def((1, 3, 2, 2))):
         with of.deprecated.variable_scope("job1_scope2"):
             fcw = of.get_variable(
                 "fc_weight",
-                shape=(20, 10),
+                shape=(180, 10),
                 dtype=a.dtype,
                 initializer=of.random_uniform_initializer(),
                 trainable=True,
             )
-            fc = of.matmul(of.reshape(conv, (1, -1)), fcw, name="fc")
+            fc = of.matmul(
+                of.reshape(conv, (conv.static_shape[0], -1)), fcw, name="fc"
+            )
             fcb = of.get_variable(
                 "fc_bias",
                 shape=(10,),
@@ -37,7 +39,7 @@ def variable_scope_test_job_1(a=of.input_blob_def((1, 3, 2, 2))):
             )
             fc_bias = of.nn.bias_add(fc, fcb)
 
-            return fc_bias
+    return fc_bias
 
 
 def variable_scope_test_job_2(a=of.input_blob_def((2, 5))):
@@ -60,9 +62,15 @@ of.add_job(variable_scope_test_job_2)
 
 
 with of.Session() as sess:
-    a1 = np.random.rand(1, 3, 2, 2).astype(np.float32)
+    a1 = np.random.rand(1, 3, 6, 6).astype(np.float32)
     a2 = np.arange(10, dtype=np.float32).reshape(2, 5)
     ret1 = sess.run(variable_scope_test_job_1, a1).get()
     ret2 = sess.run(variable_scope_test_job_2, a2).get()
+
+    print("Job1 result: ")
     print(ret1)
-    print(ret1)
+    print("shape: ", ret1.shape)
+    print("\n")
+    print("Job2 result: ")
+    print(ret2)
+    print("shape: ", ret2.shape)
