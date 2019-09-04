0f1b8efdfe72284294de7709ae4d67f8aa8500f8
ScXfjiang <sc.xfjiang@gmail.com>
2019-09-04 15:44:01 +0800

    update dense

--- oneflow/python/ops/layers.py
+++ oneflow/python/ops/layers.py
@@ -39,7 +39,7 @@ def dense(
         ),
         trainable=trainable,
         model_name="weight",
-        model_split_axis=None,
+        split_axis=None,
     )
     out = flow.matmul(
         a=inputs,
@@ -59,7 +59,7 @@ def dense(
             ),
             trainable=trainable,
             model_name="bias",
-            model_split_axis=None,
+            split_axis=None,
         )
         out = flow.nn.bias_add(out, bias, name="{}_bias_add".format(name_prefix))
     out = activation(out) if activation is not None else out
