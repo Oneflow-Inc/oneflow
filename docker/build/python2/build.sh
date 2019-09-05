ad65625d70d6baefbccc310e11d10600c93ccef5
nlqq <snow3s@qq.com>
2019-09-05 20:17:22 +0800

    add ARG CUDA and USE_PYTHON_3_OR_2

--- docker/build/python2/build.sh
+++ docker/build/python2/build.sh
@@ -1,1 +1,4 @@
-docker build . --rm -t oneflow-build -f docker/build/python2/Dockerfile
+docker build \
+  --build-arg USE_PYTHON_3_OR_2=2 \
+  --build-arg CUDA=9 \
+  -t oneflow-build -f docker/build/python2/Dockerfile .
