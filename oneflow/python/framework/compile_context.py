55a8a9f6c671d2205af15cec81c8dadd28a82d1a
leaves-zwx <kunta0932@gmail.com>
2019-09-05 16:43:42 +0800

    add check

--- oneflow/python/framework/compile_context.py
+++ oneflow/python/framework/compile_context.py
@@ -4,13 +4,11 @@ import oneflow.python.framework.placement_context as placement_context
 import oneflow.python.framework.job_builder as job_builder
 from contextlib import contextmanager
 
-
 @contextmanager
 def CurJob(job):
     _ResetCurJob(job)
     yield None
     _ResetCurJob(None)
-
 
 class BeforeNonInputOpBuildAndInferHook:
     def __init__(self, hook):
@@ -26,14 +24,9 @@ class BeforeNonInputOpBuildAndInferHook:
 def CurJobAddOp(op_conf): return _CurJobAddNonInputOp(op_conf)
 
 def CurJobAddInputOp(op_conf):
-    op_conf.device_type = placement_context.CurPlacementGroupGetDeviceType(
-        op_conf
-    )
-    job_builder.CurCtxAddAndInferOp(
-        op_conf, placement_context.ParallelConf4OpConf(op_conf)
-    )
+    op_conf.device_type = placement_context.CurPlacementGroupGetDeviceType(op_conf)
+    job_builder.CurCtxAddAndInferOp(op_conf, placement_context.ParallelConf4OpConf(op_conf))
     placement_context.CurPlacementGroupAddOpConf(op_conf)
-
 
 def _CurJobAddNonInputOp(op_conf):
     _prefixing_op_name_if_need(op_conf)
@@ -42,13 +35,13 @@ def _CurJobAddNonInputOp(op_conf):
     job_builder.CurCtxAddAndInferOp(op_conf, placement_context.ParallelConf4OpConf(op_conf))
     placement_context.CurPlacementGroupAddOpConf(op_conf)
 
-
 def _ResetCurJob(job):
     global cur_job
     cur_job = job
     global cur_job_var_op_name2var_blob
     cur_job_var_op_name2var_blob = {}
     global cur_job_variable_scope_stack
+    assert len(cur_job_variable_scope_stack) == 0
     cur_job_variable_scope_stack = []
 
 
