3065e828a449e95d802323d3db1ab33956974ebb
ShawnXuan <xiexuanx2@gmail.com>
2019-09-06 21:20:33 +0800

    update for bert

--- oneflow/python/model/bert/run_pretraining.py
+++ oneflow/python/model/bert/run_pretraining.py
@@ -28,7 +28,7 @@ parser.add_argument("-save", "--model_save_dir", type=str, default=_MODEL_SAVE_D
 parser.add_argument('--save_checkpoints_steps', default=10000, type=int)
 args = parser.parse_args()
 
-nodes = [{'addr':'192.168.1.16'},{'addr':'192.168.1.15'}]
+nodes = [{'addr':'192.168.1.15'},{'addr':'192.168.1.15'}]
 
 def _blob_conf(name, shape, dtype=flow.int32):
   return flow.data.BlobConf(name=name, shape=shape, dtype=dtype, codec=flow.data.RawCodec())
@@ -105,26 +105,22 @@ _BERT_MODEL_UPDATE_CONF = dict(
   ),
 )
 
+@flow.function
 def PretrainJob():
-    total_device_num = args.node_num * args.device_num_per_node
-    batch_size = total_device_num * args.batch_size_per_device
-    data_part_num = total_device_num #use total_device_num for test
+  total_device_num = args.node_num * args.device_num_per_node
+  batch_size = total_device_num * args.batch_size_per_device
+  data_part_num = total_device_num #use total_device_num for test
 
-    job_conf = flow.get_cur_job_conf_builder()
-    job_conf.batch_size(batch_size).default_data_type(flow.float)
-    job_conf.default_initializer_conf(dict(constant_conf=dict(value=0.0)))
-    #job_conf.enable_nccl(False)
-    #job_conf.enable_cuda_ring_all_reduce()
-    job_conf.train_conf()
-    job_conf.train_conf().batch_size = batch_size
-    job_conf.train_conf().primary_lr = 1e-4
-    job_conf.train_conf().weight_l2 = 0.01
-    job_conf.model_update_conf(_BERT_MODEL_UPDATE_CONF)
+  flow.config.piece_size(batch_size)
+  flow.config.train.batch_size(batch_size)
+  #flow.config.default_initializer_conf(dict(constant_conf=dict(value=0.0)))
+  flow.config.train.primary_lr(1e-4)
+  flow.config.train.model_update_conf(_BERT_MODEL_UPDATE_CONF)
+  flow.config.train.weight_l2(0.01)
 
-    job_conf.enable_inplace(False)
-    loss = BuildPreTrainNet(data_part_num, hidden_dropout_prob=0, attention_probs_dropout_prob=0)
-    flow.losses.add_loss(loss)
-    return loss
+  loss = BuildPreTrainNet(data_part_num, hidden_dropout_prob=0, attention_probs_dropout_prob=0)
+  flow.losses.add_loss(loss)
+  return loss
 
 cur_step = 0
 def AsyncGetCallback(result):
@@ -134,54 +130,48 @@ def AsyncGetCallback(result):
 
 if __name__ == '__main__':
   for arg in vars(args):
-    print arg, getattr(args, arg)
+    print('{} = {}'.format(arg, getattr(args, arg)))
 
   start_time = time.time()
-  config = flow.ConfigProtoBuilder()
-  config.gpu_device_num(args.device_num_per_node)
-  config.grpc_use_no_signal()
-  config.ctrl_port(9917)
-  config.data_port(9927)
-  config.machine(nodes[:args.node_num])
+  flow.config.gpu_device_num(args.device_num_per_node)
+  flow.config.ctrl_port(9788)
+  flow.config.data_port(9789)
+  flow.config.default_data_type(flow.float)
+  flow.config.machine(nodes[:args.node_num])
+  flow.config.enable_inplace(False)
 
   assert args.node_num <= len(nodes)
   if args.node_num > 1:
     flow.deprecated.init_worker(config, scp_binary=args.copy_binary_to_worker,
                                 use_uuid=args.use_uuid)
-  flow.init(config)
+  check_point = flow.train.CheckPoint()
+  if args.model_load_dir != '':
+    assert os.path.isdir(args.model_load_dir)
+    check_point.load(args.model_load_dir)
+    print('init model from {}'.format(args.model_load_dir))
+  else:
+    check_point.init()
+    print('init model on demand')
 
-  flow.add_job(PretrainJob)
-  with flow.Session() as sess:
-    check_point = flow.train.CheckPoint()
-    if args.model_load_dir != '':
-      assert os.path.isdir(args.model_load_dir)
-      check_point.load(args.model_load_dir)
-      print('init model from {}'.format(args.model_load_dir))
-    else:
-      check_point.init()
-      print('init model on demand')
+  fmt_str = "{:>12}  {:>12}  {:>12.10f}"
+  print('{:>12}  {:14}  {}'.format( "step", "loss", "time"))
+  train_start_time = time.time()
+  step_time = []
+  for step in range(args.num_steps):
+    loss_mean = PretrainJob().get().mean()
+    step_time.append(time.time())
+    train_step_time = step_time[step] - step_time[step-1]
+    print(fmt_str.format(step, loss_mean, train_step_time))
 
-    fmt_str = "{:>12}  {:>12}  {:>12.10f}"
-    print('{:>12}  {:14}  {}'.format( "step", "loss", "time"))
-    train_start_time = time.time()
-    step_time = []
-    for step in range(args.num_steps):
-      loss_mean = sess.run(PretrainJob).get().mean()
-      step_time.append(time.time())
-      train_step_time = step_time[step] - step_time[step-1]
-      print(fmt_str.format(step, loss_mean, train_step_time))
-      #sess.no_return_run(PretrainJob)#.async_get(AsyncGetCallback)
-      #sess.run(PretrainJob).async_get(AsyncGetCallback)
-
-      if args.model_save_dir != '':
-        if not os.path.exists(args.model_save_dir):
-          os.makedirs(args.model_save_dir)
-        assert args.save_checkpoints_steps > 0
-        if step % args.save_checkpoints_steps == 0:
-          snapshot_save_path = os.path.join(args.model_save_dir, 'snapshot_%d'%(step+1))
-          if os.path.exists(snapshot_save_path):
-            shutil.rmtree(snapshot_save_path)
-          check_point.save(snapshot_save_path)
+    if args.model_save_dir != '':
+      if not os.path.exists(args.model_save_dir):
+        os.makedirs(args.model_save_dir)
+      assert args.save_checkpoints_steps > 0
+      if step % args.save_checkpoints_steps == 0:
+        snapshot_save_path = os.path.join(args.model_save_dir, 'snapshot_%d'%(step+1))
+        if os.path.exists(snapshot_save_path):
+          shutil.rmtree(snapshot_save_path)
+        check_point.save(snapshot_save_path)
 
   total_time = step_time[-1] - start_time
   train_time = step_time[-1] - train_start_time
