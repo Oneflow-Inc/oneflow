97e3a8809410962cd996b43f1ee6e33c6aa8505b
ShawnXuan <xiexuanx2@gmail.com>
2019-09-06 17:56:43 +0800

    support model load/saveargs

--- oneflow/python/model/bert/run_pretraining.py
+++ oneflow/python/model/bert/run_pretraining.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import argparse
+import shutil
 import numpy as np
 from datetime import datetime
 
@@ -13,7 +14,7 @@ _DATA_DIR = '/dataset/bert/of_wiki_seq_len_128'
 #_DATA_DIR = '/dataset/bert_regression_test/0'
 #_MODEL_LOAD = "/dataset/model_zoo/bert/of_L-12_H-768_A-12_random_init"
 _MODEL_LOAD = "/dataset/model_zoo/bert_new_snapshot/of_L-12_H-768_A-12_random_init"
-
+_MODEL_SAVE_DIR = './log/snapshots'
 parser = argparse.ArgumentParser(description="flags for bert")
 parser.add_argument("-d", "--device_num_per_node", type=int, default=4)
 parser.add_argument("-n", "--node_num", type=int, default=1)
@@ -21,6 +22,10 @@ parser.add_argument("-b", "--batch_size_per_device", type=int, default=24)
 parser.add_argument("-s", "--num_steps", type=int, default=100)
 parser.add_argument("-c", "--copy_binary_to_worker", type=bool, default=True)
 parser.add_argument("-u", "--use_uuid", type=bool, default=False)
+parser.add_argument("-t", "--train_dir", type=str, default=_DATA_DIR, required=False)
+parser.add_argument("-load", "--model_load_dir", type=str, default=_MODEL_LOAD, required=False)
+parser.add_argument("-save", "--model_save_dir", type=str, default=_MODEL_SAVE_DIR, required=False)
+parser.add_argument('--save_checkpoints_steps', default=10000, type=int)
 args = parser.parse_args()
 
 nodes = [{'addr':'192.168.1.16'},{'addr':'192.168.1.15'}]
@@ -47,7 +52,7 @@ def BuildPreTrainNet(data_part_num, seq_length=128, max_position_embeddings=512,
   hidden_size = 64 * num_attention_heads#, H = 64, size per head
   intermediate_size = hidden_size * 4
 
-  decoders = BertDecoder(_DATA_DIR, data_part_num, seq_length, max_predictions_per_seq)
+  decoders = BertDecoder(args.train_dir, data_part_num, seq_length, max_predictions_per_seq)
 
   input_ids = decoders[0]
   next_sentence_labels = decoders[1]
@@ -128,10 +133,8 @@ def AsyncGetCallback(result):
   cur_step += 1
 
 if __name__ == '__main__':
-  print('node/machine num', args.node_num)
-  print('device/gpu num per node/machine', args.device_num_per_node)
-  print('batch size per device/gpu', args.batch_size_per_device)
-  print('number of steps', args.num_steps)
+  for arg in vars(args):
+    print arg, getattr(args, arg)
 
   start_time = time.time()
   config = flow.ConfigProtoBuilder()
@@ -149,22 +152,37 @@ if __name__ == '__main__':
 
   flow.add_job(PretrainJob)
   with flow.Session() as sess:
-    check_point = flow.train.SimpleCheckPointManager('model_save')
-    check_point.initialize_or_restore()
-    #check_point = flow.train.CheckPoint()
-    #check_point.load(_MODEL_LOAD)
+    check_point = flow.train.CheckPoint()
+    if args.model_load_dir != '':
+      assert os.path.isdir(args.model_load_dir)
+      check_point.load(args.model_load_dir)
+      print('init model from {}'.format(args.model_load_dir))
+    else:
+      check_point.init()
+      print('init model on demand')
+
     fmt_str = "{:>12}  {:>12}  {:>12.10f}"
     print('{:>12}  {:14}  {}'.format( "step", "loss", "time"))
     train_start_time = time.time()
     step_time = []
-    for i in range(args.num_steps):
+    for step in range(args.num_steps):
       loss_mean = sess.run(PretrainJob).get().mean()
       step_time.append(time.time())
-      train_step_time = step_time[i] - step_time[i-1]
-      print(fmt_str.format(i, loss_mean, train_step_time))
+      train_step_time = step_time[step] - step_time[step-1]
+      print(fmt_str.format(step, loss_mean, train_step_time))
       #sess.no_return_run(PretrainJob)#.async_get(AsyncGetCallback)
       #sess.run(PretrainJob).async_get(AsyncGetCallback)
-    check_point.save()
+
+      if args.model_save_dir != '':
+        if not os.path.exists(args.model_save_dir):
+          os.makedirs(args.model_save_dir)
+        assert args.save_checkpoints_steps > 0
+        if step % args.save_checkpoints_steps == 0:
+          snapshot_save_path = os.path.join(args.model_save_dir, 'snapshot_%d'%(step+1))
+          if os.path.exists(snapshot_save_path):
+            shutil.rmtree(snapshot_save_path)
+          check_point.save(snapshot_save_path)
+
   total_time = step_time[-1] - start_time
   train_time = step_time[-1] - train_start_time
   init_time = train_start_time - start_time
