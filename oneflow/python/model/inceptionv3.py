fec532ea8d112f731cb4f82d86e52e5ec834d0af
Tsai <caishenghang@oneflow.org>
2019-09-07 20:10:33 +0800

    fix name

--- oneflow/python/model/inceptionv3.py
+++ oneflow/python/model/inceptionv3.py
@@ -349,7 +349,7 @@ def InceptionD(in_blob, index):
                 padding="VALID",
             )
             branch7x7x3_4 = _conv2d_layer(
-                "conv2",
+                "conv3",
                 branch7x7x3_3,
                 filters=192,
                 kernel_size=3,
@@ -370,6 +370,9 @@ def InceptionD(in_blob, index):
         inceptionD_bn.append(branch3x3_2)
         inceptionD_bn.append(branch7x7x3_4)
         inceptionD_bn.append(branch_pool)
+        print(branch3x3_2.shape)
+        print(branch7x7x3_4.shape)
+        print(branch_pool.shape)
 
         mixed_concat = flow.concat(values=inceptionD_bn, axis=1, name="concat")
 
