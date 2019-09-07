790d1c68dbf818576fc8e308527a3ac885e3c6c7
nlqq <snow3s@qq.com>
2019-09-07 11:55:44 +0800

    model compared to be same as the old one

--- oneflow/python/model/vgg.py
+++ oneflow/python/model/vgg.py
@@ -3,7 +3,9 @@ import oneflow.core.operator.op_conf_pb2 as op_conf_util
 from datetime import datetime
 import argparse
 
-_DATA_DIR = "/dataset/imagenet_224/train/32"
+_DATA_DIR = "/dataset/PNGS/PNG224/of_record_repeated"
+_SINGLE_DATA_DIR = "/dataset/PNGS/PNG224/of_record"
+_MODEL_LOAD_DIR = "/dataset/PNGS/cnns_model_for_test/vgg16/models/of_model"
 _MODEL_SAVE_DIR = "./model_save-{}".format(
     str(datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))
 )
@@ -27,9 +29,10 @@ parser.add_argument(
 parser.add_argument(
     "-r", "--remote_by_hand", default=False, action="store_true", required=False
 )
-parser.add_argument("-e", "--eval_dir", type=str, default=_DATA_DIR, required=False)
+parser.add_argument("-e", "--eval_dir", type=str, default=_SINGLE_DATA_DIR, required=False)
 parser.add_argument("-t", "--train_dir", type=str, default=_DATA_DIR, required=False)
-parser.add_argument("-load", "--model_load_dir", type=str, default="", required=False)
+parser.add_argument("-load", "--model_load_dir", type=str, default=_MODEL_LOAD_DIR, required=False)
+#parser.add_argument("-load", "--model_load_dir", type=str, default="", required=False)
 parser.add_argument(
     "-save", "--model_save_dir", type=str, default=_MODEL_SAVE_DIR, required=False
 )
@@ -42,13 +45,13 @@ def _conv2d_layer(
     filters,
     kernel_size=3,
     strides=1,
-    padding="SAME",
+    padding="VALID",
     data_format="NCHW",
     dilation_rate=1,
     activation=op_conf_util.kRelu,
-    use_bias=False,
+    use_bias=True,
     weight_initializer=flow.random_uniform_initializer(),
-    bias_initializer=None,
+    bias_initializer=flow.constant_initializer(),
 ):
     weight_shape = (filters, input.static_shape[1], kernel_size, kernel_size)
     weight = flow.get_variable(
@@ -92,11 +95,10 @@ def _data_load_layer(data_dir):
     )
 
     return flow.data.decode_ofrecord(
-        data_dir, (label_blob_conf, image_blob_conf), data_part_num=8, name="decode"
+        data_dir, (label_blob_conf, image_blob_conf), data_part_num=32, name="decode"
     )
 
 def _conv_block(in_blob, index, filters, conv_times):
-    #dlnet = in_blob.dl_net()
     conv_block = []
     conv_block.insert(0, in_blob)
     for i in range(conv_times):
@@ -116,25 +118,25 @@ def _conv_block(in_blob, index, filters, conv_times):
 def vgg(images, labels, trainable=True):
     transposed = flow.transpose(images, name="transpose", perm=[0, 3, 1, 2])
     conv1 = _conv_block(transposed, 0, 64, 2)
-    
+    print("conv1   ", conv1[-1].shape)  
     pool1 = flow.nn.max_pool2d(conv1[-1], 2, 2, "VALID", "NCHW", name="pool1")
-
+    print("pool1   ", pool1.shape)
     conv2 = _conv_block(pool1, 2, 128, 2)
-
+    
     pool2 = flow.nn.max_pool2d(conv2[-1], 2, 2, "VALID", "NCHW", name="pool2")
-
+    print("pool2    ", pool2.shape)
     conv3 = _conv_block(pool2, 4, 256, 3)
 
     pool3 = flow.nn.max_pool2d(conv3[-1], 2, 2, "VALID", "NCHW", name="pool3")
-
+    print("pool3   ", pool3.shape)
     conv4 = _conv_block(pool3, 7, 512, 3)
 
     pool4 = flow.nn.max_pool2d(conv4[-1], 2, 2, "VALID", "NCHW", name="pool4")
-
+    print("pool4    ", pool4.shape)
     conv5 = _conv_block(pool4, 10, 512, 3)
 
     pool5 = flow.nn.max_pool2d(conv5[-1], 2, 2, "VALID", "NCHW", name="pool5")
-
+    print("pool5   ", pool5.shape)
     def _get_kernel_initializer():
         kernel_initializer = op_conf_util.InitializerConf()
         kernel_initializer.truncated_normal_conf.std = 0.816496580927726
@@ -145,11 +147,12 @@ def vgg(images, labels, trainable=True):
         bias_initializer.constant_conf.value = 0.0
         return bias_initializer
 
-    if len(pool5.shape) > 2:
-        pool5 = flow.reshape(pool5, shape=(pool5.static_shape[0], -1))
+  # if len(pool5.shape) > 2:
+    pool5 = flow.reshape(pool5, [-1, 512])
+    print("pool5   reshaped  ", pool5.shape)
 
 
-    fc1 = flow.layers.dense( 
+    fc6 = flow.layers.dense( 
       inputs=pool5, 
       units=4096,
       activation=flow.keras.activations.relu,
@@ -159,11 +162,10 @@ def vgg(images, labels, trainable=True):
       trainable=trainable,
       name="fc1"
     )
+    print("fc6   ", fc6.shape)
 
-    dropout1 = fc1
-    
-    fc2 = flow.layers.dense( 
-      inputs=dropout1, 
+    fc7 = flow.layers.dense( 
+      inputs=fc6, 
       units=4096,
       activation=flow.keras.activations.relu,
       use_bias=True,
@@ -172,32 +174,31 @@ def vgg(images, labels, trainable=True):
       trainable=trainable,
       name="fc2"
     )
+    print("fc7    ", fc7.shape)
 
-    dropout2 = fc2
-
-    fc3 = flow.layers.dense( 
-      inputs=dropout2, 
+    fc8 = flow.layers.dense( 
+      inputs=fc7, 
       units=1001,
       activation=flow.keras.activations.relu,
       use_bias=True,
       kernel_initializer=_get_kernel_initializer(),
       bias_initializer=_get_bias_initializer(),
       trainable=trainable,
-      name="fc3"
+      name="fc_final"
     )
-
+    print("fc8   :", fc8.shape)
     loss = flow.nn.sparse_softmax_cross_entropy_with_logits(
-        labels, fc3, name="softmax_loss"
+        labels, fc8, name="softmax_loss"
     )
 
     return loss
 
 
-def vgg_train_job():
+def TrainNet():
     job_conf = flow.get_cur_job_conf_builder()
-    job_conf.batch_size(12).default_data_type(flow.float)
+    job_conf.batch_size(8).default_data_type(flow.float)
     job_conf.train_conf()
-    job_conf.train_conf().batch_size = 12
+    job_conf.train_conf().batch_size = 8
     job_conf.train_conf().primary_lr = 0.00001
     job_conf.train_conf().model_update_conf.naive_conf.SetInParent()
 
@@ -207,11 +208,11 @@ def vgg_train_job():
     return loss
 
 
-def vgg_eval_job():
-    job_conf = flow.get_cur_job_conf_builder()
-    job_conf.batch_size(12).default_data_type(flow.float)
-    (labels, images) = _data_load_layer(args.eval_dir)
-    return vgg(images, labels, False)
+#def vgg_eval_job():
+#    job_conf = flow.get_cur_job_conf_builder()
+#    job_conf.batch_size(8).default_data_type(flow.float)
+#    (labels, images) = _data_load_layer(args.eval_dir)
+#    return vgg(images, labels, False)
 
 
 if __name__ == "__main__":
@@ -232,8 +233,8 @@ if __name__ == "__main__":
 
     flow.init(config)
 
-    flow.add_job(vgg_train_job)
-    flow.add_job(vgg_eval_job)
+    flow.add_job(TrainNet)
+    #flow.add_job(vgg_eval_job)
 
     with flow.Session() as sess:
         check_point = flow.train.CheckPoint()
@@ -246,15 +247,15 @@ if __name__ == "__main__":
         for i in range(10):
             print(
                 fmt_str.format(
-                    i, "train loss:", sess.run(vgg_train_job).get().mean()
+                    i, "train loss:", sess.run(TrainNet).get().mean()
                 )
             )
-            if (i + 1) % 10 == 0:
-                print(
-                    fmt_str.format(
-                        i, "eval loss:", sess.run(vgg_eval_job).get().mean()
-                    )
-                )
+          #  if (i + 1) % 10 == 0:
+          #      print(
+          #          fmt_str.format(
+          #              i, "eval loss:", sess.run(vgg_eval_job).get().mean()
+          #          )
+          #      )
             if (i + 1) % 100 == 0:
                 check_point.save(_MODEL_SAVE_DIR + str(i))
         if (
