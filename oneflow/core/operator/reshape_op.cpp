feb19940c69eb2813adfbcddc5303237bcfa5acc
lixinqi <lixinqi0703106@163.com>
2019-09-08 20:58:18 +0800

    reshape sbp

--- oneflow/core/operator/reshape_op.cpp
+++ oneflow/core/operator/reshape_op.cpp
@@ -4,6 +4,72 @@
 
 namespace oneflow {
 
+namespace {
+
+Maybe<Shape> GetLogicalOutBlobShape(const Shape& in_shape, const ShapeProto& reshape_proto) {
+  size_t total_elem_dim_exclude_minus_1 = 1;
+  bool has_minus_1 = false;
+  bool minus_1_axis = -1;
+  std::vector<int64_t> dim_vec;
+  FOR_RANGE(int, axis, 0, reshape_proto.dim_size()) {
+    int64_t dim = reshape_proto.dim(axis);
+    dim_vec.push_back(dim);
+    if (dim == -1) {
+      OF_CHECK(has_minus_1 == false) << "only one `-1' supported";
+      has_minus_1 = true;
+      minus_1_axis = axis;
+    } else if (dim > 0) {
+      OF_CHECK_LE(dim, in_shape.elem_cnt()) << "invalid axis: " << axis << ", dim: " << dim;
+      total_elem_dim_exclude_minus_1 *= dim;
+      OF_CHECK_LE(total_elem_dim_exclude_minus_1, in_shape.elem_cnt())
+          << "element number in reshape_conf is bigger than input blob";
+    } else {
+      OF_UNIMPLEMENTED() << "only positive number or -1 supported";
+    }
+  }
+  OF_CHECK_EQ(in_shape.elem_cnt() % total_elem_dim_exclude_minus_1, 0);
+  if (has_minus_1) { dim_vec[minus_1_axis] = in_shape.elem_cnt() / total_elem_dim_exclude_minus_1; }
+  return std::make_shared<Shape>(dim_vec);
+}
+
+void Squeeze(const Shape& origin, Shape* shape, HashMap<int, int>* squeezed_axis2origin_axis) {
+  CHECK_GT(origin.NumAxes(), 0);
+  std::vector<int64_t> dim_vec;
+  FOR_RANGE(int, axis, 0, origin.NumAxes()) {
+    int64_t dim = origin.At(axis);
+    CHECK_GT(dim, 0);
+    if (dim == 1) { continue; }
+    CHECK(squeezed_axis2origin_axis->emplace(dim_vec.size(), axis).second);
+    dim_vec.push_back(dim);
+  }
+  *shape = Shape(dim_vec);
+}
+
+void GetGroupStartInAxis2OutAxis(const Shape& in_shape, const Shape& out_shape,
+                                 HashMap<int, int>* group_start_in_axis2out_axis) {
+  if (in_shape.NumAxes() == 0) { return; }
+  if (out_shape.NumAxes() == 0) { return; }
+  CHECK_EQ(in_shape.elem_cnt(), out_shape.elem_cnt());
+  int in_axis = in_shape.NumAxes() - 1;
+  int out_axis = out_shape.NumAxes() - 1;
+  while (in_axis >= 0 && out_axis >= 0) {
+    if (in_shape.Count(in_axis) < out_shape.Count(out_axis)) {
+      --in_axis;
+    } else if (in_shape.Count(in_axis) > out_shape.Count(out_axis)) {
+      --out_axis;
+    } else {
+      if (in_shape.At(in_axis) == out_shape.At(out_axis)) {
+        (*group_start_in_axis2out_axis)[in_axis] = out_axis;
+      }
+      --in_axis;
+      --out_axis;
+    }
+  }
+  CHECK_EQ(in_axis + out_axis, -1);
+}
+
+}  // namespace
+
 void ReshapeOp::InitFromOpConf() {
   CHECK(op_conf().has_reshape_conf());
   EnrollInputBn("in");
@@ -55,11 +121,31 @@ Maybe<void> ReshapeOp::InferBlobDescs(
   return Maybe<void>::Ok();
 }
 
-Maybe<void> ReshapeOp::GetSbpSignatures(SbpSignatureList* sbp_sig_list) const {
-  SbpSignatureBuilder()
-      .Split(input_bns(), 0)
-      .Split(output_bns(), 0)
-      .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+Maybe<void> ReshapeOp::GetSbpSignatures(
+    const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  HashMap<int, int> squeezed_group_start_in_axis2out_axis;
+  HashMap<int, int> in_squeezed_axis2original_axis;
+  HashMap<int, int> out_squeezed_axis2original_axis;
+  {
+    const auto& in_shape = JUST(LogicalBlobDesc4Ibn("in"))->shape();
+    const auto& out_shape =
+        JUST(GetLogicalOutBlobShape(in_shape, op_conf().reshape_conf().shape()));
+    Shape squeezed_in_shape;
+    Shape squeezed_out_shape;
+    Squeeze(in_shape, &squeezed_in_shape, &in_squeezed_axis2original_axis);
+    Squeeze(*out_shape, &squeezed_out_shape, &out_squeezed_axis2original_axis);
+    GetGroupStartInAxis2OutAxis(squeezed_in_shape, squeezed_out_shape,
+                                &squeezed_group_start_in_axis2out_axis);
+  }
+  for (const auto& pair : squeezed_group_start_in_axis2out_axis) {
+    int64_t start_in_axis = in_squeezed_axis2original_axis.at(pair.first);
+    int64_t start_out_axis = out_squeezed_axis2original_axis.at(pair.second);
+    SbpSignatureBuilder()
+        .Split(input_bns(), start_in_axis)
+        .Split(output_bns(), start_out_axis)
+        .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  }
   SbpSignatureBuilder()
       .PartialSum(input_bns())
       .PartialSum(output_bns())
