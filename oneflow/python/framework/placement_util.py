fa93a2bf8e61e504775f1e58a070c2a3ed9c61c9
XIE Xuan <xiexuanx2@gmail.com>
2019-09-08 19:41:41 +0800

    docstring for placement_util.py

--- oneflow/python/framework/placement_util.py
+++ oneflow/python/framework/placement_util.py
@@ -47,6 +47,11 @@ class PlacementScope(object):
 
 @oneflow_export('fixed_placement')
 class FixedPlacementScope(PlacementScope):
+    """Base class for fixed placement scope.
+
+    This class along with `device_prior_placement` allows to define PlacementScope
+    with fixed parallel configuration.
+    """
     def __init__(self, device_tag, machine_device_ids):
         PlacementScope.__init__(self, device_tag, machine_device_ids)
 
@@ -58,6 +63,11 @@ class FixedPlacementScope(PlacementScope):
 
 @oneflow_export('device_prior_placement')
 class DevicePriorPlacementScope(PlacementScope):
+    """Base class for device prior placement scope.
+
+    This class along with `device_prior_placement` allows to define PlacementScope
+    with device prior parallel configuration.
+    """
     def __init__(self, device_tag, machine_device_ids):
         PlacementScope.__init__(self, device_tag, machine_device_ids)
 
