2b468481ca97f7e51fc6668987f6668db4c989f6
XIE Xuan <xiexuanx2@gmail.com>
2019-09-08 18:45:24 +0800

    docstring for function

--- oneflow/python/framework/function_util.py
+++ oneflow/python/framework/function_util.py
@@ -10,6 +10,15 @@ _job_name2job_func = {}
 
 @oneflow_export("function")
 def function(job_func):
+    r"""Creates a callable OneFlow graph from a Python function..
+    Args:
+        job_func: job function to be compiled.
+    Returns:
+        If func is not None, returns a callable that will execute the compiled 
+        function (and return zero or more Blob objects). 
+        If func is None, returns a decorator that, when invoked with a single 
+        func argument, returns a callable equivalent to the case above.
+    """
     @functools.wraps(job_func)
     def Func(*args):
         return session_util.GetDefaultSession().run(job_func, *args)
