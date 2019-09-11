82deac6f5062bbf9c74413071e11b761575bec4b
QiaoJing <mir-of@outlook.com>
2019-09-11 19:00:24 +0800

    add cnns test scripts for dev_python (#2170)
    * add cnns test scripts for dev_python
    
    * add alexnet test scripts
    
    * add resnet50
    
    * add inceptionv3
    
    * add resnet50
    
    * add vgg16
    
    * first version of run_cnns_test.py
    
    * remove old files

--- oneflow/python/test/models/cnns/resnet50.py
+++ oneflow/python/test/models/cnns/resnet50.py
@@ -1,15 +1,17 @@
 import oneflow as flow
 import oneflow.core.operator.op_conf_pb2 as op_conf_util
 import argparse
-
+import os
+import numpy
 from datetime import datetime
+# import hook
 
-
-DATA_DIR = "/dataset/imagenet/train/32"
+# DATA_DIR = "/dataset/PNGS/PNG228/of_record"
+DATA_DIR = "/dataset/PNGS/PNG228/of_record_repeated"
 MODEL_LOAD = "/dataset/PNGS/cnns_model_for_test/resnet50/models/of_model"
-MODEL_SAVE = "./output/model_save-{}".format(
-    str(datetime.now().strftime("%Y-%m-%d-%H:%M:%S"))
-)
+MODEL_SAVE = "./output/model_save-{}".format(str(datetime.now().strftime("%Y-%m-%d-%H:%M:%S")))
+NODE_LIST = "192.168.1.12,192.168.1.14"
+
 IMAGE_SIZE = 228
 BLOCK_COUNTS = [3, 4, 6, 3]
 BLOCK_FILTERS = [256, 512, 1024, 2048]
@@ -17,48 +19,24 @@ BLOCK_FILTERS_INNER = [64, 128, 256, 512]
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-g", "--gpu_num_per_node", type=int, default=1, required=False
-)
-parser.add_argument(
-    "-m", "--multinode", default=False, action="store_true", required=False
-)
-parser.add_argument(
-    "-s",
-    "--skip_scp_binary",
-    default=False,
-    action="store_true",
-    required=False,
-)
-parser.add_argument(
-    "-c",
-    "--scp_binary_without_uuid",
-    default=False,
-    action="store_true",
-    required=False,
-)
-parser.add_argument(
-    "-r", "--remote_by_hand", default=False, action="store_true", required=False
-)
+parser.add_argument("-g", "--gpu_num_per_node", type=int, default=1, required=False)
 parser.add_argument("-i", "--iter_num", type=int, default=10, required=False)
-parser.add_argument(
-    "-e", "--eval_dir", type=str, default=DATA_DIR, required=False
-)
-parser.add_argument(
-    "-t", "--train_dir", type=str, default=DATA_DIR, required=False
-)
-parser.add_argument(
-    "-load", "--model_load_dir", type=str, default=None, required=False
-)
-parser.add_argument(
-    "-save", "--model_save_dir", type=str, default=MODEL_SAVE, required=False
-)
-parser.add_argument(
-    "-dn", "--data_part_num", type=int, default=32, required=False
-)
+parser.add_argument("-m", "--multinode", default=False, action="store_true", required=False)
+parser.add_argument("-n", "--node_list", type=str, default=NODE_LIST, required=False)
+parser.add_argument("-s", "--skip_scp_binary", default=False, action="store_true", required=False)
+parser.add_argument("-c","--scp_binary_without_uuid",default=False,action="store_true",required=False)
+parser.add_argument("-r", "--remote_by_hand", default=False, action="store_true", required=False)
+parser.add_argument("-e", "--eval_dir", type=str, default=DATA_DIR, required=False)
+parser.add_argument("-t", "--train_dir", type=str, default=DATA_DIR, required=False)
+parser.add_argument("-load", "--model_load_dir", type=str, default=MODEL_LOAD, required=False)
+parser.add_argument("-save", "--model_save_dir", type=str, default=MODEL_SAVE, required=False)
+parser.add_argument("-dn", "--data_part_num", type=int, default=32, required=False)
 parser.add_argument("-b", "--batch_size", type=int, default=8, required=False)
 
-g_args = parser.parse_args()
+
+args = parser.parse_args()
+g_output = []
+g_output_key = []
 g_trainable = True
 
 
@@ -67,12 +45,7 @@ def _data_load(data_dir):
         "encoded",
         shape=(IMAGE_SIZE, IMAGE_SIZE, 3),
         dtype=flow.float,
-        codec=flow.data.ImageCodec(
-            [
-                flow.data.ImageResizePreprocessor(IMAGE_SIZE, IMAGE_SIZE),
-                flow.data.ImagePreprocessor("bgr2rgb"),
-            ]
-        ),
+        codec=flow.data.ImageCodec([flow.data.ImagePreprocessor("bgr2rgb")]),
         preprocessors=[
             flow.data.NormByChannelPreprocessor((123.68, 116.78, 103.94))
         ],
@@ -85,7 +58,7 @@ def _data_load(data_dir):
     return flow.data.decode_ofrecord(
         data_dir,
         (label_blob_conf, image_blob_conf),
-        data_part_num=g_args.data_part_num,
+        data_part_num=args.data_part_num,
         name="decode",
     )
 
@@ -99,7 +72,7 @@ def _conv2d(
     padding="SAME",
     data_format="NCHW",
     dilations=1,
-    weight_initializer=flow.variance_scaling_initializer(data_format="NCHW"),
+    weight_initializer=flow.variance_scaling_initializer(),
 ):
     weight = flow.get_variable(
         name + "-weight",
@@ -132,9 +105,9 @@ def conv2d_affine(
     # input data_format must be NCHW, cannot check now
     padding = "SAME" if strides > 1 or kernel_size > 1 else "VALID"
     output = _conv2d(name, input, filters, kernel_size, strides, padding)
-    output = _batch_norm(output, name + "_bn")
-    if activation != op_conf_util.kNone:
-        output = flow.keras.activations.relu(output)
+    # output = _batch_norm(output, name + "_bn")
+    # if activation != op_conf_util.kNone:
+    #     output = flow.keras.activations.relu(output)
 
     return output
 
@@ -155,7 +128,7 @@ def bottleneck_transformation(
         a,
         block_name + "_branch2b",
         filters_inner,
-        3,
+        1,  # 1 for test origin 3
         strides,
         activation=op_conf_util.kRelu,
     )
@@ -212,14 +185,22 @@ def resnet_conv_x_body(input, on_stage_end=lambda x: x):
             1 if i == 0 else 2,
         )
         on_stage_end(output)
+        g_output_key.append(stage_name)
+        g_output.append(output)
 
     return output
 
 
 def resnet_stem(input):
     conv1 = _conv2d("conv1", input, 64, 7, 2)
-    conv1_bn = flow.keras.activations.relu(_batch_norm(conv1, "conv1_bn"))
-    pool1 = flow.nn.max_pool2d(
+    g_output_key.append("conv1")
+    g_output.append(conv1)
+
+    # conv1_bn = flow.keras.activations.relu(_batch_norm(conv1, "conv1_bn"))
+    # for test
+    conv1_bn = conv1
+
+    pool1 = flow.nn.avg_pool2d(
         conv1_bn,
         ksize=3,
         strides=2,
@@ -227,12 +208,17 @@ def resnet_stem(input):
         data_format="NCHW",
         name="pool1",
     )
+    g_output_key.append("pool1")
+    g_output.append(pool1)
+
     return pool1
 
 
 def resnet50(data_dir):
     (labels, images) = _data_load(data_dir)
     images = flow.transpose(images, name="transpose", perm=[0, 3, 1, 2])
+    g_output_key.append("input_img")
+    g_output.append(images)
 
     with flow.deprecated.variable_scope("Resnet"):
         stem = resnet_stem(images)
@@ -245,6 +231,8 @@ def resnet50(data_dir):
             data_format="NCHW",
             name="pool5",
         )
+        g_output_key.append("pool5")
+        g_output.append(pool5)
 
         fc1001 = flow.layers.dense(
             flow.reshape(pool5, (pool5.shape[0], -1)),
@@ -255,10 +243,14 @@ def resnet50(data_dir):
             trainable=g_trainable,
             name="fc1001",
         )
+        g_output_key.append("fc1001")
+        g_output.append(fc1001)
 
         loss = flow.nn.sparse_softmax_cross_entropy_with_logits(
             labels, fc1001, name="softmax_loss"
         )
+        g_output_key.append("cross_entropy")
+        g_output.append(loss)
 
     return loss
 
@@ -270,12 +262,12 @@ def _set_trainable(trainable):
 
 @flow.function
 def TrainNet():
-    flow.config.train.batch_size(g_args.batch_size)
+    flow.config.train.batch_size(args.batch_size)
     flow.config.train.primary_lr(0.0032)
     flow.config.train.model_update_conf(dict(naive_conf={}))
 
     _set_trainable(True)
-    loss = resnet50(g_args.train_dir)
+    loss = resnet50(args.train_dir)
     flow.losses.add_loss(loss)
     return loss
 
@@ -283,47 +275,67 @@ def TrainNet():
 @flow.function
 def evaluate():
     _set_trainable(False)
-    return resnet50(g_args.eval_dir)
+    return resnet50(args.eval_dir)
 
 
 def main():
-    flow.config.piece_size(g_args.batch_size)
+    flow.config.piece_size(args.batch_size)
     flow.config.default_data_type(flow.float)
-    flow.config.gpu_device_num(g_args.gpu_num_per_node)
+    flow.config.gpu_device_num(args.gpu_num_per_node)
     flow.config.grpc_use_no_signal()
     flow.config.log_dir("./output/log")
     flow.config.ctrl_port(12138)
 
-    if g_args.multinode:
+    if args.multinode:
         flow.config.ctrl_port(12139)
-        flow.config.machine(
-            [{"addr": "192.168.1.15"}, {"addr": "192.168.1.16"}]
-        )
+        nodes = []
+        for n in args.node_list.strip().split(","):
+            addr_dict = {}
+            addr_dict["addr"] = n
+            nodes.append(addr_dict)
 
-        if g_args.scp_binary_without_uuid:
+        flow.config.machine(nodes)
+
+        if args.scp_binary_without_uuid:
             flow.deprecated.init_worker(scp_binary=True, use_uuid=False)
-        elif g_args.skip_scp_binary:
+        elif args.skip_scp_binary:
             flow.deprecated.init_worker(scp_binary=False, use_uuid=False)
         else:
             flow.deprecated.init_worker(scp_binary=True, use_uuid=True)
 
     check_point = flow.train.CheckPoint()
-    if not g_args.model_load_dir:
-        check_point.init()
-    else:
-        check_point.load(g_args.model_load_dir)
+    check_point.load(MODEL_LOAD)
+    # if not args.model_load_dir:
+    #     check_point.init()
+    # else:
+    #     check_point.load(args.model_load_dir)
 
-    fmt_str = "{:>12}  {:>12}  {:.11f}"
+    num_nodes = len(args.node_list.strip().split(",")) if args.multinode else 1
+    print("Traning resnet50: num_gpu_per_node = {}, num_nodes = {}.".format(args.gpu_num_per_node, num_nodes))
+    loss = []
+
+    fmt_str = "{:>12}  {:>12}  {:.6f}"
     print("{:>12}  {:>12}  {:>12}".format("iter", "loss type", "loss value"))
-    for i in range(g_args.iter_num):
-        loss = TrainNet().get().mean()
-        print(fmt_str.format(i, "train loss:", loss))
+    for i in range(args.iter_num):
+        train_loss = TrainNet().get().mean()
 
-        if (i + 1) % 100 == 0:
-            eval = evaluate().get().mean()
-            print(fmt_str.format(i, "eval loss:", eval))
+        loss.append(train_loss)
+        print(fmt_str.format(i, "train loss:", train_loss))
 
-            check_point.save(MODEL_SAVE + "_" + str(i))
+        # output_dict = dict(zip(g_output_key, g_output))
+        # hook.dump_tensor_to_file(output_dict, "./prob_output/iter_{}".format(i))
+
+        # if (i + 1) % 100 == 0:
+        #     eval = evaluate().get().mean()
+        #     print(fmt_str.format(i, "eval loss:", eval))
+
+        #     check_point.save(MODEL_SAVE + "_" + str(i))
+    
+    # save loss to file
+    loss_file = "{}n{}c.npy".format(str(num_nodes), str(args.gpu_num_per_node * num_nodes))
+    loss_path = "./of_loss/resnet50"
+    if not os.path.exists(loss_path): os.makedirs(loss_path)
+    numpy.save(os.path.join(loss_path, loss_file), loss)
 
 
 if __name__ == "__main__":
