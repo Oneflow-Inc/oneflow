ae223331af8f0da1f3342bdf894aa91f0c96a0f5
lixinqi <lixinqi0703106@163.com>
2019-09-14 13:55:46 +0800

    fix job_completer/unsorted_segment_sum_grad.cpp

--- oneflow/core/job_completer/unsorted_segment_sum_grad.cpp
+++ oneflow/core/job_completer/unsorted_segment_sum_grad.cpp
@@ -8,15 +8,17 @@ void GenerateBackwardOpConf(
     const Operator& op, std::vector<OperatorConf>* op_confs,
     const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
   CHECK(op.op_conf().has_unsorted_segment_sum_conf());
+  CHECK_GE(op.op_conf().unsorted_segment_sum_conf().axis(), 0);
   if (DiffLbi4BnInOp("data") != nullptr) {
-    OperatorConf batch_gather;
-    batch_gather.set_name("System-AutoGrad-" + op.op_name());
-    BatchGatherOpConf* conf = batch_gather.mutable_batch_gather_conf();
+    OperatorConf gather;
+    gather.set_name("System-AutoGrad-" + op.op_name());
+    GatherOpConf* conf = gather.mutable_gather_conf();
     conf->set_in(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
     conf->set_indices(GenLogicalBlobName(op.BnInOp2Lbi("segment_ids")));
+    conf->set_axis(op.op_conf().unsorted_segment_sum_conf().axis());
     conf->set_out("out");
-    op_confs->push_back(batch_gather);
-    DiffLbi4BnInOp("data")->set_op_name(batch_gather.name());
+    op_confs->push_back(gather);
+    DiffLbi4BnInOp("data")->set_op_name(gather.name());
     DiffLbi4BnInOp("data")->set_blob_name(conf->out());
   }
 }
