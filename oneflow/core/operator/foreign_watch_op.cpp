0d35327b6229611a26c9a3af10f7e46c3e4b041c
lixinqi <lixinqi0703106@163.com>
2019-09-15 22:50:31 +0800

    oneflow.watch for debug

---
+++ oneflow/core/operator/foreign_watch_op.cpp
@@ -0,0 +1,35 @@
+#include "oneflow/core/operator/foreign_watch_op.h"
+#include "oneflow/core/job/sbp_signature_builder.h"
+
+namespace oneflow {
+
+void ForeignWatchOp::InitFromOpConf() {
+  CHECK(op_conf().has_foreign_watch_conf());
+  EnrollRepeatedInputBn("in");
+}
+
+Maybe<void> ForeignWatchOp::InferBlobDescs(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx) const {
+  CHECK_EQ_OR_RETURN(parallel_ctx->parallel_num(), 1);
+  return Maybe<void>::Ok();
+}
+
+const PbMessage& ForeignWatchOp::GetCustomizedConf() const {
+  return op_conf().foreign_watch_conf();
+}
+
+Maybe<void> ForeignWatchOp::InferBatchAxis(
+    std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const {
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> ForeignWatchOp::GetSbpSignatures(
+    const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  return Maybe<void>::Ok();
+}
+
+REGISTER_OP(OperatorConf::kForeignWatchConf, ForeignWatchOp);
+
+}  // namespace oneflow
