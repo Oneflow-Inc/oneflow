b1c77ebc84fc7bdc8370b2a4377a12cb2f9c3ea3
lixinqi <lixinqi0703106@163.com>
2019-09-15 14:07:06 +0800

    refactor sigmoid_cross_entropy_loss

--- oneflow/core/kernel/sigmoid_cross_entropy_loss_kernel.cpp
+++ oneflow/core/kernel/sigmoid_cross_entropy_loss_kernel.cpp
@@ -1,6 +1,5 @@
 #include "oneflow/core/kernel/sigmoid_cross_entropy_loss_kernel.h"
-#include "oneflow/core/kernel/sparse_cross_entropy_loss_kernel.h"
-#include "oneflow/core/kernel/softmax_kernel.h"
+#include "oneflow/core/ndarray/ndarray_util.h"
 
 namespace oneflow {
 
@@ -10,24 +9,35 @@ void SigmoidCrossEntropyLossKernel<device_type, PredType, LabelType>::VirtualLos
   const SigmoidCrossEntropyLossOpConf& conf = this->op_conf().sigmoid_cross_entropy_loss_conf();
   const Blob* prediction = BnInOp2Blob("prediction");
   const Blob* label = BnInOp2Blob("label");
-  Blob* loss_buf = BnInOp2Blob("loss_buf");
-  Blob* tmp_storage = BnInOp2Blob("sum_buf");
-  const size_t tmp_storage_byte_size = static_cast<size_t>(tmp_storage->shape().elem_cnt());
+  Blob* elementwise_loss = BnInOp2Blob("elementwise_loss");
+  Blob* sum_buf = BnInOp2Blob("sum_buf");
   Blob* count = BnInOp2Blob("count");
   Blob* label_num = BnInOp2Blob("label_num");
   Blob* loss = BnInOp2Blob("loss");
-  int64_t data_dim = label->shape().Count(1);
-  int64_t data_offset = 0;
-  FOR_RANGE(int64_t, data_index, 0, prediction->shape().At(0)) {
-    data_offset = data_dim * data_index;
-    const PredType* prediction_offset = prediction->dptr<PredType>() + data_offset;
-    const LabelType* label_offset = label->dptr<LabelType>() + data_offset;
-    SigmoidCrossEntropyLossKernelUtil<device_type, PredType, LabelType>::Forward(
-        ctx.device_ctx, conf, data_dim, prediction_offset, label_offset,
-        loss_buf->mut_dptr<PredType>(), tmp_storage->mut_dptr<PredType>(), tmp_storage_byte_size,
-        count->mut_dptr<PredType>(), label_num->mut_dptr<PredType>(),
-        loss->mut_dptr<PredType>() + data_index);
+  const int64_t n = prediction->shape().elem_cnt();
+  const int64_t instance_num = prediction->shape().At(0);
+  const Shape& prediction_shape = {instance_num, prediction->shape().Count(1)};
+  const Shape& loss_shape = {instance_num, 1};
+  SigmoidCrossEntropyLossKernelUtil<device_type, PredType, LabelType>::Forward(
+      ctx.device_ctx, conf, n, prediction->dptr<PredType>(), label->dptr<LabelType>(),
+      elementwise_loss->mut_dptr<PredType>(), count->mut_dptr<PredType>());
+  NdarrayUtil<device_type, PredType>::ReduceSum(
+      ctx.device_ctx, XpuVarNdarray<PredType>(loss_shape, loss->mut_dptr<PredType>()),
+      XpuVarNdarray<const PredType>(prediction_shape, elementwise_loss->dptr<PredType>()),
+      XpuVarNdarray<PredType>(prediction_shape, sum_buf->mut_dptr<PredType>()));
+  if (conf.normalize()) {
+    NdarrayUtil<device_type, PredType>::ReduceSum(
+        ctx.device_ctx, XpuVarNdarray<PredType>(loss_shape, label_num->mut_dptr<PredType>()),
+        XpuVarNdarray<const PredType>(prediction_shape, count->dptr<PredType>()),
+        XpuVarNdarray<PredType>(prediction_shape, sum_buf->mut_dptr<PredType>()));
+    SigmoidCrossEntropyLossKernelUtil<device_type, PredType, LabelType>::ClipByEpsilon(
+        ctx.device_ctx, instance_num, label_num->mut_dptr<PredType>());
+    KernelUtil<device_type, PredType>::Div(ctx.device_ctx, instance_num, loss->dptr<PredType>(),
+                                           label_num->dptr<PredType>(), loss->mut_dptr<PredType>());
   }
+  KernelUtil<device_type, PredType>::Scal(ctx.device_ctx, instance_num,
+                                          static_cast<PredType>(conf.scale()),
+                                          loss->mut_dptr<PredType>(), 1);
 }
 
 template<DeviceType device_type, typename PredType, typename LabelType>
@@ -40,37 +50,35 @@ SigmoidCrossEntropyLossKernel<device_type, PredType, LabelType>::GetLossKernelCo
 template<typename PredType, typename LabelType>
 struct SigmoidCrossEntropyLossKernelUtil<DeviceType::kCPU, PredType, LabelType> {
   static void Forward(DeviceCtx* ctx, const SigmoidCrossEntropyLossOpConf& conf, const int64_t n,
-                      const PredType* prediction, const LabelType* label, PredType* loss_buf,
-                      PredType* tmp_storage, const size_t tmp_storage_byte_size, PredType* count,
-                      PredType* label_num, PredType* loss) {
-    loss_buf[0] = 0;
-    loss[0] = 0;
-    label_num[0] = 0;
+                      const PredType* prediction, const LabelType* label,
+                      PredType* elementwise_loss, PredType* count) {
     FOR_RANGE(int64_t, index, 0, n) {
-      if (label[index] != -1) {
-        loss_buf[0] +=
+      if (label[index] == -1) {
+        elementwise_loss[index] = 0.f;
+        count[index] = 0.f;
+      } else {
+        elementwise_loss[index] =
             -1 * prediction[index] * (label[index] - (prediction[index] >= 0))
             + logf(1 + expf(prediction[index] - 2 * prediction[index] * (prediction[index] >= 0)));
-        label_num[0] += 1;
+        count[index] = 1.f;
       }
-    }
-    loss_buf[0] *= static_cast<PredType>(conf.scale());
-    if (conf.normalize()) {
-      if (label_num[0] == 0) { label_num[0] = 1e-5; }
-      loss[0] = loss_buf[0] / label_num[0];
     }
   }
 
   static void Backward(DeviceCtx* ctx, const SigmoidCrossEntropyLossOpConf& conf, const int64_t n,
-                       const PredType* prediction, const LabelType* label,
-                       const PredType* label_num, PredType* pred_diff) {
+                       const PredType* prediction, const LabelType* label, PredType* pred_diff) {
     FOR_RANGE(int64_t, index, 0, n) {
-      if (label[index] != -1) {
+      if (label[index] == -1) {
+        pred_diff[index] = 0.f;
+      } else {
         pred_diff[index] = 1.f / (1.f + expf(-prediction[index])) - label[index];
-        pred_diff[index] *= static_cast<PredType>(conf.scale());
-        if (conf.normalize()) { pred_diff[index] /= label_num[0]; }
       }
     }
+  }
+
+  static void ClipByEpsilon(DeviceCtx* ctx, const int64_t n, PredType* x) {
+    const PredType floor_val = 1e-5;
+    FOR_RANGE(int64_t, index, 0, n) { x[index] = std::max(x[index], floor_val); }
   }
 };
 
