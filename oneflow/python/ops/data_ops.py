ec1b9ac40810ac121a5f47a4047924ee016e22d1
ShawnXuan <xiexuanx2@gmail.com>
2019-09-15 16:31:28 +0800

    support part_name_prefix and part_name_suffix_length (#2208)
    Former-commit-id: bc34652f2fb46577eceb9314e38dc8459216cc47

--- oneflow/python/ops/data_ops.py
+++ oneflow/python/ops/data_ops.py
@@ -133,7 +133,8 @@ class BlobConf(object):
 
 
 @oneflow_export("data.decode_ofrecord")
-def decode_ofrecord(ofrecord_dir, blobs, data_part_num=-1, name=None):
+def decode_ofrecord(ofrecord_dir, blobs, data_part_num=-1, part_name_prefix="part-",
+                    part_name_suffix_length=-1, name=None):
     if name is None:
         name = id_util.UniqueStr("Decode_")
 
@@ -144,6 +145,8 @@ def decode_ofrecord(ofrecord_dir, blobs, data_part_num=-1, name=None):
 
     op_conf.decode_ofrecord_conf.data_dir = ofrecord_dir
     op_conf.decode_ofrecord_conf.data_part_num = data_part_num
+    op_conf.decode_ofrecord_conf.part_name_prefix = part_name_prefix
+    op_conf.decode_ofrecord_conf.part_name_suffix_length = part_name_suffix_length
     for blob_conf in blobs:
         op_conf.decode_ofrecord_conf.blob.extend([blob_conf.to_proto()])
         lbi = logical_blob_id_util.LogicalBlobId()
