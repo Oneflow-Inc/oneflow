eb9c7ff559e6e46c278d96efa0826c8ef51bc8fb
lixinqi <lixinqi0703106@163.com>
2019-09-18 18:25:57 +0800

    device_tick

---
+++ oneflow/core/operator/device_tick_op.cpp
@@ -0,0 +1,33 @@
+#include "oneflow/core/operator/device_tick_op.h"
+#include "oneflow/core/job/sbp_signature_builder.h"
+
+namespace oneflow {
+
+void DeviceTickOp::InitFromOpConf() {
+  EnrollRepeatedInputBn("tick", false);
+  EnrollOutputBn("out", false);
+}
+
+Maybe<void> DeviceTickOp::InferBlobDescs(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx) const {
+  GetBlobDesc4BnInOp("out")->mut_shape() = Shape({1});
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> DeviceTickOp::InferBatchAxis(
+    std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const {
+  BatchAxis4BnInOp("out")->clear_value();
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> DeviceTickOp::GetSbpSignatures(
+    const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  return Maybe<void>::Ok();
+}
+
+REGISTER_OP_SAME_OUTPUT_BLOB_MEM_BLOCK_NUM(OperatorConf::kDeviceTickConf, 2);
+REGISTER_OP(OperatorConf::kDeviceTickConf, DeviceTickOp);
+
+}  // namespace oneflow
