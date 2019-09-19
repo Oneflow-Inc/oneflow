3aca9653f96e70889a33a229054bf2625e500b74
Tsai <caishenghang@oneflow.org>
2019-09-19 17:48:16 +0800

    fix

--- oneflow/core/kernel/roi_align_kernel.h
+++ oneflow/core/kernel/roi_align_kernel.h
@@ -1,35 +1,15 @@
 #ifndef ONEFLOW_CORE_KERNEL_ROI_ALIGN_KERNEL_H_
 #define ONEFLOW_CORE_KERNEL_ROI_ALIGN_KERNEL_H_
 
-#include "oneflow/core/kernel/roi_resize_kernel.h"
+#include "oneflow/core/kernel/kernel.h"
 
 namespace oneflow {
 
 template<DeviceType device_type, typename T>
-class RoIAlignKernel final : public RoIResizeKernel<device_type> {
- public:
-  OF_DISALLOW_COPY_AND_MOVE(RoIAlignKernel);
-  RoIAlignKernel() = default;
-  ~RoIAlignKernel() = default;
-
- private:
-  void ForwardDataContent(const KernelCtx&,
-                          std::function<Blob*(const std::string&)>) const override;
-  void BackwardDataContent(const KernelCtx&,
-                           std::function<Blob*(const std::string&)>) const override;
-  void ForwardDim0ValidNum(const KernelCtx& ctx,
-                           std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
-  void BackwardInDiffDim0ValidNum(
-      const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
-  void ForwardRecordIdInDevicePiece(
-      const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
-};
-
-template<DeviceType device_type, typename T>
-struct RoIAlignKernelUtil {
-  static void Forward(const KernelCtx& ctx, const RoIAlignOpConf& conf, const Blob* in_blob,
+struct RoiAlignKernelUtil {
+  static void Forward(const KernelCtx& ctx, const RoiAlignConf& conf, const Blob* in_blob,
                       const Blob* rois_blob, Blob* out_blob);
-  static void Backward(const KernelCtx& ctx, const RoIAlignOpConf& conf, const Blob* out_diff_blob,
+  static void Backward(const KernelCtx& ctx, const RoiAlignConf& conf, const Blob* out_diff_blob,
                        const Blob* rois_blob, Blob* in_diff_blob);
 };
 
--- oneflow/core/operator/roi_align_op.cpp
+++ oneflow/core/operator/roi_align_op.cpp
@@ -1,36 +1,62 @@
-#include "oneflow/core/operator/roi_align_op.h"
+#include "oneflow/core/operator/operator.h"
 
 namespace oneflow {
 
-void RoIAlignOp::InitFromOpConf() {
-  EnrollInputBn("in");
-  EnrollInputBn("rois", false);
-  EnrollOutputBn("out");
-}
+class RoiAlignOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(RoiAlignOp);
+  RoiAlignOp() = default;
+  ~RoiAlignOp() = default;
 
-const PbMessage& RoIAlignOp::GetCustomizedConf() const { return op_conf().roi_align_conf(); }
+  const PbMessage& GetCustomizedConf() const override { return op_conf().roi_align_conf(); }
 
-void RoIAlignOp::InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-                                const ParallelContext* parallel_ctx) const {
-  if (op_conf().roi_align_conf().data_format() != "channels_first") { UNIMPLEMENTED(); }
-  // in: feature map (N, C, H, W)
-  const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp("in");
-  CHECK_EQ(in_blob_desc->shape().NumAxes(), 4);
-  // rois: (R, 5)
-  const BlobDesc* rois_blob_desc = GetBlobDesc4BnInOp("rois");
-  CHECK_EQ(rois_blob_desc->shape().NumAxes(), 2);
-  CHECK_EQ(rois_blob_desc->shape().At(1), 5);
-  CHECK(rois_blob_desc->has_dim0_inner_shape());
-  // out: (R, C, pool_h, pool_w)
-  BlobDesc* out_blob_desc = GetBlobDesc4BnInOp("out");
-  *out_blob_desc = *rois_blob_desc;
-  out_blob_desc->mut_shape() =
-      Shape({rois_blob_desc->shape().At(0), in_blob_desc->shape().At(1),
-             op_conf().roi_align_conf().pooled_h(), op_conf().roi_align_conf().pooled_w()});
-  out_blob_desc->set_data_type(in_blob_desc->data_type());
-  out_blob_desc->set_has_instance_shape_field(false);
-}
+  void InitFromOpConf() override {
+    EnrollInputBn("x");
+    EnrollInputBn("rois", false);
+    EnrollOutputBn("y");
+  }
 
-REGISTER_OP(OperatorConf::kRoiAlignConf, RoIAlignOp);
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override {
+    const RoiAlignConf& roi_align_conf = op_conf().roi_align_conf().roi_align_conf();
+    if (roi_align_conf.data_format() != "channels_first") { UNIMPLEMENTED(); }
+    // in: feature map (N, C, H, W)
+    const BlobDesc* x_blob_desc = GetBlobDesc4BnInOp("x");
+    CHECK_EQ(x_blob_desc->shape().NumAxes(), 4);
+    // rois: (R, 5)
+    const BlobDesc* rois_blob_desc = GetBlobDesc4BnInOp("rois");
+    CHECK_EQ(rois_blob_desc->shape().NumAxes(), 2);
+    CHECK_EQ(rois_blob_desc->shape().At(1), 5);
+    CHECK(rois_blob_desc->has_dim0_inner_shape());
+    // out: (R, C, pool_h, pool_w)
+    BlobDesc* y_blob_desc = GetBlobDesc4BnInOp("y");
+    *y_blob_desc = *rois_blob_desc;
+    y_blob_desc->mut_shape() = Shape({rois_blob_desc->shape().At(0), x_blob_desc->shape().At(1),
+                                      roi_align_conf.pooled_h(), roi_align_conf.pooled_w()});
+    y_blob_desc->set_data_type(x_blob_desc->data_type());
+    return Maybe<void>::Ok();
+  }
+
+ private:
+  Maybe<void> InferBatchAxis(
+      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override {
+    oneflow::OptInt64* x_split_axis = BatchAxis4BnInOp("x");
+    if (x_split_axis->has_value()) { CHECK_EQ_OR_RETURN(x_split_axis->value(), 0); }
+    oneflow::OptInt64* roi_split_axis = BatchAxis4BnInOp("rois");
+    if (roi_split_axis->has_value()) { CHECK_EQ_OR_RETURN(x_split_axis->value(), 0); }
+    *BatchAxis4BnInOp("y") = *BatchAxis4BnInOp("x");
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> GetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      SbpSignatureList* sbp_sig_list) const override {
+    SbpSignatureBuilder().Split("x", 0).Split("y", 0).Build(
+        sbp_sig_list->mutable_sbp_signature()->Add());
+    return Maybe<void>::Ok();
+  }
+};
+
+REGISTER_OP(OperatorConf::kRoiAlignConf, RoiAlignOp);
 
 }  // namespace oneflow
