9ab7a40a548fef7e9fbba365be2d3e5a567625d7
Tsai <caishenghang@oneflow.org>
2019-09-19 19:55:10 +0800

    fix test
    Former-commit-id: 9d350cd1d5d0609d5ebae1e71fbf0b40f6b717c3

--- oneflow/python/test/roi_align.py
+++ oneflow/python/test/roi_align.py
@@ -6,10 +6,10 @@ flow.config.default_data_type(flow.float)
 
 
 @flow.function
-def RoiAlignJob(x=flow.input_blob_def((2, 1024, 45, 45)), rois=flow.input_blob_def((2, 5))):
+def RoiAlignJob(x=flow.input_blob_def((2, 1024, 45, 45)), rois=flow.input_blob_def((2, 5), dtype=flow.float32)):
     return flow.detection.roi_align(x, rois, 4, 4)
 
 
 fm = np.random.randn(2, 1024, 45, 45).astype(np.float32)
-rois = np.array([[0, 1, 2, 3, 4], [1, 1, 2, 3, 4]], dtype=np.int32)
+rois = np.array([[0, 1, 2, 3, 4], [1, 1, 2, 3, 4]], dtype=np.float32)
 print(RoiAlignJob(fm, rois).get())
