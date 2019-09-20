87e8e4172ddf1acc68a108f5622206a0ac0a6595
Tsai <caishenghang@oneflow.org>
2019-09-20 15:10:31 +0800

    oneflow/core/operator/local_scatter_nd_update_op.cpp
    Former-commit-id: 41a6bb90b5484e459884f05309fc8eb61cc323d4

--- oneflow/core/operator/local_scatter_nd_update_op.cpp
+++ oneflow/core/operator/local_scatter_nd_update_op.cpp
@@ -1,53 +1,56 @@
-#include "oneflow/core/operator/local_scatter_nd_update_op.h"
+#include "oneflow/core/operator/operator.h"
 
 namespace oneflow {
 
-void LocalScatterNdUpdateOp::InitFromOpConf() {
-  CHECK(op_conf().has_local_scatter_nd_update_conf());
-  EnrollInputBn("in");
-  EnrollInputBn("indices", false);
-  EnrollInputBn("updates");
-  if (this->device_type() == DeviceType::kGPU) { EnrollDataTmpBn("shape"); }
-  EnrollOutputBn("out");
-}
+class LocalScatterNdUpdateOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(LocalScatterNdUpdateOp);
+  LocalScatterNdUpdateOp() = default;
+  ~LocalScatterNdUpdateOp() override = default;
 
-const PbMessage& LocalScatterNdUpdateOp::GetCustomizedConf() const {
-  return op_conf().local_scatter_nd_update_conf();
-}
-
-void LocalScatterNdUpdateOp::VirtualGenKernelConf(
-    std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-    const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
-  LocalScatterNdUpdateConf* conf = kernel_conf->mutable_local_scatter_nd_update_conf();
-  conf->set_value_type(GetBlobDesc4BnInOp("in")->data_type());
-  conf->set_indices_type(GetBlobDesc4BnInOp("indices")->data_type());
-}
-
-void LocalScatterNdUpdateOp::InferBlobDescs(
-    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-    const ParallelContext* parallel_ctx) const {
-  // input: in, indices, updates
-  const BlobDesc* in = GetBlobDesc4BnInOp("in");
-  const BlobDesc* indices = GetBlobDesc4BnInOp("indices");
-  const BlobDesc* updates = GetBlobDesc4BnInOp("updates");
-  CHECK_EQ(in->data_type(), updates->data_type());
-  CHECK(IsIntegralDataType(indices->data_type()));
-  const auto indices_dim_vec = indices->shape().dim_vec();
-  if (this->device_type() == DeviceType::kGPU) {
-    // datatmp
-    BlobDesc* shape = GetBlobDesc4BnInOp("shape");
-    shape->mut_shape() = Shape({in->shape().NumAxes()});
-    shape->set_data_type(DataType::kInt64);
+  void InitFromOpConf() override {
+    CHECK(op_conf().has_local_scatter_nd_update_conf());
+    EnrollInputBn("in");
+    EnrollInputBn("indices", false);
+    EnrollInputBn("updates");
+    if (this->device_type() == DeviceType::kGPU) { EnrollTmpBn("shape"); }
+    EnrollOutputBn("out");
   }
 
-  // output
-  *GetBlobDesc4BnInOp("out") = *in;
-}
+  const PbMessage& GetCustomizedConf() const override {
+    return op_conf().local_scatter_nd_update_conf();
+  }
 
-void LocalScatterNdUpdateOp::GetOpParallelSignatures(
-    std::vector<std::unique_ptr<const OpParallelSignature>>* op_parallel_signatures) const {
-  op_parallel_signatures->emplace_back(MakeDataSplitOpParallelSignature(this));
-}
+  void VirtualGenKernelConf(std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                            const ParallelContext* parallel_ctx,
+                            KernelConf* kernel_conf) const override {
+    // LocalScatterNdUpdateConf* conf = kernel_conf->mutable_local_scatter_nd_update_conf();
+    // conf->set_value_type(GetBlobDesc4BnInOp("in")->data_type());
+    // conf->set_indices_type(GetBlobDesc4BnInOp("indices")->data_type());
+  }
+
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx, const SbpSignature* sbp_signature,
+                             std::function<void(OpContext*)> EnrollOpCtx) const override {
+    // input: in, indices, updates
+    const BlobDesc* in = GetBlobDesc4BnInOp("in");
+    const BlobDesc* indices = GetBlobDesc4BnInOp("indices");
+    const BlobDesc* updates = GetBlobDesc4BnInOp("updates");
+    CHECK_EQ(in->data_type(), updates->data_type());
+    CHECK(IsIntegralDataType(indices->data_type()));
+    const auto indices_dim_vec = indices->shape().dim_vec();
+    if (this->device_type() == DeviceType::kGPU) {
+      // datatmp
+      BlobDesc* shape = GetBlobDesc4BnInOp("shape");
+      shape->mut_shape() = Shape({in->shape().NumAxes()});
+      shape->set_data_type(DataType::kInt64);
+    }
+
+    // output
+    *GetBlobDesc4BnInOp("out") = *in;
+    return Maybe<void>::Ok();
+  }
+};
 
 REGISTER_OP(OperatorConf::kLocalScatterNdUpdateConf, LocalScatterNdUpdateOp);
 
