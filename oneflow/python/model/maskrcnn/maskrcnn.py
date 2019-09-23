8680ad1c1d83559903aeefe98ae38dfb691617e3
ScXfjiang <sc.xfjiang@gmail.com>
2019-09-23 15:03:57 +0800

    add mask_head

--- oneflow/python/model/maskrcnn/maskrcnn.py
+++ oneflow/python/model/maskrcnn/maskrcnn.py
@@ -3,26 +3,22 @@ import oneflow.core.operator.op_conf_pb2 as op_conf_util
 
 from backbone import Backbone
 from rpn import RPNHead, RPNLoss, RPNProposal
+from box_head import BoxHead
+from mask_head import MaskHead
 
 from config import get_default_cfgs
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--config_file",
-    default=None,
-    type=str,
-    required=True,
-    help="yaml config file",
+    "--config_file", default=None, type=str, required=True, help="yaml config file"
 )
-parser.add_argument(
-    "-load", "--model_load_dir", type=str, default="", required=False
-)
+parser.add_argument("-load", "--model_load_dir", type=str, default="", required=False)
 args = parser.parse_args()
 
 
 @flow.function
-def maskrcnn(images, image_sizes, gt_boxes, gt_labels):
+def maskrcnn(images, image_sizes, gt_boxes, gt_segms, gt_labels):
     r"""Mask-RCNN
     Args:
     images: N,C,H,W
@@ -39,10 +35,12 @@ def maskrcnn(images, image_sizes, gt_boxes, gt_labels):
     rpn_loss = RPNLoss(cfg)
     rpn_proposal = RPNProposal(cfg)
     box_head = BoxHead(cfg)
+    mask_head = MaskHead(cfg)
 
     image_size_list = flow.piece_slice(image_sizes)
     gt_boxes_list = flow.piece_slice(gt_boxes)
     gt_labels_list = flow.piece_slice(gt_labels)
+    gt_segms_list = flow.piece_slice(gt_segms)
 
     anchors = []
     for i in range(cfg.DECODER.FPN_LAYERS):
@@ -68,9 +66,16 @@ def maskrcnn(images, image_sizes, gt_boxes, gt_labels):
     )
 
     # Box Head
-    box_head.build_train(proposals, gt_boxes_list, gt_labels_list, features)
+    box_loss, cls_loss, pos_proposal_list, pos_gt_indices_list = box_head.build_train(
+        proposals, gt_boxes_list, gt_labels_list, features
+    )
 
-    return rpn_bbox_loss, rpn_objectness_loss
+    # Mask Head
+    mask_loss = mask_head.build_train(
+        pos_proposal_list, pos_gt_indices_list, gt_segms_list, gt_labels_list, features
+    )
+
+    return rpn_bbox_loss, rpn_objectness_loss, box_loss, cls_loss, mask_loss
 
 
 if __name__ == "__main__":
