a2fe2c2c02f1e8d5b9c5a0401c411bb5c453992d
scxfjiang <sc.xfjiang@gmail.com>
2019-09-23 15:09:09 +0800

    add mask_head (#2245)
    Former-commit-id: 4b7b512b7cf57f2b405bb6af654ad69e49ade4dc

---
+++ oneflow/python/model/maskrcnn/mask_head.py
@@ -0,0 +1,165 @@
+class MaskHead(object):
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    # In mask_head, we reuse pos_proposals and pos_gt_indices from box_head,
+    # instead of using matcher to calculate them. We can do this because
+    # mask_head and box_head use the same matcher in R_50_FPN_1x training.
+    # TODO: add matcher to mask_head for the case that mask_head and box_head
+    # use different matchers.
+
+    # pos_proposals: list of [num_pos_rois, 4] wrt. images
+    # pos_gt_indices: list of [num_pos_rois,] wrt. images
+    # gt_segms: list of (G, 7, 7) wrt. images
+    # gt_labels: list of (G,) wrt. images
+    # features: list of [N, C_i, H_i, W_i] wrt. fpn layers
+    def build_train(self, pos_proposals, pos_gt_indices, gt_segms, gt_labels, features):
+        with flow.deprecated.variable_scope("mask"):
+            img_ids = flow.concat(
+                flow.detection.extract_piece_slice_id(pos_proposals), axis=0
+            )
+            proposals = flow.concat(pos_proposals, axis=0)
+
+            # mask head feature extractor
+            x = self.mask_feature_extractor(proposals, img_ids, features)
+
+            # mask head predictor
+            mask_fcn_logits = self.mask_predictor(x)
+
+            gt_segm_list = []
+            gt_label_list = []
+            for img_idx in range(cfg.TRAINING.IMG_PER_GPUl):
+                gt_segm_list.append(
+                    flow.local_gather(gt_segms[img_idxl], pos_gt_indices[img_idx])
+                )
+                gt_label_list.append(
+                    flow.local_gather(gt_labels[img_idxl], pos_gt_indices[img_idx])
+                )
+            gt_segms = flow.concat(gt_segm_list, axis=0)
+            gt_labels = flow.concat(gt_label_list, axis=0)
+            elem_cnt = flow.math.elem_cnt(gt_labels)
+
+            mask_pred = flow.keras.math.sigmoid(
+                flow.squeeze(
+                    flow.gather(
+                        params=mask_fcn_logits,
+                        indices=flow.expand_dims(gt_labels, 1),
+                        batch_dims=0,
+                    ),
+                    axis=[1],
+                )
+            )
+
+            mask_loss = flow.div(
+                flow.math.reduce_sum(flow.binary_cross_entropy(mask_pred, gt_segms)),
+                elem_cnt,
+            )
+
+            return mask_loss
+
+    def mask_feature_extractor(self, proposals, img_ids, features):
+        levels = flow.detection.level_map(proposals)
+        level_idx_2 = flow.local_nonzero(
+            levels == flow.constant_scalar(int(0), flow.int32)
+        )
+        level_idx_3 = flow.local_nonzero(
+            levels == flow.constant_scalar(int(1), flow.int32)
+        )
+        level_idx_4 = flow.local_nonzero(
+            levels == flow.constant_scalar(int(2), flow.int32)
+        )
+        level_idx_5 = flow.local_nonzero(
+            levels == flow.constant_scalar(int(3), flow.int32)
+        )
+        proposals_with_img_ids = flow.concat(
+            [flow.expand_dims(flow.cast(img_ids, flow.float), 1), proposals], axis=1
+        )
+        roi_features_0 = flow.detection.roi_align(
+            in_blob=features[0],
+            rois=flow.local_gather(
+                proposals_with_img_ids, flow.squeeze(level_idx_2, axis=[1])
+            ),
+            pooled_h=self.cfg.BOX_HEAD.POOLED_H,
+            pooled_w=self.cfg.BOX_HEAD.POOLED_W,
+            spatial_scale=self.cfg.BOX_HEAD.SPATIAL_SCALE / pow(2, 0),
+            sampling_ratio=self.cfg.BOX_HEAD.SAMPLING_RATIO,
+        )
+        roi_features_1 = flow.detection.roi_align(
+            in_blob=features[1],
+            rois=flow.local_gather(
+                proposals_with_img_ids, flow.squeeze(level_idx_3, axis=[1])
+            ),
+            pooled_h=self.cfg.BOX_HEAD.POOLED_H,
+            pooled_w=self.cfg.BOX_HEAD.POOLED_W,
+            spatial_scale=self.cfg.BOX_HEAD.SPATIAL_SCALE / pow(2, 1),
+            sampling_ratio=self.cfg.BOX_HEAD.SAMPLING_RATIO,
+        )
+        roi_features_2 = flow.detection.roi_align(
+            in_blob=features[2],
+            rois=flow.local_gather(
+                proposals_with_img_ids, flow.squeeze(level_idx_4, axis=[1])
+            ),
+            pooled_h=self.cfg.BOX_HEAD.POOLED_H,
+            pooled_w=self.cfg.BOX_HEAD.POOLED_W,
+            spatial_scale=self.cfg.BOX_HEAD.SPATIAL_SCALE / pow(2, 2),
+            sampling_ratio=self.cfg.BOX_HEAD.SAMPLING_RATIO,
+        )
+        roi_features_3 = flow.detection.roi_align(
+            in_blob=features[3],
+            rois=flow.local_gather(
+                proposals_with_img_ids, flow.squeeze(level_idx_5, axis=[1])
+            ),
+            pooled_h=self.cfg.BOX_HEAD.POOLED_H,
+            pooled_w=self.cfg.BOX_HEAD.POOLED_W,
+            spatial_scale=self.cfg.BOX_HEAD.SPATIAL_SCALE / pow(2, 3),
+            sampling_ratio=self.cfg.BOX_HEAD.SAMPLING_RATIO,
+        )
+        roi_features = flow.concat(
+            [roi_features_0, roi_features_1, roi_features_2, roi_features_3], axis=0
+        )
+        origin_indices = flow.concat(
+            [level_idx_2, level_idx_3, level_idx_4, level_idx_5], axis=0
+        )
+        x = flow.local_scatter_nd_update(
+            flow.constant_like(roi_features, 0), origin_indices, roi_features
+        )
+        for i in range(1, 5):
+            x = flow.layers.conv2d(
+                inputs=x,
+                filters=256,
+                kernel_size=[3, 3],
+                strides=[1, 1],
+                padding="SAME",
+                data_format="NCHW",
+                dilation_rate=[1, 1],
+                activation=flow.keras.math.relu,
+                use_bias=True,
+                name="fcn{}".format(i),
+            )
+
+        return x
+
+    def mask_predictor(self, x):
+        x = flow.detection.deconv(
+            x,
+            filters=256,
+            kernel_size=[2, 2],
+            data_format="channels_first",
+            padding="same",
+            strides=[2, 2],
+            activation=flow.keras.math.relu,
+            name="conv5",
+        )
+
+        x = flow.layers.conv2d(
+            x,
+            filters=81,
+            kernel_size=[1, 1],
+            data_format="NCHW",
+            padding="SAME",
+            strides=[1, 1],
+            dilation_rate=[1, 1],
+            name="fcn_logits",
+        )
+
+        return x
