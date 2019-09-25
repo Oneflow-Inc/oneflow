3e951b0bde46bbb1b0fe9351889a53d942499576
Juncheng <liujuncheng1022@gmail.com>
2019-09-25 14:10:15 +0800

    Dev pr boxing v2 (#2248)
    * NcclDeviceCtx
    
    * include naive_actor
    
    * refine
    
    * use_boxing_v2
    
    * config.use_boxing_v2
    
    * SubTskGphBuilder
    
    * fix
    
    * hash<oneflow::MemoryCase>
    
    * Maybe<void>
    
    * ChainSubTskGphBuilder
    
    * SliceBoxingOp
    
    * return ok
    
    * SliceBoxingKernel
    
    * SliceBoxingActor
    
    * kSliceBoxing
    
    * nccl boxing op
    
    * nccl actor
    
    * REGISTER_OP
    
    * GetMsgFromCustomizedConf
    
    * NcclBoxingTaskNode
    
    * BldSubTskGphByBoxingV2
    
    * NcclBoxingSubTskGphBuilder
    
    * fix
    
    * fix
    
    * NcclKernel
    
    * ParallelContext
    
    * REGISTER_ACTOR
    
    * fix rank set
    
    * IsNcclTaskType
    
    * limit
    
    * 1024
    
    * multi thread reader
    
    * thread_num
    
    * IsKernelLaunchSynchronized
    
    * refine
    
    * NcclTupleReduce/BroadcastKernel use NcclDeviceCtx
    
    * MakeHostMemCase
    
    * NcclBldSubTskGph
    
    * remove use less code
    
    * use_boxing_v2
    
    * refine
    
    * refine
    
    * refine
    
    * refine
    
    * refine

--- oneflow/core/actor/nccl_actor.cpp
+++ oneflow/core/actor/nccl_actor.cpp
@@ -1,24 +1,37 @@
 #include "oneflow/core/job/nccl_comm_manager.h"
-#include "oneflow/core/actor/nccl_actor.h"
 #include "oneflow/core/device/nccl_device_context.h"
+#include "oneflow/core/actor/naive_actor.h"
 
 namespace oneflow {
 
-void NcclActor::InitDeviceCtx(const ThreadCtx& thread_ctx) {
 #ifdef WITH_CUDA
+
+class NcclActor final : public NaiveActor {
+ public:
+  NcclActor() = default;
+  ~NcclActor() override = default;
+
+ private:
+  void InitDeviceCtx(const ThreadCtx& thread_ctx) override;
+};
+
+void NcclActor::InitDeviceCtx(const ThreadCtx& thread_ctx) {
   CHECK_EQ(GetDeviceType(), DeviceType::kGPU);
-  // CHECK_EQ(GetLocalWorkStreamId(), 0);
-  mut_device_ctx().reset(new NcclDeviceCtx(
-      thread_ctx.g_cuda_stream.get(), Global<NcclCommMgr>::Get()->NcclComm4ActorId(actor_id())));
-#else
-  UNIMPLEMENTED();
-#endif  // WITH_CUDA
+  mut_device_ctx().reset(
+      new NcclDeviceCtx(Global<NcclCommMgr>::Get()->NcclComm4ActorId(actor_id())));
 }
 
 REGISTER_ACTOR(TaskType::kNcclAllReduce, NcclActor);
 REGISTER_ACTOR(TaskType::kNcclReduceScatter, NcclActor);
 REGISTER_ACTOR(TaskType::kNcclAllGather, NcclActor);
+
 REGISTER_ACTOR(TaskType::kNcclTupleBroadcast, NcclActor);
 REGISTER_ACTOR(TaskType::kNcclTupleReduce, NcclActor);
+
+REGISTER_ACTOR(TaskType::kNcclBoxingAllReduce, NcclActor);
+REGISTER_ACTOR(TaskType::kNcclBoxingReduceScatter, NcclActor);
+REGISTER_ACTOR(TaskType::kNcclBoxingAllGather, NcclActor);
+
+#endif  // WITH_CUDA
 
 }  // namespace oneflow
--- oneflow/core/device/nccl_device_context.h
+++ oneflow/core/device/nccl_device_context.h
@@ -2,23 +2,30 @@
 #define ONEFLOW_CORE_DEVICE_NCCL_DEVICE_CONTEXT_H_
 
 #include "oneflow/core/device/device_context.h"
-#include "oneflow/core/device/cuda_device_context.h"
+#include "oneflow/core/device/cuda_stream_handle.h"
 
 namespace oneflow {
 
 #ifdef WITH_CUDA
 
-class NcclDeviceCtx final : public CudaDeviceCtx {
+class NcclDeviceCtx final : public DeviceCtx {
  public:
   OF_DISALLOW_COPY_AND_MOVE(NcclDeviceCtx);
-  NcclDeviceCtx(CudaStreamHandle* cuda_handler, ncclComm_t nccl_handler)
-      : CudaDeviceCtx(cuda_handler), nccl_handler_(nccl_handler) {}
-  ~NcclDeviceCtx() override = default;
+  explicit NcclDeviceCtx(ncclComm_t nccl_handler);
+  ~NcclDeviceCtx() override;
 
   const ncclComm_t& nccl_handle() const override { return nccl_handler_; }
+  const cudaStream_t& cuda_stream() const override { return cuda_stream_; }
+  void AddCallBack(std::function<void()> callback) const override;
+  void Enqueue(const std::function<void()>& callback) const;
 
  private:
   ncclComm_t nccl_handler_;
+  cudaStream_t cuda_stream_;
+  mutable Channel<std::function<void()>> cpu_task_chan_;
+  mutable Channel<CudaCBEvent> gpu_cb_event_chan_;
+  std::thread cpu_task_poller_;
+  std::thread gpu_cb_event_poller_;
 };
 
 #endif  // WITH_CUDA
