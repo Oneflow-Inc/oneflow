c593dd212dd386a04c4dc2af847392f240f431b5
Tsai <caishenghang@oneflow.org>
2019-09-25 18:25:32 +0800

    uncomment everything
    Former-commit-id: e4d2e91b074c8e94bd6322854c30dc3bb7f22cc7

--- oneflow/python/model/maskrcnn/maskrcnn.py
+++ oneflow/python/model/maskrcnn/maskrcnn.py
@@ -106,13 +106,15 @@ def maskrcnn(
     )
 
     # Mask Head
-    # mask_loss = mask_head.build_train(
-    #     pos_proposal_list, pos_gt_indices_list, gt_segms_list, gt_labels_list, features
-    # )
+    mask_loss = mask_head.build_train(
+        pos_proposal_list,
+        pos_gt_indices_list,
+        gt_segms_list,
+        gt_labels_list,
+        features,
+    )
 
-    # return rpn_bbox_loss, rpn_objectness_loss, box_loss, cls_loss, mask_loss
-
-    return rpn_bbox_loss, rpn_objectness_loss
+    return rpn_bbox_loss, rpn_objectness_loss, box_loss, cls_loss, mask_loss
 
 
 if __name__ == "__main__":
