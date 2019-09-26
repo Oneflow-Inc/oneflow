43be127e65806ecbc70526e4ab8b7b9d812b63b1
Li Xinqi <lixinqi2010@gmail.com>
2019-09-26 15:56:02 +0800

    Infer dense shape (#2273)
    * clean kernel.proto
    
    * Kernel::parallel_ctx()
    
    * rm kernel_conf.parallel_ctx
    
    * reformat kernel.proto
    
    * support kernel with dynamic dense blob shape
    
    
    Former-commit-id: fbdf4c4f041f8ad8852f9fb5a8a9e9f7fd54f9c8

--- oneflow/core/operator/copy_hd_op.cpp
+++ oneflow/core/operator/copy_hd_op.cpp
@@ -1,10 +1,33 @@
-#include "oneflow/core/operator/copy_hd_op.h"
+#include "oneflow/core/operator/operator.h"
 
 namespace oneflow {
+
+class CopyHdOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(CopyHdOp);
+  CopyHdOp() = default;
+  ~CopyHdOp() = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override;
+
+ private:
+  LogicalBlobId ibn2lbi(const std::string& input_bn) const override { return GenPackedLbi(); }
+  LogicalBlobId obn2lbi(const std::string& output_bn) const override { return GenPackedLbi(); }
+};
 
 void CopyHdOp::InitFromOpConf() {
   EnrollInputBn("in", false);
   EnrollOutputBn("out", false);
+}
+
+Maybe<void> CopyHdOp::InferBlobDescs(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx) const {
+  *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
+  return Maybe<void>::Ok();
 }
 
 const PbMessage& CopyHdOp::GetCustomizedConf() const { return op_conf().copy_hd_conf(); }
