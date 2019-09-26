494a7962c36ccdb4f18de1aeb56c067dbf8be673
leaves-zwx <kunta0932@gmail.com>
2019-09-26 16:48:33 +0800

    Dev gather scatter nd (#2276)
    * unsuccessed compiling
    
    * fix
    
    * gather_nd_op
    
    * gather_nd op & kernel
    
    * local_scatter_nd_upate autograd
    
    * minor fix

---
+++ oneflow/core/job_completer/local_scatter_nd_update_grad.cpp
@@ -0,0 +1,29 @@
+#include "oneflow/core/job_completer/autograd.h"
+
+namespace oneflow {
+
+namespace {
+
+void GenerateBackwardOpConf(
+    const Operator& op, std::vector<OperatorConf>* op_confs,
+    const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp,
+    const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4BnInOp) {
+  CHECK(op.op_conf().has_local_scatter_nd_update_conf());
+  if (DiffLbi4BnInOp("in") != nullptr) {
+    OperatorConf grad_op_conf;
+    grad_op_conf.set_name("System-AutoGrad-" + op.op_name());
+    auto* conf = grad_op_conf.mutable_local_gather_nd_conf();
+    conf->set_in(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    conf->set_indices(GenLogicalBlobName(op.BnInOp2Lbi("indices")));
+    conf->set_out("out");
+    op_confs->push_back(grad_op_conf);
+    DiffLbi4BnInOp("in")->set_op_name(grad_op_conf.name());
+    DiffLbi4BnInOp("in")->set_blob_name(conf->out());
+  }
+}
+
+}  // namespace
+
+REGISTER_OP_GRAD(OperatorConf::kLocalScatterNdUpdateConf, &GenerateBackwardOpConf);
+
+}  // namespace oneflow
