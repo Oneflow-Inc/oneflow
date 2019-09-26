660613c54a44ec5fe526261a9ec8e3193a099d6f
Tsai <caishenghang@oneflow.org>
2019-09-26 14:57:40 +0800

    fix autograd
    Former-commit-id: a6b9900e6df274ec14ecee23dcb21380a0f77311

--- oneflow/core/job_completer/autograd.cpp
+++ oneflow/core/job_completer/autograd.cpp
@@ -449,7 +449,7 @@ void AutoGrad(const OpGraph& op_graph, JobBuilder* job_builder,
         if (out_diff_lbi_it == out_oba2out_diff_lbi.end()) { return nullptr; }
         return &out_diff_lbi_it->second;
       } else {
-        UNIMPLEMENTED();
+        LOG(FATAL) << "diff lbi for bn in op not found, bn: " << op_name << "/" << bn;
       }
     };
     auto LogicalBlobDesc4BnInOp = [&](const std::string& bn) -> const BlobDesc& {
