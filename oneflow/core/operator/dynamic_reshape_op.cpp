d382b35346929d8834cfbe267cfec05a757c7ca8
Dounm <niuchong893184@gmail.com>
2019-09-26 18:45:43 +0800

    fix: fix dynamic_reshape_op

--- oneflow/core/operator/dynamic_reshape_op.cpp
+++ oneflow/core/operator/dynamic_reshape_op.cpp
@@ -13,29 +13,25 @@ class DynamicReshapeOp final : public Operator {
   Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                              const ParallelContext* parallel_ctx,
                              const SbpSignature* sbp_signature) const {
-    const BlobDesc* in = GetBlobDesc4BnInOp("in");
-    BlobDesc* out = GetBlobDesc4BnInOp("out");
-    *out = *in;
     const DynamicReshapeOpConf& conf = op_conf().dynamic_reshape_conf();
     int32_t inferred_axis = -1;
-    int32_t num_inferred_axis = 0;
-    const std::vector<int64_t> conf_dim_vec = {conf.shape().dim().begin(),
-                                               conf.shape().dim().end()};
-    std::vector<int64_t> out_dim_vec = {};
-    for (int32_t i = 0; i < conf_dim_vec.size(); ++i) {
-      if (conf_dim_vec[i] == -1) {
+    Shape out_shape(conf.shape());
+    std::vector<int64_t> tmp_dim_vec;
+    for (int32_t i = 0; i < out_shape.NumAxes(); ++i) {
+      if (out_shape.At(i) == -1) {
+        CHECK_EQ_OR_RETURN(-1, inferred_axis);
         inferred_axis = i;
-        num_inferred_axis = 1;
       } else {
-        CHECK_GT_OR_RETURN(conf_dim_vec[i], 0);
-        out_dim_vec.push_back(conf_dim_vec[i]);
+        CHECK_GT_OR_RETURN(out_shape.At(i), 0);
+        tmp_dim_vec.push_back(out_shape.At(i));
       }
     }
     CHECK_GE_OR_RETURN(inferred_axis, 0);
-    CHECK_EQ_OR_RETURN(num_inferred_axis, 1);
-    out_dim_vec.insert(out_dim_vec.begin() + inferred_axis,
-                       in->shape().elem_cnt() / out->shape().elem_cnt());
-    out->mut_shape() = Shape(out_dim_vec);
+    const BlobDesc* in = GetBlobDesc4BnInOp("in");
+    out_shape.Set(inferred_axis, in->shape().elem_cnt() / Shape(tmp_dim_vec).elem_cnt());
+    BlobDesc* out = GetBlobDesc4BnInOp("out");
+    *out = *in;
+    out->mut_shape() = std::move(out_shape);
     return Maybe<void>::Ok();
   }
 
