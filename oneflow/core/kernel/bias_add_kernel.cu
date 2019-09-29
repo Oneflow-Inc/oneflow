e7dc6c02e004adf50e4539bc8d98ed6566bc3b6f
liujuncheng <liujuncheng1022@gmail.com>
2019-09-29 15:56:27 +0800

    fix half

--- oneflow/core/kernel/bias_add_kernel.cu
+++ oneflow/core/kernel/bias_add_kernel.cu
@@ -5,11 +5,10 @@ namespace oneflow {
 namespace {
 
 template<typename T>
-__global__ void BiasAddGpu(int64_t elem_cnt, int64_t bias_size, int64_t inner_size, const T* x,
-                           const T* bias, T* y) {
-  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
-    y[i] = x[i] + bias[i % (inner_size * bias_size) / inner_size];
-  }
+__global__ void BiasAddGpu(const int64_t elem_cnt, const int64_t bias_size,
+                           const int64_t inner_size, const T* x, const T* bias, T* y) {
+  const int64_t block_size = bias_size * inner_size;
+  CUDA_1D_KERNEL_LOOP(i, elem_cnt) { y[i] = x[i] + bias[i % block_size / inner_size]; }
 }
 
 }  // namespace
@@ -36,11 +35,11 @@ struct BiasAddUtil<DeviceType::kGPU, float16> final {
 };
 #endif
 
-#define INITIATE_BIAS_ADD_KERNEL_UTIL_GPU_IMPL(in_type_pair) \
-  template struct BiasAddUtil<DeviceType::kGPU, OF_PP_PAIR_FIRST(in_type_pair)>;
-OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INITIATE_BIAS_ADD_KERNEL_UTIL_GPU_IMPL, ARITHMETIC_DATA_TYPE_SEQ
+#define INITIATE_BIAS_ADD_KERNEL_UTIL_GPU_IMPL(type_cpp, type_proto) \
+  template struct BiasAddUtil<DeviceType::kGPU, type_cpp>;
+OF_PP_FOR_EACH_TUPLE(INITIATE_BIAS_ADD_KERNEL_UTIL_GPU_IMPL, ARITHMETIC_DATA_TYPE_SEQ
 #if defined(__CUDA_ARCH__) && __CUDA_ARCH__ >= 700
-                                                                             FLOAT16_DATA_TYPE_SEQ
+                                                                 FLOAT16_DATA_TYPE_SEQ
 #endif
 );
 #undef INITIATE_BIAS_ADD_KERNEL_UTIL_GPU_IMPL
