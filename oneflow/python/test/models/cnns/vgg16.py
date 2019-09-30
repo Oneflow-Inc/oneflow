1847269d02c070c671d12fbc6e143528e11ece7e
QiaoJing <mir-of@outlook.com>
2019-09-30 17:27:36 +0800

    Fix dev python test (#2294)
    * add decode random
    
    * fix decode random actor
    
    * fix dev_python test scripts
    
    * fix batch_size test scripts
    
    * fix
    
    
    Former-commit-id: 345a0097b04c1fb2e15a062d1f88a19091e45f7f

--- oneflow/python/test/models/cnns/vgg16.py
+++ oneflow/python/test/models/cnns/vgg16.py
@@ -44,7 +44,8 @@ parser.add_argument("-load", "--model_load_dir", type=str,
 parser.add_argument(
     "-save", "--model_save_dir", type=str, default=_MODEL_SAVE_DIR, required=False
 )
-
+parser.add_argument("-dn", "--data_part_num", type=int, default=32, required=False)
+parser.add_argument("-b", "--batch_size", type=int, default=8, required=False)
 args = parser.parse_args()
 
 
@@ -104,9 +105,11 @@ def _data_load_layer(data_dir):
         "class/label", shape=(), dtype=flow.int32, codec=flow.data.RawCodec()
     )
 
+    node_num = len(args.node_list.strip().split(',')) if args.multinode else 1
+    total_batch_size = args.batch_size * args.gpu_num_per_node * node_num
     return flow.data.decode_ofrecord(
         data_dir, (label_blob_conf, image_blob_conf),
-        batch_size=8, data_part_num=32, name="decode"
+        batch_size=total_batch_size, data_part_num=args.data_part_num, name="decode",
     )
 
 
