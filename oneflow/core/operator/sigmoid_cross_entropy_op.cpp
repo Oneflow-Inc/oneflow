7afa8fb1a945d1eff1dd321f869e5d7cf5dbbb31
Tsai <caishenghang@oneflow.org>
2019-10-01 17:28:03 +0800

    check in fw impl

---
+++ oneflow/core/operator/sigmoid_cross_entropy_op.cpp
@@ -0,0 +1,50 @@
+#include "oneflow/core/operator/operator.h"
+#include "oneflow/core/kernel/kernel_util.h"
+
+namespace oneflow {
+
+class SigmoidCrossEntropyOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SigmoidCrossEntropyOp);
+  SigmoidCrossEntropyOp() = default;
+  ~SigmoidCrossEntropyOp() = default;
+
+  void InitFromOpConf() override {
+    CHECK(op_conf().has_sigmoid_cross_entropy_loss_conf());
+    EnrollInputBn("prediction");
+    EnrollInputBn("label", false);
+    EnrollOutputBn("loss");
+  }
+
+  const PbMessage& GetCustomizedConf() const override {
+    return op_conf().sigmoid_cross_entropy_conf();
+  }
+
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override {
+    *GetBlobDesc4BnInOp("loss") = *GetBlobDesc4BnInOp("prediction");
+    return Maybe<void>::Ok();
+  }
+
+ private:
+  Maybe<void> InferBatchAxis(
+      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override {
+    return NaiveInferBatchAxis(BatchAxis4BnInOp);
+  }
+
+  Maybe<void> GetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      SbpSignatureList* sbp_sig_list) const {
+    SbpSignatureBuilder()
+        .Split(input_bns(), 0)
+        .Split(output_bns(), 0)
+        .MakeSplitSignatureListBuilder(
+            JUST(LogicalBlobDesc4Ibn(input_bns().Get(0)))->shape().NumAxes())
+        .Build(sbp_sig_list);
+    return Maybe<void>::Ok();
+  }
+};
+
+REGISTER_OP(OperatorConf::kSigmoidCrossEntropyConf, SigmoidCrossEntropyOp);
+
+}  // namespace oneflow
