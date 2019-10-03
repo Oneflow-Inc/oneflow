eeba117724549e155d0469362210007c84b37cb8
Tsai <caishenghang@oneflow.org>
2019-10-03 11:55:03 +0800

    rm roi align kernel util and adopt new naming convention
    Former-commit-id: cc92706a659de09e98984077d7454dbfdcf16de6

--- oneflow/core/operator/roi_align_op.cpp
+++ oneflow/core/operator/roi_align_op.cpp
@@ -18,20 +18,20 @@ class RoiAlignOp final : public Operator {
 
   Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                              const ParallelContext* parallel_ctx) const override {
-    const RoiAlignConf& roi_align_conf = op_conf().roi_align_conf().roi_align_conf();
-    if (roi_align_conf.data_format() != "channels_first") { UNIMPLEMENTED(); }
-    // in: feature map (N, C, H, W)
+    const RoiAlignArgs& roi_align_args = op_conf().roi_align_conf().roi_align_args();
+    if (roi_align_args.data_format() != "channels_first") { UNIMPLEMENTED(); }
+    // x: feature map (N, C, H, W)
     const BlobDesc* x_blob_desc = GetBlobDesc4BnInOp("x");
     CHECK_EQ(x_blob_desc->shape().NumAxes(), 4);
     // rois: (R, 5)
     const BlobDesc* rois_blob_desc = GetBlobDesc4BnInOp("rois");
     CHECK_EQ(rois_blob_desc->shape().NumAxes(), 2);
     CHECK_EQ(rois_blob_desc->shape().At(1), 5);
-    // out: (R, C, pool_h, pool_w)
+    // y: (R, C, pool_h, pool_w)
     BlobDesc* y_blob_desc = GetBlobDesc4BnInOp("y");
     *y_blob_desc = *rois_blob_desc;
     y_blob_desc->mut_shape() = Shape({rois_blob_desc->shape().At(0), x_blob_desc->shape().At(1),
-                                      roi_align_conf.pooled_h(), roi_align_conf.pooled_w()});
+                                      roi_align_args.pooled_h(), roi_align_args.pooled_w()});
     y_blob_desc->set_data_type(x_blob_desc->data_type());
     return Maybe<void>::Ok();
   }
@@ -39,10 +39,6 @@ class RoiAlignOp final : public Operator {
  private:
   Maybe<void> InferBatchAxis(
       std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override {
-    oneflow::OptInt64* x_split_axis = BatchAxis4BnInOp("x");
-    if (x_split_axis->has_value()) { CHECK_EQ_OR_RETURN(x_split_axis->value(), 0); }
-    oneflow::OptInt64* roi_split_axis = BatchAxis4BnInOp("rois");
-    if (roi_split_axis->has_value()) { CHECK_EQ_OR_RETURN(x_split_axis->value(), 0); }
     *BatchAxis4BnInOp("y") = *BatchAxis4BnInOp("rois");
     return Maybe<void>::Ok();
   }
@@ -75,19 +71,19 @@ class RoiAlignGradOp final : public Operator {
 
   Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                              const ParallelContext* parallel_ctx) const override {
-    const RoiAlignConf& roi_align_conf = op_conf().roi_align_grad_conf().roi_align_conf();
-    if (roi_align_conf.data_format() != "channels_first") { UNIMPLEMENTED(); }
-    // in: feature map (N, C, H, W)
+    const RoiAlignArgs& roi_align_args = op_conf().roi_align_grad_conf().roi_align_args();
+    if (roi_align_args.data_format() != "channels_first") { UNIMPLEMENTED(); }
+    // x: feature map (N, C, H, W)
     const BlobDesc* x_like_blob_desc = GetBlobDesc4BnInOp("x_like");
     CHECK_EQ_OR_RETURN(x_like_blob_desc->shape().NumAxes(), 4);
     // rois: (R, 5)
     const BlobDesc* rois_blob_desc = GetBlobDesc4BnInOp("rois");
     CHECK_EQ_OR_RETURN(rois_blob_desc->shape().NumAxes(), 2);
     CHECK_EQ_OR_RETURN(rois_blob_desc->shape().At(1), 5);
-    // out: (R, C, pool_h, pool_w)
+    // y: (R, C, pool_h, pool_w)
     BlobDesc* dy_blob_desc = GetBlobDesc4BnInOp("dy");
     const Shape& y_shape = Shape({rois_blob_desc->shape().At(0), x_like_blob_desc->shape().At(1),
-                                  roi_align_conf.pooled_h(), roi_align_conf.pooled_w()});
+                                  roi_align_args.pooled_h(), roi_align_args.pooled_w()});
     CHECK_EQ_OR_RETURN(y_shape, dy_blob_desc->shape());
     CHECK_EQ_OR_RETURN(dy_blob_desc->data_type(), x_like_blob_desc->data_type());
     GetBlobDesc4BnInOp("dx")->CopyMetaFrom(*GetBlobDesc4BnInOp("x_like"));
@@ -97,12 +93,6 @@ class RoiAlignGradOp final : public Operator {
  private:
   Maybe<void> InferBatchAxis(
       std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override {
-    oneflow::OptInt64* x_split_axis = BatchAxis4BnInOp("x_like");
-    if (x_split_axis->has_value()) { CHECK_EQ_OR_RETURN(x_split_axis->value(), 0); }
-    oneflow::OptInt64* dy_split_axis = BatchAxis4BnInOp("dy");
-    if (dy_split_axis->has_value()) { CHECK_EQ_OR_RETURN(dy_split_axis->value(), 0); }
-    oneflow::OptInt64* roi_split_axis = BatchAxis4BnInOp("rois");
-    if (roi_split_axis->has_value()) { CHECK_EQ_OR_RETURN(x_split_axis->value(), 0); }
     *BatchAxis4BnInOp("dx") = *BatchAxis4BnInOp("x_like");
     return Maybe<void>::Ok();
   }
