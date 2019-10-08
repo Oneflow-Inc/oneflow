45a2b412763295becf5d999ec7652ba350571213
Dounm <niuchong893184@gmail.com>
2019-10-08 20:31:53 +0800

    fix: add PoolingGradOp to pooling_ops.cpp

--- oneflow/core/operator/pooling_ops.cpp
+++ oneflow/core/operator/pooling_ops.cpp
@@ -282,4 +282,76 @@ REGISTER_OP(OperatorConf::kAveragePooling3DConf, AveragePooling3DOp);
 
 }  // namespace average_pooling
 
+class PoolingGradOp : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(PoolingGradOp);
+  PoolingGradOp() = default;
+  virtual ~PoolingGradOp() = default;
+
+  void InitFromOpConf() override;
+
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override;
+
+ private:
+  const PbMessage& GetCustomizedConf() const override { return op_conf().pooling_grad_conf(); }
+  void CheckPoolSizeAndStrides() const;
+  Maybe<void> InferBatchAxis(
+      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override {
+    return NaiveInferBatchAxis(BatchAxis4BnInOp);
+  }
+  Maybe<void> GetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      SbpSignatureList* sbp_sig_list) const override;
+};
+
+void PoolingGradOp::InitFromOpConf() {
+  const PoolingConf& pooling_conf = this->op_conf().pooling_grad_conf().pooling_conf();
+  std::string pool_mode = pooling_conf.pool_mode();
+  std::transform(pool_mode.begin(), pool_mode.end(), pool_mode.begin(), ::tolower);
+  if (pool_mode != "avg" && pool_mode != "max") {
+    LOG(FATAL) << "Invalid pooling type in " << op_name() << ". It must be avg or max";
+  }
+  CheckPoolSizeAndStrides();
+  EnrollInputBn("x");
+  EnrollInputBn("y");
+  EnrollInputBn("dy");
+  EnrollOutputBn("dx");
+}
+
+Maybe<void> PoolingGradOp::InferBlobDescs(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx) const {
+  // in
+  const BlobDesc* x_blob_desc = GetBlobDesc4BnInOp("x");
+  CHECK_GE_OR_RETURN(x_blob_desc->shape().NumAxes(), 3);
+  CHECK_LE_OR_RETURN(x_blob_desc->shape().NumAxes(), 5);
+  // out
+  *GetBlobDesc4BnInOp("dx") = *x_blob_desc;
+  return Maybe<void>::Ok();
+}
+
+void PoolingGradOp::CheckPoolSizeAndStrides() const {
+  const PoolingConf& pooling_conf = this->op_conf().pooling_grad_conf().pooling_conf();
+  const int32_t num_spatial_dims = pooling_conf.num_spatial_dims();
+  const PbRf<int32_t>& pool_size = pooling_conf.pool_size();
+  CHECK_EQ(pool_size.size(), num_spatial_dims);
+  for (int32_t pool_dim : pool_size) { CHECK_GT(pool_dim, 0); }
+  const PbRf<int32_t>& strides = pooling_conf.strides();
+  CHECK_EQ(strides.size(), num_spatial_dims);
+  for (int32_t stride_dim : strides) { CHECK_GT(stride_dim, 0); }
+}
+
+Maybe<void> PoolingGradOp::GetSbpSignatures(
+    const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  SbpSignatureBuilder()
+      .Split(input_bns(), 0)
+      .Split(output_bns(), 0)
+      .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  return Maybe<void>::Ok();
+}
+
+REGISTER_OP(OperatorConf::kPoolingGradConf, PoolingGradOp);
+
 }  // namespace oneflow
