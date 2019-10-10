e7e44841549b00fa73c94c8c70486af22ddc6646
Li Xinqi <lixinqi2010@gmail.com>
2019-10-10 19:14:33 +0800

    distribute_concat (#2315)
    * distribute_concat
    
    * debug/distribute_concat
    
    
    Former-commit-id: 0702256f02c8bf6ad9fb350032eb8fe9a9256880

--- oneflow/core/common/error.cpp
+++ oneflow/core/common/error.cpp
@@ -4,6 +4,15 @@
 
 namespace oneflow {
 
+namespace {
+
+void LogError(const std::string& log_str, const Error& error) {
+  // gdb break point
+  LOG(ERROR) << log_str << error->msg();
+}
+
+}  // namespace
+
 Error::operator std::string() const { return PbMessage2TxtString(*error_proto_); }
 
 Error Error::Ok() { return std::make_shared<ErrorProto>(); }
@@ -57,7 +66,7 @@ Error Error::BoxingNotSupported() {
 }
 
 Error&& operator<=(const std::string& log_str, Error&& error) {
-  LOG(ERROR) << log_str << error->msg();
+  LogError(log_str, error);
   return std::move(error);
 }
 
