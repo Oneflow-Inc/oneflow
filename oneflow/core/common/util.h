a67c372ab208fbd3f7791708445af750facab079
liujuncheng <liujuncheng1022@gmail.com>
2019-10-11 14:28:08 +0800

    kCudaAlignSize = 256

--- oneflow/core/common/util.h
+++ oneflow/core/common/util.h
@@ -180,7 +180,7 @@ inline double GetCurTime() {
   return std::chrono::high_resolution_clock::now().time_since_epoch().count();
 }
 
-const size_t kCudaAlignSize = 8;
+const size_t kCudaAlignSize = 256;
 const size_t kCudaMemAllocAlignSize = 256;
 inline size_t RoundUp(size_t n, size_t val) { return (n + val - 1) / val * val; }
 
