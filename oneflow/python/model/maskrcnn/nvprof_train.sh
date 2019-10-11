0e83ba3b92deba4ffe092dd90fc8209d763eade2
scxfjiang <sc.xfjiang@gmail.com>
2019-10-11 22:07:50 +0800

    reduce_max

--- oneflow/python/model/maskrcnn/nvprof_train.sh
+++ oneflow/python/model/maskrcnn/nvprof_train.sh
@@ -1,1 +1,2 @@
-/usr/local/cuda/bin/nvprof -o of_prof.nvvp python maskrcnn.py -rpn -md
+# /usr/local/cuda/bin/nvprof -f -o of_prof.nvvp python maskrcnn.py -md -c="mask_rcnn_R_50_FPN_1x.yaml"
+/usr/local/cuda/bin/nvprof python maskrcnn.py -md -c="mask_rcnn_R_50_FPN_1x.yaml"
