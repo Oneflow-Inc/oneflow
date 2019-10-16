7069e58ef7b6f00b8dbb7e11f88e2df4298d92be
scxfjiang <sc.xfjiang@gmail.com>
2019-10-16 17:48:26 +0800

    fix

--- oneflow/core/register/blob.cpp
+++ oneflow/core/register/blob.cpp
@@ -54,6 +54,6 @@ void Blob::CopyHeaderFrom(DeviceCtx* device_ctx, const Blob* rhs) {
                            blob_desc().ByteSizeOfBlobHeader());
 }
 
-void Blob::IsShapeEmpty() const { return shape().elem_cnt() == 0; }
+bool Blob::IsShapeEmpty() const { return shape().elem_cnt() == 0; }
 
 }  // namespace oneflow
