d4f97de721099ec3b2a947f9b5df28e25def15cf
ShawnXuan <xiexuanx2@gmail.com>
2019-10-18 15:18:11 +0800

    add mAP files (#2350)

---
+++ oneflow/python/model/maskrcnn/eval/box_head_inference.py
@@ -0,0 +1,162 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+import numpy as np
+
+from bounding_box import BoxList
+from boxlist_ops import boxlist_nms
+from boxlist_ops import cat_boxlist
+from box_coder import BoxCoder
+
+
+class PostProcessor():
+    """
+    From a set of classification scores, box regression and proposals,
+    computes the post-processed boxes, and applies NMS to obtain the
+    final results
+    """
+
+    def __init__(
+        self,
+        score_thresh=0.05,
+        nms=0.5,
+        detections_per_img=100,
+        box_coder=None,
+        cls_agnostic_bbox_reg=False,
+        bbox_aug_enabled=False
+    ):
+        """
+        Arguments:
+            score_thresh (float)
+            nms (float)
+            detections_per_img (int)
+            box_coder (BoxCoder)
+        """
+        self.score_thresh = score_thresh
+        self.nms = nms
+        self.detections_per_img = detections_per_img
+        if box_coder is None:
+            box_coder = BoxCoder(weights=(10., 10., 5., 5.))
+        self.box_coder = box_coder
+        self.cls_agnostic_bbox_reg = cls_agnostic_bbox_reg
+        self.bbox_aug_enabled = bbox_aug_enabled
+
+    def forward(self, x, boxes):
+        """
+        Arguments:
+            x (tuple[tensor, tensor]): x contains the class logits
+                and the box_regression from the model.
+            boxes (list[BoxList]): bounding boxes that are used as
+                reference, one for each image
+
+        Returns:
+            results (list[BoxList]): one BoxList for each image, containing
+                the extra fields labels and scores
+        """
+        class_prob, box_regression = x
+
+        # TODO think about a representation of batch of boxes
+        image_shapes = [box.size for box in boxes]
+        boxes_per_image = np.array([len(box) for box in boxes]).cumsum()
+        concat_boxes = np.concatenate([a.bbox for a in boxes], axis=0)
+
+        #if self.cls_agnostic_bbox_reg:
+        #    box_regression = box_regression[:, -4:]
+        proposals = self.box_coder.decode(box_regression, concat_boxes)
+        #if self.cls_agnostic_bbox_reg:
+        #    proposals = proposals.repeat(1, class_prob.shape[1])
+
+        num_classes = class_prob.shape[1]
+
+        proposals = np.squeeze(np.split(proposals, boxes_per_image, axis=0))[:-1]
+        class_prob = np.squeeze(np.split(class_prob, boxes_per_image, axis=0))[:-1]
+
+        results = []
+        for prob, boxes_per_img, image_shape in zip(class_prob, proposals, image_shapes):
+            boxlist = self.prepare_boxlist(boxes_per_img, prob, image_shape)
+            boxlist = boxlist.clip_to_image(remove_empty=False)
+            if not self.bbox_aug_enabled:
+                boxlist = self.filter_results(boxlist, num_classes)
+            results.append(boxlist)
+        return results
+
+    def prepare_boxlist(self, boxes, scores, image_shape):
+        """
+        Returns BoxList from `boxes` and adds probability scores information
+        as an extra field
+        `boxes` has shape (#detections, 4 * #classes), where each row represents
+        a list of predicted bounding boxes for each of the object classes in the
+        dataset (including the background class). The detections in each row
+        originate from the same object proposal.
+        `scores` has shape (#detection, #classes), where each row represents a list
+        of object detection confidence scores for each of the object classes in the
+        dataset (including the background class). `scores[i, j]`` corresponds to the
+        box at `boxes[i, j * 4:(j + 1) * 4]`.
+        """
+        boxes = boxes.reshape(-1, 4)
+        scores = scores.reshape(-1)
+        boxlist = BoxList(boxes, image_shape, mode="xyxy")
+        boxlist.add_field("scores", scores)
+        return boxlist
+
+    def filter_results(self, boxlist, num_classes):
+        """Returns bounding-box detection results by thresholding on scores and
+        applying non-maximum suppression (NMS).
+        """
+        # unwrap the boxlist to avoid additional overhead.
+        # if we had multi-class NMS, we could perform this directly on the boxlist
+        boxes = boxlist.bbox.reshape(-1, num_classes * 4)
+        scores = boxlist.get_field("scores").reshape(-1, num_classes)
+
+        result = []
+        # Apply threshold on detection probabilities and apply NMS
+        # Skip j = 0, because it's the background class
+        inds_all = scores > self.score_thresh
+        for j in range(1, num_classes):
+            inds = inds_all[:, j].nonzero()[0]
+            if len(inds) == 0:
+                continue
+            scores_j = scores[inds, j]
+            boxes_j = boxes[inds, j * 4 : (j + 1) * 4]
+            boxlist_for_class = BoxList(boxes_j, boxlist.size, mode="xyxy")
+            boxlist_for_class.add_field("scores", scores_j)
+            boxlist_for_class = boxlist_nms(boxlist_for_class, self.nms)
+            num_labels = len(boxlist_for_class)
+            boxlist_for_class.add_field("labels", np.full((num_labels,), j, dtype=np.int64))
+            result.append(boxlist_for_class)
+
+        result = cat_boxlist(result)
+        number_of_detections = len(result)
+
+        # Limit to max_per_image detections **over all classes**
+        if number_of_detections > self.detections_per_img > 0:
+            cls_scores = result.get_field("scores")
+            #image_thresh, _ = torch.kthvalue(
+            #    cls_scores, number_of_detections - self.detections_per_img + 1
+            #)
+            #keep = cls_scores >= image_thresh.item()
+            #keep = np.nonzero(keep).squeeze(1)
+            keep = cls_scores.argsort()[-self.detections_per_img:]
+            result = result[keep]
+        return result
+
+
+def make_roi_box_post_processor(cfg):
+    use_fpn = cfg.MODEL.ROI_HEADS.USE_FPN
+
+    bbox_reg_weights = cfg.MODEL.ROI_HEADS.BBOX_REG_WEIGHTS
+    box_coder = BoxCoder(weights=bbox_reg_weights)
+
+    score_thresh = cfg.MODEL.ROI_HEADS.SCORE_THRESH
+    nms_thresh = cfg.MODEL.ROI_HEADS.NMS
+    detections_per_img = cfg.MODEL.ROI_HEADS.DETECTIONS_PER_IMG
+    cls_agnostic_bbox_reg = cfg.MODEL.CLS_AGNOSTIC_BBOX_REG
+    bbox_aug_enabled = cfg.TEST.BBOX_AUG.ENABLED
+
+    postprocessor = PostProcessor(
+        score_thresh,
+        nms_thresh,
+        detections_per_img,
+        box_coder,
+        cls_agnostic_bbox_reg,
+        bbox_aug_enabled
+    )
+    return postprocessor
