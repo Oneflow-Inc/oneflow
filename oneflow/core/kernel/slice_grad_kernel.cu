a9cc78d3d0cbd7f1dbd163837e40503c8ba16724
scxfjiang <sc.xfjiang@gmail.com>
2019-10-25 21:35:04 +0800

    Revert "Dev support dynamic for slice op (#2365)" (#2366)
    This reverts commit 31c46dbde936a4dabf13e6b9558eb78c059572f3 [formerly 61c10f6e8ec43ad24ca1386ea3a90a11f59efee9].
    
    Former-commit-id: c41323aab601b439096cb75648210c6a646c6c7b

--- oneflow/core/kernel/slice_grad_kernel.cu
+++ oneflow/core/kernel/slice_grad_kernel.cu
@@ -1,13 +1,9 @@
 #include "oneflow/core/kernel/slice_kernel.h"
+#include "oneflow/core/kernel/slice_grad_kernel.h"
 
 namespace oneflow {
 
 namespace {
-
-template<typename T>
-__global__ void SliceForwardGpu(const int64_t n, const int64_t* offset, const T* entire, T* slice) {
-  CUDA_1D_KERNEL_LOOP(i, n) { slice[i] = entire[offset[i]]; }
-}
 
 template<typename T>
 __global__ void SliceBackwardGpu(const int64_t n, const int64_t* offset, const T* slice,
@@ -18,29 +14,31 @@ __global__ void SliceBackwardGpu(const int64_t n, const int64_t* offset, const T
 }  // namespace
 
 template<typename T>
-void SliceKernel<DeviceType::kGPU, T>::ForwardDataContent(
+void SliceGradKernel<DeviceType::kGPU, T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* in_blob = BnInOp2Blob("in");
-  const Blob* offset_blob = BnInOp2Blob("out_to_in_offset");
-  Blob* out_blob = BnInOp2Blob("out");
-  const int64_t num_output = out_blob->shape().elem_cnt();
-  SliceForwardGpu<T><<<BlocksNum4ThreadsNum(num_output), kCudaThreadsNumPerBlock, 0,
-                       ctx.device_ctx->cuda_stream()>>>(
-      num_output, offset_blob->dptr<int64_t>(), in_blob->dptr<T>(), out_blob->mut_dptr<T>());
+  const Blob* dy_blob = BnInOp2Blob("dy");
+  const Blob* offset_blob = BnInOp2Blob("y_to_x_offset");
+  Blob* dx_blob = BnInOp2Blob("dx");
+  const int64_t num_output = dy_blob->shape().elem_cnt();
+  Memset<DeviceType::kGPU>(ctx.device_ctx, dx_blob->mut_dptr<T>(), 0,
+                           dx_blob->ByteSizeOfBlobBody());
+  SliceBackwardGpu<T><<<BlocksNum4ThreadsNum(num_output), kCudaThreadsNumPerBlock, 0,
+                        ctx.device_ctx->cuda_stream()>>>(
+      num_output, offset_blob->dptr<int64_t>(), dy_blob->dptr<T>(), dx_blob->mut_dptr<T>());
 }
 
 template<typename T>
-void SliceKernel<DeviceType::kGPU, T>::InitConstBufBlobs(
+void SliceGradKernel<DeviceType::kGPU, T>::InitConstBufBlobs(
     DeviceCtx* ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   Shape in_shape(this->kernel_conf().slice_conf().in_shape());
-  InitOut2InOffsetFromHost(ctx, in_shape, BnInOp2Blob("out_to_in_offset"));
+  InitOut2InOffsetFromHost(ctx, in_shape, BnInOp2Blob("y_to_x_offset"));
 }
 
 template<typename T>
-void SliceKernel<DeviceType::kGPU, T>::InitOut2InOffsetFromHost(DeviceCtx* ctx,
-                                                                const Shape& in_shape,
-                                                                Blob* blob) const {
-  const SliceOpConf& conf = op_conf().slice_conf();
+void SliceGradKernel<DeviceType::kGPU, T>::InitOut2InOffsetFromHost(DeviceCtx* ctx,
+                                                                    const Shape& in_shape,
+                                                                    Blob* blob) const {
+  const SliceGradOpConf& conf = op_conf().slice_grad_conf();
   WithHostBlobAndStreamSynchronizeEnv(ctx, blob, [&](Blob* host_blob) {
     int64_t* host_blob_ptr = host_blob->mut_dptr<int64_t>();
     FOR_RANGE(int64_t, i, 0, host_blob->shape().elem_cnt()) {
@@ -64,7 +62,7 @@ void SliceKernel<DeviceType::kGPU, T>::InitOut2InOffsetFromHost(DeviceCtx* ctx,
 }
 
 #define INSTANTIATE_GPU_SLICE_KERNEL(type_cpp, type_proto) \
-  template struct SliceKernel<DeviceType::kGPU, type_cpp>;
+  template struct SliceGradKernel<DeviceType::kGPU, type_cpp>;
 OF_PP_FOR_EACH_TUPLE(INSTANTIATE_GPU_SLICE_KERNEL, ARITHMETIC_DATA_TYPE_SEQ)
 
 }  // namespace oneflow
