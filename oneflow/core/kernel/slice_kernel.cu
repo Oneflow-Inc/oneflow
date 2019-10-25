61c10f6e8ec43ad24ca1386ea3a90a11f59efee9
scxfjiang <sc.xfjiang@gmail.com>
2019-10-25 21:24:14 +0800

    Dev support dynamic for slice op (#2365)
    * refactor code organization for slice op
    
    * finish dynamic support for slice op
    
    
    > dynamic的slice不要采取以前那种静态的计算offset的实现方法了，那个方法性能有问题，找新的办法重新实现吧
    
    是的，要构造结构体传参给cuda kernel

--- oneflow/core/kernel/slice_kernel.cu
+++ oneflow/core/kernel/slice_kernel.cu
@@ -1,4 +1,4 @@
-#include "oneflow/core/kernel/slice_kernel.h"
+#include "oneflow/core/kernel/kernel.h"
 
 namespace oneflow {
 
@@ -18,53 +18,108 @@ __global__ void SliceBackwardGpu(const int64_t n, const int64_t* offset, const T
 }  // namespace
 
 template<typename T>
-void SliceKernel<DeviceType::kGPU, T>::ForwardDataContent(
-    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  const Blob* in_blob = BnInOp2Blob("in");
-  const Blob* offset_blob = BnInOp2Blob("out_to_in_offset");
-  Blob* out_blob = BnInOp2Blob("out");
-  const int64_t num_output = out_blob->shape().elem_cnt();
-  SliceForwardGpu<T><<<BlocksNum4ThreadsNum(num_output), kCudaThreadsNumPerBlock, 0,
-                       ctx.device_ctx->cuda_stream()>>>(
-      num_output, offset_blob->dptr<int64_t>(), in_blob->dptr<T>(), out_blob->mut_dptr<T>());
-}
+class SliceGpuKernel final : public KernelIf<DeviceType::kGPU> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SliceGpuKernel);
+  SliceGpuKernel() = default;
+  ~SliceGpuKernel() = default;
 
-template<typename T>
-void SliceKernel<DeviceType::kGPU, T>::InitConstBufBlobs(
-    DeviceCtx* ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
-  Shape in_shape(this->kernel_conf().slice_conf().in_shape());
-  InitOut2InOffsetFromHost(ctx, in_shape, BnInOp2Blob("out_to_in_offset"));
-}
+ private:
+  void ForwardDataContent(const KernelCtx& ctx,
+                          std::function<Blob*(const std::string&)> BnInOp2Blob) const override {
+    const Blob* in_blob = BnInOp2Blob("in");
+    Blob* offset_blob = BnInOp2Blob("out_to_in_offset");
+    Blob* out_blob = BnInOp2Blob("out");
 
-template<typename T>
-void SliceKernel<DeviceType::kGPU, T>::InitOut2InOffsetFromHost(DeviceCtx* ctx,
-                                                                const Shape& in_shape,
-                                                                Blob* blob) const {
-  const SliceOpConf& conf = op_conf().slice_conf();
-  WithHostBlobAndStreamSynchronizeEnv(ctx, blob, [&](Blob* host_blob) {
-    int64_t* host_blob_ptr = host_blob->mut_dptr<int64_t>();
-    FOR_RANGE(int64_t, i, 0, host_blob->shape().elem_cnt()) {
-      int64_t offset = 0;
-      int64_t index = i;
-      FOR_RANGE(int64_t, j, 0, host_blob->shape().NumAxes()) {
-        const int64_t dim_elem_cnt = host_blob->shape().Count(j + 1);
-        const int64_t dim_i = index / dim_elem_cnt;
-        index = index % dim_elem_cnt;
-        int64_t start = 0;
-        int64_t stride = 1;
-        const DimSliceConf& dim_slice_conf = conf.dim_slice_conf(j);
-        if (dim_slice_conf.has_start()) { start = dim_slice_conf.start(); }
-        if (start < 0) { start += host_blob->shape().At(j); }
-        stride = dim_slice_conf.stride();
-        offset += (start + dim_i * stride) * in_shape.Count(j + 1);
+    // TODO: Add InferTmpBlobDenseShape for op with dynamic tmp blob
+    offset_blob->dense_shape_mut_view().set_shape(static_cast<Shape>(out_blob->shape()));
+
+    const SliceOpConf& conf = this->op_conf().slice_conf();
+    WithHostBlobAndStreamSynchronizeEnv(ctx.device_ctx, offset_blob, [&](Blob* host_blob) {
+      int64_t* host_blob_ptr = host_blob->mut_dptr<int64_t>();
+      FOR_RANGE(int64_t, i, 0, host_blob->shape().elem_cnt()) {
+        int64_t offset = 0;
+        int64_t index = i;
+        FOR_RANGE(int64_t, j, 0, host_blob->shape().NumAxes()) {
+          const int64_t dim_elem_cnt = host_blob->shape().Count(j + 1);
+          const int64_t dim_i = index / dim_elem_cnt;
+          index = index % dim_elem_cnt;
+          int64_t start = 0;
+          int64_t stride = 1;
+          const DimSliceConf& dim_slice_conf = conf.dim_slice_conf(j);
+          if (dim_slice_conf.has_start()) { start = dim_slice_conf.start(); }
+          if (start < 0) { start += host_blob->shape().At(j); }
+          stride = dim_slice_conf.stride();
+          offset += (start + dim_i * stride) * in_blob->shape().Count(j + 1);
+        }
+        host_blob_ptr[i] = offset;
       }
-      host_blob_ptr[i] = offset;
-    }
-  });
-}
+    });
+    const int64_t num_output = out_blob->shape().elem_cnt();
+    SliceForwardGpu<T><<<BlocksNum4ThreadsNum(num_output), kCudaThreadsNumPerBlock, 0,
+                         ctx.device_ctx->cuda_stream()>>>(
+        num_output, offset_blob->dptr<int64_t>(), in_blob->dptr<T>(), out_blob->mut_dptr<T>());
+  }
+};
 
-#define INSTANTIATE_GPU_SLICE_KERNEL(type_cpp, type_proto) \
-  template struct SliceKernel<DeviceType::kGPU, type_cpp>;
-OF_PP_FOR_EACH_TUPLE(INSTANTIATE_GPU_SLICE_KERNEL, ARITHMETIC_DATA_TYPE_SEQ)
+template<typename T>
+class SliceGradGpuKernel final : public KernelIf<DeviceType::kGPU> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SliceGradGpuKernel);
+  SliceGradGpuKernel() = default;
+  ~SliceGradGpuKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx& ctx,
+                          std::function<Blob*(const std::string&)> BnInOp2Blob) const override {
+    const Blob* dy_blob = BnInOp2Blob("dy");
+    const Blob* like_blob = BnInOp2Blob("like");
+    Blob* offset_blob = BnInOp2Blob("y_to_x_offset");
+    Blob* dx_blob = BnInOp2Blob("dx");
+
+    // TODO: Add InferTmpBlobDenseShape for op with dynamic tmp blob
+    offset_blob->dense_shape_mut_view().set_shape(static_cast<Shape>(dy_blob->shape()));
+
+    const SliceGradOpConf& conf = op_conf().slice_grad_conf();
+    WithHostBlobAndStreamSynchronizeEnv(ctx.device_ctx, offset_blob, [&](Blob* host_blob) {
+      int64_t* host_blob_ptr = host_blob->mut_dptr<int64_t>();
+      FOR_RANGE(int64_t, i, 0, host_blob->shape().elem_cnt()) {
+        int64_t offset = 0;
+        int64_t index = i;
+        FOR_RANGE(int64_t, j, 0, host_blob->shape().NumAxes()) {
+          const int64_t dim_elem_cnt = host_blob->shape().Count(j + 1);
+          const int64_t dim_i = index / dim_elem_cnt;
+          index = index % dim_elem_cnt;
+          int64_t start = 0;
+          int64_t stride = 1;
+          const DimSliceConf& dim_slice_conf = conf.dim_slice_conf(j);
+          if (dim_slice_conf.has_start()) { start = dim_slice_conf.start(); }
+          if (start < 0) { start += host_blob->shape().At(j); }
+          stride = dim_slice_conf.stride();
+          offset += (start + dim_i * stride) * like_blob->shape().Count(j + 1);
+        }
+        host_blob_ptr[i] = offset;
+      }
+    });
+    Memset<DeviceType::kGPU>(ctx.device_ctx, dx_blob->mut_dptr<T>(), 0,
+                             dx_blob->ByteSizeOfBlobBody());
+    const int64_t num_output = dy_blob->shape().elem_cnt();
+    SliceBackwardGpu<T><<<BlocksNum4ThreadsNum(num_output), kCudaThreadsNumPerBlock, 0,
+                          ctx.device_ctx->cuda_stream()>>>(
+        num_output, offset_blob->dptr<int64_t>(), dy_blob->dptr<T>(), dx_blob->mut_dptr<T>());
+  }
+};
+
+#define REGISTER_SLICE_GPU_KERNEL(dtype)                                                       \
+  REGISTER_KERNEL_WITH_DEVICE_AND_DTYPE(OperatorConf::kSliceConf, DeviceType::kGPU, dtype,     \
+                                        SliceGpuKernel<dtype>)                                 \
+  REGISTER_KERNEL_WITH_DEVICE_AND_DTYPE(OperatorConf::kSliceGradConf, DeviceType::kGPU, dtype, \
+                                        SliceGradGpuKernel<dtype>)
+
+REGISTER_SLICE_GPU_KERNEL(float);
+REGISTER_SLICE_GPU_KERNEL(double);
+REGISTER_SLICE_GPU_KERNEL(int8_t);
+REGISTER_SLICE_GPU_KERNEL(int32_t);
+REGISTER_SLICE_GPU_KERNEL(int64_t);
 
 }  // namespace oneflow
--- oneflow/python/test/slice.py
+++ oneflow/python/test/slice.py
@@ -1,27 +1,58 @@
 import oneflow as of
+import torch
 import numpy as np
+import os
 
+of.config.ctrl_port(19739)
 
-def test_case_1():
-    r"""dim0 slice"""
+def Save(name):
+    def _save(x):
+        path = "/tmp/compare_slice/"
+        if not os.path.exists(path):
+            os.mkdir(path)
+        np.save(path + name, x.ndarray())
+    return _save
 
+def test_grad_2d(device_type):
     @of.function
-    def slice_dim0_job(
-        input_blob=of.input_blob_def(shape=(2, 10, 4), dtype=of.float32)
+    def slice_job(
+        x=of.input_blob_def(
+            (20, 20), dtype=of.float32, is_dynamic=True
+        )
     ):
-        part1 = of.slice(input_blob, (0, 0, 0), (1, -1, -1))
-        part2 = of.slice(input_blob, (1, 0, 0), (1, -1, -1))
-        return part1, part2
+        of.config.train.primary_lr(0.0001)
+        of.config.train.model_update_conf(dict(naive_conf={}))
+        y = of.get_variable(
+            name="y",
+            shape=(1,),
+            dtype=of.float32,
+            initializer=of.constant_initializer(value=0.0),
+        )
+        with of.device_prior_placement(device_type, "0:0"):
+            input = x + y
+            output = of.slice_v2(input, [(3, -1, 2), (0, 8, 2)])
 
-    input = np.random.rand(2, 10, 4).astype(np.float32)
-    p1, p2 = slice_dim0_job(input).get()
+            of.watch(output, Save("of_out"))
+            of.watch_diff(input, Save("of_in_diff"))
 
-    print("input: \n", input)
-    print("part1: \n", input[0:1, :, :])
-    print("part2: \n", input[1:, :, :])
-    assert np.allclose(p1.ndarray(), input[0:1, :, :])
-    assert np.allclose(p2.ndarray(), input[1:, :, :])
+            of.losses.add_loss(output)
 
+        return input, output
+
+    # OneFlow
+    check_point = of.train.CheckPoint()
+    check_point.init()
+    x = np.arange(100).reshape((10, 10)).astype(np.float32)
+    of_input, of_out = slice_job(x).get()
+
+    # PyTorch
+    torch_input = torch.tensor(of_input, requires_grad=True)
+    torch_out = torch_input[3:-1:2, 0:8:2]
+    torch_out.sum().backward()
+
+    np.allclose(np.load("/tmp/compare_slice/of_out.npy"), torch_out.detach().numpy())
+    np.allclose(np.load("/tmp/compare_slice/of_in_diff.npy"), torch_input.grad.detach().numpy())
 
 if __name__ == "__main__":
-    test_case_1()
+    test_grad_2d("gpu")
+    # test_grad_2d("cpu")
