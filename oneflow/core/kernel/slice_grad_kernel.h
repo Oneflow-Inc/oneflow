b25b993c0b766d059419d926cc504b6f9168e816
scxfjiang <sc.xfjiang@gmail.com>
2019-10-25 21:33:52 +0800

    Revert "Dev support dynamic for slice op (#2365)"
    This reverts commit 61c10f6e8ec43ad24ca1386ea3a90a11f59efee9.

---
+++ oneflow/core/kernel/slice_grad_kernel.h
@@ -0,0 +1,44 @@
+#ifndef ONEFLOW_CORE_KERNEL_SLICE_GRAD_KERNEL_H_
+#define ONEFLOW_CORE_KERNEL_SLICE_GRAD_KERNEL_H_
+
+#include "oneflow/core/kernel/kernel.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T>
+class SliceGradKernel;
+
+template<typename T>
+class SliceGradKernel<DeviceType::kCPU, T> final : public KernelIf<DeviceType::kCPU> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SliceGradKernel);
+  SliceGradKernel() = default;
+  ~SliceGradKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx&,
+                          std::function<Blob*(const std::string&)>) const override;
+};
+
+template<typename T>
+class SliceGradKernel<DeviceType::kGPU, T> final : public KernelIf<DeviceType::kGPU> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SliceGradKernel);
+  SliceGradKernel() = default;
+  ~SliceGradKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx&,
+                          std::function<Blob*(const std::string&)>) const override;
+  void InitConstBufBlobs(DeviceCtx*,
+                         std::function<Blob*(const std::string&)> BnInOp2Blob) const override;
+
+  void InitOut2InOffsetFromHost(DeviceCtx* ctx, const Shape& in_shape, Blob* blob) const;
+};
+
+template<typename T, size_t NDIMS>
+struct NdarraySliceUtil;
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_KERNEL_SLICE_GRAD_KERNEL_H_
