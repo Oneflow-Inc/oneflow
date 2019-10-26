dea17e5eaccfe8b5a98c2f07d24e8be068c73094
leaves-zwx <kunta0932@gmail.com>
2019-10-26 18:25:30 +0800

    multipe batch ouput compare

--- oneflow/python/test/coco_dataset_load.py
+++ oneflow/python/test/coco_dataset_load.py
@@ -2,16 +2,31 @@ import oneflow as flow
 import oneflow.core.data.data_pb2 as data_util
 import numpy as np
 import pickle as pkl
+import os
 import time
 import argparse
 
 from termcolor import colored
 
 COCO_DATASET_DIR = "/dataset/mscoco_2017"
-COCO_ANNOTATIONS_FILE = "annotations/instances_val2017.json"
-COCO_IMAGE_DIR = "val2017"
+# COCO_ANNOTATIONS_FILE = "annotations/instances_val2017.json"
+COCO_ANNOTATIONS_FILE = "annotations/instances_train2017.json"
+# COCO_IMAGE_DIR = "val2017"
+COCO_IMAGE_DIR = "train2017"
 RANDOM_SEED = 297157
-COMPARE_DATA = "data.pkl"  # batch_size == 2 data from pytorch
+# batch_size == 2 data from pytorch
+COMPARE_DATA = [
+    "mock_data/iter_1.pkl",
+    "mock_data/iter_2.pkl",
+    "mock_data/iter_3.pkl",
+    "mock_data/iter_4.pkl",
+    "mock_data/iter_5.pkl",
+    "mock_data/iter_6.pkl",
+    "mock_data/iter_7.pkl",
+    "mock_data/iter_8.pkl",
+    "mock_data/iter_9.pkl",
+    "mock_data/iter_10.pkl",
+]
 
 parser = argparse.ArgumentParser(description="flags for data loader")
 # parser.add_argument("-g", "--gpu_num_per_node", type=int, default=1, required=False)
@@ -49,9 +64,9 @@ parser.add_argument("-b", "--batch_size", type=int, default=2, required=False)
 parser.add_argument(
     "-bc", "--batch_cache_size", type=int, default=3, required=False
 )
-parser.add_argument(
-    "-s", "--save_results", default=False, action="store_true", required=False
-)
+# parser.add_argument(
+#     "-s", "--save_results", default=False, action="store_true", required=False
+# )
 parser.add_argument(
     "-cp",
     "--compare_results",
@@ -146,6 +161,7 @@ def coco_data_load_job():
 
 
 def compare(data, valid, name=None):
+    success = False
     name = name or "unknown"
     if valid.shape != data.shape:
         print(
@@ -161,9 +177,11 @@ def compare(data, valid, name=None):
             print(colored("{} identical".format(name), "green"))
         else:
             print(colored("{} allclose".format(name), "blue"))
-        return
+        success = True
     else:
         print(colored("{} not close".format(name), "red"))
+
+    return success
 
 
 def compare_test_case():
@@ -172,81 +190,100 @@ def compare_test_case():
     flow.config.exp_run_conf({"enable_experiment_run": False})
     flow.config.default_data_type(flow.float)
 
-    image, image_id, image_size, gt_bbox, gt_labels, gt_segm, gt_segm_mask = (
-        coco_data_load_job().get()
-    )
+    for i in range(len(COMPARE_DATA)):
+        image, image_id, image_size, gt_bbox, gt_labels, gt_segm, gt_segm_mask = (
+            coco_data_load_job().get()
+        )
 
-    if args.save_results:
-        np.save("image", image.ndarray())
-        np.save("image_size", image_size.ndarray())
-        np.save("gt_bbox", gt_bbox.ndarray())
-        np.save("gt_labels", gt_labels.ndarray())
-        np.save("gt_segm", gt_segm.ndarray())
-        np.save("gt_segm_mask", gt_segm_mask.ndarray())
+        def get_output_path(name, iter):
+            output_dir = "output"
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
+            return os.path.join(output_dir, "iter_{}_{}".format(iter, name))
 
-    with open(COMPARE_DATA, "rb") as f:
-        data = pkl.load(f)
+        print(colored("=== iter_{} compare begin ===".format(i), "yellow"))
 
-    # compare images
-    diff = image - np.transpose(data["images"], (0, 2, 3, 1))
-    max_diff = np.max(diff)
-    if max_diff <= 1.0:
-        print(colored("image max diff is: {}".format(max_diff), "blue"))
-    else:
-        print(colored("image max diff is: {}".format(max_diff), "red"))
+        with open(COMPARE_DATA[i], "rb") as f:
+            data = pkl.load(f)
 
-    # compare image_size
-    # of image_size format (height, width)
-    # compare data image_size format (width, height)
-    compare(
-        image_size.ndarray(),
-        np.concatenate(
-            [data["image_size"][:, 1:2], data["image_size"][:, 0:1]], axis=1
-        ),
-        name="image_size",
-    )
+        # compare images
+        contrast_image = np.transpose(data["images"], (0, 2, 3, 1))
+        diff = image - contrast_image
+        max_diff = np.max(diff)
+        if max_diff <= 1.0:
+            print(colored("image max diff is: {}".format(max_diff), "blue"))
+        else:
+            print(colored("image max diff is: {}".format(max_diff), "red"))
+            np.save(get_output_path("images", i), image.ndarray())
+            np.save(get_output_path("images_contrast", i), contrast_image)
 
-    # compare gt_bbox
-    compare(
-        gt_bbox.ndarray(),
-        np.concatenate(data["gt_bbox"], axis=0),
-        name="gt_bbox",
-    )
+        # compare image_size
+        # of image_size format (height, width)
+        # compare data image_size format (width, height)
+        if not compare(
+            image_size.ndarray(),
+            np.concatenate(
+                [data["image_size"][:, 1:2], data["image_size"][:, 0:1]], axis=1
+            ),
+            name="image_size",
+        ):
+            np.save(get_output_path("image_size", i), image_size.ndarray())
 
-    # compare gt_labels
-    compare(
-        gt_labels.ndarray(),
-        np.concatenate(data["gt_labels"], axis=0),
-        name="gt_labels",
-    )
+        # compare gt_bbox
+        if not compare(
+            gt_bbox.ndarray(),
+            np.concatenate(data["gt_bbox"], axis=0),
+            name="gt_bbox",
+        ):
+            np.save(get_output_path("gt_bbox", i), gt_bbox.ndarray())
 
-    # compare gt_segm
-    def flat_polys(polys):
-        poly_arrays = []
-        for img_polys in polys:
-            for obj_poly_list in img_polys:
-                for obj_poly in obj_poly_list:
-                    poly_arrays.append(np.array(obj_poly).reshape(-1, 2))
-        return np.concatenate(poly_arrays, axis=0)
+        # compare gt_labels
+        if not compare(
+            gt_labels.ndarray(),
+            np.concatenate(data["gt_labels"], axis=0),
+            name="gt_labels",
+        ):
+            np.save(get_output_path("gt_labels", i), gt_labels.ndarray())
 
-    compare(gt_segm.ndarray(), flat_polys(data["gt_segm"]), name="gt_segm")
+        # compare gt_segm
+        def flat_polys(polys):
+            poly_arrays = []
+            for img_polys in polys:
+                for obj_poly_list in img_polys:
+                    for obj_poly in obj_poly_list:
+                        poly_arrays.append(np.array(obj_poly).reshape(-1, 2))
+            return np.concatenate(poly_arrays, axis=0)
 
-    # compare gt_segm_mask
-    masks = []
-    for m in data["gt_segm_mask"]:
-        if len(m.shape) == 2:
-            m = np.expand_dims(m, axis=0)
+        if not compare(
+            gt_segm.ndarray(),
+            flat_polys(data["gt_segm_poly"]),
+            name="gt_segm_poly",
+        ):
+            np.save(get_output_path("gt_segm_poly", i), gt_segm.ndarray())
 
-        assert len(m.shape) == 3
-        pad_h = image.shape[1] - m.shape[1]
-        pad_w = image.shape[2] - m.shape[2]
-        masks.append(np.pad(m, ((0, 0), (0, pad_h), (0, pad_w))))
+        # compare gt_segm_mask
+        masks = []
+        for m in data["gt_segm_mask"]:
+            if len(m.shape) == 2:
+                m = np.expand_dims(m, axis=0)
 
-    compare(
-        gt_segm_mask.ndarray(),
-        np.concatenate(masks, axis=0),
-        name="gt_segm_mask",
-    )
+            assert len(m.shape) == 3
+            pad_h = image.shape[1] - m.shape[1]
+            pad_w = image.shape[2] - m.shape[2]
+            masks.append(np.pad(m, ((0, 0), (0, pad_h), (0, pad_w))))
+
+        if not compare(
+            gt_segm_mask.ndarray(),
+            np.concatenate(masks, axis=0),
+            name="gt_segm_mask",
+        ):
+            np.save(get_output_path("gt_segm_mask", i), gt_segm_mask.ndarray())
+            np.save(
+                get_output_path("gt_segm_mask_contrast", i),
+                np.concatenate(masks, axis=0),
+            )
+
+        print(colored("=== iter_{} compare done ===\n".format(i), "yellow"))
 
 
 cur_step = 0
