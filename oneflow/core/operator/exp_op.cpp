8350e4482a59598811435c55c241d231e078d40a
guo-ran <360112263@qq.com>
2019-10-26 11:01:55 +0800

    of format

--- oneflow/core/operator/exp_op.cpp
+++ oneflow/core/operator/exp_op.cpp
@@ -10,17 +10,17 @@ class ExpOp final : public Operator {
   ~ExpOp() = default;
 
   void InitFromOpConf() override {
-  CHECK(op_conf().has_exp_conf());
-  EnrollInputBn("in");
-  EnrollOutputBn("out")->set_mutable_inplace_ibn("in");
-}
+    CHECK(op_conf().has_exp_conf());
+    EnrollInputBn("in");
+    EnrollOutputBn("out")->set_mutable_inplace_ibn("in");
+  }
   const PbMessage& GetCustomizedConf() const override { return op_conf().exp_conf(); }
 
   Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                              const ParallelContext* parallel_ctx) const override {
-  *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
-  return Maybe<void>::Ok();
-}
+    *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
+    return Maybe<void>::Ok();
+  }
 
  private:
   Maybe<void> InferBatchAxis(
@@ -30,15 +30,14 @@ class ExpOp final : public Operator {
   Maybe<void> GetSbpSignatures(
       const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
       SbpSignatureList* sbp_sig_list) const override {
-  SbpSignatureBuilder()
-      .Split(input_bns(), 0)
-      .Split(output_bns(), 0)
-      .MakeSplitSignatureListBuilder(
-          JUST(LogicalBlobDesc4Ibn(input_bns().Get(0)))->shape().NumAxes())
-      .Build(sbp_sig_list);
-  return Maybe<void>::Ok();
-}
-
+    SbpSignatureBuilder()
+        .Split(input_bns(), 0)
+        .Split(output_bns(), 0)
+        .MakeSplitSignatureListBuilder(
+            JUST(LogicalBlobDesc4Ibn(input_bns().Get(0)))->shape().NumAxes())
+        .Build(sbp_sig_list);
+    return Maybe<void>::Ok();
+  }
 };
 
 REGISTER_OP(OperatorConf::kExpConf, ExpOp);
