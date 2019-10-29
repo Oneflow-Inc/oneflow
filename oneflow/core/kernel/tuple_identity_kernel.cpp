180d942543a95f658d5b3c5b5e4655bf292f5805
leaves-zwx <kunta0932@gmail.com>
2019-10-29 14:04:57 +0800

    tuple identity forward lod

--- oneflow/core/kernel/tuple_identity_kernel.cpp
+++ oneflow/core/kernel/tuple_identity_kernel.cpp
@@ -14,6 +14,21 @@ void TupleIdentityKernel<device_type>::ForwardDataContent(
   }
 }
 
+template<DeviceType device_type>
+void TupleIdentityKernel<device_type>::ForwardLoD(
+    const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
+  const auto& input_bns = this->op_attribute().input_bns();
+  const auto& output_bns = this->op_attribute().output_bns();
+  CHECK_EQ(input_bns.size(), output_bns.size());
+  FOR_RANGE(int, i, 0, output_bns.size()) {
+    const Blob* in_blob = BnInOp2Blob(input_bns.Get(i));
+    Blob* out_blob = BnInOp2Blob(output_bns.Get(i));
+    if (out_blob->blob_desc().num_of_lod_levels() > 0) {
+      out_blob->tree_lod_mut_view().UpdateLoD(in_blob->tree_lod_view().lod_tree());
+    }
+  }
+}
+
 ADD_DEVICE_TYPE_KERNEL_CREATOR(OperatorConf::kTupleIdentityConf, TupleIdentityKernel);
 
 }  // namespace oneflow
