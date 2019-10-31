c5ff455844e0fd2d8d65826af5b7322ba3300d61
leaves-zwx <kunta0932@gmail.com>
2019-10-31 21:06:02 +0800

    train with real dataset script

--- oneflow/python/model/maskrcnn/maskrcnn.py
+++ oneflow/python/model/maskrcnn/maskrcnn.py
@@ -2,6 +2,7 @@
 from config import get_default_cfgs
 
 import oneflow as flow
+import oneflow.core.data.data_pb2 as data_util
 
 from backbone import Backbone
 from rpn import RPNHead, RPNLoss, RPNProposal
@@ -454,6 +455,111 @@ if terminal_args.box_head_post_processor:
         return ret
 
 
+if terminal_args.train_with_real_dataset:
+
+    def make_data_loader(
+        batch_size,
+        batch_cache_size=3,
+        dataset_dir="/dataset/mscoco_2017",
+        annotation_file="annotations/instances_train2017.json",
+        image_dir="train2017",
+        random_seed=123456,
+        shuffle=False,
+        group_by_aspect_ratio=True,
+    ):
+        coco = flow.data.COCODataset(
+            dataset_dir,
+            annotation_file,
+            image_dir,
+            random_seed,
+            shuffle,
+            group_by_aspect_ratio,
+        )
+        data_loader = flow.data.DataLoader(coco, batch_size, batch_cache_size)
+        data_loader.add_blob(
+            "image",
+            data_util.DataSourceCase.kImage,
+            shape=(1344, 800, 3),
+            dtype=flow.float,
+            is_dynamic=True,
+        )
+        data_loader.add_blob(
+            "image_size",
+            data_util.DataSourceCase.kImageSize,
+            shape=(2,),
+            dtype=flow.int32,
+        )
+        data_loader.add_blob(
+            "gt_bbox",
+            data_util.DataSourceCase.kObjectBoundingBox,
+            shape=(64, 4),
+            dtype=flow.float,
+            variable_length_axes=(0,),
+            is_dynamic=True,
+        )
+        data_loader.add_blob(
+            "gt_labels",
+            data_util.DataSourceCase.kObjectLabel,
+            shape=(64,),
+            dtype=flow.int32,
+            variable_length_axes=(0,),
+            is_dynamic=True,
+        )
+        data_loader.add_blob(
+            "gt_segm_poly",
+            data_util.DataSourceCase.kObjectSegmentation,
+            shape=(64, 2, 256, 2),
+            dtype=flow.double,
+            variable_length_axes=(0, 1, 2),
+            is_dynamic=True,
+        )
+        data_loader.add_blob(
+            "gt_segm",
+            data_util.DataSourceCase.kObjectSegmentationAlignedMask,
+            shape=(64, 1344, 800),
+            dtype=flow.int8,
+            variable_length_axes=(0,),
+            is_dynamic=True,
+        )
+        data_loader.add_transform(flow.data.TargetResizeTransform(800, 1333))
+        data_loader.add_transform(
+            flow.data.ImageNormalizeByChannel((102.9801, 115.9465, 122.7717))
+        )
+        data_loader.add_transform(flow.data.ImageAlign(32))
+        data_loader.add_transform(
+            flow.data.SegmentationPolygonListToAlignedMask()
+        )
+        data_loader.init()
+        return data_loader
+
+    @flow.function
+    def train(
+        image_blob=flow.input_blob_def(
+            shape=(2, 1088, 800, 3), dtype=flow.float32, is_dynamic=True
+        )
+    ):
+        # make conv_filter_grad algo certain to bypass algo work space inferring bug
+        # flow.config.cudnn_conv_force_bwd_filter_algo(0)
+        flow.config.train.primary_lr(terminal_args.primary_lr)
+        flow.config.train.model_update_conf(dict(naive_conf={}))
+        data_loader = make_data_loader(
+            batch_size=2,
+            batch_cache_size=1,
+            annotation_file="annotations/instances_val2017.json",
+            image_dir="val2017",
+        )
+        outputs = maskrcnn_train(
+            image_blob,
+            data_loader("image_size"),
+            data_loader("gt_bbox"),
+            data_loader("gt_segm"),
+            data_loader("gt_labels"),
+        )
+        for loss in outputs:
+            flow.losses.add_loss(loss)
+        return outputs
+
+
 if __name__ == "__main__":
     flow.config.gpu_device_num(terminal_args.gpu_num_per_node)
     flow.config.ctrl_port(terminal_args.ctrl_port)
@@ -607,3 +713,25 @@ if __name__ == "__main__":
 
                 if (i + 1) % 10 == 0:
                     save_model()
+
+        elif terminal_args.train_with_real_dataset:
+            print(
+                "{:>8} {:>16} {:>16} {:>16} {:>16} {:>16}".format(
+                    "iter",
+                    "loss_rpn_box_reg",
+                    "loss_objectness",
+                    "loss_box_reg",
+                    "loss_classifier",
+                    "loss_mask",
+                )
+            )
+            for i in range(terminal_args.iter_num):
+                image = np.load("image_{}.npy".format(i + 90000))
+                train_loss = train(image).get()
+                fmt_str = "{:>8} " + "{:>16.10f} " * len(train_loss)
+                print_loss = [i]
+                for loss in train_loss:
+                    print_loss.append(loss.mean())
+                print(fmt_str.format(*print_loss))
+
+                save_blob_watched(i)
