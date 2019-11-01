2f76e1fdb9e00dfe2b99cf74533e06f7d04f1129
tsai <caishenghang@oneflow.org>
2019-11-01 12:19:18 +0800

    fix test

--- oneflow/python/test/random_perm_like.py
+++ oneflow/python/test/random_perm_like.py
@@ -5,11 +5,10 @@ import time
 flow.config.gpu_device_num(1)
 flow.config.default_data_type(flow.float)
 
-SIZE = 10
+SIZE = 100000
 @flow.function
 def TestJob(x = flow.input_blob_def((SIZE,), dtype=flow.float32, is_dynamic=True)):
-    return flow.argsort(x)
-    return flow.detection.random_perm_like(x) 
+    return flow.detection.random_perm_like(x + x) 
 
 for i in range(4):
     r = TestJob(np.zeros(SIZE).astype(np.float32)).get()
