08f36e868c08286e02689a8eb7ba24e8b0fc3ffc
lixinqi <lixinqi0703106@163.com>
2019-11-11 17:47:36 +0800

    oneflow.unittest.test_case

--- oneflow/python/test/relu.py
+++ oneflow/python/test/relu.py
@@ -4,11 +4,27 @@ import numpy as np
 flow.config.gpu_device_num(1)
 flow.config.default_data_type(flow.float)
 
-@flow.function
-def ReluJob(x = flow.input_blob_def((10,))):
-    return flow.keras.activations.relu(x)
+class TestRelu(flow.unittest.TestCase):
+    def testOnes(self):
+        @flow.function
+        def ReluJob(x = flow.input_blob_def((10,))):
+            return flow.keras.activations.relu(x)
+        ones = np.ones((10,), dtype=np.float32)
+        self.assertTrue(np.allclose(ReluJob(ones).get().ndarray(), ones))
 
-index = [-2, -1, 0, 1, 2]
-data = []
-for i in index: data.append(np.ones((10,), dtype=np.float32) * i)
-for x in data:  print(ReluJob(x).get())
+    def testZeros(self):
+        @flow.function
+        def ReluJob(x = flow.input_blob_def((10,))):
+            return flow.keras.activations.relu(x)
+        zeros = np.zeros((10,), dtype=np.float32)
+        self.assertTrue(np.allclose(ReluJob(zeros).get().ndarray(), zeros))
+
+    def testNegative(self):
+        @flow.function
+        def ReluJob(x = flow.input_blob_def((10,))):
+            return flow.keras.activations.relu(x)
+        data = np.ones((10,), dtype=np.float32) * -1
+        zeros = np.zeros((10,), dtype=np.float32)
+        self.assertTrue(np.allclose(ReluJob(data).get().ndarray(), zeros))
+
+flow.unittest.run_if_this_is_main()
