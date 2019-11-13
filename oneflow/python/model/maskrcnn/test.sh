28416aa2c57d7ccd13c7396e6fe62997c82a54f6
scxfjiang <sc.xfjiang@gmail.com>
2019-11-13 19:36:18 +0800

    Dev eval net with data loader (#2409)
    * run eval net with data loader normally
    
    * refine code structure

--- oneflow/python/model/maskrcnn/test.sh
+++ oneflow/python/model/maskrcnn/test.sh
@@ -1,3 +1,9 @@
 rm -r eval_dump
 export CUDA_VISIBLE_DEVICES=2
-python maskrcnn_eval.py -load="/model_zoo/detection/mask_rcnn_R_50_FPN_1x/snapshot/" -c="mask_rcnn_R_50_FPN_1x_eval.yaml" -cp=19237
+python maskrcnn_eval.py -load="/model_zoo/detection/mask_rcnn_R_50_FPN_1x/snapshot/"  \
+  -c="mask_rcnn_R_50_FPN_1x_eval.yaml" -cp=19237                                      \
+  -bz=2                                                                               \
+  -dataset_dir="/dataset/mscoco_2017"                                                 \
+  -anno="annotations/sample_2_instances_val2017.json"                                 \
+  -imgd="val2017"                                                                     \
+  -fake
