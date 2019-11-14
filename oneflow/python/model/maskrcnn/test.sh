642921ddb2b23ef620ba1c8edd92c48ad322a0f9
ScXfjiang <sc.xfjiang@gmail.com>
2019-11-14 18:32:02 +0800

    10 fake images
    Former-commit-id: 235c6e125348af1cd0accf16124707c3c4b9374c

--- oneflow/python/model/maskrcnn/test.sh
+++ oneflow/python/model/maskrcnn/test.sh
@@ -4,6 +4,6 @@ python maskrcnn_eval.py -load="/model_zoo/detection/mask_rcnn_R_50_FPN_1x/snapsh
   -c="mask_rcnn_R_50_FPN_1x_eval.yaml" -cp=19237                                      \
   -bz=2                                                                               \
   -dataset_dir="/dataset/mscoco_2017"                                                 \
-  -anno="annotations/sample_2_instances_val2017.json"                                 \
+  -anno="annotations/sample_10_instances_val2017.json"                                \
   -imgd="val2017"                                                                     \
   -fake
