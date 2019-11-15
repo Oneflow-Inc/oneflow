ff49254d40aea7b0667aa1cbc9a3169e628b9827
leaves-zwx <kunta0932@gmail.com>
2019-11-15 21:55:16 +0800

    add distribution_name_scope
    Former-commit-id: 4956861e48e92db630ad4002d8ebf023389fec32

--- oneflow/python/framework/compile_context.py
+++ oneflow/python/framework/compile_context.py
@@ -4,11 +4,13 @@ import oneflow.python.framework.placement_context as placement_context
 import oneflow.python.framework.job_builder as job_builder
 from contextlib import contextmanager
 
+
 @contextmanager
 def CurJobConf(job_conf):
     _ResetCurJobConf(job_conf)
     yield None
     _ResetCurJobConf(None)
+
 
 class BeforeNonInputOpBuildAndInferHook:
     def __init__(self, hook):
@@ -21,28 +23,56 @@ class BeforeNonInputOpBuildAndInferHook:
         global before_non_input_op_build_and_infer_hooks
         before_non_input_op_build_and_infer_hooks = []
 
-def CurJobAddOp(op_conf, parallel_conf = None): return _CurJobAddNonInputOp(op_conf, parallel_conf)
+
+def CurJobAddOp(op_conf, parallel_conf=None):
+    return _CurJobAddNonInputOp(op_conf, parallel_conf)
+
 
 def CurJobAddInputOp(op_conf):
-    op_conf.device_type = placement_context.CurPlacementGroupGetDeviceType(op_conf)
-    job_builder.CurCtxAddAndInferOp(op_conf, placement_context.ParallelConf4OpConf(op_conf))
+    op_conf.device_type = placement_context.CurPlacementGroupGetDeviceType(
+        op_conf
+    )
 
-def _CurJobAddNonInputOp(op_conf, parallel_conf = None):
+    job_builder.CurCtxAddAndInferOp(
+        op_conf, placement_context.ParallelConf4OpConf(op_conf)
+    )
+
+
+def _CurJobAddNonInputOp(op_conf, parallel_conf=None):
     _prefixing_op_name_if_need(op_conf)
-    op_conf.device_type = placement_context.CurPlacementGroupGetDeviceType(op_conf)
-    for callback in before_non_input_op_build_and_infer_hooks: callback()
-    if parallel_conf is None: parallel_conf = placement_context.ParallelConf4OpConf(op_conf)
+    op_conf.device_type = placement_context.CurPlacementGroupGetDeviceType(
+        op_conf
+    )
+
+    for callback in before_non_input_op_build_and_infer_hooks:
+        callback()
+
+    if parallel_conf is None:
+        parallel_conf = placement_context.ParallelConf4OpConf(op_conf)
+
     job_builder.CurCtxAddAndInferOp(op_conf, parallel_conf)
+
 
 def _ResetCurJobConf(job_conf):
     global cur_job_conf
     cur_job_conf = job_conf
+
     global cur_job_var_op_name2var_blob
     cur_job_var_op_name2var_blob = {}
+
     global cur_job_variable_scope_stack
     assert len(cur_job_variable_scope_stack) == 0
     cur_job_variable_scope_stack = []
+
+    global before_non_input_op_build_and_infer_hooks
     before_non_input_op_build_and_infer_hooks = []
+
+    global cur_job_distribution_name_scope
+    assert cur_job_distribution_name_scope == ""
+    cur_job_distribution_name_scope = ""
+
+    global cur_job_distribution_name_scope_exclude_variable
+    cur_job_distribution_name_scope_exclude_variable = True
 
 
 def _prefixing_op_name_if_need(op_conf):
@@ -55,7 +85,11 @@ def _prefixing_op_name_if_need(op_conf):
     if op_conf.HasField("layer_norm_conf"):
         pass
 
-    op_conf.name = _get_variable_prefix() + op_conf.name
+    op_conf.name = (
+        _get_distribution_prefix(op_conf)
+        + _get_variable_prefix()
+        + op_conf.name
+    )
 
 
 def _get_variable_prefix():
@@ -65,7 +99,22 @@ def _get_variable_prefix():
 
     return "-".join(cur_job_variable_scope_stack) + "-"
 
+
+def _get_distribution_prefix(op_conf):
+    global cur_job_distribution_name_scope
+    global cur_job_distribution_name_scope_exclude_variable
+
+    if cur_job_distribution_name_scope_exclude_variable and op_conf.HasField(
+        "variable_conf"
+    ):
+        return ""
+
+    return cur_job_distribution_name_scope
+
+
 cur_job_conf = None
 cur_job_var_op_name2var_blob = {}
 before_non_input_op_build_and_infer_hooks = []
 cur_job_variable_scope_stack = []
+cur_job_distribution_name_scope = ""
+cur_job_distribution_name_scope_exclude_variable = True
