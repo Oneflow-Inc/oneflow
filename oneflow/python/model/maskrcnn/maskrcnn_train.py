3266ce3bf7caeb8afe142f0afb78f3dbf87f2195
leaves-zwx <kunta0932@gmail.com>
2019-11-15 22:03:44 +0800

    fix merge
    Former-commit-id: ed0ca83fc3f71a88d378ee9cf1c0a46ca14c6da2

--- oneflow/python/model/maskrcnn/maskrcnn_train.py
+++ oneflow/python/model/maskrcnn/maskrcnn_train.py
@@ -564,7 +564,6 @@ if __name__ == "__main__":
     flow.config.ctrl_port(terminal_args.ctrl_port)
     flow.config.default_data_type(flow.float)
 
-    fake_image_list = []
     if terminal_args.fake_image_path:
         file_list = os.listdir(terminal_args.fake_image_path)
         fake_image_list = [
