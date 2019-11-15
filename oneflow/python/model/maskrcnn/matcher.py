c07d57d5890d1106151a168f7bd29c01c747bb77
leaves-zwx <kunta0932@gmail.com>
2019-11-15 21:56:35 +0800

    impl low quality match using matmul instead of batch gather
    Former-commit-id: 8799c96fa4a7f78f6e7d698bc6d07d40a9a425b8

--- oneflow/python/model/maskrcnn/matcher.py
+++ oneflow/python/model/maskrcnn/matcher.py
@@ -54,16 +54,25 @@ class Matcher(object):
             gt_matched_iou = flow.gather(
                 params=iou_matrix_trans,
                 # TODO: do not need expand_dims here
-                indices=flow.expand_dims(flow.math.argmax(iou_matrix_trans), axis=1),
+                indices=flow.expand_dims(
+                    flow.math.argmax(iou_matrix_trans), axis=1
+                ),
                 batch_dims=1,
             )
             box_max_gt = flow.local_nonzero(iou_matrix_trans == gt_matched_iou)
-            box_max_inds = flow.constant(
-                1,
-                dtype=flow.int32,
-                shape=(reduce(operator.mul, iou_matrix_trans.shape, 1), 1),
+            update_indices = flow.cast(
+                flow.matmul(
+                    flow.cast(box_max_gt, dtype=flow.float32),
+                    flow.concat(
+                        [
+                            flow.constant(0, shape=(1, 1), dtype=flow.float32),
+                            flow.constant(1, shape=(1, 1), dtype=flow.float32),
+                        ],
+                        axis=0,
+                    ),
+                ),
+                dtype=box_max_gt.dtype,
             )
-            update_indices = flow.gather(box_max_gt, box_max_inds, batch_dims=1)
             matched_indices = flow.local_scatter_nd_update(
                 matched_indices,
                 update_indices,
