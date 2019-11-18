7738f091986ebecc90ab997ea89be95a024bc4d2
tsai <caishenghang@oneflow.org>
2019-11-18 18:01:08 +0800

    add docs

--- oneflow/python/ops/losses/add_loss.py
+++ oneflow/python/ops/losses/add_loss.py
@@ -6,4 +6,9 @@ from oneflow.python.oneflow_export import oneflow_export
 
 @oneflow_export("losses.add_loss")
 def add_loss(loss):
+    r"""Mark a `Blob` as a loss. Auto grad starts at every loss blob. It doesn't has to be a product of typical "loss" operator like softmax loss but can be a `Blob` produced by any operator.
+
+    Args:
+        loss: A `Blob`.
+    """
     job_builder.CurCtxAddLossLogicalBlobName(loss.logical_blob_name)
