8c3d4e48bff5b955abc8bd4e1ffdab1ad9a41111
Li Xinqi <lixinqi2010@gmail.com>
2019-11-18 12:16:41 +0800

    Pr watch scope (#2419)
    * pr oneflow.watch*
    
    * merge more code to pass watch_scope.py
    
    * TODO: input_blob_def.parallel_conf

--- oneflow/python/ops/watch.py
+++ oneflow/python/ops/watch.py
@@ -6,19 +6,39 @@ import oneflow
 import oneflow.python.framework.compile_context as compile_context
 import oneflow.python.framework.id_util as id_util
 import oneflow.python.framework.watcher as watcher_util
+import oneflow.python.framework.job_builder as job_builder
 import oneflow.core.operator.op_conf_pb2 as op_conf_util
+from oneflow.core.job.lbi_diff_watcher_info_pb2 import LbiAndDiffWatcherUuidPair
 
 from oneflow.python.oneflow_export import oneflow_export
 
 @oneflow_export("watch")
-def watch(watched, handler):
-    assert callable(handler)
-    watched = list(watched) if isinstance(watched, collections.Sized) else [watched]
-    watched_lbn = [x.logical_blob_name for x in watched]
+def watch(watched, handler = None):
+    if callable(handler) == False:
+        prompt = handler
+        def _print(x):
+            if prompt is not None: print(prompt)
+            print(x)
+        handler = _print
     handler_uuid = str(uuid.uuid1())
     op_conf = op_conf_util.OperatorConf()
     op_conf.name = id_util.UniqueStr("ForeignWatch_")
-    getattr(op_conf.foreign_watch_conf, "in").extend(watched_lbn)
+    setattr(op_conf.foreign_watch_conf, "in", watched.logical_blob_name)
     op_conf.foreign_watch_conf.handler_uuid = handler_uuid
-    with oneflow.fixed_placement("cpu", "0:0"): compile_context.CurJobAddOp(op_conf)
+    compile_context.CurJobAddOp(op_conf, watched.parallel_conf)
+    watcher_util.BindUuidAndHandler(handler_uuid, handler)
+
+@oneflow_export("watch_diff")
+def watch_diff(watched, handler = None):
+    if callable(handler) == False:
+        prompt = handler
+        def _print(x):
+            if prompt is not None: print(prompt)
+            print(x)
+        handler = _print
+    handler_uuid = str(uuid.uuid1())
+    lbi_and_uuid = LbiAndDiffWatcherUuidPair()
+    lbi_and_uuid.lbi.CopyFrom(watched.lbi)
+    lbi_and_uuid.watcher_uuid = handler_uuid
+    job_builder.CurCtxAddLbiAndDiffWatcherUuidPair(lbi_and_uuid)
     watcher_util.BindUuidAndHandler(handler_uuid, handler)
