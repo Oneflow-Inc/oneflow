23f48000904420696f8ef837d82ffd9827b5411c
ScXfjiang <sc.xfjiang@gmail.com>
2019-11-19 18:04:39 +0800

    backup

--- oneflow/python/model/maskrcnn/distributed_test.sh
+++ oneflow/python/model/maskrcnn/distributed_test.sh
@@ -1,11 +1,11 @@
 rm -r eval_dump
-export CUDA_VISIBLE_DEVICES=2
-python maskrcnn_eval.py -load="/model_zoo/detection/mask_rcnn_R_50_FPN_1x/snapshot/"  \
+python distributed_maskrcnn_eval.py                                                   \
+  -load="/model_zoo/detection/mask_rcnn_R_50_FPN_1x/snapshot/"                        \
   -c="mask_rcnn_R_50_FPN_1x_eval.yaml"                                                \
   -cp=19237                                                                           \
   -bz=2                                                                               \
   -dataset_dir="/dataset/mscoco_2017"                                                 \
   -anno="annotations/sample_10_instances_val2017.json"                                \
   -imgd="val2017"                                                                     \
-  -fake                                                                               \
-  -i=5
+  -i=5                                                                                \
+  -g=2
