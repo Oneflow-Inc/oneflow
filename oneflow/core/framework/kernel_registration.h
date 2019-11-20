37cfac5ddc93a23bb19b5e846e8ea6a0aa4ae170
Dounm <niuchong893184@gmail.com>
2019-11-20 12:33:56 +0800

    Make REGISTER_USER_OP/KERNEL macro available when not in namespace of oneflow

--- oneflow/core/framework/kernel_registration.h
+++ oneflow/core/framework/kernel_registration.h
@@ -54,9 +54,10 @@ std::vector<std::string> GetAllUserOpInKernelRegistry();
 
 }  // namespace user_op
 
-#define REGISTER_USER_KERNEL(name)                                            \
-  static user_op::Registrar<user_op::KernelRegistryWrapperBuilder> OF_PP_CAT( \
-      g_registrar, __COUNTER__) = user_op::KernelRegistryWrapperBuilder(name)
 }  // namespace oneflow
+
+#define REGISTER_USER_KERNEL(name)                                                       \
+  static ::oneflow::user_op::Registrar<::oneflow::user_op::KernelRegistryWrapperBuilder> \
+      OF_PP_CAT(g_registrar, __COUNTER__) = ::oneflow::user_op::KernelRegistryWrapperBuilder(name)
 
 #endif  // ONEFLOW_CORE_FRAMEWORK_KERNEL_REGISTRATION_H_
