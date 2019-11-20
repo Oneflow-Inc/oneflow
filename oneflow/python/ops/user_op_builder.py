90aeffb5e220493e64daa22624b1a6f82431c493
chengtbf <472491134@qq.com>
2019-11-20 18:44:16 +0800

    user op wrapper

--- oneflow/python/ops/user_op_builder.py
+++ oneflow/python/ops/user_op_builder.py
@@ -10,18 +10,64 @@ import oneflow.core.register.logical_blob_id_pb2 as logical_blob_id_util
 import oneflow
 from oneflow.python.oneflow_export import oneflow_export
 
-class UserOpBuilderWrapper(object):
-    def __init__(self)
-
-class UserOpBuilder(object):
+class UserOpWrapper(object):
     def __init__(self, op_name):
         self.op_conf_ = op_conf_util.OperatorConf()
         self.user_conf_ = self.op_conf_.user_conf
         self.op_conf_.name = op_name
 
+    @property
+    def op_type_name(self): return self.user_conf_.op_type_name
+
+    @property
+    def user_conf(self): return self.user_conf_
+
+    def SelfCheck(self):
+        # TODO()
+        return self
+
+    def AddDefaultValue(self):
+        # TODO()
+        return self
+
+    def RemoteBlobList(self):
+        remote_blob_list = []
+        compile_context.CurJobAddOp(self.op_conf_)
+        for k in self.user_conf_.output:
+            for i in range(self.user_conf_.output[k].s):
+                lbi = logical_blob_id_util.LogicalBlobId()
+                lbi.op_name = self.op_conf_.name
+                lbi.blob_name = k + '_' + str(i)
+                remote_blob_list.append(remote_blob_util.RemoteBlob(lbi))
+        return tuple(remote_blob_list)
+
+class UserOpWrapperBuilder(object):
+    def __init__(self, op_name):
+        self.user_op_ = UserOpWrapper(op_name)
+
+    def Build(self):
+        assert self.user_op_.op_type_name is not ""
+        return self.user_op_
+
     def Op(self, op_type_name):
         # self.op_registration_val_ = c_api_util.LookUpInOpRegistry(op_type_name)
-        self.user_conf_.op_type_name = op_type_name
+        self.user_op_.op_type_name = op_type_name
+        return self
+
+    def Input(self, input_name, input_blob_list):
+        assert isinstance(input_blob_list, (tuple, list))
+        for input_blob in input_blob_list
+          assert type(input_blob) is blob_desc.BlobDesc
+          self.user_op_.user_conf.input[input_name].s.append(input_blob.logical_blob_name)
+        return self
+
+    def Output(self, output_name, num = 1):
+        assert type(num) is int and num >= 1
+        out_lbns = []
+        for i in range(num):
+            lbn = self.op_conf_.name + '/' output_name + '_' + str(i)
+            out_lbns.append(lbn)
+        self.user_op_.user_conf.output[output_name].s[:] = out_lbns
         return self
 
     def SetAttr(self, attr_name, attr_value, attr_type):
@@ -63,23 +109,6 @@ class UserOpBuilder(object):
             attribute.at_list_float.val[:] = list(attr_value)
         else:
             assert False, "Unknow op attribute type: {}".format(attr_type)
-        self.user_conf_.attr[attr_name] = attribute
+        self.user_op_.user_conf.attr[attr_name] = attribute
         return self
-
-    def Input(self, input_name, input_blob_list):
-        assert isinstance(input_blob_list, (tuple, list))
-        for input_blob in input_blob_list
-          assert type(input_blob) is blob_desc.BlobDesc
-          self.user_conf_.input[input_name].s.append(input_blob.logical_blob_name)
-        return self
-
-    def Output(self, output_name, num = 1):
-        assert type(num) is int and num >= 1
-        out_lbns = []
-        for i in range(num):
-            lbn = self.op_conf_.name + '/' output_name + '_' + str(i)
-            out_lbns.append(lbn)
-        self.user_conf_.output[output_name].s[:] = out_lbns
-        return self
-
 
