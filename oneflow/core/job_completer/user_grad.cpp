349c4d13de8f152aa2ad1d1e6ab5df43e0f3a1a3
cheng cheng <472491134@qq.com>
2019-11-27 19:31:56 +0800

    define grad registration value func

--- oneflow/core/job_completer/user_grad.cpp
+++ oneflow/core/job_completer/user_grad.cpp
@@ -12,22 +12,22 @@ void GenerateBackwardOpConf(
     const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4BnInOp) {
   CHECK(op.op_conf().has_user_conf());
   const UserOpConf& user_conf = op.op_conf().user_conf();
-  const user_op::GradRegistrationVal* val = LookUpInGradRegistry(user_conf.op_type_name());
+  const user_op::GradRegistrationVal* val = user_op::LookUpInGradRegistry(user_conf.op_type_name());
   CHECK(val != nullptr) << " Cannot find op_type: " << user_conf.op_type_name()
-    << " 's grad func in GradRegistration!";
-  UserOpWrapper user_op(op.op_conf(), LogicalBlobDesc4BnInOp, DiffLbi4BnInOp);
-  auto AddOp = [&] (const UserOpConfWrapper& wrapper) {
+                        << " 's grad func in GradRegistration!";
+  user_op::UserOpWrapper user_op(op.op_conf(), LogicalBlobDesc4BnInOp, DiffLbi4BnInOp);
+  auto AddOp = [&](const user_op::UserOpConfWrapper& wrapper) {
     op_confs->push_back(wrapper.op_conf());
   };
-  
-  val->gen_bw_fn(/*user_op, AddOp*/);
 
-  for(const std::string& ibn : op.input_bns()) {
+  val->gen_bw_fn(user_op, AddOp);
+
+  for (const std::string& ibn : op.input_bns()) {
     LogicalBlobId* lbi = DiffLbi4BnInOp(ibn);
     if (lbi != nullptr) {
-      CHECK(lbi.has_op_name() && lbi.has_blob_name()) << " user_op: " << op.op_name() 
-          << " op_type_name: " << user_conf.op_type_name() 
-          << " 's input blob " << ibn << " has not generate input diff blob !"; 
+      CHECK(lbi->has_op_name() && lbi->has_blob_name())
+          << " user_op: " << op.op_name() << " op_type_name: " << user_conf.op_type_name()
+          << " 's input blob " << ibn << " has not generate input diff blob !";
     }
   }
 }
