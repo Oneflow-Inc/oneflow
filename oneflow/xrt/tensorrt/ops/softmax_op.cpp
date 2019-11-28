bcf593657a919314e6faf031557d56f49e3fa6d2
nlqq <snow3s@qq.com>
2019-11-28 20:32:53 +0800

    test pass

--- oneflow/xrt/tensorrt/ops/softmax_op.cpp
+++ oneflow/xrt/tensorrt/ops/softmax_op.cpp
@@ -5,15 +5,6 @@ namespace oneflow {
 namespace xrt {
 namespace tensorrt {
 
-nvinfer1::MatrixOperation GetMatrixOperation(nvinfer1::ITensor *x,
-                                             bool transpose) {
-  if (x->getDimensions().nbDims < 2) {
-    return nvinfer1::MatrixOperation::kVECTOR;
-  }
-  return transpose ? nvinfer1::MatrixOperation::kTRANSPOSE
-                   : nvinfer1::MatrixOperation::kNONE;
-}
-
 class SoftMaxOp : public TrtOpKernel {
  public:
   void Compile(TrtOpContext *ctx) override {
@@ -21,18 +12,15 @@ class SoftMaxOp : public TrtOpKernel {
     CHECK_GE(in_shape.NumAxes(), 2);
 
     int32_t axis = ctx->GetAttr<int32_t>("axis");
+    if (axis < 0) {
+      axis += in_shape.NumAxes();
+    }
     CHECK_GE(axis, 1);
     CHECK_LT(axis, in_shape.NumAxes() - 1);
-    //dims = in_shape.NumAxes()
-    Shape transposed_shape = in_shape;
+  
     nvinfer1::ITensor *in = ctx->Input("in");
-    bool need_transpose =false; 
-    axis == in_shape.NumAxes() - 1 ? need_transpose = false : true;
-    if(need_transpose) {
-      transposed_shape = in_shape.Set(axis, in_shape.At(in_shape.NumAxes() - 1));
-      transposed_shape = transposed_shape.Set(in_shape.NumAxes() - 1, in_shape.At(axis)); 
-    }
-    auto *layer = ctx->builder()->addSoftMax(*GetMatrixOperation(*in, need_transpose));
+    auto *layer = ctx->builder()->addSoftMax(*in);
+    layer->setAxes(axis);
     ctx->SetOutput("out", layer->getOutput(0));
   }
 };
