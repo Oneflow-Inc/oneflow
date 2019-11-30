3a2cc1320d669971fb1df06d75c1f669bebc247e
nlqq <snow3s@qq.com>
2019-11-30 19:29:57 +0800

    pooling test pass

---
+++ oneflow/xrt/tensorrt/ops/pooling_op.cpp
@@ -0,0 +1,43 @@
+#include "oneflow/xrt/tensorrt/ops/op_context.h"
+#include "oneflow/xrt/tensorrt/ops/op_kernel.h"
+#include "oneflow/core/operator/op_conf.pb.h"
+#include "oneflow/xrt/tensorrt/trt_logger.h"
+
+#include "NvInfer.h"
+#include "NvInferRuntimeCommon.h"
+#include "strings.h"
+namespace oneflow {
+namespace xrt {
+namespace tensorrt {
+
+template <nvinfer1::PoolingType pooling_type>
+class PoolingOp : public TrtOpKernel {
+ public:
+  void Compile(TrtOpContext *ctx) override {
+    Shape in_shape = ctx->InputShape("in");
+    CHECK_GE(in_shape.NumAxes(), 3);
+    CHECK_LE(in_shape.NumAxes(), 5);
+
+    nvinfer1::ITensor *in = ctx->Input("in");
+    const std::string padding = ctx->GetAttr<std::string>("padding");
+    std::vector<int32_t> pool_size = ctx->GetAttr<std::vector<int32_t>>("pool_size");
+    std::vector<int32_t> strides = ctx->GetAttr<std::vector<int32_t>>("strides");
+    auto *layer = ctx->builder()->addPooling(*in, pooling_type,
+        nvinfer1::DimsHW(pool_size[0], pool_size[1]));
+    layer->setStride(nvinfer1::DimsHW(strides[0], strides[1]));
+    const std::string valid_str = "valid";
+    if(strcasecmp(padding.c_str(), valid_str.c_str()) == 0)
+      layer->setPaddingMode(nvinfer1::PaddingMode::kEXPLICIT_ROUND_DOWN);
+    const std::string same_str = "same";
+    if(strcasecmp(padding.c_str(), same_str.c_str()) == 0)
+      layer->setPaddingMode(nvinfer1::PaddingMode::kSAME_LOWER);
+    ctx->SetOutput("out", layer->getOutput(0));
+  }
+};
+
+REGISTER_TRT_OP_KERNEL(MaxPooling2D, PoolingOp<nvinfer1::PoolingType::kMAX>).EnableTrainPhase().Finalize();
+REGISTER_TRT_OP_KERNEL(AveragePooling2D, PoolingOp<nvinfer1::PoolingType::kAVERAGE>).EnableTrainPhase().Finalize();
+
+}  // namespace tensorrt
+}  // namespace xrt
+}  // namespace oneflow
