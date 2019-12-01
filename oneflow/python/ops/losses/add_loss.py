f2faabee00a95f59c88396c83b2a0ac72521394b
leaves-zwx <kunta0932@gmail.com>
2019-12-01 23:33:35 +0800

    add_loss support MirrorBlob

--- oneflow/python/ops/losses/add_loss.py
+++ oneflow/python/ops/losses/add_loss.py
@@ -5,6 +5,11 @@ import oneflow.python.framework.remote_blob as remote_blob_util
 
 from oneflow.python.oneflow_export import oneflow_export
 
+
 @oneflow_export("losses.add_loss")
 def add_loss(loss):
-    job_builder.CurCtxAddLossLogicalBlobName(loss.logical_blob_name)
+    if isinstance(loss, remote_blob_util.MirrorBlob):
+        for sub_blob in loss.sub_consistent_blob_list:
+            job_builder.CurCtxAddLossLogicalBlobName(sub_blob.logical_blob_name)
+    else:
+        job_builder.CurCtxAddLossLogicalBlobName(loss.logical_blob_name)
