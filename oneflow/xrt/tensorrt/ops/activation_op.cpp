c88d762a042434ddfe7775e9f3f44ce4c94ebfc8
nlqq <snow3s@qq.com>
2019-12-02 10:15:08 +0800

    add activation ops, test not psassed

--- oneflow/xrt/tensorrt/ops/activation_op.cpp
+++ oneflow/xrt/tensorrt/ops/activation_op.cpp
@@ -1,40 +1,28 @@
+#include "NvInferRuntimeCommon.h"
+#include "oneflow/core/operator/op_conf.pb.h"
 #include "oneflow/xrt/tensorrt/ops/op_context.h"
 #include "oneflow/xrt/tensorrt/ops/op_kernel.h"
-#include "oneflow/core/operator/op_conf.pb.h"
 #include "oneflow/xrt/tensorrt/trt_logger.h"
-#include "NvInferRuntimeCommon.h"
 
 namespace oneflow {
 namespace xrt {
 namespace tensorrt {
 
-nvinfer1::ActivationType GetActivationType() {
-  if (oneflow::ActivationType::kRelu) 
-    //case oneflow::kRelu:
-      return nvinfer1::ActivationType::kRELU;
-   else if (oneflow::ActivationType::kTanH)      
-      return nvinfer1::ActivationType::kTANH;
-    else if (oneflow::ActivationType::kSigmoid)     
-      return nvinfer1::ActivationType::kSIGMOID; 
-    else {
-      LOG(FATAL) << "No valid activation type get!"; 
-      return nvinfer1::ActivationType::kRELU;
-    } 
-  
-}
-
+template <nvinfer1::ActivationType activation_type>
 class ActivationOp : public TrtOpKernel {
  public:
   void Compile(TrtOpContext *ctx) override {
     Shape in_shape = ctx->InputShape("in");
 
     nvinfer1::ITensor *in = ctx->Input("in");
-    auto *layer = ctx->builder()->addActivation(*in, GetActivationType());
+    auto *layer = ctx->builder()->addActivation(*in, activation_type);
     ctx->SetOutput("out", layer->getOutput(0));
   }
 };
 
-REGISTER_TRT_OP_KERNEL(Activation, ActivationOp).EnableTrainPhase().Finalize();
+REGISTER_TRT_OP_KERNEL(Tanh, ActivationOp<nvinfer1::ActivationType::kTANH>).EnableTrainPhase().Finalize();
+REGISTER_TRT_OP_KERNEL(Relu, ActivationOp<nvinfer1::ActivationType::kRELU>).EnableTrainPhase().Finalize();
+REGISTER_TRT_OP_KERNEL(Sigmoid, ActivationOp<nvinfer1::ActivationType::kSIGMOID>).EnableTrainPhase().Finalize();
 
 }  // namespace tensorrt
 }  // namespace xrt
