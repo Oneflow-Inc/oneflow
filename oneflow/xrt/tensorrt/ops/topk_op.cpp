fdc6410bf1f5fd2199d719e88e8a9bfa8a1ef0b6
nlqq <snow3s@qq.com>
2019-12-03 21:17:42 +0800

    add identity op and topk op, to test

---
+++ oneflow/xrt/tensorrt/ops/topk_op.cpp
@@ -0,0 +1,29 @@
+#include "oneflow/xrt/tensorrt/ops/op_context.h"
+#include "oneflow/xrt/tensorrt/ops/op_kernel.h"
+
+namespace oneflow {
+namespace xrt {
+namespace tensorrt {
+
+class TopKOp : public TrtOpKernel {
+ public:
+  void Compile(TrtOpContext *ctx) override {
+    Shape in_shape = ctx->InputShape("in");
+//    CHECK_GE(in_shape.NumAxes(), 2);
+
+    int32_t k = ctx->GetAttr<int32_t>("k");
+    //CHECK_GE(axis, 1);
+    //CHECK_LT(axis, in_shape.NumAxes());
+    uint32_t reduceAxes = in_shape.dim_vec().back(); 
+    nvinfer1::ITensor *in = ctx->Input("in");
+    auto *layer = ctx->builder()->addTopK(*in, nvinfer1::TopKOperation::kMAX, k,
+       reduceAxes);
+    ctx->SetOutput("out", layer->getOutput(0));
+  }
+};
+
+REGISTER_TRT_OP_KERNEL(TopK, TopKOp).EnableTrainPhase().Finalize();
+
+}  // namespace tensorrt
+}  // namespace xrt
+}  // namespace oneflow
