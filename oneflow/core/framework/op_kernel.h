a231f8aad6b19d0113ed11abd585cedb17717c11
Dounm <niuchong893184@gmail.com>
2019-12-04 19:18:32 +0800

    Merge dev_user_op Part 4: add UserOp and UserKernel as the bridge between user code and system code

---
+++ oneflow/core/framework/op_kernel.h
@@ -0,0 +1,35 @@
+#ifndef ONEFLOW_CORE_FRAMEWORK_OP_KERNEL_H_
+#define ONEFLOW_CORE_FRAMEWORK_OP_KERNEL_H_
+
+#include "oneflow/core/framework/util.h"
+#include "oneflow/core/device/device_context.h"
+#include "oneflow/core/framework/tensor.h"
+#include "oneflow/core/framework/user_op_conf.h"
+#include "oneflow/core/framework/kernel_context.h"
+
+namespace oneflow {
+
+class KernelCtx;
+
+namespace user_op {
+
+class KernelInitContext final {};
+
+class OpKernel {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(OpKernel);
+  virtual ~OpKernel() = default;
+
+  virtual void Compute(KernelContext*) = 0;
+
+ protected:
+  OpKernel(const KernelInitContext&) {}
+
+ private:
+};
+
+}  // namespace user_op
+
+}  // namespace oneflow
+
+#endif
