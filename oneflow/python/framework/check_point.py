b129f5d6481695e1c6cba4e7749ec6995bdcff1f
Tsai <caishenghang@oneflow.org>
2019-12-04 16:15:22 +0800

    check point save makes more sense

--- oneflow/python/framework/check_point.py
+++ oneflow/python/framework/check_point.py
@@ -100,8 +100,11 @@ class SimpleCheckPointManager(object):
             self._checkpoint.init()
             self.save()
 
-    def save(self):
-        self._checkpoint.save(self._GetSnapshotPath(self._NextSnapshotName()))
+    def save(self, path=None):
+        if path is None:
+            self._checkpoint.save(self._GetSnapshotPath(self._NextSnapshotName()))
+        else:
+            self._checkpoint.save(path)
 
     def _NextSnapshotName(self):
         return self._prefix + datetime.datetime.now().strftime('%Y%m%d_%H%M%S_%f')
