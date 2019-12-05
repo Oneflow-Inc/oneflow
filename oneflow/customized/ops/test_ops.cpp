3ceee3a273a8ccaeca046d61bd71dfdcc23260d4
cheng cheng <472491134@qq.com>
2019-12-05 18:12:07 +0800

    fix bug of useless

--- oneflow/customized/ops/test_ops.cpp
+++ oneflow/customized/ops/test_ops.cpp
@@ -2,15 +2,17 @@
 
 namespace oneflow {
 
-REGISTER_USER_OP("ccrelu").Input("in").Output("out").SetShapeInferFn(
-    [](user_op::InferContext* ctx) -> Maybe<void> {
+REGISTER_USER_OP("ccrelu")
+    .Input("in")
+    .Output("out")
+    .SetShapeInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
       Shape* in_shape = ctx->Shape4ArgNameAndIndex("in", 0);
       Shape* out_shape = ctx->Shape4ArgNameAndIndex("out", 0);
       *out_shape = *in_shape;
       // int32_t last_axis = in_shape->NumAxes() - 1;
       // out_shape->Set(last_axis, in_shape->At(last_axis) * 2);
       return Maybe<void>::Ok();
-    });
+    })
     .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
       const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("in", 0);
       SbpSignatureBuilder()
