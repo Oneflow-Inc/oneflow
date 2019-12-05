79af6732f4056ff9543554b8c23df442fda6481f
scxfjiang <sc.xfjiang@gmail.com>
2019-12-05 19:15:04 +0800

    add device type to test activations

--- oneflow/python/test/ops/test_activations.py
+++ oneflow/python/test/ops/test_activations.py
@@ -4,13 +4,16 @@ import torch
 import math
 
 
-def test_relu(test_case):
+def _test_relu(device_type):
+    flow.clear_default_session()
+
     flow.config.gpu_device_num(1)
     flow.config.default_data_type(flow.float)
 
     @flow.function
     def ReluJob(x=flow.input_blob_def((10,))):
-        return flow.keras.activations.relu(x)
+        with flow.device_prior_placement(device_type, "0:0"):
+            return flow.keras.activations.relu(x)
 
     ratios = [-2, -1, 0, 1, 2]
     ones = np.ones((10,), dtype=np.float32)
@@ -18,16 +21,24 @@ def test_relu(test_case):
         x = ones * r
         of_ret = ReluJob(x).get()
         expected = x * (r > 0)
-        test_case.assertTrue(np.allclose(expected, of_ret))
+        assert np.allclose(expected, of_ret)
 
 
-def test_gelu(test_case):
+def test_relu(test_case):
+    _test_relu("gpu")
+    _test_relu("cpu")
+
+
+def _test_gelu(device_type):
+    flow.clear_default_session()
+
     flow.config.gpu_device_num(1)
     flow.config.default_data_type(flow.float)
 
     @flow.function
     def GeluJob(x=flow.input_blob_def((10,))):
-        return flow.keras.activations.gelu(x)
+        with flow.device_prior_placement(device_type, "0:0"):
+            return flow.keras.activations.gelu(x)
 
     def gelu(x):
         return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
@@ -38,16 +49,24 @@ def test_gelu(test_case):
         x = ones * r
         of_out = GeluJob(x).get()
         torch_out = gelu(torch.tensor(x)).numpy()
-        test_case.assertTrue(np.allclose(of_out, torch_out, rtol=1e-3, atol=1e-4))
+        assert np.allclose(of_out, torch_out, rtol=1e-3, atol=1e-4)
 
 
-def test_sigmoid(test_case):
+def test_gelu(test_case):
+    _test_gelu("gpu")
+    _test_gelu("cpu")
+
+
+def _test_sigmoid(device_type):
+    flow.clear_default_session()
+
     flow.config.gpu_device_num(1)
     flow.config.default_data_type(flow.float)
 
     @flow.function
     def SigmoidJob(x=flow.input_blob_def((10,))):
-        return flow.keras.activations.sigmoid(x)
+        with flow.device_prior_placement(device_type, "0:0"):
+            return flow.keras.activations.sigmoid(x)
 
     ratios = [-2, -1, 0, 1, 2]
     ones = np.ones((10,), dtype=np.float32)
@@ -55,16 +74,24 @@ def test_sigmoid(test_case):
         x = ones * r
         of_out = SigmoidJob(x).get()
         torch_out = torch.sigmoid(torch.Tensor(x)).numpy()
-        test_case.assertTrue(np.allclose(of_out, torch_out))
+        assert np.allclose(of_out, torch_out)
 
 
-def test_tanh(test_case):
+def test_sigmoid(test_case):
+    _test_sigmoid("gpu")
+    _test_sigmoid("cpu")
+
+
+def _test_tanh(device_type):
+    flow.clear_default_session()
+
     flow.config.gpu_device_num(1)
     flow.config.default_data_type(flow.float)
 
     @flow.function
     def TanHJob(x=flow.input_blob_def((10,))):
-        return flow.keras.activations.tanh(x)
+        with flow.device_prior_placement(device_type, "0:0"):
+            return flow.keras.activations.tanh(x)
 
     ratios = [-2, -1, 0, 1, 2]
     ones = np.ones((10,), dtype=np.float32)
@@ -72,4 +99,9 @@ def test_tanh(test_case):
         x = ones * r
         of_out = TanHJob(x).get()
         torch_out = torch.tanh(torch.Tensor(x)).numpy()
-        test_case.assertTrue(np.allclose(of_out, torch_out))
+        assert np.allclose(of_out, torch_out)
+
+
+def test_tanh(test_case):
+    _test_tanh("gpu")
+    _test_tanh("cpu")
