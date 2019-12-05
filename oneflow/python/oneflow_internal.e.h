4b9a7d056dc488a8ee3a230b8bf28f62cc1f06b7
lixinqi <lixinqi0703106@163.com>
2019-12-05 18:16:06 +0800

    remove unused code
    Former-commit-id: 6798b90610cbb689545359023cedc9aae7cb0421

--- oneflow/python/oneflow_internal.e.h
+++ oneflow/python/oneflow_internal.e.h
@@ -2,11 +2,6 @@
 #include "oneflow/core/common/data_type_seq.h"
 
 #define DEFINE_COPIER(T, type_proto)                                                     \
-  void OfBlob_CopyFromBuffer_##T(uint64_t of_blob_ptr, T* array, int size) {             \
-    using namespace oneflow;                                                             \
-    auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);                              \
-    of_blob->AutoMemCopyFrom<T>(array, size);                                            \
-  }                                                                                      \
   void OfBlob_CurTensorCopyToBuffer_##T(uint64_t of_blob_ptr, T* array, int size) {      \
     using namespace oneflow;                                                             \
     auto* of_blob = reinterpret_cast<OfBlob*>(of_blob_ptr);                              \
@@ -21,17 +16,6 @@
 OF_PP_FOR_EACH_TUPLE(DEFINE_COPIER, POD_DATA_TYPE_SEQ);
 
 #undef DEFINE_COPIER
-
-std::string Dtype_GetOfBlobCopyFromBufferFuncName(int64_t dtype) {
-  using namespace oneflow;
-  static const HashMap<int64_t, std::string> data_type2func_name{
-#define DATA_TYPE_FUNC_NAME_PAIR(type_cpp, type_proto) \
-  {type_proto, "OfBlob_CopyFromBuffer_" #type_cpp},
-      OF_PP_FOR_EACH_TUPLE(DATA_TYPE_FUNC_NAME_PAIR, POD_DATA_TYPE_SEQ)
-#undef DATA_TYPE_FUNC_NAME_PAIR
-  };
-  return data_type2func_name.at(dtype);
-}
 
 std::string Dtype_GetOfBlobCurTensorCopyToBufferFuncName(int64_t dtype) {
   using namespace oneflow;
