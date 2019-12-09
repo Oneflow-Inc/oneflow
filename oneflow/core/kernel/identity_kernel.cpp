bbcac6aadbd6e0bb7c9a0b8a2e0baf8b139150cb
Juncheng <liujuncheng1022@gmail.com>
2019-12-09 10:18:37 +0800

    Dev copy hd valid content (#2489)
    * copy_hd CopyValidDataContentFrom
    
    * refine copy hd op
    
    
    Former-commit-id: bd22cc11e56870965b6c98396157016d6ae5befb

--- oneflow/core/kernel/identity_kernel.cpp
+++ oneflow/core/kernel/identity_kernel.cpp
@@ -2,19 +2,10 @@
 
 namespace oneflow {
 
-namespace {
-
-void CheckSizeAndCopyBlob(DeviceCtx *ctx, Blob *dst, const Blob *src) {
-  CHECK_EQ(src->ByteSizeOfDataContentField(), dst->ByteSizeOfDataContentField());
-  dst->CopyDataContentFrom(ctx, src);
-}
-
-}  // namespace
-
 template<DeviceType device_type>
 void IdentityKernel<device_type>::ForwardDataContent(
     const KernelCtx &ctx, std::function<Blob *(const std::string &)> BnInOp2Blob) const {
-  CheckSizeAndCopyBlob(ctx.device_ctx, BnInOp2Blob("out"), BnInOp2Blob("in"));
+  BnInOp2Blob("out")->CopyValidDataContentFrom(ctx.device_ctx, BnInOp2Blob("in"));
 }
 
 ADD_DEVICE_TYPE_KERNEL_CREATOR(OperatorConf::kIdentityConf, IdentityKernel);
