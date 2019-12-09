d6e2c6ce70afa8498d392087be0f4cfd13dc4768
scxfjiang <sc.xfjiang@gmail.com>
2019-12-09 11:15:28 +0800

    Dev op unit test (#2484)
    * relu and gelu
    
    * matmul
    
    * activations
    
    * dense
    
    * reshape and concat
    
    * reduce_sum and reduce_mean
    
    * remove redue_demo.py
    
    * fix reduce mean
    
    * use cartesian product to generate test arguments
    
    * add device type
    
    * add device type to test activations
    
    * test gradient for matmul
    
    * relu backward
    
    * test bw for activations and reduce sum
    
    * reduce mean backward
    
    * reshape backward
    
    * concat backward
    
    * restore dense_demo
    
    * use tfa.activations.gelu
    
    * refine
    
    * add README.md
    
    * fix matmul
    
    * use OrderedDict

---
+++ oneflow/python/test/ops/README.md
@@ -0,0 +1,9 @@
+# Usage: 
+## Test all ops
+```
+python 1node_test.py
+```
+## Test specified ops (matmul, for example)
+```
+python 1node_test.py test_matmul
+```
---
+++ oneflow/python/test/ops/test_reduce_sum.py
@@ -0,0 +1,64 @@
+import os
+import numpy as np
+import tensorflow as tf
+import oneflow as flow
+from collections import OrderedDict 
+
+from test_util import GenArgList
+from test_util import GetSavePath
+from test_util import Save
+
+
+def compare_with_tensorflow(device_type, input_shape, axis, keepdims):
+    assert device_type in ["gpu", "cpu"]
+    flow.clear_default_session()
+    flow.config.gpu_device_num(1)
+    flow.config.default_data_type(flow.float)
+
+    @flow.function
+    def ReduceSumJob():
+        flow.config.train.primary_lr(1e-4)
+        flow.config.train.model_update_conf(dict(naive_conf={}))
+        with flow.device_prior_placement(device_type, "0:0"):
+            x = flow.get_variable(
+                "x",
+                shape=input_shape,
+                dtype=flow.float,
+                initializer=flow.random_uniform_initializer(minval=-10, maxval=10),
+                trainable=True,
+            )
+            loss = flow.math.reduce_sum(x, axis=axis, keepdims=keepdims)
+            flow.losses.add_loss(loss)
+
+            flow.watch(x, Save("x"))
+            flow.watch_diff(x, Save("x_diff"))
+            flow.watch(loss, Save("loss"))
+            flow.watch_diff(loss, Save("loss_diff"))
+
+            return loss
+
+    # OneFlow
+    check_point = flow.train.CheckPoint()
+    check_point.init()
+    of_out = ReduceSumJob().get()
+    # TensorFlow
+    with tf.GradientTape(persistent=True) as tape:
+        x = tf.Variable(np.load(os.path.join(GetSavePath(), "x.npy")))
+        tf_out = tf.math.reduce_sum(x, axis=axis, keepdims=keepdims)
+    loss_diff = np.load(os.path.join(GetSavePath(), "loss_diff.npy"))
+    tf_x_diff = tape.gradient(tf_out, x, loss_diff)
+
+    assert np.allclose(of_out, tf_out.numpy(), rtol=1e-5, atol=1e-5)
+    assert np.allclose(
+        np.load(os.path.join(GetSavePath(), "x_diff.npy")), tf_x_diff.numpy(), rtol=1e-5, atol=1e-5
+    )
+
+
+def test_reduce_sum(test_case):
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["gpu"]
+    arg_dict["input_shape"] = [(64, 64, 64)]
+    arg_dict["axis"] = [None, [1], [0, 2]]
+    arg_dict["keepdims"] = [True, False]
+    for arg in GenArgList(arg_dict):
+        compare_with_tensorflow(*arg)
