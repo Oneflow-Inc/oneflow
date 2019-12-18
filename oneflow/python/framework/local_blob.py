b7aad34c4acc91cc087ff889f7ebe21d1f1743e5
lixinqi <lixinqi0703106@163.com>
2019-12-18 17:09:29 +0800

    rename to LocalFixedTensor/LocalMirroredTensor/LocalMirroredTensorList

--- oneflow/python/framework/local_blob.py
+++ oneflow/python/framework/local_blob.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import oneflow.python.framework.remote_blob as remote_blob_util
 import numpy as np
 
-class LocalTensor(object):
+class LocalFixedTensor(object):
     def __init__(self, ndarray = None):
         self.ndarray_ = ndarray
         
@@ -14,13 +14,13 @@ class LocalTensor(object):
     def __getattr__(self, attr):
         return getattr(self.ndarray_, attr)
 
-class LocalTensorList(object):
+class LocalMirroredTensor(object):
     def __init__(self, ndarray_list = None):
         self.ndarray_list_ = ndarray_list
         
     def ndarray_list(self): return self.ndarray_list_
     
-class LocalTensorLists(object):
+class LocalMirroredTensorList(object):
     def __init__(self, ndarray_lists = None):
         self.ndarray_lists_ = ndarray_lists
         
@@ -29,30 +29,30 @@ class LocalTensorLists(object):
 def MakeLocalBlob(ndarray_lists, consistent_blob):
     assert type(consistent_blob) is remote_blob_util.ConsistentBlob
     if consistent_blob.is_tensor_list:
-        return LocalTensorLists(ndarray_lists)
+        return LocalMirroredTensorList(ndarray_lists)
     assert len(ndarray_lists) == 1
     if consistent_blob.is_dynamic:
-        return LocalTensorList(ndarray_lists[0])
+        return LocalMirroredTensor(ndarray_lists[0])
     assert len(ndarray_lists[0]) == 1
-    return LocalTensor(ndarray_lists[0][0])
+    return LocalFixedTensor(ndarray_lists[0][0])
 
     
 def MergeLocalBlobs(local_blob_list, mirrored_blob):
     assert type(mirrored_blob) is remote_blob_util.MirroredBlob
     if mirrored_blob.is_tensor_list:
         for local_blob in local_blob_list:
-            assert type(local_blob) is LocalTensorLists
-        return LocalTensorLists([x.ndarray_lists()[0] for x in local_blob_list])
+            assert type(local_blob) is LocalMirroredTensorList
+        return LocalMirroredTensorList([x.ndarray_lists()[0] for x in local_blob_list])
     if mirrored_blob.is_dynamic:
         for local_blob in local_blob_list:
-            assert type(local_blob) is LocalTensorList
-        return LocalTensorList([x.ndarray_list()[0] for x in local_blob_list])
+            assert type(local_blob) is LocalMirroredTensor
+        return LocalMirroredTensor([x.ndarray_list()[0] for x in local_blob_list])
     for local_blob in local_blob_list:
-        assert type(local_blob) is LocalTensor
+        assert type(local_blob) is LocalFixedTensor
         batch_axis = mirrored_blob.batch_axis
         assert type(batch_axis) is int
         ndarray = np.concatenate([x.ndarray() for x in local_blob_list], axis=batch_axis)
-        return LocalTensor(ndarray)
+        return LocalFixedTensor(ndarray)
 
 non_override_field = set([
     '__class__',
@@ -73,11 +73,11 @@ non_override_field = set([
 
 def MakeBlobMethod(field_name):
     def ConvertOtherArgs(args):
-        return [x.ndarray() if isinstance(x, LocalTensor) else x for x in args]
+        return [x.ndarray() if isinstance(x, LocalFixedTensor) else x for x in args]
     return lambda self, *args: getattr(self.ndarray(), field_name)(*ConvertOtherArgs(args))
 
 for field_name in dir(np.ndarray):
     if field_name.startswith('__') == False: continue
     if field_name in non_override_field: continue
-    if hasattr(LocalTensor, field_name) == False:
-        setattr(LocalTensor, field_name, MakeBlobMethod(field_name))
+    if hasattr(LocalFixedTensor, field_name) == False:
+        setattr(LocalFixedTensor, field_name, MakeBlobMethod(field_name))
