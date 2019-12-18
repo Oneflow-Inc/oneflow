4c80e4204e4b36c2a9d3f055c47611917d2f2749
leaves-zwx <kunta0932@gmail.com>
2019-12-19 01:34:23 +0800

    slice v3 gpu impl

--- oneflow/python/test/slice.py
+++ oneflow/python/test/slice.py
@@ -4,23 +4,25 @@ import numpy as np
 
 def test_case_1():
     r"""dim0 slice"""
+    func_config = of.FunctionConfig()
+    func_config.default_data_type(of.float)
 
-    @of.function
-    def slice_dim0_job(
-        input_blob=of.input_blob_def(shape=(2, 10, 4), dtype=of.float32)
-    ):
-        part1 = of.slice(input_blob, (0, 0, 0), (1, -1, -1))
-        part2 = of.slice(input_blob, (1, 0, 0), (1, -1, -1))
+    @of.function(func_config)
+    def slice_dim0_job(input_blob=of.FixedTensorDef(shape=(2, 5, 4), dtype=of.float32)):
+        part1 = of.slice(input_blob, (None, 0, 0), (None, 2, None))
+        part2 = of.slice(input_blob, (None, 2, 0), (None, 3, None))
         return part1, part2
 
-    input = np.random.rand(2, 10, 4).astype(np.float32)
+    input = np.random.rand(2, 5, 4).astype(np.float32)
     p1, p2 = slice_dim0_job(input).get()
 
+    ref1 = input[:, :2, :]
+    ref2 = input[:, 2:5, :]
     print("input: \n", input)
-    print("part1: \n", input[0:1, :, :])
-    print("part2: \n", input[1:, :, :])
-    assert np.allclose(p1.ndarray(), input[0:1, :, :])
-    assert np.allclose(p2.ndarray(), input[1:, :, :])
+    print("part1: \n", ref1)
+    print("part2: \n", ref2)
+    assert np.allclose(p1.ndarray(), ref1)
+    assert np.allclose(p2.ndarray(), ref2)
 
 
 if __name__ == "__main__":
