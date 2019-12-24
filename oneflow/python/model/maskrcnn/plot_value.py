a942068f579151f5f0f79a513ea544248dcb4d7d
Tsai <caishenghang@oneflow.org>
2019-12-24 17:55:29 +0800

    fix

--- oneflow/python/model/maskrcnn/plot_value.py
+++ oneflow/python/model/maskrcnn/plot_value.py
@@ -168,7 +168,7 @@ if __name__ == "__main__":
         flow_df["legend"].replace("primary_lr", "lr", inplace=True)
     flow_df = flow_df.groupby(["iter", "legend"], as_index=False).mean()
     torch_df = get_df(torch_metrics_path, "torch*.csv")
-    if torch_df["iter"].min() == 0:
+    if torch_df[torch_df["value"].notnull()]["iter"].min() == 0:
         torch_df["iter"] += 1
     limit, rate = get_metrics_sr(flow_df, torch_df)
     plot_many_by_legend(
