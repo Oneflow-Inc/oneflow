b711b1c67165066fcfa265670718a154f62fe2fc
chengcheng <472491134@qq.com>
2020-01-08 17:03:20 +0800

    abs op python wrapper and unitest scripts

--- oneflow/python/ops/math_ops.py
+++ oneflow/python/ops/math_ops.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import oneflow.python.framework.compile_context as compile_context
 import oneflow.python.framework.remote_blob as remote_blob_util
 import oneflow.python.framework.id_util as id_util
+import oneflow.python.ops.user_op_builder as user_op_builder
 import oneflow.core.operator.op_conf_pb2 as op_conf_util
 import oneflow.core.register.logical_blob_id_pb2 as logical_blob_id_util
 
@@ -621,3 +622,14 @@ def elem_cnt(input_blob, axis=None, dtype=None, name=None):
     out_lbi.op_name = op_conf.name
     out_lbi.blob_name = "y"
     return remote_blob_util.RemoteBlob(out_lbi)
+
+@oneflow_export("math.abs")
+def abs(x, name=None):
+    if name is None:
+        name = id_util.UniqueStr("Abs_")
+    return user_op_builder.UserOpConfWrapperBuilder(name).Op("unary")\
+            .Input("x",[x])\
+            .Output("y")\
+            .SetAttr("unary_math_type", "Abs", "AttrTypeString")\
+            .Build().RemoteBlobList()[0]
+
