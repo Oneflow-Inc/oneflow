28f05115f81c455301d17aff4093a6de84a64730
tsai <caishenghang@oneflow.org>
2020-01-08 14:05:06 +0800

    add test script

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -1,4 +1,5 @@
-pip3 install *.whl
+pwd=$1
+pip3 install $pwd/*.whl
 installed_path=$(python3 -c "import oneflow; print(oneflow.__path__[0])")
-python3 oneflow/python/test/ops/1node_test.py
-python3 oneflow/python/test/models/1node_test.py
+python3 $pwd/oneflow/python/test/ops/1node_test.py
+python3 $pwd/oneflow/python/test/models/1node_test.py
