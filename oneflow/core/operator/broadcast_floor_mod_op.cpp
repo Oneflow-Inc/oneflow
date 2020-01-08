76b341cbba961ec906a2c8065c9ea4f4ba834a97
ShawnXuan <xiexuanx2@gmail.com>
2020-01-08 11:14:53 +0800

    remove floor mod sbp (#2548)

--- oneflow/core/operator/broadcast_floor_mod_op.cpp
+++ oneflow/core/operator/broadcast_floor_mod_op.cpp
@@ -10,21 +10,10 @@ class BroadcastFloorModOp final : public BroadcastBinaryOp {
 
  private:
   const PbMessage& GetCustomizedConf() const override;
-  Maybe<void> VirtualGetSbpSignatures(
-      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
-      SbpSignatureList* sbp_sig_list) const override;
 };
 
 const PbMessage& BroadcastFloorModOp::GetCustomizedConf() const {
   return op_conf().broadcast_floor_mod_conf();
-}
-
-Maybe<void> BroadcastFloorModOp::VirtualGetSbpSignatures(
-    const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
-    SbpSignatureList* sbp_sig_list) const {
-  SbpSignatureBuilder().PartialSum("a").Broadcast("b").PartialSum("out").Build(
-      sbp_sig_list->mutable_sbp_signature()->Add());
-  return Maybe<void>::Ok();
 }
 
 REGISTER_OP(OperatorConf::kBroadcastFloorModConf, BroadcastFloorModOp);
