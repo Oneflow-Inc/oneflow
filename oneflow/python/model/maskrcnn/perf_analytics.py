9e2424ddf35ed524cddea6a8f9c28a23f5bec728
tsai <caishenghang@oneflow.org>
2020-01-09 15:18:45 +0800

    simple sort by std

--- oneflow/python/model/maskrcnn/perf_analytics.py
+++ oneflow/python/model/maskrcnn/perf_analytics.py
@@ -2,9 +2,16 @@ import pandas as pd
 import sqlite3
 
 
-con = sqlite3.connect("file:///home/tsai/Downloads/mask-rcnn-unknown-1x-2020-01-08-16:23:46-CST.sqlite")
+con = sqlite3.connect(
+    "file:///home/tsai/Downloads/mask-rcnn-unknown-1x-2020-01-08-16:23:46-CST.sqlite"
+)
 df = pd.read_sql_query("SELECT text, (end - start) AS duration FROM NVTX_EVENTS;", con)
-grouped = df.groupby("text")
-print(grouped.)
+grouped = (
+    df.groupby("text")
+    .agg({"duration": ["median", "mean", "std", "min", "max", "count"]})
+    .sort_values(by=[("duration", "std")], ascending=False)
+)
+
+print(grouped[:30])
 
 con.close()
