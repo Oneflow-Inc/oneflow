1c2c74350e972268c7c9171832c5b70dcf072ee1
tsai <caishenghang@oneflow.org>
2020-01-09 15:47:28 +0800

    show more

--- oneflow/python/model/maskrcnn/perf_analytics.py
+++ oneflow/python/model/maskrcnn/perf_analytics.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import sqlite3
 
-
+pd.set_option("display.max_rows", 1500)
+pd.options.display.max_colwidth = 100
 con = sqlite3.connect(
     "file:///home/tsai/Downloads/mask-rcnn-unknown-1x-2020-01-08-16:23:46-CST.sqlite"
 )
+# unit should be 1 ns
 df = pd.read_sql_query("SELECT text, (end - start) AS duration FROM NVTX_EVENTS;", con)
 grouped = (
     df.groupby("text")
     .agg({"duration": ["median", "mean", "std", "min", "max", "count"]})
-    .sort_values(by=[("duration", "std")], ascending=False)
+    .sort_values(by=[("duration", "median")], ascending=False)
 )
 
-print(grouped[:30])
+print(grouped[:1000])
 
 con.close()
