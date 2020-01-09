66351a22097154ce43773b2412ff0600ff2a7ee0
tsai <caishenghang@oneflow.org>
2020-01-09 17:56:25 +0800

    reject umbrella event

--- oneflow/python/model/maskrcnn/perf_analytics.py
+++ oneflow/python/model/maskrcnn/perf_analytics.py
@@ -4,16 +4,17 @@ import sqlite3
 pd.set_option("display.max_rows", 1500)
 pd.options.display.max_colwidth = 100
 con = sqlite3.connect(
-    "file:///home/tsai/Downloads/mask-rcnn-unknown-1x-2020-01-08-16:23:46-CST.sqlite"
+    "file:///home/tsai/Downloads/1x-perf-2/mask-rcnn-unknown-2020-01-09-17:08:15-CST.sqlite"
 )
 # unit should be 1 ns
 df = pd.read_sql_query("SELECT text, (end - start) AS duration FROM NVTX_EVENTS;", con)
+df = df[~df["text"].str.contains("Kernel::Launch")]
 grouped = (
     df.groupby("text")
     .agg({"duration": ["median", "mean", "std", "min", "max", "count"]})
-    .sort_values(by=[("duration", "median")], ascending=False)
+    .sort_values(by=[("duration", "std")], ascending=False)
 )
 
-print(grouped[:1000])
+print(grouped[:100])
 
 con.close()
