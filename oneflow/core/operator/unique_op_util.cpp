37b92b2f7df9a6a1c3ebe4bd7d2c11dabd17afb6
Your Name <you@example.com>
2020-01-10 16:50:48 +0800

    fix type seq

--- oneflow/core/operator/unique_op_util.cpp
+++ oneflow/core/operator/unique_op_util.cpp
@@ -21,11 +21,11 @@ struct SwitchUtil final {
 #define SWITCH_ENTRY(func_name, device_type, T, U) func_name<device_type, T, U>
   DEFINE_STATIC_SWITCH_FUNC(void, GetUniqueWorkspaceSizeInBytes, SWITCH_ENTRY,
                             MAKE_DEVICE_TYPE_CTRV_SEQ(DEVICE_TYPE_SEQ),
-                            MAKE_DATA_TYPE_CTRV_SEQ(INDEX_DATA_TYPE_SEQ),
+                            MAKE_DATA_TYPE_CTRV_SEQ(ARITHMETIC_DATA_TYPE_SEQ),
                             MAKE_DATA_TYPE_CTRV_SEQ(INDEX_DATA_TYPE_SEQ));
   DEFINE_STATIC_SWITCH_FUNC(void, GetUniqueWithCountsWorkspaceSizeInBytes, SWITCH_ENTRY,
                             MAKE_DEVICE_TYPE_CTRV_SEQ(DEVICE_TYPE_SEQ),
-                            MAKE_DATA_TYPE_CTRV_SEQ(INDEX_DATA_TYPE_SEQ),
+                            MAKE_DATA_TYPE_CTRV_SEQ(ARITHMETIC_DATA_TYPE_SEQ),
                             MAKE_DATA_TYPE_CTRV_SEQ(INDEX_DATA_TYPE_SEQ));
 #undef SWITCH_ENTRY
 };
