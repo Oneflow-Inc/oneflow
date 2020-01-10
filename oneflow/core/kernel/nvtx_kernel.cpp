466bc02140b560797ca4f41daada1dc09185bf7c
Tsai <caishenghang@oneflow.org>
2020-01-10 22:47:44 +0800

    add another op kernel

--- oneflow/core/kernel/nvtx_kernel.cpp
+++ oneflow/core/kernel/nvtx_kernel.cpp
@@ -12,6 +12,26 @@ void CheckSizeAndCopyBlob(DeviceCtx *ctx, Blob *dst, const Blob *src) {
 }  // namespace
 
 template<DeviceType device_type>
+class NvtxRangePushKernel final : public KernelIf<device_type> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(NvtxRangePushKernel);
+  NvtxRangePushKernel() = default;
+  ~NvtxRangePushKernel() = default;
+
+ private:
+  void ForwardDataContent(const KernelCtx &ctx,
+                          std::function<Blob *(const std::string &)> BnInOp2Blob) const override {
+    CheckSizeAndCopyBlob(ctx.device_ctx, BnInOp2Blob("out"), BnInOp2Blob("in"));
+  }
+  void ForwardLoD(const KernelCtx &ctx,
+                  std::function<Blob *(const std::string &)> BnInOp2Blob) const override {
+    const Blob *in_blob = BnInOp2Blob("in");
+    Blob *out_blob = BnInOp2Blob("out");
+    out_blob->tree_lod_mut_view().UpdateLoD(in_blob->tree_lod_view().lod_tree());
+  }
+};
+
+template<DeviceType device_type>
 class NvtxRangePopKernel final : public KernelIf<device_type> {
  public:
   OF_DISALLOW_COPY_AND_MOVE(NvtxRangePopKernel);
@@ -31,6 +51,7 @@ class NvtxRangePopKernel final : public KernelIf<device_type> {
   }
 };
 
+ADD_DEVICE_TYPE_KERNEL_CREATOR(OperatorConf::kNvtxRangePushConf, NvtxRangePushKernel);
 ADD_DEVICE_TYPE_KERNEL_CREATOR(OperatorConf::kNvtxRangePopConf, NvtxRangePopKernel);
 
 }  // namespace oneflow
