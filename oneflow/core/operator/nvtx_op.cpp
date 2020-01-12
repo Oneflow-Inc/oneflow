df58f27c0b06ca2738f4222aad0e96322564f711
Tsai <caishenghang@oneflow.org>
2020-01-12 12:15:35 +0800

    rm infer

--- oneflow/core/operator/nvtx_op.cpp
+++ oneflow/core/operator/nvtx_op.cpp
@@ -19,7 +19,6 @@ class NvtxRangePushOp final : public Operator {
   const PbMessage& GetCustomizedConf() const override { return op_conf().nvtx_range_push_conf(); }
   Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                              const ParallelContext* parallel_ctx) const override {
-    *GetBlobDesc4BnInOp("out") = *GetBlobDesc4BnInOp("in");
     return Maybe<void>::Ok();
   }
 
