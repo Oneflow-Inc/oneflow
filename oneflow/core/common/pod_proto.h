1eac55424f3ab5b50f5dc8be26b9becc3736b590
lixinqi <lixinqi0703106@163.com>
2020-01-12 16:46:45 +0800

    refactor POD_PROTO
    Former-commit-id: bccc5b7c55363f16b5030806de7ed2548068ebc3

--- oneflow/core/common/pod_proto.h
+++ oneflow/core/common/pod_proto.h
@@ -13,10 +13,10 @@
 
 #define POD_PROTO_ONEOF_CASE(oneof_name) OF_PP_CAT(oneof_name, _case)
 
-#define POD_PROTO_DEFINE_ONEOF(oneof_name, type_and_field_name_seq)      \
-  POD_PROTO_DEFINE_ONEOF_ENUM_TYPE(oneof_name, type_and_field_name_seq); \
-  POD_PROTO_DEFINE_ONEOF_ACCESSOR(oneof_name, type_and_field_name_seq)   \
-  POD_PROTO_DEFINE_ONEOF_UNION(type_and_field_name_seq);                 \
+#define POD_PROTO_DEFINE_ONEOF(oneof_name, type_and_field_name_seq)                                \
+  POD_PROTO_DEFINE_ONEOF_ENUM_TYPE(oneof_name, type_and_field_name_seq);                           \
+  POD_PROTO_DEFINE_ONEOF_ACCESSOR(POD_PROTO_ONEOF_ENUM_VALUE, oneof_name, type_and_field_name_seq) \
+  POD_PROTO_DEFINE_ONEOF_UNION(type_and_field_name_seq);                                           \
   POD_PROTO_DEFINE_FIELD(POD_PROTO_ONEOF_ENUM_TYPE(oneof_name), POD_PROTO_ONEOF_CASE(oneof_name));
 
 // details
@@ -27,22 +27,21 @@
     OF_PP_FOR_EACH_TUPLE(MAKE_POD_PROTO_ONEOF_ENUM_CASE, type_and_field_name_seq) \
   }
 
-#define MAKE_POD_PROTO_ONEOF_ENUM_CASE(type, field_name) POD_PROTO_ONEOF_ENUM_TYPE(field_name),
+#define MAKE_POD_PROTO_ONEOF_ENUM_CASE(type, field_name) POD_PROTO_ONEOF_ENUM_VALUE(field_name),
 
-#define POD_PROTO_DEFINE_ONEOF_ACCESSOR(oneof_name, type_and_field_name_seq)    \
-  OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(MAKE_POD_PROTO_ONEOF_ACCESSOR, (oneof_name), \
+#define POD_PROTO_DEFINE_ONEOF_ACCESSOR(get_enum_value, oneof_name, type_and_field_name_seq)      \
+  OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(MAKE_POD_PROTO_ONEOF_ACCESSOR, (get_enum_value), (oneof_name), \
                                    type_and_field_name_seq)
 
-#define MAKE_POD_PROTO_ONEOF_ACCESSOR(oneof_name, pair)                     \
-  const OF_PP_PAIR_FIRST(pair) & OF_PP_PAIR_SECOND(pair)() const {          \
-    CHECK(POD_PROTO_ONEOF_CASE(oneof_name)()                                \
-          == POD_PROTO_ONEOF_ENUM_VALUE(OF_PP_PAIR_SECOND(pair)));          \
-    return OF_PP_CAT(OF_PP_PAIR_SECOND(pair), _);                           \
-  }                                                                         \
-  OF_PP_PAIR_FIRST(pair) * OF_PP_CAT(mutable_, OF_PP_PAIR_SECOND(pair))() { \
-    OF_PP_CAT(set_, POD_PROTO_ONEOF_CASE(oneof_name))                       \
-    (POD_PROTO_ONEOF_ENUM_VALUE(OF_PP_PAIR_SECOND(pair)));                  \
-    return &OF_PP_CAT(OF_PP_PAIR_SECOND(pair), _);                          \
+#define MAKE_POD_PROTO_ONEOF_ACCESSOR(get_enum_value, oneof_name, pair)                   \
+  const OF_PP_PAIR_FIRST(pair) & OF_PP_PAIR_SECOND(pair)() const {                        \
+    CHECK(POD_PROTO_ONEOF_CASE(oneof_name)() == get_enum_value(OF_PP_PAIR_SECOND(pair))); \
+    return OF_PP_CAT(OF_PP_PAIR_SECOND(pair), _);                                         \
+  }                                                                                       \
+  OF_PP_PAIR_FIRST(pair) * OF_PP_CAT(mutable_, OF_PP_PAIR_SECOND(pair))() {               \
+    OF_PP_CAT(set_, POD_PROTO_ONEOF_CASE(oneof_name))                                     \
+    (get_enum_value(OF_PP_PAIR_SECOND(pair)));                                            \
+    return &OF_PP_CAT(OF_PP_PAIR_SECOND(pair), _);                                        \
   }
 
 #define POD_PROTO_DEFINE_ONEOF_UNION(type_and_field_name_seq)                       \
