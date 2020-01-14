d781319cf1551abceb89a79d4ba7ec6f5ef92d52
Your Name <you@example.com>
2020-01-14 13:13:46 +0800

    UnsortedSegmentSumKernel use UnsortedSegmentSumKernelUtil

--- oneflow/core/kernel/unsorted_segment_sum_kernel.cpp
+++ oneflow/core/kernel/unsorted_segment_sum_kernel.cpp
@@ -17,9 +17,19 @@ void UnsortedSegmentSumKernel<device_type, T, K>::ForwardDataContent(
   Blob* out = BnInOp2Blob("out");
   Memset<device_type>(ctx.device_ctx, out->mut_dptr<T>(), 0, out->ByteSizeOfBlobBody());
   if (segment_ids->IsBodyEmpty() || data->IsBodyEmpty()) { return; }
-  GatherKernelUtil<device_type, T>::Backward(
-      ctx.device_ctx, segment_ids, data, this->op_conf().unsorted_segment_sum_conf().axis(), out);
-}
+  const ShapeView& out_shape = out->shape();
+  const int64_t axis = this->op_conf().unsorted_segment_sum_conf().axis();
+  const int64_t outer_dim_size = out_shape.Count(0, axis);
+  const int64_t num_segments = this->op_conf().unsorted_segment_sum_conf().num_segments();
+  CHECK_EQ(out_shape.At(axis), num_segments);
+  const int64_t inner_dim_size = out_shape.Count(axis + 1);
+  const int64_t num_segment_ids = segment_ids->shape().elem_cnt();
+  CHECK_EQ(inner_dim_size * num_segment_ids * outer_dim_size, data->shape().elem_cnt());
+  UnsortedSegmentSumKernelUtil<device_type, T, K>::UnsortedSegmentSum(
+      ctx.device_ctx, segment_ids->dptr<K>(), data->dptr<T>(), num_segment_ids, num_segments,
+      outer_dim_size, inner_dim_size, 0, out->mut_dptr<T>());
+
+}  // namespace oneflow
 
 namespace {
 
--- oneflow/core/operator/unsorted_segment_sum_op.cpp
+++ oneflow/core/operator/unsorted_segment_sum_op.cpp
@@ -77,7 +77,7 @@ Maybe<void> UnsortedSegmentSumOp::InferBatchAxis(
 void UnsortedSegmentSumOp::VirtualGenKernelConf(
     std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
-  kernel_conf->mutable_indexed_slices_reduce_sum_conf()->set_indices_data_type(
+  kernel_conf->mutable_unsorted_segment_sum_conf()->set_indices_data_type(
       GetBlobDesc4BnInOp("segment_ids")->data_type());
 }
 
