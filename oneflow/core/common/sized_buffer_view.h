2c9e5a2875a6eb7104d399736efe22849f5ec4ec
cheng cheng <472491134@qq.com>
2020-01-15 16:28:14 +0800

    fix format (#2569)
    Former-commit-id: b6619b5a400e37aa5572a581db13a5406b2849d8

--- oneflow/core/common/sized_buffer_view.h
+++ oneflow/core/common/sized_buffer_view.h
@@ -4,11 +4,11 @@
 namespace oneflow {
 
 struct SizedBufferView {
-  size_t capacity; // allocated memory size for `data' field
-  size_t size; // valid data size
+  size_t capacity;  // allocated memory size for `data' field
+  size_t size;      // valid data size
   char data[0];
 };
 
-}
+}  // namespace oneflow
 
 #endif  // ONEFLOW_COMMON_SIZED_BUFFER_VIEW_H_
