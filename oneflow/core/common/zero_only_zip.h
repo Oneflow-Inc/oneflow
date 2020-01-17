e93d77a214a719715d8df6dc7a3bcda8827e8692
shijie <821898965@qq.com>
2020-01-17 23:24:57 +0800

    zero only zip
    Former-commit-id: de440ed00a455d7d078c6f9f792088f0621e999e

--- oneflow/core/common/zero_only_zip.h
+++ oneflow/core/common/zero_only_zip.h
@@ -7,8 +7,54 @@
 namespace oneflow {
 
 struct ZeroOnlyZipUtil final {
-  void ZipToSizedBuffer(const char* data, size_t size, SizedBufferView* sized_buffer) { TODO(); }
-  void UnzipToExpectedSize(const SizedBufferView& size_buffer, char* data, size_t expected_size) { TODO(); }
+  void ZipToSizedBuffer(const char* data, size_t size, SizedBufferView* sized_buffer) {
+    size_t cur_index = 0;
+    size_t former_index = 0;
+    size_t count = 0;
+    size_t cursor = 0;
+    while (cur_index < size) {
+      if (data[cur_index] != '0' && cur_index != size) {
+        cur_index++;
+        count++;
+      }
+      else {
+        if (count != 0){
+          size_buffer -> data[cursor++] = count;
+          for (; former_index < cur_index; former_index++) {
+            size_buffer -> data[cursor++] = data[former_index];
+          }
+        }
+        count = 0;
+        if (cur_index == size) break;
+        while (cur_index < size && data[cur_index] == '0') {
+          count++;
+          cur_index++;
+        }
+        former_index = cur_index;
+        sized_buffer -> data[cursor++] = -count;
+        count = 0
+      }
+    }
+  }
+
+  void UnzipToExpectedSize(const SizedBufferView& size_buffer, char* data, size_t expected_size) {
+    size_t cur_index = 0;
+    size_t cur_index = 0;
+    while (cur_index < expected_size) {
+      if (int(size_buffer.data[cursor]) > 0) {
+        int cur_index = size_buffer.data[cursor++];
+        for (size_t index = 0; index < cur_size; index++, cur_index++) {
+          *(data + cur_index) = size_buffer.data[cursor++];
+        }
+      }
+      else {
+        int cur_size = -int(size_buffer.data[cursor++];
+        for (size_t index = 0; index < cur_size; index++, cur_index++) {
+          *(data + cur_index) = '0';
+        }
+      }
+    }
+  }
 };
 
 }
