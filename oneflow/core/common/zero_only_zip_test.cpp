1d5780404adb5b2be53ccd4a1b6ee4d674850001
Shijie <821898965@qq.com>
2020-01-18 14:18:18 +0800

    update test case
    Former-commit-id: ca76f3de60d67564232d6bffa8121f4c2f500643

--- oneflow/core/common/zero_only_zip_test.cpp
+++ oneflow/core/common/zero_only_zip_test.cpp
@@ -3,28 +3,30 @@
 
 namespace oneflow {
 
-// test case for "000000023400000" -> -73234-5
+// test case for "\0\0\0\0\0\0\0\234\0\0\0\0\0" -> -73234-5
 TEST(ZipOnlyZipUtil, zip1) {
-  const char data[] = "000000023400000";
-  SizedBufferView *sized_buffer = new SizedBufferView;
+  const char data[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,'2','3','4',0x00,0x00,0x00,0x00,0x00};
+  char buffer[4096 + sizeof(SizedBufferView)];
+  SizedBufferView *sized_buffer = new (&buffer[0]) SizedBufferView;
   ZeroOnlyZipUtil zero_zip;
-  zero_zip.ZipToSizedBuffer(data, sizeof(data) - 1, sized_buffer);
+  zero_zip.ZipToSizedBuffer(data, sizeof(data), sized_buffer);
   ASSERT_TRUE(int(sized_buffer -> data[0]) == -7);
   ASSERT_TRUE(int(sized_buffer -> data[1]) == 3);
   ASSERT_TRUE(sized_buffer -> data[2] == '2');
   ASSERT_TRUE(sized_buffer -> data[3] == '3');
   ASSERT_TRUE(sized_buffer -> data[4] == '4');
   ASSERT_TRUE(int(sized_buffer -> data[5]) == -5);
-  delete sized_buffer;
+  //delete sized_buffer;
 }
 
 
-// test case for "abcde0000000234" -> 5abcde-73234
+// test case for "abcde\0\0\0\0\0\0\0234" -> 5abcde-73234
 TEST(ZipOnlyZipUtil, zip2) {
-  const char data[] = "abcde0000000234";
-  SizedBufferView *sized_buffer = new SizedBufferView;
+  const char data[] = {'a','b','c','d','e',0x00,0x00,0x00,0x00,0x00,0x00,0x00,'2','3','4'};
+  char buffer[4096 + sizeof(SizedBufferView)];
+  SizedBufferView *sized_buffer = new (&buffer[0]) SizedBufferView;
   ZeroOnlyZipUtil zero_zip;
-  zero_zip.ZipToSizedBuffer(data, sizeof(data) - 1, sized_buffer);
+  zero_zip.ZipToSizedBuffer(data, sizeof(data), sized_buffer);
   ASSERT_TRUE(int(sized_buffer -> data[0]) == 5);
   ASSERT_TRUE(sized_buffer -> data[1] == 'a');
   ASSERT_TRUE(sized_buffer -> data[2] == 'b');
@@ -36,22 +38,23 @@ TEST(ZipOnlyZipUtil, zip2) {
   ASSERT_TRUE(sized_buffer -> data[8] == '2');
   ASSERT_TRUE(sized_buffer -> data[9] == '3');
   ASSERT_TRUE(sized_buffer -> data[10] == '4');
-  delete sized_buffer;
+  //delete sized_buffer;
 }
 
 
-// test case for "0000000000" -> -10
+// test case for "\0\0\0\0\0\0\0\0\0\0" -> -10
 TEST(ZipOnlyZipUtil, zip3)
-  const char data[] = "0000000000";
-  SizedBufferView *sized_buffer = new SizedBufferView;
+  const char data[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+  char buffer[4096 + sizeof(SizedBufferView)];
+  SizedBufferView *sized_buffer = new (&buffer[0]) SizedBufferView;
   ZeroOnlyZipUtil zero_zip;
-  zero_zip.ZipToSizedBuffer(data, sizeof(data) - 1, sized_buffer);
+  zero_zip.ZipToSizedBuffer(data, sizeof(data), sized_buffer);
   ASSERT_TRUE(int(sized_buffer -> data[0]) == -10);
   delete sized_buffer;
 }
 
 
-// test case for -73234-5 -> "000000023400000"
+// test case for -73234-5 -> "\0\0\0\0\0\0\0234\0\0\0\0\0"
 TEST(ZipOnlyZipUtil, unzip1) {
   SizedBufferView sizedbuffer;
   sizedbuffer.data[0] = char(0xf9);
@@ -63,25 +66,25 @@ TEST(ZipOnlyZipUtil, unzip1) {
   char *data = new char;
   ZeroOnlyZipUtil zero_unzip;
   zero_unzip.UnzipToExpectedSize(sizedbuffer, data, 15);
-  ASSERT_TRUE(data[0] == '0');
-  ASSERT_TRUE(data[1] == '0');
-  ASSERT_TRUE(data[2] == '0');
-  ASSERT_TRUE(data[3] == '0');
-  ASSERT_TRUE(data[4] == '0');
-  ASSERT_TRUE(data[5] == '0');
-  ASSERT_TRUE(data[6] == '0');
+  ASSERT_TRUE(data[0] == 0x00);
+  ASSERT_TRUE(data[1] == 0x00);
+  ASSERT_TRUE(data[2] == 0x00);
+  ASSERT_TRUE(data[3] == 0x00);
+  ASSERT_TRUE(data[4] == 0x00);
+  ASSERT_TRUE(data[5] == 0x00);
+  ASSERT_TRUE(data[6] == 0x00);
   ASSERT_TRUE(data[7] == '2');
   ASSERT_TRUE(data[8] == '3');
   ASSERT_TRUE(data[9] == '4');
-  ASSERT_TRUE(data[10] == '0');
-  ASSERT_TRUE(data[11] == '0');
-  ASSERT_TRUE(data[12] == '0');
-  ASSERT_TRUE(data[13] == '0');
-  ASSERT_TRUE(data[14] == '0');
+  ASSERT_TRUE(data[10] == 0x00);
+  ASSERT_TRUE(data[11] == 0x00);
+  ASSERT_TRUE(data[12] == 0x00);
+  ASSERT_TRUE(data[13] == 0x00);
+  ASSERT_TRUE(data[14] == 0x00);
 }
 
 
-// test case for 5abcde-73234 -> "abcde0000000234"
+// test case for 5abcde-73234 -> "abcde\0\0\0\0\0\0\0234"
 TEST(ZipOnlyZipUtil, unzip2) {
   SizedBufferView sizedbuffer;
   sizedbuffer.data[0] = char(5);
@@ -102,14 +105,14 @@ TEST(ZipOnlyZipUtil, unzip2) {
   ASSERT_TRUE(data[1] == 'b');
   ASSERT_TRUE(data[2] == 'c');
   ASSERT_TRUE(data[3] == 'd');
-  ASSERT_TRUE(data[4] == '0');
-  ASSERT_TRUE(data[5] == '0');
-  ASSERT_TRUE(data[6] == '0');
-  ASSERT_TRUE(data[7] == '0');
-  ASSERT_TRUE(data[8] == '0');
-  ASSERT_TRUE(data[9] == '0');
-  ASSERT_TRUE(data[10] == '0');
-  ASSERT_TRUE(data[11] == '0');
+  ASSERT_TRUE(data[4] == 0x00);
+  ASSERT_TRUE(data[5] == 0x00);
+  ASSERT_TRUE(data[6] == 0x00);
+  ASSERT_TRUE(data[7] == 0x00);
+  ASSERT_TRUE(data[8] == 0x00);
+  ASSERT_TRUE(data[9] == 0x00);
+  ASSERT_TRUE(data[10] == 0x00);
+  ASSERT_TRUE(data[11] == 0x00);
   ASSERT_TRUE(data[12] == '2');
   ASSERT_TRUE(data[13] == '3');
   ASSERT_TRUE(data[14] == '4');
