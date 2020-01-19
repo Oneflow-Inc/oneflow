2682122404b5e0b1ff29d69b6757bb104e08cedd
simonJJJ <821898965@qq.com>
2020-01-19 19:38:41 +0800

    update
    Former-commit-id: 8650bb2c2b51ef5c28e34d2083f25b81c4e1234c

--- oneflow/core/common/sized_buffer_view.h
+++ oneflow/core/common/sized_buffer_view.h
@@ -1,12 +1,24 @@
 #ifndef ONEFLOW_COMMON_SIZED_BUFFER_VIEW_H_
 #define ONEFLOW_COMMON_SIZED_BUFFER_VIEW_H_
 
+#include <glog/logging.h>
+
 namespace oneflow {
 
 struct SizedBufferView {
-  size_t capacity; // allocated memory size for `data' field
+  size_t capacity = 4096; // allocated memory size for `data' field
   size_t size; // valid data size
   char data[0];
+
+  static SizedBufferView* PlacementNew(char* buffer, std::size_t buffer_size) {
+    CHECK_GE(buffer_size, sizeof(SizedBufferView));
+    auto* ret = new (buffer) SizedBufferView;
+    ret -> capacity = buffer_size - sizeof(SizedBufferView);
+    return ret;
+  }
+
+  private:
+    SizedBufferView() {};
 };
 
 }
--- oneflow/core/common/zero_only_zip.h
+++ oneflow/core/common/zero_only_zip.h
@@ -1,6 +1,3 @@
-#ifndef ONEFLOW_CORE_COMMON_ZERO_ONLY_ZIP_H_
-#define ONEFLOW_CORE_COMMON_ZERO_ONLY_ZIP_H_
-
 #include <memory>
 #include "oneflow/core/common/sized_buffer_view.h"
 
@@ -8,38 +5,27 @@ namespace oneflow {
 
 struct ZeroOnlyZipUtil final {
   void ZipToSizedBuffer(const char* data, size_t size, SizedBufferView* sized_buffer) {
-    size_t cur_index = 0;
-    size_t former_index = 0;
-    size_t count = 0;
-    size_t cursor = 0;
-    while (cur_index < size) {
-      if (data[cur_index] != 0x00 && cur_index != size) {
-        cur_index++;
+    size_t cur_index = 0, count = 0, cursor = 0;
+    while (cur_index <= size) {
+      while (data[cur_index + count] != 0x00 && cur_index != size)
         count++;
-      }
-      else {
-        if (count != 0){
-          size_buffer -> data[cursor++] = count;
-          for (; former_index < cur_index; former_index++) {
-            size_buffer -> data[cursor++] = data[former_index];
-          }
+      if (count != 0){
+        size_buffer -> data[cursor++] = count;
+        for (; count != 0; --count)
+          size_buffer -> data[cursor++] = data[cur_index++];
         }
-        count = 0;
         if (cur_index == size) break;
         while (cur_index < size && data[cur_index] == 0x00) {
           count++;
           cur_index++;
         }
-        former_index = cur_index;
         sized_buffer -> data[cursor++] = -count;
-        count = 0
-      }
+        count = 0;
     }
   }
 
   void UnzipToExpectedSize(const SizedBufferView& size_buffer, char* data, size_t expected_size) {
-    size_t cursor = 0;
-    size_t cur_index = 0;
+    size_t cursor = 0, cur_index = 0;
     while (cur_index < expected_size) {
       if (int(size_buffer.data[cursor]) > 0) {
         int cur_size = size_buffer.data[cursor++];
