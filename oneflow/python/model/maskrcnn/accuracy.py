f2c046e14a1b75b37ae31ec92dd04188fc0d7668
Tsai <caishenghang@oneflow.org>
2020-01-19 16:53:24 +0800

    fmt

--- oneflow/python/model/maskrcnn/accuracy.py
+++ oneflow/python/model/maskrcnn/accuracy.py
@@ -1,10 +1,14 @@
 _DICT = {}
+
+
 def get_metrics_dict():
     return _DICT
 
+
 def put_metrics(metrics):
-    _DICT.update(metrics) 
+    _DICT.update(metrics)
     return _DICT
+
 
 # in mirror mode, it is recommended to clear the dict when one rank is built
 def clear_metrics_dict():
