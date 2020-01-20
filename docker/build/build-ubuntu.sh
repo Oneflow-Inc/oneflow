512a36a07a95ee2b6dece72c343fe678716bd017
Tsai <caishenghang@oneflow.org>
2020-01-20 16:30:51 +0800

    rm build arg

--- docker/build/build-ubuntu.sh
+++ docker/build/build-ubuntu.sh
@@ -1,6 +1,5 @@
 docker build \
   --rm \
-  --build-arg CUDA=10.0 \
   --build-arg http_proxy=http://192.168.1.11:8118 \
   --build-arg https_proxy=https://192.168.1.11:8118 \
   -t oneflow-build:ubuntu -f docker/build/build.ubuntu.dockerfile .
