5155574b7b8aec11b9d8b0f0b1069ad4d8a698a3
Dounm <niuchong893184@gmail.com>
2020-01-20 15:46:53 +0800

    Update user_op::Tensor to use ShapeView

--- oneflow/core/framework/tensor.cpp
+++ oneflow/core/framework/tensor.cpp
@@ -1,16 +1,22 @@
 #include "oneflow/core/framework/tensor.h"
+#include "oneflow/core/register/blob.h"
 
 namespace oneflow {
 
 namespace user_op {
 
-Tensor::Tensor(const TensorDesc& def, char* dptr) : desc_(def), dptr_(dptr) {}
-
-Tensor::Tensor(const Shape& shape, DataType dtype, char* dptr) : desc_(shape, dtype), dptr_(dptr) {}
-
 Tensor::Tensor(const Tensor& rhs) {
-  desc_ = rhs.desc_;
   dptr_ = rhs.dptr_;
+  shape_ = rhs.shape_;
+  mut_shape_.reset(new MutShapeView(*rhs.mut_shape_));
+  data_type_ = rhs.data_type_;
+}
+
+Tensor::Tensor(Blob* blob) {
+  dptr_ = blob->mut_dptr();
+  shape_ = blob->shape();
+  mut_shape_.reset(new MutShapeView(*blob->mut_shape_view()));
+  data_type_ = blob->data_type();
 }
 
 }  // namespace user_op
