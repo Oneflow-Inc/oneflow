f5b78da651009ea0b096ff0cb8d3fbec29a58f83
Dounm <niuchong893184@gmail.com>
2020-01-20 16:24:45 +0800

    Fix the bug of nullptr MutShapeView ptr

--- oneflow/core/framework/tensor.cpp
+++ oneflow/core/framework/tensor.cpp
@@ -8,14 +8,22 @@ namespace user_op {
 Tensor::Tensor(const Tensor& rhs) {
   dptr_ = rhs.dptr_;
   shape_ = rhs.shape_;
-  mut_shape_.reset(new MutShapeView(*rhs.mut_shape_));
+  if (rhs.mut_shape_) {
+    mut_shape_.reset(new MutShapeView(*rhs.mut_shape_));
+  } else {
+    mut_shape_.reset();
+  }
   data_type_ = rhs.data_type_;
 }
 
 Tensor::Tensor(Blob* blob) {
   dptr_ = blob->mut_dptr();
   shape_ = blob->shape();
-  mut_shape_.reset(new MutShapeView(*blob->mut_shape_view()));
+  if (blob->mut_shape_view()) {
+    mut_shape_.reset(new MutShapeView(*blob->mut_shape_view()));
+  } else {
+    mut_shape_.reset();
+  }
   data_type_ = blob->data_type();
 }
 
