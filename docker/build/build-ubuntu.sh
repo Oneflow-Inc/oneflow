f114ad1e4b7792663d0bdc064f239976e3a078e8
Tsai <caishenghang@oneflow.org>
2020-01-20 18:19:50 +0800

    rm proxy arg

--- docker/build/build-ubuntu.sh
+++ docker/build/build-ubuntu.sh
@@ -1,5 +1,3 @@
 docker build \
   --rm \
-  --build-arg http_proxy=http://192.168.1.11:8118 \
-  --build-arg https_proxy=https://192.168.1.11:8118 \
   -t oneflow-build:ubuntu -f docker/build/build.ubuntu.dockerfile .
