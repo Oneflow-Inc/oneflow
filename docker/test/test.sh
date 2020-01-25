a76b05706c71a0a6ba90844930e33234c5032013
tsai <caishenghang@oneflow.org>
2020-01-25 20:02:06 +0800

    fix permission

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -1,5 +1,8 @@
+set -x
 pwd=$1
 pip3 install --user $pwd/*.whl
 installed_path=$(python3 -c "import oneflow; print(oneflow.__path__[0])")
-python3 $pwd/oneflow/python/test/ops/1node_test.py
-python3 $pwd/oneflow/python/test/models/1node_test.py
+test_dir="/test_dir"
+cp -r $pwd/oneflow/python/test $test_dir
+python3 $test_dir/ops/1node_test.py
+python3 $test_dir/models/1node_test.py
