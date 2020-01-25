c3c1dd70902df9fb03b8f712542e13c6e32ffbc2
tsai <caishenghang@oneflow.org>
2020-01-25 17:56:02 +0800

    fix whl install

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -1,5 +1,5 @@
 pwd=$1
-pip3 install $pwd/*.whl
+pip3 install --user $pwd/*.whl
 installed_path=$(python3 -c "import oneflow; print(oneflow.__path__[0])")
 python3 $pwd/oneflow/python/test/ops/1node_test.py
 python3 $pwd/oneflow/python/test/models/1node_test.py
