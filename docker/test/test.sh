56811e78015758858a01d84bff53e0638b1178be
tsai <caishenghang@oneflow.org>
2020-01-25 20:17:07 +0800

    add artifacts

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -4,5 +4,7 @@ pip3 install --user $pwd/*.whl
 installed_path=$(python3 -c "import oneflow; print(oneflow.__path__[0])")
 test_dir="/test_dir"
 cp -r $pwd/oneflow/python/test $test_dir
-python3 $test_dir/ops/1node_test.py
-python3 $test_dir/models/1node_test.py
+touch $pwd/test_result.txt
+python3 $test_dir/ops/1node_test.py >> $pwd/test_result.txt
+python3 $test_dir/models/1node_test.py >> $pwd/test_result.txt
+chmod 660 $pwd/test_result.txt
