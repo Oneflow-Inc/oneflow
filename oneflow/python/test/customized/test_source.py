ddca2b86e52fe70b80310797d78d90422da96faa
cheng cheng <472491134@qq.com>
2020-01-29 21:40:02 +0800

    Feat: UserOp support as source op (#2593)
    * Feat: UserOp support as source op
    
    * add TestSource user op/kernel/python_scripts and pass; fix bug of user op default infer batch axis

---
+++ oneflow/python/test/customized/test_source.py
@@ -0,0 +1,20 @@
+import oneflow as flow
+import numpy as np
+
+flow.config.gpu_device_num(1)
+
+func_config = flow.FunctionConfig()
+func_config.default_distribute_strategy(flow.distribute.consistent_strategy())
+func_config.default_data_type(flow.float)
+
+def my_test_source(name):
+    with flow.fixed_placement("cpu", "0:0"):
+        return flow.user_op_builder(name).Op("TestSource").Output("out").Build().RemoteBlobList()[0]
+
+@flow.function(func_config)
+def TestSourceJob():
+    return my_test_source("my_test_source")
+
+print(TestSourceJob().get())
+# 0, 1, 2, 3, 4
+
