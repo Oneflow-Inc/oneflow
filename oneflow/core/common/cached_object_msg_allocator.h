f3f42b5a271286d74190b34b6b66070525e51b7f
lixinqi <lixinqi0703106@163.com>
2020-02-02 12:06:18 +0800

    CachedObjectMsgAllocator/ThreadUnsafeObjectMsgAllocator

--- oneflow/core/common/cached_object_msg_allocator.h
+++ oneflow/core/common/cached_object_msg_allocator.h
@@ -1,6 +1,8 @@
 #ifndef ONEFLOW_CORE_COMMON_OBJECT_MSG_ALLOCATOR_CORE_H_
 #define ONEFLOW_CORE_COMMON_OBJECT_MSG_ALLOCATOR_CORE_H_
 
+#include <mutex>
+#include <thread>
 #include "oneflow/core/common/object_msg.h"
 
 namespace oneflow {
@@ -52,8 +54,8 @@ BEGIN_OBJECT_MSG(ObjMsgSizedMemPool);
   void __Init__(int64_t fiexed_mem_size, int64_t prefetch_cnt);
   void __Delete__();
 
-  char* Allocate();
-  void Deallocate(char* ptr);
+  char* Allocate(std::mutex* mutex);
+  void Deallocate(std::mutex* mutex, char* ptr);
 
   // fields
   OBJECT_MSG_DEFINE_LIST_HEAD(ObjMsgChunk, occupied_chunk_list);
@@ -64,33 +66,29 @@ BEGIN_OBJECT_MSG(ObjMsgSizedMemPool);
   OBJECT_MSG_DEFINE_SKIPLIST_KEY(5, int64_t, mem_block_size);
 
  private:
+  char* Allocate();
+  void Deallocate(char* ptr);
   void Prefetch();
   void Prefetch(FreeObjMsgChunkList* free_list);
   void AppendToFreeList(FreeObjMsgChunkList* free_list);
 END_OBJECT_MSG(ObjMsgSizedMemPool);
 // clang-format on
 
-class CachedObjectMsgAllocator final : public ObjectMsgAllocator {
+class CachedObjectMsgAllocatorBase : public ObjectMsgAllocator {
  public:
-  CachedObjectMsgAllocator(const CachedObjectMsgAllocator&) = delete;
-  CachedObjectMsgAllocator(CachedObjectMsgAllocator&&) = delete;
-  CachedObjectMsgAllocator(int mem_size_shift_max, int64_t prefetch_cnt)
-      : backend_allocator_(ObjectMsgDefaultAllocator::GlobalObjectMsgAllocator()),
-        mem_size_shift_max_(mem_size_shift_max),
-        prefetch_cnt_(prefetch_cnt) {
-    Prefetch();
-  }
-  CachedObjectMsgAllocator(ObjectMsgAllocator* backend_allocator, int64_t mem_size_shift_max,
-                           int64_t prefetch_cnt)
+  CachedObjectMsgAllocatorBase(const CachedObjectMsgAllocatorBase&) = delete;
+  CachedObjectMsgAllocatorBase(CachedObjectMsgAllocatorBase&&) = delete;
+  CachedObjectMsgAllocatorBase(ObjectMsgAllocator* backend_allocator, int64_t mem_size_shift_max,
+                               int64_t prefetch_cnt)
       : backend_allocator_(backend_allocator),
         mem_size_shift_max_(mem_size_shift_max),
         prefetch_cnt_(prefetch_cnt) {
     Prefetch();
   }
-  ~CachedObjectMsgAllocator();
+  virtual ~CachedObjectMsgAllocatorBase() override { allocators_.Clear(); }
 
-  char* Allocate(std::size_t size) override;
-  void Deallocate(char* ptr, std::size_t size) override;
+  char* RoundUpAllocate(std::mutex* mutex, std::size_t size);
+  void RoundUpDeallocate(std::mutex* mutex, char* ptr, std::size_t size);
 
  private:
   static const std::size_t kMemSizeShiftMin = 6;
@@ -101,6 +99,51 @@ class CachedObjectMsgAllocator final : public ObjectMsgAllocator {
   std::size_t mem_size_shift_max_;
   int64_t prefetch_cnt_;
   OBJECT_MSG_SKIPLIST(ObjMsgSizedMemPool, mem_block_size) allocators_;
+};
+
+class CachedObjectMsgAllocator : public CachedObjectMsgAllocatorBase {
+ public:
+  CachedObjectMsgAllocator(const CachedObjectMsgAllocator&) = delete;
+  CachedObjectMsgAllocator(CachedObjectMsgAllocator&&) = delete;
+
+  CachedObjectMsgAllocator(int mem_size_shift_max, int64_t prefetch_cnt)
+      : CachedObjectMsgAllocatorBase(ObjectMsgDefaultAllocator::GlobalObjectMsgAllocator(),
+                                     mem_size_shift_max, prefetch_cnt) {}
+  CachedObjectMsgAllocator(ObjectMsgAllocator* backend_allocator, int64_t mem_size_shift_max,
+                           int64_t prefetch_cnt)
+      : CachedObjectMsgAllocatorBase(backend_allocator, mem_size_shift_max, prefetch_cnt) {}
+
+  ~CachedObjectMsgAllocator() override = default;
+
+  char* Allocate(std::size_t size) override { return RoundUpAllocate(&mutex_, size); }
+  void Deallocate(char* ptr, std::size_t size) override {
+    return RoundUpDeallocate(&mutex_, ptr, size);
+  }
+
+ private:
+  std::mutex mutex_;
+};
+
+class ThreadUnsafeObjectMsgAllocator : public CachedObjectMsgAllocatorBase {
+ public:
+  ThreadUnsafeObjectMsgAllocator(const ThreadUnsafeObjectMsgAllocator&) = delete;
+  ThreadUnsafeObjectMsgAllocator(ThreadUnsafeObjectMsgAllocator&&) = delete;
+
+  ThreadUnsafeObjectMsgAllocator(int mem_size_shift_max, int64_t prefetch_cnt)
+      : CachedObjectMsgAllocatorBase(ObjectMsgDefaultAllocator::GlobalObjectMsgAllocator(),
+                                     mem_size_shift_max, prefetch_cnt),
+        thread_id_(std::this_thread::get_id()) {}
+  ThreadUnsafeObjectMsgAllocator(ObjectMsgAllocator* backend_allocator, int64_t mem_size_shift_max,
+                                 int64_t prefetch_cnt)
+      : CachedObjectMsgAllocatorBase(backend_allocator, mem_size_shift_max, prefetch_cnt),
+        thread_id_(std::this_thread::get_id()) {}
+
+  ~ThreadUnsafeObjectMsgAllocator() override { CHECK(thread_id_ == std::this_thread::get_id()); }
+  char* Allocate(std::size_t size) override;
+  void Deallocate(char* ptr, std::size_t size) override;
+
+ private:
+  std::thread::id thread_id_;
 };
 
 }  // namespace oneflow
