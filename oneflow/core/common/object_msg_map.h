5311aa625f765847b361aba350afdaaf8de71938
lixinqi <lixinqi0703106@163.com>
2020-02-02 16:45:36 +0800

    refactor object_msg api
    Former-commit-id: 785c2722347bac1c431223d80d7dc46f5515aa15

--- oneflow/core/common/object_msg_map.h
+++ oneflow/core/common/object_msg_map.h
@@ -4,8 +4,8 @@
 #include "oneflow/core/common/object_msg_skiplist.h"
 
 #define OBJECT_MSG_DEFINE_MAP_KEY(T, field_name) OBJECT_MSG_DEFINE_SKIPLIST_KEY(20, T, field_name)
-#define OBJECT_MSG_DEFINE_MAP_HEAD(field_type, field_name) \
-  OBJECT_MSG_DEFINE_SKIPLIST_HEAD(field_type, field_name)
-#define OBJECT_MSG_MAP(field_type, field_name) OBJECT_MSG_SKIPLIST(field_type, field_name)
+#define OBJECT_MSG_DEFINE_MAP_HEAD(elem_type, elem_field_name, field_name) \
+  OBJECT_MSG_DEFINE_SKIPLIST_HEAD(elem_type, elem_field_name, field_name)
+#define OBJECT_MSG_MAP(elem_type, elem_field_name) OBJECT_MSG_SKIPLIST(elem_type, elem_field_name)
 
 #endif  // ONEFLOW_CORE_COMMON_OBJECT_MSG_MAP_H_
