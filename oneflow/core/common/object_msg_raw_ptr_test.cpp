407fb9a7e7adf9a632c5da0c361ace619c2071a5
lixinqi <lixinqi0703106@163.com>
2020-02-04 23:26:19 +0800

    object msg Wrapper4CppObject
    Former-commit-id: 6b31ab668c92c97b8bf0a079088fefe3eef28e05

---
+++ oneflow/core/common/object_msg_raw_ptr_test.cpp
@@ -0,0 +1,26 @@
+#include "oneflow/core/common/object_msg_raw_ptr.h"
+#include "oneflow/core/common/util.h"
+
+namespace oneflow {
+
+namespace test {
+
+typedef bool (*Ok)();
+
+bool TestOk() { return true; }
+
+// clang-format off
+BEGIN_OBJECT_MSG(ObjectMsgFunctionPtrFoo);
+  OBJECT_MSG_DEFINE_RAW_PTR(Ok, ok);
+END_OBJECT_MSG(ObjectMsgFunctionPtrFoo);
+// clang-format on
+
+TEST(object_msg_raw_ptr, function_pointer) {
+  auto obj = OBJECT_MSG_PTR(ObjectMsgFunctionPtrFoo)::New();
+  obj->set_ok(&TestOk);
+  ASSERT_TRUE((*obj->ok())());
+}
+
+}  // namespace test
+
+}  // namespace oneflow
