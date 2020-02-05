ce054b44fc26884fc2432d24cf675275fe519f90
Shenghang Tsai <jackalcooper@gmail.com>
2020-02-05 11:06:56 +0800

    Dev roi align v2 (#2612)
    * v2
    
    * fix bilinear interpolate bug for certain shift
    
    * fix iInterpolate bug for backward when shifted

--- oneflow/core/kernel/roi_align_kernel.cu
+++ oneflow/core/kernel/roi_align_kernel.cu
@@ -12,8 +12,10 @@ __device__ T BilinearInterpolate(const T* channel_dptr, const int32_t height, co
                                  T y, T x) {
   if (y < -1.0 || y > height || x < -1.0 || x > width) { return 0; }
 
-  int32_t y_low = (y <= 0) ? 0 : y;
-  int32_t x_low = (x <= 0) ? 0 : x;
+  if (y <= 0) { y = 0; }
+  if (x <= 0) { x = 0; }
+  int32_t y_low = static_cast<int32_t>(y);
+  int32_t x_low = static_cast<int32_t>(x);
   int32_t y_high = 0;
   int32_t x_high = 0;
 
@@ -55,8 +57,11 @@ __device__ bool BilinearInterpolateDiff(const T bin_diff_avg, const int64_t heig
                                         int32_t& y_low, int32_t& y_high) {
   if (y < -1.0 || y > height || x < -1.0 || x > width) { return false; }
 
-  if (y > 0) { y_low = y; }
-  if (x > 0) { x_low = x; }
+  if (y <= 0) { y = 0; }
+  if (x <= 0) { x = 0; }
+
+  y_low = static_cast<int32_t>(y);
+  x_low = static_cast<int32_t>(x);
 
   if (y_low >= height - 1) {
     y_low = height - 1;
@@ -89,9 +94,10 @@ __device__ bool BilinearInterpolateDiff(const T bin_diff_avg, const int64_t heig
 template<typename T>
 __launch_bounds__(kCudaThreadsNumPerBlock) __global__
     void RoiAlignForward(const int64_t nthreads, const T* in_dptr, const T* rois_dptr,
-                         const float spatial_scale, const int32_t sampling_ratio,
+                         const T spatial_scale, const int32_t sampling_ratio,
                          const int64_t channel_num, const int64_t height, const int64_t width,
-                         const int64_t pooled_height, const int64_t pooled_width, T* out_dptr) {
+                         const int64_t pooled_height, const int64_t pooled_width,
+                         const bool aligned, T* out_dptr) {
   const int64_t pooled_area = pooled_height * pooled_width;
   const int64_t channel_pooled_area = channel_num * pooled_height * pooled_width;
   CUDA_1D_KERNEL_LOOP(index, nthreads) {
@@ -101,19 +107,26 @@ __launch_bounds__(kCudaThreadsNumPerBlock) __global__
     const int64_t r = index / channel_pooled_area;
     const T* offset_rois_dptr = rois_dptr + r * 5;
     const int64_t n = static_cast<int64_t>(offset_rois_dptr[0]);
-    const T roi_start_w = offset_rois_dptr[1] * spatial_scale;
-    const T roi_start_h = offset_rois_dptr[2] * spatial_scale;
-    const T roi_end_w = offset_rois_dptr[3] * spatial_scale;
-    const T roi_end_h = offset_rois_dptr[4] * spatial_scale;
-    const T roi_height = max(roi_end_h - roi_start_h, static_cast<T>(1.f));
-    const T roi_width = max(roi_end_w - roi_start_w, static_cast<T>(1.f));
+    const T align_offset = aligned ? static_cast<T>(0.5) : static_cast<T>(0.f);
+    const T roi_start_w = offset_rois_dptr[1] * spatial_scale - align_offset;
+    const T roi_start_h = offset_rois_dptr[2] * spatial_scale - align_offset;
+    const T roi_end_w = offset_rois_dptr[3] * spatial_scale - align_offset;
+    const T roi_end_h = offset_rois_dptr[4] * spatial_scale - align_offset;
+    T roi_height = roi_end_h - roi_start_h;
+    T roi_width = roi_end_w - roi_start_w;
+    // aligned == false is for compatibility. the argument "aligned" doesn't have the semantic of
+    // determining minimum roi size
+    if (aligned == false) {
+      roi_height = max(roi_height, static_cast<T>(1.0));
+      roi_width = max(roi_width, static_cast<T>(1.0));
+    }
     const T bin_height = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
     const T bin_width = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
     const int32_t bin_grid_height =
         (sampling_ratio > 0) ? sampling_ratio : ceil(roi_height / pooled_height);
     const int32_t bin_grid_width =
         (sampling_ratio > 0) ? sampling_ratio : ceil(roi_width / pooled_width);
-
+    const T count = max(bin_grid_height * bin_grid_width, 1);
     const T* channel_dptr = in_dptr + (n * channel_num + c) * height * width;
     T out_val = 0.0;
     FOR_RANGE(int64_t, grid_i, 0, bin_grid_height) {
@@ -126,17 +139,17 @@ __launch_bounds__(kCudaThreadsNumPerBlock) __global__
         out_val += BilinearInterpolate(channel_dptr, height, width, y, x);
       }
     }
-    out_dptr[index] = out_val / (bin_grid_height * bin_grid_width);
+    out_dptr[index] = out_val / count;
   }
 }
 
 template<typename T>
 __launch_bounds__(kCudaThreadsNumPerBlock) __global__
     void RoiAlignBackward(const int64_t nthreads, const T* out_diff_dptr, const T* rois_dptr,
-                          const float spatial_scale, const int32_t sampling_ratio,
+                          const T spatial_scale, const int32_t sampling_ratio,
                           const int64_t channel_num, const int64_t height, const int64_t width,
                           const int64_t pooled_height, const int64_t pooled_width,
-                          T* in_diff_dptr) {
+                          const bool aligned, T* in_diff_dptr) {
   const int64_t pooled_area = pooled_height * pooled_width;
   const int64_t channel_pooled_area = channel_num * pooled_height * pooled_width;
   CUDA_1D_KERNEL_LOOP(index, nthreads) {
@@ -146,12 +159,19 @@ __launch_bounds__(kCudaThreadsNumPerBlock) __global__
     const int64_t r = index / channel_pooled_area;
     const T* offset_rois_dptr = rois_dptr + r * 5;
     const int64_t n = static_cast<int64_t>(offset_rois_dptr[0]);
-    const T roi_start_w = offset_rois_dptr[1] * spatial_scale;
-    const T roi_start_h = offset_rois_dptr[2] * spatial_scale;
-    const T roi_end_w = offset_rois_dptr[3] * spatial_scale;
-    const T roi_end_h = offset_rois_dptr[4] * spatial_scale;
-    const T roi_height = max(roi_end_h - roi_start_h, static_cast<T>(1.0));
-    const T roi_width = max(roi_end_w - roi_start_w, static_cast<T>(1.0));
+    const T align_offset = aligned ? static_cast<T>(0.5) : static_cast<T>(0.f);
+    const T roi_start_w = offset_rois_dptr[1] * spatial_scale - align_offset;
+    const T roi_start_h = offset_rois_dptr[2] * spatial_scale - align_offset;
+    const T roi_end_w = offset_rois_dptr[3] * spatial_scale - align_offset;
+    const T roi_end_h = offset_rois_dptr[4] * spatial_scale - align_offset;
+    T roi_width = roi_end_w - roi_start_w;
+    T roi_height = roi_end_h - roi_start_h;
+    // aligned == false is for compatibility. the argument "aligned" doesn't have the semantic of
+    // determining minimum roi size
+    if (aligned == false) {
+      roi_height = max(roi_height, static_cast<T>(1.0));
+      roi_width = max(roi_width, static_cast<T>(1.0));
+    }
     const T bin_height = static_cast<T>(roi_height) / static_cast<T>(pooled_height);
     const T bin_width = static_cast<T>(roi_width) / static_cast<T>(pooled_width);
     const int32_t bin_grid_height =
@@ -159,8 +179,9 @@ __launch_bounds__(kCudaThreadsNumPerBlock) __global__
     const int32_t bin_grid_width =
         (sampling_ratio > 0) ? sampling_ratio : ceil(roi_width / pooled_width);
 
+    const T count = max(bin_grid_height * bin_grid_width, 1);
+    const T bin_diff_avg = out_diff_dptr[index] / count;
     T* in_diff_channel_dptr = in_diff_dptr + (n * channel_num + c) * height * width;
-    const T bin_diff_avg = out_diff_dptr[index] / (bin_grid_height * bin_grid_width);
     FOR_RANGE(int64_t, grid_i, 0, bin_grid_height) {
       // + .5f for center position
       T y = roi_start_h + h * bin_height
@@ -212,7 +233,7 @@ class RoiAlignGPUKernel final : public KernelIf<DeviceType::kGPU> {
                          ctx.device_ctx->cuda_stream()>>>(
         elem_cnt, x_blob->dptr<T>(), rois_blob->dptr<T>(), args.spatial_scale(),
         args.sampling_ratio(), x_blob->shape().At(1), x_blob->shape().At(2), x_blob->shape().At(3),
-        args.pooled_h(), args.pooled_w(), y_blob->mut_dptr<T>());
+        args.pooled_h(), args.pooled_w(), args.aligned(), y_blob->mut_dptr<T>());
   }
 };
 
@@ -238,7 +259,8 @@ class RoiAlignGradGPUKernel final : public KernelIf<DeviceType::kGPU> {
                             ctx.device_ctx->cuda_stream()>>>(
           elem_cnt, dy_blob->dptr<T>(), rois_blob->dptr<T>(), args.spatial_scale(),
           args.sampling_ratio(), dx_blob->shape().At(1), dx_blob->shape().At(2),
-          dx_blob->shape().At(3), args.pooled_h(), args.pooled_w(), dx_blob->mut_dptr<T>());
+          dx_blob->shape().At(3), args.pooled_h(), args.pooled_w(), args.aligned(),
+          dx_blob->mut_dptr<T>());
     } else {
       Memset<DeviceType::kGPU>(ctx.device_ctx, dx_blob->mut_dptr<T>(), 0,
                                dx_blob->AlignedByteSizeOfBlobBody());
