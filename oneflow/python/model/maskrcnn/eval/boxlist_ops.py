3ac2265822b5b097c9526f4c2f141fc6290835b4
scxfjiang <sc.xfjiang@gmail.com>
2020-02-06 15:09:09 +0800

    update

--- oneflow/python/model/maskrcnn/eval/boxlist_ops.py
+++ oneflow/python/model/maskrcnn/eval/boxlist_ops.py
@@ -18,43 +18,46 @@ def remove_small_boxes(boxlist, min_size):
     keep = ((ws >= min_size) & (hs >= min_size)).nonzero().squeeze(1)
     return boxlist[keep]
 
-# implementation from https://github.com/kuangliu/torchcv/blob/master/torchcv/utils/box.py	
-# with slight modifications	
-def boxlist_iou(boxlist1, boxlist2):	
-    """Compute the intersection over union of two set of boxes.	
-    The box order must be (xmin, ymin, xmax, ymax).	
-    Arguments:	
-      box1: (BoxList) bounding boxes, sized [N,4].	
-      box2: (BoxList) bounding boxes, sized [M,4].	
-    Returns:	
-      (tensor) iou, sized [N,M].	
-    Reference:	
-      https://github.com/chainer/chainercv/blob/master/chainercv/utils/bbox/bbox_iou.py	
-    """	
-    if boxlist1.size != boxlist2.size:	
-        raise RuntimeError(	
-            "boxlists should have same image size, got {}, {}".format(	
-                boxlist1, boxlist2	
-            )	
-        )	
-    boxlist1 = boxlist1.convert("xyxy")	
-    boxlist2 = boxlist2.convert("xyxy")	
-    N = len(boxlist1)	
-    M = len(boxlist2)	
+# implementation from https://github.com/kuangliu/torchcv/blob/master/torchcv/utils/box.py
+# with slight modifications
+def boxlist_iou(boxlist1, boxlist2):
+    """Compute the intersection over union of two set of boxes.
+    The box order must be (xmin, ymin, xmax, ymax).
 
-    area1 = boxlist1.area()	
-    area2 = boxlist2.area()	
+    Arguments:
+      box1: (BoxList) bounding boxes, sized [N,4].
+      box2: (BoxList) bounding boxes, sized [M,4].
 
-    box1, box2 = boxlist1.bbox, boxlist2.bbox	
+    Returns:
+      (tensor) iou, sized [N,M].
 
-    lt = np.maximum(box1[:, None, :2], box2[:, :2])  # [N,M,2]	
-    rb = np.minimum(box1[:, None, 2:], box2[:, 2:])  # [N,M,2]	
+    Reference:
+      https://github.com/chainer/chainercv/blob/master/chainercv/utils/bbox/bbox_iou.py
+    """
+    if boxlist1.size != boxlist2.size:
+        raise RuntimeError(
+            "boxlists should have same image size, got {}, {}".format(
+                boxlist1, boxlist2
+            )
+        )
+    boxlist1 = boxlist1.convert("xyxy")
+    boxlist2 = boxlist2.convert("xyxy")
+    N = len(boxlist1)
+    M = len(boxlist2)
 
-    wh = (rb - lt).clamp(min=0)  # [N,M,2]	
-    inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]	
+    area1 = boxlist1.area()
+    area2 = boxlist2.area()
 
-    iou = inter / (area1[:, None] + area2 - inter)	
-    return iou	
+    box1, box2 = boxlist1.bbox, boxlist2.bbox
+
+    lt = np.maximum(box1[:, None, :2], box2[:, :2])  # [N,M,2]
+    rb = np.minimum(box1[:, None, 2:], box2[:, 2:])  # [N,M,2]
+
+    wh = (rb - lt).clamp(min=0)  # [N,M,2]
+    inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
+
+    iou = inter / (area1[:, None] + area2 - inter)
+    return iou
 
 
 def cat_boxlist(bboxes):
