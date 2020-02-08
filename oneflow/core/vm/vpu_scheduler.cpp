67adf218dbeb07d8d4e512893887537ed9e6eaf3
lixinqi <lixinqi0703106@163.com>
2020-02-08 12:54:28 +0800

    VpuScheduler::Schedule
    Former-commit-id: cdbb46b571dad6f87bd7b8a96146989ac654ee9e

--- oneflow/core/vm/vpu_scheduler.cpp
+++ oneflow/core/vm/vpu_scheduler.cpp
@@ -3,22 +3,46 @@
 
 namespace oneflow {
 
-using VpuSchedulerCtx = OBJECT_MSG_PTR(VpuSchedulerCtx);
+using VpuSchedulerCtx = OBJECT_MSG_TYPE(VpuSchedulerCtx);
+using VpuSetCtx = OBJECT_MSG_TYPE(VpuSetCtx);
 
-namespace {}
+namespace {
+
+void HandleFinishedVpuInstructionCtx(VpuSchedulerCtx* ctx, VpuSetCtx* vpu_set_ctx) { TODO(); }
+
+void MoveToTmpPendingMsgList(VpuSchedulerCtx* ctx) {
+  std::unique_lock<std::mutex> lck(*ctx->mut_pending_msg_list_mutex()->Mutable());
+  ctx->mut_pending_msg_list()->MoveTo(ctx->mut_tmp_pending_msg_list());
+}
+
+// fill new_ctx_list with VpuInstructionCtx which created by VpuInstructionMsg in
+// tmp_pending_msg_list
+void MakeVpuInstructionCtx(VpuSchedulerCtx* ctx) { TODO(); }
+
+void MoveNewCtxToWaitingListOrReadyList(VpuSchedulerCtx* ctx) { TODO(); }
+
+void DispatchVpuInstructionCtx(VpuSchedulerCtx* ctx) { TODO(); }
+
+}  // namespace
 
 VpuScheduler::VpuScheduler() { TODO(); }
 
 void VpuScheduler::Receive(VpuInstructionMsgList* vpu_instr_list) {
-  std::unique_lock<std::mutex> lck(*ctx_->mut_pending_list_mutex()->Mutable());
-  vpu_instr_list->MoveTo(ctx_->mut_vpu_instruction_msg_pending_list());
+  std::unique_lock<std::mutex> lck(*ctx_->mut_pending_msg_list_mutex()->Mutable());
+  vpu_instr_list->MoveTo(ctx_->mut_pending_msg_list());
 }
 
 void VpuScheduler::Schedule() {
-  {
-    std::unique_lock<std::mutex> lck(*ctx_->mut_pending_list_mutex()->Mutable());
-    ctx_->mut_vpu_instruction_msg_pending_list()->MoveTo(ctx_->mut_tmp_pending_list());
+  auto* ctx = ctx_.Mutable();
+  OBJECT_MSG_LIST_FOR_EACH_UNSAFE(ctx->mut_vpu_set_ctx_list(), vpu_set_ctx) {
+    HandleFinishedVpuInstructionCtx(ctx, vpu_set_ctx);
+  };
+  if (ctx->pending_msg_list().size() > 0) {
+    MoveToTmpPendingMsgList(ctx);
+    MakeVpuInstructionCtx(ctx);
+    MoveNewCtxToWaitingListOrReadyList(ctx);
   }
+  if (ctx->ready_ctx_list().size() > 0) { DispatchVpuInstructionCtx(ctx); }
 }
 
 }  // namespace oneflow
