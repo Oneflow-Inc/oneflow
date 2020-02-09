a1cdc18c08567acc506d894affedde05c71846a7
lixinqi <lixinqi0703106@163.com>
2020-02-09 17:01:01 +0800

    rename method: VpuScheduler::Schedule() => VpuScheduler::Dispatch()
    Former-commit-id: 1b7b29bc9f33f58aa83af8d43abdc06a326faf1d

--- oneflow/core/vm/vpu_scheduler.cpp
+++ oneflow/core/vm/vpu_scheduler.cpp
@@ -5,10 +5,13 @@ namespace oneflow {
 
 using VpuSchedulerCtx = OBJECT_MSG_TYPE(VpuSchedulerCtx);
 using VpuSetCtx = OBJECT_MSG_TYPE(VpuSetCtx);
+using RunningVpuInstructionPackage = OBJECT_MSG_TYPE(RunningVpuInstructionPackage);
 
 namespace {
 
-void HandleFinishedVpuInstructionCtx(VpuSchedulerCtx* ctx, VpuSetCtx* vpu_set_ctx) { TODO(); }
+void HandleFinishedVpuInstructionCtx(VpuSchedulerCtx* ctx, RunningVpuInstructionPackage* pkg) {
+  TODO();
+}
 
 void MoveToTmpPendingMsgList(VpuSchedulerCtx* ctx) {
   std::unique_lock<std::mutex> lck(*ctx->mut_pending_msg_list_mutex()->Mutable());
@@ -25,17 +28,17 @@ void DispatchVpuInstructionCtx(VpuSchedulerCtx* ctx) { TODO(); }
 
 }  // namespace
 
-VpuScheduler::VpuScheduler() { TODO(); }
-
 void VpuScheduler::Receive(VpuInstructionMsgList* vpu_instr_list) {
   std::unique_lock<std::mutex> lck(*ctx_->mut_pending_msg_list_mutex()->Mutable());
   vpu_instr_list->MoveTo(ctx_->mut_pending_msg_list());
 }
 
-void VpuScheduler::Schedule() {
+void VpuScheduler::Dispatch() {
   auto* ctx = ctx_.Mutable();
   OBJECT_MSG_LIST_FOR_EACH_UNSAFE(ctx->mut_vpu_set_ctx_list(), vpu_set_ctx) {
-    HandleFinishedVpuInstructionCtx(ctx, vpu_set_ctx);
+    auto* begin = vpu_set_ctx->mut_launched_pkg_list()->Begin();
+    if (begin == nullptr || !begin->Done()) { continue; }
+    HandleFinishedVpuInstructionCtx(ctx, begin);
   };
   if (ctx->pending_msg_list().size() > 0) {
     MoveToTmpPendingMsgList(ctx);
