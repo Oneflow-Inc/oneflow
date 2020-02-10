6af2106a0645fab3b12ab94256463b00598c9977
lixinqi <lixinqi0703106@163.com>
2020-02-10 22:41:09 +0800

    handle finished VpuInstructionCtx package
    Former-commit-id: d47ca9c6d29be9a0d832395383a494a3b4e8f308

--- oneflow/core/vm/scheduler.msg.h
+++ oneflow/core/vm/scheduler.msg.h
@@ -19,9 +19,10 @@ BEGIN_OBJECT_MSG(VpuSchedulerCtx);
   //links
   OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionMsg, vpu_instruction_msg_link, pending_msg_list);
   OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionMsg, vpu_instruction_msg_link, tmp_pending_msg_list);
-  OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionCtx, vpu_instruction_ctx_link, new_ctx_list);
-  OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionCtx, vpu_instruction_ctx_link, waiting_ctx_list);
-  OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionCtx, vpu_instruction_ctx_link, ready_ctx_list);
+  OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionCtx, vpu_instruction_ctx_link, new_vpu_instr_ctx_list);
+  OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionCtx, vpu_instruction_ctx_link, waiting_vpu_instr_ctx_list);
+  OBJECT_MSG_DEFINE_LIST_HEAD(VpuInstructionCtx, vpu_instruction_ctx_link, ready_vpu_instr_ctx_list);
+  OBJECT_MSG_DEFINE_LIST_HEAD(MirroredObject, released_link, released_mirrored_object_list);
   OBJECT_MSG_DEFINE_SKIPLIST_HEAD(VpuCtx, vpu_id, vpu_id2vpu);
   OBJECT_MSG_DEFINE_LIST_HEAD(VpuSetCtx, vpu_set_ctx_link, vpu_set_ctx_list);
   OBJECT_MSG_DEFINE_MAP_HEAD(LogicalObject, logical_object_id, id2logical_object);
