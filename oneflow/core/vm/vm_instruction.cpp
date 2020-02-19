d5e8367b00ae2d2c8eb9dca02e633b0e6d397ac0
lixinqi <lixinqi0703106@163.com>
2020-02-19 21:50:56 +0800

    rename: VpuTypeCtx => VmStreamRtDesc
    Former-commit-id: fe0366e4c2f58223965114a566f9826d2f4efc09

--- oneflow/core/vm/vm_instruction.cpp
+++ oneflow/core/vm/vm_instruction.cpp
@@ -5,7 +5,7 @@ namespace oneflow {
 
 void VmInstructionCtx::__Init__(VmInstructionMsg* vm_instruction_msg, VmStream* vm_stream) {
   reset_vm_instruction_msg(vm_instruction_msg);
-  const auto& vm_stream_type = vm_stream->vm_thread().vpu_type_ctx().vm_stream_type();
+  const auto& vm_stream_type = vm_stream->vm_thread().vm_stream_rt_desc().vm_stream_type();
   VmInstructionOpcode opcode = vm_instruction_msg->vm_instruction_proto().opcode();
   set_vm_instruction(vm_stream_type.GetVmInstruction(opcode));
   set_vm_stream(vm_stream);
@@ -13,7 +13,7 @@ void VmInstructionCtx::__Init__(VmInstructionMsg* vm_instruction_msg, VmStream* 
 
 void RunningVmInstructionPackage::__Init__(VmStream* vm_stream) {
   set_vm_stream(vm_stream);
-  const auto* vm_stream_type = &vm_stream->vm_thread().vpu_type_ctx().vm_stream_type();
+  const auto* vm_stream_type = &vm_stream->vm_thread().vm_stream_rt_desc().vm_stream_type();
   mutable_status_querier()->__Init__(
       [vm_stream_type, vm_stream](ObjectMsgAllocator* allocator, int32_t* size) {
         return vm_stream_type->NewStatusQuerier(allocator, size, vm_stream);
