f5f2bddd7fb6694f8c3353dd867f9cf30d55c0f6
lixinqi <lixinqi0703106@163.com>
2020-02-20 14:02:13 +0800

    OBJECT_MSG_SKIPLIST_FOR_EACH

--- oneflow/core/common/object_msg_map.h
+++ oneflow/core/common/object_msg_map.h
@@ -10,4 +10,10 @@
   OBJECT_MSG_DEFINE_SKIPLIST_HEAD(elem_type, elem_field_name, field_name)
 #define OBJECT_MSG_MAP(elem_type, elem_field_name) OBJECT_MSG_SKIPLIST(elem_type, elem_field_name)
 
+#define OBJECT_MSG_MAP_FOR_EACH(skiplist_ptr, elem) OBJECT_MSG_SKIPLIST_FOR_EACH(skiplist_ptr, elem)
+#define OBJECT_MSG_MAP_FOR_EACH_PTR(skiplist_ptr, elem) \
+  OBJECT_MSG_SKIPLIST_FOR_EACH_PTR(skiplist_ptr, elem)
+#define OBJECT_MSG_MAP_UNSAFE_FOR_EACH_PTR(skiplist_ptr, elem) \
+  OBJECT_MSG_SKIPLIST_UNSAFE_FOR_EACH_PTR(skiplist_ptr, elem)
+
 #endif  // ONEFLOW_CORE_COMMON_OBJECT_MSG_MAP_H_
