72d514478480d7ea3f101b392db4f41f8071f22d
cheng cheng <472491134@qq.com>
2020-02-25 19:35:45 +0800

    Feat: UserOp support set keep header only

--- oneflow/core/framework/op_registration.h
+++ oneflow/core/framework/op_registration.h
@@ -7,6 +7,7 @@
 #include "oneflow/core/framework/user_op_def.pb.h"
 #include "oneflow/core/framework/user_op_attr.pb.h"
 #include "oneflow/core/framework/user_op_conf.pb.h"
+#include "oneflow/core/operator/op_attribute.pb.h"
 
 namespace oneflow {
 
@@ -26,6 +27,10 @@ using ShapeInferFn = std::function<Maybe<void>(InferContext*)>;
 using DtypeInferFn = std::function<Maybe<void>(InferContext*)>;
 using BatchAxisInferFn = std::function<Maybe<void>(BatchAxisContext*)>;
 using GetSbpFn = std::function<Maybe<void>(SbpContext*)>;
+using InputArgModifier = InputBlobModifier;
+using GetInputArgModifier =
+    std::function<InputArgModifier*(const std::string& in_arg_name, int32_t in_arg_index)>;
+using AdvancedInputArgModifyFn = std::function<void(GetInputArgModifier)>;
 
 struct OpRegistrationVal {
   UserOpDef op_def;
@@ -34,6 +39,7 @@ struct OpRegistrationVal {
   DtypeInferFn dtype_infer_fn;
   BatchAxisInferFn batch_axis_infer_fn;
   GetSbpFn get_sbp_fn;
+  AdvancedInputArgModifyFn advanced_input_arg_modify_fn;
 };
 
 struct OpRegistryWrapper final {
@@ -68,6 +74,7 @@ class OpRegistryWrapperBuilder final {
   OpRegistryWrapperBuilder& SetDataTypeInferFn(DtypeInferFn fn);
   OpRegistryWrapperBuilder& SetBatchAxisInferFn(BatchAxisInferFn fn);
   OpRegistryWrapperBuilder& SetGetSbpFn(GetSbpFn fn);
+  OpRegistryWrapperBuilder& SetAdvancedInputArgModifyFn(AdvancedInputArgModifyFn fn);
   OpRegistryWrapperBuilder& SetCheckAttrFn(CheckAttrFn fn);
 
   OpRegistryWrapper Build();
