68e64dfbfe8d7d2193d0fc33fa9d38f943b79936
leaves-zwx <kunta0932@gmail.com>
2020-02-26 12:05:43 +0800

    unittest

--- oneflow/python/test/ops/test_slice_v2.py
+++ oneflow/python/test/ops/test_slice_v2.py
@@ -2,10 +2,12 @@ import numpy as np
 import oneflow as flow
 
 
-def _run_slice(input, index_args, dynamic=False, dtype=flow.float):
+def _run_slice(input, index_args, dynamic=False, dtype=flow.float, input_shape=None):
     func_config = flow.FunctionConfig()
     func_config.default_data_type(dtype)
-    func_config.default_distribute_strategy(flow.distribute.consistent_strategy())
+
+    if input_shape is None:
+        input_shape = input.shape
 
     def do_slice(x, indices):
         outputs = []
@@ -15,15 +17,24 @@ def _run_slice(input, index_args, dynamic=False, dtype=flow.float):
         return outputs
 
     if dynamic is True:
-        pass
-    else:
+        func_config.default_distribute_strategy(flow.distribute.mirrored_strategy())
 
         @flow.function(func_config)
-        def slice(input_blob=flow.FixedTensorDef(shape=input.shape, dtype=dtype)):
+        def slice(input_blob=flow.MirroredTensorDef(shape=input_shape, dtype=dtype)):
             return do_slice(input_blob, index_args)
 
-    outputs = slice(input).get()
-    return map(lambda x: x.ndarray(), outputs)
+        outputs = slice([input]).get()
+        return map(lambda x: x.ndarray_list()[0], outputs)
+
+    else:
+        func_config.default_distribute_strategy(flow.distribute.consistent_strategy())
+
+        @flow.function(func_config)
+        def slice(input_blob=flow.FixedTensorDef(shape=input_shape, dtype=dtype)):
+            return do_slice(input_blob, index_args)
+
+        outputs = slice(input).get()
+        return map(lambda x: x.ndarray(), outputs)
 
 
 def _check(test_case, ref, out):
@@ -31,214 +42,108 @@ def _check(test_case, ref, out):
         test_case.assertTrue(np.allclose(_ref, _out))
 
 
-def test_slice_naive(test_case):
+def test_slice_into_two_parts(test_case):
     input = np.random.rand(2, 5, 4).astype(np.float32)
     results = [input[:, 0:2, :], input[:, 2:, :]]
-    args = [[(0, 2, 1), (None, None, None)], [(2, None, 1), (None, None, None)]]
+    args = [[(0, 2, None), (None, None, None)], [(2, None, None), (None, None, None)]]
     outputs = _run_slice(input, args)
-    _check(args, results, outputs)
+    _check(test_case, results, outputs)
 
 
-# def slice_into_two_part():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.FixedTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         part1 = of.slice(input_blob, (None, 0, 0), (None, 2, None))
-#         part2 = of.slice(input_blob, (None, 2, 0), (None, 3, None))
-#         return part1, part2
-
-#     input = np.random.rand(2, 5, 4).astype(np.float32)
-#     p1, p2 = slice(input).get()
-
-#     ref1 = input[:, :2, :]
-#     ref2 = input[:, 2:5, :]
-#     print("-" * 80)
-#     print(colored("slice_into_two_part test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("part1 shape:", ref1.shape)
-#     print("part2 shape:", ref2.shape)
-#     assert np.allclose(p1.ndarray(), ref1)
-#     assert np.allclose(p2.ndarray(), ref2)
-#     print(colored("slice_into_two_part test passed", "green"))
+def test_slice_at_two_dims(test_case):
+    input = np.random.rand(2, 5, 4).astype(np.float32)
+    results = [input[:, 0:2, 2:]]
+    args = [[(0, 2, None), (2, None, None)]]
+    outputs = _run_slice(input, args)
+    _check(test_case, results, outputs)
 
 
-# def slice_at_two_dim():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.FixedTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         return of.slice(input_blob, (None, 0, 2), (None, 2, None))
-
-#     input = np.random.rand(2, 5, 4).astype(np.float32)
-#     ret = slice(input).get().ndarray()
-#     ref = input[:, 0:2, 2:]
-
-#     print("-" * 80)
-#     print(colored("slice_at_two_dim test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("slice_at_two_dim test passed", "green"))
+def test_dynamic_slice(test_case):
+    input = np.random.rand(2, 4, 4).astype(np.float32)
+    results = [input[:, 1:, :]]
+    args = [[(1, None, None)]]
+    outputs = _run_slice(input, args, dynamic=True, input_shape=(2, 5, 5))
+    _check(test_case, results, outputs)
 
 
-# def dynamic_slice():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.MirroredTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         return of.slice(input_blob, (None, 1, None), (None, None, None))
-
-#     input = np.random.rand(2, 4, 4).astype(np.float32)
-#     ret = slice([input]).get().ndarray_list()[0]
-#     ref = input[:, 1:, :]
-
-#     print("-" * 80)
-#     print(colored("dynamic_slice test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("dynamic_slice test passed", "green"))
+def test_dynamic_slice_at_two_dims(test_case):
+    input = np.random.rand(2, 3, 2, 2).astype(np.float32)
+    results = [input[:, 2:, :, 1:]]
+    args = [[(2, None, None), (None, None, None), (1, None, None)]]
+    outputs = _run_slice(input, args, dynamic=True, input_shape=(2, 5, 3, 3))
+    _check(test_case, results, outputs)
 
 
-# def dynamic_slice_at_two_dim():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.MirroredTensorDef(shape=(2, 5, 2, 3), dtype=of.float32)):
-#         return of.slice(input_blob, (None, 2, None, 1), (None, None, None, None))
-
-#     input = np.random.rand(2, 3, 2, 2).astype(np.float32)
-#     ret = slice([input]).get().ndarray_list()[0]
-#     ref = input[:, 2:, :, 1:]
-
-#     print("-" * 80)
-#     print(colored("dynamic_slice_at_two_dim test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("dynamic_slice_at_two_dim test passed", "green"))
+def test_slice_with_stride2(test_case):
+    input = np.random.rand(2, 5, 4).astype(np.float32)
+    results = [input[:, 1::2, :]]
+    args = [[(1, None, 2)]]
+    outputs = _run_slice(input, args, dynamic=True)
+    _check(test_case, results, outputs)
 
 
-# def slice_with_stride2():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.MirroredTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         return of.slice_v3(input_blob, [(None, None, None), (1, None, 2)])
-
-#     input = np.random.rand(2, 5, 4).astype(np.float32)
-#     ret = slice([input]).get().ndarray_list()[0]
-#     ref = input[:, 1::2, :]
-
-#     print("-" * 80)
-#     print(colored("slice_with_stride2 test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("slice_with_stride2 test passed", "green"))
+def test_slice_at_two_dim_with_stride_more_than_one(test_case):
+    input = np.random.rand(2, 5, 4).astype(np.float32)
+    results = [input[:, 1::3, ::2]]
+    args = [[(1, None, 3), (None, None, 2)]]
+    outputs = _run_slice(input, args, dynamic=True)
+    _check(test_case, results, outputs)
 
 
-# def slice_at_two_dim_with_stride():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.MirroredTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         return of.slice_v3(input_blob, [(None, None, None), (1, None, 3), (None, None, 2)])
-
-#     input = np.random.rand(2, 5, 4).astype(np.float32)
-#     ret = slice([input]).get().ndarray_list()[0]
-#     ref = input[:, 1::3, ::2]
-
-#     print("-" * 80)
-#     print(colored("slice_at_two_dim_with_stride test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("slice_at_two_dim_with_stride test passed", "green"))
+def test_slice_with_neg_index(test_case):
+    input = np.random.rand(2, 5, 4).astype(np.float32)
+    results = [input[:, 2:-2, :]]
+    args = [[(2, -2, None)]]
+    outputs = _run_slice(input, args, dynamic=True)
+    _check(test_case, results, outputs)
 
 
-# def slice_with_neg_index():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.MirroredTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         return of.slice_v3(input_blob, [(None, None, None), (2, -2, 1)])
-
-#     input = np.random.rand(2, 5, 4).astype(np.float32)
-#     ret = slice([input]).get().ndarray_list()[0]
-#     ref = input[:, 2:-2, :]
-
-#     print("-" * 80)
-#     print(colored("slice_with_neg_index test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("slice_with_neg_index test passed", "green"))
+def test_slice_with_neg_stride(test_case):
+    input = np.random.rand(2, 5, 4).astype(np.float32)
+    results = [input[:, 3:-4:-1, :]]
+    args = [[(3, -4, -1)]]
+    outputs = _run_slice(input, args, dynamic=True)
+    _check(test_case, results, outputs)
 
 
-# def slice_with_neg_stride():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
-
-#     @of.function(func_config)
-#     def slice(input_blob=of.MirroredTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         return of.slice_v3(input_blob, [(None, None, None), (3, -4, -1)])
-
-#     input = np.random.rand(2, 5, 4).astype(np.float32)
-#     ret = slice([input]).get().ndarray_list()[0]
-#     ref = input[:, 3:-4:-1, :]
-
-#     print("-" * 80)
-#     print(colored("slice_with_neg_stride test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("slice_with_neg_stride test passed", "green"))
+def test_slice_with_neg_stride2(test_case):
+    input = np.random.rand(2, 5, 4).astype(np.float32)
+    results = [input[:, -1:1:-2, :]]
+    args = [[(-1, 1, -2)]]
+    outputs = _run_slice(input, args, dynamic=True)
+    _check(test_case, results, outputs)
 
 
-# def slice_with_neg_stride2():
-#     of.clear_default_session()
-#     func_config = of.FunctionConfig()
-#     func_config.default_data_type(of.float)
+def test_slice_grad(test_case):
+    input = np.random.rand(2, 5, 4).astype(np.float32)
+    ref = np.zeros(input.shape, dtype=np.float32)
+    ref[:, 2:-2, :] = np.ones(input[:, 2:-2, :].shape, dtype=np.float32)
 
-#     @of.function(func_config)
-#     def slice(input_blob=of.MirroredTensorDef(shape=(2, 5, 4), dtype=of.float32)):
-#         return of.slice_v3(input_blob, [(None, None, None), (-1, 1, -2)])
+    def slice_grad_cb(dx_blob):
+        dx = dx_blob.ndarray()
+        test_case.assertTrue(np.allclose(ref, dx))
 
-#     input = np.random.rand(2, 5, 4).astype(np.float32)
-#     ret = slice([input]).get().ndarray_list()[0]
-#     ref = input[:, -1:1:-2, :]
+    func_config = flow.FunctionConfig()
+    func_config.default_data_type(flow.float)
+    func_config.default_distribute_strategy(flow.distribute.consistent_strategy())
+    func_config.train.primary_lr(1e-3)
+    func_config.train.model_update_conf(dict(naive_conf={}))
 
-#     print("-" * 80)
-#     print(colored("slice_with_neg_stride2 test", "yellow"))
-#     print("input shape:", input.shape)
-#     print("of result shape:", ret.shape)
-#     print("np result shape:", ref.shape)
-#     assert np.allclose(ret, ref)
-#     print(colored("slice_with_neg_stride2 test passed", "green"))
+    @flow.function(func_config)
+    def slice(input_blob=flow.FixedTensorDef(shape=(2, 5, 4), dtype=flow.float)):
+        x = flow.get_variable(
+            shape=(2, 5, 4),
+            dtype=flow.float,
+            initializer=flow.random_uniform_initializer(2),
+            name="variable",
+        )
+        x = flow.identity(x)
+        flow.watch_diff(x, slice_grad_cb)
+        y = flow.slice_v2(x, [(2, -2, None)])
+        flow.losses.add_loss(y)
+        return y
+
+    slice(input).get()
 
 
 # def slice_grad():
