95ea5240ded771f870f628646c29e1e96e892b40
lixinqi <lixinqi0703106@163.com>
2020-02-28 18:49:12 +0800

    self loop list

--- oneflow/core/common/object_msg_list.h
+++ oneflow/core/common/object_msg_list.h
@@ -36,32 +36,36 @@ namespace oneflow {
 // details
 
 #define _OBJECT_MSG_DEFINE_LIST_HEAD(field_counter, elem_type, elem_field_name, field_name)    \
+  _OBJECT_MSG_DEFINE_LIST_HEAD_FIELD_TYPE(elem_type, elem_field_name, field_name)              \
+  DSS_DEFINE_FIELD(field_counter, "object message", OF_PP_CAT(field_name, _ObjectMsgListType), \
+                   OF_PP_CAT(field_name, _));                                                  \
   _OBJECT_MSG_DEFINE_LIST_HEAD_FIELD(elem_type, elem_field_name, field_name)                   \
   OBJECT_MSG_DEFINE_LIST_ELEM_STRUCT(field_counter, elem_type, elem_field_name, field_name);   \
   OBJECT_MSG_DEFINE_LIST_LINK_EDGES(field_counter, elem_type, elem_field_name, field_name);    \
-  OBJECT_MSG_OVERLOAD_INIT(field_counter, ObjectMsgEmbeddedListHeadInit);                      \
-  OBJECT_MSG_OVERLOAD_DELETE(field_counter, ObjectMsgEmbeddedListHeadDelete);                  \
-  DSS_DEFINE_FIELD(field_counter, "object message", OF_PP_CAT(field_name, _ObjectMsgListType), \
-                   OF_PP_CAT(field_name, _));
+  OBJECT_MSG_OVERLOAD_INIT_WITH_FIELD_INDEX(field_counter, ObjectMsgEmbeddedListHeadInit);     \
+  OBJECT_MSG_OVERLOAD_DELETE(field_counter, ObjectMsgEmbeddedListHeadDelete);
 
-#define _OBJECT_MSG_DEFINE_LIST_HEAD_FIELD(elem_type, elem_field_name, field_name)         \
- public:                                                                                   \
-  using OF_PP_CAT(field_name, _ObjectMsgListType) =                                        \
-      TrivialObjectMsgList<kDisableSelfLoopLink,                                           \
-                           StructField<OBJECT_MSG_TYPE_CHECK(elem_type), EmbeddedListLink, \
-                                       OBJECT_MSG_TYPE_CHECK(elem_type)::OF_PP_CAT(        \
-                                           elem_field_name, _kDssFieldOffset)>>;           \
-  const OF_PP_CAT(field_name, _ObjectMsgListType) & field_name() const {                   \
-    return OF_PP_CAT(field_name, _);                                                       \
-  }                                                                                        \
-  OF_PP_CAT(field_name, _ObjectMsgListType) * OF_PP_CAT(mut_, field_name)() {              \
-    return &OF_PP_CAT(field_name, _);                                                      \
-  }                                                                                        \
-  OF_PP_CAT(field_name, _ObjectMsgListType) * OF_PP_CAT(mutable_, field_name)() {          \
-    return &OF_PP_CAT(field_name, _);                                                      \
-  }                                                                                        \
-                                                                                           \
- private:                                                                                  \
+#define _OBJECT_MSG_DEFINE_LIST_HEAD_FIELD_TYPE(elem_type, elem_field_name, field_name)            \
+ public:                                                                                           \
+  using OF_PP_CAT(field_name, _ObjectMsgListType) =                                                \
+      TrivialObjectMsgList<GetObjectMsgLinkType<std::is_same<self_type, elem_type>::value>::value, \
+                           StructField<OBJECT_MSG_TYPE_CHECK(elem_type), EmbeddedListLink,         \
+                                       OBJECT_MSG_TYPE_CHECK(elem_type)::OF_PP_CAT(                \
+                                           elem_field_name, _kDssFieldOffset)>>;
+
+#define _OBJECT_MSG_DEFINE_LIST_HEAD_FIELD(elem_type, elem_field_name, field_name) \
+ public:                                                                           \
+  const OF_PP_CAT(field_name, _ObjectMsgListType) & field_name() const {           \
+    return OF_PP_CAT(field_name, _);                                               \
+  }                                                                                \
+  OF_PP_CAT(field_name, _ObjectMsgListType) * OF_PP_CAT(mut_, field_name)() {      \
+    return &OF_PP_CAT(field_name, _);                                              \
+  }                                                                                \
+  OF_PP_CAT(field_name, _ObjectMsgListType) * OF_PP_CAT(mutable_, field_name)() {  \
+    return &OF_PP_CAT(field_name, _);                                              \
+  }                                                                                \
+                                                                                   \
+ private:                                                                          \
   OF_PP_CAT(field_name, _ObjectMsgListType) OF_PP_CAT(field_name, _);
 
 #define OBJECT_MSG_DEFINE_LIST_ELEM_STRUCT(field_counter, elem_type, elem_field_name, field_name) \
@@ -120,9 +124,11 @@ namespace oneflow {
                    list_type::ContainerLinkOffset()>::FieldPtr4StructPtr(list_ptr)), \
       list_type::value_link_struct_field, elem)
 
-template<typename WalkCtxType, typename PtrFieldType>
+template<int field_index, typename WalkCtxType, typename PtrFieldType>
 struct ObjectMsgEmbeddedListHeadInit {
-  static void Call(WalkCtxType* ctx, PtrFieldType* field) { field->__Init__(); }
+  static void Call(WalkCtxType* ctx, PtrFieldType* field) {
+    field->template __Init__<field_index>();
+  }
 };
 
 template<typename WalkCtxType, typename PtrFieldType>
@@ -171,7 +177,10 @@ class TrivialObjectMsgList<kDisableSelfLoopLink, ValueLinkField> {
   std::size_t size() const { return list_head_.size(); }
   bool empty() const { return list_head_.empty(); }
 
-  void __Init__() { list_head_.__Init__(); }
+  template<int field_index = 0>
+  void __Init__() {
+    list_head_.__Init__();
+  }
 
   value_type* Begin() {
     if (list_head_.empty()) { return nullptr; }
@@ -254,6 +263,153 @@ class TrivialObjectMsgList<kDisableSelfLoopLink, ValueLinkField> {
 
  private:
   EmbeddedListHead<ValueLinkField> list_head_;
+};
+
+template<typename ValueLinkField>
+class TrivialObjectMsgList<kEnableSelfLoopLink, ValueLinkField> {
+ public:
+  using value_type = typename ValueLinkField::struct_type;
+  using value_link_struct_field = ValueLinkField;
+
+  template<typename Enabled = void>
+  static constexpr int ContainerLinkOffset() {
+    return offsetof(TrivialObjectMsgList, list_head_)
+           + EmbeddedListHead<ValueLinkField>::ContainerLinkOffset();
+  }
+
+  std::size_t size() const { return list_head_.size(); }
+  bool empty() const { return list_head_.empty(); }
+
+  template<int field_index>
+  void __Init__() {
+    list_head_.__Init__();
+    using ThisInContainer =
+        StructField<value_type, TrivialObjectMsgList,
+                    value_type::template __DssFieldOffset4FieldIndex__<field_index>::value>;
+    container_ = ThisInContainer::StructPtr4FieldPtr(this);
+  }
+
+  value_type* Begin() {
+    if (list_head_.empty()) { return nullptr; }
+    return list_head_.Begin();
+  }
+  value_type* Next(value_type* ptr) {
+    if (ptr == nullptr) { return nullptr; }
+    value_type* next = list_head_.Next(ptr);
+    if (next == list_head_.End()) { return nullptr; }
+    return next;
+  }
+  value_type* Last() {
+    if (list_head_.empty()) { return nullptr; }
+    return list_head_.Last();
+  }
+  constexpr value_type* End() { return nullptr; }
+
+  void MoveToDstBack(value_type* ptr, TrivialObjectMsgList* dst) {
+    list_head_.MoveToDstBack(ptr, &dst->list_head_);
+    if (ptr == container_ && ptr != dst->container_) {
+      ObjectMsgPtrUtil::Ref(ptr);
+    } else if (ptr != container_ && ptr == dst->container_) {
+      ObjectMsgPtrUtil::ReleaseRef(ptr);
+    } else {
+      // do nothing
+    }
+  }
+  void MoveToDstFront(value_type* ptr, TrivialObjectMsgList* dst) {
+    list_head_.MoveToDstFront(ptr, &dst->list_head_);
+    if (ptr == container_ && ptr != dst->container_) {
+      ObjectMsgPtrUtil::Ref(ptr);
+    } else if (ptr != container_ && ptr == dst->container_) {
+      ObjectMsgPtrUtil::ReleaseRef(ptr);
+    } else {
+      // do nothing
+    }
+  }
+  value_type* MoveFrontToDstBack(TrivialObjectMsgList* dst) {
+    value_type* begin = list_head_.Begin();
+    MoveToDstBack(begin, dst);
+    return begin;
+  }
+  value_type* MoveBackToDstBack(TrivialObjectMsgList* dst) {
+    value_type* begin = list_head_.Last();
+    MoveToDstBack(begin, dst);
+    return begin;
+  }
+
+  void PushBack(value_type* ptr) {
+    list_head_.PushBack(ptr);
+    if (container_ != ptr) { ObjectMsgPtrUtil::Ref(ptr); }
+  }
+
+  void PushFront(value_type* ptr) {
+    list_head_.PushFront(ptr);
+    if (container_ != ptr) { ObjectMsgPtrUtil::Ref(ptr); }
+  }
+
+  void EmplaceBack(ObjectMsgPtr<value_type>&& ptr) {
+    value_type* raw_ptr = nullptr;
+    if (container_ != ptr.Mutable()) {
+      ptr.__UnsafeMoveTo__(&raw_ptr);
+    } else {
+      raw_ptr = ptr.Mutable();
+    }
+    list_head_.PushBack(raw_ptr);
+  }
+
+  void EmplaceFront(ObjectMsgPtr<value_type>&& ptr) {
+    value_type* raw_ptr = nullptr;
+    if (container_ != ptr.Mutable()) {
+      ptr.__UnsafeMoveTo__(&raw_ptr);
+    } else {
+      raw_ptr = ptr.Mutable();
+    }
+    list_head_.PushFront(ptr);
+  }
+
+  ObjectMsgPtr<value_type> Erase(value_type* ptr) {
+    list_head_.Erase(ptr);
+    if (container_ != ptr) {
+      return ObjectMsgPtr<value_type>::__UnsafeMove__(ptr);
+    } else {
+      return ObjectMsgPtr<value_type>(ptr);
+    }
+  }
+
+  ObjectMsgPtr<value_type> PopBack() {
+    value_type* raw_ptr = nullptr;
+    if (!list_head_.empty()) { raw_ptr = list_head_.PopBack(); }
+    if (container_ != raw_ptr) {
+      return ObjectMsgPtr<value_type>::__UnsafeMove__(raw_ptr);
+    } else {
+      return ObjectMsgPtr<value_type>(raw_ptr);
+    }
+  }
+
+  ObjectMsgPtr<value_type> PopFront() {
+    value_type* raw_ptr = nullptr;
+    if (!list_head_.empty()) { raw_ptr = list_head_.PopFront(); }
+    if (container_ != raw_ptr) {
+      return ObjectMsgPtr<value_type>::__UnsafeMove__(raw_ptr);
+    } else {
+      return ObjectMsgPtr<value_type>(raw_ptr);
+    }
+  }
+
+  void MoveTo(TrivialObjectMsgList* list) { MoveToDstBack(list); }
+  void MoveToDstBack(TrivialObjectMsgList* list) {
+    while (!empty()) { MoveToDstBack(list_head_.PopFront(), list); }
+  }
+
+  void Clear() {
+    while (!empty()) {
+      auto* ptr = list_head_.PopFront();
+      if (container_ != ptr) { ObjectMsgPtrUtil::ReleaseRef(ptr); }
+    }
+  }
+
+ private:
+  EmbeddedListHead<ValueLinkField> list_head_;
+  const value_type* container_;
 };
 
 template<typename LinkField>
