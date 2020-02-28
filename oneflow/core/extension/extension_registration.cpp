145ccaa4fed6735e4330e48caec2f272729888e7
tsai <caishenghang@oneflow.org>
2020-02-28 14:23:38 +0800

    move impl to cpp
    Former-commit-id: 5ecc60f3af9323e4bca83f8cecee8bc232302e52

---
+++ oneflow/core/extension/extension_registration.cpp
@@ -0,0 +1,35 @@
+#include "oneflow/core/extension/extension_registration.h"
+
+namespace oneflow {
+
+namespace {
+HashMap<std::string, std::vector<std::function<extension::ExtensionBase*()>>>*
+MutExtensionRegistry() {
+  static HashMap<std::string, std::vector<std::function<extension::ExtensionBase*()>>> creators;
+  return &creators;
+}
+
+}  // namespace
+
+namespace extension {
+
+Registrar::Registrar(std::string ev_name,
+                     std::function<extension::ExtensionBase*()> ext_contructor) {
+  auto* creators = MutExtensionRegistry();
+  (*creators)[ev_name].emplace_back(std::move(ext_contructor));
+}
+
+const std::vector<std::function<extension::ExtensionBase*()>>* LookUpExtensionRegistry(
+    const std::string& ev_name) {
+  const auto registry = MutExtensionRegistry();
+  auto it = registry->find(ev_name);
+  if (it == registry->end()) {
+    return nullptr;
+  } else {
+    return &(it->second);
+  }
+}
+
+}  // namespace extension
+
+}  // namespace oneflow
--- oneflow/core/extension/extension_registration.h
+++ oneflow/core/extension/extension_registration.h
@@ -3,39 +3,17 @@
 
 #include "oneflow/core/extension/extension_base.h"
 #include <string>
+
 namespace oneflow {
-
-namespace {
-HashMap<std::string, std::vector<std::function<extension::ExtensionBase*()>>>*
-MutExtensionRegistry() {
-  static HashMap<std::string, std::vector<std::function<extension::ExtensionBase*()>>> creators;
-  return &creators;
-}
-
-}  // namespace
-
 namespace extension {
 
 struct Registrar final {
-  Registrar(std::string ev_name, std::function<extension::ExtensionBase*()> ext_contructor) {
-    auto* creators = MutExtensionRegistry();
-    (*creators)[ev_name].emplace_back(std::move(ext_contructor));
-  }
+  Registrar(std::string ev_name, std::function<extension::ExtensionBase*()> ext_contructor);
 };
 
-const std::vector<std::function<extension::ExtensionBase*()>>* LookUpExtensionRegistry(
-    const std::string& ev_name) {
-  const auto registry = MutExtensionRegistry();
-  auto it = registry->find(ev_name);
-  if (it == registry->end()) {
-    return nullptr;
-  } else {
-    return &(it->second);
-  }
-}
+const std::vector<std::function<ExtensionBase*()>>* LookUpExtensionRegistry(
+    const std::string& ev_name);
 
 }  // namespace extension
-
 }  // namespace oneflow
-
 #endif  // ONEFLOW_CORE_EXTENSION_REGISTRAR_H_
