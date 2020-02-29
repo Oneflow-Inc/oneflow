65f499f9bc827062a4a8fb7b342bbddd8def1e14
tsai <caishenghang@oneflow.org>
2020-02-29 10:45:47 +0800

    rm log
    Former-commit-id: caf6d319e4094bea25afcad36435a55afd5a6fc3

--- oneflow/core/extension/event_handler.h
+++ oneflow/core/extension/event_handler.h
@@ -1,24 +1,31 @@
 #ifndef ONEFLOW_CORE_EXTENSION_EVENT_HANDLER_H
 #define ONEFLOW_CORE_EXTENSION_EVENT_HANDLER_H
+
 #include "oneflow/core/extension/extension_base.h"
 #include "oneflow/core/extension/extension_registration.h"
+
 namespace oneflow {
+
 namespace extension {
+
 void kernel_event(std::string event_name, const Kernel* kernel) {
   auto* ext_constructors = LookUpExtensionRegistry(event_name);
   if (ext_constructors == nullptr) {
-    LOG(ERROR) << "No ext";
     return;
-  }
-  for (const std::function<extension::ExtensionBase*()> ext_constructor : *ext_constructors) {
-    KernelExtensionContext* ctx = kernel->kernel_ext_ctx.get();
-    KernelEvent event;
-    event.name = event_name;
-    event.context = ctx;
-    event.kernel_ptr = kernel;
-    ext_constructor()->callback(&event);
+  } else {
+    for (const std::function<extension::ExtensionBase*()> ext_constructor : *ext_constructors) {
+      KernelExtensionContext* ctx = kernel->kernel_ext_ctx.get();
+      KernelEvent event;
+      event.name = event_name;
+      event.context = ctx;
+      event.kernel_ptr = kernel;
+      ext_constructor()->callback(&event);
+    }
   }
 }
+
 }  // namespace extension
+
 }  // namespace oneflow
+
 #endif  // ONEFLOW_CORE_EXTENSION_EVENT_HANDLER_H
