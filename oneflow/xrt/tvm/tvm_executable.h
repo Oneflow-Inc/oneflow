b280ec565405c97030a9adee992c69c3a5b827ac
Dounm <niuchong893184@gmail.com>
2020-03-06 11:12:12 +0800

    Add all files first

---
+++ oneflow/xrt/tvm/tvm_executable.h
@@ -0,0 +1,19 @@
+#ifndef ONEFLOW_XRT_TVM_TVM_EXECUTABLE_H_
+#define ONEFLOW_XRT_TVM_TVM_EXECUTABLE_H_
+
+#include "oneflow/xrt/executable.h"
+#include "oneflow/xrt/parameter.h"
+
+namespace oneflow {
+
+namespace xrt {
+
+class TVMExecutable final : public Executable {
+
+};
+
+}
+
+}
+
+#endif // ONEFLOW_XRT_TVM_TVM_EXECUTABLE_H_
