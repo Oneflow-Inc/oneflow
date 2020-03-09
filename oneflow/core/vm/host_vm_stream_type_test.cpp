95e9d7ee2a5a82d1d12865503fbfc62bb2b02c79
lixinqi <lixinqi0703106@163.com>
2020-03-09 12:45:41 +0800

    add test cases for HostVmStreamType
    Former-commit-id: b3fae509fc1042b3875cf828e672c832a8aff962

--- oneflow/core/vm/host_vm_stream_type_test.cpp
+++ oneflow/core/vm/host_vm_stream_type_test.cpp
@@ -1,4 +1,6 @@
+#define private public
 #include "oneflow/core/common/util.h"
+#include "oneflow/core/vm/control_vm_stream_type.h"
 #include "oneflow/core/vm/host_vm_stream_type.h"
 #include "oneflow/core/vm/scheduler.msg.h"
 #include "oneflow/core/vm/vm_desc.msg.h"
@@ -11,6 +13,33 @@ namespace test {
 namespace {
 
 using VmInstructionMsgList = OBJECT_MSG_LIST(VmInstructionMsg, vm_instr_msg_link);
+
+TEST(HostVmStreamType, basic) {
+  auto host_vm_stream_desc =
+      ObjectMsgPtr<VmStreamDesc>::New(HostVmStreamType::kVmStreamTypeId, 1, 1, 1);
+  auto vm_desc = ObjectMsgPtr<VmDesc>::New();
+  vm_desc->mut_vm_stream_type_id2desc()->Insert(host_vm_stream_desc.Mutable());
+  auto scheduler = ObjectMsgPtr<VmScheduler>::New(vm_desc.Get());
+  VmInstructionMsgList list;
+  uint64_t symbol_value = 9527;
+  list.EmplaceBack(ControlVmStreamType().NewMirroredObjectSymbol(symbol_value, 1));
+  list.EmplaceBack(HostVmStreamType().CudaMallocHost(9527, 1024));
+  list.EmplaceBack(HostVmStreamType().CudaFreeHost(9527));
+  scheduler->Receive(&list);
+  scheduler->Schedule();
+  scheduler->mut_vm_thread_list()->Begin()->WaitAndRun();
+  scheduler->Schedule();
+  scheduler->mut_vm_thread_list()->Begin()->WaitAndRun();
+  scheduler->Schedule();
+  ASSERT_EQ(scheduler->waiting_vm_instr_chain_list().size(), 0);
+  ASSERT_EQ(scheduler->active_vm_stream_list().size(), 0);
+  auto* vm_thread = scheduler->mut_vm_thread_list()->Begin();
+  ASSERT_TRUE(vm_thread != nullptr);
+  auto* vm_stream = vm_thread->mut_vm_stream_list()->Begin();
+  ASSERT_TRUE(vm_stream != nullptr);
+  auto* vm_instr_chain = vm_stream->mut_running_chain_list()->Begin();
+  ASSERT_TRUE(vm_instr_chain == nullptr);
+}
 
 }  // namespace
 
