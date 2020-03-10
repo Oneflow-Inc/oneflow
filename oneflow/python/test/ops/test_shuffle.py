8d8e0485f54597c259509b7f12b0a3f295434b37
tsai <caishenghang@oneflow.org>
2020-03-10 17:58:33 +0800

    update test

--- oneflow/python/test/ops/test_shuffle.py
+++ oneflow/python/test/ops/test_shuffle.py
@@ -1,10 +1,32 @@
 import oneflow as flow
 import numpy as np
 from collections import OrderedDict
-
+import uuid
 from test_util import GenArgList
 from test_util import type_name_to_flow_type
 from test_util import type_name_to_np_type
+
+
+def generate_random_batch_permutation_indices(value, seed=None, name=None):
+    op = (
+        flow.user_op_builder(
+            name
+            if name is not None
+            else str(uuid.uuid4()) + value.op_name + "_random_batch_permutation_indices"
+        )
+        .Op("generate_random_batch_permutation_indices")
+        .Input("x", [value])
+        .Output("y")
+    )
+    if seed is not None:
+        op.SetAttr("has_seed", True, "AttrTypeBool").SetAttr(
+            "seed", seed, "AttrTypeInt64"
+        )
+    else:
+        op.SetAttr("has_seed", False, "AttrTypeBool").SetAttr(
+            "seed", -1, "AttrTypeInt64"
+        )
+    return op.Build().RemoteBlobList()[0]
 
 
 def test_shuffle(_):
@@ -34,5 +56,27 @@ def test_shuffle(_):
                 return flow.random.shuffle(x)
 
         x = np.random.randn(*x_shape).astype(type_name_to_np_type[data_type])
-        ret = TestJob(x).get()
-        assert np.array_equal(x.sort(), ret.ndarray().sort())
+        ret = TestJob(x).get().ndarray()
+        x.sort(0)
+        ret.sort(0)
+        assert np.array_equal(x, ret), x_shape
+
+        assert device_type in ["gpu", "cpu"]
+        assert data_type in ["float32", "double", "int8", "int32", "int64"]
+        flow.clear_default_session()
+        func_config = flow.FunctionConfig()
+        func_config.default_data_type(flow.float)
+
+        @flow.function(flow.FunctionConfig())
+        def TestJob1(
+            x=flow.FixedTensorDef(x_shape, dtype=type_name_to_flow_type[data_type])
+        ):
+            with flow.fixed_placement(device_type, "0:0"):
+                return generate_random_batch_permutation_indices(x)
+
+        x = np.random.randn(*x_shape).astype(type_name_to_np_type[data_type])
+        ret = TestJob1(x).get().ndarray()
+        idx = np.arange(x_shape[0]).astype(np.int32)
+        idx.sort()
+        ret.sort()
+        assert np.array_equal(idx, ret), x_shape
