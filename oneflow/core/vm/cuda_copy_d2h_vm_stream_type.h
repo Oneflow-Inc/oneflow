b13b61564cc3b3d55250f6d7a0dd43e958b2382b
lixinqi <lixinqi0703106@163.com>
2020-03-11 16:46:50 +0800

    vm/

---
+++ oneflow/core/vm/cuda_copy_d2h_vm_stream_type.h
@@ -0,0 +1,34 @@
+#ifndef ONEFLOW_CORE_VM_CUDA_COPY_D2H_VM_STREAM_TYPE_H_
+#define ONEFLOW_CORE_VM_CUDA_COPY_D2H_VM_STREAM_TYPE_H_
+
+#include "oneflow/core/vm/vm_stream_type.h"
+
+namespace oneflow {
+
+class VmScheduler;
+class VmInstructionMsg;
+
+class CudaCopyD2HVmStreamType final : public VmStreamType {
+ public:
+  CudaCopyD2HVmStreamType() = default;
+  ~CudaCopyD2HVmStreamType() = default;
+
+  static const VmStreamTypeId kVmStreamTypeId = 5;
+
+  ObjectMsgPtr<VmInstructionMsg> Copy(uint64_t dst, uint64_t src, size_t size) const;
+
+  void InitDeviceCtx(std::unique_ptr<DeviceCtx>* device_ctx,
+                     CallbackMsgListPtr callback_list) const override;
+
+  void InitVmInstructionStatus(const VmStream& vm_stream,
+                               VmInstructionStatusBuffer* status_buffer) const override;
+  void DeleteVmInstructionStatus(const VmStream& vm_stream,
+                                 VmInstructionStatusBuffer* status_buffer) const override;
+  bool QueryVmInstructionStatusDone(const VmStream& vm_stream,
+                                    const VmInstructionStatusBuffer& status_buffer) const override;
+  void Run(VmInstrChain* vm_instr_chain) const override;
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_VM_CUDA_COPY_D2H_VM_STREAM_TYPE_H_
