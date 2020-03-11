bb5d8488c6dac5d006a4835b92b1d3615adb7831
cheng cheng <472491134@qq.com>
2020-03-11 19:16:07 +0800

    Feat: UserOp support infer blob header (#2677)
    * remove DataTypeInferFn in op registration
    
    * refine blob_desc::is_tensor_list() return type; Add header flags in user_op::InferContext
    
    * Add TestDynamicSource op, and UserOp support set blob header
    
    * refine user_op::TensorDesc; ShapeInferFn->TensorDescInferFn
    
    Former-commit-id: 59d2bc41f3cb4a235b14a3784c47729fc6aaf8fb

--- oneflow/customized/ops/top_k_op.cpp
+++ oneflow/customized/ops/top_k_op.cpp
@@ -7,16 +7,13 @@ REGISTER_USER_OP("top_k")
     .Output("out")
     .Attr("k", UserOpAttrType::kAtInt32)
     .Attr("sorted", UserOpAttrType::kAtBool)
-    .SetShapeInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
       const Shape* in_shape = ctx->Shape4ArgNameAndIndex("in", 0);
       Shape* out_shape = ctx->Shape4ArgNameAndIndex("out", 0);
       *out_shape = *in_shape;
       out_shape->Set(
           in_shape->NumAxes() - 1,
           std::min(ctx->GetAttr<int32_t>("k"), static_cast<int32_t>(in_shape->dim_vec().back())));
-      return Maybe<void>::Ok();
-    })
-    .SetDataTypeInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
       *ctx->Dtype4ArgNameAndIndex("out", 0) = DataType::kInt32;
       return Maybe<void>::Ok();
     })
