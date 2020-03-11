cba84462c7e850d4df606df46dd3a7f002df3515
cheng cheng <472491134@qq.com>
2020-03-11 23:41:42 +0800

    add TestRandomSource op and test pass

--- oneflow/customized/kernels/test_kernels.cpp
+++ oneflow/customized/kernels/test_kernels.cpp
@@ -1,5 +1,6 @@
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
+#include "oneflow/core/kernel/random_generator.h"
 
 namespace oneflow {
 
@@ -243,6 +244,35 @@ class TestDynamicSourceKernel final : public user_op::OpKernel {
 
 REGISTER_USER_KERNEL("TestDynamicSource")
     .SetCreateFn([](user_op::KernelInitContext* ctx) { return new TestDynamicSourceKernel(ctx); })
+    .SetIsMatchedPred([](const user_op::KernelRegContext& ctx) {
+      const user_op::TensorDesc* out_tensor = ctx.TensorDesc4ArgNameAndIndex("out", 0);
+      if (ctx.device_type() == DeviceType::kCPU && out_tensor->data_type() == DataType::kFloat) {
+        return true;
+      }
+      return false;
+    });
+
+class TestRandomSourceKernel final : public user_op::OpKernel {
+ public:
+  TestRandomSourceKernel(user_op::KernelInitContext* ctx) : user_op::OpKernel(ctx) {
+    int64_t seed = ctx->GetAttr<int64_t>("seed");
+    random_generator_.reset(new RandomGenerator<DeviceType::kCPU>(seed, ctx->device_ctx()));
+  }
+  TestRandomSourceKernel() = default;
+  ~TestRandomSourceKernel() = default;
+
+ private:
+  void Compute(user_op::KernelContext* ctx) override {
+    user_op::Tensor* out_blob = ctx->Tensor4ArgNameAndIndex("out", 0);
+    random_generator_->Uniform<float>(out_blob->shape().elem_cnt(), 0.0, 1.0,
+                                      out_blob->mut_dptr<float>());
+  }
+
+  std::unique_ptr<RandomGenerator<DeviceType::kCPU>> random_generator_;
+};
+
+REGISTER_USER_KERNEL("TestRandomSource")
+    .SetCreateFn([](user_op::KernelInitContext* ctx) { return new TestRandomSourceKernel(ctx); })
     .SetIsMatchedPred([](const user_op::KernelRegContext& ctx) {
       const user_op::TensorDesc* out_tensor = ctx.TensorDesc4ArgNameAndIndex("out", 0);
       if (ctx.device_type() == DeviceType::kCPU && out_tensor->data_type() == DataType::kFloat) {
