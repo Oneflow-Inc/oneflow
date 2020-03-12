f933d1d1c96f1f79302bdb18229395eb98b906f2
xfjiang <sc.xfjiang@gmail.com>
2020-03-12 14:04:43 +0800

    reshape like and auto grad for squeeze & expand_dims

---
+++ oneflow/customized/kernels/reshape_like_kernel.cpp
@@ -0,0 +1,46 @@
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/core/kernel/new_kernel_util.h"
+
+namespace oneflow {
+
+template<typename T, DeviceType device_type>
+class ReshapeLikeKernel final : public user_op::OpKernel {
+ public:
+  ReshapeLikeKernel(const user_op::KernelInitContext& ctx) : user_op::OpKernel(ctx) {}
+  ReshapeLikeKernel() = default;
+  ~ReshapeLikeKernel() = default;
+
+ private:
+  void Compute(user_op::KernelContext* ctx) override {
+    const user_op::Tensor* in = ctx->Tensor4ArgNameAndIndex("in", 0);
+    const user_op::Tensor* like = ctx->Tensor4ArgNameAndIndex("like", 0);
+    user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("out", 0);
+
+    CHECK_EQ(in->shape().elem_cnt(), like->shape().elem_cnt());
+    Memcpy<device_type>(ctx->device_ctx(), out->mut_dptr<T>(), in->dptr<T>(),
+                        in->shape().elem_cnt() * sizeof(T));
+  };
+};
+
+#define REGISTER_RESHAPE_LIKE_KERNEL(dtype)                                 \
+  REGISTER_USER_KERNEL("reshape_like")                                      \
+      .SetCreateFn([](const oneflow::user_op::KernelInitContext& ctx) {     \
+        return new ReshapeLikeKernel<dtype, DeviceType::kCPU>(ctx);         \
+      })                                                                    \
+      .SetIsMatchedPred([](const oneflow::user_op::KernelRegContext& ctx) { \
+        return ctx.device_type() == DeviceType::kCPU;                       \
+      });                                                                   \
+  REGISTER_USER_KERNEL("reshape_like")                                      \
+      .SetCreateFn([](const oneflow::user_op::KernelInitContext& ctx) {     \
+        return new ReshapeLikeKernel<dtype, DeviceType::kGPU>(ctx);         \
+      })                                                                    \
+      .SetIsMatchedPred([](const oneflow::user_op::KernelRegContext& ctx) { \
+        return ctx.device_type() == DeviceType::kGPU;                       \
+      });
+
+REGISTER_RESHAPE_LIKE_KERNEL(float)
+REGISTER_RESHAPE_LIKE_KERNEL(double)
+REGISTER_RESHAPE_LIKE_KERNEL(int32_t)
+REGISTER_RESHAPE_LIKE_KERNEL(int64_t)
+
+}  // namespace oneflow
---
+++ oneflow/customized/ops/reshape_like_op.cpp
@@ -0,0 +1,34 @@
+#include "oneflow/core/framework/framework.h"
+
+namespace oneflow {
+
+REGISTER_USER_OP("reshape_like")
+    .Input("in")
+    .Input("like")
+    .Output("out")
+    .SetShapeInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      const Shape* in_shape = ctx->Shape4ArgNameAndIndex("in", 0);
+      const Shape* like_shape = ctx->Shape4ArgNameAndIndex("like", 0);
+      CHECK_EQ_OR_RETURN(in_shape->elem_cnt(), like_shape->elem_cnt());
+      *ctx->Shape4ArgNameAndIndex("out", 0) = *like_shape;
+      return Maybe<void>::Ok();
+    })
+    .SetDataTypeInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      *ctx->Dtype4ArgNameAndIndex("out", 0) = *ctx->Dtype4ArgNameAndIndex("in", 0);
+      return Maybe<void>::Ok();
+    })
+    .SetInputArgModifyFn([](user_op::GetInputArgModifier GetInputArgModifierFn) {
+      user_op::InputArgModifier* like_arg_modifier = GetInputArgModifierFn("like", 0);
+      CHECK(like_arg_modifier != nullptr);
+      like_arg_modifier->set_use_header_only(true);
+    })
+    .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
+      *ctx->BatchAxis4ArgNameAndIndex("out", 0) = *ctx->BatchAxis4ArgNameAndIndex("like", 0);
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      // TODO
+      return Maybe<void>::Ok();
+    });
+
+}  // namespace oneflow
