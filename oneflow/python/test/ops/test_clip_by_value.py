f6dccf1bec0449a837d1ca23c00f419c326ded94
leaves-zwx <kunta0932@gmail.com>
2020-03-12 17:31:12 +0800

    more test

--- oneflow/python/test/ops/test_clip_by_value.py
+++ oneflow/python/test/ops/test_clip_by_value.py
@@ -14,7 +14,7 @@ def _np_dtype_to_of_dtype(np_dtype):
         raise NotImplementedError
 
 
-def _of_clip_by_value(values, min=None, max=None, device_type="gpu", dynamic=False, grad_cb=None):
+def _of_clip_by_value(values, min, max, device_type="gpu", dynamic=False, grad_cb=None):
     data_type = _np_dtype_to_of_dtype(values.dtype)
 
     if callable(grad_cb):
@@ -115,6 +115,17 @@ def test_clip_by_min(test_case):
         test_case.assertTrue(np.array_equal(np_out, of_out))
 
 
+def test_clip_by_max(test_case):
+    values = np.random.standard_normal((2, 64, 800, 1088)).astype(np.float32)
+    np_out = np.clip(values, a_min=None, a_max=0.2)
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["cpu", "gpu"]
+    arg_dict["dynamic"] = [True, False]
+    for arg in GenArgList(arg_dict):
+        of_out = _of_clip_by_value(values, None, 0.2, *arg)
+        test_case.assertTrue(np.allclose(np_out, of_out))
+
+
 def test_clip_by_value_grad(test_case):
     values = np.random.standard_normal(1024).astype(np.float32)
     arg_dict = OrderedDict()
@@ -122,3 +133,12 @@ def test_clip_by_value_grad(test_case):
     arg_dict["dynamic"] = [True, False]
     for arg in GenArgList(arg_dict):
         _compare_with_tf(test_case, values, 0, 0.5, *arg)
+
+
+def test_clip_by_value_grad_case_1(test_case):
+    values = np.random.standard_normal((128, 10, 27)).astype(np.float32)
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["cpu", "gpu"]
+    arg_dict["dynamic"] = [True, False]
+    for arg in GenArgList(arg_dict):
+        _compare_with_tf(test_case, values, -0.2, 0.2, *arg)
