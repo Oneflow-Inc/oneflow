94604a90c07b533d831acdc7aeb9a46ba312deb0
xfjiang <sc.xfjiang@gmail.com>
2020-03-12 16:53:37 +0800

    bw test

--- oneflow/python/test/ops/test_expand_dims.py
+++ oneflow/python/test/ops/test_expand_dims.py
@@ -8,25 +8,37 @@ from test_util import type_name_to_flow_type
 from test_util import type_name_to_np_type
 
 
-def compare_with_tensorflow(device_type, in_shape, axis, data_type):
+def compare_with_tensorflow(device_type, x_shape, axis, data_type):
     assert device_type in ["gpu", "cpu"]
     assert data_type in ["float32", "double", "int8", "int32", "int64"]
     flow.clear_default_session()
     func_config = flow.FunctionConfig()
     func_config.default_data_type(flow.float)
 
-    @flow.function(func_config)
-    def ExpandDimsJob(
-        input=flow.MirroredTensorDef(in_shape, dtype=type_name_to_flow_type[data_type])
-    ):
-        with flow.fixed_placement(device_type, "0:0"):
-            return flow.expand_dims(input, axis)
+    def test_expand_dims_grad(x_diff_blob):
+        x_diff = x_diff_blob.ndarray()
+        assert np.array_equal(x_diff, np.ones(x_shape))
 
-    input = (np.random.random(in_shape) * 100).astype(type_name_to_np_type[data_type])
+    @flow.function(func_config)
+    def ExpandDimsJob():
+        with flow.fixed_placement(device_type, "0:0"):
+            x = flow.get_variable(
+                shape=x_shape,
+                dtype=type_name_to_flow_type[data_type],
+                initializer=flow.ones_initializer(type_name_to_flow_type[data_type]),
+                name="variable",
+            )
+            x = flow.identity(x)
+            y = flow.expand_dims(x, axis)
+            flow.watch_diff(x, test_expand_dims_grad)
+            return y
+
     # OneFlow
-    of_out = ExpandDimsJob([input]).get().ndarray_list()[0]
+    check_point = flow.train.CheckPoint()
+    check_point.init()
+    of_out = ExpandDimsJob().get().ndarray()
     # TensorFlow
-    tf_out = tf.expand_dims(input, axis).numpy()
+    tf_out = tf.expand_dims(np.ones(x_shape, dtype=type_name_to_np_type[data_type]), axis).numpy()
 
     assert np.array_equal(of_out, tf_out)
 
