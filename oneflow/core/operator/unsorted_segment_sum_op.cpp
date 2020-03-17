76846e24553f8d7a7fe21c41ec9a721ffba4ad95
Juncheng <liujuncheng1022@gmail.com>
2020-03-17 21:03:37 +0800

    UnsortedSegmentSumLikeOp (#2699)
    * UnsortedSegmentSumLikeOp op/kernel
    
    * Squashed commit of the following:
    
    commit a50da3368faba12e7fcb96a0d6a5d3fa37d721d8
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 16:10:19 2020 +0800
    
        fix
    
    commit c664052d6daa41ab5e31dccb778926f9088e6581
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 16:04:36 2020 +0800
    
        fix
    
    commit c52f94aa64ffd4ded30c2f64c91a89ed4b86ec02
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 15:59:59 2020 +0800
    
        fix
    
    commit 735abde147e60e7a3de6ecbdb14714a5337faa97
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 15:57:02 2020 +0800
    
        fix order
    
    commit dd027f995f9c7f4a669e8debea69436afbdacf04
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 15:54:50 2020 +0800
    
        like job
    
    commit a4de80aa1887a8fa6423eea5a77ceb6f37157efb
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 15:49:58 2020 +0800
    
        reduce test
    
    commit 1e1dc6b7565c7093d571ebf73fcb1b415884c4b8
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 15:44:10 2020 +0800
    
        more test
    
    commit 153da11b4d06b55bfe3ee110ff0000b92cdbaf24
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 15:17:07 2020 +0800
    
        fix
    
    commit 549e75bae318be767d5bb586af63a6f53e990934
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 15:02:24 2020 +0800
    
        test
    
    commit 45002a8e98bdb9f8b3cfdc8f7ffc139d3077f51b
    Author: liujuncheng <liujuncheng1022@gmail.com>
    Date:   Tue Mar 17 14:28:06 2020 +0800
    
        test np
    
    * fix gather grad
    
    * refine

--- oneflow/core/operator/unsorted_segment_sum_op.cpp
+++ oneflow/core/operator/unsorted_segment_sum_op.cpp
@@ -1,7 +1,29 @@
-#include "oneflow/core/operator/unsorted_segment_sum_op.h"
+#include "oneflow/core/operator/operator.h"
 #include "oneflow/core/job/sbp_signature_builder.h"
 
 namespace oneflow {
+
+class UnsortedSegmentSumOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(UnsortedSegmentSumOp);
+  UnsortedSegmentSumOp() = default;
+  ~UnsortedSegmentSumOp() override = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override;
+
+ private:
+  Maybe<void> InferBatchAxis(
+      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override;
+  Maybe<void> GetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      SbpSignatureList* sbp_sig_list) const override;
+  void VirtualGenKernelConf(std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                            const ParallelContext* parallel_ctx,
+                            KernelConf* kernel_conf) const override;
+};
 
 void UnsortedSegmentSumOp::InitFromOpConf() {
   CHECK(op_conf().has_unsorted_segment_sum_conf());
@@ -17,7 +39,6 @@ const PbMessage& UnsortedSegmentSumOp::GetCustomizedConf() const {
 Maybe<void> UnsortedSegmentSumOp::InferBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx) const {
-  // TODO: fix when input in fw is dynamic at gather axis dim
   const UnsortedSegmentSumOpConf& conf = op_conf().unsorted_segment_sum_conf();
   const BlobDesc* segment_ids = GetBlobDesc4BnInOp("segment_ids");
   CHECK_OR_RETURN(IsIndexDataType(segment_ids->data_type()));
@@ -79,8 +100,130 @@ void UnsortedSegmentSumOp::VirtualGenKernelConf(
     const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
   kernel_conf->mutable_unsorted_segment_sum_conf()->set_indices_data_type(
       GetBlobDesc4BnInOp("segment_ids")->data_type());
+  kernel_conf->mutable_unsorted_segment_sum_conf()->set_axis(
+      op_conf().unsorted_segment_sum_conf().axis());
+}
+
+class UnsortedSegmentSumLikeOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(UnsortedSegmentSumLikeOp);
+  UnsortedSegmentSumLikeOp() = default;
+  ~UnsortedSegmentSumLikeOp() override = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override;
+
+ private:
+  Maybe<void> InferBatchAxis(
+      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override;
+  Maybe<void> GetSbpSignatures(
+      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+      SbpSignatureList* sbp_sig_list) const override;
+  void VirtualGenKernelConf(std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                            const ParallelContext* parallel_ctx,
+                            KernelConf* kernel_conf) const override;
+};
+
+void UnsortedSegmentSumLikeOp::InitFromOpConf() {
+  CHECK(op_conf().has_unsorted_segment_sum_like_conf());
+  EnrollInputBn("segment_ids", false);
+  EnrollInputBn("data");
+  EnrollInputBn("like", false)->set_use_header_only(true);
+  EnrollOutputBn("out");
+}
+
+const PbMessage& UnsortedSegmentSumLikeOp::GetCustomizedConf() const {
+  return op_conf().unsorted_segment_sum_like_conf();
+}
+
+Maybe<void> UnsortedSegmentSumLikeOp::InferBlobDescs(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx) const {
+  const UnsortedSegmentSumLikeOpConf& conf = op_conf().unsorted_segment_sum_like_conf();
+  const BlobDesc* segment_ids = GetBlobDesc4BnInOp("segment_ids");
+  CHECK_OR_RETURN(IsIndexDataType(segment_ids->data_type()));
+  const BlobDesc* data = GetBlobDesc4BnInOp("data");
+  const BlobDesc* like = GetBlobDesc4BnInOp("like");
+  CHECK_EQ_OR_RETURN(data->data_type(), like->data_type());
+  CHECK_GE_OR_RETURN(conf.axis(), 0);
+  CHECK_LE_OR_RETURN(conf.axis(), like->shape().NumAxes());
+  FOR_RANGE(int64_t, i, 0, conf.axis()) {
+    CHECK_EQ_OR_RETURN(like->shape().At(i), data->shape().At(i));
+  }
+  CHECK_EQ_OR_RETURN(data->shape().NumAxes() - segment_ids->shape().NumAxes() + 1,
+                     like->shape().NumAxes());
+  FOR_RANGE(int64_t, i, conf.axis() + 1, like->shape().NumAxes()) {
+    CHECK_EQ_OR_RETURN(like->shape().At(i),
+                       data->shape().At(i + segment_ids->shape().NumAxes() - 1));
+  }
+  GetBlobDesc4BnInOp("out")->CopyMetaFrom(*like);
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> UnsortedSegmentSumLikeOp::GetSbpSignatures(
+    const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
+    SbpSignatureList* sbp_sig_list) const {
+  const int64_t segment_sum_axis = op_conf().unsorted_segment_sum_conf().axis();
+  const int64_t segment_ids_num_axes = JUST(LogicalBlobDesc4Ibn("segment_ids"))->shape().NumAxes();
+  const int64_t data_num_axes = JUST(LogicalBlobDesc4Ibn("data"))->shape().NumAxes();
+  FOR_RANGE(int64_t, i, 0, segment_ids_num_axes) {
+    SbpSignatureBuilder()
+        .Split("segment_ids", i)
+        .Split("data", i + segment_sum_axis)
+        .Broadcast("like")
+        .PartialSum("out")
+        .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+    SbpSignatureBuilder()
+        .Split("segment_ids", i)
+        .Split("data", i + segment_sum_axis)
+        .PartialSum("like")
+        .PartialSum("out")
+        .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  }
+  FOR_RANGE(int64_t, i, 0, data_num_axes) {
+    if (i >= segment_sum_axis && i < segment_sum_axis + segment_ids_num_axes) { continue; }
+    const int64_t out_split_axis = (i < segment_sum_axis) ? i : i - segment_ids_num_axes + 1;
+    if (out_split_axis == segment_sum_axis) { continue; }
+    SbpSignatureBuilder()
+        .Broadcast("segment_ids")
+        .Split("data", i)
+        .Split("like", out_split_axis)
+        .Split("out", out_split_axis)
+        .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  }
+  SbpSignatureBuilder()
+      .Broadcast("segment_ids")
+      .PartialSum("data")
+      .Broadcast("like")
+      .PartialSum("out")
+      .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  SbpSignatureBuilder()
+      .Broadcast("segment_ids")
+      .PartialSum("data")
+      .PartialSum("like")
+      .PartialSum("out")
+      .Build(sbp_sig_list->mutable_sbp_signature()->Add());
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> UnsortedSegmentSumLikeOp::InferBatchAxis(
+    std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const {
+  *BatchAxis4BnInOp("out") = *BatchAxis4BnInOp("like");
+  return Maybe<void>::Ok();
+}
+
+void UnsortedSegmentSumLikeOp::VirtualGenKernelConf(
+    std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
+  kernel_conf->mutable_unsorted_segment_sum_conf()->set_indices_data_type(
+      GetBlobDesc4BnInOp("segment_ids")->data_type());
+  kernel_conf->mutable_unsorted_segment_sum_conf()->set_axis(
+      op_conf().unsorted_segment_sum_like_conf().axis());
 }
 
 REGISTER_OP(OperatorConf::kUnsortedSegmentSumConf, UnsortedSegmentSumOp);
+REGISTER_OP(OperatorConf::kUnsortedSegmentSumLikeConf, UnsortedSegmentSumLikeOp);
 
 }  // namespace oneflow
