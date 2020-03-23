033a76897bfa74b32264fb62a75ec5a5fb433624
Shenghang Tsai <jackalcooper@gmail.com>
2020-03-23 17:01:32 +0800

    Refactor sync dynamic resize kernel and fix testcase (#2708)
    * check in support for all kinds of integer
    
    * check in testcase updates
    
    * check in changes
    
    * rename
    
    * minor change
    
    * check in host version
    
    * rm case to speed up
    
    * add check
    
    * mv size data type to kernel conf
    
    Co-authored-by: Tsai <caishenghang@oneflow.org>
    Former-commit-id: d51e4b53d026c933bfd8a3584fd9038e68101894

--- oneflow/core/kernel/sync_dynamic_resize_kernel.cpp
+++ oneflow/core/kernel/sync_dynamic_resize_kernel.cpp
@@ -25,11 +25,12 @@ class CudaHostMem {
 
 }  // namespace
 
-class SyncDynamicResizeKernel final : public KernelIf<DeviceType::kGPU> {
+template<typename SizeType>
+class SyncDynamicResizeGPUKernel final : public KernelIf<DeviceType::kGPU> {
  public:
-  OF_DISALLOW_COPY_AND_MOVE(SyncDynamicResizeKernel);
-  SyncDynamicResizeKernel() = default;
-  ~SyncDynamicResizeKernel() override = default;
+  OF_DISALLOW_COPY_AND_MOVE(SyncDynamicResizeGPUKernel);
+  SyncDynamicResizeGPUKernel() = default;
+  ~SyncDynamicResizeGPUKernel() override = default;
 
  private:
   bool IsKernelLaunchSynchronized() const override { return false; }
@@ -41,7 +42,7 @@ class SyncDynamicResizeKernel final : public KernelIf<DeviceType::kGPU> {
     {
       std::lock_guard<std::mutex> lock(mutex_);
       if (queue_.empty()) {
-        cuda_host_mem_ptr.reset(new CudaHostMem(sizeof(int32_t)));
+        cuda_host_mem_ptr.reset(new CudaHostMem(sizeof(SizeType)));
       } else {
         cuda_host_mem_ptr = queue_.front();
         queue_.pop();
@@ -52,10 +53,10 @@ class SyncDynamicResizeKernel final : public KernelIf<DeviceType::kGPU> {
     Blob* out = BnInOp2Blob("out");
     AutoMemcpy(ctx.device_ctx, out->mut_dptr(), in->dptr(), in->ByteSizeOfBlobBody(),
                out->mem_case(), in->mem_case());
-    AutoMemcpy(ctx.device_ctx, cuda_host_mem_ptr->Ptr(), size->dptr(), sizeof(int32_t),
+    AutoMemcpy(ctx.device_ctx, cuda_host_mem_ptr->Ptr(), size->dptr(), sizeof(SizeType),
                MakeHostMemCase(), size->mem_case());
     ctx.device_ctx->AddCallBack([out, cuda_host_mem_ptr, conf, this]() {
-      const int64_t new_size = *reinterpret_cast<int32_t*>(cuda_host_mem_ptr->Ptr());
+      const int64_t new_size = *reinterpret_cast<SizeType*>(cuda_host_mem_ptr->Ptr());
       CHECK_GE(new_size, 0);
       CHECK_LE(new_size, out->shape_view().At(conf.axis()));
       out->mut_shape_view()->Set(conf.axis(), new_size);
@@ -68,6 +69,51 @@ class SyncDynamicResizeKernel final : public KernelIf<DeviceType::kGPU> {
   mutable std::mutex mutex_;
 };
 
-REGISTER_KERNEL_WITH_NOTHING(OperatorConf::kSyncDynamicResizeConf, SyncDynamicResizeKernel);
+#define REGISTER_SYNC_DYNAMIC_RESIZE_GPU_KERNEL(stype)                                         \
+  NEW_REGISTER_KERNEL(OperatorConf::kSyncDynamicResizeConf, SyncDynamicResizeGPUKernel<stype>) \
+      .SetIsMatchedPred([](const KernelConf& kernel_conf) {                                    \
+        return (kernel_conf.op_attribute().op_conf().device_type() == DeviceType::kGPU         \
+                && GetDataType<stype>::value                                                   \
+                       == kernel_conf.sync_dynamic_resize_conf().size_data_type());            \
+      })
+REGISTER_SYNC_DYNAMIC_RESIZE_GPU_KERNEL(int8_t);
+REGISTER_SYNC_DYNAMIC_RESIZE_GPU_KERNEL(int32_t);
+REGISTER_SYNC_DYNAMIC_RESIZE_GPU_KERNEL(int64_t);
+
+template<typename SizeType>
+class SyncDynamicResizeCPUKernel final : public KernelIf<DeviceType::kCPU> {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(SyncDynamicResizeCPUKernel);
+  SyncDynamicResizeCPUKernel() = default;
+  ~SyncDynamicResizeCPUKernel() override = default;
+
+ private:
+  bool IsKernelLaunchSynchronized() const override { return false; }
+  void ForwardDataContent(const KernelCtx& ctx,
+                          std::function<Blob*(const std::string&)> BnInOp2Blob) const override {
+    const SyncDynamicResizeOpConf& conf = this->op_conf().sync_dynamic_resize_conf();
+    CHECK_EQ(conf.axis(), 0);
+    const Blob* in = BnInOp2Blob("in");
+    const Blob* size = BnInOp2Blob("size");
+    Blob* out = BnInOp2Blob("out");
+    AutoMemcpy(ctx.device_ctx, out->mut_dptr(), in->dptr(), in->ByteSizeOfBlobBody(),
+               out->mem_case(), in->mem_case());
+    const SizeType new_size = *size->dptr<SizeType>();
+    CHECK_GE(new_size, 0);
+    CHECK_LE(new_size, out->shape_view().At(conf.axis()));
+    out->mut_shape_view()->Set(conf.axis(), new_size);
+  }
+};
+
+#define REGISTER_SYNC_DYNAMIC_RESIZE_CPU_KERNEL(stype)                                         \
+  NEW_REGISTER_KERNEL(OperatorConf::kSyncDynamicResizeConf, SyncDynamicResizeCPUKernel<stype>) \
+      .SetIsMatchedPred([](const KernelConf& kernel_conf) {                                    \
+        return (kernel_conf.op_attribute().op_conf().device_type() == DeviceType::kCPU         \
+                && GetDataType<stype>::value                                                   \
+                       == kernel_conf.sync_dynamic_resize_conf().size_data_type());            \
+      })
+REGISTER_SYNC_DYNAMIC_RESIZE_CPU_KERNEL(int8_t);
+REGISTER_SYNC_DYNAMIC_RESIZE_CPU_KERNEL(int32_t);
+REGISTER_SYNC_DYNAMIC_RESIZE_CPU_KERNEL(int64_t);
 
 }  // namespace oneflow
