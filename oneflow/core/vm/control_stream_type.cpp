dc9900e362b8101a57053837a6a3f793f9105f31
lixinqi <lixinqi0703106@163.com>
2020-03-25 16:53:15 +0800

    refactor NewSymbol/DeleteSymbol

--- oneflow/core/vm/control_stream_type.cpp
+++ oneflow/core/vm/control_stream_type.cpp
@@ -3,6 +3,7 @@
 #include "oneflow/core/vm/instruction_type.h"
 #include "oneflow/core/vm/instruction.msg.h"
 #include "oneflow/core/vm/scheduler.msg.h"
+#include "oneflow/core/vm/naive_instruction_status_querier.h"
 #include "oneflow/core/common/util.h"
 #include "oneflow/core/common/flat_msg_view.h"
 #include "oneflow/core/job/resource.pb.h"
@@ -12,77 +13,90 @@ namespace vm {
 
 enum CtrlInstrOpCode { kNewSymbol = 0, kDeleteSymbol };
 
-typedef void (*CtrlInstrFunc)(Scheduler*, InstructionMsg*);
-std::vector<CtrlInstrFunc> ctrl_instr_table;
+class NewSymbolInstructionType final : public InstructionType {
+ public:
+  NewSymbolInstructionType() = default;
+  ~NewSymbolInstructionType() override = default;
 
-#define REGISTER_CTRL_INSTRUCTION(op_code, function_name) \
-  COMMAND({                                               \
-    ctrl_instr_table.resize(op_code + 1);                 \
-    ctrl_instr_table.at(op_code) = &function_name;        \
-  })
+  using stream_type = ControlStreamType;
+  static const InstructionOpcode opcode = kNewSymbol;
 
-// clang-format off
-FLAT_MSG_VIEW_BEGIN(NewSymbolCtrlInstruction);
-  FLAT_MSG_VIEW_DEFINE_PATTERN(uint64_t, logical_object_id);
-  FLAT_MSG_VIEW_DEFINE_PATTERN(int64_t, parallel_num);
-FLAT_MSG_VIEW_END(NewSymbolCtrlInstruction);
-// clang-format on
+  // clang-format off
+  FLAT_MSG_VIEW_BEGIN(NewSymbolCtrlInstruction);
+    FLAT_MSG_VIEW_DEFINE_PATTERN(uint64_t, logical_object_id);
+    FLAT_MSG_VIEW_DEFINE_PATTERN(int64_t, parallel_num);
+  FLAT_MSG_VIEW_END(NewSymbolCtrlInstruction);
+  // clang-format on
 
-void NewSymbol(Scheduler* scheduler, InstructionMsg* instr_msg) {
-  FlatMsgView<NewSymbolCtrlInstruction> view;
-  CHECK(view->Match(instr_msg->mut_operand()));
-  auto logical_object = ObjectMsgPtr<LogicalObject>::NewFrom(
-      scheduler->mut_scheduler_thread_only_allocator(), view->logical_object_id());
-  CHECK(scheduler->mut_id2logical_object()->Insert(logical_object.Mutable()).second);
-  auto* parallel_id2mirrored_object = logical_object->mut_parallel_id2mirrored_object();
-  for (int64_t i = 0; i < view->parallel_num(); ++i) {
-    auto mirrored_object = ObjectMsgPtr<MirroredObject>::NewFrom(scheduler->mut_allocator(),
-                                                                 logical_object.Mutable(), i);
-    CHECK(parallel_id2mirrored_object->Insert(mirrored_object.Mutable()).second);
+  void Compute(Instruction*) const override { UNIMPLEMENTED(); }
+  void Compute(Scheduler* scheduler, InstructionMsg* instr_msg) const override {
+    FlatMsgView<NewSymbolCtrlInstruction> view;
+    CHECK(view->Match(instr_msg->mut_operand()));
+    auto logical_object = ObjectMsgPtr<LogicalObject>::NewFrom(
+        scheduler->mut_scheduler_thread_only_allocator(), view->logical_object_id());
+    CHECK(scheduler->mut_id2logical_object()->Insert(logical_object.Mutable()).second);
+    auto* parallel_id2mirrored_object = logical_object->mut_parallel_id2mirrored_object();
+    for (int64_t i = 0; i < view->parallel_num(); ++i) {
+      auto mirrored_object = ObjectMsgPtr<MirroredObject>::NewFrom(scheduler->mut_allocator(),
+                                                                   logical_object.Mutable(), i);
+      CHECK(parallel_id2mirrored_object->Insert(mirrored_object.Mutable()).second);
+    }
   }
-}
-REGISTER_CTRL_INSTRUCTION(CtrlInstrOpCode::kNewSymbol, NewSymbol);
-COMMAND(RegisterInstrTypeId<ControlStreamType>("NewSymbol", kNewSymbol, kRemote));
-COMMAND(RegisterInstrTypeId<ControlStreamType>("LocalNewSymbol", kNewSymbol, kLocal));
+};
+COMMAND(RegisterInstrTypeId<NewSymbolInstructionType>("NewSymbol", kRemote));
+COMMAND(RegisterInstrTypeId<NewSymbolInstructionType>("LocalNewSymbol", kLocal));
 
-// clang-format off
-FLAT_MSG_VIEW_BEGIN(DeleteSymbolCtrlInstruction);
-  FLAT_MSG_VIEW_DEFINE_PATTERN(MutableMirroredObjectOperand, mirrored_object_operand);
-FLAT_MSG_VIEW_END(DeleteSymbolCtrlInstruction);
-// clang-format on
+class DeleteSymbolInstructionType final : public InstructionType {
+ public:
+  DeleteSymbolInstructionType() = default;
+  ~DeleteSymbolInstructionType() override = default;
 
-void DeleteSymbol(Scheduler* scheduler, InstructionMsg* instr_msg) {
-  FlatMsgView<DeleteSymbolCtrlInstruction> view;
-  CHECK(view->Match(instr_msg->mut_operand()));
-  const auto& logical_objectId = view->mirrored_object_operand().operand().logical_object_id();
-  auto* logical_object = scheduler->mut_id2logical_object()->FindPtr(logical_objectId);
-  CHECK_NOTNULL(logical_object);
-  auto* parallel_id2mirrored_object = logical_object->mut_parallel_id2mirrored_object();
-  for (int i = 0; i < parallel_id2mirrored_object->size(); ++i) {
-    auto* mirrored_object = parallel_id2mirrored_object->FindPtr(i);
-    CHECK(!mirrored_object->has_object_type());
-    parallel_id2mirrored_object->Erase(mirrored_object);
+  using stream_type = ControlStreamType;
+  static const InstructionOpcode opcode = kDeleteSymbol;
+
+  // clang-format off
+  FLAT_MSG_VIEW_BEGIN(DeleteSymbolCtrlInstruction);
+    FLAT_MSG_VIEW_DEFINE_PATTERN(MutableMirroredObjectOperand, mirrored_object_operand);
+  FLAT_MSG_VIEW_END(DeleteSymbolCtrlInstruction);
+  // clang-format on
+
+  void Compute(Instruction*) const override { UNIMPLEMENTED(); }
+  void Compute(Scheduler* scheduler, InstructionMsg* instr_msg) const override {
+    FlatMsgView<DeleteSymbolCtrlInstruction> view;
+    CHECK(view->Match(instr_msg->mut_operand()));
+    const auto& logical_objectId = view->mirrored_object_operand().operand().logical_object_id();
+    auto* logical_object = scheduler->mut_id2logical_object()->FindPtr(logical_objectId);
+    CHECK_NOTNULL(logical_object);
+    auto* parallel_id2mirrored_object = logical_object->mut_parallel_id2mirrored_object();
+    for (int i = 0; i < parallel_id2mirrored_object->size(); ++i) {
+      auto* mirrored_object = parallel_id2mirrored_object->FindPtr(i);
+      CHECK(!mirrored_object->has_object_type());
+      parallel_id2mirrored_object->Erase(mirrored_object);
+    }
+    scheduler->mut_id2logical_object()->Erase(logical_object);
   }
-  scheduler->mut_id2logical_object()->Erase(logical_object);
-}
-REGISTER_CTRL_INSTRUCTION(CtrlInstrOpCode::kDeleteSymbol, DeleteSymbol);
-COMMAND(RegisterInstrTypeId<ControlStreamType>("DeleteSymbol", kDeleteSymbol, kRemote));
-COMMAND(RegisterInstrTypeId<ControlStreamType>("LocalDeleteSymbol", kDeleteSymbol, kLocal));
+};
+COMMAND(RegisterInstrTypeId<DeleteSymbolInstructionType>("DeleteSymbol", kRemote));
+COMMAND(RegisterInstrTypeId<DeleteSymbolInstructionType>("LocalDeleteSymbol", kLocal));
 
 void ControlStreamType::Run(Scheduler* scheduler, InstructionMsg* instr_msg) const {
-  InstructionOpcode opcode = instr_msg->instr_type_id().opcode();
-  return ctrl_instr_table.at(opcode)(scheduler, instr_msg);
+  const auto& instr_type_id = instr_msg->instr_type_id();
+  CHECK_EQ(instr_type_id.stream_type_id().interpret_type(), InterpretType::kCompute);
+  LookupInstructionType(instr_type_id)->Compute(scheduler, instr_msg);
 }
 
 void ControlStreamType::Run(Scheduler* scheduler, InstrChain* instr_chain) const {
   OBJECT_MSG_LIST_UNSAFE_FOR_EACH_PTR(instr_chain->mut_instruction_list(), instr) {
     Run(scheduler, instr->mut_instr_msg());
   }
+  auto* status_buffer = instr_chain->mut_status_buffer();
+  NaiveInstrStatusQuerier::MutCast(status_buffer->mut_buffer()->mut_data())->set_done();
 }
 
 void ControlStreamType::InitInstructionStatus(const Stream& stream,
                                               InstructionStatusBuffer* status_buffer) const {
-  // do nothing
+  static_assert(sizeof(NaiveInstrStatusQuerier) < kInstructionStatusBufferBytes, "");
+  NaiveInstrStatusQuerier::PlacementNew(status_buffer->mut_buffer()->mut_data());
 }
 
 void ControlStreamType::DeleteInstructionStatus(const Stream& stream,
@@ -92,7 +106,7 @@ void ControlStreamType::DeleteInstructionStatus(const Stream& stream,
 
 bool ControlStreamType::QueryInstructionStatusDone(
     const Stream& stream, const InstructionStatusBuffer& status_buffer) const {
-  UNIMPLEMENTED();
+  return NaiveInstrStatusQuerier::Cast(status_buffer.buffer().data())->done();
 }
 
 bool ControlStreamType::IsSourceOpcode(InstructionOpcode opcode) const {
