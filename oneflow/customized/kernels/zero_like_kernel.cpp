245dbe159cb4021013080ca2a8816f08c4017a3d
leaves-zwx <kunta0932@gmail.com>
2020-03-28 10:49:55 +0800

    zero_like don't need T

--- oneflow/customized/kernels/zero_like_kernel.cpp
+++ oneflow/customized/kernels/zero_like_kernel.cpp
@@ -3,7 +3,7 @@
 
 namespace oneflow {
 
-template<DeviceType device_type, typename T>
+template<DeviceType device_type>
 class ZeroLikeKernel final : public user_op::OpKernel {
  public:
   ZeroLikeKernel(user_op::KernelInitContext* ctx) : user_op::OpKernel(ctx) {}
@@ -13,26 +13,20 @@ class ZeroLikeKernel final : public user_op::OpKernel {
  private:
   void Compute(user_op::KernelContext* ctx) override {
     user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("out", 0);
-    Memset<device_type>(ctx->device_ctx(), out->mut_dptr<T>(), 0,
+    Memset<device_type>(ctx->device_ctx(), out->mut_dptr(), 0,
                         out->shape().elem_cnt() * GetSizeOfDataType(out->data_type()));
   };
 };
 
-#define REGISTER_ZERO_LIKE_KERNEL(device_type_v, data_type_pair)                         \
-  REGISTER_USER_KERNEL("zero_like")                                                      \
-      .SetCreateFn([](user_op::KernelInitContext* ctx) {                                 \
-        return new ZeroLikeKernel<device_type_v, OF_PP_PAIR_FIRST(data_type_pair)>(ctx); \
-      })                                                                                 \
-      .SetIsMatchedPred([](const oneflow::user_op::KernelRegContext& ctx) {              \
-        const user_op::TensorDesc* out_desc = ctx.TensorDesc4ArgNameAndIndex("out", 0);  \
-        if (ctx.device_type() == device_type_v                                           \
-            && out_desc->data_type() == OF_PP_PAIR_SECOND(data_type_pair)) {             \
-          return true;                                                                   \
-        }                                                                                \
-        return false;                                                                    \
+#define REGISTER_ZERO_LIKE_KERNEL(device_type_v)                                                  \
+  REGISTER_USER_KERNEL("zero_like")                                                               \
+      .SetCreateFn(                                                                               \
+          [](user_op::KernelInitContext* ctx) { return new ZeroLikeKernel<device_type_v>(ctx); }) \
+      .SetIsMatchedPred([](const oneflow::user_op::KernelRegContext& ctx) {                       \
+        return ctx.device_type() == device_type_v;                                                \
       });
 
-OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(REGISTER_ZERO_LIKE_KERNEL, DEVICE_TYPE_SEQ,
-                                 ARITHMETIC_DATA_TYPE_SEQ);
+REGISTER_ZERO_LIKE_KERNEL(DeviceType::kCPU)
+REGISTER_ZERO_LIKE_KERNEL(DeviceType::kGPU)
 
 }  // namespace oneflow
