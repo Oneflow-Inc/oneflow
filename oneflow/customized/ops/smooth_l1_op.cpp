ed935ab1a0c10c6cc74607d5423e25656a3a8c97
tsai <caishenghang@oneflow.org>
2020-03-28 11:03:54 +0800

    add grad op

--- oneflow/customized/ops/smooth_l1_op.cpp
+++ oneflow/customized/ops/smooth_l1_op.cpp
@@ -31,4 +31,39 @@ REGISTER_USER_OP("smooth_l1")
           .Build(ctx->sbp_sig_list()->mutable_sbp_signature()->Add());
       return Maybe<void>::Ok();
     });
+
+REGISTER_USER_OP("smooth_l1_grad")
+    .Input("dy")
+    .Input("x")
+    .Input("label")
+    .Output("dx")
+    .Attr("beta", UserOpAttrType::kAtFloat)
+    .Attr("scale", UserOpAttrType::kAtFloat)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      *ctx->Shape4ArgNameAndIndex("y", 0) = Shape({ctx->Shape4ArgNameAndIndex("x", 0)->At(0)});
+      CHECK_EQ_OR_RETURN(*ctx->Shape4ArgNameAndIndex("dy", 0), *ctx->Shape4ArgNameAndIndex("x", 0));
+      CHECK_EQ_OR_RETURN(*ctx->Dtype4ArgNameAndIndex("dy", 0), *ctx->Dtype4ArgNameAndIndex("x", 0));
+      CHECK_EQ_OR_RETURN(*ctx->Shape4ArgNameAndIndex("x", 0),
+                         *ctx->Shape4ArgNameAndIndex("label", 0));
+      CHECK_EQ_OR_RETURN(*ctx->Dtype4ArgNameAndIndex("x", 0),
+                         *ctx->Dtype4ArgNameAndIndex("label", 0));
+      CHECK_GE_OR_RETURN(ctx->GetAttr<float>("beta"), 0);
+      *ctx->Shape4ArgNameAndIndex("dx", 0) = *ctx->Shape4ArgNameAndIndex("dy", 0);
+      *ctx->Dtype4ArgNameAndIndex("dx", 0) = *ctx->Dtype4ArgNameAndIndex("dy", 0);
+      return Maybe<void>::Ok();
+    })
+    .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
+      *ctx->BatchAxis4ArgNameAndIndex("dx", 0) = *ctx->BatchAxis4ArgNameAndIndex("x", 0);
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      SbpSignatureBuilder()
+          .Split("dy", 0, 0)
+          .Split("x", 0, 0)
+          .Split("label", 0, 0)
+          .Split("dx", 0, 0)
+          .Build(ctx->sbp_sig_list()->mutable_sbp_signature()->Add());
+      return Maybe<void>::Ok();
+    });
+
 }  // namespace oneflow
