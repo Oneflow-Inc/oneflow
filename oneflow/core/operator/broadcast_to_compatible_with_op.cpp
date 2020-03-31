67b8f3f8e3dc3f44db9704672df894d17b18765f
leaves-zwx <kunta0932@gmail.com>
2020-03-31 22:19:52 +0800

    gen grad op

--- oneflow/core/operator/broadcast_to_compatible_with_op.cpp
+++ oneflow/core/operator/broadcast_to_compatible_with_op.cpp
@@ -1,5 +1,6 @@
 #include "oneflow/core/operator/operator.h"
 #include "oneflow/core/common/shape_view.h"
+#include "oneflow/core/job_completer/autograd.h"
 
 namespace oneflow {
 
@@ -18,6 +19,37 @@ Maybe<void> GetBroadcastShape(const Shape& a_shape, const Shape& b_shape, Shape*
   }
   *broadcast_shape = max_shape;
   return Maybe<void>::Ok();
+}
+
+void GenBroadcastToCompatibleWithGradOpConf(
+    const Operator& op, std::vector<OperatorConf>* op_confs,
+    const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp,
+    const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4BnInOp) {
+  CHECK(op.op_conf().has_broadcast_to_compatible_with_conf());
+  if (DiffLbi4BnInOp("x") != nullptr) {
+    const BlobDesc& x_desc = LogicalBlobDesc4BnInOp("x");
+    const BlobDesc& y_desc = LogicalBlobDesc4BnInOp("y");
+    Shape x_extend_shape = CreateLeftExtendedShape(x_desc.shape(), y_desc.shape().NumAxes());
+    std::vector<int32_t> reduced_axes;
+    FOR_RANGE(int64_t, i, 0, y_desc.shape().NumAxes()) {
+      if (x_extend_shape.At(i) == 1) {
+        reduced_axes.push_back(i);
+      } else {
+        CHECK_EQ(x_extend_shape.At(i), y_desc.shape().At(i));
+      }
+    }
+
+    OperatorConf reduce_sum_like_op;
+    reduce_sum_like_op.set_name("System-AutoGrad-" + op.op_name());
+    ReduceSumLikeOpConf* conf = reduce_sum_like_op.mutable_reduce_sum_like_conf();
+    conf->set_x(GenLogicalBlobName(*DiffLbi4BnInOp("y")));
+    conf->set_like(GenLogicalBlobName(op.BnInOp2Lbi("x")));
+    conf->set_y("y");
+    *conf->mutable_axis() = StdVec2PbRf(reduced_axes);
+    op_confs->push_back(reduce_sum_like_op);
+    DiffLbi4BnInOp("x")->set_op_name(reduce_sum_like_op.name());
+    DiffLbi4BnInOp("x")->set_blob_name(conf->y());
+  }
 }
 
 }  // namespace
@@ -86,5 +118,7 @@ class BroadcastToCompatibleWithOp final : public Operator {
 };
 
 REGISTER_OP(OperatorConf::kBroadcastToCompatibleWithConf, BroadcastToCompatibleWithOp);
+REGISTER_OP_GRAD(OperatorConf::kBroadcastToCompatibleWithConf,
+                 &GenBroadcastToCompatibleWithGradOpConf);
 
 }  // namespace oneflow
