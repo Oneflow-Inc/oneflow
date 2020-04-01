f3128f01fd558751facae631ae74c0a80d89d94a
Shenghang Tsai <jackalcooper@gmail.com>
2020-04-01 11:46:27 +0800

    mv files to single dir

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -1,13 +1,13 @@
 set -e
 set -x
-pwd=$1
+ci_tmp=$1
 
-chmod +x $pwd/oneflow_testexe
-$pwd/oneflow_testexe
+chmod +x $ci_tmp/oneflow_testexe
+$ci_tmp/oneflow_testexe
 
-pip3 install --user $pwd/*.whl
+pip3 install --user $ci_tmp/*.whl
 installed_path=$(python3 -c "import oneflow; print(oneflow.__path__[0])")
 test_dir="/test_dir"
-cp -r $pwd/oneflow/python/test $test_dir
+cp -r $ci_tmp/oneflow/python/test $test_dir
 python3 $test_dir/ops/1node_test.py
 python3 $test_dir/models/1node_test.py
