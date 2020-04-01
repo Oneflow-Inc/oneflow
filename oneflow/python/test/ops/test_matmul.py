50f3998ba3f98199c1f747279301aa4fe1975853
Shenghang Tsai <jackalcooper@gmail.com>
2020-04-01 16:45:34 +0800

    atol=1e-03

--- oneflow/python/test/ops/test_matmul.py
+++ oneflow/python/test/ops/test_matmul.py
@@ -59,7 +59,7 @@ def compare_with_tensorflow(device_type, a_shape, b_shape, transpose_a, transpos
     tf_a_diff = tape.gradient(tf_out, a, loss_diff)
     tf_b_diff = tape.gradient(tf_out, b, loss_diff)
 
-    assert np.allclose(of_out.ndarray(), tf_out.numpy(), atol=1e-04), (of_out.ndarray(), tf_out.numpy())
+    assert np.allclose(of_out.ndarray(), tf_out.numpy(), atol=1e-03), np.max(np.abs(of_out.ndarray() - tf_out.numpy()))
     assert np.allclose(np.load(os.path.join(GetSavePath(), "a_diff.npy")), tf_a_diff.numpy())
     assert np.allclose(np.load(os.path.join(GetSavePath(), "b_diff.npy")), tf_b_diff.numpy())
 
