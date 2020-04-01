9bc93d413750ab2aa9723732e58ce5acffa21ecd
Shenghang Tsai <jackalcooper@gmail.com>
2020-04-01 16:17:14 +0800

    --user

--- ci/test.sh
+++ ci/test.sh
@@ -4,7 +4,7 @@ set -x
 chmod +x ./ci_tmp/oneflow_testexe
 ./ci_tmp/oneflow_testexe
 
-pip3 install --user ci_tmp/*.whl
+pip3 install --user ci_tmp/*.whl --user
 test_dir="/test_dir"
 cp -r oneflow/python/test $test_dir
 python3 $test_dir/ops/1node_test.py
