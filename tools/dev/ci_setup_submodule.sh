a4460e3144b39c5f01084e42bde81ffc154478f2
Shenghang Tsai <jackalcooper@gmail.com>
2020-04-01 09:57:06 +0800

    fix python

--- tools/dev/ci_setup_submodule.sh
+++ tools/dev/ci_setup_submodule.sh
@@ -1,8 +1,5 @@
 set -x
 set -e
-git reset --hard
-git submodule deinit -f .
-rm -rf .git/modules/*
 python3 tools/dev/setup_submodule.py --oneflow_src_local_path=${ONEFLOW_CI_SRC_DIR}
 git submodule sync
 git submodule update --init --recursive
