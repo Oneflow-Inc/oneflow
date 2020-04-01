05b39d8ac6b08cd442106f036799e4dcad2c2d25
Shenghang Tsai <jackalcooper@gmail.com>
2020-04-01 14:42:37 +0800

    rm unnecessary

--- ci/test.sh
+++ ci/test.sh
@@ -1,13 +1,11 @@
 set -e
 set -x
-ci_tmp=$1
 
-chmod +x $ci_tmp/oneflow_testexe
-$ci_tmp/oneflow_testexe
+chmod +x ./ci_tmp/oneflow_testexe
+./ci_tmp/oneflow_testexe
 
-pip3 install --user $ci_tmp/*.whl
-installed_path=$(python3 -c "import oneflow; print(oneflow.__path__[0])")
+pip3 install --user ci_tmp/*.whl
 test_dir="/test_dir"
-cp -r $ci_tmp/oneflow/python/test $test_dir
+cp -r oneflow/python/test $test_dir
 python3 $test_dir/ops/1node_test.py
 python3 $test_dir/models/1node_test.py
