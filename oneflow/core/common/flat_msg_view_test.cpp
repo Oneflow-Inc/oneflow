3f1858d41f9508629c95572cc8f32e76fea84793
lixinqi <lixinqi0703106@163.com>
2020-04-02 19:44:52 +0800

    FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN
    Former-commit-id: 373ce69f70b8c953a070cc5aaacbb8dfac4ff5e2

--- oneflow/core/common/flat_msg_view_test.cpp
+++ oneflow/core/common/flat_msg_view_test.cpp
@@ -73,38 +73,140 @@ TEST(FlatMsgView, match_failed_vector) {
   ASSERT_TRUE(!view->template Match(&variant_list));
 }
 
-TEST(FlatMsgView, init) {
-  FlatMsg<VariantList> variant_list;
-  {
-    FlatMsgView<ViewFoo> mut_view(variant_list.Mutable()->mutable_foo());
-    mut_view->set_int32_value(30);
-    mut_view->set_int16_value(40);
-    mut_view->set_float_value(50.0);
-  }
-  {
-    FlatMsgView<ViewFoo> view;
-    ASSERT_TRUE(view->Match(variant_list.Mutable()->mutable_foo()));
-    ASSERT_EQ(view->int32_value(), 30);
-    ASSERT_EQ(view->int16_value(), 40);
-    ASSERT_EQ(view->float_value(), 50.0);
-  }
+// clang-format off
+FLAT_MSG_VIEW_BEGIN(RepeatedFoo);
+  FLAT_MSG_VIEW_DEFINE_PATTERN(int32_t, int32_value);
+  FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(int16_t, int16_value);
+  FLAT_MSG_VIEW_DEFINE_PATTERN(float, float_value);
+FLAT_MSG_VIEW_END(RepeatedFoo);
+// clang-format on
+
+TEST(FlatMsgView, repeated_empty) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(2);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_float_value(50.0);
+  FlatMsgView<RepeatedFoo> view;
+  ASSERT_TRUE(view->Match(&variant_list));
+  ASSERT_EQ(view->int16_value_size(), 0);
 }
 
-TEST(FlatMsgView, init_vector) {
-  std::vector<FlatMsg<VariantFoo>> variant_list;
-  {
-    FlatMsgView<ViewFoo> mut_view(&variant_list);
-    mut_view->set_int32_value(30);
-    mut_view->set_int16_value(40);
-    mut_view->set_float_value(50.0);
-  }
-  {
-    FlatMsgView<ViewFoo> view;
-    ASSERT_TRUE(view->Match(&variant_list));
-    ASSERT_EQ(view->int32_value(), 30);
-    ASSERT_EQ(view->int16_value(), 40);
-    ASSERT_EQ(view->float_value(), 50.0);
-  }
+TEST(FlatMsgView, repeated_empty_failed) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(2);
+  variant_list.at(0)->set_float_value(50.0);
+  variant_list.at(1)->set_int32_value(40);
+  FlatMsgView<RepeatedFoo> view;
+  ASSERT_TRUE(!view->Match(&variant_list));
+}
+
+TEST(FlatMsgView, repeated_one) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(3);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_int16_value(45);
+  variant_list.at(2)->set_float_value(50.0);
+  FlatMsgView<RepeatedFoo> view;
+  ASSERT_TRUE(view->Match(&variant_list));
+  ASSERT_EQ(view->int16_value_size(), 1);
+  ASSERT_EQ(view->int16_value(0), 45);
+}
+
+TEST(FlatMsgView, repeated_one_failed) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(3);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_float_value(50.0);
+  variant_list.at(2)->set_int16_value(45);
+  FlatMsgView<RepeatedFoo> view;
+  ASSERT_TRUE(!view->Match(&variant_list));
+}
+
+TEST(FlatMsgView, repeated_many) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(4);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_int16_value(45);
+  variant_list.at(2)->set_int16_value(45);
+  variant_list.at(3)->set_float_value(50.0);
+  FlatMsgView<RepeatedFoo> view;
+  ASSERT_TRUE(view->Match(&variant_list));
+  ASSERT_EQ(view->int16_value_size(), 2);
+  ASSERT_EQ(view->int16_value(0), 45);
+  ASSERT_EQ(view->int16_value(1), 45);
+}
+
+TEST(FlatMsgView, repeated_many_failed) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(4);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_int16_value(45);
+  variant_list.at(2)->set_float_value(45.0);
+  variant_list.at(3)->set_float_value(50.0);
+  FlatMsgView<RepeatedFoo> view;
+  ASSERT_TRUE(!view->Match(&variant_list));
+}
+
+// clang-format off
+FLAT_MSG_VIEW_BEGIN(LastFieldRepeatedFoo);
+  FLAT_MSG_VIEW_DEFINE_PATTERN(int32_t, int32_value);
+  FLAT_MSG_VIEW_DEFINE_PATTERN(float, float_value);
+  FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(int16_t, int16_value);
+FLAT_MSG_VIEW_END(LastFieldRepeatedFoo);
+// clang-format on
+
+TEST(FlatMsgView, last_field_repeated_empty) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(2);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_float_value(50.0);
+  FlatMsgView<LastFieldRepeatedFoo> view;
+  ASSERT_TRUE(view->Match(&variant_list));
+  ASSERT_EQ(view->int16_value_size(), 0);
+}
+
+TEST(FlatMsgView, last_field_repeated_empty_failed) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(2);
+  variant_list.at(0)->set_float_value(50.0);
+  variant_list.at(1)->set_int32_value(40);
+  FlatMsgView<LastFieldRepeatedFoo> view;
+  ASSERT_TRUE(!view->Match(&variant_list));
+}
+
+TEST(FlatMsgView, last_field_repeated_one) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(3);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_float_value(50.0);
+  variant_list.at(2)->set_int16_value(45);
+  FlatMsgView<LastFieldRepeatedFoo> view;
+  ASSERT_TRUE(view->Match(&variant_list));
+  ASSERT_EQ(view->int16_value_size(), 1);
+  ASSERT_EQ(view->int16_value(0), 45);
+}
+
+TEST(FlatMsgView, last_field_repeated_one_failed) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(3);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_int16_value(45);
+  variant_list.at(2)->set_float_value(50.0);
+  FlatMsgView<LastFieldRepeatedFoo> view;
+  ASSERT_TRUE(!view->Match(&variant_list));
+}
+
+TEST(FlatMsgView, last_field_repeated_many) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(4);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_float_value(50.0);
+  variant_list.at(2)->set_int16_value(45);
+  variant_list.at(3)->set_int16_value(45);
+  FlatMsgView<LastFieldRepeatedFoo> view;
+  ASSERT_TRUE(view->Match(&variant_list));
+  ASSERT_EQ(view->int16_value_size(), 2);
+  ASSERT_EQ(view->int16_value(0), 45);
+  ASSERT_EQ(view->int16_value(1), 45);
+}
+
+TEST(FlatMsgView, last_field_repeated_many_failed) {
+  std::vector<FlatMsg<VariantFoo>> variant_list(4);
+  variant_list.at(0)->set_int32_value(40);
+  variant_list.at(1)->set_int16_value(45);
+  variant_list.at(2)->set_float_value(50.0);
+  variant_list.at(3)->set_int16_value(45);
+  FlatMsgView<LastFieldRepeatedFoo> view;
+  ASSERT_TRUE(!view->Match(&variant_list));
 }
 
 }  // namespace
