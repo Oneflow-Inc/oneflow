df1cb26a28747d8c33f03fb211e0447f9da8c549
lixinqi <lixinqi0703106@163.com>
2020-04-07 18:21:38 +0800

    only user operators support eager mode

--- oneflow/core/eager/opkernel_instruction.msg.h
+++ oneflow/core/eager/opkernel_instruction.msg.h
@@ -23,17 +23,17 @@ FLAT_MSG_VIEW_BEGIN(CallOpKernelInstrOperand);
 
   FLAT_MSG_VIEW_DEFINE_PATTERN(vm::OperandSeparator, begin_ibn);
   FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(vm::ConstOperand, ibn);
-  FLAT_MSG_VIEW_DEFINE_PATTERN(vm::OperandSeparator, begin_input_blob);
+  FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(int64_t, input_index);
   FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(vm::ConstOperand, input_blob);
 
   FLAT_MSG_VIEW_DEFINE_PATTERN(vm::OperandSeparator, begin_obn);
   FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(vm::ConstOperand, obn);
-  FLAT_MSG_VIEW_DEFINE_PATTERN(vm::OperandSeparator, begin_output_blob);
+  FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(int64_t, output_index);
   FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(vm::MutableOperand, output_blob);
 
   FLAT_MSG_VIEW_DEFINE_PATTERN(vm::OperandSeparator, begin_mut2_obn);
   FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(vm::ConstOperand, mut2_obn);
-  FLAT_MSG_VIEW_DEFINE_PATTERN(vm::OperandSeparator, begin_mut2_output_blob);
+  FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(int64_t, mut2_output_index);
   FLAT_MSG_VIEW_DEFINE_REPEATED_PATTERN(vm::Mut2Operand, mut2_output_blob);
 FLAT_MSG_VIEW_END(CallOpKernelInstrOperand);
 // clang-format on
--- oneflow/core/operator/user_op.cpp
+++ oneflow/core/operator/user_op.cpp
@@ -1,6 +1,5 @@
-#include "oneflow/core/operator/operator.h"
+#include "oneflow/core/operator/user_op.h"
 #include "oneflow/core/operator/user_op_util.h"
-#include "oneflow/core/framework/op_registration.h"
 #include "oneflow/core/framework/kernel_registration.h"
 #include "oneflow/core/framework/tensor_desc.h"
 #include "oneflow/core/framework/infer_util.h"
@@ -28,37 +27,6 @@ user_op::TensorDesc GenTensorDescFromBlobDesc(const BlobDesc* blob_desc) {
 }
 
 }  // namespace
-
-class UserOp final : public Operator {
- public:
-  OF_DISALLOW_COPY_AND_MOVE(UserOp);
-  UserOp() = default;
-  ~UserOp() = default;
-
-  void InitFromOpConf() override;
-  const PbMessage& GetCustomizedConf() const override { return op_conf().user_conf(); }
-  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-                             const ParallelContext* parallel_ctx, const SbpSignature* sbp_signature,
-                             std::function<void(OpContext*)> EnrollOpCtx) const override;
-  Maybe<void> InferOutBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-                                const ParallelContext*, const SbpSignature* sbp_signature,
-                                std::function<void(OpContext*)> EnrollOpCtx) const override;
-
- private:
-  LogicalBlobId ibn2lbi(const std::string& input_bn) const override;
-  LogicalBlobId obn2lbi(const std::string& output_bn) const override;
-  Maybe<void> InferBatchAxis(
-      const std::function<const BlobDesc&(const std::string&)>& LogicalBlobDesc4Ibn,
-      std::function<OptInt64*(const std::string&)> BatchAxis4BnInOp) const override;
-  Maybe<void> GetSbpSignatures(
-      const std::function<Maybe<const BlobDesc*>(const std::string&)>& LogicalBlobDesc4Ibn,
-      const ParallelDesc& parallel_desc, SbpSignatureList* sbp_sig_list) const override;
-  void VirtualGenKernelConf(std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-                            const ParallelContext* parallel_ctx,
-                            KernelConf* kernel_conf) const override;
-
-  const user_op::OpRegistrationVal* val_;
-};
 
 class UserOpKernelRegContext final : public user_op::KernelRegContext {
  public:
