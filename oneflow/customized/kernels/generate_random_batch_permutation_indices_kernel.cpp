6627226242714a7b4b4257e44a05db2cec96fadd
Li Xinqi <lixinqi2010@gmail.com>
2020-04-08 13:15:19 +0800

    Refactor opkernel ctx (#2728)
    * rename KernelContext to KernelComputeContext
    
    * remove kernel_compute_context.h
    
    * OpKernelContext
    
    * rename api: InitOpKernelContext => NewOpKernelContext
    
    * refactor signature of user_op::OpKernel::CreateOpKernelState
    
    * rename opkernel_ctx to opkernel_state

--- oneflow/customized/kernels/generate_random_batch_permutation_indices_kernel.cpp
+++ oneflow/customized/kernels/generate_random_batch_permutation_indices_kernel.cpp
@@ -1,35 +1,32 @@
 #include <cstdint>
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
+#include "oneflow/customized/kernels/op_kernel_state_wrapper.h"
 
 namespace oneflow {
 
 class GenerateRandomBatchPermutationIndicesCPUKernel final : public user_op::OpKernel {
  public:
-  GenerateRandomBatchPermutationIndicesCPUKernel(user_op::KernelInitContext* ctx)
-      : user_op::OpKernel(ctx) {
-    int64_t seed = ctx->GetAttr<int64_t>("seed");
-    random_generator_.reset(new std::mt19937(seed));
-  }
-
   GenerateRandomBatchPermutationIndicesCPUKernel() = default;
   ~GenerateRandomBatchPermutationIndicesCPUKernel() = default;
 
  private:
-  void Compute(user_op::KernelContext* ctx) override {
+  std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
+      user_op::KernelInitContext* ctx) const override {
+    int64_t seed = ctx->GetAttr<int64_t>("seed");
+    return std::make_shared<OpKernelStateWrapper<std::mt19937>>(seed);
+  }
+  void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
+    auto* random_generator = dynamic_cast<OpKernelStateWrapper<std::mt19937>*>(state);
     user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
     std::iota(y->mut_dptr<int32_t>(), y->mut_dptr<int32_t>() + y->shape().elem_cnt(), 0);
     std::shuffle(y->mut_dptr<int32_t>(), y->mut_dptr<int32_t>() + y->shape().elem_cnt(),
-                 *random_generator_);
+                 *random_generator->Mutable());
   };
-
-  std::unique_ptr<std::mt19937> random_generator_;
 };
 
 REGISTER_USER_KERNEL("generate_random_batch_permutation_indices")
-    .SetCreateFn([](oneflow::user_op::KernelInitContext* ctx) {
-      return new GenerateRandomBatchPermutationIndicesCPUKernel(ctx);
-    })
+    .SetCreateFn<GenerateRandomBatchPermutationIndicesCPUKernel>()
     .SetIsMatchedPred([](const oneflow::user_op::KernelRegContext& ctx) {
       return ctx.device_type() == DeviceType::kCPU;
     });
