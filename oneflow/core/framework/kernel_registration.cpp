4ffe8d203e0bddbf42fe0e752720abfec2c832b3
lixinqi <lixinqi0703106@163.com>
2020-04-09 17:25:04 +0800

    IsStatefull4OpTypeName
    Former-commit-id: 9bb33be4eb51bb1aee13b6a950f221e0ed392977

--- oneflow/core/framework/kernel_registration.cpp
+++ oneflow/core/framework/kernel_registration.cpp
@@ -50,6 +50,20 @@ std::vector<std::string> GetAllUserOpInKernelRegistry() {
   return ret;
 }
 
+HashMap<std::string, bool>* IsStateless4OpTypeName() {
+  static HashMap<std::string, bool> op_type_name2is_stateless;
+  return &op_type_name2is_stateless;
+}
+
+bool IsStatefull4OpTypeName(const std::string& op_type_name) {
+  return IsStateless4OpTypeName()->at(op_type_name);
+}
+
+void CheckStatefulnessConsistency(const std::string& op_type_name, bool is_stateless) {
+  bool prev = IsStateless4OpTypeName()->emplace(op_type_name, is_stateless).first->second;
+  CHECK_EQ(prev, is_stateless);
+}
+
 KernelRegistryWrapperBuilder::KernelRegistryWrapperBuilder(const std::string& op_type_name) {
   wrapper_.op_type_name = op_type_name;
 }
