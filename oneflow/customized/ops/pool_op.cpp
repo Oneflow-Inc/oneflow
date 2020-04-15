fe513f4d895ea4b38c40273f012c72dc221d4391
tsai <caishenghang@oneflow.org>
2020-04-15 21:19:27 +0800

    use new if

--- oneflow/customized/ops/pool_op.cpp
+++ oneflow/customized/ops/pool_op.cpp
@@ -25,8 +25,9 @@ REGISTER_USER_OP("pool")
       for (int32_t stride_dim : strides) { CHECK_GT_OR_RETURN(stride_dim, 0); }
 
       const Params3D params_3d(dim, *x_shape, data_format, padding, pool_size, strides);
-      Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
-      *y_shape = params_3d.GetYShape();
+      user_op::TensorDesc* y_desc = ctx->TensorDesc4ArgNameAndIndex("y", 0);
+      *y_desc->mut_shape() = params_3d.GetYShape();
+      *y_desc->mut_data_type() = ctx->TensorDesc4ArgNameAndIndex("x", 0)->data_type();
       return Maybe<void>::Ok();
     })
     .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
@@ -45,9 +46,15 @@ REGISTER_USER_OP("pool")
 
 REGISTER_USER_OP("pool_grad")
     .Input("x")
+    .Input("y")
     .Input("dy")
     .Output("dx")
-    .Attr("alpha", UserOpAttrType::kAtFloat)
+    .Attr("dim", UserOpAttrType::kAtInt32)
+    .Attr("pooling_type", UserOpAttrType::kAtString)
+    .Attr("padding", UserOpAttrType::kAtString)
+    .Attr("data_format", UserOpAttrType::kAtString)
+    .Attr("pool_size", UserOpAttrType::kAtListInt32)
+    .Attr("strides", UserOpAttrType::kAtListInt32)
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
       const Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
       const Shape* dy_shape = ctx->Shape4ArgNameAndIndex("dy", 0);
@@ -64,12 +71,11 @@ REGISTER_USER_OP("pool_grad")
       const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("x", 0);
       SbpSignatureBuilder()
           .Split("x", 0, 0)
+          .Split("y", 0, 0)
           .Split("dy", 0, 0)
           .Split("dx", 0, 0)
           .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
           .Build(ctx->sbp_sig_list());
-      SbpSignatureBuilder().Broadcast("x", 0).PartialSum("dy", 0).PartialSum("dx", 0).Build(
-          ctx->sbp_sig_list()->mutable_sbp_signature()->Add());
       return Maybe<void>::Ok();
     });
 
@@ -77,12 +83,18 @@ REGISTER_USER_OP_GRAD("pool").SetGenBackwardOpConfFn([](const user_op::UserOpWra
                                                         user_op::AddOpFn AddOp) {
   if (op.NeedGenGradTensor4OpInput("x", 0)) {
     user_op::UserOpConfWrapperBuilder builder(op.op_name() + "_grad");
-    user_op::UserOpConfWrapper grad_op = builder.Op("pool_grad")
-                                             .Input("x", op.input("x", 0))
-                                             .Input("dy", op.GetGradTensorWithOpOutput("y", 0))
-                                             .Output("dx")
-                                             .Attr("alpha", op.attr<float>("alpha"))
-                                             .Build();
+    user_op::UserOpConfWrapper grad_op =
+        builder.Op("pool_grad")
+            .Input("x", op.input("x", 0))
+            .Input("y", op.input("y", 0))
+            .Input("dy", op.GetGradTensorWithOpOutput("y", 0))
+            .Output("dx")
+            .Attr("dim", op.attr<int32_t>("dim"))
+            .Attr("data_format", op.attr<std::string>("data_format"))
+            .Attr("padding", op.attr<std::string>("padding"))
+            .Attr("pool_size", op.attr<std::vector<int32_t>>("pool_size"))
+            .Attr("strides", op.attr<std::vector<int32_t>>("strides"))
+            .Build();
     op.BindGradTensorWithOpInput(grad_op.output("dx", 0), "x", 0);
     AddOp(grad_op);
   }
