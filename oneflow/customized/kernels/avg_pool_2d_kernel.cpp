106682ba3887f4281d8e7deda31c5bbb5ce69873
tsai <caishenghang@oneflow.org>
2020-04-16 19:44:28 +0800

    fix op

--- oneflow/customized/kernels/avg_pool_2d_kernel.cpp
+++ oneflow/customized/kernels/avg_pool_2d_kernel.cpp
@@ -10,14 +10,14 @@ class CPUAvgpool2dKernel final : public user_op::OpKernel {
   ~CPUAvgpool2dKernel() = default;
 
  private:
-  void Compute(user_op::KernelComputeContext* ctx) const override{
-      // add your code here
+  void Compute(user_op::KernelComputeContext* ctx) const override {
+    // add your code here
   };
 };
 
-#define REGISTER_CPU_AVG_POOL_2D_KERNEL(dtype)                                      \
-  REGISTER_USER_KERNEL("avg_pool_2d")                                               \
-      .SetCreateFn<CPUAvgpool2dKernel<dtype>>()                                     \
+#define REGISTER_CPU_AVG_POOL_2D_KERNEL(dtype)                           \
+  REGISTER_USER_KERNEL("avg_pool_2d")                                          \
+      .SetCreateFn<CPUAvgpool2dKernel<dtype>>()                              \
       .SetIsMatchedPred([](const user_op::KernelRegContext& ctx) {                  \
         const user_op::TensorDesc* y_desc = ctx.TensorDesc4ArgNameAndIndex("y", 0); \
         return ctx.device_type() == DeviceType::kCPU                                \
@@ -34,14 +34,14 @@ class CpuAvgpool2dGradKernel final : public user_op::OpKernel {
   ~CpuAvgpool2dGradKernel() = default;
 
  private:
-  void Compute(user_op::KernelComputeContext* ctx) const override{
-      // add your code
+  void Compute(user_op::KernelComputeContext* ctx) const override {
+    // add your code
   };
 };
 
-#define REGISTER_CPU_AVG_POOL_2D_GRAD_KERNEL(dtype)                                   \
-  REGISTER_USER_KERNEL("avg_pool_2d_grad")                                            \
-      .SetCreateFn<CpuAvgpool2dGradKernel<dtype>>()                                   \
+#define REGISTER_CPU_AVG_POOL_2D_GRAD_KERNEL(dtype)                             \
+  REGISTER_USER_KERNEL("avg_pool_2d_grad")                                       \
+      .SetCreateFn<CpuAvgpool2dGradKernel<dtype>>()                            \
       .SetIsMatchedPred([](const user_op::KernelRegContext& ctx) {                    \
         const user_op::TensorDesc* dx_desc = ctx.TensorDesc4ArgNameAndIndex("dx", 0); \
         return ctx.device_type() == DeviceType::kCPU                                  \
--- oneflow/customized/ops/max_pool_3d_op.cpp
+++ oneflow/customized/ops/max_pool_3d_op.cpp
@@ -6,11 +6,27 @@ namespace oneflow {
 REGISTER_USER_OP("max_pool_3d")
     .Input("x")
     .Output("y")
-    .Attr("alpha", UserOpAttrType::kAtFloat)
+    .Attr("padding", UserOpAttrType::kAtString)
+    .Attr("data_format", UserOpAttrType::kAtString)
+    .Attr("pool_size", UserOpAttrType::kAtListInt32)
+    .Attr("strides", UserOpAttrType::kAtListInt32)
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
       const Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
-      Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
-      *y_shape = *x_shape;
+      const int32_t dim = 3;
+      const std::string data_format = ctx->GetAttr<std::string>("data_format");
+      const std::string padding = ctx->GetAttr<std::string>("padding");
+      const std::vector<int32_t> pool_size = ctx->GetAttr<std::vector<int32_t>>("pool_size");
+      const std::vector<int32_t> strides = ctx->GetAttr<std::vector<int32_t>>("strides");
+
+      CHECK_EQ_OR_RETURN(pool_size.size(), dim);
+      for (int32_t pool_dim : pool_size) { CHECK_GT_OR_RETURN(pool_dim, 0); }
+      CHECK_EQ_OR_RETURN(strides.size(), dim);
+      for (int32_t stride_dim : strides) { CHECK_GT_OR_RETURN(stride_dim, 0); }
+
+      const Params3D params_3d(dim, *x_shape, data_format, padding, pool_size, strides);
+      user_op::TensorDesc* y_desc = ctx->TensorDesc4ArgNameAndIndex("y", 0);
+      *y_desc->mut_shape() = params_3d.GetYShape();
+      *y_desc->mut_data_type() = ctx->TensorDesc4ArgNameAndIndex("x", 0)->data_type();
       return Maybe<void>::Ok();
     })
     .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
@@ -29,15 +45,15 @@ REGISTER_USER_OP("max_pool_3d")
 
 REGISTER_USER_OP("max_pool_3d_grad")
     .Input("x")
+    .Input("y")
     .Input("dy")
     .Output("dx")
-    .Attr("alpha", UserOpAttrType::kAtFloat)
+    .Attr("padding", UserOpAttrType::kAtString)
+    .Attr("data_format", UserOpAttrType::kAtString)
+    .Attr("pool_size", UserOpAttrType::kAtListInt32)
+    .Attr("strides", UserOpAttrType::kAtListInt32)
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      const Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
-      const Shape* dy_shape = ctx->Shape4ArgNameAndIndex("dy", 0);
-      Shape* dx_shape = ctx->Shape4ArgNameAndIndex("dx", 0);
-      CHECK(*dy_shape == *x_shape);
-      *dx_shape = *dy_shape;
+      *ctx->TensorDesc4ArgNameAndIndex("dx", 0) = *ctx->TensorDesc4ArgNameAndIndex("x", 0);
       return Maybe<void>::Ok();
     })
     .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
@@ -48,6 +64,7 @@ REGISTER_USER_OP("max_pool_3d_grad")
       const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("x", 0);
       SbpSignatureBuilder()
           .Split("x", 0, 0)
+          .Split("y", 0, 0)
           .Split("dy", 0, 0)
           .Split("dx", 0, 0)
           .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
@@ -62,10 +79,14 @@ REGISTER_USER_OP_GRAD("max_pool_3d")
       if (op.NeedGenGradTensor4OpInput("x", 0)) {
         user_op::UserOpConfWrapperBuilder builder(op.op_name() + "_grad");
         user_op::UserOpConfWrapper grad_op = builder.Op("max_pool_3d_grad")
-                                                 .Input("x", op.input("x", 0))
-                                                 .Input("dy", op.GetGradTensorWithOpOutput("y", 0))
-                                                 .Output("dx")
-                                                 .Attr("alpha", op.attr<float>("alpha"))
+            .Input("x", op.input("x", 0))
+            .Input("y", op.output("y", 0))
+            .Input("dy", op.GetGradTensorWithOpOutput("y", 0))
+            .Output("dx")
+            .Attr("data_format", op.attr<std::string>("data_format"))
+            .Attr("padding", op.attr<std::string>("padding"))
+            .Attr("pool_size", op.attr<std::vector<int32_t>>("pool_size"))
+            .Attr("strides", op.attr<std::vector<int32_t>>("strides"))
                                                  .Build();
         op.BindGradTensorWithOpInput(grad_op.output("dx", 0), "x", 0);
         AddOp(grad_op);
