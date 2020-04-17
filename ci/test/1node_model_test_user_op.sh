ddf51497b519de1a7027c70743b7fdff31ee1e7f
Shenghang Tsai <jackalcooper@gmail.com>
2020-04-17 11:02:55 +0800

    support user op in ci

---
+++ ci/test/1node_model_test_user_op.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -xe
+
+chmod +x ./ci_tmp/oneflow_testexe
+./ci_tmp/oneflow_testexe
+
+pip3 install --user ci_tmp/*.whl
+
+cp -r oneflow/python/test /test_dir
+cd /test_dir
+export ENABLE_USER_OP=True
+python3 models/1node_test.py
