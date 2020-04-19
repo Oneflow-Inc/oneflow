a3d0439ee311ca743fe313a435df9dd850cd0360
ShawnXuan <xiexuanx2@gmail.com>
2020-04-20 00:14:47 +0800

    modify InferTmpSize

--- oneflow/customized/kernels/softmax_kernel_util.h
+++ oneflow/customized/kernels/softmax_kernel_util.h
@@ -10,7 +10,7 @@ struct SoftmaxKernelUtil {
   void ComputeProb(DeviceCtx* ctx, const int64_t n, const int64_t w, const T* in, T* tmp, T* prob,
                    void* temp_storage, const size_t temp_storage_bytes);
   void ComputeDiff(DeviceCtx* ctx, const int64_t n, const int64_t w, const T* dy, const T* out,
-                        T* sum_vec, T* dx, void* temp_storage, const size_t temp_storage_bytes);
+                   T* sum_vec, T* dx, void* temp_storage, const size_t temp_storage_bytes);
 };
 // template<DeviceType device_type, typename T>
 // void SoftmaxComputeProb1(DeviceCtx* ctx, const int64_t n, const int64_t w, const T* in, T* tmp,
