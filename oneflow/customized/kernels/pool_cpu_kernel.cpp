44a4a344a4c9cb56a49664928fc09030f3a131d7
tsai <caishenghang@oneflow.org>
2020-04-20 19:22:31 +0800

    extract method

--- oneflow/customized/kernels/pool_cpu_kernel.cpp
+++ oneflow/customized/kernels/pool_cpu_kernel.cpp
@@ -100,43 +100,11 @@ class CpuAvgPool2DGradKernel final : public user_op::OpKernel {
  private:
   std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
       user_op::KernelInitContext* ctx) const override {
-    const Shape& x_shape = ctx->TensorDesc4ArgNameAndIndex("x", 0)->shape();
-    const int32_t dim = 2;
-    const std::string data_format = ctx->GetAttr<std::string>("data_format");
-    const std::string padding = ctx->GetAttr<std::string>("padding");
-    const std::vector<int32_t>& pool_size = ctx->GetAttr<std::vector<int32_t>>("pool_size");
-    const std::vector<int32_t>& strides = ctx->GetAttr<std::vector<int32_t>>("strides");
-    return std::make_shared<OpKernelStateWrapper<Params3D>>(dim, x_shape, data_format, padding,
-                                                            pool_size, strides);
+    return PoolKernelUtil<T>::CreateOpKernelState(ctx, 2);
   }
 
   void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
-    const user_op::Tensor* dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
-    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
-    const user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
-    user_op::Tensor* dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
-    // TODO: tsai: reset op kernel state when is_dynamic if ready
-    const OpKernelStateWrapper<Params3D>* params_3d =
-        dynamic_cast<OpKernelStateWrapper<Params3D>*>(state);
-    CHECK(params_3d != nullptr);
-    const std::string data_format = ctx->GetAttr<std::string>("data_format");
-    if (data_format == "channels_first") {
-      PoolKernelUtil<T>::CFirstBackward(
-          params_3d->Get(), dy, y, x, dx,
-          [](const T& in, const T& out, const T& out_diff, const int64_t size, T& in_diff) {
-            in_diff += (out_diff / static_cast<T>(size));
-          });
-    } else if (data_format == "channels_last") {
-      PoolKernelUtil<T>::CLastBackward(
-          params_3d->Get(), dy, y, x, dx,
-          [](const int64_t out_col, const int64_t in_col, const int64_t size,
-             ConstEigenArrayMap<T>& out_arr, ConstEigenArrayMap<T>& in_arr,
-             ConstEigenArrayMap<T>& out_diff_arr, EigenArrayMap<T>& in_diff_arr) {
-            in_diff_arr.col(in_col) += out_diff_arr.col(out_col) / static_cast<T>(size);
-          });
-    } else {
-      UNIMPLEMENTED();
-    }
+    PoolKernelUtil<T>::CPUAvgBWCompute(ctx, state);
   };
 };
 
@@ -161,14 +129,7 @@ class CPUAvgPool3DKernel final : public user_op::OpKernel {
  private:
   std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
       user_op::KernelInitContext* ctx) const override {
-    const Shape& x_shape = ctx->TensorDesc4ArgNameAndIndex("x", 0)->shape();
-    const int32_t dim = 3;
-    const std::string data_format = ctx->GetAttr<std::string>("data_format");
-    const std::string padding = ctx->GetAttr<std::string>("padding");
-    const std::vector<int32_t>& pool_size = ctx->GetAttr<std::vector<int32_t>>("pool_size");
-    const std::vector<int32_t>& strides = ctx->GetAttr<std::vector<int32_t>>("strides");
-    return std::make_shared<OpKernelStateWrapper<Params3D>>(dim, x_shape, data_format, padding,
-                                                            pool_size, strides);
+    return PoolKernelUtil<T>::CreateOpKernelState(ctx, 3);
   }
 
   void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
@@ -218,14 +179,7 @@ class CpuAvgPool3DGradKernel final : public user_op::OpKernel {
  private:
   std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
       user_op::KernelInitContext* ctx) const override {
-    const Shape& x_shape = ctx->TensorDesc4ArgNameAndIndex("x", 0)->shape();
-    const int32_t dim = 3;
-    const std::string data_format = ctx->GetAttr<std::string>("data_format");
-    const std::string padding = ctx->GetAttr<std::string>("padding");
-    const std::vector<int32_t>& pool_size = ctx->GetAttr<std::vector<int32_t>>("pool_size");
-    const std::vector<int32_t>& strides = ctx->GetAttr<std::vector<int32_t>>("strides");
-    return std::make_shared<OpKernelStateWrapper<Params3D>>(dim, x_shape, data_format, padding,
-                                                            pool_size, strides);
+    return PoolKernelUtil<T>::CreateOpKernelState(ctx, 3);
   }
 
   void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
