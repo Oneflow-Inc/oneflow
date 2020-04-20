e53660039f1bf325e1d6de0042bd21d49afa6262
tsai <caishenghang@oneflow.org>
2020-04-20 22:45:42 +0800

    refactor gpu 1

--- oneflow/customized/kernels/pool_gpu_kernel.cpp
+++ oneflow/customized/kernels/pool_gpu_kernel.cpp
@@ -90,6 +90,40 @@ std::function<bool(const user_op::KernelRegContext& ctx)> MakeIsMatchedPred(
     return ctx.device_type() == device_type && y_desc->data_type() == GetDataType<dtype>::value;
   };
 }
+
+template<typename T>
+struct PoolGpuKernelUtil {
+  static void FWCompute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) {
+    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
+    // TODO: tsai: reset op kernel state when is_dynamic if ready
+    const OpKernelStateWrapper<GPUPoolOpKernelState>* gpu_pool_op_kernel_state =
+        dynamic_cast<OpKernelStateWrapper<GPUPoolOpKernelState>*>(state);
+    CHECK(gpu_pool_op_kernel_state != nullptr);
+    CudaCheck(cudnnPoolingForward(
+        ctx->device_ctx()->cudnn_handle(), gpu_pool_op_kernel_state->Get().cudnn_pooling_desc(),
+        CudnnSPOnePtr<T>(), gpu_pool_op_kernel_state->Get().cudnn_x_tensor_desc(), x->dptr(),
+        CudnnSPZeroPtr<T>(), gpu_pool_op_kernel_state->Get().cudnn_y_tensor_desc(), y->mut_dptr()));
+  }
+
+  static void BWCompute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) {
+    const user_op::Tensor* dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
+    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    const user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
+    user_op::Tensor* dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
+    // TODO: tsai: reset op kernel state when is_dynamic if ready
+    const OpKernelStateWrapper<GPUPoolOpKernelState>* gpu_pool_op_kernel_state =
+        dynamic_cast<OpKernelStateWrapper<GPUPoolOpKernelState>*>(state);
+    CHECK(gpu_pool_op_kernel_state != nullptr);
+    CudaCheck(cudnnPoolingBackward(
+        ctx->device_ctx()->cudnn_handle(), gpu_pool_op_kernel_state->Get().cudnn_pooling_desc(),
+        CudnnSPOnePtr<T>(), gpu_pool_op_kernel_state->Get().cudnn_y_tensor_desc(), y->dptr(),
+        gpu_pool_op_kernel_state->Get().cudnn_y_tensor_desc(), dy->dptr(),
+        gpu_pool_op_kernel_state->Get().cudnn_x_tensor_desc(), x->dptr(), CudnnSPZeroPtr<T>(),
+        gpu_pool_op_kernel_state->Get().cudnn_x_tensor_desc(), dx->mut_dptr()));
+  }
+};
+
 }  // namespace
 
 template<typename T>
@@ -161,16 +195,7 @@ class AvgPool2DGpuKernel final : public user_op::OpKernel {
   }
 
   void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
-    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
-    user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
-    // TODO: tsai: reset op kernel state when is_dynamic if ready
-    const OpKernelStateWrapper<GPUPoolOpKernelState>* gpu_pool_op_kernel_state =
-        dynamic_cast<OpKernelStateWrapper<GPUPoolOpKernelState>*>(state);
-    CHECK(gpu_pool_op_kernel_state != nullptr);
-    CudaCheck(cudnnPoolingForward(
-        ctx->device_ctx()->cudnn_handle(), gpu_pool_op_kernel_state->Get().cudnn_pooling_desc(),
-        CudnnSPOnePtr<T>(), gpu_pool_op_kernel_state->Get().cudnn_x_tensor_desc(), x->dptr(),
-        CudnnSPZeroPtr<T>(), gpu_pool_op_kernel_state->Get().cudnn_y_tensor_desc(), y->mut_dptr()));
+    PoolGpuKernelUtil<T>::FWCompute(ctx, state);
   };
 };
 
@@ -187,20 +212,7 @@ class AvgPool2DGradGpuKernel final : public user_op::OpKernel {
   }
 
   void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
-    const user_op::Tensor* dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
-    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
-    const user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
-    user_op::Tensor* dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
-    // TODO: tsai: reset op kernel state when is_dynamic if ready
-    const OpKernelStateWrapper<GPUPoolOpKernelState>* gpu_pool_op_kernel_state =
-        dynamic_cast<OpKernelStateWrapper<GPUPoolOpKernelState>*>(state);
-    CHECK(gpu_pool_op_kernel_state != nullptr);
-    CudaCheck(cudnnPoolingBackward(
-        ctx->device_ctx()->cudnn_handle(), gpu_pool_op_kernel_state->Get().cudnn_pooling_desc(),
-        CudnnSPOnePtr<T>(), gpu_pool_op_kernel_state->Get().cudnn_y_tensor_desc(), y->dptr(),
-        gpu_pool_op_kernel_state->Get().cudnn_y_tensor_desc(), dy->dptr(),
-        gpu_pool_op_kernel_state->Get().cudnn_x_tensor_desc(), x->dptr(), CudnnSPZeroPtr<T>(),
-        gpu_pool_op_kernel_state->Get().cudnn_x_tensor_desc(), dx->mut_dptr()));
+    PoolGpuKernelUtil<T>::BWCompute(ctx, state);
   };
 };
 
