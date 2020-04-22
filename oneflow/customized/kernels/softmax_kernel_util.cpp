e7562ab8e45fd704f443a4f9bfce9961372341d0
ShawnXuan <xiexuanx2@gmail.com>
2020-04-23 00:19:17 +0800

    rm useless lines

--- oneflow/customized/kernels/softmax_kernel_util.cpp
+++ oneflow/customized/kernels/softmax_kernel_util.cpp
@@ -4,30 +4,6 @@
 
 namespace oneflow {
 
-// template<DeviceType device_type, typename T>
-// void SoftmaxComputeProb1(DeviceCtx* ctx, const int64_t n, const int64_t w, const T* in, T* tmp,
-//                         T* prob, void* temp_storage, const size_t temp_storage_bytes) {
-//   auto Val = NdarrayUtil<device_type, T>::GetValNdarrayBuilder();
-//   auto Var = NdarrayUtil<device_type, T>::GetVarNdarrayBuilder();
-//   // max | tmp[i] = Max_j(in[i][j])
-//   NdarrayUtil<device_type, T>::ReduceMax(ctx, Var({n, 1}, tmp), Val({n, w}, in),
-//                                          Var({static_cast<int64_t>(temp_storage_bytes /
-//                                          sizeof(T))},
-//                                              reinterpret_cast<T*>(temp_storage)));
-//   // sub | prob[i][j] = in[i][j] - tmp[i]
-//   NdarrayUtil<device_type, T>::BroadcastSub(ctx, Var({n, w}, prob), Val({n, w}, in),
-//                                             Val({n, 1}, tmp));
-//   // exp | prob[i][j] = exp(prob[i][j])
-//   NdarrayUtil<device_type, T>::InplaceExp(ctx, Var({n, w}, prob));
-//   // sum | tmp[i] = Sum_j(prob[i][j])
-//   NdarrayUtil<device_type, T>::ReduceSum(ctx, Var({n, 1}, tmp), Val({n, w}, prob),
-//                                          Var({static_cast<int64_t>(temp_storage_bytes /
-//                                          sizeof(T))},
-//                                              reinterpret_cast<T*>(temp_storage)));
-//   // div | prob[i][j] /= tmp[i]
-//   NdarrayUtil<device_type, T>::InplaceBroadcastDiv(ctx, Var({n, w}, prob), Val({n, 1}, tmp));
-// }
-
 template<DeviceType device_type, typename T>
 void SoftmaxKernelUtil<device_type, T>::ComputeProb(DeviceCtx* ctx, const int64_t n,
                                                     const int64_t w, const T* in, T* tmp, T* prob,
