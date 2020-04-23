1deb390a516aa860a2c158cb7f1c4cb721d05c36
ShawnXuan <xiexuanx2@gmail.com>
2020-04-23 15:59:39 +0800

    modify sbp

--- oneflow/customized/ops/softmax_op.cpp
+++ oneflow/customized/ops/softmax_op.cpp
@@ -51,8 +51,6 @@ REGISTER_USER_OP("softmax_grad")
           .Split("dx", 0, 0)
           .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
           .Build(ctx->sbp_sig_list());
-      SbpSignatureBuilder().Broadcast("y", 0).PartialSum("dy", 0).PartialSum("dx", 0).Build(
-          ctx->sbp_sig_list()->mutable_sbp_signature()->Add());
       return Maybe<void>::Ok();
     });
 
