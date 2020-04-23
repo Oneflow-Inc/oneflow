086aff70c6fd51646bcc0f86c03f14cf88eeabfd
fujiaqing <1129973724@qq.com>
2020-04-23 16:33:19 +0800

    fix x,y conflict

--- oneflow/customized/kernels/math_binary_kernels.cu
+++ oneflow/customized/kernels/math_binary_kernels.cu
@@ -20,14 +20,14 @@ __device__ float PowCalYDiff4GpuFloat(float x, float y, float dz) {
   }
 }
 
-__device__ float Atan24GpuFloat(float x1, float x2) { return atan2(x1, x2); }
+__device__ float Atan24GpuFloat(float x, float y) { return atan2(x, y); }
 
-__device__ float Atan2CalX2Diff4GpuFloat(float x1, float x2, float dz) {
-  return dz * -x1 / (x2 * x2 + x1 * x1);
+__device__ float Atan2CalYDiff4GpuFloat(float x, float y, float dz) {
+  return dz * -x / (y * y + x * x);
 }
 
-__device__ float Atan2CalX1Diff4GpuFloat(float x1, float x2, float dz) {
-  return dz * (x2 / (x1 * x1 + x2 * x2));
+__device__ float Atan2CalXDiff4GpuFloat(float x, float y, float dz) {
+  return dz * (y / (x * x + y * y));
 }
 
 __device__ float Xdivy4GpuFloat(float x, float y) {
@@ -127,7 +127,7 @@ __device__ float FloordivCalYDiff4GpuFloat(float x, float y, float dz) { return 
   OF_PP_MAKE_TUPLE_SEQ("Xlogy", Xlogy)
 
 MATH_BINARY_GPU(Pow, powf, PowCalXDiff4GpuFloat, PowCalYDiff4GpuFloat, float);
-MATH_BINARY_GPU(Atan2, Atan24GpuFloat, Atan2CalX1Diff4GpuFloat, Atan2CalX2Diff4GpuFloat, float);
+MATH_BINARY_GPU(Atan2, Atan24GpuFloat, Atan2CalXDiff4GpuFloat, Atan2CalYDiff4GpuFloat, float);
 MATH_BINARY_GPU(Floordiv, FloordivFuc, FloordivCalXDiff4GpuFloat, FloordivCalYDiff4GpuFloat, float);
 MATH_BINARY_GPU(Xdivy, Xdivy4GpuFloat, XdivyCalXDiff4GpuFloat, XdivyCalYDiff4GpuFloat, float);
 MATH_BINARY_GPU(Xlogy, Xlogy4GpuFloat, XlogyCalXDiff4GpuFloat, XlogyCalYDiff4GpuFloat, float);
