4b2542fe7b56d491df4c5d1b4f8e7d6e7456278b
ShawnXuan <xiexuanx2@gmail.com>
2020-04-23 16:01:54 +0800

    debug

--- oneflow/customized/kernels/softmax_kernel_util.cpp
+++ oneflow/customized/kernels/softmax_kernel_util.cpp
@@ -51,7 +51,6 @@ void SoftmaxKernelUtil<device_type, T>::ComputeDiff(DeviceCtx* ctx, const int64_
 
 #define INSTANTIATE_SOFTMAX_KERNEL_UTIL(device_type, data_type) \
   template struct SoftmaxKernelUtil<device_type, data_type>;
-INSTANTIATE_SOFTMAX_KERNEL_UTIL(DeviceType::kGPU, float16)
 INSTANTIATE_SOFTMAX_KERNEL_UTIL(DeviceType::kGPU, float)
 INSTANTIATE_SOFTMAX_KERNEL_UTIL(DeviceType::kGPU, double)
 INSTANTIATE_SOFTMAX_KERNEL_UTIL(DeviceType::kCPU, float)
