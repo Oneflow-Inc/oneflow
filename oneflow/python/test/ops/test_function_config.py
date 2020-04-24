c77d7acdc1c406ead698ed215d8efd7d1c464bf7
lixinqi <lixinqi0703106@163.com>
2020-04-24 12:33:35 +0800

    oneflow.current_global_function_desc

--- oneflow/python/test/ops/test_function_config.py
+++ oneflow/python/test/ops/test_function_config.py
@@ -20,3 +20,12 @@ def test_config_setter_getter(test_case):
     test_case.assertEqual(func_config.function_desc.enable_eager_execution, False)
     func_config.enable_eager_execution()
     test_case.assertEqual(func_config.function_desc.enable_eager_execution, True)
+
+def test_global_function_desc(test_case):
+    func_config = flow.FunctionConfig()
+    func_config.default_placement_scope(flow.fixed_placement("cpu", "0:0"))
+    @flow.function(func_config)
+    def Foo():
+        test_case.assertEqual(flow.current_global_function_desc().IsTrainable(), False)
+        return flow.get_variable("w", (10,), initializer=flow.constant_initializer(1))
+    Foo().get()
