f4d37e28a2367d733b33a12759a7e11ec099505a
james plur <jamesplur7@gmail.com>
2020-04-24 11:32:18 +0800

    fix state

--- oneflow/customized/kernels/pad_kernel.cpp
+++ oneflow/customized/kernels/pad_kernel.cpp
@@ -107,9 +107,7 @@ REGISTER_PAD_KERNEL(DeviceType::kCPU, int8_t)
 template<DeviceType device_type, typename T>
 class PadGradKernel final : public user_op::OpKernel {
  public:
-  PadGradKernel() {
-    device_memory_copier_ = std::unique_ptr<MemoryCopier>(NewDefaultMemoryCopier(device_type));
-  }
+  PadGradKernel() = default;
   ~PadGradKernel() = default;
 
  private:
@@ -137,11 +135,11 @@ class PadGradKernel final : public user_op::OpKernel {
     memory_copy_nd_desc.src_pos = NdIndex(src_pos_vec);
     memory_copy_nd_desc.extent = memory_copy_nd_desc.dst_shape;
     MemoryCopyNdDesc reduced_memory_copy_nd_desc = memory_copy_nd_desc.CreateDimReducedDesc();
-    device_memory_copier_->Copy(ctx->device_ctx(), dx->mut_dptr<T>(), dy->dptr<T>(),
+
+    std::unique_ptr<MemoryCopier> device_memory_copier(NewDefaultMemoryCopier(device_type));
+    device_memory_copier->Copy(ctx->device_ctx(), dx->mut_dptr<T>(), dy->dptr<T>(),
                                 reduced_memory_copy_nd_desc);
   };
-
-  std::unique_ptr<MemoryCopier> device_memory_copier_;
 };
 
 #define REGISTER_PAD_GRAD_KERNEL(dev, dtype)                                                  \
