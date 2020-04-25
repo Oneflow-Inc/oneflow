a726929298e0b4fc17de0269501da53865e956c5
tsai <caishenghang@oneflow.org>
2020-04-25 15:17:38 +0800

    fix

--- oneflow/customized/ops/layer_norm_op.cpp
+++ oneflow/customized/ops/layer_norm_op.cpp
@@ -179,7 +179,7 @@ REGISTER_USER_OP("layer_norm_param_grad")
     .Attr("begin_params_axis", UserOpAttrType::kAtInt64)
     .Attr("has_gamma", UserOpAttrType::kAtBool)
     .Attr("has_beta_diff", UserOpAttrType::kAtBool)
-    .Attr("has_beta_diff", UserOpAttrType::kAtBool)
+    .Attr("has_normalized_diff", UserOpAttrType::kAtBool)
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
       const user_op::TensorDesc* dy = ctx->TensorDesc4ArgNameAndIndex("dy", 0);
       int64_t begin_params_axis = ctx->GetAttr<int64_t>("begin_params_axis");
