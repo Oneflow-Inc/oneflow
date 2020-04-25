92e266604cbdd76db4994c91cc8228b1b7b9d224
leaves-zwx <kunta0932@gmail.com>
2020-04-25 15:27:41 +0800

    op & kernel

---
+++ oneflow/customized/kernels/coco_reader_kernel.cpp
@@ -0,0 +1,58 @@
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/customized/data/coco_data_reader.h"
+
+namespace oneflow {
+
+namespace {
+
+class COCOReaderWrapper final : public user_op::OpKernelState {
+ public:
+  explicit COCOReaderWrapper(user_op::KernelInitContext* ctx) : reader_(ctx) {}
+  ~COCOReaderWrapper() = default;
+
+  void Read(user_op::KernelComputeContext* ctx) { reader_.Read(ctx); }
+
+ private:
+  COCODataReader reader_;
+};
+
+}  // namespace
+
+class COCOReaderKernel final : public user_op::OpKernel {
+ public:
+  COCOReaderKernel() = default;
+  ~COCOReaderKernel() override = default;
+
+ private:
+  std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
+      user_op::KernelInitContext* ctx) const override {
+    std::shared_ptr<user_op::OpKernelState> reader(new COCOReaderWrapper(ctx));
+    return reader;
+  }
+
+  void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
+    auto* reader = dynamic_cast<COCOReaderWrapper*>(state);
+    reader->Read(ctx);
+  }
+};
+
+REGISTER_USER_KERNEL("COCOReader")
+    .SetCreateFn<COCOReaderKernel>()
+    .SetIsMatchedPred([](const user_op::KernelRegContext& ctx) {
+      const user_op::TensorDesc* image_desc = ctx.TensorDesc4ArgNameAndIndex("image", 0);
+      const user_op::TensorDesc* image_size_desc = ctx.TensorDesc4ArgNameAndIndex("image_size", 0);
+      const user_op::TensorDesc* bbox_desc = ctx.TensorDesc4ArgNameAndIndex("gt_bbox", 0);
+      const user_op::TensorDesc* label_desc = ctx.TensorDesc4ArgNameAndIndex("gt_label", 0);
+      const user_op::TensorDesc* segm_desc = ctx.TensorDesc4ArgNameAndIndex("gt_segm", 0);
+      const user_op::TensorDesc* segm_offset_desc =
+          ctx.TensorDesc4ArgNameAndIndex("gt_segm_offset", 0);
+      return ctx.device_type() == DeviceType::kCPU
+             && image_desc->data_type() == DataType::kTensorBuffer
+             && image_size_desc->data_type() == DataType::kInt32
+             && bbox_desc->data_type() == DataType::kTensorBuffer
+             && label_desc->data_type() == DataType::kTensorBuffer
+             && segm_desc->data_type() == DataType::kTensorBuffer
+             && segm_offset_desc->data_type() == DataType::kTensorBuffer;
+    });
+
+}  // namespace oneflow
---
+++ oneflow/customized/ops/coco_reader_op.cpp
@@ -0,0 +1,78 @@
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/core/job/sbp_parallel.h"
+
+namespace oneflow {
+
+REGISTER_USER_OP("COCOReader")
+    .Output("image")
+    .Output("image_size")
+    .Output("gt_bbox")
+    .Output("gt_label")
+    .Output("gt_segm")
+    .Output("gt_segm_offset")
+    .Attr("annotation_file", UserOpAttrType::kAtString)
+    .Attr("image_dir", UserOpAttrType::kAtString)
+    .Attr("batch_size", UserOpAttrType::kAtInt64)
+    .Attr<bool>("shuffle", UserOpAttrType::kAtBool, true)
+    .Attr<int64_t>("random_seed", UserOpAttrType::kAtInt64, -1)
+    .Attr<bool>("group_by_ratio", UserOpAttrType::kAtBool, true)
+    .Attr<int32_t>("empty_tensor_size", UserOpAttrType::kAtInt64, 32)
+    .Attr<int32_t>("tensor_init_bytes", UserOpAttrType::kAtInt32, 1048576)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      const SbpParallel& sbp = ctx->SbpParallel4ArgNameAndIndex("image", 0);
+      OF_CHECK(sbp == ctx->SbpParallel4ArgNameAndIndex("image_size", 0));
+      OF_CHECK(sbp == ctx->SbpParallel4ArgNameAndIndex("gt_bbox", 0));
+      OF_CHECK(sbp == ctx->SbpParallel4ArgNameAndIndex("gt_label", 0));
+      OF_CHECK(sbp == ctx->SbpParallel4ArgNameAndIndex("gt_segm", 0));
+      OF_CHECK(sbp == ctx->SbpParallel4ArgNameAndIndex("gt_segm_offset", 0));
+
+      int64_t batch_size = ctx->GetAttr<int32_t>("batch_size");
+      int64_t parallel_num = ctx->parallel_ctx().parallel_num();
+      int64_t device_batch_size = batch_size;
+      if (sbp.has_split_parallel() && parallel_num > 1) {
+        OF_CHECK_EQ(device_batch_size % parallel_num, 0);
+        device_batch_size /= parallel_num;
+      }
+
+      user_op::TensorDesc* image_desc = ctx->TensorDesc4ArgNameAndIndex("image", 0);
+      *image_desc->mut_shape() = Shape({device_batch_size});
+      *image_desc->mut_data_type() = DataType::kTensorBuffer;
+      user_op::TensorDesc* image_size_desc = ctx->TensorDesc4ArgNameAndIndex("image_size", 0);
+      *image_size_desc->mut_shape() = Shape({device_batch_size, 2});
+      *image_size_desc->mut_data_type() = DataType::kInt32;
+      user_op::TensorDesc* bbox_desc = ctx->TensorDesc4ArgNameAndIndex("gt_bbox", 0);
+      *bbox_desc->mut_shape() = Shape({device_batch_size});
+      *bbox_desc->mut_data_type() = DataType::kTensorBuffer;
+      user_op::TensorDesc* label_desc = ctx->TensorDesc4ArgNameAndIndex("gt_label", 0);
+      *label_desc->mut_shape() = Shape({device_batch_size});
+      *label_desc->mut_data_type() = DataType::kTensorBuffer;
+      user_op::TensorDesc* segm_desc = ctx->TensorDesc4ArgNameAndIndex("gt_segm", 0);
+      *segm_desc->mut_shape() = Shape({device_batch_size});
+      *segm_desc->mut_data_type() = DataType::kTensorBuffer;
+      user_op::TensorDesc* segm_offset_desc = ctx->TensorDesc4ArgNameAndIndex("gt_segm_offset", 0);
+      *segm_offset_desc->mut_shape() = Shape({device_batch_size});
+      *segm_offset_desc->mut_data_type() = DataType::kTensorBuffer;
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      SbpSignatureBuilder()
+          .Split("image", 0, 0)
+          .Split("image_size", 0, 0)
+          .Split("gt_bbox", 0, 0)
+          .Split("gt_label", 0, 0)
+          .Split("gt_segm", 0, 0)
+          .Split("gt_segm_offset", 0, 0)
+          .Build(ctx->sbp_sig_list()->mutable_sbp_signature()->Add());
+      return Maybe<void>::Ok();
+    })
+    .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
+      ctx->BatchAxis4ArgNameAndIndex("image", 0)->set_value(0);
+      ctx->BatchAxis4ArgNameAndIndex("image_size", 0)->set_value(0);
+      ctx->BatchAxis4ArgNameAndIndex("gt_bbox", 0)->set_value(0);
+      ctx->BatchAxis4ArgNameAndIndex("gt_label", 0)->set_value(0);
+      ctx->BatchAxis4ArgNameAndIndex("gt_segm", 0)->set_value(0);
+      ctx->BatchAxis4ArgNameAndIndex("gt_segm_offset", 0)->set_value(0);
+      return Maybe<void>::Ok();
+    });
+
+}  // namespace oneflow
