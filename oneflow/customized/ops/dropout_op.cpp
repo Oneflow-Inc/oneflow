3cf18c4c476a1b1a3b05968af60ea4b6ae6c90f4
ShawnXuan <xiexuanx2@gmail.com>
2020-04-26 22:38:06 +0800

    modify op

--- oneflow/customized/ops/dropout_op.cpp
+++ oneflow/customized/ops/dropout_op.cpp
@@ -4,13 +4,16 @@ namespace oneflow {
 
 namespace {
 
-REGISTER_USER_OP("relu")
+REGISTER_USER_OP("dropout")
     .Input("in")
+    .Input("mask")
     .Output("out")
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
       const Shape* in_shape = ctx->Shape4ArgNameAndIndex("in", 0);
       Shape* out_shape = ctx->Shape4ArgNameAndIndex("out", 0);
       *out_shape = *in_shape;
+      CHECK_EQ_OR_RETURN(*ctx->Shape4ArgNameAndIndex("mask", 0), *in_shape);
+      CHECK_EQ_OR_RETURN(*ctx->Dtype4ArgNameAndIndex("mask", 0), DataType::kInt8);
       return Maybe<void>::Ok();
     })
     .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
@@ -21,54 +24,85 @@ REGISTER_USER_OP("relu")
       const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("in", 0);
       SbpSignatureBuilder()
           .Split("in", 0, 0)
+          .Split("mask", 0, 0)
           .Split("out", 0, 0)
           .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
           .Build(ctx->sbp_sig_list());
       return Maybe<void>::Ok();
     });
 
-REGISTER_USER_OP("relu_grad")
-    .Input("y")
+REGISTER_USER_OP("dropout_grad")
     .Input("dy")
+    .Input("mask")
     .Output("dx")
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      const Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
       const Shape* dy_shape = ctx->Shape4ArgNameAndIndex("dy", 0);
       Shape* dx_shape = ctx->Shape4ArgNameAndIndex("dx", 0);
-      CHECK(*dy_shape == *y_shape);
       *dx_shape = *dy_shape;
+      CHECK_EQ_OR_RETURN(*ctx->Shape4ArgNameAndIndex("mask", 0), *dy_shape);
       return Maybe<void>::Ok();
     })
     .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
-      *ctx->BatchAxis4ArgNameAndIndex("dx", 0) = *ctx->BatchAxis4ArgNameAndIndex("y", 0);
+      *ctx->BatchAxis4ArgNameAndIndex("dx", 0) = *ctx->BatchAxis4ArgNameAndIndex("dy", 0);
       return Maybe<void>::Ok();
     })
     .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
       const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("y", 0);
       SbpSignatureBuilder()
-          .Split("y", 0, 0)
           .Split("dy", 0, 0)
+          .Split("mask", 0, 0)
           .Split("dx", 0, 0)
           .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
           .Build(ctx->sbp_sig_list());
       return Maybe<void>::Ok();
     });
 
-REGISTER_USER_OP_GRAD("relu").SetGenBackwardOpConfFn([](const user_op::UserOpWrapper& op,
+REGISTER_USER_OP_GRAD("grad").SetGenBackwardOpConfFn([](const user_op::UserOpWrapper& op,
                                                         user_op::AddOpFn AddOp) {
   if (op.NeedGenGradTensor4OpInput("in", 0)) {
     user_op::UserOpConfWrapperBuilder builder(op.op_name() + "_grad");
-    user_op::UserOpConfWrapper relu_grad_op =
-        builder.Op("relu_grad")
-            .Input("y", op.output("out", 0))
+    user_op::UserOpConfWrapper dropout_grad_op =
+        builder.Op("dropout_grad")
             .Input("dy", op.GetGradTensorWithOpOutput("out", 0))
+            .Input("mask", op.input("mask", 0))
             .Output("dx")
             .Build();
-    op.BindGradTensorWithOpInput(relu_grad_op.output("dx", 0), "in", 0);
-    AddOp(relu_grad_op);
+    op.BindGradTensorWithOpInput(dropout_grad_op.output("dx", 0), "in", 0);
+    AddOp(dropout_grad_op);
   }
 });
 
+REGISTER_USER_OP("random_mask_like")
+    .Input("like")
+    .Output("out")
+    .Attr("rate", UserOpAttrType::kAtFloat)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      const Shape* in_shape = ctx->Shape4ArgNameAndIndex("like", 0);
+      Shape* out_shape = ctx->Shape4ArgNameAndIndex("out", 0);
+      *out_shape = *in_shape;
+      return Maybe<void>::Ok();
+    })
+    .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
+      *ctx->BatchAxis4ArgNameAndIndex("out", 0) = *ctx->BatchAxis4ArgNameAndIndex("like", 0);
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("like", 0);
+      SbpSignatureBuilder()
+          .Split("like", 0, 0)
+          .Split("out", 0, 0)
+          .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
+          .Build(ctx->sbp_sig_list());
+      return Maybe<void>::Ok();
+    })
+    .SetCheckAttrFn([](const user_op::UserOpDefWrapper& op_def,
+                       const user_op::UserOpConfWrapper& op_conf) -> Maybe<void> {
+      const float& rate = op_conf.attr<float>("rate");
+      CHECK_GE_OR_RETURN(rate, 0);
+      CHECK_LT_OR_RETURN(rate, 1);
+      return Maybe<void>::Ok();
+    });
+    
 }  // namespace
 
 }  // namespace oneflow
