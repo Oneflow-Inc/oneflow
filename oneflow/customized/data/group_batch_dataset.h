4104e79a38664be892a20d9d825a8190e059734d
leaves-zwx <kunta0932@gmail.com>
2020-04-27 00:35:17 +0800

    refine group algo

--- oneflow/customized/data/group_batch_dataset.h
+++ oneflow/customized/data/group_batch_dataset.h
@@ -16,53 +16,48 @@ class GroupBatchDataset final : public Dataset<LoadTarget> {
   GroupBatchDataset(size_t batch_size,
                     const std::function<int64_t(const LoadTargetShdPtr&)>& GroupId4Sample,
                     BaseDatasetUnqPtr&& dataset)
-      : base_(std::move(dataset)), group_fn_(GroupId4Sample) {}
+      : base_(std::move(dataset)), group_fn_(GroupId4Sample), order_count_(0) {}
   ~GroupBatchDataset() = default;
 
   LoadTargetShdPtrVec Next() override {
     LoadTargetShdPtrVec ret;
-    size_t remain_cnt = batch_size_;
-    // decide batch group_id according to first sample whether from cache or base
     int64_t group_id = -1;
-    if (cache_samples_.size() > 0) {
-      group_id = cache_group_ids_.front();
-      ret.emplace_back(cache_samples_.front());
-      cache_group_ids_.pop_front();
-      cache_samples_.pop_front();
-    } else {
-      LoadTargetShdPtrVec samples = base_->Next();
-      CHECK_EQ(samples.size(), 1);
-      group_id = group_fn_(samples[0]);
-      ret.emplace_back(std::move(samples[0]));
-    }
-    remain_cnt -= 1;
-    // fetch all samples with the same group_id with batch from cache
-    auto group_it = cache_group_ids_.begin();
-    auto sample_it = cache_samples_.begin();
-    while (group_it != cache_group_ids_.end()) {
-      if (*group_it == group_id) {
-        ret.emplace_back(*sample_it);
-        remain_cnt -= 1;
-        group_it = cache_group_ids_.erase(group_it);
-        sample_it = cache_samples_.erase(sample_it);
-      } else {
-        ++group_it;
-        ++sample_it;
+    int64_t min_order = -1;
+    for (const auto& pair : group_id2buffered_samples_) {
+      if (pair.second.size() > 0) {
+        if (min_order == -1 || pair.second.front().order < min_order) {
+          min_order = pair.second.front().order;
+          group_id = pair.first;
+        }
       }
     }
-    // fetch samples from base, samples with the same group join batch,
-    // otherwise cache it
+    size_t remain_cnt = batch_size_;
+    auto& buffered_samples = group_id2buffered_samples_.at(group_id);
+    auto it = buffered_samples.begin();
+    while (remain_cnt > 0 && it != buffered_samples.end()) {
+      ret.emplace_back(it->data);
+      ++it;
+      remain_cnt -= 1;
+    }
+    buffered_samples.erase(buffered_samples.begin(), it);
     while (remain_cnt > 0) {
-      LoadTargetShdPtrVec samples = base_->Next();
-      CHECK_EQ(samples.size(), 1);
-      int64_t cur_group_id = group_fn_(samples[0]);
+      LoadTargetShdPtrVec data_inst_vec = base_->Next();
+      CHECK_EQ(data_inst_vec.size(), 1);
+      int64_t cur_group_id = group_fn_(data_inst_vec[0]);
       if (cur_group_id == group_id) {
-        ret.emplace_back(std::move(samples[0]));
+        ret.emplace_back(std::move(data_inst_vec[0]));
         remain_cnt -= 1;
       } else {
-        cache_samples_.emplace_back(std::move(samples[0]));
-        cache_group_ids_.emplace_back(cur_group_id);
+        auto it = group_id2buffered_samples_.find(cur_group_id);
+        if (it == group_id2buffered_samples_.end()) {
+          it = group_id2buffered_samples_.emplace(cur_group_id, std::vector<Sample>()).first;
+        }
+        Sample sample;
+        sample.data.swap(data_inst_vec[0]);
+        sample.order = order_count_;
+        it->second.emplace_back(std::move(sample));
       }
+      order_count_ += 1;
     }
     return ret;
   }
@@ -71,11 +66,16 @@ class GroupBatchDataset final : public Dataset<LoadTarget> {
   bool EnableGetSize() override { return false; }
 
  private:
+  struct Sample {
+    LoadTargetShdPtr data;
+    int64_t order;
+  };
+
   BaseDatasetUnqPtr base_;
   size_t batch_size_;
   std::function<int64_t(const LoadTargetShdPtr&)> group_fn_;
-  std::list<LoadTargetShdPtr> cache_samples_;
-  std::list<int64_t> cache_group_ids_;
+  std::map<int64_t, std::vector<Sample>> group_id2buffered_samples_;
+  int64_t order_count_;
 };
 
 }  // namespace oneflow
