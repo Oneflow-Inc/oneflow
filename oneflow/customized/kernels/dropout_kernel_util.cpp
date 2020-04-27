8144d7b2e6c69c94b4d3e09f9a0aaa8f7a42f833
ShawnXuan <xiexuanx2@gmail.com>
2020-04-27 19:41:47 +0800

    dropout kernel util

---
+++ oneflow/customized/kernels/dropout_kernel_util.cpp
@@ -0,0 +1,33 @@
+#include "oneflow/core/common/preprocessor.h"
+#include "oneflow/core/kernel/random_generator.h"
+#include "oneflow/core/kernel/dropout_kernel.h"
+#include "oneflow/core/kernel/kernel_util.h"
+#include "oneflow/core/common/data_type.h"
+
+namespace oneflow {
+
+template<typename T>
+struct DropoutKernelUtil<DeviceType::kCPU, T> final {
+  static void MaskAndScale(DeviceCtx* ctx, const int64_t n, float scale, const T* x,
+                           const int8_t* mask, T* y) {
+    for (int64_t i = 0; i < n; ++i) { y[i] = x[i] * static_cast<T>(mask[i]) * scale; }
+  }
+};
+
+template<>
+struct RandomMaskLikeKernelUtil<DeviceType::kCPU> final {
+  static void GenMask(DeviceCtx* ctx, const int64_t n, float threshold, const float* random_tmp,
+                      int8_t* mask) {
+    for (int64_t i = 0; i < n; ++i) { mask[i] = random_tmp[i] > threshold; }
+  }
+};
+
+template struct RandomMaskLikeKernelUtil<DeviceType::kCPU>;
+
+#define INITIATE_DROPOUT_KERNEL_UTIL_CPU(T, type_proto) \
+  template struct DropoutKernelUtil<DeviceType::kCPU, T>;
+OF_PP_FOR_EACH_TUPLE(INITIATE_DROPOUT_KERNEL_UTIL_CPU,
+                     ARITHMETIC_DATA_TYPE_SEQ FLOAT16_DATA_TYPE_SEQ);
+#undef INITIATE_DROPOUT_KERNEL_UTIL_CPU
+
+}  // namespace oneflow
