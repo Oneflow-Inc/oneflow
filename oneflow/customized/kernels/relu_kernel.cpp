c9d7ba405ab68db23991cb02156902f913590a29
ShawnXuan <xiexuanx2@gmail.com>
2020-04-29 22:33:36 +0800

    AlwaysComputeWhenAllOutputsEmpty

--- oneflow/customized/kernels/relu_kernel.cpp
+++ oneflow/customized/kernels/relu_kernel.cpp
@@ -17,7 +17,8 @@ class ReluKernel final : public user_op::OpKernel {
     user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("out", 0);
     NewKernelUtil<device_type>::Relu(ctx->device_ctx(), x->shape().elem_cnt(), x->dptr<T>(),
                                      y->mut_dptr<T>());
-  };
+  }
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
 #define REGISTER_RELU_KERNEL(device, dtype)                                                     \
@@ -53,7 +54,8 @@ class ReluGradKernel final : public user_op::OpKernel {
     NewKernelUtil<device_type>::ReluBackward(ctx->device_ctx(), y_blob->shape().elem_cnt(),
                                              y_blob->dptr<T>(), y_blob->dptr<T>(),
                                              dy_blob->dptr<T>(), dx_blob->mut_dptr<T>());
-  };
+  }
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
 #define REGISTER_RELU_GRAD_KERNEL(device, dtype)                                                 \
