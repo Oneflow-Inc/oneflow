76e05007e65f39337bcbc8b1ed7c056bc7a89eff
tsai <caishenghang@oneflow.org>
2020-05-02 20:49:20 +0800

    update cmake file

--- cmake/third_party/cub.cmake
+++ cmake/third_party/cub.cmake
@@ -3,7 +3,7 @@ include (ExternalProject)
 set(CUB_INCLUDE_DIR ${THIRD_PARTY_DIR}/cub/include)
 set(CUB_BUILD_INCLUDE ${CMAKE_CURRENT_BINARY_DIR}/cub/src/cub/cub)
 
-set(CUB_URL ${CMAKE_CURRENT_BINARY_DIR}/third_party/cub/src/cub)
+set(CUB_URL ${PROJECT_SOURCE_DIR}/third_party/cub/src/cub)
 
 
 ExternalProject_Add(cub
