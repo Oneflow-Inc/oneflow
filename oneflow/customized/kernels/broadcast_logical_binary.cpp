db85264f61f6581767ea4379b19a615f0573a187
nlqq <snow3s@qq.com>
2020-05-04 22:38:51 +0800

    refine boradcast binary op

---
+++ oneflow/customized/kernels/broadcast_logical_binary.cpp
@@ -0,0 +1,56 @@
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/core/thread/thread_manager.h"
+#include "oneflow/core/common/balanced_splitter.h"
+#include "oneflow/core/ndarray/binary_func.h"
+#include "oneflow/core/ndarray/xpu_var_ndarray.h"
+#include "oneflow/core/ndarray/ndarray_util.h"
+
+namespace oneflow {
+
+template<DeviceType device_type, typename T, typename K, void (*f)(DeviceType &device_type, T &x , T &y)>
+class BroadcastLogicalBinaryKernel final : public user_op::OpKernel {
+ public:
+  BroadcastLogicalBinaryKernel() = default;
+  ~BroadcastLogicalBinaryKernel() = default;
+
+ private:
+  void Compute(user_op::KernelComputeContext* ctx) const override {
+    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    const user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
+    user_op::Tensor* z = ctx->Tensor4ArgNameAndIndex("z", 0);
+    size_t num_axes = z->shape().NumAxes();
+
+    f(device_type, x, y);
+  }
+};
+
+#define LOGICAL_MATH_TYPE_SEQ                   \
+  OF_PP_MAKE_TUPLE_SEQ("Greater", GT)       \
+  OF_PP_MAKE_TUPLE_SEQ("GreaterEqual", GE)  \
+  OF_PP_MAKE_TUPLE_SEQ("LessThan", LT)      \
+  OF_PP_MAKE_TUPLE_SEQ("LessEqual", LE)     \
+  OF_PP_MAKE_TUPLE_SEQ("Equal", EQ)         \
+  OF_PP_MAKE_TUPLE_SEQ("NotEqual", NE)      \
+  OF_PP_MAKE_TUPLE_SEQ("LogicalAnd", AND)
+
+#define REGISTER_BROADCAST_BINARY_KERNEL(logical_math_type, device, T_dtype, K_dtype)                        \
+  REGISTER_USER_KERNEL("broadcast_binary")                                                                   \
+  .SetCreateFn<BroadcastLogicalBinaryKernel<  device, OF_PP_PAIR_FIRST(T_dtype), OF_PP_PAIR_FIRST(K_dtype),                                                               \
+      &NdarrayUtil<device_type, T>::Broadcast##OF_PP_PAIR_SECOND(logical_math_type)                           \
+           >>()                                  \
+  .SetIsMatchedPred([](const user_op::KernelRegContext& ctx) {                                               \
+      const user_op::TensorDesc* x_desc = ctx.TensorDesc4ArgNameAndIndex("x", 0);                            \
+      const user_op::TensorDesc* y_desc = ctx.TensorDesc4ArgNameAndIndex("y", 0);                            \
+      const std::string binary_math_type = ctx->GetAttr("binary_math_type");                                   \
+      user_op::TensorDesc* z_desc = ctx.TensorDesc4ArgNameAndIndex("z", 0);                                  \
+      return ctx.device_type() == device                                                                     \
+             && x_desc->data_type() == OF_PP_PAIR_SECOND(T_dtype)                                            \
+             && y_desc->data_type() == OF_PP_PAIR_SECOND(T_dtype)                                            \
+             && binary_math_type == OF_PP_PAIR_FIRST(logical_math_type)                                     \
+             && z_desc->data_type() == OF_PP_PAIR_SECOND(K_dtype);                                           \
+      });
+
+OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(REGISTER_BROADCAST_BINARY_KERNEL, LOGICAL_MATH_TYPE_SEQ,
+    DEVICE_TYPE_SEQ, FLOATING_DATA_TYPE_SEQ, INDEX_DATA_TYPE_SEQ)
+
+}  // namespace oneflow
---
+++ oneflow/customized/ops/broadcast_logical_binary_op.cpp
@@ -0,0 +1,55 @@
+#include "oneflow/core/framework/framework.h"
+
+namespace oneflow {
+
+REGISTER_USER_OP("binary")
+    .Input("x")
+    .Input("y")
+    .Output("z")
+    .Attr("binary_math_type", UserOpAttrType::kAtString)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
+      Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
+      Shape* z_shape = ctx->Shape4ArgNameAndIndex("z", 0);
+      CHECK(*y_shape == *x_shape);
+      *z_shape = *x_shape;
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      const user_op::TensorDesc& tensor_x = ctx->LogicalTensorDesc4InputArgNameAndIndex("x", 0);
+      // const user_op::TensorDesc& tensor_y = ctx->LogicalTensorDesc4InputArgNameAndIndex("y", 0);
+      SbpSignatureBuilder()
+          .Split(ctx->inputs(), 0)
+          .Split(ctx->outputs(), 0)
+          .MakeSplitSignatureListBuilder(tensor_x.shape().NumAxes())
+          .Build(ctx->sbp_sig_list());
+      return Maybe<void>::Ok();
+    });
+
+REGISTER_USER_OP("broadcast_binary")
+    .Input("x")
+    .Input("y")
+    .Output("z")
+    .Attr("binary_math_type", UserOpAttrType::kAtString)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
+      Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
+      Shape* z_shape = ctx->Shape4ArgNameAndIndex("z", 0);
+      CHECK(*y_shape ==*x_shape);
+      *z_shape = *x_shape;
+      DataType* z_dtype = ctx->Dtype4ArgNameAndIndex("z", 0);
+      *z_dtype = kInt8;
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      const user_op::TensorDesc& tensor_x = ctx->LogicalTensorDesc4InputArgNameAndIndex("x", 0);
+      const user_op::TensorDesc& tensor_y = ctx->LogicalTensorDesc4InputArgNameAndIndex("y", 0);
+      SbpSignatureBuilder()
+          .Split(ctx->inputs(), 0)
+          .Split(ctx->outputs(), 0)
+          .MakeSplitSignatureListBuilder(tensor_x.shape().NumAxes())
+          .Build(ctx->sbp_sig_list());
+      return Maybe<void>::Ok();
+    });
+
+}  // namespace oneflow
