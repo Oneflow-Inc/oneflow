827d0f54decaab2e8b2457b920175cc4de0f77c4
lixinqi <lixinqi0703106@163.com>
2020-05-05 18:37:21 +0800

    rename ConstObject to Symbol
    Former-commit-id: 528d850a25bc592d9d86f9564a6c17766313b97a

--- oneflow/core/eager/init_symbol_instruction_type.cpp
+++ oneflow/core/eager/init_symbol_instruction_type.cpp
@@ -1,5 +1,5 @@
 #include "oneflow/core/common/util.h"
-#include "oneflow/core/vm/const_object_instruction_type.h"
+#include "oneflow/core/vm/init_symbol_instruction_type.h"
 #include "oneflow/core/job/job_desc.h"
 #include "oneflow/core/operator/op_conf.pb.h"
 
@@ -7,12 +7,12 @@ namespace oneflow {
 namespace eager {
 
 COMMAND(Global<vm::Storage<JobConfigProto>>::SetAllocated(new vm::Storage<JobConfigProto>()));
-using JobDescInstr = vm::InitConstObjectInstructionType<JobDesc, JobConfigProto>;
+using JobDescInstr = vm::InitSymbolInstructionType<JobDesc, JobConfigProto>;
 COMMAND(vm::RegisterInstructionType<JobDescInstr>("InitJobDescSymbol"));
 COMMAND(vm::RegisterLocalInstructionType<JobDescInstr>("LocalInitJobDescSymbol"));
 
 COMMAND(Global<vm::Storage<OperatorConf>>::SetAllocated(new vm::Storage<OperatorConf>()));
-using OperatorConfInstr = vm::InitConstObjectInstructionType<OperatorConf, OperatorConf>;
+using OperatorConfInstr = vm::InitSymbolInstructionType<OperatorConf, OperatorConf>;
 COMMAND(vm::RegisterInstructionType<OperatorConfInstr>("InitOperatorConfSymbol"));
 COMMAND(vm::RegisterLocalInstructionType<OperatorConfInstr>("LocalInitOperatorConfSymbol"));
 
