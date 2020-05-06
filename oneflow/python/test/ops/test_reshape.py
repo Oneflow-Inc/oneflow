ee8d3770428f9ce2d72767ec96f7ed638daa1e56
欧阳宇 <64632757+xuanjiuye@users.noreply.github.com>
2020-05-06 13:48:18 +0800

    ut save data draft (#2830)
    * ut save data draft
    
    * 将数据存入全局dict里
    
    * rename global
    
    * 去除不必要的import
    
    * Remove unnecessary import
    
    * rename the method name
    
    Co-authored-by: Li Xinqi <lixinqi2010@gmail.com>

--- oneflow/python/test/ops/test_reshape.py
+++ oneflow/python/test/ops/test_reshape.py
@@ -5,8 +5,6 @@ import oneflow as flow
 from collections import OrderedDict 
 
 from test_util import GenArgDict
-from test_util import GetSavePath
-from test_util import Save
 from test_util import CompareOpWithTensorFlow
 from test_util import Args
 
