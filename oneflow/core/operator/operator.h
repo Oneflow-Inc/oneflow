0a65d562d9969ce0bd92b4ee7c3ff47cd0cafc38
liujuncheng <liujuncheng1022@gmail.com>
2020-05-07 17:30:48 +0800

    IsLossOp

--- oneflow/core/operator/operator.h
+++ oneflow/core/operator/operator.h
@@ -34,9 +34,7 @@ class Operator {
   virtual void InitFromOpConf() = 0;
 
   virtual LogicalNode* NewProperLogicalNode() const;
-
-  virtual bool IsLossOp() const { return false; }
-
+  
   // bn_in_op <-> lbi
   const LogicalBlobId& BnInOp2Lbi(const std::string& bn_in_op) const;
   LogicalBlobId* MutBnInOp2Lbi(const std::string& bn_in_op);
