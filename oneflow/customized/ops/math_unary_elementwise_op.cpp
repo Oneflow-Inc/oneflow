a56d810ce9a9d22e56b1545529cd0137a7d79407
cheng cheng <472491134@qq.com>
2020-05-07 18:42:20 +0800

    math unary elementwise op/kernel template

---
+++ oneflow/customized/ops/math_unary_elementwise_op.cpp
@@ -0,0 +1,65 @@
+#include "oneflow/core/common/data_type.pb.h"
+#include "oneflow/core/framework/framework.h"
+
+namespace oneflow {
+
+REGISTER_USER_OP("math_unary_elementwise")
+    .Input("x")
+    .Output("y")
+    .Attr("math_type", UserOpAttrType::kAtString)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
+      Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
+      *y_shape = *x_shape;
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("x", 0);
+      SbpSignatureBuilder()
+          .Split(ctx->inputs(), 0)
+          .Split(ctx->outputs(), 0)
+          .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
+          .Build(ctx->sbp_sig_list());
+      return Maybe<void>::Ok();
+    });
+
+REGISTER_USER_OP("math_unary_elementwise_grad")
+    .Input("x")
+    .Input("dy")
+    .Output("dx")
+    .Attr("math_type", UserOpAttrType::kAtString)
+    .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+      Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
+      Shape* dy_shape = ctx->Shape4ArgNameAndIndex("dy", 0);
+      Shape* dx_shape = ctx->Shape4ArgNameAndIndex("dx", 0);
+      CHECK(*dy_shape == *x_shape);
+      *dx_shape = *x_shape;
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+      const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("dy", 0);
+      SbpSignatureBuilder()
+          .Split(ctx->inputs(), 0)
+          .Split(ctx->outputs(), 0)
+          .MakeSplitSignatureListBuilder(tensor.shape().NumAxes())
+          .Build(ctx->sbp_sig_list());
+      return Maybe<void>::Ok();
+    });
+
+REGISTER_USER_OP_GRAD("math_unary_elementwise")
+    .SetGenBackwardOpConfFn([](const user_op::UserOpWrapper& op, user_op::AddOpFn AddOp) {
+      if (op.NeedGenGradTensor4OpInput("x", 0)) {
+        user_op::UserOpConfWrapperBuilder builder(op.op_name() + "_grad");
+        user_op::UserOpConfWrapper unary_grad_op =
+            builder.Op("math_unary_elementwise_grad")
+                .Input("x", op.input("x", 0))
+                .Input("dy", op.GetGradTensorWithOpOutput("y", 0))
+                .Output("dx")
+                .Attr<std::string>("math_type", op.attr<std::string>("math_type"))
+                .Build();
+        op.BindGradTensorWithOpInput(unary_grad_op.output("dx", 0), "x", 0);
+        AddOp(unary_grad_op);
+      }
+    });
+
+}  // namespace oneflow
