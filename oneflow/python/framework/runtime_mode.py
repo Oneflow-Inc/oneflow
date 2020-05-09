cb5ce5efe5ef7c0ad829e6acab54c7d1f7f6e862
lixinqi <lixinqi0703106@163.com>
2020-05-09 12:26:14 +0800

    runtime_mode

---
+++ oneflow/python/framework/runtime_mode.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+
+NORMAL_MODE = "NORMAL_MODE"
+GLOBAL_MODE = "GLOBAL_MODE"
+DEVICE_MODE = "DEVICE_MODE"
+
+def CurrentMode(): return  mode_statck[0]
+
+@contextmanager
+def ModeScope(mode):
+    global mode_statck
+    mode_statck.insert(0, mode);
+    yield
+    mode_statck.pop(0)
+
+mode_statck = [NORMAL_MODE]
--- oneflow/python/oneflow_export.py
+++ oneflow/python/oneflow_export.py
@@ -1,6 +1,11 @@
+import oneflow.python.framework.runtime_mode
 import re
 
 def oneflow_export(*field_paths):
+    modes = [runtime_mode.NORMAL_MODE, runtime_mode.GLOBAL_MODE, runtime_mode.DEVICE_MODE]
+    return _GetOneflowExportDecorator(field_paths, modes)
+
+def _GetOneflowExportDecorator(field_paths, oneflow_modes):
     def Decorator(func_or_class):
         for field_path in field_paths:
             fields = field_path.split(".")
@@ -8,7 +13,7 @@ def oneflow_export(*field_paths):
             global exported
             api = exported
             for field in fields: api = api._FindOrCreateSubApi(field)
-            api._SetApiFunction(func_or_class)
+            for mode in oneflow_modes: api._SetApiFunction(mode, func_or_class)
         return func_or_class
     return Decorator
 
@@ -16,16 +21,19 @@ def oneflow_export(*field_paths):
 class OneflowApi(object):
     def __init__(self, name_path = ""):
         self.name_path_ = name_path
-        self.api_function_ = _GetNotImplemented(name_path)
+        self.mode2api_function_ = {}
         self.sub_api_ = {}
 
     def __call__(self, *args, **kwargs):
-        return self.api_function_(*args, **kwargs)
+        mode = runtime_mode.CurrentMode()
+        supported = self.mode2api_function_
+        if mode in supported: return supported[mode](*args, **kwargs)
+        raise ApiNotImplementedError(self.name_path_, mode, supported.keys())
 
     def _SubApi(self): return self.sub_api_
 
-    def _SetApiFunction(self, api_function):
-        self.api_function_ = api_function
+    def _SetApiFunction(self, mode, api_function):
+        self.mode2api_function_[mode] = api_function
 
     def _FindOrCreateSubApi(self, field):
         assert re.match("^[\w]+[_\w\d]*$", field)
@@ -35,10 +43,16 @@ class OneflowApi(object):
         setattr(self, field, sub_api)
         return sub_api
 
-def _GetNotImplemented(api_name):
-    def _NotImplemented(*args, **kwargs):
-        raise NotImplementedError("api %s is not found".api_name)
-    return _NotImplemented
+def ApiNotImplementedError(Exception):
+    def __init__(self, api_name, current_mode, supported_modes):
+        self.api_name_ = api_name
+        self.current_mode_ = current_mode
+        self.supported_modes_ = supported_modes
+
+    def __str__(self):
+        return ("\napi name: %s\nsupported mode: %s\n current mode: %s\n"
+                %(self.api_name_, ", ".join(self.supported_modes_), self.current_mode_))
+        return ret
 
 
 exported = OneflowApi()
