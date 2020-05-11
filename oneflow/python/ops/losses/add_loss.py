fa7ad8bd5747dca00cbd89e50a4d4c3a144238f1
Li Xinqi <lixinqi2010@gmail.com>
2020-05-12 01:24:10 +0800

    refactor enable_if and oneflow_export (#2870)
    * refactor enable_if and oneflow_export
    
    * remote unused function

--- oneflow/python/ops/losses/add_loss.py
+++ oneflow/python/ops/losses/add_loss.py
@@ -5,7 +5,6 @@ import oneflow.python.framework.g_func_ctx as g_func_ctx
 import oneflow.python.framework.hob as hob
 from oneflow.python.lib.core.enable_if import enable_if
 
-
 @oneflow_export("losses.add_loss", enable_if = hob.in_global_mode & hob.is_trainable)
 def add_loss(loss):
     g_func_ctx.CurJobBuildAndInferCtx_AddLossLogicalBlobName(loss.logical_blob_name)
