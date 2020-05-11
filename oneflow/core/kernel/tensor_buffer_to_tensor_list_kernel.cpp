a3964950a8ebd2ce956a8a11db3619c98e5b1788
leaves-zwx <kunta0932@gmail.com>
2020-05-11 15:56:30 +0800

    tensor_buffer_to_tensor_list do not infer shape

--- oneflow/core/kernel/tensor_buffer_to_tensor_list_kernel.cpp
+++ oneflow/core/kernel/tensor_buffer_to_tensor_list_kernel.cpp
@@ -44,28 +44,22 @@ void TensorBufferToTensorListKernel<T>::ForwardDataContent(
     const KernelCtx& ctx, std::function<Blob*(const std::string&)> BnInOp2Blob) const {
   const Blob* in_blob = BnInOp2Blob("in");
   Blob* out_blob = BnInOp2Blob("out");
-  Shape shape(op_conf().tensor_buffer_to_tensor_list_conf().shape());
-  int64_t batch_axis = op_conf().tensor_buffer_to_tensor_list_conf().batch_axis();
+  Shape conf_shape(op_conf().tensor_buffer_to_tensor_list_conf().shape());
 
   TensorBackInserter back_inserter(out_blob);
   back_inserter.ReserveOneEmptyTensorList();
   FOR_RANGE(int, i, 0, in_blob->shape().elem_cnt()) {
     const TensorBuffer& in_buffer = in_blob->dptr<TensorBuffer>()[i];
-    DimVector dim_vec(1, 1);
-    FOR_RANGE(int, j, 0, shape.NumAxes()) {
-      if (j == batch_axis) {
-        dim_vec.push_back(1);
-      } else {
-        dim_vec.push_back(shape.At(j));
-      }
-    }
-    Shape out_shape(dim_vec);
-    CHECK_EQ(in_buffer.shape().elem_cnt() % out_shape.elem_cnt(), 0);
-    int64_t varying_dim = in_buffer.shape().elem_cnt() / out_shape.elem_cnt();
-    out_shape.Set(batch_axis + 1, varying_dim);
+    CHECK_EQ(in_buffer.shape().NumAxes(), conf_shape.NumAxes())
+        << "in_buffer.shape " << in_buffer.shape().ToString() << ", conf_shape "
+        << conf_shape.ToString();
+    CHECK_LE(in_buffer.shape().elem_cnt(), conf_shape.elem_cnt())
+        << "in_buffer.shape " << in_buffer.shape().ToString() << ", conf_shape "
+        << conf_shape.ToString();
+    DimVector dim_vec = in_buffer.shape().dim_vec();
+    dim_vec.insert(dim_vec.begin(), 1);
     FullyMutTensorView* tensor_view = back_inserter.add_tensor();
-    tensor_view->set_shape(out_shape);
-    CHECK_EQ(tensor_view->shape().elem_cnt(), in_buffer.shape().elem_cnt());
+    tensor_view->set_shape(Shape(dim_vec));
     SwitchUtil<T>::SwitchCopyTensorBufferTo(SwitchCase(in_buffer.data_type()), in_buffer,
                                             tensor_view->mut_dptr<T>());
   }
