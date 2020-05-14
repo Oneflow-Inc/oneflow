e713a8d24b47bc4b3f1f65b82fc3bfd68b9494eb
Dounm <niuchong893184@gmail.com>
2020-05-14 12:42:10 +0800

    Refactor AddN Cpu Kernel

--- oneflow/customized/kernels/add_n_kernel.cpp
+++ oneflow/customized/kernels/add_n_kernel.cpp
@@ -5,20 +5,11 @@ namespace oneflow {
 namespace {
 
 template<typename T>
-void cpu_add(const int64_t n, T* out, const T* in_0) {
-  for (int64_t i = 0; i != n; ++i) { out[i] = in_0[i]; }
-}
-template<typename T>
-void cpu_add(const int64_t n, T* out, const T* in_0, const T* in_1) {
-  for (int64_t i = 0; i != n; ++i) { out[i] = in_0[i] + in_1[i]; }
-}
-template<typename T>
-void cpu_add(const int64_t n, T* out, const T* in_0, const T* in_1, const T* in_2) {
-  for (int64_t i = 0; i != n; ++i) { out[i] = in_0[i] + in_1[i] + in_2[i]; }
-}
-template<typename T>
-void cpu_add(const int64_t n, T* out, const T* in_0, const T* in_1, const T* in_2, const T* in_3) {
-  for (int64_t i = 0; i != n; ++i) { out[i] = in_0[i] + in_1[i] + in_2[i] + in_3[i]; }
+void cpu_add(const int64_t n, T* out, const std::vector<const T*>& in) {
+  for (int64_t i = 0; i != n; ++i) {
+    out[i] = in.at(0)[i];
+    for (int32_t j = 1; j < in.size(); ++j) { out[i] += in.at(j)[i]; }
+  }
 }
 
 }  // namespace
@@ -34,9 +25,6 @@ class CpuAddNKernel : public user_op::OpKernel {
  private:
   void Compute(user_op::KernelComputeContext* ctx) const override {
     size_t in_num = ctx->inputs().size();
-    CHECK_GE(in_num, 2);
-    CHECK_LE(in_num, 4)
-        << "CpuAddNKernel of add_n op doesn't support number of operands which is bigger than 4.";
 
     user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("out", 0);
     int64_t n = out->shape().elem_cnt();
@@ -47,14 +35,7 @@ class CpuAddNKernel : public user_op::OpKernel {
       in_dptrs.at(i) = ctx->Tensor4ArgNameAndIndex("in", i)->dptr<T>();
     }
 
-    switch (in_num) {
-      case 2: cpu_add(n, out_dptr, in_dptrs.at(0), in_dptrs.at(1)); break;
-      case 3: cpu_add(n, out_dptr, in_dptrs.at(0), in_dptrs.at(1), in_dptrs.at(2)); break;
-      case 4:
-        cpu_add(n, out_dptr, in_dptrs.at(0), in_dptrs.at(1), in_dptrs.at(2), in_dptrs.at(3));
-        break;
-      default: LOG(FATAL) << "Not supported input size for CpuAddNKernel: " << in_num; break;
-    }
+    cpu_add<T>(n, out_dptr, in_dptrs);
   }
 };
 
