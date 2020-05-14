5b1d2aac33e9f1102f241565693e05e3618b361f
tsai <caishenghang@oneflow.org>
2020-05-14 10:19:12 +0800

    add if

---
+++ oneflow/customized/ops/scalar_by_tensor_op.cpp
@@ -0,0 +1,74 @@
+#include "oneflow/core/framework/framework.h"
+
+namespace oneflow {
+
+namespace {
+
+Maybe<void> InferBroadcastTensorDescFn(user_op::InferContext* ctx) {
+  const user_op::TensorDesc* x = ctx->TensorDesc4ArgNameAndIndex("x", 0);
+  const user_op::TensorDesc* scalar = ctx->TensorDesc4ArgNameAndIndex("scalar", 0);
+  CHECK_EQ_OR_RETURN(x->data_type(), scalar->data_type());
+  CHECK_EQ_OR_RETURN(scalar->shape().elem_cnt(), 1);
+  user_op::TensorDesc* y = ctx->TensorDesc4ArgNameAndIndex("y", 0);
+  *y = *x;
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> GetBasicSbpSignature(user_op::SbpContext* ctx) {
+  const auto& x = ctx->LogicalTensorDesc4InputArgNameAndIndex("x", 0);
+  FOR_RANGE(int64_t, i, 0, x.shape().NumAxes()) {
+    ctx->NewBuilder()
+        .Split(user_op::OpArg("x", 0), i)
+        .Split(user_op::OpArg("y", 0), i)
+        .Broadcast(user_op::OpArg("scalar", 0))
+        .Build();
+  }
+  return Maybe<void>::Ok();
+}
+
+using GetSbpFn = std::function<Maybe<void>(user_op::SbpContext*)>;
+GetSbpFn MakeGetSbpFn(GetSbpFn extra) {
+  return [extra](user_op::SbpContext* ctx) -> Maybe<void> {
+    JUST(extra(ctx));
+    GetBasicSbpSignature(ctx);
+    return Maybe<void>::Ok();
+  };
+}
+
+}  // namespace
+
+REGISTER_USER_OP("scalar_add_by_tensor")
+    .Input("x")
+    .Input("scalar")
+    .Output("y")
+
+    .SetTensorDescInferFn(InferBroadcastTensorDescFn)
+    .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
+      *ctx->BatchAxis4ArgNameAndIndex("y", 0) = *ctx->BatchAxis4ArgNameAndIndex("x", 0);
+      return Maybe<void>::Ok();
+    })
+    .SetGetSbpFn(MakeGetSbpFn([](user_op::SbpContext* ctx) {
+      ctx->NewBuilder()
+          .PartialSum(user_op::OpArg("x", 0))
+          .PartialSum(user_op::OpArg("scalar", 0))
+          .PartialSum(user_op::OpArg("y", 0))
+          .Build();
+      return Maybe<void>::Ok();
+    }));
+
+REGISTER_USER_OP_GRAD("scalar_add_by_tensor")
+    .SetGenBackwardOpConfFn([](const user_op::UserOpWrapper& op, user_op::AddOpFn AddOp) {
+      if (op.NeedGenGradTensor4OpInput("x", 0)) {
+        user_op::UserOpConfWrapperBuilder builder(op.op_name() + "_grad");
+        user_op::UserOpConfWrapper grad_op = builder.Op("scalar_add_by_tensor_grad")
+                                                 .Input("x", op.input("x", 0))
+                                                 .Input("dy", op.GetGradTensorWithOpOutput("y", 0))
+                                                 .Output("dx")
+                                                 .Attr("alpha", op.attr<float>("alpha"))
+                                                 .Build();
+        op.BindGradTensorWithOpInput(grad_op.output("dx", 0), "x", 0);
+        AddOp(grad_op);
+      }
+    });
+
+}  // namespace oneflow
