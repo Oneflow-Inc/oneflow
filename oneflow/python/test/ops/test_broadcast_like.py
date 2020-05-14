3b7807670b0f83fddc4252544f2cf805d22ecb38
hsj0429 <hsj0429@hotmail.com>
2020-05-14 16:57:06 +0800

    fix test bug

--- oneflow/python/test/ops/test_broadcast_like.py
+++ oneflow/python/test/ops/test_broadcast_like.py
@@ -7,21 +7,21 @@ import oneflow.core.common.data_type_pb2 as data_type_util
 
 from test_util import GenArgList
 
-def test_broadcast_like_forward(device_type, input_shape, like_shape, axis):
+def compare_broadcast_like_with_tf(device_type, input_shape, like_shape, axis, rtol=1e-5, atol=1e-5):
     assert device_type in ["gpu", "cpu"]
     flow.clear_default_session()
     func_config = flow.FunctionConfig()
     func_config.default_data_type(flow.float)
+
     @flow.function(func_config)
     def broadcast_like_forward(x=flow.FixedTensorDef(shape=input_shape, dtype=data_type_util.kFloat), y=flow.FixedTensorDef(shape=like_shape, dtype=data_type_util.kFloat)):
         with flow.fixed_placement(device_type, "0:0"):
             return flow.broadcast_like(x, y, axis=axis)
-
     x = np.random.rand(*input_shape).astype(np.float32)
     like = np.random.rand(*like_shape).astype(np.float32)
     of_out = broadcast_like_forward(x, like).get()
     np_out = np.broadcast_to(x, like_shape)
-    assert np.allclose(of_out.ndarray(), np_out, rtol=1e-5, atol=1e-5)
+    assert np.allclose(of_out.ndarray(), np_out, rtol=rtol, atol=atol)
 
 def test_broadcast_like(test_case):
     arg_dict = OrderedDict()
@@ -30,4 +30,4 @@ def test_broadcast_like(test_case):
     arg_dict["like_shape"] = [(4,5,2)]
     arg_dict["axis"] = [[0]]
     for arg in GenArgList(arg_dict):
-        test_broadcast_like_forward(*arg)
+        compare_broadcast_like_with_tf(*arg)
