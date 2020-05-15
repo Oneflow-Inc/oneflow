f3b3bfed658414a08991ea2b10a81413c8cc839c
nlqq <snow3s@qq.com>
2020-05-15 23:04:18 +0800

    add broadcast logical and unit test

---
+++ oneflow/python/test/ops/test_broadcast_logical_and.py
@@ -0,0 +1,100 @@
+import oneflow as flow
+import numpy as np
+import tensorflow as tf
+from collections import OrderedDict
+from test_util import GenArgList
+
+gpus = tf.config.experimental.list_physical_devices("GPU")
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+def _random_inputs(x_shape, y_shape):
+    x = np.random.rand(*x_shape).astype(np.float32)
+    y = np.random.rand(*y_shape).astype(np.float32)
+    return x, y
+
+
+def _make_logical_and_fn(x, y, device_type, mirrored):
+    flow.clear_default_session()
+    func_config = flow.FunctionConfig()
+    if mirrored:
+        func_config.default_distribute_strategy(flow.distribute.mirrored_strategy())
+    else:
+        func_config.default_distribute_strategy(flow.distribute.consistent_strategy())
+
+    def do_logical_and(x_blob, i_blob):
+        with flow.device_prior_placement(device_type, "0:0"):
+            y = flow.math.logical_and(x_blob, i_blob)
+        return y
+
+    if mirrored:
+
+        @flow.function(func_config)
+        def logical_and_fn(
+            x_def=flow.MirroredTensorDef(x.shape, dtype=flow.float),
+            y_def=flow.MirroredTensorDef(y.shape, dtype=flow.float),
+        ):
+            return do_logical_and(x_def, y_def)
+
+    else:
+
+        @flow.function(func_config)
+        def logical_and_fn(
+            x_def=flow.FixedTensorDef(x.shape, dtype=flow.float),
+            y_def=flow.FixedTensorDef(y.shape, dtype=flow.float),
+        ):
+            return do_logical_and(x_def, y_def)
+
+    return logical_and_fn
+
+def _compare_logical_and_with_tf(test_case, device_type, x_shape,
+        y_shape, mirrored=False):
+    x, y = _random_inputs(x_shape, y_shape)
+    logical_and_fn = _make_logical_and_fn(x, y, device_type, mirrored)
+
+    if mirrored:
+        of_y = logical_and_fn([x], [y]).get().ndarray_list()[0]
+    else:
+        of_y = logical_and_fn(x, y).get().ndarray()
+    tf_y = tf.math.logical_and(x, y)
+    test_case.assertTrue(np.array_equal(tf_y, of_y))
+
+def test_logical_and(test_case):
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["gpu"]
+    arg_dict["x_shape"] = [(3, 1, 4)]
+    arg_dict["y_shape"] = [(3,1, 4)]
+
+    for arg in GenArgList(arg_dict):
+        _compare_logical_and_with_tf(test_case, *arg)
+
+
+def test_logical_and_case_1(test_case):
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["gpu"]
+    arg_dict["x_shape"] = [(2, 10, 2)]
+    arg_dict["y_shape"] = [(10, 1)]
+    for arg in GenArgList(arg_dict):
+        _compare_logical_and_with_tf(test_case, *arg)
+
+
+def test_logical_and_case_2(test_case):
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["cpu", "gpu"]
+    arg_dict["x_shape"] = [(209, 80)]
+    arg_dict["y_shape"] = [(1, 80)]
+    arg_dict["mirrored"] = [True]
+    for arg in GenArgList(arg_dict):
+        _compare_logical_and_with_tf(test_case, *arg)
+
+def test_logical_and_case_3(test_case):
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["gpu"]
+    arg_dict["x_shape"] = [(2, 500, 20, 2)]
+    arg_dict["y_shape"] = [(2, 1, 1, 2)]
+    arg_dict["mirrored"] = [True]
+    for arg in GenArgList(arg_dict):
+        _compare_logical_and_with_tf(test_case, *arg)
+
+
