89e480dd8322fded5a936cd96fb6a2de9da9e594
Xinqi Li <lixinqi0703106@163.com>
2020-05-16 15:38:08 +0800

    GetDefaultCpuDeviceNum

--- oneflow/python/test/ops/test_eager.py
+++ oneflow/python/test/ops/test_eager.py
@@ -1,8 +1,14 @@
 import oneflow as flow
 import numpy as np
 
-def test_constant(test_case):
-    # TODO(lixinqi)
-    with flow.device("gpu:0"):
+def TestConstant(test_case, device_tag):
+    with flow.device("%s:0"%device_tag):
         x = flow.constant(0, shape=(10,), dtype=flow.float)
         test_case.assertTrue(np.array_equal(x.numpy(), np.zeros((10,), dtype=np.float32)))
+
+def test_cpu_constant(test_case):
+    TestConstant(test_case, 'cpu')
+
+
+def test_gpu_constant(test_case):
+    TestConstant(test_case, 'gpu')
