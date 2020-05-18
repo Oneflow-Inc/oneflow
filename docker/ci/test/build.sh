d0da9e85a576ce11650713e4e9aa37b5eb843ed3
Shenghang Tsai <jackalcooper@gmail.com>
2020-05-18 14:33:25 +0800

    add proxy

--- docker/ci/test/build.sh
+++ docker/ci/test/build.sh
@@ -1,3 +1,5 @@
 set -ex
 cd docker/ci/test
-docker build --rm -t oneflow-test .
+docker build --rm \
+    --build-arg http_proxy=${ONEFLOW_CI_HTTP_PROXY} --build-arg https_proxy=${ONEFLOW_CI_HTTPS_PROXY} \
+    -t oneflow-test .
