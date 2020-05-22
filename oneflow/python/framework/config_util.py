d27490f2f3a6853f3459f7e9af5673be43f953f8
ouyangyu <xuanjiuye@gmail.com>
2020-05-22 14:35:37 +0800

    enable_if draft

--- oneflow/python/framework/config_util.py
+++ oneflow/python/framework/config_util.py
@@ -11,18 +11,29 @@ from oneflow.python.oneflow_export import oneflow_export
 import oneflow.python.lib.core.pb_util as pb_util
 import oneflow.python.framework.session_context as session_ctx
 import oneflow.python.framework.hob as hob
+from oneflow.python.lib.core.enable_if import enable_if
 
-@oneflow_export('config.load_library', enable_if = hob.in_normal_mode & ~hob.session_initialized)
 def load_libray(val):
     assert type(val) is str
     sess = session_ctx.GetDefaultSession()
     sess.config_proto.load_lib_path.append(val)
 
-@oneflow_export('config.machine_num', enable_if = hob.in_normal_mode & ~hob.session_initialized)
+@oneflow_export('config.load_library')
+def api_load_libray():
+    return enable_if(
+        (load_libray, hob.in_normal_mode & ~hob.session_initialized)
+    )()
+
 def machine_num(val):
     sess = session_ctx.GetDefaultSession()
     assert type(val) is int
     sess.config_proto.resource.machine_num = val
+
+@oneflow_export('config.machine_num')
+def api_machine_num():
+    return enable_if(
+        (machine_num, hob.in_normal_mode & ~hob.session_initialized)
+    )()
 
 @oneflow_export('config.gpu_device_num')
 def gpu_device_num(val):
--- oneflow/python/ops/losses/add_loss.py
+++ oneflow/python/ops/losses/add_loss.py
@@ -5,6 +5,11 @@ import oneflow.python.framework.c_api_util as c_api_util
 import oneflow.python.framework.hob as hob
 from oneflow.python.lib.core.enable_if import enable_if
 
-@oneflow_export("losses.add_loss", enable_if = hob.in_global_mode & hob.is_trainable)
 def add_loss(loss):
     c_api_util.CurJobBuildAndInferCtx_AddLossLogicalBlobName(loss.logical_blob_name)
+
+@oneflow_export("losses.add_loss")
+def api_add_loss():
+    return enable_if(
+        (add_loss, hob.in_global_mode & hob.is_trainable)
+    )()
