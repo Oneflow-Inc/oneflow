f71d77c3aab41fba58655f6ff9307fc614229bc5
tsai <caishenghang@oneflow.org>
2020-05-22 22:19:23 +0800

    add TODO

--- oneflow/python/test/ops/test_oneflow_export.py
+++ oneflow/python/test/ops/test_oneflow_export.py
@@ -4,4 +4,5 @@ def AddLossUnderNormalMode():
     flow.losses.add_loss(None)
 
 def test_ApiAttributeError(test_case):
+    # TODO: update this testcase when enable_if ready
     test_case.assertRaises(AttributeError, AddLossUnderNormalMode)
