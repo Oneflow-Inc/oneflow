75c4d6b1dd0058ecb2c888bac9bed3650ad694df
tsai <caishenghang@oneflow.org>
2020-05-23 20:51:20 +0800

    restore enable_if case

--- oneflow/python/test/ops/test_oneflow_export.py
+++ oneflow/python/test/ops/test_oneflow_export.py
@@ -3,6 +3,5 @@ import oneflow as flow
 def AddLossUnderNormalMode():
     flow.losses.add_loss(None)
 
-def test_ApiAttributeError(test_case):
-    # TODO: update this testcase when enable_if ready
-    test_case.assertRaises(AttributeError, AddLossUnderNormalMode)
+def test_ApiNotImplementedError(test_case):
+    test_case.assertRaises(NotImplementedError, AddLossUnderNormalMode)
