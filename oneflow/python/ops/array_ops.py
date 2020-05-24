f6ffe5feca48ef5f0479020b50ca9dfb9526553d
tsai <caishenghang@oneflow.org>
2020-05-24 16:08:40 +0800

    rename input  x

--- oneflow/python/ops/array_ops.py
+++ oneflow/python/ops/array_ops.py
@@ -242,25 +242,25 @@ def transpose(a, perm=None, conjugate=False, name=None):
 
 
 @oneflow_export("slice")
-def slice(input_, begin, size, name=None):
+def slice(x, begin, size, name=None):
     r"""Extracts a slice from a tensor.
 
     Args:
-        input_: A `Blob`.
+        x: A `Blob`.
         begin: A list or a tuple, indicate each dimension slice begin, whose length must be equal
-            to input_'s number of dimensions, the first element of beign must be set to None.
+            to x's number of dimensions, the first element of beign must be set to None.
             (because oneflow internal slice op do not support slice at dim0 at present)
         size: A list or a tuple, indicate each dimension slice size, whose length must be equal
-            to input_'s number of dimensions, the first element of beign must be set to None.
+            to x's number of dimensions, the first element of beign must be set to None.
         name: A name for the operation (optional).
     """
-    ndims = len(input_.shape)
+    ndims = len(x.shape)
     assert (
         isinstance(begin, (list, tuple)) and len(begin) == ndims
-    ), "begin must be a list or tuple whose length is the same with input_'s number of dimensions."
+    ), "begin must be a list or tuple whose length is the same with x's number of dimensions."
     assert (
         isinstance(size, (list, tuple)) and len(size) == ndims
-    ), "size must be a list or tuple whose length is the same with input_'s number of dimensions."
+    ), "size must be a list or tuple whose length is the same with x's number of dimensions."
     # assert (
     #     begin[0] is None
     # ), "begin not support dim0 slice at present, the first element of begin must be set to None"
@@ -269,13 +269,13 @@ def slice(input_, begin, size, name=None):
     # ), "size not support dim0 slice at present, the first element of size must be set to None"
     if os.getenv("ENABLE_USER_OP") == 'True':
         slice_tup_list = []
-        for b, s, d in list(zip(begin, size, input_.shape)):
+        for b, s, d in list(zip(begin, size, x.shape)):
             begin, end, stride = None, None, 1
             if b is not None:
                 if b < -d or b > d - 1:
                     raise ValueError(
-                        "'i'th element of begin must be greater than or equal to negative input_'s 'i'th dimension "
-                        "and less than input_'s 'i'th dimension."
+                        "'i'th element of begin must be greater than or equal to negative x's 'i'th dimension "
+                        "and less than x's 'i'th dimension."
                     )
                 b = b + d if b < 0 else b
                 begin = b
@@ -284,7 +284,7 @@ def slice(input_, begin, size, name=None):
                     if b + s > d:
                         raise ValueError(
                             "the sum of 'i'th element of begin and 'i'th element of size must be "
-                            "less than or equal to input_'s 'i'th dimension."
+                            "less than or equal to x's 'i'th dimension."
                         )
                     end = b + s
                 elif s == -1:
@@ -294,15 +294,15 @@ def slice(input_, begin, size, name=None):
                         "elements of size must be an int that greater then 0 or equal to -1"
                     )
             slice_tup_list.append((begin, end, stride))
-        return slice_v2(input_, slice_tup_list, name=name)
+        return slice_v2(x, slice_tup_list, name=name)
     slice_conf_list = []
-    for b, s, d in list(zip(begin, size, input_.shape)):
+    for b, s, d in list(zip(begin, size, x.shape)):
         slice_conf = op_conf_util.DimSliceConf()
         if b is not None:
             if b < -d or b > d - 1:
                 raise ValueError(
-                    "'i'th element of begin must be greater than or equal to negative input_'s 'i'th dimension "
-                    "and less than input_'s 'i'th dimension."
+                    "'i'th element of begin must be greater than or equal to negative x's 'i'th dimension "
+                    "and less than x's 'i'th dimension."
                 )
             b = b + d if b < 0 else b
             slice_conf.start = b
@@ -311,7 +311,7 @@ def slice(input_, begin, size, name=None):
                 if b + s > d:
                     raise ValueError(
                         "the sum of 'i'th element of begin and 'i'th element of size must be "
-                        "less than or equal to input_'s 'i'th dimension."
+                        "less than or equal to x's 'i'th dimension."
                     )
                 slice_conf.end = b + s
             elif s == -1:
@@ -325,7 +325,7 @@ def slice(input_, begin, size, name=None):
 
     op_conf = op_conf_util.OperatorConf()
     setattr(op_conf, "name", name if name is not None else id_util.UniqueStr("Slice_"))
-    setattr(op_conf.slice_conf, "in", input_.logical_blob_name)
+    setattr(op_conf.slice_conf, "in", x.logical_blob_name)
     setattr(op_conf.slice_conf, "out", "out")
     op_conf.slice_conf.dim_slice_conf.extend(slice_conf_list)
 
@@ -337,10 +337,10 @@ def slice(input_, begin, size, name=None):
 
 
 @oneflow_export("slice_v2")
-def slice_v2(input, slice_tup_list, name=None):
+def slice_v2(x, slice_tup_list, name=None):
     r"""Extracts a slice from a tensor.
     Args:
-        input: A `Blob`.
+        x: A `Blob`.
         slice_tup_list: A list of tuple, indicate each dimension slice (begin, end, stride). 
             Note: The function don't support slice at dim0 for now , first element of slice_tup_list must be 
             (None, None, None).
@@ -350,14 +350,14 @@ def slice_v2(input, slice_tup_list, name=None):
     if not isinstance(name, str):
         raise ValueError('param "name" must be a string')
 
-    ndims = len(input.shape)
+    ndims = len(x.shape)
     if not isinstance(slice_tup_list, (list, tuple)) or len(slice_tup_list) > ndims:
         raise ValueError(
             'param "slice_tup_list" must be a list or tuple whose length should be '
-            "less than or equal to number of dimensions of input"
+            "less than or equal to number of dimensions of x"
         )
 
-    # if length of slice_tup_list is less than number of dimensions of input, fill it to length of ndims reduce 1
+    # if length of slice_tup_list is less than number of dimensions of x, fill it to length of ndims reduce 1
     if len(slice_tup_list) < ndims:
         slice_tup_list += [(None, None, None)] * (ndims - len(slice_tup_list))
 
@@ -366,7 +366,7 @@ def slice_v2(input, slice_tup_list, name=None):
     stride_list = []
     has_begin_list = []
     has_end_list = []
-    for slice_tup, dim in zip(slice_tup_list, input.shape):
+    for slice_tup, dim in zip(slice_tup_list, x.shape):
         if not isinstance(slice_tup, (tuple, list)):
             raise ValueError(
                 "element of slice_tup_list must be a list or tuple with form (begin, end, stride)"
@@ -391,7 +391,7 @@ def slice_v2(input, slice_tup_list, name=None):
     op = (
         flow.user_op_builder(name)
         .Op("slice_v2")
-        .Input("x", [input])
+        .Input("x", [x])
         .Output("y")
         .Attr("begin", begin_list, "AttrTypeListInt64")
         .Attr("end", end_list, "AttrTypeListInt64")
