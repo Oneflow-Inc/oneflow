b8bc83fa552f8df271e1d03e83d37fa9fe3bb0d5
Shenghang Tsai <jackalcooper@gmail.com>
2020-05-24 20:15:20 +0800

    API Docs (#2907)
    * docstring for relu
    
    * docstring for gelu
    
    * docstring for tanh and sigmoid
    
    * docstring add op
    
    * docstring for function
    
    * docstring for job_set_util.py
    
    * docstring for placement_util.py
    
    * update docstring for placement_util.py
    
    * docstring for check_point.py
    
    * docstring for input_blob_def
    
    * docstring for reduce_sum
    
    * docstring for initializer_util.py
    
    * docstring for array_ops.py
    
    * fix import
    
    * fix reshape
    
    * fix doc fmt
    
    * fix doc fmt
    
    * add basic index
    
    * add file
    
    * add docs
    
    * change upper case of oneflow name
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add MANIFEST.in
    
    * add docs
    
    * reorder index
    
    * add docs
    
    * add docs
    
    * add docs
    
    * fix docs
    
    * add docs
    
    * add docs
    
    * add example
    
    * add example
    
    * add docs
    
    * add docs
    
    * add docs
    
    * integrate make copy py scripts for conveniences
    
    * refine changes
    
    * refine docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * reorder and add add docs
    
    * add docs
    
    * add reference link
    
    * add docs
    
    * add docs
    
    * add docs
    
    * add docs
    
    * fix wording
    
    * rm doc entry of deprecated
    
    * rm function not exposed
    
    * fix wording
    
    * add index for losses
    
    * add docs
    
    * add & fix docs
    
    * add ref
    
    * fix wording
    
    * add docs
    
    * add docs
    
    * add reference
    
    * fix docs
    
    * add docs
    
    * add docs
    
    * reorder
    
    * move docs
    
    * add docs
    
    * add docs
    
    * better wording
    
    * rm duplicated relu if
    
    * Dev docstring tutorial (#2421)
    
    * add tutorials
    
    * Update conf.py
    
    * Update quick_start.md
    
    * Update alexnet.md
    
    * fix cap case
    
    * fix wording
    
    * fix docs
    
    * rm docs
    
    * update manifest
    
    * mv docs to new api
    
    * fix
    
    * updates for refactor
    
    * add doc string
    
    * fix docs index
    
    * build docs in ci
    
    * rm year
    
    * fix markdown docs
    
    * add year
    
    * rm duplicated
    
    * Delete MANIFEST.in
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * add deps
    
    * Delete build.sh
    
    * build docs in CI
    
    * change dir
    
    * fix path
    
    * fix func name
    
    * rename input  x
    
    * fix docs of ImageRandomFlip
    
    * fix docs of slice v2
    
    * check in auto indexing
    
    * rm index of keras.activations
    
    * rm entry of keras.activations
    
    Co-authored-by: XIE Xuan <xiexuanx2@gmail.com>
    Co-authored-by: Cai Shenghang <caishenghang@gmail.com>
    Co-authored-by: tsai <caishenghang@oneflow.org>
    Co-authored-by: QiaoJing <mir-of@outlook.com>

--- oneflow/python/ops/math_ops.py
+++ oneflow/python/ops/math_ops.py
@@ -455,6 +455,13 @@ def broadcast_floor_mod(x, y, name=None):
 
 @oneflow_export("math.tanh", "keras.activations.tanh")
 def tanh(x, name=None):
+    r"""Computes hyperbolic tangent of `x` element-wise.
+
+    Args:
+        x: Input `Blob`.
+    Returns:
+        A `Blob`
+    """
     if os.getenv("ENABLE_USER_OP") != 'True':
         op_conf = op_conf_util.OperatorConf()
         setattr(op_conf, "name", name if name is not None else id_util.UniqueStr("TanH_"))
@@ -479,6 +486,13 @@ def tanh(x, name=None):
 
 @oneflow_export("math.gelu", "keras.activations.gelu")
 def gelu(x, name=None):
+    r"""Gaussian Error Linear Units.
+
+    Args:
+        x: Input `Blob`.
+    Returns:
+        A `Blob`
+    """
     if os.getenv("ENABLE_USER_OP") == 'True':
         return (
             flow.user_op_builder(name if name is not None else id_util.UniqueStr("Gelu_"))
@@ -503,6 +517,13 @@ def gelu(x, name=None):
 
 @oneflow_export("math.relu", "nn.relu")
 def relu(x, name=None):
+    r"""ReLU activation
+
+    Args:
+        x: Input `Blob`.
+    Returns:
+        A `Blob`
+    """
     if os.getenv("ENABLE_USER_OP") != 'True':
         op_conf = op_conf_util.OperatorConf()
         setattr(op_conf, "name", name if name is not None else id_util.UniqueStr("Relu_"))
@@ -527,6 +548,13 @@ def relu(x, name=None):
 
 @oneflow_export("math.sigmoid")
 def sigmoid(x, name=None):
+    r"""Computes sigmoid of `x` element-wise.
+    
+    Args:
+        x: Input `Blob`.
+    Returns:
+        A `Blob`
+    """
     if os.getenv("ENABLE_USER_OP") != 'True':
         op_conf = op_conf_util.OperatorConf()
         setattr(
@@ -635,6 +663,14 @@ def unsorted_batch_segment_sum(data, segment_ids, num_segments, name=None):
 
 @oneflow_export("cast")
 def cast(x, dtype, name=None):
+    r"""Return a `Blob` of given data type `dtype` and indentical shape to `x`
+
+    Args:
+        x: a `Blob`.
+        dtype: a OneFlow data type. For instance, `oneflow.float`.
+    Returns:
+        A `Blob`
+    """
     if x.dtype == dtype:
         return x
     if name is None:
