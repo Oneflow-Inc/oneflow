b9a9c062693500c73d7269b133841910c3c36a0e
cheng cheng <472491134@qq.com>
2020-05-24 22:24:35 +0800

    remove EmptyTensorManager

--- oneflow/customized/data/dataset.h
+++ oneflow/customized/data/dataset.h
@@ -19,62 +19,6 @@ class Dataset {
 
 static constexpr int kOneflowDatasetSeed = 524287;
 
-template<typename LoadTarget>
-void PrepareEmptyTensor(LoadTarget& tensor, int32_t tensor_init_bytes);
-
-template<typename LoadTarget>
-class EmptyTensorManager final {
- public:
-  using LoadTargetUniquePtr = std::unique_ptr<LoadTarget>;
-  using LoadTargetSharedPtr = std::shared_ptr<LoadTarget>;
-  EmptyTensorManager(int64_t total_empty_size, int32_t tensor_init_bytes)
-      : tensor_init_bytes_(tensor_init_bytes), total_tensor_count_(0) {
-    for (int i = 0; i < total_empty_size; ++i) {
-      auto tensor_ptr = LoadTargetUniquePtr(new LoadTarget());
-      PrepareEmptyTensor<LoadTarget>(*tensor_ptr, tensor_init_bytes_);
-      empty_tensors_.push_back(std::move(tensor_ptr));
-    }
-    total_tensor_count_ += total_empty_size;
-  }
-  ~EmptyTensorManager() = default;
-
-  void Recycle(LoadTarget* tensor_ptr) {
-    LoadTargetUniquePtr recycle_ptr(tensor_ptr);
-    std::lock_guard<std::mutex> lock(empty_tensors_mutex_);
-    empty_tensors_.push_back(std::move(recycle_ptr));
-  }
-
-  LoadTargetSharedPtr Get() {
-    {
-      std::lock_guard<std::mutex> lock(empty_tensors_mutex_);
-      if (empty_tensors_.size() > 0) {
-        LoadTargetSharedPtr ret(empty_tensors_.back().release(),
-                                [this](LoadTarget* sample) { Recycle(sample); });
-        empty_tensors_.pop_back();
-        return ret;
-      }
-    }
-    auto tensor_ptr =
-        LoadTargetSharedPtr(new LoadTarget(), [this](LoadTarget* sample) { Recycle(sample); });
-    PrepareEmptyTensor<LoadTarget>(*tensor_ptr, tensor_init_bytes_);
-    total_tensor_count_++;
-    LOG(INFO) << "empty tensor is NOT enough , so we allocate one. The total tensor count is "
-              << total_tensor_count_;
-    return tensor_ptr;
-  }
-
- private:
-  const int32_t tensor_init_bytes_;
-
-  int64_t total_tensor_count_;
-
-  std::mutex empty_tensors_mutex_;
-  std::vector<LoadTargetUniquePtr> empty_tensors_;
-};
-
-template<>
-void PrepareEmptyTensor(TensorBuffer& tensor, int32_t tensor_init_bytes);
-
 }  // namespace oneflow
 
 #endif  // ONEFLOW_CUSTOMIZED_DATA_DATASET_H_
