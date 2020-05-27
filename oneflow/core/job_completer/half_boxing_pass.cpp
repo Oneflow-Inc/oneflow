fa861aef961cd43289ead48c420b538ed0a32ea1
daquexian <daquexian566@gmail.com>
2020-05-27 18:17:07 +0800

    rename variables

--- oneflow/core/job_completer/half_boxing_pass.cpp
+++ oneflow/core/job_completer/half_boxing_pass.cpp
@@ -16,34 +16,34 @@ class HalfBoxingPass final : public OpGraphPass {
 void HalfBoxingPass::Apply(const OpGraph& op_graph, JobBuilder* job_builder) const {
   op_graph.ForEachNode([&job_builder](OpNode* op_node) {
     // find cast_fp16_to_fp32 -> all_reduce(boxing) pattern
-    const OperatorConf& op_conf = op_node->op().op_conf();
-    if (!op_conf.has_collective_boxing_generic_conf()) { return; }
-    auto* prev_node = op_node->SoleInEdge()->src_node();
-    if (prev_node->out_edges().size() != 1) { return; }
-    auto prev_op_conf = prev_node->op().op_conf();
-    if (!(prev_op_conf.has_user_conf() && prev_op_conf.user_conf().op_type_name() == "Cast")) {
+    const OperatorConf& boxing_op_conf = op_node->op().op_conf();
+    if (!boxing_op_conf.has_collective_boxing_generic_conf()) { return; }
+    auto* cast_node = op_node->SoleInEdge()->src_node();
+    if (cast_node->out_edges().size() != 1) { return; }
+    auto cast_op_conf = cast_node->op().op_conf();
+    if (!(cast_op_conf.has_user_conf() && cast_op_conf.user_conf().op_type_name() == "Cast")) {
       return;
     }
-    auto prev_op_cast_conf = prev_op_conf.user_conf();
-    if (prev_op_cast_conf.attr().at("dtype").at_data_type() == DataType::kFloat) { return; }
+    auto cast_conf = cast_op_conf.user_conf();
+    if (cast_conf.attr().at("dtype").at_data_type() == DataType::kFloat) { return; }
 
     // replace boxing op input with cast op input
     {
-      auto new_op_conf = op_conf;
-      auto boxing_conf = new_op_conf.collective_boxing_generic_conf();
-      std::string cast_input = prev_op_cast_conf.input().at("in").s(0);
-      std::string boxing_input = boxing_conf.lbi().blob_name();
-      ReplaceInputLbnInOpCustomizedConf(&boxing_conf, "lbi", boxing_input, cast_input);
-      job_builder->MutOpsOnlyOnce({new_op_conf});
+      auto new_boxing_op_conf = boxing_op_conf;
+      std::string cast_input = cast_conf.input().at("in").s(0);
+      std::string boxing_input = boxing_op_conf.collective_boxing_generic_conf().lbi().blob_name();
+      ReplaceInputLbnInOpCustomizedConf(new_boxing_op_conf.mutable_collective_boxing_generic_conf(),
+                                        "lbi", boxing_input, cast_input);
+      job_builder->MutOpsOnlyOnce({new_boxing_op_conf});
     }
     // replace cast op input with boxing op output
     {
-      auto new_prev_op_conf = prev_op_conf;
-      std::string boxing_output = op_conf.collective_boxing_generic_conf().lbi().blob_name();
-      std::string cast_input = prev_op_cast_conf.input().at("in").s(0);
-      ReplaceInputLbnInOpCustomizedConf(new_prev_op_conf.mutable_user_conf(), "in_0", cast_input,
+      auto new_cast_op_conf = cast_op_conf;
+      std::string boxing_output = boxing_op_conf.collective_boxing_generic_conf().lbi().blob_name();
+      std::string cast_input = cast_conf.input().at("in").s(0);
+      ReplaceInputLbnInOpCustomizedConf(new_cast_op_conf.mutable_user_conf(), "in_0", cast_input,
                                         boxing_output);
-      job_builder->MutOpsOnlyOnce({new_prev_op_conf});
+      job_builder->MutOpsOnlyOnce({new_cast_op_conf});
     }
 
     // update all boxing op consumers
@@ -57,7 +57,7 @@ void HalfBoxingPass::Apply(const OpGraph& op_graph, JobBuilder* job_builder) con
       OperatorConf dst_op_conf = dst_node->op().op_conf();
       PbMessage* dst_op_type_conf =
           MutableMessageInPbMessage(&dst_op_conf, dst_op_conf.op_type_case());
-      std::string cast_output = prev_op_cast_conf.output().at("out").s(0);
+      std::string cast_output = cast_conf.output().at("out").s(0);
       // if (!TryUpdtBnVal4SepcialOpConf(dst_op_conf.op_type_case(), dst_op_type_conf, lbn, new_lbn,
       // dst_ibn)) {
       ReplaceInputLbnInOpCustomizedConf(dst_op_type_conf, dst_ibn, lbn, cast_output);
