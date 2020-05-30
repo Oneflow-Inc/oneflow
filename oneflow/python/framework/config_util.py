b1e1cdab05ace5d83e6bb39bcedb53f82b63baa0
ouyangyu <xuanjiuye@gmail.com>
2020-05-30 15:32:54 +0800

    if session has been initialized do nothing

--- oneflow/python/framework/config_util.py
+++ oneflow/python/framework/config_util.py
@@ -18,7 +18,7 @@ def load_library(val):
 
 @oneflow_export('config.machine_num')
 def api_machine_num(val):
-    return enable_if.unique(machine_num)(val)
+    return enable_if.unique(machine_num, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def machine_num(val):
@@ -33,7 +33,7 @@ def api_gpu_device_num(val):
     Args:
         val (int): number of GPUs. It is identical on every machine. In other words, you can't specify different number of GPUs you would like to use on each machine.
     """
-    return enable_if.unique(gpu_device_num)(val)
+    return enable_if.unique(gpu_device_num, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def gpu_device_num(val):
@@ -48,7 +48,7 @@ def api_cpu_device_num(val):
     Args:
         val (int): number of CPUs. It is identical on every machine.
     """
-    return enable_if.unique(cpu_device_num)(val)
+    return enable_if.unique(cpu_device_num, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def cpu_device_num(val):
@@ -58,7 +58,7 @@ def cpu_device_num(val):
 
 @oneflow_export('config.comm_net_worker_num')
 def api_comm_net_worker_num(val):
-    return enable_if.unique(comm_net_worker_num)(val)
+    return enable_if.unique(comm_net_worker_num, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def comm_net_worker_num(val):
@@ -68,7 +68,7 @@ def comm_net_worker_num(val):
 
 @oneflow_export('config.max_mdsave_worker_num')
 def api_max_mdsave_worker_num(val):
-    return enable_if.unique(max_mdsave_worker_num)(val)
+    return enable_if.unique(max_mdsave_worker_num, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def max_mdsave_worker_num(val):
@@ -78,7 +78,7 @@ def max_mdsave_worker_num(val):
 
 @oneflow_export('config.compute_thread_pool_size')
 def api_compute_thread_pool_size(val):
-    return enable_if.unique(compute_thread_pool_size)(val)
+    return enable_if.unique(compute_thread_pool_size, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def compute_thread_pool_size(val):
@@ -88,7 +88,7 @@ def compute_thread_pool_size(val):
 
 @oneflow_export('config.rdma_mem_block_mbyte')
 def api_rdma_mem_block_mbyte(val):
-    return enable_if.unique(rdma_mem_block_mbyte)(val)
+    return enable_if.unique(rdma_mem_block_mbyte, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def rdma_mem_block_mbyte(val):
@@ -98,7 +98,7 @@ def rdma_mem_block_mbyte(val):
 
 @oneflow_export('config.rdma_recv_msg_buf_mbyte')
 def api_rdma_recv_msg_buf_mbyte(val):
-    return enable_if.unique(rdma_recv_msg_buf_mbyte)(val)
+    return enable_if.unique(rdma_recv_msg_buf_mbyte, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def rdma_recv_msg_buf_mbyte(val):
@@ -108,7 +108,7 @@ def rdma_recv_msg_buf_mbyte(val):
 
 @oneflow_export('config.reserved_host_mem_mbyte')
 def api_reserved_host_mem_mbyte(val):
-    return enable_if.unique(reserved_host_mem_mbyte)(val)
+    return enable_if.unique(reserved_host_mem_mbyte, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def reserved_host_mem_mbyte(val):
@@ -118,7 +118,7 @@ def reserved_host_mem_mbyte(val):
 
 @oneflow_export('config.reserved_device_mem_mbyte')
 def api_reserved_device_mem_mbyte(val):
-    return enable_if.unique(reserved_device_mem_mbyte)(val)
+    return enable_if.unique(reserved_device_mem_mbyte, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def reserved_device_mem_mbyte(val):
@@ -128,7 +128,7 @@ def reserved_device_mem_mbyte(val):
 
 @oneflow_export('config.use_rdma')
 def api_use_rdma(val = True):
-    return enable_if.unique(use_rdma)(val = True)
+    return enable_if.unique(use_rdma, do_nothing)(val = True)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def use_rdma(val = True):
@@ -138,7 +138,7 @@ def use_rdma(val = True):
 
 @oneflow_export('config.thread_enable_local_message_queue')
 def api_thread_enable_local_message_queue(val):
-    return enable_if.unique(thread_enable_local_message_queue)(val)
+    return enable_if.unique(thread_enable_local_message_queue, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def thread_enable_local_message_queue(val):
@@ -148,7 +148,7 @@ def thread_enable_local_message_queue(val):
 
 @oneflow_export('config.enable_debug_mode')
 def api_enable_debug_mode(val):
-    return enable_if.unique(enable_debug_mode)(val)
+    return enable_if.unique(enable_debug_mode, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def enable_debug_mode(val):
@@ -158,7 +158,7 @@ def enable_debug_mode(val):
 
 @oneflow_export('config.save_downloaded_file_to_local_fs')
 def api_save_downloaded_file_to_local_fs(val = True):
-    return enable_if.unique(save_downloaded_file_to_local_fs)(val = True)
+    return enable_if.unique(save_downloaded_file_to_local_fs, do_nothing)(val = True)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def save_downloaded_file_to_local_fs(val = True):
@@ -168,7 +168,7 @@ def save_downloaded_file_to_local_fs(val = True):
 
 @oneflow_export('config.persistence_buf_byte')
 def api_persistence_buf_byte(val):
-    return enable_if.unique(persistence_buf_byte)(val)
+    return enable_if.unique(persistence_buf_byte, do_nothing)(val)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def persistence_buf_byte(val):
@@ -178,10 +178,15 @@ def persistence_buf_byte(val):
 
 @oneflow_export('config.collect_act_event')
 def api_collect_act_event(val = True):
-    return enable_if.unique(collect_act_event)(val = True)
+    return enable_if.unique(collect_act_event, do_nothing)(val = True)
 
 @enable_if.condition(hob.in_normal_mode & ~hob.session_initialized)
 def collect_act_event(val = True):
     sess = session_ctx.GetDefaultSession()
     assert type(val) is int
     sess.config_proto.profile_conf.collect_act_event = val
+
+@enable_if.condition(hob.in_normal_mode & hob.session_initialized)
+def do_nothing(*args, **kwargs):
+    print("Nothing happened because session has been initialized")
+    return False
