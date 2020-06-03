695fbf6ba4295f1921700ae2505b97964c0b4bb8
leaves-zwx <kunta0932@gmail.com>
2020-06-03 15:24:18 +0800

    reduce ops api

--- oneflow/python/ops/reduce_ops.py
+++ oneflow/python/ops/reduce_ops.py
@@ -1,90 +1,93 @@
-from __future__ import absolute_import
-
-from oneflow.python.oneflow_export import oneflow_export
-import oneflow.python.ops.user_op_builder as user_op_builder
+import oneflow as flow
 import oneflow.python.framework.id_util as id_util
 
-import oneflow as flow
+from oneflow.python.oneflow_export import oneflow_export
+
+
+def _check_name(name, unique_name):
+    if name is None:
+        return id_util.UniqueStr(unique_name)
+    return name
+
+
+def _check_axis(axis, shape):
+    if axis is None:
+        axis = list(range(len(shape)))
+
+    if isinstance(axis, int):
+        axis = [axis]
+
+    assert isinstance(axis, (list, tuple)), "Invalid axis {}".format(axis)
+    return axis
+
+
+def _do_reduce(x, name, op_type_name, keepdims, axis):
+    op = (
+        flow.user_op_builder(name)
+        .Op(op_type_name)
+        .Input("input_tensor", [x])
+        .Output("output_tensor")
+        .Attr("axis", axis, "AttrTypeListInt32")
+        .Attr("keepdims", keepdims, "AttrTypeBool")
+        .Build()
+    )
+    return op.InferAndTryRun().SoleOutputBlob()
+
 
 @oneflow_export("math.reduce_any")
 def reduce_any(x, axis=None, keepdims=False, name=None):
     name = _check_name(name, "ReduceAny_")
-    if axis is None:
-        axis = []
-    elif isinstance(axis, list) and len(axis) == 0:
+    axis = _check_axis(axis, x.shape)
+    if len(axis) == 0:
         return flow.math.not_equal(x, flow.constant_scalar(value=0.0, dtype=x.dtype))
-    return _get_remote_blob(x, name, "reduce_any", keepdims, axis)
+    return _do_reduce(x, name, "reduce_any", keepdims, axis)
+
 
 @oneflow_export("math.reduce_min")
 def reduce_min(x, axis=None, keepdims=False, name=None):
     name = _check_name(name, "ReduceMin_")
-    if axis is None:
-        axis = []
-    elif isinstance(axis, list) and len(axis) == 0:
+    axis = _check_axis(axis, x.shape)
+    if len(axis) == 0:
         return x
-    return _get_remote_blob(x, name, "reduce_min", keepdims, axis)
+    return _do_reduce(x, name, "reduce_min", keepdims, axis)
+
 
 @oneflow_export("math.reduce_prod")
 def reduce_prod(x, axis=None, keepdims=False, name=None):
     name = _check_name(name, "ReduceProd_")
-    if axis is None:
-        axis = []
-    elif isinstance(axis, list) and len(axis) == 0:
+    axis = _check_axis(axis, x.shape)
+    if len(axis) == 0:
         return x
-    return _get_remote_blob(x, name, "reduce_prod", keepdims, axis)
+    return _do_reduce(x, name, "reduce_prod", keepdims, axis)
+
 
 @oneflow_export("math.reduce_all")
 def reduce_all(x, axis=None, keepdims=False, name=None):
     name = _check_name(name, "ReduceAll_")
-    if axis is None:
-        axis = []
-    elif isinstance(axis, list) and len(axis) == 0:
+    axis = _check_axis(axis, x.shape)
+    if len(axis) == 0:
         return flow.math.not_equal(x, flow.constant_scalar(value=0.0, dtype=x.dtype))
-    return _get_remote_blob(x, name, "reduce_all", keepdims, axis)
+    return _do_reduce(x, name, "reduce_all", keepdims, axis)
+
 
 @oneflow_export("math.reduce_euclidean_norm")
 def reduce_euclidean_norm(input_tensor, axis=None, keepdims=False, name=None):
-    return flow.math.sqrt(
-        flow.math.reduce_sum(
-            flow.math.square(input_tensor),
-            axis,
-            keepdims
-        )
-    )
+    return flow.math.sqrt(flow.math.reduce_sum(flow.math.square(input_tensor), axis, keepdims))
+
 
 @oneflow_export("math.reduce_logsumexp")
 def reduce_logsumexp(input_tensor, axis=None, keepdims=False, name=None):
-    return flow.math.log(
-        flow.math.reduce_sum(
-            flow.math.exp(input_tensor),
-            axis,
-            keepdims
-        )
-    )
+    return flow.math.log(flow.math.reduce_sum(flow.math.exp(input_tensor), axis, keepdims))
+
 
 @oneflow_export("math.reduce_std")
 def reduce_std(input_tensor, axis=None, keepdims=False, name=None):
-    return flow.math.sqrt(
-        flow.math.reduce_variance(input_tensor, axis, keepdims)
-    )
+    return flow.math.sqrt(flow.math.reduce_variance(input_tensor, axis, keepdims))
+
 
 @oneflow_export("math.reduce_variance")
 def reduce_variance(input_tensor, axis=None, keepdims=False, name=None):
     return flow.math.subtract(
-        flow.math.reduce_mean(flow.math.square(
-            input_tensor), axis, keepdims),
-        flow.math.square(flow.math.reduce_mean(
-            input_tensor, axis, keepdims))
+        flow.math.reduce_mean(flow.math.square(input_tensor), axis, keepdims),
+        flow.math.square(flow.math.reduce_mean(input_tensor, axis, keepdims)),
     )
-
-
-def _get_remote_blob(x, name=None, op_name=None, keepdims=False, axis=None):
-    return flow.user_op_builder(name).Op(op_name)\
-        .Input("input_tensor", [x])\
-        .Output("output_tensor")\
-        .Attr("axis", axis, "AttrTypeListInt32")\
-        .Attr("keepdims", keepdims, "AttrTypeBool")\
-        .Build().InferAndTryRun().RemoteBlobList()[0]
-
-def _check_name(name, unique_name):
-    return name if name is not None else id_util.UniqueStr(unique_name)
