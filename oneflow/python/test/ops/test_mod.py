177efc2560143d8a707410ec4b9ce7ccc708c412
ouyangyu <xuanjiuye@gmail.com>
2020-06-07 21:47:01 +0800

    Modify np.array_equal to np.allclose

--- oneflow/python/test/ops/test_mod.py
+++ oneflow/python/test/ops/test_mod.py
@@ -13,7 +13,7 @@ def test_naive(test_case):
     y = np.random.rand(5, 2).astype(np.float32)
     z = None
     z = ModJob(x, y).get().ndarray()
-    test_case.assertTrue(np.array_equal(z, x % y))
+    test_case.assertTrue(np.allclose(z, x % y))
 
 def test_broadcast(test_case):
     @flow.function(func_config)
@@ -24,7 +24,7 @@ def test_broadcast(test_case):
     y = np.random.rand(1, 2).astype(np.float32)
     z = None
     z = ModJob(x, y).get().ndarray()
-    test_case.assertTrue(np.array_equal(z, x % y))
+    test_case.assertTrue(np.allclose(z, x % y))
 
 def test_xy_mod_x1(test_case):
     GenerateTest(test_case, (64, 64), (64, 1))
@@ -46,4 +46,4 @@ def GenerateTest(test_case, a_shape, b_shape):
     a = np.random.rand(*a_shape).astype(np.float32)
     b = np.random.rand(*b_shape).astype(np.float32)
     y = ModJob(a, b).get().ndarray()
-    test_case.assertTrue(np.array_equal(y, a % b))
+    test_case.assertTrue(np.allclose(y, a % b))
