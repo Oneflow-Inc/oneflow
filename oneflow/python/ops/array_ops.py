ebd552363af49d15a8e64bdc71fcbaa437c492b0
leaves-zwx <kunta0932@gmail.com>
2020-06-21 18:25:10 +0800

    tensor_list_split

--- oneflow/python/ops/array_ops.py
+++ oneflow/python/ops/array_ops.py
@@ -622,21 +622,24 @@ def where(condition, x=None, y=None, name=None):
         raise ValueError("it is not supported when exactly one of x or y is non-None")
 
 
-@oneflow_export("piece_slice")
-def piece_slice(inputs, output_size, name=None):
-    assert inputs.shape[0] == output_size
+@oneflow_export("tensor_list_split")
+def tensor_list_split(input_tensor_list, name=None):
+    if name is None:
+        name = id_util.UniqueStr("TensorListSplit_")
+
+    output_size = input_tensor_list.shape[0]
     op_conf = op_conf_util.OperatorConf()
-    setattr(
-        op_conf, "name", name if name is not None else id_util.UniqueStr("PieceSlice_")
+    setattr(op_conf, "name", name)
+    setattr(op_conf.tensor_list_split_conf, "in", input_tensor_list.unique_name)
+    op_conf.tensor_list_split_conf.out.extend(
+        ["out_{}".format(i) for i in range(output_size)]
     )
-    setattr(op_conf.piece_slice_conf, "in", inputs.unique_name)
-    op_conf.piece_slice_conf.out.extend(["out_" + str(i) for i in range(output_size)])
     compile_context.CurJobAddOp(op_conf)
     ret = []
     for i in range(output_size):
         out_lbi = logical_blob_id_util.LogicalBlobId()
         setattr(out_lbi, "op_name", op_conf.name)
-        setattr(out_lbi, "blob_name", "out_" + str(i))
+        setattr(out_lbi, "blob_name", "out_{}".format(i))
         ret.append(remote_blob_util.RemoteBlob(out_lbi))
     return tuple(ret)
 
