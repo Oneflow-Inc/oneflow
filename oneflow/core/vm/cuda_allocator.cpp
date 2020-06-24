d4eaeb5746832aaaef3c863910e4db47ac5b448d
cheng cheng <472491134@qq.com>
2020-06-24 23:03:36 +0800

    vm::cuda_allocator interface

--- oneflow/core/vm/cuda_allocator.cpp
+++ oneflow/core/vm/cuda_allocator.cpp
@@ -4,14 +4,66 @@
 namespace oneflow {
 namespace vm {
 
-void CudaAllocator::Allocate(char** mem_ptr, std::size_t size) {
+CudaAllocator::CudaAllocator(int64_t device_id) : Allocator(), device_id_(device_id), recycle_piece_list_(nullptr) {
   cudaSetDevice(device_id_);
-  CudaCheck(cudaMalloc(mem_ptr, size));
+  size_t free_bytes = -1;
+  size_t total_bytes = -1;
+  const size_t remain_bytes = 10 * 1048576;
+  CudaCheck(cudaMemGetInfo(&free_bytes, &total_bytes));
+  CHECK_GT(free_bytes, remain_bytes); // free bytes should greater than 10MiB
+  total_memory_bytes_ = std::max(free_bytes - remain_bytes, static_cast<size_t>(free_bytes * 0.95));
+  CudaCheck(cudaMalloc(&mem_ptr_, total_memory_bytes_));
+
+  bins_.resize(kBinNumSize);
+  for(int i = 0; i < kBinNumSize; ++i) {
+    size_t bin_size = BinSize4BinNum(i);
+    bins_.at(i).size = bin_size;
+    CHECK_EQ(BinNum4BinSize(bin_size), i);
+    CHECK_EQ(BinNum4BinSize(bin_size + kCudaMemAllocAlignSize - 1), i);
+    CHECK_EQ(BinNum4BinSize(bin_size * 2 - 1), i);
+    CHECK_EQ(BinNum4BinSize(bin_size * 2), i == (kBinNumSize - 1) ? i : i + 1);
+  }
+
+  Piece* piece = AllocatePiece();
+  piece->size = total_memory_bytes_;
+  piece->ptr = mem_ptr_;
+  piece->prev = nullptr;
+  piece->next = nullptr;
+  piece->is_free = true;
+  piece->bin_num = kInvalidBinNum;
+
+  InsertPiece2Bin(piece);
+}
+
+CudaAllocator::~CudaAllocator() {
+  cudaSetDevice(device_id_);
+  CudaCheck(cudaFree(mem_ptr_));
+}
+
+void CudaAllocator::InsertPiece2Bin(Piece* piece) {
+  CHECK(piece->is_free && piece->bin_num == kInvalidBinNum);
+  int32_t bin_num = BinNum4BinSize(piece->size);
+  piece->bin_num = bin_num;
+  CHECK(bins_.at(bin_num).pieces.insert(piece).second);
+}
+
+CudaAllocator::Piece* CudaAllocator::AllocatePiece() {
+  if(recycle_piece_list_) {
+    Piece* ret = recycle_piece_list_;
+    recycle_piece_list_ = recycle_piece_list_->next;
+    return ret;
+  } else {
+    pieces_.push_back(Piece());
+    return &pieces_.at(pieces_.size() - 1);
+  }
+}
+
+void CudaAllocator::Allocate(char** mem_ptr, std::size_t size) {
+  TODO();
 }
 
 void CudaAllocator::Deallocate(char* mem_ptr, std::size_t size) {
-  cudaSetDevice(device_id_);
-  CudaCheck(cudaFree(mem_ptr));
+  TODO();
 }
 
 }  // namespace vm
--- oneflow/core/vm/cuda_allocator.h
+++ oneflow/core/vm/cuda_allocator.h
@@ -3,20 +3,66 @@
 
 #include <cstdint>
 #include "oneflow/core/vm/allocator.h"
+#include "oneflow/core/common/util.h"
 
 namespace oneflow {
 namespace vm {
 
 class CudaAllocator final : public Allocator {
  public:
-  explicit CudaAllocator(int64_t device_id) : Allocator(), device_id_(device_id) {}
-  ~CudaAllocator() override = default;
+  explicit CudaAllocator(int64_t device_id);
+  ~CudaAllocator() override;
 
   void Allocate(char** mem_ptr, std::size_t size) override;
   void Deallocate(char* mem_ptr, std::size_t size) override;
 
  private:
+  static constexpr int32_t kInvalidBinNum = -1;
+  static constexpr int32_t kBinNumSize = 20;
+
+  struct Piece {
+    size_t size = 0;
+    void* ptr = nullptr;
+    bool is_free = false;
+    Piece* prev = nullptr;
+    Piece* next = nullptr;
+    int32_t bin_num = kInvalidBinNum;
+  };
+
+  struct Bin {
+    size_t size = 0;
+
+    struct PieceCmp {
+      bool operator()(const Piece* lhs, const Piece* rhs) const {
+        if (lhs->size != rhs->size) {
+          return lhs->size < rhs->size;
+        }
+        return lhs->ptr < rhs->ptr;
+      }
+    };
+    std::set<Piece*, PieceCmp> pieces;
+  };
+
+  size_t BinSize4BinNum(int32_t bin_num) {
+    return kCudaMemAllocAlignSize << bin_num;
+  }
+
+  int32_t BinNum4BinSize(size_t size) {
+    uint64_t value = std::max(size, kCudaMemAllocAlignSize) >> 9;
+    return std::min(kBinNumSize - 1, static_cast<int32_t>(63 ^ __builtin_clzll(value)));
+  }
+  
+  void InsertPiece2Bin(Piece* piece);
+  Piece* AllocatePiece();
+
   int64_t device_id_;
+  size_t total_memory_bytes_;
+  void* mem_ptr_; // maybe ptr list for dynamic growth
+
+  std::vector<Bin> bins_;
+  std::vector<Piece> pieces_;
+  HashMap<void*, Piece*> ptr2piece_;
+  Piece* recycle_piece_list_;
 };
 
 }  // namespace vm
