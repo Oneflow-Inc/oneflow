57d9008f232492c304ac989f6fcac7a05a40496e
liujuncheng <liujuncheng1022@gmail.com>
2020-06-24 16:00:01 +0800

    parallel_conf

--- oneflow/python/test/ops/eager_nccl_all_reduce_demo.py
+++ oneflow/python/test/ops/eager_nccl_all_reduce_demo.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     @flow.function(func_config)
     def test_job(x=flow.FixedTensorDef((10000,), dtype=flow.float)):
         return flow.eager_nccl_all_reduce(
-            x, device_set=((0, 0), (0, 1), (0, 2), (0, 3))
+            x, parallel_conf='''{
+                device_name: "0:gpu:0-3"
+            }'''
         )
 
     for _ in range(10):
