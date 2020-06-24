675851a3a6aad631cb89517729a872c4235240a6
leaves-zwx <kunta0932@gmail.com>
2020-06-24 12:33:38 +0800

    update test

--- oneflow/python/test/ops/test_global_function_input_output.py
+++ oneflow/python/test/ops/test_global_function_input_output.py
@@ -3,33 +3,37 @@ import numpy as np
 import os
 
 
-# def test_lazy_output(test_case):
-#     flow.clear_default_session()
-#     flow.enable_eager_execution(False)
-#     func_config = flow.FunctionConfig()
-#     func_config.default_data_type(flow.float)
-#     func_config.default_placement_scope(flow.device_prior_placement("gpu", "0:0"))
+def test_lazy_input_output(test_case):
+    flow.clear_default_session()
+    flow.enable_eager_execution(False)
 
-#     @flow.global_function(func_config)
-#     def foo_job():
-#         x = flow.constant(1, shape=(2, 5), dtype=flow.float)
-#         return x
+    @flow.global_function()
+    def foo_job(input_def=flow.FixedTensorDef(shape=(2, 5))):
+        var = flow.get_variable(
+            name="var",
+            shape=(2, 5),
+            dtype=flow.float,
+            initializer=flow.ones_initializer(),
+        )
+        output = var + input_def
+        return output
 
-#     ret = foo_job().get()
-#     print(type(ret))
+    checkpoint = flow.train.CheckPoint()
+    checkpoint.init()
+    input = np.arange(10).reshape(2, 5).astype(np.single)
+    ret = foo_job(input).get()
+    output = input + np.ones(shape=(2, 5), dtype=np.single)
+    test_case.assertTrue(np.array_equal(output, ret.ndarray()))
 
 
 def test_eager_output(test_case):
     if os.getenv("ENABLE_USER_OP") != "True":
         return
 
-    # flow.clear_default_session()
+    flow.clear_default_session()
     flow.enable_eager_execution()
-    func_config = flow.FunctionConfig()
-    func_config.default_data_type(flow.float)
-    # func_config.default_placement_scope(flow.device_prior_placement("gpu", "0:0"))
 
-    @flow.global_function(func_config)
+    @flow.global_function()
     def foo_job():
         x = flow.constant(1, shape=(2, 5), dtype=flow.float)
         # print(x.numpy_mirrored_list())
