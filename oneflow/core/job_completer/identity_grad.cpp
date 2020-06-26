fc4edb4ab94133067d906bd2afbb7614dd8e3c5d
Li Xinqi <lixinqi2010@gmail.com>
2020-06-26 13:31:19 +0800

    EagerInplaceIdentity (#3081)
    Former-commit-id: f9b31157462dc8d9f3ea7b26c556304edf41464d

--- oneflow/core/job_completer/identity_grad.cpp
+++ oneflow/core/job_completer/identity_grad.cpp
@@ -84,4 +84,25 @@ REGISTER_OP_GRAD(OperatorConf::kCastFromMirroredConf, &GenerateCastFromMirroredB
 
 }  // namespace
 
+namespace {
+
+void GenEagerInplaceIdentityBackwardOpConf(
+    const Operator& op, std::vector<OperatorConf>* op_confs,
+    const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
+  if (DiffLbi4BnInOp("in") != nullptr) {
+    OperatorConf grad_op{};
+    grad_op.set_name("System-AutoGrad-" + op.op_name());
+    auto* eager_inplace_identity_conf = grad_op.mutable_eager_inplace_identity_conf();
+    eager_inplace_identity_conf->set_in(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
+    eager_inplace_identity_conf->set_out("out");
+    op_confs->push_back(grad_op);
+    DiffLbi4BnInOp("in")->set_op_name(grad_op.name());
+    DiffLbi4BnInOp("in")->set_blob_name(eager_inplace_identity_conf->out());
+  }
+}
+
+REGISTER_OP_GRAD(OperatorConf::kEagerInplaceIdentityConf, &GenEagerInplaceIdentityBackwardOpConf);
+
+}  // namespace
+
 }  // namespace oneflow
