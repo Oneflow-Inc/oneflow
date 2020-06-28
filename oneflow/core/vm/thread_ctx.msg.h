61f9d6c17b5d53312b353ef215ce4aedcb59e613
lixinqi <lixinqi0703106@163.com>
2020-06-28 12:54:39 +0800

    OneflowVm::TryReceiveAndRun

--- oneflow/core/vm/thread_ctx.msg.h
+++ oneflow/core/vm/thread_ctx.msg.h
@@ -20,7 +20,8 @@ OBJECT_MSG_BEGIN(ThreadCtx);
   // links
   OBJECT_MSG_DEFINE_LIST_LINK(thread_ctx_link);
   OBJECT_MSG_DEFINE_LIST_HEAD(Stream, thread_ctx_stream_link, stream_list);
-  OBJECT_MSG_DEFINE_CONDITION_LIST_HEAD(Instruction, pending_instruction_link, pending_instruction_list);
+  OBJECT_MSG_DEFINE_CONDITION_LIST_HEAD(Instruction, pending_instruction_link,
+                                        pending_instruction_list);
 
   PRIVATE ObjectMsgConditionListStatus ReceiveAndRun();
   PRIVATE ObjectMsgConditionListStatus TryReceiveAndRun();
