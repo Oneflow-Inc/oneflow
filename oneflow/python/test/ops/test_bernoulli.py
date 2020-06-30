38b4b50e4eacf6e26c8ab2e69ca063fcd9314c94
cheng cheng <472491134@qq.com>
2020-06-30 23:26:52 +0800

    python test scripts format

--- oneflow/python/test/ops/test_bernoulli.py
+++ oneflow/python/test/ops/test_bernoulli.py
@@ -1,6 +1,7 @@
 import numpy as np
 import oneflow as flow
 
+
 def test_bernoulli(test_case):
     func_config = flow.FunctionConfig()
     func_config.default_distribute_strategy(flow.distribute.consistent_strategy())
@@ -22,6 +23,7 @@ def test_bernoulli(test_case):
     y = BernoulliJob(x).get().ndarray()
     test_case.assertTrue(np.allclose(y.sum(), 50.0, rtol=0.1, atol=5.0))
 
+
 def test_bernoulli_mirrored(test_case):
     func_config = flow.FunctionConfig()
     func_config.default_data_type(flow.float)
@@ -41,4 +43,3 @@ def test_bernoulli_mirrored(test_case):
     x = np.ones((10, 10), dtype=np.float32) * 0.5
     y = BernoulliJob([x]).get().ndarray_list()[0]
     test_case.assertTrue(np.allclose(y.sum(), 50.0, rtol=0.1, atol=5.0))
-
