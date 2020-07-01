abaa1c3d46aa78de31694c149bbb5a2647dcba59
ouyangyu <xuanjiuye@gmail.com>
2020-07-01 20:59:58 +0800

    Change flow.function to flow.global_function

--- oneflow/python/test/ops/test_compat_conv2d.py
+++ oneflow/python/test/ops/test_compat_conv2d.py
@@ -48,7 +48,7 @@ def compare_with_tensorflow(
     func_config.train.primary_lr(1e-4)
     func_config.train.model_update_conf(dict(naive_conf={}))
 
-    @flow.function(func_config)
+    @flow.global_function(func_config)
     def ConvJob():
         with flow.device_prior_placement(device_type, "0:0"):
             x = flow.get_variable(
