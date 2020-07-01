f5fc2476538bbd28210c1fedb1da4e4344fa1ab1
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-01 23:54:49 +0800

    Revert "named pid"
    This reverts commit 20f198b37b07ddc35b2027693a36c55e0db01946.

--- ci/test/parallel_1node_op_test.sh
+++ ci/test/parallel_1node_op_test.sh
@@ -6,13 +6,14 @@ docker run --shm-size=8g --rm \
     -w $PWD -v $PWD:$PWD -v /dataset:/dataset -v /model_zoo:/model_zoo \
     --env ENABLE_USER_OP=True \
     oneflow-test bash ci/test/1node_op_test.sh &
-pid_with_user_op=($!)
+pids+=($!)
 
 docker run --shm-size=8g --rm \
     -w $PWD -v $PWD:$PWD -v /dataset:/dataset -v /model_zoo:/model_zoo \
     --env ENABLE_USER_OP=False \
     oneflow-test bash ci/test/1node_op_test.sh
-pid_without_user_op=($!)
+pids+=($!)
 
-wait $pid_with_user_op
-wait $pid_without_user_op
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
