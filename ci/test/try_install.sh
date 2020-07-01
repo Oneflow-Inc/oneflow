4eed6a27d0ddd9f512ef4c2ef9f72a39b041d27d
tsai <caishenghang@oneflow.org>
2020-07-02 05:09:37 +0800

    fix try install.sh

--- ci/test/try_install.sh
+++ ci/test/try_install.sh
@@ -2,9 +2,11 @@
 set -xe
 
 src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
-wheel_path=${ONEFLOW_WHEEL_PATH:-"$PWD/ci_tmp/oneflow-0.0.1-cp36-cp36m-linux_x86_64.whl"}
+wheel_path=${ONEFLOW_WHEEL_PATH:-"$PWD/ci_tmp"}
 
-if [ -f "$wheel_path" ]; then
+if [ -d "$wheel_path" ]; then
+    pip3 install --user "$wheel_path/*.whl"
+elif [ -e "$wheel_path" ]; then
     pip3 install --user "$wheel_path"
 elif [ -d "$src_dir" ]; then
     pip3 install -e "$src_dir" --user
