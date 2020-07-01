5161bbc544375111b55e9f9126cfda8d9de0a939
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-01 23:25:20 +0800

    more idiomatic bash parallel

---
+++ ci/test/parallel_1node_op_test.sh
@@ -0,0 +1,19 @@
+set -xeu
+
+pids=()
+
+docker run --shm-size=8g --rm \
+    -w $PWD -v $PWD:$PWD -v /dataset:/dataset -v /model_zoo:/model_zoo \
+    --env ENABLE_USER_OP=True \
+    oneflow-test bash ci/test/1node_op_test.sh &
+pids+=($!)
+
+docker run --shm-size=8g --rm \
+    -w $PWD -v $PWD:$PWD -v /dataset:/dataset -v /model_zoo:/model_zoo \
+    --env ENABLE_USER_OP=False \
+    oneflow-test bash ci/test/1node_op_test.sh
+pids+=($!)
+
+for pid in "${pids[@]}"; do
+  wait "$pid"
+done
