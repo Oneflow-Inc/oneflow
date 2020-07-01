fefb670fa9d1f1610f5bb82cb7ea981703724f7c
tsai <caishenghang@oneflow.org>
2020-07-02 01:15:19 +0800

    make ci script runnable locally

--- ci/test/1node_op_test.sh
+++ ci/test/1node_op_test.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 set -xe
 
-pip3 install --user ci_tmp/*.whl
+src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
+wheel_path=${ONEFLOW_WHEEL_PATH:-"ci_tmp/*.whl"}
+test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"/test_tmp_dir"}
 
-cp -r oneflow/python/test /test_dir
-cd /test_dir
+if [ -f "$wheel_path" ]; then
+    pip3 install --user "$wheel_path"
+elif [ -d "$src_dir" ]; then
+    pip3 install -e "$src_dir" --user
+else
+    echo "wheel not found: $wheel_path, src dir not found: $src_dir, continue anyway..."
+fi
+
+
+rm -rf $test_tmp_dir
+cp -r $src_dir/oneflow/python/test $test_tmp_dir
+cd $test_tmp_dir
 
 python3 ops/1node_test.py
