99736d11917e0aee41c3c938e587a3dede0db4db
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-01 23:56:14 +0800

    seperate exec test

--- ci/test/parallel_1node_op_test.sh
+++ ci/test/parallel_1node_op_test.sh
@@ -5,6 +5,12 @@ pids=()
 docker run --shm-size=8g --rm \
     -w $PWD -v $PWD:$PWD -v /dataset:/dataset -v /model_zoo:/model_zoo \
     --env ENABLE_USER_OP=True \
+    oneflow-test bash ci/test/exe_test.sh &
+pids+=($!)
+
+docker run --shm-size=8g --rm \
+    -w $PWD -v $PWD:$PWD -v /dataset:/dataset -v /model_zoo:/model_zoo \
+    --env ENABLE_USER_OP=True \
     oneflow-test bash ci/test/1node_op_test.sh &
 pids+=($!)
 
