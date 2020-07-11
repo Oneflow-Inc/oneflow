70737d7811cf699b6bbc2be959433db825e4f090
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-11 22:54:29 +0800

    Dev always build oneflow (#3150)
    * fix condition
    
    * fix error msg
    
    * fix compat conv
    
    * run 2 docker containers
    
    * rm redudant script
    
    * more idiomatic bash parallel
    
    * named pid
    
    * Revert "named pid"
    
    This reverts commit 20f198b37b07ddc35b2027693a36c55e0db01946.
    
    * seperate exec test
    
    * rm env
    
    * make ci script runnable locally
    
    * check in new scripts
    
    * fmt
    
    * fix try install
    
    * fix try install.sh
    
    * fix install.sh
    
    * fix install.sh
    
    * add bash -c
    
    * add img name
    
    * fix
    
    * skip conv cpu test case always fail
    
    * rm 3rd party
    
    * add ignore
    
    * rm deps in ubuntu docker
    
    * build works
    
    * add ignore
    
    * empty new line
    
    * fix glog
    
    * update manylinux
    
    * add fake target
    
    * update protobuf
    
    * fix protobuf
    
    * check in
    
    * revert changes
    
    * copy with relative path
    
    * refactor cmake function
    
    * rm comments
    
    * revert wheel sh
    
    * update build wheel.sh
    
    * update dockerfile
    
    * fix dockerfile
    
    * add warning for proto and swig file gen
    
    * mv mkl export up
    
    * revert
    
    * use for_ci flag to skip oneflow
    
    * add for ci flag
    
    * fix glog compile
    
    * fix protobuf
    
    * revert
    
    * revert
    
    * add script
    
    * fix gflags
    
    * add header list file
    
    * rename and add grpc
    
    * add cub
    
    * add opencv
    
    * add for ci flag
    
    * add comment stating why skip oneflow cmake
    
    * add dedicate flag for building oneflow
    
    * add extra check
    
    * add ignore
    
    Co-authored-by: tsai <caishenghang@oneflow.org>

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -29,7 +29,7 @@ function(remove_test_cpp file_group)
     STR_ENDSWITH(${cc_name_we} _test is_test_cc)
     if(${is_test_cc})
       list(REMOVE_ITEM ${file_group} ${cc})
-    endif()    
+    endif()
   endforeach()
   set(${file_group} ${${file_group}} PARENT_SCOPE)
 endfunction()
@@ -82,10 +82,52 @@ macro(copy_files file_paths source_dir dest_dir target)
     foreach(file ${file_paths})
       file(RELATIVE_PATH rel_path "${source_dir}" ${file})
       add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND "${CMAKE_COMMAND}" -E copy
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "${file}"
         "${dest_dir}/${rel_path}")
     endforeach()
   endif()
 endmacro()
 
+function(add_copy_headers_target)
+  cmake_parse_arguments(
+      PARSED_ARGS
+      ""
+      "NAME;SRC;DST;INDEX_FILE"
+      "DEPS"
+      ${ARGN}
+  )
+  if(NOT PARSED_ARGS_NAME)
+      message(FATAL_ERROR "name required")
+  endif(NOT PARSED_ARGS_NAME)
+  if(NOT PARSED_ARGS_SRC)
+      message(FATAL_ERROR "src required")
+  endif(NOT PARSED_ARGS_SRC)
+  if(NOT PARSED_ARGS_DST)
+      message(FATAL_ERROR "dst required")
+  endif(NOT PARSED_ARGS_DST)
+  add_custom_target("${PARSED_ARGS_NAME}_create_header_dir"
+    COMMAND ${CMAKE_COMMAND} -E make_directory "${PARSED_ARGS_DST}"
+  DEPENDS ${PARSED_ARGS_DEPS})
+
+  add_custom_target("${PARSED_ARGS_NAME}_copy_headers_to_destination" ALL DEPENDS "${PARSED_ARGS_NAME}_create_header_dir")
+  file(GLOB_RECURSE headers "${PARSED_ARGS_SRC}/*.h")
+  file(GLOB_RECURSE cuda_headers "${PARSED_ARGS_SRC}/*.cuh")
+  file(GLOB_RECURSE hpp_headers "${PARSED_ARGS_SRC}/*.hpp")
+  list(APPEND headers ${cuda_headers})
+  list(APPEND headers ${hpp_headers})
+
+  foreach(header_file ${headers})
+    file(RELATIVE_PATH relative_file_path ${PARSED_ARGS_SRC} ${header_file})
+    add_custom_command(TARGET "${PARSED_ARGS_NAME}_copy_headers_to_destination" PRE_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different ${header_file} "${PARSED_ARGS_DST}/${relative_file_path}")
+  endforeach()
+
+  if(PARSED_ARGS_INDEX_FILE)
+    file(STRINGS ${PARSED_ARGS_INDEX_FILE} inventory_headers)
+  endif(PARSED_ARGS_INDEX_FILE)
+  foreach(header_file ${inventory_headers})
+    add_custom_command(TARGET "${PARSED_ARGS_NAME}_copy_headers_to_destination" PRE_BUILD
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different "${PARSED_ARGS_SRC}/${header_file}" "${PARSED_ARGS_DST}/${header_file}")
+  endforeach()
+endfunction()
