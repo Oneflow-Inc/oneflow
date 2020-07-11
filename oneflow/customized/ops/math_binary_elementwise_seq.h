0b582e7a08a1a70999eece4e9d3081f9bf33f313
chengtbf <472491134@qq.com>
2020-07-11 21:18:08 +0800

    implement of math binary elementwise gpu floating kernel

--- oneflow/customized/ops/math_binary_elementwise_seq.h
+++ oneflow/customized/ops/math_binary_elementwise_seq.h
@@ -6,11 +6,11 @@
 namespace oneflow {
 
 #define MATH_BINARY_ELEMENTWISE_FUNC_SEQ     \
-  OF_PP_MAKE_TUPLE_SEQ("Pow", Pow)           \
-  OF_PP_MAKE_TUPLE_SEQ("Atan2", Atan2)       \
-  OF_PP_MAKE_TUPLE_SEQ("Floordiv", Floordiv) \
-  OF_PP_MAKE_TUPLE_SEQ("Xdivy", Xdivy)       \
-  OF_PP_MAKE_TUPLE_SEQ("Xlogy", Xlogy)
+  OF_PP_MAKE_TUPLE_SEQ("pow", Pow)           \
+  OF_PP_MAKE_TUPLE_SEQ("atan2", Atan2)       \
+  OF_PP_MAKE_TUPLE_SEQ("floordiv", Floordiv) \
+  OF_PP_MAKE_TUPLE_SEQ("xdivy", Xdivy)       \
+  OF_PP_MAKE_TUPLE_SEQ("xlogy", Xlogy)
 
 }  // namespace oneflow
 
