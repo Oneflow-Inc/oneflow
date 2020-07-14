d6363dd399bef37ed936abf6709956b751a35329
strint <xiaoyulink@gmail.com>
2020-07-14 14:57:45 +0800

    fix test

--- oneflow/customized/ops/test_ops.cpp
+++ oneflow/customized/ops/test_ops.cpp
@@ -332,7 +332,9 @@ REGISTER_USER_OP("TestListDataTypeAndListShapeAndListStringAttr")
 REGISTER_USER_OP("test_user_op_attr_auto_type")
     .Input("in")
     .Output("out")
-    .Attr("int32", UserOpAttrType::kAtInt32);
+    .Attr("int1", UserOpAttrType::kAtInt32)
+    .Attr("int2", UserOpAttrType::kAtInt32)
+    .SetTensorDescInferFn(user_op::TensorDescInferFnUtil::Unchanged);                        \
 
 REGISTER_CPU_ONLY_USER_OP("cpu_only_relu_test")
     .Input("in")
--- oneflow/python/test/ops/test_user_op_attr_auto_type.py
+++ oneflow/python/test/ops/test_user_op_attr_auto_type.py
@@ -1,16 +1,17 @@
 import oneflow as flow
 
-def _test_user_op_attr_auto_type(attr1):
+def _test_user_op_attr_auto_type(attr1, attr2):
     return (
         flow.user_op_builder("test")
         .Op("test_user_op_attr_auto_type")
         .Input("in")
         .Output("out")
-        .Attr("int32", attr1)
+        .Attr("int1", attr1)
+        .Attr("int2", attr2, "AttrTypeInt32")
         .Build()
         .InferAndTryRun()
         .RemoteBlobList()
     )
 
 def test_user_op_attr_auto_type(test_case):
-    _test_user_op_attr_auto_type(1)
+    _test_user_op_attr_auto_type(1, 2)
