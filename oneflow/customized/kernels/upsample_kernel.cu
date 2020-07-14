68339227e534c595c5d42fcdd944c59d91b993d3
guo-ran <360112263@qq.com>
2020-07-14 20:50:43 +0800

    rename

--- oneflow/customized/kernels/upsample_kernel.cu
+++ oneflow/customized/kernels/upsample_kernel.cu
@@ -21,85 +21,86 @@ namespace {
 
 template<typename T>
 __global__ void UpsampleNearestForward(const int64_t nthreads, const T* in_dptr,
-                                       const int64_t channel_num, const int64_t height,
-                                       const int64_t width, const int64_t new_height,
-                                       const int64_t new_width, const float scale_h,
+                                       const int64_t channel_num, const int64_t in_height,
+                                       const int64_t in_width, const int64_t out_height,
+                                       const int64_t out_width, const float scale_h,
                                        const float scale_w, const bool align_corners, T* out_dptr) {
-  const int64_t new_area = new_height * new_width;
-  const int64_t channel_area = channel_num * height * width;
-  const int64_t channel_new_area = channel_num * new_height * new_width;
+  const int64_t new_area = out_height * out_width;
+  const int64_t channel_area = channel_num * in_height * in_width;
+  const int64_t channel_new_area = channel_num * out_height * out_width;
   CUDA_1D_KERNEL_LOOP(index, nthreads) {
-    const int64_t h = (index / new_width) % new_height;
-    const int64_t w = index % new_width;
+    const int64_t h = (index / out_width) % out_height;
+    const int64_t w = index % out_width;
     const int64_t c = (index / new_area) % channel_num;
     const int64_t n = index / channel_new_area;
     const int64_t in_h = min((align_corners) ? static_cast<int64_t>(roundf(h * scale_h))
                                              : static_cast<int64_t>(floorf(h * scale_h)),
-                             height - 1);
+                             in_height - 1);
     const int64_t in_w = min((align_corners) ? static_cast<int64_t>(roundf(w * scale_w))
                                              : static_cast<int64_t>(floorf(w * scale_w)),
-                             width - 1);
-    out_dptr[index] = in_dptr[n * channel_area + (c * height + in_h) * width + in_w];
+                             in_width - 1);
+    out_dptr[index] = in_dptr[n * channel_area + (c * in_height + in_h) * in_width + in_w];
   }
 }
 
 template<typename T>
 __global__ void UpsampleNearestBackward(const int64_t nthreads, const T* dy_dptr,
-                                        const int64_t channel_num, const int64_t height,
-                                        const int64_t width, const int64_t new_height,
-                                        const int64_t new_width, const float scale_h,
+                                        const int64_t channel_num, const int64_t dx_height,
+                                        const int64_t dx_width, const int64_t dy_height,
+                                        const int64_t dy_width, const float scale_h,
                                         const float scale_w, const bool align_corners, T* dx_dptr) {
-  const int64_t area = height * width;
-  const int64_t new_area = new_height * new_width;
-  const int64_t channel_area = channel_num * height * width;
-  const int64_t channel_new_area = channel_num * new_height * new_width;
+  const int64_t area = dx_height * dx_width;
+  const int64_t new_area = dy_height * dy_width;
+  const int64_t channel_area = channel_num * dx_height * dx_width;
+  const int64_t channel_new_area = channel_num * dy_height * dy_width;
   CUDA_1D_KERNEL_LOOP(index, nthreads) {
-    const int64_t h = (index / new_width) % new_height;
-    const int64_t w = index % new_width;
+    const int64_t h = (index / dy_width) % dy_height;
+    const int64_t w = index % dy_width;
     const int64_t c = (index / new_area) % channel_num;
     const int64_t n = index / channel_new_area;
     const int64_t in_h = min((align_corners) ? static_cast<int64_t>(roundf(h * scale_h))
                                              : static_cast<int64_t>(floorf(h * scale_h)),
-                             height - 1);
+                             dx_height - 1);
     const int64_t in_w = min((align_corners) ? static_cast<int64_t>(roundf(w * scale_w))
                                              : static_cast<int64_t>(floorf(w * scale_w)),
-                             width - 1);
-    atomicAdd(dx_dptr + n * channel_area + (c * height + in_h) * width + in_w, dy_dptr[index]);
+                             dx_width - 1);
+    atomicAdd(dx_dptr + n * channel_area + (c * dx_height + in_h) * dx_width + in_w,
+              dy_dptr[index]);
   }
 }
 
 template<typename T>
 __global__ void UpsampleBilinearForward(const int64_t nthreads, const T* in_dptr,
-                                        const int64_t channel_num, const int64_t height,
-                                        const int64_t width, const int64_t new_height,
-                                        const int64_t new_width, const float scale_h,
+                                        const int64_t channel_num, const int64_t in_height,
+                                        const int64_t in_width, const int64_t out_height,
+                                        const int64_t out_width, const float scale_h,
                                         const float scale_w, T* out_dptr) {
-  const int64_t new_area = new_height * new_width;
-  const int64_t channel_area = channel_num * height * width;
-  const int64_t channel_new_area = channel_num * new_height * new_width;
+  const int64_t new_area = out_height * out_width;
+  const int64_t channel_area = channel_num * in_height * in_width;
+  const int64_t channel_new_area = channel_num * out_height * out_width;
   CUDA_1D_KERNEL_LOOP(index, nthreads) {
-    const int64_t h = (index / new_width) % new_height;
-    const int64_t w = index % new_width;
+    const int64_t h = (index / out_width) % out_height;
+    const int64_t w = index % out_width;
     const int64_t c = (index / new_area) % channel_num;
     const int64_t n = index / channel_new_area;
 
     const float in_h = (static_cast<float>(h) + 0.5f) * scale_h - 0.5f;
     const int top_h_index = in_h > 0.0 ? floorf(in_h) : 0;
-    const int bottom_h_index = (in_h < height - 1) ? ceilf(in_h) : height - 1;
+    const int bottom_h_index = (in_h < in_height - 1) ? ceilf(in_h) : in_height - 1;
     const float h_lerp = in_h - top_h_index;
 
     const float in_w = (static_cast<float>(w) + 0.5f) * scale_w - 0.5f;
     const int left_w_index = in_w > 0.0 ? floorf(in_w) : 0;
-    const int right_w_index = (in_w < width - 1) ? ceilf(in_w) : width - 1;
+    const int right_w_index = (in_w < in_width - 1) ? ceilf(in_w) : in_width - 1;
     const float w_lerp = in_w - left_w_index;
     const float top_left(
-        in_dptr[n * channel_area + (c * height + top_h_index) * width + left_w_index]);
+        in_dptr[n * channel_area + (c * in_height + top_h_index) * in_width + left_w_index]);
     const float top_right(
-        in_dptr[n * channel_area + (c * height + top_h_index) * width + right_w_index]);
+        in_dptr[n * channel_area + (c * in_height + top_h_index) * in_width + right_w_index]);
     const float bottom_left(
-        in_dptr[n * channel_area + (c * height + bottom_h_index) * width + left_w_index]);
+        in_dptr[n * channel_area + (c * in_height + bottom_h_index) * in_width + left_w_index]);
     const float bottom_right(
-        in_dptr[n * channel_area + (c * height + bottom_h_index) * width + right_w_index]);
+        in_dptr[n * channel_area + (c * in_height + bottom_h_index) * in_width + right_w_index]);
     const float top = top_left + (top_right - top_left) * w_lerp;
     const float bottom = bottom_left + (bottom_right - bottom_left) * w_lerp;
     out_dptr[index] = top + (bottom - top) * h_lerp;
@@ -108,40 +109,42 @@ __global__ void UpsampleBilinearForward(const int64_t nthreads, const T* in_dptr
 
 template<typename T>
 __global__ void UpsampleBilinearBackward(const int64_t nthreads, const T* dy_dptr,
-                                         const int64_t channel_num, const int64_t height,
-                                         const int64_t width, const int64_t new_height,
-                                         const int64_t new_width, const float scale_h,
+                                         const int64_t channel_num, const int64_t dx_height,
+                                         const int64_t dx_width, const int64_t dy_height,
+                                         const int64_t dy_width, const float scale_h,
                                          const float scale_w, T* dx_dptr) {
-  const int64_t area = height * width;
-  const int64_t new_area = new_height * new_width;
-  const int64_t channel_area = channel_num * height * width;
-  const int64_t channel_new_area = channel_num * new_height * new_width;
+  const int64_t area = dx_height * dx_width;
+  const int64_t new_area = dy_height * dy_width;
+  const int64_t channel_area = channel_num * dx_height * dx_width;
+  const int64_t channel_new_area = channel_num * dy_height * dy_width;
   CUDA_1D_KERNEL_LOOP(index, nthreads) {
-    const int64_t h = (index / new_width) % new_height;
-    const int64_t w = index % new_width;
+    const int64_t h = (index / dy_width) % dy_height;
+    const int64_t w = index % dy_width;
     const int64_t c = (index / new_area) % channel_num;
     const int64_t n = index / channel_new_area;
 
     const float original_h = (static_cast<float>(h) + 0.5f) * scale_h - 0.5f;
     const int top_h_index = original_h > 0.0 ? floorf(original_h) : 0;
-    const int bottom_h_index = (original_h < height - 1) ? ceilf(original_h) : height - 1;
+    const int bottom_h_index = (original_h < dx_height - 1) ? ceilf(original_h) : dx_height - 1;
     const float h_lerp = original_h - floorf(original_h);
 
     const float original_w = (static_cast<float>(w) + 0.5f) * scale_w - 0.5f;
     const int left_w_index = original_w > 0.0 ? floorf(original_w) : 0;
-    const int right_w_index = (original_w < width - 1) ? ceilf(original_w) : width - 1;
+    const int right_w_index = (original_w < dx_width - 1) ? ceilf(original_w) : dx_width - 1;
     const float w_lerp = original_w - floorf(original_w);
 
     const float dtop = (1 - h_lerp) * dy_dptr[index];
-    atomicAdd(dx_dptr + n * channel_area + (c * height + top_h_index) * width + left_w_index,
+    atomicAdd(dx_dptr + n * channel_area + (c * dx_height + top_h_index) * dx_width + left_w_index,
               static_cast<T>((1 - w_lerp) * dtop));
-    atomicAdd(dx_dptr + n * channel_area + (c * height + top_h_index) * width + right_w_index,
+    atomicAdd(dx_dptr + n * channel_area + (c * dx_height + top_h_index) * dx_width + right_w_index,
               static_cast<T>(w_lerp * dtop));
     const float dbottom = h_lerp * dy_dptr[index];
-    atomicAdd(dx_dptr + n * channel_area + (c * height + bottom_h_index) * width + left_w_index,
-              static_cast<T>((1 - w_lerp) * dbottom));
-    atomicAdd(dx_dptr + n * channel_area + (c * height + bottom_h_index) * width + right_w_index,
-              static_cast<T>(w_lerp * dbottom));
+    atomicAdd(
+        dx_dptr + n * channel_area + (c * dx_height + bottom_h_index) * dx_width + left_w_index,
+        static_cast<T>((1 - w_lerp) * dbottom));
+    atomicAdd(
+        dx_dptr + n * channel_area + (c * dx_height + bottom_h_index) * dx_width + right_w_index,
+        static_cast<T>(w_lerp * dbottom));
   }
 }
 
