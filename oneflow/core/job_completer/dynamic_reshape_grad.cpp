94cf6a11cdcdd216e33929d91e2b42f95e2a7c52
Juncheng <liujuncheng1022@gmail.com>
2020-07-15 16:24:38 +0800

    Remove broadcast/reduce/reshape system op (#3199)
    * Remove reduce/broadcast system op
    
    * fix GenBroadcastToCompatibleWithGradOpConf
    
    * remove useless file
    
    * Remove reshape system op
    
    * revert dynamic reshape grad
    
    * revert DynamicReshapeKernel
    
    * revert DynamicReshapeLikeKernel

--- oneflow/core/job_completer/dynamic_reshape_grad.cpp
+++ oneflow/core/job_completer/dynamic_reshape_grad.cpp
@@ -4,23 +4,6 @@ namespace oneflow {
 
 namespace {
 
-void GenerateBackwardOpConf4Reshape(
-    const Operator& op, std::vector<OperatorConf>* op_confs,
-    const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
-  CHECK(op.op_conf().has_reshape_conf());
-  if (DiffLbi4BnInOp("in") != nullptr) {
-    OperatorConf reverse_reshape_op;
-    reverse_reshape_op.set_name(op.op_name() + "_grad");
-    ReshapeLikeOpConf* reshape_like_op_conf = reverse_reshape_op.mutable_reshape_like_conf();
-    reshape_like_op_conf->set_x(GenLogicalBlobName(*DiffLbi4BnInOp("out")));
-    reshape_like_op_conf->set_like(GenLogicalBlobName(op.BnInOp2Lbi("in")));
-    reshape_like_op_conf->set_y("y");
-    op_confs->push_back(reverse_reshape_op);
-    DiffLbi4BnInOp("in")->set_op_name(reverse_reshape_op.name());
-    DiffLbi4BnInOp("in")->set_blob_name("y");
-  }
-}
-
 void GenerateBackwardOpConf4DynamicReshape(
     const Operator& op, std::vector<OperatorConf>* op_confs,
     const std::function<LogicalBlobId*(const std::string&)>& DiffLbi4BnInOp) {
@@ -41,7 +24,6 @@ void GenerateBackwardOpConf4DynamicReshape(
 
 }  // namespace
 
-REGISTER_OP_GRAD(OperatorConf::kReshapeConf, GenerateBackwardOpConf4Reshape);
 REGISTER_OP_GRAD(OperatorConf::kDynamicReshapeConf, GenerateBackwardOpConf4DynamicReshape);
 
 }  // namespace oneflow
