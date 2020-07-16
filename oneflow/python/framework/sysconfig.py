4421e9107cef0f55783c29afbf3834a334bad53e
OuYang Yu <xuanjiuye@gmail.com>
2020-07-16 20:07:48 +0800

    add signature to framework api (#3163)
    * add signature fo framework api
    
    * function signature returns type Callable
    
    Former-commit-id: 7c67992e08d1fed68fff86dab1fb32fe91836e16

--- oneflow/python/framework/sysconfig.py
+++ oneflow/python/framework/sysconfig.py
@@ -5,20 +5,21 @@ import os
 import oneflow
 from oneflow.python.oneflow_export import oneflow_export
 from .sysconfig_gen import generated_compile_flags
+from typing import List
 
 
 @oneflow_export("sysconfig.get_include")
-def get_include():
+def get_include() -> str:
     return os.path.join(os.path.dirname(oneflow.__file__), "include")
 
 
 @oneflow_export("sysconfig.get_lib")
-def get_lib():
+def get_lib() -> str:
     return os.path.join(os.path.dirname(oneflow.__file__))
 
 
 @oneflow_export("sysconfig.get_compile_flags")
-def get_compile_flags():
+def get_compile_flags() -> List[str]:
     flags = []
     flags.append("-I%s" % get_include())
     flags.append("-DHALF_ENABLE_CPP11_USER_LITERALS=0")
@@ -27,7 +28,7 @@ def get_compile_flags():
 
 
 @oneflow_export("sysconfig.get_link_flags")
-def get_link_flags():
+def get_link_flags() -> List[str]:
     flags = []
     flags.append("-L%s" % get_lib())
     flags.append("-l:_oneflow_internal.so")
