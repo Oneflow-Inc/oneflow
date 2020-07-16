9b7dafe195a728d3f3fcd6031363abd961c65766
Yao Chi <yaochi@oneflow.org>
2020-07-17 00:27:52 +0800

    index range [-2:-1] -> [-2] when print traceback

--- oneflow/python/framework/placement_util.py
+++ oneflow/python/framework/placement_util.py
@@ -50,7 +50,7 @@ def deprecated_placement(*args, **kwargs):
             "oneflow.scope.placement"
         ),
     )
-    print(" ".join(traceback.format_stack()[-2:-1]))
+    print(" ".join(traceback.format_stack()[-2]))
     return api_placement(*args, **kwargs)
 
 
