8bc72c1e9f0381bbff9135d0f96a58f1fd686a4f
liujuncheng <liujuncheng1022@gmail.com>
2020-07-18 13:15:52 +0800

    Revert BoxingOp/Kernel

---
+++ oneflow/core/operator/boxing_op.cpp
@@ -0,0 +1,121 @@
+#include "oneflow/core/operator/boxing_op.h"
+#include "oneflow/core/common/protobuf.h"
+
+namespace oneflow {
+
+void BoxingOp::VirtualGenKernelConf(
+    std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx, KernelConf* kernel_conf) const {
+  OpAttribute* op_attribute = kernel_conf->mutable_op_attribute();
+  EraseEmptyBnInVec(GetBlobDesc4BnInOp, op_attribute->mutable_input_bns());
+  EraseEmptyBnInVec(GetBlobDesc4BnInOp, op_attribute->mutable_output_bns());
+}
+
+void BoxingOp::InitFromOpConf() {
+  CHECK(op_conf().has_boxing_conf());
+  const BoxingOpConf& boxing_conf = op_conf().boxing_conf();
+
+  for (int32_t i = 0; i < boxing_conf.in_num(); ++i) {
+    EnrollInputBn("in_" + std::to_string(i), false);
+  }
+  if (boxing_conf.in_box_case() == BoxingOpConf::kAddBox
+      && boxing_conf.out_box_case() == BoxingOpConf::kSplitBox) {
+    EnrollTmpBn("middle");
+  }
+  for (int32_t i = 0; i < boxing_conf.out_num(); ++i) {
+    EnrollOutputBn("out_" + std::to_string(i), false);
+  }
+}
+
+const PbMessage& BoxingOp::GetCustomizedConf() const { return op_conf().boxing_conf(); }
+
+LogicalBlobId BoxingOp::lbi4ibn(const std::string& input_bn) const {
+  return GetMsgFromCustomizedConf<LogicalBlobId>("lbi");
+}
+
+LogicalBlobId BoxingOp::lbi4obn(const std::string& output_bn) const {
+  return GetMsgFromCustomizedConf<LogicalBlobId>("lbi");
+}
+
+Symbol<OperatorConf> BoxingOp::GetOpConfWithoutOpNameAndLbn() const {
+  OperatorConf op_conf(this->op_conf());
+  op_conf.set_name("undefined-op-name");
+  CHECK(op_conf.has_boxing_conf());
+  auto* boxing_conf = op_conf.mutable_boxing_conf();
+  LogicalBlobId empty_logical_blob_id;
+  *boxing_conf->mutable_lbi() = empty_logical_blob_id;
+  return SymbolOf(op_conf);
+}
+
+Maybe<void> BoxingOp::InferBlobDescs(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    const ParallelContext* parallel_ctx) const {
+  const BoxingOpConf& conf = op_conf().boxing_conf();
+  BlobDesc* first_in_blob = GetBlobDesc4BnInOp(input_bns().Get(0));
+  if (conf.in_box_case() == BoxingOpConf::kAddBox) {
+    const Shape& first_in_blob_shape = first_in_blob->shape();
+    for (const std::string& ibn : input_bns()) {
+      CHECK_EQ_OR_RETURN(first_in_blob_shape, GetBlobDesc4BnInOp(ibn)->shape());
+    }
+  }
+
+  DimVector data_tmp_blob_shape_vec = GetBlobDesc4BnInOp(input_bns().Get(0))->shape().dim_vec();
+  InferTmpBlobDesc(GetBlobDesc4BnInOp, &data_tmp_blob_shape_vec);
+
+  if (conf.out_box_case() == BoxingOpConf::kSplitBox) {
+    const BoxSplitConf& split_conf = conf.split_box();
+    CHECK_GE_OR_RETURN(split_conf.axis(), 0);
+    CHECK_LT_OR_RETURN(split_conf.axis(), data_tmp_blob_shape_vec.size());
+    FOR_RANGE(size_t, i, 0, output_bns().size()) {
+      BlobDesc* out_blob_desc = GetBlobDesc4BnInOp(output_bns().Get(i));
+      *out_blob_desc = *first_in_blob;
+      CHECK_GT_OR_RETURN(split_conf.part_num(i), 0);
+      data_tmp_blob_shape_vec[split_conf.axis()] = split_conf.part_num(i);
+      out_blob_desc->mut_shape() = Shape(data_tmp_blob_shape_vec);
+    }
+  } else if (conf.out_box_case() == BoxingOpConf::kCloneBox) {
+    for (const std::string& obn : output_bns()) {
+      BlobDesc* out_blob_desc = GetBlobDesc4BnInOp(obn);
+      *out_blob_desc = *first_in_blob;
+      out_blob_desc->mut_shape() = Shape(data_tmp_blob_shape_vec);
+    }
+  } else {
+    UNIMPLEMENTED_THEN_RETURN();
+  }
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> BoxingOp::InferTmpBlobDesc(
+    std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+    DimVector* data_tmp_vec_ptr) const {
+  const BoxingOpConf& conf = op_conf().boxing_conf();
+  if (conf.in_box_case() == BoxingOpConf::kConcatBox) {
+    int32_t concat_axis = conf.concat_box().axis();
+    CHECK_GE_OR_RETURN(concat_axis, 0);
+    FOR_RANGE(size_t, ib_idx, 1, input_bns().size()) {
+      const BlobDesc* in_blob_desc = GetBlobDesc4BnInOp(input_bns().Get(ib_idx));
+      const DimVector& in_blob_shape_vec = in_blob_desc->shape().dim_vec();
+      CHECK_LT_OR_RETURN(concat_axis, in_blob_shape_vec.size());
+      FOR_RANGE(size_t, i, 0, in_blob_shape_vec.size()) {
+        if (i == concat_axis) {
+          (*data_tmp_vec_ptr)[i] += in_blob_shape_vec[i];
+        } else {
+          CHECK_EQ_OR_RETURN((*data_tmp_vec_ptr)[i], in_blob_shape_vec[i]);
+        }
+      }
+    }
+  }
+
+  CHECK_NE_OR_RETURN(conf.out_box_case(), BoxingOpConf::OUT_BOX_NOT_SET);
+  if (conf.in_box_case() == BoxingOpConf::kAddBox
+      && conf.out_box_case() == BoxingOpConf::kSplitBox) {
+    BlobDesc* data_tmp_blob_desc = GetBlobDesc4BnInOp(SoleTbn());
+    data_tmp_blob_desc->mut_shape() = Shape(*data_tmp_vec_ptr);
+    data_tmp_blob_desc->set_data_type(GetBlobDesc4BnInOp(input_bns().Get(0))->data_type());
+  }
+  return Maybe<void>::Ok();
+}
+
+REGISTER_OP(OperatorConf::kBoxingConf, BoxingOp);
+
+}  // namespace oneflow
---
+++ oneflow/core/operator/boxing_op.h
@@ -0,0 +1,36 @@
+#ifndef ONEFLOW_CORE_OPERATOR_BOXING_OP_H_
+#define ONEFLOW_CORE_OPERATOR_BOXING_OP_H_
+
+#include "oneflow/core/operator/operator.h"
+
+namespace oneflow {
+
+class BoxingOp final : public Operator {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BoxingOp);
+  BoxingOp() = default;
+  ~BoxingOp() = default;
+
+  void InitFromOpConf() override;
+  const PbMessage& GetCustomizedConf() const override;
+
+  Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                             const ParallelContext* parallel_ctx) const override;
+
+ protected:
+  void VirtualGenKernelConf(std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                            const ParallelContext* parallel_ctx,
+                            KernelConf* kernel_conf) const override;
+
+ private:
+  LogicalBlobId lbi4ibn(const std::string& input_bn) const override;
+  LogicalBlobId lbi4obn(const std::string& output_bn) const override;
+  Maybe<void> InferTmpBlobDesc(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
+                               DimVector* data_tmp_vec_ptr) const;
+  Symbol<OperatorConf> GetOpConfWithoutOpNameAndLbn() const override;
+  void EmplaceLbi2Obn(const LogicalBlobId& lbi, const std::string& obn) override {}
+};
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_OPERATOR_BOXING_OP_H_
