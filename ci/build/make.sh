ea75e2cc937f26d48e042fd3c2c859e06d2ac469
tsai <caishenghang@1f-dev.kbaeegfb1x0ubnoznzequyxzve.bx.internal.cloudapp.net>
2020-07-23 04:06:11 +0000

    quick fix

--- ci/build/make.sh
+++ ci/build/make.sh
@@ -29,6 +29,6 @@ cached_build_ret=$?
 set -e
 if [ $cached_build_ret -ne 0 ]; then
     echo "retry after cleaning build dir"
-    docker run --rm -it-v $src_dir:/oneflow-src busybox rm -rf /oneflow-src/manylinux2014-build-cache
+    docker run --rm -it -v $src_dir:/oneflow-src busybox rm -rf /oneflow-src/manylinux2014-build-cache
     build
 fi
