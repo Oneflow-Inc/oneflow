e00493946b4689a968dffc2dea8402a7a82f8080
guo-ran <360112263@qq.com>
2020-07-23 16:04:06 +0800

    test case

---
+++ oneflow/python/test/ops/test_nn_conv2d_padding.py
@@ -0,0 +1,166 @@
+import os
+from collections import OrderedDict
+
+import numpy as np
+import oneflow as flow
+import tensorflow as tf
+import test_global_storage
+from test_util import GenArgList
+
+gpus = tf.config.experimental.list_physical_devices("GPU")
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+
+def compare_with_tensorflow(
+    device_type,
+    x_shape,
+    filters,
+    kernel_size,
+    groups,
+    of_padding="SAME",
+    tf_padding="SAME",
+    stride=1,
+    is_dynamic=False,
+):
+    assert device_type in ["gpu", "cpu"]
+    flow.clear_default_session()
+    func_config = flow.FunctionConfig()
+    func_config.default_data_type(flow.float)
+    func_config.train.primary_lr(1e-4)
+    func_config.train.model_update_conf(dict(naive_conf={}))
+
+    @flow.global_function(func_config)
+    def ConvJob():
+        with flow.scope.placement(device_type, "0:0"):
+            x = flow.get_variable(
+                "x",
+                shape=x_shape,
+                dtype=flow.float,
+                initializer=flow.random_uniform_initializer(minval=0, maxval=100),
+                trainable=True,
+            )
+            weight_shape = (filters, int(x.shape[1] / groups), kernel_size, kernel_size)
+            weight = flow.get_variable(
+                "conv-weight",
+                shape=weight_shape,
+                dtype=flow.float,
+                initializer=flow.random_uniform_initializer(minval=0, maxval=100),
+            )
+            loss = flow.nn.conv2d(
+                x,
+                weight,
+                strides=[stride, stride],
+                padding=of_padding,
+                data_format="NCHW",
+                dilations=[1, 1],
+                groups=groups,
+            )
+            flow.losses.add_loss(loss)
+
+            flow.watch(x, test_global_storage.Setter("x"))
+            flow.watch_diff(x, test_global_storage.Setter("x_diff"))
+            flow.watch(weight, test_global_storage.Setter("weight"))
+            flow.watch_diff(weight, test_global_storage.Setter("weight_diff"))
+            flow.watch(loss, test_global_storage.Setter("loss"))
+            flow.watch_diff(loss, test_global_storage.Setter("loss_diff"))
+
+            return loss
+
+    # OneFlow
+    check_point = flow.train.CheckPoint()
+    check_point.init()
+    of_out = ConvJob().get()
+    # TensorFlow
+    with tf.GradientTape(persistent=True) as tape:
+        x = tf.Variable(test_global_storage.Get("x").transpose(0, 2, 3, 1))
+        assert groups > 0
+        assert x_shape[1] % groups == 0
+        assert filters % groups == 0
+        weight = tf.Variable(test_global_storage.Get("weight").transpose(2, 3, 1, 0))
+        print("tf padding", tf_padding)
+
+        tf_out = tf.nn.conv2d(
+            x,
+            weight,
+            strides=[1, stride, stride, 1],
+            padding=tf_padding,
+            data_format="NHWC",
+        )
+
+    loss_diff = test_global_storage.Get("loss_diff").transpose(0, 2, 3, 1)
+    tf_x_diff = tape.gradient(tf_out, x, loss_diff)
+    tf_weight_diff = tape.gradient(tf_out, weight, loss_diff)
+    print("of_out", of_out.numpy().transpose(0, 2, 3, 1).flatten()[0:20])
+    print("tf_out", tf_out.numpy().flatten()[0:20])
+    print("of_out", of_out.numpy().transpose(0, 2, 3, 1).flatten()[-20:-1])
+    print("tf_out", tf_out.numpy().flatten()[-20:-1])
+    idx = np.where(np.abs(of_out.numpy().transpose(0, 2, 3, 1) - tf_out.numpy()) > 5e-4)
+    print("of", of_out.numpy().transpose(0, 2, 3, 1)[idx])
+    print("tf", tf_out.numpy()[idx])
+    max_diff = np.max(
+        np.absolute(of_out.numpy().transpose(0, 2, 3, 1) - tf_out.numpy())
+    )
+    assert np.allclose(
+        of_out.numpy().transpose(0, 2, 3, 1), tf_out.numpy(), rtol=1e-5, atol=1e-5
+    ), max_diff
+
+    # print(
+    #    "of_x_diff",
+    #    test_global_storage.Get("x_diff").transpose(0, 2, 3, 1).flatten()[0:20],
+    # )
+    # print("tf_x_diff", tf_x_diff.numpy().flatten()[0:20])
+    # print(
+    #    "of_x_diff",
+    #    test_global_storage.Get("x_diff").transpose(0, 2, 3, 1).flatten()[-20:-1],
+    # )
+    # print("tf_x_diff", tf_x_diff.numpy().flatten()[-20:-1])
+    print(
+        "max",
+        np.abs(
+            test_global_storage.Get("x_diff").transpose(0, 2, 3, 1) - tf_x_diff.numpy()
+        ).max(),
+    )
+
+    assert np.allclose(
+        test_global_storage.Get("x_diff").transpose(0, 2, 3, 1),
+        tf_x_diff.numpy(),
+        rtol=1e-4,
+        atol=1e-4,
+    )
+    assert np.allclose(
+        test_global_storage.Get("weight_diff").transpose(2, 3, 1, 0),
+        tf_weight_diff.numpy(),
+        rtol=1e-5,
+        atol=1e-5,
+    )
+
+
+def test_cpu1(test_case):
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["gpu", "cpu"]
+    arg_dict["x_shape"] = [(10, 32, 10, 10), (10, 32, 11, 11)]
+    arg_dict["filters"] = [64]
+    arg_dict["kernel_size"] = [3]
+    arg_dict["groups"] = [1]
+    arg_dict["of_padding"] = ["VALID"]
+    arg_dict["tf_padding"] = ["VALID"]
+    arg_dict["stride"] = [1, 2]
+    for arg in GenArgList(arg_dict):
+        print(*arg)
+        compare_with_tensorflow(*arg)
+
+
+def test_cpu1(test_case):
+    arg_dict = OrderedDict()
+    arg_dict["device_type"] = ["gpu", "cpu"]
+    arg_dict["x_shape"] = [(10, 32, 10, 10), (10, 32, 11, 11)]
+    arg_dict["filters"] = [64]
+    arg_dict["kernel_size"] = [3, 2]
+    arg_dict["groups"] = [1]
+    arg_dict["of_padding"] = ["SAME_UPPER"]
+    arg_dict["tf_padding"] = ["SAME"]
+    arg_dict["stride"] = [1, 2]
+    for arg in GenArgList(arg_dict):
+        print(*arg)
+        compare_with_tensorflow(*arg)
