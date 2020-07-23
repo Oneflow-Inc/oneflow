dec2a34f981299a3752bb6accafc894b58c2146c
daquexian <daquexian566@gmail.com>
2020-07-23 13:50:54 +0800

    add license

--- oneflow/python/onnx/optimizer/const_fold_optimizer.py
+++ oneflow/python/onnx/optimizer/const_fold_optimizer.py
@@ -1,10 +1,24 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
 
-"""const fold Optimizer.
-   if op's inputs are all const then do op computation when building the graph to improve performance
-   for example, input of transpose node is const then we can do transpose statically instead of at runtime
-"""
+# const fold Optimizer.
+# if op's inputs are all const then do op computation when building the graph to improve performance
+# for example, input of transpose node is const then we can do transpose statically instead of at runtime
 
 from oneflow.python.framework import id_util
 from .. import util
--- oneflow/python/onnx/optimizer/merge_duplicated_nodes_optimizer.py
+++ oneflow/python/onnx/optimizer/merge_duplicated_nodes_optimizer.py
@@ -1,11 +1,25 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
 
-"""Merge Duplicated Nodes Optimizer.
-   Remove duplicate nodes except identity nodes which should be handled by identity optimizer.
-   for example, node a is input of node b and node c, and computation of node b, c are same such as "abs" op.
-   then b and c can be merged into one node to avoid duplicated computation
-"""
+# Merge Duplicated Nodes Optimizer.
+# duplicate nodes except identity nodes which should be handled by identity optimizer.
+# example, node a is input of node b and node c, and computation of node b, c are same such as "abs" op.
+# b and c can be merged into one node to avoid duplicated computation
 
 from collections import defaultdict, namedtuple
 
