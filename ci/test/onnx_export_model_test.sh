43b54e43dfdeb6647eafb4ecf82a909071c178af
daquexian <daquexian566@gmail.com>
2020-07-23 11:41:49 +0800

    run onnx export model test in ci

---
+++ ci/test/onnx_export_model_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xe
+
+pip3 install --user ci_tmp/*.whl
+
+test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"/test_tmp_dir"}
+rm -rf $test_tmp_dir
+mkdir -p $test_tmp_dir
+cp -r oneflow/python/test/onnx/ $test_tmp_dir
+cd $test_tmp_dir
+
+python3 test_models.py
