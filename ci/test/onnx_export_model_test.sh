c5050acf0c0cbee968e4a741634c901429f05284
daquexian <daquexian566@gmail.com>
2020-07-24 09:30:38 +0800

    run onnx test before op test

--- ci/test/onnx_export_model_test.sh
+++ ci/test/onnx_export_model_test.sh
@@ -7,7 +7,8 @@ pip3 install --user ci_tmp/*.whl
 test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"/test_tmp_dir"}
 rm -rf $test_tmp_dir
 mkdir -p $test_tmp_dir
+ls oneflow/python/test/onnx
 cp -r oneflow/python/test/onnx/ $test_tmp_dir
 cd $test_tmp_dir
-
+ls
 python3 test_model.py
