3c7801853d5c264305784bc82b7930cea4f25d2e
lixinqi <lixinqi0703106@163.com>
2020-07-24 21:21:47 +0800

    fix compatible bug under python 3.7

--- oneflow/python/framework/typing.py
+++ oneflow/python/framework/typing.py
@@ -195,8 +195,6 @@ class Callback(typing.Generic[typing.TypeVar("T")]):
 def OriginFrom(parameterised, generic):
     if inspect.isclass(parameterised) and inspect.isclass(generic):
         return issubclass(parameterised, generic)
-    if inspect.isclass(parameterised) != inspect.isclass(generic):
-        return False
     if (sys.version_info.major, sys.version_info.minor) >= (3, 7):
         if not hasattr(parameterised, "__origin__"):
             return False
