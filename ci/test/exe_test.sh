3b5ce64bebae92c3432317de651d4447eee0f488
tsai <caishenghang@oneflow.org>
2020-07-24 09:30:57 +0800

    fix exe test

--- ci/test/exe_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xe
-
-chmod +x ./manylinux2014-build-cache/build-oneflow/bin/oneflow_testexe/oneflow_testexe
-./manylinux2014-build-cache/build-oneflow/bin/oneflow_testexe/oneflow_testexe
