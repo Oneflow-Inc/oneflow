8355675c2e23ddc98c7bd3d5e48ee6420f1188bf
leaves-zwx <kunta0932@gmail.com>
2020-07-24 23:10:00 +0800

    fix test_categorical_ordinal_encoder

--- oneflow/python/ops/categorical_ordinal_encode_op.py
+++ oneflow/python/ops/categorical_ordinal_encode_op.py
@@ -14,19 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from __future__ import absolute_import
-
 from typing import Optional
+from oneflow.python.oneflow_export import oneflow_export
 
 import oneflow as flow
 import oneflow.python.framework.id_util as id_util
 import oneflow.python.framework.remote_blob as remote_blob_util
-from oneflow.python.oneflow_export import oneflow_export
 
 
 @oneflow_export("categorical_ordinal_encode")
 def categorical_ordinal_encode(
     table: remote_blob_util.BlobDef,
-    size: int,
+    size: remote_blob_util.BlobDef,
     input_tensor: remote_blob_util.BlobDef,
     hash_precomputed: bool = True,
     name: Optional[str] = None,
@@ -51,30 +50,27 @@ def categorical_ordinal_encoder(
     input_tensor: remote_blob_util.BlobDef,
     capacity: int,
     hash_precomputed: bool = True,
-    name: Optional[str] = None,
+    name: str = "CategoricalOrdinalEncoder",
 ) -> remote_blob_util.BlobDef:
     assert hash_precomputed is True
-    name_prefix = (
-        name if name is not None else id_util.UniqueStr("CategoricalOrdinalEncoder_")
-    )
     dtype = input_tensor.dtype
-    table = flow.get_variable(
-        name="{}-Table".format(name_prefix),
-        shape=(capacity * 2,),
-        dtype=dtype,
-        initializer=flow.constant_initializer(0, dtype=dtype),
-        trainable=False,
-    )
-    size = flow.get_variable(
-        name="{}-Size".format(name_prefix),
-        shape=(1,),
-        dtype=dtype,
-        initializer=flow.constant_initializer(0, dtype=dtype),
-        trainable=False,
-    )
-    return categorical_ordinal_encode(
-        table=table,
-        size=size,
-        input_tensor=input_tensor,
-        name="{}-Encode".format(name_prefix),
-    )
+    with flow.scope.namespace(name):
+        table = flow.get_variable(
+            name="Table",
+            shape=(capacity * 2,),
+            dtype=dtype,
+            initializer=flow.constant_initializer(0, dtype=dtype),
+            trainable=False,
+            reuse=False,
+        )
+        size = flow.get_variable(
+            name="Size",
+            shape=(1,),
+            dtype=dtype,
+            initializer=flow.constant_initializer(0, dtype=dtype),
+            trainable=False,
+            reuse=False,
+        )
+        return categorical_ordinal_encode(
+            table=table, size=size, input_tensor=input_tensor, name="Encode",
+        )
