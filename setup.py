e16c940689417737dfe5ba5f10577d879e599efd
tsai <caishenghang@oneflow.org>
2020-07-24 12:54:37 +0800

    fmt

--- setup.py
+++ setup.py
@@ -21,29 +21,31 @@ class InstallPlatlib(install):
         if self.distribution.has_ext_modules():
             self.install_lib = self.install_platlib
 
+
 class BuildPyWithCustomBuildLib(build_py):
     def finalize_options(self):
         build_py.finalize_options(self)
-        self.build_lib = os.path.join(args.build_dir, 'temp_lib')
+        self.build_lib = os.path.join(args.build_dir, "temp_lib")
+
 
 parser = argparse.ArgumentParser()
 parser.register("type", "bool", lambda v: v.lower() == "true")
 parser.add_argument(
     "--with_xla",
-    type='bool',
+    type="bool",
     default=False,
-    help="Package xla libraries if true, otherwise not."
+    help="Package xla libraries if true, otherwise not.",
 )
-parser.add_argument('--build_dir', type=str, default='build')
+parser.add_argument("--build_dir", type=str, default="build")
 args, remain_args = parser.parse_known_args()
-sys.argv = ['setup.py'] + remain_args
+sys.argv = ["setup.py"] + remain_args
 
 
 REQUIRED_PACKAGES = [
-    'numpy',
-    'protobuf',
-    'tqdm',
-    'requests',
+    "numpy",
+    "protobuf",
+    "tqdm",
+    "requests",
 ]
 
 
@@ -54,37 +56,46 @@ class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
+
 packages = find_packages("{}/python_scripts".format(args.build_dir))
 package_dir = {
-    '':'{}/python_scripts'.format(args.build_dir),
+    "": "{}/python_scripts".format(args.build_dir),
 }
 
-include_files = glob.glob("{}/python_scripts/oneflow/include/**/*".format(args.build_dir), recursive=True)
-include_files = [os.path.relpath(p, "{}/python_scripts/oneflow".format(args.build_dir)) for p in include_files]
-package_data = {'oneflow': ['_oneflow_internal.so'] + include_files}
+include_files = glob.glob(
+    "{}/python_scripts/oneflow/include/**/*".format(args.build_dir), recursive=True
+)
+include_files = [
+    os.path.relpath(p, "{}/python_scripts/oneflow".format(args.build_dir))
+    for p in include_files
+]
+package_data = {"oneflow": ["_oneflow_internal.so"] + include_files}
 
 if args.with_xla:
-    packages += ['oneflow.libs']
-    package_dir['oneflow.libs'] = 'third_party/tensorflow/lib'
-    package_data['oneflow.libs'] = ['libtensorflow_framework.so.1', 'libxla_core.so']
+    packages += ["oneflow.libs"]
+    package_dir["oneflow.libs"] = "third_party/tensorflow/lib"
+    package_data["oneflow.libs"] = ["libtensorflow_framework.so.1", "libxla_core.so"]
     # Patchelf >= 0.9 is required.
-    oneflow_internal_so = "{}/python_scripts/oneflow/_oneflow_internal.so".format(args.build_dir)
+    oneflow_internal_so = "{}/python_scripts/oneflow/_oneflow_internal.so".format(
+        args.build_dir
+    )
     rpath = os.popen("patchelf --print-rpath " + oneflow_internal_so).read()
-    command = "patchelf --set-rpath '$ORIGIN/:$ORIGIN/libs/:%s' %s" % \
-              (rpath.strip(), oneflow_internal_so)
+    command = "patchelf --set-rpath '$ORIGIN/:$ORIGIN/libs/:%s' %s" % (
+        rpath.strip(),
+        oneflow_internal_so,
+    )
     if os.system(command) != 0:
         raise Exception("Patchelf set rpath failed. command is: %s" % command)
 
 setup(
-    name='oneflow',
-    version='0.1.3',
-    url='https://www.oneflow.org/',
+    name="oneflow",
+    version="0.1.3",
+    url="https://www.oneflow.org/",
     install_requires=REQUIRED_PACKAGES,
     packages=packages,
     package_dir=package_dir,
     package_data=package_data,
     zip_safe=False,
     distclass=BinaryDistribution,
-    cmdclass={'install': InstallPlatlib
-        'build_py': BuildPyWithCustomBuildLib},
+    cmdclass={"install": InstallPlatlib, "build_py": BuildPyWithCustomBuildLib},
 )
