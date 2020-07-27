2d690d01aef7fd14b67ac9e112c994375bfd4c4e
james plur <jamesplur7@gmail.com>
2020-07-27 09:51:11 +0800

    typo

--- oneflow/python/test/models/test_dcgan.py
+++ oneflow/python/test/models/test_dcgan.py
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import oneflow as flow
-import oneflow.python.framework.distribute as distribute_util
-
 import numpy as np
 import os
 
