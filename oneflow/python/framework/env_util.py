025a53d78898ed1f6982baae595052bc1c481549
binbinHan <han_binbin@163.com>
2020-07-27 05:20:37 -0500

    Dev remove enable eager at session (#3261)
    * remove_enable_eager_at_session
    
    * fix code style
    
    * fix unit test code
    
    * Recover deleted test files
    
    * conver EagerExecution to a normal class

--- oneflow/python/framework/env_util.py
+++ oneflow/python/framework/env_util.py
@@ -27,7 +27,7 @@ import oneflow.python.lib.core.enable_if as enable_if
 from oneflow.python.oneflow_export import oneflow_export
 
 
-@oneflow_export("env.enable_eager_environment")
+@oneflow_export("enable_eager_execution")
 def api_enable_eager_execution(val: bool = True) -> None:
     return enable_if.unique([enable_eager_environment])(val)
 
--- oneflow/python/test/ops/eager_test.py
+++ oneflow/python/test/ops/eager_test.py
@@ -20,17 +20,24 @@ import oneflow as flow
 from absl import app
 from absl.testing import absltest
 
+
+class _ClearDefaultSession(object):
+    def setUp(self):
+        oneflow.clear_default_session()
+        oneflow.enable_eager_execution(True)
+
+
 flow.unittest.register_test_cases(
     scope=globals(),
     directory=os.path.dirname(os.path.realpath(__file__)),
     filter_by_num_nodes=lambda x: x == 1,
     base_class=absltest.TestCase,
+    test_case_mixin=_ClearDefaultSession,
 )
 
 
 def main(argv):
     flow.env.init()
-    flow.env.enable_eager_environment()
     absltest.main()
 
 
