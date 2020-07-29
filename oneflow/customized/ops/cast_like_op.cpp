a63176acaa947f9cbbacbf03f3ba92a86e8ae0c3
guo-ran <360112263@qq.com>
2020-07-29 21:51:38 +0800

    fix batch axis

--- oneflow/customized/ops/cast_like_op.cpp
+++ oneflow/customized/ops/cast_like_op.cpp
@@ -37,7 +37,7 @@ REGISTER_USER_OP("cast_like")
       like_modifier->set_requires_grad(false);
     })
     .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
-      *ctx->BatchAxis4ArgNameAndIndex("out", 0) = *ctx->BatchAxis4ArgNameAndIndex("like", 0);
+      *ctx->BatchAxis4ArgNameAndIndex("out", 0) = *ctx->BatchAxis4ArgNameAndIndex("in", 0);
       return Maybe<void>::Ok();
     })
     .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
