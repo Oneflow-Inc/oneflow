1a5b636d404ca226ddcb92148972f5bb236da544
guo-ran <360112263@qq.com>
2020-07-29 11:17:28 +0800

    merge develop

--- oneflow/python/test/ops/test_sparse_softmax_cross_entropy_ms.py
+++ oneflow/python/test/ops/test_sparse_softmax_cross_entropy_ms.py
@@ -1,7 +1,23 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import os
 import numpy as np
 import tensorflow as tf
 import oneflow as flow
+import oneflow.typing as oft
 from collections import OrderedDict
 
 from test_util import GenArgList
@@ -21,18 +37,17 @@ def compare_with_tensorflow(
     flow.clear_default_session()
     flow.config.gpu_device_num(4)
     func_config = flow.FunctionConfig()
-    func_config.default_distribute_strategy(flow.distribute.consistent_strategy())
     func_config.default_data_type(flow.float)
     func_config.train.primary_lr(1e-4)
     func_config.train.model_update_conf(dict(naive_conf={}))
 
     @flow.global_function(func_config)
     def SparseSoftmaxCrossEntropyWithLogitsJob(
-        labels=flow.FixedTensorDef(
+        labels: oft.Numpy.Placeholder(
             (batch_size,), dtype=type_name_to_flow_type[label_type]
         )
     ):
-        with flow.device_prior_placement(device_type, "0:0"):
+        with flow.scope.placement(device_type, "0:0"):
             x = flow.get_variable(
                 "x",
                 shape=(batch_size, num_classes),
@@ -41,20 +56,16 @@ def compare_with_tensorflow(
                 trainable=True,
             )
 
-        with flow.device_prior_placement(device_type, "0:0-3"):
-            if os.getenv("ENABLE_USER_OP") == "True":
-                lebels_distribute = flow.distribute.broadcast()
-                logits_distribute = flow.distribute.split(len(x.shape) - 1)
-            else:
-                lebels_distribute = flow.distribute.split(0)
-                logits_distribute = flow.distribute.split(0)
+        with flow.scope.placement(device_type, "0:0-3"):
+            lebels_distribute = flow.distribute.broadcast()
+            logits_distribute = flow.distribute.split(len(x.shape) - 1)
             loss = flow.nn.sparse_softmax_cross_entropy_with_logits(
                 labels=labels.with_distribute(lebels_distribute),
                 logits=x.with_distribute(logits_distribute),
             )
             loss = flow.math.square(loss)
 
-        with flow.device_prior_placement(device_type, "0:0"):
+        with flow.scope.placement(device_type, "0:0"):
             loss = flow.identity(loss)
             flow.losses.add_loss(loss)
 
@@ -82,7 +93,7 @@ def compare_with_tensorflow(
     loss_diff = test_global_storage.Get("loss_diff")
     tf_x_diff = tape.gradient(tf_out, x, loss_diff)
 
-    assert np.allclose(of_out.ndarray(), tf_out.numpy(), rtol=1e-5, atol=1e-5)
+    assert np.allclose(of_out.numpy(), tf_out.numpy(), rtol=1e-5, atol=1e-5)
     assert np.allclose(
         test_global_storage.Get("x_diff"), tf_x_diff.numpy(), rtol=1e-5, atol=1e-5
     )
