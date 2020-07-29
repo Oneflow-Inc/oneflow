bcadc90bacf856b0194455831e1b1e44f2b2f576
strint <xiaoyulink@gmail.com>
2020-07-29 15:32:43 +0800

    refactor relu grad

--- oneflow/customized/ops/relu_op.cpp
+++ oneflow/customized/ops/relu_op.cpp
@@ -87,6 +87,20 @@ REGISTER_USER_OP_GRAD("relu").SetGenBackwardOpConfFn([](const user_op::UserOpWra
   }
 });
 
+REGISTER_USER_OP_GRAD("relu").SetBackwardOpConfGenFn([](user_op::BackwardOpConfContext* ctx) {
+  const auto relu_grad_op_name = ctx->FwOp().op_name() + "_grad";
+  ctx->DefineOp(relu_grad_op_name, [&ctx](user_op::BackwardOpBuilder& builder) {
+    return builder.OpTypeName("relu_grad")
+        .InputBind("y", ctx->FwOp().output("out", 0))
+        .InputBind("dy", ctx->FwOp().output_grad("out", 0))
+        .Output("dx")
+        .Build();
+  });
+  ctx->FwOp().InputGradBind(user_op::OpArg("in", 0), [&ctx, &relu_grad_op_name]() {
+    return ctx->GetOp(relu_grad_op_name).output("dx", 0);
+  });
+});
+
 }  // namespace
 
 }  // namespace oneflow
