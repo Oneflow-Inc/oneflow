6f2fcbfe5aaaf851bdf6ac760ddcce62d526ba2c
strint <xiaoyulink@gmail.com>
2020-07-29 15:40:57 +0800

    rm relu grad

--- oneflow/customized/ops/relu_op.cpp
+++ oneflow/customized/ops/relu_op.cpp
@@ -72,21 +72,6 @@ REGISTER_USER_OP("relu_grad")
       return Maybe<void>::Ok();
     });
 
-REGISTER_USER_OP_GRAD("relu").SetGenBackwardOpConfFn([](const user_op::UserOpWrapper& op,
-                                                        user_op::AddOpFn AddOp) {
-  if (op.NeedGenGradTensor4OpInput("in", 0)) {
-    user_op::UserOpConfWrapperBuilder builder(op.op_name() + "_grad");
-    user_op::UserOpConfWrapper relu_grad_op =
-        builder.Op("relu_grad")
-            .Input("y", op.output("out", 0))
-            .Input("dy", op.GetGradTensorWithOpOutput("out", 0))
-            .Output("dx")
-            .Build();
-    op.BindGradTensorWithOpInput(relu_grad_op.output("dx", 0), "in", 0);
-    AddOp(relu_grad_op);
-  }
-});
-
 REGISTER_USER_OP_GRAD("relu").SetBackwardOpConfGenFn([](user_op::BackwardOpConfContext* ctx) {
   const auto relu_grad_op_name = ctx->FwOp().op_name() + "_grad";
   ctx->DefineOp(relu_grad_op_name, [&ctx](user_op::BackwardOpBuilder& builder) {
