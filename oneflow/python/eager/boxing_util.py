618f083d78b03aee8b0917205e5db60212923fc7
lixinqi <lixinqi0703106@163.com>
2020-07-29 23:21:39 +0800

    set device "gpu" in eager_nccl_all_reduce op_conf

--- oneflow/python/eager/boxing_util.py
+++ oneflow/python/eager/boxing_util.py
@@ -722,6 +722,7 @@ def ReplaceDeviceTag(parallel_desc_symbol, device_tag, builder=None):
 
 def _GetEagerNcclAllReduce(parallel_conf, ibn2blob_object):
     op_conf = op_conf_pb.OperatorConf()
+    op_conf.device_type = c_api_util.DeviceType4DeviceTag("gpu")
     op_conf.name = "eager_nccl_all_reduce"
     op_conf.user_conf.op_type_name = "eager_nccl_all_reduce"
     op_conf.user_conf.input["in"].s.append("eager_nccl_all_reduce/in_0")
