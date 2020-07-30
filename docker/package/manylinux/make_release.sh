e38e980261e7b8f4c190d05d11bd732958c9cb63
tsai <caishenghang@1f-dev.kbaeegfb1x0ubnoznzequyxzve.bx.internal.cloudapp.net>
2020-07-30 11:45:50 +0000

    one wheel dir

--- docker/package/manylinux/make_release.sh
+++ docker/package/manylinux/make_release.sh
@@ -7,7 +7,7 @@ function release() {
     docker build --build-arg from=nvidia/cuda:$1-cudnn7-devel-centos7 -f docker/package/manylinux/Dockerfile -t $docker_tag .
     docker run --rm -it -v `pwd`:/oneflow-src -w /oneflow-src $docker_tag \
         /oneflow-src/docker/package/manylinux/build_wheel.sh --cache-dir /oneflow-src/manylinux2014-build-cache-cuda-$1 \
-        --house-dir /oneflow-src/wheelhouse-cuda-$1 \
+        --house-dir /oneflow-src/wheelhouse \
         --package-name $package_name
 }
 
