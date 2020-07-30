bb5d93179381eddab3496d69b848c1aa18f8cfd0
tsai <caishenghang@1f-dev.kbaeegfb1x0ubnoznzequyxzve.bx.internal.cloudapp.net>
2020-07-30 13:24:06 +0000

    use oneflow_cu102 in ci

--- ci/build/make.sh
+++ ci/build/make.sh
@@ -33,6 +33,7 @@ function build() {
         -v $tmp_dir/py-build-lib:/oneflow-src/build/lib/ \
         -w /ci-tmp \
         "$docker_tag" \
+        --package-name oneflow_cu102 \
         /oneflow-src/docker/package/manylinux/build_wheel.sh --python3.6
 }
 
