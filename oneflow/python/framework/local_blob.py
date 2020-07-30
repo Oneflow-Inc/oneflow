1d10f597191aebf67067c6842af03791ce689fa4
Shenghang Tsai <jackalcooper@gmail.com>
2020-07-30 13:40:50 +0800

    revert undoc methods

--- oneflow/python/framework/local_blob.py
+++ oneflow/python/framework/local_blob.py
@@ -18,7 +18,6 @@ from __future__ import absolute_import
 import numpy as np
 import oneflow.python.framework.remote_blob as remote_blob_util
 import traceback
-from oneflow.python.oneflow_export import oneflow_deprecate
 
 
 class LocalMirroredTensor(object):
@@ -36,7 +35,6 @@ class LocalMirroredTensor(object):
                 # do nothing
                 pass
 
-    @oneflow_deprecate()
     def ndarray_list(self):
         print(
             "WARNING:",
@@ -48,7 +46,6 @@ class LocalMirroredTensor(object):
     def numpy_list(self):
         return self.ndarray_list_
 
-    @oneflow_deprecate()
     def ndarray(self):
         print(
             "WARNING:",
@@ -81,7 +78,6 @@ class LocalMirroredTensorList(object):
             assert all(isinstance(ndarray, np.ndarray) for ndarray in ndarray_list)
         self.ndarray_lists_ = ndarray_lists
 
-    @oneflow_deprecate()
     def ndarray_lists(self):
         print(
             "WARNING:",
