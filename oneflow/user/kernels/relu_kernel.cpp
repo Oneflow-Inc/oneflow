da6bf2014aeccbaee217b1b9f269360da50e6e80
tsai <caishenghang@oneflow.org>
2020-08-04 19:27:54 +0800

    fix relu

--- oneflow/user/kernels/relu_kernel.cpp
+++ oneflow/user/kernels/relu_kernel.cpp
@@ -49,9 +49,11 @@ class ReluKernel final : public user_op::OpKernel {
 
 REGISTER_RELU_KERNEL(DeviceType::kCPU, float)
 REGISTER_RELU_KERNEL(DeviceType::kCPU, double)
+#ifdef WITH_CUDA
 REGISTER_RELU_KERNEL(DeviceType::kGPU, float)
 REGISTER_RELU_KERNEL(DeviceType::kGPU, double)
 REGISTER_RELU_KERNEL(DeviceType::kGPU, float16)
+#endif
 
 template<DeviceType device_type, typename T>
 class ReluGradKernel final : public user_op::OpKernel {
@@ -84,9 +86,11 @@ class ReluGradKernel final : public user_op::OpKernel {
 
 REGISTER_RELU_GRAD_KERNEL(DeviceType::kCPU, float)
 REGISTER_RELU_GRAD_KERNEL(DeviceType::kCPU, double)
+#ifdef WITH_CUDA
 REGISTER_RELU_GRAD_KERNEL(DeviceType::kGPU, float)
 REGISTER_RELU_GRAD_KERNEL(DeviceType::kGPU, double)
 REGISTER_RELU_GRAD_KERNEL(DeviceType::kGPU, float16)
+#endif
 
 }  // namespace
 
