23878a071db88c9dcc6baeb5b8a1412c6fea97fc
tsai <caishenghang@oneflow.org>
2020-08-04 19:19:03 +0800

    fix CopyElemOnGpu

--- oneflow/user/kernels/two_stage_reduce_kernel.cpp
+++ oneflow/user/kernels/two_stage_reduce_kernel.cpp
@@ -32,7 +32,11 @@ struct CopyTensor<DeviceType::kCPU, T, U> {
 template<typename T, typename U>
 struct CopyTensor<DeviceType::kGPU, T, U> {
   static void Call(DeviceCtx* ctx, const int64_t n, const T* src, U* dst) {
+#ifdef WITH_CUDA
     CopyElemOnGpu(ctx, src, dst, n);
+#else
+    UNIMPLEMENTED();
+#endif
   }
 };
 
