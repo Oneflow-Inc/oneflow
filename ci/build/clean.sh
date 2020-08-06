9591489e7c0d5016176058324299c71f8b5c0b0b
tsai <caishenghang@oneflow.org>
2020-08-06 10:01:40 +0800

    rm tmp_wheel

--- ci/build/clean.sh
+++ ci/build/clean.sh
@@ -4,3 +4,4 @@ docker run --rm \
     -v $tmp_dir:/ci-tmp \
     -w $tmp_dir:/ci-tmp busybox rm -rf /ci-tmp/wheelhouse
 docker run --rm -v $PWD:/p -w /p busybox rm -rf build
+docker run --rm -v $PWD:/p -w /p busybox rm -rf tmp_wheel
