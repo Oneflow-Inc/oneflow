e18239276e6034fbd015067cccdb4ac4bc3b896f
ouyangyu <xuanjiuye@gmail.com>
2020-08-06 15:36:21 +0800

    fix test cpu cases

--- oneflow/python/test/ops/test_copy_comm_net_pass_empty.py
+++ oneflow/python/test/ops/test_copy_comm_net_pass_empty.py
@@ -32,7 +32,7 @@ def ccrelu(x, name):
     )
 
 
-@unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY") == "True", "only test cpu cases")
+@unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
 @flow.unittest.num_nodes_required(2)
 def test_multi_node_comm_net(test_case):
     func_config = flow.FunctionConfig()
