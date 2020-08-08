09c69d23a45a6a074a436d2ce7d9b9cf9852f2d2
leaves-zwx <kunta0932@gmail.com>
2020-08-08 21:00:14 +0800

    fix merge

--- cmake/oneflow.cmake
+++ cmake/oneflow.cmake
@@ -151,7 +151,7 @@ message(STATUS "Installing necessary Python packages...")
 set(requirements_txt ${PROJECT_SOURCE_DIR}/dev-requirements.txt)
 set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${requirements_txt})
 execute_process(
-  COMMAND ${Python_EXECUTABLE} -m pip install -r ${requirements_txt}
+  COMMAND ${Python_EXECUTABLE} -m pip install -r ${requirements_txt} --user
 )
 message(STATUS "Python packages are installed.")
 
