ba5037d9921b5c85607876bc5dfa2d4668ff4ef8
leaves-zwx <kunta0932@gmail.com>
2020-08-11 20:08:49 +0800

    image_resize_keep_aspect_ratio all output tensor buffer

--- oneflow/user/kernels/image_resize_kernels.cpp
+++ oneflow/user/kernels/image_resize_kernels.cpp
@@ -187,36 +187,36 @@ class ImageResizeKeepAspectRatioKernel final : public user_op::OpKernel {
     user_op::Tensor* out_tensor = ctx->Tensor4ArgNameAndIndex("out", 0);
     user_op::Tensor* size_tensor = ctx->Tensor4ArgNameAndIndex("size", 0);
     user_op::Tensor* scale_tensor = ctx->Tensor4ArgNameAndIndex("scale", 0);
-    CHECK_GT(in_tensor->shape().elem_cnt(), 0);
-    CHECK_EQ(in_tensor->shape().elem_cnt(), out_tensor->shape().elem_cnt());
-    CHECK_EQ(in_tensor->shape().elem_cnt(), size_tensor->shape().At(0));
-    CHECK_EQ(in_tensor->shape().elem_cnt(), scale_tensor->shape().At(0));
-
+    CHECK_NOTNULL(out_tensor);
+    CHECK_NOTNULL(size_tensor);
+    CHECK_NOTNULL(scale_tensor);
     const TensorBuffer* in_img_buf = in_tensor->dptr<TensorBuffer>();
     TensorBuffer* out_img_buf = out_tensor->mut_dptr<TensorBuffer>();
-    int32_t* size_ptr = size_tensor ? size_tensor->mut_dptr<int32_t>() : nullptr;
-    float* scale_ptr = scale_tensor ? scale_tensor->mut_dptr<float>() : nullptr;
+    TensorBuffer* scale_buf = scale_tensor->mut_dptr<TensorBuffer>();
+    TensorBuffer* size_buf = size_tensor->mut_dptr<TensorBuffer>();
 
+    const int64_t num_images = in_tensor->shape().elem_cnt();
     const bool resize_longer = ctx->Attr<bool>("resize_longer");
     const int32_t target_size = ctx->Attr<int32_t>("target_size");
     const int32_t min_size = ctx->Attr<int32_t>("min_size");
     const int32_t max_size = ctx->Attr<int32_t>("max_size");
+    const std::string& interp_type = ctx->Attr<std::string>("interpolation_type");
 
-    MultiThreadLoop(in_tensor->shape().elem_cnt(), [&](size_t i) {
+    MultiThreadLoop(num_images, [&](size_t i) {
       ImageTargetResize(in_img_buf[i], out_img_buf + i, resize_longer, target_size, min_size,
-                        max_size, ctx->Attr<std::string>("interpolation_type"));
+                        max_size, interp_type);
       const int64_t org_h = in_img_buf[i].shape().At(0);
       const int64_t org_w = in_img_buf[i].shape().At(1);
       const int64_t res_h = out_img_buf[i].shape().At(0);
       const int64_t res_w = out_img_buf[i].shape().At(1);
-      if (size_ptr != nullptr) {
-        size_ptr[i * 2 + 0] = static_cast<int32_t>(res_w);
-        size_ptr[i * 2 + 1] = static_cast<int32_t>(res_h);
-      }
-      if (scale_ptr != nullptr) {
-        scale_ptr[i * 2 + 0] = static_cast<float>(res_w) / static_cast<float>(org_w);
-        scale_ptr[i * 2 + 1] = static_cast<float>(res_h) / static_cast<float>(org_h);
-      }
+
+      scale_buf[i].Resize(Shape({2}), DataType::kFloat);
+      scale_buf[i].mut_data<float>()[0] = static_cast<float>(res_w) / static_cast<float>(org_w);
+      scale_buf[i].mut_data<float>()[1] = static_cast<float>(res_h) / static_cast<float>(org_h);
+
+      size_buf[i].Resize(Shape({2}), DataType::kInt32);
+      size_buf[i].mut_data<int32_t>()[0] = static_cast<int32_t>(res_w);
+      size_buf[i].mut_data<int32_t>()[1] = static_cast<int32_t>(res_h);
     });
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
@@ -227,7 +227,7 @@ class ImageResizeKeepAspectRatioKernel final : public user_op::OpKernel {
 #define REGISTER_IMAGE_RESIZE_KERNEL(dtype)                                         \
   REGISTER_USER_KERNEL("image_resize_to_fixed")                                     \
       .SetCreateFn<ImageResizeToFixedSizeKernel>()                                  \
-      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kCPU)               \
+      .SetIsMatchedHob((user_op::HobDeviceTag() == "cpu")                           \
                        & (user_op::HobDataType("in", 0) == DataType::kTensorBuffer) \
                        & (user_op::HobAttr<DataType>("data_type") == GetDataType<dtype>::value));
 
@@ -236,10 +236,10 @@ REGISTER_IMAGE_RESIZE_KERNEL(uint8_t)
 
 REGISTER_USER_KERNEL("image_resize_keep_aspect_ratio")
     .SetCreateFn<ImageResizeKeepAspectRatioKernel>()
-    .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kCPU)
+    .SetIsMatchedHob((user_op::HobDeviceTag() == "cpu")
                      & (user_op::HobDataType("in", 0) == DataType::kTensorBuffer)
                      & (user_op::HobDataType("out", 0) == DataType::kTensorBuffer)
-                     & (user_op::HobDataType("size", 0) == DataType::kInt32)
-                     & (user_op::HobDataType("scale", 0) == DataType::kFloat));
+                     & (user_op::HobDataType("size", 0) == DataType::kTensorBuffer)
+                     & (user_op::HobDataType("scale", 0) == DataType::kTensorBuffer));
 
 }  // namespace oneflow
