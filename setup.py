17ad0c68121e9d40f1c295b05079b2eeeacf7eeb
tsai <caishenghang@oneflow.org>
2020-08-17 23:12:04 +0800

    simplify script and fmt tools

--- setup.py
+++ setup.py
@@ -25,22 +25,22 @@ parser = argparse.ArgumentParser()
 parser.register("type", "bool", lambda v: v.lower() == "true")
 parser.add_argument(
     "--with_xla",
-    type='bool',
+    type="bool",
     default=False,
-    help="Package xla libraries if true, otherwise not."
+    help="Package xla libraries if true, otherwise not.",
 )
-parser.add_argument('--build_dir', type=str, default='build')
-parser.add_argument('--package_name', type=str, default='oneflow')
+parser.add_argument("--build_dir", type=str, default="build")
+parser.add_argument("--package_name", type=str, default="oneflow")
 args, remain_args = parser.parse_known_args()
-sys.argv = ['setup.py'] + remain_args
+sys.argv = ["setup.py"] + remain_args
 
 
 REQUIRED_PACKAGES = [
-    'numpy',
-    'protobuf',
-    'tqdm',
-    'requests',
-    'onnx',
+    "numpy",
+    "protobuf",
+    "tqdm",
+    "requests",
+    "onnx",
 ]
 
 
@@ -51,44 +51,59 @@ class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
+
 python_scripts_dir = os.path.join(args.build_dir, "python_scripts")
 packages = find_packages(python_scripts_dir)
 package_dir = {
-    '':python_scripts_dir,
+    "": python_scripts_dir,
 }
 
-include_files = glob.glob("{}/python_scripts/oneflow/include/**/*".format(args.build_dir), recursive=True)
-include_files = [os.path.relpath(p, "{}/python_scripts/oneflow".format(args.build_dir)) for p in include_files]
-package_data = {'oneflow': ['_oneflow_internal.so'] + include_files}
+include_files = glob.glob(
+    "{}/python_scripts/oneflow/include/**/*".format(args.build_dir), recursive=True
+)
+include_files = [
+    os.path.relpath(p, "{}/python_scripts/oneflow".format(args.build_dir))
+    for p in include_files
+]
+package_data = {"oneflow": ["_oneflow_internal.so"] + include_files}
 
 if args.with_xla:
-    packages += ['oneflow.libs']
-    package_dir['oneflow.libs'] = 'third_party/tensorflow/lib'
-    package_data['oneflow.libs'] = ['libtensorflow_framework.so.1', 'libxla_core.so']
+    packages += ["oneflow.libs"]
+    package_dir["oneflow.libs"] = "third_party/tensorflow/lib"
+    package_data["oneflow.libs"] = ["libtensorflow_framework.so.1", "libxla_core.so"]
     # Patchelf >= 0.9 is required.
-    oneflow_internal_so = "{}/python_scripts/oneflow/_oneflow_internal.so".format(args.build_dir)
+    oneflow_internal_so = "{}/python_scripts/oneflow/_oneflow_internal.so".format(
+        args.build_dir
+    )
     rpath = os.popen("patchelf --print-rpath " + oneflow_internal_so).read()
-    command = "patchelf --set-rpath '$ORIGIN/:$ORIGIN/libs/:%s' %s" % \
-              (rpath.strip(), oneflow_internal_so)
+    command = "patchelf --set-rpath '$ORIGIN/:$ORIGIN/libs/:%s' %s" % (
+        rpath.strip(),
+        oneflow_internal_so,
+    )
     if os.system(command) != 0:
         raise Exception("Patchelf set rpath failed. command is: %s" % command)
 
+
 def get_version():
     import importlib.util
-    spec = importlib.util.spec_from_file_location("version", os.path.join(python_scripts_dir, "oneflow", "python", "version.py"))
+
+    spec = importlib.util.spec_from_file_location(
+        "version", os.path.join(python_scripts_dir, "oneflow", "python", "version.py")
+    )
     m = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(m)
     return m.__version__
 
+
 setup(
     name=args.package_name,
     version=get_version(),
-    url='https://www.oneflow.org/',
+    url="https://www.oneflow.org/",
     install_requires=REQUIRED_PACKAGES,
     packages=packages,
     package_dir=package_dir,
     package_data=package_data,
     zip_safe=False,
     distclass=BinaryDistribution,
-    cmdclass={'install': InstallPlatlib},
+    cmdclass={"install": InstallPlatlib},
 )
--- tools/generate_oneflow_symbols_export_file.py
+++ tools/generate_oneflow_symbols_export_file.py
@@ -7,30 +7,35 @@ project_source_dir = sys.argv[1]
 python_dir = project_source_dir + "/oneflow/python"
 output_filepath = sys.argv[2]
 
+
 def GetImportPath(filepath):
     assert filepath.startswith(python_dir)
     assert filepath.endswith(".py")
     assert len(filepath) > len(python_dir) + len("*.py")
-    relative_path = filepath[len(python_dir):-len(".py")]
-    while relative_path[0] == '/': relative_path = relative_path[1:]
+    relative_path = filepath[len(python_dir) : -len(".py")]
+    while relative_path[0] == "/":
+        relative_path = relative_path[1:]
     relative_path = re.sub(r"/+", ".", relative_path)
     assert re.match(r"^[_\w]+[_\w\d]*(.[_\w]+[_\w\d]*)*$", relative_path)
     return relative_path
+
 
 def RecursiveFindPythonFile(directory):
     for file_name in os.listdir(directory):
         file_path = os.path.join(directory, file_name)
         if os.path.isdir(file_path):
-            for d in RecursiveFindPythonFile(file_path): yield d
+            for d in RecursiveFindPythonFile(file_path):
+                yield d
         elif file_path.endswith(".py"):
             yield file_path
 
+
 import_filepaths = []
 for py_script in RecursiveFindPythonFile(python_dir):
-    file_content = open(py_script, 'r').read()
+    file_content = open(py_script, "r").read()
     if re.search(r"@\s?oneflow_export\s?\(", file_content) is not None:
         import_filepaths.append(py_script)
-    
+
 python_scripts = "from __future__ import absolute_import\n"
 for filepath in import_filepaths:
     python_scripts += "import oneflow.python.%s\n" % GetImportPath(filepath)
