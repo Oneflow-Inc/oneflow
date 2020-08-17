af8102a10251fb67d3bb0044fdb30b2fd2073779
Shenghang Tsai <jackalcooper@gmail.com>
2020-08-18 01:06:48 +0800

    py fmt respects gitignore (#3492)
    * simplify script and fmt tools
    
    * fix ci
    
    * add ll
    
    * fix root edit file
    
    * use docker root
    
    * w for all users
    
    * fix cpu
    
    * add back clean
    
    * rm debug print
    
    Co-authored-by: tsai <caishenghang@oneflow.org>

--- tools/clean_generated_api.py
+++ tools/clean_generated_api.py
@@ -4,17 +4,17 @@ import os
 import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-root", "--root_path", type=str, required=True
-)
+parser.add_argument("-root", "--root_path", type=str, required=True)
 args = parser.parse_args()
 
+
 def main():
-    for p in glob.glob(os.path.join(args.root_path, "oneflow/*/")): 
+    for p in glob.glob(os.path.join(args.root_path, "oneflow/*/")):
         if p.endswith("python/"):
             pass
         else:
             shutil.rmtree(p)
+
 
 if __name__ == "__main__":
     main()
