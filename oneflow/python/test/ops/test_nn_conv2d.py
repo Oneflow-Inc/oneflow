79256ae200e260a1f0a76900e92e9acec94bb06b
guo ran <360112263@qq.com>
2020-08-18 12:05:23 +0800

    conv2d cpu support groups (#3488)
    * conv2d cpu groups
    
    * address review
    
    Co-authored-by: oneflow-bot <69100618+oneflow-bot@users.noreply.github.com>

--- oneflow/python/test/ops/test_nn_conv2d.py
+++ oneflow/python/test/ops/test_nn_conv2d.py
@@ -54,12 +54,25 @@ def grouped_convolution2D(
 
 
 def compare_with_tensorflow(
-    device_type, x_shape, filters, kernel_size, groups, padding="VALID", stride=1
+    device_type,
+    x_shape,
+    filters,
+    kernel_size,
+    groups,
+    data_format="NCHW",
+    padding="VALID",
+    stride=1,
 ):
     assert device_type in ["gpu", "cpu"]
     flow.clear_default_session()
     func_config = flow.FunctionConfig()
     func_config.default_data_type(flow.float)
+    if data_format == "NCHW":
+        xy_data_transpose = (0, 2, 3, 1)
+        weight_data_transpose = (2, 3, 1, 0)
+    else:
+        xy_data_transpose = (0, 1, 2, 3)
+        weight_data_transpose = (1, 2, 3, 0)
 
     @flow.global_function(type="train", function_config=func_config)
     def ConvJob():
@@ -71,7 +84,10 @@ def compare_with_tensorflow(
                 initializer=flow.random_uniform_initializer(minval=0, maxval=100),
                 trainable=True,
             )
-            weight_shape = (filters, int(x.shape[1] / groups), kernel_size, kernel_size)
+            if data_format == "NCHW":
+                weight_shape = (filters, x.shape[1] // groups, kernel_size, kernel_size)
+            else:
+                weight_shape = (filters, kernel_size, kernel_size, x.shape[3] // groups)
             weight = flow.get_variable(
                 "conv-weight",
                 shape=weight_shape,
@@ -83,9 +99,10 @@ def compare_with_tensorflow(
                 weight,
                 strides=[stride, stride],
                 padding=padding,
-                data_format="NCHW",
+                data_format=data_format,
                 dilations=[1, 1],
                 groups=groups,
+                name="conv",
             )
             flow.optimizer.SGD(
                 flow.optimizer.PiecewiseConstantScheduler([], [1e-4]), momentum=0
@@ -104,15 +121,15 @@ def compare_with_tensorflow(
     check_point = flow.train.CheckPoint()
     check_point.init()
     of_out = ConvJob().get()
+
     # TensorFlow
     with tf.GradientTape(persistent=True) as tape:
-        x = tf.Variable(test_global_storage.Get("x").transpose(0, 2, 3, 1))
+        x = tf.Variable(test_global_storage.Get("x").transpose(xy_data_transpose))
         assert groups > 0
-        assert x_shape[1] % groups == 0
         assert filters % groups == 0
         if groups == 1:
             weight = tf.Variable(
-                test_global_storage.Get("weight").transpose(2, 3, 1, 0)
+                test_global_storage.Get("weight").transpose(weight_data_transpose)
             )
             tf_out = tf.nn.conv2d(
                 x,
@@ -123,29 +140,32 @@ def compare_with_tensorflow(
             )
         else:
             weight = tf.Variable(
-                test_global_storage.Get("weight").transpose(2, 3, 1, 0)
+                test_global_storage.Get("weight").transpose(weight_data_transpose)
             )
             tf_out = grouped_convolution2D(
                 x, weight, padding=padding, num_groups=groups
             )
 
-    loss_diff = test_global_storage.Get("loss_diff").transpose(0, 2, 3, 1)
+    loss_diff = test_global_storage.Get("loss_diff").transpose(xy_data_transpose)
     tf_x_diff = tape.gradient(tf_out, x, loss_diff)
     tf_weight_diff = tape.gradient(tf_out, weight, loss_diff)
     max_diff = np.max(
-        np.absolute(of_out.numpy().transpose(0, 2, 3, 1) - tf_out.numpy())
+        np.absolute(of_out.numpy().transpose(xy_data_transpose) - tf_out.numpy())
     )
     assert np.allclose(
-        of_out.numpy().transpose(0, 2, 3, 1), tf_out.numpy(), rtol=1e-5, atol=1e-5
+        of_out.numpy().transpose(xy_data_transpose),
+        tf_out.numpy(),
+        rtol=1e-5,
+        atol=1e-5,
     ), max_diff
     assert np.allclose(
-        test_global_storage.Get("x_diff").transpose(0, 2, 3, 1),
+        test_global_storage.Get("x_diff").transpose(xy_data_transpose),
         tf_x_diff.numpy(),
         rtol=1e-4,
         atol=1e-4,
     )
     assert np.allclose(
-        test_global_storage.Get("weight_diff").transpose(2, 3, 1, 0),
+        test_global_storage.Get("weight_diff").transpose(weight_data_transpose),
         tf_weight_diff.numpy(),
         rtol=1e-5,
         atol=1e-5,
@@ -153,25 +173,25 @@ def compare_with_tensorflow(
 
 
 def test_cpu1(test_case):
-    return
     arg_dict = OrderedDict()
     arg_dict["device_type"] = ["cpu"]
-    arg_dict["x_shape"] = [(10, 32, 226, 226)]
+    arg_dict["x_shape"] = [(10, 10, 10, 32)]
     arg_dict["filters"] = [128]
     arg_dict["kernel_size"] = [1]
-    arg_dict["groups"] = [1]
+    arg_dict["groups"] = [32]
+    arg_dict["data_format"] = ["NHWC"]
     for arg in GenArgList(arg_dict):
         compare_with_tensorflow(*arg)
 
 
 def test_cpu2(test_case):
-    return
     arg_dict = OrderedDict()
     arg_dict["device_type"] = ["cpu"]
     arg_dict["x_shape"] = [(10, 32, 226, 226)]
     arg_dict["filters"] = [64]
     arg_dict["kernel_size"] = [1]
-    arg_dict["groups"] = [1]
+    arg_dict["groups"] = [4]
+    arg_dict["data_format"] = ["NCHW"]
     for arg in GenArgList(arg_dict):
         compare_with_tensorflow(*arg)
 
@@ -194,7 +214,8 @@ def test_conv1(test_case):
     arg_dict["x_shape"] = [(10, 32, 20, 20)]
     arg_dict["filters"] = [64]
     arg_dict["kernel_size"] = [3]
-    arg_dict["groups"] = [1]
+    arg_dict["groups"] = [32]
+    arg_dict["data_format"] = ["NCHW"]
     for arg in GenArgList(arg_dict):
         compare_with_tensorflow(*arg)
 
