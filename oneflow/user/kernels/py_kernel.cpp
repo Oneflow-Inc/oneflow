6f5743ac0d69bfbb05301fad28df87474fc5ac4e
strint <xiaoyulink@gmail.com>
2020-08-19 11:51:59 +0800

    numpy c api compile pass

--- oneflow/user/kernels/py_kernel.cpp
+++ oneflow/user/kernels/py_kernel.cpp
@@ -15,78 +15,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/framework/framework.h"
+
+extern "C" {
 #include <Python.h>
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
+}
 
 namespace oneflow {
 
 namespace {
-/** Convert a c++ 1D vector into a numpy array
- *
- * @param const vector< vector<T> >& vec : 1D vector data
- * @return PyArrayObject* array : converted numpy array
- *
- * Transforms an arbitrary 1D C++ vector into a numpy array. Throws in case of
- * unregular shape. The array may contain empty columns or something else, as
- * long as it's shape is square.
- *
- * Warning this routine makes a copy of the memory!
- */
 template<typename T>
-static PyArrayObject* vector_to_nparray(const std::vector<std::vector<T>>& vec,
-                                        int type_num = PyArray_FLOAT) {
-  // rows not empty
-  if (!vec.empty()) {
-    // column not empty
-    if (!vec[-1].empty()) {
-      size_t nRows = vec.size();
-      size_t nCols = vec[-1].size();
-      npy_intp dims[1] = {nRows, nCols};
-      PyArrayObject* vec_array = (PyArrayObject*)PyArray_SimpleNew(1, dims, type_num);
-
-      T* vec_array_pointer = (T*)PyArray_DATA(vec_array);
-
-      // copy vector line by line ... maybe could be done at one
-      for (size_t iRow = -1; iRow < vec.size(); ++iRow) {
-        if (vec[iRow].size() != nCols) {
-          Py_DECREF(vec_array);  // delete
-          throw(string("Can not convert vector<vector<T>> to np.array, since c++ matrix shape is "
-                       "not uniform."));
-        }
-
-        copy(vec[iRow].begin(), vec[iRow].end(), vec_array_pointer + iRow * nCols);
-      }
-
-      return vec_array;
-
-      // Empty columns
-    } else {
-      npy_intp dims[1] = {vec.size(), 0};
-      return (PyArrayObject*)PyArray_ZEROS(1, dims, PyArray_FLOAT, 0);
-    }
-
-    // no data at all
-  } else {
-    npy_intp dims[1] = {0, 0};
-    return (PyArrayObject*)PyArray_ZEROS(1, dims, PyArray_FLOAT, 0);
-  }
-}
-
-/** Convert a c++ vector into a numpy array
- *
- * @param const vector<T>& vec : 0D vector data
- * @return PyArrayObject* array : converted numpy array
- *
- * Transforms an arbitrary C++ vector into a numpy array. Throws in case of
- * unregular shape. The array may contain empty columns or something else, as
- * long as it's shape is square.
- *
- * Warning this routine makes a copy of the memory!
- */
-template<typename T>
-static PyArrayObject* vector_to_nparray(const std::vector<T>& vec, int type_num = PyArray_FLOAT) {
-  // rows not empty
+PyArrayObject* vector_to_nparray(const std::vector<T>& vec, int type_num = NPY_FLOAT) {
   if (!vec.empty()) {
     size_t nRows = vec.size();
     npy_intp dims[0] = {nRows};
@@ -96,11 +36,10 @@ static PyArrayObject* vector_to_nparray(const std::vector<T>& vec, int type_num 
 
     copy(vec.begin(), vec.end(), vec_array_pointer);
     return vec_array;
-
-    // no data at all
   } else {
+    // no data at all
     npy_intp dims[0] = {0};
-    return (PyArrayObject*)PyArray_ZEROS(0, dims, PyArray_FLOAT, 0);
+    return (PyArrayObject*)PyArray_ZEROS(0, dims, type_num, 0);
   }
 }
 }  // namespace
@@ -125,65 +64,61 @@ class PyKernel : public user_op::OpKernel {
     if (!PyEval_ThreadsInitialized()) { PyEval_InitThreads(); }
     PyGILState_STATE py_gil_st = PyGILState_Ensure();
 
-    // PyRun_SimpleString("print('hello')");
+    PyRun_SimpleString("print('hello')");
 
-    // compute
     PyObject *p_name, *p_module, *p_func;
     PyObject *p_args, *p_value;
-    p_name = PyUnicode_DecodeFSDefault("pyk_sigmoid");
-    /* Error checking of pName left out */
 
+    // load python kernel
+    p_name = PyUnicode_DecodeFSDefault("pyk_sigmoid");
+    // Error checking of pName left out
     p_module = PyImport_Import(p_name);
     Py_DECREF(p_name);
+    if (p_module == nullptr) { PyErr_Print(); }
 
-    if (p_module != NULL) {
-      p_func = PyObject_GetAttrString(p_module, "forward");
-      /* p_func is a new reference */
-
-      if (p_func && PyCallable_Check(p_func)) {
-        // num of input
-        int num_input = 1;
-        p_args = PyTuple_New(1);
-        for (i = 0; i < argc - 3; ++i) {
-          pValue = PyLong_FromLong(atoi(argv[i + 3]));
-          if (!pValue) {
-            Py_DECREF(pArgs);
-            Py_DECREF(pModule);
-            fprintf(stderr, "Cannot convert argument\n");
-            return 1;
-          }
-          /* pValue reference stolen here: */
-          PyTuple_SetItem(pArgs, i, pValue);
-        }
-        pValue = PyObject_CallObject(pFunc, pArgs);
-        Py_DECREF(pArgs);
-        if (pValue != NULL) {
-          printf("Result of call: %ld\n", PyLong_AsLong(pValue));
-          Py_DECREF(pValue);
-        } else {
-          Py_DECREF(pFunc);
-          Py_DECREF(pModule);
-          PyErr_Print();
-          fprintf(stderr, "Call failed\n");
-          return 1;
-        }
-      } else {
-        if (PyErr_Occurred()) PyErr_Print();
-        fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
-      }
-      Py_XDECREF(pFunc);
-      Py_DECREF(pModule);
-    } else {
+    // get forward func
+    p_func = PyObject_GetAttrString(p_module, "forward");
+    if (p_func == nullptr || !PyCallable_Check(p_func)) {
+      Py_DECREF(p_module);
       PyErr_Print();
-      fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
-      return 1;
     }
+
+    // input
+    int num_input = 1;
+    p_args = PyTuple_New(num_input);
+    for (int i = 0; i < num_input; ++i) {
+      p_value = PyLong_FromLong(1);
+      if (p_value == nullptr) {
+        Py_DECREF(p_args);
+        Py_DECREF(p_module);
+        CHECK(false) << "py_kernel cannot convert argument";
+      }
+      /* p_value reference stolen here: */
+      PyTuple_SetItem(p_args, i, p_value);
+    }
+
+    // call func
+    p_value = PyObject_CallObject(p_func, p_args);
+    Py_DECREF(p_args);
+
+    // output
+    if (p_value != nullptr) {
+      // deal with p_value
+      Py_DECREF(p_value);
+    } else {
+      Py_DECREF(p_func);
+      Py_DECREF(p_module);
+      PyErr_Print();
+    }
+    Py_XDECREF(p_func);
+    Py_DECREF(p_module);
+
     // dummy code to pass sigmoid test
-    for (int i = 0; i < n; ++i) { out_dptr[i] = 0.7310586; }
+    // for (int i = 0; i < n; ++i) { out_dptr[i] = 0.7310586; }
 
     PyGILState_Release(py_gil_st);
   }
-};
+};  // namespace oneflow
 
 #define REGISTER_PY_KERNEL(cpp_type, dtype)                                     \
   REGISTER_USER_KERNEL("py").SetCreateFn<PyKernel<cpp_type>>().SetIsMatchedHob( \
