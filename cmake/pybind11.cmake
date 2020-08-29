4aeb93f8f9154035053a5432526717d0324e1be5
tsai <caishenghang@oneflow.org>
2020-08-29 13:46:41 +0800

    always add pybind11 in cmake

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -1,13 +1,11 @@
 include(FetchContent)
 
-if(THIRD_PARTY)
-    FetchContent_Declare(
-        pybind11
-        URL https://github.com/pybind/pybind11/archive/v2.5.0.zip
-    )
-    FetchContent_GetProperties(pybind11)
-    if(NOT pybind11_POPULATED)
-        FetchContent_Populate(pybind11)
-        add_subdirectory(${pybind11_SOURCE_DIR} ${pybind11_BINARY_DIR})
-    endif()
-endif(THIRD_PARTY)
+FetchContent_Declare(
+    pybind11
+    URL https://github.com/pybind/pybind11/archive/v2.5.0.zip
+)
+FetchContent_GetProperties(pybind11)
+if(NOT pybind11_POPULATED)
+    FetchContent_Populate(pybind11)
+    add_subdirectory(${pybind11_SOURCE_DIR} ${pybind11_BINARY_DIR})
+endif()
