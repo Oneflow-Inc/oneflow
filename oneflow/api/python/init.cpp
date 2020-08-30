da5d86a1b84c95771b285e38ee601748d912069f
tsai <caishenghang@oneflow.org>
2020-08-30 09:51:16 +0800

    rm unused include header

--- oneflow/api/python/init.cpp
+++ oneflow/api/python/init.cpp
@@ -14,10 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include <pybind11/pybind11.h>
-#include "oneflow/core/job/job_build_and_infer_ctx.h"
 #include "oneflow/core/job/job_build_and_infer_ctx_mgr.h"
-#include "oneflow/core/common/global.h"
-#include "oneflow/core/job/global_for.h"
 
 namespace py = pybind11;
 
