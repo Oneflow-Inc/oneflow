cc0d5862d49b08982aefca451f15caaf18172166
tsai <caishenghang@1f-dev.kbaeegfb1x0ubnoznzequyxzve.bx.internal.cloudapp.net>
2020-09-03 09:20:07 +0000

    fix path in script

--- docker/package/manylinux/build_xla.sh
+++ docker/package/manylinux/build_xla.sh
@@ -1,6 +1,6 @@
 set -ex
 wheelhouse_dir=/oneflow-src/wheelhouse-xla
-ONEFLOW_SRC_DIR=${ONEFLOW_SRC_DIR:-/oneflow-src}
+ONEFLOW_SRC_DIR=${ONEFLOW_SRC_DIR:-${PWD}}
 
 # TF requires py3 to build
 PY_ROOT=/opt/python/cp37-cp37m
@@ -10,8 +10,11 @@ python --version
 
 source scl_source enable devtoolset-7
 
+cache_dir=$ONEFLOW_SRC_DIR/manylinux2014-build-cache-cuda-10.2-xla
+export TEST_TMPDIR=$cache_dir/bazel_cache
 bash docker/package/manylinux/build_wheel.sh \
     --python3.7 \
-    --cache-dir $ONEFLOW_SRC_DIR/manylinux2014-build-cache-cuda-10.2-xla \
+    --cache-dir $cache_dir \
     --house-dir $wheelhouse_dir \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
     -DWITH_XLA=ON
