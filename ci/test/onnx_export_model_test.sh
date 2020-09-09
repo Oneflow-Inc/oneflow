7ee4a23dc4527abfe1ca3f3ae5f052822677e631
daquexian <daquexian566@gmail.com>
2020-09-09 17:10:00 +0800

    move onnx save test into test/onnx/save, rename load/test_pooling.py->load/test_pooling.py

--- ci/test/onnx_export_model_test.sh
+++ ci/test/onnx_export_model_test.sh
@@ -6,6 +6,9 @@ test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"/test_tmp_dir"}
 rm -rf $test_tmp_dir
 mkdir -p $test_tmp_dir
 cp -r oneflow/python/test/onnx/* $test_tmp_dir
-cd $test_tmp_dir
+cd $test_tmp_dir/save
+python3 test_model.py
+python3 test_node.py test_conv test_pooling
+cd $test_tmp_dir/load
 python3 test_model.py
 python3 test_node.py test_conv test_pooling
