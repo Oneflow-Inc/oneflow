47b9f717f574d08745b291bd71b1c0e3959cace7
WangTuoxyty <824023604@qq.com>
2020-09-12 12:01:55 +0800

    rm return

--- oneflow/python/ops/math_ops.py
+++ oneflow/python/ops/math_ops.py
@@ -1863,13 +1863,4 @@ def invert_permutation(
 
         # out [2 4 3 0 1]
     """
-    return (
-        flow.user_op_builder(name if name is not None else id_util.UniqueStr("InvertPermutation_"))
-        .Op("invert_permutation")
-        .Input("in", [input])
-        .Output("out")
-        .Build()
-        .InferAndTryRun()
-        .RemoteBlobList()[0]
-    )
 
