07e1e0dfdd110ddd1e53f04366d9e33d2b344646
poohRui <yuruil@qq.com>
2020-09-13 11:19:18 +0800

    modified openssl install directory and format cmake file

--- cmake/third_party/absl.cmake
+++ cmake/third_party/absl.cmake
@@ -5,27 +5,23 @@ SET(ABSL_PROJECT absl)
 SET(ABSL_GIT_URL https://github.com/abseil/abseil-cpp.git)
 SET(ABSL_GIT_TAG 43ef2148c0936ebf7cb4be6b19927a9d9d145b8f)
  
-# SET(ABSL_SOURCE_DIR ${THIRD_PARTY_SUBMODULE_DIR}/absl)
 SET(ABSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/absl/src/absl)
-SET(ABSL_INSTALL_DIR ${THIRD_PARTY_DIR}/absl)
+SET(ABSL_INSTALL ${CMAKE_CURRENT_BINARY_DIR}/absl/install)
 
-SET(ABSL_INCLUDE_DIR ${ABSL_INSTALL_DIR}/include CACHE PATH "" FORCE)
-SET(ABSL_LIBRARY_DIR ${ABSL_INSTALL_DIR}/lib CACHE PATH "" FORCE)
-
-# INCLUDE_DIRECTORIES(${ABSL_INCLUDE_DIR})
-# LINK_DIRECTORIES(${ABSL_LIBRARY_DIR})
+SET(ABSL_INCLUDE_DIR ${THIRD_PARTY_DIR}/absl/include CACHE PATH "" FORCE)
+SET(ABSL_LIBRARY_DIR ${THIRD_PARTY_DIR}/absl/lib CACHE PATH "" FORCE)
 
 SET(ABSL_LIBRARIES
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_base.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_spinlock_wait.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_dynamic_annotations.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_malloc_internal.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_throw_delegate.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_int128.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_strings.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_str_format_internal.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_time.a
-    ${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}/libabsl_bad_optional_access.a)
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_base.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_spinlock_wait.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_dynamic_annotations.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_malloc_internal.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_throw_delegate.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_int128.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_strings.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_str_format_internal.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_time.a
+    ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}/libabsl_bad_optional_access.a)
 
 if (THIRD_PARTY)
   ExternalProject_Add(${ABSL_PROJECT}
@@ -40,19 +36,18 @@ if (THIRD_PARTY)
         -DCMAKE_CXX_FLAGS_DEBUG:STRING=${CMAKE_CXX_FLAGS_DEBUG}
         -DCMAKE_CXX_FLAGS_RELEASE:STRING=${CMAKE_CXX_FLAGS_RELEASE}
     CMAKE_CACHE_ARGS
-        -DCMAKE_INSTALL_PREFIX:PATH=${ABSL_SOURCE_DIR}
-        -DCMAKE_INSTALL_LIBDIR:PATH=${ABSL_SOURCE_DIR}/${CMAKE_INSTALL_LIBDIR}
+        -DCMAKE_INSTALL_PREFIX:PATH=${ABSL_INSTALL}
+        -DCMAKE_INSTALL_LIBDIR:PATH=${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}
         -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
         -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
   )
-
 
 add_custom_target(absl_create_library_dir
   COMMAND ${CMAKE_COMMAND} -E make_directory ${ABSL_LIBRARY_DIR}
   DEPENDS absl)
 
 add_custom_target(absl_copy_headers_to_destination
-  COMMAND ${CMAKE_COMMAND} -E create_symlink ${ABSL_SOURCE_DIR}/include ${THIRD_PARTY_DIR}/absl/include
+  COMMAND ${CMAKE_COMMAND} -E create_symlink ${ABSL_INSTALL}/include ${ABSL_INCLUDE_DIR}
   DEPENDS absl_create_library_dir)
 
 add_custom_target(absl_copy_libs_to_destination
--- cmake/third_party/openssl.cmake
+++ cmake/third_party/openssl.cmake
@@ -1,13 +1,9 @@
 include (ExternalProject)
 
-set(OPENSSL_INCLUDE_DIR ${THIRD_PARTY_DIR}/openssl/include)
-set(OPENSSL_LIBRARY_DIR ${THIRD_PARTY_DIR}/openssl/lib)
+set(OPENSSL_INSTALL ${CMAKE_CURRENT_BINARY_DIR}/openssl/install)
 
-set(OPENSSL_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/openssl/src/openssl/include)
 SET(OPENSSL_TAR_URL https://github.com/openssl/openssl/archive/OpenSSL_1_1_1g.tar.gz)
 set(OPENSSL_URL_HASH dd32f35dd5d543c571bc9ebb90ebe54e)
-set(OPENSSL_INSTALL ${THIRD_PARTY_DIR}/openssl)
-# SET(OPENSSL_SOURCE_DIR ${THIRD_PARTY_SUBMODULE_DIR}/openssl)
 set (OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/openssl)
 
 if(THIRD_PARTY)
