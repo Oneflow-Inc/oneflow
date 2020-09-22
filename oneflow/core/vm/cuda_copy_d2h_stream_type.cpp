6bfff16e1b087e98610a6688717beb085a5f1efb
hsj0429 <hsj0429@hotmail.com>
2020-09-22 20:15:24 +0800

    add comments for cuda_copy_d2h_stream_type.cpp

--- oneflow/core/vm/cuda_copy_d2h_stream_type.cpp
+++ oneflow/core/vm/cuda_copy_d2h_stream_type.cpp
@@ -21,27 +21,42 @@ limitations under the License.
 namespace oneflow {
 namespace vm {
 
+// Initializes CudaCopyD2HDeviceCtx by stream, CudaCopyD2HDeviceCtx contains CudaStreamHandle object and CallbackMsgListPtr object, 
+// the relative istructions will be send to this DeviceCtx
+// inputs_0: stream
+// outputs_0: device_ctx 
 void CudaCopyD2HStreamType::InitDeviceCtx(std::unique_ptr<DeviceCtx>* device_ctx,
                                           Stream* stream) const {
   device_ctx->reset(new CudaCopyD2HDeviceCtx(stream->mut_callback_list()));
 }
 
+// Initializes instruction status by stream, the instruction status should be true or false,
+// it will new a CudaInstrStatusQuerier object and puts it into status_buffer
+// inputs_0: stream
+// outputs_0: status_buffer
 void CudaCopyD2HStreamType::InitInstructionStatus(const Stream& stream,
                                                   InstructionStatusBuffer* status_buffer) const {
   static_assert(sizeof(CudaInstrStatusQuerier) < kInstructionStatusBufferBytes, "");
   CudaInstrStatusQuerier::PlacementNew(status_buffer->mut_buffer()->mut_data(), stream.device_id());
 }
 
+// Delete instruction status by stream
+// inputs_0: stream
+// outputs_0: status_buffer
 void CudaCopyD2HStreamType::DeleteInstructionStatus(const Stream& stream,
                                                     InstructionStatusBuffer* status_buffer) const {
   // do nothing
 }
 
+// Returns both of the instruction is laungched and event is completed or not
 bool CudaCopyD2HStreamType::QueryInstructionStatusDone(
     const Stream& stream, const InstructionStatusBuffer& status_buffer) const {
   return CudaInstrStatusQuerier::Cast(status_buffer.buffer().data())->done();
 }
 
+// Execs instruction by instruction type, puts stream callback_list into instruction callback_list, 
+// and then set instruction laungched
+// inputs: instruction
 void CudaCopyD2HStreamType::Compute(Instruction* instruction) const {
   auto* stream = instruction->mut_stream();
   cudaSetDevice(stream->device_id());
@@ -56,6 +71,9 @@ void CudaCopyD2HStreamType::Compute(Instruction* instruction) const {
   CudaInstrStatusQuerier::MutCast(data_ptr)->SetLaunched(stream->device_ctx().get());
 }
 
+// Initializes stream desc
+// inputs_0: resource
+// inputs_1: this_machine_id
 ObjectMsgPtr<StreamDesc> CudaCopyD2HStreamType::MakeStreamDesc(const Resource& resource,
                                                                int64_t this_machine_id) const {
   if (!resource.has_gpu_device_num()) { return ObjectMsgPtr<StreamDesc>(); }
