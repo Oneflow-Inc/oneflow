5a6e02ba88834bef953d38ff8301e1b6a625935e
doombeaker <later@usopp.net>
2020-09-28 12:53:30 +0800

    segment fault to be fixed

--- oneflow/user/kernels/torch_gather_kernel.cpp
+++ oneflow/user/kernels/torch_gather_kernel.cpp
@@ -14,33 +14,19 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/framework/framework.h"
-#include "oneflow/core/kernel/gather_kernel_util.h"
 #include "oneflow/core/common/balanced_splitter.h"
 
 namespace oneflow {
 
 namespace user_op {
 
-namespace {
-
-Shape GetFlatShape(const ShapeView& shape, int64_t axis) {
-  return Shape({shape.Count(0, axis), shape.At(axis), shape.Count(axis + 1)});
+template <typename T>
+void MyRelu(DeviceCtx *ctx, const int64_t n, const T *x, T *y) {
+  T zero = (T)(0);
+  for (int64_t i = 0; i != n; ++i) {
+    y[i] = std::max(x[i], zero);
+  }
 }
-
-class GatherOpKernelState final : public user_op::OpKernelState {
- public:
-  GatherOpKernelState(int64_t lower, int64_t upper) : lower_(lower), upper_(upper) {}
-  ~GatherOpKernelState() override = default;
-
-  int64_t lower() const { return lower_; }
-  int64_t upper() const { return upper_; }
-
- private:
-  const int64_t lower_;
-  const int64_t upper_;
-};
-
-}  // namespace
 
 template<DeviceType device_type, typename T, typename K>
 class GatherKernel final : public user_op::OpKernel {
@@ -48,48 +34,33 @@ class GatherKernel final : public user_op::OpKernel {
   GatherKernel() = default;
   ~GatherKernel() override = default;
 
-  std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
-      user_op::KernelInitContext* ctx) const override {
-    const auto axis = ctx->Attr<int64_t>("axis");
-    const SbpParallel& in_sbp = ctx->SbpParallel4ArgNameAndIndex("in", 0);
-    if (in_sbp.has_split_parallel() && in_sbp.split_parallel().axis() == axis
-        && ctx->parallel_ctx().parallel_num() > 1) {
-      CHECK(ctx->SbpParallel4ArgNameAndIndex("indices", 0).has_broadcast_parallel());
-      CHECK(ctx->SbpParallel4ArgNameAndIndex("out", 0).has_partial_sum_parallel());
-      const TensorDesc* in_logical_desc = ctx->LogicalTensorDesc4ArgNameAndIndex("in", 0);
-      const int64_t gather_dim_size = in_logical_desc->shape().At(axis);
-      BalancedSplitter bs(gather_dim_size, ctx->parallel_ctx().parallel_num());
-      return std::make_shared<GatherOpKernelState>(bs.At(ctx->parallel_ctx().parallel_id()).begin(),
-                                                   bs.At(ctx->parallel_ctx().parallel_id()).end());
-    } else {
-      return std::shared_ptr<OpKernelState>(nullptr);
-    }
-  }
-
  private:
-  void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state) const override {
-    const user_op::Tensor* in = ctx->Tensor4ArgNameAndIndex("input", 0);
-    const user_op::Tensor* indices = ctx->Tensor4ArgNameAndIndex("index", 0);
-    const int64_t axis = ctx->Attr<int64_t>("dim");
-    const bool is_sparse_grad = ctx->Attr<bool>("sparse_grad");
-    const int64_t num_indices = indices->shape().elem_cnt();
-    user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("out", 0);
-
-    //to be added...
+  void Compute(user_op::KernelComputeContext* ctx) const override {
+   const user_op::Tensor *in_tensor = ctx->Tensor4ArgNameAndIndex("in", 0);
+    user_op::Tensor *out_tensor = ctx->Tensor4ArgNameAndIndex("out", 0);
+    MyRelu<T>(ctx->device_ctx(),
+           in_tensor->shape().elem_cnt(),
+           in_tensor->dptr<T>(), 
+           out_tensor->mut_dptr<T>());
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_GATHER_KERNEL(device, in_type, indices_type)                                \
+#define REGISTER_PYTORCH_GATHER_KERNEL(device, in_type, indices_type)                                \
   REGISTER_USER_KERNEL("torch_gather")                                                             \
       .SetCreateFn<                                                                          \
           GatherKernel<device, OF_PP_PAIR_FIRST(in_type), OF_PP_PAIR_FIRST(indices_type)>>() \
       .SetIsMatchedHob((user_op::HobDeviceTag() == device)                                   \
-                       & (user_op::HobDataType("input", 0) == OF_PP_PAIR_SECOND(in_type))       \
-                       & (user_op::HobDataType("index", 0) == OF_PP_PAIR_SECOND(indices_type)));
+                       & (user_op::HobDataType("in", 0) == OF_PP_PAIR_SECOND(in_type))       \
+                       & (user_op::HobDataType("indices", 0) == OF_PP_PAIR_SECOND(indices_type)));
 
-OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(REGISTER_GATHER_KERNEL, DEVICE_TYPE_SEQ, GATHER_DATA_TYPE_SEQ,
-                                 INDEX_DATA_TYPE_SEQ)
+#define GATHER_DATA_TYPE_SEQ ARITHMETIC_DATA_TYPE_SEQ FLOAT16_DATA_TYPE_SEQ
+
+
+OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(REGISTER_PYTORCH_GATHER_KERNEL,
+                                DEVICE_TYPE_SEQ, 
+                                GATHER_DATA_TYPE_SEQ,
+                                INDEX_DATA_TYPE_SEQ)
 
 }  // namespace user_op
 
--- oneflow/user/ops/torch_gather_op.cpp
+++ oneflow/user/ops/torch_gather_op.cpp
@@ -18,83 +18,18 @@ limitations under the License.
 namespace oneflow {
 
 REGISTER_USER_OP("torch_gather")
-    .Input("input")
+    .Input("in")
     .Input("index")
     .Output("out")
-    .Attr("dim", UserOpAttrType::kAtInt64)
+    .Attr("axis", UserOpAttrType::kAtInt64)
     .Attr("sparse_grad", UserOpAttrType::kAtBool)
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      const user_op::TensorDesc* in = ctx->TensorDesc4ArgNameAndIndex("input", 0);
-      CHECK_GT_OR_RETURN(in->shape().NumAxes(), 0);
-      const int64_t axis = ctx->Attr<int64_t>("dim");
-      const user_op::TensorDesc* indices = ctx->TensorDesc4ArgNameAndIndex("index", 0);
-      CHECK_OR_RETURN(IsIndexDataType(indices->data_type()));
-      CHECK_GT_OR_RETURN(indices->shape().NumAxes(), 0);
-      user_op::TensorDesc* out = ctx->TensorDesc4ArgNameAndIndex("out", 0);
-
-      DimVector dim_vec;
-      dim_vec.insert(dim_vec.end(), in->shape().dim_vec().cbegin(),
-                     in->shape().dim_vec().cbegin() + axis);
-      dim_vec.insert(dim_vec.end(), indices->shape().dim_vec().cbegin(),
-                     indices->shape().dim_vec().cend());
-      dim_vec.insert(dim_vec.end(), in->shape().dim_vec().cbegin() + axis + 1,
-                     in->shape().dim_vec().end());
-      *out->mut_shape() = Shape(dim_vec);
-      out->set_is_dynamic(indices->is_dynamic());
-      *out->mut_data_type() = in->data_type();
-      return Maybe<void>::Ok();
-    })
-    .SetInputArgModifyFn([](user_op::GetInputArgModifier GetInputArgModifierFn,
-                            const user_op::UserOpConfWrapper&) {
-      user_op::InputArgModifier* indices_modifier = GetInputArgModifierFn("indices", 0);
-      CHECK(indices_modifier != nullptr);
-      indices_modifier->set_requires_grad(false);
-    })
-    .SetBatchAxisInferFn([](user_op::BatchAxisContext* ctx) -> Maybe<void> {
-      if (ctx->BatchAxis4ArgNameAndIndex("index", 0)->has_value()) {
-        ctx->BatchAxis4ArgNameAndIndex("out", 0)->set_value(
-            ctx->Attr<int64_t>("dim") + ctx->BatchAxis4ArgNameAndIndex("index", 0)->value());
-      } else {
-        ctx->BatchAxis4ArgNameAndIndex("out", 0)->clear_value();
-      }
-      return Maybe<void>::Ok();
-    })
-    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
-      const int64_t in_num_axes =
-          ctx->LogicalTensorDesc4InputArgNameAndIndex("input", 0).shape().NumAxes();
-      const int64_t indices_num_axes =
-          ctx->LogicalTensorDesc4InputArgNameAndIndex("index", 0).shape().NumAxes();
-      const int64_t gather_axis = ctx->Attr<int64_t>("dim");
-      CHECK_GE_OR_RETURN(gather_axis, 0);
-      CHECK_LT_OR_RETURN(gather_axis, in_num_axes);
-      FOR_RANGE(int64_t, i, 0, indices_num_axes) {
-        ctx->NewBuilder()
-            .Split(user_op::OpArg("index", 0), i)
-            .Broadcast(user_op::OpArg("input", 0))
-            .Split(user_op::OpArg("out", 0), gather_axis + i)
-            .Build();
-      }
-      FOR_RANGE(int64_t, i, 0, in_num_axes) {
-        if (i == gather_axis) {
-          ctx->NewBuilder()
-              .Broadcast(user_op::OpArg("index", 0))
-              .Split(user_op::OpArg("input", 0), i)
-              .PartialSum(user_op::OpArg("out", 0))
-              .Build();
-        } else {
-          ctx->NewBuilder()
-              .Broadcast(user_op::OpArg("index", 0))
-              .Split(user_op::OpArg("input", 0), i)
-              .Split(user_op::OpArg("out", 0), i < gather_axis ? i : i + indices_num_axes - 1)
-              .Build();
-        }
-      }
+      *ctx->Shape4ArgNameAndIndex("out", 0) =
+      *ctx->Shape4ArgNameAndIndex("in", 0);
+      *ctx->Dtype4ArgNameAndIndex("out", 0) =
+      *ctx->Dtype4ArgNameAndIndex("in", 0);
+      
       return Maybe<void>::Ok();
     });
-
-REGISTER_USER_OP_GRAD("torch_gather").SetGenBackwardOpConfFn([](const user_op::UserOpWrapper& op,
-                              user_op::AddOpFn AddOp) {
-    // to be added...
-  });
 
 }  // namespace oneflow
