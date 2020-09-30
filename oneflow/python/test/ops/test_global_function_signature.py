2d498d27e7754ebeae9f28e8253b9dfa81f3c580
Shenghang Tsai <jackalcooper@gmail.com>
2020-09-30 19:11:17 +0800

    fix import unittest

--- oneflow/python/test/ops/test_global_function_signature.py
+++ oneflow/python/test/ops/test_global_function_signature.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import unittest
 import oneflow as flow
 import oneflow.typing as oft
 import numpy as np
