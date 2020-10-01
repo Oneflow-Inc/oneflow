3a418defcafe9ea8a64b1a52b03be9362bca495c
lixinqi <lixinqi0703106@163.com>
2020-10-01 20:47:18 +0800

    rename OF_BARRIAER

--- oneflow/core/comm_network/epoll/epoll_comm_network.cpp
+++ oneflow/core/comm_network/epoll/epoll_comm_network.cpp
@@ -78,7 +78,7 @@ EpollCommNet::~EpollCommNet() {
     LOG(INFO) << "CommNet Thread " << i << " finish";
     pollers_[i]->Stop();
   }
-  OF_BARRIER();
+  OF_SESSION_BARRIER();
   for (IOEventPoller* poller : pollers_) { delete poller; }
   for (auto& pair : sockfd2helper_) { delete pair.second; }
 }
