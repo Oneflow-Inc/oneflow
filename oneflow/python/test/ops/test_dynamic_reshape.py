93ca08356f44d5e8e783eb944ed03e281e21f695
tsai <caishenghang@oneflow.org>
2020-10-03 00:47:17 +0800

    fix dynamic reshape

--- oneflow/python/test/ops/test_dynamic_reshape.py
+++ oneflow/python/test/ops/test_dynamic_reshape.py
@@ -49,7 +49,7 @@ class TestDynamicReshape(flow.unittest.TestCase):
 
         data = [np.random.rand(*data_shape).astype(np.float32) for i in range(2)]
         out = DynamicReshapeJob(data).get().numpy_list()
-        for i in range(num_gpus):
+        for i in range(2):
             test_case.assertTrue(np.array_equal(np.reshape(data[i], (50, 20)), out[i]))
 
 
