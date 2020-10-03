aaeef1a8728f6cd8baa795ee0b2e2c3e9713db74
Shenghang Tsai <jackalcooper@gmail.com>
2020-10-03 17:55:15 +0800

    add mirror

--- cmake/third_party/cub.cmake
+++ cmake/third_party/cub.cmake
@@ -3,11 +3,14 @@ include (ExternalProject)
 set(CUB_INCLUDE_DIR ${THIRD_PARTY_DIR}/cub/include)
 set(CUB_BUILD_INCLUDE ${CMAKE_CURRENT_BINARY_DIR}/cub/src/cub/cub)
 
+set(CUB_URL https://github.com/NVlabs/cub/archive/v1.8.0.tar.gz)
+use_mirror(VARIABLE CUB_URL URL ${CUB_URL})
+
 if(THIRD_PARTY)
 
 ExternalProject_Add(cub
     PREFIX cub
-    URL https://github.com/NVlabs/cub/archive/v1.8.0.tar.gz
+    URL ${CUB_URL}
     URL_MD5 9203ea2499b56782601fddf8a12e9b08
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
