f943f6f239869484da78a17bc7871b8627159ff3
MARD1NO <359521840@qq.com>
2020-10-13 12:25:49 +0800

    delete static cast

--- oneflow/user/ops/broadcast_like_op.cpp
+++ oneflow/user/ops/broadcast_like_op.cpp
@@ -106,8 +106,7 @@ REGISTER_USER_OP_GRAD("broadcast_like")
             .InputBind("x", ctx->FwOp().output_grad("y", 0))
             .InputBind("like", ctx->FwOp().input("x", 0))
             .Output("y")
-            .Attr("axis", static_cast<std::vector<int32_t>>(
-                              ctx->FwOp().attr<std::vector<int32_t>>("broadcast_axes")))
+            .Attr("axis", ctx->FwOp().attr<std::vector<int32_t>>("broadcast_axes"))
             .Build();
       });
 
