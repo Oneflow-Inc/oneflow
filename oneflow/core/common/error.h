9f16222ee89a7a386080600d01bfe7af0e0d9ad4
clackhan <hanbinbin17@mails.ucas.ac.cn>
2020-10-13 17:27:31 +0800

    fix code style

--- oneflow/core/common/error.h
+++ oneflow/core/common/error.h
@@ -26,7 +26,8 @@ namespace oneflow {
 class Error final {
  public:
   Error(const std::shared_ptr<cfg::ErrorProto>& error_proto) : error_proto_(error_proto) {}
-  Error(const std::shared_ptr<ErrorProto>& error_proto) : Error(std::make_shared<cfg::ErrorProto>(*(error_proto.get()))) {}
+  Error(const std::shared_ptr<ErrorProto>& error_proto)
+      : Error(std::make_shared<cfg::ErrorProto>(*(error_proto.get()))) {}
   Error(const Error&) = default;
   ~Error() = default;
 
