772971b3ea8632b839ca5fc9cd13ce519405ff42
tsai <caishenghang@oneflow.org>
2020-10-16 19:35:59 +0800

    cmake working

--- docker/package/manylinux/build_oneflow.py
+++ docker/package/manylinux/build_oneflow.py
@@ -7,28 +7,56 @@ def build_arg_env(env_var_name):
     return f"--build-arg {env_var_name}={val}"
 
 
-def build_img(cuda_version, oneflow_src_dir, use_tuna, use_system_proxy):
+def build_img(cuda_version, oneflow_src_dir, use_tuna, use_system_proxy, skip_img):
     cudnn_version = 7
     if str(cuda_version).startswith("11"):
         cudnn_version = 8
     from_img = f"nvidia/cuda:{cuda_version}-cudnn{cudnn_version}-devel-centos7"
     img_tag = f"oneflow:manylinux2014-cuda{cuda_version}"
-    tuna_build_arg = ""
-    if use_tuna:
-        tuna_build_arg = '--build-arg use_tuna_yum=1 --build-arg pip_args="-i https://pypi.tuna.tsinghua.edu.cn/simple"'
-    proxy_build_args = []
-    if use_system_proxy:
-        for v in ["HTTP_PROXY", "HTTPS_PROXY", "ALL_PROXY"]:
-            proxy_build_args.append(build_arg_env(v))
-    proxy_build_arg = " ".join(proxy_build_args)
-    cmd = f"docker build -f docker/package/manylinux/Dockerfile {proxy_build_arg} {tuna_build_arg} --build-arg from={from_img} -t {img_tag} ."
-    print(cmd)
-    subprocess.check_call(cmd, cwd=oneflow_src_dir, shell=True)
+    if skip_img == False:
+        tuna_build_arg = ""
+        if use_tuna:
+            tuna_build_arg = '--build-arg use_tuna_yum=1 --build-arg pip_args="-i https://pypi.tuna.tsinghua.edu.cn/simple"'
+        proxy_build_args = []
+        if use_system_proxy:
+            for v in ["HTTP_PROXY", "HTTPS_PROXY", "ALL_PROXY"]:
+                proxy_build_args.append(build_arg_env(v))
+        proxy_build_arg = " ".join(proxy_build_args)
+        cmd = f"docker build -f docker/package/manylinux/Dockerfile {proxy_build_arg} {tuna_build_arg} --build-arg from={from_img} -t {img_tag} ."
+        print(cmd)
+        subprocess.check_call(cmd, cwd=oneflow_src_dir, shell=True)
     return img_tag
 
 
-def build_third_party():
-    pass
+def common_cmake_args(cache_dir):
+    third_party_install_dir = os.path.join(cache_dir, "build-third-party-install")
+    return f"-DCMAKE_BUILD_TYPE=Release -DBUILD_RDMA=ON -DTHIRD_PARTY_DIR={third_party_install_dir}"
+
+
+def get_build_dir_arg(cache_dir, oneflow_src_dir):
+    build_dir_real = os.path.join(cache_dir, "build")
+    build_dir_mount = os.path.join(oneflow_src_dir, "build")
+    return f"-v {build_dir_real}:{build_dir_mount}"
+
+
+def build_third_party(img_tag, oneflow_src_dir, cache_dir, extra_oneflow_cmake_args):
+    third_party_build_dir = os.path.join(cache_dir, "build-third-party")
+    cmake_cmd = " ".join(
+        [
+            "cmake",
+            common_cmake_args(cache_dir),
+            "-DTHIRD_PARTY=ON, -DONEFLOW=OFF",
+            extra_oneflow_cmake_args,
+            oneflow_src_dir,
+        ]
+    )
+    cwd = os.getcwd()
+    pwd_arg = f"-v {cwd}:{cwd}"
+    cache_dir_arg = f"-v {cache_dir}:{cache_dir}"
+    build_dir_arg = get_build_dir_arg(cache_dir, oneflow_src_dir)
+    cmd = f"docker run --rm -it -v {oneflow_src_dir}:{oneflow_src_dir} {pwd_arg} {cache_dir_arg} {build_dir_arg} -w {third_party_build_dir} {img_tag} {cmake_cmd}"
+    print(cmd)
+    subprocess.check_call(cmd, shell=True)
 
 
 def build_oneflow():
@@ -40,7 +68,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--cache_dir", type=str, required=False, default="manylinux2014-build-cache"
+        "--cache_dir",
+        type=str,
+        required=False,
+        default=os.path.join(os.getcwd(), "manylinux2014-build-cache"),
     )
     parser.add_argument(
         "--wheel_house_dir", type=str, required=False, default="wheelhouse",
@@ -64,12 +95,34 @@ if __name__ == "__main__":
         "--skip_wheel", default=False, action="store_true", required=False
     )
     parser.add_argument(
+        "--skip_img", default=False, action="store_true", required=False
+    )
+    parser.add_argument(
         "--use_tuna", default=False, action="store_true", required=False
     )
     parser.add_argument(
         "--use_system_proxy", default=False, action="store_true", required=False
     )
+    parser.add_argument("--xla", default=False, action="store_true", required=False)
+    parser.add_argument(
+        "--aliyun_mirror", default=False, action="store_true", required=False
+    )
+    parser.add_argument("--cpu", default=False, action="store_true", required=False)
     args = parser.parse_args()
-    build_img(
-        args.cuda_version, args.oneflow_src_dir, args.use_tuna, args.use_system_proxy
+    img_tag = build_img(
+        args.cuda_version,
+        args.oneflow_src_dir,
+        args.use_tuna,
+        args.use_system_proxy,
+        args.skip_img,
+    )
+    extra_oneflow_cmake_args = args.extra_oneflow_cmake_args
+    if args.aliyun_mirror:
+        extra_oneflow_cmake_args += " -DTHIRD_PARTY_MIRROR=aliyun"
+    if args.cpu:
+        extra_oneflow_cmake_args += " -DBUILD_CUDA=OFF"
+    else:
+        extra_oneflow_cmake_args += " -DBUILD_CUDA=ON"
+    build_third_party(
+        img_tag, args.oneflow_src_dir, args.cache_dir, extra_oneflow_cmake_args
     )
