53b532b188874e24b7d58f1b73dd488a16fdbfd8
daquexian <daquexian566@gmail.com>
2020-10-23 16:38:02 +0800

    address comments

--- oneflow/python/eager/vm_util.py
+++ oneflow/python/eager/vm_util.py
@@ -274,28 +274,29 @@ class InstructionsBuilder(object):
                 AppendPhyParallelDescSymbol(machine_id, device_id)
         return phy_parallel_desc_symbols
 
-    def GetPhysicalOpArgBlobAttrs(self, logical_blob_object):
+    def _GetPhysicalOpArgBlobAttrs(self, logical_blob_object):
         parallel_num = logical_blob_object.parallel_desc_symbol.parallel_num
         logical_blob_attr = logical_blob_object.op_arg_blob_attr
         sbp_parallel = logical_blob_object.op_arg_parallel_attr.sbp_parallel
+
+        def GetSplittedBlobAttr(
+            logical_blob_attr, split_axis, parallel_num, parallel_id
+        ):
+            blob_desc = blob_desc_pb.BlobDescProto()
+            blob_desc.CopyFrom(logical_blob_attr.blob_desc)
+            physical_len = balanced_splitter.BalancedPartNums(
+                logical_blob_attr.shape[split_axis], parallel_num
+            )[parallel_id]
+            blob_desc.body.shape.dim[split_axis] = physical_len
+            physical_blob_attr = op_arg_util.OpArgBlobAttribute(
+                logical_blob_attr.batch_axis,
+                blob_desc,
+                logical_blob_attr.logical_blob_name,
+            )
+            return physical_blob_attr
+
         if sbp_parallel.HasField("split_parallel"):
             split_axis = sbp_parallel.split_parallel.axis
-
-            def GetSplittedBlobAttr(
-                logical_blob_attr, split_axis, parallel_num, parallel_id
-            ):
-                blob_desc = blob_desc_pb.BlobDescProto()
-                blob_desc.CopyFrom(logical_blob_attr.blob_desc)
-                physical_len = balanced_splitter.BalancedPartNums(
-                    logical_blob_attr.shape[split_axis], parallel_num
-                )[parallel_id]
-                blob_desc.body.shape.dim[split_axis] = physical_len
-                physical_blob_attr = op_arg_util.OpArgBlobAttribute(
-                    logical_blob_attr.batch_axis,
-                    blob_desc,
-                    logical_blob_attr.logical_blob_name,
-                )
-                return physical_blob_attr
 
             return [
                 GetSplittedBlobAttr(logical_blob_attr, split_axis, parallel_num, i)
@@ -308,7 +309,7 @@ class InstructionsBuilder(object):
         phy_parallel_desc_symbols = self.GetPhysicalParallelDescSymbols(
             blob_object.parallel_desc_symbol
         )
-        phy_op_arg_blob_attrs = self.GetPhysicalOpArgBlobAttrs(blob_object)
+        phy_op_arg_blob_attrs = self._GetPhysicalOpArgBlobAttrs(blob_object)
 
         def GetPhysicalBlob(parallel_desc_sym, blob_attr):
             op_arg_parallel_attr = op_arg_util.MakeMirroredOpArgParallelAttribute(
