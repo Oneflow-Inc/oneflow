5554445b59df086962581c2ce7134f3d2a4af458
daquexian <daquexian566@gmail.com>
2020-10-23 13:43:58 +0800

    detect_cxx11_abi_availibility

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -103,3 +103,23 @@ function(use_mirror)
     endif()
   endif()
 endfunction()
+
+function(check_cxx11_abi OUTPUT_VAR)
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E echo "#include <string>\n void test(std::string){}\n int main(){}" OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/temp.cpp)
+  try_compile(_ ${CMAKE_CURRENT_BINARY_DIR} ${CMAKE_CURRENT_BINARY_DIR}/temp.cpp
+    COMPILE_DEFINITIONS -D_GLIBCXX_USE_CXX11_ABI=1
+    COPY_FILE ${CMAKE_CURRENT_BINARY_DIR}/temp)
+  execute_process(
+    COMMAND nm ${CMAKE_CURRENT_BINARY_DIR}/temp
+    COMMAND grep -q cxx11
+    RESULT_VARIABLE RET_CODE)
+  if (RET_CODE EQUAL 0)
+    set(CXX11_ABI_AVAILABLE ON)
+  else()
+    set(CXX11_ABI_AVAILABLE OFF)
+  endif()
+  execute_process(
+    COMMAND rm ${CMAKE_CURRENT_BINARY_DIR}/temp ${CMAKE_CURRENT_BINARY_DIR}/temp.cpp)
+  set(${OUTPUT_VAR} ${CXX11_ABI_AVAILABLE} PARENT_SCOPE)
+endfunction()
