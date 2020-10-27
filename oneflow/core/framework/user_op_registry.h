ed8c9466d0dda9ba7ff74afcd13bdacf3c81cda8
doombeaker <later@usopp.net>
2020-10-27 13:55:07 +0800

    demo(take user_op_registry.h as example)
    Former-commit-id: 1f2ca91872a262e4a950a242f2ddeab4bd4111ac

--- oneflow/core/framework/user_op_registry.h
+++ oneflow/core/framework/user_op_registry.h
@@ -23,6 +23,15 @@ limitations under the License.
 #include "oneflow/core/framework/user_op_conf.pb.h"
 #include "oneflow/core/operator/op_attribute.pb.h"
 
+/**
+ * @brief When we register user op by calling macro REGISTER_USER_OP, 
+ * we are actually calling methods of OpRegistry.
+ * This file contains:
+ *  1. prototype of callback function used by OpRegistry
+ *  2. the prototype of methods in class OpRegistry.
+ * 
+ */
+
 namespace oneflow {
 
 namespace user_op {
@@ -70,15 +79,68 @@ struct OpRegistryResult {
   InferOutputBlobTimeShapeFn infer_output_blob_time_shape_fn;
 };
 
+/**
+ * @brief When we use "REGISTER_USER_OP(op_type_name)" to register a op, we indeed get a OpRegistry
+ * object and use its methods to set up the op, including input, output, attr etc.
+ */
 class OpRegistry final {
  public:
   OpRegistry& Name(const std::string& op_type_name);
 
+  /**
+   * @brief Specify the name of the input, equals to Input(name, 1)
+   * 
+   * @param name The name of the input blob.
+   * @return OpRegistry& 
+   */
   OpRegistry& Input(const std::string& name);
+
+  /**
+   * @brief Specify the name and number of input blobs. eg: Input("myinput", 5) means 
+   * 5 blobs named "myinput" must be required at runtime.
+   * 
+   * @param name The name of input blob
+   * @param num The number of input
+   * @return OpRegistry& 
+   */
   OpRegistry& Input(const std::string& name, int32_t num);
+
+  /**
+   * @brief Specify the name and the minimum number of input blobs. eg: InputWithMinimum("input", 3) means
+   * at least 3 blobs named "input" should be required at runtime.
+   *
+   * @param name 
+   * @param min_num 
+   * @return OpRegistry& 
+   */
   OpRegistry& InputWithMinimum(const std::string& name, int32_t min_num);
+
+  /**
+   * @brief Specify the name of optional input blob, equals to OptionalInput(name, 1).
+   * 
+   * @param name 
+   * @return OpRegistry& 
+   */
   OpRegistry& OptionalInput(const std::string& name);
+
+  /**
+   * @brief Specify the name of optional input blob. The number of blobs at runtime should be 0 or num.
+   * eg: If OptionalInput("myinput", 3) set, the number of blobs named "myinput" should be 0 or 3.
+   * 
+   * @param name 
+   * @param num 
+   * @return OpRegistry& 
+   */
   OpRegistry& OptionalInput(const std::string& name, int32_t num);
+
+  /**
+   * @brief Specify the name of optional input with minimum number required. 
+   * eg: If OptionalInputWithMinimum("myinput", 3) set, the nunber of blobs named "myinput" could be 0, 1, 2, or 3.
+   * 
+   * @param name 
+   * @param min_num 
+   * @return OpRegistry& 
+   */
   OpRegistry& OptionalInputWithMinimum(const std::string& name, int32_t min_num);
 
   OpRegistry& Output(const std::string& name);
@@ -95,10 +157,38 @@ class OpRegistry final {
   template<typename T>
   OpRegistry& Attr(const std::string& name, UserOpAttrType type, T&& default_val);
 
+  /**
+   * @brief Set the TensorDescInferFn which is mainly used to infer the shape or data type of
+   * inputs/outputs blobs' of op
+   *
+   * @param fn TensorDescInferFn
+   * @return OpRegistry& 
+   */
   OpRegistry& SetTensorDescInferFn(TensorDescInferFn fn);
+
+  /**
+   * @brief Set the BatchAxisInferFn which is used to set batch axis for input/output blobs
+   * 
+   * @param fn BatchAxisInferFn
+   * @return OpRegistry& 
+   */
   OpRegistry& SetBatchAxisInferFn(BatchAxisInferFn fn);
   OpRegistry& SetGetSbpFn(GetSbpFn fn);
+
+  /**
+   * @brief Set the InferSbpSignatureFn which is mainly used to set SBP Signatures of op.
+   * 
+   * @param fn InferSbpSignatureFn
+   * @return OpRegistry& 
+   */
   OpRegistry& SetInferSbpSignatureFn(InferSbpSignatureFn fn);
+
+  /**
+   * @brief Set the InputArgModifyFn which is used to set modifier of input. The modifiers include 
+   *  "is_mutable", "use_header_only", "requires_grad" which defined in oneflow\core\operator\arg_modifier_signature.proto
+   * @param fn InputArgModifyFn 
+   * @return OpRegistry& 
+   */
   OpRegistry& SetInputArgModifyFn(InputArgModifyFn fn);
   OpRegistry& SetOutputArgModifyFn(OutputArgModifyFn fn);
   OpRegistry& SetInferOutputBlobTimeShapeFn(InferOutputBlobTimeShapeFn fn);
