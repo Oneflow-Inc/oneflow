b989f49639c75d873fc9f021e4f30d2e41b598b7
Zhenhua <huangzhenhua@zhejianglab.com>
2020-10-28 15:36:16 +0800

    using NdIndexOffsetHelper

--- oneflow/core/kernel/unsorted_segment_sum_kernel_util.cu
+++ oneflow/core/kernel/unsorted_segment_sum_kernel_util.cu
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#include "oneflow/core/common/nd_index_offset_helper.h"
 #include "oneflow/core/kernel/unsorted_segment_sum_kernel_util.h"
 #include "oneflow/core/kernel/kernel_util.cuh"
 #include "oneflow/core/kernel/kernel.h"
@@ -41,16 +42,23 @@ __device__ IDX GetOutOffset(const IDX data_offset, const K* segment_ids, const I
 }
 
 template<typename T, typename K, typename IDX, typename U>
-__global__ void UnsortedSegmentSumGpu(const IDX data_elem_cnt, const K* segment_ids,
-                                      const IDX num_segment_ids, const T* data,
-                                      const IDX num_segments, const IDX inner_dim_size, U* out,
-                                      const IDX segment_id_offset) {
+__global__ void UnsortedSegmentSumGpu(const IDX data_elem_cnt,
+                                      const NdIndexOffsetHelper<IDX, 3> in_helper,
+                                      const NdIndexOffsetHelper<IDX, 3> out_helper, const T* data,
+                                      const K* segment_ids, const IDX num_segments,
+                                      const IDX segment_id_offset, U* out) {
   CUDA_1D_KERNEL_LOOP_T(IDX, i, data_elem_cnt) {
     const T val = data[i];
     if (val != static_cast<T>(0)) {
-      const int64_t out_offset = GetOutOffset<K, IDX>(i, segment_ids, num_segment_ids, num_segments,
-                                                      inner_dim_size, segment_id_offset);
-      if (out_offset >= 0) { gpu_atomic_add(out + out_offset, static_cast<U>(val)); }
+      IDX outer_idx, segment_id_idx, inner_idx;
+      in_helper.OffsetToNdIndex(i, outer_idx, segment_id_idx, inner_idx);
+      const K origin_idx = segment_ids[segment_id_idx];
+      assert(origin_idx >= 0);
+      const IDX idx = origin_idx - segment_id_offset;
+      if (idx >= 0 && idx < num_segments) {
+        const int64_t out_offset = out_helper.NdIndexToOffset(outer_idx, idx, inner_idx);
+        if (out_offset >= 0) { gpu_atomic_add(out + out_offset, static_cast<U>(val)); }
+      }
     }
   }
 }
@@ -70,17 +78,23 @@ struct UnsortedSegmentSumKernelUtil<DeviceType::kGPU, T, K, U> final {
                                  int64_t num_segment_ids, int64_t num_segments,
                                  int64_t outer_dim_size, int64_t inner_dim_size,
                                  int64_t segment_id_offset, U* out) {
-    const int64_t data_elem_cnt = outer_dim_size * num_segment_ids * inner_dim_size;
-    if (IsSafeUseIndex32(num_segment_ids, num_segments, outer_dim_size, inner_dim_size)) {
+    const int64_t data_elem_cnt = num_segment_ids * outer_dim_size * inner_dim_size;
+    const int64_t out_elem_cnt = outer_dim_size * num_segments * inner_dim_size;
+
+    if (std::max(out_elem_cnt, data_elem_cnt) < GetMaxVal<int32_t>() / 2) {
+      NdIndexOffsetHelper<int32_t, 3> in_helper(outer_dim_size, num_segment_ids, inner_dim_size);
+      NdIndexOffsetHelper<int32_t, 3> out_helper(outer_dim_size, num_segments, inner_dim_size);
       UnsortedSegmentSumGpu<T, K, int32_t, U>
           <<<BlocksNum4ThreadsNum(data_elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
-              data_elem_cnt, segment_ids, num_segment_ids, data, num_segments, inner_dim_size, out,
-              segment_id_offset);
+              data_elem_cnt, in_helper, out_helper, data, segment_ids, num_segments,
+              segment_id_offset, out);
     } else {
+      NdIndexOffsetHelper<int64_t, 3> in_helper(outer_dim_size, num_segment_ids, inner_dim_size);
+      NdIndexOffsetHelper<int64_t, 3> out_helper(outer_dim_size, num_segments, inner_dim_size);
       UnsortedSegmentSumGpu<T, K, int64_t, U>
           <<<BlocksNum4ThreadsNum(data_elem_cnt), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
-              data_elem_cnt, segment_ids, num_segment_ids, data, num_segments, inner_dim_size, out,
-              segment_id_offset);
+              data_elem_cnt, in_helper, out_helper, data, segment_ids, num_segments,
+              segment_id_offset, out);
     }
   }
 };
