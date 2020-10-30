b138929b33b2be54c6ccfc7cf11b1d933853e8ad
Ziheng ZHAO <68211760+Ziheng-ZHAO@users.noreply.github.com>
2020-10-30 14:52:27 +0800

    Update get_variable.py

--- oneflow/python/ops/get_variable.py
+++ oneflow/python/ops/get_variable.py
@@ -51,13 +51,13 @@ def api_get_variable(
     r"""Create a variable or retrieve an existing one.
 
     Args:
-        name: Name of this variable. One variable could be shared by multiple OneFlow functions. `None` by defauilt
-        shape: Shape of the variable. `None` by defauilt
-        dtype: Data type of the variable. `None` by defauilt
-        initializer: A initializer object. For instance, a :func:`~oneflow.ones_initializer`. `None` by defauilt
-        trainable: A `bool` to indicate if this variable is trainable. `True` by defauilt
-        model_name: A `string`. `'weight'` or `'bias'`. `None` by defauilt
-        random_seed: Random seed for random initializers. `None` by defauilt
+        name: Name of this variable. One variable could be shared by multiple OneFlow functions. `None` by default
+        shape: Shape of the variable. `None` by default
+        dtype: Data type of the variable. `None` by default
+        initializer: A initializer object. For instance, a :func:`~oneflow.ones_initializer`. `None` by default
+        trainable: A `bool` to indicate if this variable is trainable. `True` by default
+        model_name: A `string`. `'weight'` or `'bias'`. `None` by default
+        random_seed: Random seed for random initializers. `None` by default
 
     For example: 
 
