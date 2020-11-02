7880a2e19d556be59f122ef2a4500517a38cdf12
lixinqi <lixinqi0703106@163.com>
2020-11-02 18:18:33 +0800

    rename UserOpAttrVal to AttrValue
    Former-commit-id: e85ee367b8fac56a5ecee4cad5044404017b27cd

--- oneflow/core/framework/user_op_conf.h
+++ oneflow/core/framework/user_op_conf.h
@@ -165,7 +165,7 @@ class UserOpConfWrapperBuilder final {
   std::string op_type_name_;
   HashMap<std::string, std::vector<std::string>> input_;
   HashMap<std::string, std::vector<std::string>> output_;
-  HashMap<std::string, UserOpAttrVal> attr_;
+  HashMap<std::string, AttrValue> attr_;
   OptInt64 scope_symbol_id_;
 };
 
@@ -190,8 +190,7 @@ class BackwardOpConfContext final {
 
 }  // namespace user_op
 
-Maybe<long long> GetUserOpAttrTypeImpl(const std::string& op_type_name,
-                                       const std::string& attr_name);
+Maybe<long long> GetAttrTypeImpl(const std::string& op_type_name, const std::string& attr_name);
 Maybe<OperatorConf> CheckAndCompleteUserOpConfImpl(const OperatorConf& op_conf);
 
 }  // namespace oneflow
