115f42a2a7d7773a4e0a57879195883cbb492816
wyg1997 <wyg19970408@gmail.com>
2020-11-03 09:54:37 +0800

    Revert "fix(OutputOp): fix bug in OutputOP when change S to B (#3722)"
    This reverts commit 9ba446007bf76f6e8f778f66858e94c9927490c6.

--- oneflow/core/operator/output_op.h
+++ oneflow/core/operator/output_op.h
@@ -29,8 +29,7 @@ class OutputOp final : public Operator {
   void InitFromOpConf() override;
   const PbMessage& GetCustomizedConf() const override;
   Maybe<void> InferBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-                             const ParallelContext* parallel_ctx,
-                             const SbpSignature* sbp_signature) const override;
+                             const ParallelContext* parallel_ctx) const override;
 
  private:
   Maybe<void> InferBatchAxis(
