b0d7a116955d3a76f9602bec5302e952e11f7500
lixinqi <lixinqi0703106@163.com>
2020-11-03 13:05:49 +0800

    AddSspVariableProxy
    Former-commit-id: 96bb91db322f69bad5edb628ae9cbaf169c0870e

--- oneflow/core/job/job_build_and_infer_ctx.cpp
+++ oneflow/core/job/job_build_and_infer_ctx.cpp
@@ -453,7 +453,7 @@ Maybe<OpAttribute> JobBuildAndInferCtx::AddAndInferMirroredOp(const OperatorConf
   CHECK_OR_RETURN(op_conf.has_scope_symbol_id());
   const auto& scope = Global<vm::SymbolStorage<Scope>>::Get()->Get(op_conf.scope_symbol_id());
   const auto* job_desc = JUST(scope.job_desc());
-  const auto& parallel_desc = *JUST(scope.GetParallelDesc(op_conf));
+  const auto& parallel_desc = JUST(scope.GetParallelDesc(op_conf));
   auto op = ConstructOp(op_conf, parallel_desc.device_type(), job_desc);
   JUST(CheckAllInputsConvertableToMirroredBlob(*op));
   int32_t parallel_num = parallel_desc.parallel_num();
@@ -504,7 +504,7 @@ Maybe<const LogicalBlobId*> JobBuildAndInferCtx::GetSubLbi(const LogicalBlobId& 
 Maybe<OpAttribute> JobBuildAndInferCtx::AddAndInferConsistentOp(const OperatorConf& op_conf) {
   CHECK_OR_RETURN(op_conf.has_scope_symbol_id());
   const auto& scope = Global<vm::SymbolStorage<Scope>>::Get()->Get(op_conf.scope_symbol_id());
-  const auto& parallel_desc = *JUST(scope.GetParallelDesc(op_conf));
+  const auto& parallel_desc = JUST(scope.GetParallelDesc(op_conf));
   const auto* job_desc = JUST(scope.job_desc());
   return AddAndInferOp(op_conf, parallel_desc.parallel_conf(), job_desc, false);
 }
--- oneflow/core/job_rewriter/add_ssp_variable_proxy.cpp
+++ oneflow/core/job_rewriter/add_ssp_variable_proxy.cpp
@@ -15,6 +15,9 @@ limitations under the License.
 */
 #include "oneflow/core/job_rewriter/job_pass.h"
 #include "oneflow/core/job/job.pb.h"
+#include "oneflow/core/job/scope.h"
+#include "oneflow/core/job_rewriter/autograd.h"
+#include "oneflow/core/vm/symbol_storage.h"
 #include "oneflow/core/framework/framework.h"
 
 namespace oneflow {
@@ -40,7 +43,77 @@ class AddSspVariableProxyPass final : public JobPass {
   }
 
   Maybe<void> Apply(const OpGraph& op_graph, JobBuilder* job_builder) const {
-    TODO();
+    JUST(ForEachTrainableVarOpNode(op_graph, [&](OpNode* op_node) -> Maybe<void> {
+      return ReplaceVarWithSspVarProxyOp(op_node, job_builder);
+    }));
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> ForEachTrainableVarOpNode(const OpGraph& op_graph,
+                                        const std::function<Maybe<void>(OpNode*)>& DoEach) const {
+    std::function<bool(OpNode*)> NeedBackwardOp;
+    JUST(MakePredicatorNeedBackwardOp(op_graph, &NeedBackwardOp));
+    const auto& IsSspVarProxy = [](const OperatorConf& op_conf) {
+      return op_conf.has_user_conf() && op_conf.user_conf().op_type_name() == "ssp_variable_proxy";
+    };
+    JUST(op_graph.MaybeForEachNode([&](OpNode* op_node) -> Maybe<void> {
+      const auto& op_conf = op_node->op().op_conf();
+      CHECK_OR_RETURN(!IsSspVarProxy(op_conf)) << "AddSspVariableProxy can not be applied twice";
+      if (op_conf.has_variable_conf() && NeedBackwardOp(op_node)) { return DoEach(op_node); }
+      return Maybe<void>::Ok();
+    }));
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> ReplaceVarWithSspVarProxyOp(OpNode* op_node, JobBuilder* job_builder) const {
+    const LogicalBlobId& old_var_out_lbi = op_node->op().BnInOp2Lbi("out");
+    std::string ref_lbn;
+    std::string value_lbn;
+    const Scope* scope = nullptr;
+    {
+      int64_t scope_symbol_id = op_node->op().op_conf().scope_symbol_id();
+      scope = &JUST(Global<vm::SymbolStorage<Scope>>::Get()->MaybeGet(scope_symbol_id));
+    }
+    JUST(AddSspVarProxyOp(old_var_out_lbi, *scope, job_builder, &ref_lbn, &value_lbn));
+    JUST(op_node->ForEachOutNode([&](OpNode* consumer) -> Maybe<void> {
+      const auto& op = consumer->op();
+      std::vector<OperatorConf> new_op_confs({op.op_conf()});
+      auto* new_op_conf = &new_op_confs.at(0);
+      for (const auto& ibn : op.input_bns()) {
+        if (op.BnInOp2Lbi(ibn) != old_var_out_lbi) { continue; }
+        // TODO(lixinqi): variable should be always consumed mutablely by optimizer
+        const auto* lbn = (op.InputBlobModifier4Ibn(ibn).is_mutable() ? &ref_lbn : &value_lbn);
+        ReplaceInputLbnInOpCustomizedConf(new_op_conf, ibn, *lbn);
+      }
+      job_builder->MutOpsOnlyOnce(new_op_confs);
+      return Maybe<void>::Ok();
+    }));
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> AddSspVarProxyOp(const LogicalBlobId& old_var_out_lbi, const Scope& scope,
+                               JobBuilder* job_builder, std::string* ref_lbn,
+                               std::string* value_lbn) const {
+    int64_t buffer_size = 0;
+    {
+      int64_t num_stages = scope.Int64("ssp_num_stages");
+      int64_t stage_id = scope.Int64("ssp_stage_id");
+      CHECK_GT(num_stages, 0);
+      CHECK_GE(stage_id, 0);
+      CHECK_LT(stage_id, num_stages);
+      buffer_size = num_stages - stage_id;
+    }
+    std::string op_name = old_var_out_lbi.op_name() + "_ssp_variable_proxy";
+    const auto proxy_op = user_op::UserOpConfWrapperBuilder(op_name)
+                              .Op("ssp_variable_proxy")
+                              .ScopeSymbolId(scope.scope_proto().symbol_id())
+                              .Input("var", GenLogicalBlobName(old_var_out_lbi))
+                              .Output("ref")
+                              .Output("value")
+                              .Attr<int64_t>("buffer_size", buffer_size)
+                              .Build();
+    const auto& parallel_desc = JUST(scope.GetParallelDesc(proxy_op.op_conf()));
+    job_builder->AddOps(parallel_desc.parallel_conf(), {proxy_op.op_conf()});
     return Maybe<void>::Ok();
   }
 };
