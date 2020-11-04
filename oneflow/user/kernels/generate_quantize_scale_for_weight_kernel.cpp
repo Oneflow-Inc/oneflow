8369e5b140a11cddea62e1bf51b621077fc740b9
Ldpe2G <liangdepeng@gmail.com>
2020-11-04 11:28:43 +0800

    add calculate weight scale and zero_point op & unit tests

---
+++ oneflow/user/kernels/generate_quantize_scale_for_weight_kernel.cpp
@@ -0,0 +1,107 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "oneflow/core/framework/framework.h"
+
+#include <algorithm>
+
+namespace oneflow {
+
+template<typename T>
+void gen_quant_scale_per_layer_symmetric(const int32_t quantize_to_bit, const int64_t num_elements,
+                                         const T *weight_ptr, T *weight_scale, T *zero_point) {
+  T weight_max = *std::max_element(weight_ptr, weight_ptr + num_elements);
+  T weight_min = *std::min_element(weight_ptr, weight_ptr + num_elements);
+
+  weight_max = std::max(std::abs(weight_max), std::abs(weight_min));
+
+  T denominator = T(pow(2.0, quantize_to_bit - 1)) - 1;
+
+  weight_scale[0] = weight_max / denominator;
+  zero_point[0] = 0;
+}
+
+template<typename T>
+void gen_quant_scale_per_layer_affine(const int32_t quantize_to_bit, const int64_t num_elements,
+                                      const T *weight_ptr, T *weight_scale, T *zero_point) {
+  T weight_max = *std::max_element(weight_ptr, weight_ptr + num_elements);
+  T weight_min = *std::min_element(weight_ptr, weight_ptr + num_elements);
+
+  T denominator = T(pow(2.0, quantize_to_bit)) - 1;
+
+  weight_scale[0] = (weight_max - weight_min) / denominator;
+  zero_point[0] = -weight_min / weight_scale[0];
+}
+
+template<typename T>
+class CpuGenerateQuantizeScaleForWeightKernel final : public user_op::OpKernel {
+ public:
+  CpuGenerateQuantizeScaleForWeightKernel() = default;
+  ~CpuGenerateQuantizeScaleForWeightKernel() = default;
+
+ private:
+  void Compute(user_op::KernelComputeContext *ctx) const override {
+    const user_op::Tensor *weight = ctx->Tensor4ArgNameAndIndex("weight", 0);
+    user_op::Tensor *weight_scale = ctx->Tensor4ArgNameAndIndex("scale", 0);
+    user_op::Tensor *zero_point = ctx->Tensor4ArgNameAndIndex("zero_point", 0);
+
+    const std::string quantizer_type = ctx->Attr<std::string>("quantizer_type");
+    const int32_t quantize_to_bit = ctx->Attr<int32_t>("quantize_to_bit");
+    const bool per_layer_quantization = ctx->Attr<bool>("per_layer_quantization");
+
+    const T *weight_ptr = weight->dptr<T>();
+    T *weight_scale_ptr = weight_scale->mut_dptr<T>();
+    T *zero_point_ptr = zero_point->mut_dptr<T>();
+
+    // NOTE(Liang Depeng): default is per layer quantization
+    int64_t outer_num = 1;
+    int64_t inner_num = weight->shape().elem_cnt();
+    if (!per_layer_quantization) {  // per-channel quantization
+      outer_num = weight->shape().At(0);
+      inner_num = weight->shape().Count(1);
+    }
+
+    if (quantizer_type == "symmetric") {
+      FOR_RANGE(int64_t, c, 0, outer_num) {
+        gen_quant_scale_per_layer_symmetric(quantize_to_bit, inner_num, weight_ptr,
+                                            weight_scale_ptr, zero_point_ptr);
+        weight_ptr += inner_num;
+        weight_scale_ptr += 1;
+        zero_point_ptr += 1;
+      }
+    } else {  // quantizer_type == "affine"
+      FOR_RANGE(int64_t, c, 0, outer_num) {
+        gen_quant_scale_per_layer_affine(quantize_to_bit, inner_num, weight_ptr, weight_scale_ptr,
+                                         zero_point_ptr);
+        weight_ptr += inner_num;
+        weight_scale_ptr += 1;
+        zero_point_ptr += 1;
+      }
+    }
+  };
+
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_GENERATE_QUANTIZE_SCALE_FOR_WEIGHT_KERNEL(dtype)    \
+  REGISTER_USER_KERNEL("generate_quantize_scale_for_weight")         \
+      .SetCreateFn<CpuGenerateQuantizeScaleForWeightKernel<dtype>>() \
+      .SetIsMatchedHob((user_op::HobDeviceTag() == DeviceType::kCPU) \
+                       & (user_op::HobDataType("weight", 0) == GetDataType<dtype>::value))
+
+REGISTER_GENERATE_QUANTIZE_SCALE_FOR_WEIGHT_KERNEL(float);
+REGISTER_GENERATE_QUANTIZE_SCALE_FOR_WEIGHT_KERNEL(double);
+
+}  // namespace oneflow
---
+++ oneflow/user/kernels/generate_quantize_scale_for_weight_kernel.cu
@@ -0,0 +1,217 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "oneflow/core/device/cuda_util.h"
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/core/kernel/kernel_util.cuh"
+
+#include <float.h>
+
+namespace oneflow {
+
+namespace {
+
+// NOTE(Liang Depeng): refer to
+// https://stackoverflow.com/questions/17371275/implementing-max-reduce-in-cuda
+template<typename T>
+__global__ void ReduceMaxMinPerLayer(const T *input_ptr, const int64_t elements, T *max_ptr,
+                                     T *min_ptr) {
+  extern __shared__ unsigned char shared_max_min_memory[];
+  T *shared_max = reinterpret_cast<T *>(shared_max_min_memory);
+  T *shared_min = shared_max + blockDim.x;
+
+  int64_t tid = threadIdx.x;
+  int64_t gid = (blockDim.x * blockIdx.x) + tid;
+  shared_max[tid] = -FLT_MAX;
+  shared_min[tid] = -FLT_MAX;
+
+  while (gid < elements) {
+    shared_max[tid] = max(shared_max[tid], input_ptr[gid]);
+    shared_min[tid] = max(shared_min[tid], -input_ptr[gid]);
+    gid += gridDim.x * blockDim.x;
+  }
+  __syncthreads();
+  gid = (blockDim.x * blockIdx.x) + tid;
+  for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+    if (tid < s && gid < elements) {
+      shared_max[tid] = max(shared_max[tid], shared_max[tid + s]);
+      shared_min[tid] = max(shared_min[tid], shared_min[tid + s]);
+    }
+    __syncthreads();
+  }
+
+  if (tid == 0) {
+    gpu_atomic_max(max_ptr, shared_max[0]);
+    gpu_atomic_max(min_ptr, shared_min[0]);
+  }
+}
+
+template<typename T>
+__global__ void ReduceMaxMinPerChannel(const T *input_ptr, const int64_t elements,
+                                       const int64_t num_channels, const int64_t panel_size,
+                                       T *max_ptr, T *min_ptr) {
+  extern __shared__ unsigned char shared_max_min_memory[];
+  T *shared_max = reinterpret_cast<T *>(shared_max_min_memory);
+  T *shared_min = shared_max + blockDim.x;
+
+  int64_t cur_channel = blockIdx.x;
+  int64_t tid = threadIdx.x;
+
+  while (cur_channel < num_channels) {
+    shared_max[tid] = -FLT_MAX;
+    shared_min[tid] = -FLT_MAX;
+
+    int64_t index = (panel_size * cur_channel) + tid;
+    int64_t end = panel_size * (cur_channel + 1);
+
+    while (index < end && index < elements) {
+      shared_max[tid] = max(shared_max[tid], input_ptr[index]);
+      shared_min[tid] = max(shared_min[tid], -input_ptr[index]);
+      index += blockDim.x;
+    }
+    __syncthreads();
+
+    for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
+      if (tid < s) {
+        shared_max[tid] = max(shared_max[tid], shared_max[tid + s]);
+        shared_min[tid] = max(shared_min[tid], shared_min[tid + s]);
+      }
+      __syncthreads();
+    }
+
+    if (tid == 0) {
+      gpu_atomic_max(&max_ptr[cur_channel], shared_max[0]);
+      gpu_atomic_max(&min_ptr[cur_channel], shared_min[0]);
+    }
+
+    __syncthreads();
+    cur_channel += gridDim.x;
+  }
+}
+
+template<typename T>
+__global__ void InitMaxMin(const int64_t elements, T *max_ptr, T *min_ptr) {
+  int64_t tid = threadIdx.x;
+  int64_t gid = (blockDim.x * blockIdx.x) + tid;
+
+  if (gid < elements) {
+    max_ptr[gid] = -FLT_MAX;
+    min_ptr[gid] = -FLT_MAX;
+  }
+}
+
+template<typename T>
+__global__ void CalScaleZeroPointSymmetric(const T *max_ptr, const T *min_ptr,
+                                           const int64_t elements, const double quantize_to_bit,
+                                           T *scale, T *zero_point) {
+  int64_t tid = threadIdx.x;
+  int64_t gid = (blockDim.x * blockIdx.x) + tid;
+
+  if (gid < elements) {
+    T weight_max = max(fabs(max_ptr[gid]), fabs(min_ptr[gid]));
+    T denominator = T(pow(2.0, quantize_to_bit - 1)) - 1;
+    scale[gid] = weight_max / denominator;
+    zero_point[gid] = 0;
+  }
+}
+
+template<typename T>
+__global__ void CalScaleZeroPointAffine(const T *max_ptr, const T *min_ptr, const int64_t elements,
+                                        const double quantize_to_bit, T *scale, T *zero_point) {
+  int64_t tid = threadIdx.x;
+  int64_t gid = (blockDim.x * blockIdx.x) + tid;
+
+  if (gid < elements) {
+    T denominator = T(pow(2.0, quantize_to_bit)) - 1;
+    T min = -min_ptr[gid];
+    T s = (max_ptr[gid] - min) / denominator;
+    scale[gid] = s;
+    zero_point[gid] = -min / s;
+  }
+}
+
+}  // namespace
+
+#define LAUNCH_CUDA_KERNEL(func, device_ctx_ptr, thread_num, shared_mem_size, ...)     \
+  func<<<SMBlocksNum4ThreadsNum(thread_num), kCudaThreadsNumPerBlock, shared_mem_size, \
+         (device_ctx_ptr)->cuda_stream()>>>(__VA_ARGS__)
+
+template<typename T>
+class GpuGenerateQuantizeScaleForWeightKernel final : public user_op::OpKernel {
+ public:
+  GpuGenerateQuantizeScaleForWeightKernel() = default;
+  ~GpuGenerateQuantizeScaleForWeightKernel() = default;
+
+ private:
+  void Compute(user_op::KernelComputeContext *ctx) const override {
+    const user_op::Tensor *weight = ctx->Tensor4ArgNameAndIndex("weight", 0);
+    user_op::Tensor *weight_scale = ctx->Tensor4ArgNameAndIndex("scale", 0);
+    user_op::Tensor *zero_point = ctx->Tensor4ArgNameAndIndex("zero_point", 0);
+    user_op::Tensor *tmp_buffer = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
+
+    const std::string quantizer_type = ctx->Attr<std::string>("quantizer_type");
+    const int32_t quantize_to_bit = ctx->Attr<int32_t>("quantize_to_bit");
+    const bool per_layer_quantization = ctx->Attr<bool>("per_layer_quantization");
+
+    int64_t elements = weight->shape().elem_cnt();
+    int64_t channel = weight_scale->shape().At(0);
+    int64_t panel_size = elements / channel;
+    T *max_ptr = tmp_buffer->mut_dptr<T>();
+    T *min_ptr = max_ptr + channel;
+
+    LAUNCH_CUDA_KERNEL((InitMaxMin<T>), ctx->device_ctx(), channel, 0, channel, max_ptr, min_ptr);
+
+    if (per_layer_quantization) {
+      LAUNCH_CUDA_KERNEL((ReduceMaxMinPerLayer<T>), ctx->device_ctx(), elements,
+                         kCudaThreadsNumPerBlock * 2 * sizeof(T), weight->dptr<T>(), elements,
+                         max_ptr, min_ptr);
+    } else {  // per-channel quantization
+      LAUNCH_CUDA_KERNEL((ReduceMaxMinPerChannel<T>), ctx->device_ctx(), channel,
+                         kCudaThreadsNumPerBlock * 2 * sizeof(T), weight->dptr<T>(), elements,
+                         channel, panel_size, max_ptr, min_ptr);
+    }
+
+    if (quantizer_type == "symmetric") {
+      LAUNCH_CUDA_KERNEL((CalScaleZeroPointSymmetric<T>), ctx->device_ctx(), channel, 0, max_ptr,
+                         min_ptr, channel, static_cast<double>(quantize_to_bit),
+                         weight_scale->mut_dptr<T>(), zero_point->mut_dptr<T>());
+    } else {  // quantizer_type == "affine"
+      LAUNCH_CUDA_KERNEL((CalScaleZeroPointAffine<T>), ctx->device_ctx(), channel, 0, max_ptr,
+                         min_ptr, channel, static_cast<double>(quantize_to_bit),
+                         weight_scale->mut_dptr<T>(), zero_point->mut_dptr<T>());
+    }
+  };
+
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_GENERATE_QUANTIZE_SCALE_FOR_WEIGHT_KERNEL(dtype)                          \
+  REGISTER_USER_KERNEL("generate_quantize_scale_for_weight")                               \
+      .SetCreateFn<GpuGenerateQuantizeScaleForWeightKernel<dtype>>()                       \
+      .SetIsMatchedHob((user_op::HobDeviceTag() == DeviceType::kGPU)                       \
+                       & (user_op::HobDataType("weight", 0) == GetDataType<dtype>::value)) \
+      .SetInferTmpSizeFn([](user_op::InferContext *ctx) -> size_t {                        \
+        size_t tmp_buffer_size = 1;                                                        \
+        Shape *weight_shape = ctx->Shape4ArgNameAndIndex("weight", 0);                     \
+        if (ctx->Attr<bool>("per_layer_quantization") == false) {                          \
+          tmp_buffer_size = weight_shape->At(0);                                           \
+        }                                                                                  \
+        return 2 * tmp_buffer_size * sizeof(dtype);                                        \
+      })
+
+REGISTER_GENERATE_QUANTIZE_SCALE_FOR_WEIGHT_KERNEL(float);
+REGISTER_GENERATE_QUANTIZE_SCALE_FOR_WEIGHT_KERNEL(double);
+
+}  // namespace oneflow
