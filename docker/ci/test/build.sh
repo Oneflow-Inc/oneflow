9bab0af58cda270948f901919e7a9fe25aeabbda
Tsai <caishenghang@oneflow.org>
2020-11-06 12:43:17 +0800

    quick fix

--- docker/ci/test/build.sh
+++ docker/ci/test/build.sh
@@ -1,6 +1,6 @@
 set -ex
 test_img_dir="$(dirname "${BASH_SOURCE[0]}")"
 test_img_dir="$(realpath "${test_img_dir}")"
-echo $test_img_dir
+cd $test_img_dir
 docker build --rm \
     -t oneflow-test:$USER .
