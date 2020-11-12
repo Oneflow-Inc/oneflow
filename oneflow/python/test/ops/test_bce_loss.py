d11a07d5482522417a267f22a54a98e18473d7cb
MARD1NO <359521840@qq.com>
2020-11-12 12:29:53 +0800

    add bce loss diff evaluate

---
+++ oneflow/python/test/ops/test_bce_loss.py
@@ -0,0 +1,147 @@
+import oneflow as flow
+import numpy as np
+import oneflow.typing as tp
+from test_util import GenArgList
+import unittest
+from collections import OrderedDict
+from typing import List
+import os 
+
+
+def _compare_bceloss_with_np(
+    input_shape, target_shape, weight_shape, device_type, machine_ids, device_counts
+):
+    input = np.random.random(size=input_shape).astype(np.float32)
+    target = np.random.random(size=target_shape).astype(np.float32)
+    weight = np.random.random(size=weight_shape).astype(np.float32)
+
+    assert device_type in ["cpu", "gpu"]
+
+    flow.clear_default_session()
+    flow.env.init()
+    if device_type == "cpu":
+        flow.config.cpu_device_num(device_counts)
+    else:
+        flow.config.gpu_device_num(device_counts)
+
+    func_config = flow.FunctionConfig()
+
+    def _np_sigmoid_fn(x): 
+        # Compute sigmoid function
+        return 1 / (1+np.exp(-x))
+
+    def np_bceloss(np_input, np_target, np_weight):
+        np_sigmoid_input = _np_sigmoid_fn(np_input)
+        np_bce = -np_weight*((np_target*np.log(np_sigmoid_input) + (1-np_target)*(np.log(1-np_sigmoid_input))))
+        
+        np_bce_mean = np.mean(np_bce)
+        np_bce_sum = np.sum(np_bce)
+
+        return [np_bce, np_bce_mean, np_bce_sum]
+
+    def np_bce_loss_diff(np_input, np_target, np_weight): 
+        # Use numpy to compute diff
+        elemcnt = np_target.size
+        elemcnt = np.reshape(elemcnt, -1)
+        diff = -(np_weight/elemcnt)*(np_target - (np.exp(np_input) / (1+np.exp(np_input))))
+        return diff
+
+    np_out_bceloss = np_bceloss(input, target, weight)
+
+    # Compute diff
+    np_diff = np_bce_loss_diff(input, target, weight)
+
+    def assert_prediction_grad(blob: tp.Numpy):
+        # Evaluate the gradient
+        assert np.allclose(blob, np_diff)
+
+    @flow.global_function(
+        type="train", function_config=func_config,
+    )
+    def oneflow_bceloss(
+        of_input: tp.Numpy.Placeholder(shape=input.shape),
+        of_target: tp.Numpy.Placeholder(shape=target.shape),
+        of_weight: tp.Numpy.Placeholder(shape=weight.shape)
+    ) -> List[tp.Numpy]:
+        v = flow.get_variable(
+            shape=target.shape,
+            dtype=flow.float32,
+            initializer=flow.constant_initializer(1),
+            name="v",
+        )
+
+        x_var = of_input + v
+        
+        flow.watch_diff(x_var, assert_prediction_grad)
+
+        with flow.scope.placement(device_type, machine_ids):
+            bceloss = flow.nn.BCELoss(
+                x_var, of_target, of_weight, reduction="none", name="of_mseloss"
+            )
+            bceloss_mean = flow.nn.BCELoss(
+                x_var, of_target, of_weight, reduction="mean", name="of_mseloss_reduce_mean"
+            )
+            bceloss_sum = flow.nn.BCELoss(
+                x_var, of_target, of_weight, reduction="sum", name="of_mseloss_reduce_sum"
+            )
+            # Because our gradient is use "mean" mode to compute
+            out = bceloss_mean
+
+            flow.optimizer.SGD(
+                flow.optimizer.PiecewiseConstantScheduler([], [1e-3]), momentum=0
+            ).minimize(out)
+
+        return [bceloss, bceloss_mean, bceloss_sum]
+
+    of_out_bceloss = oneflow_bceloss(input, target, weight)
+
+    assert np.allclose(of_out_bceloss[0], np_out_bceloss[0])
+
+    for i in range(1, len(np_out_bceloss)):
+        assert np.allclose(of_out_bceloss[i][0], np_out_bceloss[i])
+
+
+@flow.unittest.skip_unless_1n1d()
+class Testl1loss1n1d(flow.unittest.TestCase):
+    def test_bceloss_cpu(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["input"] = [(3, 16)]
+        arg_dict["target"] = [(3, 16)]
+        arg_dict["weight"] = [(3, 16)]
+        arg_dict["device_type"] = ["cpu"]
+        arg_dict["machine_ids"] = ["0:0"]
+        arg_dict["device_counts"] = [1]
+        for arg in GenArgList(arg_dict):
+            _compare_bceloss_with_np(*arg)
+
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
+    def test_bceloss_gpu(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["input"] = [(3, 16, 32)]
+        arg_dict["target"] = [(3, 16, 32)]
+        arg_dict["weight"] = [(3, 16, 32)]
+        arg_dict["device_type"] = ["gpu"]
+        arg_dict["machine_ids"] = ["0:0"]
+        arg_dict["device_counts"] = [1]
+        for arg in GenArgList(arg_dict):
+            _compare_bceloss_with_np(*arg)
+
+
+@flow.unittest.skip_unless_1n2d()
+class Testrange1n2d(flow.unittest.TestCase):
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
+    def test_bceloss_gpu_1n2d(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["input"] = [(3, 16, 16)]
+        arg_dict["target"] = [(3, 16, 16)]
+        arg_dict["weight"] = [(3, 16, 16)]
+        arg_dict["device_type"] = ["gpu"]
+        arg_dict["machine_ids"] = ["0:0-1"]
+        arg_dict["device_counts"] = [2]
+        for arg in GenArgList(arg_dict):
+            _compare_bceloss_with_np(*arg)
+
+
+if __name__ == "__main__":
+    unittest.main()
+    
