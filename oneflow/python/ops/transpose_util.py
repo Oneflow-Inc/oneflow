f1d4ab112c2630637c1a450a4fd343c5ce47ea9c
scxfjiang <sc.xfjiang@gmail.com>
2020-11-14 19:33:57 +0800

    final

--- oneflow/python/ops/transpose_util.py
+++ oneflow/python/ops/transpose_util.py
@@ -29,8 +29,7 @@ def get_perm_when_transpose_axis_to_last_dim(
     axis: int,
 ) -> tuple:
     axis = axis if axis >= 0 else axis + num_axes
-    if axis == num_axes - 1:
-        return tuple(range(num_axes))
+    assert(axis < num_axes)
     perm = [dim if dim < axis else dim + 1 for dim in range(num_axes - 1)]
     perm.append(axis)
     return tuple(perm)
