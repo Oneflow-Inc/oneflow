90603272ced2d4f69e2636e1ac3aba8c139cf556
Zhenhua <huangzhenhua@zhejianglab.com>
2020-11-19 23:16:09 +0800

    Add polyval bw test

--- oneflow/python/test/ops/test_polyval.py
+++ oneflow/python/test/ops/test_polyval.py
@@ -15,14 +15,16 @@ limitations under the License.
 """
 import unittest
 import numpy as np
+import tensorflow as tf
 import oneflow as flow
+import oneflow.typing as tp
 from collections import OrderedDict
 
 import test_global_storage
-from test_util import GenArgList, type_name_to_flow_type
+from test_util import GenArgList, type_name_to_flow_type, type_name_to_np_type
 
 
-def compare_with_tensorflow(device_type, device_num, in_shape, data_type, degree):
+def compare_with_tensorflow(device_type, device_num, in_shape, data_type, coeffs):
     assert device_type in ["cpu", "gpu"]
     assert data_type in ["float32", "double"]
     flow_data_type = type_name_to_flow_type[data_type]
@@ -31,41 +33,40 @@ def compare_with_tensorflow(device_type, device_num, in_shape, data_type, degree
     func_config = flow.FunctionConfig()
     func_config.default_data_type(flow_data_type)
 
-    @flow.global_function(function_config=func_config)
-    def PolyValJob():
+    @flow.global_function(type="train", function_config=func_config)
+    def PolyValJob(x: tp.Numpy.Placeholder(shape=in_shape)):
         with flow.scope.placement(device_type, "0:0-{}".format(device_num - 1)):
-            x = flow.get_variable(
-                "x",
+            x += flow.get_variable(
+                name="x",
                 shape=in_shape,
                 dtype=flow_data_type,
-                initializer=flow.random_uniform_initializer(minval=-10, maxval=10),
+                initializer=flow.zeros_initializer(),
                 trainable=True,
             )
-            flow.watch(x, test_global_storage.Setter("x"))
-            coeffs = []
-            for i in range(degree):
-                tmp = flow.get_variable(
-                    name="coeff_%d" % i,
-                    shape=in_shape,
-                    dtype=flow_data_type,
-                    initializer=flow.random_uniform_initializer(minval=-10, maxval=10),
-                )
-                coeffs.append(tmp)
-                flow.watch(tmp, test_global_storage.Setter("coeff_%d" % i))
-            return flow.math.polyval(coeffs, x)
+            out = flow.math.polyval(coeffs, x)
+            flow.optimizer.SGD(
+                flow.optimizer.PiecewiseConstantScheduler([], [1e-4]), momentum=0
+            ).minimize(out)
+
+            flow.watch(out, test_global_storage.Setter("out"))
+            flow.watch_diff(x, test_global_storage.Setter("x_diff"))
+
+            return out
 
     # OneFlow
     check_point = flow.train.CheckPoint()
     check_point.init()
-    of_out = PolyValJob().get().numpy()
-    # numpy
-    x = test_global_storage.Get("x")
-    coeffs = []
-    for i in range(degree):
-        coeffs.append(test_global_storage.Get("coeff_%d" % i))
-    np_out = np.polyval(coeffs, x)
+    x = (np.random.random(in_shape) * 100).astype(type_name_to_np_type[data_type])
+    of_out = PolyValJob(x).get().numpy()
+    of_x_diff = test_global_storage.Get("x_diff")
+    # TensorFlow
+    with tf.GradientTape(persistent=True) as tape:
+        x = tf.Variable(x)
+        tf_out = tf.math.polyval(coeffs, x)
+    tf_x_diff = tape.gradient(tf_out, x)
 
-    assert np.allclose(of_out, np_out, rtol=1e-5, atol=1e-5)
+    assert np.allclose(of_out, tf_out.numpy(), rtol=1e-5, atol=1e-5)
+    assert np.allclose(of_x_diff, tf_x_diff.numpy(), rtol=1e-5, atol=1e-5)
 
 
 def gen_arg_list(type):
@@ -77,8 +78,8 @@ def gen_arg_list(type):
         arg_dict["device_type"] = ["cpu", "gpu"]
         arg_dict["device_num"] = [1]
     arg_dict["in_shape"] = [(1,), (2, 1), (1, 2), (2, 2)]
-    arg_dict["data_type"] = ["float32", "double"]
-    arg_dict["degree"] = [0, 1, 3, 5]
+    arg_dict["data_type"] = ["float32"]
+    arg_dict["coeffs"] = [[1.0, 2.0], [1.0, 2.0, 3.0]]
     return GenArgList(arg_dict)
 
 
