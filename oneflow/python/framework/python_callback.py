d0a38f625ff2894e093dc90971151c9095453371
clackhan <han_binbin@163.com>
2020-11-22 19:47:01 +0800

    import modules in python_callback.py

--- oneflow/python/framework/python_callback.py
+++ oneflow/python/framework/python_callback.py
@@ -18,6 +18,10 @@ from __future__ import absolute_import
 import traceback
 
 import oneflow.python.framework.ofblob as ofblob
+import oneflow_api.oneflow.core.operator.op_attribute as op_attribute_cfg
+import oneflow_api.oneflow.core.job.placement as placement_cfg
+import oneflow_api.oneflow.core.job.job_conf as job_conf_cfg
+import oneflow_api.oneflow.core.job.scope as scope_cfg
 import oneflow_api
 
 
