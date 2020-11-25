bd39388cba4e17bce2d2dc4b36426faa0a2e41b1
ShawnXuan <xiexuanx2@gmail.com>
2020-11-25 16:12:28 +0800

    rm usless lines

--- oneflow/python/test/ops/test_reshapeV3.py
+++ oneflow/python/test/ops/test_reshapeV3.py
@@ -15,23 +15,16 @@ limitations under the License.
 """
 import unittest
 import os
-import shutil
 from collections import OrderedDict
 
-import time
-import numpy as np
 import oneflow as flow
-import tensorflow as tf
-import test_global_storage
 from test_util import GenArgList
 
 
-def compare_with_tensorflow(device_type, device_num, input_shape, shape):
+def distribute_reshape_test(device_type, device_num, input_shape, shape):
     assert device_type in ["gpu", "cpu"]
     flow.clear_default_session()
     flow.config.gpu_device_num(device_num)
-    case = device_type + '_' + str(device_num)# + '_' + '-'.join([str(e) for e in input_shape]) \
-           #+ '_' + '-'.join([str(e) for e in shape])
     func_config = flow.FunctionConfig()
     func_config.default_data_type(flow.float)
 
@@ -59,26 +52,6 @@ def compare_with_tensorflow(device_type, device_num, input_shape, shape):
     check_point.init()
     x, loss = ReshapeJob().get()
 
-    path = os.path.join('log/snapshot', case)
-    if os.path.exists(path):
-        shutil.rmtree(path)
-
-    print('save checkpoint to ', path)
-    check_point.save(path)
-    #print(x.numpy(), loss.numpy())
-    time.sleep(5)
-    # TensorFlow
-    #with tf.GradientTape(persistent=True) as tape:
-    #    x = tf.Variable(test_global_storage.Get("x"))
-    #    tf_out = tf.reshape(x, shape)
-    #loss_diff = test_global_storage.Get("loss_diff")
-    #tf_x_diff = tape.gradient(tf_out, x, loss_diff)
-
-    #assert np.allclose(of_out.numpy(), tf_out.numpy(), rtol=1e-5, atol=1e-5)
-    #assert np.allclose(
-    #    test_global_storage.Get("x_diff"), tf_x_diff.numpy(), rtol=1e-5, atol=1e-5
-    #)
-
 
 class TestReshapeV2(flow.unittest.TestCase):
     def test_reshape(test_case):
@@ -88,7 +61,7 @@ class TestReshapeV2(flow.unittest.TestCase):
         arg_dict["input_shape"] = [(5, 8, 16)]
         arg_dict["shape"] = [[-1, 16]]
         for arg in GenArgList(arg_dict):
-            compare_with_tensorflow(*arg)
+            distribute_reshape_test(*arg)
 
 
 if __name__ == "__main__":
