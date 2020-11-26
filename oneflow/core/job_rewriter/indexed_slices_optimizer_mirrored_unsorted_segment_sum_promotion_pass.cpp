a0a8c1449ae5d2176785e22b6e288ef27f0511f1
liujuncheng <liujuncheng1022@gmail.com>
2020-11-26 17:33:21 +0800

    pass
    Former-commit-id: ed5d72e7b3c678406b40108c0840709f88ca770c

--- oneflow/core/job_rewriter/indexed_slices_optimizer_mirrored_unsorted_segment_sum_promotion_pass.cpp
+++ oneflow/core/job_rewriter/indexed_slices_optimizer_mirrored_unsorted_segment_sum_promotion_pass.cpp
@@ -29,7 +29,10 @@ class IndexedSlicesOptimizerMirroredUnsortedSegmentSumPromotionPass final : publ
 Maybe<void> IndexedSlicesOptimizerMirroredUnsortedSegmentSumPromotionPass::Apply(
     Job* job, JobPassCtx* ctx) const {
   if (!(ctx->job_desc().job_conf().has_indexed_slices_optimizer_conf()
-        && ctx->job_desc().job_conf().indexed_slices_optimizer_conf().enable())) {}
+        && ctx->job_desc().job_conf().indexed_slices_optimizer_conf().enable())) {
+    return Maybe<void>::Ok();
+  }
+  HashMap<std::string, OperatorConf> op_name2op_conf;
   const OpGraph op_graph(*job);
   JobBuilder job_builder(job);
   const PbRpf<std::string>& include_op_names =
@@ -80,11 +83,16 @@ Maybe<void> IndexedSlicesOptimizerMirroredUnsortedSegmentSumPromotionPass::Apply
     }
     const ParallelDesc& consistent_parallel_desc = src_node->parallel_desc();
     const BlobDesc& variable_blob_desc = variable_node->LogicalBlobDesc4Lbi(model_lbi);
+    if (variable_blob_desc.shape().At(0) % consistent_parallel_desc.parallel_num() != 0) { return; }
+    const int64_t num_local_classes =
+        variable_blob_desc.shape().At(0) / consistent_parallel_desc.parallel_num();
     std::vector<std::string> segment_ids_lbn_vec;
     std::vector<std::string> data_lbn_vec;
     std::vector<const OpNode*> unsorted_segment_sum_like_nodes;
     std::unique_ptr<BlobDesc> segment_ids_blob_desc;
     std::unique_ptr<BlobDesc> data_blob_desc;
+    const std::string op_name_prefix =
+        "System-Optimizer-IndexedSlices-MirroredUnsortedSegmentSumPromotion-";
     for (int64_t i = 0; i < distribute_concat_conf.in_size(); ++i) {
       const LogicalBlobId in_lbn = GenLogicalBlobId(distribute_concat_conf.in(i));
       const OpNode* in_node = op_graph.OpNode4OpName(in_lbn.op_name());
@@ -105,10 +113,11 @@ Maybe<void> IndexedSlicesOptimizerMirroredUnsortedSegmentSumPromotionPass::Apply
                  != CHECK_JUST(consistent_parallel_desc.DeviceId4ParallelId(i))) {
         return;
       }
+      const std::string segment_ids_lbn = in_user_op_conf.input("segment_ids", 0);
       const BlobDesc& in_segment_ids_blob_desc =
-          in_node->LogicalBlobDesc4Lbi(GenLogicalBlobId(in_user_op_conf.input("segment_ids", 0)));
-      const BlobDesc& in_data_blob_desc =
-          in_node->LogicalBlobDesc4Lbi(GenLogicalBlobId(in_user_op_conf.input("data", 0)));
+          in_node->LogicalBlobDesc4Lbi(GenLogicalBlobId(segment_ids_lbn));
+      const std::string data_lbn = in_user_op_conf.input("data", 0);
+      const BlobDesc& in_data_blob_desc = in_node->LogicalBlobDesc4Lbi(GenLogicalBlobId(data_lbn));
       if (!segment_ids_blob_desc) {
         CHECK(!data_blob_desc);
         segment_ids_blob_desc.reset(new BlobDesc(in_segment_ids_blob_desc));
@@ -118,10 +127,73 @@ Maybe<void> IndexedSlicesOptimizerMirroredUnsortedSegmentSumPromotionPass::Apply
         if (!(in_segment_ids_blob_desc == *segment_ids_blob_desc)) { return; }
         if (!(in_data_blob_desc == *data_blob_desc)) { return; }
       }
-      segment_ids_lbn_vec.push_back(in_user_op_conf.input("segment_ids", 0));
-      data_lbn_vec.push_back(in_user_op_conf.input("data", 0));
+      segment_ids_lbn_vec.push_back(segment_ids_lbn);
+      data_lbn_vec.push_back(data_lbn);
       unsorted_segment_sum_like_nodes.push_back(in_node);
     }
+    OperatorConf segment_ids_disable_boxing_op_conf;
+    segment_ids_disable_boxing_op_conf.set_name(op_name_prefix + "DisableBoxing-" + NewUniqueId());
+    const int64_t scope_symbol_id = src_node->op().op_conf().scope_symbol_id();
+    segment_ids_disable_boxing_op_conf.set_scope_symbol_id(scope_symbol_id);
+    DistributeCastToDisableBoxingOpConf* segment_ids_disable_boxing_conf =
+        segment_ids_disable_boxing_op_conf.mutable_distribute_cast_to_disable_boxing_conf();
+    OperatorConf data_disable_boxing_op_conf;
+    data_disable_boxing_op_conf.set_name(op_name_prefix + "DisableBoxing-" + NewUniqueId());
+    data_disable_boxing_op_conf.set_scope_symbol_id(scope_symbol_id);
+    DistributeCastToDisableBoxingOpConf* data_disable_boxing_conf =
+        data_disable_boxing_op_conf.mutable_distribute_cast_to_disable_boxing_conf();
+    for (int64_t i = 0; i < consistent_parallel_desc.parallel_num(); ++i) {
+      const OpNode* old_unsorted_segment_sum_op = unsorted_segment_sum_like_nodes.at(i);
+      const auto shift_op =
+          user_op::UserOpConfWrapperBuilder(op_name_prefix + "Shift-" + NewUniqueId())
+              .OpTypeName("scalar_add")
+              .Input("in", segment_ids_lbn_vec.at(i))
+              .Output("out")
+              .Attr<bool>("has_float_operand", false)
+              .Attr<double>("float_operand", 0.0)
+              .Attr("has_int_operand", true)
+              .Attr<int64_t>("int_operand", i * num_local_classes)
+              .ScopeSymbolId(old_unsorted_segment_sum_op->op().op_conf().scope_symbol_id())
+              .Build();
+      *segment_ids_disable_boxing_conf->mutable_in()->Add() = shift_op.output("out", 0);
+      *data_disable_boxing_conf->mutable_in()->Add() = data_lbn_vec.at(i);
+      job_builder.AddOps(old_unsorted_segment_sum_op->parallel_desc().parallel_conf(),
+                         {shift_op.op_conf()});
+      job_builder.DelOps({old_unsorted_segment_sum_op->op().op_conf()});
+    }
+    segment_ids_disable_boxing_conf->set_out("out");
+    data_disable_boxing_conf->set_out("out");
+    const auto new_unsorted_segment_sum_like_op =
+        user_op::UserOpConfWrapperBuilder(op_name_prefix + "DisableBoxing-")
+            .OpTypeName("unsorted_segment_sum_like")
+            .Input("segment_ids", GenLogicalBlobName(segment_ids_disable_boxing_op_conf.name(),
+                                                     segment_ids_disable_boxing_conf->out()))
+            .Input("data", GenLogicalBlobName(data_disable_boxing_op_conf.name(),
+                                              data_disable_boxing_conf->out()))
+            .Input("like", GenLogicalBlobName(model_lbi))
+            .Output("out")
+            .ScopeSymbolId(scope_symbol_id)
+            .Build();
+    const LogicalBlobId& old_lbi = src_node->op().BnInOp2Lbi("out");
+    for (const OpEdge* out_edge : src_node->out_edges()) {
+      const OpNode* consumer = out_edge->dst_node();
+      const std::string& consumer_op_name = consumer->op().op_name();
+      if (op_name2op_conf.find(consumer_op_name) == op_name2op_conf.end()) {
+        op_name2op_conf[consumer_op_name] = consumer->op().op_conf();
+      }
+      OperatorConf& consumer_op_conf = op_name2op_conf.at(consumer_op_name);
+      for (const std::string& ibn : consumer->op().input_bns()) {
+        if (consumer->op().BnInOp2Lbi(ibn) == old_lbi) {
+          const auto& old_val = ReplaceInputLbnInOpCustomizedConf(
+              &consumer_op_conf, ibn, new_unsorted_segment_sum_like_op.output("out", 0));
+          CHECK_EQ(GenLogicalBlobName(old_lbi), old_val);
+        }
+      }
+    }
+    job_builder.DelOps({src_node->op().op_conf()});
+    job_builder.AddOps(consistent_parallel_desc.parallel_conf(),
+                       {new_unsorted_segment_sum_like_op.op_conf(),
+                        segment_ids_disable_boxing_op_conf, data_disable_boxing_op_conf});
   });
   return Maybe<void>::Ok();
 }
