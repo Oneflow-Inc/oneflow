aefd557f5d39d51ef5e87bb6dd78d6b487885bb5
clackhan <han_binbin@163.com>
2020-11-27 16:05:57 +0800

    copy cfg head file

--- cmake/cfg.cmake
+++ cmake/cfg.cmake
@@ -25,6 +25,8 @@ execute_process(
 
 include_directories(${CFG_INCLUDE_DIR})
 
+list(APPEND ONEFLOW_INCLUDE_SRC_DIRS ${CFG_INCLUDE_DIR})
+
 function(GENERATE_CFG_AND_PYBIND11_CPP SRCS HDRS PYBIND_SRCS ROOT_DIR)
   list(APPEND ALL_CFG_CONVERT_PROTO
       oneflow/core/common/error.proto
