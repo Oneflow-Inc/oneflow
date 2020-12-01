5df93202d70a0eb8883ceaeeeb80fb0f71bf9749
YongtaoShi <847615435@qq.com>
2020-12-01 19:25:17 +0800

    test case of 2 devices

--- oneflow/python/test/ops/test_unfold_padding.py
+++ oneflow/python/test/ops/test_unfold_padding.py
@@ -211,118 +211,154 @@ def _GetTorchPadding(padding, dim, in_dhw, ksize, strides, dilation_rate):
     return valid_case, tuple(torch_padding)
 
 
+def _compare_with_samples(case):
+    (device_type, device_count, machine_ids, unfold_conf, data_type) = case
+    torch_device = torch.device("cuda" if device_type == "gpu" else "cpu")
+    x_shape = unfold_conf["x_shape"]
+    ksize = unfold_conf["ksize"]
+    strides = unfold_conf["strides"]
+    dilation_rate = unfold_conf["dilation_rate"]
+    padding = unfold_conf["padding"]
+    data_format = unfold_conf["data_format"]
+    flow.clear_default_session()
+
+    dim = len(x_shape) - 2
+    ksize = _GetSequence(ksize, dim, "ksize")
+    strides = _GetSequence(strides, dim, "strides")
+    dilation_rate = _GetSequence(dilation_rate, dim, "dilation_rate")
+    in_dhw = list(x_shape)[-dim:]
+    valid_case, torch_padding = _GetTorchPadding(
+        padding, 2, in_dhw, ksize, strides, dilation_rate
+    )
+
+    # Random inputs
+    x = np.random.randn(*x_shape).astype(type_name_to_np_type[data_type])
+
+    # torch results
+    x_torch = torch.tensor(
+        x, requires_grad=True, device=torch_device, dtype=torch.float
+    )
+    model = torch.nn.Unfold(
+        ksize, stride=strides, padding=torch_padding, dilation=dilation_rate,
+    )
+    model.to(torch_device)
+    y_torch = model(x_torch)
+    z = y_torch.sum()
+    z.backward()
+
+    def assert_grad(b):
+        if not valid_case:
+            print("not valid: ", case)
+            return
+        x_torch_grad_numpy = x_torch.grad.cpu().numpy()
+        assert np.allclose(x_torch_grad_numpy, b.numpy()), (
+            case,
+            x_torch_grad_numpy,
+            b.numpy(),
+        )
+
+    # 1F results
+    flow.clear_default_session()
+    if device_type == "cpu":
+        flow.config.cpu_device_num(device_count)
+    else:
+        flow.config.gpu_device_num(device_count)
+    
+    func_config = flow.FunctionConfig()
+    func_config.default_data_type(flow.float)
+    #func_config.default_placement_scope(flow.scope.placement(device_type, machine_ids))
+    func_config.default_logical_view(flow.scope.consistent_view())
+
+    dtype = type_name_to_flow_type[data_type]
+    tensor_def = oft.Numpy.Placeholder
+
+    @flow.global_function(type="train", function_config=func_config)
+    def unfold_job(x: tensor_def(x_shape, dtype=dtype)):
+        v = flow.get_variable(
+            "x",
+            shape=x_shape,
+            dtype=dtype,
+            initializer=flow.constant_initializer(0),
+            trainable=True,
+        )
+        v = flow.cast_to_current_logical_view(v)
+        flow.watch_diff(v, assert_grad)
+        x += v
+        with flow.scope.placement(device_type, machine_ids):
+            unfold_f = getattr(flow.nn, "unfold{}d".format(dim))
+
+            padding = unfold_conf["padding"]
+            if padding == "SAME":
+                padding = "SAME_UPPER"
+            y = unfold_f(
+                x,
+                ksize=ksize,
+                strides=strides,
+                dilation_rate=dilation_rate,
+                padding=padding,
+                data_format=data_format,
+            )
+            flow.optimizer.SGD(
+                flow.optimizer.PiecewiseConstantScheduler([], [1e-4]),
+                momentum=0,
+            ).minimize(y)
+        return y
+
+    y = unfold_job(x).get()
+    y_ndarray = y.numpy()
+    if not valid_case:
+        return
+
+    y_torch_numpy = y_torch.detach().cpu().numpy()
+    assert y_ndarray.shape == y_torch_numpy.shape, (
+        y_ndarray.shape,
+        y_torch_numpy.shape,
+    )
+    assert np.allclose(y_torch_numpy, y_ndarray, rtol=1e-5, atol=1e-5), (
+        case,
+        y_ndarray.shape,
+        y_torch_numpy,
+        y_ndarray,
+    )
+
+
 @flow.unittest.skip_unless_1n1d()
-class TestUnfoldPadding(flow.unittest.TestCase):
-    def test_unfold(_):
+class TestUnfoldPadding1n1d(flow.unittest.TestCase):
+    def test_unfold_cpu(_):
         arg_dict = OrderedDict()
-        arg_dict["device_type"] = ["cpu", "gpu"]
+        arg_dict["device_type"] = ["cpu"]
+        arg_dict["device_count"] = [1]
+        arg_dict["machine_ids"] = ["0:0"]
         arg_dict["unfold_conf"] = unfold_confs
         arg_dict["data_type"] = ["float32", "double"]
-        torch_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         for case in GenArgList(arg_dict):
-            (device_type, unfold_conf, data_type) = case
-            x_shape = unfold_conf["x_shape"]
-            ksize = unfold_conf["ksize"]
-            strides = unfold_conf["strides"]
-            dilation_rate = unfold_conf["dilation_rate"]
-            padding = unfold_conf["padding"]
-            data_format = unfold_conf["data_format"]
-            flow.clear_default_session()
+            _compare_with_samples(case)
 
-            dim = len(x_shape) - 2
-            ksize = _GetSequence(ksize, dim, "ksize")
-            strides = _GetSequence(strides, dim, "strides")
-            dilation_rate = _GetSequence(dilation_rate, dim, "dilation_rate")
-            in_dhw = list(x_shape)[-dim:]
-            valid_case, torch_padding = _GetTorchPadding(
-                padding, 2, in_dhw, ksize, strides, dilation_rate
-            )
+    def test_unfold_gpu_1n1d(_):
+        arg_dict = OrderedDict()
+        arg_dict["device_type"] = ["gpu"]
+        arg_dict["device_count"] = [1]
+        arg_dict["machine_ids"] = ["0:0"]
+        arg_dict["unfold_conf"] = unfold_confs
+        arg_dict["data_type"] = ["float32", "double"]
 
-            # Random inputs
-            x = np.random.randn(*x_shape).astype(type_name_to_np_type[data_type])
+        for case in GenArgList(arg_dict):
+            _compare_with_samples(case)
 
-            # torch results
-            x_torch = torch.tensor(
-                x, requires_grad=True, device=torch_device, dtype=torch.float
-            )
-            model = torch.nn.Unfold(
-                ksize, stride=strides, padding=torch_padding, dilation=dilation_rate,
-            )
-            model.to(torch_device)
-            y_torch = model(x_torch)
-            z = y_torch.sum()
-            z.backward()
 
-            def assert_grad(b):
-                if not valid_case:
-                    print("not valid: ", case)
-                    return
-                x_torch_grad_numpy = x_torch.grad.cpu().numpy()
-                assert np.allclose(x_torch_grad_numpy, b.numpy()), (
-                    case,
-                    x_torch_grad_numpy,
-                    b.numpy(),
-                )
+@flow.unittest.skip_unless_1n2d()
+class TestUnfoldPadding1n2d(flow.unittest.TestCase):
+    def test_unfold_gpu_1n2d(_):
+        arg_dict = OrderedDict()
+        arg_dict["device_type"] = ["gpu"]
+        arg_dict["device_count"] = [2]
+        arg_dict["machine_ids"] = ["0:0-1"]
+        arg_dict["unfold_conf"] = unfold_confs
+        arg_dict["data_type"] = ["float32", "double"]
 
-            # 1F results
-            dtype = type_name_to_flow_type[data_type]
-
-            func_config = flow.FunctionConfig()
-            func_config.default_data_type(flow.float)
-            flow.config.gpu_device_num(1)
-
-            tensor_def = None
-            tensor_def = oft.Numpy.Placeholder
-
-            @flow.global_function(type="train", function_config=func_config)
-            def unfold_job(x: tensor_def(x_shape, dtype=dtype)):
-                v = flow.get_variable(
-                    "x",
-                    shape=x_shape,
-                    dtype=dtype,
-                    initializer=flow.constant_initializer(0),
-                    trainable=True,
-                )
-                v = flow.cast_to_current_logical_view(v)
-                flow.watch_diff(v, assert_grad)
-                x += v
-                with flow.scope.placement(device_type, "0:0"):
-                    unfold_f = getattr(flow.nn, "unfold{}d".format(dim))
-
-                    padding = unfold_conf["padding"]
-                    if padding == "SAME":
-                        padding = "SAME_UPPER"
-                    y = unfold_f(
-                        x,
-                        ksize=ksize,
-                        strides=strides,
-                        dilation_rate=dilation_rate,
-                        padding=padding,
-                        data_format=data_format,
-                    )
-                    flow.optimizer.SGD(
-                        flow.optimizer.PiecewiseConstantScheduler([], [1e-4]),
-                        momentum=0,
-                    ).minimize(y)
-                return y
-
-            y = unfold_job(x).get()
-            y_ndarray = y.numpy()
-            if not valid_case:
-                continue
-
-            y_torch_numpy = y_torch.detach().cpu().numpy()
-            assert y_ndarray.shape == y_torch_numpy.shape, (
-                y_ndarray.shape,
-                y_torch_numpy.shape,
-            )
-            assert np.allclose(y_torch_numpy, y_ndarray, rtol=1e-5, atol=1e-5), (
-                case,
-                y_ndarray.shape,
-                y_torch_numpy,
-                y_ndarray,
-            )
+        for case in GenArgList(arg_dict):
+            _compare_with_samples(case)
 
 
 if __name__ == "__main__":
