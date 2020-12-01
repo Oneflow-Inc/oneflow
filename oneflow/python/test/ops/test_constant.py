1b467905bcbb71a259ca0ae78eea1f5b41222fd4
liujuncheng <liujuncheng1022@gmail.com>
2020-12-01 12:00:15 +0800

    refine test

--- oneflow/python/test/ops/test_constant.py
+++ oneflow/python/test/ops/test_constant.py
@@ -19,32 +19,30 @@ from collections import OrderedDict
 
 import numpy as np
 import oneflow as flow
-import tensorflow as tf
 from test_util import GenArgList
+from test_util import type_name_to_flow_type
+from test_util import type_name_to_np_type
+import math
 
 func_config = flow.FunctionConfig()
 func_config.default_data_type(flow.float)
 
 
-def compare_with_tensorflow(test_case, device_type, value, shape, rtol=1e-5, atol=1e-5):
+def _test(test_case, device_type, type_name_value):
+    type_name, value = type_name_value
     assert device_type in ["gpu", "cpu"]
     flow.clear_default_session()
 
-    @flow.global_function(function_config=func_config)
-    def ConstantJob():
-        with flow.scope.placement(device_type, "0:0"):
-            x = flow.constant(value, dtype=flow.float, shape=shape)
-            y = flow.math.relu(x)
-            z = flow.math.relu(y)
-            return x
+    shape = (1024, 1024)
 
-    numpy0 = ConstantJob().get().numpy()
-    of_out = ConstantJob().get()
-    test_case.assertTrue(np.allclose(of_out.numpy(), numpy0, rtol=rtol, atol=atol))
-    tf_out = tf.constant(value, dtype=float, shape=shape)
-    test_case.assertTrue(
-        np.allclose(of_out.numpy(), tf_out.numpy(), rtol=rtol, atol=atol)
-    )
+    @flow.global_function(function_config=func_config)
+    def constant_job():
+        flow_type = type_name_to_flow_type(type_name)
+        return flow.constant(value, dtype=flow_type, shape=shape)
+
+    of_out = constant_job().get().numpy()
+    np_type = type_name_to_np_type(type_name)
+    test_case.assertTrue(np.array_equal(of_out, np.full(shape, value).astype(np_type)))
 
 
 @flow.unittest.skip_unless_1n1d()
@@ -52,10 +50,28 @@ class TestConstant(flow.unittest.TestCase):
     def test_constant(test_case):
         arg_dict = OrderedDict()
         arg_dict["device_type"] = ["gpu", "cpu"]
-        arg_dict["value"] = [6, 6.66]
-        arg_dict["shape"] = [(2, 3), (3, 3, 3)]
+        arg_dict["type_name_value"] = [
+            (flow.float, 0),
+            (flow.float, 0.0),
+            (flow.float, 1),
+            (flow.float, 1.0),
+            (flow.float, -1),
+            (flow.float, -1.0),
+            (flow.float, math.pi),
+            (flow.float, -math.pi),
+            (flow.float, float("inf")),
+            (flow.float, float("-inf")),
+            (flow.int32, 0),
+            (flow.int32, 0.0),
+            (flow.int32, 1),
+            (flow.int32, 1.0),
+            (flow.int32, -1),
+            (flow.int32, -1.0),
+            (flow.int32, 2 ** 31 - 1),
+            (flow.int32, -(2 ** 31)),
+        ]
         for arg in GenArgList(arg_dict):
-            compare_with_tensorflow(test_case, *arg)
+            _test(test_case, *arg)
 
 
 if __name__ == "__main__":
