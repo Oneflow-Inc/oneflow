9557002e97ac01ec205ccada5376c2fe9086a61b
ouyangyu <xuanjiuye@gmail.com>
2020-12-05 16:12:53 +0800

    refactor function_config.ssp_stage
    Former-commit-id: a7f5d2ac9269a1d45b7a27c6c7b2972c6089359d

--- oneflow/python/framework/placement_util.py
+++ oneflow/python/framework/placement_util.py
@@ -90,7 +90,7 @@ def api_placement(
 
 
 @enable_if.condition(
-    hob.in_normal_mode & hob.env_initialized & ~hob.session_initialized
+    hob.in_normal_mode & ~hob.session_initialized
 )
 def GetEmptyPlacementScope(device_tag, machine_device_ids):
     return placement_ctx.EmptyPlacementScope(
