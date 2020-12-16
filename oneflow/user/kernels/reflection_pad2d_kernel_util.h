24f5439fe6e6e49c3b85adf9f460982606a723be
Flowingsun007 <flowingsun007@163.com>
2020-12-16 16:53:07 +0800

    update atomic add
    Former-commit-id: cb764ddaaaff6decf09f19c5eb4f3c15f9f2568e

--- oneflow/user/kernels/reflection_pad2d_kernel_util.h
+++ oneflow/user/kernels/reflection_pad2d_kernel_util.h
@@ -26,24 +26,21 @@ namespace oneflow {
   
 #define REFLECTION_PAD2D_DATA_TYPE_CPU_SEQ \
   FLOATING_DATA_TYPE_SEQ                     \
-  OF_PP_MAKE_TUPLE_SEQ(int8_t, DataType::kInt8) \
-  OF_PP_MAKE_TUPLE_SEQ(int32_t, DataType::kInt32) \
-  OF_PP_MAKE_TUPLE_SEQ(int64_t, DataType::kInt64)
+  OF_PP_MAKE_TUPLE_SEQ(int32_t, DataType::kInt32)
+
 
 #define REFLECTION_PAD2D_DATA_TYPE_GPU_SEQ \
-  REFLECTION_PAD2D_DATA_TYPE_CPU_SEQ   \
-  FLOAT16_DATA_TYPE_SEQ
+  FLOAT16_DATA_TYPE_SEQ \
+  REFLECTION_PAD2D_DATA_TYPE_CPU_SEQ
   
 
 #define REFLECTION_PAD2D_GRAD_DATA_TYPE_CPU_SEQ \
   FLOATING_DATA_TYPE_SEQ                     \
-  OF_PP_MAKE_TUPLE_SEQ(int8_t, DataType::kInt8) \
-  OF_PP_MAKE_TUPLE_SEQ(int32_t, DataType::kInt32) \
-  OF_PP_MAKE_TUPLE_SEQ(int64_t, DataType::kInt64)
+  OF_PP_MAKE_TUPLE_SEQ(int32_t, DataType::kInt32)
 
 #define REFLECTION_PAD2D_GRAD_DATA_TYPE_GPU_SEQ \
-  REFLECTION_PAD2D_GRAD_DATA_TYPE_CPU_SEQ   \
-  FLOAT16_DATA_TYPE_SEQ
+  FLOAT16_DATA_TYPE_SEQ \
+  REFLECTION_PAD2D_GRAD_DATA_TYPE_CPU_SEQ
 
 namespace user_op {
 
@@ -110,7 +107,6 @@ OF_DEVICE_FUNC void DoReflectionPad2d(
           }
           ip_x = ip_x - pad_left;
           ip_y = ip_y - pad_top;
-          int64_t num = n * dest_num + c*n_channel + i*y_height + j;
           int64_t  dest_index = n * dest_num + c * y_width * y_height + i * y_width + j;
           int64_t src_index = n * src_num + c * x_width * x_height + ip_y * x_width + ip_x;
           dest[dest_index] = src[src_index];
@@ -121,12 +117,12 @@ OF_DEVICE_FUNC void DoReflectionPad2d(
   
 }
 
-
+// dest[dest_index] += src[src_index];
 template<typename T>
 OF_DEVICE_FUNC void DoReflectionPad2dGrad(
     const T* src, T* dest, int64_t n_batch, int64_t n_channel,
     int64_t dy_height, int64_t dy_width, int64_t dx_height, int64_t dx_width, 
-    int64_t pad_left, int64_t pad_top 
+    int64_t pad_left, int64_t pad_top
 ) {
   int64_t src_num = n_channel * dy_height * dy_width;
   int64_t dest_num = n_channel * dx_height * dx_width;
@@ -154,8 +150,7 @@ OF_DEVICE_FUNC void DoReflectionPad2dGrad(
           ip_y = ip_y - pad_top;
           int64_t src_index = n * src_num + c * dy_width * dy_height + i * dy_width + j;
           int64_t dest_index = n * dest_num + c * dx_width * dx_height + ip_y * dx_width + ip_x;
-          dest[dest_index] += src[src_index];
-          //DeviceAdd<T>::Invoke(src + s_index, dest + d_index);
+          DeviceAdd<T>::Invoke(src + src_index, dest + dest_index);
         }
       }
     }
