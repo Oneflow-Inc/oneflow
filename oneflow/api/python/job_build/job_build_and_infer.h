cb19f2956e53f34a42e84f4d23c5a7b9f2029801
binbinHan <han_binbin@163.com>
2020-12-17 12:46:05 -0600

    Migrate oneflow internal api to pybind11 (#3953)
    * replace ErrorProto with cfg::ErrorProto
    
    * fix macro name
    
    * optimize error
    
    * optimize error
    
    * fix code style
    
    * Organize the code
    
    * fix code style
    
    * fix code style
    
    * migrate env rel api to pybind11
    
    * migrate session rel api to pybind11
    
    * migrate vm rel api to pybind11
    
    * migrate op rel api to pybind11
    
    * migrate calibration rel api to pybind11
    
    * migrate framework rel api to pybind11
    
    * migrate partial  ofblob rel api to pybind11
    
    * migrate ofblob copy_shape rel api to pybind11
    
    * migrate ofblob copy_data rel api to pybind11
    
    * change RunInstruction's res to cfg::ErrorProto
    
    * move compiler conditions to type_traits.h
    
    * fix macro error
    
    * remove swig in compilation process
    
    * fix format
    
    * remove oneflow_internal in ofblob.py
    
    * remove oneflow_internal in c_api_util.py
    
    * rename macro switch
    
    * move py_kernel_registry.cpp to oneflow/api/python/framework/
    
    * move py_kernel_registry.cpp to oneflow/api/python/framework/
    
    * add inline and  macro lock in head file
    
    * fix bug
    
    * add head file
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>
    Former-commit-id: 95702539fc57fb1a51676def031e9d31d7dbacf4

--- oneflow/api/python/job_build/job_build_and_infer.h
+++ oneflow/api/python/job_build/job_build_and_infer.h
@@ -16,182 +16,221 @@ limitations under the License.
 #ifndef ONEFLOW_API_PYTHON_JOB_BUILD_JOB_BUILD_AND_INFER_H_
 #define ONEFLOW_API_PYTHON_JOB_BUILD_JOB_BUILD_AND_INFER_H_
 
-#include <utility>
+#include "oneflow/core/job/global_for.h"
+#include "oneflow/core/common/protobuf.h"
 #include "oneflow/core/job/job_build_and_infer_ctx.h"
 #include "oneflow/core/record/record.pb.h"
 #include "oneflow/core/job/job_build_and_infer_ctx_mgr.h"
 #include "oneflow/core/job/job.pb.h"
-#include "oneflow/core/job/placement.pb.h"
-#include "oneflow/core/operator/op_conf.pb.h"
-#include "oneflow/api/python/job_build/job_build_and_infer_helper.h"
 
-std::shared_ptr<oneflow::cfg::ErrorProto> JobBuildAndInferCtx_Open(const std::string& job_name) {
-  return oneflow::JobBuildAndInferCtx_Open(job_name).GetDataAndErrorProto();
+namespace oneflow {
+
+namespace {
+
+inline Maybe<JobBuildAndInferCtxMgr*> GlobalJobBuildAndInferCtxMgr() {
+  if (EagerExecutionEnabled()) {
+    return JUST(GlobalMaybe<EagerJobBuildAndInferCtxMgr>());
+  } else {
+    return JUST(GlobalMaybe<LazyJobBuildAndInferCtxMgr>());
+  }
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_GetCurrentJobName() {
-  return oneflow::JobBuildAndInferCtx_GetCurrentJobName().GetDataAndErrorProto(std::string(""));
+inline Maybe<JobBuildAndInferCtx*> GetJobBuildAndInferCtx(const std::string& job_name) {
+  auto* mgr = JUST(GlobalJobBuildAndInferCtxMgr());
+  return mgr->FindJobBuildAndInferCtx(job_name);
 }
 
-std::shared_ptr<oneflow::cfg::ErrorProto> JobBuildAndInferCtx_Close() {
-  return oneflow::JobBuildAndInferCtx_Close().GetDataAndErrorProto();
+inline Maybe<JobBuildAndInferCtx*> GetCurInferCtx() {
+  auto* mgr = JUST(GlobalJobBuildAndInferCtxMgr());
+  return mgr->FindJobBuildAndInferCtx(*JUST(mgr->GetCurrentJobName()));
 }
 
-std::shared_ptr<oneflow::cfg::ErrorProto> CurJobBuildAndInferCtx_CheckJob() {
-  return oneflow::CurJobBuildAndInferCtx_CheckJob().GetDataAndErrorProto();
+}  // namespace
+
+inline Maybe<void> JobBuildAndInferCtx_Open(const std::string& job_name) {
+  auto* mgr = JUST(GlobalJobBuildAndInferCtxMgr());
+  return mgr->OpenJobBuildAndInferCtx(job_name);
 }
 
-std::shared_ptr<oneflow::cfg::ErrorProto> CurJobBuildAndInferCtx_SetJobConf(
-    const std::string& serialized_job_conf) {
-  return oneflow::CurJobBuildAndInferCtx_SetJobConf(serialized_job_conf).GetDataAndErrorProto();
+inline Maybe<std::string> JobBuildAndInferCtx_GetCurrentJobName() {
+  auto* mgr = JUST(GlobalJobBuildAndInferCtxMgr());
+  return mgr->GetCurrentJobName();
 }
 
-std::shared_ptr<oneflow::cfg::ErrorProto> CurJobBuildAndInferCtx_SetTrainConf(
-    const std::string& serialized_train_conf) {
-  return oneflow::CurJobBuildAndInferCtx_SetTrainConf(serialized_train_conf).GetDataAndErrorProto();
+inline Maybe<void> JobBuildAndInferCtx_Close() {
+  auto* mgr = JUST(GlobalJobBuildAndInferCtxMgr());
+  JUST(mgr->CloseCurrentJobBuildAndInferCtx());
+  return Maybe<void>::Ok();
 }
 
-std::shared_ptr<oneflow::cfg::ErrorProto> CurJobBuildAndInferCtx_Complete() {
-  return oneflow::CurJobBuildAndInferCtx_Complete().GetDataAndErrorProto();
+inline Maybe<void> CurJobBuildAndInferCtx_CheckJob() { return JUST(GetCurInferCtx())->CheckJob(); }
+
+inline Maybe<void> CurJobBuildAndInferCtx_SetJobConf(const std::string& serialized_job_conf) {
+  // parse
+  JobConfigProto job_conf;
+  CHECK_OR_RETURN(TxtString2PbMessage(serialized_job_conf, &job_conf)) << "job conf parse failed";
+  return JUST(GetCurInferCtx())->SetJobConf(job_conf);
 }
 
-std::pair<bool, std::shared_ptr<oneflow::cfg::ErrorProto>> CurJobBuildAndInferCtx_HasJobConf() {
-  return oneflow::CurJobBuildAndInferCtx_HasJobConf().GetDataAndErrorProto(false);
+inline Maybe<void> CurJobBuildAndInferCtx_SetTrainConf(const std::string& train_conf_str) {
+  TrainConf train_conf;
+  CHECK_OR_RETURN(TxtString2PbMessage(train_conf_str, &train_conf)) << "train conf parse failed";
+  return JUST(GetCurInferCtx())->SetTrainConf(train_conf);
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-CurJobBuildAndInferCtx_AddAndInferMirroredOp(const std::string& serialized_op_conf) {
-  return oneflow::CurJobBuildAndInferCtx_AddAndInferMirroredOp(serialized_op_conf)
-      .GetDataAndErrorProto(std::string(""));
+inline Maybe<void> CurJobBuildAndInferCtx_Complete() { return JUST(GetCurInferCtx())->Complete(); }
+
+inline Maybe<bool> CurJobBuildAndInferCtx_HasJobConf() {
+  return JUST(GetCurInferCtx())->HasJobConf();
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-CurJobBuildAndInferCtx_AddAndInferConsistentOp(const std::string& serialized_op_conf) {
-  return oneflow::CurJobBuildAndInferCtx_AddAndInferConsistentOp(serialized_op_conf)
-      .GetDataAndErrorProto(std::string(""));
+inline Maybe<std::string> CurJobBuildAndInferCtx_AddAndInferMirroredOp(
+    const std::string& op_conf_str) {
+  OperatorConf op_conf;
+  CHECK_OR_RETURN(TxtString2PbMessage(op_conf_str, &op_conf)) << "operator conf parse failed";
+  auto* ctx = JUST(GetCurInferCtx());
+  const auto& op_attribute = JUST(ctx->AddAndInferMirroredOp(op_conf));
+  return PbMessage2TxtString(*op_attribute);
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobGetSerializedParallelConfFromProducerView(
+inline Maybe<std::string> CurJobBuildAndInferCtx_AddAndInferConsistentOp(
+    const std::string& op_conf_str) {
+  OperatorConf op_conf;
+  CHECK_OR_RETURN(TxtString2PbMessage(op_conf_str, &op_conf)) << "operator conf parse failed";
+  auto* ctx = JUST(GetCurInferCtx());
+  const auto& op_attribute = JUST(ctx->AddAndInferConsistentOp(op_conf));
+  return PbMessage2TxtString(*op_attribute);
+}
+
+inline Maybe<std::string> JobBuildAndInferCtx_MirroredBlobGetSerializedParallelConfFromProducerView(
     const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobGetSerializedParallelConfFromProducerView(
-             job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return PbMessage2TxtString(
+      JUST(ctx->MirroredBlobGetParallelDescFromProducerView(lbn))->parallel_conf());
 }
 
-std::shared_ptr<oneflow::cfg::ErrorProto> CurJobBuildAndInferCtx_AddLbiAndDiffWatcherUuidPair(
-    const std::string& lbi_uuid_pair) {
-  return oneflow::CurJobBuildAndInferCtx_AddLbiAndDiffWatcherUuidPair(lbi_uuid_pair)
-      .GetDataAndErrorProto();
+inline Maybe<void> CurJobBuildAndInferCtx_AddLbiAndDiffWatcherUuidPair(
+    const std::string& lbi_uuid_pair_str) {
+  auto* ctx = JUST(GetCurInferCtx());
+  LbiAndDiffWatcherUuidPair lbi_uuid_pair;
+  CHECK_OR_RETURN(TxtString2PbMessage(lbi_uuid_pair_str, &lbi_uuid_pair))
+      << "LbiAndDiffWatcherUuidPair parse failed";
+  return ctx->AddLbiAndDiffWatcherUuidPair(lbi_uuid_pair);
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_GetSerializedIdListAsStaticShape(const std::string& job_name,
-                                                     const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_GetSerializedIdListAsStaticShape(job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
-}
-
-std::pair<long long, std::shared_ptr<oneflow::cfg::ErrorProto>> JobBuildAndInferCtx_GetDataType(
+inline Maybe<std::string> JobBuildAndInferCtx_GetSerializedIdListAsStaticShape(
     const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_GetDataType(job_name, lbn).GetDataAndErrorProto(0LL);
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  const auto& shape = JUST(ctx->GetStaticShape(lbn));
+  Int64List id_list;
+  *id_list.mutable_value() = {shape->dim_vec().begin(), shape->dim_vec().end()};
+  return PbMessage2TxtString(id_list);
 }
 
-std::pair<bool, std::shared_ptr<oneflow::cfg::ErrorProto>> JobBuildAndInferCtx_IsDynamic(
-    const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_IsDynamic(job_name, lbn).GetDataAndErrorProto(false);
+inline Maybe<long long> JobBuildAndInferCtx_GetDataType(const std::string& job_name,
+                                                        const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return JUST(ctx->GetDataType(lbn));
 }
 
-std::pair<bool, std::shared_ptr<oneflow::cfg::ErrorProto>> JobBuildAndInferCtx_DisableBoxing(
-    const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_DisableBoxing(job_name, lbn).GetDataAndErrorProto(false);
-}
-
-std::pair<bool, std::shared_ptr<oneflow::cfg::ErrorProto>> JobBuildAndInferCtx_IsTensorList(
-    const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_IsTensorList(job_name, lbn).GetDataAndErrorProto(false);
-}
-
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>> JobBuildAndInferCtx_GetBatchAxis(
-    const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_GetBatchAxis(job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
-}
-
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_GetSplitAxisFromProducerView(const std::string& job_name,
+inline Maybe<bool> JobBuildAndInferCtx_IsDynamic(const std::string& job_name,
                                                  const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_GetSplitAxisFromProducerView(job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return ctx->IsDynamic(lbn);
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_GetSerializedParallelConfFromProducerView(const std::string& job_name,
-                                                              const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_GetSerializedParallelConfFromProducerView(job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
+inline Maybe<bool> JobBuildAndInferCtx_DisableBoxing(const std::string& job_name,
+                                                     const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return ctx->DisableBoxing(lbn);
 }
 
-std::shared_ptr<oneflow::cfg::ErrorProto> CurJobBuildAndInferCtx_AddLossLogicalBlobName(
-    const std::string& lbn) {
-  return oneflow::CurJobBuildAndInferCtx_AddLossLogicalBlobName(lbn).GetDataAndErrorProto();
+inline Maybe<bool> JobBuildAndInferCtx_IsTensorList(const std::string& job_name,
+                                                    const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return ctx->IsTensorList(lbn);
 }
 
-std::pair<bool, std::shared_ptr<oneflow::cfg::ErrorProto>> JobBuildAndInferCtx_IsMirroredBlob(
+inline Maybe<std::string> JobBuildAndInferCtx_GetBatchAxis(const std::string& job_name,
+                                                           const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return PbMessage2TxtString(*JUST(ctx->GetBatchAxis(lbn)));
+}
+
+inline Maybe<std::string> JobBuildAndInferCtx_GetSplitAxisFromProducerView(
     const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_IsMirroredBlob(job_name, lbn).GetDataAndErrorProto(false);
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return PbMessage2TxtString(*JUST(ctx->GetSplitAxisFromProducerView(lbn)));
 }
 
-std::pair<int, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobGetNumSubLbi(const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobGetNumSubLbi(job_name, lbn)
-      .GetDataAndErrorProto(0);
+inline Maybe<std::string> JobBuildAndInferCtx_GetSerializedParallelConfFromProducerView(
+    const std::string& job_name, const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return PbMessage2TxtString(JUST(ctx->GetParallelDescFromProducerView(lbn))->parallel_conf());
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobGetSerializedSubLbi(const std::string& job_name,
-                                                    const std::string& lbn, int index) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobGetSubLbi(job_name, lbn, index)
-      .GetDataAndErrorProto(std::string(""));
+inline Maybe<void> CurJobBuildAndInferCtx_AddLossLogicalBlobName(const std::string& lbn) {
+  return JUST(GetCurInferCtx())->AddLossLogicalBlobName(lbn);
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobGetSerializedIdListAsStaticShape(const std::string& job_name,
-                                                                 const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobGetSerializedIdListAsStaticShape(job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
+inline Maybe<bool> JobBuildAndInferCtx_IsMirroredBlob(const std::string& job_name,
+                                                      const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return ctx->IsMirroredBlob(lbn);
 }
 
-std::pair<long long, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobGetDataType(const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobGetDataType(job_name, lbn)
-      .GetDataAndErrorProto(0LL);
+inline Maybe<int> JobBuildAndInferCtx_MirroredBlobGetNumSubLbi(const std::string& job_name,
+                                                               const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return ctx->MirroredBlobGetNumSubLbi(lbn);
 }
 
-std::pair<bool, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobIsDynamic(const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobIsDynamic(job_name, lbn)
-      .GetDataAndErrorProto(false);
+inline Maybe<std::string> JobBuildAndInferCtx_MirroredBlobGetSubLbi(const std::string& job_name,
+                                                                    const std::string& lbn,
+                                                                    int index) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return PbMessage2TxtString(*JUST(ctx->MirroredBlobGetSubLbi(lbn, index)));
 }
 
-std::pair<bool, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobIsTensorList(const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobIsTensorList(job_name, lbn)
-      .GetDataAndErrorProto(0LL);
+inline Maybe<std::string> JobBuildAndInferCtx_MirroredBlobGetSerializedIdListAsStaticShape(
+    const std::string& job_name, const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  const auto& shape = JUST(ctx->MirroredBlobGetStaticShape(lbn));
+  Int64List id_list;
+  *id_list.mutable_value() = {shape->dim_vec().begin(), shape->dim_vec().end()};
+  return PbMessage2TxtString(id_list);
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobGetBatchAxis(const std::string& job_name, const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobGetBatchAxis(job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
+inline Maybe<long long> JobBuildAndInferCtx_MirroredBlobGetDataType(const std::string& job_name,
+                                                                    const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return JUST(ctx->MirroredBlobGetDataType(lbn));
 }
 
-std::pair<std::string, std::shared_ptr<oneflow::cfg::ErrorProto>>
-JobBuildAndInferCtx_MirroredBlobGetSplitAxisFromProducerView(const std::string& job_name,
+inline Maybe<bool> JobBuildAndInferCtx_MirroredBlobIsDynamic(const std::string& job_name,
                                                              const std::string& lbn) {
-  return oneflow::JobBuildAndInferCtx_MirroredBlobGetSplitAxisFromProducerView(job_name, lbn)
-      .GetDataAndErrorProto(std::string(""));
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return ctx->MirroredBlobIsDynamic(lbn);
 }
+
+inline Maybe<bool> JobBuildAndInferCtx_MirroredBlobIsTensorList(const std::string& job_name,
+                                                                const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return ctx->MirroredBlobIsTensorList(lbn);
+}
+
+inline Maybe<std::string> JobBuildAndInferCtx_MirroredBlobGetBatchAxis(const std::string& job_name,
+                                                                       const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return PbMessage2TxtString(*JUST(ctx->MirroredBlobGetBatchAxis(lbn)));
+}
+
+inline Maybe<std::string> JobBuildAndInferCtx_MirroredBlobGetSplitAxisFromProducerView(
+    const std::string& job_name, const std::string& lbn) {
+  auto* ctx = JUST(GetJobBuildAndInferCtx(job_name));
+  return PbMessage2TxtString(*JUST(ctx->MirroredBlobGetSplitAxisFromProducerView(lbn)));
+}
+
+}  // namespace oneflow
 
 #endif  // ONEFLOW_API_PYTHON_JOB_BUILD_JOB_BUILD_AND_INFER_H_
