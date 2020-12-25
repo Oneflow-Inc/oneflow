e0a7475f4a34ddeaf55de62a4ca42c78dad0b406
tsai <caishenghang@oneflow.org>
2020-12-25 17:11:41 +0800

    rename to result

--- oneflow/ir/oneflow-translate/oneflow-translate.cpp
+++ oneflow/ir/oneflow-translate/oneflow-translate.cpp
@@ -55,24 +55,25 @@ class Importer {
   ModuleOp module;
   /// Cached FileLineColLoc::get("imported-protobuf", 0, 0).
   Location unknownLoc;
-  std::unordered_map<std::string, mlir::Value> lbn2op;
+  std::unordered_map<std::string, mlir::Value> lbn2result;
 };
 
 LogicalResult Importer::processUserOp(const ::oneflow::OperatorConf &op) {
   if (op.has_user_conf() == false) { return failure(); }
   const std::string &type_name = op.user_conf().op_type_name();
   if (type_name == "relu") {
-    mlir::Value in = lbn2op.at(op.user_conf().input().at("in").s(0));
-    mlir::Value created = b.create<oneflow::ReluOp>(unknownLoc, in);
+    mlir::Value in = lbn2result.at(op.user_conf().input().at("in").s(0));
+    mlir::Value created = b.create<oneflow::ReluOp>(unknownLoc, in).getResult();
     const std::string &lbn = op.user_conf().output().at("out").s(0);
-    lbn2op.insert({lbn, created});
+    lbn2result.insert({lbn, created});
     return success();
   } else if (type_name == "constant") {
     if (op.user_conf().attr().at("is_floating_value").at_bool()) {
       mlir::Value created = b.create<oneflow::ConstantOp>(
-          unknownLoc, op.user_conf().attr().at("floating_value").at_double());
+                                 unknownLoc, op.user_conf().attr().at("floating_value").at_double())
+                                .getResult();
       const std::string &lbn = op.user_conf().output().at("out").s(0);
-      lbn2op.insert({lbn, created});
+      lbn2result.insert({lbn, created});
     } else {
       // b.create<ConstantOp>(unknownLoc, op.user_conf().attr().at("integer_value").at_int64());
     }
