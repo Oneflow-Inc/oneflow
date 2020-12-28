7d725d173eee43c620169745a32dcc566de94097
Ziheng ZHAO <68211760+Ziheng-ZHAO@users.noreply.github.com>
2020-12-28 15:10:51 +0800

    Update test_logsoftmax.py

--- oneflow/python/test/ops/test_logsoftmax.py
+++ oneflow/python/test/ops/test_logsoftmax.py
@@ -13,6 +13,18 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import oneflow as flow
 import numpy as np
 import oneflow.typing as tp
@@ -131,7 +143,7 @@ class Testlogsoftmax1n2d(flow.unittest.TestCase):
     def test_logsoftmax_gpu_1n2d(test_case):
         arg_dict = _gen_arg_dict(
             shape=(2, 8),
-            axis=1,
+            axis=(-1, 1),
             device_type="gpu",
             machine_ids="0:0-1",
             device_counts=2,
