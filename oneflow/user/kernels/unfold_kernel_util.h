1f8db927893b1905f2712b2675adbbadd973a994
leaves-zwx <kunta0932@gmail.com>
2020-12-28 09:02:05 +0800

    Generic unfold kernel implementation (#4033)
    * generic unfold kernel implementation
    
    * fix bug and pretty code
    
    * cpu version UnfoldKernelUtil::Forward

--- oneflow/user/kernels/unfold_kernel_util.h
+++ oneflow/user/kernels/unfold_kernel_util.h
@@ -15,11 +15,98 @@ limitations under the License.
 */
 #ifndef ONEFLOW_USER_KERNELS_UNFOLD_KERNEL_UTIL_H_
 #define ONEFLOW_USER_KERNELS_UNFOLD_KERNEL_UTIL_H_
-#include "oneflow/core/framework/framework.h"
+
+#include "oneflow/core/device/device_context.h"
+#include "oneflow/core/common/shape.h"
+#include "oneflow/core/common/nd_index_offset_helper.h"
+#include "oneflow/core/common/switch_func.h"
+#include "oneflow/core/ndarray/xpu_util.h"
 
 namespace oneflow {
 
 namespace user_op {
+
+constexpr int kUnfoldPaddingValue = 0;
+
+// NDIM range: (1, 2, 3)
+// SDIM range: (1, 2), 1 indicates channels_last, 2 indicates channels_first
+template<typename INDEX_T, int NDIM, int SDIM>
+struct UnfoldParams {
+  static constexpr int kInputNDim = NDIM + 2;
+  static constexpr int kOutputNDim = NDIM * 2 + 2;
+  static constexpr int kInputChannelDim = (2 - SDIM) * NDIM + 1;
+  static constexpr int kOutputChannelDim = (2 - SDIM) * NDIM * 2 + 1;
+  static_assert(kInputChannelDim < kInputNDim, "");
+  static_assert(kOutputChannelDim < kOutputNDim, "");
+  UnfoldParams(const int64_t batch_size, const int64_t channels, const int64_t* spatial_dims,
+               const int32_t* kernel_size, const int32_t* padding_before,
+               const int32_t* padding_after, const int32_t* stride, const int32_t* dilation);
+  INDEX_T elem_cnt;
+  INDEX_T dims[NDIM];
+  int padding[NDIM];
+  int stride[NDIM];
+  int dilation[NDIM];
+  NdIndexOffsetHelper<INDEX_T, kInputNDim> in_index_helper;
+  NdIndexOffsetHelper<INDEX_T, kOutputNDim> out_index_helper;
+};
+
+template<typename INDEX_T, int NDIM, int SDIM>
+UnfoldParams<INDEX_T, NDIM, SDIM>::UnfoldParams(const int64_t batch_size, const int64_t channels,
+                                                const int64_t* spatial_dims,
+                                                const int32_t* kernel_size,
+                                                const int32_t* padding_before,
+                                                const int32_t* padding_after, const int32_t* stride,
+                                                const int32_t* dilation)
+    : elem_cnt(0), in_index_helper(0), out_index_helper(0) {
+  INDEX_T input_dims[kInputNDim] = {0};
+  INDEX_T output_dims[kOutputNDim] = {0};
+  this->elem_cnt = batch_size * channels;
+  input_dims[0] = batch_size;
+  output_dims[0] = batch_size;
+  input_dims[kInputChannelDim] = channels;
+  output_dims[kOutputChannelDim] = channels;
+  for (int d = 0; d < NDIM; ++d) {
+    this->elem_cnt *= spatial_dims[d];
+    this->dims[d] = spatial_dims[d];
+    this->padding[d] = padding_before[d];
+    this->stride[d] = stride[d];
+    this->dilation[d] = dilation[d];
+    input_dims[SDIM + d] = spatial_dims[d];
+    output_dims[SDIM + d] = kernel_size[d];
+    output_dims[SDIM + NDIM + d] = (spatial_dims[d] + padding_before[d] + padding_after[d]
+                                    - dilation[d] * (kernel_size[d] - 1) - 1)
+                                       / stride[d]
+                                   + 1;
+  }
+  in_index_helper = NdIndexOffsetHelper<INDEX_T, kInputNDim>(input_dims);
+  out_index_helper = NdIndexOffsetHelper<INDEX_T, kOutputNDim>(output_dims);
+}
+
+// index_a format: (N, C, di, hi, wi, db, hb, wb) or (N, di, hi, wi, db, hb, wb, C)
+// index_b format: (N, C, D, H, W) or (N, D, H, W, C)
+// return: false indicates out-of-bound, otherwise in-bound
+template<typename INDEX_T, int NDIM, int SDIM>
+OF_DEVICE_FUNC bool UnfoldIndexTransform(const UnfoldParams<INDEX_T, NDIM, SDIM>& params,
+                                         const INDEX_T* index_a, INDEX_T* index_b) {
+  // batch dim index transform
+  index_b[0] = index_a[0];
+  // channel dim index transform
+  using ParamType = UnfoldParams<INDEX_T, NDIM, SDIM>;
+  index_b[ParamType::kInputChannelDim] = index_a[ParamType::kOutputChannelDim];
+  // spatial dim index transform
+  bool out_bound = false;
+#ifdef __CUDA_ARCH__
+#pragma unroll
+#endif
+  // D,H,W spatial dim index transform
+  for (int64_t d = 0; d < NDIM; ++d) {
+    INDEX_T idx = index_a[SDIM + NDIM + d] * params.stride[d]
+                  + index_a[SDIM + d] * params.dilation[d] - params.padding[d];
+    out_bound = out_bound && (idx < 0 || idx >= params.dims[d]);
+    if (!out_bound) { index_b[d] = idx; }
+  }
+  return out_bound;
+}
 
 template<DeviceType device_type, typename T>
 class UnfoldKernelUtil {
@@ -39,9 +126,31 @@ class UnfoldKernelUtil {
                              T* data_im);
 };
 
+template<DeviceType device_type, typename T, typename INDEX_T, int NDIM, int SDIM>
+struct UnfoldKernelUtilV2 {
+  static void Forward(DeviceCtx* ctx, const void* params, const T* input_ptr, T* output_ptr);
+};
+
+#define SPATIAL_NDIM_SEQ OF_PP_MAKE_TUPLE_SEQ(1) OF_PP_MAKE_TUPLE_SEQ(2) OF_PP_MAKE_TUPLE_SEQ(3)
+#define SPATIAL_DIM_SEQ OF_PP_MAKE_TUPLE_SEQ(1) OF_PP_MAKE_TUPLE_SEQ(2)
+
+#define INSTANTIATE_UNFOLD_KERNEL_UTIL_V2(device, dtype, itype, ndim, sdim) \
+  template struct UnfoldKernelUtilV2<device, dtype, itype, ndim, sdim>;
+
+#define INSTANTIATE_UNFOLD_KERNEL_UTIL_WITH_TYPE_PAIR(device, dtype_pair, itype_pair, ndim, sdim) \
+  INSTANTIATE_UNFOLD_KERNEL_UTIL_V2(device, OF_PP_PAIR_FIRST(dtype_pair),                         \
+                                    OF_PP_PAIR_FIRST(itype_pair), ndim, sdim)
+
+#define INSTANTIATE_UNFOLD_KERNEL_UTIL_FOR_DEVICE(device)                                   \
+  OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_UNFOLD_KERNEL_UTIL_WITH_TYPE_PAIR, (device), \
+                                   ARITHMETIC_DATA_TYPE_SEQ, INDEX_DATA_TYPE_SEQ,           \
+                                   SPATIAL_NDIM_SEQ, SPATIAL_DIM_SEQ)
+
 #define INSTANTIATE_UNFOLD_KERNEL_UTIL(device, dtype) \
   template class UnfoldKernelUtil<device, dtype>;
+
 }  // namespace user_op
+
 }  // namespace oneflow
 
 #endif  // ONEFLOW_USER_KERNELS_UNFOLD_KERNEL_UTIL_H_
