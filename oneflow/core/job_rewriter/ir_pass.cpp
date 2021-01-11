39618578533b5bfe8a931e2e0f191e06e8120dd2
tsai <caishenghang@oneflow.org>
2021-01-11 16:24:21 +0800

    rm unused

--- oneflow/core/job_rewriter/ir_pass.cpp
+++ oneflow/core/job_rewriter/ir_pass.cpp
@@ -14,30 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/job_rewriter/job_pass.h"
-#include "oneflow/core/register/runtime_blob_desc.h"
-#include "oneflow/core/framework/framework.h"
 #include "oneflow/ir/oneflow-translate/include/OneFlow/MLIROneFlowTranslation.h"
 
 namespace oneflow {
 
 namespace {
-
-std::function<bool(const OpNode* op_node)> MakePredicatorIsSafeToDelete(const OpGraph& op_graph) {
-  HashSet<std::string> ctrl_in_op_names;
-  op_graph.ForEachNode([&](const OpNode* op_node) {
-    for (const std::string& ctrl_in_op_name : op_node->op().op_conf().ctrl_in_op_name()) {
-      ctrl_in_op_names.insert(ctrl_in_op_name);
-    }
-  });
-  return [=](const OpNode* op_node) {
-    if (op_node->out_edges().size() > 1) { return false; }
-    if (!op_node->op().op_conf().ctrl_in_op_name().empty()) { return false; }
-    if (ctrl_in_op_names.find(op_node->op().op_conf().name()) != ctrl_in_op_names.end()) {
-      return false;
-    }
-    return true;
-  };
-}
 
 class IRPass final : public JobPass {
  public:
@@ -48,24 +29,12 @@ class IRPass final : public JobPass {
     // TODO: add compiler definition for MLIR and job conf flags
     return true;
   }
-  Maybe<void> Apply(const OpGraph& op_graph, JobBuilder* job_builder) const;
-
   Maybe<void> Apply(Job* job, JobPassCtx* ctx) const override {
     if (!IsEnabled(*ctx)) { return Maybe<void>::Ok(); }
-    const OpGraph op_graph(*job);
-    JobBuilder job_builder(job);
     mlir::translateFromOneFlowJobToMLIR(job);
-    return Apply(op_graph, &job_builder);
+    return Maybe<void>::Ok();
   }
 };
-
-Maybe<void> IRPass::Apply(const OpGraph& op_graph, JobBuilder* job_builder) const {
-  const auto IsSafeToDelete = MakePredicatorIsSafeToDelete(op_graph);
-  op_graph.ForEachNode([&](const OpNode* op_node) {
-    if (!IsSafeToDelete(op_node)) { return; }
-  });
-  return Maybe<void>::Ok();
-}
 
 }  // namespace
 
