7dec9ae1993c30c867eac805b5a5d012fb01ef5a
tsai <caishenghang@oneflow.org>
2021-01-11 14:06:49 +0800

    separate lib and bin

---
+++ oneflow/ir/oneflow-translate/include/OneFlow/MLIROneFlowTranslate.h
@@ -0,0 +1,10 @@
+#ifndef ONEFLOW_MLIRONEFLOWTRANSLATE_H
+#define ONEFLOW_MLIRONEFLOWTRANSLATE_H
+
+namespace mlir {
+
+void registerFromOneFlowJobTranslation();
+
+}  // namespace mlir
+
+#endif /* ONEFLOW_MLIRONEFLOWTRANSLATE_H */
