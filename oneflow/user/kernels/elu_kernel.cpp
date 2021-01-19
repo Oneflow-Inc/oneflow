d97c455ac56215e6a83e4fe811ef5a57212c6e81
MARD1NO <359521840@qq.com>
2021-01-19 16:09:50 +0800

    restruct elu

--- oneflow/user/kernels/elu_kernel.cpp
+++ oneflow/user/kernels/elu_kernel.cpp
@@ -13,78 +13,28 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/core/framework/framework.h"
-#include "oneflow/core/common/data_type.h"
+#include "oneflow/user/kernels/elu_kernel.h"
 
 namespace oneflow {
 
-namespace user_op {
-
-template<DeviceType device_type, typename T>
-class CpuEluKernel final : public OpKernel {
- public:
-  CpuEluKernel() = default;
-  ~CpuEluKernel() = default;
-
- private:
-  void Compute(KernelComputeContext* ctx) const override {
-    const Tensor* in_tensor = ctx->Tensor4ArgNameAndIndex("in", 0);
-    Tensor* out_tensor = ctx->Tensor4ArgNameAndIndex("out", 0);
-    const T alpha = static_cast<T>(ctx->Attr<double>("alpha"));
-    const T* in_ptr = in_tensor->dptr<T>();
-    T* out_ptr = out_tensor->mut_dptr<T>();
-
-    const int32_t elem_cnt = in_tensor->shape().elem_cnt();
-    FOR_RANGE(int32_t, i, 0, elem_cnt) {
-      out_ptr[i] = (in_ptr[i] > static_cast<T>(0))
-                       ? in_ptr[i]
-                       : alpha * (std::exp(in_ptr[i]) - static_cast<T>(1));
-    }
+namespace {
+template<template<typename> class Opt, typename T>
+struct ElemwiseEluFunctor<DeviceType::kCPU, Opt, T> final {
+  void operator()(DeviceCtx* ctx, const int64_t elem_cnt, T alpha, T* out, const T* in) {
+    FOR_RANGE(int64_t, i, 0, elem_cnt) { out[i] = Opt<T>(alpha)(in[i]); }
   }
-  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_CPU_ELU_KERNEL(device, dtype)                                            \
-  REGISTER_USER_KERNEL("elu").SetCreateFn<CpuEluKernel<device, dtype>>().SetIsMatchedHob( \
-      (HobDeviceTag() == device) & (HobDataType("out", 0) == GetDataType<dtype>::value));
-
-REGISTER_CPU_ELU_KERNEL(DeviceType::kCPU, float);
-REGISTER_CPU_ELU_KERNEL(DeviceType::kCPU, double);
-
-template<DeviceType device_type, typename T>
-class CpuEluGradKernel final : public OpKernel {
- public:
-  CpuEluGradKernel() = default;
-  ~CpuEluGradKernel() = default;
-
- private:
-  void Compute(KernelComputeContext* ctx) const override {
-    const Tensor* x_tensor = ctx->Tensor4ArgNameAndIndex("x", 0);
-    const Tensor* dy_tensor = ctx->Tensor4ArgNameAndIndex("dy", 0);
-    Tensor* dx_tensor = ctx->Tensor4ArgNameAndIndex("dx", 0);
-    const T alpha = static_cast<T>(ctx->Attr<double>("alpha"));
-    const T* x_ptr = x_tensor->dptr<T>();
-    const T* dy_ptr = dy_tensor->dptr<T>();
-    T* dx_ptr = dx_tensor->mut_dptr<T>();
-    const int32_t elem_cnt = x_tensor->shape().elem_cnt();
-
-    FOR_RANGE(int32_t, i, 0, elem_cnt) {
-      dx_ptr[i] =
-          (x_ptr[i] > static_cast<T>(0)) ? dy_ptr[i] : dy_ptr[i] * alpha * (std::exp(x_ptr[i]));
-    }
+template<template<typename> class Opt, typename T>
+struct ElemwiseEluGradFunctor<DeviceType::kCPU, Opt, T> final {
+  void operator()(DeviceCtx* ctx, const int64_t elem_cnt, T alpha, T* dx, const T* x, const T* dy) {
+    FOR_RANGE(int64_t, i, 0, elem_cnt) { dx[i] = Opt<T>(alpha)(x[i], dy[i]); }
   }
-  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_CPU_ELU_BACKWARD_KERNEL(device, dtype) \
-  REGISTER_USER_KERNEL("elu_grad")                      \
-      .SetCreateFn<CpuEluGradKernel<device, dtype>>()   \
-      .SetIsMatchedHob((HobDeviceTag() == device)       \
-                       & (HobDataType("dx", 0) == GetDataType<dtype>::value));
+}  // namespace
 
-REGISTER_CPU_ELU_BACKWARD_KERNEL(DeviceType::kCPU, float);
-REGISTER_CPU_ELU_BACKWARD_KERNEL(DeviceType::kCPU, double);
-
-}  // namespace user_op
+REGISTER_ELU_KERNELS(DeviceType::kCPU, float);
+REGISTER_ELU_KERNELS(DeviceType::kCPU, double);
 
 }  // namespace oneflow
