898d057a776d569062112cb23b9661ac0d346685
Shenghang Tsai <jackalcooper@gmail.com>
2021-01-26 10:56:28 +0800

    MLIR ctrl edge (#4142)
    * rm unused log
    
    * add type case int
    
    * check in naive impl
    
    * TODO: reject if it is a ctrl edge
    
    * add todo note
    
    * define a base op without control edge
    
    * add control out
    
    * rm todo note
    
    * fix name of macro for switch case
    
    * reorder
    
    * add todo from ctrl in
    
    * rm todo
    
    * TODO: add result lookup for ctrl in
    
    * add control out
    
    * rename AddUserOpInputOutputSegments
    
    * add seg for sys op
    
    * data_input_size
    
    * add AddOperandSegmentSizes
    
    * refactor convert
    
    * refactor class and def in ods
    
    * fix comment
    
    * op_name2ctrl_result_
    
    * ctrl in lookup
    
    * rm duplicated code
    
    * rm todo
    
    * extract AppendCtrlInOperand
    
    * check in test bert code
    
    * rm py code
    
    * extract AppendCtrlOutType
    
    * rm tf import
    
    * port gen code
    
    * fix AppendCtrlOutType bug
    
    * add todo
    
    * fix const
    
    * add ConvertCtrlInputs
    
    * update sys op ibn
    
    * refine const
    
    * add todo
    
    * add todo
    
    * add Canonicalization pass
    
    * refactor rewrite
    
    Co-authored-by: tsai <caishenghang@oneflow.org>

--- oneflow/ir/lib/OneFlow/OneFlowOps.cpp
+++ oneflow/ir/lib/OneFlow/OneFlowOps.cpp
@@ -1,6 +1,9 @@
 #include "OneFlow/OneFlowOps.h"
+#include <iostream>
 #include "OneFlow/OneFlowDialect.h"
 #include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Support/LogicalResult.h"
 
 using namespace mlir;
 using namespace mlir::oneflow;
@@ -16,6 +19,30 @@ static mlir::ParseResult parseConstantOp(mlir::OpAsmParser &parser, mlir::Operat
 }
 
 static mlir::LogicalResult verify(ConstantOp op) { return mlir::success(); }
+
+struct ConcreteUserOps : public mlir::OpRewritePattern<oneflow::UserOp> {
+  ConcreteUserOps(mlir::MLIRContext *context)
+      : OpRewritePattern<oneflow::UserOp>(context, /*benefit=*/1) {}
+  mlir::LogicalResult matchAndRewrite(oneflow::UserOp op,
+                                      mlir::PatternRewriter &rewriter) const override {
+    if (op->getAttrOfType<StringAttr>("op_type_name").getValue().equals("relu")) {
+      if (op.ctrl_inputs().empty() && op.ctrl_output().use_empty()) {
+        if (auto relu = rewriter.create<oneflow::ReluOp>(
+                op->getLoc(), op->getOperands().take_front(), op.getAttrs())) {
+          op.data_output().front().replaceAllUsesWith(relu.y());
+          op->erase();
+          return success();
+        }
+      }
+    }
+    return failure();
+  }
+};
+
+void UserOp::getCanonicalizationPatterns(::mlir::OwningRewritePatternList &results,
+                                         ::mlir::MLIRContext *context) {
+  results.insert<ConcreteUserOps>(context);
+}
 
 #include "OneFlow/OneFlowEnums.cpp.inc"
 
