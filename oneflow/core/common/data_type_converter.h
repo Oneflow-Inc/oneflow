129fb96cfc359191182585268754354a190a48a2
Flowingsun007 <flowingsun007@163.com>
2021-02-02 14:22:50 +0800

    reduction

--- oneflow/core/common/data_type_converter.h
+++ oneflow/core/common/data_type_converter.h
@@ -1,12 +1,9 @@
 /*
 Copyright 2020 The OneFlow Authors. All rights reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -400,6 +397,6 @@ OF_DEVICE_FUNC const Out ConvertSatNorm(In value) {
   return converter_t<Out, In>::ConvertSatNorm(value);
 }
 
-}  // namespace
+}  // namespace oneflow
 
 #endif  // ONEFLOW_CORE_COMMON_DATA_TYPE_CONVERTER_H_
