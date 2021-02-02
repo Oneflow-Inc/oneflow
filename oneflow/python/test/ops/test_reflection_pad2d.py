85f6b8befcb810181a561edfcf9f73437674579c
Flowingsun007 <flowingsun007@163.com>
2021-02-02 14:28:09 +0800

    update test case

--- oneflow/python/test/ops/test_reflection_pad2d.py
+++ oneflow/python/test/ops/test_reflection_pad2d.py
@@ -265,7 +265,7 @@ def _gen_arg_dict(
     arg_dict["samples"] = []
     arg_dict["samples"].append(gen_numpy_test_sample((2, 1, 2, 2), [1, 1, 1, 1]))
     arg_dict["samples"].append(gen_numpy_test_sample((4, 2, 3, 3), [2, 2, 2, 2]))
-    arg_dict["samples"].append(gen_numpy_test_sample((2, 3, 4, 5), [3, 3, 2, 2]))
+    arg_dict["samples"].append(gen_numpy_test_sample((2, 3, 4, 5), [3, 2, 1, 2]))
     if value_type == "float":
         if device_type == "gpu":
             arg_dict["value_type"] = [
