a119b676eedebe7cc487e7683a5b3c9e81f2fdd4
MARD1NO <359521840@qq.com>
2021-02-03 15:07:35 +0800

    remove hardswish useless code

--- oneflow/user/kernels/hardswish_kernel.cpp
+++
@@ -1,8 +0,0 @@
-// #include "oneflow/user/kernels/hardswish_kernel.h"
-
-// namespace oneflow {
-
-// REGISTER_HARDSWISH_KERNEL(DeviceType::kCPU, float);
-// REGISTER_HARDSWISH_KERNEL(DeviceType::kCPU, double);
-
-// }
