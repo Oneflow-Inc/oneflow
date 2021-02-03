c00a1d9fcfb6a32a5d7e8e0bfeb978dad95d9b0f
MARD1NO <359521840@qq.com>
2021-02-03 15:06:52 +0800

    remove elu useless code

--- oneflow/user/kernels/elu_kernel.cpp
+++
@@ -1,8 +0,0 @@
-// #include "oneflow/user/kernels/elu_kernel.h"
-
-// namespace oneflow {
-
-// REGISTER_ELU_KERNEL(DeviceType::kCPU, float);
-// REGISTER_ELU_KERNEL(DeviceType::kCPU, double);
-
-// }
