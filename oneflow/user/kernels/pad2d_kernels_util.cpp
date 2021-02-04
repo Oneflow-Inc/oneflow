4dc31ec9925f436bb58dace61d958cced6aaead8
iamyf <yf88888888@126.com>
2021-02-03 22:40:23 -0500

    add pad2d ops and kernels

--- oneflow/user/kernels/pad2d_kernels_util.cpp
+++ oneflow/user/kernels/pad2d_kernels_util.cpp
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/framework/framework.h"
-#include "oneflow/user/kernels/reflection_pad2d_kernel_util.h"
+#include "oneflow/user/kernels/pad2d_kernels_util.h"
 
 namespace oneflow {
 
@@ -49,10 +49,45 @@ struct ReflectionPad2dGradFunctor<DeviceType::kCPU, IN_T> final {
 };
 
 OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_REFLECTION_PAD2D_FUNCTOR, (DeviceType::kCPU),
-                                 REFLECTION_PAD2D_DATA_TYPE_CPU_SEQ);
+                                 PAD2D_DATA_TYPE_CPU_SEQ);
 
 OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_REFLECTION_PAD2D_GRAD_FUNCTOR, (DeviceType::kCPU),
-                                 REFLECTION_PAD2D_GRAD_DATA_TYPE_CPU_SEQ);
+                                 PAD2D_DATA_TYPE_CPU_SEQ);
+
+
+template<typename IN_T>
+struct ReplicationPad2dFunctor<DeviceType::kCPU, IN_T> final {
+  void operator()(DeviceCtx* ctx, const IN_T* src, IN_T* dest,
+                  const NdIndexOffsetHelper<int64_t, 4>& index_helper, int64_t n_batch,
+                  int64_t n_channel, int64_t y_height, int64_t y_width, int64_t x_height,
+                  int64_t x_width, int64_t pad_left, int64_t pad_top) {
+    int64_t dest_num = n_channel * y_height * y_width;
+    int64_t src_num = n_channel * x_height * x_width;
+    int64_t elem_num = n_batch * dest_num;
+    DoReplicationPad2d<IN_T>(src, dest, index_helper, elem_num, src_num, dest_num, y_height, y_width,
+                            x_height, x_width, pad_left, pad_top);
+  }
+};
+
+template<typename IN_T>
+struct ReplicationPad2dGradFunctor<DeviceType::kCPU, IN_T> final {
+  void operator()(DeviceCtx* ctx, const IN_T* src, IN_T* dest,
+                  const NdIndexOffsetHelper<int64_t, 4>& index_helper, int64_t n_batch,
+                  int64_t n_channel, int64_t dy_height, int64_t dy_width, int64_t dx_height,
+                  int64_t dx_width, int64_t pad_left, int64_t pad_top) {
+    int64_t dest_num = n_channel * dx_height * dx_width;
+    int64_t src_num = n_channel * dy_height * dy_width;
+    int64_t elem_num = n_batch * src_num;
+    DoReplicationPad2dGrad<IN_T>(src, dest, index_helper, elem_num, src_num, dest_num, dy_height,
+                                dy_width, dx_height, dx_width, pad_left, pad_top);
+  }
+};
+
+OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_REPLICATION_PAD2D_FUNCTOR, (DeviceType::kCPU),
+                                 PAD2D_DATA_TYPE_CPU_SEQ);
+
+OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_REPLICATION_PAD2D_GRAD_FUNCTOR, (DeviceType::kCPU),
+                                 PAD2D_DATA_TYPE_CPU_SEQ);                                 
 
 }  // namespace user_op
 }  // namespace oneflow
