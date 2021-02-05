443c4794192e7a1b3e9b8fb82c393c1e2a7490cc
iamyf <yf88888888@126.com>
2021-02-04 21:09:51 -0500

    reformat, change padding_data_type seq, change dim2vector method usage

--- oneflow/user/kernels/pad2d_kernels.cpp
+++ oneflow/user/kernels/pad2d_kernels.cpp
@@ -25,35 +25,37 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/core/framework/framework.h"
-#include "oneflow/core/device/memory_copier.h"
-#include "oneflow/core/kernel/new_kernel_util.h"
 #include "oneflow/core/common/nd_index_offset_helper.h"
+#include "oneflow/core/device/memory_copier.h"
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/core/kernel/new_kernel_util.h"
 #include "oneflow/user/kernels/pad2d_kernels_util.h"
 
 namespace oneflow {
 namespace user_op {
 
 // Fill ShapeView into dim vector
-DimVector ShapeViewToDimVector(const ShapeView& tensor_shape) {
+DimVector ShapeViewToDimVector(const ShapeView &tensor_shape) {
   int64_t ndims = tensor_shape.NumAxes();
   DimVector shape_vec(ndims);
-  for (int64_t i = 0; i < ndims; ++i) { shape_vec[i] = tensor_shape.At(i); }
+  for (int64_t i = 0; i < ndims; ++i) {
+    shape_vec[i] = tensor_shape.At(i);
+  }
   shape_vec[ndims - 1] = shape_vec[ndims - 1];
   return shape_vec;
 }
 
-template<DeviceType device_type, typename IN_T>
+template <DeviceType device_type, typename IN_T>
 class ReflectionPad2dKernel final : public OpKernel {
- public:
+public:
   ReflectionPad2dKernel() = default;
   ~ReflectionPad2dKernel() = default;
 
- private:
-  void Compute(user_op::KernelComputeContext* ctx) const override {
-    const Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
-    Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
-    const auto& padding = ctx->Attr<std::vector<int64_t>>("padding");
+private:
+  void Compute(user_op::KernelComputeContext *ctx) const override {
+    const Tensor *x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    Tensor *y = ctx->Tensor4ArgNameAndIndex("y", 0);
+    const auto &padding = ctx->Attr<std::vector<int64_t>>("padding");
     const int64_t ndims = x->shape().NumAxes();
     CHECK_EQ(padding.size(), ndims);
     const int64_t n_idx = 0;
@@ -71,29 +73,29 @@ class ReflectionPad2dKernel final : public OpKernel {
     const int64_t x_height = x->shape().At(h_idx);
     const int64_t x_width = x->shape().At(w_idx);
 
-    IN_T* dest = y->mut_dptr<IN_T>();
-    const IN_T* src = x->dptr<IN_T>();
+    IN_T *dest = y->mut_dptr<IN_T>();
+    const IN_T *src = x->dptr<IN_T>();
     DimVector y_vector = ShapeViewToDimVector(y->shape());
     NdIndexOffsetHelper<int64_t, 4> index_helper(y_vector.data());
 
-    ReflectionPad2dFunctor<device_type, IN_T>()(ctx->device_ctx(), src, dest, index_helper, n_batch,
-                                                n_channel, y_height, y_width, x_height, x_width,
-                                                pad_left, pad_top);
+    ReflectionPad2dFunctor<device_type, IN_T>()(
+        ctx->device_ctx(), src, dest, index_helper, n_batch, n_channel,
+        y_height, y_width, x_height, x_width, pad_left, pad_top);
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-template<DeviceType device_type, typename IN_T>
+template <DeviceType device_type, typename IN_T>
 class ReflectionPad2dGradKernel final : public OpKernel {
- public:
+public:
   ReflectionPad2dGradKernel() = default;
   ~ReflectionPad2dGradKernel() = default;
 
- private:
-  void Compute(KernelComputeContext* ctx) const override {
-    const Tensor* dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
-    Tensor* dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
-    const auto& padding = ctx->Attr<std::vector<int64_t>>("padding");
+private:
+  void Compute(KernelComputeContext *ctx) const override {
+    const Tensor *dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
+    Tensor *dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
+    const auto &padding = ctx->Attr<std::vector<int64_t>>("padding");
     const int64_t ndims = dy->shape().NumAxes();
     CHECK_EQ(padding.size(), ndims);
 
@@ -111,35 +113,37 @@ class ReflectionPad2dGradKernel final : public OpKernel {
     int64_t dx_height = dx->shape().At(h_idx);
     int64_t dx_width = dx->shape().At(w_idx);
 
-    const IN_T* src = dy->dptr<IN_T>();
-    IN_T* dest = dx->mut_dptr<IN_T>();
+    const IN_T *src = dy->dptr<IN_T>();
+    IN_T *dest = dx->mut_dptr<IN_T>();
     DimVector dy_vector = ShapeViewToDimVector(dy->shape());
     NdIndexOffsetHelper<int64_t, 4> index_helper(dy_vector.data());
 
-    size_t out_bytes_size = dx->shape().elem_cnt() * GetSizeOfDataType(dx->data_type());
+    size_t out_bytes_size =
+        dx->shape().elem_cnt() * GetSizeOfDataType(dx->data_type());
     Memset<device_type>(ctx->device_ctx(), dest, 0, out_bytes_size);
 
-    ReflectionPad2dGradFunctor<device_type, IN_T>()(ctx->device_ctx(), src, dest, index_helper,
-                                                    n_batch, n_channel, dy_height, dy_width,
-                                                    dx_height, dx_width, pad_left, pad_top);
+    ReflectionPad2dGradFunctor<device_type, IN_T>()(
+        ctx->device_ctx(), src, dest, index_helper, n_batch, n_channel,
+        dy_height, dy_width, dx_height, dx_width, pad_left, pad_top);
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
+#define REGISTER_REFLECTION_PAD2D_KERNELS(device, dtype)                       \
+  REGISTER_USER_KERNEL("reflection_pad2d")                                     \
+      .SetCreateFn<ReflectionPad2dKernel<device, dtype>>()                     \
+      .SetIsMatchedHob(                                                        \
+          (user_op::HobDeviceTag() == device) &                                \
+          (user_op::HobDataType("y", 0) == GetDataType<dtype>::value));        \
+  REGISTER_USER_KERNEL("reflection_pad2d_grad")                                \
+      .SetCreateFn<ReflectionPad2dGradKernel<device, dtype>>()                 \
+      .SetIsMatchedHob(                                                        \
+          (user_op::HobDeviceTag() == device) &                                \
+          (user_op::HobDataType("dx", 0) == GetDataType<dtype>::value));
 
-#define REGISTER_REFLECTION_PAD2D_KERNELS(device, dtype)                               \
-  REGISTER_USER_KERNEL("reflection_pad2d")                                             \
-      .SetCreateFn<ReflectionPad2dKernel<device, dtype>>()                             \
-      .SetIsMatchedHob((user_op::HobDeviceTag() == device)                             \
-                       & (user_op::HobDataType("y", 0) == GetDataType<dtype>::value)); \
-  REGISTER_USER_KERNEL("reflection_pad2d_grad")                                        \
-      .SetCreateFn<ReflectionPad2dGradKernel<device, dtype>>()                         \
-      .SetIsMatchedHob((user_op::HobDeviceTag() == device)                             \
-                       & (user_op::HobDataType("dx", 0) == GetDataType<dtype>::value));
-
-#define REGISTER_REFLECTION_PAD2D_WITH_DEVICE(device) \
-  REGISTER_REFLECTION_PAD2D_KERNELS(device, float)    \
-  REGISTER_REFLECTION_PAD2D_KERNELS(device, double)   \
+#define REGISTER_REFLECTION_PAD2D_WITH_DEVICE(device)                          \
+  REGISTER_REFLECTION_PAD2D_KERNELS(device, float)                             \
+  REGISTER_REFLECTION_PAD2D_KERNELS(device, double)                            \
   REGISTER_REFLECTION_PAD2D_KERNELS(device, int32_t)
 
 REGISTER_REFLECTION_PAD2D_WITH_DEVICE(DeviceType::kCPU)
@@ -148,17 +152,17 @@ REGISTER_REFLECTION_PAD2D_WITH_DEVICE(DeviceType::kGPU)
 REGISTER_REFLECTION_PAD2D_KERNELS(DeviceType::kGPU, float16)
 #endif
 
-template<DeviceType device_type, typename IN_T>
+template <DeviceType device_type, typename IN_T>
 class ReplicationPad2dKernel final : public OpKernel {
- public:
+public:
   ReplicationPad2dKernel() = default;
   ~ReplicationPad2dKernel() = default;
 
- private:
-  void Compute(user_op::KernelComputeContext* ctx) const override {
-    const Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
-    Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
-    const auto& padding = ctx->Attr<std::vector<int64_t>>("padding");
+private:
+  void Compute(user_op::KernelComputeContext *ctx) const override {
+    const Tensor *x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    Tensor *y = ctx->Tensor4ArgNameAndIndex("y", 0);
+    const auto &padding = ctx->Attr<std::vector<int64_t>>("padding");
     const int64_t ndims = x->shape().NumAxes();
     CHECK_EQ(padding.size(), ndims);
     const int64_t n_idx = 0;
@@ -176,29 +180,30 @@ class ReplicationPad2dKernel final : public OpKernel {
     const int64_t x_height = x->shape().At(h_idx);
     const int64_t x_width = x->shape().At(w_idx);
 
-    IN_T* dest = y->mut_dptr<IN_T>();
-    const IN_T* src = x->dptr<IN_T>();
-    DimVector y_vector = ShapeViewToDimVector(y->shape());
+    IN_T *dest = y->mut_dptr<IN_T>();
+    const IN_T *src = x->dptr<IN_T>();
+    DimVector dy_vector;
+    dy->shape().ToDimVector(&dy_vector);
     NdIndexOffsetHelper<int64_t, 4> index_helper(y_vector.data());
 
-    ReplicationPad2dFunctor<device_type, IN_T>()(ctx->device_ctx(), src, dest, index_helper, n_batch,
-                                                n_channel, y_height, y_width, x_height, x_width,
-                                                pad_left, pad_top);
+    ReplicationPad2dFunctor<device_type, IN_T>()(
+        ctx->device_ctx(), src, dest, index_helper, n_batch, n_channel,
+        y_height, y_width, x_height, x_width, pad_left, pad_top);
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-template<DeviceType device_type, typename IN_T>
+template <DeviceType device_type, typename IN_T>
 class ReplicationPad2dGradKernel final : public OpKernel {
- public:
+public:
   ReplicationPad2dGradKernel() = default;
   ~ReplicationPad2dGradKernel() = default;
 
- private:
-  void Compute(KernelComputeContext* ctx) const override {
-    const Tensor* dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
-    Tensor* dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
-    const auto& padding = ctx->Attr<std::vector<int64_t>>("padding");
+private:
+  void Compute(KernelComputeContext *ctx) const override {
+    const Tensor *dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
+    Tensor *dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
+    const auto &padding = ctx->Attr<std::vector<int64_t>>("padding");
     const int64_t ndims = dy->shape().NumAxes();
     CHECK_EQ(padding.size(), ndims);
 
@@ -216,34 +221,38 @@ class ReplicationPad2dGradKernel final : public OpKernel {
     int64_t dx_height = dx->shape().At(h_idx);
     int64_t dx_width = dx->shape().At(w_idx);
 
-    const IN_T* src = dy->dptr<IN_T>();
-    IN_T* dest = dx->mut_dptr<IN_T>();
-    DimVector dy_vector = ShapeViewToDimVector(dy->shape());
+    const IN_T *src = dy->dptr<IN_T>();
+    IN_T *dest = dx->mut_dptr<IN_T>();
+    DimVector dy_vector;
+    dy->shape().ToDimVector(&dy_vector);
     NdIndexOffsetHelper<int64_t, 4> index_helper(dy_vector.data());
 
-    size_t out_bytes_size = dx->shape().elem_cnt() * GetSizeOfDataType(dx->data_type());
+    size_t out_bytes_size =
+        dx->shape().elem_cnt() * GetSizeOfDataType(dx->data_type());
     Memset<device_type>(ctx->device_ctx(), dest, 0, out_bytes_size);
 
-    ReplicationPad2dGradFunctor<device_type, IN_T>()(ctx->device_ctx(), src, dest, index_helper,
-                                                    n_batch, n_channel, dy_height, dy_width,
-                                                    dx_height, dx_width, pad_left, pad_top);
+    ReplicationPad2dGradFunctor<device_type, IN_T>()(
+        ctx->device_ctx(), src, dest, index_helper, n_batch, n_channel,
+        dy_height, dy_width, dx_height, dx_width, pad_left, pad_top);
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_REPLICATION_PAD2D_KERNELS(device, dtype)                               \
-  REGISTER_USER_KERNEL("replication_pad2d")                                             \
-      .SetCreateFn<ReplicationPad2dKernel<device, dtype>>()                             \
-      .SetIsMatchedHob((user_op::HobDeviceTag() == device)                             \
-                       & (user_op::HobDataType("y", 0) == GetDataType<dtype>::value)); \
-  REGISTER_USER_KERNEL("replication_pad2d_grad")                                        \
-      .SetCreateFn<ReplicationPad2dGradKernel<device, dtype>>()                         \
-      .SetIsMatchedHob((user_op::HobDeviceTag() == device)                             \
-                       & (user_op::HobDataType("dx", 0) == GetDataType<dtype>::value));
+#define REGISTER_REPLICATION_PAD2D_KERNELS(device, dtype)                      \
+  REGISTER_USER_KERNEL("replication_pad2d")                                    \
+      .SetCreateFn<ReplicationPad2dKernel<device, dtype>>()                    \
+      .SetIsMatchedHob(                                                        \
+          (user_op::HobDeviceTag() == device) &                                \
+          (user_op::HobDataType("y", 0) == GetDataType<dtype>::value));        \
+  REGISTER_USER_KERNEL("replication_pad2d_grad")                               \
+      .SetCreateFn<ReplicationPad2dGradKernel<device, dtype>>()                \
+      .SetIsMatchedHob(                                                        \
+          (user_op::HobDeviceTag() == device) &                                \
+          (user_op::HobDataType("dx", 0) == GetDataType<dtype>::value));
 
-#define REGISTER_REPLICATION_PAD2D_WITH_DEVICE(device) \
-  REGISTER_REPLICATION_PAD2D_KERNELS(device, float)    \
-  REGISTER_REPLICATION_PAD2D_KERNELS(device, double)   \
+#define REGISTER_REPLICATION_PAD2D_WITH_DEVICE(device)                         \
+  REGISTER_REPLICATION_PAD2D_KERNELS(device, float)                            \
+  REGISTER_REPLICATION_PAD2D_KERNELS(device, double)                           \
   REGISTER_REPLICATION_PAD2D_KERNELS(device, int32_t)
 
 REGISTER_REPLICATION_PAD2D_WITH_DEVICE(DeviceType::kCPU)
@@ -252,5 +261,5 @@ REGISTER_REPLICATION_PAD2D_WITH_DEVICE(DeviceType::kGPU)
 REGISTER_REPLICATION_PAD2D_KERNELS(DeviceType::kGPU, float16)
 #endif
 
-}  // namespace user_op
-}  // namespace oneflow
+} // namespace user_op
+} // namespace oneflow
