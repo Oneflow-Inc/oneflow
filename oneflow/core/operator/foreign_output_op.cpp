e37879f4f0c2414d43121b01ca949c00aef26208
liujuncheng <liujuncheng1022@gmail.com>
2021-02-19 14:49:44 +0800

    fi/fo

--- oneflow/core/operator/foreign_output_op.cpp
+++ oneflow/core/operator/foreign_output_op.cpp
@@ -23,6 +23,13 @@ void ForeignOutputOp::InitFromOpConf() {
   EnrollInputBn("in");
 }
 
+Maybe<void> ForeignOutputOp::InferLogicalOutBlobDescs(
+    const std::function<BlobDesc*(const std::string&)>& BlobDesc4BnInOp,
+    const ParallelDesc& parallel_desc) const {
+  CHECK_EQ_OR_RETURN(parallel_desc.parallel_num(), 1);
+  return Maybe<void>::Ok();
+}
+
 Maybe<void> ForeignOutputOp::InferOutBlobDescs(
     std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
     const ParallelContext* parallel_ctx, const SbpSignature* sbp_signature) const {
