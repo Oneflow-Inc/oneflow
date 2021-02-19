36a3799a5b3db3f3dd53d5674cf4d46b0d334bcd
liujuncheng <liujuncheng1022@gmail.com>
2021-02-19 12:59:10 +0800

    fix

--- oneflow/core/graph/op_graph.cpp
+++ oneflow/core/graph/op_graph.cpp
@@ -462,12 +462,12 @@ Maybe<void> OpGraph::InferLogicalBlobDesc(const Job& job) const {
     }
     InferOpNodeSbpSignature(op_node, sbp_sig_conf);
     op_node->InferBlobParallelDesc();
+    UpdateJobParallelViewConf(*op_node, oba2sbp_identical_obas, &job_parallel_view_conf);
+    JUST(op_node->mut_op()->InferLogicalOutBlobDescsIf());
     for (const auto& bn : op_node->op().output_bns()) {
       *op_node->MutLogicalBlobDesc4Lbi(op_node->op().BnInOp2Lbi(bn)) =
           *JUST(op_node->op().GetLogicalBlobDesc4Obn(bn));
     }
-    UpdateJobParallelViewConf(*op_node, oba2sbp_identical_obas, &job_parallel_view_conf);
-    JUST(op_node->mut_op()->InferLogicalOutBlobDescsIf());
     return Maybe<void>::Ok();
   }));
   return Maybe<void>::Ok();
