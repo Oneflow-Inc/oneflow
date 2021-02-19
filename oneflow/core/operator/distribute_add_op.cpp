ea1da384fb3562e11db7622ff1a75667757b856e
liujuncheng <liujuncheng1022@gmail.com>
2021-02-19 17:24:36 +0800

    DistributeAddOp::InferLogicalOutBlobDescs

--- oneflow/core/operator/distribute_add_op.cpp
+++ oneflow/core/operator/distribute_add_op.cpp
@@ -30,6 +30,9 @@ class DistributeAddOp final : public Operator {
 
   void InitFromOpConf() override;
 
+  Maybe<void> InferLogicalOutBlobDescs(
+      const std::function<BlobDesc*(const std::string&)>& BlobDesc4BnInOp,
+      const ParallelDesc& parallel_desc) const override;
   Maybe<void> InferOutBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                                 const ParallelContext* parallel_ctx,
                                 const SbpSignature* sbp_signature) const override;
@@ -69,6 +72,18 @@ Maybe<void> DistributeAddOp::InferParallelSignature() {
     (*map)[input_bns().Get(i)] =
         Global<ForeignCallback>::Get()->MakeParallelDescSymbol(cfg_in_parallel_conf);
   }
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> DistributeAddOp::InferLogicalOutBlobDescs(
+    const std::function<BlobDesc*(const std::string&)>& BlobDesc4BnInOp,
+    const ParallelDesc& parallel_desc) const {
+  const BlobDesc* in_0 = BlobDesc4BnInOp(input_bns().Get(0));
+  FOR_RANGE(int, i, 1, output_bns().size()) {
+    const BlobDesc* in_i = BlobDesc4BnInOp(input_bns().Get(i));
+    CHECK_OR_RETURN(*in_i == *in_0);
+  }
+  *BlobDesc4BnInOp("out") = *in_0;
   return Maybe<void>::Ok();
 }
 
