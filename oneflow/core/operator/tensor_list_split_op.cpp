b821c1909cc7d84d43c20a301e265160f666cb90
liujuncheng <liujuncheng1022@gmail.com>
2021-02-20 17:00:22 +0800

    TensorListSplitOp::InferLogicalOutBlobDescs

--- oneflow/core/operator/tensor_list_split_op.cpp
+++ oneflow/core/operator/tensor_list_split_op.cpp
@@ -32,6 +32,24 @@ class TensorListSplitOp final : public Operator {
     });
   }
 
+  Maybe<void> InferLogicalOutBlobDescs(
+      const std::function<BlobDesc*(const std::string&)>& BlobDesc4BnInOp,
+      const ParallelDesc& parallel_desc) const override {
+    const BlobDesc* in_desc = BlobDesc4BnInOp(SoleIbn());
+    CHECK_OR_RETURN(in_desc->is_tensor_list());
+    CHECK_GT_OR_RETURN(in_desc->shape().NumAxes(), 1);
+    const int64_t N = in_desc->shape().At(0);
+    CHECK_EQ_OR_RETURN(N, output_bns().size());
+    DimVector dim_vec{in_desc->shape().dim_vec().begin() + 1, in_desc->shape().dim_vec().end()};
+    FOR_RANGE(int, i, 0, N) {
+      BlobDesc* out_i = BlobDesc4BnInOp(output_bns().Get(i));
+      out_i->mut_shape() = Shape(dim_vec);
+      out_i->set_data_type(in_desc->data_type());
+      out_i->set_is_dynamic(true);
+    }
+    return Maybe<void>::Ok();
+  }
+
   Maybe<void> InferOutBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                                 const ParallelContext* parallel_ctx,
                                 const SbpSignature* sbp_signature) const override {
