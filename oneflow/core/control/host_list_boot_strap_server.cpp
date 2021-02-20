c370830355048f01d22aa806089b7b635aef6ca2
clackhan <han_binbin@163.com>
2021-02-20 11:55:01 +0800

    add HostListBootStrapServer
    Former-commit-id: 590c136b92a08d1b326429526152286cbf973481

---
+++ oneflow/core/control/host_list_boot_strap_server.cpp
@@ -0,0 +1,54 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "oneflow/core/control/host_list_boot_strap_server.h"
+#include "oneflow/core/actor/act_event_logger.h"
+#include "oneflow/core/job/profiler.h"
+#include "oneflow/core/job/env_desc.h"
+#include "grpc/grpc_posix.h"
+
+namespace oneflow {
+
+HostListBootStrapServer::HostListBootStrapServer()
+    : RpcServer(), is_first_connect_(true), this_machine_addr_("") {
+  Init();
+  int port = Global<EnvDesc>::Get()->ctrl_port();
+  grpc::ServerBuilder server_builder;
+  server_builder.SetMaxMessageSize(INT_MAX);
+  int bound_port = 0;
+  server_builder.AddListeningPort("0.0.0.0:" + std::to_string(port),
+                                  grpc::InsecureServerCredentials(), &bound_port);
+  grpc_service_.reset(new CtrlService::AsyncService);
+  server_builder.RegisterService(grpc_service_.get());
+  cq_ = server_builder.AddCompletionQueue();
+  grpc_server_ = server_builder.BuildAndStart();
+  CHECK_EQ(port, bound_port) << "Port " << port << " is unavailable";
+  LOG(INFO) << "HostListBootStrapServer listening on "
+            << "0.0.0.0:" + std::to_string(port);
+  loop_thread_ = std::thread(&HostListBootStrapServer::HandleRpcs, this);
+}
+
+void HostListBootStrapServer::OnLoadServer(CtrlCall<CtrlMethod::kLoadServer>* call) {
+  if (this->is_first_connect_) {
+    this->this_machine_addr_ = call->request().addr();
+    this->is_first_connect_ = false;
+  } else {
+    CHECK_EQ(call->request().addr(), this->this_machine_addr_);
+  }
+  call->SendResponse();
+  EnqueueRequest<CtrlMethod::kLoadServer>();
+}
+
+}  // namespace oneflow
