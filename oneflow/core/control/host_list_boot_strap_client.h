79b592e5649a4a04fb48bf8b37584e4034438692
clackhan <han_binbin@163.com>
2021-02-20 11:54:18 +0800

    add HostListBootStrapClient
    Former-commit-id: 257f49f2709f1392a45f4edfc23c3c531dda835a

---
+++ oneflow/core/control/host_list_boot_strap_client.h
@@ -0,0 +1,74 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef ONEFLOW_CORE_CONTROL_HOST_LIST_BOOT_STRAP_CLIENT_H_
+#define ONEFLOW_CORE_CONTROL_HOST_LIST_BOOT_STRAP_CLIENT_H_
+
+#include "oneflow/core/control/rpc_client.h"
+
+namespace oneflow {
+
+class HostListBootStrapClient final : public RpcClient {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(HostListBootStrapClient);
+  ~HostListBootStrapClient() override = default;
+
+ private:
+  friend class Global<HostListBootStrapClient>;
+  HostListBootStrapClient();
+};
+
+#define FILE_LINE_STR __FILE__ ":" OF_PP_STRINGIZE(__LINE__)
+
+#define OF_ENV_BARRIER() Global<HostListBootStrapClient>::Get()->Barrier(FILE_LINE_STR)
+#define OF_SESSION_BARRIER()                       \
+  Global<HostListBootStrapClient>::Get()->Barrier( \
+      FILE_LINE_STR, Global<ResourceDesc, ForSession>::Get()->TotalMachineNum())
+
+static void OfCallOnce(const std::string& name, std::function<void()> f) {
+  TryLockResult lock_ret = Global<HostListBootStrapClient>::Get()->TryLock(name);
+  if (lock_ret == TryLockResult::kLocked) {
+    f();
+    Global<HostListBootStrapClient>::Get()->NotifyDone(name);
+  } else if (lock_ret == TryLockResult::kDone) {
+  } else if (lock_ret == TryLockResult::kDoing) {
+    Global<HostListBootStrapClient>::Get()->WaitUntilDone(name);
+  } else {
+    UNIMPLEMENTED();
+  }
+}
+
+template<typename Self, typename F, typename Arg, typename... Args>
+static void OfCallOnce(const std::string& name, Self self, F f, Arg&& arg, Args&&... args) {
+  std::function<void()> fn =
+      std::bind(f, self, std::forward<Arg>(arg), std::forward<Args>(args)...);
+  OfCallOnce(name, std::move(fn));
+}
+
+template<typename Self, typename F>
+static void OfCallOnce(const std::string& name, Self self, F f) {
+  std::function<void()> fn = std::bind(f, self, name);
+  OfCallOnce(name, std::move(fn));
+}
+
+template<typename F, typename Arg, typename... Args>
+static void OfCallOnce(const std::string& name, F f, Arg&& arg, Args&&... args) {
+  std::function<void()> fn = std::bind(f, std::forward<Arg>(arg), std::forward<Args>(args)...);
+  OfCallOnce(name, std::move(fn));
+}
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_CONTROL_HOST_LIST_BOOT_STRAP_CLIENT_H_
