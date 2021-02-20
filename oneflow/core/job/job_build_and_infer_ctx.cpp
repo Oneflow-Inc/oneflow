26f2207a1b6cbebaf38a505d559a90484913aeb8
scxfjiang <sc.xfjiang@gmail.com>
2021-02-20 12:57:31 +0800

    fix job build and infer ctx

--- oneflow/core/job/job_build_and_infer_ctx.cpp
+++ oneflow/core/job/job_build_and_infer_ctx.cpp
@@ -939,8 +939,7 @@ Maybe<void> LazyJobBuildAndInferCtx::Complete() {
   CHECK_NOTNULL(Global<JobDesc>::Get());
   Global<JobDesc>::Delete();
   if (job().job_conf().has_train_conf()) {
-    CHECK_OR_RETURN(job().job_conf().train_conf().has_model_update_conf());
-    CHECK_OR_RETURN(job().job_conf().train_conf().has_primary_lr());
+    CHECK_GT_OR_RETURN(job().job_conf().train_conf().optimizer_conf_size(), 0);
   }
   auto scope = std::make_unique<GlobalJobDescScope>(mut_job()->job_conf(), job_id());
   JobPassCtx job_pass_ctx(GlobalJobDesc());
