b71732b8bbc9a491890caa65e20d5c4117b4980c
liujuncheng <liujuncheng1022@gmail.com>
2021-02-20 09:58:06 +0800

    InferLogicalOutBlobDescs

--- oneflow/core/operator/broadcast_to_compatible_with_op.cpp
+++ oneflow/core/operator/broadcast_to_compatible_with_op.cpp
@@ -50,6 +50,22 @@ class BroadcastToCompatibleWithOp final : public Operator {
     EnrollOutputBn("y");
   }
 
+  Maybe<void> InferLogicalOutBlobDescs(
+      const std::function<BlobDesc*(const std::string&)>& BlobDesc4BnInOp,
+      const ParallelDesc& parallel_desc) const override {
+    int64_t num_compatibles = op_conf().broadcast_to_compatible_with_conf().compatible_size();
+    const BlobDesc* x_desc = BlobDesc4BnInOp("x");
+    Shape broadcasted_shape(x_desc->shape());
+    FOR_RANGE(int64_t, i, 0, num_compatibles) {
+      const BlobDesc* compatible_i = BlobDesc4BnInOp(GenRepeatedBn("compatible", i));
+      GetBroadcastShape(broadcasted_shape, compatible_i->shape(), &broadcasted_shape);
+    }
+    BlobDesc* y_desc = BlobDesc4BnInOp("y");
+    y_desc->CopyFrom(*x_desc);
+    y_desc->mut_shape() = broadcasted_shape;
+    return Maybe<void>::Ok();
+  }
+
   Maybe<void> InferOutBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                                 const ParallelContext* parallel_ctx,
                                 const SbpSignature* sbp_signature) const override {
