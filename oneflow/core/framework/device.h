231c40c77aa6252f47310a480eaa7bb467a9a390
poohRui <yuruil@qq.com>
2021-02-23 07:14:26 +0000

    refactor device

--- oneflow/core/framework/device.h
+++ oneflow/core/framework/device.h
@@ -13,25 +13,28 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#ifndef ONEFLOW_FRAMEWORK_CORE_DEVICE_H_
-#define ONEFLOW_FRAMEWORK_CORE_DEVICE_H_
+#ifndef ONEFLOW_CORE_FRAMEWORK_DEVICE_H_
+#define ONEFLOW_CORE_FRAMEWORK_DEVICE_H_
 
 #include "oneflow/core/common/device_type.pb.h"
 
 namespace oneflow {
-namespace one {
+
 class Device {
  public:
   Device(const std::string& type, int64_t device_id)
       : type_(type), device_id_(device_id) {}
+  Device(const Device&) = default;
+  Device(Device&&) = default;
+  ~Device() = default;
   std::string type() const { return type_; }
   int64_t device_id() const { return device_id_; }
 
  private:
-  std::string type_;
-  int64_t device_id_;
+  const std::string type_;
+  const int64_t device_id_;
 };
-}  // namespace one
+
 }  // namespace oneflow
 #endif
 
