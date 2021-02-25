a66b20e8b501d13b1405b35e3b1eee4075e0a060
binbinHan <han_binbin@163.com>
2021-02-26 01:37:49 +0800

    Mig id util and scope util (#4217)
    * mig parallel_conf_util
    
    * mig BuildInitialScope BuildScopeWithNewParallelDesc BuildScopeWithNewParallelConf
    
    * add test of GetDeviceTagAndMachineDeviceIds
    
    * mig GetOpConfSymbol
    
    * fix BuildScopeWithNewParallelDesc input type error
    
    * use TRY
    
    * use symbol::Storage<OperatorConfSymbol>
    
    * _NewOpKernelObject
    
    * mig OpKernelObject
    
    * mig object_storage
    
    * make of_format
    
    * del comment
    
    * std::function<void(Object*)
    
    * mig NewOpKernelObject and _StatefulCallOpKernel
    
    * mig _StatefulCallOpKernel and GetSharedOpKernelObject4ParallelConfSymbol
    
    * del object_storage.cpp
    
    * use name GLOBAL_PARA_SYM2SHARED_OPKENEL_OBJ_MUTEX
    
    * mig CheckRefInBlobObjectParallelDesc and  OperandBlobObjects rel api
    
    * mig _StatelessCall
    
    * mig _StatelessCall
    
    * del comment
    
    * mig id_util and scope_util
    
    * use cfg_op_conf and Object*
    
    * use Object*
    
    * del _
    
    * fix func name error
    
    * use MapAt and shared_ptr
    
    * use shared_ptr or const ref
    
    * minor fix
    
    * add todo
    
    * minor fix
    
    * minor djustment
    
    * minor fix
    
    * minor fix
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/core/framework/scope_util.h
+++ oneflow/core/framework/scope_util.h
@@ -1,4 +1,4 @@
-"""
+/*
 Copyright 2020 The OneFlow Authors. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,13 +12,23 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-"""
-from __future__ import absolute_import
+*/
+#ifndef ONEFLOW_CORE_FRAMEWORK_SCOPE_UTIL_H_
+#define ONEFLOW_CORE_FRAMEWORK_SCOPE_UTIL_H_
 
-from oneflow.python.oneflow_export import oneflow_export
-import oneflow_api
+#include <vector>
+#include "oneflow/core/job/scope.h"
 
+namespace oneflow {
 
-@oneflow_export("util.unique_str")
-def UniqueStr(prefix):
-    return oneflow_api.UniqueStr(prefix)
+Maybe<Scope> GetCurrentScope();
+
+Maybe<void> InitThreadLocalScopeStack(const std::shared_ptr<Scope>& scope);
+
+Maybe<void> ThreadLocalScopeStackPush(const std::shared_ptr<Scope>& scope);
+
+Maybe<void> ThreadLocalScopeStackPop();
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_FRAMEWORK_SCOPE_UTIL_H_
