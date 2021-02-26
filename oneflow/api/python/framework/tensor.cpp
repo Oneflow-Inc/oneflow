8b5013a26b3efd0b6639b448855b87cc942ac95f
poohRui <yuruil@qq.com>
2021-02-26 15:12:18 +0800

    remove get_dtype interface

--- oneflow/api/python/framework/tensor.cpp
+++ oneflow/api/python/framework/tensor.cpp
@@ -68,6 +68,7 @@ ONEFLOW_API_PYBIND11_MODULE("", m) {
       .def_property_readonly("device", &MirroredTensor::device)
       .def_property_readonly("ndim", &MirroredTensor::ndim)
       .def_property_readonly("is_cuda", &MirroredTensor::is_cuda)
+      .def_property_readonly("dtype", &MirroredTensor::dtype)
       .def_property_readonly("data", &MirroredTensor::data)
       .def_property_readonly("grad", &MirroredTensor::grad)
       .def_property_readonly("grad_fn", &MirroredTensor::grad_fn)
@@ -87,8 +88,6 @@ ONEFLOW_API_PYBIND11_MODULE("", m) {
       .def("__repr__", &MirroredTensor::ToString)
       .def("__array__", &MirroredTensor::ToArray)
       .def("__sizeof__", &MirroredTensor::SizeOf)
-      .def("get_dtype",
-           [](std::shared_ptr<MirroredTensor>& x) { return static_cast<int>(x->dtype()); });
 
   py::class_<ConsistentTensor, std::shared_ptr<ConsistentTensor>>(m, "ConsistentTensor")
       .def(py::init(&MakeConsistentTensor))
--- oneflow/python/framework/tensor.py
+++ oneflow/python/framework/tensor.py
@@ -19,36 +19,7 @@ from oneflow.python.oneflow_export import oneflow_export
 import oneflow.python.framework.device as oneflow_device
 
 
-@oneflow_export("MirroredTensor")
-class MirroredTensor(oneflow_api.MirroredTensor):
-    def __init__(
-        self,
-        shape: tuple = (),
-        dtype: dtype_util.dtype = dtype_util.float,
-        device: oneflow_device = oneflow_device.Device("cpu"),
-    ):
-        of_dtype = dtype_util.convert_oneflow_dtype_to_proto_dtype(dtype)
-        oneflow_api.MirroredTensor.__init__(self, shape, of_dtype, device)
+oneflow_export("MirroredTensor")(oneflow_api.MirroredTensor)
 
-    @property
-    def dtype(self):
-        return dtype_util.convert_proto_dtype_to_oneflow_dtype(self.get_dtype())
+oneflow_export("ConsistentTensor")(oneflow_api.ConsistentTensor)
 
-
-@oneflow_export("ConsistentTensor")
-class ConsistentTensor(oneflow_api.ConsistentTensor):
-    def __init__(
-        self,
-        shape: tuple = (),
-        dtype: dtype_util.dtype = dtype_util.float,
-        distribute=None,
-        parallel_desc=None,
-    ):
-        of_dtype = dtype_util.convert_oneflow_dtype_to_proto_dtype(dtype)
-        oneflow_api.ConsistentTensor.__init__(
-            self, shape, of_dtype, distribute, parallel_desc
-        )
-
-    @property
-    def dtype(self):
-        return dtype_util.convert_proto_dtype_to_oneflow_dtype(self.get_dtype())
