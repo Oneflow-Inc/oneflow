d4be1c08d7dc2b8460a21feec6b5900cb1285903
lixinqi <lixinqi0703106@163.com>
2021-02-26 15:00:33 +0800

    class DType

---
+++ oneflow/core/framework/dtype.h
@@ -0,0 +1,12 @@
+#ifndef ONEFLOW_CORE_FRAMEWORK_DTYPE_H_
+#define ONEFLOW_CORE_FRAMEWORK_DTYPE_H_
+
+namespace oneflow {
+
+class final DType {
+ public:
+};
+
+}
+
+#endif  // ONEFLOW_CORE_FRAMEWORK_DTYPE_H_
