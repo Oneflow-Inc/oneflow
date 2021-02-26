63bf521187d419675090c62607fe94317ad0ae5d
iamyf <yf88888888@126.com>
2021-02-26 13:59:08 +0800

    Yangfei/constant pad2d (#4249)
    * add constant_pad2d op, kernels
    
    * test code for constant pad2d
    
    * fix op name bug
    
    * reformat
    
    * move some test functions to utils
    
    * reformat test files
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>
    Former-commit-id: 7e880ff22470de1b1d625f25d0ef05e137c9f105

--- oneflow/user/kernels/pad2d_kernels_util.cu
+++ oneflow/user/kernels/pad2d_kernels_util.cu
@@ -202,6 +202,97 @@ OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_REPLICATION_PAD2D_FUNCTOR,
 OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_REPLICATION_PAD2D_GRAD_FUNCTOR,
                                  OF_PP_MAKE_TUPLE_SEQ(DeviceType::kGPU), PADDING_DATA_TYPE_GPU_SEQ);
 
+template<typename IN_T>
+__global__ void DoCUDAConstantPad2d(const IN_T *src, IN_T *dest,
+                                    const NdIndexOffsetHelper<int64_t, 4> index_helper,
+                                    int64_t elem_num, int64_t src_num, int64_t dest_num,
+                                    int64_t y_height, int64_t y_width, int64_t x_height,
+                                    int64_t x_width, int64_t pad_left, int64_t pad_top,
+                                    const IN_T const_value) {
+  DoConstantPad2d<IN_T>(src, dest, index_helper, elem_num, src_num, dest_num, y_height, y_width,
+                        x_height, x_width, pad_left, pad_top, const_value);
+};
+
+template<typename IN_T>
+__global__ void DoCUDAConstantPad2dGrad(const IN_T *src, IN_T *dest,
+                                        const NdIndexOffsetHelper<int64_t, 4> index_helper,
+                                        int64_t elem_num, int64_t src_num, int64_t dest_num,
+                                        int64_t dy_height, int64_t dy_width, int64_t dx_height,
+                                        int64_t dx_width, int64_t pad_left, int64_t pad_top) {
+  DoConstantPad2dGrad<IN_T>(src, dest, index_helper, elem_num, src_num, dest_num, dy_height,
+                            dy_width, dx_height, dx_width, pad_left, pad_top);
+};
+
+template<typename IN_T>
+struct ConstantPad2dFunctor<DeviceType::kGPU, IN_T> final {
+  void operator()(DeviceCtx *ctx, const IN_T *src, IN_T *dest,
+                  const NdIndexOffsetHelper<int64_t, 4> &index_helper, int64_t n_batch,
+                  int64_t n_channel, int64_t y_height, int64_t y_width, int64_t x_height,
+                  int64_t x_width, int64_t pad_left, int64_t pad_top, const IN_T const_value) {
+    int64_t dest_num = n_channel * y_height * y_width;
+    int64_t src_num = n_channel * x_height * x_width;
+    int64_t elem_num = n_batch * dest_num;
+    DoCUDAConstantPad2d<IN_T>
+        <<<BlocksNum4ThreadsNum(elem_num), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            src, dest, index_helper, elem_num, src_num, dest_num, y_height, y_width, x_height,
+            x_width, pad_left, pad_top, const_value);
+  }
+};
+
+// float16 implementation
+template<>
+void ConstantPad2dFunctor<DeviceType::kGPU, float16>::operator()(
+    DeviceCtx *ctx, const float16 *src, float16 *dest,
+    const NdIndexOffsetHelper<int64_t, 4> &index_helper, int64_t n_batch, int64_t n_channel,
+    int64_t y_height, int64_t y_width, int64_t x_height, int64_t x_width, int64_t pad_left,
+    int64_t pad_top, const float16 const_value) {
+  int64_t dest_num = n_channel * y_height * y_width;
+  int64_t src_num = n_channel * x_height * x_width;
+  int64_t elem_num = n_batch * dest_num;
+  DoCUDAConstantPad2d<half>
+      <<<BlocksNum4ThreadsNum(elem_num), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+          reinterpret_cast<const half *>(src), reinterpret_cast<half *>(dest), index_helper,
+          elem_num, src_num, dest_num, y_height, y_width, x_height, x_width, pad_left, pad_top,
+          static_cast<const half>(const_value));
+}
+
+template<typename IN_T>
+struct ConstantPad2dGradFunctor<DeviceType::kGPU, IN_T> final {
+  void operator()(DeviceCtx *ctx, const IN_T *src, IN_T *dest,
+                  const NdIndexOffsetHelper<int64_t, 4> &index_helper, int64_t n_batch,
+                  int64_t n_channel, int64_t dy_height, int64_t dy_width, int64_t dx_height,
+                  int64_t dx_width, int64_t pad_left, int64_t pad_top) {
+    int64_t dest_num = n_channel * dx_height * dx_width;
+    int64_t src_num = n_channel * dy_height * dy_width;
+    int64_t elem_num = n_batch * src_num;
+    DoCUDAConstantPad2dGrad<IN_T>
+        <<<BlocksNum4ThreadsNum(elem_num), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+            src, dest, index_helper, elem_num, src_num, dest_num, dy_height, dy_width, dx_height,
+            dx_width, pad_left, pad_top);
+  }
+};
+
+// float16 implementation
+template<>
+void ConstantPad2dGradFunctor<DeviceType::kGPU, float16>::operator()(
+    DeviceCtx *ctx, const float16 *src, float16 *dest,
+    const NdIndexOffsetHelper<int64_t, 4> &index_helper, int64_t n_batch, int64_t n_channel,
+    int64_t dy_height, int64_t dy_width, int64_t dx_height, int64_t dx_width, int64_t pad_left,
+    int64_t pad_top) {
+  int64_t dest_num = n_channel * dx_height * dx_width;
+  int64_t src_num = n_channel * dy_height * dy_width;
+  int64_t elem_num = n_batch * src_num;
+  DoCUDAConstantPad2dGrad<half>
+      <<<BlocksNum4ThreadsNum(elem_num), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
+          reinterpret_cast<const half *>(src), reinterpret_cast<half *>(dest), index_helper,
+          elem_num, src_num, dest_num, dy_height, dy_width, dx_height, dx_width, pad_left, pad_top);
+}
+
+OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_CONSTANT_PAD2D_FUNCTOR,
+                                 OF_PP_MAKE_TUPLE_SEQ(DeviceType::kGPU), PADDING_DATA_TYPE_GPU_SEQ);
+
+OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(INSTANTIATE_CONSTANT_PAD2D_GRAD_FUNCTOR,
+                                 OF_PP_MAKE_TUPLE_SEQ(DeviceType::kGPU), PADDING_DATA_TYPE_GPU_SEQ);
 }  // namespace user_op
 }  // namespace oneflow
 
