150e21546bed675854c4565bb50d30eb36e4adc8
leaves-zwx <kunta0932@gmail.com>
2021-02-26 23:37:03 +0800

    refine id structs
    Former-commit-id: 8a978e707a8bbbbbecf88a3185583ef4602c41f0

--- oneflow/core/common/id_util.h
+++ oneflow/core/common/id_util.h
@@ -23,109 +23,131 @@ limitations under the License.
 namespace oneflow {
 
 // TaskId encode (may be extended to 128 bit in future)
-// | ----------------------------------- 64 bit ----------------------------------------- |
-// | ----------- 19 ----------- | ------------------- 24 ------------------- | --- 21 --- |
-// |         ProcessId          |                  StreamId                  |            |
-// | --- 12 --- | ----- 7 ----- | ---- 5 ---- | ----- 7 ----- | ---- 12 ---- | --- 21 --- |
-// | node_index | process_index | device_type | device_index  | stream_index | task_index |
+// | node_index | process_index |                                                         |
+// | --- 12 --- | ----- 7 ----- |                                                         |
+// |         ProcessId          | device_type | device_index  |                           |
+// | ----------- 19 ----------- | ---- 5 ---- | ----- 7 ----- |                           |
+// |                        DeviceId                          | stream_index |            |
+// | ------------------------- 31 --------------------------- | ---- 12 ---- |            |
+// |                               StreamId                                  | task_index |
+// | -------------------------------- 43 ----------------------------------- | --- 21 --- |
 // |                                      TaskId                                          |
+// | ----------------------------------- 64 bit ----------------------------------------- |
+
+using node_index_t = uint32_t;
+using process_index_t = uint32_t;
+using device_index_t = uint32_t;
+using stream_index_t = uint32_t;
+using task_index_t = uint32_t;
+constexpr int kCPUDeviceIndex = 0;
 
 class ProcessId {
  public:
-  ProcessId(uint32_t node_index, uint32_t process_index);
-  uint32_t node_index() const;
-  uint32_t process_index() const;
-  bool operator==(const ProcessId& rhs) const { return val_ == rhs.val_; }
-  bool operator!=(const ProcessId& rhs) const { return !(*this == rhs); }
-  size_t hash() const { return std::hash<underlying_t>{}(val_); }
-
- private:
-  using underlying_t = uint32_t;
   constexpr static int kNodeIndexBits = 12;
   constexpr static int kProcessIndexBits = 7;
-  constexpr static int kBits = kNodeIndexBits + kProcessIndexBits;
-  constexpr static int kFullBits = sizeof(underlying_t) * CHAR_BIT;
-  static_assert(kBits <= kFullBits, "ProcessId bits layout is illegal");
-  constexpr static int kReservedBits = kFullBits - kBits;
+  constexpr static node_index_t kMaxNodeIndex =
+      (static_cast<node_index_t>(1) << kNodeIndexBits) - 1;
+  constexpr static process_index_t kMaxProcessIndex =
+      (static_cast<process_index_t>(1) << kProcessIndexBits) - 1;
 
-  friend class TaskId;
+  ProcessId(node_index_t node_index, process_index_t process_index)
+      : node_index_(node_index), process_index_(process_index) {
+    CHECK_LE(node_index, kMaxNodeIndex);
+    CHECK_LE(process_index, kMaxProcessIndex);
+  }
+  node_index_t node_index() const { return node_index_; }
+  process_index_t process_index() const { return process_index_; }
+  bool operator==(const ProcessId& rhs) const {
+    return node_index_ == rhs.node_index_ && process_index_ == rhs.process_index_;
+  }
+  bool operator!=(const ProcessId& rhs) const { return !(*this == rhs); }
+  size_t hash() const {
+    return std::hash<node_index_t>{}(node_index_) ^ std::hash<process_index_t>{}(process_index_);
+  }
 
-  underlying_t val_;
+ private:
+  node_index_t node_index_;
+  process_index_t process_index_;
 };
 
 class DeviceId {
  public:
-  DeviceId(DeviceType device_type, uint32_t device_index);
-  DeviceType device_type() const;
-  uint32_t device_index() const;
-  bool operator==(const DeviceId& rhs) const { return val_ == rhs.val_; }
-  bool operator!=(const DeviceId& rhs) const { return !(*this == rhs); }
-  size_t hash() const { return std::hash<underlying_t>{}(val_); }
-
- private:
-  using underlying_t = uint32_t;
   constexpr static int kDeviceTypeBits = 5;
   constexpr static int kDeviceIndexBits = 7;
-  constexpr static int kBits = kDeviceTypeBits + kDeviceIndexBits;
-  constexpr static int kFullBits = sizeof(underlying_t) * CHAR_BIT;
-  static_assert(kBits <= kFullBits, "DeviceId bits layout is illegal");
-  constexpr static int kReservedBits = kFullBits - kBits;
+  constexpr static int kMaxDeviceTypeVal = (static_cast<int>(1) << kDeviceTypeBits) - 1;
+  constexpr static device_index_t kMaxDeviceIndex =
+      (static_cast<device_index_t>(1) << kDeviceIndexBits) - 1;
 
-  friend class StreamId;
+  DeviceId(const ProcessId& process_id, DeviceType device_type, device_index_t device_index)
+      : process_id_(process_id), device_type_(device_type), device_index_(device_index) {
+    CHECK_LE(static_cast<int>(device_type), kMaxDeviceTypeVal);
+    CHECK_LE(device_index, kMaxDeviceIndex);
+  }
+  const ProcessId& process_id() const { return process_id_; }
+  DeviceType device_type() const { return device_type_; }
+  device_index_t device_index() const { return device_index_; }
+  bool operator==(const DeviceId& rhs) const {
+    return process_id_ == rhs.process_id_ && device_type_ == rhs.device_type_
+           && device_index_ == rhs.device_index_;
+  }
+  bool operator!=(const DeviceId& rhs) const { return !(*this == rhs); }
+  size_t hash() const {
+    return process_id_.hash() ^ std::hash<int>{}(static_cast<int>(device_type_))
+           ^ std::hash<device_index_t>{}(device_index_);
+  }
 
-  underlying_t val_;
+ private:
+  ProcessId process_id_;
+  DeviceType device_type_;
+  device_index_t device_index_;
 };
 
 class StreamId {
  public:
-  StreamId(DeviceId device_id, uint32_t stream_index);
-  StreamId(DeviceType device_type, uint32_t device_index, uint32_t stream_index);
-  DeviceId device_id() const;
-  DeviceType device_type() const;
-  uint32_t device_index() const;
-  uint32_t stream_index() const;
-  bool operator==(const StreamId& rhs) const { return val_ == rhs.val_; }
+  constexpr static int kStreamIndexBits = 12;
+  constexpr static stream_index_t kMaxStreamIndex =
+      (static_cast<stream_index_t>(1) << kStreamIndexBits) - 1;
+
+  StreamId(const DeviceId& device_id, stream_index_t stream_index)
+      : device_id_(device_id), stream_index_(stream_index) {
+    CHECK_LE(stream_index, kMaxStreamIndex);
+  }
+  const DeviceId& device_id() const { return device_id_; }
+  stream_index_t stream_index() const { return stream_index_; }
+  bool operator==(const StreamId& rhs) const {
+    return device_id_ == rhs.device_id_ && stream_index_ == rhs.stream_index_;
+  }
   bool operator!=(const StreamId& rhs) const { return !(*this == rhs); }
-  size_t hash() const { return std::hash<underlying_t>{}(val_); }
+  size_t hash() const { return device_id_.hash() ^ std::hash<stream_index_t>{}(stream_index_); }
 
  private:
-  using underlying_t = uint32_t;
-  constexpr static int kDeviceIdBits = DeviceId::kBits;
-  constexpr static int kDeviceTypeBits = DeviceId::kDeviceTypeBits;
-  constexpr static int kDeviceIndexBits = DeviceId::kDeviceIndexBits;
-  constexpr static int kStreamIndexBits = 12;
-  constexpr static int kBits = kDeviceIdBits + kStreamIndexBits;
-  constexpr static int kFullBits = sizeof(underlying_t) * CHAR_BIT;
-  static_assert(kBits <= kFullBits, "StreamId bits layout is illegal");
-  constexpr static int kReservedBits = kFullBits - kBits;
-
-  friend class TaskId;
-
-  underlying_t val_;
+  DeviceId device_id_;
+  stream_index_t stream_index_;
 };
 
 class TaskId {
  public:
-  TaskId(ProcessId process_id, StreamId stream_id, uint32_t task_index);
-  TaskId(uint64_t global_stream_index, uint32_t task_index);
-  ProcessId process_id() const;
-  StreamId stream_id() const;
-  uint64_t global_stream_index() const;
-  uint32_t task_index() const;
-  bool operator==(const TaskId& rhs) const { return val_ == rhs.val_; }
+  const static int kTaskIndexBits = 21;
+  constexpr static task_index_t kMaxTaskIndex =
+      (static_cast<task_index_t>(1) << kTaskIndexBits) - 1;
+
+  TaskId(const StreamId& stream_id, task_index_t task_index)
+      : stream_id_(stream_id), task_index_(task_index) {
+    CHECK_LE(task_index_, kMaxTaskIndex);
+  }
+  // TaskId(uint64_t global_stream_index, uint32_t task_index);
+  const StreamId& stream_id() const { return stream_id_; }
+  // uint64_t global_stream_index() const;
+  task_index_t task_index() const { return task_index_; }
+  bool operator==(const TaskId& rhs) const {
+    return stream_id_ == rhs.stream_id_ && task_index_ == rhs.task_index_;
+  }
   bool operator!=(const TaskId& rhs) const { return !(*this == rhs); }
-  size_t hash() const { return std::hash<underlying_t>{}(val_); }
+  size_t hash() const { return stream_id_.hash() ^ std::hash<task_index_t>{}(task_index_); }
 
  private:
-  using underlying_t = uint64_t;
-  const static int kProcessIdBits = ProcessId::kBits;
-  const static int kStreamIdBits = StreamId::kBits;
-  const static int kTaskIndexBits = 21;
-  const static int kBits = kProcessIdBits + kStreamIdBits + kTaskIndexBits;
-  const static int kFullBits = sizeof(underlying_t) * CHAR_BIT;
-  static_assert(kBits == kFullBits, "TaskId bits layout is illegal");
-
-  underlying_t val_;
+  StreamId stream_id_;
+  task_index_t task_index_;
 };
 
 }  // namespace oneflow
