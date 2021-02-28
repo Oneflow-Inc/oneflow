4647cc18be5f10f14530b2d4c015b02efab54b1b
MARD1NO <359521840@qq.com>
2021-02-28 22:58:58 +0800

    change header file name

--- oneflow/user/kernels/activation_kernels.cu
+++ oneflow/user/kernels/activation_kernels.cu
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/user/kernels/activation_ops.h"
+#include "oneflow/user/kernels/activation_kernels.h"
 #include "oneflow/user/kernels/elementwise_xpu_kernel.cuh"
 
 namespace oneflow {
