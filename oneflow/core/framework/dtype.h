46176ebab5f4de2d1110b12057b79348f02f48a3
binbinHan <han_binbin@163.com>
2021-03-01 11:01:28 +0800

    add dtype (#4279)
    * add dtype
    
    * fix error
    
    * make of_format
    
    * use DType of c++
    
    * assign pytorch api
    
    * minor fix
    
    * minor optimize
    
    Former-commit-id: 4e49604fcb39780feab635d59ce6d897caf5e5f2

--- oneflow/core/framework/dtype.h
+++ oneflow/core/framework/dtype.h
@@ -16,10 +16,53 @@ limitations under the License.
 #ifndef ONEFLOW_CORE_FRAMEWORK_DTYPE_H_
 #define ONEFLOW_CORE_FRAMEWORK_DTYPE_H_
 
+#include "oneflow/core/common/maybe.h"
+#include "oneflow/core/common/preprocessor.h"
+#include "oneflow/core/common/data_type.pb.h"
+
 namespace oneflow {
+
+#define DTYPE_SEQ                       \
+  OF_PP_MAKE_TUPLE_SEQ(InvalidDataType) \
+  OF_PP_MAKE_TUPLE_SEQ(Char)            \
+  OF_PP_MAKE_TUPLE_SEQ(Float16)         \
+  OF_PP_MAKE_TUPLE_SEQ(Float)           \
+  OF_PP_MAKE_TUPLE_SEQ(Double)          \
+  OF_PP_MAKE_TUPLE_SEQ(Int8)            \
+  OF_PP_MAKE_TUPLE_SEQ(Int32)           \
+  OF_PP_MAKE_TUPLE_SEQ(Int64)           \
+  OF_PP_MAKE_TUPLE_SEQ(UInt8)           \
+  OF_PP_MAKE_TUPLE_SEQ(OFRecord)        \
+  OF_PP_MAKE_TUPLE_SEQ(TensorBuffer)
 
 class DType final {
  public:
+  DType(const DataType& data_type, const std::string& name, bool is_signed, bool is_floating_point,
+        bool is_complex)
+      : data_type_(data_type),
+        name_(name),
+        is_signed_(is_signed),
+        is_floating_point_(is_floating_point),
+        is_complex_(is_complex) {}
+
+  DataType data_type() const { return data_type_; }
+  bool is_signed() const { return is_signed_; }
+  bool is_complex() const { return is_complex_; }
+  bool is_floating_point() const { return is_floating_point_; }
+  std::string name() const { return name_; }
+
+  static Maybe<DType> GetDTypeByDataType(const DataType&);
+
+#define DEFINE_GET_DATA_TYPE_FUNCTION(data_type) static Maybe<DType> data_type();
+  OF_PP_FOR_EACH_TUPLE(DEFINE_GET_DATA_TYPE_FUNCTION, DTYPE_SEQ)
+#undef DEFINE_GET_DATA_TYPE_FUNCTION
+
+ private:
+  const DataType data_type_;
+  const std::string name_;
+  const bool is_signed_;
+  const bool is_floating_point_;
+  const bool is_complex_;
 };
 
 }  // namespace oneflow
