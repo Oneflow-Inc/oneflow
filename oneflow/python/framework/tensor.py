6ee12c97a51f89e063494b6b58fe6b9a0e851f3b
poohRui <yuruil@qq.com>
2021-03-02 19:21:34 +0800

    python tensor api
    Former-commit-id: 86060b77d461c34fb7803008e8ded58fc7eb680c

--- oneflow/python/framework/tensor.py
+++ oneflow/python/framework/tensor.py
@@ -14,11 +14,159 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import oneflow_api
-import oneflow.python.framework.dtype as dtype_util
 from oneflow.python.oneflow_export import oneflow_export
 import oneflow.python.framework.device as oneflow_device
-
 
 oneflow_export("LocalTensor")(oneflow_api.LocalTensor)
 
 oneflow_export("ConsistentTensor")(oneflow_api.ConsistentTensor)
+
+
+@oneflow_export("tensor")
+class Tensor:
+    def __init__(
+        self,
+        shape,
+        dtype,
+        device=None,
+        placement=None,
+        distribute=None,
+        is_lazy=False,
+        is_consistent=False,
+        is_determined=False,
+        requires_grad=True,
+        retain_grad=False,
+    ):
+        if (is_determined):
+            if (is_consistent):
+                self.consistent_tensor = oneflow_api.ConsistentTensor()
+            else:
+                self.mirrored_tensor = oneflow_api.LocalTensor()
+        else:
+            self.shape = shape
+            self.dtype = dtype
+            self.device = device
+            self.placement = placement
+            self.distribute = distribute
+            self.is_lazy = is_lazy
+            self.is_consistent = is_consistent
+            self.is_determined = is_determined
+            self.requires_grad = requires_grad
+            self.retain_grad = retain_grad
+            self.local_tensor = None
+            self.consistent_tensor = None
+
+    @property
+    def shape(self):
+        return self.shape
+
+    @property
+    def device(self):
+        pass
+
+    @property
+    def ndim(self):
+        pass
+
+    @property
+    def is_cuda(self):
+        pass
+
+    @property
+    def dtype(self):
+        pass
+
+    @property
+    def data(self):
+        pass
+
+    @property
+    def grad(self):
+        pass
+
+    @property
+    def grad_fn(self):
+        pass
+
+    @property
+    def requires_grad(self):
+        pass
+
+    @property
+    def is_leaf(self):
+        pass
+
+    @property
+    def placement(self):
+        pass
+
+    @property
+    def is_lazy(self):
+        pass
+
+    @property
+    def is_consistent(self):
+        pass
+
+    def size(self):
+        pass
+    
+    def dim(self):
+        pass
+
+    def ndimension(self):
+        pass
+
+    def get_device(self):
+        pass
+
+    def nelemenet(self):
+        pass
+
+    def data_ptr(self):
+        pass
+
+    def element_size(self):
+        pass
+
+    def numpy(self):
+        pass
+
+    def tolist(self):
+        pass
+
+    def backward(self):
+        pass
+
+    def set_device(self, device):
+        pass
+
+    def set_placement(self, placement):
+        pass
+
+    def set_distribute(self, distribute):
+        pass
+
+    def set_dtyp(self, dtype):
+        pass
+
+    def set_consistent(self, is_consistent):
+        pass
+
+    def determined(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def __array__(self):
+        pass
+
+    def __sizeof__(self):
+        pass
+
+
+
