75198c1207201c57c435b32536c2f8f85ff50a84
jackalcooper <jackalcooper@gmail.com>
2021-03-02 11:47:51 +0800

    fmt

--- docker/package/manylinux/build_wheel.py
+++ docker/package/manylinux/build_wheel.py
@@ -205,53 +205,29 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--custom_img_tag",
-        type=str,
-        required=False,
-        default=None,
+        "--custom_img_tag", type=str, required=False, default=None,
     )
     parser.add_argument(
-        "--cache_dir",
-        type=str,
-        required=False,
-        default=None,
+        "--cache_dir", type=str, required=False, default=None,
     )
     default_wheel_house_dir = os.path.join(os.getcwd(), "wheelhouse")
     parser.add_argument(
-        "--wheel_house_dir",
-        type=str,
-        required=False,
-        default=default_wheel_house_dir,
+        "--wheel_house_dir", type=str, required=False, default=default_wheel_house_dir,
     )
     parser.add_argument(
-        "--python_version",
-        type=str,
-        required=False,
-        default="3.5, 3.6, 3.7, 3.8",
+        "--python_version", type=str, required=False, default="3.5, 3.6, 3.7, 3.8",
     )
     parser.add_argument(
-        "--cuda_version",
-        type=str,
-        required=False,
-        default="10.2",
+        "--cuda_version", type=str, required=False, default="10.2",
     )
     parser.add_argument(
-        "--extra_oneflow_cmake_args",
-        type=str,
-        required=False,
-        default="",
+        "--extra_oneflow_cmake_args", type=str, required=False, default="",
     )
     parser.add_argument(
-        "--extra_docker_args",
-        type=str,
-        required=False,
-        default="",
+        "--extra_docker_args", type=str, required=False, default="",
     )
     parser.add_argument(
-        "--oneflow_src_dir",
-        type=str,
-        required=False,
-        default=os.getcwd(),
+        "--oneflow_src_dir", type=str, required=False, default=os.getcwd(),
     )
     parser.add_argument(
         "--skip_third_party", default=False, action="store_true", required=False
