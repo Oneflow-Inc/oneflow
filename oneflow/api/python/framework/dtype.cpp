ae494cc28cfb021b7961c6c3653cc7c35024a7f2
clackhan <han_binbin@163.com>
2021-03-03 17:23:31 +0800

    fix fun location

--- oneflow/api/python/framework/dtype.cpp
+++ oneflow/api/python/framework/dtype.cpp
@@ -26,10 +26,10 @@ ONEFLOW_API_PYBIND11_MODULE("", m) {
       .def_property_readonly("is_signed", &DType::is_signed)
       .def_property_readonly("is_complex", &DType::is_complex)
       .def_property_readonly("is_floating_point", &DType::is_floating_point)
-      .def_property_readonly(
-          "bytes", [](const std::shared_ptr<DType>& x) { return x->bytes().GetOrThrow(); })
       .def("__str__", &DType::name)
-      .def("__repr__", &DType::name);
+      .def("__repr__", &DType::name)
+      .def_property_readonly(
+          "bytes", [](const std::shared_ptr<DType>& x) { return x->bytes().GetOrThrow(); });
 
   m.attr("char") = DType::Char().GetPtrOrThrow();
   m.attr("float16") = DType::Float16().GetPtrOrThrow();
