c594d4b07c441b7c098de1436f440c69d025e8c6
binbinHan <han_binbin@163.com>
2021-03-03 17:23:31 +0800

    Mig blob cache (#4202)
    * mig parallel_conf_util
    
    * mig BuildInitialScope BuildScopeWithNewParallelDesc BuildScopeWithNewParallelConf
    
    * add test of GetDeviceTagAndMachineDeviceIds
    
    * mig GetOpConfSymbol
    
    * fix BuildScopeWithNewParallelDesc input type error
    
    * use TRY
    
    * use symbol::Storage<OperatorConfSymbol>
    
    * _NewOpKernelObject
    
    * mig OpKernelObject
    
    * mig object_storage
    
    * make of_format
    
    * del comment
    
    * std::function<void(Object*)
    
    * mig NewOpKernelObject and _StatefulCallOpKernel
    
    * mig _StatefulCallOpKernel and GetSharedOpKernelObject4ParallelConfSymbol
    
    * del object_storage.cpp
    
    * use name GLOBAL_PARA_SYM2SHARED_OPKENEL_OBJ_MUTEX
    
    * mig CheckRefInBlobObjectParallelDesc and  OperandBlobObjects rel api
    
    * mig _StatelessCall
    
    * mig _StatelessCall
    
    * mig StatelessCall api
    
    * mig StatefulCall
    
    * mig callback api
    
    * mig MakeLazyRefBlobObject
    
    * refactor CudaHostPinBlob
    
    * sort out InstructionsBuilder api
    
    * mig PhysicalRun and LogicalRun
    
    * use oneflow_api.deprecated.LogicalRun & PhysicalRun
    
    * delete vm_util.py
    
    * change FindOrCreateDelegateBlobObject args
    
    * add SetShuttingDown
    
    * rm python_interpreter_util.py
    
    * add blank line
    
    * mig BlobCache
    
    * use FindOrCreateDelegateBlobObject in c++
    
    * refactor session_util.cpp
    
    * use IsShutDown
    
    * refactor BlobRegister
    
    * fix distribute test exit bug
    
    * del comment
    
    * mig id_util and scope_util
    
    * use cfg_op_conf and Object*
    
    * use Object*
    
    * del _
    
    * fix func name error
    
    * use MapAt and shared_ptr
    
    * use shared_ptr or const ref
    
    * minor fix
    
    * add todo
    
    * minor fix
    
    * minor djustment
    
    * minor fix
    
    * minor optimize
    
    * minor fix
    
    * minor optimize
    
    * minor optimize
    
    * fix bug
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix dtype
    
    * rename std::function arg
    
    * rename std::function arg
    
    * minor fix error
    
    * minor fix error
    
    * use const ref
    
    * rename std::function arg
    
    * refactor session_util in c++
    
    * optimize session_util
    
    * use Maybe<void>
    
    * del vm_util
    
    * minor fix
    
    * refactor session_util
    
    * minor optimize
    
    * add mutex
    
    * add comment
    
    * fix according to comment
    
    * minor fix error
    
    * use const auto& or auto*
    
    * fix bug
    
    * make of_format
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/python/eager/boxing_util.py
+++ oneflow/python/eager/boxing_util.py
@@ -367,10 +367,7 @@ def GpuNcclAllReduce(builder, produced_blob_object, consumer_op_arg_parallel_att
     op_attribute = _GetEagerNcclAllReduce(parallel_conf, bn_in_op2blob_object)
     cfg_op_attribute = oneflow_api.deprecated.MakeOpAttributeByString(str(op_attribute))
     builder.NoBoxingStatelessCall(
-        cfg_op_attribute,
-        parallel_conf,
-        bn_in_op2blob_object,
-        blob_cache_util.FindOrCreateDelegateBlobObject,
+        cfg_op_attribute, parallel_conf, bn_in_op2blob_object,
     )
     y_blob_object = bn_in_op2blob_object["out_0"]
     y_blob_object.op_arg_parallel_attr.Assign(consumer_op_arg_parallel_attr)
@@ -551,7 +548,6 @@ def BuildNaiveCpuBoxing(
         cfg_op_attribute,
         boxing_parallel_desc_symbol.parallel_conf,
         bn_in_op2blob_object,
-        blob_cache_util.FindOrCreateDelegateBlobObject,
     )
     return [bn_in_op2blob_object["out_%s" % i] for i in range(out_parallel_num)]
 
@@ -746,7 +742,7 @@ def _AssignOpConf():
 
 
 def BuildAssignInstruction(builder, ref_blob_object, value_blob_object, op_conf):
-    blob_cache_util.TryDisableBlobCache(ref_blob_object)
+    oneflow_api.TryDisableBlobCache(ref_blob_object)
     ref_parallel_conf = ref_blob_object.parallel_desc_symbol.parallel_conf
     ref_devices = ref_blob_object.parallel_desc_symbol.machine_id2device_id_list
     value_devices = value_blob_object.parallel_desc_symbol.machine_id2device_id_list
@@ -763,10 +759,7 @@ def BuildAssignInstruction(builder, ref_blob_object, value_blob_object, op_conf)
     cfg_op_attribute = oneflow_api.deprecated.MakeOpAttributeByString(str(op_attribute))
     if ref_device_tag == value_device_tag:
         builder.NoBoxingStatelessCall(
-            cfg_op_attribute,
-            ref_parallel_conf,
-            bn_in_op2blob_object,
-            blob_cache_util.FindOrCreateDelegateBlobObject,
+            cfg_op_attribute, ref_parallel_conf, bn_in_op2blob_object,
         )
     elif ref_device_tag == "cpu" and value_device_tag == "gpu":
         value_parallel_conf = value_blob_object.parallel_desc_symbol.parallel_conf
