dcec5f4b0f0e9c064e92b8a257a0906c5af59190
jackalcooper <jackalcooper@gmail.com>
2021-03-03 12:15:46 +0800

    fix container

--- oneflow/core/common/container_util.h
+++ oneflow/core/common/container_util.h
@@ -23,11 +23,14 @@ limitations under the License.
 
 namespace oneflow {
 
+#ifndef USING_HASH_CONTAINER
 template<typename Key, typename T, typename Hash = std::hash<Key>>
 using HashMap = std::unordered_map<Key, T, Hash>;
 
 template<typename Key, typename Hash = std::hash<Key>>
 using HashSet = std::unordered_set<Key, Hash>;
+#define USING_HASH_CONTAINER
+#endif  // USING_HASH_CONTAINER
 
 template<typename MapT, typename KeyT>
 scalar_or_const_ref_t<typename MapT::mapped_type> MapAtOrDefault(const MapT& map, const KeyT& key) {
