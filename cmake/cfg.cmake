5974c7b3294dde05c64f00a3968bd8ea9c8bf41c
clackhan <han_binbin@163.com>
2021-03-03 19:22:18 +0800

    fix bug

--- cmake/cfg.cmake
+++ cmake/cfg.cmake
@@ -103,9 +103,11 @@ function(GENERATE_CFG_AND_PYBIND11_CPP SRCS HDRS PYBIND_SRCS ROOT_DIR)
       oneflow/core/job/job_conf.proto
       oneflow/core/job/placement.proto
       oneflow/core/framework/user_op_attr.proto
+      oneflow/core/job/sbp_parallel.proto
       oneflow/core/job/scope.proto
       oneflow/core/operator/op_attribute.proto
       oneflow/core/operator/op_node_signature.proto
+      oneflow/core/job/mirrored_parallel.proto
       oneflow/core/job/parallel_conf_signature.proto
       oneflow/core/job/initializer_conf.proto
       oneflow/core/job/regularizer_conf.proto
@@ -115,6 +117,8 @@ function(GENERATE_CFG_AND_PYBIND11_CPP SRCS HDRS PYBIND_SRCS ROOT_DIR)
       oneflow/core/register/logical_blob_id.proto
       oneflow/core/operator/interface_blob_conf.proto
       oneflow/core/common/shape.proto
+      oneflow/core/register/blob_desc.proto
+      oneflow/core/register/pod.proto
       oneflow/core/operator/op_conf.proto
   )
 
