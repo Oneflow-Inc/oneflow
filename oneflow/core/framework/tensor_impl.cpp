7928fca8d6f516e6297afbd39c2f4748dcde3c00
poohRui <yuruil@qq.com>
2021-03-08 15:07:19 +0800

    make format

--- oneflow/core/framework/tensor_impl.cpp
+++ oneflow/core/framework/tensor_impl.cpp
@@ -24,7 +24,7 @@ namespace one {
 namespace {
 
 std::shared_ptr<const ParallelDesc> MakeParallelDescByDevice(const Device& device) {
-  TODO(); // liyurui
+  TODO();  // liyurui
   return std::shared_ptr<const ParallelDesc>();
 }
 
