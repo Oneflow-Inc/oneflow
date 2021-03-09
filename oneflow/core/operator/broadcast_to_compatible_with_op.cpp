692d03ae62a7a1e529b0e79fb62769be868d0b12
Juncheng <liujuncheng1022@gmail.com>
2021-03-09 11:18:04 +0800

    Refine InferOutBlobDescs signature (#4338)
    Former-commit-id: 3bd8b8f724aaac3e666bcea653f3673660053e63

--- oneflow/core/operator/broadcast_to_compatible_with_op.cpp
+++ oneflow/core/operator/broadcast_to_compatible_with_op.cpp
@@ -71,8 +71,9 @@ class BroadcastToCompatibleWithOp final : public Operator {
     return InferBlobDescs(op_conf(), BlobDesc4BnInOp);
   }
 
-  Maybe<void> InferOutBlobDescs(std::function<BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
-                                const ParallelContext* parallel_ctx) const override {
+  Maybe<void> InferOutBlobDescs(
+      const std::function<BlobDesc*(const std::string&)>& GetBlobDesc4BnInOp,
+      const ParallelContext* parallel_ctx) const override {
     return InferBlobDescs(op_conf(), GetBlobDesc4BnInOp);
   }
 
