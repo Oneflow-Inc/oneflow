08df78b21f0e171125486c23183a2d6f71e3ccb8
Flowingsun007 <flowingsun007@163.com>
2021-03-11 21:48:22 +0800

    add test_linear

---
+++ oneflow/python/test/modules/test_linear.py
@@ -0,0 +1,40 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+
+import numpy as np
+
+import oneflow as flow
+import oneflow.typing as tp
+
+
+@unittest.skipIf(
+    not flow.unittest.env.eager_execution_enabled(),
+    ".numpy() doesn't work in eager mode",
+)
+class TestModule(flow.unittest.TestCase):
+    
+
+    def test_identity(test_case):
+        m = flow.nn.Identity(54, unused_argument1=0.1, unused_argument2=False)
+        x = flow.Tensor(3, 2)
+        print("test_identity >> input:", x.numpy())
+        y = m(x)
+        print("test_identity >> output", y.numpy())
+
+
+if __name__ == "__main__":
+    unittest.main()
