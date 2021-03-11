4f2f02a19594f561eae2ebc09d92971ef0f1d6bb
scxfjiang <sc.xfjiang@gmail.com>
2021-03-11 12:15:36 +0800

    update

--- oneflow/python/test/ops/test_optimizers.py
+++ oneflow/python/test/ops/test_optimizers.py
@@ -327,16 +327,18 @@ def compare_with_numpy_lars(
     func_config.default_data_type(flow.float32)
 
     @flow.global_function(type="train", function_config=func_config)
-    def testLars() -> flow.typing.Numpy:
+    def testLars(
+        random_mask: flow.typing.Numpy.Placeholder(x_shape, dtype=flow.float32)
+    ) -> flow.typing.Numpy:
         with flow.scope.placement(device_type, "0:0-0"):
             x = flow.get_variable(
                 name="x",
                 shape=x_shape,
                 dtype=flow.float32,
-                initializer=flow.ones_initializer(),
+                initializer=flow.random_uniform_initializer(minval=0, maxval=100),
                 trainable=True,
             )
-            loss = flow.math.reduce_mean(x)
+            loss = flow.math.reduce_mean(x * random_mask)
             flow.optimizer.LARS(
                 flow.optimizer.PiecewiseConstantScheduler([], [learning_rate]),
                 momentum_beta=momentum_beta,
@@ -346,14 +348,16 @@ def compare_with_numpy_lars(
             ).minimize(loss)
             return x
 
-    init_value = None
-    of_results = []
+    # generate random number sequences
+    random_masks_seq = []
     for i in range(train_iters + 1):
-        x = testLars()
+        random_masks_seq.append(np.random.uniform(size=x_shape).astype(np.float32))
+
+    init_value = None
+    for i in range(train_iters + 1):
+        x = testLars(random_masks_seq[i])
         if i == 0:
             init_value = np.copy(x)
-        else:
-            of_results.append(x)
 
     def lars_update_numpy(
         param,
@@ -391,11 +395,10 @@ def compare_with_numpy_lars(
     gradient = np.full(param.shape, 1.0 / np.prod(param.shape))
     momentum = np.zeros(param.shape)
 
-    numpy_results = []
     for i in range(train_iters):
         param, momentum = lars_update_numpy(
             param,
-            gradient,
+            gradient * random_masks_seq[i],
             momentum,
             learning_rate,
             momentum_beta,
@@ -403,17 +406,8 @@ def compare_with_numpy_lars(
             epsilon,
             lars_coefficient,
         )
-        numpy_results.append(param)
 
-    assert len(of_results) == len(numpy_results)
-    for i in range(len(of_results)):
-        x = of_results[i]
-        param = numpy_results[i]
-        print("x" * 50)
-        print(x.flatten())
-        print(param.flatten())
-        print(x.flatten() - param.flatten())
-        assert np.allclose(x.flatten(), param.flatten(), rtol=1e-4, atol=1e-4,)
+    assert np.allclose(x.flatten(), param.flatten(), rtol=1e-4, atol=1e-4,)
 
 
 def compare_with_tensorflow_sgd(
@@ -1016,12 +1010,11 @@ class TestOptimizers(flow.unittest.TestCase):
         arg_dict["x_shape"] = [(10,)]
         arg_dict["momentum_beta"] = [0.9]
         arg_dict["epsilon"] = [1e-9]
-        arg_dict["lars_coefficient"] = [0.01]
+        arg_dict["lars_coefficient"] = [0.0001]
         arg_dict["learning_rate"] = [1]
         arg_dict["weight_decay"] = [100]
         arg_dict["train_iters"] = [10]
         for arg in GenArgList(arg_dict):
-            print("y" * 100)
             compare_with_numpy_lars(*arg)
 
     def test_sgd(test_case):
