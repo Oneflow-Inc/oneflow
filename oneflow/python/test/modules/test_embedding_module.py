af43e90cd512298b82ce9d1bbef8492a8754d74c
Ldpe2G <liangdepeng@gmail.com>
2021-03-12 18:15:51 +0800

    add gather module

---
+++ oneflow/python/test/modules/test_embedding_module.py
@@ -0,0 +1,41 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+
+import numpy as np
+
+import oneflow as flow
+import oneflow.typing as tp
+
+
+@unittest.skipIf(
+    not flow.unittest.env.eager_execution_enabled(),
+    ".numpy() doesn't work in eager mode",
+)
+class TestModule(flow.unittest.TestCase):
+    def test_embedding(test_case):
+        m = flow.nn.Gather(axis=0)
+        x = flow.Tensor(3, 4)
+        indices = flow.Tensor([2])
+        # indices = flow.Tensor([])
+        print("test_dropout >> input:", x.numpy(), indices.numpy())
+        # y = m(x, )
+
+        # print("test_dropout >> output", y.numpy())
+
+
+if __name__ == "__main__":
+    unittest.main()
