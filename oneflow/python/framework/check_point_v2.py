2782554d3b9402d215173e48fb8c2f0cbd5d7d99
poohRui <yuruil@qq.com>
2021-03-12 16:24:18 +0800

    add note

--- oneflow/python/framework/check_point_v2.py
+++ oneflow/python/framework/check_point_v2.py
@@ -536,10 +536,11 @@ def Init() -> None:
         g = initializer_util.GetInitializer(
             var_conf.initializer, var_conf.random_seed, var_blob.shape
         )
+        # g is None if and only if the initializer is empty_initializer
+        if g is None:
+            continue
 
         def GenerateValueAndAssign(var_blob, start_nd_idx, stop_nd_idx):
-            if g is None:
-                return
             np_dtype = np.dtype(
                 dtype_util.convert_oneflow_dtype_to_numpy_dtype(var_blob.dtype)
             )
