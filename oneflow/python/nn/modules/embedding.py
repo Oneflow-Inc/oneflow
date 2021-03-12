154f64bcc778cc4fcb36d671b586822d55a57151
Ldpe2G <liangdepeng@gmail.com>
2021-03-12 20:16:54 +0800

    add embedding module

--- oneflow/python/nn/modules/embedding.py
+++ oneflow/python/nn/modules/embedding.py
@@ -15,8 +15,10 @@ limitations under the License.
 """
 import oneflow as flow
 import oneflow.python.framework.id_util as id_util
-from oneflow.python.nn.module import Module
+
+from oneflow.python.framework.tensor import Tensor
 from oneflow.python.oneflow_export import oneflow_export
+from oneflow.python.nn.module import Module
 
 from typing import Optional, List, Tuple
 
@@ -39,4 +41,57 @@ class Gather(Module):
 
     def forward(self, x, indices):
         res = self._op(x, indices)[0]
+        return res
+
+
+@oneflow_export("nn.Embedding")
+class Embedding(Module):
+    def __init__(
+        self,
+        num_embeddings: int,
+        embedding_dim: int,
+        padding_idx: Optional[int] = None,
+        max_norm: Optional[float] = None,
+        norm_type: float = 2.0,
+        scale_grad_by_freq: bool = False,
+        sparse: bool = False,
+        _weight: Optional[Tensor] = None,
+        name: Optional[str] = None,
+    ):
+        super().__init__()
+
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        if padding_idx is not None:
+            if padding_idx > 0:
+                assert (
+                    padding_idx < self.num_embeddings
+                ), "Padding_idx must be within num_embeddings"
+            elif padding_idx < 0:
+                assert (
+                    padding_idx >= -self.num_embeddings
+                ), "Padding_idx must be within num_embeddings"
+                padding_idx = self.num_embeddings + padding_idx
+        self.padding_idx = padding_idx
+        self.max_norm = max_norm
+        self.norm_type = norm_type
+        self.scale_grad_by_freq = scale_grad_by_freq
+        if _weight is None:
+            self.weight = flow.nn.Parameter(Tensor(num_embeddings, embedding_dim))
+            # self.reset_parameters()
+        else:
+            assert list(_weight.shape) == [
+                num_embeddings,
+                embedding_dim,
+            ], "Shape of weight does not match num_embeddings and embedding_dim"
+            self.weight = flow.nn.Parameter(_weight)
+            # self._fill_padding_idx_with_zero()
+        self.sparse = sparse
+
+        self.gather_op = Gather(
+            name=name if name is not None else id_util.UniqueStr("Embedding_")
+        )
+
+    def forward(self, indices):
+        res = self.gather_op(self.weight, indices)
         return res
