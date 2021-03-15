e9e470b663526951d667cba0703651653fc61b85
jackalcooper <jackalcooper@gmail.com>
2021-03-15 19:03:47 +0800

    add header and proto field

---
+++ oneflow/core/rpc/include/manager.h
@@ -0,0 +1,16 @@
+#ifndef ONEFLOW_CORE_RPC_INCLUDE_MANAGER_H_
+#define ONEFLOW_CORE_RPC_INCLUDE_MANAGER_H_
+
+namespace oneflow {
+
+class RpcManager {
+ public:
+  RpcManager() {}
+  virtual ~RpcManager() {}
+  virtual void CreateServer() {}
+  virtual void CreateClient() {}
+  virtual void Bootstrap() {}
+};
+
+}  // namespace oneflow
+#endif  // ONEFLOW_CORE_RPC_INCLUDE_MANAGER_H_
