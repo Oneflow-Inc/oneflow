e95dcb6858b9c75edee2902cdd7d6ac4f6cc38f7
Ldpe2G <liangdepeng@gmail.com>
2021-03-15 17:39:28 +0800

    change unit test

--- oneflow/python/test/modules/test_embedding_module.py
+++ oneflow/python/test/modules/test_embedding_module.py
@@ -19,27 +19,52 @@ import oneflow.typing as tp
 import numpy as np
 import unittest
 
+
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
     ".numpy() doesn't work in eager mode",
 )
 class TestModule(flow.unittest.TestCase):
     def test_embedding(test_case):
-        m = flow.nn.Embedding(6, 4)
-        indices = flow.Tensor([1,3,5], dtype=flow.int32)
-        print("test_embedding >> input:")
-        print(m.weight.numpy(), indices.numpy())
-        y = m(indices)
-        print("test_embedding >> output")
-        print(y.numpy())
+        torch_weight = np.array(
+            [
+                [0.68258786, 0.6957856, 1.1829041],
+                [1.0154, -1.0616943, 0.50303376],
+                [0.29679507, 0.65562993, 1.0424724],
+                [-0.42980736, -0.35347632, -0.15600166],
+                [0.6763601, -0.24286619, -2.0873115],
+                [-0.13371214, -0.5589277, 1.9173933],
+                [0.08762296, 1.0264007, -0.67938024],
+                [0.32019204, -0.26137325, -1.3534237],
+                [-1.1555519, -0.67776406, 0.27372134],
+                [1.0615997, -0.59715784, 1.9855849],
+            ],
+            dtype=np.float32,
+        )
 
-        weight = flow.Tensor(np.random.rand(6, 4))
-        m2 = flow.nn.Embedding(6, 4, _weight=weight)
-        print("test_embedding with predefined weight >> input:")
-        print(weight.numpy(), indices.numpy())
+        torch_out = np.array(
+            [
+                [
+                    [1.0154, -1.0616943, 0.50303376],
+                    [0.29679507, 0.65562993, 1.0424724],
+                    [0.6763601, -0.24286619, -2.0873115],
+                    [-0.13371214, -0.5589277, 1.9173933],
+                ],
+                [
+                    [0.6763601, -0.24286619, -2.0873115],
+                    [-0.42980736, -0.35347632, -0.15600166],
+                    [0.29679507, 0.65562993, 1.0424724],
+                    [1.0615997, -0.59715784, 1.9855849],
+                ],
+            ],
+            dtype=np.float32,
+        )
+
+        indices = flow.Tensor([[1, 2, 4, 5], [4, 3, 2, 9]], dtype=flow.int32)
+        m2 = flow.nn.Embedding(10, 3, _weight=flow.Tensor(torch_weight))
         y = m2(indices)
-        print("test_embedding with predefined weight  >> output")
-        print(y.numpy())
+        print(np.allclose(y.numpy(), torch_out))
+
 
 if __name__ == "__main__":
     unittest.main()
