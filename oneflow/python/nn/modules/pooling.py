5a688604fc8b1304cd54693ad9f8a7a6eaaf6365
yaochi <later@usopp.net>
2021-03-17 00:05:36 +0800

    add docstring

--- oneflow/python/nn/modules/pooling.py
+++ oneflow/python/nn/modules/pooling.py
@@ -31,8 +31,61 @@ import oneflow.python.framework.id_util as id_util
 
 @oneflow_export("nn.AvgPool2d")
 class AvgPool2d(Module):
-    r"""Applies a 2D average pooling over an input signal composed of several input
-    planes.
+    r"""Applies a 2D average pooling over an input.
+
+    In the simplest case, the output value of the layer with input size :math:`(N, C, H, W)`,
+    output :math:`(N, C, H_{out}, W_{out})` and :attr:`kernel_size` :math:`(kH, kW)`
+    can be precisely described as:
+
+    .. math::
+
+        out(N_i, C_j, h, w)  = \frac{1}{kH * kW} \sum_{m=0}^{kH-1} \sum_{n=0}^{kW-1}
+                               input(N_i, C_j, stride[0] \times h + m, stride[1] \times w + n)
+
+    If :attr:`padding` is non-zero, then the input is implicitly zero-padded on both sides
+    for :attr:`padding` number of points.
+
+    Note:
+        When ceil_mode=True, sliding windows are allowed to go off-bounds if they start within the left padding
+        or the input. Sliding windows that would start in the right padded region are ignored.
+
+    The parameters :attr:`kernel_size`, :attr:`stride`, :attr:`padding` can either be:
+
+        - a single ``int`` -- in which case the same value is used for the height and width dimension
+        - a ``tuple`` of two ints -- in which case, the first `int` is used for the height dimension,
+          and the second `int` for the width dimension
+
+    Args:
+        kernel_size: the size of the window
+        stride: the stride of the window. Default value is :attr:`kernel_size`
+        padding: padding(value 0) to be added on both sides
+        ceil_mode: when True, will use `ceil` instead of `floor` to compute the output shape
+
+    Shape:
+        - Input: :math:`(N, C, H_{in}, W_{in})`
+        - Output: :math:`(N, C, H_{out}, W_{out})`, where
+
+          .. math::
+              H_{out} = \left\lfloor\frac{H_{in}  + 2 \times \text{padding}[0] -
+                \text{kernel\_size}[0]}{\text{stride}[0]} + 1\right\rfloor
+
+          .. math::
+              W_{out} = \left\lfloor\frac{W_{in}  + 2 \times \text{padding}[1] -
+                \text{kernel\_size}[1]}{\text{stride}[1]} + 1\right\rfloor
+
+    Examples::
+
+        input = flow.Tensor(np.random.randn(20, 16, 50, 32))
+
+        # pool of square window of size=3, stride=2
+        m1 = flow.nn.AvgPool2d(kernel_size=3, stride=2)
+        output = m1(input) # output.shape: [20, 16, 24, 15]
+
+        # pool of non-square window
+        m2 = flow.nn.AvgPool2d(kernel_size=(3, 2), stride=(2, 1))
+        output = m2(input) # output.shape: [20, 16, 24, 31]
+
+
     """
 
     def __init__(
