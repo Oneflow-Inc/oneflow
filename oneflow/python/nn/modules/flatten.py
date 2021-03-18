151c0929178a54a8befd4abef42c84d9795ee5fe
Liang Depeng <liangdepeng@gmail.com>
2021-03-18 10:02:59 +0800

    Dev add flatten module (#4444)
    * rename batchnorm
    
    * add flatten module and fix format

---
+++ oneflow/python/nn/modules/flatten.py
@@ -0,0 +1,63 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import oneflow as flow
+from oneflow.python.nn.module import Module
+from oneflow.python.oneflow_export import oneflow_export
+
+
+@oneflow_export("nn.Flatten")
+class Flatten(Module):
+    r"""
+    Flattens a contiguous range of dims into a tensor. For use with :class:`~nn.Sequential`.
+
+    Shape:
+        - Input: :math:`(N, *dims)`
+        - Output: :math:`(N, \prod *dims)` (for the default case).
+
+    Args:
+        start_dim: first dim to flatten (default = 1).
+        end_dim: last dim to flatten (default = -1).
+
+    Examples::
+        >>> input = flow.randn(32, 1, 5, 5)
+        >>> m = nn.Sequential(
+        >>>     nn.Conv2d(1, 32, 5, 1, 1),
+        >>>     nn.Flatten()
+        >>> )
+        >>> output = m(input)
+        >>> output.size()
+        flow.Size([32, 288])
+    """
+    __constants__ = ["start_dim", "end_dim"]
+    start_dim: int
+    end_dim: int
+
+    def __init__(self, start_dim: int = 1, end_dim: int = -1, name: str = None) -> None:
+        super().__init__()
+        self.start_dim = start_dim
+        self.end_dim = end_dim
+        self.op_ = (
+            flow.builtin_op("flatten")
+            .Name("relu")
+            .Input("in")
+            .Output("out")
+            .Attr("start_dim", start_dim)
+            .Attr("end_dim", end_dim)
+            .Build()
+        )
+
+    def forward(self, input):
+        return self.op_(input)[0]
