aeb0932428b0a06c2cb5aec264ec7933609d96dd
liujuncheng <liujuncheng1022@gmail.com>
2021-03-18 17:31:31 +0800

    Fix FillOpParallelDesc order

--- oneflow/core/operator/operator.cpp
+++ oneflow/core/operator/operator.cpp
@@ -478,7 +478,7 @@ void Operator::ForEachBnInOp(std::function<void(const std::string&)> Handler) co
 Maybe<void> Operator::FillSbpSignature(const SbpSignature& sbp_signature) {
   ParallelDistributionSignature parallel_distribution_signature;
   SbpSignatureToParallelDistributionSignature(sbp_signature, &parallel_distribution_signature);
-  FillParallelDistributionSignature(parallel_distribution_signature);
+  JUST(FillParallelDistributionSignature(parallel_distribution_signature));
   return Maybe<void>::Ok();
 }
 
