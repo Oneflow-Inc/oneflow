38884347e86d359d17d09f857bb4326052bd8218
hjchen2 <hjchen2>
2021-03-18 14:42:00 +0800

    Remove unused header

--- oneflow/api/python/framework/session_util.cpp
+++ oneflow/api/python/framework/session_util.cpp
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
 #include "oneflow/api/python/of_api_registry.h"
 #include "oneflow/core/framework/session_util.h"
 
