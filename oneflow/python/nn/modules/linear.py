e652ee66aa2666d34e21e4411b9ac352d4e0f746
Lyon <flowingsun007@163.com>
2021-03-18 11:09:38 +0800

    Dev module identity (#4378)
    * add test_linear
    
    * add module nn.Identity
    
    * update test case
    
    * refine
    
    * refine and format
    
    * refine
    
    * update
    
    Former-commit-id: 97e7f08cad762b580e488f6ced38084193b9a1df

--- oneflow/python/nn/modules/linear.py
+++ oneflow/python/nn/modules/linear.py
@@ -14,11 +14,34 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import oneflow as flow
-
 from oneflow.python.oneflow_export import oneflow_export
+from oneflow.python.framework.tensor import Tensor
 from oneflow.python.nn.module import Module
 from oneflow.python.nn.common_types import _size_1_t, _size_2_t, _size_3_t
 from typing import Optional, List, Tuple
+
+
+@oneflow_export("nn.Identity")
+class Identity(Module):
+    r"""A placeholder identity operator that is argument-insensitive.
+
+    Args:
+        args: any argument (unused)
+        kwargs: any keyword argument (unused)
+
+    Examples::
+
+        >>> m = flow.nn.Identity(54, unused_argument1=0.1, unused_argument2=False)
+        >>> input = flow.Tensor(np.random.rand(2, 3, 4, 5))
+        >>> output = m(input)
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Identity, self).__init__()
+
+    def forward(self, input: Tensor) -> Tensor:
+        return input
 
 
 @oneflow_export("nn.Linear")
