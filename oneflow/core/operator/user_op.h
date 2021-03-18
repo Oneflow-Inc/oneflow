b6b6f80db832f0a9781c2bdc4c674979e01fe0f2
liujuncheng <liujuncheng1022@gmail.com>
2021-03-18 12:41:53 +0800

    Optimize InferOpTimeShape

--- oneflow/core/operator/user_op.h
+++ oneflow/core/operator/user_op.h
@@ -61,8 +61,8 @@ class UserOp final : public Operator {
       const std::function<Maybe<const BlobDesc&>(const std::string&)>& LogicalBlobDesc4Ibn,
       const ParallelDesc& parallel_desc, SbpSignatureList* sbp_sig_list) const override;
   Maybe<void> InferOpTimeShape(
-      const std::function<const Shape*(const std::string&)>& GetTimeShape4BnInOp,
-      Shape* time_shape) const override;
+      const std::function<Maybe<const Shape>(const std::string&)>& GetTimeShape4BnInOp,
+      std::shared_ptr<const Shape>* time_shape) const override;
   void VirtualGenKernelConf(std::function<const BlobDesc*(const std::string&)> GetBlobDesc4BnInOp,
                             const ParallelContext* parallel_ctx,
                             KernelConf* kernel_conf) const override;
