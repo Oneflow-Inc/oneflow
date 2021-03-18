210c9696d7c133506d0198d359af74d3743b95ba
hjchen2 <hjchen2>
2021-03-18 14:22:56 +0800

    Revert SnapshotManager and Session

--- oneflow/core/persistence/posix/posix_file_system.h
+++ oneflow/core/persistence/posix/posix_file_system.h
@@ -53,8 +53,6 @@ class PosixFileSystem final : public FileSystem {
 
   bool IsDirectory(const std::string& fname) override;
 
-  Maybe<bool> IsRegularFile(const std::string& fname) override;
-
  private:
 };
 
