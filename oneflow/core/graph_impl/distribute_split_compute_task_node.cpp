a5b39526cabdee89f129e6c808bac5aff1e48e22
cheng cheng <472491134@qq.com>
2021-03-19 21:09:36 +0800

    Refine graph/ header file (#4459)
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>
    Former-commit-id: b278c08d3e967ac4a83a1361ac6f15d2e8847db8

--- oneflow/core/graph_impl/distribute_split_compute_task_node.cpp
+++ oneflow/core/graph_impl/distribute_split_compute_task_node.cpp
@@ -13,11 +13,30 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/core/graph/distribute_split_compute_task_node.h"
+#include "oneflow/core/graph/compute_task_node.h"
 #include "oneflow/core/graph/task_graph.h"
 #include "oneflow/core/operator/variable_op.h"
 
 namespace oneflow {
+
+class DistributeSplitCompTaskNode final : public CompTaskNode {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(DistributeSplitCompTaskNode);
+  DistributeSplitCompTaskNode() = default;
+  ~DistributeSplitCompTaskNode() = default;
+
+  void ProduceAllRegstsAndBindEdges() override;
+  void ConsumeAllRegsts() override;
+  bool IsReadyForBuild() override;
+
+  TaskType GetTaskType() const override { return TaskType::kDistributeSplit; }
+  bool HasBackwardCompTaskNode();
+
+ private:
+  void BuildExecGphAndRegst() override;
+  void BuildExecGphStructAndBindInRegst();
+  void BuildOutRegst();
+};
 
 bool DistributeSplitCompTaskNode::HasBackwardCompTaskNode() { return false; }
 
