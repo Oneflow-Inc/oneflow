68ba65aaea623d96fe2ec1e1c55bcf283096b1e5
BBuf <1182563586@qq.com>
2021-03-23 20:22:15 +0800

    add paddle unit_test

---
+++ oneflow/python/test/onnx/load/nodes/paddle/test_reduction.py
@@ -0,0 +1,44 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+
+from oneflow.python.test.onnx.load.util import load_paddle_module_and_check
+
+
+def test_reduce_mean(test_case):
+    class Net(nn.Layer):
+        def forward(self, x):
+            return paddle.mean(x)
+
+    load_paddle_module_and_check(test_case, Net)
+
+
+def test_reduce_mean_axis(test_case):
+    class Net(nn.Layer):
+        def forward(self, x):
+            return paddle.mean(x, dim=2)
+
+    load_paddle_module_and_check(test_case, Net)
+
+
+def test_reduce_mean_axis_keepdim(test_case):
+    class Net(nn.Layer):
+        def forward(self, x):
+            return paddle.mean(x, dim=3, keepdim=True)
+
+    load_paddle_module_and_check(test_case, Net)
---
+++ oneflow/python/test/onnx/load/nodes/paddle/test_reshape.py
@@ -0,0 +1,30 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+
+from oneflow.python.test.onnx.load.util import load_paddle_module_and_check
+
+
+# TODO(daquexian): add tests for 0 and -1 after flow.reshape supports it
+def test_reshape(test_case):
+    class Net(nn.Layer):
+        def forward(self, x):
+            x = paddle.reshape(x, (5, 12))
+            return x
+
+    load_paddle_module_and_check(test_case, Net, (2, 5, 3, 2))
