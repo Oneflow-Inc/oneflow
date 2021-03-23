97c436fedf9025424e593acc953cb608fcb85d54
BBuf <1182563586@qq.com>
2021-03-23 18:00:34 +0800

    add paddle unit_test

---
+++ oneflow/python/test/onnx/load/nodes/paddle/test_pad.py
@@ -0,0 +1,47 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+
+from oneflow.python.test.onnx.load.util import load_paddle_module_and_check
+
+# from absl import app
+# from absl.testing import absltest
+
+
+def test_pad(test_case):
+    class Net(nn.Layer):
+        def forward(self, x):
+            my_pad = nn.Pad2D(padding=(2, 2, 3, 3), mode="constant")
+            x = my_pad(x)
+            return x
+
+    load_paddle_module_and_check(test_case, Net)
+
+
+def test_pad_with_value(test_case):
+    class Net(nn.Layer):
+        def forward(self, x):
+            my_pad = nn.Pad2D(padding=(2, 2, 3, 3), mode="constant", value='3.5')
+            x = my_pad(x)
+            return x
+
+    load_paddle_module_and_check(test_case, Net)
+
+
+# test_case = absltest.TestCase
+# test_pad(test_case)
---
+++ oneflow/python/test/onnx/load/nodes/paddle/test_pooling.py
@@ -0,0 +1,125 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+
+from oneflow.python.test.onnx.load.util import load_paddle_module_and_check
+
+
+def _test_k3s1p1(test_case, pt_pool):
+    class Net(nn.Layer):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.pool = pt_pool(kernel_size=3, stride=1, padding=1)
+
+        def forward(self, x):
+            x = self.pool(x)
+            return x
+
+    load_paddle_module_and_check(test_case, Net, input_size=(2, 4, 3, 5))
+
+
+def test_maxpool_k3s1p1(test_case):
+    _test_k3s1p1(test_case, nn.MaxPool2D)
+
+
+def test_avgpool_k3s1p1(test_case):
+    _test_k3s1p1(test_case, nn.AvgPool2D)
+
+
+def _test_k4s2p2(test_case, pt_pool):
+    class Net(nn.Layer):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.pool = pt_pool(kernel_size=4, stride=2, padding=2)
+
+        def forward(self, x):
+            x = self.pool(x)
+            return x
+
+    load_paddle_module_and_check(test_case, Net, input_size=(2, 4, 10, 9))
+
+
+def test_maxpool_k4s2p2(test_case):
+    _test_k4s2p2(test_case, nn.MaxPool2D)
+
+
+def test_avgpool_k4s2p3(test_case):
+    _test_k4s2p2(test_case, nn.AvgPool2D)
+
+
+def _test_k43s2p1(test_case, pt_pool):
+    class Net(nn.Layer):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.pool = pt_pool(kernel_size=(4, 3), stride=2, padding=1)
+
+        def forward(self, x):
+            x = self.pool(x)
+            return x
+
+    load_paddle_module_and_check(test_case, Net, input_size=(2, 4, 10, 9))
+
+
+def test_maxpool_k43s2p1(test_case):
+    _test_k43s2p1(test_case, nn.MaxPool2D)
+
+
+def test_avgpool_k43s2p1(test_case):
+    _test_k43s2p1(test_case, nn.AvgPool2D)
+
+
+def _test_k43s2p21(test_case, pt_pool):
+    class Net(nn.Layer):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.pool = pt_pool((4, 3), stride=2, padding=(2, 1))
+
+        def forward(self, x):
+            x = self.pool(x)
+            return x
+
+    load_paddle_module_and_check(test_case, Net, input_size=(2, 4, 10, 9))
+
+
+def test_maxpool_k43s2p21(test_case):
+    _test_k43s2p21(test_case, nn.MaxPool2D)
+
+
+def test_avgpool_k43s2p21(test_case):
+    _test_k43s2p21(test_case, nn.AvgPool2D)
+
+
+def _test_global_pooling(test_case, pt_pool):
+    class Net(nn.Layer):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.pool = pt_pool((1, 1))
+
+        def forward(self, x):
+            x = self.pool(x)
+            return x
+
+    load_paddle_module_and_check(test_case, Net, input_size=(2, 4, 10, 9))
+
+
+def test_global_avg_pooling(test_case):
+    _test_global_pooling(test_case, nn.AdaptiveAvgPool2D)
+
+
+def test_global_max_pooling(test_case):
+    _test_global_pooling(test_case, nn.AdaptiveMaxPool2D)
