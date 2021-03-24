4f90c15481411cd0fc75b8d37e77fe4563782e92
jackalcooper <jackalcooper@gmail.com>
2021-03-24 11:23:39 +0000

    refine

--- tools/generate_pip_version.py
+++ tools/generate_pip_version.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import argparse
+from datetime import date
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--xla", default=False, action="store_true", required=False)
@@ -11,6 +12,32 @@ args = parser.parse_args()
 if args.xla:
     assert args.cuda
 
+local_label = ""
+version = ""
+
+if os.getenv("ONEFLOW_RELEASE_VERSION"):
+    release_version = os.getenv("ONEFLOW_RELEASE_VERSION")
+    version = f"{release_version}"
+else:
+    version = f"0.3b6"
+    if os.getenv("ONEFLOW_RELEASE_NIGHTLY"):
+        today = date.today()
+        date_str = today.strftime("%Y%m%d")
+        version += f".dev{date_str}"
+    else:
+        try:
+            git_hash = (
+                subprocess.check_output("git rev-parse --short HEAD", shell=True, cwd=args.src)
+                .decode()
+                .strip()
+            )
+        except:
+            git_hash = "unknown"
+        version += f".git.{git_hash}"
+
+# append compute_platform
+
+compute_platform = ""
 if args.cuda:
     compute_platform = "".join(args.cuda.split("."))
     assert len(compute_platform) == 3, compute_platform
@@ -19,24 +46,8 @@ if args.cuda:
         compute_platform += ".xla"
 else:
     compute_platform = "cpu"
-
-local_label = ""
-version = ""
-
-if os.getenv("ONEFLOW_RELEASE_VERSION"):
-    release_version = os.getenv("ONEFLOW_RELEASE_VERSION")
-    version = f"{release_version}+{compute_platform}"
-else:
-    try:
-        git_hash = (
-            subprocess.check_output("git rev-parse --short HEAD", shell=True, cwd=args.src)
-            .decode()
-            .strip()
-        )
-    except:
-        git_hash = "unknown"
-
-    version = f"0.3b6+{compute_platform}.git.{git_hash}"
+assert compute_platform
+version += f"+{compute_platform}"
 
 dst = os.path.join(args.src, "oneflow/python/version.py")
 print(f"-- Generating pip version: {version} to: {dst}")
