9c721f2f8c19ff7d4ffe8361b76eadc410ab639c
jackalcooper <jackalcooper@gmail.com>
2021-03-25 07:24:17 +0000

    add script

---
+++ ci/test/dry_run_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xe
+
+export PYTHONUNBUFFERED=1
+
+src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
+test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"/test_tmp_dir"}
+
+
+rm -rf $test_tmp_dir
+mkdir -p $test_tmp_dir
+cp -r $src_dir/oneflow/python/test $test_tmp_dir
+cd $test_tmp_dir
+
+export ONEFLOW_DRY_RUN=1
+ONEFLOW_TEST_DEVICE_NUM=1 python3 test/ops/test_assign.py --failfast --verbose
+
+ONEFLOW_TEST_ENABLE_INIT_BY_HOST_LIST=1 ONEFLOW_TEST_DEVICE_NUM=1 python3 -m unittest discover test/ops --failfast --verbose
+ONEFLOW_TEST_ENABLE_INIT_BY_HOST_LIST=1 ONEFLOW_TEST_DEVICE_NUM=2 python3 -m unittest discover test/ops --failfast --verbose
+ONEFLOW_TEST_ENABLE_INIT_BY_HOST_LIST=1 ONEFLOW_TEST_DEVICE_NUM=4 python3 -m unittest discover test/ops --failfast --verbose
