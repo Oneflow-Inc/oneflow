652e9768beda9d6d4ae0fb2760b7a876ee39b6ad
Flowingsun007 <flowingsun007@163.com>
2021-03-25 19:02:01 +0800

    add module test case

---
+++ oneflow/python/test/modules/test_math_ops.py
@@ -0,0 +1,43 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import oneflow as flow
+import unittest
+import numpy as np
+
+
+@unittest.skipIf(
+    not flow.unittest.env.eager_execution_enabled(),
+    ".numpy() doesn't work in eager mode",
+)
+class TestModule(flow.unittest.TestCase):
+
+    def test_sin(test_case):
+        sin = flow.Sin()
+        input = flow.Tensor(np.random.randn(3, 4), dtype=flow.float32)
+        of_out = sin(input)
+        np_out = np.sin(input.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out))
+
+    
+    def test_cos(test_case):
+        cos = flow.Cos()
+        input = flow.Tensor(np.random.randn(6, 7), dtype=flow.float32)
+        of_out = cos(input)
+        np_out = np.cos(input.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out))
+
+if __name__ == "__main__":
+    unittest.main()
