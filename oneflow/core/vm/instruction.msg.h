4c26152e32a1e65e84162b628271b081314f0202
Yurui Li <32978179+poohRui@users.noreply.github.com>
2021-03-25 11:07:09 +0800

    construct pure intr msg list without pb  (#4494)
    * refactor_builder_instr_pb_list_to_instr_msg_list todo list
    
    * modified intruction proto list to msg list
    
    * remove header to cpp, add cfg constructor
    
    * modified variable life-time, using template for instruction msg and operand
    
    * fix bug in instruction builder construction
    
    * add cfg and template init for operand
    
    * fix id to symbol
    
    * fix function parameter name
    
    * fix template link error
    
    * format
    
    * replace mirror
    
    * broadcast object
    
    * build send instruction
    
    * build recv instruction
    
    * cuda host
    
    * lazy reference
    
    * remain instruction
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * minor fix
    
    * add_del_object_operand
    
    * fix typo
    
    * add delete dependent instructions
    
    * fix mutable operand error
    
    * remove useless functions
    
    * add ignore instruction function
    
    * fix wrong use for intr_msg_list constructor
    
    Co-authored-by: lixinqi <lixinqi0703106@163.com>
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/core/vm/instruction.msg.h
+++ oneflow/core/vm/instruction.msg.h
@@ -65,6 +65,7 @@ OBJECT_MSG_BEGIN(InstructionMsg);
   OF_PUBLIC ObjectMsgPtr<InstructionMsg> add_mut2_operand(ObjectId logical_object_id);
   OF_PUBLIC ObjectMsgPtr<InstructionMsg> add_mut2_operand(ObjectId logical_object_id, const SoleMirroredObject&);
   OF_PUBLIC ObjectMsgPtr<InstructionMsg> add_mut2_operand(ObjectId logical_object_id, const AllMirroredObject&);
+  OF_PUBLIC ObjectMsgPtr<InstructionMsg> add_del_object_operand(ObjectId logical_object_id);
   OF_PUBLIC const std::vector<FlatMsg<InstructionOperand>>& operand() const {
     return operand_list().operand();
   }
