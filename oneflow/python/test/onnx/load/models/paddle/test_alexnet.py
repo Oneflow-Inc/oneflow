275b0e506f8d608be392c68272241849817e08c8
BBuf <1182563586@qq.com>
2021-03-26 18:01:06 +0800

    support paddle alexnet to oneflow

--- oneflow/python/test/onnx/load/models/paddle/test_alexnet.py
+++ oneflow/python/test/onnx/load/models/paddle/test_alexnet.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+# https://github.com/PaddlePaddle/PaddleClas/blob/develop/ppcls/modeling/architectures/alexnet.py
 import paddle
 from paddle import ParamAttr
 import paddle.nn as nn
