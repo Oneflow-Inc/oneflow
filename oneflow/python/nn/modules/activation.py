da70cd6c170b8be8bc0fadcaa61a06443895734f
doombeaker <later@usopp.net>
2021-03-26 13:47:06 +0800

    add transpose function and nn.Softmax for eager (#4516)
    * add slice function for eager
    
    * add transpose module
    
    * remove useless slice op
    
    * add softmax module
    
    Former-commit-id: c425db9df1355eb2c92820190a4823632e1f3193

--- oneflow/python/nn/modules/activation.py
+++ oneflow/python/nn/modules/activation.py
@@ -18,6 +18,26 @@ from oneflow.python.nn.module import Module
 from oneflow.python.oneflow_export import oneflow_export
 from oneflow.python.framework.tensor import register_tensor_op_by_module
 from oneflow.python.framework.tensor import register_op_by_module
+from typing import Optional, Sequence, Sized, Union, List, Tuple
+from oneflow.python.nn.modules.array_ops import transpose
+
+
+def _softmax_need_transpose(x, axis):
+    assert type(axis) is int
+    dim_num = len(x.shape)
+    assert dim_num >= 2
+    if axis < 0:
+        axis += dim_num
+    assert axis >= 0
+    assert axis < dim_num
+
+    need_transpose = False
+    permute = list(range(dim_num))
+    if axis != dim_num - 1:
+        need_transpose = True
+        permute[axis] = permute[-1]
+        permute[-1] = axis
+    return need_transpose, permute
 
 
 @oneflow_export("nn.Sigmoid")
@@ -44,3 +64,37 @@ class ReLU(Module):
     def forward(self, x):
         res = self._op(x)[0]
         return res
+
+
+@oneflow_export("nn.Softmax")
+class Softmax(Module):
+    def __init__(
+        self, axis: Optional[int] = None, name: Optional[str] = None,
+    ):
+        super().__init__()
+
+        if axis is None:
+            axis = -1
+        self.axis = axis
+
+        self._op = flow.builtin_op("softmax", name).Input("in").Output("out").Build()
+
+    def forward(self, x):
+        print(x.shape)
+        need_transpose, permute = _softmax_need_transpose(x, self.axis)
+        if need_transpose:
+            logits = flow.transpose(logits, perm=permute)
+
+        res = self._op(x)[0]
+        if need_transpose:
+            res = transpose(res, perm=permute)
+        return res
+
+
+if __name__ == "__main__":
+    flow.enable_eager_execution(True)
+    import numpy as np
+
+    x = flow.Tensor(np.array([[1, 2, 1, 5, 4]]))
+    out = Softmax(1)(x)
+    print(out.numpy())
