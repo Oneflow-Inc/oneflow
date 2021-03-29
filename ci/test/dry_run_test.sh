4ef62785d1483d07ddb44f6e3e7e2eacc2a5f19a
jackalcooper <jackalcooper@gmail.com>
2021-03-29 03:51:24 +0000

    refine

--- ci/test/dry_run_test.sh
+++ ci/test/dry_run_test.sh
@@ -16,8 +16,8 @@ export ONEFLOW_DRY_RUN=1
 # trun on ONEFLOW_DEBUG_MODE will cause protobuf err
 # export ONEFLOW_DEBUG_MODE=1
 
-generated_node_list=$(python -c 'print(",".join(["mocknode_" + str(x) for x in range(4)]))')
-time python3 bert_benchmark/run_pretraining.py \
+generated_node_list=$(python3 -c 'print(",".join(["mocknode_" + str(x) for x in range(16)]))')
+heaptrack python3 bert_benchmark/run_pretraining.py \
     --gpu_num_per_node=1 \
     --node_num=1 \
     --learning_rate=1e-4 \
@@ -38,5 +38,5 @@ time python3 bert_benchmark/run_pretraining.py \
     --hidden_dropout_prob=0.1 \
     --hidden_size_per_head=64 \
     --node_list=${generated_node_list} \
-    --node_num=4 \
+    --node_num=16 \
     --gpu_num_per_node=8
