b1a869b10dd7848b28cad1d28512f7af9c94d8f8
BBuf <1182563586@qq.com>
2021-03-31 20:27:16 +0800

    add tensorflow reshape && softmax op test

---
+++ oneflow/python/test/onnx/load/nodes/tensorflow/test_softmax.py
@@ -0,0 +1,39 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import tensorflow as tf
+
+from oneflow.python.test.onnx.load.util import load_tensorflow2_module_and_check
+
+
+def test_softmax(test_case):
+    class Net(tf.keras.Model):
+        def call(self, x):
+            m = tf.keras.layers.Softmax()
+            x = m(x)
+            return x
+
+    load_tensorflow2_module_and_check(test_case, Net)
+
+
+def test_softmax_with_axis(test_case):
+    class Net(tf.keras.Model):
+        def call(self, x):
+            m = tf.keras.layers.Softmax(axis=1)
+            x = m(x)
+            return x
+
+    load_tensorflow2_module_and_check(test_case, Net)
+
