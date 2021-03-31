a1064959bd69707b9b8997e363d4fcf9a2abdcd0
Hongsheng Wang <31394900+wanghongsheng01@users.noreply.github.com>
2021-03-31 18:30:28 +0800

    Dev refactor split infer data type (#4547)
    * softmax_op.cpp
    
    * 107:sort_op.cpp
    
    * 106-115
    
    * 108-109
    
    * 110
    
    * 111
    
    * 112
    
    * 113
    
    * 115
    
    * 106-115
    
    * modify 106-115
    
    * modify assign
    
    * zzk fix
    
    Co-authored-by: MARD1NO <359521840@qq.com>
    Co-authored-by: Yurui Li <32978179+poohRui@users.noreply.github.com>

--- oneflow/user/ops/sparse_cross_entropy_op.cpp
+++ oneflow/user/ops/sparse_cross_entropy_op.cpp
@@ -21,7 +21,6 @@ namespace {
 
 Maybe<void> CheckPredictionLabelDesc(const user_op::TensorDesc* prediction_desc,
                                      const user_op::TensorDesc* label_desc) {
-  CHECK_OR_RETURN(IsIndexDataType(label_desc->data_type()));
   CHECK_EQ_OR_RETURN(prediction_desc->is_dynamic(), label_desc->is_dynamic());
   CHECK_GE_OR_RETURN(prediction_desc->shape().NumAxes(), 2);
   const int64_t num_out_axes = prediction_desc->shape().NumAxes() - 1;
@@ -37,7 +36,7 @@ Maybe<void> InferTensorDescFn(user_op::InferContext* ctx) {
   const user_op::TensorDesc* label_desc = ctx->TensorDesc4ArgNameAndIndex("label", 0);
   JUST(CheckPredictionLabelDesc(prediction_desc, label_desc));
   user_op::TensorDesc* out_desc = ctx->TensorDesc4ArgNameAndIndex("out", 0);
-  *out_desc = *prediction_desc;
+  *out_desc->mut_is_dynamic() = prediction_desc->is_dynamic();
   *out_desc->mut_shape() = label_desc->shape();
   return Maybe<void>::Ok();
 }
@@ -48,8 +47,27 @@ Maybe<void> InferGradTensorDescFn(user_op::InferContext* ctx) {
   const user_op::TensorDesc* dy_desc = ctx->TensorDesc4ArgNameAndIndex("dy", 0);
   JUST(CheckPredictionLabelDesc(prediction_desc, label_desc));
   CHECK_EQ_OR_RETURN(dy_desc->shape(), label_desc->shape());
+  *ctx->Shape4ArgNameAndIndex("prediction_diff", 0) = *ctx->Shape4ArgNameAndIndex("prob_desc", 0);
+  *ctx->IsDynamic4ArgNameAndIndex("prediction_diff", 0) = *ctx->IsDynamic4ArgNameAndIndex("prob_desc", 0);
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> InferDataType(user_op::InferContext* ctx) {
+  const user_op::TensorDesc* prediction_desc = ctx->TensorDesc4ArgNameAndIndex("prediction", 0);
+  const user_op::TensorDesc* label_desc = ctx->TensorDesc4ArgNameAndIndex("label", 0);
+  CHECK_OR_RETURN(IsIndexDataType(label_desc->data_type()));
+  user_op::TensorDesc* out_desc = ctx->TensorDesc4ArgNameAndIndex("out", 0);
+  *out_desc->mut_data_type() = prediction_desc->data_type();
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> InferDataTypeGrad(user_op::InferContext* ctx) {
+  const user_op::TensorDesc* prediction_desc = ctx->TensorDesc4ArgNameAndIndex("prediction", 0);
+  const user_op::TensorDesc* dy_desc = ctx->TensorDesc4ArgNameAndIndex("dy", 0);
+  const user_op::TensorDesc* label_desc = ctx->TensorDesc4ArgNameAndIndex("label", 0);
+  CHECK_OR_RETURN(IsIndexDataType(label_desc->data_type()));
   CHECK_EQ_OR_RETURN(dy_desc->data_type(), prediction_desc->data_type());
-  *ctx->TensorDesc4ArgNameAndIndex("prediction_diff", 0) = *prediction_desc;
+  *ctx->Dtype4ArgNameAndIndex("prediction_diff", 0) = *ctx->Dtype4ArgNameAndIndex("prediction", 0);
   return Maybe<void>::Ok();
 }
 
@@ -133,7 +151,8 @@ void GenBackwardOpConf4SparseCrossEntropy(const std::string& op_type_name,
         CHECK(label_modifier != nullptr);                                              \
         label_modifier->set_requires_grad(false);                                      \
       })                                                                               \
-      .SetGetSbpFn(GetSbpFn<sbp_sig>);
+      .SetGetSbpFn(GetSbpFn<sbp_sig>)                                                  \
+      .SetInferDataTypeFn(InferDataType);                                              
 
 #define REGISTER_SPAESE_CROSS_ENTROPY_GRAD_USER_OP(op_name, sbp_sig) \
   REGISTER_USER_OP(op_name)                                          \
@@ -143,7 +162,8 @@ void GenBackwardOpConf4SparseCrossEntropy(const std::string& op_type_name,
       .Output("prediction_diff")                                     \
       .Attr<int64_t>("depth")                                        \
       .SetTensorDescInferFn(InferGradTensorDescFn)                   \
-      .SetGetSbpFn(GetSbpFn<sbp_sig>);
+      .SetGetSbpFn(GetSbpFn<sbp_sig>)                                \
+      .SetInferDataTypeFn(InferDataTypeGrad);                            
 
 REGISTER_SPAESE_CROSS_ENTROPY_USER_OP("sparse_cross_entropy", AddSignature);
 REGISTER_SPAESE_CROSS_ENTROPY_USER_OP("sparse_cross_entropy_ms", AddMsSignature);
