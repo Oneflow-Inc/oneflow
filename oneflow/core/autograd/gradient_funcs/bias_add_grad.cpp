5e74381f6a785953260d3bb3eff880aae2f1edf2
hjchen2 <hjchen2>
2021-04-01 23:03:49 +0800

    Refine bias_add grad function.
    Former-commit-id: afca5dd854e13feffa4f6c7b3e7b81f2ab66bc7b

--- oneflow/core/autograd/gradient_funcs/bias_add_grad.cpp
+++ oneflow/core/autograd/gradient_funcs/bias_add_grad.cpp
@@ -13,11 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 #include "oneflow/core/autograd/gradient_funcs/utility.h"
 #include "oneflow/core/framework/op_expr_grad_function.h"
 #include "oneflow/core/framework/op_builder.h"
 #include "oneflow/core/framework/op_expr.h"
+#include "oneflow/core/framework/op_expr_helper.h"
 #include "oneflow/core/framework/op_interpreter_util.h"
 
 namespace oneflow {
@@ -30,37 +30,38 @@ class BiasAdd : public OpExprGradFunction {
     CHECK_NOTNULL_OR_RETURN(fw_op_expr);
     op_name_ = fw_op_expr->op_name();
     bias_add_axis_ = GetAttr<int32_t>(fw_op_expr->proto(), "axis");
-    backward_input_op_ = JUST(OpBuilder("identity", op_name_ + "_input" + FakeGradientOpSuffix)
-                                  .Input("in")
-                                  .Output("out")
-                                  .Build());
+    backward_input_op_ = JUST(op_expr_helper::IdentityOp(GradientOpName(op_name_ + "_input")));
     return Maybe<void>::Ok();
   }
 
   Maybe<void> Capture(OpExprInterpState* ctx, const TensorTuple& inputs,
                       const TensorTuple& outputs) const override {
+    CHECK_EQ_OR_RETURN(inputs.size(), 2);
+    input_requires_grad_ = inputs.at(0)->requires_grad();
+    bias_requires_grad_ = inputs.at(1)->requires_grad();
     return Maybe<void>::Ok();
   }
 
   Maybe<void> Apply(const OpExprInterpState* ctx, const TensorTuple& out_grads,
                     TensorTuple* in_grads) const override {
     const int64_t num_axes = out_grads.at(0)->shape()->NumAxes();
-    if (reduce_axes_vec_.size() != num_axes - 1 || !backward_bias_op_.get()) {
+    if (bias_requires_grad_
+        && (reduce_axes_vec_.size() != num_axes - 1 || !backward_bias_op_.get())) {
       reduce_axes_vec_.clear();
       for (int i = 0; i < num_axes; ++i) {
         if (i != bias_add_axis_) { reduce_axes_vec_.push_back(i); }
       }
-      backward_bias_op_ = JUST(OpBuilder("reduce_sum", op_name_ + "_bias" + FakeGradientOpSuffix)
-                                   .Input("input_tensor")
-                                   .Output("output_tensor")
-                                   .Attr("axis", reduce_axes_vec_)
-                                   .Attr("keepdims", false)
-                                   .Build());
+      backward_bias_op_ = JUST(op_expr_helper::ReduceSumOp(reduce_axes_vec_, /*keepdims=*/false,
+                                                           GradientOpName(op_name_ + "_bias")));
     }
     const auto& interpreter = JUST(OpInterpUtil::GetInterpreter());
     TensorTuple input_grad(1), bias_grad(1);
-    JUST(interpreter->Apply(*backward_input_op_, {out_grads.at(0)}, &input_grad));
-    JUST(interpreter->Apply(*backward_bias_op_, {out_grads.at(0)}, &bias_grad));
+    if (input_requires_grad_) {
+      JUST(interpreter->Apply(*backward_input_op_, {out_grads.at(0)}, &input_grad));
+    }
+    if (bias_requires_grad_) {
+      JUST(interpreter->Apply(*backward_bias_op_, {out_grads.at(0)}, &bias_grad));
+    }
     in_grads->resize(2);
     in_grads->at(0) = input_grad.at(0);
     in_grads->at(1) = bias_grad.at(0);
@@ -70,6 +71,8 @@ class BiasAdd : public OpExprGradFunction {
  private:
   std::string op_name_;
   int32_t bias_add_axis_;
+  mutable bool input_requires_grad_;
+  mutable bool bias_requires_grad_;
   std::shared_ptr<OpExpr> backward_input_op_;
   mutable std::shared_ptr<OpExpr> backward_bias_op_;
   mutable std::vector<int32_t> reduce_axes_vec_;
