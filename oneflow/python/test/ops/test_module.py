1de92978b601d24eb5aae86870a9639b1018ac64
Zailiang <zailiangyu@gmail.com>
2021-04-02 17:26:40 +0800

    delete test tanh case from test_module.py
    Former-commit-id: 487c06dcd5d2459c837be13e2515204edcc2b26a

--- oneflow/python/test/ops/test_module.py
+++ oneflow/python/test/ops/test_module.py
@@ -58,16 +58,6 @@ class TestModule(flow.unittest.TestCase):
         y = relu(x)
         print(y.numpy())
 
-    def test_tanh(test_case):
-        input_arr = np.array([-1, 0, 1]).astype(np.float32)
-        x = flow.Tensor(input_arr)
-
-        tanh = flow.nn.Tanh()
-        y = tanh(x)
-        z = np.tanh(input_arr)
-
-        test_case.assertTrue(np.allclose(y.numpy(), z, rtol=1e-4, atol=1e-4))
-
     def test_load_state_dict(test_case):
         class CustomModule(flow.nn.Module):
             def __init__(self):
