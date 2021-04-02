639eb8c4ad063f5cf309ceb6c9cb9cfca2c1d679
poohRui <yuruil@qq.com>
2021-04-02 15:38:40 +0800

    fix cast to static shape op bug

--- oneflow/user/ops/cast_to_static_shape_op.cpp
+++ oneflow/user/ops/cast_to_static_shape_op.cpp
@@ -43,7 +43,7 @@ REGISTER_USER_OP("cast_to_static_shape")
       return Maybe<void>::Ok();
     })
     .SetInferDataTypeFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      *ctx->Dtype4ArgNameAndIndex("input", 0) = *ctx->Dtype4ArgNameAndIndex("output", 0);
+      *ctx->Dtype4ArgNameAndIndex("output", 0) = *ctx->Dtype4ArgNameAndIndex("input", 0);
       return Maybe<void>::Ok();
     });
 
