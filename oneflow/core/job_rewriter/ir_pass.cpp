d02fe5260baa04eb8f477d7eaa96041de703e4af
jackalcooper <jackalcooper@gmail.com>
2021-04-06 11:44:01 +0000

    fix

--- oneflow/core/job_rewriter/ir_pass.cpp
+++ oneflow/core/job_rewriter/ir_pass.cpp
@@ -46,7 +46,7 @@ class RoundTripOneFlowJobWrapper : public mlir::RoundTripOneFlowJobWrapperInterf
     return job_builder_.ParallelConf4OpName(op_name);
   }
   const ::oneflow::OperatorConf& OpConf4OpName(const std::string& op_name) const {
-    job_builder_.OpConf4OpName(op_name);
+    return job_builder_.OpConf4OpName(op_name);
   }
   std::pair<std::vector<std::string>, std::vector<std::string>> InputBns4OpName(
       const std::string& op_name) const {
