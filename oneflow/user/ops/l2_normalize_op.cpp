2e31a38ef2459ea23aa45aaa82dbe80275f56fcd
poohRui <yuruil@qq.com>
2021-04-06 14:46:15 +0800

    format

--- oneflow/user/ops/l2_normalize_op.cpp
+++ oneflow/user/ops/l2_normalize_op.cpp
@@ -101,8 +101,7 @@ REGISTER_USER_OP("l2_normalize_grad")
       return Maybe<void>::Ok();
     })
     .SetInferDataTypeFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      CHECK_EQ_OR_RETURN(*ctx->Dtype4ArgNameAndIndex("y", 0),
-                         *ctx->Dtype4ArgNameAndIndex("dy", 0));
+      CHECK_EQ_OR_RETURN(*ctx->Dtype4ArgNameAndIndex("y", 0), *ctx->Dtype4ArgNameAndIndex("dy", 0));
       CHECK_EQ_OR_RETURN(*ctx->Dtype4ArgNameAndIndex("y", 0),
                          *ctx->Dtype4ArgNameAndIndex("square_x_sum", 0));
       *ctx->Dtype4ArgNameAndIndex("dx", 0) = *ctx->Dtype4ArgNameAndIndex("dy", 0);
