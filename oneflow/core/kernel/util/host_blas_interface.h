9ae08cbb83e0cfb26e885729e54e2e2c87d665e9
guo ran <360112263@qq.com>
2021-04-08 22:01:18 +0800

    Matmul add alpha Attr (#4603)
    * matmul add alpha attr
    
    * rm useless
    
    * refine
    
    Former-commit-id: 18ecf3c56235c4dcf4811e9fee5842de7a63a148

--- oneflow/core/kernel/util/host_blas_interface.h
+++ oneflow/core/kernel/util/host_blas_interface.h
@@ -26,24 +26,20 @@ class Blob;
 
 template<>
 struct BlasIf<DeviceType::kCPU> {
-  static void BlobGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_TRANSPOSE trans_b,
-                       float alpha, float beta, const Blob* a, const Blob* b, Blob* c);
-  static void BlobGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_TRANSPOSE trans_b,
-                       double alpha, double beta, const Blob* a, const Blob* b, Blob* c);
   static void OFGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_TRANSPOSE trans_b,
-                     const int m, const int n, const int k, const float alpha, const float* a,
-                     const float* b, const float beta, float* c);
+                     const int m, const int n, const int k, const double alpha, const float* a,
+                     const float* b, const double beta, float* c);
   static void OFGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a, enum CBLAS_TRANSPOSE trans_b,
                      const int m, const int n, const int k, const double alpha, const double* a,
                      const double* b, const double beta, double* c);
   static void OFBatchedGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a,
                             enum CBLAS_TRANSPOSE trans_b, const int batch_size, const int m,
-                            const int n, const int k, const float alpha, const float* a,
-                            const float* b, const float beta, float* c, float** buf);
+                            const int n, const int k, const double alpha, const float* a,
+                            const float* b, const double beta, float* c);
   static void OFBatchedGemm(DeviceCtx* ctx, enum CBLAS_TRANSPOSE trans_a,
                             enum CBLAS_TRANSPOSE trans_b, const int batch_size, const int m,
                             const int n, const int k, const double alpha, const double* a,
-                            const double* b, const double beta, double* c, double** buf);
+                            const double* b, const double beta, double* c);
 
   static void Axpy(DeviceCtx* ctx, const int n, const float alpha, const float* x, const int incx,
                    float* y, const int incy);
