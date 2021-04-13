695697389f62efa863ddd27cc9607757159bbccb
clackhan <han_binbin@163.com>
2021-04-13 17:36:50 +0800

    fix dead lock bug

--- oneflow/core/job/env_global_objects_scope.cpp
+++ oneflow/core/job/env_global_objects_scope.cpp
@@ -129,14 +129,18 @@ Maybe<void> EnvGlobalObjectsScope::Init(const EnvProto& env_proto) {
   Global<EagerNcclCommMgr>::New();
   Global<CudnnConvAlgoCache>::New();
 #endif
-  Global<EpollCommNet>::New();
-  Global<Transport>::New();
+  if (!Global<ResourceDesc, ForSession>::Get()->enable_dry_run()) {
+    Global<EpollCommNet>::New();
+    Global<Transport>::New();
+  }
   return Maybe<void>::Ok();
 }
 
 EnvGlobalObjectsScope::~EnvGlobalObjectsScope() {
-  Global<Transport>::Delete();
-  Global<EpollCommNet>::Delete();
+  if (!Global<ResourceDesc, ForSession>::Get()->enable_dry_run()) {
+    Global<Transport>::Delete();
+    Global<EpollCommNet>::Delete();
+  }
 #ifdef WITH_CUDA
   Global<CudnnConvAlgoCache>::Delete();
   Global<EagerNcclCommMgr>::Delete();
