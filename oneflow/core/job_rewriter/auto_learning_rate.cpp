fded90ef55cad231dca3635f15af4d26538c7a17
Xuefei Jiang <sc.xfjiang@gmail.com>
2021-04-13 16:30:30 +0800

    Xfjiang/dev save lr to csv (#4633)
    * save lr to csv
    
    * fix typo
    
    * add header
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>
    Former-commit-id: 1691120e9f1807ebd779d6a113b116b2c00bae75

--- oneflow/core/job_rewriter/auto_learning_rate.cpp
+++ oneflow/core/job_rewriter/auto_learning_rate.cpp
@@ -48,38 +48,21 @@ Maybe<void> AutoLearningRate::Apply(const OpGraph& op_graph, Job* job) const {
         op_graph.OpNode4OpName(GenLogicalBlobId(train_conf.train_step_lbn()).op_name());
     CHECK_OR_RETURN(op_node != nullptr) << "op node not found in op graph, op name: " << op_name;
     const ParallelConf& parallel_conf = op_node->parallel_desc().parallel_conf();
-    if (optimizer_conf.has_warmup_conf() || optimizer_conf.has_learning_rate_decay()) {
-      OperatorConf schedule_op_conf{};
-      schedule_op_conf.set_name(op_name);
-      LearningRateScheduleOpConf* schedule_conf =
-          schedule_op_conf.mutable_learning_rate_schedule_conf();
-      schedule_conf->set_train_step(train_conf.train_step_lbn());
-      schedule_conf->set_learning_rate(optimizer_conf.base_learning_rate());
-      schedule_conf->set_out("out");
-      if (optimizer_conf.has_warmup_conf()) {
-        *schedule_conf->mutable_warmup_conf() = optimizer_conf.warmup_conf();
-      }
-      if (optimizer_conf.has_learning_rate_decay()) {
-        *schedule_conf->mutable_learning_rate_decay() = optimizer_conf.learning_rate_decay();
-      }
-      schedule_op_conf.set_scope_symbol_id(op_node->op().op_conf().scope_symbol_id());
-      job_builder.AddOps(parallel_conf, {schedule_op_conf});
-      return GenLogicalBlobName(op_name, schedule_conf->out());
-    } else {
-      const auto constant_op =
-          user_op::UserOpConfWrapperBuilder(op_name)
-              .Op("constant")
-              .Attr<double>("floating_value", optimizer_conf.base_learning_rate())
-              .Attr<int64_t>("integer_value", 0)
-              .Attr<bool>("is_floating_value", true)
-              .Attr<DataType>("dtype", DataType::kFloat)
-              .Attr<Shape>("shape", Shape({1}))
-              .Output("out")
-              .ScopeSymbolId(op_node->op().op_conf().scope_symbol_id())
-              .Build();
-      job_builder.AddOps(parallel_conf, {constant_op.op_conf()});
-      return constant_op.output("out", 0);
+    OperatorConf schedule_op_conf{};
+    schedule_op_conf.set_name(op_name);
+    auto* schedule_conf = schedule_op_conf.mutable_learning_rate_schedule_conf();
+    schedule_conf->set_train_step(train_conf.train_step_lbn());
+    schedule_conf->set_learning_rate(optimizer_conf.base_learning_rate());
+    schedule_conf->set_out("out");
+    if (optimizer_conf.has_warmup_conf()) {
+      *schedule_conf->mutable_warmup_conf() = optimizer_conf.warmup_conf();
     }
+    if (optimizer_conf.has_learning_rate_decay()) {
+      *schedule_conf->mutable_learning_rate_decay() = optimizer_conf.learning_rate_decay();
+    }
+    schedule_op_conf.set_scope_symbol_id(op_node->op().op_conf().scope_symbol_id());
+    job_builder.AddOps(parallel_conf, {schedule_op_conf});
+    return GenLogicalBlobName(op_name, schedule_conf->out());
   };
   FOR_RANGE(int64_t, i, 0, train_conf.optimizer_conf_size()) {
     const auto& optimizer_conf = train_conf.optimizer_conf(i);
