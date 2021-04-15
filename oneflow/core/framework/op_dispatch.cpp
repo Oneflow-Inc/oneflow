448aabe4957fb8a4401db4b948722d43987dc701
hjchen2 <hjchen2>
2021-04-15 17:07:58 +0800

    Dev autograd interpreter and rewrite batch gather grad function.

---
+++ oneflow/core/framework/op_dispatch.cpp
@@ -0,0 +1,38 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "oneflow/core/framework/op_dispatch.h"
+#include "oneflow/core/framework/op_interpreter/op_interpreter_util.h"
+
+namespace oneflow {
+namespace one {
+
+template<>
+Maybe<TensorTuple> Dispatch(const OpExpr& op_expr, const TensorTuple& inputs,
+                            const AttrValueMap& attrs) {
+  auto outputs = std::make_shared<TensorTuple>(op_expr.output_num());
+  JUST(OpInterpUtil::GetInterpreter())->Apply(op_expr, inputs, outputs.get(), attrs);
+  return outputs;
+}
+
+template<>
+Maybe<Tensor> Dispatch<Tensor>(const OpExpr& op_expr, const TensorTuple& inputs,
+                               const AttrValueMap& attrs) {
+  return JUST(Dispatch<TensorTuple>(op_expr, inputs, attrs))->at(0);
+}
+
+}  // namespace one
+}  // namespace oneflow
