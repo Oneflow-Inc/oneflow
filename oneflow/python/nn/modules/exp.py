aa12b446d8ed28c0ef0c7871c06abded8fd0ec9f
BBuf <1182563586@qq.com>
2021-04-16 14:20:53 +0800

    add nn.modules.exp

---
+++ oneflow/python/nn/modules/exp.py
@@ -0,0 +1,38 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import oneflow as flow
+from oneflow.python.nn.module import Module
+from oneflow.python.oneflow_export import oneflow_export
+from oneflow.python.framework.tensor import register_tensor_op_by_module
+from oneflow.python.framework.tensor import register_op_by_module
+
+@oneflow_export("Exp")
+@register_tensor_op_by_module("exp")
+@register_op_by_module("exp")
+class Exp(Module):
+   r"""
+    Returns a new tensor with the exp of the elements of :attr:`input`.
+    .. math::
+        \text{out}_{i} = \exp(\text{input}_{i})
+    Args:
+        {input}
+    Keyword args:
+        {out}
+    """
+
+    def __init__(self) -> None:
+            super().__init__()
+        self._op = flow.builtin_op("exp").Input("x").Output("y").Build()
+
+    def forward(self, x):
+        return self._op(x)[0]
