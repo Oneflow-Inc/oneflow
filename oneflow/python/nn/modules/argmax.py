32bf6b7dd32ea0a4317d8c74176b8fc43d276735
BBuf <1182563586@qq.com>
2021-04-16 15:23:28 +0800

    add exp and argmax

--- oneflow/python/nn/modules/argmax.py
+++ oneflow/python/nn/modules/argmax.py
@@ -16,31 +16,23 @@ from oneflow.python.oneflow_export import oneflow_export
 from oneflow.python.framework.tensor import register_tensor_op_by_module
 from oneflow.python.framework.tensor import register_op_by_module
 
-@oneflow_export("Exp")
-@register_tensor_op_by_module("exp")
-@register_op_by_module("exp")
-class Exp(Module):
+@oneflow_export("Argmax")
+@register_tensor_op_by_module("argmax")
+@register_op_by_module("argmax")
+class Argmax(Module):
    r"""
-    Returns a new tensor with the exp of the elements of :attr:`x`.
-    .. math::
-        \text{y}_{i} = \exp(\text{x}_{i})
+    Returns the largest value of the :attr:`input` at specified axis.
     Args:
-        {x}
-    
-    For example: 
-    .. code-block:: python 
-        import numpy as np
-        import oneflow as flow
-        x = flow.Tensor(np.random.rand(2, 3, 4))
-        y = x.exp()
-        print(y.shape)
-        # (2, 3, 4)
-
+        {input}
+    Keyword args:
+        {axis}
     """
 
     def __init__(self) -> None:
             super().__init__()
-        self._op = flow.builtin_op("exp").Input("x").Output("y").Build()
+        self._op = flow.builtin_op("argmax").Input("input").Output("output").Build()
 
-    def forward(self, x):
-        return self._op(x)[0]
+    def forward(self, input, axis=-1):
+        assert axis <= len(input.size()), "axis should <= length of input tensor!"
+        self._op = self._op.Attr("axis", axis).Build()
+        return self._op(input)[0]
