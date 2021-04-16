5439c6126d19084623fd5f68b45c72cae5504870
leaves-zwx <kunta0932@gmail.com>
2021-04-16 16:27:06 +0800

    mv bgemm to self_att*.cu

--- oneflow/user/kernels/fused_self_attention_query_mul_key_and_value_kernel.cu
+++ oneflow/user/kernels/fused_self_attention_query_mul_key_and_value_kernel.cu
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/framework/framework.h"
-#include "oneflow/core/cuda/bgemm.h"
 #include "oneflow/user/kernels/slice_util.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
 
@@ -22,13 +21,116 @@ namespace oneflow {
 
 namespace {
 
+inline cublasOperation_t GetCublasOp(char op) {
+  switch (op) {
+    case 'n':
+    case 'N': {
+      return CUBLAS_OP_N;
+    }
+    case 't':
+    case 'T': {
+      return CUBLAS_OP_T;
+    }
+    case 'c':
+    case 'C': {
+      return CUBLAS_OP_C;
+    }
+    default: {
+      UNIMPLEMENTED();
+    }
+  }
+  return CUBLAS_OP_N;
+}
+
+template<typename T>
+struct CudaDataTypeTrait;
+
+template<>
+struct CudaDataTypeTrait<float> {
+  const static cudaDataType_t value = CUDA_R_32F;
+};
+
+template<>
+struct CudaDataTypeTrait<half> {
+  const static cudaDataType_t value = CUDA_R_16F;
+};
+
+template<typename T>
+void CublasBatchGemm(cublasHandle_t handle, char transa, char transb, int64_t m, int64_t n,
+                     int64_t k, T alpha, const T* a, int64_t lda, int64_t stridea, const T* b,
+                     int64_t ldb, int64_t strideb, T beta, T* c, int64_t ldc, int64_t stridec,
+                     int64_t batch_size) {
+  cublasOperation_t opa = GetCublasOp(transa);
+  cublasOperation_t opb = GetCublasOp(transb);
+  if (CUDA_VERSION >= 9010 && GetCudaSmVersion() >= 500) {
+#if CUDA_VERSION >= 9010
+    cudaDataType_t data_type = CudaDataTypeTrait<T>::value;
+    OF_CUBLAS_CHECK(cublasGemmStridedBatchedEx(
+        handle, opa, opb, m, n, k, reinterpret_cast<const void*>(&alpha),
+        reinterpret_cast<const void*>(a), data_type, lda, stridea, reinterpret_cast<const void*>(b),
+        data_type, ldb, strideb, reinterpret_cast<const void*>(&beta), reinterpret_cast<void*>(c),
+        data_type, ldc, stridec, batch_size, data_type, CUBLAS_GEMM_DEFAULT));
+#else
+    UNIMPLEMENTED();
+#endif
+  } else {
+    cublas_gemmStridedBatched<T>(handle, opa, opb, m, n, k, &alpha, a, ldb, stridea, b, ldb,
+                                 strideb, &beta, c, ldc, stridec, batch_size);
+  }
+}
+
+#if CUDA_VERSION >= 9010
+
+template<>
+void CublasBatchGemm<half>(cublasHandle_t handle, char transa, char transb, int64_t m, int64_t n,
+                           int64_t k, half alpha, const half* a, int64_t lda, int64_t stridea,
+                           const half* b, int64_t ldb, int64_t strideb, half beta, half* c,
+                           int64_t ldc, int64_t stridec, int64_t batch_size) {
+  using comp_t = float;
+  cublasOperation_t opa = GetCublasOp(transa);
+  cublasOperation_t opb = GetCublasOp(transb);
+
+  if (GetCudaSmVersion() >= 500) {
+    float alpha_f = static_cast<comp_t>(alpha);
+    float beta_f = static_cast<comp_t>(beta);
+#if CUDA_VERSION >= 11000
+    cublasGemmAlgo_t algo = CUBLAS_GEMM_DEFAULT;
+#else
+    cublasGemmAlgo_t algo = CUBLAS_GEMM_DEFAULT_TENSOR_OP;
+#endif
+    cudaDataType_t data_type = CudaDataTypeTrait<half>::value;
+    cudaDataType_t comp_type = CudaDataTypeTrait<comp_t>::value;
+    OF_CUBLAS_CHECK(cublasGemmStridedBatchedEx(
+        handle, opa, opb, m, n, k, &alpha_f, reinterpret_cast<const void*>(a), data_type, lda,
+        stridea, reinterpret_cast<const void*>(b), data_type, ldb, strideb, &beta_f,
+        reinterpret_cast<void*>(c), data_type, ldc, stridec, batch_size, comp_type, algo));
+  } else {
+    cublas_gemmStridedBatched<half>(handle, opa, opb, m, n, k, &alpha, a, lda, stridea, b, ldb,
+                                    strideb, &beta, c, ldc, stridec, batch_size);
+  }
+}
+
+template<>
+void CublasBatchGemm<float16>(cublasHandle_t handle, char transa, char transb, int64_t m, int64_t n,
+                              int64_t k, float16 alpha, const float16* a, int64_t lda,
+                              int64_t stridea, const float16* b, int64_t ldb, int64_t strideb,
+                              float16 beta, float16* c, int64_t ldc, int64_t stridec,
+                              int64_t batch_size) {
+  CublasBatchGemm<half>(handle, transa, transb, m, n, k, static_cast<half>(alpha),
+                        reinterpret_cast<const half*>(a), lda, stridea,
+                        reinterpret_cast<const half*>(b), ldb, strideb, static_cast<half>(beta),
+                        reinterpret_cast<half*>(c), ldc, stridec, batch_size);
+}
+
+#endif  // CUDA_VERSION >= 9010
+
 template<typename T>
 void bgemm(DeviceCtx* ctx, char opa, char opb, int64_t m, int64_t n, int64_t k, float alpha,
            const T* a, int64_t lda, int64_t stridea, const T* b, int64_t ldb, int64_t strideb,
            float beta, T* c, int64_t ldc, int64_t stridec, int64_t batch_size) {
   // swap m and n, a and b to convert from row-major to col-major
-  cuda::blas::bgemm<T>(ctx->cublas_pmd_handle(), opb, opa, n, m, k, static_cast<T>(alpha), b, ldb,
-                       strideb, a, lda, stridea, static_cast<T>(beta), c, ldc, stridec, batch_size);
+  CublasBatchGemm<T>(ctx->cublas_pmh_handle(), opb, opa, n, m, k, static_cast<T>(alpha), b, ldb,
+                     strideb, a, lda, stridea, static_cast<T>(beta), c, ldc, stridec, batch_size);
 }
 
 SliceParams ConstructSliceParams4Value(int64_t seq_len, int64_t batch_size, int64_t num_heads,
@@ -57,12 +159,10 @@ SliceParams ConstructSliceParams4Value(int64_t seq_len, int64_t batch_size, int6
 }
 
 template<typename T>
-void transpose(DeviceCtx* ctx, const user_op::Tensor* in, const std::vector<int32_t>& perm,
-               user_op::Tensor* out) {
+void transpose(DeviceCtx* ctx, const ShapeView& in_shape, const ShapeView& out_shape,
+               const std::vector<int32_t>& perm, const T* in, T* out) {
   using PackType = int64_t;
-  constexpr size_t pack_size = sizeof(PackType) / sizeof(T);
-  const ShapeView& in_shape = in->shape();
-  const ShapeView& out_shape = out->shape();
+  const size_t pack_size = sizeof(PackType) / sizeof(T);
   int64_t in_last_dim = in_shape.At(in_shape.NumAxes() - 1);
   int64_t out_last_dim = out_shape.At(out_shape.NumAxes() - 1);
   if (pack_size != 1 && perm.back() == perm.size() - 1 && in_last_dim % pack_size == 0) {
@@ -77,12 +177,11 @@ void transpose(DeviceCtx* ctx, const user_op::Tensor* in, const std::vector<int3
     const Shape packed_out_shape(packed_out_dim_vec);
     NewKernelUtil<DeviceType::kGPU>::Transpose(ctx, packed_in_shape.NumAxes(), packed_in_shape,
                                                packed_out_shape, perm, packed_in_shape.elem_cnt(),
-                                               reinterpret_cast<const PackType*>(in->dptr<T>()),
-                                               reinterpret_cast<PackType*>(out->mut_dptr<T>()));
+                                               reinterpret_cast<const PackType*>(in),
+                                               reinterpret_cast<PackType*>(out));
   } else {
     NewKernelUtil<DeviceType::kGPU>::Transpose(ctx, in_shape.NumAxes(), in_shape, out_shape, perm,
-                                               in_shape.elem_cnt(), in->dptr<T>(),
-                                               out->mut_dptr<T>());
+                                               in_shape.elem_cnt(), in, out);
   }
 }
 
@@ -118,7 +217,9 @@ class FusedSelfAttentionQueryMulKeyAndValueGpuKernel final : public user_op::OpK
     SliceKernelUtil<DeviceType::kGPU, T>::Forward(ctx->device_ctx(), params, h_tensor->dptr<T>(),
                                                   tmp_v_tensor->mut_dptr<T>());
     // v from (s, b, n, h) transpose to (b, n, s, h)
-    transpose<T>(ctx->device_ctx(), tmp_v_tensor, {1, 2, 0, 3}, v_tensor);
+    Shape value_shape({seq_len, batch_size, num_heads, head_size});
+    transpose<T>(ctx->device_ctx(), value_shape, v_tensor->shape(), {1, 2, 0, 3},
+                 tmp_v_tensor->dptr<T>(), v_tensor->mut_dptr<T>());
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
@@ -131,27 +232,31 @@ class FusedSelfAttentionQueryMulKeyAndValueGradGpuKernel final : public user_op:
 
  private:
   void Compute(user_op::KernelComputeContext* ctx) const override {
+    const user_op::Tensor* v_grad_tensor = ctx->Tensor4ArgNameAndIndex("value_grad", 0);
+    const user_op::Tensor* qmk_grad_tensor = ctx->Tensor4ArgNameAndIndex("query_mul_key_grad", 0);
+    const user_op::Tensor* h_tensor = ctx->Tensor4ArgNameAndIndex("hidden_states", 0);
+    user_op::Tensor* tmp_v_tensor = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
     user_op::Tensor* h_grad_tensor = ctx->Tensor4ArgNameAndIndex("hidden_states_grad", 0);
+
+    float alpha = ctx->Attr<float>("alpha");
     int64_t seq_len = h_grad_tensor->shape().At(0);
     int64_t batch_size = h_grad_tensor->shape().At(1);
     int64_t hidden_size = h_grad_tensor->shape().At(2);
-    int64_t head_size = ctx->Attr<int64_t>("head_size");
+    int64_t num_heads = v_grad_tensor->shape().At(1);
+    int64_t head_size = v_grad_tensor->shape().At(3);
     int64_t ld = batch_size * hidden_size;
     int64_t stride = 3 * head_size;
-    int64_t num_heads = hidden_size / stride;
+    CHECK_EQ(hidden_size, num_heads * stride);
 
-    const user_op::Tensor* v_grad_tensor = ctx->Tensor4ArgNameAndIndex("value_grad", 0);
-    user_op::Tensor* tmp_v_tensor = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
     // transpose from (b, n, s, h) to (s, b, n, h)
-    transpose<T>(ctx->device_ctx(), v_grad_tensor, {1, 2, 0, 3}, tmp_v_tensor);
+    Shape value_shape({seq_len, batch_size, num_heads, head_size});
+    transpose<T>(ctx->device_ctx(), v_grad_tensor->shape(), value_shape, {1, 2, 0, 3},
+                 v_grad_tensor->dptr<T>(), tmp_v_tensor->mut_dptr<T>());
     // slice v grad
     SliceParams params = ConstructSliceParams4Value(seq_len, batch_size, num_heads, head_size);
     SliceKernelUtil<DeviceType::kGPU, T>::Backward(
         ctx->device_ctx(), params, tmp_v_tensor->dptr<T>(), h_grad_tensor->mut_dptr<T>());
 
-    float alpha = ctx->Attr<float>("alpha");
-    const user_op::Tensor* qmk_grad_tensor = ctx->Tensor4ArgNameAndIndex("query_mul_key_grad", 0);
-    const user_op::Tensor* h_tensor = ctx->Tensor4ArgNameAndIndex("hidden_states", 0);
     // grad_q = grad_qmk * k
     // (b, n, sq, sk) x (b, n, sk, h) -> (b, n, s, h) <= (s, b, n, h) <= (s, b, n, 3, h)
     bgemm<T>(ctx->device_ctx(), 'N', 'N', seq_len, head_size, seq_len, alpha,
