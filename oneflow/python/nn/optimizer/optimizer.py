d166e211bd1363b4a1e290e5ad11a0ab8ec140af
wyg1997 <wyg19970408@gmail.com>
2021-04-19 22:24:29 +0800

    refactor(Optim): refactor optimizer

--- oneflow/python/nn/optimizer/optimizer.py
+++ oneflow/python/nn/optimizer/optimizer.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 
 from collections import OrderedDict
-from typing import List, Dict, Callable, Union
+from typing import List, Dict, Callable, Union, Any
 
 import oneflow as flow
 from oneflow.python.oneflow_export import oneflow_export
@@ -23,12 +23,38 @@ from oneflow.python.nn.parameter import Parameter
 from oneflow.python.framework.tensor import Tensor
 
 
+class ParamGroup(object):
+    def __init__(
+        self,
+        parameters: Union[List[Parameter], Dict[str, Any]],
+        default_options: Dict,
+    ):
+        if isinstance(parameters, list):
+            self._parameters = parameters
+            self._options = default_options
+        else:  # Dict
+            assert "param" in parameters
+            self._parameters = parameters["param"]
+            self._options = default_options
+            for key, value in default_options.items():
+                if key in parameters:
+                    parameters[key] = value
+
+    @property
+    def options(self):
+        return self._options
+
+    @property
+    def parameters(self):
+        return self._parameters
+
+
 class Optimizer(object):
     def __init__(self):
-        # self._parameters = OrderedDict()
-        self._parameters = list()
-        self._default_state = OrderedDict()
-        self._default_state["step"] = 0
+        self._param_groups = list()
+        self._default_options = dict()
+        self._state = dict()
+        self._state["step"] = 0
         self._op = None
 
     def add_param_group(self, param_group) -> None:
@@ -62,45 +88,75 @@ class SGD(Optimizer):
         self,
         parameters: Union[List[Parameter], List[Dict]],
         lr: float,
-        scale: float = 1.0,
         momentum: float = 0.0,
+        scale: float = 1.0,
     ):
         super().__init__()
-        assert lr >= 0.0, f"Invalid learning rate: {lr}"
-        self._default_state["lr"] = lr
-        self._default_state["scale"] = scale
-        # self._default_state["momentum"] = momentum # not use now
+        assert lr > 0.0, f"Invalid learning rate: {lr}"
+        assert momentum >= 0.0, f"Invalid momentum: {momentum}"
+        assert scale > 0.0, f"Invalid scale factor: {scale}"
 
-        self._op = (
-            flow.builtin_op("sgd_update")
-            .Input("model")
-            .Input("model_diff")
-            .Input("learning_rate")
-            .Attr("scale", self._default_state["scale"])
-            .Attr("weight_decay", 0.0)
-            .Attr("l1", 0.0)
-            .Attr("l2", 0.0)
-            .Build()
-        )
+        self._default_options = dict()
+        self._default_options["lr"] = lr
+        self._default_options["scale"] = scale
+        if momentum != 0.0:
+            self._default_options["momentum"] = momentum
 
-        for param in parameters:
-            if isinstance(param, Parameter):
-                self._parameters.append(param)
-            else:  # Dict
-                assert "param" in param
-                self._parameters.append(param["param"])
-                if any(x in param for x in ["lr", "momentum"]):
-                    raise NotImplementedError(
-                        "Not support specifying per-parameter options now"
-                    )
+        # Add parameters
+        if len(parameters) == 0:
+            pass
+        elif isinstance(parameters[0], Parameter):
+            self._param_groups.append(ParamGroup(parameters, self._default_options))
+        else:  # List[Dict]
+            for param in parameters:
+                self._param_groups.append(param, self._default_options)
+
+        for param_group in self._param_groups:
+            for param in param_group:
+                assert param.is_leaf, "parameters must be leaf tensor"
+                self._state[param] = dict()
+
+        if "momentum" in self._default_options.keys():
+            self._op = (
+                flow.builtin_op("momentum_update")
+                .Input("model")
+                .Input("model_diff")
+                .Input("learning_rate")
+                .Input("momentum")
+                .Attr("scale", self._default_options["scale"])
+                .Attr("l1", 0.0)
+                .Attr("l2", 0.0)
+                .Attr("beta", self._default_options["momentum"])
+                .Attr("weight_decay", 0.0)
+                .Build()
+            )
+        else:
+            self._op = (
+                flow.builtin_op("sgd_update")
+                .Input("model")
+                .Input("model_diff")
+                .Input("learning_rate")
+                .Attr("scale", self._default_options["scale"])
+                .Attr("weight_decay", 0.0)
+                .Attr("l1", 0.0)
+                .Attr("l2", 0.0)
+                .Build()
+            )
 
     def step(self, closure: Callable = None):
         loss = None
         if closure is not None:
             loss = closure()
-        lr_tensor = flow.Tensor([self._default_state["lr"]])
-        for param in self._parameters:
-            if param.grad is not None:
-                self._op(param, param.grad, lr_tensor)
-        self._default_state["step"] = self._default_state["step"] + 1
+
+        for param_group in self._param_groups:
+            lr_tensor = flow.Tensor([param_group.options["lr"]])
+            for param in param_group:
+                if param.grad is None:
+                    continue
+                if "momentum" in self._default_options:
+                    raise NotImplementedError()
+                else:
+                    self._op(param, param.grad, lr_tensor)
+
+        self._state["step"] = self._state["step"] + 1
         return loss
