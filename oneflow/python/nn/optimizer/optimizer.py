cc892f44bf4497bf05ef11fd7b738d4b5d1537c0
wyg1997 <wyg19970408@gmail.com>
2021-04-20 10:41:34 +0800

    fix(Optim): fix bugs

--- oneflow/python/nn/optimizer/optimizer.py
+++ oneflow/python/nn/optimizer/optimizer.py
@@ -15,7 +15,8 @@ limitations under the License.
 """
 
 from collections import OrderedDict
-from typing import List, Dict, Callable, Union, Any
+from typing import List, Dict, Callable, Union, Any, Iterator
+from types import GeneratorType
 
 import oneflow as flow
 from oneflow.python.oneflow_export import oneflow_export
@@ -26,10 +27,10 @@ from oneflow.python.framework.tensor import Tensor
 class ParamGroup(object):
     def __init__(
         self,
-        parameters: Union[List[Parameter], Dict[str, Any]],
+        parameters: Union[Iterator[Parameter], Dict[str, Any]],
         default_options: Dict,
     ):
-        if isinstance(parameters, list):
+        if isinstance(parameters, GeneratorType):
             self._parameters = parameters
             self._options = default_options
         else:  # Dict
@@ -46,7 +47,8 @@ class ParamGroup(object):
 
     @property
     def parameters(self):
-        return self._parameters
+        for param in self._parameters:
+            yield param
 
 
 class Optimizer(object):
@@ -70,12 +72,13 @@ class Optimizer(object):
         raise NotImplementedError()
 
     def zero_grad(self, set_to_none: bool = False):
-        for param in self._parameters:
-            if set_to_none:
-                param.grad = None
-            else:
-                param.grad.fill_(0)
-                # param.grad.zeros_()
+        for param_group in self._param_groups:
+            for param in param_group.parameters:
+                if set_to_none:
+                    param.grad = None
+                else:
+                    param.grad.fill_(0)
+                    # param.grad.zeros_()
 
 
 @oneflow_export("optim.SGD")
@@ -86,7 +89,7 @@ class SGD(Optimizer):
 
     def __init__(
         self,
-        parameters: Union[List[Parameter], List[Dict]],
+        parameters: Union[Iterator[Parameter], List[Dict]],
         lr: float,
         momentum: float = 0.0,
         scale: float = 1.0,
@@ -103,16 +106,14 @@ class SGD(Optimizer):
             self._default_options["momentum"] = momentum
 
         # Add parameters
-        if len(parameters) == 0:
-            pass
-        elif isinstance(parameters[0], Parameter):
+        if isinstance(parameters, GeneratorType):
             self._param_groups.append(ParamGroup(parameters, self._default_options))
         else:  # List[Dict]
             for param in parameters:
-                self._param_groups.append(param, self._default_options)
+                self._param_groups.append(ParamGroup(param, self._default_options))
 
         for param_group in self._param_groups:
-            for param in param_group:
+            for param in param_group.parameters:
                 assert param.is_leaf, "parameters must be leaf tensor"
                 self._state[param] = dict()
 
@@ -150,7 +151,7 @@ class SGD(Optimizer):
 
         for param_group in self._param_groups:
             lr_tensor = flow.Tensor([param_group.options["lr"]])
-            for param in param_group:
+            for param in param_group.parameters:
                 if param.grad is None:
                     continue
                 if "momentum" in self._default_options:
