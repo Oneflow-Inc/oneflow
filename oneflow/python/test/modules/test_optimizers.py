9bfad5691cbf0e5ca31fe7de0aec7dfe01d6e31c
Ldpe2G <liangdepeng@gmail.com>
2021-04-22 14:43:33 +0800

    code format
    Former-commit-id: 06ea1bfb0c2d310172f16f4a977b6ee8541de79f

--- oneflow/python/test/modules/test_optimizers.py
+++ oneflow/python/test/modules/test_optimizers.py
@@ -37,7 +37,9 @@ def compare_with_numpy_sgd(
         x = Parameter(flow.Tensor(init_value))
         param_list = list()
         param_list.append(x)
-        sgd = flow.optim.SGD([{"param": param_list}], lr=learning_rate, momentum=momentum, scale=scale)
+        sgd = flow.optim.SGD(
+            [{"param": param_list}], lr=learning_rate, momentum=momentum, scale=scale
+        )
 
         def train_one_iter(grad):
             grad_tensor = flow.Tensor(grad, requires_grad=False)
