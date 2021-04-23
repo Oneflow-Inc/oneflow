5fa0265cd82dfc40220d6d3f3a240e93dfcee8b3
jackalcooper <jackalcooper@gmail.com>
2021-04-23 08:15:08 +0000

    only insert bn parallel_conf if different from op

--- oneflow/core/operator/operator.cpp
+++ oneflow/core/operator/operator.cpp
@@ -1182,7 +1182,9 @@ Maybe<void> Operator::ToOpAttribute(OpAttribute* op_attribute) const {
   if (bn2parallel_desc_) {
     auto* map = op_attribute->mutable_parallel_conf_signature()->mutable_bn_in_op2parallel_conf();
     for (const auto& pair : *bn2parallel_desc_) {
-      (*map)[pair.first] = pair.second->parallel_conf();
+      if (op_parallel_desc_->parallel_conf() != pair.second->parallel_conf()) {
+        (*map)[pair.first] = pair.second->parallel_conf();
+      }
     }
   }
   if (op_parallel_desc_ && bn2parallel_desc_) {
