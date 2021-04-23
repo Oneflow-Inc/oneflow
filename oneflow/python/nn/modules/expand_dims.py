df1c9170a40b54a537e6731467e3c93f72509a02
BBuf <1182563586@qq.com>
2021-04-23 17:03:14 +0800

    add expand_dims module docs

--- oneflow/python/nn/modules/expand_dims.py
+++ oneflow/python/nn/modules/expand_dims.py
@@ -21,11 +21,33 @@ from oneflow.python.framework.tensor import register_op_by_module
 from typing import Optional
 
 
-@oneflow_export("ExpandDims")
+@oneflow_export("Expand_Dims")
 @register_tensor_op_by_module("tmp.expand_dims")
 @register_op_by_module("tmp.expand_dims")
-class ExpandDims(Module):
-    r"""
+class Expand_Dims(Module):
+    """This operator inserts a dimention at the specified axis in the input Tensor.
+    The size of new dimension can only be 1, and the amount of element in return value is the same as Tensor `input`.
+
+    Args:
+        input (oneflow.Tensor): The input Tensor.
+        axis (int): The specified dimension index.
+        name (Optional[str], optional): The name for the operation. Defaults to None.
+
+    Returns:
+        oneflow.Tensor: The result Tensor.
+
+    For example:
+
+    .. code-block:: python
+
+        import numpy as np
+        import oneflow as flow
+
+        input = flow.Tensor(np.random.randn(2, 6, 5), dtype=flow.float32)
+        out = flow.tmp.expand_dims(input, axis=-1).numpy().shape
+
+        # out (2, 6, 5, 1)
+
     """
 
     def __init__(self, axis: int, name: Optional[str] = None,) -> None:
