99d45d877b52a72146730b26eb52e6cf1b1c4bed
Yurui Li <32978179+poohRui@users.noreply.github.com>
2021-04-26 13:13:33 +0800

    fix mirrored eager tensor with empty parallel desc (#4670)
    * fix empty parallel desc
    
    * refactor device
    
    * format
    
    * minor fix
    
    * format
    
    * add default environment
    
    * python interface for default environment
    
    * remove mutex
    
    * move hash value to device
    
    * refine
    
    * remove env global object scope
    
    * minor fix
    
    * format
    
    * refine device
    
    * recursively delete log file in default mode
    
    * add directory for default log
    
    * revert
    
    * fix duplicate methods
    
    * try to fix multiprocess faill
    
    * add default init env
    
    * minor fix
    
    * rename
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/api/python/env/env.cpp
+++ oneflow/api/python/env/env.cpp
@@ -26,6 +26,7 @@ ONEFLOW_API_PYBIND11_MODULE("", m) {
 
   m.def("IsEnvInited", &IsEnvInited);
   m.def("InitEnv", &InitEnv);
+  m.def("InitDefaultEnv", &InitDefaultEnv);
   m.def("DestroyEnv", &DestroyEnv, py::call_guard<py::gil_scoped_release>());
 
   m.def("CurrentMachineId", &CurrentMachineId);
