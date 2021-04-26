2938e2c5a518e328da3b1485b6f19d93d6a62190
Flowingsun007 <flowingsun007@163.com>
2021-04-26 17:22:06 +0800

    update repeat module

--- oneflow/python/nn/modules/repeat.py
+++ oneflow/python/nn/modules/repeat.py
@@ -1,20 +1,5 @@
 """
 Copyright 2020 The OneFlow Authors. All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-"""
-Copyright 2020 The OneFlow Authors. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -40,7 +25,28 @@ from oneflow.python.ops.transpose_util import (
 @register_tensor_op_by_module("tmp.repeat")
 @register_op_by_module("tmp.repeat")
 class Repeat(Module):
-    r"""
+    """This operator repeat the input tensor to a larger size along the specified dimensions.
+    Args:
+        x (oneflow.Tensor): The input Tensor. 
+        size (Sequence[int]): The number of times to repeat this tensor along each dimension
+    Returns:
+        oneflow.Tensor: The result Tensor. 
+    For example: 
+    .. code-block:: python
+        import oneflow as flow
+        import numpy as np
+        x = np.array([[[[0, 1]],
+                       [[2, 3]],
+                       [[4, 5]]]]).astype(np.int32)
+        input = flow.Tensor(x)
+        out = flow.tmp.repeat(input, sizes=[1, 1, 2, 2]).numpy()
+        # out shape: [1, 3, 2, 4]
+        # [[[[0. 1. 0. 1.]
+        # [0. 1. 0. 1.]]
+        # [[2. 3. 2. 3.]
+        # [2. 3. 2. 3.]]
+        # [[4. 5. 4. 5.]
+        # [4. 5. 4. 5.]]]]
     """
 
     def __init__(self, sizes) -> None:
