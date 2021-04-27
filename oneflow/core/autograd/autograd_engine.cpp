9e9b28c170aa76cc735bfed85f4a2783755c80b3
wyg1997 <wyg19970408@gmail.com>
2021-04-27 15:55:31 +0800

    fix(*): fix merge bugs

--- oneflow/core/autograd/autograd_engine.cpp
+++ oneflow/core/autograd/autograd_engine.cpp
@@ -58,9 +58,13 @@ StackFunctionNode::StackFunctionNode(
     const std::shared_ptr<const std::function<Maybe<void>(const TensorTuple&, TensorTuple*, bool)>>&
         backward_fn,
     const TensorTuple& inputs, const TensorTuple& outputs) {
+  input_tensors_.resize(inputs.size());
   input_meta_datas_.resize(inputs.size());
   for (int i = 0; i < inputs.size(); ++i) {
     input_meta_datas_.at(i) = inputs.at(i)->mut_autograd_meta();
+    if (input_meta_datas_.at(i)->requires_grad()) {
+        input_tensors_.at(i) = inputs.at(i);
+    }
   }
 
   output_meta_datas_.resize(outputs.size());
@@ -99,6 +103,7 @@ void StackFunctionNode::ReleaseOutTensorArgs() {
 void StackFunctionNode::ReleaseData() {
   // Releases backward function and makes useless tensors release as early as possible
   if (!input_meta_datas_.empty()) { backward_fn_.reset(); }
+  input_tensors_.clear();
   is_in_stack_ = false;
 }
 
