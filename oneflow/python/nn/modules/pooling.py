a24a35431074712de56d49e7da974b78a89c9388
YaoChi <later@usopp.net>
2021-04-28 10:21:57 +0800

    refine according to comments of wyg

--- oneflow/python/nn/modules/pooling.py
+++ oneflow/python/nn/modules/pooling.py
@@ -13,20 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-"""
-Copyright 2020 The OneFlow Authors. All rights reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-import oneflow as flow
+from typing import Optional, List, Tuple
 
+import oneflow as flow
 from oneflow.python.oneflow_export import oneflow_export
 from oneflow.python.nn.module import Module
 from oneflow.python.nn.modules.utils import (
@@ -36,7 +25,6 @@ from oneflow.python.nn.modules.utils import (
     _reverse_repeat_tuple,
 )
 from oneflow.python.nn.common_types import _size_1_t, _size_2_t, _size_3_t
-from typing import Optional, List, Tuple
 from oneflow.python.ops.nn_ops import calc_pool_padding, get_dhw_offset
 
 
@@ -57,7 +45,7 @@ class AvgPool2d(Module):
         kernel_size (Union[int, Tuple[int, int]]):  An int or list of ints that has length 1, 2. The size of the window for each dimension of the input Tensor.
         strides (Union[int, Tuple[int, int]]): An int or list of ints that has length 1, 2. The stride of the sliding window for each dimension of the input Tensor.
         padding (Tuple[int, int]): An int or list of ints that has length 1, 2. Implicit zero padding to be added on both sides.
-        ceil_mode (bool, default to False): when True, will use ceil instead of floor to compute the output shape.
+        ceil_mode (bool, default to False): When True, will use ceil instead of floor to compute the output shape.
 
     For example:
 
