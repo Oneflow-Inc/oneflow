ffad72ae11eedb46863c92561ea21f5eda2f40d4
hjchen2 <hjchen2>
2021-04-28 14:28:04 +0800

    Refactor

--- oneflow/core/framework/attr_value_map.h
+++ oneflow/core/framework/attr_value_map.h
@@ -18,109 +18,67 @@ limitations under the License.
 
 #include "oneflow/core/common/util.h"
 #include "oneflow/core/framework/user_op_attr.cfg.h"
+#include "oneflow/core/framework/user_op_conf.h"
 
 namespace oneflow {
 
+class MutableAttrValueMap;
+
+using AttrName2AttrVal = HashMap<std::string, std::shared_ptr<const AttrVal>>;
+
 class AttrValueMap {
  public:
-  AttrValueMap()
-      : attrs_(new HashMap<std::string, std::shared_ptr<cfg::AttrValue>>{}),
-        attr_names_(new HashSet<std::string>{}) {}
-  virtual ~AttrValueMap() = default;
+  explicit AttrValueMap(const std::shared_ptr<const AttrName2AttrVal>& attrs) : attrs_(attrs) {}
 
-  size_t size() const { return attr_names_->size(); }
-  bool empty() const { return attr_names_->empty(); }
+  explicit AttrValueMap(const MutableAttrValueMap& other);
 
-  bool has_parents() const { return parents_ && !parents_->empty(); }
-  Maybe<std::vector<AttrValueMap>> parents() const { return parents_; }
-
-  Maybe<bool> HasAttr(const std::string& attr_name) const {
-    CHECK_OR_RETURN(attr_names_);
-    return attr_names_->count(attr_name);
-  }
+  AttrValueMap(const AttrValueMap&) = default;
+  AttrValueMap(AttrValueMap&&) = default;
+  ~AttrValueMap() = default;
 
   template<typename T>
-  Maybe<T> GetAttr(const std::string& attr_name) const;
+  Maybe<const T&> GetAttr(const std::string& attr_name) const;
 
-  static Maybe<AttrValueMap> Compose(const AttrValueMap& attrs, const AttrValueMap& parent);
+  using const_iterator = AttrName2AttrVal::const_iterator;
+  const_iterator begin() const { return attrs_->begin(); }
+  const_iterator end() const { return attrs_->end(); }
 
-  class iterator {
-   public:
-    using NameSetIter = HashSet<std::string>::const_iterator;
-    using AttrValueMapIter = HashMap<std::string, std::shared_ptr<cfg::AttrValue>>::const_iterator;
-    using const_reference = HashMap<std::string, std::shared_ptr<cfg::AttrValue>>::const_reference;
-    using const_pointer = HashMap<std::string, std::shared_ptr<cfg::AttrValue>>::const_pointer;
+  const_iterator find(const std::string& attr_name) const { return attrs_->find(attr_name); }
 
-    iterator() = default;
-    explicit iterator(const AttrValueMap* self, const NameSetIter& it, const NameSetIter& end_it)
-        : self_(self), name_set_it_(it), name_set_end_it_(end_it) {
-      UpdateAttrValueMapIter();
-    }
-
-    iterator& operator++() {
-      ++name_set_it_;
-      UpdateAttrValueMapIter();
-      return *this;
-    }
-    explicit operator bool() const { return name_set_it_ != name_set_end_it_; }
-
-    const_reference operator*() const { return attr_value_map_it_.operator*(); }
-    const_pointer operator->() const { return attr_value_map_it_.operator->(); }
-
-    bool operator==(const iterator& other) const {
-      return self_ == other.self_ && name_set_it_ == other.name_set_it_;
-    }
-    bool operator!=(const iterator& other) const {
-      return self_ != other.self_ || name_set_it_ != other.name_set_it_;
-    }
-
-    AttrValueMapIter internal() const { return attr_value_map_it_; }
-
-   private:
-    void UpdateAttrValueMapIter() {
-      if (name_set_it_ != name_set_end_it_) { attr_value_map_it_ = self_->Find(*name_set_it_); }
-    }
-
-   private:
-    const AttrValueMap* self_;
-    NameSetIter name_set_it_;
-    NameSetIter name_set_end_it_;
-
-    AttrValueMapIter attr_value_map_it_;
-  };
-
-  friend class iterator;
-
-  iterator begin() const { return iterator(this, attr_names_->begin(), attr_names_->end()); }
-  iterator end() const { return iterator(this, attr_names_->end(), attr_names_->end()); }
-
-  iterator find(const std::string& attr_name) const {
-    const auto& name_set_it = attr_names_->find(attr_name);
-    return iterator(this, name_set_it, attr_names_->end());
-  }
-
- protected:
-  friend class MutableAttrValueMap;
-  iterator::AttrValueMapIter Find(const std::string& attr_name) const;
-
-  std::shared_ptr<std::vector<AttrValueMap>> parents_;
-  std::shared_ptr<HashMap<std::string, std::shared_ptr<cfg::AttrValue>>> attrs_;
-  std::shared_ptr<HashSet<std::string>> attr_names_;
+ private:
+  std::shared_ptr<const AttrName2AttrVal> attrs_;
 };
 
-class MutableAttrValueMap : public AttrValueMap {
+class ComposedAttrValueMap final {
  public:
-  MutableAttrValueMap() : AttrValueMap() {}
-  MutableAttrValueMap(const MutableAttrValueMap&) = delete;
-  virtual ~MutableAttrValueMap() = default;
+  ComposedAttrValueMap(const AttrValueMap& base) : base_(base) {}
+  ComposedAttrValueMap(const AttrValueMap& prior, const AttrValueMap& base)
+      : prior_(prior), base_(base) {}
 
-  MutableAttrValueMap& operator=(const MutableAttrValueMap&) = delete;
+  template<typename T>
+  Maybe<const T&> GetAttr(const std::string& attr_name) const;
+
+  Maybe<void> ResetPrior(const AttrValueMap& prior) {
+    prior_ = prior;
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> ResetBase(const AttrValueMap& base) {
+    base_ = base;
+    return Maybe<void>::Ok();
+  }
+
+ private:
+  AttrValueMap prior_;
+  AttrValueMap base_;
+};
+
+class MutableAttrValueMap : public HashMap<std::string, std::shared_ptr<cfg::AttrValue>> {
+ public:
+  using HashMap<std::string, std::shared_ptr<cfg::AttrValue>>::HashMap;
 
   template<typename T>
   Maybe<void> SetAttr(const std::string& attr_name, const T& attr_val);
-
-  Maybe<MutableAttrValueMap&> Compose(const AttrValueMap& parent);
-  static Maybe<MutableAttrValueMap> Compose(const AttrValueMap& attrs, const AttrValueMap& parent);
 };
 
 }  // namespace oneflow
