85bb913a819dce39dfca2114e511030ad3d5f8d8
Lyon <flowingsun007@163.com>
2021-04-28 20:24:05 +0800

    Update stateful_local_opkernel.cpp

--- oneflow/user/kernels/stateful_local_opkernel.cpp
+++ oneflow/user/kernels/stateful_local_opkernel.cpp
@@ -105,9 +105,6 @@ user_op::TensorDesc* ZeroCopyBaseContext::TensorDesc4ArgNameAndIndex(const std::
 
 user_op::Tensor* ZeroCopyBaseContext::Tensor4ArgNameAndIndex(const std::string& arg_name,
                                                              const int32_t index) const {
-  if (arg_name == "tmp_buffer" && index == 0 && tmp_buffer_view_ != nullptr) {
-    return tmp_buffer_view_.get();
-  }
   int32_t i = TryGetTensorTupleIndex(arg_name2bn_index2input_tensor_tuple_index_, arg_name, index);
   if (i >= 0) { return input_tensor_views_.at(i).get(); }
   i = TryGetTensorTupleIndex(arg_name2bn_index2output_tensor_tuple_index_, arg_name, index);
