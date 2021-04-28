42e1548138bef63612a368eb574e9caf63d22104
mosout <mosout@qq.com>
2021-04-28 19:38:27 +0800

    return nullptr instead of LOG(FATAL) when arg is not found

--- oneflow/user/kernels/stateful_local_opkernel.cpp
+++ oneflow/user/kernels/stateful_local_opkernel.cpp
@@ -99,7 +99,6 @@ user_op::TensorDesc* ZeroCopyBaseContext::TensorDesc4ArgNameAndIndex(const std::
   if (i >= 0) { return input_tensor_desc_views_.at(i).get(); }
   i = TryGetTensorTupleIndex(arg_name2bn_index2output_tensor_tuple_index_, arg_name, index);
   if (i >= 0) { return output_tensor_desc_views_.at(i).get(); }
-  LOG(FATAL) << "Arg (" << arg_name << "," << index << ") is not found";
   return nullptr;
 }
 
@@ -110,7 +109,6 @@ user_op::Tensor* ZeroCopyBaseContext::Tensor4ArgNameAndIndex(const std::string& 
   i = TryGetTensorTupleIndex(arg_name2bn_index2output_tensor_tuple_index_, arg_name, index);
   if (i >= 0) { return output_tensor_views_.at(i).get(); }
   if (arg_name == "tmp_buffer" && index == 0) { return CHECK_NOTNULL(tmp_buffer_view_.get()); }
-  LOG(FATAL) << "Arg (" << arg_name << "," << index << ") is not found";
   return nullptr;
 }
 
