12f23d1a0fc96d2ab981a9c10a5b3fada0aa7b2a
linli <linli199310@126.com>
2021-04-29 11:49:53 +0800

    rm prefix Protected

--- oneflow/core/graph/task_graph.cpp
+++ oneflow/core/graph/task_graph.cpp
@@ -563,7 +563,7 @@ void CreateOpAttributeRef(Plan* plan, int64_t job_id, TaskProto* task_proto) {
 }
 
 void TaskGraph::MoveContentsIntoPlanAndDestory(Plan* plan, int64_t job_id) {
-  ProtectedDestoryEdges();
+  DestoryEdges();
   ForEachNode([&](TaskNode* task_node) {
     if (task_node->IsMeaningLess()) { return; }
     TaskProto task_proto;
