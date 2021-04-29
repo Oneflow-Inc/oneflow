2a209b8c7735d7310aaaaae9fb8945d380e82b88
chengtbf <472491134@qq.com>
2021-04-29 09:52:16 +0800

    fix data size check in 2d s1-b

--- oneflow/user/kernels/nccl_logical_2d_sbp_kernels.cpp
+++ oneflow/user/kernels/nccl_logical_2d_sbp_kernels.cpp
@@ -144,7 +144,7 @@ class NcclLogical2DSameDim0AllGatherNoncontinuous final : public user_op::OpKern
     user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("out", 0);
     user_op::Tensor* tmp_buffer = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
     const int64_t dtype_size = GetSizeOfDataType(in->data_type());
-    int64_t data_size = GetCudaAlignedSize(in->shape().elem_cnt() * dtype_size);
+    int64_t data_size = GetCudaAlignedSize(out->shape().elem_cnt() * dtype_size);
     void* unpack_from_ptr = tmp_buffer->mut_dptr();
     CHECK_EQ(tmp_buffer->shape().elem_cnt(), data_size);
 
