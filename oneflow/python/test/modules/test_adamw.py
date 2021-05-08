aa0faa50d70e80496ece69e9bbf6053716f03349
BBuf <1182563586@qq.com>
2021-05-08 09:59:40 +0800

    fix adamw optimizer bug

--- oneflow/python/test/modules/test_adamw.py
+++ oneflow/python/test/modules/test_adamw.py
@@ -1,9 +1,12 @@
 """
 Copyright 2020 The OneFlow Authors. All rights reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,8 +22,8 @@ from oneflow.python.test.modules.test_util import GenArgList
 from oneflow.python.nn.parameter import Parameter
 
 
-def compare_with_numpy_adam(
-    test_case, x_shape, scale, learning_rate, train_iters,
+def compare_with_numpy_adamw(
+    test_case, x_shape, scale, learning_rate, train_iters, weight_decay
 ):
     # generate random number sequences
     random_grad_seq = []
@@ -33,7 +36,12 @@ def compare_with_numpy_adam(
         x = Parameter(flow.Tensor(init_value))
         param_list = list()
         param_list.append(x)
-        adam = flow.optim.Adam([{"param": param_list}], lr=learning_rate, scale=scale)
+        adam = flow.optim.AdamW(
+            [{"param": param_list}],
+            lr=learning_rate,
+            scale=scale,
+            weight_decay=weight_decay,
+        )
 
         def train_one_iter(grad):
             grad_tensor = flow.Tensor(grad, requires_grad=False)
@@ -59,7 +67,10 @@ def compare_with_numpy_adam(
             grad = grad * scale
             v = beta1 * vt + (1 - beta1) * grad
             s = beta2 * st + (1 - beta2) * grad * grad
-            g = learning_rate / (np.sqrt(s) + 1e-8) * v
+            g = (
+                learning_rate / (np.sqrt(s) + 1e-8) * v
+                + learning_rate * weight_decay * x
+            )
             param = x - g
             return param, v, s
 
@@ -79,15 +90,16 @@ def compare_with_numpy_adam(
     not flow.unittest.env.eager_execution_enabled(),
     ".numpy() doesn't work in lazy mode",
 )
-class TestAdam(flow.unittest.TestCase):
-    def test_adam(test_case):
+class TestAdamW(flow.unittest.TestCase):
+    def test_adamw(test_case):
         arg_dict = OrderedDict()
         arg_dict["x_shape"] = [(10,)]
         arg_dict["scale"] = [1.0, 0.9]
         arg_dict["learning_rate"] = [1]
         arg_dict["train_iters"] = [10]
+        arg_dict["weight_decay"] = [1e-3, 1e-2]
         for arg in GenArgList(arg_dict):
-            compare_with_numpy_adam(test_case, *arg)
+            compare_with_numpy_adamw(test_case, *arg)
 
 
 if __name__ == "__main__":
