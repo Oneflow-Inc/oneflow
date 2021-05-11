c1707fc706fe56448a4f89aa274116040ac6c7f8
BBuf <1182563586@qq.com>
2021-05-11 10:41:11 +0800

    fix commnet

--- oneflow/python/nn/optimizer/adam.py
+++ oneflow/python/nn/optimizer/adam.py
@@ -117,7 +117,7 @@ class Adam(Optimizer):
             .Input("v")
             .Attr("scale", self._default_options["scale"])
             .Attr("l1", 0.0)
-            .Attr("l2", self._default_options["weight_decay"])
+            .Attr("l2", self._default_options["weight_decay"]) # Adam's weight_deacy actually does is L2 Norm
             .Attr("beta1", self._default_options["beta"][0])
             .Attr("beta2", self._default_options["beta"][1])
             .Attr("epsilon", self._default_options["eps"])
