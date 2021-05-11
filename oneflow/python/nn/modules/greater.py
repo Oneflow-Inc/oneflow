c50ff3cc54c05dd1ad6f924388f8f06057ade663
Liang Depeng <liangdepeng@gmail.com>
2021-05-11 09:37:59 +0800

    add experimental apis (#4817)
    * add experimental apis
    
    * merge master fix conflict
    
    * revert flow._oneflow_internal.dtype to flow.dtype
    
    * refine
    
    * fix test optimizer
    
    * update module docs
    
    * fix unit tests
    
    * fix matmul module test
    
    * fix adamw and rmsprop tests
    
    * fix crossentropy loss grad
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/python/nn/modules/greater.py
+++ oneflow/python/nn/modules/greater.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 import oneflow as flow
 from oneflow.python.nn.module import Module
-from oneflow.python.oneflow_export import oneflow_export
+from oneflow.python.oneflow_export import oneflow_export, experimental_api
 from oneflow.python.framework.tensor import register_tensor_op
 
 
@@ -38,6 +38,7 @@ class Greater(Module):
 
 @oneflow_export("gt")
 @register_tensor_op("gt")
+@experimental_api
 def greater_op(x, y):
     r"""Returns the truth value of :math:`x > y` element-wise.
 
@@ -53,7 +54,7 @@ def greater_op(x, y):
 
     .. code-block:: python
         
-        import oneflow as flow
+        import oneflow.experimental as flow
         import numpy as np
 
         input1 = flow.Tensor(np.random.randn(2, 6, 5, 3), dtype=flow.float32)
--- oneflow/python/nn/modules/repeat.py
+++ oneflow/python/nn/modules/repeat.py
@@ -15,7 +15,7 @@ limitations under the License.
 """
 import oneflow as flow
 from oneflow.python.nn.module import Module
-from oneflow.python.oneflow_export import oneflow_export
+from oneflow.python.oneflow_export import oneflow_export, experimental_api
 from oneflow.python.framework.tensor import register_tensor_op
 
 
@@ -55,14 +55,15 @@ class Repeat(Module):
                 expand_dim.insert(0, repeat[i])
                 out_reshape.insert(0, repeat[i])
 
-        new_tensor = flow.tmp.reshape(input, in_reshape)
-        tmp_tensor = flow.tmp.expand(new_tensor, expand_dim)
-        out = flow.tmp.reshape(tmp_tensor, out_reshape)
+        new_tensor = flow.experimental.reshape(input, in_reshape)
+        tmp_tensor = flow.experimental.expand(new_tensor, expand_dim)
+        out = flow.experimental.reshape(tmp_tensor, out_reshape)
         return out
 
 
-@oneflow_export("tmp.repeat")
+@oneflow_export("repeat")
 @register_tensor_op("repeat")
+@experimental_api
 def repeat_op(x, sizes):
     """This operator repeat the input tensor to a larger size along the specified dimensions.
 
@@ -77,7 +78,7 @@ def repeat_op(x, sizes):
 
     .. code-block:: python
 
-        import oneflow as flow
+        import oneflow.experimental as flow
         import numpy as np
 
         x = np.array([[[[0, 1]],
