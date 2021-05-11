30d8e45f81c7ce866d0a26c38249f2337a9b7d94
Flowingsun007 <flowingsun007@163.com>
2021-05-11 16:24:20 +0800

    add usage

--- oneflow/python/test/modules/test_all.sh
+++ oneflow/python/test/modules/test_all.sh
@@ -1,3 +1,5 @@
+# test all test cases in current dir
+# usage : bash test_all.sh
 ls |grep 'test.*.py' > test_cases_names.txt
 while read line
 do
@@ -5,3 +7,4 @@ do
     ONEFLOW_TEST_ENABLE_EAGER=1 python3 $line
     sleep 1
 done < test_cases_names.txt
+rm test_cases_names.txt
