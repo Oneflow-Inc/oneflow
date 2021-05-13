9da8a845de39ee8ff552ba035e1997d5339072eb
YaoChi <later@usopp.net>
2021-05-14 01:02:12 +0800

    startup of add gpu test

--- oneflow/python/test/modules/test_add.py
+++ oneflow/python/test/modules/test_add.py
@@ -54,6 +54,76 @@ class TestAddModule(flow.unittest.TestCase):
         np_out = np.add(x.numpy(), y.numpy())
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
 
+    def test_add_cuda(test_case):
+        x = flow.Tensor(np.random.randn(2, 3))
+        y = flow.Tensor(np.random.randn(2, 3))
+        x.to("cuda")
+        y.to("cuda")
+        of_out = flow.add(x, y)
+        np_out = np.add(x.numpy(), y.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+        x = 5
+        y = flow.Tensor(np.random.randn(2, 3))
+        y.to("cuda")
+        of_out = flow.add(x, y)
+        np_out = np.add(x, y.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+        x = flow.Tensor(np.random.randn(2, 3))
+        y = 5
+        x.to("cuda")
+        of_out = flow.add(x, y)
+        np_out = np.add(x.numpy(), y)
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+        x = flow.Tensor(np.random.randn(2, 3))
+        y = flow.Tensor(np.array([5.0]))
+        x.to("cuda")
+        y.to("cuda")
+        of_out = flow.add(x, y)
+        np_out = np.add(x.numpy(), y.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+        x = flow.Tensor(np.random.randn(1, 1))
+        y = flow.Tensor(np.random.randn(2, 3))
+        x.to("cuda")
+        y.to("cuda")
+        of_out = flow.add(x, y)
+        np_out = np.add(x.numpy(), y.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+
+import time
+
+
+@unittest.skipIf(
+    not flow.unittest.env.eager_execution_enabled(),
+    ".numpy() doesn't work in lazy mode",
+)
+class TestAddCudaBigTensorModule(flow.unittest.TestCase):
+    def test_add_cuda(test_case):
+        x = flow.Tensor(np.random.randn(10000, 10000))
+        y = flow.Tensor(np.random.randn(10000, 10000))
+        x.to("cuda")
+        y.to("cuda")
+        of_out = flow.add(x, y)
+        np_out = np.add(x.numpy(), y.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+
+@unittest.skipIf(
+    not flow.unittest.env.eager_execution_enabled(),
+    ".numpy() doesn't work in lazy mode",
+)
+class TestAddCpuBigTensorModule(flow.unittest.TestCase):
+    def test_add_cuda(test_case):
+        x = flow.Tensor(np.random.randn(10000, 10000))
+        y = flow.Tensor(np.random.randn(10000, 10000))
+        of_out = flow.add(x, y)
+        np_out = np.add(x.numpy(), y.numpy())
+        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
 
 if __name__ == "__main__":
     unittest.main()
