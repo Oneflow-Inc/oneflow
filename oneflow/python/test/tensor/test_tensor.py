5ad352c38ade006969141056b9493ce9e89d333c
poohRui <yuruil@qq.com>
2021-05-14 12:41:16 +0800

    remove skip tensor test case:

--- oneflow/python/test/tensor/test_tensor.py
+++ oneflow/python/test/tensor/test_tensor.py
@@ -136,8 +136,8 @@ class TestTensor(flow.unittest.TestCase):
         shape = (2, 3, 4, 5)
         x = flow.Tensor(*shape)
         test_case.assertTrue(not x.is_cuda)
-        # x = flow.Tensor(*shape, device=flow.device("cuda"))
-        # test_case.assertTrue(x.is_cuda)
+        x = flow.Tensor(*shape, device=flow.device("cuda"))
+        test_case.assertTrue(x.is_cuda)
         x = flow.Tensor(*shape, device=flow.device("cpu"))
         test_case.assertTrue(not x.is_cuda)
 
