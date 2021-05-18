7823669101cd489634669497f04172ed3002910c
YaoChi <later@usopp.net>
2021-05-18 19:41:43 +0800

    refine: construct specified device tensor

--- oneflow/python/test/modules/test_add.py
+++ oneflow/python/test/modules/test_add.py
@@ -55,40 +55,32 @@ class TestAddModule(flow.unittest.TestCase):
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
 
     def test_add_cuda(test_case):
-        x = flow.Tensor(np.random.randn(2, 3))
-        y = flow.Tensor(np.random.randn(2, 3))
-        x = x.to("cuda")
-        y = y.to("cuda")
+        x = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
+        y = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
         of_out = flow.add(x, y)
         np_out = np.add(x.numpy(), y.numpy())
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
 
         x = 5
-        y = flow.Tensor(np.random.randn(2, 3))
-        y = y.to("cuda")
+        y = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
         of_out = flow.add(x, y)
         np_out = np.add(x, y.numpy())
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
 
-        x = flow.Tensor(np.random.randn(2, 3))
+        x = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
         y = 5
-        x = x.to("cuda")
         of_out = flow.add(x, y)
         np_out = np.add(x.numpy(), y)
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
 
-        x = flow.Tensor(np.random.randn(2, 3))
-        y = flow.Tensor(np.array([5.0]))
-        x = x.to("cuda")
-        y = y.to("cuda")
+        x = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
+        y = flow.Tensor(np.array([5.0]), device=flow.device("cuda"))
         of_out = flow.add(x, y)
         np_out = np.add(x.numpy(), y.numpy())
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
 
-        x = flow.Tensor(np.random.randn(1, 1))
-        y = flow.Tensor(np.random.randn(2, 3))
-        x = x.to("cuda")
-        y = y.to("cuda")
+        x = flow.Tensor(np.random.randn(1, 1), device=flow.device("cuda"))
+        y = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
         of_out = flow.add(x, y)
         np_out = np.add(x.numpy(), y.numpy())
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
@@ -100,10 +92,8 @@ class TestAddModule(flow.unittest.TestCase):
 )
 class TestAddCudaBigTensorModule(flow.unittest.TestCase):
     def test_add_cuda(test_case):
-        x = flow.Tensor(np.random.randn(10000, 10000))
-        y = flow.Tensor(np.random.randn(10000, 10000))
-        x.to("cuda")
-        y.to("cuda")
+        x = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
+        y = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
         of_out = flow.add(x, y)
         np_out = np.add(x.numpy(), y.numpy())
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
@@ -115,8 +105,8 @@ class TestAddCudaBigTensorModule(flow.unittest.TestCase):
 )
 class TestAddCpuBigTensorModule(flow.unittest.TestCase):
     def test_add_cpu(test_case):
-        x = flow.Tensor(np.random.randn(10000, 10000))
-        y = flow.Tensor(np.random.randn(10000, 10000))
+        x = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
+        y = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
         of_out = flow.add(x, y)
         np_out = np.add(x.numpy(), y.numpy())
         test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
@@ -136,9 +126,10 @@ class TestAddBackwardModule(flow.unittest.TestCase):
 
     def test_backward_gpu(test_case):
         x = 5
-        y = flow.Tensor(np.random.randn(2, 3), requires_grad=True)
-        y_gpu = y.to("cuda")
-        of_out = flow.add(x, y_gpu).sum()
+        y = flow.Tensor(
+            np.random.randn(2, 3), requires_grad=True, device=flow.device("cuda")
+        )
+        of_out = flow.add(x, y).sum()
         of_out.backward()
         test_case.assertTrue(np.allclose(y.grad.numpy(), np.ones((2, 3)), 1e-4, 1e-4))
 
