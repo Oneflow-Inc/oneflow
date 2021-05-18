a139f49eaff8a00512367abd5f9af65fc6ee786f
Yao Chi <later@usopp.net>
2021-05-17 20:04:37 -0500

    add InitEagerSession for eager mode (#4589)
    * try to merge eager ofrecord to master branch
    
    * refine
    
    * temp fix
    
    * try to add seed but fails
    
    * try to add seed but failsclear
    
    * use global function to init mirror/conssitent flag
    
    * fix test
    
    * add modules
    
    * fix record modules
    
    * fix destruction order
    
    * fix mirror gen seed
    
    * skip record unit test
    
    * remove TODO
    
    Co-authored-by: daquexian <daquexian566@gmail.com>
    Co-authored-by: mosout <mosout@qq.com>
    Co-authored-by: Ldpe2G <liangdepeng@gmail.com>
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/core/comm_network/epoll/epoll_comm_network.cpp
+++ oneflow/core/comm_network/epoll/epoll_comm_network.cpp
@@ -87,8 +87,7 @@ EpollCommNet::~EpollCommNet() {
     LOG(INFO) << "CommNet Thread " << i << " finish";
     pollers_[i]->Stop();
   }
-  // TODO(chengcheng): change to OF_ENV_BARRIER
-  OF_SESSION_BARRIER();
+  OF_ENV_BARRIER();
   for (IOEventPoller* poller : pollers_) { delete poller; }
   for (auto& pair : sockfd2helper_) { delete pair.second; }
 }
