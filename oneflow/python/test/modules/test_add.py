c5e8dbe168061ccc8072a74cc6ad0ba80c0b2619
YaoChi <later@usopp.net>
2021-05-18 20:07:43 +0800

    refine testcase

--- oneflow/python/test/modules/test_add.py
+++ oneflow/python/test/modules/test_add.py
@@ -13,9 +13,55 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import oneflow.experimental as flow
 import unittest
+from test_util import GenArgList
+from collections import OrderedDict
+
 import numpy as np
+
+import oneflow.experimental as flow
+
+
+def _test_add_forward(test_case, device):
+    x = flow.Tensor(np.random.randn(2, 3), device=flow.device(device))
+    y = flow.Tensor(np.random.randn(2, 3), device=flow.device(device))
+    of_out = flow.add(x, y)
+    np_out = np.add(x.numpy(), y.numpy())
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+    x = 5
+    y = flow.Tensor(np.random.randn(2, 3), device=flow.device(device))
+    of_out = flow.add(x, y)
+    np_out = np.add(x, y.numpy())
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+    x = flow.Tensor(np.random.randn(2, 3), device=flow.device(device))
+    y = 5
+    of_out = flow.add(x, y)
+    np_out = np.add(x.numpy(), y)
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+    x = flow.Tensor(np.random.randn(2, 3), device=flow.device(device))
+    y = flow.Tensor(np.array([5.0]), device=flow.device(device))
+    of_out = flow.add(x, y)
+    np_out = np.add(x.numpy(), y.numpy())
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+    x = flow.Tensor(np.random.randn(1, 1), device=flow.device(device))
+    y = flow.Tensor(np.random.randn(2, 3), device=flow.device(device))
+    of_out = flow.add(x, y)
+    np_out = np.add(x.numpy(), y.numpy())
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
+
+
+def _test_add_backward(test_case, device):
+    x = 5
+    y = flow.Tensor(
+        np.random.randn(2, 3), requires_grad=True, device=flow.device(device)
+    )
+    of_out = flow.add(x, y).sum()
+    of_out.backward()
+    test_case.assertTrue(np.allclose(y.grad.numpy(), np.ones((2, 3)), 1e-4, 1e-4))
 
 
 @unittest.skipIf(
@@ -23,115 +69,12 @@ import numpy as np
     ".numpy() doesn't work in lazy mode",
 )
 class TestAddModule(flow.unittest.TestCase):
-    def test_add(test_case):
-        x = flow.Tensor(np.random.randn(2, 3))
-        y = flow.Tensor(np.random.randn(2, 3))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = 5
-        y = flow.Tensor(np.random.randn(2, 3))
-        of_out = flow.add(x, y)
-        np_out = np.add(x, y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = flow.Tensor(np.random.randn(2, 3))
-        y = 5
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y)
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = flow.Tensor(np.random.randn(2, 3))
-        y = flow.Tensor(np.array([5.0]))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = flow.Tensor(np.random.randn(1, 1))
-        y = flow.Tensor(np.random.randn(2, 3))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-    def test_add_cuda(test_case):
-        x = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
-        y = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = 5
-        y = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
-        of_out = flow.add(x, y)
-        np_out = np.add(x, y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
-        y = 5
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y)
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
-        y = flow.Tensor(np.array([5.0]), device=flow.device("cuda"))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-        x = flow.Tensor(np.random.randn(1, 1), device=flow.device("cuda"))
-        y = flow.Tensor(np.random.randn(2, 3), device=flow.device("cuda"))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-
-@unittest.skipIf(
-    not flow.unittest.env.eager_execution_enabled(),
-    ".numpy() doesn't work in lazy mode",
-)
-class TestAddCudaBigTensorModule(flow.unittest.TestCase):
-    def test_add_cuda(test_case):
-        x = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
-        y = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-
-@unittest.skipIf(
-    not flow.unittest.env.eager_execution_enabled(),
-    ".numpy() doesn't work in lazy mode",
-)
-class TestAddCpuBigTensorModule(flow.unittest.TestCase):
-    def test_add_cpu(test_case):
-        x = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
-        y = flow.Tensor(np.random.randn(10000, 10000), device=flow.device("cuda"))
-        of_out = flow.add(x, y)
-        np_out = np.add(x.numpy(), y.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-4, 1e-4))
-
-
-@unittest.skipIf(
-    not flow.unittest.env.eager_execution_enabled(),
-    ".numpy() doesn't work in lazy mode",
-)
-class TestAddBackwardModule(flow.unittest.TestCase):
-    def test_backward_cpu(test_case):
-        x = 5
-        y = flow.Tensor(np.random.randn(2, 3), requires_grad=True)
-        of_out = flow.add(x, y).sum()
-        of_out.backward()
-        test_case.assertTrue(np.allclose(y.grad.numpy(), np.ones((2, 3)), 1e-4, 1e-4))
-
-    def test_backward_gpu(test_case):
-        x = 5
-        y = flow.Tensor(
-            np.random.randn(2, 3), requires_grad=True, device=flow.device("cuda")
-        )
-        of_out = flow.add(x, y).sum()
-        of_out.backward()
-        test_case.assertTrue(np.allclose(y.grad.numpy(), np.ones((2, 3)), 1e-4, 1e-4))
+    def test_add_forward(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["fun"] = [_test_add_forward, _test_add_backward]
+        arg_dict["device"] = ["cpu", "cuda"]
+        for arg in GenArgList(arg_dict):
+            arg[0](test_case, *arg[1:])
 
 
 if __name__ == "__main__":
