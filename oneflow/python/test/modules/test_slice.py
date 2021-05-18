ec6c4e18ed67b9d9f395d460cc40757e2723a369
Flowingsun007 <flowingsun007@163.com>
2021-05-18 16:20:16 +0800

    add tenser slice test case

--- oneflow/python/test/modules/test_slice.py
+++ oneflow/python/test/modules/test_slice.py
@@ -31,6 +31,16 @@ class TestSlice(flow.unittest.TestCase):
         y = flow.tmp.slice(input, slice_tup_list=tup_list)
         test_case.assertTrue(y.shape == flow.Size([3, 3, 2]))
 
+    def test_tensor_slice(test_case):
+        x = np.random.randn(2, 3, 4, 5).astype(np.float32)
+        input = flow.Tensor(x)
+        test_case.assertTrue(np.allclose(input[0].numpy(), x[0], 1e-5, 1e-5))
+        test_case.assertTrue(np.allclose(input[1].numpy(), x[1], 1e-5, 1e-5))
+        test_case.assertTrue(np.allclose(input[0, :].numpy(), x[0, :], 1e-5, 1e-5))
+        test_case.assertTrue(
+            np.allclose(input[0, :, 0:2].numpy(), x[0, :, 0:2], 1e-5, 1e-5)
+        )
+
 
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
@@ -44,6 +54,14 @@ class TestSliceUpdate(flow.unittest.TestCase):
         output = np.array([1.0, 2.0, 3.0, 4.0, 1.0])
         y = flow.tmp.slice_update(input, update, slice_tup_list=[[1, 4, 1]])
         test_case.assertTrue(np.array_equal(y.numpy(), output))
+
+    def test_tensor_slice_update(test_case):
+        x = np.random.randn(2, 3, 4, 5).astype(np.float32)
+        input = flow.Tensor(x)
+        input[0] = 3.1415
+        x[0] = 3.1415
+        # TODO：False,input seem not set value success!
+        test_case.assertTrue(np.allclose(input[0].numpy(), x[0], 1e-5, 1e-5))
 
 
 if __name__ == "__main__":
