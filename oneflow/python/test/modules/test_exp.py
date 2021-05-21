f47dd384cd593d320ab1098ba84a3975268d2e83
BBuf <1182563586@qq.com>
2021-05-21 10:46:46 +0800

    add exp module backward

--- oneflow/python/test/modules/test_exp.py
+++ oneflow/python/test/modules/test_exp.py
@@ -16,8 +16,30 @@ limitations under the License.
 import unittest
 
 import numpy as np
+from collections import OrderedDict
 import oneflow.experimental as flow
+from test_util import GenArgList
 
+def _test_exp(test_case, device):
+    input = flow.Tensor(np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device))
+    of_out = flow.exp(input)
+    np_out = np.exp(input.numpy())
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out))
+
+def _test_tensor_exp(test_case, device):
+    input = flow.Tensor(np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device))
+    of_out = input.exp()
+    np_out = np.exp(input.numpy())
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out))
+
+def _test_exp_backward(test_case, device):
+    input = flow.Tensor(np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device), requires_grad=True)
+    of_out = flow.exp(input)
+    np_grad = of_out.numpy()
+    of_out = of_out.sum()
+    of_out.backward()
+    test_case.assertTrue(np.allclose(input.grad.numpy(), np_grad, rtol=1e-05))
+    
 
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
@@ -25,16 +47,15 @@ import oneflow.experimental as flow
 )
 class TestExp(flow.unittest.TestCase):
     def test_exp(test_case):
-        input = flow.Tensor(np.random.randn(2, 6, 5, 3), dtype=flow.float32)
-        of_out = flow.exp(input)
-        np_out = np.exp(input.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out))
-
-    def test_tensor_exp(test_case):
-        input = flow.Tensor(np.random.randn(2, 6, 5, 3), dtype=flow.float32)
-        of_out = input.exp()
-        np_out = np.exp(input.numpy())
-        test_case.assertTrue(np.allclose(of_out.numpy(), np_out))
+        arg_dict = OrderedDict()
+        arg_dict["test_fun"] = [
+            _test_exp,
+            _test_tensor_exp,
+            _test_exp_backward,
+        ]
+        arg_dict["device"] = ["cpu", "cuda"]
+        for arg in GenArgList(arg_dict):
+            arg[0](test_case, *arg[1:])
 
 
 if __name__ == "__main__":
