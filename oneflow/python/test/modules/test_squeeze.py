6202ababf04b43d9a54689057ffd1ad89a205a68
BBuf <1182563586@qq.com>
2021-05-21 09:25:33 +0800

    code format

--- oneflow/python/test/modules/test_squeeze.py
+++ oneflow/python/test/modules/test_squeeze.py
@@ -20,26 +20,40 @@ from collections import OrderedDict
 import oneflow.experimental as flow
 from test_util import GenArgList
 
+
 def _test_squeeze(test_case, device):
-    input = flow.Tensor(np.array([[[[1, 1, 1]]]]).astype(np.int32), device=flow.device(device))
+    input = flow.Tensor(
+        np.array([[[[1, 1, 1]]]]).astype(np.int32), device=flow.device(device)
+    )
     of_shape = flow.squeeze(input, dim=[1, 2]).numpy().shape
     np_shape = (1, 3)
     test_case.assertTrue(np.array_equal(of_shape, np_shape))
 
+
 def _test_tensor_squeeze(test_case, device):
-    input = flow.Tensor(np.array([[[[1, 1, 1]]]]).astype(np.int32), device=flow.device(device))
+    input = flow.Tensor(
+        np.array([[[[1, 1, 1]]]]).astype(np.int32), device=flow.device(device)
+    )
     of_shape = input.squeeze(dim=[1, 2]).numpy().shape
     np_shape = (1, 3)
     test_case.assertTrue(np.array_equal(of_shape, np_shape))
 
+
 def _test_squeeze_int(test_case, device):
-    input = flow.Tensor(np.array([[[[1, 1, 1]]]]).astype(np.int32), device=flow.device(device))
+    input = flow.Tensor(
+        np.array([[[[1, 1, 1]]]]).astype(np.int32), device=flow.device(device)
+    )
     of_shape = flow.squeeze(input, 1).numpy().shape
     np_shape = (1, 1, 3)
     test_case.assertTrue(np.array_equal(of_shape, np_shape))
 
+
 def _test_squeeze_backward(test_case, device):
-    input = flow.Tensor(np.array([[[[1, 1, 1]]]]).astype(np.int32), device=flow.device(device), requires_grad=True)
+    input = flow.Tensor(
+        np.array([[[[1, 1, 1]]]]).astype(np.int32),
+        device=flow.device(device),
+        requires_grad=True,
+    )
     y = flow.squeeze(input, dim=1).sum()
     y.backward()
     np_grad = [[[[1.0, 1.0, 1.0]]]]
