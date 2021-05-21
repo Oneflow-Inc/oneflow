37f72d12360b325f35b07a91b196d79b37705c71
BBuf <1182563586@qq.com>
2021-05-21 09:57:22 +0800

    code format

--- oneflow/python/test/modules/test_repeat.py
+++ oneflow/python/test/modules/test_repeat.py
@@ -24,44 +24,96 @@ from test_util import GenArgList
 def np_repeat(x, sizes):
     return np.tile(x, sizes)
 
+
 def _test_repeat_new_dim(test_case, device):
-    input = flow.Tensor(np.random.randn(2, 4, 1, 3), dtype=flow.float32, device=flow.device(device))
+    input = flow.Tensor(
+        np.random.randn(2, 4, 1, 3), dtype=flow.float32, device=flow.device(device)
+    )
     sizes = (4, 3, 2, 3, 3)
     np_out = np_repeat(input.numpy(), sizes)
     of_out = input.repeat(sizes=sizes)
     test_case.assertTrue(np.array_equal(of_out.numpy(), np_out))
 
+
 def _test_repeat_same_dim(test_case, device):
-    input = flow.Tensor(np.random.randn(1, 2, 5, 3), dtype=flow.float32, device=flow.device(device))
+    input = flow.Tensor(
+        np.random.randn(1, 2, 5, 3), dtype=flow.float32, device=flow.device(device)
+    )
     sizes = (4, 2, 3, 19)
     of_out = input.repeat(sizes=sizes)
     np_out = np_repeat(input.numpy(), sizes)
     test_case.assertTrue(np.array_equal(of_out.numpy(), np_out))
 
+
 def _test_repeat_same_dim_int(test_case, device):
-    input = flow.Tensor(np.random.randn(1, 2, 5, 3), dtype=flow.int32, device=flow.device(device))
+    input = flow.Tensor(
+        np.random.randn(1, 2, 5, 3), dtype=flow.int32, device=flow.device(device)
+    )
     sizes = (4, 2, 3, 19)
     of_out = input.repeat(sizes=sizes)
     np_out = np_repeat(input.numpy(), sizes)
     test_case.assertTrue(np.array_equal(of_out.numpy(), np_out.astype(np.int32)))
 
+
 def _test_repeat_new_dim_backward(test_case, device):
-    input = flow.Tensor(np.random.randn(2, 4, 1, 3), dtype=flow.float32, device=flow.device(device), requires_grad=True)
+    input = flow.Tensor(
+        np.random.randn(2, 4, 1, 3),
+        dtype=flow.float32,
+        device=flow.device(device),
+        requires_grad=True,
+    )
     sizes = (4, 3, 2, 3, 3)
     of_out = input.repeat(sizes=sizes)
     of_out = of_out.sum()
     of_out.backward()
-    np_grad = [[[[216.0, 216.0, 216.0]], [[216.0, 216.0, 216.0]], [[216.0, 216.0, 216.0]], [[216.0, 216.0, 216.0]]], [[[216.0, 216.0, 216.0]], [[216.0, 216.0, 216.0]], [[216.0, 216.0, 216.0]], [[216.0, 216.0, 216.0]]]]
+    np_grad = [
+        [
+            [[216.0, 216.0, 216.0]],
+            [[216.0, 216.0, 216.0]],
+            [[216.0, 216.0, 216.0]],
+            [[216.0, 216.0, 216.0]],
+        ],
+        [
+            [[216.0, 216.0, 216.0]],
+            [[216.0, 216.0, 216.0]],
+            [[216.0, 216.0, 216.0]],
+            [[216.0, 216.0, 216.0]],
+        ],
+    ]
     test_case.assertTrue(np.array_equal(input.grad.numpy(), np_grad))
 
+
 def _test_repeat_same_dim_backward(test_case, device):
-    input = flow.Tensor(np.random.randn(1, 2, 5, 3), dtype=flow.float32, device=flow.device(device), requires_grad=True)
+    input = flow.Tensor(
+        np.random.randn(1, 2, 5, 3),
+        dtype=flow.float32,
+        device=flow.device(device),
+        requires_grad=True,
+    )
     sizes = (1, 2, 3, 1)
     of_out = input.repeat(sizes=sizes)
     of_out = of_out.sum()
     of_out.backward()
-    np_grad = [[[[6.0, 6.0, 6.0], [6.0, 6.0, 6.0], [6.0, 6.0, 6.0], [6.0, 6.0, 6.0], [6.0, 6.0, 6.0]], [[6.0, 6.0, 6.0], [6.0, 6.0, 6.0], [6.0, 6.0, 6.0], [6.0, 6.0, 6.0], [6.0, 6.0, 6.0]]]]
+    np_grad = [
+        [
+            [
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+            ],
+            [
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+                [6.0, 6.0, 6.0],
+            ],
+        ]
+    ]
     test_case.assertTrue(np.array_equal(input.grad.numpy(), np_grad))
+
 
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
