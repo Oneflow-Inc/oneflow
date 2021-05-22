5c8145c848f2bbba29384a20a0c3d596a854f4f5
BBuf <1182563586@qq.com>
2021-05-22 21:02:46 +0800

    add batch_matmul backward

--- oneflow/python/test/modules/test_matmul.py
+++ oneflow/python/test/modules/test_matmul.py
@@ -198,6 +198,55 @@ def _test_broadcast_matmul_backward(test_case, device):
     test_case.assertTrue(np.allclose(input1.grad.numpy(), np_grad, rtol=1e-05))
 
 
+def _test_batch_matmul_backward(test_case, device):
+    input1 = flow.Tensor(
+        [
+            [
+                [-0.0036776792258024216, 1.9946473836898804, -0.423959881067276],
+                [1.0892143249511719, 0.04005361348390579, -0.27883127331733704],
+            ],
+            [
+                [-0.970306396484375, 0.017771577462553978, 0.019596196711063385],
+                [0.27402883768081665, -0.8192587494850159, -0.3135920464992523],
+            ],
+        ],
+        dtype=flow.float32,
+        device=flow.device(device),
+        requires_grad=True,
+    )
+    input2 = flow.Tensor(
+        [
+            [
+                [1.118346929550171, -0.930071234703064],
+                [1.1238232851028442, 1.373764157295227],
+                [0.17178462445735931, -1.1010534763336182],
+            ],
+            [
+                [0.6694859862327576, 0.9250285029411316],
+                [-1.0835869312286377, 0.4192655086517334],
+                [1.2616937160491943, 0.33809131383895874],
+            ],
+        ],
+        dtype=flow.float32,
+        device=flow.device(device),
+        requires_grad=True,
+    )
+    of_out = flow.matmul(input1, input2)
+    of_out = of_out.sum()
+    of_out.backward()
+    np_grad = [
+        [
+            [0.18827569484710693, 2.4975874423980713, -0.9292688369750977],
+            [0.18827569484710693, 2.4975874423980713, -0.9292688369750977],
+        ],
+        [
+            [1.5945144891738892, -0.6643214225769043, 1.5997850894927979],
+            [1.5945144891738892, -0.6643214225769043, 1.5997850894927979],
+        ],
+    ]
+    test_case.assertTrue(np.allclose(input1.grad.numpy(), np_grad, rtol=1e-05))
+
+
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
     ".numpy() doesn't work in lazy mode",
@@ -211,6 +260,7 @@ class TestModule(flow.unittest.TestCase):
             _test_batch_matmul,
             _test_matmul_backward,
             _test_broadcast_matmul_backward,
+            _test_batch_matmul_backward,
         ]
         arg_dict["device"] = ["cpu", "cuda"]
         for arg in GenArgList(arg_dict):
