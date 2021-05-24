de331e81e481df4785f4a6b14ed8bca5aa154f3a
BBuf <1182563586@qq.com>
2021-05-24 16:54:00 +0800

    add reshape module backward

--- oneflow/python/test/modules/test_reshape.py
+++ oneflow/python/test/modules/test_reshape.py
@@ -14,10 +14,23 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import unittest
+from collections import OrderedDict
 
 import numpy as np
-import oneflow.experimental as flow
 
+import oneflow.experimental as flow
+from test_util import GenArgList
+
+def _test_reshape_impl(test_case, device):
+    x = np.random.randn(4, 4)
+    input = flow.Tensor(x, dtype=flow.float32, device=flow.device(device), requires_grad=True)
+    of_shape = flow.reshape(input, shape=[2, 2, 2, -1]).numpy().shape
+    np_shape = (2, 2, 2, 2)
+    test_case.assertTrue(np.array_equal(of_shape, np_shape))
+    of_out = flow.reshape(input, shape=[2, 2, 2, -1]).sum()
+    of_out.backward()
+    np_grad = [[1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0], [1.0, 1.0, 1.0, 1.0]]
+    test_case.assertTrue(np.allclose(input.grad.numpy(), np_grad, 1e-4, 1e-4))
 
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
@@ -25,22 +38,10 @@ import oneflow.experimental as flow
 )
 class TestModule(flow.unittest.TestCase):
     def test_reshape(test_case):
-        x = np.array(
-            [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-        ).astype(np.float32)
-        input = flow.Tensor(x)
-        of_shape = flow.reshape(input, shape=[2, 2, 2, -1]).numpy().shape
-        np_shape = (2, 2, 2, 2)
-        test_case.assertTrue(np.array_equal(of_shape, np_shape))
-
-    def test_tensor_reshape(test_case):
-        x = np.array(
-            [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-        ).astype(np.float32)
-        input = flow.Tensor(x)
-        of_shape = input.reshape(shape=[2, 2, 2, -1]).numpy().shape
-        np_shape = (2, 2, 2, 2)
-        test_case.assertTrue(np.array_equal(of_shape, np_shape))
+        arg_dict = OrderedDict()
+        arg_dict["device"] = ["cpu", "cuda"]
+        for arg in GenArgList(arg_dict):
+            _test_reshape_impl(test_case, *arg)
 
 
 if __name__ == "__main__":
