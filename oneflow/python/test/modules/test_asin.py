73e6c63bffad05f79291aba5d22cfaef8ab7819b
yayeoCddy <Dy_Chen95@163.com>
2021-05-24 11:37:24 +0800

    update testcase of asin and asinh

--- oneflow/python/test/modules/test_asin.py
+++ oneflow/python/test/modules/test_asin.py
@@ -35,18 +35,69 @@ def _test_asin(test_case, shape, device):
 
     test_case.assertTrue(np.allclose(of_input.grad.numpy(), np_out_grad, 1e-4, 1e-4, equal_nan=True))
 
+def _test_op_asin(test_case, shape, device):
+    np_input = np.random.randn(*shape)
+    of_input = flow.Tensor(
+        np_input, dtype=flow.float32, device=flow.device(device), requires_grad=True
+    )
+
+    of_out = of_input.asin()
+    np_out = np.arcsin(np_input)
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5, equal_nan=True))
+
+    of_out = of_out.sum()
+    of_out.backward()
+    np_out_grad = 1 /np.sqrt(1 - np_input ** 2)
+
+    test_case.assertTrue(np.allclose(of_input.grad.numpy(), np_out_grad, 1e-4, 1e-4, equal_nan=True))
+
+def _test_arcsin(test_case, shape, device):
+    np_input = np.random.randn(*shape)
+    of_input = flow.Tensor(
+        np_input, dtype=flow.float32, device=flow.device(device), requires_grad=True
+    )
+
+    of_out = flow.arcsin(of_input)
+    np_out = np.arcsin(np_input)
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5, equal_nan=True))
+
+    of_out = of_out.sum()
+    of_out.backward()
+    np_out_grad = 1 /np.sqrt(1 - np_input ** 2)
+
+    test_case.assertTrue(np.allclose(of_input.grad.numpy(), np_out_grad, 1e-4, 1e-4, equal_nan=True))
+
+def _test_op_arcsin(test_case, shape, device):
+    np_input = np.random.randn(*shape)
+    of_input = flow.Tensor(
+        np_input, dtype=flow.float32, device=flow.device(device), requires_grad=True
+    )
+
+    of_out = of_input.arcsin()
+    np_out = np.arcsin(np_input)
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5, equal_nan=True))
+
+    of_out = of_out.sum()
+    of_out.backward()
+    np_out_grad = 1 /np.sqrt(1 - np_input ** 2)
+
+    test_case.assertTrue(np.allclose(of_input.grad.numpy(), np_out_grad, 1e-4, 1e-4, equal_nan=True))
+
 
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
     ".numpy() doesn't work in lazy mode",
 )
-class TestExp(flow.unittest.TestCase):
-    def test_exp(test_case):
+class TestAsin(flow.unittest.TestCase):
+    def test_asin(test_case):
         arg_dict = OrderedDict()
         arg_dict["shape"] = [(2, ), (2, 3), (2, 4, 5, 6)]
         arg_dict["device"] = ["cpu", "cuda"]
         for arg in GenArgList(arg_dict):
             _test_asin(test_case, *arg)
+            _test_op_asin(test_case, *arg)
+            _test_arcsin(test_case, *arg)
+            _test_op_arcsin(test_case, *arg)
 
 
 if __name__ == "__main__":
--- oneflow/python/test/modules/test_asinh.py
+++ oneflow/python/test/modules/test_asinh.py
@@ -35,18 +35,35 @@ def _test_asinh(test_case, shape, device):
 
     test_case.assertTrue(np.allclose(of_input.grad.numpy(), np_out_grad, 1e-4, 1e-4))
 
+def _test_op_asinh(test_case, shape, device):
+    np_input = np.random.randn(*shape)
+    of_input = flow.Tensor(
+        np_input, dtype=flow.float32, device=flow.device(device), requires_grad=True
+    )
+
+    of_out = of_input.asinh()
+    np_out = np.arcsinh(np_input)
+    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5))
+
+    of_out = of_out.sum()
+    of_out.backward()
+    np_out_grad = 1 /np.sqrt(1 + np_input ** 2)
+
+    test_case.assertTrue(np.allclose(of_input.grad.numpy(), np_out_grad, 1e-4, 1e-4))
+
 
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
     ".numpy() doesn't work in lazy mode",
 )
-class TestExp(flow.unittest.TestCase):
-    def test_exp(test_case):
+class TestAsinh(flow.unittest.TestCase):
+    def test_asinh(test_case):
         arg_dict = OrderedDict()
         arg_dict["shape"] = [(2, ), (2, 3), (2, 4, 5, 6)]
         arg_dict["device"] = ["cpu", "cuda"]
         for arg in GenArgList(arg_dict):
             _test_asinh(test_case, *arg)
+            _test_op_asinh(test_case, *arg)
 
 
 if __name__ == "__main__":
