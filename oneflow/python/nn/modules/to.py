ab0f8b17703e7f713182cd3ce142087c898430c9
BBuf <1182563586@qq.com>
2021-05-25 15:49:33 +0800

    refine code

--- oneflow/python/nn/modules/to.py
+++ oneflow/python/nn/modules/to.py
@@ -99,6 +99,4 @@ def to_op(input, *args, **kwargs):
             return To(copy)(input, device, dtype)
         raise TypeError("to() received an invalid combination of arguments")
     except flow._oneflow_internal.exception.InputDeviceNotMatchException:
-        print('wa')
-
-
+        print("wa")
