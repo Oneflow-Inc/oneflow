58a8d1a6c1aa741ef23c995c2adff187e5f0fb13
jackalcooper <jackalcooper@gmail.com>
2021-05-28 17:08:22 +0800

    add check and log

--- oneflow/core/job/cluster.cpp
+++ oneflow/core/job/cluster.cpp
@@ -75,6 +75,8 @@ Maybe<void> Cluster::WorkerLoop() {
         ClusterInstruction::NewSessionBarrier();
         AsyncRunLazyJobSet(&lazy_runtime_thread);
       } else if (mut_cluster_instruction->has_eager_instruction()) {
+        auto* ptr = Global<ResourceDesc, ForSession>::Get();
+        CHECK(ptr != nullptr) << mut_cluster_instruction->DebugString();
         Global<vm::EagerOneflow>::Get()->RunPhysicalInstruction(
             std::const_pointer_cast<const ClusterInstructionProto>(mut_cluster_instruction));
       } else {
