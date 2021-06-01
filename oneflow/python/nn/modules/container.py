c2a0ac0ddc6255e80e343652eff7de242f95c01b
jackalcooper <jackalcooper@gmail.com>
2021-06-01 17:17:05 +0800

    address review

--- oneflow/python/nn/modules/container.py
+++ oneflow/python/nn/modules/container.py
@@ -53,6 +53,13 @@ class Sequential(Module):
         >>> import oneflow.experimental.nn as nn
         >>> nn.Sequential(nn.Conv2d(1,20,5), nn.ReLU(), nn.Conv2d(20,64,5), nn.ReLU()) #doctest: +ELLIPSIS
         <oneflow.python.nn.modules.container.Sequential object at 0x...>
+        >>> nn.Sequential(OrderedDict([
+        ...    ('conv1', nn.Conv2d(1,20,5)),
+        ...    ('relu1', nn.ReLU()),
+        ...    ('conv2', nn.Conv2d(20,64,5)),
+        ...    ('relu2', nn.ReLU())
+        ... ])) #doctest: +ELLIPSIS
+        <oneflow.python.nn.modules.container.Sequential object at 0x...>
 
     """
 
