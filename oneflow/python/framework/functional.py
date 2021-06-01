c611c1b377c8b8a64bd4dfdb727f4bd3ed97d084
hjchen2 <chenhoujiangcug@gmail.com>
2021-06-01 21:34:04 +0800

    Recursive determine input tensors.

--- oneflow/python/framework/functional.py
+++ oneflow/python/framework/functional.py
@@ -15,7 +15,23 @@ limitations under the License.
 """
 import oneflow as flow
 import oneflow._oneflow_internal
-from oneflow.python.framework.attr_util import convert_to_user_attr_value
+
+
+def RecursveDetermine(arg):
+    if isinstance(arg, flow.Tensor):
+        if not arg.is_determined:
+            arg.determine()
+        return arg._local_or_consistent_tensor
+    elif isinstance(arg, list) or isinstance(arg, tuple):
+        arg = list(arg)
+        for i in range(len(arg)):
+            arg[i] = RecursveDetermine(arg[i])
+        return arg
+    elif isinstance(arg, dict):
+        for k, v in arg.items():
+            arg[k] = RecursveDetermine(v)
+    else:
+        return arg
 
 
 class Function:
@@ -26,11 +42,9 @@ class Function:
     def __call__(self, *args, **kwargs):
         args = list(args)
         for i in range(len(args)):
-            arg = args[i]
-            if isinstance(arg, flow.Tensor):
-                if not arg.is_determined:
-                    arg.determine()
-                args[i] = arg._local_or_consistent_tensor
+            args[i] = RecursveDetermine(args[i])
+        for k, v in kwargs.items():
+            kwargs[k] = RecursveDetermine(v)
         return self.handle(*args, **kwargs)
 
 
