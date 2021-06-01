b35cffc27ca589fd0f888e1e09c7efa581dd567e
lixinqi <lixinqi0703106@163.com>
2021-06-01 11:17:04 +0800

    Export const DType* into python

--- oneflow/api/python/framework/dtype.cpp
+++ oneflow/api/python/framework/dtype.cpp
@@ -34,21 +34,21 @@ ONEFLOW_API_PYBIND11_MODULE("", m) {
       .def_property_readonly("bytes",
                              [](const DType& dtype) { return dtype.bytes().GetOrThrow(); });
 
-  m.attr("char") = DType::Char();
-  m.attr("float16") = DType::Float16();
-  m.attr("float") = DType::Float();
+  m.attr("char") = DType::Char().get();
+  m.attr("float16") = DType::Float16().get();
+  m.attr("float") = DType::Float().get();
 
-  m.attr("float32") = DType::Float();
-  m.attr("double") = DType::Double();
-  m.attr("float64") = DType::Double();
+  m.attr("float32") = DType::Float().get();
+  m.attr("double") = DType::Double().get();
+  m.attr("float64") = DType::Double().get();
 
-  m.attr("int8") = DType::Int8();
-  m.attr("int32") = DType::Int32();
-  m.attr("int64") = DType::Int64();
+  m.attr("int8") = DType::Int8().get();
+  m.attr("int32") = DType::Int32().get();
+  m.attr("int64") = DType::Int64().get();
 
-  m.attr("uint8") = DType::UInt8();
-  m.attr("record") = DType::OFRecord();
-  m.attr("tensor_buffer") = DType::TensorBuffer();
+  m.attr("uint8") = DType::UInt8().get();
+  m.attr("record") = DType::OFRecord().get();
+  m.attr("tensor_buffer") = DType::TensorBuffer().get();
 }
 
 }  // namespace oneflow
