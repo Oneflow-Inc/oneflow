9c86b03b54cdfb05c729ef6a03e1ad3f4c29e874
Flowingsun007 <flowingsun007@163.com>
2021-06-03 09:56:34 +0800

    add cast backward test

--- oneflow/python/test/modules/test_cast.py
+++ oneflow/python/test/modules/test_cast.py
@@ -14,9 +14,47 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import unittest
+from collections import OrderedDict
 
 import numpy as np
+
 import oneflow.experimental as flow
+from test_util import GenArgList
+
+
+def _test_cast_float2int(test_case, device, shape):
+    np_arr = np.random.randn(*shape).astype(np.float32)
+    input = flow.Tensor(np_arr, dtype=flow.float32, device=flow.device(device))
+    output = flow.cast(input, flow.int8)
+    np_out = np_arr.astype(np.int8)
+    test_case.assertTrue(np.array_equal(output.numpy(), np_out))
+
+def _test_cast_int2float(test_case, device, shape):
+    np_arr = np.random.randn(*shape).astype(np.int8)
+    input = flow.Tensor(np_arr, dtype=flow.int8, device=flow.device(device))
+    output = flow.cast(input, flow.float32)
+    np_out = np_arr.astype(np.float32)
+    test_case.assertTrue(np.array_equal(output.numpy(), np_out))
+
+def _test_cast_tensor_function(test_case, device, shape):
+    np_arr = np.random.randn(*shape).astype(np.float32)
+    input = flow.Tensor(np_arr, dtype=flow.float32, device=flow.device(device))
+    output = input.cast(flow.int8)
+    np_out = np_arr.astype(np.int8)
+    test_case.assertTrue(np.array_equal(output.numpy(), np_out))
+
+def _test_cast_backward(test_case, device, shape):
+    np_arr = np.random.randn(*shape).astype(np.float32)
+    x = flow.Tensor(np_arr, 
+        dtype=flow.float32, 
+        device=flow.device(device),
+        requires_grad=True
+    )
+    y = flow.cast(x, flow.int8)
+    z = y.sum()
+    z.backward()
+    np_out = np_arr.astype(np.int8)
+    test_case.assertTrue(np.array_equal(x.grad.numpy(), np.ones(shape=shape)))
 
 
 @unittest.skipIf(
@@ -24,26 +62,18 @@ import oneflow.experimental as flow
     ".numpy() doesn't work in lazy mode",
 )
 class TestCast(flow.unittest.TestCase):
-    def test_cast_float2int(test_case):
-        np_arr = np.random.randn(2, 3, 4, 5).astype(np.float32)
-        input = flow.Tensor(np_arr, dtype=flow.float32)
-        output = flow.cast(input, flow.int8)
-        np_out = np_arr.astype(np.int8)
-        test_case.assertTrue(np.array_equal(output.numpy(), np_out))
-
-    def test_cast_int2float(test_case):
-        np_arr = np.random.randn(5, 2, 3, 4).astype(np.int8)
-        input = flow.Tensor(np_arr, dtype=flow.int8)
-        output = flow.cast(input, flow.float32)
-        np_out = np_arr.astype(np.float32)
-        test_case.assertTrue(np.array_equal(output.numpy(), np_out))
-
-    def test_cast_tensor_function(test_case):
-        np_arr = np.random.randn(1, 2, 3, 4).astype(np.float32)
-        input = flow.Tensor(np_arr, dtype=flow.float32)
-        output = input.cast(flow.int8)
-        np_out = np_arr.astype(np.int8)
-        test_case.assertTrue(np.array_equal(output.numpy(), np_out))
+    def test_cast(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["test_fun"] = [
+            _test_cast_float2int, 
+            _test_cast_int2float,
+            _test_cast_tensor_function,
+            _test_cast_backward
+        ]
+        arg_dict["device"] = ["cpu", "cuda"]
+        arg_dict["shape"] = [(2, 3), (2, 3, 4), (2, 3, 4, 5)]
+        for arg in GenArgList(arg_dict):
+            arg[0](test_case, *arg[1:])
 
 
 if __name__ == "__main__":
