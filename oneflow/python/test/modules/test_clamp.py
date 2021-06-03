ac120d488b46efd8eae8d192015d895c1fd61daa
puchapu <495106868@qq.com>
2021-06-03 15:03:56 +0800

    update_test_clamp

--- oneflow/python/test/modules/test_clamp.py
+++ oneflow/python/test/modules/test_clamp.py
@@ -22,45 +22,45 @@ import oneflow.experimental as flow
 from test_util import GenArgList
 
 
-def _test_clamp(test_case, device):
+def _test_clamp(test_case, shape, device):
     input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device)
+        np.random.randn(*shape), dtype=flow.float32, device=flow.device(device)
     )
     of_out = flow.clamp(input, 0.1, 0.5)
     np_out = np.clip(input.numpy(), 0.1, 0.5)
     test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5))
 
 
-def _test_tensor_clamp(test_case, device):
+def _test_tensor_clamp(test_case, shape, device):
     input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device)
+        np.random.randn(*shape), dtype=flow.float32, device=flow.device(device)
     )
     of_out = input.clamp(0.1, 0.5)
     np_out = np.clip(input.numpy(), 0.1, 0.5)
     test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5))
 
 
-def _test_clamp_scalar_min(test_case, device):
+def _test_clamp_scalar_min(test_case, shape, device):
     input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device)
+        np.random.randn(*shape), dtype=flow.float32, device=flow.device(device)
     )
     of_out = flow.clamp(input, 0.1, None)
     np_out = np.clip(input.numpy(), 0.1, None)
     test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5))
 
 
-def _test_clamp_scalar_max(test_case, device):
+def _test_clamp_scalar_max(test_case, shape, device):
     input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device)
+        np.random.randn(*shape), dtype=flow.float32, device=flow.device(device)
     )
     of_out = flow.clamp(input, None, 0.5)
     np_out = np.clip(input.numpy(), None, 0.5)
     test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 1e-5, 1e-5))
 
 
-def _test_clamp_integral(test_case, device):
+def _test_clamp_integral(test_case, shape, device):
     input = flow.Tensor(
-        np.random.randint(3, 10, (2, 6, 5, 3)), device=flow.device(device)
+        np.random.randint(3, 10, (shape)), device=flow.device(device)
     )
     of_out = flow.clamp(input, 1, 5)
     np_out = np.clip(input.numpy(), 1, 5)
@@ -73,9 +73,9 @@ def _numpy_clamp_grad(arr, min, max):
     return grad
 
 
-def _test_clamp_backward(test_case, device):
+def _test_clamp_backward(test_case, shape, device):
     x = flow.Tensor(
-        np.random.randn(2, 6, 5, 3),
+        np.random.randn(*shape),
         dtype=flow.float32,
         device=flow.device(device),
         requires_grad=True,
@@ -87,10 +87,6 @@ def _test_clamp_backward(test_case, device):
     )
 
 
-@unittest.skipIf(
-    not flow.unittest.env.eager_execution_enabled(),
-    ".numpy() doesn't work in lazy mode",
-)
 class TestClampModule(flow.unittest.TestCase):
     def test_clamp(test_case):
         arg_dict = OrderedDict()
@@ -102,6 +98,7 @@ class TestClampModule(flow.unittest.TestCase):
             _test_clamp_integral,
             _test_clamp_backward,
         ]
+        arg_dict["shape"] = [(2,), (2, 3), (2, 4, 5, 6)]
         arg_dict["device"] = ["cpu", "cuda"]
         for arg in GenArgList(arg_dict):
             arg[0](test_case, *arg[1:])
