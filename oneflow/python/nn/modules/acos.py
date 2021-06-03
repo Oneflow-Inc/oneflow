e5ae8027b92bcf6c3ab437de021baff33bdb4202
JiaojiaoYe1994 <jiaojiaoye@10.5.30.42>
2021-06-03 14:34:03 +0800

    fix format bug

--- oneflow/python/nn/modules/acos.py
+++ oneflow/python/nn/modules/acos.py
@@ -68,3 +68,4 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    
--- oneflow/python/nn/modules/math_ops.py
+++ oneflow/python/nn/modules/math_ops.py
@@ -1149,3 +1149,4 @@ if __name__ == "__main__":
     import doctest
 
     doctest.testmod(name="asinh_op")
+    
