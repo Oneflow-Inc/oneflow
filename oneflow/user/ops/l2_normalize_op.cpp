9183cea209125d05e8cb7bf3ef7bd495218cd2bd
Li Xinqi <lixinqi2010@gmail.com>
2021-06-04 11:45:42 +0800

    add interface InferContext::InputShape (#5048)
    * add interface InferContext::InputShape
    
    * delete repeated interface calling in dropout_op.cpp
    
    * auto format by CI
    
    Co-authored-by: liufengwei <2472937968@qq.com>
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/user/ops/l2_normalize_op.cpp
+++ oneflow/user/ops/l2_normalize_op.cpp
@@ -24,16 +24,16 @@ REGISTER_USER_OP("l2_normalize")
     .Attr<int32_t>("axis")
     .Attr<float>("epsilon")
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      const Shape* x_shape = ctx->Shape4ArgNameAndIndex("x", 0);
+      const Shape& x_shape = ctx->InputShape("x", 0);
       Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
       Shape* square_x_sum_shape = ctx->Shape4ArgNameAndIndex("square_x_sum", 0);
       const int32_t axis = ctx->Attr<int32_t>("axis");
       const float epsilon = ctx->Attr<float>("epsilon");
       CHECK_GE_OR_RETURN(axis, 0);
-      CHECK_LT_OR_RETURN(axis, x_shape->NumAxes());
+      CHECK_LT_OR_RETURN(axis, x_shape.NumAxes());
       CHECK_GT_OR_RETURN(epsilon, 0);
-      *y_shape = *x_shape;
-      *square_x_sum_shape = *x_shape;
+      *y_shape = x_shape;
+      *square_x_sum_shape = x_shape;
       square_x_sum_shape->Set(axis, 1);
       return Maybe<void>::Ok();
     })
@@ -65,24 +65,24 @@ REGISTER_USER_OP("l2_normalize_grad")
     .Attr<int32_t>("axis")
     .Attr<float>("epsilon")
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      const Shape* dy_shape = ctx->Shape4ArgNameAndIndex("dy", 0);
-      const Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
-      const Shape* square_x_sum_shape = ctx->Shape4ArgNameAndIndex("square_x_sum", 0);
+      const Shape& dy_shape = ctx->InputShape("dy", 0);
+      const Shape& y_shape = ctx->InputShape("y", 0);
+      const Shape& square_x_sum_shape = ctx->InputShape("square_x_sum", 0);
       Shape* dx_shape = ctx->Shape4ArgNameAndIndex("dx", 0);
       const int32_t axis = ctx->Attr<int32_t>("axis");
       const float epsilon = ctx->Attr<float>("epsilon");
-      CHECK_EQ_OR_RETURN(*dy_shape, *y_shape);
+      CHECK_EQ_OR_RETURN(dy_shape, y_shape);
       CHECK_GE_OR_RETURN(axis, 0);
-      CHECK_LT_OR_RETURN(axis, dy_shape->NumAxes());
+      CHECK_LT_OR_RETURN(axis, dy_shape.NumAxes());
       CHECK_GT_OR_RETURN(epsilon, 0);
-      FOR_RANGE(int32_t, i, 0, dy_shape->NumAxes()) {
+      FOR_RANGE(int32_t, i, 0, dy_shape.NumAxes()) {
         if (i == axis) {
-          CHECK_EQ_OR_RETURN(square_x_sum_shape->At(i), 1);
+          CHECK_EQ_OR_RETURN(square_x_sum_shape.At(i), 1);
         } else {
-          CHECK_EQ_OR_RETURN(square_x_sum_shape->At(i), dy_shape->At(i));
+          CHECK_EQ_OR_RETURN(square_x_sum_shape.At(i), dy_shape.At(i));
         }
       }
-      *dx_shape = *dy_shape;
+      *dx_shape = dy_shape;
       return Maybe<void>::Ok();
     })
     .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
--- oneflow/user/ops/relu_op.cpp
+++ oneflow/user/ops/relu_op.cpp
@@ -23,9 +23,9 @@ REGISTER_USER_OP("relu")
     .Input("in")
     .Output("out")
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      const Shape* in_shape = ctx->Shape4ArgNameAndIndex("in", 0);
+      const Shape& in_shape = ctx->InputShape("in", 0);
       Shape* out_shape = ctx->Shape4ArgNameAndIndex("out", 0);
-      *out_shape = *in_shape;
+      *out_shape = in_shape;
       return Maybe<void>::Ok();
     })
     .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
@@ -48,11 +48,11 @@ REGISTER_USER_OP("relu_grad")
     .Input("dy")
     .Output("dx")
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      const Shape* y_shape = ctx->Shape4ArgNameAndIndex("y", 0);
-      const Shape* dy_shape = ctx->Shape4ArgNameAndIndex("dy", 0);
+      const Shape& y_shape = ctx->InputShape("y", 0);
+      const Shape& dy_shape = ctx->InputShape("dy", 0);
       Shape* dx_shape = ctx->Shape4ArgNameAndIndex("dx", 0);
-      CHECK(*dy_shape == *y_shape);
-      *dx_shape = *dy_shape;
+      CHECK(dy_shape == y_shape);
+      *dx_shape = dy_shape;
       return Maybe<void>::Ok();
     })
     .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
