c5c9ac92c0beaa91b2914689252f5b0f5a8e7875
duchengyao <duchengyao@zhejianglab.edu.cn>
2021-06-07 11:07:17 +0800

    init consine annealing lr

---
+++ oneflow/python/nn/optimizer/cosine_annealing_lr.py
@@ -0,0 +1,80 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import math
+
+from oneflow.python.oneflow_export import experimental_api, oneflow_export
+from .lr_scheduler import LrScheduler
+
+
+@oneflow_export("optim.lr_scheduler.CosineScheduler")
+@experimental_api
+class CosineAnnealingLR(LrScheduler):
+
+    r"""Set the learning rate of each parameter group using a cosine annealing
+    schedule, where :math:`\eta_{max}` is set to the initial lr and
+    :math:`T_{cur}` is the number of epochs since the last restart in SGDR:
+
+    .. math::
+        \begin{aligned}
+            \eta_t & = \eta_{min} + \frac{1}{2}(\eta_{max} - \eta_{min})\left(1
+            + \cos\left(\frac{T_{cur}}{T_{max}}\pi\right)\right),
+            & T_{cur} \neq (2k+1)T_{max}; \\
+            \eta_{t+1} & = \eta_{t} + \frac{1}{2}(\eta_{max} - \eta_{min})
+            \left(1 - \cos\left(\frac{1}{T_{max}}\pi\right)\right),
+            & T_{cur} = (2k+1)T_{max}.
+        \end{aligned}
+
+    When last_epoch=-1, sets initial lr as lr. Notice that because the schedule
+    is defined recursively, the learning rate can be simultaneously modified
+    outside this scheduler by other operators. If the learning rate is set
+    solely by this scheduler, the learning rate at each step becomes:
+
+    .. math::
+        \eta_t = \eta_{min} + \frac{1}{2}(\eta_{max} - \eta_{min})\left(1 +
+        \cos\left(\frac{T_{cur}}{T_{max}}\pi\right)\right)
+
+    It has been proposed in
+    `SGDR: Stochastic Gradient Descent with Warm Restarts`_. Note that this only
+    implements the cosine annealing part of SGDR, and not the restarts.
+
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        T_max (int): Maximum number of iterations.
+        eta_min (float): Minimum learning rate. Default: 0.
+        last_epoch (int): The index of last epoch. Default: -1.
+        verbose (bool): If ``True``, prints a message to stdout for
+            each update. Default: ``False``.
+
+    .. _SGDR\: Stochastic Gradient Descent with Warm Restarts:
+        https://arxiv.org/abs/1608.03983
+    """
+    def __init__(
+            self, optimizer, steps: int, alpha: float = 0.0, last_step=-1, verbose=False
+    ):
+        assert steps > 0, f"steps must greater than zero, but got {steps}"
+
+        self.steps = steps
+        self.alpha = alpha
+        super().__init__(optimizer, last_step, verbose)
+
+    def get_lr(self):
+        if self.last_step < self.steps:
+            cos_decay = 0.5 * (1 + math.cos(math.pi * self.last_step / self.steps))
+            decay_factor = (1 - self.alpha) * cos_decay + self.alpha
+            return [base_lr * decay_factor for base_lr in self.base_lrs]
+        else:
+            return [base_lr * self.alpha for base_lr in self.base_lrs]
