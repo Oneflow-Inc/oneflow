f09f85b8e23f9cd44049758ee9914f601d8c1db8
zzk0 <zzk9460@gmail.com>
2021-06-07 13:44:59 +0000

    move option to top level CMakeLists.txt

--- cmake/oneflow.cmake
+++ cmake/oneflow.cmake
@@ -419,11 +419,10 @@ copy_files("${OF_CORE_HDRS}" "${PROJECT_SOURCE_DIR}" "${ONEFLOW_INCLUDE_DIR}" of
 
 add_dependencies(pip_install of_include_copy)
 
-option(BUILD_JNI "If build with JNI bindings" OFF)
 
 if (BUILD_JNI)
   add_subdirectory(${PROJECT_SOURCE_DIR}/oneflow/api/java)
-  
+
   target_link_libraries(oneflow 
     of_ccobj
     of_cfgobj
