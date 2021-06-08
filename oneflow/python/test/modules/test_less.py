44dd2bc725b571b3139722f7c77ee0b5312f19e4
BBuf <1182563586@qq.com>
2021-06-08 11:23:25 +0800

    fix conflict

--- oneflow/python/test/modules/test_less.py
+++ oneflow/python/test/modules/test_less.py
@@ -58,6 +58,7 @@ def _test_less_int_scalar(test_case, device):
     np_out = np.less(np_arr, input2)
     test_case.assertTrue(np.array_equal(of_out.numpy(), np_out))
 
+
 def _test_less_int_tensor_int_scalr(test_case, device):
     np_arr = np.random.randint(2, size=(2, 3, 4, 5))
     input1 = flow.Tensor(np_arr, dtype=flow.int, device=flow.device(device))
