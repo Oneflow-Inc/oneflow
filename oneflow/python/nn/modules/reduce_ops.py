5e693163fe9bbcf9086affdb69b7b721c71833a7
Zhenhua <huangzhenhua@zhejianglab.com>
2021-06-08 15:56:47 +0800

    Update doctest

--- oneflow/python/nn/modules/reduce_ops.py
+++ oneflow/python/nn/modules/reduce_ops.py
@@ -62,16 +62,71 @@ def _sum(input, dim=None, keepdims=False):
     .. code-block:: python
 
         >>> import oneflow.experimental as flow
-        >>> import numpy as np
         >>> flow.enable_eager_execution()
-        >>> input = flow.Tensor(np.array([1.3, 0.5, 2.7]))
-        >>> out = flow.sum(input)
-        >>> out
-        tensor([4.5], dtype=oneflow.float32)
+        >>> input = flow.Tensor([[1, 2, 3], [4, 5, 6]])
+        >>> flow.sum(input)
+        tensor([21.], dtype=oneflow.float32)
+        >>> flow.sum(input, dim=0)
+        tensor([5., 7., 9.], dtype=oneflow.float32)
+        >>> flow.sum(input, dim=1)
+        tensor([ 6., 15.], dtype=oneflow.float32)
 
     """
 
     return Sum(dim, keepdims)(input)
+
+
+class Mean(Module):
+    def __init__(
+        self, axis: Optional[Union[int, Sequence[int]]] = None, keepdims: bool = False
+    ) -> None:
+        super().__init__()
+
+        self.axis = axis
+        self.keepdims = keepdims
+        if axis is None:
+            self.axes = []
+        else:
+            self.axes = list(axis) if isinstance(axis, collections.Sized) else [axis]
+
+    def forward(self, input):
+        axis_checked = _check_axis(self.axis, input.shape)
+        if len(axis_checked) == 0:
+            return input
+        reduce_sum = flow.experimental.sum(input, dim=self.axis, keepdims=self.keepdims)
+        reduce_count = 1
+        if len(self.axes) == 0:
+            for dim in input.shape:
+                reduce_count *= dim
+        else:
+            for i in self.axes:
+                reduce_count *= input.shape[i]
+        return flow.experimental.mul(reduce_sum, 1.0 / reduce_count)
+
+
+@oneflow_export("mean")
+@register_tensor_op("mean")
+@experimental_api
+def _mean(input, dim=None, keepdims=False):
+    r"""Computes the mean of row of elements in a tensor in the given axis, if the axis is None, mean of all elements will be caculated.
+    
+    For example:
+
+    .. code-block:: python
+
+        >>> import oneflow.experimental as flow
+        >>> flow.enable_eager_execution()
+        >>> input = flow.Tensor([[1, 2, 3], [4, 5, 6]])
+        >>> flow.mean(input)
+        tensor([3.5], dtype=oneflow.float32)
+        >>> flow.mean(input, dim=0)
+        tensor([2.5, 3.5, 4.5], dtype=oneflow.float32)
+        >>> flow.mean(input, dim=1)
+        tensor([2., 5.], dtype=oneflow.float32)
+
+    """
+
+    return Mean(dim, keepdims)(input)
 
 
 class Min(Module):
@@ -102,12 +157,14 @@ def _min(input, dim=None, keepdims=False):
     .. code-block:: python
 
         >>> import oneflow.experimental as flow
-        >>> import numpy as np
         >>> flow.enable_eager_execution()
-        >>> input = flow.Tensor(np.array([1.3, 0.5, 2.7]))
-        >>> out = flow.min(input)
-        >>> out
-        tensor([0.5], dtype=oneflow.float32)
+        >>> input = flow.Tensor([[4, 1, 5], [2, 6, 3]])
+        >>> flow.min(input)
+        tensor([1.], dtype=oneflow.float32)
+        >>> flow.min(input, dim=0)
+        tensor([2., 1., 3.], dtype=oneflow.float32)
+        >>> flow.min(input, dim=1)
+        tensor([1., 2.], dtype=oneflow.float32)
 
     """
 
@@ -142,12 +199,14 @@ def _max(input, dim=None, keepdims=False):
     .. code-block:: python
 
         >>> import oneflow.experimental as flow
-        >>> import numpy as np
         >>> flow.enable_eager_execution()
-        >>> input = flow.Tensor(np.array([1.3, 0.5, 2.7]))
-        >>> out = flow.max(input)
-        >>> out
-        tensor([2.7], dtype=oneflow.float32)
+        >>> input = flow.Tensor([[4, 1, 5], [2, 6, 3]])
+        >>> flow.max(input)
+        tensor([6.], dtype=oneflow.float32)
+        >>> flow.max(input, dim=0)
+        tensor([4., 6., 5.], dtype=oneflow.float32)
+        >>> flow.max(input, dim=1)
+        tensor([5., 6.], dtype=oneflow.float32)
 
     """
 
@@ -157,4 +216,4 @@ def _max(input, dim=None, keepdims=False):
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod()
+    doctest.testmod(raise_on_error=True)
