b053d559a18876aacae966351622618b307f4517
tea321000 <834914152@qq.com>
2021-06-08 18:07:22 +0800

    complete ops and test program

--- oneflow/python/experimental/stack.py
+++ oneflow/python/experimental/stack.py
@@ -1,32 +1,85 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import oneflow as flow
 from typing import List, Tuple
 from oneflow.python.oneflow_export import oneflow_export, experimental_api
 from oneflow.python.framework.tensor import register_tensor_op
-import oneflow.python.nn as nn
+from oneflow.python.nn.module import Module
+from oneflow.python.framework.tensor import Tensor
+
+
+class Stack(Module):
+    def __init__(self, dim: int = 0) -> None:
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, inputs):
+        assert isinstance(inputs, (List, Tuple))
+        input_shape = inputs[0].shape
+        max_dim = len(input_shape)
+
+        # The axis must be in range [-(_max_dim +1), _max_dim]
+        if self.dim < 0:
+            self.dim = self.dim + max_dim + 1
+        assert (self.dim >= 0) and (self.dim <= max_dim)
+        input_list_length = len(inputs)
+        for i in range(input_list_length):
+            current_shape = inputs[i].shape
+            assert (
+                input_shape == current_shape
+            ), "Each tensor should have the same shape ! Found a tensor instance shape is: {}".format(
+                current_shape
+            )
+            inputs[i] = flow.experimental.unsqueeze(inputs[i], dim=self.dim)
+        return flow.experimental.cat(inputs, dim=self.dim)
 
 
 @oneflow_export("stack")
 @register_tensor_op("stack")
 @experimental_api
-def Stack(inputs, dim=0):
-    _input_shape = inputs[0].shape
-    _max_dim = len(_input_shape)
-
-    assert isinstance(inputs, (List, Tuple))
-    _input_shape = inputs[0].shape
-    _max_dim = len(_input_shape)
-
-    # The axis must be in range [-(_max_dim +1), _max_dim]
-    if dim < 0:
-        dim = dim + _max_dim + 1
-    assert (dim >= 0) and (dim <= _max_dim)
-    _input_list_length = len(inputs)
-    for i in range(_input_list_length):
-        _current_shape = inputs[i].shape
-        assert (
-                _input_shape == _current_shape
-        ), "Each tensor should have the same shape ! Found a tensor instance shape is: {}".format(
-            _current_shape
-        )
-        inputs[i] = flow.experimental.unsqueeze(inputs[i], dim=dim)
-    return flow.experimental.cat(inputs, dim=dim)
+def stack(inputs: Tensor, dim: int = 0) -> None:
+    r"""Concatenates a sequence of tensors along a new dimension.
+    The returned tensor shares the same underlying data with input tensors.
+    A :attr:`dim` value within the range `[-input.ndimension() - 1, input.ndimension() + 1)`
+    can be used. Negative :attr:`dim` will correspond to :meth:`unsqueeze`
+    applied at :attr:`dim` = ``dim + input.ndimension() + 1``.
+    Args:
+        inputs (List[Tensor]): the list of input tensors. Each tensor should have the same shape.
+        dim (int): the index at which to insert the singleton dimension
+    For example:
+    .. code-block:: python
+        >>> import numpy as np
+        >>> import oneflow.experimental as flow
+        >>> flow.enable_eager_execution()
+        >>> x = flow.Tensor(np.random.rand(2, 4, 6))
+        >>> y = flow.Tensor(np.random.rand(2, 4, 6))
+        >>> out = flow.experimental.stack([x, y])
+        >>> out.shape
+        flow.Size([4, 4, 6])
+    """
+    return Stack(dim)(inputs)
--- oneflow/python/test/modules/test_stack.py
+++ oneflow/python/test/modules/test_stack.py
@@ -1,5 +1,20 @@
 """
 Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -16,6 +31,7 @@ import numpy as np
 import unittest
 from collections import OrderedDict
 import random
+
 
 def _test_stack(test_case, device, shapes):
     for s in shapes:
@@ -36,8 +52,12 @@ def _test_stack_backward(test_case, device, shapes):
         y_tensor = flow.Tensor(y, device=flow.device(device), requires_grad=True)
         out_of = flow.experimental.stack([x_tensor, y_tensor]).sum()
         out_of.backward()
-        test_case.assertTrue(np.allclose(x_tensor.grad.numpy(), np.ones(x_tensor.shape), 1e-5, 1e-5))
-        test_case.assertTrue(np.allclose(y_tensor.grad.numpy(), np.ones(y_tensor.shape), 1e-5, 1e-5))
+        test_case.assertTrue(
+            np.allclose(x_tensor.grad.numpy(), np.ones(x_tensor.shape), 1e-5, 1e-5)
+        )
+        test_case.assertTrue(
+            np.allclose(y_tensor.grad.numpy(), np.ones(y_tensor.shape), 1e-5, 1e-5)
+        )
 
 
 def _test_stack_different_dim(test_case, device, shapes):
@@ -59,13 +79,19 @@ def _test_stack_different_dim(test_case, device, shapes):
 class TestStack(flow.unittest.TestCase):
     def test_stack(test_case):
         arg_dict = OrderedDict()
-        arg_dict["test_fun"] = [_test_stack,  _test_stack_backward, _test_stack_different_dim]
+        arg_dict["test_fun"] = [
+            _test_stack,
+            _test_stack_backward,
+            _test_stack_different_dim,
+        ]
         arg_dict["device"] = ["cpu", "cuda"]
         # Generate random tuple from 3D to 10D with values ranging from 1 to 10
-        arg_dict["shape"] = [tuple(random.randrange(1, 10) for _ in range(i)) for i in range(3, 10)]
+        arg_dict["shape"] = [
+            tuple(random.randrange(1, 10) for _ in range(i)) for i in range(3, 10)
+        ]
         for arg in GenArgList(arg_dict):
             arg[0](test_case, *arg[1:])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
