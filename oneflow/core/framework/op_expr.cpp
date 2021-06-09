5c07bc625a37e6510f4400a0ad12081de5d3ea98
clackhan <han_binbin@163.com>
2021-06-09 18:14:56 +0800

    override OutputDType

--- oneflow/core/framework/op_expr.cpp
+++ oneflow/core/framework/op_expr.cpp
@@ -190,6 +190,9 @@ class UserOpExprInferContext : public user_op::InferContext {
   Shape* Shape4ArgNameAndIndex(const std::string& arg_name, int32_t index) override {
     return TensorDesc4ArgNameAndIndex(arg_name, index)->mut_shape();
   }
+  DataType* OutputDType(const std::string& arg_name, int32_t index) override {
+    return const_cast<UserOpExprInferContext*>(this)->Dtype4ArgNameAndIndex(arg_name, index);
+  }
   DataType* Dtype4ArgNameAndIndex(const std::string& arg_name, int32_t index) override {
     return TensorDesc4ArgNameAndIndex(arg_name, index)->mut_data_type();
   }
