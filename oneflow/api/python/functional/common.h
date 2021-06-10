8733a330546f03d433c7e49702a5768a1949e120
hjchen2 <chenhoujiangcug@gmail.com>
2021-06-10 12:55:13 +0800

    Check and throw error.

--- oneflow/api/python/functional/common.h
+++ oneflow/api/python/functional/common.h
@@ -20,12 +20,20 @@ limitations under the License.
 #include <vector>
 #include <pybind11/pybind11.h>
 
+#include "oneflow/core/common/maybe.h"
 #include "oneflow/core/common/preprocessor.h"
 
 namespace py = pybind11;
 
 namespace oneflow {
+
+class MutableCfgAttrMap;
+
 namespace one {
+
+class Tensor;
+class TensorTuple;
+
 namespace functional {
 
 namespace detail {
@@ -48,12 +56,12 @@ inline bool isinstance(py::handle obj) {
   return py::isinstance<T>(obj);
 }
 
-#define SPECIALIZE_IS_INSTANCE(T)                    \
-  template<>                                         \
-  inline bool isinstance<T>(py::handle obj) {        \
-    static py::object dummy = py::cast(T());         \
-    CHECK_NOTNULL(dummy.ptr());                      \
-    return py::isinstance(obj, py::type::of(dummy)); \
+#define SPECIALIZE_IS_INSTANCE(T)                                                \
+  template<>                                                                     \
+  inline bool isinstance<T>(py::handle obj) {                                    \
+    static py::object dummy = py::cast(T());                                     \
+    CHECK_NOTNULL_OR_THROW(dummy.ptr()) << "Pybind has no internal type " << #T; \
+    return py::isinstance(obj, py::type::of(dummy));                             \
   }
 
 OF_PP_FOR_EACH_TUPLE(SPECIALIZE_IS_INSTANCE, ARITHMETIC_TYPE_SEQ OF_PP_MAKE_TUPLE_SEQ(std::string));
@@ -68,25 +76,49 @@ struct type_caster {
 };
 
 template<typename T>
-struct type_caster<std::vector<T>> {
-  static std::vector<T> cast(py::handle src);
-};
-
-template<typename T>
 inline T cast(py::handle obj) {
   return type_caster<T>::cast(obj);
 }
 
 template<typename T>
+struct type_caster<std::vector<T>> {
+  static std::vector<T> cast(py::handle src);
+};
+
+#define SPECIALIZE_INTERNAL_IS_INSTANCE_ADN_CAST(T)                           \
+  template<>                                                                  \
+  inline bool isinstance<std::shared_ptr<T>>(py::handle obj) {                \
+    return detail::isinstance<T>(obj);                                        \
+  }                                                                           \
+                                                                              \
+  template<>                                                                  \
+  struct type_caster<std::shared_ptr<T>> {                                    \
+    static std::shared_ptr<T> cast(py::handle src) {                          \
+      CHECK_OR_THROW(detail::isinstance<T>(src))                              \
+          << "Can not cast to " << #T << " from python object whose type is " \
+          << detail::cast<std::string>(py::str(py::type::of(src)));           \
+      return py::cast<std::shared_ptr<T>>(src);                               \
+    }                                                                         \
+  };
+
+SPECIALIZE_INTERNAL_IS_INSTANCE_ADN_CAST(one::Tensor);
+SPECIALIZE_INTERNAL_IS_INSTANCE_ADN_CAST(one::TensorTuple);
+SPECIALIZE_INTERNAL_IS_INSTANCE_ADN_CAST(MutableCfgAttrMap);
+
+#undef SPECIALIZE_INTERNAL_IS_INSTANCE_ADN_CAST
+
+template<typename T>
 /*static*/ std::vector<T> type_caster<std::vector<T>>::cast(py::handle src) {
   PyObject* obj = src.ptr();
   bool is_tuple = PyTuple_Check(obj);
-  CHECK(is_tuple || PyList_Check(obj)) << "The python object is not list or tuple, but is "
-                                       << detail::cast<std::string>(py::str(py::type::of(src)));
+  CHECK_OR_THROW(is_tuple || PyList_Check(obj))
+      << "The python object is not list or tuple, but is "
+      << detail::cast<std::string>(py::str(py::type::of(src)));
   size_t size = is_tuple ? PyTuple_GET_SIZE(obj) : PyList_GET_SIZE(obj);
   std::vector<T> values(size);
   for (int i = 0; i < size; ++i) {
-    values[i] = detail::cast<T>(is_tuple ? PyTuple_GET_ITEM(obj, i) : PyList_GET_ITEM(obj, i));
+    values[i] = detail::cast<T>(is_tuple ? py::handle(PyTuple_GET_ITEM(obj, i))
+                                         : py::handle(PyList_GET_ITEM(obj, i)));
   }
   return values;
 }
