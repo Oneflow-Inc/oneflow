5a432332fe7a5d6b72ec63d65b7fc87de00de76d
BBuf <1182563586@qq.com>
2021-06-10 14:25:09 +0800

    add adaptive_avg_pool2d ops register

--- oneflow/user/ops/adaptive_pool.cpp
+++ oneflow/user/ops/adaptive_pool.cpp
@@ -22,13 +22,26 @@ namespace {
 
 Maybe<void> InferTensorDesc(user_op::InferContext* ctx) {
   const user_op::TensorDesc* first_in_desc = ctx->TensorDesc4ArgNameAndIndex("in", 0);
-  const int64_t axis = ctx->Attr<int64_t>("axis");
-  
+  std::vector<int> output_size = ctx->Attr<std::vector<int>>("output_size");
+  DimVector out_dim_vec = first_in_desc->shape().dim_vec();
+  int h = out_dim_vec[2];
+  int w = out_dim_vec[3];
+  if(output_size.size() >= 1){
+    h = output_size[0]; //h
+    w = output_size[0];
+    if(output_size.size() == 2){
+      h = output_size[1]; //w
+    }
+  }
+  user_op::TensorDesc* out_desc = ctx->TensorDesc4ArgNameAndIndex("out", 0);
+  *out_desc->mut_shape() = Shape(out_dim_vec);
+  *out_desc->mut_is_dynamic() = *ctx->IsDynamic4ArgNameAndIndex("x", 0);
 }
 
 Maybe<void> FwGetSbpFn(user_op::SbpContext* ctx) {
   const user_op::TensorDesc& tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("x", 0);
-  FOR_RANGE(int64_t, i, 0, tensor.shape().NumAxes()) {
+  // only for nchw
+  FOR_RANGE(int64_t, i, 0, std::min(2, (int)tensor.shape().NumAxes())) {
     ctx->NewBuilder().Split(user_op::OpArg("x", 0), i).Split(user_op::OpArg("y", 0), i).Build();
   }
   return Maybe<void>::Ok();
@@ -55,8 +68,7 @@ REGISTER_USER_OP_GRAD("adaptive_avg_pool2d").SetBackwardOpConfGenFn([](user_op::
     return builder.OpTypeName("adaptive_avg_pool2d_grad")
         .InputBind("x", ctx->FwOp().input("in", 0))
         .InputBind("dy", ctx->FwOp().output_grad("out", 0))
-        .Attr("scale", ctx->FwOp().attr<double>("scale"))
-        .Attr("alpha", ctx->FwOp().attr<double>("alpha"))
+        .Attr("output_size", ctx->FwOp().attr<std::vector<int>>("output_size"))
         .Output("dx")
         .Build();
   });
