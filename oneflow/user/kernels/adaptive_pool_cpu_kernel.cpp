71f7bd94b7d737195af1c0d005f348040c1daaae
BBuf <1182563586@qq.com>
2021-06-10 17:10:38 +0800

    add adaptive_avg_pool2d backward kernel

--- oneflow/user/kernels/adaptive_pool_cpu_kernel.cpp
+++ oneflow/user/kernels/adaptive_pool_cpu_kernel.cpp
@@ -29,32 +29,12 @@ static inline int64_t end_index(int64_t a, int64_t b, int64_t c) {
 }
 
 template<DeviceType device_type, typename T>
-class ConcatKernel final : public user_op::OpKernel {
+class AdaptivePoolCpuKernel final : public user_op::OpKernel {
  public:
-  ConcatKernel() = default;
-  ~ConcatKernel() = default;
+  AdaptivePoolCpuKernel() = default;
+  ~AdaptivePoolCpuKernel() = default;
 
  private:
-  void InferShape(user_op::KernelInferContext* ctx) const override {
-    std::vector <int> out_size = ctx->Attr<std::vector<int64_t>>("output_size"); 
-    const auto& in_arg_pair =  ctx->inputs()[0];
-    const ShapeView& input_shape_view =
-          ctx->ShapeView4ArgNameAndIndex(in_arg_pair.first, in_arg_pair.second);
-    DimVector dim_vec;
-    input_shape_view.ToDimVector(&dim_vec);
-    int h = dim_vec[2];
-    int w = dim_vec[3];
-    if(out_size.size() >= 1){
-      h = out_size[0]; //h
-      w = out_size[0];
-      if(out_size.size() == 2){
-        h = out_size[1]; //w
-      }
-    }
-    dim_vec[2] = h;
-    dim_vec[3] = w;
-    ctx->MutShapeView4ArgNameAndIndex("out", 0)->set_shape(Shape(dim_vec));
-  }
 
   void Compute(user_op::KernelComputeContext* ctx) const override {
     user_op::Tensor* out_tensor = ctx->Tensor4ArgNameAndIndex("out", 0);
@@ -63,7 +43,7 @@ class ConcatKernel final : public user_op::OpKernel {
     const T* in_ptr = in_tensor->dptr<T>();
     T* out_ptr = out_tensor->mut_dptr<T>();
 
-    const int64_t ndims = x->shape().NumAxes();
+    const int64_t ndims = in_tensor->shape().NumAxes();
     CHECK_EQ(ndims, 4);
 
     const int64_t n_idx = 0;
@@ -80,8 +60,8 @@ class ConcatKernel final : public user_op::OpKernel {
 
     FOR_RANGE(int64_t, b, 0, n_bacth){
       FOR_RANGE(int64_t, c, 0, n_channnel){
-        T* input_ptr = in_ptr + b * n_channnel * input_height * input_width;
-        T* output_ptr = out_ptr + b * n_channnel * output_height * output_width;
+        T* input_ptr = in_ptr + b * n_channnel * input_height * input_width + c * input_height * input_width;
+        T* output_ptr = out_ptr + b * n_channnel * output_height * output_width + c * output_height * output_width;
         FOR_RANGE(int64_t, oh, 0, output_height){
           int64_t ih0 = start_index(oh, output_height, input_height);
           int64_t ih1 = end_index(oh, output_height, input_height);
@@ -107,24 +87,96 @@ class ConcatKernel final : public user_op::OpKernel {
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-}  // namespace
 
-#define REGISTER_CONCAT_KERNEL(device, dtype)                                                \
-  REGISTER_USER_KERNEL("adaptive_avg_pool2d").SetCreateFn<ConcatKernel<device, dtype>>().SetIsMatchedHob( \
+#define REGISTER_ADAPTIVE_POOL_KERNEL(device, dtype)                                                \
+  REGISTER_USER_KERNEL("adaptive_avg_pool2d").SetCreateFn<AdaptivePoolCpuKernel<device, dtype>>().SetIsMatchedHob( \
       (user_op::HobDeviceTag() == device)                                                    \
       & (user_op::HobDataType("out", 0) == GetDataType<dtype>::value));
 
-#define REGISTER_CONCAT_KERNEL_WITH_DEVICE(device) \
-  REGISTER_CONCAT_KERNEL(device, float)            \
-  REGISTER_CONCAT_KERNEL(device, double)           \
-  REGISTER_CONCAT_KERNEL(device, int8_t)           \
-  REGISTER_CONCAT_KERNEL(device, int32_t)          \
-  REGISTER_CONCAT_KERNEL(device, int64_t)
+#define REGISTER_ADAPTIVE_POOL_KERNEL_WITH_DEVICE(device) \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, float)            \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, double)           \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, int8_t)           \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, int32_t)          \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, int64_t)
 
-REGISTER_CONCAT_KERNEL_WITH_DEVICE(DeviceType::kCPU)
-#ifdef WITH_CUDA
-REGISTER_CONCAT_KERNEL_WITH_DEVICE(DeviceType::kGPU)
-REGISTER_CONCAT_KERNEL(DeviceType::kGPU, float16)
-#endif
+REGISTER_ADAPTIVE_POOL_KERNEL_WITH_DEVICE(DeviceType::kCPU)
+
+
+template<DeviceType device_type, typename T>
+class AdaptivePoolCpuGradKernel final : public OpKernel {
+ public:
+  AdaptivePoolCpuGradKernel() = default;
+  ~AdaptivePoolCpuGradKernel() = default;
+
+ private:
+  void Compute(KernelComputeContext* ctx) const override {
+    const Tensor* grad_output = ctx->Tensor4ArgNameAndIndex("dy", 0);
+    Tensor* grad_input = ctx->Tensor4ArgNameAndIndex("dx", 0);
+    const std::vector <int> out_size = ctx->Attr<std::vector<int64_t>>("output_size");
+
+    const T* out_ptr = grad_output->dptr<T>();
+    T* in_ptr = grad_input->mut_dptr<T>();
+
+    const int64_t ndims = grad_output->shape().NumAxes();
+    CHECK_EQ(ndims, 4);
+
+    const int64_t n_idx = 0;
+    const int64_t c_idx = 1;
+    const int64_t h_idx = 2;
+    const int64_t w_idx = 3;
+
+    const int64_t n_batch = grad_output->shape().At(n_idx);
+    const int64_t n_channnel = grad_output->shape().At(c_idx);
+    const int input_height = grad_input->shape().At(h_idx);
+    const int input_width = grad_input->shape().At(w_idx);
+    const int output_height = grad_output->shape().At(h_idx);
+    const int output_width = grad_output->shape().At(w_idx);
+
+    FOR_RANGE(int64_t, b, 0, n_bacth){
+      FOR_RANGE(int64_t, c, 0, n_channnel){
+        T* input_ptr = in_ptr + b * n_channnel * input_height * input_width + c * input_height * input_width;
+        T* output_ptr = out_ptr + b * n_channnel * output_height * output_width + c * output_height * output_width;
+        FOR_RANGE(int64_t, oh, 0, output_height){
+          int64_t ih0 = start_index(oh, output_height, input_height);
+          int64_t ih1 = end_index(oh, output_height, input_height);
+          int64_t kh = ih1 - ih0;
+          FOR_RANGE(int64_t, ow, 0, output_width){
+            int64_t iw0 = start_index(ow, output_width, input_width);
+            int64_t iw1 = end_index(ow, output_width, input_width);
+            int64_t kw = iw1 - iw0;
+            
+            T grad_delta = output_ptr[oh * output_width + ow] / kh / kw;
+            FOR_RANGE(int64_t, ih, ih0, ih1){
+              FOR_RANGE(int64_t, iw, iw0, iw1){
+                input_ptr[ih * input_width + iw] += grad_delta;
+              }
+            }
+            output_ptr[oh * output_width + ow] = sum / kh / kw;
+          }
+        }
+      }
+    }
+
+  }
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_CPU_ELU_BACKWARD_KERNEL(device, dtype) \
+  REGISTER_USER_KERNEL("adaptive_avg_pool2d_grad")                      \
+      .SetCreateFn<AdaptivePoolCpuGradKernel<device, dtype>>()   \
+      .SetIsMatchedHob((HobDeviceTag() == device)       \
+                       & (HobDataType("dx", 0) == GetDataType<dtype>::value));
+
+#define REGISTER_ADAPTIVE_POOL_BACKWARD_KERNEL_WITH_DEVICE(device) \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, float)            \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, double)           \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, int8_t)           \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, int32_t)          \
+  REGISTER_ADAPTIVE_POOL_KERNEL(device, int64_t)
+
+REGISTER_ADAPTIVE_POOL_BACKWARD_KERNEL_WITH_DEVICE(DeviceType::kCPU)
+
+}  // namespace
 
 }  // namespace oneflow
