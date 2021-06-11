af8891574e4c474ff87ac6f831e052e14a05df86
ShiYongtao <847615435@qq.com>
2021-06-11 11:26:07 +0800

    delete deprecated argument

--- oneflow/python/nn/modules/loss.py
+++ oneflow/python/nn/modules/loss.py
@@ -53,10 +53,7 @@ class CrossEntropyLoss(Module):
         reduction (string, optional): Specifies the reduction to apply to the output:
             ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will
             be applied, ``'mean'``: the weighted mean of the output is taken,
-            ``'sum'``: the output will be summed. Note: :attr:`size_average`
-            and :attr:`reduce` are in the process of being deprecated, and in
-            the meantime, specifying either of those two args will override
-            :attr:`reduction`. Default: ``'mean'``
+            ``'sum'``: the output will be summed. Default: ``'mean'``
 
     For example:
 
@@ -194,10 +191,7 @@ class NLLLoss(Module):
         reduction (string, optional): Specifies the reduction to apply to the output:
             ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will
             be applied, ``'mean'``: the weighted mean of the output is taken,
-            ``'sum'``: the output will be summed. Note: :attr:`size_average`
-            and :attr:`reduce` are in the process of being deprecated, and in
-            the meantime, specifying either of those two args will override
-            :attr:`reduction`. Default: ``'mean'``
+            ``'sum'``: the output will be summed. Default: ``'mean'``
 
     For example:
 
@@ -342,15 +336,6 @@ class KLDivLoss(Module):
     .. _`kullback-leibler divergence`: https://en.wikipedia.org/wiki/Kullback-Leibler_divergence
 
     Args:
-        size_average (bool, optional): Deprecated (see :attr:`reduction`). By default,
-            the losses are averaged over each loss element in the batch. Note that for
-            some losses, there are multiple elements per sample. If the field :attr:`size_average`
-            is set to ``False``, the losses are instead summed for each minibatch. Ignored
-            when :attr:`reduce` is ``False``. Default: ``True``
-        reduce (bool, optional): Deprecated (see :attr:`reduction`). By default, the
-            losses are averaged or summed over observations for each minibatch depending
-            on :attr:`size_average`. When :attr:`reduce` is ``False``, returns a loss per
-            batch element instead and ignores :attr:`size_average`. Default: ``True``
         reduction (string, optional): Specifies the reduction to apply to the output:
             ``'none'`` | ``'batchmean'`` | ``'sum'`` | ``'mean'``.
             ``'none'``: no reduction will be applied.
@@ -360,10 +345,6 @@ class KLDivLoss(Module):
             Default: ``'mean'``
         log_target (bool, optional): Specifies whether `target` is passed in the log space.
             Default: ``False``
-
-    .. note::
-        :attr:`size_average` and :attr:`reduce` are in the process of being deprecated,
-        and in the meantime, specifying either of those two args will override :attr:`reduction`.
 
     .. note::
         :attr:`reduction` = ``'mean'`` doesn't return the true kl divergence value, please use
@@ -402,18 +383,8 @@ class KLDivLoss(Module):
 
     """
 
-    def __init__(
-        self,
-        size_average=None,
-        reduce=None,
-        reduction: str = "mean",
-        log_target: bool = False,
-    ) -> None:
+    def __init__(self, reduction: str = "mean", log_target: bool = False,) -> None:
         super().__init__()
-        if size_average is False:
-            raise ValueError("Argument size_average is not supported yet")
-        if reduce is False:
-            raise ValueError("Argument reduce is not supported yet")
         assert reduction in [
             "sum",
             "none",
@@ -479,21 +450,10 @@ class MSELoss(Module):
     The division by :math:`n` can be avoided if one sets ``reduction = 'sum'``.
 
     Args:
-        size_average (bool, optional): Deprecated (see :attr:`reduction`). By default,
-            the losses are averaged over each loss element in the batch. Note that for
-            some losses, there are multiple elements per sample. If the field :attr:`size_average`
-            is set to ``False``, the losses are instead summed for each minibatch. Ignored
-            when :attr:`reduce` is ``False``. Default: ``True``
-        reduce (bool, optional): Deprecated (see :attr:`reduction`). By default, the
-            losses are averaged or summed over observations for each minibatch depending
-            on :attr:`size_average`. When :attr:`reduce` is ``False``, returns a loss per
-            batch element instead and ignores :attr:`size_average`. Default: ``True``
         reduction (string, optional): Specifies the reduction to apply to the output:
             ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
             ``'mean'``: the sum of the output will be divided by the number of
-            elements in the output, ``'sum'``: the output will be summed. Note: :attr:`size_average`
-            and :attr:`reduce` are in the process of being deprecated, and in the meantime,
-            specifying either of those two args will override :attr:`reduction`. Default: ``'mean'``
+            elements in the output, ``'sum'``: the output will be summed. Default: ``'mean'``
 
     Shape:
         - Input: :math:`(N, *)` where :math:`*` means, any number of additional
@@ -530,14 +490,8 @@ class MSELoss(Module):
 
     """
 
-    def __init__(
-        self, reduction: str = "mean", size_average: bool = True, reduce: bool = True
-    ) -> None:
+    def __init__(self, reduction: str = "mean") -> None:
         super().__init__()
-        if size_average is False:
-            raise ValueError("Argument size_average is not supported yet")
-        if reduce is False:
-            raise ValueError("Argument reduce is not supported yet")
         assert reduction in [
             "sum",
             "none",
