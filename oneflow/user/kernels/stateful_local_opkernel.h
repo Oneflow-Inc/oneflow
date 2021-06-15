3495ecd8a70a640fbbb2b691710ccc36472fb381
liufengwei <2472937968@qq.com>
2021-06-15 15:53:15 +0800

    delete const_cast in Output

--- oneflow/user/kernels/stateful_local_opkernel.h
+++ oneflow/user/kernels/stateful_local_opkernel.h
@@ -166,13 +166,13 @@ class LocalUserOpInferContext : public user_op::InferContext {
     return *const_cast<LocalUserOpInferContext*>(this)->Shape4ArgNameAndIndex(arg_name, index);
   }
   Shape* OutputShape(const std::string& arg_name, int32_t index) override {
-    return const_cast<LocalUserOpInferContext*>(this)->Shape4ArgNameAndIndex(arg_name, index);
+    return Shape4ArgNameAndIndex(arg_name, index);
   }
   Shape* Shape4ArgNameAndIndex(const std::string& arg_name, int32_t index) override {
     return NonNullTensorDesc4ArgNameAndIndex(arg_name, index)->mut_shape();
   }
   DataType* OutputDType(const std::string& arg_name, int32_t index) override {
-    return const_cast<LocalUserOpInferContext*>(this)->Dtype4ArgNameAndIndex(arg_name, index);
+    return Dtype4ArgNameAndIndex(arg_name, index);
   }
   DataType* Dtype4ArgNameAndIndex(const std::string& arg_name, int32_t index) override {
     return NonNullTensorDesc4ArgNameAndIndex(arg_name, index)->mut_data_type();
