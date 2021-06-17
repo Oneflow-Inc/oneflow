13cedfdaed2d3f9141a47305e88c919dcd1dc35f
ZhongHW <35329085+puchapu@users.noreply.github.com>
2021-06-18 01:05:32 +0800

    Dev norm op (#5178)
    * init-norm
    
    * checkpoint
    
    * update order 0
    
    * first_push
    
    * refine-norm
    
    * delete-tripletloss-part
    
    * fix-conlict
    
    * add-tensor-norm
    
    * fix-docstring
    
    * fix-docstring
    
    * auto format by CI
    
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/python/ops/nn_ops.py
+++ oneflow/python/ops/nn_ops.py
@@ -4136,6 +4136,7 @@ def margin_ranking_loss(
 
 
 @oneflow_export("nn.TripletMarginLoss")
+@stable_api
 def triplet_margin_loss(
     anchor: oneflow._oneflow_internal.BlobDesc,
     positive: oneflow._oneflow_internal.BlobDesc,
