24eeffa4e3ec44e48eefa41490c64b9b08a86967
guo-ran <360112263@qq.com>
2021-06-17 19:37:44 +0800

    add nccl_logical_op

--- oneflow/core/job_rewriter/insert_nccl_logical_op_pass.cpp
+++ oneflow/core/job_rewriter/insert_nccl_logical_op_pass.cpp
@@ -338,6 +338,90 @@ bool TryBuildNcclBy2DHierarchySameDim1(OperatorConf* ret,
   return false;
 }
 
+bool TryBuildNcclBy3DHierarchyChangeDim0(OperatorConf* ret,
+                                         const ParallelDistribution& src_parallel_distribution,
+                                         const ParallelDistribution& dst_parallel_distribution,
+                                         const std::shared_ptr<Shape> hierarchy,
+                                         const std::string& lbn, const int64_t scope_symbol_id,
+                                         const BlobDesc& logical_blob_desc) {
+  CHECK_EQ(src_parallel_distribution.sbp_parallel_size(), 3);
+  CHECK_EQ(dst_parallel_distribution.sbp_parallel_size(), 3);
+  CHECK(src_parallel_distribution.sbp_parallel(1) == dst_parallel_distribution.sbp_parallel(1));
+  CHECK(src_parallel_distribution.sbp_parallel(2) == dst_parallel_distribution.sbp_parallel(2));
+  const SbpParallel& src_dim0_sbp = src_parallel_distribution.sbp_parallel(0);
+  const SbpParallel& dst_dim0_sbp = dst_parallel_distribution.sbp_parallel(0);
+
+  if (src_dim0_sbp.has_partial_sum_parallel() && dst_dim0_sbp.has_broadcast_parallel()) {
+    // (*, P)->(*, B) : AllReduce
+    *ret =
+        user_op::UserOpConfWrapperBuilder(kNcclLogicalOpNamePrefix + "-(*P)2(*B)-" + NewUniqueId())
+            .Op("_nccl_logical_3D_change_dim0_all_reduce")
+            .Input("in", lbn)
+            .Output("out")
+            .ScopeSymbolId(scope_symbol_id)
+            .Build()
+            .op_conf();
+    return true;
+  }
+  return false;
+}
+
+bool TryBuildNcclBy3DHierarchyChangeDim1(OperatorConf* ret,
+                                         const ParallelDistribution& src_parallel_distribution,
+                                         const ParallelDistribution& dst_parallel_distribution,
+                                         const std::shared_ptr<Shape> hierarchy,
+                                         const std::string& lbn, const int64_t scope_symbol_id,
+                                         const BlobDesc& logical_blob_desc) {
+  CHECK_EQ(src_parallel_distribution.sbp_parallel_size(), 3);
+  CHECK_EQ(dst_parallel_distribution.sbp_parallel_size(), 3);
+  CHECK(src_parallel_distribution.sbp_parallel(0) == dst_parallel_distribution.sbp_parallel(0));
+  CHECK(src_parallel_distribution.sbp_parallel(2) == dst_parallel_distribution.sbp_parallel(2));
+  const SbpParallel& src_dim1_sbp = src_parallel_distribution.sbp_parallel(1);
+  const SbpParallel& dst_dim1_sbp = dst_parallel_distribution.sbp_parallel(1);
+
+  if (src_dim1_sbp.has_partial_sum_parallel() && dst_dim1_sbp.has_broadcast_parallel()) {
+    // (*, P)->(*, B) : AllReduce
+    *ret =
+        user_op::UserOpConfWrapperBuilder(kNcclLogicalOpNamePrefix + "-(*P)2(*B)-" + NewUniqueId())
+            .Op("_nccl_logical_3D_change_dim1_all_reduce")
+            .Input("in", lbn)
+            .Output("out")
+            .ScopeSymbolId(scope_symbol_id)
+            .Build()
+            .op_conf();
+    return true;
+  }
+  return false;
+}
+
+bool TryBuildNcclBy3DHierarchyChangeDim2(OperatorConf* ret,
+                                         const ParallelDistribution& src_parallel_distribution,
+                                         const ParallelDistribution& dst_parallel_distribution,
+                                         const std::shared_ptr<Shape> hierarchy,
+                                         const std::string& lbn, const int64_t scope_symbol_id,
+                                         const BlobDesc& logical_blob_desc) {
+  CHECK_EQ(src_parallel_distribution.sbp_parallel_size(), 3);
+  CHECK_EQ(dst_parallel_distribution.sbp_parallel_size(), 3);
+  CHECK(src_parallel_distribution.sbp_parallel(0) == dst_parallel_distribution.sbp_parallel(0));
+  CHECK(src_parallel_distribution.sbp_parallel(1) == dst_parallel_distribution.sbp_parallel(1));
+  const SbpParallel& src_dim2_sbp = src_parallel_distribution.sbp_parallel(2);
+  const SbpParallel& dst_dim2_sbp = dst_parallel_distribution.sbp_parallel(2);
+
+  if (src_dim2_sbp.has_partial_sum_parallel() && dst_dim2_sbp.has_broadcast_parallel()) {
+    // (*, P)->(*, B) : AllReduce
+    *ret =
+        user_op::UserOpConfWrapperBuilder(kNcclLogicalOpNamePrefix + "-(*P)2(*B)-" + NewUniqueId())
+            .Op("_nccl_logical_3D_change_dim2_all_reduce")
+            .Input("in", lbn)
+            .Output("out")
+            .ScopeSymbolId(scope_symbol_id)
+            .Build()
+            .op_conf();
+    return true;
+  }
+  return false;
+}
+
 bool TryBuildNcclLogicalOpConf(OperatorConf* ret, const OpNode* src_node, const OpNode* dst_node,
                                const LogicalBlobId& lbi) {
   if (!src_node->op().op_conf().has_scope_symbol_id()) { return false; /* device_tick */ }
@@ -388,6 +472,27 @@ bool TryBuildNcclLogicalOpConf(OperatorConf* ret, const OpNode* src_node, const 
                                                  dst_parallel_distribution, src_hierarchy, lbn,
                                                  scope_symbol_id, logical_blob_desc);
       }
+    }
+  } else if (src_hierarchy->NumAxes() == 3 && (*src_hierarchy == *dst_hierarchy)) {
+    if (src_parallel_distribution.sbp_parallel(0) == dst_parallel_distribution.sbp_parallel(0)
+        && src_parallel_distribution.sbp_parallel(1) == dst_parallel_distribution.sbp_parallel(1)) {
+      return TryBuildNcclBy3DHierarchyChangeDim2(ret, src_parallel_distribution,
+                                                 dst_parallel_distribution, src_hierarchy, lbn,
+                                                 scope_symbol_id, logical_blob_desc);
+    } else if (src_parallel_distribution.sbp_parallel(0)
+                   == dst_parallel_distribution.sbp_parallel(0)
+               && src_parallel_distribution.sbp_parallel(2)
+                      == dst_parallel_distribution.sbp_parallel(2)) {
+      return TryBuildNcclBy3DHierarchyChangeDim1(ret, src_parallel_distribution,
+                                                 dst_parallel_distribution, src_hierarchy, lbn,
+                                                 scope_symbol_id, logical_blob_desc);
+    } else if (src_parallel_distribution.sbp_parallel(1)
+                   == dst_parallel_distribution.sbp_parallel(1)
+               && src_parallel_distribution.sbp_parallel(2)
+                      == dst_parallel_distribution.sbp_parallel(2)) {
+      return TryBuildNcclBy3DHierarchyChangeDim0(ret, src_parallel_distribution,
+                                                 dst_parallel_distribution, src_hierarchy, lbn,
+                                                 scope_symbol_id, logical_blob_desc);
     }
   }
   return false;
