71c39c02894ded08e933039c62963154182284fe
simonJJJ <821898965@qq.com>
2021-06-22 22:41:42 +0800

    refine testcase

--- oneflow/python/test/modules/test_argsort.py
+++ oneflow/python/test/modules/test_argsort.py
@@ -19,47 +19,31 @@ from collections import OrderedDict
 import numpy as np
 
 import oneflow.experimental as flow
-from test_util import GenArgList
+from test_util import GenArgList, type_name_to_flow_type
 
 
-def _test_argsort_dim_negative(test_case, device):
+def _test_argsort(test_case, data_shape, axis, descending, data_type, device):
     input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device),
+        np.random.randn(*data_shape),
+        dtype=type_name_to_flow_type[data_type],
+        device=flow.device(device),
     )
-    axis = -1
-    of_out = flow.argsort(input, dim=axis)
-    np_out = np.argsort(input.numpy(), axis=axis)
+    of_out = flow.argsort(input, dim=axis, descending=descending)
+    np_input = -input.numpy() if descending else input.numpy()
+    np_out = np.argsort(np_input, axis=axis)
     test_case.assertTrue(np.array_equal(of_out.numpy().flatten(), np_out.flatten()))
 
 
-def _test_tensor_argsort(test_case, device):
+def _test_tensor_argsort(test_case, data_shape, axis, descending, data_type, device):
     input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device),
+        np.random.randn(*data_shape),
+        dtype=type_name_to_flow_type[data_type],
+        device=flow.device(device),
     )
-    axis = 0
-    of_out = input.argsort(dim=axis)
-    np_out = np.argsort(input.numpy(), axis=axis)
+    of_out = input.argsort(dim=axis, descending=descending)
+    np_input = -input.numpy() if descending else input.numpy()
+    np_out = np.argsort(np_input, axis=axis)
     test_case.assertTrue(np.array_equal(of_out.numpy().shape, np_out.shape))
-    test_case.assertTrue(np.array_equal(of_out.numpy().flatten(), np_out.flatten()))
-
-
-def _test_argsort_dim_positive(test_case, device):
-    input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device),
-    )
-    axis = 1
-    of_out = flow.argsort(input, dim=axis)
-    np_out = np.argsort(input.numpy(), axis=axis)
-    test_case.assertTrue(np.array_equal(of_out.numpy().flatten(), np_out.flatten()))
-
-
-def _test_argsort_dim_descending(test_case, device):
-    input = flow.Tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device),
-    )
-    axis = 0
-    of_out = flow.argsort(input, dim=axis, descending=True)
-    np_out = np.argsort(-input.numpy(), axis=axis)
     test_case.assertTrue(np.array_equal(of_out.numpy().flatten(), np_out.flatten()))
 
 
@@ -70,12 +54,11 @@ def _test_argsort_dim_descending(test_case, device):
 class TestArgsort(flow.unittest.TestCase):
     def test_argsort(test_case):
         arg_dict = OrderedDict()
-        arg_dict["test_fun"] = [
-            _test_argsort_dim_negative,
-            _test_tensor_argsort,
-            _test_argsort_dim_positive,
-            _test_argsort_dim_descending,
-        ]
+        arg_dict["test_fun"] = [_test_argsort, _test_tensor_argsort]
+        arg_dict["data_shape"] = [(2, 6, 5, 4), (3, 4, 8)]
+        arg_dict["axis"] = [-1, 0, 2]
+        arg_dict["descending"] = [True, False]
+        arg_dict["data_type"] = ["double", "float32", "int32"]
         arg_dict["device"] = ["cpu", "cuda"]
         for arg in GenArgList(arg_dict):
             arg[0](test_case, *arg[1:])
