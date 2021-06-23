Fix functional normalization.

oneflow/python/nn/modules/normalization.py
@@ -308,7 +308,7 @@ class LayerNorm(Module):
             return affined
         else:
             if self.elementwise_affine:
-                flow.F.layer_norm_affine(
+                res = flow.F.layer_norm_affine(
                     x,
                     self.weight,
                     self.bias,
@@ -317,7 +317,7 @@ class LayerNorm(Module):
                     epsilon=self.epsilon,
                 )
             else:
-                flow.F.layer_norm(
+                res = flow.F.layer_norm(
                     x,
                     begin_norm_axis=self.begin_norm_axis,
                     begin_params_axis=self.begin_params_axis,