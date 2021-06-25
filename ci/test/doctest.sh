ab2b48c5125e75cc119e3993504ba3cb84895a62
tsai <jackalcooper@gmail.com>
2021-06-25 13:40:45 +0800

    refine

--- ci/test/doctest.sh
+++ ci/test/doctest.sh
@@ -3,7 +3,11 @@ set -xe
 export PYTHONUNBUFFERED=1
 src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
 
-cd `python3 -c "import oneflow; print(oneflow.__path__[0])"`
+unset val_stdout val_stderr
+eval "$(python3 -c 'import oneflow; print(oneflow.__path__[0])' \
+        2> >(readarray -t val_stderr; typeset -p val_stderr) \
+         > >(readarray -t val_stdout; typeset -p val_stdout) )"
+cd ${val_stdout}
 cd ..
 
 gpu_num=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
