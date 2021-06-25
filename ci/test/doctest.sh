d0c1e5695ef4fdd4610a443470af3811ea4180d1
tsai <jackalcooper@gmail.com>
2021-06-25 15:13:34 +0800

    refine

--- ci/test/doctest.sh
+++ ci/test/doctest.sh
@@ -2,18 +2,16 @@
 set -xe
 export PYTHONUNBUFFERED=1
 src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
+test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"./test_tmp_dir"}
 
-unset val_stdout val_stderr
-eval "$(python3 -c 'import oneflow; print(oneflow.__path__[0])' \
-        2> >(readarray -t val_stderr; typeset -p val_stderr) \
-         > >(readarray -t val_stdout; typeset -p val_stdout) )"
-cd ${val_stdout}
-cd ..
+mkdir -p ${test_tmp_dir}
+cd ${test_tmp_dir}
+python3 -c 'import oneflow; f=open("oneflow_path.txt", "w"); f.write(oneflow.__path__[0])'
 
 gpu_num=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 python3 $src_dir/ci/test/parallel_run.py \
     --gpu_num=${gpu_num} \
-    --dir=${PWD} \
+    --dir=$(cat oneflow_path.txt) \
     --timeout=1 \
     --verbose \
     --chunk=1 \
