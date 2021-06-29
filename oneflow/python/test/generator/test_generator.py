ffa0a754a3164a59583f26abcb0b4cbc913b4d27
VertexC <bob2420083992@gmail.com>
2021-06-29 13:45:46 +0800

    add random generator

---
+++ oneflow/python/test/generator/test_generator.py
@@ -0,0 +1,29 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+import oneflow as flow
+
+
+class TestGenerator(flow.unittest.TestCase):
+    def test_generator_manual_seed(test_case):
+        generator = flow.Generator()
+        generator.manual_seed(1)
+        test_case.assertTrue(generator.initial_seed() == 1)
+        generator.manual_seed(2)
+        test_case.assertTrue(generator.initial_seed() == 2)
+
+if __name__ == "__main__":
+    unittest.main()
