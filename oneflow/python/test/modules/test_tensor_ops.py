5278022a67066547c6fcee1708fb8cfc1eaa60ea
Flowingsun007 <flowingsun007@163.com>
2021-06-29 23:01:57 +0800

    add more test

--- oneflow/python/test/modules/test_tensor_ops.py
+++ oneflow/python/test/modules/test_tensor_ops.py
@@ -22,17 +22,17 @@ import oneflow.experimental as flow
 from test_util import GenArgList
 
 
-def _test_type_as(test_case, shape, device):
+def _test_type_as(test_case, shape, device, src_dtype, tgt_dtype):
     np_input = np.random.rand(*shape)
-    input = flow.tensor(np_input, dtype=flow.float, device=device)
-    target = flow.tensor(np_input, dtype=flow.int32, device=device)
+    input = flow.tensor(np_input, dtype=src_dtype, device=device)
+    target = flow.tensor(np_input, dtype=tgt_dtype, device=device)
     input = input.type_as(target)
     test_case.assertEqual(input.dtype, target.dtype)
 
 
-def _test_long(test_case, shape, device):
+def _test_long(test_case, shape, device, dtype):
     np_input = np.random.rand(*shape)
-    input = flow.tensor(np_input, dtype=flow.float, device=device)
+    input = flow.tensor(np_input, dtype=dtype, device=device)
     input = input.long()
     test_case.assertEqual(input.dtype, flow.int64)
 
@@ -46,6 +46,8 @@ class TestTensorOps(flow.unittest.TestCase):
         arg_dict = OrderedDict()
         arg_dict["shape"] = [(1, 2), (3, 4, 5), (2, 3, 4, 5)]
         arg_dict["device"] = ["cpu", "cuda"]
+        arg_dict["src_dtype"] = [flow.int64, flow.int32, flow.float32, flow.float64]
+        arg_dict["tgt_dtype"] = [flow.int64, flow.int32, flow.float32, flow.float64]
         for arg in GenArgList(arg_dict):
             _test_type_as(test_case, *arg)
 
@@ -53,6 +55,7 @@ class TestTensorOps(flow.unittest.TestCase):
         arg_dict = OrderedDict()
         arg_dict["shape"] = [(1, 2), (3, 4, 5), (2, 3, 4, 5)]
         arg_dict["device"] = ["cpu", "cuda"]
+        arg_dict["dtype"] = [flow.int64, flow.int32, flow.float32, flow.float64]
         for arg in GenArgList(arg_dict):
             _test_long(test_case, *arg)
 
