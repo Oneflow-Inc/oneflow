4e0a912a57dd196e5ffc8673903608e41bd9acd9
tsai <jackalcooper@gmail.com>
2021-07-01 17:28:31 +0800

    refine

--- ci/test/try_install.sh
+++ ci/test/try_install.sh
@@ -10,7 +10,7 @@ if [ -n "$index" ]; then
     python3 -m pip install --find-links ${index} ${pkg_name}
 elif [ -d "$wheel_path" ]; then
     ls -la $wheel_path
-    python3 -m pip install --user --extra-index-url $wheel_path ${pkg_name}
+    python3 -m pip install -U --user --extra-index-url $wheel_path ${pkg_name}
 elif [ -e "$wheel_path" ]; then
     python3 -m pip install --user "$wheel_path"
 elif [ -d "$src_dir" ]; then
