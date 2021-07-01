73c469e42b3fdbb8a4cd8b9c69989cfe8d30ffb1
duchengyao <duchengyao@zhejianglab.edu.cn>
2021-07-01 15:03:29 +0800

    clean code

--- oneflow/python/test/modules/test_view.py
+++ oneflow/python/test/modules/test_view.py
@@ -22,42 +22,43 @@ import oneflow.experimental as flow
 from test_util import GenArgList
 
 
-def _test_reshape(test_case, device):
+def _test_view(test_case, device):
     x = np.array(
         [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
     ).astype(np.float32)
     input = flow.Tensor(x, device=flow.device(device))
-    of_shape = flow.reshape(input, shape=[2, 2, 2, -1]).numpy().shape
+    of_shape = flow.view(input, shape=[2, 2, 2, -1]).numpy().shape
+    of_shape = flow.view(input, shape=[2, 2, 2, -1]).numpy().shape
     np_shape = (2, 2, 2, 2)
     test_case.assertTrue(np.array_equal(of_shape, np_shape))
 
 
-def _test_reshape_tuple(test_case, device):
+def _test_view_tuple(test_case, device):
     x = np.array(
         [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
     ).astype(np.float32)
     input = flow.Tensor(x, device=flow.device(device))
-    of_shape = flow.reshape(input, shape=(2, 2, 2, -1)).numpy().shape
+    of_shape = flow.view(input, shape=(2, 2, 2, -1)).numpy().shape
     np_shape = (2, 2, 2, 2)
     test_case.assertTrue(np.array_equal(of_shape, np_shape))
 
 
-def _test_tensor_reshape(test_case, device):
+def _test_tensor_view(test_case, device):
     x = np.array(
         [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
     ).astype(np.float32)
     input = flow.Tensor(x, device=flow.device(device))
-    of_shape = input.reshape(shape=[2, 2, 2, -1]).numpy().shape
+    of_shape = input.view(shape=[2, 2, 2, -1]).numpy().shape
     np_shape = (2, 2, 2, 2)
     test_case.assertTrue(np.array_equal(of_shape, np_shape))
 
 
-def _test_reshape_backward(test_case, device):
+def _test_view_backward(test_case, device):
     x = np.array(
         [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
     ).astype(np.float32)
     input = flow.Tensor(x, device=flow.device(device), requires_grad=True)
-    of_out = flow.reshape(input, shape=[2, 2, 2, -1]).sum()
+    of_out = flow.view(input, shape=[2, 2, 2, -1]).sum()
     of_out.backward()
     np_grad = np.array(
         [
@@ -78,10 +79,10 @@ class TestModule(flow.unittest.TestCase):
     def test_reshape(test_case):
         arg_dict = OrderedDict()
         arg_dict["test_fun"] = [
-            _test_reshape,
-            _test_reshape_tuple,
-            _test_tensor_reshape,
-            _test_reshape_backward,
+            _test_view,
+            _test_view_tuple,
+            _test_tensor_view,
+            _test_view_backward,
         ]
         arg_dict["device"] = ["cpu", "cuda"]
         for arg in GenArgList(arg_dict):
