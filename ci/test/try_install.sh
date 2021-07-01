21d30bff2d29265a6d9cdf551d5ed9ebb139704f
tsai <jackalcooper@gmail.com>
2021-07-01 17:26:57 +0800

    fix

--- ci/test/try_install.sh
+++ ci/test/try_install.sh
@@ -4,13 +4,13 @@ set -xe
 src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
 wheel_path=${ONEFLOW_WHEEL_PATH:-"$PWD/wheelhouse"}
 index=${ONEFLOW_PIP_INDEX}
-pkg_name=${ONEFLOW_PACKAGE_NAME}
+pkg_name=${ONEFLOW_PACKAGE_NAME:-"oneflow"}
 
 if [ -n "$index" ]; then
     python3 -m pip install --find-links ${index} ${pkg_name}
 elif [ -d "$wheel_path" ]; then
     ls -la $wheel_path
-    python3 -m pip install --user $wheel_path/*.whl
+    python3 -m pip install --user --extra-index-url $wheel_path ${pkg_name}
 elif [ -e "$wheel_path" ]; then
     python3 -m pip install --user "$wheel_path"
 elif [ -d "$src_dir" ]; then
