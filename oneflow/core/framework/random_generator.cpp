224e85652743133009e01f711b285ace59561ea9
Houjiang Chen <chenhoujiangcug@gmail.com>
2021-07-02 20:39:55 +0800

    Refine code style (#5375)

--- oneflow/core/framework/random_generator.cpp
+++ oneflow/core/framework/random_generator.cpp
@@ -18,21 +18,6 @@ limitations under the License.
 namespace oneflow {
 namespace one {
 
-/**
- * [Copy from pytorch]
- * Gets a non deterministic random number number from either the
- * /dev/urandom or the current time. For CUDA, gets random from
- * std::random_device and adds a transformation on it.
- *
- * FIXME: The behavior in this function is from legacy code
- * (THRandom_seed/THCRandom_seed) and is probably not the right thing to do,
- * even though our tests pass. Figure out if tests get perturbed
- * - when the same algorithm is used for all backends. Note that the current
- * behavior is different for CPU, CUDA and Windows CPU.
- * - when using C++11 std objects, such as std::random_device
- * - when constructing a 64 bit seed properly, rather than static casting
- *   a 32 bit number to 64 bit.
- */
 uint64_t getNonDeterministicRandom() {
   std::random_device rd;
   // limit to 53 bits to ensure unique representation in double
@@ -40,29 +25,58 @@ uint64_t getNonDeterministicRandom() {
   return s;
 }
 
-void manual_seed(uint64_t seed) {
+/*static*/ Maybe<Generator> Generator::New(const std::string& device, uint64_t seed) {
+  std::shared_ptr<Generator> generator(new Generator);
+  JUST(generator->Init(device, seed));
+  return generator;
+}
+
+Maybe<void> Generator::Init(const std::string& device, uint64_t seed) {
+  if (device == "cpu") {
+    gen_impl_ = std::make_shared<DeviceGeneratorImpl<DeviceType::kCPU>>(seed);
+  }
+#ifdef WITH_CUDA
+  else if (device == "cuda") {
+    gen_impl_ = std::make_shared<DeviceGeneratorImpl<DeviceType::kGPU>>(seed);
+  }
+#endif  // WITH_CUDA
+  else if (device == "auto") {
+    gen_impl_ = std::make_shared<AutoGeneratorImpl>(seed);
+  } else {
+    UNIMPLEMENTED_THEN_RETURN() << " device unimplemented, device name: " << device;
+  }
+  return Maybe<void>::Ok();
+}
+
+uint64_t Generator::seed() {
+  uint64_t seed = getNonDeterministicRandom();
+  set_current_seed(seed);
+  return seed;
+}
+
+void ManualSeed(uint64_t seed) {
 #ifdef WITH_CUDA
   const auto& cuda_gen = GetDefaultDeviceGenerator<DeviceType::kGPU>();
-  cuda_gen->set_seed(seed);
+  cuda_gen->set_current_seed(seed);
 #endif  // WITH_CUDA
   const auto& cpu_gen = GetDefaultDeviceGenerator<DeviceType::kCPU>();
-  cpu_gen->set_seed(seed);
+  cpu_gen->set_current_seed(seed);
   const auto& auto_gen = GetDefaultAutoGenerator();
-  auto_gen->set_seed(seed);
+  auto_gen->set_current_seed(seed);
 }
 
 std::shared_ptr<AutoGeneratorImpl> CreateAutoGenerator(uint64_t seed) {
   return std::make_shared<AutoGeneratorImpl>(seed);
 }
 
-const std::shared_ptr<AutoGeneratorImpl>& GetDefaultAutoGenerator() {
-  static auto generator = CreateAutoGenerator(getNonDeterministicRandom());
-  return generator;
-}
-
 template<DeviceType device_type>
 std::shared_ptr<DeviceGeneratorImpl<device_type>> CreateDeviceGenerator(uint64_t seed) {
   return std::make_shared<DeviceGeneratorImpl<device_type>>(seed);
+}
+
+const std::shared_ptr<AutoGeneratorImpl>& GetDefaultAutoGenerator() {
+  static auto generator = CreateAutoGenerator(getNonDeterministicRandom());
+  return generator;
 }
 
 template<DeviceType device_type>
@@ -72,14 +86,12 @@ const std::shared_ptr<DeviceGeneratorImpl<device_type>>& GetDefaultDeviceGenerat
 }
 
 template<DeviceType device_type>
-const Maybe<DeviceGeneratorImpl<device_type>> TryGetDeviceGenerator(
+Maybe<DeviceGeneratorImpl<device_type>> TryGetDeviceGenerator(
     const std::shared_ptr<GeneratorImpl>& generator) {
-  if (generator->device_type() == "auto") {
-    const auto auto_gen = std::dynamic_pointer_cast<AutoGeneratorImpl>(generator);
-    CHECK_NOTNULL_OR_RETURN(auto_gen);
+  if (auto auto_gen = std::dynamic_pointer_cast<AutoGeneratorImpl>(generator)) {
     return auto_gen->template GetDeviceGenerator<device_type>();
   }
-  const auto device_gen = std::dynamic_pointer_cast<DeviceGeneratorImpl<device_type>>(generator);
+  auto device_gen = std::dynamic_pointer_cast<DeviceGeneratorImpl<device_type>>(generator);
   CHECK_NOTNULL_OR_RETURN(device_gen);
   return device_gen;
 }
