be690e83ac01f8c2ba76474381fb473581e9d393
tea321000 <834914152@qq.com>
2021-07-06 13:03:04 +0800

    refine zero_grad() docstring

--- oneflow/python/nn/optimizer/optimizer.py
+++ oneflow/python/nn/optimizer/optimizer.py
@@ -81,18 +81,27 @@ class Optimizer(object):
         raise NotImplementedError()
 
     def zero_grad(self, set_to_none: bool = False):
-        r"""
-            Sets the gradients of all optimized torch.Tensor s to zero.
+        r"""Sets the gradients of all optimized torch.Tensor s to zero.
 
-            Args:
-                set_to_none (bool): instead of setting to zero, set the grads to None. This will in general have lower memory footprint, and can modestly improve performance. However, it changes certain behaviors.
-            For example:
-                1. When the user tries to access a gradient and perform manual ops on it, a None attribute or a Tensor full of 0s will behave differently.
-                2. If the user requests zero_grad(set_to_none=True) followed by a backward pass, .grads are guaranteed to be None for params that did not receive a gradient.
-                3. Optimizers have a different behavior if the gradient is 0 or None (in one case it does the step with a gradient of 0 and in the other it skips the step altogether).
+        Args:
+            set_to_none (bool): instead of setting to zero, set the grads to None.
+                This will in general have lower memory footprint, and can modestly
+                improve performance. However, it changes certain behaviors.
+        For example:
+            1. When the user tries to access a gradient and perform manual ops on
+            it, a None attribute or a Tensor full of 0s will behave differently.
 
-            Returns:
-                None
+            2. If the user requests zero_grad(set_to_none=True) followed by a
+            backward pass, grads are guaranteed to be None for params that did not
+            receive a gradient.
+
+            3. Optimizers have a different behavior if the gradient is 0 or None
+            (in one case it does the step with a gradient of 0 and in the other
+            it skips the step altogether).
+
+        Returns:
+            None
+
         """
         all_grad_is_none = True
         for param_group in self.param_groups:
