d88991e2d2383c8e6e4acddabec01ebe8b59bd96
BBuf <1182563586@qq.com>
2021-07-06 17:22:49 +0800

    fix comment

--- oneflow/python/test/modules/automated_test_util.py
+++ oneflow/python/test/modules/automated_test_util.py
@@ -13,8 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-from email.policy import default
 import inspect
 import typing  # This unused import is needed
 from typing import Dict, Optional, Tuple, Any, Union
