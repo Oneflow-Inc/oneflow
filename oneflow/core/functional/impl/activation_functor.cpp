5dd344ca2388c22da0594c7394c835cb24760d8a
wyg1997 <wyg19970408@gmail.com>
2021-07-07 18:29:18 +0800

    fix merge master bug

--- oneflow/core/functional/impl/activation_functor.cpp
+++ oneflow/core/functional/impl/activation_functor.cpp
@@ -42,7 +42,7 @@ class ReluFunctor {
     if (inplace) {
       std::shared_ptr<TensorTuple> outputs = std::make_shared<TensorTuple>(1);
       outputs->at(0) = x;
-      JUST(JUST(OpInterpUtil::GetInterpreter())->Apply(*op_, {x}, outputs.get(), {}));
+      JUST(JUST(OpInterpUtil::GetInterpreter())->Apply(*op_, {x}, outputs.get(), AttrMap{}));
       return outputs->at(0);
     } else {
       return OpInterpUtil::Dispatch<Tensor>(*op_, {x});
