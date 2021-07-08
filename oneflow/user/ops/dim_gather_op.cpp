e55594818dfec8410930ff6cc08fbbf1f105fd71
MARD1NO <359521840@qq.com>
2021-07-08 20:13:10 +0800

    align with master

--- oneflow/user/ops/dim_gather_op.cpp
+++ oneflow/user/ops/dim_gather_op.cpp
@@ -19,8 +19,13 @@ limitations under the License.
 namespace oneflow {
 namespace user_op {
 
-namespace {
-Maybe<void> InferTensorDesc(user_op::InferContext* ctx) {
+
+REGISTER_USER_OP("dim_gather")
+.Input("input")
+.Input("index")
+.Output("output")
+.Attr<int32_t>("dim")
+.SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
   const TensorDesc* in = ctx->TensorDesc4ArgNameAndIndex("input", 0);
   int64_t input_num_axes = in->shape().NumAxes();
   CHECK_GT_OR_RETURN(input_num_axes, 0);
@@ -28,33 +33,41 @@ Maybe<void> InferTensorDesc(user_op::InferContext* ctx) {
 
   const TensorDesc* index = ctx->TensorDesc4ArgNameAndIndex("index", 0);
   int64_t index_num_axes = index->shape().NumAxes();
-  CHECK_OR_RETURN(IsIndexDataType(index->data_type()));
 
   const int32_t dim = ctx->Attr<int32_t>("dim");
   CHECK_GE_OR_RETURN(dim, 0);
   CHECK_LT_OR_RETURN(dim, input_num_axes);
   CHECK_EQ_OR_RETURN(input_num_axes, index_num_axes);
 
-  CHECK_OR_RETURN(!in->is_dynamic());
-  CHECK_OR_RETURN(!index->is_dynamic());
+  CHECK_EQ_OR_RETURN(in->is_dynamic(), index->is_dynamic());
 
-  user_op::TensorDesc* out = ctx->TensorDesc4ArgNameAndIndex("output", 0);
+  FOR_RANGE(int64_t, i, 0, input_num_axes) {
+    if (i == dim) { continue; }
+    CHECK_EQ_OR_RETURN(in->shape().At(i), index->shape().At(i));
+  }
+
+  user_op::TensorDesc* out = ctx->OutputTensorDesc("output", 0);
   *out->mut_shape() = index->shape();
-  *out->mut_data_type() = in->data_type();
 
   return Maybe<void>::Ok();
-}
-
-void GatherInputArgModifierFn(user_op::GetInputArgModifier GetInputArgModifierFn,
-                              const user_op::UserOpConfWrapper&) {
+})
+.SetDataTypeInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
+  const TensorDesc* index = ctx->TensorDesc4ArgNameAndIndex("index", 0);
+  CHECK_OR_RETURN(IsIndexDataType(index->data_type()));
+  const TensorDesc* in = ctx->TensorDesc4ArgNameAndIndex("input", 0);
+  user_op::TensorDesc* out = ctx->OutputTensorDesc("output", 0);
+  *out->mut_data_type() = in->data_type();
+  return Maybe<void>::Ok();
+})
+.SetInputArgModifyFn([](user_op::GetInputArgModifier GetInputArgModifierFn,
+                        const user_op::UserOpConfWrapper&) {
   user_op::InputArgModifier* indices_modifier = GetInputArgModifierFn("index", 0);
   CHECK(indices_modifier != nullptr);
   indices_modifier->set_requires_grad(false);
-}
-
-
-Maybe<void> BuildSbp(user_op::SbpContext* ctx) {
-  const user_op::TensorDesc& index_tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("index", 0);
+})
+.SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
+  const user_op::TensorDesc& index_tensor =
+      ctx->LogicalTensorDesc4InputArgNameAndIndex("index", 0);
   int64_t index_num_axes = index_tensor.shape().NumAxes();
   const int32_t dim = ctx->Attr<int32_t>("dim");
 
@@ -73,35 +86,14 @@ Maybe<void> BuildSbp(user_op::SbpContext* ctx) {
           .Build();
     }
   }
-
   ctx->NewBuilder()
       .PartialSum(user_op::OpArg("input", 0))
       .Broadcast(user_op::OpArg("index", 0))
       .PartialSum(user_op::OpArg("output", 0))
       .Build();
   return Maybe<void>::Ok();
-}
+});
 
-Maybe<void> InferDtype(user_op::InferContext* ctx) {
-  const TensorDesc* index = ctx->TensorDesc4ArgNameAndIndex("index", 0);
-  CHECK_OR_RETURN(IsIndexDataType(index->data_type()));
-  const TensorDesc* in = ctx->TensorDesc4ArgNameAndIndex("input", 0);
-  user_op::TensorDesc* out = ctx->OutputTensorDesc("output", 0);
-  *out->mut_data_type() = in->data_type();
-  return Maybe<void>::Ok();
-}
-
-}  // namespace
-
-REGISTER_USER_OP("dim_gather")
-    .Input("input")
-    .Input("index")
-    .Output("output")
-    .Attr<int32_t>("dim")
-    .SetTensorDescInferFn(InferTensorDesc)
-    .SetInputArgModifyFn(GatherInputArgModifierFn)
-    .SetDataTypeInferFn(InferDtype)
-    .SetGetSbpFn(BuildSbp);
 
 REGISTER_USER_OP_GRAD("dim_gather").SetBackwardOpConfGenFn([](user_op::BackwardOpConfContext* ctx) {
   const auto op_grad_name = ctx->FwOp().op_name() + "_grad";
@@ -111,7 +103,7 @@ REGISTER_USER_OP_GRAD("dim_gather").SetBackwardOpConfGenFn([](user_op::BackwardO
         .OpTypeName(
             "dim_scatter_add_like")  // dim_scatter_add_like(like, dim, index, input) -> output
         .InputBind("index", ctx->FwOp().input("index", 0))  // scatter.index <- gather.index
-        .InputBind("input",
+        .InputBind("src",
                    ctx->FwOp().output_grad("output", 0))  // scatter.input <- grad of gather.out
         .InputBind("like", ctx->FwOp().input("input", 0))
         .Output("output")
