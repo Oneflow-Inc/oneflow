5f5c94b76c12394b819176fe30e382ce0bdb0760
tangnana <tnn_personal@163.com>
2021-07-09 17:53:21 +0800

    amend diag functional API

--- oneflow/core/functional/impl/array_functor.cpp
+++ oneflow/core/functional/impl/array_functor.cpp
@@ -459,12 +459,10 @@ class TriuFunctor {
 
 class DiagFunctor{
  public:
-  DiagFunctor() {
-    op_ = CHECK_JUST(one::OpBuilder("diag").Input("in").Output("out").Build());
-  }
-  Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x, const int64_t& diagonal) const {
+  DiagFunctor() {op_ = CHECK_JUST(one::OpBuilder("diag").Input("in").Output("out").Build()); }
+  Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x, const int32_t& diagonal) const {
     MutableAttrMap attrs;
-    JUST(attrs.SetAttr<int64_t>("diagonal", diagonal));
+    JUST(attrs.SetAttr<int32_t>("diagonal", diagonal));
     return OpInterpUtil::Dispatch<Tensor>(*op_, {x}, attrs);
   }
 
@@ -472,6 +470,22 @@ class DiagFunctor{
   std::shared_ptr<OpExpr> op_;
 
 };
+
+class DiagGradFunctor{
+ public:
+  DiagGradFunctor() {op_ = CHECK_JUST(one::OpBuilder("diag_grad").Input("dy").Input("in").Output("dx").Build()); }
+  Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& dy, const std::shared_ptr<one::Tensor>& x, const int32_t& diagonal) const {
+    MutableAttrMap attrs;
+    JUST(attrs.SetAttr<int32_t>("diagonal", diagonal));
+    return OpInterpUtil::Dispatch<Tensor>(*op_, {x}, attrs);
+  }
+
+ private:
+  std::shared_ptr<OpExpr> op_;
+
+};
+
+
 
 }  // namespace impl
 
@@ -501,6 +515,7 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
   m.add_functor<impl::UnsortedSegmentSumLikeFunctor>("UnsortedSegmentSumLike");
   m.add_functor<impl::TriuFunctor>("Triu");
   m.add_functor<impl::DiagFunctor>("Diag");
+  m.add_functor<impl::DiagGradFunctor>("DiagGrad");
 };
 
 }  // namespace functional
