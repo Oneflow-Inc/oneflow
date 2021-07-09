3871faf98b8f5b9451378eaf4277440e1ac35e0a
daquexian <daquexian566@gmail.com>
2021-07-09 19:18:40 +0800

    default dev id = local rank % gpu num
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- oneflow/core/framework/device.cpp
+++ oneflow/core/framework/device.cpp
@@ -18,6 +18,8 @@ limitations under the License.
 #include "oneflow/core/framework/vm_local_dep_object.h"
 #include "oneflow/core/control/global_process_ctx.h"
 #include "oneflow/core/common/str_util.h"
+#include "oneflow/core/job/resource_desc.h"
+#include "oneflow/core/job/global_for.h"
 #include "oneflow/core/job/parallel_desc.h"
 #include "oneflow/core/job/env_global_objects_scope.h"
 #include "oneflow/core/memory/memory_case_util.h"
@@ -78,7 +80,7 @@ Maybe<void> Device::Init() {
 }
 
 /*static*/ Maybe<Symbol<Device>> Device::New(const std::string& type) {
-  return New(type, GlobalProcessCtx::LocalRank());
+  return New(type, GlobalProcessCtx::LocalRank() % Global<ResourceDesc, ForEnv>::Get()->GpuDeviceNum());
 }
 
 const std::shared_ptr<const ParallelDesc>& Device::parallel_desc_ptr() const {
