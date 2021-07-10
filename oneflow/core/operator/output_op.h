dc45b30d8b75b72a3a9557c29b0c4f51d75f4be5
liufengwei <2472937968@qq.com>
2021-07-10 08:14:31 +0800

    maybe error stack from CheckAndConstructOp to OutputArgModifier callback function

--- oneflow/core/operator/output_op.h
+++ oneflow/core/operator/output_op.h
@@ -26,7 +26,7 @@ class OutputOp final : public Operator {
   OutputOp() = default;
   ~OutputOp() override = default;
 
-  void InitFromOpConf() override;
+  Maybe<void> InitFromOpConf() override;
   Maybe<void> InferOutBlobDescs(
       const std::function<BlobDesc*(const std::string&)>& GetBlobDesc4BnInOp,
       const ParallelContext* parallel_ctx) const override;
