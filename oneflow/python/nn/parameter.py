fc776fd256d93b36da4088e3da6ea1c0c2701167
wyg1997 <wyg19970408@gmail.com>
2021-07-12 20:52:34 +0800

    fix bug

--- oneflow/python/nn/parameter.py
+++ oneflow/python/nn/parameter.py
@@ -26,7 +26,9 @@ class Parameter(Tensor):
         # TODO: uncomment the following two lines when consistent <-> local conversion is ready
         # data.set_is_consistent(True)
         # data.set_placement(flow.placement("gpu", ["0:0"], None))
-        self._data = flow.Tensor(data)
+        if not isinstance(data, Tensor):
+            data = Tensor(data)
+        self._data = data
         self._data.requires_grad = requires_grad
 
     def __getattr__(self, name):
