87952beb96aa25febd78c220a08e4f40ed3ce37d
chengtbf <472491134@qq.com>
2021-07-13 22:12:20 +0800

    refine test_input_op_expr.py

--- oneflow/python/test/graph/test_input_op_expr.py
+++ oneflow/python/test/graph/test_input_op_expr.py
@@ -31,47 +31,52 @@ import oneflow._oneflow_internal
 from oneflow.python.framework.multi_client_session import MultiClientSession
 import oneflow.python.framework.c_api_util as c_api_util
 
-print("IsMultiClient?:", oneflow.distributed.is_multi_client())
-print(
-    "HasAllMultiClientEnvVars?:",
-    oneflow.python.framework.env_util.HasAllMultiClientEnvVars(),
-)
 
-oneflow.enable_eager_execution()
+class TestFeedInputTensor(unittest.TestCase):
+    def test_feed_input_tensor(test_case):
+        test_case.assertTrue(oneflow.distributed.is_multi_client())
+        test_case.assertTrue(
+            oneflow.python.framework.env_util.HasAllMultiClientEnvVars()
+        )
+        # TODO(chengcheng):  Using new lazy mode
+        oneflow.enable_eager_execution()
+
+        x = flow.Tensor(1, 1, 10, 10)
+        flow.nn.init.uniform_(x, a=-1.0, b=1.0)
+
+        session = session_ctx.GetDefaultSession()
+        test_case.assertTrue(isinstance(session, MultiClientSession))
+        session.TryInit()
+
+        oneflow.enable_eager_execution(False)
+
+        oneflow._oneflow_internal.JobBuildAndInferCtx_Open("cc_test_input_op_expr_job")
+        job_conf = oneflow._oneflow_internal.oneflow.core.job.job_conf.JobConfigProto()
+        job_conf.set_job_name("cc_test_input_op_expr_job")
+        job_conf.mutable_predict_conf()
+        c_api_util.CurJobBuildAndInferCtx_SetJobConf(job_conf)
+
+        op_name = "cc_Input_0"
+        input_conf = (
+            oneflow._oneflow_internal.oneflow.core.operator.op_conf.FeedInputOpConf()
+        )
+        input_conf.set_in_0("EagerTensorInput")
+        input_conf.set_out_0("out_0")
+
+        input_op = oneflow._oneflow_internal.one.FeedInputOpExpr(
+            op_name, input_conf, ["in_0"], ["out_0"]
+        )
+        attrs = oneflow._oneflow_internal.MutableCfgAttrMap()
+
+        if not x.is_determined:
+            x.determine()
+        x_tensor_in_c = x._local_or_consistent_tensor
+
+        out_tensor = input_op.apply([x_tensor_in_c], attrs)[0]
+        test_case.assertEqual(out_tensor.shape, (1, 1, 10, 10))
+        test_case.assertTrue(out_tensor.is_lazy)
+        test_case.assertTrue(out_tensor.is_consistent)
+
 
 if __name__ == "__main__":
-    # unittest.main()
-    x = flow.Tensor(1, 1, 10, 10)
-    flow.nn.init.uniform_(x, a=-1.0, b=1.0)
-
-    session = session_ctx.GetDefaultSession()
-    assert type(session) is MultiClientSession
-    session.TryInit()
-
-    oneflow.enable_eager_execution(False)
-
-    oneflow._oneflow_internal.JobBuildAndInferCtx_Open("cc_test_input_op_expr_job")
-    job_conf = oneflow._oneflow_internal.oneflow.core.job.job_conf.JobConfigProto()
-    job_conf.set_job_name("cc_test_input_op_expr_job")
-    job_conf.mutable_predict_conf()
-    c_api_util.CurJobBuildAndInferCtx_SetJobConf(job_conf)
-
-    op_name = "cc_Input_0"
-    input_conf = (
-        oneflow._oneflow_internal.oneflow.core.operator.op_conf.FeedInputOpConf()
-    )
-    input_conf.set_in_0("EagerTensorInput")
-    input_conf.set_out_0("out_0")
-    print("input_conf:", input_conf)
-    print(type(input_conf))
-
-    input_op = oneflow._oneflow_internal.one.FeedInputOpExpr(
-        op_name, input_conf, ["in_0"], ["out_0"]
-    )
-    attrs = oneflow._oneflow_internal.MutableCfgAttrMap()
-
-    if not x.is_determined:
-        x.determine()
-
-    out_tensor = input_op.apply([x._local_or_consistent_tensor], attrs)[0]
-    print("out_tensor shape:", out_tensor.shape)
+    unittest.main()
