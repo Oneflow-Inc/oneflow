25377f5f357523e8d5730787debb0278d1b4b400
clackhan <han_binbin@163.com>
2021-07-13 20:31:14 +0800

    mv single client python file to compatible_single_client_python

--- ci/test/1node_benchmark_test.sh
+++ ci/test/1node_benchmark_test.sh
@@ -1,7 +1,7 @@
 set -xe
 
 rm -rf /benchmarks
-cp -r oneflow/compatible/single_client/python/benchmarks /benchmarks
+cp -r oneflow/compatible_single_client_python/benchmarks /benchmarks
 cd /benchmarks
 
 python3 cnn_benchmark/of_cnn_benchmarks.py \
