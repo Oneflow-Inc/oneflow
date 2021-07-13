b840a030eed8f8b914a962ffc868665bf287deac
MARD1NO <359521840@qq.com>
2021-07-13 17:24:29 +0800

    add torch style conv3d unit test

--- oneflow/python/test/modules/test_conv3d.py
+++ oneflow/python/test/modules/test_conv3d.py
@@ -1,7 +1,6 @@
 import unittest
 import oneflow.experimental as flow 
 from automated_test_util import *
-import torch 
 
 @unittest.skipIf(
     not flow.unittest.env.eager_execution_enabled(),
@@ -14,8 +13,9 @@ class TestConv3DModule(flow.unittest.TestCase):
                 test_case, 
                 "nn.Conv3d", 
                 device=device, 
+                n=2, 
                 extra_generators={
-                    "input": random_tensor(ndim=5, dim1=4),
+                    "input": random_tensor(ndim=5, dim0=2, dim1=4, dim2=4, dim3=6, dim4=6),
                     "in_channels": constant(4),
                     "out_channels": constant(8),
                     "kernel_size": random(1, 3),
@@ -28,43 +28,45 @@ class TestConv3DModule(flow.unittest.TestCase):
                 }
             )
 
-    # def test_conv3d_group(test_case):
-    #     for device in ["cpu", "cuda"]:
-    #         test_module_against_pytorch(
-    #             test_case, 
-    #             "nn.Conv3d", 
-    #             device=device, 
-    #             extra_generators={
-    #                 "input": random_tensor(ndim=5, dim1=4),
-    #                 "in_channels": constant(4),
-    #                 "out_channels": constant(8),
-    #                 "kernel_size": random(1, 3),
-    #                 "stride": random(1, 2),
-    #                 "padding": random(1, 2),
-    #                 "dilation": constant(1),
-    #                 "groups": constant(2),
-    #                 "padding_mode": constant("zeros"),
-    #             },
-    #         )
+    def test_conv3d_group(test_case):
+        for device in ["cpu", "cuda"]:
+            test_module_against_pytorch(
+                test_case, 
+                "nn.Conv3d", 
+                device=device, 
+                n=1, 
+                extra_generators={
+                    "input": random_tensor(ndim=5, dim0=2, dim1=4, dim2=4, dim3=6, dim4=6),
+                    "in_channels": constant(4),
+                    "out_channels": constant(8),
+                    "kernel_size": random(1, 3),
+                    "stride": random(1, 2),
+                    "padding": random(1, 2),
+                    "dilation": constant(1),
+                    "groups": constant(2),
+                    "padding_mode": constant("zeros"),
+                },
+            )
     
-    # def test_conv3d_depthwise(test_case):
-    #     for device in ["cpu", "cuda"]:
-    #         test_module_against_pytorch(
-    #             test_case, 
-    #             "nn.Conv3d", 
-    #             device=device, 
-    #             extra_generators={
-    #                 "input": random_tensor(ndim=5, dim1=4),
-    #                 "in_channels": constant(4),
-    #                 "out_channels": constant(8),
-    #                 "kernel_size": random(1, 3),
-    #                 "stride": random(1, 2),
-    #                 "padding": random(1, 2),
-    #                 "dilation": constant(1),
-    #                 "groups": constant(4),
-    #                 "padding_mode": constant("zeros"),
-    #             },
-    #         )
+    def test_conv3d_depthwise(test_case):
+        for device in ["cpu", "cuda"]:
+            test_module_against_pytorch(
+                test_case, 
+                "nn.Conv3d", 
+                n=1, 
+                device=device, 
+                extra_generators={
+                    "input": random_tensor(ndim=5, dim0=2, dim1=4, dim2=4, dim3=6, dim4=6),
+                    "in_channels": constant(4),
+                    "out_channels": constant(8),
+                    "kernel_size": random(1, 3),
+                    "stride": random(1, 2),
+                    "padding": random(1, 2),
+                    "dilation": constant(1),
+                    "groups": constant(4),
+                    "padding_mode": constant("zeros"),
+                },
+            )
 
 if __name__ == "__main__":
     unittest.main()
