2769e5b48887218cd5e7699a83b087e13273b9c5
BBuf <1182563586@qq.com>
2021-07-13 10:58:24 +0800

    refine

--- oneflow/python/test/modules/test_atan2.py
+++ oneflow/python/test/modules/test_atan2.py
@@ -134,13 +134,6 @@ class TestAtan2(flow.unittest.TestCase):
                 device=device,
             )
 
-    # def test_flow_tensor_atan2_with_random_data(test_case):
-    #     for device in ["cpu", "cuda"]:
-    #         test_tensor_against_pytorch(
-    #             test_case,
-    #             "atan2",
-    #             device=device,
-    #         )
 
 
 if __name__ == "__main__":
