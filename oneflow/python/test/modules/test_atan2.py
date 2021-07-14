c0931364f6a6ec2a0e699673ffda1aa2655f4701
liufengwei <2472937968@qq.com>
2021-07-14 22:35:42 +0800

    delete skip if in test/(modules/tensor)/* because of default eager env

--- oneflow/python/test/modules/test_atan2.py
+++ oneflow/python/test/modules/test_atan2.py
@@ -102,10 +102,6 @@ def _test_atan2_backward(test_case, device):
     test_y_grad()
 
 
-@unittest.skipIf(
-    not flow.unittest.env.eager_execution_enabled(),
-    ".numpy() doesn't work in lazy mode",
-)
 class TestAtan2(flow.unittest.TestCase):
     def test_atan2_forward(test_case):
         arg_dict = OrderedDict()
--- oneflow/python/test/modules/test_ceil.py
+++ oneflow/python/test/modules/test_ceil.py
@@ -34,10 +34,6 @@ def _test_ceil_impl(test_case, device, shape):
     test_case.assertTrue(np.allclose(x.grad.numpy(), np.zeros(shape), 1e-4, 1e-4))
 
 
-@unittest.skipIf(
-    not flow.unittest.env.eager_execution_enabled(),
-    ".numpy() doesn't work in lazy mode",
-)
 class TestCeilModule(flow.unittest.TestCase):
     def test_ceil(test_case):
         arg_dict = OrderedDict()
