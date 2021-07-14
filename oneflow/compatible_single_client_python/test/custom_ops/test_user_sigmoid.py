de6dcad0c563313e86d98dc7c85f162f092a56b2
clackhan <han_binbin@163.com>
2021-07-14 11:23:29 +0800

    fix bug

--- oneflow/compatible_single_client_python/test/custom_ops/test_user_sigmoid.py
+++ oneflow/compatible_single_client_python/test/custom_ops/test_user_sigmoid.py
@@ -18,8 +18,8 @@ import os
 import numpy as np
 import math
 
-from oneflow.compatible import single_client as flow
-from oneflow.compatible.single_client import typing as oft
+import oneflow.compatible.single_client as flow
+import oneflow.compatible.single_client.typing as oft
 
 
 func_config = flow.FunctionConfig()
