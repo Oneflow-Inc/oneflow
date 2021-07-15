09d7543945e71176fa5a789fe3e17eb602d73341
Xinqi Li <lixinqi0703106@163.com>
2021-07-15 14:52:11 +0800

    add job_pass MultiClientAutoSourceAndSinkTick

--- oneflow/core/job_rewriter/autotick.cpp
+++ oneflow/core/job_rewriter/autotick.cpp
@@ -420,6 +420,54 @@ Maybe<void> AddGlobalInputOutputCriticalSection(
   return Maybe<void>::Ok();
 }
 
+Maybe<void> AddMultiClientWaitAndSendIds(JobBuilder* job_builder, int64_t machine_id,
+                                         const std::string& src_op_name) {
+  ParallelConf parallel_conf;
+  {
+    parallel_conf.set_device_tag("cpu");
+    parallel_conf.add_device_name(std::string("@") + std::to_string(machine_id) + ":0");
+  }
+  OperatorConf wait_and_send_ids_op_conf;
+  {
+    wait_and_send_ids_op_conf.set_name(std::string("System-Src-WaitAndSendIds_") + NewUniqueId());
+    wait_and_send_ids_op_conf.set_pass_tag(kMainOp);
+    auto* wait_and_send_ids_conf = wait_and_send_ids_op_conf.mutable_wait_and_send_ids_conf();
+    wait_and_send_ids_conf->set_out("out");
+    wait_and_send_ids_conf->set_wait_buffer_name("UnimplementedBufferName");
+    wait_and_send_ids_conf->set_data_type(DataType::kInt32);
+    // wait_and_send_ids_conf->id_list() is unused in multi-client mode.
+  }
+  JUST(job_builder->AddOp(parallel_conf, wait_and_send_ids_op_conf));
+  OperatorConf source_tick_op = JUST(job_builder->OpConf4OpName(src_op_name));
+  {
+    CHECK_OR_RETURN(source_tick_op.has_source_tick_conf());
+    auto* source_tick_op_conf = source_tick_op.mutable_source_tick_conf();
+    CHECK_OR_RETURN(!source_tick_op_conf->has_wait_in());
+    source_tick_op_conf->set_wait_in(GenLogicalBlobName(wait_and_send_ids_op_conf.name(), "out"));
+  }
+  JUST(job_builder->MutOpOnlyOnce(source_tick_op));
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> AddMultiClientCallbackNotifier(JobBuilder* job_builder, int64_t machine_id,
+                                           const std::string& sink_op_name) {
+  ParallelConf parallel_conf;
+  {
+    parallel_conf.set_device_tag("cpu");
+    parallel_conf.add_device_name(std::string("@") + std::to_string(machine_id) + ":0");
+  }
+  OperatorConf callback_notify_op_conf;
+  {
+    callback_notify_op_conf.set_name(std::string("System-Sink-CallbackNotify_") + NewUniqueId());
+    callback_notify_op_conf.set_pass_tag(kMainOp);
+    auto* callback_notify_conf = callback_notify_op_conf.mutable_callback_notify_conf();
+    callback_notify_conf->set_in(GenLogicalBlobName(sink_op_name, "/out"));
+    // callback_notify_conf->callback_buffer_name() is unused in multi-client mode.
+  }
+  JUST(job_builder->AddOp(parallel_conf, callback_notify_op_conf));
+  return Maybe<void>::Ok();
+}
+
 }  // namespace
 
 Maybe<void> AutoPrependTick(const OpGraph& op_graph, JobBuilder* job_builder) {
@@ -487,6 +535,34 @@ Maybe<void> SingleClientAutoSourceAndSinkTick(const OpGraph& op_graph, JobBuilde
     return Maybe<void>::Ok();
   };
   JUST(AutoSourceAndSinkTick(op_graph, job_builder, DoEachSrc, DoEachSink));
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> MultiClientAutoSourceAndSinkTick(const OpGraph& op_graph, Job* job) {
+  if (!JUST(*Global<Maybe<bool>, MultiClient>::Get())) { return Maybe<void>::Ok(); }
+  HashMap<int64_t, std::string> machine_id2src_op_name;
+  HashMap<int64_t, std::string> machine_id2sink_op_name;
+  {
+    JobBuilder job_builder(job);
+    const auto& DoEachSrc = [&](int64_t machine_id, const std::string& op_name) -> Maybe<void> {
+      CHECK_OR_RETURN(machine_id2src_op_name.emplace(machine_id, op_name).second);
+      return Maybe<void>::Ok();
+    };
+    const auto& DoEachSink = [&](int64_t machine_id, const std::string& op_name) -> Maybe<void> {
+      CHECK_OR_RETURN(machine_id2sink_op_name.emplace(machine_id, op_name).second);
+      return Maybe<void>::Ok();
+    };
+    JUST(AutoSourceAndSinkTick(op_graph, &job_builder, DoEachSrc, DoEachSink));
+  }
+  {
+    JobBuilder job_builder(job);
+    for (const auto& pair : machine_id2src_op_name) {
+      JUST(AddMultiClientWaitAndSendIds(&job_builder, pair.first, pair.second));
+    }
+    for (const auto& pair : machine_id2sink_op_name) {
+      JUST(AddMultiClientCallbackNotifier(&job_builder, pair.first, pair.second));
+    }
+  }
   return Maybe<void>::Ok();
 }
 
