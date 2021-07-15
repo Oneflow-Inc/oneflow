ac3233b982219fd6f3005a726ca3062aac159f4a
clackhan <han_binbin@163.com>
2021-07-15 11:22:58 +0800

    fix bug

--- oneflow/core/framework/instructions_builder.cpp
+++ oneflow/core/framework/instructions_builder.cpp
@@ -1600,8 +1600,9 @@ Maybe<void> LogicalRun(const std::function<Maybe<void>(InstructionsBuilder*)>& B
 Maybe<void> PhysicalRun(const std::function<Maybe<void>(InstructionsBuilder*)>& Build) {
   vm::InstructionMsgList instruction_list;
   vm::cfg::EagerSymbolList eager_symbol_list;
-  InstructionsBuilder instructions_builder(std::shared_ptr<vm::PhysicalIdGenerator>(),
-                                           &instruction_list, &eager_symbol_list,
+  const std::shared_ptr<vm::LogicalIdGenerator> id_generator =
+      std::make_shared<vm::LogicalIdGenerator>();
+  InstructionsBuilder instructions_builder(id_generator, &instruction_list, &eager_symbol_list,
                                            _ReleasePhysicalObject);
   JUST(Build(&instructions_builder));
   if (debug::RecordingInstructions()) {
