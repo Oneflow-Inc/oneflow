d2c0a75700d6b7075599bb6493bcb732a04dc844
daquexian <daquexian566@gmail.com>
2021-07-15 09:47:50 +0800

    use actions/checkout@v2
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- ci/test_multi_client/test_speed.sh
+++ ci/test_multi_client/test_speed.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -uex
 
-git clone https://github.com/Oneflow-Inc/models 
-cd models
-# TODO: remove this line when merged
-git co add_speed_comparing_script
+cd $ONEFLOW_MODEL_DIR
 python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 8x3x224x224
