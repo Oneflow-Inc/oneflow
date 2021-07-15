3e2d67892b44f0ed743592dd8e1b132fe97293af
daquexian <daquexian566@gmail.com>
2021-07-15 10:02:29 +0800

    add more input size
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- ci/test_multi_client/test_speed.sh
+++ ci/test_multi_client/test_speed.sh
@@ -3,4 +3,8 @@
 set -uex
 
 cd $ONEFLOW_MODEL_DIR
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 16x3x224x224
 python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 8x3x224x224
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 4x3x224x224
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 2x3x224x224
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 1x3x224x224
