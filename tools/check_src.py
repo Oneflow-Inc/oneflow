d495b8a9bb64b8a61e0504546408c5792cfba421
tsai <jackalcooper@gmail.com>
2021-07-17 15:57:24 +0800

    remove files and add checks

--- tools/check_src.py
+++ tools/check_src.py
@@ -39,16 +39,12 @@ def check_unwanted_test_scripts(python_test_dir=None, allowed=None):
 
 check_unwanted_test_scripts(
     python_test_dir=os.path.join(src_root, "oneflow/python/test"),
-    allowed=[
-        "customized",
-        "custom_ops",
-        "dataloader",
-        "graph",
-        "models",
-        "modules",
-        "ops",
-        "serving",
-        "tensor",
-        "xrt",
-    ],
+    allowed=["custom_ops", "dataloader", "graph", "models", "modules", "tensor",],
+)
+
+check_unwanted_test_scripts(
+    python_test_dir=os.path.join(
+        src_root, "oneflow/compatible_single_client_python/test"
+    ),
+    allowed=["models", "ops", "serving", "xrt",],
 )
