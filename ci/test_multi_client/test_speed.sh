3e5fde0298415fb37d3a1f52aa7c17ed7d7af219
daquexian <daquexian566@gmail.com>
2021-07-18 19:36:54 +0800

    update test_speed.sh to check rel speed
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- ci/test_multi_client/test_speed.sh
+++ ci/test_multi_client/test_speed.sh
@@ -3,8 +3,12 @@
 set -uex
 
 cd $ONEFLOW_MODEL_DIR
-python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 16x3x224x224
-python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 8x3x224x224
-python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 4x3x224x224
-python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 2x3x224x224
-python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 1x3x224x224
+
+function check_relative_speed {
+  awk -F'[:(]' -v threshold=$1 'BEGIN { ret=2 } /Relative speed/{ if ($2 > threshold) { ret=0 } else { ret=1 }} {print $0} END { exit ret }'
+}
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 16x3x224x224 | check_relative_speed 1.05
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 8x3x224x224 | check_relative_speed 1.05
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 4x3x224x224 | check_relative_speed 1
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 2x3x224x224 | check_relative_speed 0.8
+python3 scripts/compare_speed_with_pytorch.py resnet50/models/resnet50.py resnet50 1x3x224x224 | check_relative_speed 0.8
