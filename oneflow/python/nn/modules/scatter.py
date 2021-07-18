33c536195fb4f0bb8dd0aca4b1e7164be75f68d6
YaoChi <later@usopp.net>
2021-07-18 13:42:34 +0800

    add scatter op

---
+++ oneflow/python/nn/modules/scatter.py
@@ -0,0 +1,90 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import oneflow as flow
+
+from oneflow.python.framework.tensor import Tensor
+from oneflow.python.oneflow_export import oneflow_export, experimental_api
+from oneflow.python.framework.tensor import register_tensor_op
+from oneflow.python.nn.module import Module
+
+from typing import Optional, List, Tuple
+
+class Scatter(Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def forward(self, input, dim, index, src, reduce):
+        assert type(src) in [flow.Tensor, float], f"type of src must be oneflow.Tensor or float, but %s givien" % type(src)
+        if isinstance(src, flow.Tensor):
+            return flow.F.dim_scatter(input, index, src, dim)
+        elif isinstance(src, float):
+            return flow.F.dim_scatter_scalar(input, index, src, dim)
+
+@oneflow_export("scatter")
+@experimental_api
+def scatter_op(input, dim, index, src, reduce:Optional[str]=None):
+    r"""This operator writes the elements specified by `index` along with the axis 
+    `dim` from the `src` into the `input`.
+
+    Take a 3-D blob as example, the output is specified by:
+    
+    .. code-block:: python
+
+        input[index[i][j][k]][j][k] = src[i][j][k]  # if dim == 0
+        input[i][index[i][j][k]][k] = src[i][j][k]  # if dim == 1
+        input[i][j][index[i][j][k]] = src[i][j][k]  # if dim == 2
+
+    input, index and src (if it is a Tensor) should all have the same number of dimensions. 
+    It is also required that index.shape(d) <= src.shape(d) for all dimensions d, 
+    and that index.shape(d) <= self.shape(d) for all dimensions d != dim.
+    Note that index and src do not broadcast.
+
+    Args:
+        input (Tensor): The input blob.
+        dim (int): The axis along which to index
+        index (Tensor): The index blob of elements to scatter. 
+        src (Tensor or float): The source blob whose elements will be scatterd and updated to output.
+
+    Returns:
+        Tensor: The scatterd Tensor. 
+
+    For example: 
+
+    .. code-block:: python
+
+        >>> import oneflow.experimental as flow
+        >>> import numpy as np
+
+        >>> input = flow.ones((3,5))
+        >>> index = flow.tensor(np.array([[0,1,2],[0,1,4]], ), dtype=flow.int32)
+        >>> src = flow.Tensor(np.random.rand(2,5))
+        >>> out = flow.scatter(input, 1, index, src)
+        >>> out.shape
+        flow.Size([3, 5])
+        >>> out = flow.scatter(input, 1, index, 3.14)
+        >>> out
+        tensor([[3.14, 3.14, 3.14, 1.  , 1.  ],
+                [3.14, 3.14, 1.  , 1.  , 3.14],
+                [1.  , 1.  , 1.  , 1.  , 1.  ]], dtype=oneflow.float32)
+     
+    """
+
+    return Scatter()(input, dim, index, src, reduce)
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(raise_on_error=True)
