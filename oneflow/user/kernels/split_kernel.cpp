13720d1f48b0d671734b7215097e7a149bc3ffd7
tea321000 <834914152@qq.com>
2021-07-19 16:55:12 +0800

    delete comment

--- oneflow/user/kernels/split_kernel.cpp
+++ oneflow/user/kernels/split_kernel.cpp
@@ -10,16 +10,10 @@ namespace oneflow {
 
         SliceParams ConstructSplitParams(const user_op::Tensor *entire, const user_op::Tensor *sliced,
                                          const int64_t dim, int64_t start_idx, int64_t end_idx) {
-//            const auto& start_vec = ctx->Attr<std::vector<int64_t>>("start");
-//            const auto& stop_vec = ctx->Attr<std::vector<int64_t>>("stop");
-//            const auto& step_vec = ctx->Attr<std::vector<int64_t>>("step");
 
             const int64_t ndim = entire->shape().NumAxes();
             CHECK_LE(ndim, kSliceMaxDims);
             CHECK_EQ(sliced->shape().NumAxes(), ndim);
-//            CHECK_EQ(start_vec.size(), ndim);
-//            CHECK_EQ(stop_vec.size(), ndim);
-//            CHECK_EQ(step_vec.size(), ndim);
 
             SliceParams params;
             std::memset(&params, 0, sizeof(SliceParams));
@@ -28,8 +22,6 @@ namespace oneflow {
             {
                 const int64_t dim_size = entire->shape().At(i);
                 const int64_t slice_size = sliced->shape().At(i);
-//                const int64_t step = step_vec.at(i);
-//                CHECK_NE(step, 0);
                 CHECK_GE(start_idx, 0);
                 CHECK_LT(start_idx, dim_size);
                 CHECK_GE(end_idx, 0);
@@ -39,13 +31,8 @@ namespace oneflow {
                 const int64_t start = RegulateSliceStart(dim_start_idx, dim_size);
                 const int64_t stop = RegulateSliceStop(dim_end_idx, dim_size);
 
-//                const int64_t start = RegulateSliceStart(start_vec.at(i), dim_size);
-//                const int64_t stop = RegulateSliceStop(stop_vec.at(i), dim_size);
-//                if (step > 0) {
                 CHECK_LT(start + slice_size - 1, stop);
-//                } else {
-//                    CHECK_GT(start + step * (slice_size - 1), stop);
-//                }
+
                 params.dims[i] = dim_size;
                 params.start[i] = start;
                 params.step[i] = 1;
@@ -55,11 +42,6 @@ namespace oneflow {
         }
 
 
-//        template<typename T>
-//        template<DeviceType device_type, typename T>
-//        void SplitSectionUtil(user_op::KernelComputeContext *ctx) {
-//
-//        }
 
         template<DeviceType device_type, typename T>
         class SplitKernel final : public user_op::OpKernel {
@@ -69,76 +51,9 @@ namespace oneflow {
             ~SplitKernel() override = default;
 
         private:
-//            void InferShape(user_op::KernelInferContext *ctx) const override {
-//                const auto dim = ctx->Attr<int64_t>("axis");
-//                const auto sections = ctx->Attr<int64_t>("sections");
-//                const ShapeView &in_shape_view = ctx->ShapeView4ArgNameAndIndex("in", 0);
-//                int64_t total_dim_size = 0;
-////        const int64_t like_num_axes = ctx->ShapeView4ArgNameAndIndex("like", 0).NumAxes();
-//                const int64_t in_num_axes = in_shape_view.NumAxes();
-////        CHECK_LE(like_num_axes, in_num_axes);
-////        CHECK_LT(axis, like_num_axes);
-//                FOR_RANGE(int32_t, i, 0, ctx->outputs().size())
-//                    {
-//                        ////            const ShapeView& like_shape_view = ctx->ShapeView4ArgNameAndIndex("like", i);
-//                        ////            CHECK_EQ(like_shape_view.NumAxes(), like_num_axes);
-//                        //            FOR_RANGE(int64_t, j, 0, like_num_axes) {
-//                        //                if (j == axis) {
-//                        //                total_dim_size += like_shape_view.At(j);
-//                        //                    } else {
-//                        //                    CHECK_EQ(like_shape_view.At(j), in_shape_view.At(j));
-//                        //                }
-//                        //            }
-//                        if (ctx->TensorDesc4ArgNameAndIndex("out", i)->is_dynamic()) {
-//                            auto *mut_shape_view = ctx->MutShapeView4ArgNameAndIndex("out", i);
-//                            CHECK_NOTNULL(mut_shape_view);
-//                            DimVector out_i_dim_vec;
-//                            in_shape_view.ToDimVector(&out_i_dim_vec);
-//                            FOR_RANGE(int64_t, j, 0, in_num_axes)
-//                            {
-//                                if (dim == j)
-//                                    out_i_dim_vec.push_back(sections);
-//                                else
-//                                    out_i_dim_vec.push_back(in_shape_view.At(j));
-//                            }
-//                            mut_shape_view->set_shape(Shape(out_i_dim_vec));
-//                        }
-//                    }
-//
-//                CHECK_EQ(total_dim_size, in_shape_view.At(dim));
-//            }
 
             void Compute(user_op::KernelComputeContext *ctx) const override {
-//                const auto sizes = ctx->Attr < std::vector < int64_t >> ("sizes");
-//                const user_op::Tensor* in_tensor = ctx->Tensor4ArgNameAndIndex("in", 0);
-//                const auto dim = ctx->Attr<int64_t>("axis");
-//                CHECK_GE(dim, 0);
-//                const auto sections = ctx->Attr<int64_t>("sections");
-//                const auto sizes = ctx->Attr<std::vector<int64_t>>("sizes");
-//                const ShapeView& in_shape_view = ctx->ShapeView4ArgNameAndIndex("in", 0);
-//                const int64_t dim_size = in_shape_view.At(i);
-//                SplitSectionUtil(ctx);
-//                    CHECK_GE(sections, 0);
-
-
-//                const int64_t in_cols = in_tensor->shape().Count(axis);
-//                const int64_t rows = in_tensor->shape().elem_cnt() / in_cols;
-//                CHECK_GT(rows, 0);
-//                int64_t in_col_offset = 0;
-//                for (const auto& out_arg_pair : ctx->outputs()) {
-//                    user_op::Tensor* out_tensor =
-//                            ctx->Tensor4ArgNameAndIndex(out_arg_pair.first, out_arg_pair.second);
-//                    const int64_t out_cols = out_tensor->shape().Count(axis);
-//                    CHECK_EQ(out_tensor->shape().elem_cnt(), rows * out_cols);
-//                    if (out_cols > 0) {
-//                        NewKernelUtil<device_type>::CopyColsRegion(ctx->device_ctx(), rows, out_cols,
-//                                                                   in_tensor->dptr<T>(), in_col_offset, in_cols,
-//                                                                   out_tensor->mut_dptr<T>(), 0, out_cols);
-//                    }
-//                    in_col_offset += out_cols;
-//                }
-//                CHECK_EQ(in_col_offset, in_cols);
-
+//
                 const user_op::Tensor *in_tensor = ctx->Tensor4ArgNameAndIndex("in", 0);
                 const auto dim = ctx->Attr<int64_t>("axis");
                 CHECK_GE(dim, 0);
@@ -157,8 +72,6 @@ namespace oneflow {
                     SliceParams params = ConstructSplitParams(in_tensor, out_i, dim, start_idx, end_idx);
                     SliceKernelUtil<device_type, T>::Forward(ctx->device_ctx(), params, in_tensor->dptr<T>(),
                                                              out_i->mut_dptr<T>());
-//                int64_t split_size = (split_idx < num_splits_one_extra) ? (min_split_size + 1) : min_split_size;
-//                splits[split_idx] = at::slice(self, dim_, start_idx, start_idx + split_size);
                     start_idx += sections;
                 }
             }
--- oneflow/user/ops/split_op.cpp
+++ oneflow/user/ops/split_op.cpp
@@ -23,10 +23,7 @@ namespace oneflow {
             const auto dim = ctx->Attr<int64_t>("axis");
             const auto sections = ctx->Attr<int64_t>("sections");
             const user_op::TensorDesc &in_desc = ctx->InputTensorDesc("in", 0);
-//  int64_t dynamic_dim_size = 0;
-//  int64_t static_dim_size = 0;
             const int64_t in_num_axes = ctx->InputTensorDesc("in", 0).shape().NumAxes();
-//  const int64_t like_num_axes = ctx->InputTensorDesc("like", 0).shape().NumAxes();
             CHECK_GE_OR_RETURN(dim, 0);
             CHECK_LT_OR_RETURN(dim, in_num_axes);
             printf("in inferTensorDesc\n");
@@ -37,38 +34,12 @@ namespace oneflow {
             FOR_RANGE(int64_t, i, 0, num_splits)
             {
                 printf("i:%d\n", i);
-                //    const user_op::TensorDesc& like_i_desc = ctx->InputTensorDesc("like", i);
                 user_op::TensorDesc *out_i_desc = ctx->OutputTensorDesc("out", i);
-                //    CHECK_EQ_OR_RETURN(like_i_desc.shape().NumAxes(), like_num_axes);
-                //    FOR_RANGE(int64_t, j, 0, like_num_axes) {
-                //      if (j == axis) {
-                //        if (like_i_desc.is_dynamic()) {
-                //          dynamic_dim_size += like_i_desc.shape().At(j);
-                //        } else {
-                //          static_dim_size += like_i_desc.shape().At(j);
-                //        }
-                //      } else {
-                //        CHECK_EQ_OR_RETURN(in_desc.shape().At(j), like_i_desc.shape().At(j));
-                //      }
-//            }
                 DimVector out_i_dim_vec = in_desc.shape().dim_vec();
                 out_i_dim_vec[dim] = i < min_split_size ? sections : num_splits_one_extra;
                 for(auto it:out_i_dim_vec){
                     printf("it of out_dim:%d\n", it);
                 }
-//                FOR_RANGE(int64_t, j, 0, in_num_axes)
-//                {
-//                    if (dim != j)
-//                        out_i_dim_vec.push_back(in_desc.shape().At(j));
-//                    else
-//                    {
-//                        if(i < min_split_size)
-//                            out_i_dim_vec.push_back(sections);
-//                        else
-//                            out_i_dim_vec.push_back(num_splits_one_extra);
-//                    }
-//
-//                }
                 printf("before mut\n");
                 *out_i_desc->mut_shape() = Shape(out_i_dim_vec);
                 printf("after mut\n");
@@ -76,11 +47,6 @@ namespace oneflow {
             printf("in inferTensorDesc\n");
 
 
-//  if (dynamic_dim_size == 0) {
-//    CHECK_EQ_OR_RETURN(static_dim_size, in_desc.shape().At(dim));
-//  } else {
-//    CHECK_LE_OR_RETURN(static_dim_size, in_desc.shape().At(dim));
-//  }
             return Maybe<void>::Ok();
         }
 
@@ -99,112 +65,30 @@ namespace oneflow {
             return Maybe<void>::Ok();
         }
 
-//Maybe<void> SetLikeArgModifier(user_op::GetInputArgModifier GetInputArgModifierFn,
-//                               const user_op::UserOpConfWrapper& user_op_conf) {
-//  FOR_RANGE(int32_t, i, 0, user_op_conf.input_size("like")) {
-//    user_op::InputArgModifier* like_modifier = GetInputArgModifierFn("like", i);
-//    CHECK_NOTNULL_OR_RETURN(like_modifier);
-//    like_modifier->set_requires_grad(false);
-//  }
-//  return Maybe<void>::Ok();
-//}
 
         Maybe<void> GetSbpSignature(user_op::SbpContext *ctx) {
             const auto dim = ctx->Attr<int64_t>("axis");
             const int64_t in_num_axes =
                     ctx->LogicalTensorDesc4InputArgNameAndIndex("in", 0).shape().NumAxes();
-//  const int64_t like_num_axes =
-//      ctx->LogicalTensorDesc4InputArgNameAndIndex("like", 0).shape().NumAxes();
             FOR_RANGE(int64_t, i, 0, in_num_axes)
             {
                 if (i == dim) { continue; }
                 ctx->NewBuilder().Split(ctx->inputs(), i).Split(ctx->outputs(), i).Build();
             }
             ctx->NewBuilder().PartialSum(ctx->inputs()).PartialSum(ctx->outputs()).Build();
-//  std::vector<user_op::OpArg> like_arg_vec;
-//  const size_t like_arg_size = ctx->outputs().size();
-//  like_arg_vec.reserve(like_arg_size);
-//  FOR_RANGE(int32_t, i, 0, like_arg_size) { like_arg_vec.emplace_back("like", i); }
-//  FOR_RANGE(int64_t, i, like_num_axes, in_num_axes) {
-//    ctx->NewBuilder()
-//        .Split(user_op::OpArg("in", 0), i)
-//        .Broadcast(like_arg_vec)
-//        .Split(ctx->outputs(), i)
-//        .Build();
-//    ctx->NewBuilder()
-//        .Split(user_op::OpArg("in", 0), i)
-//        .PartialSum(like_arg_vec)
-//        .Split(ctx->outputs(), i)
-//        .Build();
-//  }
-//  ctx->NewBuilder()
-//      .PartialSum(user_op::OpArg("in", 0))
-//      .PartialSum(like_arg_vec)
-//      .PartialSum(ctx->outputs())
-//      .Build();
-//  ctx->NewBuilder()
-//      .PartialSum(user_op::OpArg("in", 0))
-//      .Broadcast(like_arg_vec)
-//      .PartialSum(ctx->outputs())
-//      .Build();
-//  ctx->NewBuilder()
-//      .Broadcast(user_op::OpArg("in", 0))
-//      .PartialSum(like_arg_vec)
-//      .Broadcast(ctx->outputs())
-//      .Build();
             return Maybe<void>::Ok();
         }
 
-//        void GenGradOp(const user_op::UserOpWrapper &op, user_op::AddOpFn AddOp) {
-//            const int64_t axis = op.attr<int64_t>("axis");
-//            const int32_t out_size = op.output_size("out");
-//            int64_t max_dim_size = 0;
-//            FOR_RANGE(int32_t, i, 0, out_size)
-//            {
-//                max_dim_size += op.TensorDesc4ArgNameAndIndex("like", i).shape().At(axis);
-//            }
-//            if (op.NeedGenGradTensor4OpInput("in", 0)) {
-//                user_op::UserOpConfWrapperBuilder builder(op.op_name() + "_grad");
-//                builder = builder.Op("concat");
-//                FOR_RANGE(int32_t, i, 0, out_size)
-//                {
-//                    std::string out_diff_lbn;
-//                    if (op.HasGradTensor4OpOutput("out", i)) {
-//                        out_diff_lbn = op.GetGradTensorWithOpOutput("out", i);
-//                    } else {
-//                        auto zero_like_op = user_op::UserOpConfWrapperBuilder(op.op_name() + "_grad_zero_like_out_"
-//                                                                              + std::to_string(i))
-//                                .Op("zero_like")
-//                                .Input("like", op.output("out", i))
-//                                .Output("out")
-//                                .Build();
-//                        AddOp(zero_like_op);
-//                        out_diff_lbn = zero_like_op.output("out", 0);
-//                    }
-//                    builder = builder.Input("in", out_diff_lbn);
-//                }
-//                user_op::UserOpConfWrapper grad_op =
-//                        builder.Output("out").Attr("axis", axis).Attr("max_dim_size", max_dim_size).Build();
-//
-//                op.BindGradTensorWithOpInput(grad_op.output("out", 0), "in", 0);
-//                AddOp(grad_op);
-//            }
-//        }
-
-    }  // namespace
+    }
 
     REGISTER_USER_OP("split")
     .Input("in")
     .Output("out")
-//    .InputWithMinimum("like", 2)
-//    .OutputWithMinimum("out", 2)
     .Attr<int64_t>("axis")
     .Attr<int64_t>("sections")
     .SetTensorDescInferFn(InferTensorDesc)
-//    .SetInputArgModifyFn(SetLikeArgModifier)
     .SetGetSbpFn(GetSbpSignature)
     .SetDataTypeInferFn(InferDataType);
 
-//REGISTER_USER_OP_GRAD("split").SetGenBackwardOpConfFn(GenGradOp);
 
 }  // namespace oneflow
