1070956e95816af5a36c26da09d6f95b58299b97
tsai <jackalcooper@gmail.com>
2021-07-21 20:23:37 +0800

    dirtyfix

--- oneflow/python/test/modules/automated_test_util.py
+++ oneflow/python/test/modules/automated_test_util.py
@@ -16,7 +16,10 @@ limitations under the License.
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(BASE_DIR)
-
+test_util_dir = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
+if os.getenv("ONEFLOW_TEST_UTIL_DIR"):
+    test_util_dir = os.getenv("ONEFLOW_TEST_UTIL_DIR")
+sys.path.append(test_util_dir)
 from test_utils.automated_test_util import *
