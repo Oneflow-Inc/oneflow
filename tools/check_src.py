81fb8bd7820561e2aab2777367a34b58b5109891
tsai <jackalcooper@gmail.com>
2021-07-21 16:56:36 +0800

    fix code

--- tools/check_src.py
+++ tools/check_src.py
@@ -12,7 +12,10 @@ def check_unwanted_test_scripts(python_test_dir=None, allowed=None):
         os.path.relpath(os.path.join(python_test_dir, a), src_root) for a in allowed
     ]
     for (dirpath, dirnames, filenames) in os.walk(src_root):
-        if python_test_dir in dirpath and "__pycache__" not in dirpath:
+        if (
+            dirpath.startswith(os.path.abspath(python_test_dir) + os.sep)
+            and "__pycache__" not in dirpath
+        ):
             rel_to_python_test = os.path.relpath(dirpath, python_test_dir)
             rel_to_src_root = os.path.relpath(dirpath, src_root)
             print(f"checking: {rel_to_src_root}")
