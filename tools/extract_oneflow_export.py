caa6f2bdecbb3f1ad83a98bb3bf98c83ab50e28a
tsai <jackalcooper@gmail.com>
2021-07-22 19:31:49 +0800

    refine

--- tools/extract_oneflow_export.py
+++ tools/extract_oneflow_export.py
@@ -122,6 +122,7 @@ class ExportVisitor(ast.NodeTransformer):
                         level=0,
                     )
                     import_from_exports.append(import_from_export)
+                # TODO: insert imports in func body
                 self.append_export(target_module=target_module, node=node)
                 return import_from_exports
         return node
