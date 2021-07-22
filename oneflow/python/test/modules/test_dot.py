53c8ed7264ffb0c8018cfc4912dd1286d656bcb7
luqiang-guo <702572275@qq.com>
2021-07-22 16:14:48 +0800

    modified test_dot.py

--- oneflow/python/test/modules/test_dot.py
+++ oneflow/python/test/modules/test_dot.py
@@ -22,7 +22,7 @@ from test_util import GenArgList
 import oneflow.experimental as flow
 
 
-def _test_dot(test_case, device, dtype):
+def _test_dot_forward(test_case, device, dtype):
     np_x = np.random.randn(1000).astype(dtype)
     np_y = np.random.randn(1000).astype(dtype)
 
@@ -32,6 +32,7 @@ def _test_dot(test_case, device, dtype):
     y = flow.tensor(np_y, device=flow.device(device))
 
     out = flow.dot(x, y)
+    print(np_out, out)
     test_case.assertTrue(np.allclose(np_out, out.numpy(), rtol=1e-04, atol=1e-10))
 
 
@@ -40,17 +41,21 @@ def _test_dot(test_case, device, dtype):
     ".numpy() doesn't work in lazy mode",
 )
 class TestDot(flow.unittest.TestCase):
-    def test_cpu_dot(test_case):
+    def test_fw_dot(test_case):
         arg_dict = OrderedDict()
+        arg_dict["test_fun"] = [
+            _test_dot_forward,
+        ]
+        arg_dict["device"] = ["cpu", "cuda"]
         arg_dict["dtype"] = [np.float32, np.double]
         for arg in GenArgList(arg_dict):
-            _test_dot(test_case, "cpu", *arg)
+            arg[0](test_case, *arg[1:])
 
-    def test_gpu_dot(test_case):
-        arg_dict = OrderedDict()
-        arg_dict["dtype"] = [np.float32, np.double]
-        for arg in GenArgList(arg_dict):
-            _test_dot(test_case, "cuda", *arg)
+    # def test_gpu_dot(test_case):
+    #     arg_dict = OrderedDict()
+    #     arg_dict["dtype"] = [np.float32, np.double]
+    #     for arg in GenArgList(arg_dict):
+    #         _test_dot(test_case, "cuda", *arg)
 
 
 if __name__ == "__main__":
