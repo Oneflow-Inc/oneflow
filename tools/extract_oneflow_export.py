bc19cbdcaacfd25c7c4511bd808119118d608406
tsai <jackalcooper@gmail.com>
2021-07-22 11:21:57 +0800

    refine

--- tools/extract_oneflow_export.py
+++ tools/extract_oneflow_export.py
@@ -6,11 +6,9 @@ import ast
 from posixpath import basename, relpath
 import subprocess
 import multiprocessing
+from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--src_dir", type=str, default="oneflow/python",
-)
 parser.add_argument(
     "--out_dir", type=str, default="python",
 )
@@ -20,12 +18,37 @@ assert args.out_dir
 assert args.out_dir != "~"
 assert args.out_dir != "/"
 out_oneflow_dir = os.path.join(args.out_dir, "oneflow")
-subprocess.check_call(f"rm -rf {args.out_dir}", shell=True)
-subprocess.check_call(f"mkdir -p {out_oneflow_dir}", shell=True)
+
+
+class SrcFile:
+    def __init__(self, path: Path = None, destiny=None) -> None:
+        self.destiny = destiny
+
+        # pool = multiprocessing.Pool()
+        txt = path.read_text()
+        module = ast.parse(txt)
+        self.node2seg = dict(
+            [(node, ast.get_source_segment(txt, node)) for node in module.body]
+        )
+
+
+def get_files():
+    pool = multiprocessing.Pool()
+    segs = pool.map(
+        SrcFile, Path("oneflow/compatible_single_client_python").rglob("*.py"),
+    )
+    pool.close()
+    # for path in Path("oneflow/compatible_single_client_python").rglob("*.py"):
+    #     SrcFile(path=path)
+    # for path in Path("oneflow/python").rglob("*.py"):
+    #     print(path.absolute())
+
 
 if __name__ == "__main__":
-
+    subprocess.check_call(f"rm -rf {out_oneflow_dir}", shell=True)
+    subprocess.check_call(f"mkdir -p {out_oneflow_dir}", shell=True)
     # step 0: parse and load all segs into memory
+    get_files()
     # step 1: extract all exports
     # step 2: merge files under python/ into generated files
     # step 3: rename all
