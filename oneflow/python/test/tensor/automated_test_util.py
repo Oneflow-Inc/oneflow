7903eba833ff2b10842ced9edec1d96ed2792b57
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2021-07-23 10:36:52 +0800

    Add new autotest (#5562)
    * fix upsample nearest bug
    
    * fix upsample nearest bug (#5347)
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>
    
    * fix upsample bilinear bug
    
    * add new autotest case
    
    * add more tests
    
    * fix bug
    
    * auto format by CI
    
    * add rtol and atol
    
    * add rtol and atol
    
    * add nothing
    
    * auto format by CI
    
    * fix export bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * fix bug
    
    * auto format by CI
    
    * fix bug
    
    * fix bug
    
    * fix ci error
    
    * fix bug
    
    * auto format by CI
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

--- oneflow/python/test/tensor/automated_test_util.py
+++ oneflow/python/test/tensor/automated_test_util.py
@@ -16,8 +16,16 @@ limitations under the License.
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(BASE_DIR)
+test_util_parent_dir = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+)
 
+oneflow_test_utils_dir_from_env = os.getenv("ONEFLOW_TEST_UTILS_DIR")
+if oneflow_test_utils_dir_from_env:
+    from pathlib import Path
 
+    oneflow_test_utils_dir_from_env = Path(oneflow_test_utils_dir_from_env)
+    test_util_parent_dir = str(oneflow_test_utils_dir_from_env.parent.absolute())
+
+sys.path.append(test_util_parent_dir)
 from test_utils.automated_test_util import *
