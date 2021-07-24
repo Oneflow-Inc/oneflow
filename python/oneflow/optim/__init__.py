79b9c9b39c9047f0fa74508dc1bde4e0bca78739
jackalcooper <jackalcooper@gmail.com>
2021-07-24 07:51:06 +0000

    fix from . import lr_scheduler

--- python/oneflow/optim/__init__.py
+++ python/oneflow/optim/__init__.py
@@ -18,3 +18,5 @@ from oneflow.nn.optimizer.adamw import AdamW
 from oneflow.nn.optimizer.optimizer import Optimizer
 from oneflow.nn.optimizer.rmsprop import RMSprop
 from oneflow.nn.optimizer.sgd import SGD
+
+from . import lr_scheduler
