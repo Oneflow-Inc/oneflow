bec79f4e7c846ec2a3b15c135ff064272aab131c
YaoChi <later@usopp.net>
2021-07-24 11:30:39 +0800

    refine scatter_nd

--- oneflow/python/nn/modules/scatter_nd.py
+++ oneflow/python/nn/modules/scatter_nd.py
@@ -20,30 +20,8 @@ from oneflow.python.oneflow_export import oneflow_export
 from oneflow.python.nn.module import Module
 
 
-@oneflow_export("scatter_nd")
-class Scatter_nd(Module):
-    r"""This operator inserts the elements in `updates` according to the `index` and create a new Tensor.
 
-    Args:
-        index: The indices of `updates`. Its type should be `flow.int`.
-        updates: The update Tensor.
-        shape (Sequence[int]): The constant tensor shape, the constant tensor elements are all zero.
-
-    For example:
-
-    .. code-block:: python
-
-        >>> import oneflow as flow
-        >>> import numpy as np
-        >>> scatter_nd_layer = flow.scatter_nd([8])
-        >>> index = flow.Tensor(np.array([[1], [6], [4]]), dtype=flow.int)
-        >>> update = flow.Tensor(np.array([10.2,5.1,12.7]), dtype=flow.float)
-        >>> out = scatter_nd_layer(index,update)
-        >>> out
-        tensor([ 0. , 10.2,  0. ,  0. , 12.7,  0. ,  5.1,  0. ], dtype=oneflow.float32)
-
-    """
-
+class ScatterNd(Module):
     def __init__(self, shape: list):
         super().__init__()
         if not isinstance(shape, list):
@@ -63,8 +41,31 @@ class Scatter_nd(Module):
         res = self._op(index, updates)[0]
         return res
 
+@oneflow_export("scatter_nd")
+def _scatter_nd_op(index, update, shape):
+    r"""This operator inserts the elements in `updates` according to the `index` and create a new Tensor.
+
+    Args:
+        index: The indices of `updates`. Its type should be `flow.int`.
+        updates: The update Tensor.
+        shape (Sequence[int]): The constant tensor shape, the constant tensor elements are all zero.
+
+    For example:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> import numpy as np
+        >>> index = flow.Tensor(np.array([[1], [6], [4]]), dtype=flow.int)
+        >>> update = flow.Tensor(np.array([10.2,5.1,12.7]), dtype=flow.float)
+        >>> out = flow.scatter_nd(index,update, [8])
+        >>> out
+        tensor([ 0. , 10.2,  0. ,  0. , 12.7,  0. ,  5.1,  0. ], dtype=oneflow.float32)
+
+    """
+    return ScatterNd(shape)(index, update)
 
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod(raise_on_error=True)
+    doctest.testmod(raise_on_error=False)
