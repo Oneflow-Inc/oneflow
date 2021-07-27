71de7a554f6d945113a819466721b01c32e7d4d8
Kevin-XiongC <kevin_xiong1997@outlook.com>
2021-07-27 12:49:49 +0800

    format the code

--- python/oneflow/nn/__init__.py
+++ python/oneflow/nn/__init__.py
@@ -55,6 +55,7 @@ from oneflow.nn.modules.dataset import (
     OFRecordImageDecoderRandomCrop,
     OfrecordRawDecoder,
     OfrecordReader,
+    OFRecordBytesDecoder,
 )
 from oneflow.nn.modules.deconv import ConvTranspose2d
 from oneflow.nn.modules.dropout import Dropout
--- python/oneflow/nn/modules/dataset.py
+++ python/oneflow/nn/modules/dataset.py
@@ -587,6 +587,33 @@ class ImageBatchAlign(Module):
         return self._op(input)[0]
 
 
+class OFRecordBytesDecoder(Module):
+    r"""This operator reads an tensor as bytes. The output might need
+    further decoding process like cv2.imdecode() for images and decode("utf-8")
+    for characters,depending on the downstream task.
+    Args:
+        blob_name: The name of the target feature in OFRecored.
+        name: The name for this component in the graph?
+        input: the Tensor which might be provided by an OfrecordReader.
+    Returns:
+        The result bytes Tensor.
+    """
+
+    def __init__(self, blob_name: str, name: Optional[str] = None):
+        super().__init__()
+        self._op = (
+            flow.builtin_op("ofrecord_bytes_decoder", name)
+            .Input("in")
+            .Output("out")
+            .Attr("name", blob_name)
+            .Build()
+        )
+
+    def forward(self, input):
+        return self._op(input)[0]
+
+
+
 if __name__ == "__main__":
     import doctest
 
