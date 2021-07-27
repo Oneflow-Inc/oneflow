bc235bd91427282012a8a3ef6d01450183dbf824
MARD1NO <359521840@qq.com>
2021-07-27 17:54:57 +0800

    unittest still error

--- python/oneflow/test/modules/test_conv3d.py
+++ python/oneflow/test/modules/test_conv3d.py
@@ -14,81 +14,37 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import unittest
-import oneflow.experimental as flow
+import oneflow as flow
+import oneflow.unittest
 from automated_test_util import *
 
 
-@unittest.skipIf(
-    not flow.unittest.env.eager_execution_enabled(),
-    ".numpy() doesn't work in lazy mode",
-)
+@flow.unittest.skip_unless_1n1d()
 class TestConv3DModule(flow.unittest.TestCase):
-    def test_conv3d(test_case):
-        for device in ["cpu", "cuda"]:
-            test_module_against_pytorch(
-                test_case,
-                "nn.Conv3d",
-                device=device,
-                n=2,
-                extra_generators={
-                    "input": random_tensor(
-                        ndim=5, dim0=2, dim1=4, dim2=4, dim3=6, dim4=6
-                    ),
-                    "in_channels": constant(4),
-                    "out_channels": constant(8),
-                    "kernel_size": random(1, 3),
-                    "stride": random(1, 2),
-                    "padding": random(1, 2),
-                    "dilation": constant(1),
-                    "groups": constant(1),
-                    "padding_mode": constant("zeros"),
-                    "bias": constant(True),
-                },
-            )
+    @autotest(n=2)
+    def test_against_pytorch(test_case):
+        channels = random(1, 6)
+        device = random_device()
+        m = torch.nn.Conv3d(
+            channels,
+            random(1, 6),
+            random(1, 6),
+            stride=random(1, 3) | nothing(),
+            padding=random(1, 3) | nothing(),
+            dilation=random(1, 3) | nothing(),
+            groups=random(1, 3) | nothing(),
+            bias=random() | nothing(),
+            padding_mode=constant("zeros") | nothing(), 
+        )
+        m.train(random())
+        m.to(device)
+        x = random_pytorch_tensor(
+            ndim=5, dim1=channels, dim2=random(1, 8), dim3=random(1, 8)
+        ).to(device)
+        y = m(x)
+        return y
 
-    def test_conv3d_group(test_case):
-        for device in ["cpu", "cuda"]:
-            test_module_against_pytorch(
-                test_case,
-                "nn.Conv3d",
-                device=device,
-                n=1,
-                extra_generators={
-                    "input": random_tensor(
-                        ndim=5, dim0=2, dim1=4, dim2=4, dim3=6, dim4=6
-                    ),
-                    "in_channels": constant(4),
-                    "out_channels": constant(8),
-                    "kernel_size": random(1, 3),
-                    "stride": random(1, 2),
-                    "padding": random(1, 2),
-                    "dilation": constant(1),
-                    "groups": constant(2),
-                    "padding_mode": constant("zeros"),
-                },
-            )
-
-    def test_conv3d_depthwise(test_case):
-        for device in ["cpu", "cuda"]:
-            test_module_against_pytorch(
-                test_case,
-                "nn.Conv3d",
-                n=1,
-                device=device,
-                extra_generators={
-                    "input": random_tensor(
-                        ndim=5, dim0=2, dim1=4, dim2=4, dim3=6, dim4=6
-                    ),
-                    "in_channels": constant(4),
-                    "out_channels": constant(8),
-                    "kernel_size": random(1, 3),
-                    "stride": random(1, 2),
-                    "padding": random(1, 2),
-                    "dilation": constant(1),
-                    "groups": constant(4),
-                    "padding_mode": constant("zeros"),
-                },
-            )
+    
 
 
 if __name__ == "__main__":
