1902ea107ca670690ab30483e25d29462450fc03
caishenghang <64256569+caishenghang@users.noreply.github.com>
2021-07-27 14:47:59 +0800

    Update linalg.py

--- python/oneflow/linalg.py
+++ python/oneflow/linalg.py
@@ -15,4 +15,4 @@ limitations under the License.
 """
 from oneflow.nn.modules.norm import matrix_norm_tensor_op as matrix_norm
 from oneflow.nn.modules.norm import norm_op as norm
-from oneflow.nn.modules.norm import vector_norm_tensor_op as vector_norm
+from oneflow.nn.modules.norm import  vector_norm_tensor_op as vector_norm
