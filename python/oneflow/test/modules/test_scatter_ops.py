beac5127abe83af8ff6623d6d9388e8e545ebe3a
YaoChi <later@usopp.net>
2021-07-28 19:15:03 +0800

    add scatter, scatter_add test cases

---
+++ python/oneflow/test/modules/test_scatter_ops.py
@@ -0,0 +1,64 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+from collections import OrderedDict
+
+import numpy as np
+from test_util import GenArgList
+
+import oneflow as flow
+import oneflow.unittest
+from automated_test_util import *
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestClampModule(flow.unittest.TestCase):
+    @autotest()
+    def test_scatter_random_data(test_case):
+        device = random_device()
+        input = random_pytorch_tensor(ndim=2, dim0=2, dim1=2).to(device)
+        src = random_pytorch_tensor(ndim=2, dim0=2, dim1=2).to(device)
+        index = random_pytorch_tensor(ndim=2, dim0=2, dim1=2, high=2, dtype=int).to(
+            device
+        )
+        y = torch.scatter(input, 0, index, src)
+        return y
+
+    @autotest()
+    def test_scatter_scalar_random_data(test_case):
+        device = random_device()
+        input = random_pytorch_tensor(ndim=2, dim0=2, dim1=2).to(device)
+        src = random_pytorch_tensor(ndim=2, dim0=2, dim1=2).to(device)
+        index = random_pytorch_tensor(ndim=2, dim0=2, dim1=2, high=2, dtype=int).to(
+            device
+        )
+        y = torch.scatter(input, 0, index, 3.14)
+        return y
+
+    @autotest()
+    def test_scatter_add_random_data(test_case):
+        device = random_device()
+        input = random_pytorch_tensor(ndim=2, dim0=2, dim1=2).to(device)
+        src = random_pytorch_tensor(ndim=2, dim0=2, dim1=2).to(device)
+        index = random_pytorch_tensor(ndim=2, dim0=2, dim1=2, high=2, dtype=int).to(
+            device
+        )
+        y = torch.scatter_add(input, 0, index, src)
+        return y
+
+
+if __name__ == "__main__":
+    unittest.main()
