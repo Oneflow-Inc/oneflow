5f984f180776a4573f29aa21767c82b82ebe2abb
wyg1997 <wyg19970408@gmail.com>
2021-07-30 12:32:59 +0800

    feat(ReduceOp): reduce op kernels support 0shape tensor

--- oneflow/user/kernels/reduce_kernel.cpp
+++ oneflow/user/kernels/reduce_kernel.cpp
@@ -33,6 +33,8 @@ class ReduceKernel final : public user_op::OpKernel {
     user_op::Tensor* output_tensor = ctx->Tensor4ArgNameAndIndex("output_tensor", 0);
     user_op::Tensor* tmp_buffer = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
     const auto& axis = ctx->Attr<std::vector<int32_t>>("axis");
+
+    if (input_tensor->shape().elem_cnt() == 0) { return; }
     const Shape& reduced_shape =
         CreateReducedShape(input_tensor->shape(), {axis.begin(), axis.end()});
     NdarrayReduce<device_type, T, BinaryFunc>::Reduce(
