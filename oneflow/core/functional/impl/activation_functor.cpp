7cb86b819e629ad2979cf8ada9a977433e72625a
MARD1NO <359521840@qq.com>
2021-07-30 15:32:48 +0800

    process prelu grad op

--- oneflow/core/functional/impl/activation_functor.cpp
+++ oneflow/core/functional/impl/activation_functor.cpp
@@ -67,6 +67,20 @@ class PReluFunctor : public BinaryFunctor {
   }
 };
 
+class PReluGradFunctor {
+ public:
+  PReluGradFunctor() {
+    op_ = CHECK_JUST(one::OpBuilder("prelu_grad").Input("dy").Input("x").Input("alpha").Output("dx").Output("alpha_diff").Build());
+  }
+  Maybe<TensorTuple> operator()(const std::shared_ptr<Tensor>& dy, 
+                                const std::shared_ptr<Tensor>& x, 
+                                const std::shared_ptr<Tensor>& alpha) const {
+    return OpInterpUtil::Dispatch<one::TensorTuple>(*op_, {dy, x, alpha});
+  }
+ private:
+  std::shared_ptr<OpExpr> op_;
+};
+
 class HardTanhFunctor {
  public:
   HardTanhFunctor() {
@@ -266,6 +280,7 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
   m.add_functor<impl::ReluFunctor>("Relu");
   m.add_functor<impl::ReluGradFunctor>("ReluGrad");
   m.add_functor<impl::PReluFunctor>("PRelu");
+  m.add_functor<impl::PReluGradFunctor>("PReluGrad");
   m.add_functor<impl::HardTanhFunctor>("HardTanh");
   m.add_functor<impl::HardTanhGradFunctor>("HardTanhGrad");
   m.add_functor<impl::EluFunctor>("Elu");
