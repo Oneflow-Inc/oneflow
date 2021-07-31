358766cd23f932152edfd24474f484058b0735a5
simonJJJ <821898965@qq.com>
2021-07-31 18:43:01 +0800

    convert to functional

--- oneflow/core/autograd/gradient_funcs/reduce_ops.cpp
+++ oneflow/core/autograd/gradient_funcs/reduce_ops.cpp
@@ -16,8 +16,6 @@ limitations under the License.
 #include "oneflow/core/framework/attr_map.h"
 #include "oneflow/core/framework/op_expr_grad_function.h"
 #include "oneflow/core/framework/op_builder.h"
-#include "oneflow/core/framework/op_expr.h"
-#include "oneflow/core/framework/op_expr_helper.h"
 #include "oneflow/core/framework/op_interpreter/op_interpreter_util.h"
 #include "oneflow/core/functional/functional.h"
 
@@ -38,16 +36,12 @@ class ReduceSumOp : public OpExprGradFunction<ReduceSumOpInterpState> {
 
  private:
   AttrMap base_attrs_;
-  std::shared_ptr<OpExpr> grad_op_;
 };
 
 Maybe<void> ReduceSumOp::Init(const OpExpr& op) {
   const auto* fw_op_expr = dynamic_cast<const UserOpExpr*>(&op);
   CHECK_NOTNULL_OR_RETURN(fw_op_expr);
   base_attrs_ = MakeAttrMapFromUserOpConf(fw_op_expr->proto());
-  const std::string& op_name = fw_op_expr->op_name();
-  grad_op_ =
-      JUST(op_expr_helper::BroadcastLikeOp(/*broadcast_axes=*/{-1}, GradientOpName(op_name)));
   return Maybe<void>::Ok();
 }
 
@@ -63,10 +57,8 @@ Maybe<void> ReduceSumOp::Apply(const ReduceSumOpInterpState* ctx, const TensorTu
                                TensorTuple* in_grads) const {
   const auto& input = ctx->SavedTensors().at(0);
   const auto& dy = out_grads.at(0);
-  MutableAttrMap attrs;
-  JUST(attrs.SetAttr<std::vector<int32_t>>("broadcast_axes", ctx->axis));
   in_grads->resize(1);
-  in_grads->at(0) = JUST(OpInterpUtil::Dispatch<Tensor>(*grad_op_, {dy, input}, attrs));
+  in_grads->at(0) = JUST(functional::BroadcastLike(dy, input, ctx->axis));
   return Maybe<void>::Ok();
 }
 
