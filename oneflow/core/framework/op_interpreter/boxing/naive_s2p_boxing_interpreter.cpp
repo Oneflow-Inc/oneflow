75d3b255dca7b9e87e7fe9234ff3269438015345
clackhan <han_binbin@163.com>
2021-08-01 23:30:49 +0800

    fix p2s backward bug

---
+++ oneflow/core/framework/op_interpreter/boxing/naive_s2p_boxing_interpreter.cpp
@@ -0,0 +1,43 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "oneflow/core/framework/op_interpreter/boxing/naive_s2p_boxing_interpreter.h"
+#include "oneflow/core/framework/op_interpreter/boxing/naive_b2p_boxing_interpreter.h"
+#include "oneflow/core/framework/op_interpreter/boxing/collective_boxing_interpreter.h"
+#include "oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter_util.h"
+
+namespace oneflow {
+
+Maybe<one::Tensor> NcclS2PBoxingInterpreter::Interpret(
+    const std::shared_ptr<one::Tensor>& input,
+    Symbol<cfg::ParallelDistribution> in_parallel_distribution,
+    Symbol<cfg::ParallelDistribution> out_parallel_distribution,
+    Symbol<ParallelDesc> in_parallel_desc, Symbol<ParallelDesc> out_parallel_desc) const {
+  CHECK_OR_RETURN(EagerBoxingInterpreterUtil::IsBoxingS2P(
+      in_parallel_distribution->sbp_parallel(0), out_parallel_distribution->sbp_parallel(0)));
+  CHECK_EQ_OR_RETURN(in_parallel_desc, out_parallel_desc);
+  cfg::ParallelDistribution mid_parallel_distribution;
+  mid_parallel_distribution.mutable_sbp_parallel()->Add()->mutable_broadcast_parallel();
+  const auto& s2b_interpreter = std::make_shared<NcclCollectiveAllGatherBoxingInterpreter>();
+  const auto& b2p_interpreter = std::make_shared<NaiveB2PBoxingInterpreter>();
+  const auto& mid_tesnor = JUST(s2b_interpreter->Interpret(input, in_parallel_distribution,
+                                                           SymbolOf(mid_parallel_distribution),
+                                                           in_parallel_desc, out_parallel_desc));
+  return JUST(b2p_interpreter->Interpret(mid_tesnor, SymbolOf(mid_parallel_distribution),
+                                         out_parallel_distribution, in_parallel_desc,
+                                         out_parallel_desc));
+}
+
+}  // namespace oneflow
