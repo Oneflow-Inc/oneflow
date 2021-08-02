8c71398f5ba8c8e37deb2d9e05126d248be5663e
QiangX-man <445653859@qq.com>
2021-08-02 17:26:41 +0800

    add CI testCase, update index_select op

--- python/oneflow/nn/modules/index_select.py
+++ python/oneflow/nn/modules/index_select.py
@@ -1,3 +1,18 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import oneflow as flow 
 
 import oneflow as flow
@@ -12,28 +27,28 @@ def shape2list(args):
         out.append(x)
     return out
 
+def _input_args_is_int(args):
+    return all((isinstance(x, int) for x in args))
 
 
 class IndexSelect(Module):
-    def __init__(
-        self, dim: int = 0, sparse_grad: bool = False,
-    ):
-        super.__init__()
+    def __init__(self, dim: int = 0, sparse_grad: bool = False):
+        super().__init__()
         assert sparse_grad is False, "Only support bool = False for now!"
         self.dim = dim
     
     def forward(self, input, index):
-        assert len(index.shape) == 1, "Dimensions of index should equal 1"
-        assert self.dim <= len(input.shape
-        ), "Value of dim is out of range(dim should be 0 or 1)"
+        assert len(index.shape) == 1, "Dimensions of index should be an LongTensor"
+        assert self.dim < len(input.shape
+        ), "Value of dim is out of range"
                
-        index_rshp = shape2list(input.shape)                           # shape list
+        index_rshp = shape2list(input.shape)                           # shape to list
+        assert _input_args_is_int(index.tolist()), "input sizes parameter is not illegal!"
+
         for index_i in index:
             assert index_i < index_rshp[0], \
             "value of index out of range(index shuold lower than the first dimension of input)"                    
         
-        index_gather = index
-      
         index_rshp[self.dim] = 1
         index_gather = index[0].expand(index_rshp)                     # reshape
         for index_i in index[1:]:        
@@ -44,7 +59,7 @@ class IndexSelect(Module):
 
 
 @register_tensor_op("index_select")
-def index_select_op(input, index, dim=0, sparse_grad=False):
+def index_select_op(input, dim, index, sparse_grad=False):
     r"""Select values along an axis specified by `dim`.
 
     :attr:`index` must be an Int32 Tensor with dimension=1.
@@ -54,7 +69,7 @@ def index_select_op(input, index, dim=0, sparse_grad=False):
     Args:
         input (Tensor): the source tensor
         dim (int): the axis along which to index
-        index (Int32Tensor): the indices of elements to select
+        index (LongTensor): the indices of elements to select
     
     For example:
 
@@ -63,11 +78,12 @@ def index_select_op(input, index, dim=0, sparse_grad=False):
         >>> import numpy as np
         >>> input = np.random.randn(3, 4, 5)
         >>> index = flow.tensor([0,1], dtype=flow.int32)
-        >>> output = flow.index_select(flow.Tensor(input), index, dim=1)
+        >>> output = flow.index_select(flow.Tensor(input), dim=1, index)
         >>> output.shape
         flow.Size([3, 2, 3])
 
     """
+    return IndexSelect(dim, sparse_grad)(input, index)
 
 if __name__ == '__main__':
     
---
+++ python/oneflow/test/modules/test_index_select.py
@@ -0,0 +1,52 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+import oneflow as flow
+import unittest
+
+from automated_test_util import *
+
+
+
+@autotest()
+def test_index_select_by_cpu(test_case):
+    axis = random(0,1).to(int)  
+    index = random_pytorch_tensor(ndim=1, low=0, high=1, dtype=int)
+
+    x = random_pytorch_tensor(ndim=2,dim0=2,dim1=2)  
+    y = torch.index_select(input=x,dim=axis,index=index)   
+     
+    return y
+
+def test_index_select_by_gpu(test_case):
+    device = random_device()
+
+    #test 4 dimensions tensor
+    axis = radom(0,4).to(int)
+    index= random_pytorch_tensor(ndim=1,low=0, high=dim[axis], dtype=int).to(device)
+
+    dim = []
+    for i in range(0,4):
+        dim.append(random(3,6).to(int))
+
+    x = random_pytorch_tensor(ndim=4,dim0=dim[0],dim1=dim[1],dim2=dim[2],dim3=dim[3]).to(device)  
+    y = torch.index_select(input=x,dim=axis,index=index)  
+
+    return y
+
+if __name__ == "__main__":
+   unittest.main() 
