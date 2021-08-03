9a11d91e6d154ad292d0c5cdb7af447dd33d53b3
clackhan <han_binbin@163.com>
2021-08-03 09:49:34 +0800

    refine to_consistent api

--- python/oneflow/nn/modules/consistent_cast.py
+++ python/oneflow/nn/modules/consistent_cast.py
@@ -35,7 +35,7 @@ class ToConsistent(Module):
 
 
 @register_tensor_op("to_consistent")
-def to_consistent_op(input, placement, sbp, shape=None):
+def to_consistent_op(input, placement=None, sbp=None, shape=None):
     """Cast a local tensor to consistent tensor or cast a
     consistent tensor to another consistent tensor with 
     different sbp or placement
@@ -43,8 +43,8 @@ def to_consistent_op(input, placement, sbp, shape=None):
 
     Args:
         input (Tensor): the input tensor.
-        placement (flow.placement, optional) – the desired placement of returned consistent tensor. Default: if None, the returned tensor is local one using the argument `device`.
-        sbp (flow.sbp.sbp or tuple of flow.sbp.sbp, optional) – the desired sbp descriptor of returned consistent tensor. Default: if None, the returned tensor is local one using the argument `device`.
+        placement (flow.placement, optional) – the desired placement of returned consistent tensor. Default: if None, the input tensor must be consistent one and use its own placement.
+        sbp (flow.sbp.sbp or tuple of flow.sbp.sbp, optional) – the desired sbp descriptor of returned consistent tensor. Default: if None, the input tensor must be consistent one and use its own sbp.
         shape (flow.Size, optional) the logical shape of returned consistent tensor.
 
     For example:
@@ -62,6 +62,12 @@ def to_consistent_op(input, placement, sbp, shape=None):
     """
     if isinstance(sbp, flow.sbp.sbp):
         sbp = (sbp,)
+    if placement is None or sbp is None:
+        assert (
+            input.is_consistent
+        ), "Converting a local tensor to consistent tensor must have placement and sbp parameters!"
+        placement = input.placement if placement is None else placement
+        sbp = input.sbp if sbp is None else sbp
     return flow.F.to_consistent(input, placement, sbp, shape)
 
 
