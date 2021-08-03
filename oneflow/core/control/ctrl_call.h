4aec3f60a981968b976d9ffc34254f722f1664fe
leaves-zwx <kunta0932@gmail.com>
2021-08-03 22:04:14 +0800

    rm debug code

--- oneflow/core/control/ctrl_call.h
+++ oneflow/core/control/ctrl_call.h
@@ -56,7 +56,6 @@ class CtrlCall final : public CtrlCallIf {
   void Process() override {
     switch (status_) {
       case Status::kBeforeHandleRequest: {
-        LOG(ERROR) << "CtrlCall " << ctrl_method << " processed with status kBeforeHandleRequest";
         request_handler_();
         return;
       }
