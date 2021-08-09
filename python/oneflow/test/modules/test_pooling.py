72eef606909757dbce0864ca81fd16361acb1766
Guikarist <guikarist@gmail.com>
2021-08-09 09:24:54 +0800

    Choose better generators for `nn.MaxPoolXd`

--- python/oneflow/test/modules/test_pooling.py
+++ python/oneflow/test/modules/test_pooling.py
@@ -18,6 +18,7 @@ import unittest
 import oneflow as flow
 import oneflow.unittest
 from automated_test_util import *
+from oneflow.nn.common_types import _size_1_t, _size_2_t, _size_3_t
 
 
 @flow.unittest.skip_unless_1n1d()
@@ -25,47 +26,45 @@ class TestMaxPooling(flow.unittest.TestCase):
     @autotest()
     def test_maxpool1d_with_random_data(test_case):
         m = torch.nn.MaxPool1d(
-            kernel_size=random(2, 3).to(int),
-            stride=random(1, 3).to(int),
-            padding=random(0, 1).to(int),
-            dilation=random(1, 2).to(int),
+            kernel_size=random(4, 6).to(_size_1_t),
+            stride=random(1, 3).to(_size_1_t),
+            padding=random(0, 2).to(_size_1_t),
+            dilation=random(1, 3).to(_size_1_t),
         )
         m.train(random())
         device = random_device()
         m.to(device)
-        x = random_pytorch_tensor(ndim=3, dim0=random(25, 46), dim1=random(33, 55), dim2=random(1, 3)).to(device)
+        x = random_pytorch_tensor(ndim=3, dim2=random(10, 12)).to(device)
         y = m(x)
         return y
 
     @autotest()
     def test_maxpool2d_with_random_data(test_case):
         m = torch.nn.MaxPool2d(
-            kernel_size=random(2, 3).to(int),
-            stride=random(1, 3).to(int),
-            padding=random(0, 1).to(int),
-            dilation=random(1, 2).to(int),
+            kernel_size=random(4, 6).to(_size_2_t),
+            stride=random(1, 3).to(_size_2_t),
+            padding=random(0, 2).to(_size_2_t),
+            dilation=random(1, 3).to(_size_2_t),
         )
         m.train(random())
         device = random_device()
         m.to(device)
-        x = random_pytorch_tensor(ndim=4, dim0=random(2, 4), dim1=random(3, 5), dim2=random(25, 46),
-                                  dim3=random(25, 46)).to(device)
+        x = random_pytorch_tensor(ndim=4, dim2=random(10, 12), dim3=random(10, 12)).to(device)
         y = m(x)
         return y
 
     @autotest()
     def test_maxpool3d_with_random_data(test_case):
         m = torch.nn.MaxPool3d(
-            kernel_size=random(2, 3).to(int),
-            stride=random(1, 3).to(int),
-            padding=random(0, 1).to(int),
-            dilation=random(1, 2).to(int),
+            kernel_size=random(4, 6).to(_size_3_t),
+            stride=random(1, 3).to(_size_3_t),
+            padding=random(0, 2).to(_size_3_t),
+            dilation=random(1, 3).to(_size_3_t),
         )
         m.train(random())
         device = random_device()
         m.to(device)
-        x = random_pytorch_tensor(ndim=5, dim0=random(2, 4), dim1=random(3, 5), dim2=random(5, 6),
-                                  dim3=random(25, 46), dim4=random(25, 46)).to(device)
+        x = random_pytorch_tensor(ndim=5, dim2=random(10, 12), dim3=random(10, 12), dim4=random(10, 12)).to(device)
         y = m(x)
         return y
 
