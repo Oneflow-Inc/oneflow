2f659139a695826feff82924c72a48bce492b68d
BBuf <1182563586@qq.com>
2021-08-10 21:09:58 +0800

    add prelu gpu kernel

---
+++ oneflow/user/kernels/prelu_kernel.cu
@@ -0,0 +1,130 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/core/ndarray/ndarray_util.h"
+
+namespace oneflow {
+
+namespace {
+
+template<typename T>
+__global__ void PReluForwardGpu(const int32_t elem_cnt, const int32_t alpha_size,
+                                const int32_t inner_size, const T* x, const T* alpha, T* y) {
+  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
+    const T x_i = x[i];
+    const T alpha_i = alpha[(i / inner_size) % alpha_size];
+    y[i] = x_i > 0 ? x_i : x_i * alpha_i;
+  }
+}
+
+template<typename T>
+__global__ void PReluBackwardGpu(const int32_t elem_cnt, const int32_t alpha_size,
+                                 const int32_t inner_size, const T* x, const T* alpha, const T* dy,
+                                 T* dx, T* alpha_diff) {
+  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
+    const T x_i = x[i];
+    const T dy_i = dy[i];
+    const T alpha_i = alpha[(i / inner_size) % alpha_size];
+    T dx_i = 0;
+    T alpha_diff_i = 0;
+    if (x_i > 0) {
+      dx_i = dy_i;
+      alpha_diff_i = 0;
+    } else {
+      dx_i = dy_i * alpha_i;
+      alpha_diff_i = dy_i * x_i;
+    }
+    dx[i] = dx_i;
+    alpha_diff[i] = alpha_diff_i;
+  }
+}
+
+}  // namespace
+
+template<typename T>
+class GpuPReluKernel final : public user_op::OpKernel {
+ public:
+  GpuPReluKernel() = default;
+  ~GpuPReluKernel() = default;
+
+ private:
+  void Compute(user_op::KernelComputeContext* ctx) const override {
+    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    const user_op::Tensor* alpha = ctx->Tensor4ArgNameAndIndex("alpha", 0);
+    user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
+    const int32_t elem_cnt = x->shape().elem_cnt();
+    const int32_t alpha_size = alpha->shape().elem_cnt();
+    int outer_size = x->shape().At(0);
+    for (int i = 2; i < x->shape().NumAxes(); i++) outer_size *= x->shape().At(i);
+    const int32_t inner_size = elem_cnt / outer_size / alpha_size;
+    PReluForwardGpu<T><<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0,
+                         ctx->device_ctx()->cuda_stream()>>>(
+        elem_cnt, alpha_size, inner_size, x->dptr<T>(), alpha->dptr<T>(), y->mut_dptr<T>());
+  }
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_GPU_PRELU_KERNEL(dtype)                                              \
+  REGISTER_USER_KERNEL("prelu")                                                       \
+      .SetCreateFn<GpuPReluKernel<dtype>>()                                           \
+      .SetIsMatchedHob((user_op::HobDeviceTag() == "cpu")                             \
+                       & (user_op::HobDataType("y", 0) == GetDataType<dtype>::value)) \
+      .SetInferTmpSizeFn([](user_op::InferContext* ctx) {                             \
+        const Shape& in_shape = ctx->InputShape("x", 0);                              \
+        return GetCudaAlignedSize(in_shape.elem_cnt() * sizeof(dtype));               \
+      });
+
+REGISTER_GPU_PRELU_KERNEL(float)
+REGISTER_GPU_PRELU_KERNEL(double)
+
+template<typename T>
+class CpuPReluGradKernel final : public user_op::OpKernel {
+ public:
+  CpuPReluGradKernel() = default;
+  ~CpuPReluGradKernel() = default;
+
+ private:
+  void Compute(user_op::KernelComputeContext* ctx) const override {
+    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    const user_op::Tensor* alpha = ctx->Tensor4ArgNameAndIndex("alpha", 0);
+    const user_op::Tensor* dy = ctx->Tensor4ArgNameAndIndex("dy", 0);
+    user_op::Tensor* dx = ctx->Tensor4ArgNameAndIndex("dx", 0);
+    user_op::Tensor* alpha_diff = ctx->Tensor4ArgNameAndIndex("alpha_diff", 0);
+
+    const int32_t elem_cnt = x->shape().elem_cnt();
+    const int32_t alpha_size = alpha->shape().elem_cnt();
+    int outer_size = x->shape().At(0);
+    for (int i = 2; i < x->shape().NumAxes(); i++) outer_size *= x->shape().At(i);
+    const int32_t inner_size = elem_cnt / outer_size / alpha_size;
+
+    PReluBackwardGpu<T><<<BlocksNum4ThreadsNum(elem_cnt), kCudaThreadsNumPerBlock, 0,
+                          ctx->device_ctx()->cuda_stream()>>>(
+        elem_cnt, alpha_size, inner_size, x->dptr<T>(), alpha->dptr<T>(), dy->dptr<T>(),
+        dx->mut_dptr<T>(), alpha_diff->mut_dptr<T>());
+  }
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_GPU_PRELU_GRAD_KERNEL(dtype)             \
+  REGISTER_USER_KERNEL("prelu_grad")                      \
+      .SetCreateFn<CpuPReluGradKernel<dtype>>()           \
+      .SetIsMatchedHob((user_op::HobDeviceTag() == "cpu") \
+                       & (user_op::HobDataType("dx", 0) == GetDataType<dtype>::value));
+
+REGISTER_GPU_PRELU_GRAD_KERNEL(float)
+REGISTER_GPU_PRELU_GRAD_KERNEL(double)
+
+}  // namespace oneflow
