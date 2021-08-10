4315009b873b524421850c443041781c416d55a6
Ldpe2G <liangdepeng@gmail.com>
2021-08-10 12:11:39 +0800

    add sgd graph clip grad test

---
+++ python/oneflow/test/graph/test_graph_sgd_optim.py
@@ -0,0 +1,170 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+import os
+import sys
+
+sys.path.append("../modules/")
+
+import numpy as np
+
+from collections import OrderedDict
+
+from test_util import GenArgDict
+
+import oneflow as flow
+
+
+def _clip_grad_norm_np(np_grad, max_norm, norm_type):
+    max_norm = float(max_norm)
+    norm_type = float(norm_type)
+    if norm_type == float("inf"):
+        total_norm = np.max(np.abs(np_grad))
+    if norm_type == float("-inf"):
+        total_norm = np.min(np.abs(np_grad))
+    elif norm_type == 0:
+        total_norm = np.sum(np.stack([np.sum(np_grad != 0)]) != 0)
+    else:
+        total_norm = np_grad
+        for i in range(np_grad.ndim, 0, -1):
+            total_norm = np.linalg.norm(total_norm, norm_type, axis=i - 1)
+    clip_coef = max_norm / (total_norm + 1e-6)
+    if clip_coef < 1:
+        np_grad = np_grad * clip_coef
+    return total_norm, np_grad
+
+
+def compare_with_numpy_sgd_clip_grad(
+    test_case,
+    device,
+    x_shape,
+    learning_rate,
+    momentum,
+    weight_decay,
+    clip_grad_max_norm,
+    clip_grad_norm_type,
+    train_iters,
+):
+    random_grad_seq = []
+    for _ in range(train_iters):
+        random_grad_seq.append(np.random.uniform(size=x_shape).astype(np.float32))
+    init_value = np.random.uniform(size=x_shape).astype(np.float32)
+
+    class CustomModule(flow.nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.para0 = flow.nn.Parameter(
+                flow.Tensor(init_value, device=flow.device(device))
+            )
+
+        def forward(self, mask):
+            return self.para0 * mask
+
+    simp_module = CustomModule()
+    simp_module.to(device)
+    simp_module.train()
+
+    sgd0 = flow.optim.SGD(
+        [
+            {
+                "params": simp_module.parameters(),
+                "lr": learning_rate,
+                "momentum": momentum,
+                "weight_decay": weight_decay,
+                "clip_grad_max_norm": clip_grad_max_norm,
+                "clip_grad_norm_type": clip_grad_norm_type,
+            }
+        ]
+    )
+
+    class CustomSGDGraph(flow.nn.Graph):
+        def __init__(self):
+            super().__init__()
+            self.m = simp_module
+            self.add_optimizer("sgd", sgd0)
+
+        def build(self, mask_tensor):
+            loss = flow.sum(self.m(mask_tensor))
+            loss.backward()
+            return loss
+
+    of_res_list = []
+    sgd_graph = CustomSGDGraph()
+    for i in range(train_iters):
+        mask_tensor = flow.Tensor(
+            random_grad_seq[i], requires_grad=False, device=flow.device(device)
+        )
+        sgd_x = sgd_graph(mask_tensor)
+        of_res_list.append(simp_module.para0.numpy())
+
+    np_res_list = []
+
+    def train_by_numpy():
+        x = init_value
+        vt = np.zeros_like(x)
+
+        def np_train_one_iter(grad):
+            norm, grad = _clip_grad_norm_np(
+                grad, clip_grad_max_norm, clip_grad_norm_type
+            )
+            grad = grad + weight_decay * x
+            v = momentum * vt - learning_rate * grad
+            param = x + v
+            return (param, v)
+
+        for i in range(train_iters):
+            (x, vt) = np_train_one_iter(random_grad_seq[i])
+            np_res_list.append(x)
+
+    train_by_numpy()
+    for np_res, of_res in zip(np_res_list, of_res_list):
+        test_case.assertTrue(np.allclose(np_res, of_res, rtol=0.001, atol=0.001))
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestCpuSGD(flow.unittest.TestCase):
+    def test_sgd_with_clip_grad(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["device"] = ["cpu", "cuda"]
+        arg_dict["x_shape"] = [(10,)]
+        arg_dict["learning_rate"] = [1, 0.1]
+        arg_dict["momentum"] = [0.0, 0.9]
+        arg_dict["weight_decay"] = [
+            0.0
+        ]  # NOTE(Liang Depeng): test will fail when weight_decay > 0
+        arg_dict["clip_grad_max_norm"] = [1.0]
+        arg_dict["clip_grad_norm_type"] = [2.0]
+        arg_dict["train_iters"] = [10]
+
+        for arg in GenArgDict(arg_dict):
+            print(arg)
+            compare_with_numpy_sgd_clip_grad(test_case, **arg)
+
+
+if __name__ == "__main__":
+    unittest.main()
