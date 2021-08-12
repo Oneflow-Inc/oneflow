9e1891cf1be0e6a24dccc65e36dd4d3631e6b436
lambda7xx <lambda7xx@gmail.com>
2021-08-12 15:06:23 +0800

    fix the bug of the messagePool in IBVerbsQP

--- oneflow/core/comm_network/ibverbs/ibverbs_qp.h
+++ oneflow/core/comm_network/ibverbs/ibverbs_qp.h
@@ -143,7 +143,7 @@ class IBVerbsQP final {
   IBVerbsQP() = delete;
   IBVerbsQP(ibv_context*, ibv_pd*, uint8_t port_num, ibv_cq* send_cq, ibv_cq* recv_cq);
   IBVerbsQP(ibv_context *, ibv_pd*, uint8_t port_num, ibv_cq * send_cq, ibv_cq* recv_cq,
-            std::shared_ptr<MessagePool> recv_msg_buf, std::shared_ptr<MessagePool> send_msg_buf);
+            MessagePool * recv_msg_buf, MessagePool * send_msg_buf);
   ~IBVerbsQP();
 
   uint32_t qp_num() const { return qp_->qp_num; }
