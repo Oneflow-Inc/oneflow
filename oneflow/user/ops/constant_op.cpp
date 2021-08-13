025e7c9d1fe95aacff9394171e0ea61f5399c34b
leaves-zwx <kunta0932@gmail.com>
2021-08-13 23:10:36 +0800

    clear code

--- oneflow/user/ops/constant_op.cpp
+++ oneflow/user/ops/constant_op.cpp
@@ -21,6 +21,45 @@ limitations under the License.
 
 namespace oneflow {
 
+namespace {
+
+Maybe<void> InferNdSbp4Constant(user_op::InferParallelDistributionFnContext* ctx) {
+  const Shape& hierarchy = ctx->parallel_hierarchy();
+  const auto& sbp_str_list = ctx->user_op_conf().attr<std::vector<std::string>>("nd_sbp");
+
+  // constant may have tick inputs whose sbp should be broadcast
+  for (const auto& arg_pair : ctx->inputs()) {
+    cfg::ParallelDistribution* input_nd_sbp =
+        ctx->ParallelDistribution4ArgNameAndIndex(arg_pair.first, arg_pair.second);
+    FOR_RANGE(int, i, 0, hierarchy.NumAxes()) {
+      input_nd_sbp->add_sbp_parallel()->mutable_broadcast_parallel();
+    }
+  }
+
+  const auto& outputs = ctx->outputs();
+  CHECK_EQ_OR_RETURN(outputs.size(), 1);
+  cfg::ParallelDistribution* output_nd_sbp =
+      ctx->ParallelDistribution4ArgNameAndIndex(outputs[0].first, outputs[0].second);
+  if (sbp_str_list.size() == 0) {
+    // the default sbp of constant's output should be broadcast
+    FOR_RANGE(int, i, 0, hierarchy.NumAxes()) {
+      output_nd_sbp->add_sbp_parallel()->mutable_broadcast_parallel();
+    }
+  } else {
+    CHECK_EQ_OR_RETURN(sbp_str_list.size(), hierarchy.NumAxes());
+    for (const std::string& sbp_str : sbp_str_list) {
+      cfg::SbpParallel sbp;
+      CHECK_OR_RETURN(ParseSbpParallelFromString(sbp_str, &sbp));
+      CHECK_OR_RETURN(sbp.has_split_parallel() || sbp.has_broadcast_parallel());
+      *output_nd_sbp->add_sbp_parallel() = sbp;
+    }
+  }
+
+  return Maybe<void>::Ok();
+}
+
+}  // namespace
+
 REGISTER_NO_GRAD_USER_OP("constant")
     .Output("out")
     .SetOutputBufferNum(1)
@@ -31,58 +70,14 @@ REGISTER_NO_GRAD_USER_OP("constant")
     .Attr<Shape>("shape")
     .Attr<std::vector<std::string>>("nd_sbp")
     .SetTensorDescInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      Shape* out_shape = ctx->OutputShape("out", 0);
-      const Shape& shape = ctx->Attr<Shape>("shape");
-      DimVector dim_vec;
-      if (shape.NumAxes() > 0) {
-        dim_vec.insert(dim_vec.end(), shape.dim_vec().cbegin(), shape.dim_vec().cend());
-      }
-      *out_shape = Shape(dim_vec);
+      *ctx->OutputShape("out", 0) = Shape(ctx->Attr<Shape>("shape").dim_vec());
       return Maybe<void>::Ok();
     })
     .SetDataTypeInferFn([](user_op::InferContext* ctx) -> Maybe<void> {
-      auto dtype = ctx->Attr<DataType>("dtype");
-      *ctx->OutputDType("out", 0) = dtype;
+      *ctx->OutputDType("out", 0) = ctx->Attr<DataType>("dtype");
       return Maybe<void>::Ok();
     })
-    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> {
-      return Maybe<void>::Ok();
-    })
-    .SetParallelDistributionInferFn(
-        [](user_op::InferParallelDistributionFnContext* ctx) -> Maybe<void> {
-          const Shape& hierarchy = ctx->parallel_hierarchy();
-          const auto& dist_conf = ctx->user_op_conf().attr<std::vector<std::string>>("nd_sbp");
-
-          // constant may have tick inputs whose sbp should be broadcast
-          for (const auto& arg_pair : ctx->inputs()) {
-            cfg::ParallelDistribution* input_dist =
-                ctx->ParallelDistribution4ArgNameAndIndex(arg_pair.first, arg_pair.second);
-            FOR_RANGE(int, i, 0, hierarchy.NumAxes()) {
-              input_dist->add_sbp_parallel()->mutable_broadcast_parallel();
-            }
-          }
-
-          const auto& outputs = ctx->outputs();
-          CHECK_EQ_OR_RETURN(outputs.size(), 1);
-          cfg::ParallelDistribution* output_dist =
-              ctx->ParallelDistribution4ArgNameAndIndex(outputs[0].first, outputs[0].second);
-          if (dist_conf.size() == 0) {
-            // the default sbp of constant's output should be broadcast
-            FOR_RANGE(int, i, 0, hierarchy.NumAxes()) {
-              output_dist->add_sbp_parallel()->mutable_broadcast_parallel();
-            }
-          } else {
-            CHECK_EQ_OR_RETURN(dist_conf.size(), hierarchy.NumAxes());
-            for (const std::string& sbp_str : dist_conf) {
-              cfg::SbpParallel sbp_parallel;
-              CHECK_OR_RETURN(ParseSbpParallelFromString(sbp_str, &sbp_parallel));
-              CHECK_OR_RETURN(sbp_parallel.has_split_parallel()
-                              || sbp_parallel.has_broadcast_parallel());
-              *output_dist->add_sbp_parallel() = sbp_parallel;
-            }
-          }
-
-          return Maybe<void>::Ok();
-        });
+    .SetGetSbpFn([](user_op::SbpContext* ctx) -> Maybe<void> { return Maybe<void>::Ok(); })
+    .SetParallelDistributionInferFn(InferNdSbp4Constant);
 
 }  // namespace oneflow
