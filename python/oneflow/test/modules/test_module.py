c7c34a6ec3778babde98314bf4f9047075fd213c
simonJJJ <821898965@qq.com>
2021-08-15 13:38:18 +0800

    recover module test

--- python/oneflow/test/modules/test_module.py
+++ python/oneflow/test/modules/test_module.py
@@ -32,89 +32,89 @@ def np_relu(np_arr):
 
 @flow.unittest.skip_unless_1n1d()
 class TestModule(flow.unittest.TestCase):
-    # def test_nested_module(test_case):
-    #     class CustomModule(flow.nn.Module):
-    #         def __init__(self):
-    #             super().__init__()
-    #             self.relu = flow.nn.ReLU()
+    def test_nested_module(test_case):
+        class CustomModule(flow.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.relu = flow.nn.ReLU()
 
-    #         def forward(self, x):
-    #             return self.relu(x)
+            def forward(self, x):
+                return self.relu(x)
 
-    #     m = CustomModule()
-    #     x = flow.Tensor(2, 3)
-    #     flow.nn.init.uniform_(x, a=-1.0, b=1.0)
-    #     y = m(x)
-    #     test_case.assertTrue(np.array_equal(np_relu(x.numpy()), y.numpy()))
+        m = CustomModule()
+        x = flow.Tensor(2, 3)
+        flow.nn.init.uniform_(x, a=-1.0, b=1.0)
+        y = m(x)
+        test_case.assertTrue(np.array_equal(np_relu(x.numpy()), y.numpy()))
 
-    # def test_relu(test_case):
-    #     relu = flow.nn.ReLU()
-    #     x = flow.Tensor(2, 3)
-    #     flow.nn.init.uniform_(x, a=-1.0, b=1.0)
-    #     y = relu(x)
-    #     test_case.assertTrue(np.array_equal(np_relu(x.numpy()), y.numpy()))
+    def test_relu(test_case):
+        relu = flow.nn.ReLU()
+        x = flow.Tensor(2, 3)
+        flow.nn.init.uniform_(x, a=-1.0, b=1.0)
+        y = relu(x)
+        test_case.assertTrue(np.array_equal(np_relu(x.numpy()), y.numpy()))
 
-    # def test_load_state_dict(test_case):
-    #     class CustomModule(flow.nn.Module):
-    #         def __init__(self):
-    #             super().__init__()
-    #             self.w = flow.nn.Parameter(flow.Tensor(2, 3))
+    def test_load_state_dict(test_case):
+        class CustomModule(flow.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.w = flow.nn.Parameter(flow.Tensor(2, 3))
 
-    #         def forward(self, x):
-    #             return self.w
+            def forward(self, x):
+                return self.w
 
-    #     m = CustomModule()
-    #     ones = np.ones((2, 3), dtype=np.float32)
-    #     m.load_state_dict({"w": ones})
-    #     x = flow.Tensor(2, 3)
-    #     y = m(x).numpy()
-    #     test_case.assertTrue(np.array_equal(y, ones))
+        m = CustomModule()
+        ones = np.ones((2, 3), dtype=np.float32)
+        m.load_state_dict({"w": ones})
+        x = flow.Tensor(2, 3)
+        y = m(x).numpy()
+        test_case.assertTrue(np.array_equal(y, ones))
 
-    # def test_state_dict(test_case):
-    #     class CustomModule(flow.nn.Module):
-    #         def __init__(self, param1, param2):
-    #             super().__init__()
-    #             self.param1 = param1
-    #             self.param2 = param2
+    def test_state_dict(test_case):
+        class CustomModule(flow.nn.Module):
+            def __init__(self, param1, param2):
+                super().__init__()
+                self.param1 = param1
+                self.param2 = param2
 
-    #     tensor0 = flow.nn.Parameter(flow.Tensor(2, 3))
-    #     tensor1 = flow.nn.Parameter(flow.Tensor(2, 3))
-    #     sub_module = CustomModule(tensor0, tensor1)
-    #     m = CustomModule(tensor1, sub_module)
-    #     state_dict = m.state_dict()
-    #     test_case.assertEqual(
-    #         state_dict,
-    #         {"param2.param1": tensor0, "param2.param2": tensor1, "param1": tensor1},
-    #     )
+        tensor0 = flow.nn.Parameter(flow.Tensor(2, 3))
+        tensor1 = flow.nn.Parameter(flow.Tensor(2, 3))
+        sub_module = CustomModule(tensor0, tensor1)
+        m = CustomModule(tensor1, sub_module)
+        state_dict = m.state_dict()
+        test_case.assertEqual(
+            state_dict,
+            {"param2.param1": tensor0, "param2.param2": tensor1, "param1": tensor1},
+        )
 
-    # def test_parameter(test_case):
-    #     shape = (3, 4)
-    #     t = flow.Tensor(*shape)
-    #     p = flow.nn.Parameter(t)
-    #     test_case.assertEqual(type(p), flow.nn.Parameter)
-    #     test_case.assertEqual(p.shape, shape)
+    def test_parameter(test_case):
+        shape = (3, 4)
+        t = flow.Tensor(*shape)
+        p = flow.nn.Parameter(t)
+        test_case.assertEqual(type(p), flow.nn.Parameter)
+        test_case.assertEqual(p.shape, shape)
 
-    # def test_module_forward(test_case):
-    #     class CustomModule(flow.nn.Module):
-    #         def __init__(self, w):
-    #             super().__init__()
-    #             self.w = w
+    def test_module_forward(test_case):
+        class CustomModule(flow.nn.Module):
+            def __init__(self, w):
+                super().__init__()
+                self.w = w
 
-    #         def forward(self, x):
-    #             return x + self.w
+            def forward(self, x):
+                return x + self.w
 
-    #     m = CustomModule(5)
-    #     test_case.assertEqual(m(1), 6)
-    #     m = CustomModule(4)
-    #     test_case.assertEqual(m(3), 7)
+        m = CustomModule(5)
+        test_case.assertEqual(m(1), 6)
+        m = CustomModule(4)
+        test_case.assertEqual(m(3), 7)
 
-    # def test_train_eval(test_case):
-    #     m = flow.nn.Module()
-    #     test_case.assertEqual(m.training, True)
-    #     m.train()
-    #     test_case.assertEqual(m.training, True)
-    #     m.eval()
-    #     test_case.assertEqual(m.training, False)
+    def test_train_eval(test_case):
+        m = flow.nn.Module()
+        test_case.assertEqual(m.training, True)
+        m.train()
+        test_case.assertEqual(m.training, True)
+        m.eval()
+        test_case.assertEqual(m.training, False)
 
     def test_module_setattr(test_case):
         class CustomModule(flow.nn.Module):
