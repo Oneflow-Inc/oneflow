4e444a82ee832c7047fad798a7db06c6889041c2
QiangX-man <445653859@qq.com>
2021-08-16 16:27:21 +0800

    add test_prod

---
+++ python/oneflow/test/modules/test_prod.py
@@ -0,0 +1,35 @@
+import unittest
+from automated_test_util import *
+import oneflow as flow
+import oneflow.unittest
+
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestReduceProd(flow.unittest.TestCase):
+    @autotest
+    def test_reduce_prod_without_dim(test_case):
+        device = random_device()
+
+        ndim = random(1, 5).to(int)
+        x = random_pytorch_tensor(ndim=ndim).to(device)
+        y = torch.prod(x)
+
+        return y
+
+    @autotest
+    def test_reduce_prod_with_dim(test_case):
+        device = random_device()
+
+        ndim = random(1, 5).to(int)
+        x = random_pytorch_tensor(ndim=ndim).to(device)
+
+        dim = random(0, ndim).to(int)
+
+        y = torch.prod(x, dim)
+
+        return y
+
+
+if __name__ == "__main__":
+    unittest.main()
