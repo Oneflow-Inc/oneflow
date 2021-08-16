c461bf6f03ca01884a41cbf846e58f1e8a345a1c
PragmaTwice <i@twice.moe>
2021-08-16 21:54:49 +0800

    xrt: clang format

--- oneflow/xrt/fix_ostream_nullptr.h
+++ oneflow/xrt/fix_ostream_nullptr.h
@@ -19,22 +19,29 @@ limitations under the License.
 
 #include <iostream>
 
-// This header fix the overload error below, while using glog instead of tensorflow/core/platform/default/logging.h:
-// In file included from oneflow/oneflow/core/common/util.h:22,
+// This header fix the overload error below, while using glog instead of
+// tensorflow/core/platform/default/logging.h: In file included from
+// oneflow/oneflow/core/common/util.h:22,
 //                  from oneflow/oneflow/xrt/xla/xla_allocator.h:19,
 //                  from oneflow/oneflow/xrt/xla/xla_resource_manager.h:25,
 //                  from oneflow/oneflow/xrt/xla/xla_resource_manager.cpp:18:
-// oneflow/build/third_party_install/glog/install/include/glog/logging.h: In instantiation of ‘void google::MakeCheckOpValueString(std::ostream*, const T&) [with T = std::nullptr_t; std::ostream = std::basic_ostream<char>]’:
-// oneflow/build/third_party_install/glog/install/include/glog/logging.h:693:25:   required from ‘std::string* google::MakeCheckOpString(const T1&, const T2&, const char*) [with T1 = std::nullptr_t; T2 = xla::HloComputation*; std::string = std::__cxx11::basic_string<char>]’
-// oneflow/build/third_party_install/glog/install/include/glog/logging.h:718:1:   required from ‘std::string* google::Check_NEImpl(const T1&, const T2&, const char*) [with T1 = std::nullptr_t; T2 = xla::HloComputation*; std::string = std::__cxx11::basic_string<char>]’
-// oneflow/build/third_party_install/tensorflow/include/tensorflow_inc/tensorflow/compiler/xla/service/hlo_module.h:116:5:   required from here
-// oneflow/build/third_party_install/glog/install/include/glog/logging.h:638:9: error: ambiguous overload for ‘operator<<’ (operand types are ‘std::ostream’ {aka ‘std::basic_ostream<char>’} and ‘std::nullptr_t’)
+// oneflow/build/third_party_install/glog/install/include/glog/logging.h: In instantiation of ‘void
+// google::MakeCheckOpValueString(std::ostream*, const T&) [with T = std::nullptr_t; std::ostream =
+// std::basic_ostream<char>]’:
+// oneflow/build/third_party_install/glog/install/include/glog/logging.h:693:25:   required from
+// ‘std::string* google::MakeCheckOpString(const T1&, const T2&, const char*) [with T1 =
+// std::nullptr_t; T2 = xla::HloComputation*; std::string = std::__cxx11::basic_string<char>]’
+// oneflow/build/third_party_install/glog/install/include/glog/logging.h:718:1:   required from
+// ‘std::string* google::Check_NEImpl(const T1&, const T2&, const char*) [with T1 = std::nullptr_t;
+// T2 = xla::HloComputation*; std::string = std::__cxx11::basic_string<char>]’
+// oneflow/build/third_party_install/tensorflow/include/tensorflow_inc/tensorflow/compiler/xla/service/hlo_module.h:116:5:
+// required from here oneflow/build/third_party_install/glog/install/include/glog/logging.h:638:9:
+// error: ambiguous overload for ‘operator<<’ (operand types are ‘std::ostream’ {aka
+// ‘std::basic_ostream<char>’} and ‘std::nullptr_t’)
 //   638 |   (*os) << v;
 //       |   ~~~~~~^~~~
 namespace std {
-    inline ostream& operator<<(ostream& os, nullptr_t) {
-        return os << "nullptr";
-    }
-}
+inline ostream& operator<<(ostream& os, nullptr_t) { return os << "nullptr"; }
+}  // namespace std
 
-#endif //ONEFLOW_XRT_FIX_OSTREAM_NULLPTR_H_
+#endif  // ONEFLOW_XRT_FIX_OSTREAM_NULLPTR_H_
