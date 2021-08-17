9669c726d6b3291036213379fcaad62949dd018f
GehangZhang <649975329@qq.com>
2021-08-17 12:28:26 +0800

    fix some bugs

--- python/oneflow/test/modules/test_sub.py
+++ python/oneflow/test/modules/test_sub.py
@@ -62,7 +62,7 @@ class TestSub(flow.unittest.TestCase):
         return x.sub(y)
 
     @autotest()
-    def test_tensor__sub_with_broadcast_data(test_case):
+    def test_tensor_sub_with_broadcast_data(test_case):
         device = random_device()
         x = random_pytorch_tensor(ndim=5, dim0=1, dim1=4, dim2=1).to(device)
         y = random_pytorch_tensor(ndim=3, dim0=1, dim1=1).to(device)
