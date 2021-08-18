24a34c414fd98b3000ea4085de28e4cd7c468647
GehangZhang <649975329@qq.com>
2021-08-18 10:28:19 +0800

    add new autotest for flow.div & flow.Tensor.div

--- python/oneflow/test/modules/test_div.py
+++ python/oneflow/test/modules/test_div.py
@@ -15,98 +15,65 @@ limitations under the License.
 """
 
 import unittest
-from collections import OrderedDict
 
 import numpy as np
 from automated_test_util import *
-from test_util import GenArgList
-
 import oneflow as flow
 import oneflow.unittest
-from automated_test_util import *
 
 
-def _test_div_impl(test_case, shape, device):
-    x = flow.Tensor(np.random.randn(*shape), device=flow.device(device))
-    y = flow.Tensor(np.random.randn(*shape), device=flow.device(device))
-    of_out = flow.div(x, y)
-    np_out = np.divide(x.numpy(), y.numpy())
-    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 0.0001, 0.0001))
-    x = 5
-    y = flow.Tensor(np.random.randn(*shape), device=flow.device(device))
-    of_out = flow.div(x, y)
-    np_out = np.divide(x, y.numpy())
-    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 0.0001, 0.0001))
-    x = flow.Tensor(np.random.randn(*shape), device=flow.device(device))
-    y = 5
-    of_out = flow.div(x, y)
-    np_out = np.divide(x.numpy(), y)
-    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 0.0001, 0.0001))
-    x = flow.Tensor(np.random.randn(*shape), device=flow.device(device))
-    y = flow.Tensor(np.random.randn(1, 1), device=flow.device(device))
-    of_out = flow.div(x, y)
-    np_out = np.divide(x.numpy(), y.numpy())
-    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 0.0001, 0.0001))
-    x = flow.Tensor(np.array([5.0]), device=flow.device(device))
-    y = flow.Tensor(np.random.randn(*shape), device=flow.device(device))
-    of_out = flow.div(x, y)
-    np_out = np.divide(x.numpy(), y.numpy())
-    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 0.0001, 0.0001))
-    x = flow.Tensor(
-        np.random.randn(*shape), device=flow.device(device), requires_grad=True
-    )
-    y = flow.Tensor(np.array([5.0]), device=flow.device(device), requires_grad=True)
-    of_out = flow.div(x, y)
-    np_out = np.divide(x.numpy(), y.numpy())
-    test_case.assertTrue(np.allclose(of_out.numpy(), np_out, 0.0001, 0.0001))
-    of_out = of_out.sum()
-    of_out.backward()
-    np_grad_x = np.full(shape, 0.2)
-    test_case.assertTrue(np.allclose(x.grad.numpy(), np_grad_x, 0.0001, 0.0001))
 
 
 @flow.unittest.skip_unless_1n1d()
 class TestDiv(flow.unittest.TestCase):
-    def test_div(test_case):
-        arg_dict = OrderedDict()
-        arg_dict["shape"] = [(2, 3), (2, 3, 4), (2, 4, 5, 6)]
-        arg_dict["device"] = ["cpu", "cuda"]
-        for arg in GenArgList(arg_dict):
-            _test_div_impl(test_case, *arg)
-
-    def test_sub_against_pytorch(test_case):
-        arg_dict = OrderedDict()
-        arg_dict["test_type"] = [test_flow_against_pytorch, test_tensor_against_pytorch]
-        arg_dict["device"] = ["cpu", "cuda"]
-        arg_dict["op"] = ["div"]
-        for arg in GenArgList(arg_dict):
-            arg[0](
-                test_case,
-                arg[2],
-                extra_annotations={"other": flow.Tensor},
-                extra_generators={
-                    "input": random_tensor(ndim=2, dim0=2, dim1=3),
-                    "other": random_tensor(ndim=2, dim0=2, dim1=3),
-                },
-                device=arg[1],
-            )
-            arg[0](
-                test_case,
-                arg[2],
-                extra_annotations={"other": float},
-                extra_generators={
-                    "input": random_tensor(ndim=2, dim0=2, dim1=3),
-                    "other": random(0, 5),
-                },
-                device=arg[1],
-            )
-
-    @autotest(auto_backward=False)
-    def test_0shape_div(test_case):
+    @autotest()
+    def test_div_with_random_data_Number(test_case):
         device = random_device()
-        x = random_pytorch_tensor(4, 2, 1, 0, 3).to(device)
-        y = random_pytorch_tensor(4, 2, 1, 0, 3).to(device)
-        z = x / y
+        x = random_pytorch_tensor(ndim=4, dim0=1).to(device)
+        k = random(1,4).to(float)
+        y = torch.div(x, k)
+        return y
+
+    
+    @autotest(auto_backward=False)
+    def test_div_with_0shape(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=4, dim0=2, dim1=1, dim2=0, dim3=3).to(device)
+        y = random_pytorch_tensor(ndim=4, dim0=2, dim1=1, dim2=0, dim3=3).to(device)
+        z = torch.div(x,y)
+        return z
+    
+    @autotest(auto_backward=False)
+    def test_div_with_diff_shape(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=4, dim0=3).to(device)
+        y = random_pytorch_tensor(ndim=4).to(device)
+        z = torch.div(x, y)
+        return z
+
+    @autotest()
+    def test_tensor_div_with_random_data_Number(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=4, dim0=1).to(device)
+        k = random(1,4).to(float)
+        y = x.div(k)
+        return y
+
+    
+    @autotest(auto_backward=False)
+    def test_tensor_div_with_0shape(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=4, dim0=2, dim1=1, dim2=0, dim3=3).to(device)
+        y = random_pytorch_tensor(ndim=4, dim0=2, dim1=1, dim2=0, dim3=3).to(device)
+        z = x.div(y)
+        return z
+    
+    @autotest(auto_backward=False)
+    def test_tensor_div_with_diff_shape(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=4, dim0=3).to(device)
+        y = random_pytorch_tensor(ndim=4).to(device)
+        z = x.div(y)
         return z
 
 
