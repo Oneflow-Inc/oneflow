79a57cc140e1fe9de62751a22cb89939883f782e
lambda7xx <lambda7xx@gmail.com>
2021-08-19 15:42:40 +0800

    modify the deconstructof of mr  and add deconstructor  v0.25

--- oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
+++ oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
@@ -72,8 +72,6 @@ IBVerbsCommNet::~IBVerbsCommNet() {
   for (IBVerbsQP* qp : qp_vec_) {
     if (qp) { delete qp; }
   }
-  recv_msg_buf_.reset();
-  send_msg_buf_.reset();
   CHECK_EQ(ibv::wrapper.ibv_destroy_cq(cq_), 0);
  // CHECK_EQ(ibv::wrapper.ibv_dealloc_pd(pd_), 0);
   CHECK_EQ(ibv::wrapper.ibv_close_device(context_), 0);
