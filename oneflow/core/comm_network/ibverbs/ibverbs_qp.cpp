2e0bf9f7c32c3dc8b1d539d55e77b349387e0355
lambda7xx <lambda7xx@gmail.com>
2021-08-19 14:12:59 +0800

    fix the error of IB constructor

--- oneflow/core/comm_network/ibverbs/ibverbs_qp.cpp
+++ oneflow/core/comm_network/ibverbs/ibverbs_qp.cpp
@@ -1,12 +1,9 @@
 /*
 Copyright 2020 The OneFlow Authors. All rights reserved.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -72,6 +69,8 @@ IBVerbsQP::IBVerbsQP(ibv_context* ctx, ibv_pd* pd, uint8_t port_num, ibv_cq* sen
 
 IBVerbsQP::~IBVerbsQP() {
   CHECK_EQ(ibv::wrapper.ibv_destroy_qp(qp_), 0);
+  recv_msg_buf_.reset();
+  send_msg_buf_.reset();
 }
 
 void IBVerbsQP::Connect(const IBVerbsConnectionInfo& peer_info) {
