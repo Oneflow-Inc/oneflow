e03af8b4eea0a7b09fb1a99cd0e654dba0d5549d
lambda7xx <lambda7xx@gmail.com>
2021-08-23 14:35:12 +0800

    use the pointer in the ibverbs_comm_network and shard_ptr in IBverbsQP and not use the delete v0.25

--- oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
+++ oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
@@ -78,6 +78,8 @@ IBVerbsCommNet::~IBVerbsCommNet() {
   for (IBVerbsQP* qp : qp_vec_) {
     if (qp) { delete qp; }
   }
+  delete recv_msg_buf_;
+  delete send_msg_buf_;
   CHECK_EQ(ibv::wrapper.ibv_destroy_cq(cq_), 0);
   CHECK_EQ(ibv::wrapper.ibv_dealloc_pd(pd_), 0);
   CHECK_EQ(ibv::wrapper.ibv_close_device(context_), 0);
