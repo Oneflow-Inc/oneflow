eac936c6bd2f86600483e710e4c727f441b35d30
clackhan <han_binbin@163.com>
2021-08-24 09:40:09 +0800

    minor fix

--- oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter.cpp
+++ oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter.cpp
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include <typeinfo>
-#include "oneflow/core/framework/tensor_rpc_util.h"
 #include "oneflow/core/common/container_util.h"
 #include "oneflow/core/common/registry_error.h"
 #include "oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter.h"
