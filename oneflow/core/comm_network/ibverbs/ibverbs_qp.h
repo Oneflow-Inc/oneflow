d46e910b1c5ef41e71f493fd65aeece57d3b6988
lambda7xx <lambda7xx@gmail.com>
2021-08-25 15:49:36 +0800

    use the raw pointer instead of the shared_ptr

--- oneflow/core/comm_network/ibverbs/ibverbs_qp.h
+++ oneflow/core/comm_network/ibverbs/ibverbs_qp.h
@@ -199,7 +199,7 @@ class IBVerbsQP final {
   uint32_t num_outstanding_send_wr_;
   uint32_t max_outstanding_send_wr_;
   std::queue<std::pair<ibv_send_wr, ibv_sge>> pending_send_wr_queue_;
-  std::shared_ptr<MessagePool> msg_buf_;
+  MessagePool * msg_Pool_buf_;
  };
 
 }  // namespace oneflow
