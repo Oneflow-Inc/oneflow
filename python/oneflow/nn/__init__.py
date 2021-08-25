50767711c0595acd7745ae00c1377acb3f421758
luyang <flowingsun007@163.com>
2021-08-25 15:16:36 +0800

    legacy maxpool2d module

--- python/oneflow/nn/__init__.py
+++ python/oneflow/nn/__init__.py
@@ -107,6 +107,7 @@ from oneflow.nn.modules.pooling import (
     MaxPool1d,
     MaxPool2d,
     MaxPool3d,
+    LegacyMaxPool2d
 )
 from oneflow.nn.modules.sparse import Embedding
 from oneflow.nn.modules.upsampling import (
