8acd61efcca24a584f20584a41b8fcf1c175dc68
jackalcooper <jackalcooper@gmail.com>
2021-08-27 16:07:36 +0800

    refine

--- cmake/caches/cn/fast/cuda-75.cmake
+++ cmake/caches/cn/fast/cuda-75.cmake
@@ -10,6 +10,7 @@ set(CMAKE_C_COMPILER_LAUNCHER sccache CACHE STRING "")
 set(CMAKE_CXX_COMPILER_LAUNCHER sccache CACHE STRING "")
 set(CMAKE_CUDA_COMPILER_LAUNCHER sccache CACHE STRING "")
 set(CMAKE_INTERPROCEDURAL_OPTIMIZATION OFF CACHE BOOL "")
+# uncomment these when necessary, otherwise it is for the demonstration purpose
 # set(CUDA_TOOLKIT_ROOT_DIR /usr/local/cuda CACHE STRING "")
 # set(CUDNN_ROOT_DIR /usr/local/cudnn CACHE STRING "")
 # set(CMAKE_CUDA_HOST_COMPILER clang++ CACHE STRING "")
