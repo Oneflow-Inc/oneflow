f30bbee12a97853e0676ae9a7a67b735c936ca1d
luqiang-guo <702572275@qq.com>
2021-08-29 13:33:35 +0800

    fix tensor_impl.h error msg

--- oneflow/core/framework/tensor_impl.h
+++ oneflow/core/framework/tensor_impl.h
@@ -112,11 +112,11 @@ class MirroredTensorImpl : public TensorImpl {
   }
   Maybe<Symbol<Device>*> mut_device() { return mut_tensor_meta()->mut_device(); }
   virtual Maybe<EagerMirroredTensorImpl*> mut_eager_mirrored_tensor_impl() {
-    OF_RUNTIME_ERROR() << "MirroredTensorImpl has no mut_eager_mirrored_tensor_impl property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
 
   virtual Maybe<MirroredTensorImpl> detach() const {
-    OF_RUNTIME_ERROR() << "MirroredTensorImpl has no detach property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
 
  protected:
@@ -142,26 +142,26 @@ class ConsistentTensorImpl : public TensorImpl {
     return consumer_nd_sbp_constraint_;
   }
   virtual Maybe<MirroredTensor> cur_rank_phy_tensor() const {
-    OF_RUNTIME_ERROR() << "ConsistentTensorImpl has no cur_rank_phy_tensor property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
   Symbol<ConsistentTensorMeta> tensor_meta() const { return tensor_meta_; }
 
   // Getters valid only for EagerMirroredTensorImpl
   Maybe<vm::EagerBlobObject> eager_blob_object() const override {
-    OF_RUNTIME_ERROR() << "ConsistentTensorImpl has no eager_blob_object property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
   Maybe<LocalDepObject*> compute_local_dep_object() const override {
-    OF_RUNTIME_ERROR() << "ConsistentTensorImpl has no compute_local_dep_object property";
+    OF_RUNTIME_ERROR() ERROR_ISSUE_MSG ;
   }
   Maybe<bool> has_eager_blob_object() const override {
-    OF_RUNTIME_ERROR() << "ConsistentTensorImpl has no has_eager_blob_object property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
 
   // Setters
   void set_consumer_nd_sbp_constraint(Symbol<cfg::NdSbp> val) { consumer_nd_sbp_constraint_ = val; }
 
   ConsistentTensorMeta* mut_tensor_meta() {
-    LOG(FATAL) << "RuntimeError: ConsistentTensorImpl don't have mut_tensor_meta property";
+    LOG(FATAL) << "RuntimeError: " << ERROR_ISSUE_MSG;
     return nullptr;
   }
 
@@ -199,16 +199,16 @@ class LazyMirroredTensorImpl final : public MirroredTensorImpl {
 
   // Getters valid only for EagerMirroredTensorImpl
   Maybe<vm::EagerBlobObject> eager_blob_object() const override {
-    OF_RUNTIME_ERROR() << "LazyMirroredTensorImpl has no eager_blob_object property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
   Maybe<LocalDepObject*> compute_local_dep_object() const override {
-    OF_RUNTIME_ERROR() << "LazyMirroredTensorImpl has no compute_local_dep_object property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
   Maybe<TensorStorage> tensor_storage() const override {
-    OF_RUNTIME_ERROR() << "LazyMirroredTensorImpl has no tensor_storage property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
   Maybe<bool> has_eager_blob_object() const override {
-    OF_RUNTIME_ERROR() << "LazyMirroredTensorImpl has no has_eager_blob_object property";
+    OF_RUNTIME_ERROR() << ERROR_ISSUE_MSG ;
   }
   Maybe<MirroredTensorImpl> detach() const override;
 };
