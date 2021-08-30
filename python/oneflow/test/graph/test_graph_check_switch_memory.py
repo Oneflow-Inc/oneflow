292c64b08f83bbf57869ab8601cf7611612ebabd
luqiang-guo <702572275@qq.com>
2021-08-30 23:08:07 +0800

    modified 1000

--- python/oneflow/test/graph/test_graph_check_switch_memory.py
+++ python/oneflow/test/graph/test_graph_check_switch_memory.py
@@ -44,7 +44,7 @@ def _test_memmory_graph(test_case, device):
 
     ones_np = np.ones(shape)
     
-    Random = RandomModule(shape).to(device)
+    random = RandomModule(shape).to(device)
 
     Ones = OnesModule(shape).to(device)
 
@@ -59,7 +59,7 @@ def _test_memmory_graph(test_case, device):
     ones_g = OnesGraph()
 
     for i in range(1000):
-        of_eager_out = Random()
+        of_eager_out = random()
         of_lazy_out = ones_g()
         test_case.assertTrue(np.array_equal(of_lazy_out.numpy(), ones_np))
 
