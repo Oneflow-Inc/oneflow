362bb874350c929e1bb3000fd27d46f6ebfc0fa6
rentainhe <596106517@qq.com>
2021-09-01 16:38:26 +0800

    add test on shufflenet inception_v3 googlenet mobilenetv3

--- python/oneflow/test_utils/oneflow_pytorch_compatiblity/oneflow_pytorch_compatiblity_test.py
+++ python/oneflow/test_utils/oneflow_pytorch_compatiblity/oneflow_pytorch_compatiblity_test.py
@@ -217,7 +217,7 @@ def test_train_loss_oneflow_pytorch(
     test_case, model_path: str, module_name: str, device: str = "cuda",
 ):
     batch_size = 16
-    image_nd = np.random.rand(batch_size, 3, 224, 224).astype(np.float32)
+    image_nd = np.random.rand(batch_size, 3, 224, 224).astype(np.float32)  # change to (batch_size, 3, 299, 299) when testing inception_v3 model
     label_nd = np.array([e for e in range(batch_size)], dtype=np.int32)
     oneflow_model_loss = []
     pytorch_model_loss = []
