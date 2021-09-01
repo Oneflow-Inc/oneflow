42f624725850223b162d4d5ac7767b865235548e
clackhan <han_binbin@163.com>
2021-09-01 22:08:43 +0800

    del useless code

--- oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter_mgr.cpp
+++ oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter_mgr.cpp
@@ -16,53 +16,14 @@ limitations under the License.
 #include <utility>
 #include "oneflow/core/common/constant.h"
 #include "oneflow/core/common/decorator.h"
-#include "oneflow/core/common/container_util.h"
 #include "oneflow/core/framework/nd_sbp.h"
 #include "oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter_mgr.h"
 #include "oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter_util.h"
-#include "oneflow/core/framework/op_interpreter/boxing/collective_boxing_interpreter.h"
-#include "oneflow/core/framework/op_interpreter/boxing/identity_boxing_interpreter.h"
-#include "oneflow/core/framework/op_interpreter/boxing/naive_b2p_boxing_interpreter.h"
-#include "oneflow/core/framework/op_interpreter/boxing/naive_s2p_boxing_interpreter.h"
-#include "oneflow/core/framework/op_interpreter/boxing/cuda_copy_boxing_interpreter.h"
-#include "oneflow/core/framework/op_interpreter/boxing/cuda_based_cpu_mpi_boxing_interpreter.h"
 #include "oneflow/core/framework/op_interpreter/boxing/boxing_dividor_util.h"
 
 namespace oneflow {
 
 namespace {
-using SbpPair2EagerBoxingInterpreter =
-    HashMap<std::pair<cfg::SbpParallel, cfg::SbpParallel>, std::shared_ptr<EagerBoxingInterpreter>>;
-
-Maybe<EagerBoxingInterpreter> GetOneDimNcclCollectiveEagerBoxingInterpreter(
-    Symbol<cfg::NdSbp> in_nd_sbp, Symbol<cfg::NdSbp> out_nd_sbp) {
-  static SbpPair2EagerBoxingInterpreter sbp_pair2eager_boxing_interpreter = {
-      {{*JUST(MakeSplitSbpParallel(0)), *JUST(MakeBroadcastSbpParallel())},  // S(0) -> B
-       std::make_shared<NcclCollectiveAllGatherBoxingInterpreter>()},
-      {{*JUST(MakeBroadcastSbpParallel()), *JUST(MakeSplitSbpParallel(0))},  // B -> S(0)
-       std::make_shared<NcclCollectiveReduceScatterBoxingInterpreter>("max")},
-      {{*JUST(MakePartialSumSbpParallel()), *JUST(MakeBroadcastSbpParallel())},  // P -> B
-       std::make_shared<NcclCollectiveAllReduceBoxingInterpreter>()},
-      {{*JUST(MakePartialSumSbpParallel()), *JUST(MakeSplitSbpParallel(0))},  // P -> S(0)
-       std::make_shared<NcclCollectiveReduceScatterBoxingInterpreter>("sum")},
-      {{*JUST(MakeSplitSbpParallel(0)), *JUST(MakePartialSumSbpParallel())},  // S(0) -> P
-       std::make_shared<NcclS2PBoxingInterpreter>()},
-  };
-  const auto& key = std::make_pair(in_nd_sbp->sbp_parallel(0), out_nd_sbp->sbp_parallel(0));
-  return JUST(MapAt(sbp_pair2eager_boxing_interpreter, key));
-}
-
-Maybe<EagerBoxingInterpreter> GetCudaBasedCpuMpiBoxingInterpreter(
-    Symbol<cfg::NdSbp> in_nd_sbp, Symbol<cfg::NdSbp> out_nd_sbp,
-    Symbol<ParallelDesc> in_parallel_desc, Symbol<ParallelDesc> out_parallel_desc) {
-  CHECK_OR_RETURN(in_nd_sbp != out_nd_sbp);
-  const auto& gpu_in_parallel_desc = JUST(ReplaceDeviceType(in_parallel_desc, DeviceType::kGPU));
-  const auto& gpu_out_parallel_desc = JUST(ReplaceDeviceType(out_parallel_desc, DeviceType::kGPU));
-  CHECK_OR_RETURN(gpu_in_parallel_desc == gpu_out_parallel_desc);
-  const auto& gpu_boxing_interpreter =
-      JUST(GetOneDimNcclCollectiveEagerBoxingInterpreter(in_nd_sbp, out_nd_sbp));
-  return std::shared_ptr<EagerBoxingInterpreter>(new CudaBasedCpuMpiBoxingInterpreter());
-}
 
 Maybe<bool> IgnoringDeviceTypeEqual(Symbol<ParallelDesc> lhs, Symbol<ParallelDesc> rhs) {
   if (lhs == rhs) { return true; }
