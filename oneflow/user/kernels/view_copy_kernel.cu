36c7ec270249a56a6a5359d32d981e3f2d9e9fdf
PragmaTwice <i@twice.moe>
2021-09-01 22:17:48 +0800

    remove cudamalloc

--- oneflow/user/kernels/view_copy_kernel.cu
+++ oneflow/user/kernels/view_copy_kernel.cu
@@ -20,28 +20,39 @@ namespace oneflow {
 
 namespace {
 
-__device__ void compute_index(int64_t offset, int ndim, const int64_t stride[], int64_t index[]) {
-  int64_t v = offset;
-
-#pragma unroll
-  for (int i = 0; i < ndim; ++i) {
-    int64_t idx = v / stride[i];
-    index[i] = idx;
-    v -= idx * stride[i];
+struct StrideParam {
+  int64_t stride[SHAPE_MAX_AXIS_SIZE];
+  
+  //NOLINTNEXTLINE
+  StrideParam(const int64_t* input, size_t n) {
+    for (int i = 0; i < n; ++i) {
+      stride[i] = input[i];
+    }
   }
-}
 
-__device__ int64_t compute_offset(const int64_t index[], int ndim, const int64_t stride[]) {
-  int64_t v = 0;
+  __device__ void compute_index(int64_t offset, int ndim, int64_t index[]) const {
+    int64_t v = offset;
+  
+  #pragma unroll
+    for (int i = 0; i < ndim; ++i) {
+      int64_t idx = v / stride[i];
+      index[i] = idx;
+      v -= idx * stride[i];
+    }
+  }
+  
+  __device__ int64_t compute_offset(const int64_t index[], int ndim) const {
+    int64_t v = 0;
+  
+  #pragma unroll
+    for (int i = 0; i < ndim; ++i) { v += index[i] * stride[i]; }
+  
+    return v;
+  }
+};
 
-#pragma unroll
-  for (int i = 0; i < ndim; ++i) { v += index[i] * stride[i]; }
-
-  return v;
-}
-
-__global__ void copy_view(cudaStream_t cuda_stream, int64_t count, size_t dsize,
-                          const int64_t in_stride[], const int64_t out_stride[],
+__global__ void copy_view(int64_t count, size_t dsize,
+                          StrideParam in_stride, StrideParam out_stride,
                           const char* in_dptr, char* out_dptr, int64_t ndim,
                           int64_t contiguous_block_size) {
   int64_t in_index[SHAPE_MAX_AXIS_SIZE];
@@ -49,8 +60,8 @@ __global__ void copy_view(cudaStream_t cuda_stream, int64_t count, size_t dsize,
   CUDA_1D_KERNEL_LOOP_T(int64_t, i, count) {
     const int64_t out_offset = i * contiguous_block_size;
 
-    compute_index(out_offset, ndim, out_stride, in_index);
-    const int64_t in_offset = compute_offset(in_index, ndim, in_stride);
+    out_stride.compute_index(out_offset, ndim, in_index);
+    const int64_t in_offset = in_stride.compute_offset(in_index, ndim);
 
     char *out_dptr_offset = out_dptr + out_offset * dsize;
     const char *in_dptr_offset = in_dptr + in_offset * dsize;
@@ -72,25 +83,12 @@ void ViewCopyUtil<kGPU>::operator()() {
     init_out_stride();
 
     int ndim = contiguous_dim + 1;
-    int stride_size = ndim * sizeof(int64_t);
 
-    int64_t *cuda_in_stride, *cuda_out_stride;
-    OF_CUDA_CHECK(cudaMalloc(&cuda_in_stride, stride_size));
-    OF_CUDA_CHECK(cudaMalloc(&cuda_out_stride, stride_size));
-
-    OF_CUDA_CHECK(
-        cudaMemcpy(cuda_in_stride, in_stride.data(), stride_size, cudaMemcpyHostToDevice));
-    OF_CUDA_CHECK(
-        cudaMemcpy(cuda_out_stride, out_stride.data(), stride_size, cudaMemcpyHostToDevice));
+    StrideParam param_in_stride(in_stride.data(), ndim), param_out_stride(out_stride.data(), ndim);
 
     copy_view<<<BlocksNum4ThreadsNum(count), kCudaThreadsNumPerBlock, 0, ctx->cuda_stream()>>>(
-        ctx->cuda_stream(), count, dsize, cuda_in_stride, cuda_out_stride, in_dptr, out_dptr, ndim,
+        count, dsize, param_in_stride, param_out_stride, in_dptr, out_dptr, ndim,
         contiguous_block_size);
-
-    OF_CUDA_CHECK(cudaDeviceSynchronize());
-
-    OF_CUDA_CHECK(cudaFree(cuda_in_stride));
-    OF_CUDA_CHECK(cudaFree(cuda_out_stride));
   }
 }
 
