43507e6d762eba99e78a029b51fe84c4e069f298
lixinqi <lixinqi0703106@163.com>
2021-09-01 12:02:01 +0800

    disable_print_stack_in_tensor_numpy

--- oneflow/core/framework/transport_util.cpp
+++ oneflow/core/framework/transport_util.cpp
@@ -31,12 +31,12 @@ namespace oneflow {
 /*static*/ Maybe<void> TransportUtil::WaitUntilDoneOrTimeout(const AsyncTransportCtx& ctx,
                                                              int64_t seconds) {
   bool is_printed = false;
+  const auto& TryPrintStackInfo = [&is_printed]{
+    if (!is_printed) { blocking::StackInfoCallback(); }
+    is_printed = true;
+  };
   JUST(SpinWaitUntilTimeout([&] { return *ctx.flying_cnt() > 0; }, seconds,
-                            [&is_printed] {
-                              if (!is_printed) { LOG(ERROR) << blocking::GetStackInfo(); }
-                              is_printed = true;
-                            },
-                            TransportUtil::BlockingWarningIntervalSeconds()));
+                            TryPrintStackInfo, TransportUtil::BlockingWarningIntervalSeconds()));
   return Maybe<void>::Ok();
 }
 
