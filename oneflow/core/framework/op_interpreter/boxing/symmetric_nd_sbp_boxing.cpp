6ad4b5e0764839369bc2dfcaa4d20b5add3f45e1
liufengwei <2472937968@qq.com>
2021-09-02 16:55:26 +0800

    fix include bug

--- oneflow/core/framework/op_interpreter/boxing/symmetric_nd_sbp_boxing.cpp
+++ oneflow/core/framework/op_interpreter/boxing/symmetric_nd_sbp_boxing.cpp
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/core/framework/op_interpreter/boxing/eager_boxing_interpreter_mgr.h"
+#include "oneflow/core/boxing/eager_boxing_interpreter_mgr.h"
 #include "oneflow/core/framework/op_interpreter/op_interpreter_util.h"
 #include "oneflow/core/framework/placement_sbp_util.h"
 #include "oneflow/core/framework/placed_nd_sbp.h"
