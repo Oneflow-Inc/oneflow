1a801094659391b2b9d39cd450dc4d8d763fab5a
clackhan <han_binbin@163.com>
2021-09-03 18:03:37 +0800

    refine

--- oneflow/core/boxing/ccl_boxing_function.cpp
+++ oneflow/core/boxing/ccl_boxing_function.cpp
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/framework/nd_sbp.h"
-#include "oneflow/core/boxing/eager_boxing_interpreter_util.h"
 #include "oneflow/core/boxing/eager_boxing_interpreter.h"
 #include "oneflow/core/common/decorator.h"
 #include "oneflow/core/functional/functional.h"
