ea8f52c9ab3b9950a081e40b1739ac31f4a25383
Flowingsun007 <flowingsun007@163.com>
2021-09-03 11:03:42 +0800

    fix comments

--- python/oneflow/cuda/__init__.py
+++ python/oneflow/cuda/__init__.py
@@ -18,8 +18,10 @@ import oneflow as flow
 
 def is_available() -> bool:
     r"""Returns a bool indicating if CUDA is currently available."""
-    if not hasattr(flow._oneflow_internal, "CudaGetDeviceCount"):
-        return False
     # This function never throws and returns 0 if driver is missing or can't
     # be initialized
     return flow._oneflow_internal.CudaGetDeviceCount() > 0
+
+def device_count() -> int:
+    r"""Returns the number of GPUs available.."""
+    return flow._oneflow_internal.CudaGetDeviceCount()
