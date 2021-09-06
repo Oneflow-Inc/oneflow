4e8f40cde71dbb597a8732d3f8196bfb4f35db46
jackalcooper <jackalcooper@gmail.com>
2021-09-06 16:43:29 +0800

    refine

--- ci/manylinux/build-gcc7.sh
+++ ci/manylinux/build-gcc7.sh
@@ -2,11 +2,25 @@ source scl_source enable devtoolset-7
 set -ex
 gcc --version
 export PATH=/usr/lib64/ccache:$PATH
-mkdir -p ${ONEFLOW_CI_BUILD_DIR}
-cd ${ONEFLOW_CI_BUILD_DIR}
-find ${ONEFLOW_CI_BUILD_DIR} -name CMakeCache.txt -delete
-cmake -S ${ONEFLOW_CI_SRC_DIR} -C ${ONEFLOW_CI_CMAKE_INIT_CACHE} -DPython3_EXECUTABLE=${ONEFLOW_CI_PYTHON_EXE}
-cmake --build . -j $(nproc)
+
+# clean python dir
 cd ${ONEFLOW_CI_SRC_DIR}
 cd python
-python setup.py bdist_wheel
+git clean -nXd -e \!dist -e \!dist/**
+git clean -fXd -e \!dist -e \!dist/**
+
+# cmake config
+mkdir -p ${ONEFLOW_CI_BUILD_DIR}
+cd ${ONEFLOW_CI_BUILD_DIR}
+find ${ONEFLOW_CI_BUILD_DIR} -name CMakeCache.txt
+find ${ONEFLOW_CI_BUILD_DIR} -name CMakeCache.txt -delete
+cmake -S ${ONEFLOW_CI_SRC_DIR} -C ${ONEFLOW_CI_CMAKE_INIT_CACHE} -DPython3_EXECUTABLE=${ONEFLOW_CI_PYTHON_EXE}
+
+# cmake build
+cd ${ONEFLOW_CI_BUILD_DIR}
+cmake --build . -j $(nproc)
+
+# build pip
+cd ${ONEFLOW_CI_SRC_DIR}
+cd python
+${ONEFLOW_CI_PYTHON_EXE} setup.py bdist_wheel
