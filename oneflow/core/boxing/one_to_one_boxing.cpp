b60996a93a969a94a2faf35c01688bbde3d681ca
PragmaTwice <i@twice.moe>
2021-09-07 17:08:54 +0800

    optional: fix typo

--- oneflow/core/boxing/one_to_one_boxing.cpp
+++ oneflow/core/boxing/one_to_one_boxing.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/core/common/data_type.pb.h"
 #include "oneflow/core/control/global_process_ctx.h"
 #include "oneflow/core/framework/nd_sbp.h"
 #include "oneflow/core/framework/device.h"
