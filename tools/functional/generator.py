248238dabe44ca7fd27f08a6d8cd5e70129c572e
MARD1NO <359521840@qq.com>
2021-09-08 13:36:17 +0800

    fix comment

--- tools/functional/generator.py
+++ tools/functional/generator.py
@@ -127,8 +127,6 @@ value_aliases = {
     "kBool": "DType::Bool()",
 }
 
-int_list_type = {"Int32List", "Int64List"}
-
 
 def _escape_quote(fmt):
     return re.sub(r"\"|\'", '\\"', fmt)
@@ -224,16 +222,13 @@ class Argument:
         sp = self._name.find("=")
         if sp != -1:
             self._default_value = _normalize(self._name[sp + 1 :])
-            if self._type in int_list_type:
-                _size_list = []
-                for i in range(self._size):
-                    # For int32List[2] = 1, _size_list will be [1, 1]
-                    _size_list.append(int(self._default_value))
-                self._default_cpp_value = str(_size_list)
-                self._default_cpp_value = self._default_cpp_value.replace("[", "{")
-                self._default_cpp_value = self._default_cpp_value.replace(
-                    "]", "}"
-                )  # "[1, 1]" -> "{1, 1}"
+            if self._type.endswith("List"):
+                _value_list = [
+                    self._default_value for i in range(self._size)
+                ]  # For int32List[2] = 1, _value_list will be [1, 1]
+                self._default_cpp_value = (
+                    "{" + ", ".join(_value_list) + "}"
+                )  # [1, 1] -> "{1, 1}"
             elif self._default_value == "None":
                 self._optional = True
                 self._default_cpp_value = ""
