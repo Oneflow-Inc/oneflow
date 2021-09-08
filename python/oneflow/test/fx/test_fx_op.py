c45dd88654317dd04bc2d85963bace086cf6e0ee
bbuf <1182563586@qq.com>
2021-09-08 18:49:32 +0800

    add conv op

--- python/oneflow/test/fx/test_fx_op.py
+++ python/oneflow/test/fx/test_fx_op.py
@@ -14,6 +14,17 @@ def acos_func(x):
 
 def atanh_func(x):
     return flow.atanh(x)
+
+def sign_func(x):
+    return flow.sign(x)
+class ConvModule(flow.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_mod = flow.nn.Conv2d(5, 5, 3)
+
+    def forward(self, x):
+        return self.conv_mod(x)
+
 @flow.unittest.skip_unless_1n1d()
 class TestFX(flow.unittest.TestCase):
     def test_abs(test_case):
@@ -36,6 +47,16 @@ class TestFX(flow.unittest.TestCase):
         input = flow.randn(3, 4)
         assert(np.allclose(gm(input).numpy(), atanh_func(input).numpy(), equal_nan=True))
     
+    def test_sign(test_case):
+        gm : flow.fx.GraphModule = symbolic_trace(sign_func)
+        input = flow.randn(3, 4)
+        assert(np.allclose(gm(input).numpy(), sign_func(input).numpy(), equal_nan=True))
+
+    def test_conv(test_case):
+        m = ConvModule()
+        gm : flow.fx.GraphModule = symbolic_trace(m)
+        input = flow.randn(1, 5, 5, 5)
+        assert(np.allclose(gm(input).numpy(), m(input).numpy(), equal_nan=True))
 
 if __name__ == "__main__":
     unittest.main()
