77f7b976189a1617dc6bc13dc74203b284b07ece
BBuf <1182563586@qq.com>
2021-09-08 14:45:36 +0800

    delete useless

--- python/oneflow/test_utils/automated_test_util/torch_flow_dual_object.py
+++ python/oneflow/test_utils/automated_test_util/torch_flow_dual_object.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import collections.abc
-from enum import unique
 import functools
 import inspect
 import os
