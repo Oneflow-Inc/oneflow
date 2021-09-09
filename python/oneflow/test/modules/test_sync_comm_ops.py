fix bug comm_ops broadcast

python/oneflow/test/modules/test_sync_comm_ops.py
@@ -62,8 +62,8 @@ class TestBroadCast(flow.unittest.TestCase):
         elif flow.env.get_rank() == 1:
             np_arr = np.array([[4, 5], [6, 7]])
         tensor = flow.tensor(np_arr, device="cuda", dtype=flow.int32)
-        flow.comm.broadcast(tensor)
-        test_case.assertTrue(np.allclose(tensor.numpy(), np.array([[1, 2], [3, 4]])))
+        flow.comm.broadcast(tensor, 1)
+        test_case.assertTrue(np.allclose(tensor.numpy(), np.array([[4, 5], [6, 7]])))
 
 
 @flow.unittest.skip_unless_1n2d()