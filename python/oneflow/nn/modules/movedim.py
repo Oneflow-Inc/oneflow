e658a13369ea0e5765248b4b4be6d5925a4e1918
lichunyou <2695928308@qq.com>
2021-09-10 11:57:40 +0800

    testfile

--- python/oneflow/nn/modules/movedim.py
+++ python/oneflow/nn/modules/movedim.py
@@ -17,6 +17,50 @@ import oneflow as flow
 from oneflow.framework.tensor import register_tensor_op
 
 def movedim_op(input, source, destination):
+     """
+    Moves the dimension(s) of input at the position(s) in source to the position(s) in destination.
+    Other dimensions of input that are not explicitly moved remain in their original order and appear at the positions not specified in destination.
+
+    Args:
+        input (Tensor): the input tensor.
+        source  (int or tuple of python:ints): Original positions of the dims to move. These must be unique. 
+        destination (int or tuple of python:ints) – Destination positions for each of the original dims. These must also be unique.
+    
+    Returns:
+        oneflow.Tensor: the output Tensor.
+
+    For example:
+
+    .. code-block:: python
+        
+        >>> import oneflow as flow
+        >>> t = flow.randn(3,2,1)
+        >>> t
+        tensor([[[-0.1523],
+                [-0.6242]],
+
+                [[-0.1521],
+                [ 0.5220]],
+
+                [[-0.2166],
+                [ 1.5300]]], dtype=oneflow.float32)
+        >>> flow.movedim(t,1,0).shape
+        flow.Size([2, 3, 1])
+        >>> flow.movedim(t,1,0)
+        tensor([[[-0.1523],
+                [-0.1521],
+                [-0.2166]],
+
+                [[-0.6242],
+                [ 0.5220],
+                [ 1.5300]]], dtype=oneflow.float32)
+        >>> flow.movedim(t, (1, 2), (0, 1)).shape
+        flow.Size([2, 1, 3])
+        >>> flow.movedim(t, (1, 2), (0, 1))
+        tensor([[[-0.1523, -0.1521, -0.2166]],
+
+                [[-0.6242,  0.5220,  1.5300]]], dtype=oneflow.float32)
+    """
      return flow._C.movedim(input, source, destination)
 
 if __name__ == "__main__":
