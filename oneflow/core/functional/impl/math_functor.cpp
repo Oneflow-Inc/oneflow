e2be52844b439dbe820e8b3be73e5aa41fb2d7ed
lichunyou <2695928308@qq.com>
2021-09-10 11:59:46 +0800

    rmline

--- oneflow/core/functional/impl/math_functor.cpp
+++ oneflow/core/functional/impl/math_functor.cpp
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include<iostream>
+
 #include "oneflow/core/framework/attr_map.h"
 #include "oneflow/core/framework/nd_sbp.h"
 #include "oneflow/core/framework/op_builder.h"
