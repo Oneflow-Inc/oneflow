ff8a44438d94022885b4f98d3b461cc565b860b9
cheng cheng <472491134@qq.com>
2021-09-13 22:51:34 +0800

    Replace reshape conf in grad acc pass (#6268)

--- oneflow/core/common/shape.cpp
+++ oneflow/core/common/shape.cpp
@@ -122,8 +122,7 @@ void Shape::Set(int64_t index, int64_t val) {
   CHECK_GE(index, 0);
   CHECK_LT(index, this->NumAxes()) << " Shape: " << DebugStr() << " visit index: " << index
                                    << " > num_axes: " << this->NumAxes();
-  // NOTE(chengcheng): ONLY support set dim 0 need infer axis as -1
-  CHECK(val >= 0 || (index == 0 && val == -1));
+  CHECK_GE(val, 0);
   dim_vec_.at(index) = val;
   UpdateElemCnt();
 }
