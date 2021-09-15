d2d48e4243c9701c825784176059a2c6e9d732ae
clackhan <han_binbin@163.com>
2021-09-15 11:00:50 +0800

    refine

--- oneflow/user/kernels/communicate_util.cpp
+++ oneflow/user/kernels/communicate_util.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/user/kernels/communicate_util.h"
+#include "oneflow/core/device/nccl_util.h"
 #include "oneflow/core/common/container_util.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
@@ -65,7 +66,7 @@ template Maybe<void> Send<DeviceType::kCPU>(const void* in, size_t elem_cnt, Dat
 template Maybe<void> Recv<DeviceType::kCPU>(void* out, size_t elem_cnt, DataType dtype, int64_t src,
                                             DeviceCtx* ctx);
 
-#if defined(WITH_CUDA) && NCCL_VERSION_CODE > 2700
+#if defined(WITH_CUDA) && HAS_GPU_SEND_RECV
 template Maybe<void> Send<DeviceType::kGPU>(const void* in, size_t elem_cnt, DataType dtype,
                                             int64_t dst, DeviceCtx* ctx);
 
--- oneflow/user/kernels/eager_s_to_s_kernel.cpp
+++ oneflow/user/kernels/eager_s_to_s_kernel.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/user/kernels/communicate_util.h"
+#include "oneflow/core/device/nccl_util.h"
 #include "oneflow/core/common/container_util.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
@@ -203,7 +204,8 @@ class EagerNaiveSToSKernel final : public user_op::OpKernel {
       .SetInferTmpSizeFn(InferNaiveSToSKernelTmpBufferSize);
 
 REGISTER_EAGER_NAIVE_S_TO_S_KERNEL(DeviceType::kCPU)
-#if defined(WITH_CUDA) && NCCL_VERSION_CODE > 2700
+
+#if defined(WITH_CUDA) && HAS_GPU_SEND_RECV
 REGISTER_EAGER_NAIVE_S_TO_S_KERNEL(DeviceType::kGPU)
 #endif
 
