95be57c18c504a09d90a666eb4af237b42e68ea7
YaoChi <later@usopp.net>
2021-09-16 22:17:30 +0800

    add test on reset_docstr

--- python/oneflow/framework/docstr/utils.py
+++ python/oneflow/framework/docstr/utils.py
@@ -15,6 +15,17 @@ limitations under the License.
 """
 
 import oneflow._oneflow_internal
+from doctest import DocTestParser, DebugRunner, DocTestRunner
+
+
+def _test_docstr(docstr, verbose=True, optionflags=0, raise_on_error=True):
+    parser = DocTestParser()
+    if raise_on_error:
+        runner = DebugRunner(verbose=verbose, optionflags=optionflags)
+    else:
+        runner = DocTestRunner(verbose=verbose, optionflags=optionflags)
+    test = parser.get_doctest(docstr, {}, __name__, __file__, 0)
+    runner.run(test)
 
 
 def add_docstr(fun, docstr: str):
@@ -22,6 +33,7 @@ def add_docstr(fun, docstr: str):
 
 
 def reset_docstr(o, docstr):
+    _test_docstr(docstr)
     if type(o) == type:
         assert hasattr(o, "__doc__"), str(o) + " does not have a docstring!"
         setattr(o, "__doc__", docstr)
