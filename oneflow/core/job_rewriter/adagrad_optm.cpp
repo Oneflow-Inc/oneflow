f4ddb27e64125b5ec45211adc7a576e0b04ed7e6
MARD1NO <359521840@qq.com>
2021-09-17 10:05:49 +0800

    remove string header file

--- oneflow/core/job_rewriter/adagrad_optm.cpp
+++ oneflow/core/job_rewriter/adagrad_optm.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <string>
 #include "oneflow/core/framework/user_op_conf.h"
 #include "oneflow/core/job/initializer_conf.pb.h"
 #include "oneflow/core/job/job_builder.h"
