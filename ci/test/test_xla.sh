3396c611e34b08f181453c483e909a94e320e57f
jackalcooper <jackalcooper@gmail.com>
2021-09-23 15:06:47 +0800

    refien

--- ci/test/test_xla.sh
+++ ci/test/test_xla.sh
@@ -8,6 +8,7 @@ mkdir -p $test_tmp_dir
 cp -r $src_dir/python/oneflow/compatible/single_client/test/xrt $test_tmp_dir
 cd $test_tmp_dir
 python3 -c "import oneflow.compatible.single_client as flow; assert flow.sysconfig.with_xla()"
+gpu_num=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 python3 $src_dir/ci/test/parallel_run.py \
     --gpu_num=${gpu_num} \
     --dir=${PWD} \
