71ff2f17666479e949809818709a83a65ca6861a
LYZFU <fuzhendong97@gmail.com>
2021-09-23 10:21:08 +0800

    make of_format

--- python/oneflow/nn/utils/weight_norm.py
+++ python/oneflow/nn/utils/weight_norm.py
@@ -1,15 +1,19 @@
 """
 Copyright 2020 The OneFlow Authors. All rights reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
 
 import oneflow as flow
 from oneflow.framework.tensor import Tensor
@@ -87,11 +91,12 @@ class WeightNorm(object):
 
 T_module = TypeVar("T_module", bound=Module)
 
-
 def weight_norm(module: T_module, name: str = "weight", dim: int = 0) -> T_module:
     r"""Applies weight normalization to a parameter in the given module.
+
     .. math::
-         \mathbf{w} = g \dfrac{\mathbf{v}}{\|\mathbf{v}\|}
+        \mathbf{w}=g \frac{\mathbf{v}}{\|\mathbf{v}\|}
+
     Weight normalization is a reparameterization that decouples the magnitude
     of a weight tensor from its direction. This replaces the parameter specified
     by :attr:`name` (e.g. ``'weight'``) with two parameters: one specifying the magnitude
@@ -99,17 +104,25 @@ def weight_norm(module: T_module, name: str = "weight", dim: int = 0) -> T_modul
     Weight normalization is implemented via a hook that recomputes the weight
     tensor from the magnitude and direction before every :meth:`~Module.forward`
     call.
+
     By default, with ``dim=0``, the norm is computed independently per output
     channel/plane. To compute a norm over the entire weight tensor, use
     ``dim=None``.
+    
     See https://arxiv.org/abs/1602.07868
+
     Args:
         module (Module): containing module
         name (str, optional): name of weight parameter
         dim (int, optional): dimension over which to compute the norm
+
     Returns:
         The original module with the weight norm hook
-    Example::
+    
+    For example:
+
+    .. code-block:: python
+
         >>> m = weight_norm(flow.Linear(20, 40), name='weight')
         >>> m
         Linear(in_features=20, out_features=40, bias=True)
@@ -117,6 +130,7 @@ def weight_norm(module: T_module, name: str = "weight", dim: int = 0) -> T_modul
         torch.Size([40, 1])
         >>> m.weight_v.size()
         torch.Size([40, 20])
+
     """
     WeightNorm.apply(module, name, dim)
     return module
