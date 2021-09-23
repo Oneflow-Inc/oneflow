b7d46bf9963d3b6dac49e0f4b9f6956a4db0e524
jackalcooper <jackalcooper@gmail.com>
2021-09-23 16:59:20 +0800

    refein

--- ci/test/test_xla.sh
+++ ci/test/test_xla.sh
@@ -4,7 +4,6 @@ src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
 test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"./test_tmp_dir"}
 
 rm -rf $test_tmp_dir
-mkdir -p $test_tmp_dir
 cp -r $src_dir/python/oneflow/compatible/single_client/test/xrt $test_tmp_dir
 cd $test_tmp_dir
 python3 -c "import oneflow.compatible.single_client as flow; assert flow.sysconfig.with_xla()"
