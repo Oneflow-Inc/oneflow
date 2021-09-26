f20193d595ddd6d228ba38b5fc883f86e6892cde
clackhan <han_binbin@163.com>
2021-09-26 15:05:02 +0800

    refine

--- oneflow/core/functional/impl/slice_boxing_functor.cpp
+++ oneflow/core/functional/impl/slice_boxing_functor.cpp
@@ -88,10 +88,12 @@ Maybe<one::UserOpExpr> EagerBToS(Symbol<ParallelDesc> in_parallel_desc,
 static constexpr auto* CachedEagerBToSpExpr = DECORATE(&EagerBToS, ThreadLocalCopiable);
 
 Maybe<one::UserOpExpr> EagerPToS(Symbol<ParallelDesc> in_parallel_desc,
-                                 Symbol<ParallelDesc> out_parallel_desc, const Shape& shape) {
+                                 Symbol<ParallelDesc> out_parallel_desc,
+                                 Symbol<cfg::SbpParallel> dst_sbp, const Shape& shape) {
   return one::OpBuilder("eager_p_to_s", *JUST(UniqueStr("eager_p_to_s")))
       .Input("in")
       .Output("out")
+      .Attr<int64_t>("out_split_axis", dst_sbp->split_parallel().axis())
       .Attr<std::string>("in_parallel_conf", PbMessage2TxtString(in_parallel_desc->parallel_conf()))
       .Attr<std::string>("out_parallel_conf",
                          PbMessage2TxtString(out_parallel_desc->parallel_conf()))
@@ -176,13 +178,18 @@ class EagerPToSFunctor {
   EagerPToSFunctor() = default;
   Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x,
                            Symbol<ParallelDesc> in_parallel_desc,
-                           Symbol<ParallelDesc> out_parallel_desc, const Shape& shape) const {
+                           Symbol<ParallelDesc> out_parallel_desc,
+                           const std::vector<Symbol<cfg::SbpParallel>>& out_sbp_parallels,
+                           const Shape& shape) const {
+    Symbol<cfg::NdSbp> out_nd_sbp = JUST(GetNdSbp(out_sbp_parallels));
     {
       CHECK_OR_RETURN(x->is_local());
       CHECK_OR_RETURN(x->is_eager());
+      CHECK_EQ_OR_RETURN(out_nd_sbp->sbp_parallel_size(), 1);
+      CHECK_OR_RETURN(IsSplitSbp(out_nd_sbp->sbp_parallel(0)));
     }
-    std::shared_ptr<OpExpr> op_expr =
-        JUST(CachedEagerPToSpExpr(in_parallel_desc, out_parallel_desc, shape));
+    std::shared_ptr<OpExpr> op_expr = JUST(CachedEagerPToSpExpr(
+        in_parallel_desc, out_parallel_desc, SymbolOf(out_nd_sbp->sbp_parallel(0)), shape));
     return JUST(OpInterpUtil::Dispatch<Tensor>(*op_expr, {x}));
   }
 };
