18f805f4e6996cf3e5082545ade24eea24f99579
guo-ran <360112263@qq.com>
2021-09-26 16:31:16 +0800

    refine

--- oneflow/core/primitive/include/copy_nd.h
+++ oneflow/core/primitive/include/copy_nd.h
@@ -13,8 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#ifndef ONEFLOW_CORE_PRIMITIVE_MEMORY_COPY_ND_H_
-#define ONEFLOW_CORE_PRIMITIVE_MEMORY_COPY_ND_H_
+#ifndef ONEFLOW_CORE_PRIMITIVE_COPY_ND_H_
+#define ONEFLOW_CORE_PRIMITIVE_COPY_ND_H_
 
 #include "oneflow/core/primitive/include/primitive.h"
 
@@ -22,11 +22,11 @@ namespace oneflow {
 
 namespace primitive {
 
-class MemoryCopyNd : public Primitive {
+class CopyNd : public Primitive {
  public:
-  OF_DISALLOW_COPY_AND_MOVE(MemoryCopyNd);
-  MemoryCopyNd() = default;
-  ~MemoryCopyNd() override = default;
+  OF_DISALLOW_COPY_AND_MOVE(CopyNd);
+  CopyNd() = default;
+  ~CopyNd() override = default;
 
   virtual void Launch(StreamContext* stream_ctx, DataType data_type, size_t num_dims, void* dst,
                       const int64_t* dst_dims, const int64_t* dst_pos, const void* src,
@@ -34,17 +34,17 @@ class MemoryCopyNd : public Primitive {
                       const int64_t* extent) const = 0;
 };
 
-class MemoryCopyNdFactory : public Factory<MemoryCopyNd> {
+class CopyNdFactory : public Factory<CopyNd> {
  public:
-  OF_DISALLOW_COPY_AND_MOVE(MemoryCopyNdFactory);
-  MemoryCopyNdFactory() = default;
-  ~MemoryCopyNdFactory() override = default;
+  OF_DISALLOW_COPY_AND_MOVE(CopyNdFactory);
+  CopyNdFactory() = default;
+  ~CopyNdFactory() override = default;
 
-  virtual std::unique_ptr<MemoryCopyNd> New() = 0;
+  virtual std::unique_ptr<CopyNd> New(size_t max_num_dims) = 0;
 };
 
 }  // namespace primitive
 
 }  // namespace oneflow
 
-#endif  // ONEFLOW_CORE_PRIMITIVE_MEMORY_COPY_ND_H_
+#endif  // ONEFLOW_CORE_PRIMITIVE_COPY_ND_H_
