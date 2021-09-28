474dd49dd29670d4cd5e3a2431d7a88e9e2548ff
lambda7xx <lambda7xx@gmail.com>
2021-09-28 15:11:47 +0800

    alloc memory

--- oneflow/core/comm_network/epoll/epoll_comm_network.cpp
+++ oneflow/core/comm_network/epoll/epoll_comm_network.cpp
@@ -97,6 +97,7 @@ void EpollCommNet::SendMsg(int64_t dst_machine_id, uint64_t addr, size_t size) {
     SocketMsg msg;
     msg.msg_type = SocketMsgType::kActor;
     //msg.data = data;
+    msg.data = (char*)malloc(size) ;
     std::memcpy(msg.data,data,size);
     msg.size = size;
     GetSocketHelper(dst_machine_id)->AsyncWrite(msg);
