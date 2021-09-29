dd62e519c5f9b6592226f4f14103367d7e37d671
Flowingsun007 <flowingsun007@163.com>
2021-09-29 22:54:26 +0800

    refactor transpose

--- python/oneflow/nn/modules/transpose.py
+++ python/oneflow/nn/modules/transpose.py
@@ -45,23 +45,24 @@ def transpose_op(input, dim0, dim1):
 
     """
 
-    x_shape = input.shape
+    # x_shape = input.shape
 
-    if dim0 < 0:
-        dim0 += len(x_shape)
-    if dim1 < 0:
-        dim1 += len(x_shape)
-    assert dim0 >= 0 and dim0 < len(x_shape), "Invalid dim0 {}, len(shape): {}".format(
-        dim0, len(x_shape)
-    )
-    assert dim1 >= 0 and dim1 < len(x_shape), "Invalid dim1 {}, len(shape): {}".format(
-        dim1, len(x_shape)
-    )
-    perm = []
-    for i in range(len(x_shape)):
-        perm.append(i)
-    (perm[dim0], perm[dim1]) = (perm[dim1], perm[dim0])
-    return flow._C.transpose(input, perm=perm)
+    # if dim0 < 0:
+    #     dim0 += len(x_shape)
+    # if dim1 < 0:
+    #     dim1 += len(x_shape)
+    # assert dim0 >= 0 and dim0 < len(x_shape), "Invalid dim0 {}, len(shape): {}".format(
+    #     dim0, len(x_shape)
+    # )
+    # assert dim1 >= 0 and dim1 < len(x_shape), "Invalid dim1 {}, len(shape): {}".format(
+    #     dim1, len(x_shape)
+    # )
+    # perm = []
+    # for i in range(len(x_shape)):
+    #     perm.append(i)
+    # (perm[dim0], perm[dim1]) = (perm[dim1], perm[dim0])
+    # return flow._C.transpose(input, perm=perm)
+    return flow._C.transpose(input, dim0=dim0, dim1=dim1)
 
 
 if __name__ == "__main__":
