5ba3931790da775776788a2f33b1e3d7838b8caf
lambda7xx <lambda7xx@gmail.com>
2021-09-29 16:44:03 +0800

    add  '\0'

--- oneflow/core/comm_network/epoll/epoll_comm_network.cpp
+++ oneflow/core/comm_network/epoll/epoll_comm_network.cpp
@@ -102,6 +102,7 @@ void EpollCommNet::SendMsg(int64_t dst_machine_id, uint64_t addr, size_t size) {
     std::memcpy(msg.actor_msg.data,data,size);
     //msg.actor_msg.size  = size;
     msg.size = size;
+    msg.actor_msg.data[size] = '\0';
     std::cout<<"EpollCommNet::SendMsg, size:"<<size << std::endl;
     //std::cout<<"EpollCommNet::SendMsg,msg.actor_msg.size:"<<msg.actor_msg.size << std::endl;
     std::cout<<std::endl << std::endl;
