806419d369c6b16f28c1deec880bc954a3f12a79
lixinqi <lixinqi0703106@163.com>
2021-10-07 18:03:55 +0800

    rename or delete test files in object_msg/

--- oneflow/core/object_msg/object_msg_test.cpp
+++ oneflow/core/object_msg/object_msg_test.cpp
@@ -13,6 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+// include sstream first to avoid some compiling error
+// caused by the following trick
+// reference: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=65899
+#include <sstream>
+#define private public
 #include "oneflow/core/common/util.h"
 #include "oneflow/core/object_msg/object_msg.h"
 #include "oneflow/core/object_msg/flat_msg.h"
@@ -23,6 +28,19 @@ namespace oneflow {
 namespace test {
 
 namespace {
+
+TEST(ObjectMsgStruct, ref_cnt) {
+  class Foo final : public ObjectMsgBase {
+   public:
+    Foo() = default;
+  };
+  Foo foo;
+  foo.InitRefCount();
+  foo.IncreaseRefCount();
+  foo.IncreaseRefCount();
+  ASSERT_EQ(foo.DecreaseRefCount(), 1);
+  ASSERT_EQ(foo.DecreaseRefCount(), 0);
+}
 
 // clang-format off
 OBJECT_MSG_BEGIN(ObjectMsgFoo)
