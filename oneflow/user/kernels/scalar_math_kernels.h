b73a5ad1c78907ab6a450dfa48632e5be6e261db
simonJJJ <821898965@qq.com>
2021-10-12 22:17:57 +0800

    restruct scalar math

--- oneflow/user/kernels/scalar_math_kernels.h
+++ oneflow/user/kernels/scalar_math_kernels.h
@@ -13,30 +13,32 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-#ifndef _ONEFLOW_USER_KERNELS_SCALAR_FMOD_KERNEL_H_
-#define _ONEFLOW_USER_KERNELS_SCALAR_FMOD_KERNEL_H_
+#ifndef _ONEFLOW_USER_KERNELS_SCALAR_MATH_KERNELS_H_
+#define _ONEFLOW_USER_KERNELS_SCALAR_MATH_KERNELS_H_
+#include "oneflow/user/kernels/elementwise_xpu_kernel.h"
 #include "oneflow/core/ndarray/binary_func.h"
 #include "oneflow/core/ndarray/xpu_util.h"
 
 namespace oneflow {
 
+#define INSTANTIATE_SCALAR_MATH_FUNCTORS(device_type, binary_op)      \
+  template struct ScalarMathFunctor<device_type, binary_op, uint8_t>; \
+  template struct ScalarMathFunctor<device_type, binary_op, int8_t>;  \
+  template struct ScalarMathFunctor<device_type, binary_op, int32_t>; \
+  template struct ScalarMathFunctor<device_type, binary_op, int64_t>; \
+  template struct ScalarMathFunctor<device_type, binary_op, float>;   \
+  template struct ScalarMathFunctor<device_type, binary_op, double>;
+
 template<DeviceType device_type, template<typename T> class BIN_OP, typename T>
-struct ScalarFmodFunctor final {
+struct ScalarMathFunctor final {
   void operator()(DeviceCtx* ctx, const int64_t elem_cnt, const T scalar, const T* in, T* out);
 };
 
 template<template<typename> class UnaryFunctor, typename T>
-OF_DEVICE_FUNC void DoScalarFmod(const int64_t elem_cnt, const T scalar, const T* in, T* out) {
+OF_DEVICE_FUNC void DoScalarMath(const int64_t elem_cnt, const T scalar, const T* in, T* out) {
   XPU_1D_KERNEL_LOOP(idx, elem_cnt) { out[idx] = UnaryFunctor<T>::Invoke(in[idx], scalar); }
 }
 
-#define REGISTER_SCALAR_FMOD_KERNEL(device, dtype)                    \
-  REGISTER_USER_KERNEL("scalar_fmod")                                 \
-      .SetCreateFn<ScalarFmodKernel<device, BinaryFuncFMod, dtype>>() \
-      .SetIsMatchedHob((user_op::HobDeviceTag() == device)            \
-                       & (user_op::HobDataType("in", 0) == GetDataType<dtype>::value));
-
 }  // namespace oneflow
 
-#endif  //_ONEFLOW_USER_KERNELS_SCALAR_FMOD_KERNEL_H_
+#endif  // _ONEFLOW_USER_KERNELS_SCALAR_MATH_KERNELS_H_
