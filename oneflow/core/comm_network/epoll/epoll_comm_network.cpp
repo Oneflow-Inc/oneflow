89c705462c18fd17e8d6ac8e3f3997047980b602
lambda7xx <lambda7xx@gmail.com>
2021-10-13 11:30:27 +0800

    create one pr

--- oneflow/core/comm_network/epoll/epoll_comm_network.cpp
+++ oneflow/core/comm_network/epoll/epoll_comm_network.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <cstdint>
 #ifdef __linux__
 
 #include "oneflow/core/comm_network/epoll/epoll_comm_network.h"
