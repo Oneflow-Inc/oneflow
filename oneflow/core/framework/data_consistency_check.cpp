006d8a8dbd879c69979a170aeb03d6766faf5109
binbinHan <han_binbin@163.com>
2021-10-15 14:43:27 +0800

    Refine check vec equal (#6536)
    * fix_bug_about_AsyncTransportCtx
    
    * refine CheckVecEqual
    
    * refine
    
    * refine
    
    * minor fix
    
    * Update tensor_utils.cpp
    
    * Update tensor_utils.cpp
    
    * auto format by CI
    
    Co-authored-by: Yu OuYang <xuanjiuye@gmail.com>
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

--- oneflow/core/framework/data_consistency_check.cpp
+++ oneflow/core/framework/data_consistency_check.cpp
@@ -13,62 +13,42 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/core/common/util.h"
+#include <cstring>
 #include "oneflow/core/framework/data_consistency_check.h"
 #include "oneflow/core/job/rank_group.h"
 #include "oneflow/core/framework/transport_util.h"
 
 namespace oneflow {
 
-namespace {
-
-template<typename T>
-bool CheckVecEqual(size_t size, const T* in0, const T* in1) {
-  for (size_t i = 0; i < size; ++i) {
-    if (*(in0 + i) != *(in1 + i)) { return false; }
-  }
-  return true;
-}
-
-}  // namespace
-
-template<typename T>
-Maybe<void> DataConsistencyCheck(const void* buffer_ptr, size_t elem_cnt,
+Maybe<void> DataConsistencyCheck(const void* buffer_ptr, size_t buffer_size,
                                  Symbol<ParallelDesc> placement) {
   const auto& rank_group = JUST(RankGroup::New(placement));
-  size_t data_size = elem_cnt * sizeof(T);
 
-  std::vector<T> recv_buffer(elem_cnt);
-  T* recv_ptr = recv_buffer.data();
+  std::vector<char> recv_buffer(buffer_size);
+  char* recv_ptr = recv_buffer.data();
 
   TransportToken transport_token = JUST(TransportToken::NewTransportToken(kTransportTokenTypeData));
   NaiveAsyncTransportCtx ctx(
       transport_token,
       [&](void** buffer, std::size_t* size, std::function<void()>* Cb) -> Maybe<void> {
         *buffer = const_cast<void*>(buffer_ptr);
-        *size = data_size;
+        *size = buffer_size;
         *Cb = [] {};
         return Maybe<void>::Ok();
       },
       [&](void** buffer, std::size_t* size, std::function<void()>* Cb) -> Maybe<void> {
         *buffer = recv_ptr;
-        *size = data_size;
+        *size = buffer_size;
         *Cb = [] {};
         return Maybe<void>::Ok();
       });
   JUST(TransportUtil::SendToNextRankInRing(rank_group, transport_token, &ctx));
   JUST(TransportUtil::ReceiveFromPrevRankInRing(rank_group, transport_token, &ctx));
   JUST(TransportUtil::WaitUntilDoneOrTimeout(ctx, TransportUtil::TimeoutSeconds()));
-  CHECK_OR_RETURN(CheckVecEqual(elem_cnt, reinterpret_cast<const T*>(buffer_ptr), recv_ptr))
+  CHECK_OR_RETURN(std::memcmp(buffer_ptr, reinterpret_cast<const void*>(recv_ptr), buffer_size)
+                  == 0)
       << "Each rank must have same input sequence or numpy array";
   return Maybe<void>::Ok();
 }
-
-#define INSTATIATION_DATA_CONSISTENCY_CHECK(type_cpp, type_proto)                              \
-  template Maybe<void> DataConsistencyCheck<type_cpp>(const void* buffer_ptr, size_t elem_cnt, \
-                                                      Symbol<ParallelDesc> placement);
-
-OF_PP_FOR_EACH_TUPLE(INSTATIATION_DATA_CONSISTENCY_CHECK, POD_DATA_TYPE_SEQ)
-#undef INSTATIATION_DATA_CONSISTENCY_CHECK
 
 }  // namespace oneflow
