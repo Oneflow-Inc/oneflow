b5e1513656614d1aedb3ac7fcacea9b952afb2f3
Twice <i@twice.moe>
2021-10-18 10:51:55 +0800

    Update oneflow/core/intrusive/force_standard_layout_test.cpp

--- oneflow/core/intrusive/force_standard_layout_test.cpp
+++ oneflow/core/intrusive/force_standard_layout_test.cpp
@@ -77,7 +77,7 @@ TEST(ForceStandardLayout, move_constructor) {
 TEST(ForceStandardLayout, copy_assign) {
   int value = Unstandard::standard_value;
   const ForceStandardLayout<Unstandard> const_sl(&value);
-  ForceStandardLayout<Unstandard> sl = const_sl;
+  ForceStandardLayout<Unstandard> sl = const_sl; // NOLINT
   ASSERT_EQ(sl.Get().x, Unstandard::unstandard_value);
   ASSERT_EQ(sl.Get().ptr(), &value);
 }
