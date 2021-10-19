357da40a8a6767c8ce0fea2e256444872ada23d9
guo ran <360112263@qq.com>
2021-10-19 10:55:13 +0800

    concat/split_like use copy_nd_primitive (#6547)
    * concat/split_like use copy_nd_primitive
    
    * rm NewKernelUtil::CopyColsRegion
    
    * auto format by CI
    
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

--- oneflow/user/kernels/split_like_kernel.cpp
+++ oneflow/user/kernels/split_like_kernel.cpp
@@ -14,13 +14,17 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/framework/framework.h"
-#include "oneflow/core/kernel/new_kernel_util.h"
+#include "oneflow/core/primitive/include/copy_nd.h"
 
 namespace oneflow {
 
 namespace {
 
-template<DeviceType device_type, typename T>
+template<typename Context>
+std::unique_ptr<primitive::CopyNd> NewCopyNdPrimitive(Context* ctx) {
+  return primitive::NewPrimitive<primitive::CopyNdFactory>(ctx->device_type(), 2);
+}
+
 class SplitLikeKernel final : public user_op::OpKernel {
  public:
   SplitLikeKernel() = default;
@@ -65,6 +69,9 @@ class SplitLikeKernel final : public user_op::OpKernel {
     const int64_t in_cols = in_tensor->shape().Count(axis);
     const int64_t rows = in_tensor->shape().elem_cnt() / in_cols;
     CHECK_GT(rows, 0);
+
+    auto primitive = NewCopyNdPrimitive(ctx);
+    CHECK(primitive);
     int64_t in_col_offset = 0;
     for (const auto& out_arg_pair : ctx->outputs()) {
       user_op::Tensor* out_tensor =
@@ -72,9 +79,14 @@ class SplitLikeKernel final : public user_op::OpKernel {
       const int64_t out_cols = out_tensor->shape().Count(axis);
       CHECK_EQ(out_tensor->shape().elem_cnt(), rows * out_cols);
       if (out_cols > 0) {
-        NewKernelUtil<device_type>::CopyColsRegion(ctx->device_ctx(), rows, out_cols,
-                                                   in_tensor->dptr<T>(), in_col_offset, in_cols,
-                                                   out_tensor->mut_dptr<T>(), 0, out_cols);
+        DimVector dst_shape = {rows, out_cols};
+        DimVector dst_pos_vec = {0, 0};
+        DimVector src_shape = {rows, in_cols};
+        DimVector src_pos_vec = {0, in_col_offset};
+        DimVector extent_vec = {rows, out_cols};
+        primitive->Launch(ctx->stream_ctx(), out_tensor->data_type(), 2, out_tensor->mut_dptr(),
+                          dst_shape.data(), dst_pos_vec.data(), in_tensor->dptr(), src_shape.data(),
+                          src_pos_vec.data(), extent_vec.data());
       }
       in_col_offset += out_cols;
     }
@@ -84,25 +96,17 @@ class SplitLikeKernel final : public user_op::OpKernel {
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
+hob::HobContextGetter<user_op::KernelRegContext, bool> CopyNdPrimitiveExists() {
+  return user_op::HobCtxGetter<bool>("CopyNdPrimitiveExists",
+                                     [](const user_op::KernelRegContext& ctx) {
+                                       return NewCopyNdPrimitive(&ctx).operator bool();
+                                     });
+}
+
 }  // namespace
 
-#define REGISTER_SPLIT_LIKE_KERNEL(device, dtype)          \
-  REGISTER_USER_KERNEL("split_like")                       \
-      .SetCreateFn<SplitLikeKernel<device, dtype>>()       \
-      .SetIsMatchedHob((user_op::HobDeviceTag() == device) \
-                       & (user_op::HobDataType("out", 0) == GetDataType<dtype>::value));
-
-#define REGISTER_SPLIT_LIKE_KERNEL_WITH_DEVICE(device) \
-  REGISTER_SPLIT_LIKE_KERNEL(device, float)            \
-  REGISTER_SPLIT_LIKE_KERNEL(device, double)           \
-  REGISTER_SPLIT_LIKE_KERNEL(device, int8_t)           \
-  REGISTER_SPLIT_LIKE_KERNEL(device, int32_t)          \
-  REGISTER_SPLIT_LIKE_KERNEL(device, int64_t)
-
-REGISTER_SPLIT_LIKE_KERNEL_WITH_DEVICE(DeviceType::kCPU)
-#ifdef WITH_CUDA
-REGISTER_SPLIT_LIKE_KERNEL_WITH_DEVICE(DeviceType::kGPU)
-REGISTER_SPLIT_LIKE_KERNEL(DeviceType::kGPU, float16)
-#endif
+REGISTER_USER_KERNEL("split_like")
+    .SetCreateFn<SplitLikeKernel>()
+    .SetIsMatchedHob(CopyNdPrimitiveExists() == true);
 
 }  // namespace oneflow
