25504b11f717baf9bc0f8a5965dd2f763d30c3b6
Houjiang Chen <chenhoujiangcug@gmail.com>
2021-10-22 22:05:32 +0800

    decrease ref

--- oneflow/api/python/framework/tensor.cpp
+++ oneflow/api/python/framework/tensor.cpp
@@ -53,9 +53,9 @@ void ApiEagerMirroredTensorZeros(const std::shared_ptr<Tensor>& tensor) {
 
 template<typename T>
 void ApiCopyMirroredTensorToNumpy(const std::shared_ptr<Tensor>& tensor, py::array_t<T> array) {
-  return CopyBetweenMirroredTensorAndNumpy<T>(tensor, array.ptr(),
-                                              OfBlob_CopyBuffer::template To<T>, "const",
-                                              /*block_host_until_done=*/true)
+  CopyBetweenMirroredTensorAndNumpy<T>(tensor, array.ptr(), OfBlob_CopyBuffer::template To<T>,
+                                       "const",
+                                       /*block_host_until_done=*/true)
       .GetOrThrow();
 }
 
@@ -64,11 +64,12 @@ void ApiCopyMirroredTensorFromNumpy(const std::shared_ptr<Tensor>& tensor, py::a
   // When asynchronously copying array data to tensor, we need to back up the
   // array at the same time.
   // Only NPY_CORDER is supported, and it makes sure that the array is C-style contiguous.
-  auto* copied_array = PyArray_NewCopy((PyArrayObject*)array.ptr(), NPY_CORDER);
-  return CopyBetweenMirroredTensorAndNumpy<T>(tensor, copied_array,
-                                              OfBlob_CopyBuffer::template From<T>, "mut",
-                                              /*block_host_until_done=*/false)
+  CopyBetweenMirroredTensorAndNumpy<T>(tensor, copied_array, OfBlob_CopyBuffer::template From<T>,
+                                       "mut",
+                                       /*block_host_until_done=*/false)
       .GetOrThrow();
+
+  Py_DECREF(copied_array);
 }
 
 const std::string& ApiGetCopyMirroredTensorToNumpyFuncName(const Tensor& tensor) {
