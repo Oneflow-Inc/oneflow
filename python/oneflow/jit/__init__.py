2e34456c82f7f9754f56f4499e7fba8b42fc5313
jackalcooper <jackalcooper@gmail.com>
2021-10-23 20:40:26 +0800

    add check

--- python/oneflow/jit/__init__.py
+++ python/oneflow/jit/__init__.py
@@ -4,6 +4,7 @@ import uuid
 
 def trace(f):
     def wrapper(*args, **kwargs):
+        assert isinstance(args[0], oneflow.nn.Module)
         func_name = str(uuid.uuid4()).replace("-", "")
         func_name = f"jit{func_name}"
         assert oneflow._oneflow_internal.ir.toggle_jit(func_name)
