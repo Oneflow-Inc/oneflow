cb79f785782c6f412155540206cf9128006ba66b
Flowingsun007 <flowingsun007@163.com>
2021-10-23 12:18:35 +0800

    refine

--- python/oneflow/framework/tensor.py
+++ python/oneflow/framework/tensor.py
@@ -482,8 +482,14 @@ def _unsqueeze(self, dim):
     return flow._C.unsqueeze(self, dim=dim)
 
 
-def _permute(self, dims):
-    return flow._C.permute(self, dims=dims)
+def _permute(self, *dims):
+    if len(dims) == 1:
+        new_dims = dims[0]
+        if isinstance(new_dims, int):
+            new_dims = (new_dims, )
+    else:
+        new_dims = dims
+    return flow._C.transpose(self, new_dims)
 
 
 def _matmul(self, other):
