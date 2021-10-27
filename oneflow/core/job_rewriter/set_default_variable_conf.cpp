07a2ee6473a59e3cca6be7f349314dbc6f3d8652
Zhanghuihong <garfield.gzhh@gmail.com>
2021-10-27 19:23:33 +0800

    double check for unnecessary changes

--- oneflow/core/job_rewriter/set_default_variable_conf.cpp
+++ oneflow/core/job_rewriter/set_default_variable_conf.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/api/python/env/env.h"
 #include "oneflow/core/common/multi_client.h"
 #include "oneflow/core/job_rewriter/job_pass.h"
 #include "oneflow/core/job/global_for.h"
