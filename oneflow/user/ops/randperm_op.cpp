d973109c0ab0ba9dba2b7ac27122846d070a49d9
Zhanghuihong <garfield.gzhh@gmail.com>
2021-10-27 19:26:53 +0800

    remove unnecessary changes

--- oneflow/user/ops/randperm_op.cpp
+++ oneflow/user/ops/randperm_op.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/api/python/env/env.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/common/global.h"
 #include "oneflow/core/common/multi_client.h"
