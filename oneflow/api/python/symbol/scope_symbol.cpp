98a77e6e9b1430fb3ad4d4cf0381c64787e68c81
Twice <i@twice.moe>
2021-10-29 15:43:28 +0800

    Update oneflow/api/python/symbol/scope_symbol.cpp

--- oneflow/api/python/symbol/scope_symbol.cpp
+++ oneflow/api/python/symbol/scope_symbol.cpp
@@ -42,7 +42,7 @@ ONEFLOW_API_PYBIND11_MODULE("", m) {
                                if (!x.symbol_id().has_value()) {
                                  THROW(RuntimeError) << "symbol_id not initialized";
                                }
-                               return x.symbol_id().value();
+                               return CHECK_JUST(x.symbol_id());
                              })
       .def_property_readonly("_proto_str",
                              [](const Scope& x) { return PbMessage2TxtString(x.scope_proto()); })
