5d30cdcca0b74a6d90188dc6b1999a24be3941e8
YaoChi <later@usopp.net>
2021-11-01 16:40:21 +0800

    reformat

--- python/oneflow/test/modules/test_parital_fc.py
+++ python/oneflow/test/modules/test_parital_fc.py
@@ -19,6 +19,7 @@ from oneflow.test_utils.automated_test_util import *
 import oneflow as flow
 import oneflow.unittest
 
+
 @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
 class TestParitalFC(flow.unittest.TestCase):
     def test_parital_fc(test_case):
