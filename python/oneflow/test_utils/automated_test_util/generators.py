066ab7603bc9f7f1a7ac79b56a8ff186059ac91e
strint <xiaoyulink@gmail.com>
2021-11-04 20:10:01 +0800

    fix gen

--- python/oneflow/test_utils/automated_test_util/generators.py
+++ python/oneflow/test_utils/automated_test_util/generators.py
@@ -248,7 +248,7 @@ class random(generator):
             val = float(rng.random() * (high - low) + low)
         elif annotation == bool:
             val = random_util.choice([True, False])
-        elif annotation == NoneType:
+        elif annotation is None:
             val = None
         else:
             raise NotImplementedError(
