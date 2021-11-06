e16461fac2eb9694aa13a535e894a0389049811d
jackalcooper <jackalcooper@gmail.com>
2021-11-06 19:37:20 +0800

    fix

--- oneflow/ir/test/OneFlow/test_fuse_cast_scale.py
+++ oneflow/ir/test/OneFlow/test_fuse_cast_scale.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+# RUN: python3 %s | FileCheck %s
 from typing import Tuple
 import unittest
 import numpy as np
@@ -78,6 +79,8 @@ class TestMLIROptimizations(flow.unittest.TestCase):
         (loss, scale) = ret
         test_case.assertTrue(np.allclose(loss, x * scale))
 
+
+# CHECK: %0 = oneflow.mlir_jit
 
 if __name__ == "__main__":
     unittest.main()
