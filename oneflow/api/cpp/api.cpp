9388f708ad99954fb8ce51ff69c4075d478591e2
mosout <mosout@qq.com>
2021-11-08 17:23:58 +0800

    refine initEnv

--- oneflow/api/cpp/api.cpp
+++ oneflow/api/cpp/api.cpp
@@ -13,11 +13,23 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+#include <glog/logging.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <random>
+#include <type_traits>
 #include "api.h"
 #include "oneflow/core/common/just.h"
 #include "oneflow/core/common/multi_client.h"
+#include "oneflow/core/common/optional.h"
 #include "oneflow/core/framework/shut_down_util.h"
 #include "oneflow/core/job/cluster_instruction.h"
+#include "oneflow/core/job/env.pb.h"
 #include "oneflow/core/job/env_global_objects_scope.h"
 #include "oneflow/core/control/ctrl_bootstrap.h"
 #include "oneflow/core/rpc/include/base.h"
@@ -31,14 +43,70 @@ namespace {  // for inltialize
 
 inline bool isEnvInited() { return of::Global<of::EnvGlobalObjectsScope>::Get() != nullptr; }
 
+bool hasEnvVar(const std::string& key) {
+  const char* value = getenv(key.c_str());
+  return value != nullptr;
+}
+
+std::string getEnvVar(const std::string& key, const std::string& default_value) {
+  const char* value = getenv(key.c_str());
+  if (value == nullptr) { return default_value; }
+  return std::string(value);
+}
+
+int64_t getEnvVar(const std::string& key, int64_t default_value) {
+  const char* value = getenv(key.c_str());
+  if (value == nullptr) { return default_value; }
+  return std::atoll(value);
+}
+
+int32_t findFreePort(const std::string& addr) {
+#ifdef __linux__
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  CHECK_GE(sock, 0) << "fail to find a free port.";
+  int optval = 1;
+  setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_int_distribution<std::mt19937::result_type> dist(1, 1000);
+
+  int count = 0;
+  int num_attempts = 200;
+  do {
+    int port = 5000 + dist(rng);
+    struct sockaddr_in sockaddr {};
+    memset(&sockaddr, 0, sizeof(sockaddr));
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_port = htons(port);
+    sockaddr.sin_addr.s_addr = inet_addr(addr.c_str());
+    int error = bind(sock, (struct sockaddr*)&sockaddr, sizeof(sockaddr));
+    if (error == 0) { return port; }
+    ++count;
+  } while (count < num_attempts);
+  CHECK_NE(count, num_attempts) << "fail to find a free port.";
+#endif  // __linux__
+  return -1;
+}
+
 of::Maybe<void> initEnv() {
-  const auto completeEnvProto = [](of::EnvProto& env_proto) {
+  const auto completeEnvProto = [&](of::EnvProto& env_proto) {
     auto bootstrap_conf = env_proto.mutable_ctrl_bootstrap_conf();
     auto master_addr = bootstrap_conf->mutable_master_addr();
-    master_addr->set_host("127.0.0.1");
-    master_addr->set_port(38799);
-    bootstrap_conf->set_world_size(1);
-    bootstrap_conf->set_rank(0);
+    const std::string addr = getEnvVar("MASTER_ADDR", "127.0.0.1");
+    master_addr->set_host(addr);
+    master_addr->set_port(getEnvVar("MASTER_PORT", findFreePort(addr)));
+    bootstrap_conf->set_world_size(getEnvVar("WORLD_SIZE", 1));
+    bootstrap_conf->set_rank(getEnvVar("RANK", 0));
+
+    auto cpp_logging_conf = env_proto.mutable_cpp_logging_conf();
+    if (hasEnvVar("GLOG_log_dir")) { cpp_logging_conf->set_log_dir(getEnvVar("GLOG_log_dir", "")); }
+    if (hasEnvVar("GLOG_logtostderr")) {
+      cpp_logging_conf->set_logtostderr(getEnvVar("GLOG_logtostderr", -1));
+    }
+    if (hasEnvVar("GLOG_logbuflevel")) {
+      cpp_logging_conf->set_logbuflevel(getEnvVar("GLOG_logbuflevel", -1));
+    }
   };
 
   of::EnvProto env_proto;
