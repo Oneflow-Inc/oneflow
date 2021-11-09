bed283ef0304dd94a291120d598f8d910aab7f5e
lambda7xx <lambda7xx@gmail.com>
2021-11-09 15:47:55 +0800

    remove the bash in the callback

--- mpush.sh
+++
@@ -1,5 +0,0 @@
-git push test start-callback 
-cd build 
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON
-
-make -j$(nproc)
--- xpull.sh
+++
@@ -1,7 +0,0 @@
-git pull test start-callback
-
-cd build 
-
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON  -DCUDNN_ROOT_DIR=/usr/local/cudnn
-
-make -j$(nproc)
