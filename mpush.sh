38b72c82a3ecfe81d53f6f341693286ccb6e4a4e
lambda7xx <lambda7xx@gmail.com>
2021-11-09 15:43:29 +0800

    remove the bash and push it to the master

--- mpush.sh
+++
@@ -1,5 +0,0 @@
-git push test start-serial 
-cd build 
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON
-
-make -j$(nproc)
--- xpull.sh
+++
@@ -1,7 +0,0 @@
-git pull test start-serial 
-
-cd build 
-
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON  -DCUDNN_ROOT_DIR=/usr/local/cudnn
-
-make -j$(nproc)
