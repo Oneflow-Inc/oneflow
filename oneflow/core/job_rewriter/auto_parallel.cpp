d5047046aa6dd804c19cd6fcf020e12462c62f24
Yipeng Li <jamesonli1313@gmail.com>
2021-11-11 12:28:54 +0800

    Only print sbp graph in rank 0.

--- oneflow/core/job_rewriter/auto_parallel.cpp
+++ oneflow/core/job_rewriter/auto_parallel.cpp
@@ -19,6 +19,7 @@ limitations under the License.
 #include "oneflow/core/job/job.pb.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/auto_parallel/sbp_constructor.h"
+#include "oneflow/core/rpc/include/global_process_ctx.h"
 
 namespace oneflow {
 
@@ -33,7 +34,11 @@ class AutoParallelPass final : public JobPass {
   Maybe<void> Apply(const OpGraph& op_graph, Job* job) const;
 
   Maybe<void> Apply(Job* job, JobPassCtx* ctx) const override {
-    if (!job->job_conf().enable_auto_parallel()) { return Maybe<void>::Ok(); }
+    if (!job->job_conf().enable_auto_parallel()) {  
+      // Print Auto Parallel SBP
+      Global<OpGraph>::Get()->PrintSBPGraphDebugInfo();
+      return Maybe<void>::Ok();
+    }
     LOG(INFO) << "=== Enable AutoParallel ===";
     const OpGraph op_graph(*job);
     return Apply(op_graph, job);
@@ -53,7 +58,7 @@ Maybe<void> AutoParallelPass::Apply(const OpGraph& op_graph, Job* job) const {
   std::cout << "Auto parallel took "
             << std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_begin).count()
             << " ms\n";
-  sbp_constructor.PrintSBPGraphDebugInfo();
+  if (GlobalProcessCtx::Rank() == 0) sbp_constructor.PrintSBPGraphDebugInfo();
   return Maybe<void>::Ok();
 }
 
