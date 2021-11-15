e82f3f1a03d230d2a38acdc66b91f8c85c748516
lambda7xx <lambda7xx@gmail.com>
2021-11-15 14:16:06 +0800

    continue to debug

--- mpush.sh
+++ mpush.sh
@@ -1,5 +1,6 @@
-git push test start-epoll
+#git push test start-epoll
+#git remote set-utl test 
+git push 
 cd build 
 cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON
 
-make -j$(nproc)
