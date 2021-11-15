7aaf734d9c73549d3aa9b1fd7757919ed55c0c0d
lambda7xx <lambda7xx@gmail.com>
2021-11-15 17:59:02 +0800

    fix the format of socket_message v0.14

--- oneflow/core/comm_network/epoll/socket_read_helper.cpp
+++ oneflow/core/comm_network/epoll/socket_read_helper.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <ios>
 #ifdef __linux__
 
 #include "oneflow/core/comm_network/epoll/socket_read_helper.h"
