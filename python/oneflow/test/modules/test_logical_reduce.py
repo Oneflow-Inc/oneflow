b51c1bd14597118103f6160f8a35cc741015c6d8
simonJJJ <821898965@qq.com>
2021-11-16 15:54:45 +0800

    testcase

---
+++ python/oneflow/test/modules/test_logical_reduce.py
@@ -0,0 +1,58 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+from collections import OrderedDict
+
+import numpy as np
+
+from oneflow.test_utils.automated_test_util import *
+from test_util import GenArgList
+
+import oneflow as flow
+import oneflow.unittest
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestLogicalReduce(flow.unittest.TestCase):
+    @autotest(auto_backward=False)
+    def test_all_with_random_data(test_case):
+        device = random_device()
+        dim = random(1, 4).to(int)
+        x = random_pytorch_tensor(ndim=4, dtype=float, requires_grad=False).to(device)
+        return torch.all(x, dim)
+
+    @autotest(auto_backward=False)
+    def test_any_with_random_data(test_case):
+        device = random_device()
+        dim = random(1, 4).to(int)
+        x = random_pytorch_tensor(ndim=4, dtype=float, requires_grad=False).to(device)
+        return torch.any(x, dim)
+
+    @autotest(auto_backward=False)
+    def test_scalar_reduce_all_with_random_data(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=4, dtype=float, requires_grad=False).to(device)
+        return torch.all(x)
+
+    @autotest(auto_backward=False)
+    def test_scalar_reduce_any_with_random_data(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=4, dtype=float, requires_grad=False).to(device)
+        return torch.any(x)
+
+
+if __name__ == "__main__":
+    unittest.main()
