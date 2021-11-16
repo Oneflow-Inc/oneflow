129201b21f2e4e490e765e1dbbc8aaec7ad8590f
lambda7xx <lambda7xx@gmail.com>
2021-11-16 16:19:59 +0800

    cean the bash

--- mpush.sh
+++
@@ -1,8 +0,0 @@
-git push upstream clean-start-epoll-again
-
-cd build 
-
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON
-
-
-make -j$(nproc)
--- xpull.sh
+++
@@ -1,8 +0,0 @@
-git pull upstream clean-start-epoll-again
-
-cd build 
-
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON -DCUDNN_ROOT_DIR=/usr/local/cudnn
-
-
-make -j$(nproc)
