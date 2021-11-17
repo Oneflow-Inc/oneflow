ec3805561aa53603e8bfc09f7d76287a95433f83
Juncheng <liujuncheng1022@gmail.com>
2021-11-18 05:40:13 +0800

    [ep::Stream] Cuda Graphs /  ExecutionContextHook (#6801)
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- oneflow/core/stream/common/stream_context_adapter.cpp
+++ oneflow/core/stream/common/stream_context_adapter.cpp
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/stream/include/stream_context_adapter.h"
-#include "oneflow/core/stream/cuda/cuda_stream_context.h"
 
 namespace oneflow {
 
@@ -41,7 +40,7 @@ class DeviceCtxStreamContextAdapter : public StreamContext {
 
 #ifdef WITH_CUDA
 
-class CudaDeviceCtxStreamContextAdapter : public CudaStreamContext {
+class CudaDeviceCtxStreamContextAdapter : public StreamContext {
  public:
   OF_DISALLOW_COPY_AND_MOVE(CudaDeviceCtxStreamContextAdapter);
   explicit CudaDeviceCtxStreamContextAdapter(DeviceCtx* device_ctx) : device_ctx_(device_ctx) {}
@@ -55,12 +54,6 @@ class CudaDeviceCtxStreamContextAdapter : public CudaStreamContext {
   DeviceType device_type() const override { return device_ctx_->device_type(); }
 
   ep::Stream* stream() override { return device_ctx_->stream(); }
-
-  cudaStream_t cuda_stream() const override { return device_ctx_->cuda_stream(); }
-
-  cublasHandle_t cublas_handle() const override { return device_ctx_->cublas_handle(); }
-
-  cudnnHandle_t cudnn_handle() const override { return device_ctx_->cudnn_handle(); }
 
  private:
   DeviceCtx* device_ctx_;
