8b0d6edede882a7056bfd96bc5e270b151ea4762
MARD1NO <359521840@qq.com>
2021-11-22 13:47:43 +0800

    add more test case

--- python/oneflow/test/modules/test_batchnorm_add_relu.py
+++ python/oneflow/test/modules/test_batchnorm_add_relu.py
@@ -280,6 +280,81 @@ def _test_bn_relu_track_running_states_false(test_case, device, channel, height,
     test_case.assertIsNone(origin_batch_norm.running_var)
 
 
+def _test_bn_add_relu_eval(test_case, device, channel, height, width):
+    weight_numpy = np.random.randn(channel)
+    bias_numpy = np.random.randn(channel)
+
+    fused_x = np.random.randn(1, channel, height, width)
+    fused_x_tensor = flow.Tensor(fused_x).to(device)
+
+    fused_addend = np.random.randn(1, channel, height, width)
+    fused_addend_tensor = flow.Tensor(fused_addend).to(device)
+
+    fused_weight_tensor = flow.nn.Parameter(flow.Tensor(weight_numpy).to(device))
+    fused_bias_tensor = flow.nn.Parameter(flow.Tensor(bias_numpy).to(device))
+
+    fused_bn = flow.nn.FusedBatchNorm2d(channel).to(device)
+    fused_bn.eval()
+    fused_bn.weight = fused_weight_tensor
+    fused_bn.bias = fused_bias_tensor
+    fused_out = fused_bn(fused_x_tensor, fused_addend_tensor)
+
+    origin_x_tensor = flow.Tensor(fused_x).to(device)
+
+    origin_addend_tensor = flow.Tensor(fused_addend).to(device)
+
+    origin_weight_tensor = flow.nn.Parameter(flow.Tensor(weight_numpy).to(device))
+    origin_bias_tensor = flow.nn.Parameter(flow.Tensor(bias_numpy).to(device))
+
+    origin_batch_norm = flow.nn.BatchNorm2d(channel).to(device)
+    origin_batch_norm.eval()
+    origin_batch_norm.weight = origin_weight_tensor
+    origin_batch_norm.bias = origin_bias_tensor
+
+    origin_out = origin_batch_norm(origin_x_tensor) + origin_addend_tensor
+    origin_out = flow.nn.functional.relu(origin_out)
+
+    # test output.
+    test_case.assertTrue(
+        np.allclose(fused_out.numpy(), origin_out.numpy(), atol=1e-4, rtol=1e-4)
+    )
+
+
+def _test_bn_relu_eval(test_case, device, channel, height, width):
+    weight_numpy = np.random.randn(channel)
+    bias_numpy = np.random.randn(channel)
+
+    fused_x = np.random.randn(1, channel, height, width)
+    fused_x_tensor = flow.Tensor(fused_x).to(device)
+
+    fused_weight_tensor = flow.nn.Parameter(flow.Tensor(weight_numpy).to(device))
+    fused_bias_tensor = flow.nn.Parameter(flow.Tensor(bias_numpy).to(device))
+
+    fused_bn = flow.nn.FusedBatchNorm2d(channel).to(device)
+    fused_bn.eval()
+    fused_bn.weight = fused_weight_tensor
+    fused_bn.bias = fused_bias_tensor
+    fused_out = fused_bn(fused_x_tensor)
+
+    origin_x_tensor = flow.Tensor(fused_x).to(device)
+
+    origin_weight_tensor = flow.nn.Parameter(flow.Tensor(weight_numpy).to(device))
+    origin_bias_tensor = flow.nn.Parameter(flow.Tensor(bias_numpy).to(device))
+
+    origin_batch_norm = flow.nn.BatchNorm2d(channel).to(device)
+    origin_batch_norm.eval()
+    origin_batch_norm.weight = origin_weight_tensor
+    origin_batch_norm.bias = origin_bias_tensor
+
+    origin_out = origin_batch_norm(origin_x_tensor)
+    origin_out = flow.nn.functional.relu(origin_out)
+
+    # test output.
+    test_case.assertTrue(
+        np.allclose(fused_out.numpy(), origin_out.numpy(), atol=1e-4, rtol=1e-4)
+    )
+
+
 @flow.unittest.skip_unless_1n1d()
 @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test gpu cases")
 class TestBnAddRelu(flow.unittest.TestCase):
@@ -289,6 +364,8 @@ class TestBnAddRelu(flow.unittest.TestCase):
             _test_bn_add_relu,
             _test_bn_relu,
             _test_bn_relu_track_running_states_false,
+            _test_bn_add_relu_eval,
+            _test_bn_relu_eval,
         ]
         arg_dict["device"] = ["cpu", "cuda"]
         arg_dict["channels"] = [4, 6, 8, 10]
