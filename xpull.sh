388e5aa76b82cae97d2189ce27b227b7897684f4
lambda7xx <lambda7xx@gmail.com>
2021-11-24 15:19:24 +0800

    sunccess do the epoll

--- xpull.sh
+++
@@ -1,7 +0,0 @@
-git pull upstream test-token 
-
-cd build 
-
-cmake .. -C ../cmake/caches/cn/cuda.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_RDMA=ON -DCUDNN_ROOT_DIR=/usr/local/cudnn
-
-make -j$(nproc)
