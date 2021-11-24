89b20822fa5d9d32ec82c04b98d2210030484a95
bbuf <1182563586@qq.com>
2021-11-24 16:34:06 +0800

    delete useless file

--- python/oneflow/test/modules/test_dropout.py
+++ python/oneflow/test/modules/test_dropout.py
@@ -35,20 +35,20 @@ class TestDropout(flow.unittest.TestCase):
         m = torch.nn.Dropout(p=0.0)
         return m(x)
 
-    # @autotest()
-    # def test_dropout_p1(test_case):
-    #     device = random_device()
-    #     x = random_pytorch_tensor(ndim=random(), dim0=random()).to(device)
-    #     m = torch.nn.Dropout(p=1.0)
-    #     return m(x)
+    @autotest()
+    def test_dropout_p1(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=random(), dim0=random()).to(device)
+        m = torch.nn.Dropout(p=1.0)
+        return m(x)
 
-    # @autotest()
-    # def test_dropout_eval(test_case):
-    #     device = random_device()
-    #     x = random_pytorch_tensor(ndim=random(), dim0=random()).to(device)
-    #     m = torch.nn.Dropout(p=1.0)
-    #     m.eval()
-    #     return m(x)
+    @autotest()
+    def test_dropout_eval(test_case):
+        device = random_device()
+        x = random_pytorch_tensor(ndim=random(), dim0=random()).to(device)
+        m = torch.nn.Dropout(p=1.0)
+        m.eval()
+        return m(x)
 
 
 if __name__ == "__main__":
