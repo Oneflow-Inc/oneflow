fcfd9cdd1e9cdb63483558e964ff8ecde8d92cf0
guo-ran <360112263@qq.com>
2021-11-24 20:34:16 +0800

    fix T mask to int mask, refine code

--- oneflow/core/cuda/layer_norm.cuh
+++ oneflow/core/cuda/layer_norm.cuh
@@ -47,8 +47,8 @@ __inline__ __device__ double Div<double>(double a, double b) {
 }
 
 template<class Func>
-inline cudaError_t GetNumBlocks(Func func, int64_t block_size, size_t dynamic_smem_size, int64_t max_blocks, int64_t waves,
-                                int* num_blocks) {
+inline cudaError_t GetNumBlocks(Func func, int64_t block_size, size_t dynamic_smem_size,
+                                int64_t max_blocks, int64_t waves, int* num_blocks) {
   int dev;
   {
     cudaError_t err = cudaGetDevice(&dev);
@@ -61,8 +61,8 @@ inline cudaError_t GetNumBlocks(Func func, int64_t block_size, size_t dynamic_sm
   }
   int max_active_blocks;
   {
-    cudaError_t err =
-        cudaOccupancyMaxActiveBlocksPerMultiprocessor(&max_active_blocks, func, block_size, dynamic_smem_size);
+    cudaError_t err = cudaOccupancyMaxActiveBlocksPerMultiprocessor(&max_active_blocks, func,
+                                                                    block_size, dynamic_smem_size);
   }
   *num_blocks =
       std::max<int>(1, std::min<int64_t>(max_blocks, sm_count * max_active_blocks * waves));
@@ -170,7 +170,7 @@ __inline__ __device__ void WelfordWarpReduce(T thread_mean, T thread_m2, T threa
   *mean = thread_mean;
   *m2 = thread_m2;
   *count = thread_count;
-  for (T mask = thread_group_width / 2; mask > 0; mask /= 2) {
+  for (int mask = thread_group_width / 2; mask > 0; mask /= 2) {
     T b_mean = __shfl_down_sync(0xffffffff, *mean, mask);
     T b_m2 = __shfl_down_sync(0xffffffff, *m2, mask);
     T b_count = __shfl_down_sync(0xffffffff, *count, mask);
@@ -255,16 +255,17 @@ __global__ void LayerNormWarpImpl(LOAD load, STORE store, const int64_t rows, co
 #pragma unroll
       for (int pack_id = 0; pack_id < num_packs; ++pack_id) {
         const int col = (pack_id * thread_group_width + lane_id) * pack_size;
+        const int pack_offset = pack_id * pack_size;
         if (!padding || col < cols) {
-          load.template load<pack_size>(row_buf + pack_id * pack_size, row + row_id, col);
+          load.template load<pack_size>(row_buf + pack_offset, row + row_id, col);
 #pragma unroll
           for (int i = 0; i < pack_size; ++i) {
-            WelfordCombine(row_buf[pack_id * pack_size + i], thread_mean + row_id,
-                           thread_m2 + row_id, thread_count + row_id);
+            WelfordCombine(row_buf[pack_offset + i], thread_mean + row_id, thread_m2 + row_id,
+                           thread_count + row_id);
           }
         } else {
 #pragma unroll
-          for (int i = 0; i < pack_size; ++i) { row_buf[pack_id * pack_size + i] = 0; }
+          for (int i = 0; i < pack_size; ++i) { row_buf[pack_offset + i] = 0; }
         }
       }
     }
@@ -273,18 +274,18 @@ __global__ void LayerNormWarpImpl(LOAD load, STORE store, const int64_t rows, co
     ComputeType warp_count[rows_per_access];
 #pragma unroll
     for (int row_id = 0; row_id < rows_per_access; ++row_id) {
+      int global_row_id = row + row_id;
       ComputeType* row_buf = buf[row_id];
       WelfordWarpAllReduce<ComputeType, thread_group_width>(
           thread_mean[row_id], thread_m2[row_id], thread_count[row_id], warp_mean + row_id,
           warp_m2 + row_id, warp_count + row_id);
       ComputeType row_mean = warp_mean[row_id];
       ComputeType row_variance =
-          max(Div(warp_m2[row_id], static_cast<ComputeType>(warp_count[row_id])),
-              static_cast<ComputeType>(0.0));
+          max(Div(warp_m2[row_id], warp_count[row_id]), static_cast<ComputeType>(0.0));
       ComputeType row_inv_var = rsqrt(row_variance + static_cast<ComputeType>(epsilon));
       if (lane_id == 0) {
-        mean[row + row_id] = row_mean;
-        inv_variance[row + row_id] = row_inv_var;
+        mean[global_row_id] = row_mean;
+        inv_variance[global_row_id] = row_inv_var;
       }
 #pragma unroll
       for (int i = 0; i < cols_per_thread; ++i) {
@@ -294,7 +295,7 @@ __global__ void LayerNormWarpImpl(LOAD load, STORE store, const int64_t rows, co
       for (int i = 0; i < num_packs; ++i) {
         const int col = (i * thread_group_width + lane_id) * pack_size;
         if (!padding || col < cols) {
-          store.template store<pack_size>(row_buf + i * pack_size, row + row_id, col);
+          store.template store<pack_size>(row_buf + i * pack_size, global_row_id, col);
         }
       }
     }
@@ -560,8 +561,7 @@ __global__ void LayerNormBlockSMemImpl(LOAD load, STORE store, const int64_t row
     ComputeType row_count = 0;
     WelfordBlockAllReduce<ComputeType>(thread_mean, thread_m2, thread_count, &row_mean, &row_m2,
                                        &row_count);
-    ComputeType row_variance =
-        max(Div(row_m2, row_count), static_cast<ComputeType>(0.0));
+    ComputeType row_variance = max(Div(row_m2, row_count), static_cast<ComputeType>(0.0));
     ComputeType row_inv_var = rsqrt(row_variance + static_cast<ComputeType>(epsilon));
     if (threadIdx.x == 0) {
       mean[row] = row_mean;
@@ -717,8 +717,7 @@ __global__ void LayerNormBlockUncachedImpl(LOAD load, STORE store, const int64_t
     ComputeType row_count = 0;
     WelfordBlockAllReduce<ComputeType>(thread_mean, thread_m2, thread_count, &row_mean, &row_m2,
                                        &row_count);
-    ComputeType row_variance =
-        max(Div(row_m2, static_cast<ComputeType>(row_count)), static_cast<ComputeType>(0.0));
+    ComputeType row_variance = max(Div(row_m2, row_count), static_cast<ComputeType>(0.0));
     ComputeType row_inv_var = rsqrt(row_variance + static_cast<ComputeType>(epsilon));
     if (threadIdx.x == 0) {
       mean[row] = row_mean;
@@ -726,10 +725,11 @@ __global__ void LayerNormBlockUncachedImpl(LOAD load, STORE store, const int64_t
     }
     for (int pack_id = tid; pack_id < num_packs; pack_id += block_size) {
       ComputeType pack[pack_size];
-      load.template load<pack_size>(pack, row, pack_id * pack_size);
+      const int pack_offset = pack_id * pack_size;
+      load.template load<pack_size>(pack, row, pack_offset);
 #pragma unroll
       for (int i = 0; i < pack_size; ++i) { pack[i] = (pack[i] - row_mean) * row_inv_var; }
-      store.template store<pack_size>(pack, row, pack_id * pack_size);
+      store.template store<pack_size>(pack, row, pack_offset);
     }
   }
 }
