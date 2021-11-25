1c6e70090a40aef51c8c57d5006f4e764fa86c95
luqiang-guo <702572275@qq.com>
2021-11-25 18:09:59 +0800

    print  ci error

--- python/oneflow/compatible/single_client/test/ops/test_repeat_acc.py
+++ python/oneflow/compatible/single_client/test/ops/test_repeat_acc.py
@@ -30,6 +30,7 @@ func_config.default_data_type(flow.float)
 
 
 def test_repeat_acc(test_case, device_type, shape, dtype, acc_num):
+    print("mu:-->", device_type, dtype)
     flow.clear_default_session()
     if flow.eager_execution_enabled():
         return
@@ -50,6 +51,7 @@ def test_repeat_acc(test_case, device_type, shape, dtype, acc_num):
     if dtype == "float16":
         z = x.astype(np.float16) * acc_num
         z = z.astype(np.float32)
+    print(y-z)
     test_case.assertTrue(np.allclose(y, z, rtol=1e-05, atol=1e-05))
 
 
