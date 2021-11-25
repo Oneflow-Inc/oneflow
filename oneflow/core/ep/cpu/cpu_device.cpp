a0c5ae934b6e7de75b34eaa95d8d7587c2a32e0a
Juncheng <liujuncheng1022@gmail.com>
2021-11-25 08:36:55 +0800

    [EP] Add ep::Device/ep::Event (#6851)
    * [EP] Add ep::Device/ep::Event
    
    * Refine ActiveDeviceContext
    
    * fix
    
    * refine include
    
    * fix tidy error
    
    * fix cudaEventRecord
    
    * fix test
    
    * refine

--- oneflow/core/ep/cpu/cpu_device.cpp
+++ oneflow/core/ep/cpu/cpu_device.cpp
@@ -13,28 +13,28 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#ifndef ONEFLOW_CORE_EP_CPU_CPU_STREAM_H_
-#define ONEFLOW_CORE_EP_CPU_CPU_STREAM_H_
-
-#include "oneflow/core/ep/include/stream.h"
+#include "oneflow/core/ep/cpu/cpu_device.h"
+#include "oneflow/core/ep/cpu/cpu_event.h"
+#include "oneflow/core/ep/cpu/cpu_stream.h"
 
 namespace oneflow {
 
 namespace ep {
 
-class CpuStream : public Stream {
- public:
-  OF_DISALLOW_COPY_AND_MOVE(CpuStream);
-  CpuStream() = default;
-  ~CpuStream() override = default;
+void CpuDevice::SetAsActiveDevice() {}
 
-  DeviceType device_type() const override;
-  Maybe<void> Sync() override;
-  void RecordEvent(Event* event) override;
-};
+Stream* CpuDevice::CreateStream() { return new CpuStream(); }
+
+void CpuDevice::DestroyStream(Stream* stream) { delete stream; }
+
+void CpuDevice::CreateEvents(Event** events, size_t count) {
+  for (size_t i = 0; i < count; ++i) { events[i] = new CpuEvent(); }
+}
+
+void CpuDevice::DestroyEvents(Event** events, size_t count) {
+  for (size_t i = 0; i < count; ++i) { delete events[i]; }
+}
 
 }  // namespace ep
 
 }  // namespace oneflow
-
-#endif  // ONEFLOW_CORE_EP_CPU_CPU_STREAM_H_
