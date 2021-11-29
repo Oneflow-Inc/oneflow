ca06c752e4d74f76ce97a877c873a9ab94beea69
mosout <mosout@qq.com>
2021-11-29 16:55:26 +0800

    add ivalue

---
+++ oneflow/api/cpp/ivalue.cpp
@@ -0,0 +1,64 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "ivalue.h"
+#include <glog/logging.h>
+#include <memory>
+
+namespace oneflow_api {
+
+namespace of = oneflow;
+
+std::ostream& operator<<(std::ostream& os, const IValue::Tag& tag) {
+  os << static_cast<int>(tag);
+  return os;
+}
+
+IValue::IValue(int64_t value) : tag_(IValue::Tag::kInt) { payload_.i.v_int = value; }
+IValue::IValue(double value) : tag_(IValue::Tag::kDouble) { payload_.i.v_double = value; }
+IValue::IValue(bool value) : tag_(IValue::Tag::kBool) { payload_.i.v_bool = value; }
+IValue::IValue(const Tensor& value) : tag_(IValue::Tag::kTensor) { payload_.v_tensor = value; }
+IValue::IValue(const std::vector<Tensor>& value) : tag_(IValue::Tag::kTensorVector) {
+  payload_.v_tensor_vector = value;
+}
+
+bool IValue::IsInt() { return tag_ == Tag::kInt; }
+bool IValue::IsDouble() { return tag_ == Tag::kDouble; }
+bool IValue::IsBool() { return tag_ == Tag::kBool; }
+bool IValue::IsTensor() { return tag_ == Tag::kTensor; }
+bool IValue::IsTensorVector() { return tag_ == Tag::kTensorVector; }
+
+const int64_t IValue::ToInt() const {
+  CHECK_EQ(tag_, Tag::kInt) << "Current value is not int.";
+  return payload_.i.v_int;
+}
+double IValue::ToDouble() {
+  CHECK_EQ(tag_, Tag::kDouble) << "Current value is not double.";
+  return payload_.i.v_double;
+}
+bool IValue::ToBool() {
+  CHECK_EQ(tag_, Tag::kBool) << "Current value is not bool.";
+  return payload_.i.v_bool;
+}
+Tensor IValue::ToTensor() {
+  CHECK_EQ(tag_, Tag::kTensor) << "Current value is not tensor.";
+  return payload_.v_tensor;
+}
+std::vector<Tensor> IValue::ToTensorVector() {
+  CHECK_EQ(tag_, Tag::kTensor) << "Current value is not vector of tensor.";
+  return payload_.v_tensor_vector;
+}
+
+}  // namespace oneflow_api
---
+++ oneflow/api/cpp/ivalue.h
@@ -0,0 +1,72 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef ONEFLOW_API_CPP_IVALUE_H_
+#define ONEFLOW_API_CPP_IVALUE_H_
+
+#include <cstdint>
+#include <memory>
+#include <vector>
+#include "tensor.h"
+
+namespace oneflow_api {
+
+class IValue {
+ public:
+  IValue(int64_t value);  // NOLINT
+  IValue(double value);   // NOLINT
+  explicit IValue(bool value);
+  explicit IValue(const Tensor& value);
+  explicit IValue(const std::vector<Tensor>& value);
+
+  bool IsInt();
+  bool IsDouble();
+  bool IsBool();
+  bool IsTensor();
+  bool IsTensorVector();
+
+  const int64_t ToInt() const;
+  double ToDouble();
+  bool ToBool();
+  Tensor ToTensor();
+  std::vector<Tensor> ToTensorVector();
+
+ private:
+  enum class Tag { kInt = 0, kDouble = 1, kBool = 2, kTensor = 3, kTensorVector = 4 };
+  friend std::ostream & operator << (std::ostream &, const Tag &);
+
+  union Payload {  // NOLINT
+    union InternalPayload {
+      InternalPayload() : v_int(0) {}
+
+      int64_t v_int;
+      double v_double;
+      bool v_bool;
+    } i;
+
+    Tensor v_tensor;
+    std::vector<Tensor> v_tensor_vector;
+
+    Payload() : i() {}
+    ~Payload() {}
+  };
+
+  Payload payload_;
+  Tag tag_;
+};
+
+}  // namespace oneflow_api
+
+#endif  // ONEFLOW_API_CPP_IVALUE_H_
