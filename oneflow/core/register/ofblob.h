cb1bb4551eafd83b414d3c0bf389774d42f7d94c
Flowingsun007 <flowingsun007@163.com>
2021-11-29 23:10:31 +0800

    CopyBetweenMirroredTensorAndNumpy support vstorage_offset

--- oneflow/core/register/ofblob.h
+++ oneflow/core/register/ofblob.h
@@ -46,6 +46,11 @@ class OfBlob final {
   void AutoMemCopyFrom(const T* ptr, int64_t len) const;
   void AsyncAutoMemset(const char value) const;
 
+  template<typename T>
+  void AutoMemCopyTo(T* ptr, int64_t storage_offset, int64_t len) const;
+  template<typename T>
+  void AutoMemCopyFrom(const T* ptr, int64_t storage_offset, int64_t len) const;
+
   Blob* mut_blob() { return blob_; }
   DeviceCtx* mut_device_ctx() { return device_ctx_; }
 
@@ -90,6 +95,23 @@ void OfBlob::AutoMemCopyFrom(const T* ptr, int64_t len) const {
   CHECK_EQ(blob_->shape().elem_cnt(), len);
   CHECK(blob_->data_type() == GetDataType<T>::value);
   SyncAutoMemcpy(device_ctx_->stream(), blob_->mut_dptr(), ptr, len * sizeof(T), blob_->mem_case(),
+                 mem_case_);
+}
+
+template<typename T>
+void OfBlob::AutoMemCopyTo(T* ptr, int64_t storage_offset, int64_t len) const {
+  CHECK_EQ(blob_->shape().elem_cnt(), len);
+  CHECK(blob_->data_type() == GetDataType<T>::value);
+  SyncAutoMemcpy(device_ctx_->stream(), ptr, (char*)(blob_->dptr())+storage_offset * sizeof(T), len * sizeof(T), mem_case_,
+                 blob_->mem_case());
+}
+
+template<typename T>
+void OfBlob::AutoMemCopyFrom(const T* ptr, int64_t storage_offset, int64_t len) const {
+  blob_->blob_access_checker()->CheckBodyMutable();
+  CHECK_EQ(blob_->shape().elem_cnt(), len);
+  CHECK(blob_->data_type() == GetDataType<T>::value);
+  SyncAutoMemcpy(device_ctx_->stream(), (char*)(blob_->mut_dptr())+storage_offset * sizeof(T), ptr, len * sizeof(T), blob_->mem_case(),
                  mem_case_);
 }
 
