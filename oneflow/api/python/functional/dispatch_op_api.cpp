821234a020699f7e9948053d55b905378edb5d2f
Zhanghuihong <garfield.gzhh@gmail.com>
2021-12-01 16:59:12 +0800

    format code

--- oneflow/api/python/functional/dispatch_op_api.cpp
+++ oneflow/api/python/functional/dispatch_op_api.cpp
@@ -42,13 +42,14 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
         auto ctx = std::make_shared<FetchOutputOpInterpCtx>();
         return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
       });
-  m.add_functor("DispatchFeedVariable",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor>& input,
-                   const Scalar& l2) -> Maybe<Tensor> {
-                  auto ctx = std::make_shared<FeedVariableOpInterpCtx>();
-                  ctx->l2 = JUST(l2.As<double>());
-                  return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
-                });
+  m.add_functor(
+    "DispatchFeedVariable",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor>& input,
+        const Scalar& l2) -> Maybe<Tensor> {
+      auto ctx = std::make_shared<FeedVariableOpInterpCtx>();
+      ctx->l2 = JUST(l2.As<double>());
+      return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
+    });
   m.add_functor(
       "DispatchOfrecordReader",
       [](const std::shared_ptr<OpExpr>& op, const std::string& data_dir, int32_t data_part_num,
@@ -89,18 +90,19 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
         ctx->nd_sbp = JUST(GetNdSbp(sbp_tuple));
         return OpInterpUtil::Dispatch<Tensor>(*op, {}, ctx);
       });
-  m.add_functor("DispatchOfrecordRawDecoder",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor>& input,
-                   const std::string name, const Shape& shape, const Symbol<DType>& data_type,
-                   bool dim1_varying_length, bool truncate) -> Maybe<Tensor> {
-                  auto ctx = std::make_shared<OfrecordRawDecoderOpInterpCtx>();
-                  ctx->name = name;
-                  ctx->shape = shape;
-                  ctx->data_type = data_type->data_type();
-                  ctx->dim1_varying_length = dim1_varying_length;
-                  ctx->truncate = truncate;
-                  return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
-                });
+  m.add_functor(
+    "DispatchOfrecordRawDecoder",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor>& input,
+        const std::string name, const Shape& shape, const Symbol<DType>& data_type,
+        bool dim1_varying_length, bool truncate) -> Maybe<Tensor> {
+      auto ctx = std::make_shared<OfrecordRawDecoderOpInterpCtx>();
+      ctx->name = name;
+      ctx->shape = shape;
+      ctx->data_type = data_type->data_type();
+      ctx->dim1_varying_length = dim1_varying_length;
+      ctx->truncate = truncate;
+      return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
+    });
   m.add_functor(
       "DispatchCoinFlip",
       [](const std::shared_ptr<OpExpr>& op, int64_t batch_size, Scalar probability, int64_t seed,
@@ -113,19 +115,20 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
         ctx->device = device;
         return OpInterpUtil::Dispatch<Tensor>(*op, {}, ctx);
       });
-  m.add_functor("DispatchCoinFlip",
-                [](const std::shared_ptr<OpExpr>& op, int64_t batch_size, Scalar probability,
-                   int64_t seed, bool has_seed, const Symbol<ParallelDesc>& placement,
-                   const std::vector<Symbol<cfg::SbpParallel>>& sbp_tuple) -> Maybe<Tensor> {
-                  auto ctx = std::make_shared<CoinFlipOpInterpCtx>();
-                  ctx->probability = JUST(probability.As<float>());
-                  ctx->batch_size = batch_size;
-                  ctx->seed = seed;
-                  ctx->has_seed = has_seed;
-                  ctx->parallel_desc = placement;
-                  ctx->nd_sbp = JUST(GetNdSbp(sbp_tuple));
-                  return OpInterpUtil::Dispatch<Tensor>(*op, {}, ctx);
-                });
+  m.add_functor(
+    "DispatchCoinFlip",
+    [](const std::shared_ptr<OpExpr>& op, int64_t batch_size, Scalar probability,
+        int64_t seed, bool has_seed, const Symbol<ParallelDesc>& placement,
+        const std::vector<Symbol<cfg::SbpParallel>>& sbp_tuple) -> Maybe<Tensor> {
+      auto ctx = std::make_shared<CoinFlipOpInterpCtx>();
+      ctx->probability = JUST(probability.As<float>());
+      ctx->batch_size = batch_size;
+      ctx->seed = seed;
+      ctx->has_seed = has_seed;
+      ctx->parallel_desc = placement;
+      ctx->nd_sbp = JUST(GetNdSbp(sbp_tuple));
+      return OpInterpUtil::Dispatch<Tensor>(*op, {}, ctx);
+    });
   m.add_functor(
       "DispatchCropMirrorNormalizeFromUint8",
       [](const std::shared_ptr<OpExpr>& op, const TensorTuple& input, int64_t crop_h,
@@ -178,34 +181,36 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
         ctx->random_aspect_ratio = random_aspect_ratio;
         return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
       });
-  m.add_functor("DispatchOfrecordImageDecoder",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor>& input,
-                   const std::string name, const std::string color_space) -> Maybe<Tensor> {
-                  auto ctx = std::make_shared<OfrecordImageDecoderRandomCropOpInterpCtx>();
-                  ctx->name = name;
-                  ctx->color_space = color_space;
-                  return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
-                });
-  m.add_functor("DispatchImageDecoderRandomCropResize",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
-                   int64_t target_width, int64_t target_height, int64_t seed, int64_t num_workers,
-                   int64_t max_num_pixels, float random_area_min, float random_area_max,
-                   float random_aspect_ratio_min, float random_aspect_ratio_max,
-                   int64_t warmup_size, int64_t num_attempts) -> Maybe<Tensor> {
-                  auto ctx = std::make_shared<ImageDecoderRandomCropResizeOpInterpCtx>();
-                  ctx->target_width = target_width;
-                  ctx->target_height = target_height;
-                  ctx->seed = seed;
-                  ctx->num_workers = num_workers;
-                  ctx->max_num_pixels = max_num_pixels;
-                  ctx->random_area_min = random_area_min;
-                  ctx->random_area_max = random_area_max;
-                  ctx->random_aspect_ratio_min = random_aspect_ratio_min;
-                  ctx->random_aspect_ratio_max = random_aspect_ratio_max;
-                  ctx->warmup_size = warmup_size;
-                  ctx->num_attempts = num_attempts;
-                  return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
-                });
+  m.add_functor(
+    "DispatchOfrecordImageDecoder",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor>& input,
+        const std::string name, const std::string color_space) -> Maybe<Tensor> {
+      auto ctx = std::make_shared<OfrecordImageDecoderRandomCropOpInterpCtx>();
+      ctx->name = name;
+      ctx->color_space = color_space;
+      return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
+    });
+  m.add_functor(
+    "DispatchImageDecoderRandomCropResize",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
+        int64_t target_width, int64_t target_height, int64_t seed, int64_t num_workers,
+        int64_t max_num_pixels, float random_area_min, float random_area_max,
+        float random_aspect_ratio_min, float random_aspect_ratio_max,
+        int64_t warmup_size, int64_t num_attempts) -> Maybe<Tensor> {
+      auto ctx = std::make_shared<ImageDecoderRandomCropResizeOpInterpCtx>();
+      ctx->target_width = target_width;
+      ctx->target_height = target_height;
+      ctx->seed = seed;
+      ctx->num_workers = num_workers;
+      ctx->max_num_pixels = max_num_pixels;
+      ctx->random_area_min = random_area_min;
+      ctx->random_area_max = random_area_max;
+      ctx->random_aspect_ratio_min = random_aspect_ratio_min;
+      ctx->random_aspect_ratio_max = random_aspect_ratio_max;
+      ctx->warmup_size = warmup_size;
+      ctx->num_attempts = num_attempts;
+      return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
+    });
   m.add_functor(
       "DispatchTensorBufferToListOfTensorsV2",
       [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
@@ -221,78 +226,83 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
         ctx->out_dtypes = out_data_types;
         return OpInterpUtil::Dispatch<TensorTuple>(*op, {input}, ctx);
       });
-  m.add_functor("DispatchImageResizeKeepAspectRatio",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
-                   int32_t target_size, int32_t min_size, int32_t max_size, bool resize_longer,
-                   const std::string interpolation_type) -> Maybe<TensorTuple> {
-                  auto ctx = std::make_shared<ImageResizeKeepAspectRatioOpInterpCtx>();
-                  ctx->target_size = target_size;
-                  ctx->min_size = min_size;
-                  ctx->max_size = max_size;
-                  ctx->resize_longer = resize_longer;
-                  ctx->interpolation_type = interpolation_type;
-                  return OpInterpUtil::Dispatch<TensorTuple>(*op, {input}, ctx);
-                });
-  m.add_functor("DispatchImageResizeToFixed",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
-                   int64_t target_width, int64_t target_height, int64_t channels,
-                   const Symbol<DType>& data_type,
-                   const std::string interpolation_type) -> Maybe<TensorTuple> {
-                  auto ctx = std::make_shared<ImageResizeToFixedOpInterpCtx>();
-                  ctx->target_width = target_width;
-                  ctx->target_height = target_height;
-                  ctx->channels = channels;
-                  ctx->data_type = data_type->data_type();
-                  ctx->interpolation_type = interpolation_type;
-                  return OpInterpUtil::Dispatch<TensorTuple>(*op, {input}, ctx);
-                });
-  m.add_functor("DispatchImageNormalize",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
-                   const std::vector<float>& mean, const std::vector<float>& std) -> Maybe<Tensor> {
-                  auto ctx = std::make_shared<ImageNormalizeOpInterpCtx>();
-                  ctx->std = std;
-                  ctx->mean = mean;
-                  return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
-                });
-  m.add_functor("DispatchCOCOReader",
-                [](const std::shared_ptr<OpExpr>& op, const std::string image_dir,
-                   const std::string annotation_file, int64_t batch_size, bool shuffle_after_epoch,
-                   int64_t random_seed, bool group_by_ratio, bool remove_images_without_annotations,
-                   bool stride_partition, int64_t session_id,
-                   const Optional<Symbol<Device>>& device) -> Maybe<TensorTuple> {
-                  auto ctx = std::make_shared<COCOReaderOpInterpCtx>();
-                  ctx->session_id = session_id;
-                  ctx->annotation_file = annotation_file;
-                  ctx->image_dir = image_dir;
-                  ctx->batch_size = batch_size;
-                  ctx->shuffle_after_epoch = shuffle_after_epoch;
-                  ctx->random_seed = random_seed;
-                  ctx->group_by_ratio = group_by_ratio;
-                  ctx->remove_images_without_annotations = remove_images_without_annotations;
-                  ctx->stride_partition = stride_partition;
-                  ctx->device = device;
-                  return OpInterpUtil::Dispatch<TensorTuple>(*op, {}, ctx);
-                });
-  m.add_functor("DispatchCOCOReader",
-                [](const std::shared_ptr<OpExpr>& op, const std::string image_dir,
-                   const std::string annotation_file, int64_t batch_size, bool shuffle_after_epoch,
-                   int64_t random_seed, bool group_by_ratio, bool remove_images_without_annotations,
-                   bool stride_partition, int64_t session_id, const Symbol<ParallelDesc>& placement,
-                   const std::vector<Symbol<cfg::SbpParallel>>& sbp_tuple) -> Maybe<TensorTuple> {
-                  auto ctx = std::make_shared<COCOReaderOpInterpCtx>();
-                  ctx->session_id = session_id;
-                  ctx->annotation_file = annotation_file;
-                  ctx->image_dir = image_dir;
-                  ctx->batch_size = batch_size;
-                  ctx->shuffle_after_epoch = shuffle_after_epoch;
-                  ctx->random_seed = random_seed;
-                  ctx->group_by_ratio = group_by_ratio;
-                  ctx->remove_images_without_annotations = remove_images_without_annotations;
-                  ctx->stride_partition = stride_partition;
-                  ctx->parallel_desc = placement;
-                  ctx->nd_sbp = JUST(GetNdSbp(sbp_tuple));
-                  return OpInterpUtil::Dispatch<TensorTuple>(*op, {}, ctx);
-                });
+  m.add_functor(
+    "DispatchImageResizeKeepAspectRatio",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
+        int32_t target_size, int32_t min_size, int32_t max_size, bool resize_longer,
+        const std::string interpolation_type) -> Maybe<TensorTuple> {
+      auto ctx = std::make_shared<ImageResizeKeepAspectRatioOpInterpCtx>();
+      ctx->target_size = target_size;
+      ctx->min_size = min_size;
+      ctx->max_size = max_size;
+      ctx->resize_longer = resize_longer;
+      ctx->interpolation_type = interpolation_type;
+      return OpInterpUtil::Dispatch<TensorTuple>(*op, {input}, ctx);
+    });
+  m.add_functor(
+    "DispatchImageResizeToFixed",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
+        int64_t target_width, int64_t target_height, int64_t channels,
+        const Symbol<DType>& data_type,
+        const std::string interpolation_type) -> Maybe<TensorTuple> {
+      auto ctx = std::make_shared<ImageResizeToFixedOpInterpCtx>();
+      ctx->target_width = target_width;
+      ctx->target_height = target_height;
+      ctx->channels = channels;
+      ctx->data_type = data_type->data_type();
+      ctx->interpolation_type = interpolation_type;
+      return OpInterpUtil::Dispatch<TensorTuple>(*op, {input}, ctx);
+    });
+  m.add_functor(
+    "DispatchImageNormalize",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
+        const std::vector<float>& mean, const std::vector<float>& std) -> Maybe<Tensor> {
+      auto ctx = std::make_shared<ImageNormalizeOpInterpCtx>();
+      ctx->std = std;
+      ctx->mean = mean;
+      return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
+    });
+  m.add_functor(
+    "DispatchCOCOReader",
+    [](const std::shared_ptr<OpExpr>& op, const std::string image_dir,
+        const std::string annotation_file, int64_t batch_size, bool shuffle_after_epoch,
+        int64_t random_seed, bool group_by_ratio, bool remove_images_without_annotations,
+        bool stride_partition, int64_t session_id,
+        const Optional<Symbol<Device>>& device) -> Maybe<TensorTuple> {
+      auto ctx = std::make_shared<COCOReaderOpInterpCtx>();
+      ctx->session_id = session_id;
+      ctx->annotation_file = annotation_file;
+      ctx->image_dir = image_dir;
+      ctx->batch_size = batch_size;
+      ctx->shuffle_after_epoch = shuffle_after_epoch;
+      ctx->random_seed = random_seed;
+      ctx->group_by_ratio = group_by_ratio;
+      ctx->remove_images_without_annotations = remove_images_without_annotations;
+      ctx->stride_partition = stride_partition;
+      ctx->device = device;
+      return OpInterpUtil::Dispatch<TensorTuple>(*op, {}, ctx);
+    });
+  m.add_functor(
+    "DispatchCOCOReader",
+    [](const std::shared_ptr<OpExpr>& op, const std::string image_dir,
+        const std::string annotation_file, int64_t batch_size, bool shuffle_after_epoch,
+        int64_t random_seed, bool group_by_ratio, bool remove_images_without_annotations,
+        bool stride_partition, int64_t session_id, const Symbol<ParallelDesc>& placement,
+        const std::vector<Symbol<cfg::SbpParallel>>& sbp_tuple) -> Maybe<TensorTuple> {
+      auto ctx = std::make_shared<COCOReaderOpInterpCtx>();
+      ctx->session_id = session_id;
+      ctx->annotation_file = annotation_file;
+      ctx->image_dir = image_dir;
+      ctx->batch_size = batch_size;
+      ctx->shuffle_after_epoch = shuffle_after_epoch;
+      ctx->random_seed = random_seed;
+      ctx->group_by_ratio = group_by_ratio;
+      ctx->remove_images_without_annotations = remove_images_without_annotations;
+      ctx->stride_partition = stride_partition;
+      ctx->parallel_desc = placement;
+      ctx->nd_sbp = JUST(GetNdSbp(sbp_tuple));
+      return OpInterpUtil::Dispatch<TensorTuple>(*op, {}, ctx);
+    });
   m.add_functor(
       "DispatchImageBatchAlign",
       [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input, int32_t alignment,
@@ -304,13 +314,14 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
         ctx->dynamic_out = dynamic_out;
         return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
       });
-  m.add_functor("DispatchOfrecordBytesDecoder",
-                [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
-                   const std::string name) -> Maybe<Tensor> {
-                  auto ctx = std::make_shared<OfrecordBytesDecoderOpInterpCtx>();
-                  ctx->name = name;
-                  return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
-                });
+  m.add_functor(
+    "DispatchOfrecordBytesDecoder",
+    [](const std::shared_ptr<OpExpr>& op, const std::shared_ptr<Tensor> input,
+        const std::string name) -> Maybe<Tensor> {
+      auto ctx = std::make_shared<OfrecordBytesDecoderOpInterpCtx>();
+      ctx->name = name;
+      return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
+    });
   m.add_functor(
       "DispatchMegatronGptMmapDataLoader",
       [](const std::shared_ptr<OpExpr>& op, const std::string data_file_prefix, int64_t seq_length,
