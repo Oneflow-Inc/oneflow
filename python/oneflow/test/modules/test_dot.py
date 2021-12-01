4086f1105916b7118fcbb017573ee6d835538569
luqiang-guo <702572275@qq.com>
2021-12-01 15:48:04 +0800

    fix error

--- python/oneflow/test/modules/test_dot.py
+++ python/oneflow/test/modules/test_dot.py
@@ -14,11 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import unittest
-from collections import OrderedDict
-
-import numpy as np
-
-from test_util import GenArgList
 import oneflow as flow
 from oneflow.test_utils.automated_test_util import *
 
