3ad2fc239d523790c6b9afee121353cff4a22a1d
hjchen2 <chenhoujiangcug@gmail.com>
2021-12-01 10:30:34 +0800

    Add dispatch gpt reader

--- oneflow/api/python/functional/dispatch_op_api.cpp
+++ oneflow/api/python/functional/dispatch_op_api.cpp
@@ -15,6 +15,7 @@ limitations under the License.
 */
 
 #include "oneflow/core/common/scalar.h"
+#include "oneflow/core/framework/nd_sbp.h"
 #include "oneflow/core/framework/op_interp_ctx.h"
 #include "oneflow/core/framework/op_interpreter/op_interpreter_util.h"
 #include "oneflow/core/framework/tensor.h"
@@ -48,6 +49,46 @@ ONEFLOW_FUNCTION_LIBRARY(m) {
                   ctx->l2 = JUST(l2.As<double>());
                   return OpInterpUtil::Dispatch<Tensor>(*op, {input}, ctx);
                 });
+  m.add_functor(
+      "DispatchOfrecordReader",
+      [](const std::shared_ptr<OpExpr>& op, const std::string& data_dir, int32_t data_part_num,
+         const std::string& part_name_prefix, int32_t part_name_suffix_length, int32_t batch_size,
+         int32_t shuffle_buffer_size, bool random_shuffle, bool shuffle_after_epoch, int64_t seed,
+         const Optional<Symbol<Device>>& device) -> Maybe<Tensor> {
+        auto ctx = std::make_shared<OFRecordReaderOpInterpCtx>();
+        ctx->data_dir = data_dir;
+        ctx->data_part_num = data_part_num;
+        ctx->part_name_prefix = part_name_prefix;
+        ctx->part_name_suffix_length = part_name_suffix_length;
+        ctx->batch_size = batch_size;
+        ctx->shuffle_buffer_size = shuffle_buffer_size;
+        ctx->random_shuffle = random_shuffle;
+        ctx->shuffle_after_epoch = shuffle_after_epoch;
+        ctx->seed = seed;
+        ctx->device = device;
+        return OpInterpUtil::Dispatch<Tensor>(*op, {}, ctx);
+      });
+  m.add_functor(
+      "DispatchOfrecordReader",
+      [](const std::shared_ptr<OpExpr>& op, const std::string& data_dir, int32_t data_part_num,
+         const std::string& part_name_prefix, int32_t part_name_suffix_length, int32_t batch_size,
+         int32_t shuffle_buffer_size, bool random_shuffle, bool shuffle_after_epoch, int64_t seed,
+         const Symbol<ParallelDesc>& placement,
+         const std::vector<Symbol<cfg::SbpParallel>>& sbp_tuple) -> Maybe<Tensor> {
+        auto ctx = std::make_shared<OFRecordReaderOpInterpCtx>();
+        ctx->data_dir = data_dir;
+        ctx->data_part_num = data_part_num;
+        ctx->part_name_prefix = part_name_prefix;
+        ctx->part_name_suffix_length = part_name_suffix_length;
+        ctx->batch_size = batch_size;
+        ctx->shuffle_buffer_size = shuffle_buffer_size;
+        ctx->random_shuffle = random_shuffle;
+        ctx->shuffle_after_epoch = shuffle_after_epoch;
+        ctx->seed = seed;
+        ctx->parallel_desc = placement;
+        ctx->nd_sbp = JUST(GetNdSbp(sbp_tuple));
+        return OpInterpUtil::Dispatch<Tensor>(*op, {}, ctx);
+      });
 }
 
 }  // namespace impl
