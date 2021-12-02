4eeb214724102e9d45d8a8f5b3382f7a51655b38
jackalcooper <jackalcooper@gmail.com>
2021-12-02 22:59:59 +0800

    auto dl dataset

--- python/oneflow/test/modules/test_one_rec_ops.py
+++ python/oneflow/test/modules/test_one_rec_ops.py
@@ -16,17 +16,67 @@ limitations under the License.
 
 import unittest
 
+
 import oneflow as flow
 import oneflow.unittest
 
 
+ONEREC_URL = "https://oneflow-public.oss-cn-beijing.aliyuncs.com/datasets/onerec_test/part-00000-713a0aee-1337-4686-b418-0ada6face4de-c000.onerec"
+MD5 = "cc857a451cd796c12ff5d293177d1d24"
+
+
+def md5(fname):
+    import hashlib
+
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    result = hash_md5.hexdigest()
+    print("md5", fname, result)
+    return result
+
+
+def download_file(out_path: str, url):
+    import requests
+    from tqdm import tqdm
+
+    resp = requests.get(url=url, stream=True)
+    MB = 1024 ** 2
+    size = int(resp.headers["Content-Length"]) / MB
+    print("File size: %.4f MB, downloading..." % size)
+    with open(out_path, "wb") as f:
+        for data in tqdm(
+            iterable=resp.iter_content(MB), total=size, unit="m", desc=out_path
+        ):
+            f.write(data)
+        print("Done!")
+
+
+def ensure_dataset():
+    import os
+    import pathlib
+
+    data_dir = os.path.join(
+        os.getenv("ONEFLOW_TEST_CACHE_DIR", "./data-test"), "onerec_test"
+    )
+    file_path = pathlib.Path(data_dir) / ONEREC_URL.split("/")[-1]
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    absolute_file_path = str(file_path.absolute())
+    if file_path.exists():
+        if MD5 != md5(absolute_file_path):
+            file_path.unlink()
+            download_file(absolute_file_path, ONEREC_URL)
+    else:
+        download_file(str(absolute_file_path), ONEREC_URL)
+    assert MD5 == md5(absolute_file_path)
+    return absolute_file_path
+
+
 @flow.unittest.skip_unless_1n1d()
-@unittest.skipIf(True, "skip until one_rec dataset is ready on CI server")
 class TestOneRecOpsModule(flow.unittest.TestCase):
     def test_read_decode(test_case):
-        files = [
-            "/dataset/onerec_test/part-00000-713a0aee-1337-4686-b418-0ada6face4de-c000.onerec"
-        ]
+        files = [ensure_dataset()]
         readdata = flow.read_onerec(
             files, batch_size=10, random_shuffle=True, shuffle_mode="batch"
         )
