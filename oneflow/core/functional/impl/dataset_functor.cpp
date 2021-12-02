c8d1f912ec2c59220d05cd0d14af88244e0302c0
YaoChi <later@usopp.net>
2021-12-02 21:42:16 +0800

    remove useless header files

--- oneflow/core/functional/impl/dataset_functor.cpp
+++ oneflow/core/functional/impl/dataset_functor.cpp
@@ -13,8 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <string>
-#include "oneflow/core/common/shape.h"
 #include "oneflow/core/framework/op_builder.h"
 #include "oneflow/core/framework/op_expr.h"
 #include "oneflow/core/framework/op_interpreter/op_interpreter_util.h"
