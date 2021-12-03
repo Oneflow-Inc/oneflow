bbaca39b69571d5e3c72a9c097ef1b9b5742cfd7
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2021-12-03 16:27:00 +0800

    transfer flatten/floor op and fix flatten backward bug (#6898)
    * transfer flatten op
    
    * transfer floor op
    
    * fix autotest print no input bug
    
    * fix flatten bug and restruct test
    
    * code format
    
    * fix comment
    
    * code format
    
    * fix bug
    
    * fix test
    
    Co-authored-by: oneflow-ci-bot <69100618+oneflow-ci-bot@users.noreply.github.com>

--- python/oneflow/framework/docstr/flatten.py
+++ python/oneflow/framework/docstr/flatten.py
@@ -13,26 +13,27 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from .math_ops import *
-from .random import *
-from .conv import *
-from .pooling import *
-from .activation import *
-from .dropout import *
-from .vision import *
-from .norm import *
-from .loss import *
-from .onehot import *
-from .comparison import *
-from .cast import *
-from .constant import *
-from .array_ops import *
-from .tensor import *
-from .comm import *
-from .ctc_decode import *
-from .placement import *
-from .sbp import *
-from .trigonometric_ops import *
-from .tensor_ops import *
-from .meshgrid import *
-from .flatten import *
+import oneflow
+from oneflow.framework.docstr.utils import add_docstr
+
+add_docstr(
+    oneflow.flatten,
+    """Flattens a contiguous range of dims into a tensor.
+
+    Args:
+        start_dim: first dim to flatten (default = 0).
+        end_dim: last dim to flatten (default = -1).
+    
+    For example: 
+
+    .. code-block:: python 
+
+        >>> import numpy as np
+        >>> import oneflow as flow
+        >>> input = flow.randn(32, 1, 5, 5)
+        >>> output = flow.flatten(input, start_dim=1)
+        >>> output.shape
+        oneflow.Size([32, 25])
+
+    """,
+)
