b659ab85e349550df1e6cf91692fe401ed3f8766
chengtbf <472491134@qq.com>
2021-12-07 15:14:31 +0800

    fix test scripts

--- ci/test/custom_test_multi_client.sh
+++ ci/test/custom_test_multi_client.sh
@@ -7,5 +7,5 @@ export ONEFLOW_TEST_UTILS_DIR=$src_dir/python/oneflow/test_utils
 
 export ONEFLOW_TEST_DEVICE_NUM=8
 echo "test_neq_device_process_num begin"
-python3 -m oneflow.distributed.launch --nproc_per_node 8 -m unittest discover ${PWD}/python/oneflow/test/graph/test_neq_device_process_num.py --failfast --verbose
+python3 -m oneflow.distributed.launch --nproc_per_node 8 ${PWD}/python/oneflow/test/graph/test_neq_device_process_num.py --failfast --verbose
 echo "test_neq_device_process_num end"
