a8f2fe2a6b3009f8aa319f432caeac4e71d7ab53
Shenghang Tsai <jackalcooper@gmail.com>
2021-12-08 15:15:06 +0800

    Update oneflow/ir/test/OneFlow/test_mlir_opt.py

--- oneflow/ir/test/OneFlow/test_mlir_opt.py
+++ oneflow/ir/test/OneFlow/test_mlir_opt.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-#  RUN: python3 %s | FileCheck %s
+# RUN: python3 %s | FileCheck %s
 import unittest
 import numpy as np
 import oneflow.compatible.single_client as flow
