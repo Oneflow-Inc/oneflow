b4543ce7e31cd00989c51293bc34e94c1b5517b0
liufengwei <2472937968@qq.com>
2021-12-08 15:08:25 +0800

    add graph test

---
+++ python/oneflow/test/graph/test_graph_eye.py
@@ -0,0 +1,43 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+import numpy as np
+import random
+import oneflow as flow
+import oneflow.unittest
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestEyeGraph(oneflow.unittest.TestCase):
+    def test_eye_graph(test_case):
+        n = random.randint(1, 10)
+        m = random.randint(1, 10)
+        y_eager = flow.eye(n, m)
+
+        class EyeGraph(flow.nn.Graph):
+            def __init__(self):
+                super().__init__()
+                self.eye = flow._C.eye
+            def build(self):
+                return self.eye(n, m)
+
+        eye_g = EyeGraph()
+        y_lazy = eye_g()
+        test_case.assertTrue(np.array_equal(y_eager.numpy(), y_lazy.numpy()))
+
+
+if __name__ == "__main__":
+    unittest.main()
