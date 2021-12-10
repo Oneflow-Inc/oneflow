888ee604814852bc86995ff9073feab7bc014333
jackalcooper <jackalcooper@gmail.com>
2021-12-10 11:58:13 +0000

    refine

--- cmake/oneflow.cmake
+++ cmake/oneflow.cmake
@@ -301,7 +301,7 @@ elseif(WIN32)
   set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /WHOLEARCHIVE:oneflow")
 endif()
 
-target_link_libraries(oneflow-gen-ods ${of_libs} ${oneflow_third_party_libs} ${oneflow_exe_third_party_libs})
+target_link_libraries(oneflow-gen-ods ${of_libs} -Wl,--whole-archive protobuf_imported -Wl,--no-whole-archive glog_imported)
 if (BUILD_CUDA)
   target_link_libraries(oneflow-gen-ods CUDA::cudart_static)
 endif()
