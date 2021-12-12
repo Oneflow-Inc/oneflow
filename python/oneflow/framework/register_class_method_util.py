35fcf70858b837dc08594c61bb00ddafbdacadd8
daquexian <daquexian566@gmail.com>
2021-12-12 09:18:42 +0800

    fix circular reference
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- python/oneflow/framework/register_class_method_util.py
+++ python/oneflow/framework/register_class_method_util.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import oneflow._oneflow_internal
+import oneflow.framework.check_point_v2 as check_point_v2
 import oneflow.framework.generator as generator
 import oneflow.framework.op_expr_util as op_expr_util
 import oneflow.framework.tensor as tensor_util
@@ -21,4 +22,5 @@ import oneflow.framework.tensor as tensor_util
 
 def RegisterMethod4Class():
     tensor_util.RegisterMethods()
+    check_point_v2.RegisterMethods()
     op_expr_util.RegisterMethod4UserOpExpr()
