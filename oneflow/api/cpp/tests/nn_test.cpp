fb80f26cce4682a255ed05664f6552e42f82047a
mosout <mosout@qq.com>
2021-12-12 20:32:55 +0800

    fix device test and add add test on gpu for relu

--- oneflow/api/cpp/tests/nn_test.cpp
+++ oneflow/api/cpp/tests/nn_test.cpp
@@ -37,13 +37,14 @@ std::vector<T> Relu(const std::vector<T>& data) {
 
 }  // namespace
 
-void TestRelu() {
+void TestRelu(bool on_gpu = false) {
   const auto shape = RandomShape();
   const auto data = RandomData<float>(shape.Count(0));
   const auto target_data = Relu(data);
   std::vector<float> result(shape.Count(0));
 
-  auto tensor = Tensor::from_buffer(data.data(), shape, Device("cpu"), DType::kFloat);
+  auto tensor = Tensor::from_buffer(data.data(), shape, on_gpu ? Device("cuda:0") : Device("cpu"),
+                                    DType::kFloat);
   auto result_tensor = nn::relu(tensor);
 
   result_tensor.copy_to(result.data());
@@ -55,6 +56,10 @@ TEST(Api, nn_relu) {
   EnvScope scope;
 
   TestRelu();
+
+#ifdef WITH_CUDA
+  TestRelu(true);
+#endif
 }
 
 TEST(Api, nn_relu_multithreading) {
@@ -64,7 +69,7 @@ TEST(Api, nn_relu_multithreading) {
   std::uniform_int_distribution<> dist(8, 32);
   int n_threads = dist(rng);
 
-  for (int i = 0; i < n_threads; ++i) { threads.emplace_back(std::thread(TestRelu)); }
+  for (int i = 0; i < n_threads; ++i) { threads.emplace_back(std::thread(TestRelu, false)); }
 
   for (auto& x : threads) { x.join(); }
 }
