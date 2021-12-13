08e10b704895dd43e8b7390784f2082b4af1cb42
wyg1997 <wyg19970408@gmail.com>
2021-12-13 10:54:47 +0800

    test(FromNumpy): add test

---
+++ python/oneflow/test/modules/test_from_numpy.py
@@ -0,0 +1,43 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import random
+import unittest
+
+import numpy as np
+import oneflow as flow
+import oneflow.unittest
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestFromNumpy(flow.unittest.TestCase):
+    def test_same_data(test_case):
+        np_arr = np.random.randn(3, 4, 5)
+        tensor = flow.from_numpy(np_arr)
+        test_case.assertTrue(np.array_equal(np_arr, tensor.numpy()))
+
+        np_arr[1:2, 2:3, 3:4] = random.random()
+        test_case.assertTrue(np.array_equal(np_arr, tensor.numpy()))
+
+    def test_use_ops(test_case):
+        np_arr = np.random.randn(3, 4, 5)
+        tensor = flow.from_numpy(np_arr)
+        res = tensor ** 2
+        test_case.assertTrue(np.allclose(np_arr ** 2, res.numpy()))
+
+
+if __name__ == "__main__":
+    unittest.main()
