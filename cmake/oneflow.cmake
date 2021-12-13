829e314b507425f4ea01285eaa559b7dc36e6338
jackalcooper <jackalcooper@gmail.com>
2021-12-13 06:47:50 +0000

    refine

--- cmake/oneflow.cmake
+++ cmake/oneflow.cmake
@@ -282,7 +282,6 @@ set_target_properties(oneflow-gen-ods PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${PRO
 set(LLVM_MONO_REPO_URL "https://github.com/llvm/llvm-project/archive/649d95371680cbf7f740c990c0357372c2bd4058.zip" CACHE STRING "")
 use_mirror(VARIABLE LLVM_MONO_REPO_URL URL ${LLVM_MONO_REPO_URL})
 set(LLVM_MONO_REPO_MD5 "9bda804e5cc61899085fb0f0dce1089f" CACHE STRING "")
-add_subdirectory(${PROJECT_SOURCE_DIR}/oneflow/ir/llvm-mono)
 set(ONEFLOW_BUILD_ROOT_DIR "${PROJECT_BINARY_DIR}")
 if (WITH_MLIR)
   add_subdirectory(${PROJECT_SOURCE_DIR}/oneflow/ir)
