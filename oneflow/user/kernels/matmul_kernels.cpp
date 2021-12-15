ae48c65d6ac0c384c1236bfa47aa06a7d86ef369
MARD1NO <359521840@qq.com>
2021-12-15 09:59:37 +0800

    revert old bcast matmul grad b kernel

--- oneflow/user/kernels/matmul_kernels.cpp
+++ oneflow/user/kernels/matmul_kernels.cpp
@@ -347,18 +347,11 @@ class BroadcastMatmulGradBKernel final : public user_op::OpKernel,
       beta = 1.0;
     }
 
-    CHECK_EQ(b->shape().NumAxes(), 2);
-    CHECK_GT(a->shape().NumAxes(), b->shape().NumAxes());
-    int64_t m = a->shape().Count(0, a->shape().NumAxes() - 1);
-    int64_t k = a->shape().At(a->shape().NumAxes() - 1);
-    int64_t n = -1;
-    if (!transpose_b) {
-      n = b->shape().At(1);
-      CHECK_EQ(k, b->shape().At(0));
-    } else {
-      n = b->shape().At(0);
-      CHECK_EQ(k, b->shape().At(1));
-    }
+    CHECK_EQ(a->shape().NumAxes(), b->shape().NumAxes());
+    int64_t k = a->shape().Count(0, a->shape().NumAxes() - 1);
+    CHECK_EQ(b->shape().Count(0, b->shape().NumAxes() - 1), k);
+    int64_t m = a->shape().At(a->shape().NumAxes() - 1);
+    int64_t n = b->shape().At(b->shape().NumAxes() - 1);
     auto matmul = NewMatmulPrimitiveForBroadcastMatmulGradB(ctx);
     CHECK(matmul);
     matmul->Launch(ctx->stream(), m, n, k, alpha, a->dptr(), b->dptr(), beta, out->mut_dptr());
