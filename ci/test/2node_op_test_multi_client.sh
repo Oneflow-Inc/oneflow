bcb12eac57e8cecb85634f0d1c0a83f7cb8d5082
daquexian <daquexian566@gmail.com>
2021-12-20 18:08:09 +0800

    remove sleep
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- ci/test/2node_op_test_multi_client.sh
+++ ci/test/2node_op_test_multi_client.sh
@@ -17,7 +17,6 @@ cd ${test_tmp_dir}/$(basename $test_dir)
 
 for device_num in 1 2 4
 do
-    sleep 99999
     ONEFLOW_TEST_NODE_NUM=2 ONEFLOW_TEST_DEVICE_NUM=$device_num python3 -m oneflow.distributed.launch --nproc_per_node $device_num --nnodes=2 --node_rank=$NODE_RANK --master_addr $MASTER_ADDR -m unittest discover ${PWD} --failfast --verbose
     # use a invalid ibverbs lib to test if falling back to epoll works
     ONEFLOW_TEST_NODE_NUM=2 ONEFLOW_TEST_DEVICE_NUM=$device_num ONEFLOW_LIBIBVERBS_PATH=invalid_lib python3 -m oneflow.distributed.launch --nproc_per_node $device_num --nnodes=2 --node_rank=$NODE_RANK --master_addr $MASTER_ADDR -m unittest discover ${PWD} --failfast --verbose
