5b08a29dffd90f2096fe633a88afd04e2b7a7e5c
jackalcooper <jackalcooper@gmail.com>
2021-12-20 04:22:14 +0000

    refine

--- oneflow/api/python/ir.cpp
+++ oneflow/api/python/ir.cpp
@@ -16,114 +16,21 @@ limitations under the License.
 
 #ifdef WITH_MLIR
 
-#include <glog/logging.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-
-#include <utility>
 #include "oneflow/ir/include/OneFlow/Extension.h"
 #include "oneflow/ir/oneflow-extension/include/OneFlow/OneFlowRoundTrip.h"
+#include <glog/logging.h>
 #include "oneflow/api/python/of_api_registry.h"
-#include "oneflow/ir/include/OneFlow/Extension.h"
-#include "oneflow/core/framework/op_interpreter/op_interpreter_util.h"
-#include "oneflow/ir/oneflow-jit/include/OneFlow/jit_op_interpreter.h"
-#include "oneflow/api/python/functional/common.h"
-
-namespace py = pybind11;
 
 namespace oneflow {
-
-using namespace one::ir;
-
-namespace jit {
-struct Module {
-  explicit Module(py::object py_module)
-      : py_module_(std::move(py_module)),
-        context_(new MLIRContext()),
-        module_(CreateJitModule(context_)),
-        importer_(context_, *module_) {}
-  std::shared_ptr<one::Tensor> forward(const py::args& args, const py::kwargs& kwargs) {
-    auto jit_interpreter =
-        std::dynamic_pointer_cast<one::JitInterpreter>(one::JitInterpreter::Get());
-    CHECK(jit_interpreter != nullptr) << "JIT interpreter is not initialized";
-    auto i_this = reinterpret_cast<std::uintptr_t>(this);
-    std::string func_name = "jitModule" + std::to_string(i_this);
-    auto inputs = args.cast<std::vector<std::shared_ptr<one::Tensor>>>();
-    auto parameters_generator = py_module_.attr("parameters")();
-    std::vector<std::shared_ptr<one::Tensor>> parameters{};
-    for (auto p : parameters_generator) {
-      parameters.push_back(p.cast<std::shared_ptr<one::Tensor>>());
-    }
-    std::vector<std::shared_ptr<one::Tensor>> arg_tensors{};
-    arg_tensors.insert(arg_tensors.end(), inputs.begin(), inputs.end());
-    arg_tensors.insert(arg_tensors.end(), parameters.begin(), parameters.end());
-    std::vector<std::shared_ptr<one::Tensor>> tensors_to_materialize{};
-    jit_interpreter->MarkMlirTraceStart();
-    if (!func_op_) {
-      func_op_ = jit_interpreter->Trace(importer_, func_name, arg_tensors, [&]() {
-        auto returned_obj = py_module_.attr("forward")(*args, **kwargs);
-        if (auto tensor = returned_obj.cast<std::shared_ptr<one::Tensor>>()) {
-          importer_.FinalizeProcessFunction(tensor);
-        } else {
-          LOG(FATAL) << "return type not supported: " << returned_obj.get_type();
-        }
-      });
-    }
-    jit_interpreter->MarkMlirTraceEnd();
-    auto ret = jit_interpreter->DispatchFunc(func_op_.getValue(), arg_tensors);
-    jit_interpreter->MarkMlirDispatchEnd();
-    LOG(ERROR) << "JIT trace overhead: " << jit_interpreter->TraceOverhead();
-    return ret;
-  }
-  void dump_ir() { module_->dump(); }
-
- private:
-  py::object py_module_;
-  MLIRContext* context_;
-  OwningOpRef<ModuleOp> module_;
-  mlir::oneflow::JitImporter importer_;
-  llvm::Optional<FuncOp> func_op_;
-};
-}  // namespace jit
 
 ONEFLOW_API_PYBIND11_MODULE("ir", m) {
   m.def("load_jit_shared_lib",
         [](const std::string& lib_path) { MutSharedLibPaths()->insert(lib_path); });
-  m.def("toggle_jit", [](const std::string& func_name) {
-    *one::MutJitEnabled() = !*one::MutJitEnabled();
-    // when false => true, start jit
-    auto jit_interpreter =
-        std::dynamic_pointer_cast<one::JitInterpreter>(one::JitInterpreter::Get());
-    CHECK(jit_interpreter != nullptr) << "JIT interpreter is not initialized";
-    if (one::IsJitEnabled() == true) { jit_interpreter->MarkMlirTraceStart(); }
-    // when true => false, start exec
-    if (one::IsJitEnabled() == false) {
-      jit_interpreter->Interrupt();
-      jit_interpreter->MarkMlirTraceEnd();
-      if (ParseBooleanFromEnv("ONEFLOW_MLIR_ENABLE_TRACE_PROFILING", false)) {
-        LOG(ERROR) << "JIT trace overhead: " << jit_interpreter->TraceOverhead();
-      }
-    }
-    return *one::MutJitEnabled();
-  });
-  m.def("set_jit_forward_args", [](const std::vector<std::shared_ptr<one::Tensor>>& tensors,
-                                   const std::vector<std::shared_ptr<one::Tensor>>& parameters) {
-    auto arg_tensors(tensors);
-    for (const auto& p : parameters) { arg_tensors.push_back((p)); }
-    TODO() << "set global fw args";
-  });
-
-  py::class_<jit::Module, std::shared_ptr<jit::Module>>(m, "JitModule")
-      .def(py::init<py::object>())
-      .def("__call__", &jit::Module::forward)
-      .def("dump_ir", &jit::Module::dump_ir);
 }
 
+REGISTER_JOB_PASS("IRRoundTripBeforeAD", IRRoundTrip<kBeforeAD>);
+REGISTER_JOB_PASS("IRRoundTrip", IRRoundTrip<kAfterAD>);
+
 }  // namespace oneflow
-
-REGISTER_JOB_PASS("IRRoundTripBeforeAD", ::oneflow::IRRoundTrip<::oneflow::kBeforeAD>);
-REGISTER_JOB_PASS("IRRoundTrip", ::oneflow::IRRoundTrip<::oneflow::kAfterAD>);
-
-COMMAND(SetJitInterpreter(::oneflow::one::JitInterpreter::Get()));
 
 #endif  // WITH_MLIR
--- python/oneflow/jit/__init__.py
+++ python/oneflow/jit/__init__.py
@@ -28,21 +28,21 @@ def exec(f):
             if isinstance(m, oneflow.nn.Module):
                 for arg in args[1::]:
                     assert isinstance(arg, oneflow._oneflow_internal.Tensor)
-                    oneflow._oneflow_internal.ir.set_jit_forward_args(
+                    oneflow._oneflow_internal.jit.set_jit_forward_args(
                         args[1::], list(m.parameters())
                     )
             else:
                 for arg in args:
                     assert isinstance(arg, oneflow._oneflow_internal.Tensor)
-                oneflow._oneflow_internal.ir.set_jit_forward_args(list(args), [])
+                oneflow._oneflow_internal.jit.set_jit_forward_args(list(args), [])
 
         func_name = str(uuid.uuid4()).replace("-", "")
         func_name = f"jit{func_name}"
-        assert oneflow._oneflow_internal.ir.toggle_jit(func_name)
+        assert oneflow._oneflow_internal.jit.toggle_jit(func_name)
         start = timer()
         # NOTE: forbid calling __repr__ in the forward function
         result = f(*args, **kwargs)
-        assert not oneflow._oneflow_internal.ir.toggle_jit(func_name)
+        assert not oneflow._oneflow_internal.jit.toggle_jit(func_name)
         end = timer()
         print("JIT optimizations and dispatch ends in", end - start)
         return result
@@ -53,7 +53,7 @@ def exec(f):
 class JitModule(object):
     def __init__(self, py_module):
         self.py_module = py_module
-        self.jit_module = oneflow._oneflow_internal.ir.JitModule(py_module)
+        self.jit_module = oneflow._oneflow_internal.jit.JitModule(py_module)
 
     def __call__(self, *args, **kwargs):
         return self.jit_module.__call__(*args, **kwargs)
