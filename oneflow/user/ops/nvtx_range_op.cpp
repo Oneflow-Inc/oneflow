0a171f1cc66f89dbfed858fd6430a1f86137910e
jackalcooper <jackalcooper@gmail.com>
2021-12-21 04:10:58 +0000

    add with cuda

--- oneflow/user/ops/nvtx_range_op.cpp
+++ oneflow/user/ops/nvtx_range_op.cpp
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#ifdef WITH_CUDA
+
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/framework/op_generated.h"
 
@@ -107,3 +109,5 @@ REGISTER_USER_OP_GRAD("nvtx_end")
     });
 
 }  // namespace oneflow
+
+#endif  // WITH_CUDA
