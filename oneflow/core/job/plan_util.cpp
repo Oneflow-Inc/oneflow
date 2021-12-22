0a2f97bb07e3d0c78e506bef339180396430a155
wyg1997 <wyg19970408@gmail.com>
2021-12-22 18:54:48 +0800

    add TODO in register_num check

--- oneflow/core/job/plan_util.cpp
+++ oneflow/core/job/plan_util.cpp
@@ -260,6 +260,7 @@ void PlanUtil::GenMemBlockAndChunkWithVariableOpNames4Plan(
     if (is_variable_regst) {
       CHECK(!var_name.empty());
       CHECK_EQ(regst_desc->register_num(), 1);
+      // TODO(chengcheng, wyg): some wrong with these checks in auto_parallel
       // CHECK_EQ(regst_desc->min_register_num(), 1);
       // CHECK_EQ(regst_desc->max_register_num(), 1);
       regst_desc->set_variable_op_name(var_name);
