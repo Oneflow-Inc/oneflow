caf8671dec6f6e4225cf109e6fd4ea989d7c06d9
wyushun <wyushun@foxmail.com>
2021-12-22 15:46:00 +0800

    change original test_cumsum to autotest version

--- python/oneflow/test/modules/test_cumsum.py
+++ python/oneflow/test/modules/test_cumsum.py
@@ -13,126 +13,24 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import random
 import unittest
 from collections import OrderedDict
 
-import torch
-import numpy as np
 import oneflow as flow
 import oneflow.unittest
 
-def test_cumsum_forward(test_case, np_arr, device):
-    for i in range(0, np_arr.ndim):
-        torch_tensor = torch.tensor(np_arr, device=device)
-        torch_rlt = torch.cumsum(torch_tensor, i)
-
-        of_tensor = flow.tensor(np_arr, device=device)
-        of_rlt = flow.cumsum(of_tensor, i)
-
-        if device == "cpu":
-            test_case.assertTrue(np.allclose(torch_rlt.numpy(), of_rlt.numpy()))
-        elif device == "cuda":
-            test_case.assertTrue(np.allclose(torch_rlt.cpu().numpy(), of_rlt.cpu().numpy()))
-
-def test_cumsum_backward(test_case, np_arr, device):
-    for i in range(0, np_arr.ndim):
-        torch_tensor = torch.tensor(np_arr, device=device, requires_grad=True)
-        torch_rlt = torch.cumsum(torch_tensor, i)
-        torch_sum = torch_rlt.sum()
-        torch_sum.backward()
-
-        of_tensor = flow.tensor(np_arr, device=device, requires_grad=True)
-        of_rlt = flow.cumsum(of_tensor, i)
-        of_sum = of_rlt.sum()
-        of_sum.backward()
-
-        if device == "cpu":
-            test_case.assertTrue(np.allclose(torch_tensor.grad.numpy(), of_tensor.grad.numpy()))
-        elif device == "cuda":
-            test_case.assertTrue(np.allclose(torch_tensor.grad.cpu().numpy(), of_tensor.grad.cpu().numpy()))
+from oneflow.test_utils.automated_test_util import *
 
 @flow.unittest.skip_unless_1n1d()
 class TestCumsum(flow.unittest.TestCase):
-    def test_cumsum_forward_cpu(test_case):
-        for i in range(0, 20):
-            np_arr = np.random.randn(3)
-            test_cumsum_forward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3)
-            test_cumsum_forward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cpu")
+    @autotest(check_graph=False)
+    def test_cumsum(test_case):
+        device = random_device()
+        x = random_pytorch_tensor().to(device)
+        dim = random(0, x.ndim.pytorch).to(int)
+        z = torch.cumsum(x, dim)
+        return z
 
-    def test_cumsum_forward_gpu(test_case):
-        for i in range(0, 20):
-            np_arr = np.random.randn(3)
-            test_cumsum_forward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3)
-            test_cumsum_forward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3, 3)
-            test_cumsum_forward(test_case, np_arr, "cuda")
-
-    def test_cumsum_backward_cpu(test_case):
-        for i in range(0, 20):
-            np_arr = np.random.randn(3)
-            test_cumsum_backward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3)
-            test_cumsum_backward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cpu")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cpu")
-
-    def test_cumsum_backward_gpu(test_case):
-        for i in range(0, 20):
-            np_arr = np.random.randn(3)
-            test_cumsum_backward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3)
-            test_cumsum_backward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cuda")
-        for i in range(0, 20):
-            np_arr = np.random.randn(3, 3, 3, 3, 3, 3)
-            test_cumsum_backward(test_case, np_arr, "cuda")
 
 if __name__ == "__main__":
     unittest.main()
