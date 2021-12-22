ac312a614a1cfaea66629cb4e3ca89c5bc22d17a
guo-ran <360112263@qq.com>
2021-12-22 20:04:59 +0800

    update

--- oneflow/user/kernels/embedding_kernels.cu
+++ oneflow/user/kernels/embedding_kernels.cu
@@ -240,8 +240,10 @@ class EmbeddingPrefetchKernel final : public user_op::OpKernel {
 
     cache->Test(ctx->stream(), num_keys, unique_ids->dptr(), buffer_manager.NumCacheMissingPtr(),
                 buffer_manager.CacheMissingKeysPtr(), buffer_manager.CacheMissingIndicesPtr());
-    DumpToFile(ctx->stream(), "CacheMissingKeysPtr", ctx->parallel_ctx().parallel_id(),
-               num_keys * sizeof(K), buffer_manager.CacheMissingKeysPtr());
+    if (ParseBooleanFromEnv("DEBUG_SHUFFLE", false)) {
+      DumpToFile(ctx->stream(), "CacheMissingKeysPtr", ctx->parallel_ctx().parallel_id(),
+                 num_keys * sizeof(K), buffer_manager.CacheMissingKeysPtr());
+    }
     copyd2h_primitive->Launch(ctx->stream(), host_num_keys, buffer_manager.NumCacheMissingPtr(),
                               sizeof(uint32_t));
     CHECK_JUST(ctx->stream()->Sync());
@@ -331,7 +333,7 @@ class EmbeddingLookupKernel final : public user_op::OpKernel {
     const user_op::Tensor* context = ctx->Tensor4ArgNameAndIndex("context", 0);
     user_op::Tensor* embeddings = ctx->Tensor4ArgNameAndIndex("embeddings", 0);
 
-    // just for debug, not need such tmp_buffer
+    // just for debug, lookup not need so much tmp_buffer like prefetch kernel
     user_op::Tensor* tmp_buffer = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
     const int64_t embedding_size = 128;
     PrefetchTmpBufferManager<T, K> buffer_manager(tmp_buffer->mut_dptr(),
@@ -417,6 +419,8 @@ class EmbeddingUpdateKernel final : public user_op::OpKernel {
                const user_op::OpKernelCache*) const override {
     auto* kernel_state = dynamic_cast<EmbeddingKernelState*>(state);
     CHECK(kernel_state != nullptr);
+    embedding::Cache* cache =
+        Global<EmbeddingMgr>::Get()->GetCache("MyEmbeddingTest", ctx->parallel_ctx().parallel_id());
     embedding::KeyValueStore* store = Global<EmbeddingMgr>::Get()->GetKeyValueStore(
         "MyEmbeddingTest", ctx->parallel_ctx().parallel_id());
 
@@ -427,8 +431,12 @@ class EmbeddingUpdateKernel final : public user_op::OpKernel {
     const user_op::Tensor* embedding_diff = ctx->Tensor4ArgNameAndIndex("embedding_diff", 0);
     const int64_t embedding_size =
         unique_embeddings->shape().elem_cnt() / unique_ids->shape().elem_cnt();
+    // just for debug, lookup not need so much tmp_buffer like prefetch kernel
     user_op::Tensor* tmp_buffer = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
-    T* update_unique_embeddings = reinterpret_cast<T*>(tmp_buffer->mut_dptr<char>());
+    PrefetchTmpBufferManager<T, K> buffer_manager(tmp_buffer->mut_dptr(),
+                                                  unique_ids->shape().elem_cnt(), embedding_size);
+    T* update_unique_embeddings = buffer_manager.StoreValuesPtr();
+    uint64_t* mut_context = reinterpret_cast<uint64_t*>(buffer_manager.CacheMissingKeysPtr());
 
     IDX* host_num_keys = reinterpret_cast<IDX*>(kernel_state->HostNumKeys());
     std::unique_ptr<ep::primitive::Memcpy> copyd2h_primitive =
@@ -450,10 +458,17 @@ class EmbeddingUpdateKernel final : public user_op::OpKernel {
            ctx->stream()->As<ep::CudaStream>()->cuda_stream()>>>(
             embedding_size, num_unique_ids->dptr<IDX>(), learning_rate_ptr, learning_rate_val,
             embedding_diff->dptr<T>(), unique_embeddings->dptr<T>(), update_unique_embeddings);
+    cache->Put(ctx->stream(), *host_num_keys, unique_ids->dptr(), update_unique_embeddings,
+               buffer_manager.NumCacheEvictedPtr(), buffer_manager.CacheEvictedKeysPtr(),
+               buffer_manager.CacheEvictedValuesPtr());
 
-    //    store->Update(ctx->stream(), *host_num_keys, unique_ids->dptr(),
-    //                  reinterpret_cast<const uint64_t*>(context->dptr()),
-    //                  update_unique_embeddings);
+    copyd2h_primitive->Launch(ctx->stream(), host_num_keys, buffer_manager.NumCacheEvictedPtr(),
+                              sizeof(uint32_t));
+    CHECK_JUST(ctx->stream()->Sync());
+    uint32_t num_evicted_keys = *host_num_keys;
+    LOG(ERROR) << ctx->parallel_ctx().parallel_id() << " num_evicted_keys " << num_evicted_keys;
+    store->Put(ctx->stream(), num_evicted_keys, buffer_manager.CacheEvictedKeysPtr(),
+               buffer_manager.CacheEvictedValuesPtr(), mut_context);
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
@@ -467,9 +482,11 @@ class EmbeddingUpdateKernel final : public user_op::OpKernel {
           && (user_op::HobDataType("unique_ids", 0) == GetDataType<k_dtype>::value)         \
           && (user_op::HobDataType("unique_embeddings", 0) == GetDataType<t_dtype>::value)) \
       .SetInferTmpSizeFn([](user_op::InferContext* ctx) {                                   \
-        const user_op::TensorDesc& unique_embeddings =                                      \
-            ctx->InputTensorDesc("unique_embeddings", 0);                                   \
-        return unique_embeddings.shape().elem_cnt() * sizeof(t_dtype);                      \
+        const int64_t embedding_size = 128;                                                 \
+        const user_op::TensorDesc& unique_ids = ctx->InputTensorDesc("unique_ids", 0);      \
+        PrefetchTmpBufferManager<t_dtype, k_dtype> buffer_manager(                          \
+            nullptr, unique_ids.shape().elem_cnt(), embedding_size);                        \
+        return buffer_manager.TotalBufferSize();                                            \
       });
 
 REGISTER_CUDA_EMBEDDING_UPDATE_KERNEL(float, int64_t, int32_t)
