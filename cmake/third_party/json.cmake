4836a85d64610c01605032ba418cd6d8905d7001
PragmaTwice <i@twice.moe>
2021-12-28 16:24:04 +0800

    add cache var

--- cmake/third_party/json.cmake
+++ cmake/third_party/json.cmake
@@ -4,7 +4,7 @@ set_mirror_url_with_hash(JSON_URL
   https://github.com/nlohmann/json/archive/refs/tags/v3.10.4.zip
   59c2a25e17b94d612fdb32a1a37378cf
 )
-set(JSON_Install ON)
+set(JSON_Install ON CACHE STRING "" FORCE)
 
 FetchContent_Declare(
     json
