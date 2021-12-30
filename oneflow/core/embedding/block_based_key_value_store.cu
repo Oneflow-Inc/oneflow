501d9a698b9fe4ee1ef5052d227744c77e236a12
liujuncheng <liujuncheng1022@gmail.com>
2021-12-30 19:03:50 +0800

    direct write

--- oneflow/core/embedding/block_based_key_value_store.cu
+++ oneflow/core/embedding/block_based_key_value_store.cu
@@ -37,7 +37,13 @@ class FileGuard final {
   FileGuard(const std::string& filename, int flag, size_t trunc) : fd_(-1) {
     fd_ = open(filename.c_str(), flag, 0644);
     PCHECK(fd_ != -1);
-    if (trunc != 0) { PCHECK(ftruncate(fd_, trunc) == 0); }
+    if (trunc != 0) {
+      const size_t size = Size();
+      if (size != trunc) {
+        CHECK_EQ(size, 0);
+        PCHECK(ftruncate(fd_, trunc) == 0);
+      }
+    }
   }
   FileGuard& operator=(FileGuard&& other) noexcept {
     fd_ = other.fd_;
@@ -66,61 +72,28 @@ class FileGuard final {
   int fd_;
 };
 
-enum class ChunkType {
-  kInvalid,
-  kMapped,
-  kFile,
-};
-
 struct Chunk {
   Chunk() {}
   Chunk(Chunk&& other) noexcept { *this = std::move(other); }
   Chunk& operator=(Chunk&& other) noexcept {
-    chunk_type = other.chunk_type;
-    other.chunk_type = ChunkType::kInvalid;
-    ptr = other.ptr;
-    other.ptr = nullptr;
     file = std::move(other.file);
     size = other.size;
     other.size = 0;
     return *this;
   }
   size_t size = 0;
-  void* ptr = nullptr;
-  ChunkType chunk_type = ChunkType::kInvalid;
   FileGuard file;
 };
 
-Chunk OpenMappedChunk(const std::string& filename, size_t size) {
+Chunk OpenFileChunk(const std::string& filename, size_t size) {
   Chunk chunk;
-  chunk.chunk_type = ChunkType::kMapped;
-  FileGuard file(filename, O_RDWR | O_CREAT, size);
-  chunk.file = std::move(file);
-  chunk.ptr = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_SHARED, chunk.file.fd(), 0);
-  chunk.size = size;
-  PCHECK(chunk.ptr != MAP_FAILED);
-  return chunk;
-}
-
-void CloseMappedChunk(Chunk* chunk) {
-  CHECK(chunk->chunk_type == ChunkType::kMapped);
-  PCHECK(munmap(chunk->ptr, chunk->size) == 0);
-  chunk->file.Close();
-}
-
-Chunk OpenFileChunk(const std::string& filename) {
-  Chunk chunk;
-  chunk.chunk_type = ChunkType::kFile;
-  FileGuard file(filename, O_RDONLY | O_DIRECT, 0);
+  FileGuard file(filename, O_CREAT | O_RDWR | O_DIRECT, size);
   chunk.file = std::move(file);
   chunk.size = file.Size();
   return chunk;
 }
 
-void CloseFileChunk(Chunk* chunk) {
-  CHECK(chunk->chunk_type == ChunkType::kFile);
-  chunk->file.Close();
-}
+void CloseFileChunk(Chunk* chunk) { chunk->file.Close(); }
 
 template<typename Key>
 class KeyValueStoreImpl : public KeyValueStore {
@@ -138,25 +111,29 @@ class KeyValueStoreImpl : public KeyValueStore {
     value_size_ = GetSizeOfDataType(options.value_type) * value_length_;
     CHECK_GE(block_size_, value_size_);
     values_per_block_ = block_size_ / value_size_;
-    OF_CUDA_CHECK(
-        NumaAwareCudaMallocHost(device_index_, &host_query_keys_, key_size_ * max_query_length_));
-    OF_CUDA_CHECK(NumaAwareCudaMallocHost(device_index_, &host_query_values_,
+    OF_CUDA_CHECK(NumaAwareCudaMallocHost(
+        device_index_, reinterpret_cast<void**>(&host_query_keys_), key_size_ * max_query_length_));
+    OF_CUDA_CHECK(NumaAwareCudaMallocHost(device_index_,
+                                          reinterpret_cast<void**>(&host_query_values_),
                                           value_size_ * max_query_length_));
     OF_CUDA_CHECK(NumaAwareCudaMallocHost(device_index_, reinterpret_cast<void**>(&host_n_missing_),
                                           sizeof(uint32_t)));
-    OF_CUDA_CHECK(
-        NumaAwareCudaMallocHost(device_index_, &host_missing_keys_, key_size_ * max_query_length_));
+    OF_CUDA_CHECK(NumaAwareCudaMallocHost(device_index_,
+                                          reinterpret_cast<void**>(&host_missing_keys_),
+                                          key_size_ * max_query_length_));
     OF_CUDA_CHECK(NumaAwareCudaMallocHost(device_index_,
                                           reinterpret_cast<void**>(&host_missing_indices_),
                                           sizeof(uint32_t) * max_query_length_));
+    host_query_blocks_ =
+        static_cast<uint8_t*>(aligned_alloc(block_size_, (max_query_length_ + values_per_block_ - 1)
+                                                             / values_per_block_ * block_size_));
   }
   ~KeyValueStoreImpl() {
     CudaCurrentDeviceGuard guard(device_index_);
-    for (auto& chunk : chunks_) {
-      if (chunk.chunk_type == ChunkType::kMapped) { CloseMappedChunk(&chunk); }
-    }
+    for (auto& chunk : chunks_) { CloseFileChunk(&chunk); }
     OF_CUDA_CHECK(cudaFreeHost(host_query_keys_));
     OF_CUDA_CHECK(cudaFreeHost(host_query_values_));
+    free(host_query_blocks_);
   }
 
   void Get(ep::Stream* stream, uint32_t num_keys, const void* keys, void* values,
@@ -173,10 +150,10 @@ class KeyValueStoreImpl : public KeyValueStore {
   uint32_t max_query_length_;
   uint32_t key_size_;
   uint32_t value_size_;
-  void* host_query_keys_{};
-  void* host_query_values_{};
+  Key* host_query_keys_{};
+  uint8_t* host_query_values_{};
   uint32_t* host_n_missing_{};
-  void* host_missing_keys_{};
+  Key* host_missing_keys_{};
   uint32_t* host_missing_indices_{};
   std::atomic<uint64_t> counter_;
   uint64_t block_size_;
@@ -184,6 +161,7 @@ class KeyValueStoreImpl : public KeyValueStore {
   robin_hood::unordered_flat_map<Key, uint64_t> key2id_;
   std::vector<Chunk> chunks_;
   std::string path_;
+  uint8_t* host_query_blocks_;
 };
 
 template<typename Key>
@@ -213,12 +191,12 @@ void KeyValueStoreImpl<Key>::Get(ep::Stream* stream, uint32_t num_keys, const vo
     const uint64_t start_i = thread_id * keys_per_thread;
     const uint64_t end_i = std::min(start_i + keys_per_thread, static_cast<uint64_t>(num_keys));
     for (uint64_t i = start_i; i < end_i; ++i) {
-      const Key key = static_cast<const Key*>(host_query_keys_)[i];
+      const Key key = host_query_keys_[i];
       auto it = key2id_.find(key);
       if (it == key2id_.end()) {
         const uint64_t missing_i = missing_counter.fetch_add(1, std::memory_order_relaxed);
         host_missing_indices_[missing_i] = i;
-        static_cast<Key*>(host_missing_keys_)[missing_i] = key;
+        host_missing_keys_[missing_i] = key;
       } else {
         const uint64_t id = it->second;
         const uint64_t block_id = id / values_per_block_;
@@ -228,17 +206,9 @@ void KeyValueStoreImpl<Key>::Get(ep::Stream* stream, uint32_t num_keys, const vo
         const uint64_t block_offset = block_in_chunk * block_size_;
         const uint64_t offset_in_chunk = block_offset + id_in_block * value_size_;
         Chunk& chunk = chunks_.at(chunk_id);
-        if (chunk.chunk_type == ChunkType::kMapped) {
-          std::memcpy(static_cast<uint8_t*>(host_query_values_) + i * value_size_,
-                      static_cast<uint8_t*>(chunk.ptr) + offset_in_chunk, value_size_);
-        } else if (chunk.chunk_type == ChunkType::kFile) {
-          PCHECK(pread(chunk.file.fd(), block_data.get(), block_size_, block_offset)
-                 == block_size_);
-          std::memcpy(static_cast<uint8_t*>(host_query_values_) + i * value_size_,
-                      block_data.get() + id_in_block * value_size_, value_size_);
-        } else {
-          UNIMPLEMENTED();
-        }
+        PCHECK(pread(chunk.file.fd(), block_data.get(), block_size_, block_offset) == block_size_);
+        std::memcpy(static_cast<uint8_t*>(host_query_values_) + i * value_size_,
+                    block_data.get() + id_in_block * value_size_, value_size_);
       }
     }
   }
@@ -266,43 +236,40 @@ void KeyValueStoreImpl<Key>::Put(ep::Stream* stream, uint32_t num_keys, const vo
   OF_CUDA_CHECK(cudaMemcpyAsync(host_query_values_, values, value_size_ * num_keys,
                                 cudaMemcpyDefault, cuda_stream->cuda_stream()));
   CHECK_JUST(cuda_stream->Sync());
-  const Key* keys_ptr = static_cast<const Key*>(host_query_keys_);
-  const uint64_t start = counter_.fetch_add(num_keys);
-  const uint64_t end = start + num_keys;
-  for (uint64_t i = 0; i < num_keys; ++i) { key2id_[keys_ptr[i]] = start + i; }
-  const uint64_t min_block_id = start / values_per_block_;
-  const uint64_t max_block_id = (end - 1) / values_per_block_;
-  const uint64_t min_chunk_id = min_block_id / NUM_BLOCKS_PER_CHUNK;
-  const uint64_t max_chunk_id = max_block_id / NUM_BLOCKS_PER_CHUNK;
-  chunks_.reserve(max_chunk_id + 1);
-  for (uint64_t new_chunk_id = chunks_.size(); new_chunk_id <= max_chunk_id; ++new_chunk_id) {
+  const uint32_t num_blocks = (num_keys + values_per_block_ - 1) / values_per_block_;
+  const uint32_t num_padded_keys = num_blocks * values_per_block_;
+  const uint64_t start = counter_.fetch_add(num_padded_keys);
+  CHECK_EQ(start % values_per_block_, 0);
+  const uint64_t start_block_id = start / values_per_block_;
+  for (uint64_t i = 0; i < num_keys; ++i) {
+    const uint64_t block_id = i / values_per_block_;
+    const uint64_t idx_in_block = i - block_id * values_per_block_;
+    std::memcpy(host_query_blocks_ + block_id * block_size_ + idx_in_block * value_size_,
+                host_query_values_ + i * value_size_, value_size_);
+  }
+  for (uint64_t i = 0; i < num_keys; ++i) { key2id_[host_query_keys_[i]] = start + i; }
+  const uint64_t end_block_id = start_block_id + num_blocks - 1;
+  const uint64_t start_chunk_id = start_block_id / NUM_BLOCKS_PER_CHUNK;
+  const uint64_t end_chunk_id = end_block_id / NUM_BLOCKS_PER_CHUNK;
+  chunks_.reserve(end_chunk_id + 1);
+  for (uint64_t new_chunk_id = chunks_.size(); new_chunk_id <= end_chunk_id; ++new_chunk_id) {
     chunks_.emplace_back(
-        OpenMappedChunk(ChunkName(new_chunk_id), NUM_BLOCKS_PER_CHUNK * block_size_));
+        OpenFileChunk(ChunkName(new_chunk_id), NUM_BLOCKS_PER_CHUNK * block_size_));
   }
-#pragma omp parallel num_threads(4)
-  {
-    const uint32_t thread_id = omp_get_thread_num();
-    const uint32_t num_threads = omp_get_num_threads();
-    const uint32_t keys_per_thread = (num_keys + num_threads - 1) / num_threads;
-    const uint64_t start_i = thread_id * keys_per_thread;
-    const uint64_t end_i = std::min(start_i + keys_per_thread, static_cast<uint64_t>(num_keys));
-    for (uint64_t i = start_i; i < end_i; ++i) {
-      const uint64_t block_id = (start + i) / values_per_block_;
-      const uint64_t id_in_block = (start + i) - block_id * values_per_block_;
-      const uint64_t chunk_id = block_id / NUM_BLOCKS_PER_CHUNK;
-      const uint64_t block_in_chunk = block_id - chunk_id * NUM_BLOCKS_PER_CHUNK;
-      const uint64_t block_offset = block_in_chunk * block_size_;
-      const uint64_t offset_in_chunk = block_offset + id_in_block * value_size_;
-      std::memcpy(static_cast<uint8_t*>(chunks_.at(chunk_id).ptr) + offset_in_chunk,
-                  static_cast<const uint8_t*>(host_query_values_) + i * value_size_, value_size_);
-    }
-  }
-  const uint64_t next_chunk_id = end / values_per_block_ / NUM_BLOCKS_PER_CHUNK;
-  const uint64_t max_close_chunk_id =
-      next_chunk_id != max_chunk_id ? max_chunk_id + 1 : max_chunk_id;
-  for (uint64_t chunk_id = min_chunk_id; chunk_id < max_close_chunk_id; ++chunk_id) {
-    CloseMappedChunk(&chunks_.at(chunk_id));
-    chunks_.at(chunk_id) = std::move(OpenFileChunk(ChunkName(chunk_id)));
+  uint64_t write_blocks = 0;
+  while (write_blocks < num_blocks) {
+    const uint64_t batch_start_block_id = start_block_id + write_blocks;
+    const uint64_t batch_chunk_id = batch_start_block_id / NUM_BLOCKS_PER_CHUNK;
+    const uint64_t block_in_chunk = batch_start_block_id - batch_chunk_id * NUM_BLOCKS_PER_CHUNK;
+    const uint64_t blocks_to_write =
+        std::min(num_blocks - write_blocks,
+                 (batch_chunk_id + 1) * NUM_BLOCKS_PER_CHUNK - batch_start_block_id);
+    const uint64_t bytes_to_write = blocks_to_write * block_size_;
+    Chunk& chunk = chunks_.at(batch_chunk_id);
+    PCHECK(pwrite(chunk.file.fd(), host_query_blocks_ + write_blocks * block_size_, bytes_to_write,
+                  block_in_chunk * block_size_)
+           == bytes_to_write);
+    write_blocks += blocks_to_write;
   }
 }
 
