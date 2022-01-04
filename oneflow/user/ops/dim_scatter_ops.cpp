4c46530057dce48c92cf90da415910af661a1270
wangyi <buaawangyi03@gmail.com>
2022-01-04 14:05:54 +0800

    refine error message on modified UNIMPLEMENTED

--- oneflow/user/ops/dim_scatter_ops.cpp
+++ oneflow/user/ops/dim_scatter_ops.cpp
@@ -45,8 +45,7 @@ Maybe<void> InferTensorDesc(user_op::InferContext* ctx) {
   } else if (like) {
     output_num_axes = like->shape().NumAxes();
   } else {
-    OF_UNIMPLEMENTED() << "Input tensor and like tensor cannot be empty simultaneously."
-                       << std::endl;
+    OF_UNIMPLEMENTED() << "Input tensor and like tensor cannot be empty simultaneously.";
   }
   CHECK_EQ_OR_RETURN(output_num_axes, index_num_axes);
 
