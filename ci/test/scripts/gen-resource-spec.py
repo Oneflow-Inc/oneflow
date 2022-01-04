569c2ca84b328147a65773de8ca963a38a55372d
jackalcooper <jackalcooper@gmail.com>
2022-01-04 15:39:18 +0800

    support run without docker in bash wrapper

--- ci/test/scripts/gen-resource-spec.py
+++ ci/test/scripts/gen-resource-spec.py
@@ -2,21 +2,29 @@ import subprocess
 import csv
 import json
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--cuda", default=False, action="store_true", required=False)
+
+args = parser.parse_args()
 
 output = {
     "version": {"major": 1, "minor": 0},
     "local": [],
 }
 
-gpus_str = subprocess.check_output(
-    "nvidia-smi --query-gpu=index,memory.total --format=csv,nounits,noheader",
-    shell=True,
-).decode()
-reader = csv.reader(gpus_str.splitlines(), delimiter=",")
-resource = {
-    ("cuda" + line[0].strip()): [{"id": "0", "slots": int(line[1].strip())}]
-    for line in reader
-}
+resource = {}
+if args.cuda:
+    gpus_str = subprocess.check_output(
+        "nvidia-smi --query-gpu=index,memory.total --format=csv,nounits,noheader",
+        shell=True,
+    ).decode()
+    reader = csv.reader(gpus_str.splitlines(), delimiter=",")
+    resource = {
+        ("cuda" + line[0].strip()): [{"id": "0", "slots": int(line[1].strip())}]
+        for line in reader
+    }
 
 resource["cpu"] = [{"id": "0", "slots": os.cpu_count()}]
 
