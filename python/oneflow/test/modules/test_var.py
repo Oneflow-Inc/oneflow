1036e0b7c311cc62592b40fe06dd60cead606f81
liufengwei <2472937968@qq.com>
2022-01-05 18:17:38 +0800

    refine

--- python/oneflow/test/modules/test_var.py
+++ python/oneflow/test/modules/test_var.py
@@ -41,8 +41,8 @@ class TestVar(flow.unittest.TestCase):
         )
         return y
 
-    # TODO(): 'var backward' is composed of several other op,
-    # reducemean not support 0-shape for now
+    # TODO(): 'var backward' is composed of several other ops,
+    # reducemean doesn't support 0-shape for now
     @autotest(auto_backward=False, check_graph=False)
     def test_flow_var_0_size_data_with_random_data(test_case):
         device = random_device()
