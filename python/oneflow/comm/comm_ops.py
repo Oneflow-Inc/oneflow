dd635afbafbf2836e614d6296a91c7cb0b4917ec
liufengwei <2472937968@qq.com>
2022-01-12 17:21:21 +0800

    refine

--- python/oneflow/comm/comm_ops.py
+++ python/oneflow/comm/comm_ops.py
@@ -112,6 +112,7 @@ def all_gather(tensor_list, tensor):
         placement=placement, sbp=flow.sbp.split(0)
     ).to_consistent(placement=placement, sbp=flow.sbp.broadcast).to_local()
     assert len(tensor_list) == flow.env.get_world_size()
+    # TODO(): getitem has bug on consistent tensor with size = [2, 1].
     for i in range(tensor.shape[0]):
         tensor_list[i] = tensor[i]
 
