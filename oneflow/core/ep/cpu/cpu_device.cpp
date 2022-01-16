964f0a86138b889ecf67bd94e68fcf7da46c3c8a
luqiang-guo <702572275@qq.com>
2022-01-16 22:51:03 +0800

    fix

--- oneflow/core/ep/cpu/cpu_device.cpp
+++ oneflow/core/ep/cpu/cpu_device.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <sys/sysinfo.h>
 #include "oneflow/core/ep/cpu/cpu_device.h"
 #include "oneflow/core/ep/cpu/cpu_event.h"
 #include "oneflow/core/ep/cpu/cpu_stream.h"
