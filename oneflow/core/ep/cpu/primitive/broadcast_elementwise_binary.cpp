936760920cc609966a90b76d7f597041f5c944ef
luqiang-guo <702572275@qq.com>
2022-01-17 11:24:46 +0800

    fix

--- oneflow/core/ep/cpu/primitive/broadcast_elementwise_binary.cpp
+++ oneflow/core/ep/cpu/primitive/broadcast_elementwise_binary.cpp
@@ -13,8 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <tbb/blocked_range.h>
-#include <tbb/parallel_for.h>
 #include "oneflow/core/ep/include/primitive/broadcast_elementwise_binary.h"
 #include "oneflow/core/ep/common/primitive/broadcast_elementwise_binary.h"
 #include "oneflow/core/ep/cpu/primitive/binary_functor.h"
