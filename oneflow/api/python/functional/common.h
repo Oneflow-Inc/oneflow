46e9ebaef026e67c20d07353d03da71cad890dad
lixinqi <lixinqi0703106@163.com>
2022-01-17 17:59:47 +0800

    replace gil lock with foreign lock helper

--- oneflow/api/python/functional/common.h
+++ oneflow/api/python/functional/common.h
@@ -31,6 +31,8 @@ limitations under the License.
 #include "oneflow/core/framework/tensor_tuple.h"
 #include "oneflow/core/framework/random_generator.h"
 #include "oneflow/core/functional/tensor_index.h"
+#include "oneflow/core/common/global.h"
+#include "oneflow/core/common/foreign_lock_helper.h"
 
 namespace py = pybind11;
 
@@ -40,9 +42,11 @@ namespace functional {
 
 struct PyObjectPtrDeleter {
   inline void operator()(PyObject* obj) {
-    py::gil_scoped_acquire acquire;
-    if (obj) { Py_DECREF(obj); }
-    obj = NULL;
+    CHECK_JUST(Global<ForeignLockHelper>::Get()->WithScopedAcquire([&]() -> Maybe<void> {
+      if (obj) { Py_DECREF(obj); }
+      obj = NULL;
+      return Maybe<void>::Ok();
+    }));
   }
 };
 
