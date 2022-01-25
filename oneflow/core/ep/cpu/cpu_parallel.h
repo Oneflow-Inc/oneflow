da9e17387f289cd3a54c9cfdf673724ceda4f3be
luqiang-guo <702572275@qq.com>
2022-01-25 17:42:35 +0800

    add set_num_threads

--- oneflow/core/ep/cpu/cpu_parallel.h
+++ oneflow/core/ep/cpu/cpu_parallel.h
@@ -34,6 +34,16 @@ namespace oneflow {
 namespace ep {
 namespace primitive {
 
+
+void set_num_threads(int nthr) {
+#if OF_CPU_THREADING_RUNTIME == OF_RUNTIME_OMP
+  // Affects omp_get_max_threads() Get the logical core book
+  omp_set_num_threads(nthr);
+#elif OF_CPU_THREADING_RUNTIME == OF_RUNTIME_TBB
+  tbb::global_control global_thread_limit(tbb::global_control::max_allowed_parallelism, nthr);
+#endif
+}
+
 inline size_t divup(int64_t x, int64_t y) { return (x + y - 1) / y; }
 
 // params nthr : Maximum number of threads to open
