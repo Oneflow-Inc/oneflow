0473cd30ae44c680fee15506086283bf5439a5b3
CHI LIU <42956025+thinksoso@users.noreply.github.com>
2022-01-28 19:11:12 +0800

    Update tensor.py

--- python/oneflow/framework/tensor.py
+++ python/oneflow/framework/tensor.py
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from re import T
 import oneflow as flow
 from oneflow._oneflow_internal.exception import IndexException
 import oneflow.framework.tensor_str as tensor_str
