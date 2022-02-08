2dbbe428e176d01be6c452e8a9f8d1b94e3a3560
liufengwei <2472937968@qq.com>
2022-02-08 10:42:57 +0800

    fix return type

--- oneflow/api/python/utils/tensor_utils.h
+++ oneflow/api/python/utils/tensor_utils.h
@@ -38,14 +38,14 @@ namespace pybind11 {
 template<>
 struct format_descriptor<oneflow::float16> {
   static pybind11::dtype dtype() {
-    handle ptr = pybind11::detail::npy_api::get().PyArray_DescrFromType_(NPY_FLOAT16);
+    handle ptr = detail::npy_api::get().PyArray_DescrFromType_(NPY_FLOAT16);
     return reinterpret_borrow<pybind11::dtype>(ptr);
   }
   static std::string format() {
     // following: https://docs.python.org/3/library/struct.html#format-characters
     return "e";
   }
-  static constexpr auto name() { return "float16"; }
+  static constexpr auto name() { return detail::_("float16"); }
 };
 }  // namespace pybind11
 
