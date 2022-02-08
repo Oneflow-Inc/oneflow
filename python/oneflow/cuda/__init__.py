39fa8a3b5ebb66a1ac05915932a5cb43c8affd66
Aries-chen <1416535234@qq.com>
2022-02-08 19:26:46 +0800

    add specific interface of tensor

--- python/oneflow/cuda/__init__.py
+++ python/oneflow/cuda/__init__.py
@@ -17,6 +17,7 @@ import oneflow as flow
 
 from oneflow.cuda.type_tensor import *
 
+
 def is_available() -> bool:
     r"""Returns a bool indicating if CUDA is currently available."""
     # This function never throws and returns 0 if driver is missing or can't
