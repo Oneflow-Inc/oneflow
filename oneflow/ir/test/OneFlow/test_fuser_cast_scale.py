e88f3c3d306b210c11cf1079a344d5acadb470d0
jackalcooper <jackalcooper@gmail.com>
2022-02-09 13:16:28 +0000

    port test to nn graph

---
+++ oneflow/ir/test/OneFlow/test_fuser_cast_scale.py
@@ -0,0 +1,61 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+# RUN: python3 %s
+
+import unittest
+import numpy as np
+
+import oneflow as flow
+import oneflow.unittest
+import os
+
+os.environ["ONEFLOW_MLIR_ENABLE_ROUND_TRIP"] = "1"
+os.environ["ONEFLOW_MLIR_STDOUT"] = "1"
+os.environ["ONEFLOW_MLIR_ENABLE_CODEGEN_FUSERS"] = "1"
+
+
+class CastModule(flow.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x.to(dtype=flow.float32) * 3.3
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestFuseCastScale(oneflow.unittest.TestCase):
+    def test_relu_graph(test_case):
+        data = np.array([2.0, 1.0, 0.0, -1.0, -2.0])
+        x = flow.tensor(data, dtype=flow.int64)
+
+        module_to_run = CastModule()
+        y_eager = module_to_run(x)
+
+        class GraphToRun(flow.nn.Graph):
+            def __init__(self):
+                super().__init__()
+                self.fw = module_to_run
+
+            def build(self, x):
+                return self.fw(x)
+
+        graph_to_run = GraphToRun()
+        y_lazy = graph_to_run(x)
+        test_case.assertTrue(np.array_equal(y_eager.numpy(), y_lazy.numpy()))
+
+
+if __name__ == "__main__":
+    unittest.main()
