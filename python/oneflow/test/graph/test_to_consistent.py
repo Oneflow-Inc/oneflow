e9e5b75a2b82bc52bc4065139ab00c76895fc555
chengtbf <472491134@qq.com>
2022-02-09 17:50:11 +0800

    refine test code

--- python/oneflow/test/graph/test_to_consistent.py
+++ python/oneflow/test/graph/test_to_consistent.py
@@ -482,17 +482,17 @@ class ToConsistent2DGraphTestCase(oneflow.unittest.TestCase):
 @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
 class TestLazy1dTo2dConsistent(flow.unittest.TestCase):
     def test_lazy_1d_to_2d_sbp(test_case):
+        P_1d = flow.placement(
+            device_type="cuda", device_ids={0: range(4)}, hierarchy=(4,)
+        )
+        P_2d = flow.placement(
+            device_type="cuda", device_ids={0: range(4)}, hierarchy=(2, 2)
+        )
+        B = flow.sbp.broadcast
+
         class Test1dTo2dModule(flow.nn.Module):
             def forward(self, x):
-                x = x.to_consistent(
-                    placement=flow.placement(
-                        device_type="cuda",
-                        device_ids={0: [0, 1, 2, 3]},
-                        hierarchy=(2, 2),
-                    ),
-                    sbp=[flow.sbp.broadcast, flow.sbp.broadcast],
-                )
-                return x
+                return x.to_consistent(placement=P_2d, sbp=[B, B])
 
         class Test1dTo2dGraph(flow.nn.Graph):
             def __init__(self, model):
@@ -500,29 +500,36 @@ class TestLazy1dTo2dConsistent(flow.unittest.TestCase):
                 self.model = model
 
             def build(self, x):
-                x = self.model(x)
-                return x
+                return self.model(x)
 
-        model = Test1dTo2dModule()
-        graph = Test1dTo2dGraph(model)
+        class Test2dTo1dModule(flow.nn.Module):
+            def forward(self, x):
+                return x.to_consistent(placement=P_1d, sbp=[B])
 
-        x = flow.zeros(
-            4,
-            4,
-            4,
-            4,
-            sbp=[flow.sbp.broadcast, flow.sbp.broadcast],
-            placement=flow.placement("cuda", {0: range(4)}, (2, 2)),
-        )
-        x = x.to_consistent(
-            placement=flow.placement(
-                device_type="cuda", device_ids={0: [0, 1, 2, 3]}, hierarchy=(4,)
-            ),
-            sbp=[flow.sbp.broadcast],
-        )
-        print(x.sbp)
-        y = graph(x)
-        print(y.sbp)
+        class Test2dTo1dGraph(flow.nn.Graph):
+            def __init__(self, model):
+                super().__init__()
+                self.model = model
+
+            def build(self, x):
+                return self.model(x)
+
+        model_1d_to_2d = Test1dTo2dModule()
+        graph_1d_to_2d = Test1dTo2dGraph(model_1d_to_2d)
+
+        x = flow.zeros(4, 4, 4, 4, sbp=[B, B], placement=P_2d)
+        x = x.to_consistent(placement=P_1d, sbp=[B])
+        test_case.assertTrue(x.sbp == (B,))
+        test_case.assertTrue(x.placement == P_1d)
+        y = graph_1d_to_2d(x)
+        test_case.assertTrue(y.sbp == (B, B))
+        test_case.assertTrue(y.placement == P_2d)
+
+        model_2d_to_1d = Test2dTo1dModule()
+        graph_2d_to_1d = Test2dTo1dGraph(model_2d_to_1d)
+        z = graph_2d_to_1d(y)
+        test_case.assertTrue(z.sbp == x.sbp)
+        test_case.assertTrue(z.placement == x.placement)
 
 
 if __name__ == "__main__":
