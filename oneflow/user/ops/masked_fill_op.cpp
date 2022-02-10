df94b9198e372694ad203b1aad0f4befc870c6a1
liufengwei <2472937968@qq.com>
2022-02-10 10:48:58 +0800

    eager consistent masked fill test case

--- oneflow/user/ops/masked_fill_op.cpp
+++ oneflow/user/ops/masked_fill_op.cpp
@@ -58,8 +58,8 @@ Maybe<void> GetMaskedFillSbpSignatures(user_op::SbpContext* ctx) {
   }
   ctx->NewBuilder()
       .Broadcast(user_op::OpArg("mask", 0))
-      .PartialSum(user_op::OpArg("x", 0))
-      .PartialSum(user_op::OpArg("out", 0))
+      .Broadcast(user_op::OpArg("x", 0))
+      .Broadcast(user_op::OpArg("out", 0))
       .Build();
   return Maybe<void>::Ok();
 }
