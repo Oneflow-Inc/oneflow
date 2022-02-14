4bbffe14db9264cee28209e6c2fa0e7e37c1381b
liufengwei0103 <2472937968@qq.com>
2022-02-14 17:58:22 +0800

    refine error msg

--- python/oneflow/nn/modules/masked_select.py
+++ python/oneflow/nn/modules/masked_select.py
@@ -47,7 +47,7 @@ def masked_select_op(input, mask):
     ), f"The dim of masked_select module's inputs can not match, please check!"
     assert (
         input.is_global == mask.is_global
-    ), f"tensor type of input is different from mask."
+    ), f"input tensor is %s tensor, but mask is %s tensor" % ("global" if input.is_global else "local", "global" if mask.is_global else "local")
     res = flow._C.mul(input, mask)
 
     indices = flow.argwhere(res)
