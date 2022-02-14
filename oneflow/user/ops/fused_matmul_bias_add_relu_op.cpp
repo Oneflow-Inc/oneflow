b88b04057b974e49537ececb4d1a8a9273e2ac52
MARD1NO <359521840@qq.com>
2022-02-14 15:27:51 +0800

    add multi input

--- oneflow/user/ops/fused_matmul_bias_add_relu_op.cpp
+++ oneflow/user/ops/fused_matmul_bias_add_relu_op.cpp
@@ -14,73 +14,129 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/common/just.h"
+#include "oneflow/core/common/maybe.h"
 #include "oneflow/core/framework/framework.h"
+#include "oneflow/core/framework/infer_util.h"
 #include "oneflow/core/framework/op_generated.h"
 
 namespace oneflow {
 
 namespace {
 
+// Maybe<void> InferTensorDesc4FusedMatmul(user_op::InferContext* ctx) {
+//   const user_op::TensorDesc& a = ctx->InputTensorDesc("a", 0);
+//   const user_op::TensorDesc& b = ctx->InputTensorDesc("b", 0);
+//   const user_op::TensorDesc& bias1 = ctx->InputTensorDesc("bias1", 0);
+//   const user_op::TensorDesc& c = ctx->InputTensorDesc("c", 0);
+//   const user_op::TensorDesc& bias2 = ctx->InputTensorDesc("bias2", 0);
+
+//   CHECK_EQ_OR_RETURN(a.shape().NumAxes(), b.shape().NumAxes())
+//       << "Num axes size of a and b should be equal.";
+//   CHECK_EQ_OR_RETURN(b.shape().NumAxes(), c.shape().NumAxes())
+//       << "Num axes size of b and c should be equal.";
+
+//   CHECK_EQ_OR_RETURN(bias1.shape().NumAxes(), 1) << "Bias1 num axes size should be 1.";
+//   CHECK_EQ_OR_RETURN(bias2.shape().NumAxes(), 1) << "Bias2 num axes size should be 1.";
+//   // Currently only support 2d matmul. 
+//   CHECK_EQ_OR_RETURN(a.shape().NumAxes(), 2);
+//   size_t a_num_axes = a.shape().NumAxes();
+//   size_t b_num_axes = b.shape().NumAxes();
+//   size_t c_num_axes = c.shape().NumAxes();
+
+//   user_op::TensorDesc* out = ctx->OutputTensorDesc("out", 0);
+//   user_op::TensorDesc* aux = ctx->OutputTensorDesc("aux", 0);
+
+//   *ctx->OutputShape("out", 0) = ctx->InputShape("a", 0);
+//   *ctx->OutputIsDynamic("out", 0) = ctx->InputIsDynamic("a", 0);
+//   *ctx->OutputShape("aux", 0) = ctx->InputShape("a", 0);
+//   *ctx->OutputIsDynamic("aux", 0) = ctx->InputIsDynamic("a", 0);
+//   /*
+//   A: (m, k)
+//   B: (n, k) need transpose
+//   C: (j, n) need transpose
+//   */
+//   int64_t m = 0, n = 0, k = 0, j = 0;  // tensor a (no trans): m*k, tensor b (no trans): k*n
+//   m = a.shape().At(a_num_axes - 2);
+//   k = a.shape().At(a_num_axes - 1);
+//   CHECK_EQ_OR_RETURN(k, b.shape().At(b_num_axes - 1));
+//   n = b.shape().At(b_num_axes - 2);
+//   CHECK_EQ_OR_RETURN(bias1.shape().At(0), n)
+//       << "Bias1 shape cannot be added (" << bias1.shape().At(0) << ") and (" << n << ")";
+//   aux->mut_shape()->Set(a_num_axes - 2, m);
+//   aux->mut_shape()->Set(a_num_axes - 1, n);
+//   CHECK_EQ_OR_RETURN(n, c.shape().At(c_num_axes - 1));
+//   j = c.shape().At(c_num_axes - 2);
+//   CHECK_EQ_OR_RETURN(bias2.shape().At(0), j)  
+//       << "Bias2 shape cannot be added (" << bias2.shape().At(0) << ") and (" << j << ")";
+//   out->mut_shape()->Set(a_num_axes - 2, m);
+//   out->mut_shape()->Set(a_num_axes - 1, j);
+//   return Maybe<void>::Ok();
+// }
+
 Maybe<void> InferTensorDesc4FusedMatmul(user_op::InferContext* ctx) {
-  const user_op::TensorDesc& a = ctx->InputTensorDesc("a", 0);
-  const user_op::TensorDesc& b = ctx->InputTensorDesc("b", 0);
-  const user_op::TensorDesc& bias1 = ctx->InputTensorDesc("bias1", 0);
-  const user_op::TensorDesc& c = ctx->InputTensorDesc("c", 0);
-  const user_op::TensorDesc& bias2 = ctx->InputTensorDesc("bias2", 0);
-
-  CHECK_EQ_OR_RETURN(a.shape().NumAxes(), b.shape().NumAxes())
-      << "Num axes size of a and b should be equal.";
-  CHECK_EQ_OR_RETURN(b.shape().NumAxes(), c.shape().NumAxes())
-      << "Num axes size of b and c should be equal.";
-
-  CHECK_EQ_OR_RETURN(bias1.shape().NumAxes(), 1) << "Bias1 num axes size should be 1.";
-  CHECK_EQ_OR_RETURN(bias2.shape().NumAxes(), 1) << "Bias2 num axes size should be 1.";
-  // Currently only support 2d matmul. 
-  CHECK_EQ_OR_RETURN(a.shape().NumAxes(), 2);
-  size_t a_num_axes = a.shape().NumAxes();
-  size_t b_num_axes = b.shape().NumAxes();
-  size_t c_num_axes = c.shape().NumAxes();
-
+  const user_op::TensorDesc& x_desc = ctx->InputTensorDesc("x", 0);
+  const user_op::TensorDesc& first_weight_desc = ctx->InputTensorDesc("weights", 0);
+  const user_op::TensorDesc& first_bias_desc = ctx->InputTensorDesc("biases", 0);
   user_op::TensorDesc* out = ctx->OutputTensorDesc("out", 0);
-  user_op::TensorDesc* aux = ctx->OutputTensorDesc("aux", 0);
-
-  *ctx->OutputShape("out", 0) = ctx->InputShape("a", 0);
-  *ctx->OutputIsDynamic("out", 0) = ctx->InputIsDynamic("a", 0);
-  *ctx->OutputShape("aux", 0) = ctx->InputShape("a", 0);
-  *ctx->OutputIsDynamic("aux", 0) = ctx->InputIsDynamic("a", 0);
+  int32_t weight_size = ctx->input_size("weights"); 
+  int32_t bias_size = ctx->input_size("weights"); 
+  CHECK_EQ_OR_RETURN(weight_size, bias_size); 
   /*
   A: (m, k)
   B: (n, k) need transpose
-  C: (j, n) need transpose
+  C: (m, n)
+  next: (n2, k2): k2 = n
   */
-  int64_t m = 0, n = 0, k = 0, j = 0;  // tensor a (no trans): m*k, tensor b (no trans): k*n
-  m = a.shape().At(a_num_axes - 2);
-  k = a.shape().At(a_num_axes - 1);
-  CHECK_EQ_OR_RETURN(k, b.shape().At(b_num_axes - 1));
-  n = b.shape().At(b_num_axes - 2);
-  CHECK_EQ_OR_RETURN(bias1.shape().At(0), n)
-      << "Bias1 shape cannot be added (" << bias1.shape().At(0) << ") and (" << n << ")";
-  aux->mut_shape()->Set(a_num_axes - 2, m);
-  aux->mut_shape()->Set(a_num_axes - 1, n);
-  CHECK_EQ_OR_RETURN(n, c.shape().At(c_num_axes - 1));
-  j = c.shape().At(c_num_axes - 2);
-  CHECK_EQ_OR_RETURN(bias2.shape().At(0), j)  
-      << "Bias2 shape cannot be added (" << bias2.shape().At(0) << ") and (" << j << ")";
-  out->mut_shape()->Set(a_num_axes - 2, m);
-  out->mut_shape()->Set(a_num_axes - 1, j);
+  int64_t m = 0, n = 0, k = 0;
+  m = x_desc.shape().At(0); 
+  k = x_desc.shape().At(1); 
+  n = first_weight_desc.shape().At(0); 
+  CHECK_EQ_OR_RETURN(first_weight_desc.shape().At(1), k); 
+  CHECK_EQ_OR_RETURN(first_bias_desc.shape().At(0), n); 
+
+  for (int32_t idx=1; idx < weight_size; idx++) {
+    // skip first input weight. 
+    const user_op::TensorDesc& weight_desc =
+        ctx->InputTensorDesc("weights", idx);
+    CHECK_EQ_OR_RETURN(weight_desc.shape().At(1), n); 
+    n = weight_desc.shape().At(0);
+    const user_op::TensorDesc& bias_desc =
+        ctx->InputTensorDesc("biases", idx);
+    CHECK_EQ_OR_RETURN(bias_desc.shape().At(0), n); 
+  }
+  *ctx->OutputShape("out", 0) = x_desc.shape();
+  printf("Here m is: %ld \n", m); 
+  printf("Here b is: %ld \n", n); 
+  out->mut_shape()->Set(1, n);
   return Maybe<void>::Ok();
 }
 
-Maybe<void> InferDataType4Matmul(user_op::InferContext* ctx) {
-  const DataType& dtype = ctx->InputDType("a", 0);
-  CHECK_EQ_OR_RETURN(ctx->InputDType("b", 0), dtype);
-  CHECK_EQ_OR_RETURN(ctx->InputDType("bias1", 0), dtype);
-  CHECK_EQ_OR_RETURN(ctx->InputDType("c", 0), dtype);
-  CHECK_EQ_OR_RETURN(ctx->InputDType("bias2", 0), dtype);
-  *ctx->OutputDType("out", 0) = dtype;
-  *ctx->OutputDType("aux", 0) = dtype;
-  return Maybe<void>::Ok();
+
+// Maybe<void> InferDataType4Matmul(user_op::InferContext* ctx) {
+//   const DataType& dtype = ctx->InputDType("a", 0);
+//   CHECK_EQ_OR_RETURN(ctx->InputDType("b", 0), dtype);
+//   CHECK_EQ_OR_RETURN(ctx->InputDType("bias1", 0), dtype);
+//   CHECK_EQ_OR_RETURN(ctx->InputDType("c", 0), dtype);
+//   CHECK_EQ_OR_RETURN(ctx->InputDType("bias2", 0), dtype);
+//   *ctx->OutputDType("out", 0) = dtype;
+//   *ctx->OutputDType("aux", 0) = dtype;
+//   return Maybe<void>::Ok();
+// }
+
+Maybe<void> InferDataType4Matmul(user_op::InferContext* ctx){
+  const user_op::TensorDesc& first_in_desc = ctx->InputTensorDesc("in", 0);
+  
+  for (const auto& in_arg_pair : ctx->inputs()) {
+    const user_op::TensorDesc& in_desc =
+        ctx->InputTensorDesc(in_arg_pair.first, in_arg_pair.second);
+    CHECK_EQ_OR_RETURN(in_desc.data_type(), first_in_desc.data_type());
+  }
+
+  user_op::TensorDesc* out_desc = ctx->OutputTensorDesc("out", 0);
+  *out_desc->mut_data_type() = first_in_desc.data_type();
+  return Maybe<void>::Ok(); 
 }
+
 
 }  // namespace
 
@@ -94,6 +150,7 @@ Maybe<void> InferDataType4Matmul(user_op::InferContext* ctx) {
   return InferLogicalTensorDesc(ctx);
 }
 
+// TODO(zzk): I don't know how to write. 
 /* static */ Maybe<void> FusedMatmulBiasAddReluOp::GetSbp(user_op::SbpContext* ctx) {
   /*
   A: (m, k)
