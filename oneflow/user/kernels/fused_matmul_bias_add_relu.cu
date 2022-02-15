1b4b9475f9bf1dbf06eab14a33d5748c22b82a49
MARD1NO <359521840@qq.com>
2022-02-15 09:02:43 +0800

    simplify code

--- oneflow/user/kernels/fused_matmul_bias_add_relu.cu
+++ oneflow/user/kernels/fused_matmul_bias_add_relu.cu
@@ -70,52 +70,15 @@ CublasScalarParameter GetCublasScalarParameter(Scalar scalar, cublasComputeType_
   return sp;
 }
 
-// void InferMatmulCublasMNK(const MutShapeView* a_shape, const ShapeView& b_shape, 
-//                           ep::primitive::BlasTransposeType transpose_a,
-//                           ep::primitive::BlasTransposeType transpose_b, 
-//                           size_t* cublas_m, size_t* cublas_n, size_t* cublas_k, 
-//                           int64_t* cublas_lda, int64_t* cublas_ldb, int64_t* cublas_ldc) {
-//   const int64_t num_a_axes = a_shape->NumAxes();
-//   CHECK_GE(num_a_axes, 2);
-//   const int64_t num_b_axes = b_shape.NumAxes();
-//   CHECK_GE(num_b_axes, 2);
-//   size_t m = 0, n = 0, k = 0; 
-//   if (transpose_a == ep::primitive::BlasTransposeType::N) {
-//     m = a_shape->At(num_a_axes - 2);
-//     k = a_shape->At(num_a_axes - 1);
-//     *cublas_ldb = k;
-//   } else if (transpose_a == ep::primitive::BlasTransposeType::T) {
-//     m = a_shape->At(num_a_axes - 1);
-//     k = a_shape->At(num_a_axes - 2);
-//     *cublas_ldb = m;
-//   } else {
-//     UNIMPLEMENTED();
-//   }
-//   if (transpose_b == ep::primitive::BlasTransposeType::N) {
-//     CHECK_EQ(b_shape.At(num_b_axes - 2), k);
-//     n = b_shape.At(num_b_axes - 1);
-//     *cublas_lda = n;
-//   } else if (transpose_b == ep::primitive::BlasTransposeType::T) {
-//     CHECK_EQ(b_shape.At(num_b_axes - 1), k);
-//     n = b_shape.At(num_b_axes - 2);
-//     *cublas_lda = k;
-//   } else {
-//     UNIMPLEMENTED();
-//   }
-//   *cublas_m = n; 
-//   *cublas_n = m; 
-//   *cublas_k = k; 
-//   *cublas_ldc = n;
-// }
 void InferMatmulCublasMNK(const DimVector& a_shape, const DimVector& b_shape, 
                           ep::primitive::BlasTransposeType transpose_a,
                           ep::primitive::BlasTransposeType transpose_b, 
                           size_t* cublas_m, size_t* cublas_n, size_t* cublas_k, 
                           int64_t* cublas_lda, int64_t* cublas_ldb, int64_t* cublas_ldc) {
-    const int64_t num_a_axes = 2;
-    // CHECK_GE(num_a_axes, 2);
-    const int64_t num_b_axes = 2;
-    // CHECK_GE(num_b_axes, 2);
+    const int64_t num_a_axes = a_shape.size();
+    CHECK_GE(num_a_axes, 2);
+    const int64_t num_b_axes = b_shape.size();
+    CHECK_GE(num_b_axes, 2);
     size_t m = 0, n = 0, k = 0; 
     if (transpose_a == ep::primitive::BlasTransposeType::N) {
       m = a_shape.at(num_a_axes - 2);
@@ -146,27 +109,25 @@ void InferMatmulCublasMNK(const DimVector& a_shape, const DimVector& b_shape,
   }
 
 
-
 class FusedMatmulBiasAddReluKernelCache final : public user_op::OpKernelCache {
  public:
   FusedMatmulBiasAddReluKernelCache() {
-// Just for init.
-    OF_CUBLAS_CHECK(cublasLtMatmulDescCreate(&operation_desc1, CUBLAS_COMPUTE_32F, CUDA_R_32F));
-    OF_CUBLAS_CHECK(cublasLtMatrixLayoutCreate(&cublas_a1_desc, CUDA_R_32F, 1, 1, 1));
-    OF_CUBLAS_CHECK(cublasLtMatrixLayoutCreate(&cublas_b1_desc, CUDA_R_32F, 1, 1, 1));
-    OF_CUBLAS_CHECK(cublasLtMatrixLayoutCreate(&cublas_c1_desc, CUDA_R_32F, 1, 1, 1));
-    
+  // Just for init.
+  OF_CUBLAS_CHECK(cublasLtMatmulDescCreate(&operation_desc, CUBLAS_COMPUTE_32F, CUDA_R_32F));
+  OF_CUBLAS_CHECK(cublasLtMatrixLayoutCreate(&cublas_a_desc, CUDA_R_32F, 1, 1, 1));
+  OF_CUBLAS_CHECK(cublasLtMatrixLayoutCreate(&cublas_b_desc, CUDA_R_32F, 1, 1, 1));
+  OF_CUBLAS_CHECK(cublasLtMatrixLayoutCreate(&cublas_c_desc, CUDA_R_32F, 1, 1, 1));
   }
   ~FusedMatmulBiasAddReluKernelCache() override {
-    OF_CUBLAS_CHECK(cublasLtMatmulDescDestroy(operation_desc1));
-    OF_CUBLAS_CHECK(cublasLtMatrixLayoutDestroy(cublas_a1_desc));
-    OF_CUBLAS_CHECK(cublasLtMatrixLayoutDestroy(cublas_b1_desc));
-    OF_CUBLAS_CHECK(cublasLtMatrixLayoutDestroy(cublas_c1_desc));
+    OF_CUBLAS_CHECK(cublasLtMatmulDescDestroy(operation_desc));
+    OF_CUBLAS_CHECK(cublasLtMatrixLayoutDestroy(cublas_a_desc));
+    OF_CUBLAS_CHECK(cublasLtMatrixLayoutDestroy(cublas_b_desc));
+    OF_CUBLAS_CHECK(cublasLtMatrixLayoutDestroy(cublas_c_desc));
   }
-  cublasLtMatmulDesc_t operation_desc1;
-  cublasLtMatrixLayout_t cublas_a1_desc;
-  cublasLtMatrixLayout_t cublas_b1_desc;
-  cublasLtMatrixLayout_t cublas_c1_desc;
+  cublasLtMatmulDesc_t operation_desc;
+  cublasLtMatrixLayout_t cublas_a_desc;
+  cublasLtMatrixLayout_t cublas_b_desc;
+  cublasLtMatrixLayout_t cublas_c_desc;
 };
 
 std::shared_ptr<FusedMatmulBiasAddReluKernelCache> CreateFusedMatmulBiasAddReluKernelCache() {
@@ -196,16 +157,16 @@ void SetCublasEpilogue(const FusedMatmulBiasAddReluKernelCache* matmul_cache,
   if(epilogue == CUBLASLT_EPILOGUE_RELU_BIAS || epilogue == CUBLASLT_EPILOGUE_BIAS){
     // Set epilogue
     OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(
-      matmul_cache->operation_desc1, CUBLASLT_MATMUL_DESC_EPILOGUE, &epilogue, sizeof(epilogue)));
+      matmul_cache->operation_desc, CUBLASLT_MATMUL_DESC_EPILOGUE, &epilogue, sizeof(epilogue)));
     // Set bias ptr
-    OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc1,
+    OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc,
       CUBLASLT_MATMUL_DESC_BIAS_POINTER, &bias_ptr,
       sizeof(bias_ptr)));
   }
   // // TODO: GELU_AUX_BIAS
   // if(epilogue == CUBLASLT_EPILOGUE_RELU_AUX_BIAS){
   //   // Set aux ptr for backward. 
-  //   OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc1,
+  //   OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc,
   //     CUBLASLT_MATMUL_DESC_AUX_POINTER, &aux_ptr,
   //     sizeof(aux_ptr)));
   // }
@@ -225,7 +186,7 @@ void SetCublasAttr(const FusedMatmulBiasAddReluKernelCache* matmul_cache,
                    int64_t cublas_ldc
                    ){
   OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(
-    matmul_cache->operation_desc1, CUBLASLT_MATMUL_DESC_COMPUTE_TYPE, &cublas_compute_dtype,
+    matmul_cache->operation_desc, CUBLASLT_MATMUL_DESC_COMPUTE_TYPE, &cublas_compute_dtype,
     sizeof(cublas_compute_dtype)));
 
   // For best performance when using the bias vector, specify beta == 0 and
@@ -233,15 +194,15 @@ void SetCublasAttr(const FusedMatmulBiasAddReluKernelCache* matmul_cache,
   // https://docs.nvidia.com/cuda/cublas/index.html#cublasLtPointerMode_t)
   cublasLtPointerMode_t mode = CUBLASLT_POINTER_MODE_HOST;
   OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(
-      matmul_cache->operation_desc1, CUBLASLT_MATMUL_DESC_POINTER_MODE, &mode, sizeof(mode)));
+      matmul_cache->operation_desc, CUBLASLT_MATMUL_DESC_POINTER_MODE, &mode, sizeof(mode)));
   
   // transpose_a = False, transpose_b = True. But in cublas is reversed. 
   const cublasOperation_t cublas_trans_a = CUBLAS_OP_T;
   const cublasOperation_t cublas_trans_b = CUBLAS_OP_N;
-  OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc1,
+  OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc,
                                                 CUBLASLT_MATMUL_DESC_TRANSA, &cublas_trans_a,
                                                 sizeof(cublas_trans_a)));
-  OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc1,
+  OF_CUBLAS_CHECK(cublasLtMatmulDescSetAttribute(matmul_cache->operation_desc,
                                                 CUBLASLT_MATMUL_DESC_TRANSB, &cublas_trans_b,
                                                 sizeof(cublas_trans_b)));
   
@@ -249,11 +210,11 @@ void SetCublasAttr(const FusedMatmulBiasAddReluKernelCache* matmul_cache,
   SetCublasEpilogue(matmul_cache, epilogue, bias_ptr, aux_ptr);
 
   // Set matrix layout
-  SetCublasMatrixLayout(matmul_cache->cublas_a1_desc, cuda_data_type, cublas_trans_a, cublas_m,
+  SetCublasMatrixLayout(matmul_cache->cublas_a_desc, cuda_data_type, cublas_trans_a, cublas_m,
                         cublas_k, cublas_lda);
-  SetCublasMatrixLayout(matmul_cache->cublas_b1_desc, cuda_data_type, cublas_trans_b, cublas_k,
+  SetCublasMatrixLayout(matmul_cache->cublas_b_desc, cuda_data_type, cublas_trans_b, cublas_k,
                         cublas_n, cublas_ldb);
-  SetCublasMatrixLayout(matmul_cache->cublas_c1_desc, cuda_data_type, CUBLAS_OP_N, cublas_m,
+  SetCublasMatrixLayout(matmul_cache->cublas_c_desc, cuda_data_type, CUBLAS_OP_N, cublas_m,
                         cublas_n, cublas_ldc);
 }
 
@@ -274,9 +235,8 @@ class FusedMatmulBiasAddReluKernel final : public user_op::OpKernel {
  private:
   void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState*,
                const user_op::OpKernelCache* cache) const override {
-    printf("Enter here \n"); 
     /*
-    Fused Dense+Activation+Dense Layer. 
+    Fused DenseActivation Layer. Assume we have two layers: 
     A: (m, k)
     B: (n, k) need transpose
     C: (j, n) need transpose
@@ -286,8 +246,6 @@ class FusedMatmulBiasAddReluKernel final : public user_op::OpKernel {
     const int32_t weight_size = ctx->input_size("weights"); 
     const int32_t bias_size = ctx->input_size("biases"); 
     CHECK_EQ(weight_size, bias_size) << "The number of weight and bias is not equal!. "; 
-    printf("weight size is: %d \n", weight_size); 
-    printf("111 \n"); 
     auto* cuda_stream = ctx->stream()->As<ep::CudaStream>();
     const auto* matmul_cache =
         CHECK_NOTNULL(dynamic_cast<const FusedMatmulBiasAddReluKernelCache*>(cache));
@@ -295,46 +253,34 @@ class FusedMatmulBiasAddReluKernel final : public user_op::OpKernel {
     user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0); 
     user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("out", 0);
     bool skip_final_activation = ctx->Attr<bool>("skip_final_activation"); 
-    printf("221 \n"); 
 
     const DataType data_type = out->data_type();
     const cublasComputeType_t cublas_compute_dtype = GetComputeType(data_type);
     const cudaDataType_t cuda_data_type = GetCudaDataType(data_type);
     size_t cublas_m = 0, cublas_n = 0, cublas_k = 0; 
     int64_t cublas_lda = 0, cublas_ldb = 0, cublas_ldc = 0; 
-    printf("1133331 \n"); 
-     
 
-    const double alpha1 = 1.0;
-    const auto sp_alpha1 = GetCublasScalarParameter(alpha1, cublas_compute_dtype);
-    const double beta1 = 0.0;
-    const auto sp_beta1 = GetCublasScalarParameter(beta1, cublas_compute_dtype);
-    printf("1114444 \n"); 
+    const double alpha = 1.0;
+    const auto sp_alpha = GetCublasScalarParameter(alpha, cublas_compute_dtype);
+    const double beta = 0.0;
+    const auto sp_beta = GetCublasScalarParameter(beta, cublas_compute_dtype);
 
     const int64_t batch_size = ctx->Tensor4ArgNameAndIndex("x", 0)->shape().At(0); 
     int64_t in_feature = ctx->Tensor4ArgNameAndIndex("x", 0)->shape().At(1); 
-    printf("1114443123213123 \n"); 
     
-    // MutShapeView* x_shape = x->mut_shape(); 
-    // MutShapeView tmp_shape(*x_shape);
+    // currently only support 2D matmul. 
     DimVector x_shape(2); 
     x->shape().ToDimVector(&x_shape); 
-    // MutShapeView tmp_shape(*x_shape);
     DimVector tmp_shape(2); 
     tmp_shape.at(0) = x_shape.at(0); 
-    printf("111000000000"); 
     DimVector weight_shape(2); 
     
     int64_t out_feature = 0; 
-    // user_op::Tensor* tmp_buffer = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
     user_op::Tensor* tmp_buffer = ctx->Tensor4ArgNameAndIndex("tmp_buffer", 0);
-    printf("1118889999999"); 
-    void* tmp_x_buffer = nullptr;
+    T* tmp_x_buffer = x->mut_dptr<T>();
     T* tmp_y_buffer = tmp_buffer->mut_dptr<T>();
-    printf("11188888"); 
     void* x_ptr = nullptr; 
     void* y_ptr = nullptr; 
-    printf("1117777 \n"); 
 
     for(int idx = 0; idx < weight_size; idx++){
       const user_op::Tensor* weight = ctx->Tensor4ArgNameAndIndex("weights", idx);
@@ -351,8 +297,6 @@ class FusedMatmulBiasAddReluKernel final : public user_op::OpKernel {
                             &cublas_m, &cublas_n, &cublas_k, 
                             &cublas_lda, &cublas_ldb, &cublas_ldc);
       }else{
-        // can be remove. 
-        x_ptr = tmp_x_buffer; 
         InferMatmulCublasMNK(tmp_shape, weight_shape, 
                             /*transpose_a=*/ep::primitive::BlasTransposeType::N, 
                             /*transpose_b=*/ep::primitive::BlasTransposeType::T, 
@@ -360,7 +304,6 @@ class FusedMatmulBiasAddReluKernel final : public user_op::OpKernel {
                             &cublas_lda, &cublas_ldb, &cublas_ldc);
       }
       if(idx == weight_size-1){
-        printf("here??? \n"); 
         y_ptr = ctx->Tensor4ArgNameAndIndex("out", 0)->mut_dptr();
         if(skip_final_activation){
           epilogue = CUBLASLT_EPILOGUE_BIAS;
@@ -383,15 +326,15 @@ class FusedMatmulBiasAddReluKernel final : public user_op::OpKernel {
                     cublas_ldc); 
 
       OF_CUBLAS_CHECK(cublasLtMatmul(
-          cuda_stream->cublas_lt_handle(), matmul_cache->operation_desc1, &sp_alpha1, weight->dptr(),
-          matmul_cache->cublas_a1_desc, x_ptr, matmul_cache->cublas_b1_desc, &sp_beta1,
-          y_ptr, matmul_cache->cublas_c1_desc, y_ptr, matmul_cache->cublas_c1_desc,
+          cuda_stream->cublas_lt_handle(), matmul_cache->operation_desc, &sp_alpha, weight->dptr(),
+          matmul_cache->cublas_a_desc, tmp_x_buffer, matmul_cache->cublas_b_desc, &sp_beta,
+          y_ptr, matmul_cache->cublas_c_desc, y_ptr, matmul_cache->cublas_c_desc,
           nullptr, cuda_stream->cublas_workspace(), cuda_stream->cublas_workspace_size(),
           cuda_stream->cuda_stream()));
       
       tmp_x_buffer = tmp_y_buffer; 
+      // move to next buffer to store result. 
       tmp_y_buffer += batch_size * out_feature; 
-      // tmp_shape.Set(1, out_feature); 
       tmp_shape.at(1) = out_feature; 
     }
   }
@@ -407,11 +350,11 @@ class FusedMatmulBiasAddReluKernel final : public user_op::OpKernel {
         const Shape& x_shape = ctx->InputShape("x", 0); \
         int64_t batch_size = x_shape.At(0); \
         int32_t weight_size = ctx->input_size("weights"); \
-        for(int i = 0; i < weight_size; i++){ \
+        for(int i = 0; i < weight_size-1; i++){ \
+          /* If there is only one layer, we don't need to allocate temp buffer to store result. */\
           tmp_size += batch_size * ctx->InputShape("weights", i).At(0); \
         }          \
-        printf("Tmp size is: %ld \n", tmp_size); \
-        const int64_t tmp_buffer_size = GetCudaAlignedSize(tmp_size);    \
+        const int64_t tmp_buffer_size = GetCudaAlignedSize(tmp_size*sizeof(cpp_type));    \
         return tmp_buffer_size;                                          \
     });
 
