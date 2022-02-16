2f43b966c965d3c264dd49b18112dcb5ae384920
wyg1997 <wyg19970408@gmail.com>
2022-02-16 15:19:17 +0800

    fix(Randint): test randint bug in 2d sbp in graph

--- oneflow/user/ops/distributions/uniform_int_op.cpp
+++ oneflow/user/ops/distributions/uniform_int_op.cpp
@@ -15,7 +15,8 @@ limitations under the License.
 */
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/framework/op_generated.h"
-#include "oneflow/core/common/balanced_splitter.h"
+#include "oneflow/user/ops/source_op_util.h"
+
 namespace oneflow {
 
 /* static */ Maybe<void> UniformIntOp::InferLogicalTensorDesc(user_op::InferContext* ctx) {
@@ -30,22 +31,7 @@ namespace oneflow {
 }
 
 /*static*/ Maybe<void> UniformIntOp::InferPhysicalTensorDesc(user_op::InferContext* ctx) {
-  const Shape& shape = ctx->Attr<Shape>("shape");
-  DimVector dim_vec{shape.dim_vec()};
-
-  const cfg::SbpParallel& out_sbp_para = ctx->SbpParallel4ArgNameAndIndex("out", 0);
-  if (out_sbp_para.has_split_parallel()) {
-    const int64_t& parallel_num = ctx->parallel_ctx().parallel_num();
-    if (parallel_num > 1) {
-      const int64_t& split_axis = out_sbp_para.split_parallel().axis();
-      CHECK_LT_OR_RETURN(split_axis, dim_vec.size());
-      BalancedSplitter bs(shape.At(split_axis), parallel_num);
-      dim_vec[split_axis] = bs.At(ctx->parallel_ctx().parallel_id()).size();
-    }
-  }
-
-  *ctx->OutputShape("out", 0) = Shape(dim_vec);
-  return Maybe<void>::Ok();
+  return user_op::InferSourceOpPhysicalTensorDesc(ctx);
 }
 
 /* static */ Maybe<void> UniformIntOp::GetSbp(user_op::SbpContext* ctx) {
