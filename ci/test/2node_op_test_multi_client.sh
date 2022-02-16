64dfe9563d04e7a9fb58c5f6afa83dc1206fa864
tsai <jackalcooper@gmail.com>
2022-02-16 22:54:25 +0800

    refine

--- ci/test/2node_op_test_multi_client.sh
+++ ci/test/2node_op_test_multi_client.sh
@@ -4,10 +4,13 @@ set -xeu
 
 export PYTHONUNBUFFERED=1
 
+function do_test() {
+    ONEFLOW_TEST_NODE_NUM=2 ONEFLOW_TEST_DEVICE_NUM=$device_num python3 -m oneflow.distributed.launch --nproc_per_node $device_num --nnodes=2 --node_rank=$NODE_RANK --master_addr $_MASTER_ADDR -m unittest discover ${ONEFLOW_TEST_DIR} --failfast --verbose
+}
 
 for device_num in 1 2 4
 do
-    ONEFLOW_TEST_NODE_NUM=2 ONEFLOW_TEST_DEVICE_NUM=$device_num python3 -m oneflow.distributed.launch --nproc_per_node $device_num --nnodes=2 --node_rank=$NODE_RANK --master_addr $_MASTER_ADDR -m unittest discover ${ONEFLOW_TEST_DIR} --failfast --verbose
     # use a invalid ibverbs lib to test if falling back to epoll works
-    ONEFLOW_TEST_NODE_NUM=2 ONEFLOW_TEST_DEVICE_NUM=$device_num ONEFLOW_LIBIBVERBS_PATH=invalid_lib python3 -m oneflow.distributed.launch --nproc_per_node $device_num --nnodes=2 --node_rank=$NODE_RANK --master_addr $_MASTER_ADDR -m unittest discover ${ONEFLOW_TEST_DIR} --failfast --verbose
+    do_test
+    ONEFLOW_LIBIBVERBS_PATH=invalid_lib do_test
 done
