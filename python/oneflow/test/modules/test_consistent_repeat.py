cf3d99870c1a01707cacf7ae269e41717bd8384c
wyg1997 <wyg19970408@gmail.com>
2022-02-21 10:49:34 +0800

    test(RepeatOp): fix repeat test

--- python/oneflow/test/modules/test_consistent_repeat.py
+++ python/oneflow/test/modules/test_consistent_repeat.py
@@ -25,10 +25,10 @@ from oneflow.test_utils.automated_test_util import *
 @autotest(n=5, check_graph=False)
 def test_repeat_impl(test_case, ndim, placement, sbp):
     dims = [random(1, 4) * 8 for _ in range(ndim)]
-    repeat_size = [random(1, 4) for _ in range(ndim)]
+    repeat_size = [random(1, 4).to(int).value() for _ in range(ndim)]
     x = random_tensor(ndim, *dims)
     y = x.to_global(placement=placement, sbp=sbp)
-    z = torch.reciprocal(y)
+    z = y.repeat(repeat_size)
     return z
 
 
