930706f596d5aec8ad568d0280f8bb1c05ca7731
clackhan <han_binbin@163.com>
2022-02-21 11:43:58 +0800

    add multi_client_exception_test.sh

---
+++ ci/test/multi_client_exception_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -xe
+
+export PYTHONUNBUFFERED=1
+
+src_dir=${ONEFLOW_SRC_DIR:-"$PWD"}
+test_dir="$PWD/python/oneflow/test/exceptions"
+test_tmp_dir=${ONEFLOW_TEST_TMP_DIR:-"./test_tmp_dir"}
+export ONEFLOW_TEST_UTILS_DIR=$src_dir/python/oneflow/test_utils
+
+
+rm -rf $test_tmp_dir
+mkdir -p $test_tmp_dir
+cp -r $test_dir $test_tmp_dir
+cd ${test_tmp_dir}/$(basename $test_dir)
+
+if [[ "$(python3 -c 'import oneflow.sysconfig;print(oneflow.sysconfig.has_rpc_backend_grpc())')" == *"True"* ]]; then
+    export ONEFLOW_TEST_DEVICE_NUM=2
+    python3 -m oneflow.distributed.launch --nproc_per_node 2 ${PWD}/test_global_branch_error_with_global_mean.py --failfast --verbose
+    python3 -m oneflow.distributed.launch --nproc_per_node 2 ${PWD}/test_global_branch_error_with_local_to_global.py --failfast --verbose
+else
+    python3 -c 'import oneflow.sysconfig;assert(oneflow.sysconfig.has_rpc_backend_grpc() == False)'
+fi
