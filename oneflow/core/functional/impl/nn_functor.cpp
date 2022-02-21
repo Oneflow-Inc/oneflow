0adbc45e6c6cb50e0d076d9adbdc7c716263437e
MARD1NO <359521840@qq.com>
2022-02-21 14:18:35 +0800

    fix ops td

--- oneflow/core/functional/impl/nn_functor.cpp
+++ oneflow/core/functional/impl/nn_functor.cpp
@@ -28,7 +28,6 @@ limitations under the License.
 #include "oneflow/core/framework/random_generator.h"
 #include "oneflow/core/functional/functional.h"
 #include "oneflow/core/functional/function_library.h"
-#include "oneflow/core/functional/functional_api.yaml.h"
 #include "oneflow/core/functional/sequence_function.h"
 #include "oneflow/core/functional/impl/common.h"
 #include "oneflow/core/functional/impl/unary_functor.h"
