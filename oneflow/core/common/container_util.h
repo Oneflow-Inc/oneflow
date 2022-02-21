935eac1780271e9879ed73e0d75c006a0f6582bf
daquexian <daquexian566@gmail.com>
2022-02-21 10:28:57 +0800

    fix VectorAt on vector<bool>
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- oneflow/core/common/container_util.h
+++ oneflow/core/common/container_util.h
@@ -49,11 +49,14 @@ template<typename VecT>
 Maybe<scalar_or_const_ref_t<typename VecT::value_type>> VectorAt(const VecT& vec,
                                                                  typename VecT::size_type index) {
   CHECK_LT_OR_RETURN(index, vec.size());
-  return vec[index];
+  // convert vector bool proxy to bool
+  return static_cast<typename VecT::value_type>(vec[index]);
 }
 
 template<typename VecT>
 Maybe<typename VecT::value_type*> VectorAt(VecT* vec, typename VecT::size_type index) {
+  static_assert(!std::is_same<typename VecT::value_type, bool>::value,
+                "VectorAt(vector<bool>*, size_t) is not supported.");
   CHECK_LT_OR_RETURN(index, vec->size());
   return &(*vec)[index];
 }
