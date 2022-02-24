d1d6e3bbc4e198c6676361a31870e33279c12579
jackalcooper <jackalcooper@gmail.com>
2022-02-24 15:32:30 +0800

    better dist mode for cuda

--- ci/test/generic_test_multi_client.sh
+++ ci/test/generic_test_multi_client.sh
@@ -10,7 +10,7 @@ cd $ONEFLOW_TEST_DIR
 
 gpu_num=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 export ONEFLOW_TEST_DEVICE_NUM=1
-python3 -m pytest -n 4  ${PWD} --verbose --durations=50 -x
+python3 -m pytest -n 4  ${PWD} --verbose --durations=50 -x --dist loadfile
 if [[ "$(python3 -c 'import oneflow.sysconfig;print(oneflow.sysconfig.has_rpc_backend_grpc())')" == *"True"* ]]; then
     export ONEFLOW_TEST_DEVICE_NUM=2
     python3 -m oneflow.distributed.launch --nproc_per_node 2 -m unittest discover ${PWD} --failfast --verbose
