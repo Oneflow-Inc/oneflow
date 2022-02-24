9da2b4f229a9dcda106499393bc75a384c9dabe4
wyushun <wyushun@foxmail.com>
2022-02-24 18:17:38 +0800

    update

--- python/oneflow/test/modules/test_consistent_diag.py
+++ python/oneflow/test/modules/test_consistent_diag.py
@@ -15,10 +15,6 @@ limitations under the License.
 """
 
 import unittest
-from collections import OrderedDict
-
-import numpy as np
-from test_util import GenArgList
 
 import oneflow as flow
 import oneflow.unittest
@@ -26,7 +22,7 @@ import oneflow.unittest
 from oneflow.test_utils.automated_test_util import *
 
 
-@autotest(n=2, check_graph=False)
+@autotest(n=1, check_graph=False)
 def test_diag_impl(test_case, ndim, placement, sbp):
     dims = [random(1, 4) * 8 for i in range(ndim)]
     x = random_tensor(ndim, *dims)
