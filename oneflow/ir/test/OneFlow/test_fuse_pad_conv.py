bfdbe4940586f4162d493e3af691d5cca5eba535
BBuf <1182563586@qq.com>
2022-02-26 11:55:42 +0800

    refine

--- oneflow/ir/test/OneFlow/test_fuse_pad_conv.py
+++ oneflow/ir/test/OneFlow/test_fuse_pad_conv.py
@@ -47,7 +47,6 @@ def do_pad_conv_graph(test_case, with_cuda, with_bias):
 
     graph_to_run = GraphToRun()
     lazy_conv_x = graph_to_run(x)
-    # TODO: xiaoyu, investigate when the assert fails here
     test_case.assertTrue(np.array_equal(eager_conv_x.numpy(), lazy_conv_x.numpy()))
 
 
