547f34114f23c8e4cec0b71d71eb39405f094685
tsai <caishenghang@oneflow.org>
2022-02-26 18:16:37 +0800

    fix

--- python/oneflow/test/modules/test_consistent_floor.py
+++ python/oneflow/test/modules/test_consistent_floor.py
@@ -21,16 +21,15 @@ import oneflow.unittest
 from oneflow.test_utils.automated_test_util import *
 
 
-@autotest(n=1, check_graph=False)
-def test_floor_impl(test_case, ndim, placement, sbp):
-    dims = [random(1, 4) * 8 for i in range(ndim)]
-    x = random_tensor(ndim, *dims)
-    y = x.to_global(placement=placement, sbp=sbp)
-    z = torch.floor(y)
-    return z
-
-
 class TestFloorConsistent(flow.unittest.TestCase):
+    @autotest(n=1, check_graph=False)
+    def test_floor_impl(test_case, ndim, placement, sbp):
+        dims = [random(1, 4) * 8 for i in range(ndim)]
+        x = random_tensor(ndim, *dims)
+        y = x.to_global(placement=placement, sbp=sbp)
+        z = torch.floor(y)
+        return z
+
     @globaltest
     def test_floor(test_case):
         # random ndim in range [1,4]
