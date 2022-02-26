0d3fe7a17d6c15de89f062a836a0fe88afcf267a
bbuf <1182563586@qq.com>
2022-02-26 10:32:38 +0800

    fix error

--- python/oneflow/test/modules/test_activation.py
+++ python/oneflow/test/modules/test_activation.py
@@ -587,7 +587,9 @@ class TestLeakyReLUModule(flow.unittest.TestCase):
         y = m(x)
         return y
 
+
     @autotest()
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
     def test_leakyrelu_module_with_half_random_data(test_case):
         m = torch.nn.LeakyReLU(negative_slope=random() | nothing())
         m.train(random())
