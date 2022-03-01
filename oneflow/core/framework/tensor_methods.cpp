16e2cfc65523311e851b8b93c0aa426865454d0f
luyang <flowingsun007@163.com>
2022-03-01 12:30:21 +0800

    fix clang check

--- oneflow/core/framework/tensor_methods.cpp
+++ oneflow/core/framework/tensor_methods.cpp
@@ -271,8 +271,7 @@ Maybe<Tensor> Squeeze(const std::shared_ptr<Tensor>& input,
               autograd::AutoGradMode mode(create_graph);
               CHECK_EQ_OR_RETURN(out_grads.size(), 1);
               in_grads->resize(1);
-              in_grads->at(0) =
-                  JUST(functional::Reshape(out_grads.at(0), Shape(input->shape()->dim_vec())));
+              *JUST(oneflow::VectorAt(in_grads, 0) )= JUST(functional::Reshape(JUST(oneflow::VectorAt(out_grads, 0) ), Shape(input->shape()->dim_vec())));
               return Maybe<void>::Ok();
             });
     TensorTuple outputs{output};
