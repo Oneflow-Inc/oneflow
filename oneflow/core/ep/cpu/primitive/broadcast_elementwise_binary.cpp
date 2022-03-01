c9dc964d1d7707eb1ab509fca8dbbf8c72b93ee0
luqiang-guo <702572275@qq.com>
2022-03-01 17:57:35 +0800

    delete printf

--- oneflow/core/ep/cpu/primitive/broadcast_elementwise_binary.cpp
+++ oneflow/core/ep/cpu/primitive/broadcast_elementwise_binary.cpp
@@ -181,8 +181,6 @@ class OneDnnBroadcastElementwiseBinaryImpl : public BroadcastElementwiseBinary {
     CpuStream* cpu_stream = stream->As<CpuStream>();
     size_t num_threads = static_cast<CpuDevice*>(cpu_stream->device())->GetNumThreads();
     CpuNumThreadsGuard guard(num_threads);
-    printf("thread = %ld\n",
-           tbb::global_control::active_value(tbb::global_control::max_allowed_parallelism));
 
     dnnl::engine* onednn_engine = stream->As<CpuStream>()->onednn_engine();
     dnnl::stream* onednn_stream = stream->As<CpuStream>()->onednn_stream();
