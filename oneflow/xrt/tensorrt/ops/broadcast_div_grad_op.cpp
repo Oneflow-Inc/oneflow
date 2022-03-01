a0834bd7ad4fc6df12443ff9ff318dd083d82a4f
hjchen2 <chenhoujiangcug@gmail.com>
2022-03-01 22:44:45 +0800

    refactor broadcast_div_grad op

--- oneflow/xrt/tensorrt/ops/broadcast_div_grad_op.cpp
+++ oneflow/xrt/tensorrt/ops/broadcast_div_grad_op.cpp
@@ -15,22 +15,37 @@ limitations under the License.
 */
 #include "oneflow/xrt/tensorrt/ops/op_context.h"
 #include "oneflow/xrt/tensorrt/ops/op_kernel.h"
-#include "oneflow/xrt/tensorrt/plugin/broadcast_div_grad_plugin.h"
+#include "oneflow/xrt/tensorrt/trt_helpers.h"
 
 namespace oneflow {
 namespace xrt {
 namespace tensorrt {
 
+// dy = reduce(-dz * (z / y))
 class BroadcastDivGradOp : public TrtOpKernel {
  public:
   void Compile(TrtOpContext* ctx) override {
-    std::vector<nvinfer1::ITensor*> inputs(3);
-    inputs[0] = ctx->Input("y_0");
-    inputs[1] = ctx->Input("z_0");
-    inputs[2] = ctx->Input("dz_0");
-    BroadcastDivGradPlugin plugin(ctx->op_name());
-    auto* layer = ctx->builder()->addPluginV2(inputs.data(), 3, plugin);
-    ctx->SetOutput("dy_0", layer->getOutput(0));
+    // r0 = z / y
+    auto* r0 = ctx->builder()->addElementWise(*(ctx->Input("z_0")), *(ctx->Input("y_0")),
+                                              nvinfer1::ElementWiseOperation::kDIV);
+    // r1 = dz * r0
+    auto* r1 = ctx->builder()->addElementWise(*(ctx->Input("dz_0")), *(r0->getOutput(0)),
+                                              nvinfer1::ElementWiseOperation::kPROD);
+    // r3 = -r1
+    auto* r3 = ctx->builder()->addUnary(*(r1->getOutput(0)), nvinfer1::UnaryOperation::kNEG);
+    // reduce
+    const auto& large_shape = ctx->InputShape("z_0");
+    const auto& reduced_shape = ctx->InputShape("y_0");
+    uint32_t reduce_axes = 0;
+    for (int i = large_shape.NumAxes() - 1; i >= reduced_shape.NumAxes(); --i) {
+      reduce_axes |= (1U << i);
+    }
+    for (int i = reduced_shape.NumAxes() - 1; i >= 0; --i) {
+      if (large_shape.At(i) != reduced_shape.At(i)) { reduce_axes |= (1U << i); }
+    }
+    auto* reduce = ctx->builder()->addReduce(*(r3->getOutput(0)), nvinfer1::ReduceOperation::kSUM,
+                                             reduce_axes, /*keepdims*/ true);
+    ctx->SetOutput("dy_0", helpers::Reshape(ctx, reduce->getOutput(0), reduced_shape));
   }
 };
 
