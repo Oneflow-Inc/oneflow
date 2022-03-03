1459d01f0d8541d99a1db9cc5467b7464d198fa2
fengdaozhuo <fengdaozhuo960@163.com>
2022-03-03 13:45:48 +0800

    refine notes

--- oneflow/user/kernels/distributions/uniform_int_kernel.h
+++ oneflow/user/kernels/distributions/uniform_int_kernel.h
@@ -76,8 +76,7 @@ class UniformIntKernel final : public user_op::OpKernel {
   std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
       user_op::KernelInitContext* ctx) const override {
     const auto& generator = CHECK_JUST(one::MakeAutoGenerator());
-    // Each rank get seed depend on SBP,when SBP is Spit, use different seeds;\
-    when SBP is B and P,use the same seed;
+    // When SBP is Spit, each rank uses a different seed, otherwise, ranks use the same seed
     generator->set_current_seed(
         CHECK_JUST(GetOpKernelRandomSeedInCurrentRank(ctx, ctx->Attr<int64_t>("seed"))));
     return std::make_shared<DistributionKernelState>(generator);
