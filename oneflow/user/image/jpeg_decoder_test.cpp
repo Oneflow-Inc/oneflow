8db9337fbe48a659c2d263c0e4947301c5077c3e
luqiang-guo <702572275@qq.com>
2022-03-03 19:35:58 +0800

    fix

--- oneflow/user/image/jpeg_decoder_test.cpp
+++ oneflow/user/image/jpeg_decoder_test.cpp
@@ -107,17 +107,16 @@ TEST(JPEG, decoder) {
     cv::Mat libjpeg_image_mat;
     JpegDecoder jpeg_decode;
 
-    RandomCropGenerator* libjpeg_random_crop_gen =
-        new RandomCropGenerator({0.1, 0.9}, {0.4, 0.6}, seeds[i], 1);
-    RandomCropGenerator* opencv_random_crop_gen =
-        new RandomCropGenerator({0.1, 0.9}, {0.4, 0.6}, seeds[i], 1);
+
+    RandomCropGenerator libjpeg_random_crop_gen({0.1, 0.9}, {0.4, 0.6}, seeds[i], 1);
+    RandomCropGenerator opencv_random_crop_gen({0.1, 0.9}, {0.4, 0.6}, seeds[i], 1);
     jpeg_decode.PartialDecode((const unsigned char*)(jpg.data()), jpg.size(),
-                              libjpeg_random_crop_gen, nullptr, 0, libjpeg_image_mat);
+                              &libjpeg_random_crop_gen, nullptr, 0, libjpeg_image_mat);
 
     cv::Mat opencv_image_mat;
     std::string color_space("RGB");
 
-    OpenCvPartialDecode((const unsigned char*)(jpg.data()), jpg.size(), opencv_random_crop_gen,
+    OpenCvPartialDecode((const unsigned char*)(jpg.data()), jpg.size(), &opencv_random_crop_gen,
                         color_space, opencv_image_mat);
     ImageUtil::ConvertColor("BGR", opencv_image_mat, color_space, opencv_image_mat);
 
