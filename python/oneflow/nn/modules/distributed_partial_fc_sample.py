fdff0efb565056b0d3b03d399c19c05affddeb89
lcy <2695928308@qq.com>
2022-03-03 17:02:31 +0800

    test braodcast

--- python/oneflow/nn/modules/distributed_partial_fc_sample.py
+++ python/oneflow/nn/modules/distributed_partial_fc_sample.py
@@ -13,8 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import oneflow as flow
 import warnings
+import oneflow as flow
 import oneflow._oneflow_internal._C as _C
 from oneflow.nn.module import Module
 
--- python/oneflow/test/modules/test_consistent_partical_fc.py
+++ python/oneflow/test/modules/test_consistent_partical_fc.py
@@ -28,17 +28,21 @@ class TestParitalFC(flow.unittest.TestCase):
     @globaltest
     def test_parital_fc(test_case):
         placement = flow.env.all_device_placement("cuda")
-        w = flow.randn(5000, 128, placement=placement, sbp=flow.sbp.split(0))
-        label = flow.randint(
-            0, 5000, (512,), placement=placement, sbp=flow.sbp.split(0)
-        )
+        w =  flow.randn(5000, 128, placement=placement, sbp=flow.sbp.split(0))
+        label = flow.randint(0, 5000, (512,), placement=placement, sbp=flow.sbp.split(0))
         num_sample = 500
         out = flow.distributed_partial_fc_sample(w, label, num_sample)
-
         test_case.assertTrue(out[0].shape == flow.Size([512]))
         test_case.assertTrue(out[1].shape == flow.Size([500]))
         test_case.assertTrue(out[2].shape == flow.Size([500, 128]))
 
+        w =  flow.randn(5000, 128, placement=placement, sbp=flow.sbp.broadcast)
+        label = flow.randint(0, 5000, (512,), placement=placement, sbp=flow.sbp.split(0))
+        num_sample = 500
+        out = flow.distributed_partial_fc_sample(w, label, num_sample)
+        test_case.assertTrue(out[0].shape == flow.Size([512]))
+        test_case.assertTrue(out[1].shape == flow.Size([500]))
+        test_case.assertTrue(out[2].shape == flow.Size([500, 128]))
 
 if __name__ == "__main__":
     unittest.main()
