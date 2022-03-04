7bcbfa3316f9fe3bab7f1ace87f2925d6fc60581
chengyuma <chengyuma@github.com>
2022-03-04 17:35:07 +0800

    fix python/oneflow/framework/docstr/swapdims.py

--- python/oneflow/framework/docstr/swapdims.py
+++ python/oneflow/framework/docstr/swapdims.py
@@ -25,24 +25,23 @@ add_docstr(
     .. code-block:: python
     
         >>> import oneflow as flow
-               
         >>> x = flow.tensor([[[0,1],[2,3]],[[4,5],[6,7]]])
         >>> x
         tensor([[[0, 1],
-                [2, 3]],
-
-               [[4, 5],
-                [6, 7]]], dtype=oneflow.int64)
+                 [2, 3]],
+        <BLANKLINE>
+                [[4, 5],
+                 [6, 7]]], dtype=oneflow.int64)
         >>> flow.swapdims(x, 0, 1)
         tensor([[[0, 1],
                  [4, 5]],
-
+        <BLANKLINE>
                 [[2, 3],
                  [6, 7]]], dtype=oneflow.int64)
         >>> flow.swapdims(x, 0, 2)
         tensor([[[0, 4],
                  [2, 6]],
-
+        <BLANKLINE>
                 [[1, 5],
                  [3, 7]]], dtype=oneflow.int64)
 
