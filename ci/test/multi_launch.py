78f6d7cd96bef398d574dc48007f3f2618ee13e5
jackalcooper <jackalcooper@gmail.com>
2022-03-05 09:00:24 +0800

    refine

--- ci/test/multi_launch.py
+++ ci/test/multi_launch.py
@@ -126,7 +126,7 @@ def main():
     args = parse_args()
     # find files and chuck them
     files = glob.glob(args.files, recursive=True)
-    random.seed(8675309)
+    random.seed(6628309)
     if args.shuffle:
         random.shuffle(files)
     if args.parallel_num == "master_port":
