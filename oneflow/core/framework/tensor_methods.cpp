ee46a582299d6337bb621a99b39f5688c8e438f6
lyon <flowingsun007@163.com>
2022-03-08 06:00:34 +0000

    refine

--- oneflow/core/framework/tensor_methods.cpp
+++ oneflow/core/framework/tensor_methods.cpp
@@ -13,9 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 #include "oneflow/core/framework/tensor_methods.h"
-
 #include "oneflow/core/autograd/autograd_engine.h"
 #include "oneflow/core/autograd/autograd_mode.h"
 #include "oneflow/core/common/container_util.h"
