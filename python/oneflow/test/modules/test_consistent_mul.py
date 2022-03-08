d81539e111ce934fcaba34f02aff3bdea9748ed4
liufengwei0103 <2472937968@qq.com>
2022-03-08 10:52:02 +0800

    refine

--- python/oneflow/test/modules/test_consistent_mul.py
+++ python/oneflow/test/modules/test_consistent_mul.py
@@ -14,11 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import unittest
-
 import oneflow as flow
 import oneflow.unittest
-
-
 from oneflow.test_utils.automated_test_util import *
 
 
