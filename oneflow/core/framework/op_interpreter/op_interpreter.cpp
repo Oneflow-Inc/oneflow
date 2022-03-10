64f4220c8d3d3f72088c71a19b99e6942e76a33a
shihan-ma <mmasss@sjtu.edu.cn>
2022-03-10 20:19:17 +0800

    check dtr_enabled before replace grad_fn_node

--- oneflow/core/framework/op_interpreter/op_interpreter.cpp
+++ oneflow/core/framework/op_interpreter/op_interpreter.cpp
@@ -92,7 +92,7 @@ Maybe<void> AutogradInterpreter::Apply(const OpExpr& op_expr, const TensorTuple&
   }
 
   TensorTuple mut_inputs(op_expr.output_size());
-  if (requires_grad) {
+  if (oneflow::DTREnabled() && requires_grad) {
     for (int i = 0; i < outputs->size(); ++i) {
       if (outputs->at(i)) { mut_inputs.at(i) = outputs->at(i); }
     }
@@ -127,10 +127,12 @@ Maybe<void> AutogradInterpreter::Apply(const OpExpr& op_expr, const TensorTuple&
     JUST(GetThreadLocalAutogradEngine()->AddBackwardFuncPtr(op_expr.op_type_name() + "_backward",
                                                             backward_fn, inputs, outputs));
 
-    // If inplace, set grad_fn_node for inputs(mut_inputs) the same as the outputs
-    for (int i = 0; i < outputs->size(); ++i) {
-      if (mut_inputs.at(i)) {
-        mut_inputs.at(i)->set_grad_fn_node(outputs->at(i)->mut_grad_fn_node());
+    // With DTR if inplace, set grad_fn_node for inputs(mut_inputs) the same as the outputs
+    if (oneflow::DTREnabled()) {
+      for (int i = 0; i < outputs->size(); ++i) {
+        if (mut_inputs.at(i)) {
+          mut_inputs.at(i)->set_grad_fn_node(outputs->at(i)->mut_grad_fn_node());
+        }
       }
     }
   }
