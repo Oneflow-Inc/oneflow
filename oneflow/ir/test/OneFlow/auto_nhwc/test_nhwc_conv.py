a18c7e7fa9f3147f397ea75bbac5c6f559677809
BBuf <1182563586@qq.com>
2022-03-11 17:54:00 +0800

    refine

--- oneflow/ir/test/OneFlow/auto_nhwc/test_nhwc_conv.py
+++ oneflow/ir/test/OneFlow/auto_nhwc/test_nhwc_conv.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 # RUN: python3 %s | FileCheck %s
-# CHECK-NOT: oneflow.pad
 
 import unittest
 import numpy as np
