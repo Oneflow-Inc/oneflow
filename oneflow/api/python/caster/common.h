de22350677f70df6f94746a52766c1c9876c480d
daquexian <daquexian566@gmail.com>
2022-03-13 17:28:53 +0800

    add comments
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- oneflow/api/python/caster/common.h
+++ oneflow/api/python/caster/common.h
@@ -5,6 +5,7 @@
 namespace pybind11 {
 namespace detail {
 
+// The condition follows the pybind11 source code
 template<typename T>
 using IsSupportedByPybind11WhenInsideSharedPtr =
     std::is_base_of<type_caster_base<T>, type_caster<T>>;
