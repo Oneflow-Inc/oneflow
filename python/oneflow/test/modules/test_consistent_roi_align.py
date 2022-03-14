875ac38ed043bed3a4e4800fe1106ceee68402fe
wyg1997 <wyg19970408@gmail.com>
2022-03-14 19:24:03 +0800

    remove useless code

--- python/oneflow/test/modules/test_consistent_roi_align.py
+++ python/oneflow/test/modules/test_consistent_roi_align.py
@@ -103,9 +103,6 @@ def _test_roi_align(test_case, placement, rois_sbp):
         placement=flow.env.all_device_placement("cpu"), sbp=[flow.sbp.broadcast,]
     ).to_local()
     torch_input_grad = x.pytorch.grad.detach().cpu()
-
-    of_grad_np = of_input_grad.numpy()
-    torch_grad_np = torch_input_grad.detach().cpu().numpy()
     test_case.assertTrue(np.allclose(of_input_grad.numpy(), torch_input_grad.numpy(), rtol=1e-04, atol=1e-4))
 
 
