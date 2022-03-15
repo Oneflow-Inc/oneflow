f602509220d97676b380e38ab70f05df6b6236ec
hjchen2 <chenhoujiangcug@gmail.com>
2022-03-15 19:42:49 +0800

    update

--- oneflow/core/autograd/autograd_captured_tensor.h
+++ oneflow/core/autograd/autograd_captured_tensor.h
@@ -16,6 +16,7 @@ limitations under the License.
 #ifndef ONEFLOW_CORE_AUTOGRAD_AUTOGRAD_CAPTURED_TENSOR_H_
 #define ONEFLOW_CORE_AUTOGRAD_AUTOGRAD_CAPTURED_TENSOR_H_
 
+#include "oneflow/core/framework/functional/functional.h"
 #include "oneflow/core/framework/tensor.h"
 
 namespace oneflow {
