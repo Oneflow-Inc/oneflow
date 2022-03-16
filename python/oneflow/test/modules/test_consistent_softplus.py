95b94d6528aae10b675d0804109edc6e2223dcee
wyg1997 <wyg19970408@gmail.com>
2022-03-16 17:48:02 +0800

    test(SoftPlusOp): add eager global test

---
+++ python/oneflow/test/modules/test_consistent_softplus.py
@@ -0,0 +1,43 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+
+import oneflow as flow
+import oneflow.unittest
+from oneflow.test_utils.automated_test_util import *
+
+
+@autotest(n=1, check_graph=False)
+def _test_softplus(test_case, placement, sbp):
+    dims = [random(1, 3) * 8 for i in range(4)]
+    x = random_tensor(4, *dims).to_global(placement=placement, sbp=sbp)
+    # TODO: test beta and threshold:
+    # issue: https://github.com/Oneflow-Inc/oneflow/issues/7817
+    y = torch.nn.functional.softplus(x)
+    return y
+
+
+class TestSoftPlus(flow.unittest.TestCase):
+    @globaltest
+    def test_softplus(test_case):
+        for placement in all_placement():
+            for sbp in all_sbp(placement, max_dim=4):
+                _test_softplus(test_case, placement, sbp)
+
+
+if __name__ == "__main__":
+    unittest.main()
