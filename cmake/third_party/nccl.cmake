1cc115150fd13ba8fae83501a6ffeb8fb9085b7d
liujuncheng <liujuncheng1022@gmail.com>
2022-03-17 11:42:44 +0800

    Bump NCCL from 2.11.4 to 2.12.7

--- cmake/third_party/nccl.cmake
+++ cmake/third_party/nccl.cmake
@@ -34,7 +34,7 @@ else()
   set(NCCL_INCLUDE_DIR ${NCCL_INSTALL_DIR}/include)
   set(NCCL_LIBRARY_DIR ${NCCL_INSTALL_DIR}/lib)
 
-  set(NCCL_URL https://github.com/NVIDIA/nccl/archive/30ca3fcacf8a73c48d7b8f7aaa54ae8bff89e884.zip)
+  set(NCCL_URL https://github.com/NVIDIA/nccl/archive/refs/tags/v2.12.7-1.tar.gz)
   use_mirror(VARIABLE NCCL_URL URL ${NCCL_URL})
 
   list(APPEND NCCL_LIBRARIES ${NCCL_LIBRARY_DIR}/${NCCL_LIBRARY_NAME})
@@ -47,7 +47,7 @@ else()
       nccl
       PREFIX nccl
       URL ${NCCL_URL}
-      URL_MD5 84d390b56922332486bb92f4e7895d1d
+      URL_MD5 990e391de7d3bda47e10ea66d621626c
       UPDATE_COMMAND ""
       CONFIGURE_COMMAND ""
       BUILD_IN_SOURCE 1
