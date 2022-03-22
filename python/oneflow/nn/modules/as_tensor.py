1bb8438cac2d3a428c5c51d3a2df5dd62abc9e5b
BBuf <1182563586@qq.com>
2022-03-22 10:56:36 +0800

    add as_tensor op align with pytorch

--- python/oneflow/nn/modules/as_tensor.py
+++ python/oneflow/nn/modules/as_tensor.py
@@ -13,10 +13,67 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
+import numpy as np
 import oneflow as flow
 
 
-def as_tensor(*args, **kwargs):
-    # TODO: support construct tensor with sharing memory with data
-    return flow.tensor(*args, **kwargs)
+def isdtype(data, dt):
+    return data.dtype.num == np.dtype(dt).num
+
+
+def infer_type(data):
+    if isdtype(data, np.int32):
+        return flow.int32
+    elif isdtype(data, np.int64):
+        return flow.int64
+    elif isdtype(data, np.int8):
+        return flow.int8
+    elif isdtype(data, np.uint8):
+        return flow.uint8
+    elif isdtype(data, np.bool):
+        return flow.bool
+    elif isdtype(data, np.float64):
+        return flow.float64
+    elif isdtype(data, np.float32):
+        return flow.float32
+    elif isdtype(data, np.float16):
+        return flow.float16
+
+
+def as_tensor(data, dtype=None, device=None):
+    if flow.is_tensor(data):
+        if dtype is None:
+            dtype = data.dtype
+        if device is None:
+            device = data.device
+        if data.dtype is dtype and data.device is device:
+            return data
+        else:
+            data = data.to(dtype=dtype, device=device)
+    elif isinstance(data, (np.ndarray)):
+        if dtype is None:
+            if (device is None) or (device.type == "cpu"):
+                data = flow.from_numpy(data)
+            else:
+                data = flow.tensor(data, device=device)
+        else:
+            if infer_type(data) is dtype:
+                if (device is None) or (device.type == "cpu"):
+                    data = flow.from_numpy(data)
+                else:
+                    data = flow.tensor(data, dtype=dtype, device=device)
+            else:
+                if (device is None) or (device.type == "cpu"):
+                    data = flow.tensor(data, dtype=dtype)
+                else:
+                    data = flow.tensor(data, dtype=dtype, device=device)
+    else:
+        # handle tuple, list, scalar
+        data = np.array(data)
+        # not shared memory in this case
+        data = flow.tensor(data)
+        if device is not None:
+            data = data.to(device)
+        if dtype is not None:
+            data = data.to(dtype)
+    return data
---
+++ python/oneflow/test/modules/test_as_tensor.py
@@ -0,0 +1,159 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import random
+import unittest
+
+import numpy as np
+import oneflow as flow
+import oneflow.unittest
+
+
+def isdtype(data, dt):
+    return data.dtype.num == np.dtype(dt).num
+
+
+def infer_type(data):
+    if isdtype(data, np.int32):
+        return flow.int32
+    elif isdtype(data, np.int64):
+        return flow.int64
+    elif isdtype(data, np.int8):
+        return flow.int8
+    elif isdtype(data, np.uint8):
+        return flow.uint8
+    elif isdtype(data, np.bool):
+        return flow.bool
+    elif isdtype(data, np.float64):
+        return flow.float64
+    elif isdtype(data, np.float32):
+        return flow.float32
+    elif isdtype(data, np.float16):
+        return flow.float16
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestAsTensor(flow.unittest.TestCase):
+    def test_tensor_type(test_case):
+        x = flow.randn(2, 3)
+        y = flow.as_tensor(x)
+        y[0] = 2.0
+        test_case.assertTrue(np.array_equal(x.numpy(), y.numpy()))
+        test_case.assertTrue(np.array_equal(id(x), id(y)))
+
+        x = flow.randn(2, 3)
+        x = x.to("cuda")
+        y = flow.as_tensor(x)
+        y[0] = 2.0
+        test_case.assertTrue(np.array_equal(x.numpy(), y.numpy()))
+        test_case.assertTrue(np.array_equal(id(x), id(y)))
+
+        x = flow.randn(2, 3)
+        y = flow.as_tensor(x, device=flow.device("cuda:0"))
+        test_case.assertTrue(id(x) != id(y))
+
+        for dtype in [
+            flow.float64,
+            flow.float16,
+            flow.int64,
+            flow.int32,
+            flow.int8,
+            flow.uint8,
+        ]:
+            x = flow.randn(2, 3)
+            y = flow.as_tensor(x, dtype=dtype)
+            test_case.assertTrue(id(x) != id(y))
+
+    def test_numpy_type(test_case):
+        for device in [flow.device("cpu"), flow.device("cuda:0"), None]:
+            for np_dtype in [
+                np.float64,
+                np.float32,
+                np.float16,
+                np.int64,
+                np.int32,
+                np.int8,
+                np.uint8,
+            ]:
+                for flow_dtype in [
+                    flow.float64,
+                    flow.float16,
+                    flow.int64,
+                    flow.int32,
+                    flow.int8,
+                    flow.uint8,
+                ]:
+                    np_arr = np.ones((2, 3), dtype=np_dtype)
+                    try:
+                        tensor = flow.as_tensor(np_arr, dtype=flow_dtype)
+                        if infer_type(
+                            np_arr
+                        ) == flow_dtype and device is not flow.device("cuda:0"):
+                            tensor[0][0] += 1.0
+                            test_case.assertTrue(np.array_equal(np_arr, tensor.numpy()))
+                        else:
+                            test_case.assertTrue(np.array_equal(np_arr, tensor.numpy()))
+                    except Exception as e:
+                        # Ignore cast or kernel mismatch error in test example
+                        pass
+
+    def test_other_type(test_case):
+        for device in [flow.device("cpu"), flow.device("cuda:0"), None]:
+            for np_dtype in [
+                np.float64,
+                np.float32,
+                np.float16,
+                np.int64,
+                np.int32,
+                np.int8,
+                np.uint8,
+            ]:
+                for flow_dtype in [
+                    flow.float64,
+                    flow.float16,
+                    flow.int64,
+                    flow.int32,
+                    flow.int8,
+                    flow.uint8,
+                ]:
+                    # tuple
+                    np_arr = (1.0, 2.0, 3.0)
+                    try:
+                        tensor = flow.as_tensor(np_arr, dtype=flow_dtype)
+                        test_case.assertTrue(np.array_equal(np_arr, tensor.numpy()))
+                    except Exception as e:
+                        # Ignore cast or kernel mismatch error in test example
+                        pass
+                    # tuple
+                    np_arr = [1.0, 2.0, 3.0]
+                    try:
+                        tensor = flow.as_tensor(np_arr, dtype=flow_dtype)
+                        test_case.assertTrue(np.array_equal(np_arr, tensor.numpy()))
+                    except Exception as e:
+                        # Ignore cast or kernel mismatch error in test example
+                        pass
+                    # scalar
+                    np_arr = 4.0
+                    try:
+                        tensor = flow.as_tensor(np_arr, dtype=flow_dtype)
+                        test_case.assertTrue(np.array_equal(np_arr, tensor.numpy()))
+                    except Exception as e:
+                        # Ignore cast or kernel mismatch error in test example
+                        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
