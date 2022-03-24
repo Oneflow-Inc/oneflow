0e8d32a5d98e1e3f8b9541aea27f62e76140bb77
BBuf <1182563586@qq.com>
2022-03-24 19:34:19 +0800

    revert

--- oneflow/core/common/shape.cpp
+++ oneflow/core/common/shape.cpp
@@ -21,8 +21,6 @@ limitations under the License.
 namespace oneflow {
 
 Shape CreateReducedShape(const ShapeView& shape, const AxisVector& axis_vec) {
-  // For 0-dim Tensor
-  if (axis_vec.empty()) { return Shape({}); }
   DimVector dim_vec;
   shape.ToDimVector(&dim_vec);
   for (int64_t axis : axis_vec) { dim_vec.at(ShiftNegativeAxis(axis, shape.NumAxes())) = 1; }
