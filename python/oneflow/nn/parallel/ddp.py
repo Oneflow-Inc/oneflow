fbd92dcdec10912a3a3bfeb9e16efde8257bffcc
luyang <flowingsun007@163.com>
2022-03-25 19:21:59 +0800

    refine

--- python/oneflow/nn/parallel/ddp.py
+++ python/oneflow/nn/parallel/ddp.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import os
-import warning
+import warnings
 from collections import OrderedDict
 
 import oneflow as flow
