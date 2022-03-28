bffe120471260e6b676821eb73d3052c5e0a6de0
luyang <flowingsun007@163.com>
2022-03-28 15:59:11 +0800

    fix topo

--- python/oneflow/nn/parallel/ddp.py
+++ python/oneflow/nn/parallel/ddp.py
@@ -80,8 +80,8 @@ def DistributedDataParallel(
     module: "flow.nn.Module", *, broadcast_buffers: bool = True, bucket_size: int = 10
 ):
     assert all(x.dtype == flow.float32 for x in module.parameters())
-    if parse_boolean_form_env("ONEFLOW_DESABLE_VIEW", True):
-        warnings.warn("because the environment variable 'ONEFLOW_DESABLE_VIEW' is set to true, so the view mechanism is disabled, and we will set bucket_size = 1")
+    if parse_boolean_form_env("ONEFLOW_DISABLE_VIEW", True):
+        warnings.warn("because the environment variable 'ONEFLOW_DISABLE_VIEW' is set to true, so the view mechanism is disabled, and we will set bucket_size = 1")
         bucket_size = 1
     world_size = flow.env.get_world_size()
     with flow.no_grad():
