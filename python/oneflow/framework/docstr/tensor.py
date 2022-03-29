66bac94783d7c53eba755a73070dc44514aa8563
luyang <flowingsun007@163.com>
2022-03-29 13:02:23 +0800

    add doc

--- python/oneflow/framework/docstr/tensor.py
+++ python/oneflow/framework/docstr/tensor.py
@@ -206,6 +206,24 @@ add_docstr(
 )
 
 add_docstr(
+    oneflow.Tensor.storage_offset,
+    """
+    Tensor.storage_offset() -> Tensor
+
+    Returns self tensor’s offset in the underlying storage in terms of number of storage elements (not bytes).
+
+    Example:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> x = flow.tensor([1, 2, 3, 4, 5])
+        >>> x.storage_offset()
+        0
+    """,
+)
+
+add_docstr(
     oneflow.Tensor.to_global,
     """
     Tensor.to_global(placement=None, sbp=None, grad_sbp=None) -> Tensor
