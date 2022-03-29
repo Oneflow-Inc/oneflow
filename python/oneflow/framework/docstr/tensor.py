dfbdb52167615da059482cd262b0476f69bb5a48
Li Xiang <54010254+lixiang007666@users.noreply.github.com>
2022-03-29 21:44:17 +0800

    Fix docstr[lixiang] (#7844)
    * Fix docstr[liiang]
    
    * Format
    
    * Update tensor.py
    
    * Update tensor.py
    
    * Refine
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- python/oneflow/framework/docstr/tensor.py
+++ python/oneflow/framework/docstr/tensor.py
@@ -507,38 +507,8 @@ add_docstr(
 
 add_docstr(
     oneflow.Tensor.argsort,
-    """This operator sorts the input Tensor at specified dim and return the indices of the sorted Tensor.
-
-    Args:
-        input (oneflow.Tensor): The input Tensor.
-        dim (int, optional): dimension to be sorted. Defaults to the last dim (-1).
-        descending (bool, optional): controls the sorting order (ascending or descending).
-
-    Returns:
-        oneflow.Tensor: The indices of the sorted Tensor.
-
-    For example:
-
-    .. code-block:: python
-
-        >>> import numpy as np
-        >>> import oneflow as flow
-        >>> x = np.array([[10, 2, 9, 3, 7],
-        ...               [1, 9, 4, 3, 2]]).astype("float32")
-        >>> input = flow.Tensor(x)
-        >>> output = flow.argsort(input)
-        >>> output
-        tensor([[1, 3, 4, 2, 0],
-                [0, 4, 3, 2, 1]], dtype=oneflow.int32)
-        >>> output = flow.argsort(input, descending=True)
-        >>> output
-        tensor([[0, 2, 4, 3, 1],
-                [1, 2, 3, 4, 0]], dtype=oneflow.int32)
-        >>> output = flow.argsort(input, dim=0)
-        >>> output
-        tensor([[1, 0, 1, 0, 1],
-                [0, 1, 0, 1, 0]], dtype=oneflow.int32)
-
+    """
+    See :func:`oneflow.argsort`
     """,
 )
 
