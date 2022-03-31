1c15a4885f5bd3a7840dc9bef25929c88cd5b761
luyang <flowingsun007@163.com>
2022-03-31 12:06:43 +0800

    refine

--- python/oneflow/test_utils/automated_test_util/torch_flow_dual_object.py
+++ python/oneflow/test_utils/automated_test_util/torch_flow_dual_object.py
@@ -910,8 +910,6 @@ def check_tensor_equality(torch_tensor, flow_tensor, rtol=0.0001, atol=1e-05, ch
     )
     # NOTE: if check_dtype=True, then check the equality of data type
     if check_dtype:
-        print("torch_numpy.dtype >>>> ", torch_numpy.dtype)
-        print("oneflow_numpy.dtype >>>> ", oneflow_numpy.dtype)
         equality_res = equality_res and (torch_numpy.dtype == oneflow_numpy.dtype)
 
     if equality_res == False:
