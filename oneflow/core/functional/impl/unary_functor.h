6c69381ac9a47933c8e5ed8979bcdf32ae3699f6
Flowingsun007 <flowingsun007@163.com>
2022-04-02 17:34:46 +0800

    refine

--- oneflow/core/functional/impl/unary_functor.h
+++ oneflow/core/functional/impl/unary_functor.h
@@ -33,7 +33,7 @@ namespace impl {
 class UnaryFunctor {
  public:
   Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x) const {
-    return OpInterpUtil::Dispatch<Tensor>(*op_, {x->)});
+    return OpInterpUtil::Dispatch<Tensor>(*op_, {x});
   }
 
  protected:
@@ -50,10 +50,10 @@ class InplaceUnaryFunctor {
     std::shared_ptr<TensorTuple> outputs = std::make_shared<TensorTuple>(1);
     outputs->at(0) = x;
     if (x->requires_grad()) {
-      JUST(OpInterpUtil::Dispatch(*op_, {JUST(functional::Identity(x->)))},
+      JUST(OpInterpUtil::Dispatch(*op_, {JUST(functional::Identity(x))},
                                   outputs.get()));
     } else {
-      JUST(OpInterpUtil::Dispatch(*op_, {x->)}, outputs.get()));
+      JUST(OpInterpUtil::Dispatch(*op_, {x}, outputs.get()));
     }
     return outputs->at(0);
   }
@@ -70,7 +70,7 @@ class FloatUnaryFunctor {
   Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x) const {
     // The functor lowest Dtype is Float32. (For sigmoid, tanh and etc. )
     TensorProcessor tensor_processor;
-    JUST(tensor_processor.AddInputs({x->)}, DType::Float()).Apply());
+    JUST(tensor_processor.AddInputs({x}, DType::Float()).Apply());
     TensorTuple input_tuple = JUST(tensor_processor.GetInputs());
     return OpInterpUtil::Dispatch<one::Tensor>(*op_, input_tuple);
   }
@@ -84,9 +84,8 @@ class FloatUnaryFunctor {
 
 class InplaceFloatUnaryFunctor {
  public:
-  Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& input) const {
+  Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x) const {
     TensorProcessor tensor_processor;
-    auto x = input->);
     JUST(tensor_processor.AddInputs({x}, DType::Float()).Apply());
     TensorTuple input_tuple = JUST(tensor_processor.GetInputs());
     JUST(CheckInplaceCastValid(x, input_tuple.at(0)));
@@ -96,10 +95,10 @@ class InplaceFloatUnaryFunctor {
     if (x->requires_grad()) {
       // It should copy input tensor in autograd_mode because these operators can't calculate
       // in_grad with output.
-      JUST(OpInterpUtil::Dispatch(*op_, {JUST(functional::Identity(x->)))},
+      JUST(OpInterpUtil::Dispatch(*op_, {JUST(functional::Identity(x))},
                                   outputs.get()));
     } else {
-      JUST(OpInterpUtil::Dispatch(*op_, {x->)}, outputs.get()));
+      JUST(OpInterpUtil::Dispatch(*op_, {x}, outputs.get()));
     }
     return outputs->at(0);
   }
