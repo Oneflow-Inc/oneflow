ec5a799e485423bb8fd2f731bd931a568c49423b
Ldpe2G <liangdepeng@gmail.com>
2022-04-05 21:18:40 +0800

    fix ci

--- python/oneflow/test/expensive/pytorch_resmlp.py
+++ python/oneflow/test/expensive/pytorch_resmlp.py
@@ -15,10 +15,42 @@ limitations under the License.
 """
 import torch
 import torch.nn as nn
-from timm.models.layers import trunc_normal_, DropPath, to_2tuple, Mlp
+from timm.models.layers import trunc_normal_, DropPath, to_2tuple
 
 
 __all__ = ["resmlp_12"]
+
+
+class Mlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=nn.GELU,
+        drop=0.0,
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        drop_probs = to_2tuple(drop)
+
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
 
 
 class PatchEmbed(nn.Module):
--- python/oneflow/test/expensive/pytroch_mlp_mixer.py
+++ python/oneflow/test/expensive/pytroch_mlp_mixer.py
@@ -16,10 +16,42 @@ limitations under the License.
 import math
 import torch
 import torch.nn as nn
-from timm.models.layers import Mlp, DropPath, lecun_normal_, to_2tuple
+from timm.models.layers import DropPath, lecun_normal_, to_2tuple
 
 from functools import partial
 from typing import Callable
+
+
+class Mlp(nn.Module):
+    """ MLP as used in Vision Transformer, MLP-Mixer and related networks
+    """
+
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=nn.GELU,
+        drop=0.0,
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        drop_probs = to_2tuple(drop)
+
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.drop1 = nn.Dropout(drop_probs[0])
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop2 = nn.Dropout(drop_probs[1])
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop1(x)
+        x = self.fc2(x)
+        x = self.drop2(x)
+        return x
 
 
 class PatchEmbed(nn.Module):
