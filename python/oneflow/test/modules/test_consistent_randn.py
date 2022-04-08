19ba6852f024de845c41ff72dfd89b61e3980e84
fengdaozhuo <fengdaozhuo960@163.com>
2022-04-08 16:02:22 +0800

    Randn op support global tensor

--- python/oneflow/test/modules/test_consistent_randn.py
+++ python/oneflow/test/modules/test_consistent_randn.py
@@ -28,17 +28,18 @@ from oneflow.test_utils.test_util import GenArgDict
 def _test_consistent_randn(test_case, shape, placement, sbp):
     x1 = flow.randn(*shape, placement=placement, sbp=sbp)
     x2 = flow.randn(*shape, placement=placement, sbp=sbp)
-    print("x1: ",x1,"x2: ",x2)
     test_case.assertTrue(not np.allclose(x1.numpy(), x2.numpy(), atol=1e-4, rtol=1e-4))
     test_case.assertEqual(x1.shape, flow.Size(shape))
     test_case.assertEqual(x1.sbp, sbp)
     test_case.assertEqual(x1.placement, placement)
 
+
 def _test_different_dtype(test_case, shape, placement, sbp):
-    x1 = flow.randn(*shape,dtype=flow.float32, placement=placement, sbp=sbp)
-    x2 = flow.randn(*shape,dtype=flow.float64, placement=placement, sbp=sbp)
+    x1 = flow.randn(*shape, dtype=flow.float32, placement=placement, sbp=sbp)
+    x2 = flow.randn(*shape, dtype=flow.float64, placement=placement, sbp=sbp)
     test_case.assertTrue(not np.allclose(x1.numpy(), x2.numpy(), atol=1e-4, rtol=1e-4))
     test_case.assertEqual(x1.shape, flow.Size(shape))
+
 
 def _test_backward(test_case, shape, placement, sbp):
     x = flow.randn(*shape, placement=placement, sbp=sbp, requires_grad=True)
@@ -48,17 +49,15 @@ def _test_backward(test_case, shape, placement, sbp):
         np.allclose(np.ones(shape), x.grad.numpy(), atol=1e-4, rtol=1e-4)
     )
 
+
 def _test_with_generator(test_case, shape, placement, sbp):
     gen = flow.Generator()
     gen.manual_seed(0)
-    y1 = flow.randn(
-        *shape, placement=placement, sbp=sbp, generator=gen
-    )
+    y1 = flow.randn(*shape, placement=placement, sbp=sbp, generator=gen)
     gen.manual_seed(0)
-    y2 = flow.randn(
-        *shape, placement=placement, sbp=sbp, generator=gen
-    )
+    y2 = flow.randn(*shape, placement=placement, sbp=sbp, generator=gen)
     test_case.assertTrue(np.allclose(y1.numpy(), y2.numpy(), atol=1e-4, rtol=1e-4))
+
 
 def _test_randn_tuple_shape(test_case, shape, placement, sbp):
     y1 = flow.randn(*shape, placement=placement, sbp=sbp)
@@ -99,6 +98,7 @@ class TestRandnConsistent(flow.unittest.TestCase):
                     _test_backward(test_case, shape, placement, sbp)
                     _test_with_generator(test_case, shape, placement, sbp)
                     _test_randn_tuple_shape(test_case, shape, placement, sbp)
+
     @globaltest
     def test_randn_graph(test_case):
         arg_dict = OrderedDict()
