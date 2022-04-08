d82d1deb59f87fe11a6ef9870838d8989d5c2956
robin978 <robin978@onflow.com>
2022-04-08 14:52:37 +0800

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,7 +82,6 @@ pygments_style = None
 # html_theme = "furo"
 html_theme = "sphinx_rtd_theme"
 
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
