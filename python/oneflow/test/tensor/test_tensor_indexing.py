af1955b79b2c8f0379c3ee69da021056c70a3430
mosout <mosout@qq.com>
2022-04-08 18:25:32 +0800

    refine

--- python/oneflow/test/tensor/test_tensor_indexing.py
+++ python/oneflow/test/tensor/test_tensor_indexing.py
@@ -327,15 +327,21 @@ class TestTensorIndexing(flow.unittest.TestCase):
         for arg in GenArgList(arg_dict):
             arg[0](test_case, *arg[1:])
 
-    def test_indecies_on_different_devices(test_case):
+    def test_indecies_on_different_devices_1_gpu(test_case):
         x = flow.ones(3, 10)
-        y = flow.ones(3, 10).cuda()
+        y = flow.ones(3, 10, device=flow.device("cuda:0"))
 
-        idx = [flow.tensor([1, 2]), flow.tensor([2, 0]).cuda()]
-        idx1 = [flow.tensor([1, 2]).cuda(), flow.tensor([2, 0])]
+        x_idx = [flow.tensor([1, 2]), flow.tensor([2, 0], device=flow.device("cuda:0"))]
+        y_idx = [flow.tensor([1, 2], device=flow.device("cuda:0")), flow.tensor([2, 0])]
 
+        test_case.assertTrue(np.allclose(x[x_idx].numpy(), np.array([1, 1])))
+        test_case.assertTrue(np.allclose(y[y_idx].numpy(), np.array([1, 1])))
+
+    @flow.unittest.skip_unless_1n2d()
+    def test_indecies_on_different_devices_2_gpu(test_case):
+        x = flow.ones(3, 10, device=flow.device("cuda:0"))
+        idx = [flow.tensor([1, 2], device=flow.device("cuda:1")), flow.tensor([2, 0])]
         test_case.assertTrue(np.allclose(x[idx].numpy(), np.array([1, 1])))
-        test_case.assertTrue(np.allclose(y[idx1].numpy(), np.array([1, 1])))
 
 
 if __name__ == "__main__":
