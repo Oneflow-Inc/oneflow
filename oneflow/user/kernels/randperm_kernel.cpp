34248aa83e0f9ad6d4c015fadf641736ed8a61c6
fengdaozhuo <fengdaozhuo960@163.com>
2022-04-08 21:11:38 +0800

    randperm op support global tensor

--- oneflow/user/kernels/randperm_kernel.cpp
+++ oneflow/user/kernels/randperm_kernel.cpp
@@ -51,7 +51,8 @@ class CpuRandPermKernel final : public user_op::OpKernel {
       int64_t parallel_id = ctx->parallel_ctx().parallel_id();
       int32_t n = ctx->Attr<int32_t>("n");
       const Shape& logical_shape = Shape({n});
-      TensorSliceView view = GetTensorSliceView4ParallelId(hierarchy, nd_sbp, logical_shape, parallel_id);
+      TensorSliceView view =
+          GetTensorSliceView4ParallelId(hierarchy, nd_sbp, logical_shape, parallel_id);
       std::shared_ptr<CpuRandPermKernelCache> cache(
           new CpuRandPermKernelCache(view.At(0).begin(), view.At(0).end()));
       return cache;
