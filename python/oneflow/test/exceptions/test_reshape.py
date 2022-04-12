e4cd3ed501d1815d50f1e5ee330317f99c021706
clackhan <han_binbin@163.com>
2022-04-12 18:31:47 +0800

    fix test error

--- python/oneflow/test/exceptions/test_reshape.py
+++ python/oneflow/test/exceptions/test_reshape.py
@@ -28,9 +28,11 @@ class TestModule(flow.unittest.TestCase):
         #   RuntimeError: only one dimension can be inferred
         #
         x = flow.tensor((2, 2))
+        y = x.reshape((-1, -1))
         with test_case.assertRaises(RuntimeError) as ctx:
             y = x.reshape((-1, -1))
-        test_case.assertEqual("only one dimension can be inferred", str(ctx.exception))
+        # test_case.assertEqual("only one dimension can be inferred", str(ctx.exception))
+        test_case.assertTrue("only one dimension can be inferred" in str(ctx.exception))
 
 
 if __name__ == "__main__":
