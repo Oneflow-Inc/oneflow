7e5a0da399c5f0eaa0c3c9ea59af79beb9074193
fengdaozhuo <fengdaozhuo960@163.com>
2022-04-12 11:25:08 +0800

    arange op support global tensor

--- oneflow/user/ops/arange_op.cpp
+++ oneflow/user/ops/arange_op.cpp
@@ -55,24 +55,24 @@ namespace oneflow {
   int64_t range_elem_cnt = 0;
   if (IsIntegralDataType(dtype)) {
     int64_t integer_delta = ctx->Attr<int64_t>("integer_delta");
-    if (integer_delta == static_cast<int64_t>(0)) { return Error::RuntimeError(); }
+    if (integer_delta == static_cast<int64_t>(0)) { return Error::RuntimeError() << " step must be nonzero. ";}
     int64_t integer_start = ctx->Attr<int64_t>("integer_start");
     int64_t integer_limit = ctx->Attr<int64_t>("integer_limit");
     // CHECK when limit > start, delta > 0; limit < start, delta < 0;
     if ((integer_limit - integer_start) / integer_delta == static_cast<int64_t>(0)) {
-      return Error::RuntimeError();
+      return Error::RuntimeError() << " upper bound and larger bound inconsistent with step sign";;
     }
 
     range_elem_cnt = std::ceil(static_cast<double>(integer_limit - integer_start) / integer_delta);
   } else {
     double float_delta = ctx->Attr<double>("float_delta");
-    if (float_delta == static_cast<double>(0.0)) { return Error::RuntimeError(); }
+    if (float_delta == static_cast<double>(0.0)) { return Error::RuntimeError() << " step must be nonzero. "; }
     double float_start = ctx->Attr<double>("float_start");
     double float_limit = ctx->Attr<double>("float_limit");
     // CHECK when limit > start, delta > 0; limit < start, delta < 0;
     // CHECK_GE For 0-Dim Tensor
     if ((float_limit - float_start) / float_delta == static_cast<double>(0.0)) {
-      return Error::RuntimeError();
+      return Error::RuntimeError()  << " upper bound and larger bound inconsistent with step sign";
     }
     range_elem_cnt = std::ceil(static_cast<double>(float_limit - float_start) / float_delta);
   }
