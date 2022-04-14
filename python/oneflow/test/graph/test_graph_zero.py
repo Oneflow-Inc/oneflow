66c8ac3b8f619affb799b078448e785c63f0012d
strint <xiaoyulink@gmail.com>
2022-04-14 23:48:34 +0800

    add 2d test

--- python/oneflow/test/graph/test_graph_zero.py
+++ python/oneflow/test/graph/test_graph_zero.py
@@ -100,6 +100,85 @@ def _test_linear_train_graph_with_zero(test_case, zero_stage=1):
     iter_num = 1
     graph_check_list = train_with_graph(iter_num)
 
+def _test_linear_train_graph_2d_with_zero(test_case, zero_stage=1):
+    def train_with_graph(iter_num=1):
+        P = flow.placement("cuda", ranks=[[0, 1], [2, 3]])
+        B = flow.sbp.broadcast
+        S0 = flow.sbp.split(0)
+        S1 = flow.sbp.split(1)
+
+        linear_dp_mp = flow.nn.Linear(8, 4, bias=False)
+        linear_dp_mp = linear_dp_mp.to_global(placement=P, sbp=[B, S0])
+        flow.nn.init.constant_(linear_dp_mp.weight, 2.068758)
+
+        linear_mp_dp = flow.nn.Linear(8, 5, bias=False)
+        linear_mp_dp = linear_mp_dp.to_global(placement=P, sbp=[S0, B])
+        flow.nn.init.constant_(linear_mp_dp.weight, 2.068758)
+
+        of_sgd = flow.optim.SGD([{"params": linear_dp_mp.parameters()}, {"params": linear_mp_dp.parameters()}], lr=0.001, momentum=0.9)
+        grad_scaler = flow.amp.StaticGradScaler(200)
+
+        x = flow.randint(1, 100, (6, 8), dtype=flow.float32, placement=P, sbp=[S0, B])
+
+        class LinearTrainGraph2DWithZeRO(flow.nn.Graph):
+            def __init__(self):
+                super().__init__()
+                self.linear_dp_mp = linear_dp_mp
+                self.linear_mp_dp = linear_mp_dp
+                self.add_optimizer(of_sgd)
+
+                self.config.enable_amp(True)
+                self.set_grad_scaler(grad_scaler)
+                #self.config.enable_zero(True, stage=zero_stage, min_splited_size=1)
+                self.debug(2)
+
+            def build(self, x):
+                out = self.linear_dp_mp(x)
+                out = out.to_global(placement=P, sbp=[B, S0])
+                out = self.linear_mp_dp(out)
+                loss = out.sum()
+                loss.backward()
+                return out
+
+        class LinearEvalGraph2DWithZeRO(flow.nn.Graph):
+            def __init__(self):
+                super().__init__()
+                self.linear_dp_mp = linear_dp_mp
+                self.linear_mp_dp = linear_mp_dp
+
+                self.config.enable_amp(True)
+
+            def build(self, x):
+                out = self.linear_dp_mp(x)
+                out = out.to_global(placement=P, sbp=[B, S0])
+                out = self.linear_mp_dp(out)
+                return out
+
+        linear_t_g = LinearTrainGraph2DWithZeRO()
+        linear_t_g.debug(1)
+        linear_e_g = LinearEvalGraph2DWithZeRO()
+        linear_e_g.debug(1)
+
+        def one_train_iter():
+            out = linear_t_g(x)
+
+        def one_eval_iter():
+            out = linear_e_g(x)
+
+        for i in range(iter_num):
+            one_train_iter()
+
+        # After pass rewrite in training graph, parameters' sbp has been
+        # changed from flow.sbp.broadcast to flow.sbp.split(0)
+        #test_case.assertEqual(linear_dp.weight.sbp[0], S0)
+        #test_case.assertEqual(linear_mp.weight.sbp[0], S0)
+
+        # In evaluation graph, paramters's sbp are flow.sbp.split(0).
+        # But their consumer will consum them as flow.sbp.broadcast.
+        one_eval_iter()
+
+    iter_num = 1
+    graph_check_list = train_with_graph(iter_num)
 
 @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
 @flow.unittest.skip_unless_1n2d()
@@ -113,6 +192,11 @@ class TestLinearTrainGraphWithZeRO(oneflow.unittest.TestCase):
     def _test_linear_train_graph_with_zero_3(test_case):
         _test_linear_train_graph_with_zero(test_case, 3)
 
+@unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
+@flow.unittest.skip_unless_1n4d()
+class TestLinearTrainGraph2DWithZeRO(oneflow.unittest.TestCase):
+    def test_linear_train_graph_2d_with_zero_1(test_case):
+        _test_linear_train_graph_2d_with_zero(test_case, 1)
 
 if __name__ == "__main__":
     unittest.main()
