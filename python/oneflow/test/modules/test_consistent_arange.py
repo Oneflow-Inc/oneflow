151c6e812b2fa5a0ac47f0cc504d9bd1efe1804c
fengdaozhuo <fengdaozhuo960@163.com>
2022-04-18 14:26:33 +0800

    arange kernel support global tensor

--- python/oneflow/test/modules/test_consistent_arange.py
+++ python/oneflow/test/modules/test_consistent_arange.py
@@ -113,6 +113,7 @@ class TestArangeConsistent(flow.unittest.TestCase):
                         test_case, **args, placement=placement, sbp=sbp
                     )
 
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
     @globaltest
     def test_arange_graph(test_case):
         arg_dict = OrderedDict()
