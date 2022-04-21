4f2520785b93ea10946733108ae9d7025ee84ecb
WangYi <buaawangyi03@gmail.com>
2022-04-21 13:52:56 +0800

    add more docs

--- python/oneflow/framework/docstr/tensordot.py
+++ python/oneflow/framework/docstr/tensordot.py
@@ -19,7 +19,7 @@ from oneflow.framework.docstr.utils import add_docstr
 add_docstr(
     oneflow.tensordot,
     r"""
-    tensordot(a, b, dims=Union[int, Tensor, Tuple]) -> Tensor
+    tensordot(a, b, dims=Union[int, Tensor, Tuple[List[int]], List[List[int]]]) -> Tensor
     
     Compute tensor dot along given dimensions.
     
@@ -27,12 +27,17 @@ add_docstr(
     lists and calculate the tensor dot along every dim pair.
 
     Args:
-        a: The input tensor to compute tensordot
-        b: The input tensor to compute tensordot
-        dims: int or array-like
+        a (oneflow.Tensor): The input tensor to compute tensordot
+        b (oneflow.Tensor): The input tensor to compute tensordot
+        dims (int or list or tuple or oneflow.Tensor):
+            The dims to calculate tensordot.
+            If it's an integer or oneflow.Tensor with only one element,
+            the last `dims` of tensor a and the first `dims` of tensor b will be calculated.
+            If it's a list or tuple or oneflow.Tensor,
+            it must contain two array-like object, which represent the dims of tensor a and tensor b to be calculated.
         
     Returns:
-        Oneflow.Tensor: The result tensor
+        oneflow.Tensor: The result tensor
 
     For example:
     
@@ -50,5 +55,37 @@ add_docstr(
         >>> flow.tensordot(a, b, dims=[[0, 1], [0, 1]]).shape
         oneflow.Size([5, 7])
     
+    Note:
+        The operation is equivalent to the series of operations:
+
+        - Permute the dimensions of the tensor A that require tensordot to the end
+
+        - Permute the dimensions of the tensor B that require tensordot to the start
+
+        - Reshape the permuted tensor A into a 2-dimensional tensor, where the size of the 0th dimension is the product of the dimensions that do not require dot product, and the size of the 1st dimension is the product of the dimensions that require dot product
+
+        - Reshape the permuted tensor B into a 2-dimensional tensor, where the size of the 0th dimension is the product of the dimensions that require dot product, and the size of the 1st dimension is the product of the dimensions that do not require dot product
+
+        - Calculate the matrix multiplication of reshaped tensor A and reshaped tensor B
+
+        - Reshape the result of matrix multiplication, the target shape is the concatenation of the dimensions that do not require tensordot of tensor A and B
+
+    This series of operations can be equivalently represented by the following code:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> a = flow.randn(2, 4, 3)
+        >>> b = flow.randn(3, 4, 2)
+        >>> dims = [[0, 2], [2, 0]]
+        >>> permuted_a = a.permute(1, 0, 2) # 0, 2 are the dimensions requiring tensordot and are placed in the end in permuting
+        >>> permuted_b = b.permute(2, 0, 1) # 2, 0 are the dimensions requiring tensordot and are placed at the beginning in permuting
+        >>> reshaped_a = permuted_a.reshape(4, 2 * 3) # 4 is the dimensions of a that do not require tensordot
+        >>> reshaped_b = permuted_b.reshape(2 * 3, 4) # 4 is the dimensions of a that do not require tensordot
+        >>> matmul_result = flow.matmul(reshaped_a, reshaped_b)
+        >>> result = matmul_result.reshape(4, 4) # 4, 4 are the concatentation of dimensions that do not require tensordot of a and b
+        >>> flow.all(result == flow.tensordot(a, b, dims))
+        tensor(True, dtype=oneflow.bool)
+
     """,
 )
