bc122c60b99f47873233bea6a3c99dd6f8570eb4
yuhao <1171760467@qq.com>
2022-04-27 16:28:32 +0800

    add OneflowToFunc.cpp

---
+++ oneflow/ir/lib/OneFlow/Conversion/OneflowToFunc.cpp
@@ -0,0 +1,88 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "OneFlow/OneFlowOps.h"
+#include <iostream>
+#include <string>
+#include "OneFlow/OneFlowDialect.h"
+#include "OneFlow/Passes.h"
+#include "llvm/ADT/STLExtras.h"
+#include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
+#include "mlir/Conversion/TosaToLinalg/TosaToLinalg.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Linalg/Passes.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Func/Transforms/Passes.h"
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/OpImplementation.h"
+
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Transforms/Passes.h"
+
+namespace mlir {
+namespace oneflow {
+
+struct JobLowering final : public OpConversionPattern<Job> {
+ public:
+  using OpConversionPattern<Job>::OpConversionPattern;
+  LogicalResult matchAndRewrite(Job op, OpAdaptor adaptor,
+                                ConversionPatternRewriter& rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::func::FuncOp>(op,
+                                                    /* sym_name */ op.sym_name(),
+                                                    /* type     */ op.fuction_type());
+    return success();
+  }
+};
+struct ReturnOpLowering final : public OpConversionPattern<ReturnOp> {
+ public:
+  using OpConversionPattern<ReturnOp>::OpConversionPattern;
+  LogicalResult matchAndRewrite(ReturnOp op, OpAdaptor adaptor,
+                                ConversionPatternRewriter& rewriter) const override {
+    rewriter.replaceOpWithNewOp<mlir::func::ReturnOp>(op,
+                                                      /* operands */ op.operands());
+    return success();
+  }
+};
+
+namespace {
+struct OneFlowLoweringToFuncPass : public LowerOneFlowToFuncPassBase<OneFlowLoweringToFuncPass> {
+  void runOnOperation() override;
+};
+}  // namespace
+
+std::unique_ptr<Pass> createLowerOneFlowToFuncPass() {
+  return std::make_unique<OneFlowLoweringToFuncPass>();
+}
+
+void OneFlowLoweringToFuncPass::runOnOperation() {
+  ConversionTarget target(getContext());
+  target.addLegalDialect<memref::MemRefDialect, func::FuncDialect>();
+  target.addIllegalDialect<OneFlowDialect>();
+  RewritePatternSet patterns(&getContext());
+  patterns.insert<JobLowering, ReturnOpLowering>(&getContext());
+  if (failed(applyPartialConversion(getOperation(), target, std::move(patterns)))) {
+    getOperation()->dump();
+    signalPassFailure();
+  }
+}
+
+} // oneflow
+} //mlir
