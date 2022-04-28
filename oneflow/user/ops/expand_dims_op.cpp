5760e432fb9e2c045defd213fe2689dfe6194387
liufengwei <2472937968@qq.com>
2022-04-28 22:57:48 +0800

    expand_dims op check error

--- oneflow/user/ops/expand_dims_op.cpp
+++ oneflow/user/ops/expand_dims_op.cpp
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#include "oneflow/core/common/maybe.h"
+#include "oneflow/core/common/throw.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/framework/op_generated.h"
 
@@ -20,10 +22,11 @@ namespace oneflow {
 
 namespace {
 
-int32_t TransformNegativeAxisToPositive(int32_t axis, const int32_t num_axes) {
+Maybe<int32_t> TransformNegativeAxisToPositive(int32_t axis, const int32_t num_axes) {
   axis = axis < 0 ? axis + num_axes + 1 : axis;
-  CHECK_GE(axis, 0);
-  CHECK_LE(axis, num_axes);
+  CHECK_OR_RETURN(axis >= 0 && axis <= num_axes)
+      << " Dimension out of range, expected to be in range of [" << -(num_axes + 1) << ", "
+      << num_axes << "], but got: " << axis;
   return axis;
 }
 
@@ -33,7 +36,7 @@ int32_t TransformNegativeAxisToPositive(int32_t axis, const int32_t num_axes) {
   const Shape& in_shape = ctx->InputShape("in", 0);
   Shape* out_shape = ctx->OutputShape("out", 0);
   const int32_t axis =
-      TransformNegativeAxisToPositive(ctx->Attr<int32_t>("axis"), in_shape.NumAxes());
+      JUST(TransformNegativeAxisToPositive(ctx->Attr<int32_t>("axis"), in_shape.NumAxes()));
 
   auto dim_vec = in_shape.dim_vec();
   dim_vec.insert(dim_vec.begin() + axis, 1);
@@ -47,8 +50,8 @@ int32_t TransformNegativeAxisToPositive(int32_t axis, const int32_t num_axes) {
 
 /* static */ Maybe<void> ExpandDimsOp::GetSbp(user_op::SbpContext* ctx) {
   const user_op::TensorDesc& in_tensor = ctx->LogicalTensorDesc4InputArgNameAndIndex("in", 0);
-  const int32_t axis =
-      TransformNegativeAxisToPositive(ctx->Attr<int32_t>("axis"), in_tensor.shape().NumAxes());
+  const int32_t axis = JUST(
+      TransformNegativeAxisToPositive(ctx->Attr<int32_t>("axis"), in_tensor.shape().NumAxes()));
 
   auto dim_vec = in_tensor.shape().dim_vec();
   FOR_RANGE(int32_t, in_axis, 0, dim_vec.size()) {
