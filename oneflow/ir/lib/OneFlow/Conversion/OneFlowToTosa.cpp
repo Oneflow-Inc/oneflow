9ba8aaee8f5cfc418abfdbf743cfa533c5538b1d
howin98 <1171760467@qq.com>
2022-04-30 16:12:04 +0800

    fix

--- oneflow/ir/lib/OneFlow/Conversion/OneFlowToTosa.cpp
+++ oneflow/ir/lib/OneFlow/Conversion/OneFlowToTosa.cpp
@@ -103,7 +103,6 @@ struct ReluOpLowering final : public OpConversionPattern<ReluOp> {
   }
 };
 
-    // %y_267, %indice = "oneflow.maxpool_2d"(%1) {ceil_mode = false, data_format = "channels_first", device_name = ["@0:0"], device_tag = "cpu", dilation = [1 : si32, 1 : si32], hierarchy = [1], kernel_size = [3 : si32, 3 : si32], op_name = "maxpool_2d-8", output_lbns = ["maxpool_2d-8/y_0", "maxpool_2d-8/indice_0"], padding = [1 : si32, 1 : si32], return_indices = false, scope_symbol_id = 4611686018427416575 : i64, stride = [2 : si32, 2 : si32]} : (tensor<16x64x112x112xf32>) -> (tensor<16x64x56x56xf32>, tensor<16x64x56x56xi64>)
 
   //  ::mlir::Type output, ::mlir::Value input, ::mlir::ArrayAttr kernel, ::mlir::ArrayAttr stride, ::mlir::ArrayAttr pad);
   // TODO
