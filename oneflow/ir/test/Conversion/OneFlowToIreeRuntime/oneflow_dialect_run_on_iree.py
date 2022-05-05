6d4e941b7a3f0f5b239bc2cac75791b1ba37c388
yuhao <1171760467@qq.com>
2022-05-05 20:28:07 +0800

    go through oneflow dialect to tosa in python

--- oneflow/ir/test/Conversion/OneFlowToIreeRuntime/oneflow_dialect_run_on_iree.py
+++ oneflow/ir/test/Conversion/OneFlowToIreeRuntime/oneflow_dialect_run_on_iree.py
@@ -3,7 +3,7 @@ import numpy as np
 from iree import runtime as ireert
 from iree.compiler import compile_str
 
-SIMPLE_RELU_ASM = '''
+SIMPLE_RELU_ONEFLOW_DIALECT = '''
 module  {
   func @main(%arg0: tensor<2x2xf32>) -> tensor<2x2xf32> {
     %1 = "oneflow.relu"(%arg0) {device_name = ["@0:0"], device_tag = "cpu", hierarchy = [1], op_name = "relu-7", output_lbns = ["relu-7/y_0"], scope_symbol_id = 4611686018427416575 : i64} : (tensor<2x2xf32>) -> tensor<2x2xf32>
@@ -12,7 +12,19 @@ module  {
 }
 '''
 
-compiled_flatbuffer = compile_str(SIMPLE_RELU_ASM, target_backends=['dylib-llvm-aot'], input_type='tosa')
+import subprocess
+run_args = {}
+run_args["stdout"] = subprocess.PIPE
+run_args["stderr"] = subprocess.PIPE
+cmd = ['oneflow-opt', '-lower-oneflow-to-tosa']
+process = subprocess.run(cmd, input=SIMPLE_RELU_ONEFLOW_DIALECT.encode(), **run_args)
+if process.returncode != 0:
+  print('oneflow-opt failed')
+  exit(1)
+SIMPLE_RELU_TOSA_DIALECT = process.stdout
+
+
+compiled_flatbuffer = compile_str(SIMPLE_RELU_TOSA_DIALECT, target_backends=['dylib-llvm-aot'], input_type='tosa')
 vm_module = ireert.VmModule.from_flatbuffer(compiled_flatbuffer)
 
 config = ireert.Config('dylib')
