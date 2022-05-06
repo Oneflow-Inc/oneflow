c92476b2df38418cb631d24cb754cd591fd2a7b5
mosout <mosout@qq.com>
2022-05-06 17:43:49 +0800

    fix consistent bug

--- oneflow/core/functional/tensor_index.h
+++ oneflow/core/functional/tensor_index.h
@@ -115,6 +115,9 @@ Maybe<std::vector<detail::Slice>> RemoveExpandDimSlice(
 Maybe<Tensor> ApplyAdvancedIndexing(const std::shared_ptr<Tensor>& input,
                                     const TensorTuple& indices);
 
+Maybe<void> UnifyLocalTensorAndIndicesOnDevice(const std::shared_ptr<Tensor>& x,
+                                          TensorTuple& tensor_indices);
+
 }  // namespace functional
 }  // namespace one
 }  // namespace oneflow
