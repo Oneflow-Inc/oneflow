95f1f03026b2fdc70832a546802a23eb22e7a32d
zhongshsh <zhongshsh5@mail2.sysu.edu.cn>
2022-05-09 17:36:10 +0800

    rm useless import in cpp

--- oneflow/user/kernels/upsample_bilinear_2d_kernel.cpp
+++ oneflow/user/kernels/upsample_bilinear_2d_kernel.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <iterator>
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
 #include "oneflow/core/common/nd_index_offset_helper.h"
