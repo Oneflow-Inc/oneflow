a19c3a639e2d2a3069575c45431388922a7d0857
luyang <flowingsun007@163.com>
2022-05-12 16:23:58 +0800

    fix consistent case

--- oneflow/api/python/functional/tensor_api.cpp
+++ oneflow/api/python/functional/tensor_api.cpp
@@ -120,9 +120,11 @@ class TensorWithOtherCtorFunctor {
   Maybe<Tensor> operator()(const std::shared_ptr<Tensor>& other) const {
     // NOTE(chengcheng): flow.Tensor or flow.tensor ONLY created by EagerTensor now.
     LazyMode::Guard lazy_mode_disabled_guard(/*is_enabled*/ false);
-    return MakeTensorFromOtherTensor(
-        other,
-        /*pin_memory=*/JUST(JUST(other->AsMirroredTensor())->eager_blob_object())->pin_memory());
+    bool pin_memory = false;
+    if(other->is_local()){
+      pin_memory = JUST(JUST(other->AsMirroredTensor())->eager_blob_object())->pin_memory();
+    }
+    return MakeTensorFromOtherTensor(other, pin_memory);
   }
 };
 
