1a292e20d300190d199aa16268180d878f2f6ea9
MARD1NO <359521840@qq.com>
2022-05-13 11:02:08 +0800

    add pad2d test

---
+++ oneflow/core/ep/test/primitive/constant_pad_test.cpp
@@ -0,0 +1,121 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include <gtest/gtest.h>
+#include "oneflow/core/ep/test/primitive/primitive_test.h"
+#include "oneflow/core/ep/include/primitive/memcpy.h"
+#include "oneflow/core/ep/include/primitive/constant_pad.h"
+#include <Eigen/Core>
+#include <unsupported/Eigen/CXX11/Tensor>
+namespace oneflow {
+
+namespace ep {
+
+namespace primitive {
+
+namespace test {
+
+template<typename T, DataType dtype, int NumDims>
+void TestConstantPad(DeviceManagerRegistry* registry, const std::set<DeviceType>& device_types,
+                     const int dims[NumDims], const std::vector<int64_t> padding_before,
+                     const std::vector<int64_t> padding_after) {
+  using EigenVec = Eigen::Matrix<T, 1, Eigen::Dynamic>;
+  int elem_cnt = 1;
+  for (int i = 0; i < NumDims; i++) {
+    elem_cnt *= (dims[i] + padding_before[i] + padding_after[i]);
+  }
+  const int matrix_size = elem_cnt * sizeof(T);
+
+  for (const auto& device_type : device_types) {
+    Eigen::Tensor<T, NumDims, Eigen::RowMajor> mat(dims[0], dims[1]);
+
+    mat.setRandom();
+    auto device = registry->GetDevice(device_type, 0);
+
+    ep::test::PinnedMemoryGuard host_src(device.get(), matrix_size);
+    ep::test::PinnedMemoryGuard host_dst(device.get(), matrix_size);
+    ep::test::DeviceMemoryGuard device_src(device.get(), matrix_size);
+    ep::test::DeviceMemoryGuard device_dst(device.get(), matrix_size);
+
+    ep::test::StreamGuard stream(device.get());
+    std::unique_ptr<ConstantPad> constant_pad =
+        NewPrimitive<ConstantPadFactory>(device_type, dtype);
+    ASSERT_TRUE(constant_pad.operator bool());
+    std::unique_ptr<Memcpy> h2d = NewPrimitive<MemcpyFactory>(device_type, MemcpyKind::kHtoD);
+    std::unique_ptr<Memcpy> d2h = NewPrimitive<MemcpyFactory>(device_type, MemcpyKind::kDtoH);
+    ASSERT_TRUE(d2h.operator bool());
+    ASSERT_TRUE(h2d.operator bool());
+    T* mat_data = mat.data();
+    std::memcpy(host_src.ptr(), mat_data, matrix_size);
+    h2d->Launch(stream.stream(), device_src.ptr<T>(), host_src.ptr<T>(), matrix_size);
+    const int64_t src_dims[NumDims] = {dims[0], dims[1]};
+    constant_pad->Launch(stream.stream(), /*num_dims=*/NumDims, src_dims, device_src.ptr<T>(),
+                         padding_before.data(), padding_after.data(), Scalar(0),
+                         device_dst.ptr<T>());
+    d2h->Launch(stream.stream(), host_dst.ptr<T>(), device_dst.ptr<T>(), matrix_size);
+    CHECK_JUST(stream.stream()->Sync());
+
+    Eigen::array<std::pair<int, int>, NumDims> paddings;
+    for (int i = 0; i < NumDims; i++) {
+      paddings[i] = std::make_pair(padding_before[i], padding_after[i]);
+    }
+
+    Eigen::Tensor<T, NumDims, Eigen::RowMajor> mat_padded = mat.pad(paddings);
+
+    auto eigen_padded_res =
+        Eigen::Map<EigenVec, Eigen::Unaligned>(reinterpret_cast<T*>(mat_padded.data()), elem_cnt);
+    auto constant_pad_primitive_res =
+        Eigen::Map<EigenVec, Eigen::Unaligned>(host_dst.ptr<T>(), elem_cnt);
+    ASSERT_TRUE(eigen_padded_res.template isApprox(constant_pad_primitive_res));
+  }
+}
+
+TEST_F(PrimitiveTest, TestConstantPad2d) {
+  const int32_t dims1[2] = {7, 9};
+  const int32_t dims2[2] = {10, 3};
+  const int32_t dims3[2] = {31, 4};
+  const int32_t dims4[2] = {6, 8};
+  const int32_t dims5[2] = {4, 11};
+
+  const std::vector<int64_t> padding_before1 = {1, 1};
+  const std::vector<int64_t> padding_after1 = {1, 1};
+  const std::vector<int64_t> padding_before2 = {1, 2};
+  const std::vector<int64_t> padding_after2 = {2, 1};
+  const std::vector<int64_t> padding_before3 = {2, 1};
+  const std::vector<int64_t> padding_after3 = {1, 2};
+  const std::vector<int64_t> padding_before4 = {3, 1};
+  const std::vector<int64_t> padding_after4 = {1, 3};
+  const std::vector<int64_t> padding_before5 = {1, 3};
+  const std::vector<int64_t> padding_after5 = {3, 1};
+
+  TestConstantPad<float, DataType::kFloat, 2>(&device_manager_registry_, available_device_types_,
+                                              dims1, padding_before1, padding_after1);
+  TestConstantPad<double, DataType::kDouble, 2>(&device_manager_registry_, available_device_types_,
+                                                dims2, padding_before2, padding_after2);
+  TestConstantPad<int32_t, DataType::kInt32, 2>(&device_manager_registry_, available_device_types_,
+                                                dims3, padding_before3, padding_after3);
+  TestConstantPad<int64_t, DataType::kInt64, 2>(&device_manager_registry_, available_device_types_,
+                                                dims4, padding_before4, padding_after4);
+  TestConstantPad<Eigen::half, DataType::kFloat16, 2>(
+      &device_manager_registry_, available_device_types_, dims5, padding_before5, padding_after5);
+}
+
+}  // namespace test
+
+}  // namespace primitive
+
+}  // namespace ep
+
+}  // namespace oneflow
