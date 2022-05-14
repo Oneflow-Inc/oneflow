bc48acf6e00394e718180c125c49328ddbbddbc7
luyang <flowingsun007@163.com>
2022-05-14 22:09:51 +0000

    refine

--- python/oneflow/utils/data/_utils/pin_memory.py
+++ python/oneflow/utils/data/_utils/pin_memory.py
@@ -13,21 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-"""
-Copyright 2020 The OneFlow Authors. All rights reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-lim
-"""
 r""""Contains definitions of the methods used by the _BaseDataLoaderIter to put
 fetched tensors into pinned memory.
 
@@ -51,7 +37,6 @@ def _pin_memory_loop(in_queue, out_queue, device_id, done_event):
     # consuming all CPU cores.
     flow.set_num_threads(1)
     
-    print("device_id >>>>>>>>> ", device_id)
     # TODO: support flow.cuda.set_device
     # flow.cuda.set_device(device_id)
 
