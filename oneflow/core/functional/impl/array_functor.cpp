Oneflow-Inc/oneflow

commit 84f8e8073d2768487cd3fe4dae95f8da589f4b1e

optimize operations about dataloader (#8066)
Signed-off-by: daquexian <daquexian566@gmail.com>

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

oneflow/core/functional/impl/array_functor.cpp
@@ -1123,8 +1123,6 @@ class ReshapeFunctor {
   }
   Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x, const Shape& shape) const {
     Shape infered_shape = *JUST(InferShape(x, shape));
-    MutableAttrMap attrs;
-    JUST(attrs.SetAttr<Shape>("shape", infered_shape));
 
     if (view::IsViewApplicable(x)) {
       Optional<Stride> infered_stride =
@@ -1133,6 +1131,8 @@ class ReshapeFunctor {
         return view::Reshape(x, infered_shape, *JUST(infered_stride));
       }
     }
+    MutableAttrMap attrs;
+    JUST(attrs.SetAttr<Shape>("shape", infered_shape));
     return OpInterpUtil::Dispatch<Tensor>(*op_, {x}, attrs);
   }
 
@@ -2070,7 +2070,11 @@ class TensorSetItemFunctor {
         value_tensor = JUST(Reshape(value_tensor, slice_shape));
       }
       if (x->is_local()) {
-        JUST(SliceUpdate(x, value_tensor, start, end, step, /*inplace=*/true));
+        if (x->requires_grad() && autograd::GradMode::is_enabled()) {
+          JUST(SliceUpdate(x, value_tensor, start, end, step, /*inplace=*/true));
+        } else {
+          JUST(LogicalSliceAssign(x, value_tensor, start, end, step));
+        }
       } else {
         if (x->requires_grad() && autograd::GradMode::is_enabled()) {
           return Error::RuntimeError() << "Backward is not support for consistent tensor setitem,"

oneflow/api/python/utils/tensor_utils.cpp
@@ -144,14 +144,28 @@ Maybe<Tensor> MakeLocalTensorFromData(PyObject* data, const Optional<Symbol<DTyp
                                       const Optional<Symbol<Device>>& device,
                                       const bool requires_grad, const bool pin_memory) {
   PyObject* array = NULL;
-  if (PyArray_Check(data)) {
-    // Only NPY_CORDER is supported, and returns a new C-style contiguous array.
-    array = PyArray_NewCopy((PyArrayObject*)data, NPY_CORDER);
-  } else {
-    // NPY_ARRAY_DEFAULT is NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_BEHAVED, so the
-    // array with NPY_ARRAY_DEFAULT flag is C-style contiguous.
-    array = PyArray_FromAny(data, nullptr, 0, 0, NPY_ARRAY_DEFAULT | NPY_ARRAY_ENSURECOPY, nullptr);
-    if (!array) { return Error::RuntimeError() << "Can not convert input data to a numpy array."; }
+  PyArray_Descr* np_dtype =
+      dtype.has_value()
+          ? PyArray_DescrFromType(JUST(numpy::OFDataTypeToNumpyType(JUST(dtype)->data_type())))
+          : nullptr;
+  // PyArray_FromAny steals a reference to np_dtype object, so no need to decref it.
+  // NPY_ARRAY_DEFAULT is NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_BEHAVED, so the
+  // array with NPY_ARRAY_DEFAULT flag is C-style contiguous.
+  // NPY_ARRAY_FORCECAST is needed otherwise there will a segfault.
+  array = PyArray_FromAny(data, np_dtype, 0, 0,
+                          NPY_ARRAY_DEFAULT | NPY_ARRAY_ENSURECOPY | NPY_ARRAY_FORCECAST, nullptr);
+  if (!array) {
+    return Error::RuntimeError() << "Can not convert input data to a new numpy array.";
+  }
+  // flow.tensor([1., 2.]).dtype should be flow.float32 rather than flow.float64
+  if (!PyArray_Check(data)) {
+    int np_array_type = PyArray_TYPE(reinterpret_cast<PyArrayObject*>(array));
+    // Cast to float if data is double sequence, rather than numpy array.
+    if (np_array_type == NPY_DOUBLE && np_dtype == nullptr) {
+      PyObject* fp32_array = PyArray_Cast(reinterpret_cast<PyArrayObject*>(array), NPY_FLOAT);
+      Py_DECREF(array);
+      array = fp32_array;
+    }
   }
   auto* np_arr = reinterpret_cast<PyArrayObject*>(array);
   const npy_intp* dims_ptr = PyArray_SHAPE(np_arr);
@@ -169,14 +183,6 @@ Maybe<Tensor> MakeLocalTensorFromData(PyObject* data, const Optional<Symbol<DTyp
   JUST(SwitchCopyMirroredTensorFromUntypedArray(SwitchCase(data_type), tensor, array));
 
   Py_DECREF(array);
-  // Cast to float if data is double sequence, rather than numpy array.
-  Symbol<DType> dtype_;
-  if (dtype) {
-    dtype_ = JUST(dtype);
-  } else if (!dtype && data_type == DataType::kDouble && !PyArray_Check(data)) {
-    dtype_ = DType::Float();
-  }
-  if (dtype_) { tensor = JUST(functional::Cast(tensor, dtype_, pin_memory)); }
   JUST(tensor->set_requires_grad(requires_grad));
   return tensor;
 }

python/oneflow/__init__.py
@@ -27,7 +27,6 @@ device = oneflow._oneflow_internal.device
 placement = oneflow._oneflow_internal.placement
 locals()["dtype"] = oneflow._oneflow_internal.dtype
 locals()["bool"] = oneflow._oneflow_internal.bool
-locals()["char"] = oneflow._oneflow_internal.char
 locals()["float16"] = oneflow._oneflow_internal.float16
 locals()["half"] = oneflow._oneflow_internal.float16
 locals()["float32"] = oneflow._oneflow_internal.float32

python/oneflow/framework/dtype.py
@@ -21,7 +21,6 @@ import oneflow.core.common.data_type_pb2 as data_type_pb2
 
 _dtypes = [
     oneflow.bool,
-    oneflow.char,
     oneflow.float,
     oneflow.float32,
     oneflow.double,
@@ -47,7 +46,6 @@ def convert_proto_dtype_to_oneflow_dtype(proto_dtype):
 
 _ONEFLOW_DTYPE_TO_NUMPY_DTYPE = {
     oneflow.bool: np.bool,
-    oneflow.char: np.byte,
     oneflow.float: np.float32,
     oneflow.float16: np.float16,
     oneflow.float32: np.float32,

python/oneflow/nn/modules/as_tensor.py
@@ -16,17 +16,6 @@ limitations under the License.
 import numpy as np
 import oneflow as flow
 
-numpy_dtype_to_oneflow_dtype_dict = {
-    np.int32: flow.int32,
-    np.int64: flow.int64,
-    np.int8: flow.int8,
-    np.uint8: flow.uint8,
-    np.bool: flow.bool,
-    np.float64: flow.float64,
-    np.float32: flow.float32,
-    np.float16: flow.float16,
-}
-
 
 def as_tensor(data, dtype=None, device=None):
     if flow.is_tensor(data):
@@ -45,10 +34,9 @@ def as_tensor(data, dtype=None, device=None):
             else:
                 data = flow.tensor(data, device=device)
         else:
-            if data.dtype in numpy_dtype_to_oneflow_dtype_dict:
-                data_infer_flow_type = numpy_dtype_to_oneflow_dtype_dict[data.dtype]
-            else:
-                raise TypeError("numpy-ndarray holds elements of unsupported datatype")
+            data_infer_flow_type = flow.framework.dtype.convert_numpy_dtype_to_oneflow_dtype(
+                data.dtype
+            )
             if data_infer_flow_type is dtype:
                 if (device is None) or (device.type == "cpu"):
                     data = flow.from_numpy(data)
@@ -60,8 +48,6 @@ def as_tensor(data, dtype=None, device=None):
                 else:
                     data = flow.tensor(data, dtype=dtype, device=device)
     else:
-        # handle tuple, list, scalar
-        data = np.array(data)
         # not shared memory in this case
         data = flow.tensor(data)
         if device is not None:

python/oneflow/test/expensive/test_util.py
@@ -68,7 +68,6 @@ type_name_to_flow_type = {
     "int8": flow.int8,
     "int32": flow.int32,
     "int64": flow.int64,
-    "char": flow.char,
     "uint8": flow.uint8,
 }
 type_name_to_np_type = {
@@ -78,7 +77,6 @@ type_name_to_np_type = {
     "int8": np.int8,
     "int32": np.int32,
     "int64": np.int64,
-    "char": np.byte,
     "uint8": np.uint8,
 }
 

python/oneflow/test/graph/test_util.py
@@ -68,7 +68,6 @@ type_name_to_flow_type = {
     "int8": flow.int8,
     "int32": flow.int32,
     "int64": flow.int64,
-    "char": flow.char,
     "uint8": flow.uint8,
 }
 type_name_to_np_type = {
@@ -78,7 +77,6 @@ type_name_to_np_type = {
     "int8": np.int8,
     "int32": np.int32,
     "int64": np.int64,
-    "char": np.byte,
     "uint8": np.uint8,
 }
 

python/oneflow/test/modules/test_as_tensor.py
@@ -145,6 +145,14 @@ class TestAsTensor(flow.unittest.TestCase):
                         # Ignore cast or kernel mismatch error in test example
                         pass
 
+    def test_numpy_dtype_bug(test_case):
+        test_case.assertEqual(flow.as_tensor([1.0]).dtype, flow.float32)
+        x = np.random.randn(10)
+        y1 = flow.as_tensor(x, dtype=flow.int64)
+        y2 = flow.as_tensor(x, dtype=flow.float64)
+        test_case.assertEqual(y1.dtype, flow.int64)
+        test_case.assertEqual(y2.dtype, flow.float64)
+
 
 if __name__ == "__main__":
     unittest.main()

commit fa5ac8fa8bd126d22694fc473ec7119dcf7ae8ce

fix slice bug introduced in #8066
Signed-off-by: daquexian <daquexian566@gmail.com>

oneflow/core/functional/impl/array_functor.cpp
@@ -2076,14 +2076,16 @@ class TensorSetItemFunctor {
       if (slice_shape != *(value_tensor->shape())) {
         value_tensor = JUST(Reshape(value_tensor, slice_shape));
       }
+      bool requires_grad =
+          (x->requires_grad() || value_tensor->requires_grad()) && autograd::GradMode::is_enabled();
       if (x->is_local()) {
-        if (x->requires_grad() && autograd::GradMode::is_enabled()) {
+        if (requires_grad) {
           JUST(SliceUpdate(x, value_tensor, start, end, step, /*inplace=*/true));
         } else {
           JUST(LogicalSliceAssign(x, value_tensor, start, end, step));
         }
       } else {
-        if (x->requires_grad() && autograd::GradMode::is_enabled()) {
+        if (requires_grad) {
           return Error::RuntimeError() << "Backward is not support for consistent tensor setitem,"
                                           "please use oneflow.no_grad() to disable autograd "
                                           "currently. We will fix this problem soon.";