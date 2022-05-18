Oneflow-Inc/oneflow

commit 84f8e8073d2768487cd3fe4dae95f8da589f4b1e

optimize operations about dataloader (#8066)
Signed-off-by: daquexian <daquexian566@gmail.com>

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

oneflow/core/functional/impl/array_functor.cpp
@@ -1123,8 +1123,6 @@ class ReshapeFunctor {
   }
   Maybe<Tensor> operator()(const std::shared_ptr<one::Tensor>& x, const Shape& shape) const {
     Shape infered_shape = *JUST(InferShape(x, shape));
-    MutableAttrMap attrs;
-    JUST(attrs.SetAttr<Shape>("shape", infered_shape));
 
     if (view::IsViewApplicable(x)) {
       Optional<Stride> infered_stride =
@@ -1133,6 +1131,8 @@ class ReshapeFunctor {
         return view::Reshape(x, infered_shape, *JUST(infered_stride));
       }
     }
+    MutableAttrMap attrs;
+    JUST(attrs.SetAttr<Shape>("shape", infered_shape));
     return OpInterpUtil::Dispatch<Tensor>(*op_, {x}, attrs);
   }
 
@@ -2070,7 +2070,11 @@ class TensorSetItemFunctor {
         value_tensor = JUST(Reshape(value_tensor, slice_shape));
       }
       if (x->is_local()) {
-        JUST(SliceUpdate(x, value_tensor, start, end, step, /*inplace=*/true));
+        if (x->requires_grad() && autograd::GradMode::is_enabled()) {
+          JUST(SliceUpdate(x, value_tensor, start, end, step, /*inplace=*/true));
+        } else {
+          JUST(LogicalSliceAssign(x, value_tensor, start, end, step));
+        }
       } else {
         if (x->requires_grad() && autograd::GradMode::is_enabled()) {
           return Error::RuntimeError() << "Backward is not support for consistent tensor setitem,"

python/oneflow/__init__.py
@@ -27,7 +27,6 @@ device = oneflow._oneflow_internal.device
 placement = oneflow._oneflow_internal.placement
 locals()["dtype"] = oneflow._oneflow_internal.dtype
 locals()["bool"] = oneflow._oneflow_internal.bool
-locals()["char"] = oneflow._oneflow_internal.char
 locals()["float16"] = oneflow._oneflow_internal.float16
 locals()["half"] = oneflow._oneflow_internal.float16
 locals()["float32"] = oneflow._oneflow_internal.float32

oneflow/api/python/utils/tensor_utils.cpp
@@ -144,14 +144,28 @@ Maybe<Tensor> MakeLocalTensorFromData(PyObject* data, const Optional<Symbol<DTyp
                                       const Optional<Symbol<Device>>& device,
                                       const bool requires_grad, const bool pin_memory) {
   PyObject* array = NULL;
-  if (PyArray_Check(data)) {
-    // Only NPY_CORDER is supported, and returns a new C-style contiguous array.
-    array = PyArray_NewCopy((PyArrayObject*)data, NPY_CORDER);
-  } else {
-    // NPY_ARRAY_DEFAULT is NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_BEHAVED, so the
-    // array with NPY_ARRAY_DEFAULT flag is C-style contiguous.
-    array = PyArray_FromAny(data, nullptr, 0, 0, NPY_ARRAY_DEFAULT | NPY_ARRAY_ENSURECOPY, nullptr);
-    if (!array) { return Error::RuntimeError() << "Can not convert input data to a numpy array."; }
+  PyArray_Descr* np_dtype =
+      dtype.has_value()
+          ? PyArray_DescrFromType(JUST(numpy::OFDataTypeToNumpyType(JUST(dtype)->data_type())))
+          : nullptr;
+  // PyArray_FromAny steals a reference to np_dtype object, so no need to decref it.
+  // NPY_ARRAY_DEFAULT is NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_BEHAVED, so the
+  // array with NPY_ARRAY_DEFAULT flag is C-style contiguous.
+  // NPY_ARRAY_FORCECAST is needed otherwise there will a segfault.
+  array = PyArray_FromAny(data, np_dtype, 0, 0,
+                          NPY_ARRAY_DEFAULT | NPY_ARRAY_ENSURECOPY | NPY_ARRAY_FORCECAST, nullptr);
+  if (!array) {
+    return Error::RuntimeError() << "Can not convert input data to a new numpy array.";
+  }
+  // flow.tensor([1., 2.]).dtype should be flow.float32 rather than flow.float64
+  if (!PyArray_Check(data)) {
+    int np_array_type = PyArray_TYPE(reinterpret_cast<PyArrayObject*>(array));
+    // Cast to float if data is double sequence, rather than numpy array.
+    if (np_array_type == NPY_DOUBLE && np_dtype == nullptr) {
+      PyObject* fp32_array = PyArray_Cast(reinterpret_cast<PyArrayObject*>(array), NPY_FLOAT);
+      Py_DECREF(array);
+      array = fp32_array;
+    }
   }
   auto* np_arr = reinterpret_cast<PyArrayObject*>(array);
   const npy_intp* dims_ptr = PyArray_SHAPE(np_arr);
@@ -169,14 +183,6 @@ Maybe<Tensor> MakeLocalTensorFromData(PyObject* data, const Optional<Symbol<DTyp
   JUST(SwitchCopyMirroredTensorFromUntypedArray(SwitchCase(data_type), tensor, array));
 
   Py_DECREF(array);
-  // Cast to float if data is double sequence, rather than numpy array.
-  Symbol<DType> dtype_;
-  if (dtype) {
-    dtype_ = JUST(dtype);
-  } else if (!dtype && data_type == DataType::kDouble && !PyArray_Check(data)) {
-    dtype_ = DType::Float();
-  }
-  if (dtype_) { tensor = JUST(functional::Cast(tensor, dtype_, pin_memory)); }
   JUST(tensor->set_requires_grad(requires_grad));
   return tensor;
 }

commit 65eab5148580b1a93640208895826aaa20a1682f

auto profiler (#8207)
* implement auto profiler

Signed-off-by: daquexian <daquexian566@gmail.com>

* update

Signed-off-by: daquexian <daquexian566@gmail.com>

* update .gitignore

Signed-off-by: daquexian <daquexian566@gmail.com>

* flush after every write, and other changes

Signed-off-by: daquexian <daquexian566@gmail.com>

* All Time -> Total Time

Signed-off-by: daquexian <daquexian566@gmail.com>

* fix bug about what is 'kernel time'

Signed-off-by: daquexian <daquexian566@gmail.com>

* auto profiler supports gpu, and refactor

Signed-off-by: daquexian <daquexian566@gmail.com>

* add env var to control whether print prof result

Signed-off-by: daquexian <daquexian566@gmail.com>

* refine

Signed-off-by: daquexian <daquexian566@gmail.com>

* refine

Signed-off-by: daquexian <daquexian566@gmail.com>

* oneflow -> OneFlow, pytorch -> PyTorch

Signed-off-by: daquexian <daquexian566@gmail.com>

* address reviews, fix some profiler bugs

Signed-off-by: daquexian <daquexian566@gmail.com>

* refine

Signed-off-by: daquexian <daquexian566@gmail.com>

* auto format by CI

* auto format by CI

* fix slice bug introduced in #8066

Signed-off-by: daquexian <daquexian566@gmail.com>

* move profile.py to oneflow.autoprof

Signed-off-by: daquexian <daquexian566@gmail.com>

* auto format by CI

Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>
Co-authored-by: Shenghang Tsai <jackalcooper@gmail.com>

.gitignore
@@ -44,3 +44,5 @@ unittest-log-*
 /target
 saved_model
 /devcontainer-cache
+
+op_prof.csv

oneflow/core/functional/impl/array_functor.cpp
@@ -2042,14 +2042,16 @@ class TensorSetItemFunctor {
       if (slice_shape != *(value_tensor->shape())) {
         value_tensor = JUST(Reshape(value_tensor, slice_shape));
       }
+      bool requires_grad =
+          (x->requires_grad() || value_tensor->requires_grad()) && autograd::GradMode::is_enabled();
       if (x->is_local()) {
-        if (x->requires_grad() && autograd::GradMode::is_enabled()) {
+        if (requires_grad) {
           JUST(SliceUpdate(x, value_tensor, start, end, step, /*inplace=*/true));
         } else {
           JUST(LogicalSliceAssign(x, value_tensor, start, end, step));
         }
       } else {
-        if (x->requires_grad() && autograd::GradMode::is_enabled()) {
+        if (requires_grad) {
           return Error::RuntimeError() << "Backward is not support for consistent tensor setitem,"
                                           "please use oneflow.no_grad() to disable autograd "
                                           "currently. We will fix this problem soon.";

oneflow/core/profiler/collection.cpp
@@ -172,7 +172,7 @@ Maybe<EventRecorder> EventRecorder::CreateKernelEventRecorder(
   if (pmgr) {
     if ((pmgr->use_cpu_ && (!cuda_stream)) || (pmgr->use_cuda_ && cuda_stream)) {
       auto event = KernelEvent::Create(name, pmgr->record_shapes_ ? shape_getter : nullptr);
-      if (pmgr->use_cuda_) { event->InitCudaEventPair(cuda_stream); }
+      if (pmgr->use_cuda_ && cuda_stream) { event->InitCudaEventPair(cuda_stream); }
       return std::make_shared<EventRecorder>(event);
     }
   }

oneflow/core/profiler/collection.h
@@ -90,6 +90,8 @@ class CUDAEventPair {
 
   double ElapsedTime() const {
     float elapsed_time_ms = 0;
+    OF_CUDA_CHECK(cudaEventSynchronize(cuda_event_start_));
+    OF_CUDA_CHECK(cudaEventSynchronize(cuda_event_finish_));
     OF_CUDA_CHECK(cudaEventElapsedTime(&elapsed_time_ms, cuda_event_start_, cuda_event_finish_));
     return elapsed_time_ms * 1000.0;  // convert to us
   }

python/oneflow/__init__.py
@@ -412,6 +412,7 @@ import oneflow.framework.docstr as docstr
 import oneflow.cuda
 import oneflow.multiprocessing
 import oneflow.one_embedding
+import oneflow.profiler
 
 if oneflow._oneflow_internal.flags.with_mlir():
     oneflow_internal_path = oneflow._oneflow_internal.__file__

python/oneflow/autoprof/__main__.py
@@ -1,0 +1,161 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import atexit
+import csv
+import unittest
+import os
+
+import oneflow.test_utils.automated_test_util.profiler as auto_profiler
+
+
+def get_sole_value(x):
+    s = set(x)
+    assert len(s) == 1
+    return list(s)[0]
+
+
+def get_pytorch_cpu_kernel_time(prof):
+    assert prof.num > 1
+    cpu_kernel_items = list(filter(lambda x: x.count >= prof.num, prof.key_averages()))
+    assert len(cpu_kernel_items) > 0
+    kernel_cpu_time = (
+        sum(map(lambda x: x.self_cpu_time_total, cpu_kernel_items)) / prof.num
+    )
+    return round(kernel_cpu_time, 1)
+
+
+def get_oneflow_cpu_kernel_time(prof):
+    assert prof.num > 1
+    cpu_kernel_items = list(filter(lambda x: x.count >= prof.num, prof.key_averages()))
+    assert len(cpu_kernel_items) > 0
+    kernel_cpu_time = sum(map(lambda x: x.time_total, cpu_kernel_items)) / prof.num
+    return round(kernel_cpu_time, 1)
+
+
+def get_pytorch_gpu_kernel_time(prof):
+    gpu_kernel_items = list(filter(lambda x: x.count >= prof.num, prof.key_averages()))
+    assert len(gpu_kernel_items) > 0
+    kernel_gpu_time = (
+        sum(map(lambda x: x.self_cuda_time_total, gpu_kernel_items)) / prof.num
+    )
+    return round(kernel_gpu_time, 1)
+
+
+def get_oneflow_gpu_kernel_time(prof):
+    gpu_kernel_items = list(
+        filter(lambda x: x.event_type == 1 and x.on_gpu, prof.key_averages())
+    )
+    assert len(gpu_kernel_items) > 0
+    kernel_gpu_time = sum(map(lambda x: x.time_total, gpu_kernel_items)) / prof.num
+    return round(kernel_gpu_time, 1)
+
+
+def get_pytorch_cpu_end_to_end_time(prof):
+    total = get_sole_value(
+        filter(lambda x: x.key == auto_profiler.END_TO_END, prof.key_averages())
+    )
+    assert total.count == 1
+    return round(total.cpu_time / prof.num, 1)
+
+
+def get_oneflow_cpu_end_to_end_time(prof):
+    total = list(
+        filter(lambda x: x.name == auto_profiler.END_TO_END, prof.key_averages())
+    )[0]
+    assert total.count == 1
+    return round(total.time / prof.num, 1)
+
+
+csv_filename = os.getenv("ONEFLOW_PROFILE_CSV", "op_prof")
+
+if csv_filename[:-4] != ".csv":
+    csv_filename += ".csv"
+
+f = open(csv_filename, "w")
+atexit.register(lambda f: f.close(), f)
+writer = csv.writer(f)
+writer.writerow(
+    [
+        "OP",
+        "Args",
+        "Description",
+        "Library",
+        "Kernel Time (us, GPU)",
+        "Kernel Time (us, CPU N=1)",
+        "Total Time (us, CPU N=1)",
+        "Kernel Time (us, CPU N=8)",
+        "Total Time (us, CPU N=8)",
+        "Kernel Time (us, CPU N=32)",
+        "Total Time (us, CPU N=32)",
+    ]
+)
+
+
+auto_profiler.set_hardware_info_list(
+    [("cuda", None), ("cpu", 1), ("cpu", 8), ("cpu", 32)]
+)
+
+
+def add_row(profs):
+    op_name = get_sole_value([prof.op_name for prof in profs])
+    args_description = get_sole_value([prof.args_description for prof in profs])
+    additional_description = get_sole_value(
+        [prof.additional_description for prof in profs]
+    )
+    writer.writerow(
+        [
+            op_name,
+            args_description,
+            additional_description,
+            "OneFlow",
+            get_oneflow_gpu_kernel_time(profs[0]),
+            get_oneflow_cpu_kernel_time(profs[1]),
+            get_oneflow_cpu_end_to_end_time(profs[1]),
+            get_oneflow_cpu_kernel_time(profs[2]),
+            get_oneflow_cpu_end_to_end_time(profs[2]),
+            get_oneflow_cpu_kernel_time(profs[3]),
+            get_oneflow_cpu_end_to_end_time(profs[3]),
+        ]
+    )
+    writer.writerow(
+        [
+            op_name,
+            args_description,
+            additional_description,
+            "PyTorch",
+            get_pytorch_gpu_kernel_time(profs[4]),
+            get_pytorch_cpu_kernel_time(profs[5]),
+            get_pytorch_cpu_end_to_end_time(profs[5]),
+            get_pytorch_cpu_kernel_time(profs[6]),
+            get_pytorch_cpu_end_to_end_time(profs[6]),
+            get_pytorch_cpu_kernel_time(profs[7]),
+            get_pytorch_cpu_end_to_end_time(profs[7]),
+        ]
+    )
+    f.flush()
+
+
+auto_profiler.set_profiler_hook(add_row)
+
+# Align with https://github.com/python/cpython/blob/3.10/Lib/unittest/__main__.py
+__unittest = True
+
+from unittest.main import main
+
+loader = unittest.TestLoader()
+loader.testMethodPrefix = "profile_"
+
+main(module=None, testLoader=loader)

python/oneflow/profiler/profiler.py
@@ -41,6 +41,10 @@ class profile:
         assert (
             len(self.activities) > 0
         ), "At least one ProfilerActivity must be specified."
+        for item in self.activities:
+            assert (
+                item in supported_activities()
+            ), f"Unsupported ProfilerActivity {item}"
         self.record_shapes = record_shapes
         self.profile_events: Optional[Events] = None
 

python/oneflow/test_utils/automated_test_util/__init__.py
@@ -16,4 +16,5 @@ limitations under the License.
 from .generators import *
 from .torch_flow_dual_object import *
 from .torch_flow_dual_object import torch
+from .profiler import profile
 import os

python/oneflow/test_utils/automated_test_util/profiler.py
@@ -1,0 +1,247 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import functools
+import os
+from typing import Any, Callable, Iterable, List, Optional, Tuple
+
+import torch
+import oneflow as flow
+import oneflow.support.env_var_util
+import oneflow.test_utils.automated_test_util.torch_flow_dual_object as dual_object_module
+
+__all__ = ["profile", "set_profiler_hook", "profile_dual_object"]
+
+
+def compose(*fs):
+    def compose2(f, g):
+        return lambda *a, **kw: f(g(*a, **kw))
+
+    return functools.reduce(compose2, fs)
+
+
+class ProfResult:
+    def __init__(
+        self,
+        prof,
+        num,
+        kind,
+        device,
+        thread_num,
+        op_name,
+        args_description,
+        additional_description=None,
+    ):
+        self.prof = prof
+        self.num = num
+        self.kind = kind
+        self.device = device
+        self.thread_num = thread_num
+        self.op_name = op_name
+        self.args_description = args_description
+        self.additional_description = additional_description
+
+    def __getattr__(self, attr):
+        return getattr(self.prof, attr)
+
+
+WARMUP_NUM = int(os.getenv("ONEFLOW_PROFILE_WARMUP_NUM", 10))
+RUN_NUM = int(os.getenv("ONEFLOW_PROFILE_RUN_NUM", 1000))
+PRINT_PROF_RESULT = flow.support.env_var_util.parse_boolean_form_env(
+    "ONEFLOW_PROFILE_PRINT_RESULT", True
+)
+END_TO_END = "end-to-end"
+
+
+def run_torch(
+    op,
+    args,
+    kwargs,
+    device,
+    num_threads,
+    op_name,
+    args_description,
+    additional_description=None,
+):
+    assert device in ["cpu", "cuda"]
+    if device == "cpu":
+        torch.set_num_threads(num_threads)
+        assert torch.get_num_threads() == num_threads
+        activities = [torch.profiler.ProfilerActivity.CPU]
+    else:
+        activities = [torch.profiler.ProfilerActivity.CUDA]
+
+    def tensor_to_device(x):
+        if isinstance(x, torch.Tensor):
+            return x.to(device)
+        return x
+
+    args = [tensor_to_device(arg) for arg in args]
+    kwargs = {k: tensor_to_device(v) for k, v in kwargs.items()}
+    for _ in range(WARMUP_NUM):
+        op(*args, **kwargs)
+
+    if PRINT_PROF_RESULT:
+        print(
+            f'PyTorch ({f"CPU, num_threads={num_threads}" if device == "cpu" else "GPU"}):'
+        )
+    with torch.profiler.profile(activities=activities) as prof:
+        with torch.profiler.record_function(END_TO_END):
+            for _ in range(RUN_NUM):
+                op(*args, **kwargs)
+
+    if PRINT_PROF_RESULT:
+        print(prof.key_averages().table(row_limit=10))
+    return ProfResult(
+        prof,
+        RUN_NUM,
+        "PyTorch",
+        device,
+        num_threads,
+        op_name,
+        args_description,
+        additional_description,
+    )
+
+
+def run_flow(
+    op,
+    args,
+    kwargs,
+    device,
+    num_threads,
+    op_name,
+    args_description,
+    additional_description=None,
+):
+    assert device in ["cpu", "cuda"]
+    if device == "cpu":
+        # NOTE: there is no flow.get_num_threads()
+        flow.set_num_threads(num_threads)
+        activities = [flow.profiler.ProfilerActivity.CPU]
+    else:
+        activities = [flow.profiler.ProfilerActivity.CUDA]
+
+    def tensor_to_device(x):
+        if isinstance(x, flow.Tensor):
+            return x.to(device)
+        return x
+
+    args = [tensor_to_device(arg) for arg in args]
+    kwargs = {k: tensor_to_device(v) for k, v in kwargs.items()}
+    for _ in range(WARMUP_NUM):
+        op(*args, **kwargs)
+
+    if PRINT_PROF_RESULT:
+        print(
+            f'OneFlow ({f"CPU, num_threads={num_threads}" if device == "cpu" else "GPU"}):'
+        )
+    with flow.profiler.profile(activities=activities) as prof:
+        with flow.profiler.record_function(END_TO_END):
+            for _ in range(RUN_NUM):
+                op(*args, **kwargs)
+
+    if PRINT_PROF_RESULT:
+        print(prof.key_averages())
+    return ProfResult(
+        prof,
+        RUN_NUM,
+        "OneFlow",
+        device,
+        num_threads,
+        op_name,
+        args_description,
+        additional_description,
+    )
+
+
+def profile_dual_object(op):
+    assert isinstance(op, dual_object_module.DualObject)
+    torch_op = op.pytorch
+    flow_op = op.oneflow
+
+    def profiled_op(*args, **kwargs):
+        if "profile_description" in kwargs:
+            additional_description = kwargs["profile_description"]
+            del kwargs["profile_description"]
+        else:
+            additional_description = None
+
+        (
+            torch_args,
+            torch_kwargs,
+            flow_args,
+            flow_kwargs,
+        ) = dual_object_module.get_args(torch_op, *args, **kwargs)
+
+        op_name = dual_object_module.to_string(op)
+        args_description = dual_object_module.to_string(*args, **kwargs)
+
+        result = []
+        for hardware_info in _hardware_info_list:
+            result.append(
+                run_flow(
+                    flow_op,
+                    flow_args,
+                    flow_kwargs,
+                    *hardware_info,
+                    op_name,
+                    args_description,
+                    additional_description,
+                )
+            )
+        for hardware_info in _hardware_info_list:
+            result.append(
+                run_torch(
+                    torch_op,
+                    torch_args,
+                    torch_kwargs,
+                    *hardware_info,
+                    op_name,
+                    args_description,
+                    additional_description,
+                )
+            )
+        return _profiler_hook(result)
+
+    return profiled_op
+
+
+HardwareInfo = Tuple[str, Optional[int]]  # (device_type, num_threads)
+_hardware_info_list: List[HardwareInfo] = [("cpu", 1), ("cuda", None)]
+_profiler_hook: Callable[[List[ProfResult]], Any] = lambda x: x
+
+
+def set_hardware_info_list(hardware_info_list: List[HardwareInfo]) -> None:
+    global _hardware_info_list
+    _hardware_info_list = hardware_info_list
+
+
+def set_profiler_hook(hook: Callable[[List[ProfResult]], Any]) -> None:
+    global _profiler_hook
+    _profiler_hook = hook
+
+
+def profile(op):
+    def deco(f):
+        def new_f(*args, **kwargs):
+            dual_object_module.profiled_method_name.append(op.name)
+            res = f(*args, **kwargs)
+            dual_object_module.profiled_method_name.pop()
+            return res
+
+        return new_f
+
+    return deco

python/oneflow/test_utils/automated_test_util/torch_flow_dual_object.py
@@ -23,6 +23,7 @@ import gc
 
 import numpy as np
 import oneflow as flow
+import oneflow.test_utils.automated_test_util.profiler as auto_profiler
 
 flow.backends.cudnn.deterministic = True
 
@@ -216,6 +217,20 @@ def get_args(callable, *args, **kwargs):
     note_pytorch_kwargs.append(new_pytorch_kwargs)
 
     return (pytorch_args, pytorch_kwargs, oneflow_args, oneflow_kwargs)
+
+
+def to_string(*args, **kwargs) -> str:
+    def _to_string(x):
+        if isinstance(x, DualObject):
+            return x.name
+        return str(x)
+
+    strs = []
+    if len(args) > 0:
+        strs.append(", ".join([_to_string(arg) for arg in args]))
+    if len(kwargs) > 0:
+        strs.append(", ".join([f"{k}={_to_string(v)}" for k, v in kwargs.items()]))
+    return ", ".join(strs)
 
 
 counter = 0
@@ -607,6 +622,9 @@ def get_pytorch_oneflow_tensor_res(
     return pytorch_res, oneflow_res
 
 
+profiled_method_name = []
+
+
 def GetDualObject(name, pytorch, oneflow):
     global counter
     counter += 1
@@ -637,7 +655,17 @@ def GetDualObject(name, pytorch, oneflow):
             def get_dual_method(method_name):
                 if method_name == "__call__":
 
+                    if name in profiled_method_name:
+
+                        def method(self, *args, **kwargs):
+                            return auto_profiler.profile_dual_object(self)(
+                                *args, **kwargs
+                            )
+
+                        return method
+
                     def dual_method(self, *args, **kwargs):
+                        param_str = to_string(*args, **kwargs)
                         (
                             pytorch_args,
                             pytorch_kwargs,
@@ -657,7 +685,9 @@ def GetDualObject(name, pytorch, oneflow):
                             testing_graph,
                             *args,
                         )
-                        return GetDualObject("unused", pytorch_res, oneflow_res)
+                        return GetDualObject(
+                            f"{name}({param_str})", pytorch_res, oneflow_res
+                        )
 
                 else:
 
@@ -1224,5 +1254,5 @@ def choice_tensor(
     return GetDualObject("unused", pytorch_tensor, flow_tensor)
 
 
-torch = GetDualObject("", torch_original, flow)
+torch = GetDualObject("torch", torch_original, flow)
 __all__ = ["autotest", "globaltest", "random_tensor", "choice_tensor"]

python/oneflow/test/modules/test_activation.py
@@ -59,6 +59,11 @@ class TestReLUModule(flow.unittest.TestCase):
         y = m(x)
         return y
 
+    @profile(torch.nn.functional.relu)
+    def profile_relu(test_case):
+        torch.nn.functional.relu(torch.ones(1, 32, 64, 64))
+        torch.nn.functional.relu(torch.ones(16, 32, 64, 64))
+
 
 @flow.unittest.skip_unless_1n1d()
 class TestReLU6Module(flow.unittest.TestCase):
@@ -91,6 +96,11 @@ class TestReLU6Module(flow.unittest.TestCase):
         x = random_tensor(4, 2, 3, 0, 3).to(device)
         y = m(x)
         return y
+
+    @profile(torch.nn.functional.relu6)
+    def profile_relu6(test_case):
+        torch.nn.functional.relu6(torch.ones(1, 32, 64, 64))
+        torch.nn.functional.relu6(torch.ones(16, 32, 64, 64))
 
 
 @flow.unittest.skip_unless_1n1d()
@@ -145,6 +155,11 @@ class TestTanh(flow.unittest.TestCase):
         x = random_tensor(4, 2, 3, 0, 3).to(device)
         y = torch.tanh(x)
         return y
+
+    @profile(torch.tanh)
+    def profile_tanh(test_case):
+        torch.tanh(torch.ones(1, 32, 64, 64))
+        torch.tanh(torch.ones(16, 32, 64, 64))
 
 
 @flow.unittest.skip_unless_1n1d()
@@ -296,6 +311,11 @@ class TestSigmoidModule(flow.unittest.TestCase):
         y = x.sigmoid()
         return y
 
+    @profile(torch.sigmoid)
+    def profile_sigmoid(test_case):
+        torch.sigmoid(torch.ones(1, 32, 64, 64))
+        torch.sigmoid(torch.ones(16, 32, 64, 64))
+
 
 @flow.unittest.skip_unless_1n1d()
 class TestHardsigmoidModule(flow.unittest.TestCase):
@@ -406,6 +426,11 @@ class TestSoftmax(flow.unittest.TestCase):
     @autotest(n=2, check_graph=True)
     def test_softmax_module_with_batch_size_equal_10240(test_case):
         return do_test_softmax(batch_size=10240, log_softmax=False)
+
+    @profile(torch.nn.functional.softmax)
+    def profile_softmax(test_case):
+        torch.nn.functional.softmax(torch.ones(1, 32, 64, 64))
+        torch.nn.functional.softmax(torch.ones(16, 32, 64, 64))
 
 
 @flow.unittest.skip_unless_1n1d()

python/oneflow/test/modules/test_argmax.py
@@ -101,6 +101,11 @@ class TestArgmax(flow.unittest.TestCase):
         y = torch.argmax(x, dim=random(0, ndim).to(int), keepdim=random().to(bool))
         return y
 
+    @profile(torch.argmax)
+    def profile_argmax(test_case):
+        torch.argmax(torch.ones(100000))
+        torch.argmax(torch.ones(1000000))
+
 
 if __name__ == "__main__":
     unittest.main()

python/oneflow/test/modules/test_avgpool.py
@@ -123,6 +123,28 @@ class TestAvgPoolingFunctional(flow.unittest.TestCase):
         )
         return y
 
+    @profile(torch.nn.functional.avg_pool2d)
+    def profile_avgpool2d(test_case):
+        torch.nn.functional.avg_pool2d(
+            torch.ones(1, 32, 64, 64), kernel_size=3, padding=1
+        )
+        torch.nn.functional.avg_pool2d(
+            torch.ones(1, 32, 64, 64), kernel_size=3, stride=2, padding=1
+        )
+        torch.nn.functional.avg_pool2d(
+            torch.ones(16, 32, 64, 64), kernel_size=3, padding=1
+        )
+        torch.nn.functional.avg_pool2d(
+            torch.ones(16, 32, 64, 64), kernel_size=3, stride=2, padding=1
+        )
+        torch.nn.functional.avg_pool2d(
+            torch.ones(16, 32, 64, 64),
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            ceil_mode=True,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()

python/oneflow/test/modules/test_conv2d.py
@@ -1894,6 +1894,16 @@ class TestConv2d(flow.unittest.TestCase):
         y = m(x)
         return y
 
+    @profile(torch.nn.functional.conv2d)
+    def profile_conv2d(test_case):
+        input = torch.ones(8, 16, 3, 3)
+        weight = torch.ones(16, 16, 3, 3)
+        bias = torch.ones(16)
+        torch.nn.functional.conv2d(input, weight, padding=1)
+        torch.nn.functional.conv2d(input, weight, padding=1, stride=2)
+        torch.nn.functional.conv2d(input, weight, bias=bias, padding=1)
+        torch.nn.functional.conv2d(input, weight, bias=bias, padding=1, stride=2)
+
 
 if __name__ == "__main__":
     unittest.main()

python/oneflow/test/modules/test_maxpool.py
@@ -219,6 +219,29 @@ class TestMaxPoolingFunctional(flow.unittest.TestCase):
         else:
             return y
 
+    @profile(torch.nn.functional.max_pool2d)
+    def profile_maxpool2d(test_case):
+        torch.nn.functional.max_pool2d(
+            torch.ones(1, 32, 64, 64), kernel_size=3, padding=1
+        )
+        torch.nn.functional.max_pool2d(
+            torch.ones(1, 32, 64, 64), kernel_size=3, stride=2, padding=1
+        )
+        torch.nn.functional.max_pool2d(
+            torch.ones(16, 32, 64, 64), kernel_size=3, padding=1
+        )
+        torch.nn.functional.max_pool2d(
+            torch.ones(16, 32, 64, 64), kernel_size=3, stride=2, padding=1
+        )
+        torch.nn.functional.max_pool2d(
+            torch.ones(16, 32, 64, 64),
+            kernel_size=3,
+            stride=2,
+            padding=1,
+            ceil_mode=True,
+        )
+        # torch.nn.functional.max_pool2d(torch.ones(16, 32, 64, 64), kernel_size=3, dilation=2, padding=2)
+
 
 if __name__ == "__main__":
     unittest.main()