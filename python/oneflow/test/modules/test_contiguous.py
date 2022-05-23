e64a3812c1a825c1b1f7a4efe80b54bf960d562b
luyang <flowingsun007@163.com>
2022-05-23 14:17:28 +0000

    refine

--- python/oneflow/test/modules/test_contiguous.py
+++ python/oneflow/test/modules/test_contiguous.py
@@ -26,61 +26,61 @@ import oneflow.unittest
 import oneflow as flow
 
 
-# @flow.unittest.skip_unless_1n1d()
-# class TestContiguous(flow.unittest.TestCase):
-    # @autotest(n=10, check_graph=True)
-    # def test_transpose_with_random_data(test_case):
-    #     device = random_device()
-    #     x = random_tensor(ndim=4).to(device)
-    #     y = torch.transpose(x, dim0=random(1, 3).to(int), dim1=random(1, 3).to(int))
-    #     z = y.contiguous()
-    #     return y
+@flow.unittest.skip_unless_1n1d()
+class TestContiguous(flow.unittest.TestCase):
+    @autotest(n=10, check_graph=True)
+    def test_transpose_with_random_data(test_case):
+        device = random_device()
+        x = random_tensor(ndim=4).to(device)
+        y = torch.transpose(x, dim0=random(1, 3).to(int), dim1=random(1, 3).to(int))
+        z = y.contiguous()
+        return y
 
-    # @autotest(n=10, check_graph=True)
-    # def test_permute2d_tensor_with_random_data(test_case):
-    #     device = random_device()
-    #     ndim = 2
-    #     permute_list = [0, 1]
-    #     shuffle(permute_list)
-    #     x = random_tensor(
-    #         ndim=ndim, dim0=random(1, 32).to(int), dim1=random(1, 59).to(int),
-    #     ).to(device)
-    #     y = x.permute(permute_list)
-    #     z = y.contiguous()
-    #     return z
+    @autotest(n=10, check_graph=True)
+    def test_permute2d_tensor_with_random_data(test_case):
+        device = random_device()
+        ndim = 2
+        permute_list = [0, 1]
+        shuffle(permute_list)
+        x = random_tensor(
+            ndim=ndim, dim0=random(1, 32).to(int), dim1=random(1, 59).to(int),
+        ).to(device)
+        y = x.permute(permute_list)
+        z = y.contiguous()
+        return z
 
-    # @autotest(n=10, check_graph=True)
-    # def test_permute3d_tensor_with_random_data(test_case):
-    #     device = random_device()
-    #     ndim = 3
-    #     permute_list = [0, 1, 2]
-    #     shuffle(permute_list)
-    #     x = random_tensor(
-    #         ndim=ndim,
-    #         dim0=random(1, 7).to(int),
-    #         dim1=random(1, 15).to(int),
-    #         dim2=random(1, 9).to(int),
-    #     ).to(device)
-    #     y = x.permute(permute_list)
-    #     z = y.contiguous()
-    #     return z
+    @autotest(n=10, check_graph=True)
+    def test_permute3d_tensor_with_random_data(test_case):
+        device = random_device()
+        ndim = 3
+        permute_list = [0, 1, 2]
+        shuffle(permute_list)
+        x = random_tensor(
+            ndim=ndim,
+            dim0=random(1, 7).to(int),
+            dim1=random(1, 15).to(int),
+            dim2=random(1, 9).to(int),
+        ).to(device)
+        y = x.permute(permute_list)
+        z = y.contiguous()
+        return z
 
-    # @autotest(n=10, check_graph=True)
-    # def test_permute4d_tensor_with_random_data(test_case):
-    #     device = random_device()
-    #     ndim = 4
-    #     permute_list = [0, 1, 2, 3]
-    #     shuffle(permute_list)
-    #     x = random_tensor(
-    #         ndim=ndim,
-    #         dim0=random(1, 7).to(int),
-    #         dim1=random(1, 15).to(int),
-    #         dim2=random(1, 9).to(int),
-    #         dim3=random(1, 19).to(int),
-    #     ).to(device)
-    #     y = x.permute(permute_list)
-    #     z = y.contiguous()
-    #     return z
+    @autotest(n=10, check_graph=True)
+    def test_permute4d_tensor_with_random_data(test_case):
+        device = random_device()
+        ndim = 4
+        permute_list = [0, 1, 2, 3]
+        shuffle(permute_list)
+        x = random_tensor(
+            ndim=ndim,
+            dim0=random(1, 7).to(int),
+            dim1=random(1, 15).to(int),
+            dim2=random(1, 9).to(int),
+            dim3=random(1, 19).to(int),
+        ).to(device)
+        y = x.permute(permute_list)
+        z = y.contiguous()
+        return z
 
 
 def _tets_inplace_contiguous(test_case, device):
