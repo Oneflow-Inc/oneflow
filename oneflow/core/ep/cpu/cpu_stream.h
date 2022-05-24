19ac5830e53973df08806a529268c9686a3cb004
mosout <mosout@qq.com>
2022-05-24 19:40:55 +0800

    clean

--- oneflow/core/ep/cpu/cpu_stream.h
+++ oneflow/core/ep/cpu/cpu_stream.h
@@ -16,8 +16,6 @@ limitations under the License.
 #ifndef ONEFLOW_CORE_EP_CPU_CPU_STREAM_H_
 #define ONEFLOW_CORE_EP_CPU_CPU_STREAM_H_
 
-#include <functional>
-#include <memory>
 #include "oneflow/core/ep/include/stream.h"
 #include "oneflow/core/ep/cpu/cpu_device.h"
 
