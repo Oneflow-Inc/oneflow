86153be3afbc94ea97f01bd1ef19cf69108de009
luyang <flowingsun007@163.com>
2022-05-24 14:08:26 +0000

    refine

--- python/oneflow/test/modules/test_cast.py
+++ python/oneflow/test/modules/test_cast.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from random import random
 from random import shuffle
 import unittest
 from collections import OrderedDict
