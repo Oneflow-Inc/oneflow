0ffc6b4a7190915ba8306b175f0809216e8e238c
Flowingsun007 <flowingsun007@163.com>
2022-05-26 07:20:34 +0800

    format

--- python/oneflow/test/tensor/test_tensor_part_1.py
+++ python/oneflow/test/tensor/test_tensor_part_1.py
@@ -1006,7 +1006,7 @@ class TestTensor(flow.unittest.TestCase):
         x = random_tensor(ndim=4).to(device)
         y = x.reshape(-1,)
         return y
-    
+
     @autotest(check_graph=True)
     def test_reshape_as_tensor_with_random_data(test_case):
         device = random_device()
