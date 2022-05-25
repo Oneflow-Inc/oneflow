e44fa9a72825d7ad0855710ad26bc490dba35c1f
zhongshsh <zhongshsh5@mail2.sysu.edu.cn>
2022-05-25 09:11:33 +0800

    update

--- python/oneflow/test/modules/test_consistent_fill_.py
+++ python/oneflow/test/modules/test_consistent_fill_.py
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from re import X
 import unittest
 import oneflow as flow
 import oneflow.unittest
