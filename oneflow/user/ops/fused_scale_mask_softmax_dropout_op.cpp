f89c670e94623e9d96e52c6da427b83c50158d82
MARD1NO <359521840@qq.com>
2022-05-27 15:41:44 +0800

    Remove shape check

--- oneflow/user/ops/fused_scale_mask_softmax_dropout_op.cpp
+++ oneflow/user/ops/fused_scale_mask_softmax_dropout_op.cpp
@@ -22,7 +22,6 @@ namespace oneflow {
     -> Maybe<void> {
   const user_op::TensorDesc& x_desc = ctx->InputTensorDesc("x", 0);
   const user_op::TensorDesc& mask_desc = ctx->InputTensorDesc("mask", 0);
-  CHECK_OR_RETURN(x_desc.shape() == mask_desc.shape());
   const auto x_shape = x_desc.shape();
   const auto mask_shape = mask_desc.shape();
   CHECK_OR_RETURN(x_desc.shape().At(x_shape.NumAxes() - 1)
@@ -80,7 +79,6 @@ namespace oneflow {
   CHECK_EQ_OR_RETURN(dy_desc.shape(), softmax_y_desc.shape());
   CHECK_OR_RETURN(dy_desc.shape().At(dy_desc.shape().NumAxes() - 1)
                   == mask_desc.shape().At(mask_desc.shape().NumAxes() - 1));
-  CHECK_OR_RETURN(dy_desc.shape() == mask_desc.shape());
   user_op::TensorDesc* dx_desc = ctx->OutputTensorDesc("dx", 0);
   *dx_desc->mut_shape() = dy_desc.shape();
   *dx_desc->mut_is_dynamic() = dy_desc.is_dynamic();
