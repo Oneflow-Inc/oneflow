3373222f63beccd85b5323248e404aaf21b9279e
Liang Depeng <liangdepeng@gmail.com>
2022-05-27 16:17:01 +0800

    Update oneflow/core/autograd/gradient_funcs/fused_gru_cell.cpp
    Co-authored-by: Yinggang Wang <wyg19970408@gmail.com>

--- oneflow/core/autograd/gradient_funcs/fused_gru_cell.cpp
+++ oneflow/core/autograd/gradient_funcs/fused_gru_cell.cpp
@@ -36,17 +36,10 @@ class FusedGruCellGrad : public OpExprGradFunction<FusedGruCellGradCaptureState>
 
   Maybe<void> Capture(FusedGruCellGradCaptureState* ctx, const TensorTuple& inputs,
                       const TensorTuple& outputs, const AttrMap& attrs) const override {
-    if (inputs.size() == 3) {
-      ctx->has_bias = false;
-    } else {
-      CHECK_EQ_OR_RETURN(inputs.size(), 5) << "FusedGruCellGrad::Capture input size should be 5.";
-      ctx->has_bias = true;
-    }
-    if (inputs[2]->requires_grad()) {
-      ctx->hx_needs_grad = true;
-    } else {
-      ctx->hx_needs_grad = false;
-    }
+    const size_t in_size = inputs.size();
+    CHECK_OR_RETURN(in_size == 3 || in_size == 5) << "FusedGruCellGrad input's size must be 3 or 5";
+    ctx->has_bias = inputs.size() == 5;
+    ctx->hx_needs_grad = inputs[2]->requires_grad();
     ctx->SaveTensorForBackward(outputs[1]);  // workspace
     return Maybe<void>::Ok();
   }
