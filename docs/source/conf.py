ec1c5189bcf7a066c993afd7ba2dcf830c3e1a1c
Chenqll <2398653527@qq.com>
2022-05-29 15:00:26 +0800

    first rebuild

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,17 @@ release = u""
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "recommonmark"]
+extensions = [
+    "sphinx.ext.autodoc", 
+    "sphinx.ext.napoleon", 
+    "recommonmark", 
+    "sphinx_copybutton",
+    "sphinx.ext.autosummary"
+    ]
+
+# build the templated autosummary files
+autosummary_generate = True
+numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
