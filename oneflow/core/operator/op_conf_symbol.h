45f3b32d8f45a518806d2649a564043b04fe0d70
clackhan <han_binbin@163.com>
2022-05-30 17:24:46 +0800

    merge_id_cache_and_symbol_storage

--- oneflow/core/operator/op_conf_symbol.h
+++ oneflow/core/operator/op_conf_symbol.h
@@ -31,8 +31,9 @@ class OperatorConfSymbol final {
   ~OperatorConfSymbol() = default;
 
   const OperatorConf& op_conf() const { return op_conf_; }
+  // only used for symbol storage
+  const OperatorConf& data() const { return op_conf_; }
   const Optional<int64_t>& symbol_id() const { return symbol_id_; }
-  std::shared_ptr<OperatorConf> data() const { return data_; }
 
  private:
   Optional<int64_t> symbol_id_;
