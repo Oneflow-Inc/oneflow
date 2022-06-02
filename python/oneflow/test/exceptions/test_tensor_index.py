36df2234339b7965e85ff107bdd49147abdeccd9
zhongshsh <zhongshsh5@mail2.sysu.edu.cn>
2022-06-02 14:43:17 +0800

    add exception type

--- python/oneflow/test/exceptions/test_tensor_index.py
+++ python/oneflow/test/exceptions/test_tensor_index.py
@@ -19,8 +19,8 @@ import oneflow as flow
 
 
 class TestTensorIndexError(flow.unittest.TestCase):
-    def test_PrepareSliceIndices_indices_amount_runtime_error(test_case):
-        with test_case.assertRaises(Exception) as context:
+    def test_PrepareSliceIndices_indices_amount_index_error(test_case):
+        with test_case.assertRaises(IndexError) as context:
             x = flow.arange(16).reshape(4, 4)
             x[0, 0, 0] = 0
         test_case.assertTrue(
@@ -28,14 +28,14 @@ class TestTensorIndexError(flow.unittest.TestCase):
         )
 
     def test_PrepareSliceIndices_slice_step_runtime_error(test_case):
-        with test_case.assertRaises(Exception) as context:
+        with test_case.assertRaises(RuntimeError) as context:
             x = flow.tensor([0, 1, 2, 3], dtype=flow.int32)
             s = slice(0, 2, -1)
             y = x[s]
         test_case.assertTrue("Step must be greater than zero" in str(context.exception))
 
     def test_ApplySelectIndexing_input_dim_runtime_error(test_case):
-        with test_case.assertRaises(Exception) as context:
+        with test_case.assertRaises(RuntimeError) as context:
             x = flow.tensor(5, dtype=flow.int32)
             y = x[0]
         test_case.assertTrue(
@@ -43,15 +43,15 @@ class TestTensorIndexError(flow.unittest.TestCase):
         )
 
     def test_ApplySelectIndexing_index_error(test_case):
-        with test_case.assertRaises(Exception) as context:
+        with test_case.assertRaises(IndexError) as context:
             x = flow.ones(2, 3, dtype=flow.int32)
             y = x[3]
         test_case.assertTrue(
             "Index out of range (expected to be in range of" in str(context.exception)
         )
 
-    def test_ApplyAdvancedIndexing_runtime_error(test_case):
-        with test_case.assertRaises(Exception) as context:
+    def test_ApplyAdvancedIndexing_index_error(test_case):
+        with test_case.assertRaises(IndexError) as context:
             x = flow.ones(2, 2, dtype=flow.int32)
             index = (flow.tensor(1, dtype=flow.int32), flow.tensor(1, dtype=flow.int32), flow.tensor(1, dtype=flow.int32))
             y = x[index]
