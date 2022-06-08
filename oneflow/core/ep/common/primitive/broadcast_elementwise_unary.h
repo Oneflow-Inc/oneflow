dee27ed3a9021ecb8ccf03d4017d171f37cada66
EsdeathYZH <1162526220@qq.com>
2022-06-08 11:59:53 +0800

    Add code skeleton for broadcast unary primitive

---
+++ oneflow/core/ep/common/primitive/broadcast_elementwise_unary.h
@@ -0,0 +1,56 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef ONEFLOW_CORE_PRIMITIVE_COMMON_BROADCAST_ELEMENTWISE_UNARY
+#define ONEFLOW_CORE_PRIMITIVE_COMMON_BROADCAST_ELEMENTWISE_UNARY
+
+#include "oneflow/core/ep/include/primitive/primitive.h"
+#include "oneflow/core/common/nd_index_offset_helper.h"
+#include "oneflow/core/ep/common/primitive/util.h"
+
+namespace oneflow {
+
+namespace ep {
+namespace primitive {
+
+namespace broadcast_elementwise_unary {
+
+constexpr size_t kMaxNumDims = 8;
+
+inline void CheckInplace(size_t num_dims, const int64_t* src0_dims, const void* src0,
+                         const int64_t* src1_dims, const void* src1, const int64_t* dst_dims,
+                         const void* dst) {
+  for (int64_t i = 0; i < num_dims; ++i) {
+    if (src0 == dst) { CHECK_EQ(src0_dims[i], dst_dims[i]); }
+    if (src1 == dst) { CHECK_EQ(src1_dims[i], dst_dims[i]); }
+  }
+}
+
+inline bool IsDimsEquals(size_t num_src0_dims, const int64_t* src0_dims, size_t num_src1_dims,
+                         const int64_t* src1_dims) {
+  if (num_src0_dims != num_src1_dims) { return false; }
+  for (size_t i = 0; i < num_src1_dims; ++i) {
+    if (src0_dims[i] != src1_dims[i]) { return false; }
+  }
+  return true;
+}
+
+}  // namespace broadcast_elementwise_unary
+}  // namespace primitive
+}  // namespace ep
+
+}  // namespace oneflow
+
+#endif  // ONEFLOW_CORE_PRIMITIVE_COMMON_BROADCAST_ELEMENTWISE_UNARY
---
+++ oneflow/core/ep/cpu/primitive/broadcast_elementwise_unary.cpp
@@ -0,0 +1,95 @@
+/*
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "oneflow/core/ep/include/primitive/broadcast_elementwise_unary.h"
+#include "oneflow/core/common/data_type.h"
+#include "oneflow/core/ep/common/primitive/broadcast_elementwise_unary.h"
+#include "oneflow/core/ep/cpu/primitive/type_seq.h"
+#include "oneflow/core/ep/cpu/cpu_stream.h"
+#include "oneflow/core/ep/cpu/cpu_device.h"
+
+namespace oneflow {
+
+namespace ep {
+namespace primitive {
+namespace broadcast_elementwise_unary {
+
+namespace {
+
+template<UnaryOp unary_op, typename Src, typename Dst>
+class BroadcastElementwiseUnaryImpl : public BroadcastElementwiseUnary {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BroadcastElementwiseUnaryImpl);
+  BroadcastElementwiseUnaryImpl(Scalar attr0, Scalar attr1) = default;
+  ~BroadcastElementwiseUnaryImpl() override = default;
+
+  void Launch(Stream* stream, size_t num_src_dims, const int64_t* src_dims,
+                      const int64_t* src_strides, const void* src, size_t num_dst_dims,
+                      const int64_t* dst_dims, const int64_t* dst_strides, void* dst) override {
+    // TODO(yaozihang): impl
+  }
+
+ protected:
+  Scalar attr0, attr1;
+};
+
+template<UnaryOp unary_op, typename Src, typename Dst>
+std::unique_ptr<BroadcastElementwiseUnary> NewBroadcastElementwiseUnary(Scalar attr0,
+                                                                          Scalar attr1) {
+  return std::unique_ptr<BroadcastElementwiseUnary>(
+      new BroadcastElementwiseUnaryImpl<unary_op, Src, Dst>(attr0, attr1));
+}
+
+class BroadcastElementwiseUnaryFactoryImpl : public BroadcastElementwiseUnaryFactory {
+ public:
+  OF_DISALLOW_COPY_AND_MOVE(BroadcastElementwiseUnaryFactoryImpl);
+  BroadcastElementwiseUnaryFactoryImpl() = default;
+  ~BroadcastElementwiseUnaryFactoryImpl() override = default;
+
+  std::unique_ptr<BroadcastElementwiseUnary> New(UnaryOp unary_op, DataType src_type, DataType dst_type,
+                                                 size_t max_num_dims, Scalar attr0, Scalar attr1) override {
+    if (max_num_dims > kMaxNumDims) { return nullptr; }
+#define MAKE_NEW_SAME_DTYPE_BROADCAST_ELEMENTWISE_UNARY_ENTRY(unary_op, dtype_pair)                   \
+  {std::make_tuple(unary_op, OF_PP_PAIR_SECOND(dtype_pair), OF_PP_PAIR_SECOND(dtype_pair)), \
+   NewBroadcastElementwiseUnary<unary_op, OF_PP_PAIR_FIRST(dtype_pair), OF_PP_PAIR_FIRST(dtype_pair)>},
+
+    static const std::map<std::tuple<UnaryOp, DataType, DataType>,
+        std::function<std::unique_ptr<BroadcastElementwiseUnary>(Scalar, Scalar)>>
+        new_broadcast_elementwise_unary_handle {
+        // TODO(yaozihang): add registry for ops which use BroadcastElementwiseUnary primitive
+        };
+
+#undef MAKE_NEW_SAME_DTYPE_BROADCAST_ELEMENTWISE_UNARY_ENTRY
+
+    const auto iter = new_broadcast_elementwise_unary_handle.find(
+        std::make_tuple(unary_op, src_type, dst_type));
+    if (iter != new_broadcast_elementwise_unary_handle.end()) {
+      return iter->second(attr0, attr1);
+    } else {
+      return nullptr;
+    }
+  }
+};
+
+REGISTER_PRIMITIVE_FACTORY(DeviceType::kCPU, BroadcastElementwiseUnaryFactory,
+                           BroadcastElementwiseUnaryFactoryImpl);
+
+}  // namespace
+}  // namespace broadcast_elementwise_unary
+}  // namespace primitive
+}  // namespace ep
+
+}  // namespace oneflow
