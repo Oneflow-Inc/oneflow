1066fa6e92271f15c3795961be75d3f5f09e2231
daquexian <daquexian566@gmail.com>
2022-06-10 16:55:24 +0800

    update script
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- run_full.sh
+++ run_full.sh
@@ -4,33 +4,34 @@ set -ux
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-export ONEFLOW_DTR_MODEL_NAME=resnet152
+export ONEFLOW_DTR_MODEL_NAME=densenet121
 
-array=( 3000 4000 5000 6000 7000 8000 9000 10000 )
+array=( 1700 2000 3000 4000 5000 6000 7000 8000 9000 10000 )
+BS=70
 
 METHOD=nlr
 for threshold in "${array[@]}"
 do
-  ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py 55 ${threshold}mb 40 tmp --no-dataloader --no-o-one --nlr
+  ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py $BS ${threshold}mb 40 tmp --no-dataloader --no-o-one --nlr
 done
 
-# METHOD=no-lr
-# for threshold in "${array[@]}"
-# do
-#   ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py 55 ${threshold}mb 40 tmp --no-dataloader --no-o-one --no-lr
-# done
-#
-# METHOD=no-allo
-# for threshold in "${array[@]}"
-# do
-#   ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py 55 ${threshold}mb 40 tmp --no-dataloader --no-o-one --no-allo
-# done
-#
-# METHOD=normal
-# for threshold in "${array[@]}"
-# do
-#   ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py 55 ${threshold}mb 40 tmp --no-dataloader --no-o-one
-# done
+METHOD=no-lr
+for threshold in "${array[@]}"
+do
+  ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py $BS ${threshold}mb 40 tmp --no-dataloader --no-o-one --no-lr
+done
+
+METHOD=no-allo
+for threshold in "${array[@]}"
+do
+  ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py $BS ${threshold}mb 40 tmp --no-dataloader --no-o-one --no-allo
+done
+
+METHOD=normal
+for threshold in "${array[@]}"
+do
+  ONEFLOW_DTR_SUMMARY_FILE_PREFIX=$ONEFLOW_DTR_MODEL_NAME-$METHOD-$threshold CUDA_VISIBLE_DEVICES=3 ONEFLOW_DISABLE_VIEW=1 python3 -u $SCRIPT_DIR/rn50_dtr.py $BS ${threshold}mb 40 tmp --no-dataloader --no-o-one
+done
 
 
 # python3 $SCRIPT_DIR/run.py 10 115 4500mb 40 --no-o-one
