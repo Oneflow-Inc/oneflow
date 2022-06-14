015393ec50092a456d052a3066b53b692aac7e3a
yuhao <1171760467@qq.com>
2022-06-14 16:21:25 +0800

    build script add flag

--- ci/manylinux/build.sh
+++ ci/manylinux/build.sh
@@ -27,6 +27,9 @@ cmake -S ${ONEFLOW_CI_SRC_DIR} -C ${ONEFLOW_CI_CMAKE_INIT_CACHE} -DPython3_EXECU
 # cmake build
 cd ${ONEFLOW_CI_BUILD_DIR}
 cmake --build . --parallel ${ONEFLOW_CI_BUILD_PARALLEL}
+if [ ! -z "$ONEFLOW_CI_BUILD_RUN_LIT" ]; then
+    cmake --build . -t c1
+fi
 
 # build pip
 cd ${ONEFLOW_CI_SRC_DIR}
