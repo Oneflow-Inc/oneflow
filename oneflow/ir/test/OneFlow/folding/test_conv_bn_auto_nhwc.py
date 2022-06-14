ceb6e3f9800ab5fb28a5043e9b23ed2495493560
mosout <mosout@qq.com>
2022-06-14 12:03:10 +0800

    fix constant folding tests

--- oneflow/ir/test/OneFlow/folding/test_conv_bn_auto_nhwc.py
+++ oneflow/ir/test/OneFlow/folding/test_conv_bn_auto_nhwc.py
@@ -29,11 +29,14 @@ os.environ["ONEFLOW_MLIR_PREFER_NHWC"] = "1"
 os.environ["ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION"] = "1"
 
 
-def _test_fuse_conv_bn(test_case):
-    data = flow.randn(1, 3, 224, 224).to("cuda")
+def _test_fuse_conv_bn(test_case, with_cuda):
+    data = flow.randn(1, 3, 224, 224)
+    if with_cuda:
+        data = data.to("cuda")
 
     model = resnet50(pretrained=True, progress=True)
-    model.to("cuda")
+    if with_cuda:
+        model.to("cuda")
     model.eval()
     eager_res = model(data)
 
@@ -56,7 +59,11 @@ def _test_fuse_conv_bn(test_case):
 @flow.unittest.skip_unless_1n1d()
 class TestFuseConvBn(oneflow.unittest.TestCase):
     def test_fuse_conv_bn(test_case):
-        _test_fuse_conv_bn(test_case)
+        _test_fuse_conv_bn(test_case, False)
+
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
+    def test_fuse_conv_bn_cuda(test_case):
+        _test_fuse_conv_bn(test_case, True)
 
 
 if __name__ == "__main__":
--- oneflow/ir/test/OneFlow/folding/test_simple_multiply.py
+++ oneflow/ir/test/OneFlow/folding/test_simple_multiply.py
@@ -87,10 +87,16 @@ def _test_fold_multiply(test_case, module, with_cuda, *args):
 class TestFoldMultiply(oneflow.unittest.TestCase):
     def test_fold_multiply(test_case):
         _test_fold_multiply(test_case, MultiplyModel, with_cuda=False)
+
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
+    def test_fold_multiply_cuda(test_case):
         _test_fold_multiply(test_case, MultiplyModel, with_cuda=True)
 
     def test_fold_multiply_complex(test_case):
         _test_fold_multiply(test_case, MultiplyModelComplex, with_cuda=False)
+
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
+    def test_fold_multiply_complex_cuda(test_case):
         _test_fold_multiply(test_case, MultiplyModelComplex, with_cuda=True)
 
     def test_fold_multiply_with_input(test_case):
@@ -98,6 +104,8 @@ class TestFoldMultiply(oneflow.unittest.TestCase):
         b = flow.tensor([9, -1], dtype=flow.float32)
         _test_fold_multiply(test_case, MultiplyModelWithInput, False, a, b)
 
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
+    def test_fold_multiply_with_input_cuda(test_case):
         a = a.to("cuda")
         b = b.to("cuda")
         _test_fold_multiply(test_case, MultiplyModelWithInput, True, a, b)
