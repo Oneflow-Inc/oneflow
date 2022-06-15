a7f96b7c46b5e6272ffa5788fc86141a91db0562
leaves-zwx <kunta0932@gmail.com>
2022-06-15 13:29:19 +0000

    update test

--- python/oneflow/test/modules/test_nll_loss.py
+++ python/oneflow/test/modules/test_nll_loss.py
@@ -14,7 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import numpy as np
 import unittest
+
 import oneflow as flow
 import oneflow.unittest
 
@@ -22,54 +24,110 @@ from oneflow.test_utils.automated_test_util import *
 
 
 @autotest(n=1)
-def _test_nll_loss(test_case, placement=None, sbp_list=None):
+def _test_nll_loss(
+    test_case, has_weight=False, split_batch_dim=False, split_class_dim=False
+):
+    # N = random(1, 4) * 2
+    # C = random(1, 10) * 2
     N = 4
     C = 10
-    ndim = random(2, 5).to(int).value()
-    dims = [random(1, 2) * random(1, 3) for i in range(ndim - 2)]
+    ndim = random(2, 3).to(int).value()
+    dims = [random(2, 10) for i in range(ndim - 2)]
     input_dims = [N, C] + dims
-    input = random_tensor(ndim, *input_dims)
     target_dims = [N] + dims
+    input = random_tensor(ndim, *input_dims)
     target = random_tensor(
-        ndim, *target_dims, low=0, high=C, dtype=flow.int32, requires_grad=False
+        ndim - 1, *target_dims, low=0, high=C, dtype=int, requires_grad=False
     )
+    weight = None
+    if has_weight:
+        weight = random_tensor(1, C, requires_grad=False)
 
-    if placement and sbp_list:
-        if len(sbp_list) == 1:
-            input_sbp = sbp_list[0]
-            target_sbp = sbp_list[0]
-        elif len(sbp_list) > 1:
-            input_sbp = sbp_list[0]
-            target_sbp = sbp_list[1]
+    device = random_device().value()
+    if not split_class_dim and not split_batch_dim:
+        input = input.to(device)
+        target = target.to(device)
+        if has_weight:
+            weight = weight.to(device)
+    else:
+        world_size = flow.env.get_world_size()
+        assert world_size % 2 == 0
+        ranks = np.array(range(world_size))
+
+        if split_batch_dim and split_class_dim:
+            placement = flow.placement(device, ranks.reshape((ranks.size // 2, 2)))
+            input_sbp = [flow.sbp.split(0), flow.sbp.split(1)]
+            target_sbp = [flow.sbp.split(0), flow.sbp.broadcast()]
+            weight_sbp = [flow.sbp.broadcast(), flow.sbp.split(0)]
+        elif split_batch_dim:
+            placement = flow.placement(device, ranks)
+            input_sbp = flow.sbp.split(0)
+            target_sbp = flow.sbp.split(0)
+            weight_sbp = flow.sbp.broadcast()
         else:
-            raise RuntimeError
+            placement = flow.placement(device, ranks)
+            input_sbp = flow.sbp.split(1)
+            target_sbp = flow.sbp.broadcast()
+            weight_sbp = flow.sbp.split(0)
 
-        input = input.to_global(placement, input_sbp)
-        target = target.to_global(placement, target_sbp)
+        input = input.to_global(placement=placement, sbp=input_sbp)
+        target = target.to_global(placement=placement, sbp=target_sbp)
+        if has_weight:
+            weight = weight.to_global(placement=placement, sbp=weight_sbp)
 
-    reduction = oneof("none", "sum", "mean")
-    nll = torch.nn.NLLLoss(reduction=reduction)
+        print(
+            f"** input: {input.oneflow.shape} {input.oneflow.placement} {input.oneflow.sbp}"
+            f", target: {target.oneflow.shape} {target.oneflow.placement} {target.oneflow.sbp}"
+        )
+
+    # reduction = oneof("none", "sum", "mean")
+    reduction = "mean"
+    if has_weight:
+        nll = torch.nn.NLLLoss(weight=weight, reduction=reduction)
+    else:
+        nll = torch.nn.NLLLoss(reduction=reduction)
     return nll(input, target)
 
 
-class TestNLLLossModule(flow.unitest.TestCase):
-    def test_nll_loss(test_case):
+@flow.unittest.skip_unless_1n1d()
+class NLLLossTestCase(flow.unittest.TestCase):
+    def test_local(test_case):
         _test_nll_loss(test_case)
 
+    def test_weighted(test_case):
+        _test_nll_loss(test_case, has_weight=True)
 
-# class TestAddModule(flow.unittest.TestCase):
-#     @globaltest
-#     def test_add_with_alpha(test_case):
-#         ndim = random(1, 4).to(int).value()
-#         for placement in all_placement():
-#             for sbp in all_sbp(placement, max_dim=ndim):
-#                 _test_add_with_alpha(test_case, ndim, placement, sbp)
-#             zerodim = random(0, ndim).to(int).value()
-#             valid_split_axis = [i for i in range(ndim) if i != zerodim]
-#             for sbp in all_sbp(
-#                 placement, max_dim=ndim, valid_split_axis=valid_split_axis
-#             ):
-#                 _test_add_with_0size(test_case, ndim, zerodim, placement, sbp)
+
+@flow.unittest.skip_unless_1n2d()
+class ParallelNLLLossTestCase(flow.unittest.TestCase):
+    @globaltest
+    def test_data_parallel(test_case):
+        _test_nll_loss(test_case, split_batch_dim=True)
+
+    @globaltest
+    def test_data_parallel_weighted(test_case):
+        _test_nll_loss(test_case, has_weight=True, split_batch_dim=True)
+
+    @globaltest
+    def test_model_parallel(test_case):
+        _test_nll_loss(test_case, split_class_dim=True)
+
+    @globaltest
+    def test_model_parallel_weighted(test_case):
+        _test_nll_loss(test_case, has_weight=True, split_class_dim=True)
+
+
+@flow.unittest.skip_unless_1n4d()
+class TowDParallelNLLLossTestCase(flow.unittest.TestCase):
+    # @globaltest
+    # def test_2d_parallel(test_case):
+    #     _test_nll_loss(test_case, split_batch_dim=True, split_class_dim=True)
+
+    @globaltest
+    def test_2d_parallel_weighted(test_case):
+        _test_nll_loss(
+            test_case, has_weight=True, split_batch_dim=True, split_class_dim=True
+        )
 
 
 if __name__ == "__main__":
