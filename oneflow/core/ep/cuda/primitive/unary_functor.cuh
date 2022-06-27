b91282694c3870be1b7d1036bf9ee57fd6f67dc3
EsdeathYZH <1162526220@qq.com>
2022-06-27 16:54:47 +0800

    fix

--- oneflow/core/ep/cuda/primitive/unary_functor.cuh
+++ oneflow/core/ep/cuda/primitive/unary_functor.cuh
@@ -54,13 +54,6 @@ struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kTanh, half, half> {
 };
 
 template<>
-struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kIsInf, bool, nv_bfloat16> {
-  UnaryFunctor(Scalar attr0, Scalar attr1) {}
-
-  OF_DEVICE_FUNC bool operator()(nv_bfloat16 src) const { return isinf(__bfloat162float(src)); }
-};
-
-template<>
 struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kIsInf, bool, half> {
   UnaryFunctor(Scalar attr0, Scalar attr1) {}
 
@@ -79,13 +72,6 @@ struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kIsInf, bool, double> {
   UnaryFunctor(Scalar attr0, Scalar attr1) {}
 
   OF_DEVICE_FUNC bool operator()(double src) const { return isinf(src); }
-};
-
-template<>
-struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kIsNan, bool, nv_bfloat16> {
-  UnaryFunctor(Scalar attr0, Scalar attr1) {}
-
-  OF_DEVICE_FUNC bool operator()(nv_bfloat16 src) const { return isnan(__bfloat162float(src)); }
 };
 
 template<>
@@ -160,6 +146,20 @@ SPECIALIZATION_PSEUDO_BFLOAT16_FUNCTOR(UnaryOp::kSoftSign);
 SPECIALIZATION_PSEUDO_BFLOAT16_FUNCTOR(UnaryOp::kSoftPlus);
 SPECIALIZATION_PSEUDO_BFLOAT16_FUNCTOR(UnaryOp::kTanh);
 SPECIALIZATION_PSEUDO_BFLOAT16_FUNCTOR(UnaryOp::kThreshold);
+
+template<>
+struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kIsInf, bool, nv_bfloat16> {
+  UnaryFunctor(Scalar attr0, Scalar attr1) {}
+
+  OF_DEVICE_FUNC bool operator()(nv_bfloat16 src) const { return isinf(__bfloat162float(src)); }
+};
+
+template<>
+struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kIsNan, bool, nv_bfloat16> {
+  UnaryFunctor(Scalar attr0, Scalar attr1) {}
+
+  OF_DEVICE_FUNC bool operator()(nv_bfloat16 src) const { return isnan(__bfloat162float(src)); }
+};
 
 #endif
 
