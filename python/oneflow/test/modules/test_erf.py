554bc0dd0f45135bc09a0d47c69c088f5a576ebd
BBuf <1182563586@qq.com>
2022-06-27 20:38:16 +0800

    fix autotest bug and optimize eager test speed in ci

--- python/oneflow/test/modules/test_erf.py
+++ python/oneflow/test/modules/test_erf.py
@@ -29,14 +29,14 @@ from oneflow.test_utils.automated_test_util import *
 
 @flow.unittest.skip_unless_1n1d()
 class TestErfModule(flow.unittest.TestCase):
-    @autotest(check_graph=True)
+    @autotest(n=5)
     def test_flow_erf_with_random_data(test_case):
         device = random_device()
         x = random_tensor().to(device)
         y = torch.erf(x)
         return y
 
-    @autotest(check_graph=True)
+    @autotest(n=5)
     def test_flow_erf_with_0dim_data(test_case):
         device = random_device()
         x = random_tensor(ndim=0).to(device)
