315578d6f7654ebfcbedadb6f8a0612db5756eb9
zhongshsh <zhongshsh5@mail2.sysu.edu.cn>
2022-06-29 17:00:44 +0800

    modify random in python

--- python/oneflow/framework/docstr/random.py
+++ python/oneflow/framework/docstr/random.py
@@ -55,3 +55,207 @@ add_docstr(
 
     """,
 )
+
+add_docstr(
+    oneflow._C.randn,
+    """
+    Returns a tensor filled with random numbers from a normal distribution with mean 0 and variance 1 (also called the standard normal distribution).
+
+    The shape of the tensor is defined by the variable argument ``size``.
+
+    Args:
+        size (int... or oneflow.Size): Defining the shape of the output tensor.
+          Can be a variable number of arguments or a collection like a list or tuple or oneflow.Size.
+        out (optional): The output tensor.
+        dtype (flow.dtype, optional): The desired data type of returned tensor. Default: ``flow.float32``.
+        layout (optional): The desired layout of returned Tensor.
+        generator (flow.Generator, optional): a pseudorandom number generator for sampling
+        device (flow.device, optional): The desired device of returned local tensor. If None, uses the
+          current device.
+        placement (flow.placement, optional): The desired device of returned global tensor. If None, will
+          construct local tensor.
+        sbp (flow.sbp, optional): The desired sbp of returned global tensor. It must be equal with the
+          numbers of placement.
+        requires_grad (bool, optional): If autograd should record operations on the returned tensor. Default: False.
+
+    For example:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> x = flow.randn(3,3) # construct local tensor
+        >>> x.shape
+        oneflow.Size([3, 3])
+        >>> x.is_global
+        False
+        >>> placement = flow.placement("cpu", ranks=[0])
+        >>> sbp = flow.sbp.broadcast
+        >>> x = flow.randn(3,3,placement=placement,sbp=sbp) # construct global tensor
+        >>> x.is_global
+        True
+
+    """,
+)
+
+add_docstr(
+    oneflow._C.rand,
+    """
+    Returns a tensor filled with random numbers from a uniform distribution on the interval [0, 1)
+
+    The shape of the tensor is defined by the variable argument ``size``.
+
+    Args:
+        size (int... or oneflow.Size): Defining the shape of the output tensor.
+          Can be a variable number of arguments or a collection like a list or tuple or oneflow.Size.
+        out (optional): The output tensor.
+        dtype (flow.dtype, optional): The desired data type of returned tensor. Default: ``flow.float32``.
+        layout (optional): The desired layout of returned Tensor.
+        generator (flow.Generator, optional): a pseudorandom number generator for sampling
+        device (flow.device, optional): The desired device of returned local tensor. If None, uses the
+          current device.
+        placement (flow.placement, optional): The desired device of returned global tensor. If None, will
+          construct local tensor.
+        sbp (flow.sbp, optional): The desired sbp of returned global tensor. It must be equal with the
+          numbers of placement.
+        requires_grad (bool, optional): If autograd should record operations on the returned tensor. Default: False.
+
+    For example:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> x = flow.rand(3,3) # construct local tensor
+        >>> x.shape
+        oneflow.Size([3, 3])
+        >>> x.is_global
+        False
+        >>> placement = flow.placement("cpu", ranks=[0])
+        >>> sbp = flow.sbp.broadcast
+        >>> x = flow.rand(3, 3, placement=placement, sbp=sbp) # construct global tensor
+        >>> x.is_global
+        True
+
+
+    """,
+)
+
+add_docstr(
+    oneflow._C.normal,
+    r"""
+    Returns a tensor of random numbers drawn from separate normal distributions whose mean and standard deviation are given.
+
+    Args:
+        mean (float):  the mean for all distributions
+        std (float):  the standard deviation for all distributions
+        size (int...):  a sequence of integers defining the shape of the output tensor.
+
+    Keyword args:
+        out (Tensor, optional):  the output tensor.
+        placement (flow.placement, optional): The desired device of returned global tensor. If None, will
+          construct local tensor.
+        sbp (flow.sbp, optional): The desired sbp of returned global tensor. It must be equal with the
+          numbers of placement.
+        generator(:class:`oneflow.Generator`, optional):  a pseudorandom number generator for sampling
+        dtype (:class:`oneflow.dtype`, optional): the desired data type of returned tensor.
+            Default: `oneflow.float32`.
+        device: the desired device of returned tensor. Default: cpu.
+        requires_grad(bool, optional): If autograd should record operations on the returned tensor. Default: False.
+
+    Example:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> generator = flow.Generator()
+        >>> generator.manual_seed(0)
+        >>> y = flow.normal(0, 1, 5, generator=generator)
+        >>> y
+        tensor([2.2122, 1.1631, 0.7740, 0.4838, 1.0434], dtype=oneflow.float32)
+    """,
+)
+
+add_docstr(
+    oneflow._C.randint,
+    """
+    Returns a tensor filled with random integers generated uniformly between low (inclusive) and high (exclusive).
+
+    The shape of the tensor is defined by the variable argument ``size``.
+
+    Args:
+        size (int... or oneflow.Size): Defining the shape of the output tensor.
+          Can be a variable number of arguments or a collection like a list or tuple or oneflow.Size.
+        out (optional): The output tensor.
+        dtype (flow.dtype, optional): The desired data type of returned tensor. Default: ``flow.int64``.
+        layout (optional): The desired layout of returned Tensor.
+        generator (flow.Generator, optional) – a pseudorandom number generator for sampling
+        device (flow.device, optional): The desired device of returned local tensor. If None, uses the
+          current device.
+        placement (flow.placement, optional): The desired device of returned global tensor. If None, will
+          construct local tensor.
+        sbp (flow.sbp, optional): The desired sbp of returned global tensor. It must be equal with the
+          numbers of placement.
+        requires_grad (bool, optional): If autograd should record operations on the returned tensor. Default: False.
+
+    For example:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> generator = flow.Generator()
+        >>> generator.manual_seed(0)
+        >>> y = flow.randint(0, 5, (3,3), generator=generator) # construct local tensor
+        >>> y
+        tensor([[2, 2, 3],
+                [4, 3, 4],
+                [2, 4, 2]], dtype=oneflow.int64)
+        >>> y.is_global
+        False
+        >>> placement = flow.placement("cpu", ranks=[0])
+        >>> y = flow.randint(0, 5, (3,3), generator=generator, placement=placement, sbp=flow.sbp.broadcast) # construct global tensor
+        >>> y.is_global
+        True
+
+    """,
+)
+
+add_docstr(
+    oneflow._C.randperm,
+    r"""
+    Returns a random permutation of integers from ``0`` to ``n - 1``.
+
+    Args:
+        n (int): the upper bound (exclusive)
+
+    Keyword args:
+        generator(:class:`oneflow.Generator`, optional):  a pseudorandom number generator for sampling
+        out (Tensor, optional): output Tensor,not supported yet.
+        dtype (:class:`oneflow.dtype`, optional): the desired data type of returned tensor.
+            Default: ``oneflow.int64``.
+        layout: layout is not supported yet.
+        device: the desired device of returned tensor. Default: cpu.
+        placement:(:class:`flow.placement`, optional): The desired device of returned global tensor. If None,
+            will construct local tensor.
+        sbp: (:class:`flow.sbp`, optional): The desired sbp of returned global tensor. It must be equal with the
+            numbers of placement.
+        requires_grad(bool, optional): If autograd should record operations on the returned tensor. Default: False.
+        pin_memory(bool, optional):pin_memory is not supported yet.
+
+    Example:
+
+    .. code-block:: python
+
+        >>> import oneflow as flow
+        >>> generator = flow.Generator()
+        >>> generator.manual_seed(0)
+        >>> y = flow.randperm(5, generator=generator) # construct local tensor
+        >>> y
+        tensor([2, 4, 3, 0, 1], dtype=oneflow.int64)
+        >>> y.is_global
+        False
+        >>> placement = flow.placement("cpu", ranks=[0])
+        >>> y = flow.randperm(5, generator=generator, placement=placement, sbp=flow.sbp.broadcast) # construct global tensor
+        >>> y.is_global
+        True
+
+    """,
+)
