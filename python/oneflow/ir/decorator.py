e9c20c8702fd995345a12b861634edba92a51adb
yuhao <1171760467@qq.com>
2022-06-29 15:50:25 +0800

    enable test all existent get_lr of oneflow in python

--- python/oneflow/ir/decorator.py
+++ python/oneflow/ir/decorator.py
@@ -31,13 +31,56 @@ def lr_def(func):
 
     return wrapper
 
+def lr_jit_register(lr_class):
+    import astpretty
+    _id = lr_class.__class__.__name__
+    _src = textwrap.dedent(inspect.getsource(lr_class.get_lr))
+    _ast = ast.parse(_src)
+    transformer = SelfParamsTransformer(lr_class)
+    transformer.visit(_ast)
+    try:
+        astpretty.pprint(_ast)
+    except:
+        print(ast.dump(_ast))
 
+    # _ast = _ast.body[0]
+    # res = oneflow._oneflow_internal.ir.compile_and_register_lr_jit(_ast, _id)
+    # print(res)
+
+
+
+from oneflow.nn.optimizer.constant_lr import ConstantLR
+from oneflow.nn.optimizer.cosine_annealing_lr import CosineAnnealingLR
+from oneflow.nn.optimizer.cosine_decay_lr import CosineDecayLR
+from oneflow.nn.optimizer.exponential_lr import ExponentialLR
+from oneflow.nn.optimizer.lambda_lr import LambdaLR
+from oneflow.nn.optimizer.linear_lr import LinearLR
+from oneflow.nn.optimizer.multistep_lr import MultiStepLR
+from oneflow.nn.optimizer.polynomial_lr import PolynomialLR
+from oneflow.nn.optimizer.sequential_lr import SequentialLR
+from oneflow.nn.optimizer.step_lr import StepLR
+from oneflow.nn.optimizer.warmup_lr import WarmupLR
+
+from oneflow.optim import SGD
+from oneflow.nn import Parameter
 
 if __name__ == "__main__":
-    class TestJITLR:
-        var = 1
-        @lr_def
-        def get_lr(self, base_lr:float, step:int):
-            return base_lr*step*self.var*3
+    param = Parameter(oneflow.ones(3, 4))
+    optimizer = SGD([param], lr=0.001)
 
-    TestJITLR().get_lr(0.01, 2)
+
+    lr_class_list = [
+            WarmupLR(optimizer),
+            StepLR(optimizer, 5),
+            # SequentialLR(optimizer),
+            PolynomialLR(optimizer, 5),
+            MultiStepLR(optimizer, [10]),
+            LinearLR(optimizer),
+            LambdaLR(optimizer, [lambda step: 0.95 * step]),
+            ExponentialLR(optimizer, 1.1),
+            CosineDecayLR(optimizer, 10),
+            CosineAnnealingLR(optimizer, 50),
+            ConstantLR(optimizer)
+        ]
+    for lr_class in lr_class_list:
+        lr_jit_register(lr_class)
