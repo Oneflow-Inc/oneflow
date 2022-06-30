978fb890f641d1c191075bac14e00381dc4dd3bc
Zhimin Yang <76760002+small1945@users.noreply.github.com>
2022-06-30 10:48:57 +0800

    modify the format->[ 0,1 ] to [0, 1]

--- oneflow/user/ops/bias_add_op.cpp
+++ oneflow/user/ops/bias_add_op.cpp
@@ -28,8 +28,8 @@ namespace oneflow {
       << Error::RuntimeError() << "The size of the axis must greater than or equal to 0, "
       << "but got " << bias_add_axis;
   CHECK_LT_OR_RETURN(bias_add_axis, a_tensor_desc.shape().NumAxes())
-      << Error::RuntimeError() << "Dimension out of range (expected to be in range of [ 0"
-      << "," << a_tensor_desc.shape().NumAxes() - 1 << " ],"
+      << Error::RuntimeError() << "Dimension out of range (expected to be in range of [0"
+      << ", " << a_tensor_desc.shape().NumAxes() - 1 << "],"
       << " but got " << bias_add_axis << ")";
   CHECK_EQ_OR_RETURN(a_tensor_desc.shape().At(bias_add_axis), b_tensor_desc.shape().At(0))
       << Error::RuntimeError() << "The size of tensor " << a_tensor_desc.shape().ToString()
