2cbcfc34550cd0c2cbd991ad5deff7ad85687b5d
MARD1NO <359521840@qq.com>
2022-06-30 09:37:53 +0800

    fix clang

--- oneflow/core/autograd/gradient_funcs/binary_cross_entropy_with_logits_reduce_mean.cpp
+++ oneflow/core/autograd/gradient_funcs/binary_cross_entropy_with_logits_reduce_mean.cpp
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/framework/op_expr_grad_function.h"
+#include "oneflow/core/common/container_util.h"
 #include "oneflow/core/functional/functional.h"
 
 namespace oneflow {
@@ -44,31 +45,36 @@ Maybe<void> BinaryCrossEntropyWithLogitsReduceMean::Init(const OpExpr& op) {
   base_attrs_ = MakeAttrMapFromUserOpConf(fw_op_expr->proto());
   return Maybe<void>::Ok();
 }
+
 Maybe<void> BinaryCrossEntropyWithLogitsReduceMean::Capture(
     BinaryCrossEntropyWithLogitsReduceMeanCaptureState* ctx, const TensorTuple& inputs,
     const TensorTuple& outputs, const AttrMap& attrs) const {
-  ctx->requires_grad = inputs.at(0)->requires_grad();
+  ctx->requires_grad = JUST(VectorAt(inputs, 0))->requires_grad();
   if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
 
   ComposedAttrMap composed_attrs(attrs, base_attrs_);
-  ctx->SaveTensorForBackward(inputs.at(0));  // input
-  ctx->SaveTensorForBackward(inputs.at(1));  // target
+  ctx->SaveTensorForBackward(JUST(VectorAt(inputs, 0)));  // input
+  ctx->SaveTensorForBackward(JUST(VectorAt(inputs, 1)));  // target
   return Maybe<void>::Ok();
 }
+
 Maybe<void> BinaryCrossEntropyWithLogitsReduceMean::Apply(
     const BinaryCrossEntropyWithLogitsReduceMeanCaptureState* ctx, const TensorTuple& out_grads,
     TensorTuple* in_grads) const {
   if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
   CHECK_EQ_OR_RETURN(out_grads.size(), 1);
-  const auto& dy = out_grads.at(0);
-  const auto& input = ctx->SavedTensors().at(0);
-  const auto& target = ctx->SavedTensors().at(1);
+  const auto& dy = JUST(VectorAt(out_grads, 0));
+  const auto& input = JUST(VectorAt(ctx->SavedTensors(), 0));
+  const auto& target = JUST(VectorAt(ctx->SavedTensors(), 1));
   in_grads->resize(ctx->SavedTensors().size());
-  in_grads->at(0) =
+  JUST(VectorAt(*in_grads, 0)) =
       JUST(functional::BinaryCrossEntropyWithLogitsReduceMeanLossGrad(dy, input, target));
   return Maybe<void>::Ok();
 }
+
 REGISTER_OP_EXPR_GRAD_FUNCTION("binary_cross_entropy_with_logits_reduce_mean",
                                BinaryCrossEntropyWithLogitsReduceMean);
+
 }  // namespace one
+
 }  // namespace oneflow
