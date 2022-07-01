5ac0af9cf65dd5100fe77e5eec36d1a8d346f07e
EsdeathYZH <1162526220@qq.com>
2022-07-01 14:46:02 +0800

    use broadcast unary primitive in fill_tensor_ kernel

--- oneflow/user/kernels/fill_kernel.cpp
+++ oneflow/user/kernels/fill_kernel.cpp
@@ -15,6 +15,7 @@ limitations under the License.
 */
 #include "oneflow/core/common/scalar.h"
 #include "oneflow/core/ep/include/primitive/fill.h"
+#include "oneflow/core/ep/include/primitive/broadcast_elementwise_unary.h"
 #include "oneflow/core/framework/framework.h"
 
 namespace oneflow {
@@ -26,7 +27,28 @@ std::unique_ptr<ep::primitive::Fill> NewFillPrimitive(Context* ctx) {
   return ep::primitive::NewPrimitive<ep::primitive::FillFactory>(ctx->device_type(), data_type);
 }
 
+template<typename Context>
+std::unique_ptr<ep::primitive::BroadcastElementwiseUnary> NewBroadcastUnaryPrimitive(Context* ctx) {
+  const user_op::TensorDesc* src = ctx->TensorDesc4ArgNameAndIndex("value", 0);
+  const user_op::TensorDesc* dst = ctx->TensorDesc4ArgNameAndIndex("out", 0);
+  return ep::primitive::NewPrimitive<ep::primitive::BroadcastElementwiseUnaryFactory>(
+      ctx->device_type(), ep::primitive::UnaryOp::kIdentity, src->data_type(), dst->data_type(), 1);
+}
+
 }  // namespace
+
+auto FillPrimitiveExists() {
+  return hob::make_custom("FillPrimitiveExists", [](const user_op::KernelRegContext& ctx) {
+    return NewFillPrimitive(&ctx).operator bool();
+  });
+}
+
+auto BroadcastUnaryPrimitiveExists() {
+  return hob::make_custom("BroadcastElementwiseUnaryPrimitiveExists",
+                          [=](const user_op::KernelRegContext& ctx) {
+                            return NewBroadcastUnaryPrimitive(&ctx).operator bool();
+                          });
+}
 
 class FillKernel final : public user_op::OpKernel {
  public:
@@ -43,49 +65,37 @@ class FillKernel final : public user_op::OpKernel {
     const int32_t elem_cnt = in->shape_view().elem_cnt();
     CHECK_GE(elem_cnt, 0);
     if (elem_cnt == 0) { return; }
-    std::unique_ptr<ep::primitive::Fill> fill = NewFillPrimitive(ctx);
-    CHECK(fill);
-    fill->Launch(ctx->stream(), out->mut_dptr(), value, elem_cnt);
+    auto fill_primitive = NewFillPrimitive(ctx);
+    CHECK(fill_primitive);
+    fill_primitive->Launch(ctx->stream(), out->mut_dptr(), value, elem_cnt);
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-auto FillPrimitiveExists() {
-  return hob::make_custom("FillPrimitiveExists", [](const user_op::KernelRegContext& ctx) {
-    return NewFillPrimitive(&ctx).operator bool();
-  });
-}
-
-template<typename T>
-class FillTensorCpuKernel final : public user_op::OpKernel {
+class FillTensorKernel final : public user_op::OpKernel {
  public:
-  FillTensorCpuKernel() = default;
-  ~FillTensorCpuKernel() = default;
+  FillTensorKernel() = default;
+  ~FillTensorKernel() = default;
 
  private:
   void Compute(user_op::KernelComputeContext* ctx) const override {
-    const user_op::Tensor* in = ctx->Tensor4ArgNameAndIndex("in", 0);
     user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("out", 0);
     const user_op::Tensor* value = ctx->Tensor4ArgNameAndIndex("value", 0);
-    const T value_ = value->dptr<T>()[0];
-    const int32_t elem_cnt = in->shape_view().elem_cnt();
-    T* out_ptr = out->mut_dptr<T>();
-    FOR_RANGE(int32_t, i, 0, elem_cnt) { out_ptr[i] = value_; }
+
+    auto primitive = NewBroadcastUnaryPrimitive(ctx);
+    CHECK(primitive);
+
+    primitive->Launch(ctx->stream(), value->shape_view().NumAxes(), value->shape_view().data(),
+                      value->stride().data(), value->dptr(), out->shape_view().NumAxes(),
+                      out->shape_view().data(), out->stride().data(), out->mut_dptr());
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_FILL_CPU_KERNEL(dtype)                               \
-  REGISTER_USER_KERNEL("fill_tensor_")                                \
-      .SetCreateFn<FillTensorCpuKernel<dtype>>()                      \
-      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kCPU) \
-                       && (user_op::HobDataType("out", 0) == GetDataType<dtype>::value));
+REGISTER_USER_KERNEL("fill_tensor_")
+    .SetCreateFn<FillTensorKernel>()
+    .SetIsMatchedHob(BroadcastUnaryPrimitiveExists());
 
-REGISTER_FILL_CPU_KERNEL(float)
-REGISTER_FILL_CPU_KERNEL(double)
-REGISTER_FILL_CPU_KERNEL(int8_t)
-REGISTER_FILL_CPU_KERNEL(int32_t)
-REGISTER_FILL_CPU_KERNEL(int64_t)
 REGISTER_USER_KERNEL("fill_").SetCreateFn<FillKernel>().SetIsMatchedHob(FillPrimitiveExists()
                                                                         == true);
 
