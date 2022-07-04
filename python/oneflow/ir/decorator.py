e11035de0ccfa153991709e1e0375fbbacc9c83f
jackalcooper <jackalcooper@gmail.com>
2022-07-04 14:04:14 +0800

    add some docs

--- python/oneflow/ir/decorator.py
+++ python/oneflow/ir/decorator.py
@@ -4,19 +4,20 @@ import textwrap
 import inspect
 import oneflow
 
+
 class SelfParamsTransformer(ast.NodeTransformer):
     def __init__(self, lr_def_class):
         super().__init__()
         self.lr_def_class = lr_def_class
 
     def visit_Attribute(self, node):
-        if node.value.id == 'self':
+        if node.value.id == "self":
             _name = node.attr
             _attr = getattr(self.lr_def_class, _name)
             return ast.Constant(_attr, None)
 
     def visit_arguments(self, node: ast.arguments):
-        for index,item in enumerate(node.args):
+        for index, item in enumerate(node.args):
             if item.arg == "self":
                 node.args.pop(index)
         return node
@@ -24,13 +25,14 @@ class SelfParamsTransformer(ast.NodeTransformer):
 
 def lr_jit_register(lr_class):
     _id = lr_class.__class__.__name__
+    # load source txt
     _src = textwrap.dedent(inspect.getsource(lr_class.get_lr))
     _ast = ast.parse(_src).body[0]
+    # transform param self
     transformer = SelfParamsTransformer(lr_class)
     transformer.visit(_ast)
-
+    # feed transformed as to C++
     res = oneflow._oneflow_internal.ir.compile_and_register_lr_jit(_ast, _id)
-
 
 
 from oneflow.nn.optimizer.constant_lr import ConstantLR
@@ -52,19 +54,18 @@ if __name__ == "__main__":
     param = Parameter(oneflow.ones(3, 4))
     optimizer = SGD([param], lr=0.001)
 
-
     lr_class_list = [
-            WarmupLR(optimizer),
-            # StepLR(optimizer, 5),
-            # SequentialLR(optimizer),
-            # PolynomialLR(optimizer, 5),
-            # MultiStepLR(optimizer, [10]),
-            # LinearLR(optimizer),
-            # LambdaLR(optimizer, [lambda step: 0.95 * step]),
-            # ExponentialLR(optimizer, 1.1),
-            # CosineDecayLR(optimizer, 10),
-            # CosineAnnealingLR(optimizer, 50),
-            # ConstantLR(optimizer)
-        ]
+        WarmupLR(optimizer),
+        # StepLR(optimizer, 5),
+        # SequentialLR(optimizer),
+        # PolynomialLR(optimizer, 5),
+        # MultiStepLR(optimizer, [10]),
+        # LinearLR(optimizer),
+        # LambdaLR(optimizer, [lambda step: 0.95 * step]),
+        # ExponentialLR(optimizer, 1.1),
+        # CosineDecayLR(optimizer, 10),
+        # CosineAnnealingLR(optimizer, 50),
+        # ConstantLR(optimizer)
+    ]
     for lr_class in lr_class_list:
         lr_jit_register(lr_class)
