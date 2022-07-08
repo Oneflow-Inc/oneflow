32aa3d0731180342748b5ea59b5b9940b39a6f72
Zhimin Yang <76760002+small1945@users.noreply.github.com>
2022-07-08 09:26:38 +0800

    Update scalar_by_tensor_op.cpp

--- oneflow/user/ops/scalar_by_tensor_op.cpp
+++ oneflow/user/ops/scalar_by_tensor_op.cpp
@@ -35,7 +35,7 @@ Maybe<void> DataTypeInferFn(user_op::InferContext* ctx) {
   const user_op::TensorDesc& x = ctx->InputTensorDesc("x", 0);
   const user_op::TensorDesc& scalar = ctx->InputTensorDesc("scalar", 0);
   CHECK_EQ_OR_RETURN(x.data_type(), scalar.data_type())
-      << Error::TypeError() << "Tensors x and scalar have diffence type";
+      << Error::TypeError() << "Tensors x and scalar have different type";
   user_op::TensorDesc* y = ctx->OutputTensorDesc("y", 0);
   *y->mut_data_type() = x.data_type();
   return Maybe<void>::Ok();
