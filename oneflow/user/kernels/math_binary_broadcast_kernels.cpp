fe90ccdbf7afef71d4c4c6ee241523924b899d68
guo-ran <360112263@qq.com>
2022-07-12 17:14:29 +0800

    broadcast floormod and fmod

--- oneflow/user/kernels/math_binary_broadcast_kernels.cpp
+++ oneflow/user/kernels/math_binary_broadcast_kernels.cpp
@@ -110,52 +110,7 @@ REGISTER_BINARY_BROADCAST_EP_KERNEL("broadcast_less_equal", ep::primitive::Binar
 REGISTER_BINARY_BROADCAST_EP_KERNEL("broadcast_logical_and", ep::primitive::BinaryOp::kLogicalAnd)
 REGISTER_BINARY_BROADCAST_EP_KERNEL("broadcast_logical_or", ep::primitive::BinaryOp::kLogicalOr)
 REGISTER_BINARY_BROADCAST_EP_KERNEL("broadcast_logical_xor", ep::primitive::BinaryOp::kLogicalXor)
-
-template<DeviceType device_type, typename T, typename K,
-         void (*binary_func)(ep::Stream* stream, const XpuVarNdarray<K>& z,
-                             const XpuVarNdarray<const T>& x, const XpuVarNdarray<const T>& y)>
-class MathBinaryBroadcastKernel final : public user_op::OpKernel, public user_op::CudaGraphSupport {
- public:
-  MathBinaryBroadcastKernel() = default;
-  ~MathBinaryBroadcastKernel() = default;
-
- private:
-  void Compute(user_op::KernelComputeContext* ctx) const override {
-    user_op::Tensor* tensor_x = ctx->Tensor4ArgNameAndIndex("x", 0);
-    user_op::Tensor* tensor_y = ctx->Tensor4ArgNameAndIndex("y", 0);
-    user_op::Tensor* tensor_z = ctx->Tensor4ArgNameAndIndex("z", 0);
-    const T* dptr_x = tensor_x->dptr<T>();
-    const T* dptr_y = tensor_y->dptr<T>();
-    K* dptr_z = tensor_z->mut_dptr<K>();
-    size_t num_axes = tensor_z->shape_view().NumAxes();
-    binary_func(ctx->stream(), XpuVarNdarray<K>(tensor_z->shape_view(), dptr_z, num_axes),
-                XpuVarNdarray<const T>(tensor_x->shape_view(), dptr_x, num_axes),
-                XpuVarNdarray<const T>(tensor_y->shape_view(), dptr_y, num_axes));
-  }
-  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
-};
-
-#define MATH_BINARY_BROADCAST_DEFAULT_FUNC_SEQ          \
-  OF_PP_MAKE_TUPLE_SEQ("broadcast_floor_mod", FloorMod) \
-  OF_PP_MAKE_TUPLE_SEQ("broadcast_fmod", FMod)
-
-#define REGISTER_MATH_BINARY_BROADCAST_KERNEL(math_type_pair, device, data_type_pair) \
-  REGISTER_USER_KERNEL(OF_PP_PAIR_FIRST(math_type_pair))                              \
-      .SetCreateFn<MathBinaryBroadcastKernel<                                         \
-          device, OF_PP_PAIR_FIRST(data_type_pair), OF_PP_PAIR_FIRST(data_type_pair), \
-          &NdarrayUtil<device, OF_PP_PAIR_FIRST(data_type_pair)>::OF_PP_CAT(          \
-              Broadcast, OF_PP_PAIR_SECOND(math_type_pair))>>()                       \
-      .SetIsMatchedHob((user_op::HobDeviceType() == device)                           \
-                       && (user_op::HobDataType("z", 0) == OF_PP_PAIR_SECOND(data_type_pair)));
-
-OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(
-    REGISTER_MATH_BINARY_BROADCAST_KERNEL, MATH_BINARY_BROADCAST_DEFAULT_FUNC_SEQ, DEVICE_TYPE_SEQ,
-    ARITHMETIC_DATA_TYPE_SEQ UNSIGNED_INT_DATA_TYPE_SEQ BOOL_DATA_TYPE_SEQ)
-// gpu half
-#ifdef WITH_CUDA
-OF_PP_SEQ_PRODUCT_FOR_EACH_TUPLE(REGISTER_MATH_BINARY_BROADCAST_KERNEL,
-                                 MATH_BINARY_BROADCAST_DEFAULT_FUNC_SEQ, (DeviceType::kCUDA),
-                                 FLOAT16_DATA_TYPE_SEQ)
-#endif
+REGISTER_BINARY_BROADCAST_EP_KERNEL("broadcast_floor_mod", ep::primitive::BinaryOp::kFloorMod)
+REGISTER_BINARY_BROADCAST_EP_KERNEL("broadcast_fmod", ep::primitive::BinaryOp::kFmod)
 
 }  // namespace oneflow
