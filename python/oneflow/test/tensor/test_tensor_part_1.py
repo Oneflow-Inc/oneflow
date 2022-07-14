2e3ebb168be9c52564d2c7a43e2070ff9d34e76e
BBuf <1182563586@qq.com>
2022-07-14 10:25:26 +0800

    fix bug

--- python/oneflow/test/tensor/test_tensor_part_1.py
+++ python/oneflow/test/tensor/test_tensor_part_1.py
@@ -755,6 +755,24 @@ class TestTensor(flow.unittest.TestCase):
         x = random_tensor(ndim=ndim).to(device)
         y = x.argmax(dim=random(0, ndim).to(int), keepdim=random().to(bool))
         return y
+    
+    @autotest(auto_backward=False, check_graph=False)
+    def test_max_bool_input_with_random_data(test_case):
+        device = random_device()
+        dim = random(1, 4).to(int)
+        x = random_tensor(ndim=4, dtype=float, requires_grad=False).to(
+            device, dtype=torch.bool
+        )
+        return x.max(dim)
+
+    @autotest(auto_backward=False, check_graph=False)
+    def test_min_bool_input_with_random_data(test_case):
+        device = random_device()
+        dim = random(1, 4).to(int)
+        x = random_tensor(ndim=4, dtype=float, requires_grad=False).to(
+            device, dtype=torch.bool
+        )
+        return x.min(dim)
 
     @flow.unittest.skip_unless_1n1d()
     @autotest(n=5)
