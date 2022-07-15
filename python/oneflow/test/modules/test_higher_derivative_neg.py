67d8f4b224bf6975aebeee41e1a38078776b86c7
Zhu, Ping <pingzhuu@outlook.com>
2022-07-15 19:01:09 +0800

    optimize testcase

--- python/oneflow/test/modules/test_higher_derivative_neg.py
+++ python/oneflow/test/modules/test_higher_derivative_neg.py
@@ -23,26 +23,27 @@ from oneflow.test_utils.automated_test_util import *
 
 class TestNegHigherDerivative(flow.unittest.TestCase):
     def test_neg_grad_grad(test_case):
-        x = flow.randn(
-            np.random.randint(1, 10), np.random.randint(1, 10), requires_grad=True
-        )
-        y = x.neg()
-        np_arr = np.random.rand(*x.shape)
-        init_grad = flow.tensor(np_arr).requires_grad_()
+        x = random_tensor(ndim=2).requires_grad_(True)
+        y = torch.neg(x)
+        np_arr = np.random.rand(*x.oneflow.shape)
+        init_grad = torch.tensor(np_arr).requires_grad_()
 
-        x_grad = flow.autograd.grad(y, x, init_grad, create_graph=True)[0]
-        test_case.assertTrue(np.allclose(-init_grad, x_grad.detach().numpy()))
-
-        x_grad_grad = flow.autograd.grad(x_grad, x, init_grad, create_graph=True)[0]
+        x_grad = torch.autograd.grad(y, x, init_grad, create_graph=True)[0]
         test_case.assertTrue(
-            np.allclose(np.full(x.shape, 0.0), x_grad_grad.detach().numpy(),)
+            np.allclose(
+                x_grad.pytorch.detach().cpu().numpy(), x_grad.oneflow.detach().numpy()
+            )
         )
 
-        init_grad_grad = flow.tensor(np_arr).requires_grad_()
-        dgrad = flow.autograd.grad(
-            x_grad, init_grad, init_grad_grad, create_graph=True
+        init_grad_grad = torch.tensor(np_arr).requires_grad_()
+        dgrad = torch.autograd.grad(
+            x_grad, init_grad, init_grad_grad, create_graph=False
         )[0]
-        test_case.assertTrue(np.allclose(-init_grad_grad, dgrad.detach().numpy(),))
+        test_case.assertTrue(
+            np.allclose(
+                dgrad.pytorch.detach().cpu().numpy(), dgrad.oneflow.detach().numpy(),
+            )
+        )
 
 
 if __name__ == "__main__":
