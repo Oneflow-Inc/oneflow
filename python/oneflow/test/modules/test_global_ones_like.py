d531bb68d184fc23f4749847436e8bd49b294250
BBuf <1182563586@qq.com>
2022-07-18 10:56:30 +0800

    fix import error

--- python/oneflow/test/modules/test_global_ones_like.py
+++ python/oneflow/test/modules/test_global_ones_like.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from sys import platlibdir
 import unittest
 from collections import OrderedDict
 import numpy as np
