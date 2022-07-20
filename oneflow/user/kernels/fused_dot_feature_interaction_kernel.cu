bb25bcf64c00c518eeb4793847decc0261a23dab
guo-ran <360112263@qq.com>
2022-07-20 16:52:12 +0800

    add cuda_graph

--- oneflow/user/kernels/fused_dot_feature_interaction_kernel.cu
+++ oneflow/user/kernels/fused_dot_feature_interaction_kernel.cu
@@ -1174,7 +1174,8 @@ bool TryLaunchTensorCoreDotBackwardKernel(user_op::KernelComputeContext* ctx) {
   }
 }
 template<typename T>
-class FusedDotFeatureInteractionKernel final : public user_op::OpKernel {
+class FusedDotFeatureInteractionKernel final : public user_op::OpKernel,
+                                               public user_op::CudaGraphSupport {
  public:
   FusedDotFeatureInteractionKernel() = default;
   ~FusedDotFeatureInteractionKernel() override = default;
@@ -1281,7 +1282,8 @@ REGISTER_FUSED_DOT_FEATURE_INTERACTION_KERNEL(float)
 REGISTER_FUSED_DOT_FEATURE_INTERACTION_KERNEL(half)
 
 template<typename T>
-class FusedDotFeatureInteractionGradKernel final : public user_op::OpKernel {
+class FusedDotFeatureInteractionGradKernel final : public user_op::OpKernel,
+                                                   public user_op::CudaGraphSupport {
  public:
   FusedDotFeatureInteractionGradKernel() = default;
   ~FusedDotFeatureInteractionGradKernel() override = default;
