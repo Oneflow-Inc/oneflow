81261a217c1e9d8a58bf3cca90fc1ff2d2d881db
lixiang007666 <88304454@qq.com>
2022-07-20 10:44:23 +0800

    Modify global graph test except P

--- python/oneflow/test/modules/test_global_gru_cell.py
+++ python/oneflow/test/modules/test_global_gru_cell.py
@@ -23,63 +23,13 @@ from oneflow.test_utils.automated_test_util import *
 
 
 @autotest(n=1, check_graph=True)
-def _test_rnn_relu_cell(test_case, placement, sbp):
+def _test_gru_cell(test_case, placement, sbp):
     batch_size = random(2, 3) * 8
     time_steps = random(2, 3) * 8
     input_size = random(2, 3) * 8
     hidden_size = random(2, 3) * 8
     has_bias = random().to(bool)
-    m = torch.nn.RNNCell(
-        input_size=input_size,
-        hidden_size=hidden_size,
-        bias=has_bias,
-        nonlinearity="relu",
-    )
-
-    weight_sbp = random_sbp(placement, max_dim=2, except_partial_sum=True)
-    m.weight_ih = torch.nn.Parameter(
-        m.weight_ih.to_global(placement=placement, sbp=weight_sbp)
-    )
-    m.weight_hh = torch.nn.Parameter(
-        m.weight_hh.to_global(placement=placement, sbp=weight_sbp)
-    )
-    if m.bias_ih is not None:
-        # bias is 1-d tensor
-        bias_sbp = random_sbp(placement, max_dim=1, except_partial_sum=True)
-        m.bias_ih = torch.nn.Parameter(
-            m.bias_ih.to_global(placement=placement, sbp=bias_sbp)
-        )
-        m.bias_hh = torch.nn.Parameter(
-            m.bias_hh.to_global(placement=placement, sbp=bias_sbp)
-        )
-
-    input_sbp = random_sbp(placement, max_dim=3, valid_split_axis=1)
-    input = random_tensor(
-        ndim=3, dim0=time_steps, dim1=batch_size, dim2=input_size
-    ).to_global(placement=placement, sbp=input_sbp)
-    hx = random_tensor(
-        ndim=2, dim0=batch_size, dim1=hidden_size, requires_grad=False
-    ).to_global(placement=placement, sbp=sbp)
-
-    for i in range(time_steps.to(int).value()):
-        hx = m(input[i], hx)
-
-    return hx
-
-
-@autotest(n=1, check_graph=True)
-def _test_rnn_tanh_cell(test_case, placement, sbp):
-    batch_size = random(2, 3) * 8
-    time_steps = random(2, 3) * 8
-    input_size = random(2, 3) * 8
-    hidden_size = random(2, 3) * 8
-    has_bias = random().to(bool)
-    m = torch.nn.RNNCell(
-        input_size=input_size,
-        hidden_size=hidden_size,
-        bias=has_bias,
-        nonlinearity="tanh",
-    )
+    m = torch.nn.GRUCell(input_size=input_size, hidden_size=hidden_size, bias=has_bias,)
 
     weight_sbp = random_sbp(placement, max_dim=2, except_partial_sum=True)
     m.weight_ih = torch.nn.Parameter(
@@ -114,16 +64,10 @@ def _test_rnn_tanh_cell(test_case, placement, sbp):
 
 class TestRNNCellGlobal(flow.unittest.TestCase):
     @globaltest
-    def test_rnn_relu_cell(test_case):
+    def test_gru_cell(test_case):
         for placement in all_placement():
             for sbp in all_sbp(placement, max_dim=2):
-                _test_rnn_relu_cell(test_case, placement, sbp)
-
-    @globaltest
-    def test_rnn_tanh_cell(test_case):
-        for placement in all_placement():
-            for sbp in all_sbp(placement, max_dim=2):
-                _test_rnn_tanh_cell(test_case, placement, sbp)
+                _test_gru_cell(test_case, placement, sbp)
 
 
 if __name__ == "__main__":
