d3a8a22e8e7ac22f97ef44fbf610f05f42ace325
clackhan <han_binbin@163.com>
2022-07-24 09:24:55 +0800

    refine

--- oneflow/core/operator/user_op.cpp
+++ oneflow/core/operator/user_op.cpp
@@ -155,7 +155,10 @@ class UserOpInferContext final : public user_op::InferContext {
   const user_op::TensorDesc& TensorDesc4ArgNameAndIndex(const std::string& arg_name,
                                                         int32_t index) const {
     auto it = arg2tensor_desc_.find(std::make_pair(arg_name, index));
-    if (it == arg2tensor_desc_.end()) { return *(user_op::TensorDesc*)nullptr; }  // NOLINT
+    if (it == arg2tensor_desc_.end()) {
+      PRINT_BUG_PROMPT_AND_ABORT();
+      return *(user_op::TensorDesc*)nullptr;  // NOLINT
+    }
     return it->second;
   }
   user_op::TensorDesc* MutTensorDesc4ArgNameAndIndex(const std::string& arg_name, int32_t index) {
