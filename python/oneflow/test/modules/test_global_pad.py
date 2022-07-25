21b21bbf372c45a899608970eb461809846d0b35
luyang <flowingsun007@163.com>
2022-07-25 07:05:20 +0000

    refine test

--- python/oneflow/test/modules/test_global_pad.py
+++ python/oneflow/test/modules/test_global_pad.py
@@ -25,7 +25,7 @@ import oneflow.unittest
 def _test_pad_1d_impl(test_case, placement, sbp):
     pad = [random(0, 5).to(int) for i in range(2)]
     x = random_tensor(
-        ndim=3, dim0=3, dim1=random(2, 8).to(int), dim2=random(2, 8).to(int)
+        ndim=3, dim0=8, dim1=random(2, 8).to(int)*8, dim2=random(2, 8).to(int)*8
     ).to_global(placement=placement, sbp=sbp)
     y = torch.nn.functional.pad(x, pad, mode=oneof("constant", "reflect", "replicate"))
     return y
@@ -35,7 +35,7 @@ def _test_pad_1d_impl(test_case, placement, sbp):
 def _test_pad_2d_impl(test_case, placement, sbp):
     pad = [random(0, 5).to(int) for i in range(4)]
     x = random_tensor(
-        ndim=4, dim0=4, dim1=8, dim2=random(2, 8).to(int), dim3=random(2, 8).to(int)
+        ndim=4, dim0=8, dim1=8, dim2=random(2, 8).to(int)*8, dim3=random(2, 8).to(int)*8
     ).to_global(placement=placement, sbp=sbp)
     y = torch.nn.functional.pad(x, pad, mode=oneof("constant", "reflect", "replicate"))
     return y
