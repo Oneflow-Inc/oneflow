bc79f342a437eaae83f834e8174cac491f95337b
liujuncheng <liujuncheng1022@gmail.com>
2022-07-25 15:17:57 +0800

    Mut

--- oneflow/user/ops/raw_reader_op.cpp
+++ oneflow/user/ops/raw_reader_op.cpp
@@ -67,7 +67,7 @@ namespace oneflow {
 }
 
 /* static */ Maybe<void> RawReaderOp::InferDataType(user_op::InferContext* ctx) {
-  *ctx->OutputDType("out", 0) = ctx->Attr<DataType>("data_type");
+  *ctx->MutOutputDType("out", 0) = ctx->Attr<DataType>("data_type");
   return Maybe<void>::Ok();
 }
 
