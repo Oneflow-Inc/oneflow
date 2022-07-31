9c1d6028c9d7a16f0f91b18226998405110fa6c2
lixinqi <lixinqi0703106@163.com>
2022-07-31 15:12:37 +0800

    merge master

--- oneflow/core/vm/stream_wait_instruction_policy.cpp
+++ oneflow/core/vm/stream_wait_instruction_policy.cpp
@@ -21,7 +21,7 @@ limitations under the License.
 #include "oneflow/core/ep/cuda/cuda_event.h"
 #include "oneflow/core/ep/cuda/cuda_stream.h"
 #include "oneflow/core/ep/cuda/cuda_device.h"
-#include "oneflow/core/vm/ep_device_context.h"
+#include "oneflow/core/vm/ep_stream_policy_base.h"
 #include "oneflow/core/vm/ep_optional_event_record_status_querier.h"
 
 namespace oneflow {
@@ -45,11 +45,10 @@ bool StreamWaitInstructionPolicy::Prescheduleable(const Stream* src, const Strea
 void StreamWaitInstructionPolicy::InitInstructionStatus(Instruction* instruction) {
   {
     auto* stream = mut_from_vm_stream();
-    NaiveStreamPolicy* naive_stream_policy =
-        CHECK_NOTNULL(dynamic_cast<NaiveStreamPolicy*>(instruction->mut_stream_policy()));
-    naive_stream_policy->InitInstructionStatus(*stream, instruction->mut_status_buffer());
-    auto* ep_device_ctx = dynamic_cast<EpDeviceCtx*>(naive_stream_policy->device_ctx().get());
-    auto* ep_event_provider = ep_device_ctx->ep_event_provider();
+    auto* ep_stream_policy_base =
+        CHECK_NOTNULL(dynamic_cast<EpStreamPolicyBase*>(instruction->mut_stream_policy()));
+    ep_stream_policy_base->InitInstructionStatus(*stream, instruction->mut_status_buffer());
+    auto* ep_event_provider = ep_stream_policy_base->ep_event_provider();
     const auto& ep_event = CHECK_NOTNULL(ep_event_provider)->GetReusedEpEvent();
     mut_ep_event() = ep_event;
   }
@@ -73,21 +72,17 @@ void StreamWaitInstructionPolicy::Compute(vm::Instruction* instruction) {
   {
     // Record event.
     auto* from_naive_stream_policy =
-        dynamic_cast<NaiveStreamPolicy*>(mut_from_vm_stream()->mut_stream_policy());
+        dynamic_cast<EpStreamPolicyBase*>(mut_from_vm_stream()->mut_stream_policy());
     CHECK_NOTNULL(from_naive_stream_policy);
-    auto* from_device_ctx = from_naive_stream_policy->device_ctx().get();
-    auto* from_ep_device_ctx = CHECK_NOTNULL(dynamic_cast<vm::EpDeviceCtx*>(from_device_ctx));
-    auto* from_stream = from_ep_device_ctx->stream();
+    auto* from_stream = from_naive_stream_policy->stream();
     from_stream->RecordEvent(ep_event->mut_event());
   }
   {
     // Wait event.
-    auto* to_naive_stream_policy =
-        dynamic_cast<NaiveStreamPolicy*>(instruction->mut_stream()->mut_stream_policy());
-    CHECK_NOTNULL(to_naive_stream_policy);
-    auto* to_device_ctx = to_naive_stream_policy->device_ctx().get();
-    auto* to_ep_device_ctx = CHECK_NOTNULL(dynamic_cast<vm::EpDeviceCtx*>(to_device_ctx));
-    auto* to_ep_stream = to_ep_device_ctx->stream();
+    auto* to_ep_stream_policy_base =
+        dynamic_cast<EpStreamPolicyBase*>(instruction->mut_stream()->mut_stream_policy());
+    CHECK_NOTNULL(to_ep_stream_policy_base);
+    auto* to_ep_stream = to_ep_stream_policy_base->stream();
     CHECK_EQ(ep_event->mut_device(), to_ep_stream->device())
         << "only support waiting events from same device";
     ep_event->mut_device()->SetAsActiveDevice();
