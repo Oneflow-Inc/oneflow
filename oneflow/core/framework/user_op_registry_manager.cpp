829e253656e77efec5c61b73c57fbb88a9acf353
binbinHan <han_binbin@163.com>
2022-07-31 19:25:07 +0800

    Refactor ccl allreduce (#8760)
    * refactor_ccl_allreduce
    
    * reslove comment
    
    * move collective_communication/ to oneflow/user/kernels/
    
    * fix static check error
    
    * fix static check error
    
    * refine
    
    * refine
    
    * refine
    
    * use collective_communication namespace
    
    * add UserOpRegistryMgr::IsOpKernelRegistered
    
    * rename CommunicationContext and ccl
    
    * remove CollectiveCommunicationFactory
    
    * refine
    
    * reslove comment and fix static check
    
    * minor fix
    
    * fix static check error
    
    Co-authored-by: Houjiang Chen <chenhoujiangcug@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- oneflow/core/framework/user_op_registry_manager.cpp
+++ oneflow/core/framework/user_op_registry_manager.cpp
@@ -138,6 +138,30 @@ Maybe<const OpKernelRegistryResult*> UserOpRegistryMgr::GetOpKernelRegistryResul
   return ret;
 }
 
+Maybe<bool> UserOpRegistryMgr::IsOpKernelRegistered(const std::string& op_type_name,
+                                                    const KernelRegContext& ctx) {
+  auto it = op_kernel_reg_result_.find(op_type_name);
+  if (it == op_kernel_reg_result_.end()) { return false; }
+  const OpKernelRegistryResult* ret = nullptr;
+  for (const auto& reg_val : it->second) {
+    if (reg_val.is_matched_hob->get(ctx)) {
+      if (ret != nullptr) {
+        std::vector<std::string> debug_msgs;
+        for (const auto& local_reg_val : it->second) {
+          if (local_reg_val.is_matched_hob->get(ctx)) {
+            debug_msgs.emplace_back(local_reg_val.is_matched_hob->DebugStr(ctx));
+          }
+        }
+        return Error::MultipleOpKernelsMatchedError(debug_msgs)
+               << "There are more than one kernels matching Current OperatorConf: " << op_type_name;
+      }
+      ret = &reg_val;
+    }
+  }
+  if (ret == nullptr) { return false; }
+  return true;
+}
+
 }  // namespace user_op
 
 }  // namespace oneflow
