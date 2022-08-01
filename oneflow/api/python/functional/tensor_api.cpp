cd3f145cafe2fa46a465a6eef36e5e33f4d8ec49
luyang <flowingsun007@163.com>
2022-08-01 14:21:04 +0800

    refine

--- oneflow/api/python/functional/tensor_api.cpp
+++ oneflow/api/python/functional/tensor_api.cpp
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include <Python.h>
-#include <cstdio>
 #include <memory>
 
 #include "oneflow/api/python/utils/tensor_utils.h"
