d4b3badb0b1b7c4a2f6d45a4634bf72a7dcb366d
luyang <flowingsun007@163.com>
2022-08-01 04:33:38 +0000

    refine

--- oneflow/core/ep/cuda/primitive/unary_functor.cuh
+++ oneflow/core/ep/cuda/primitive/unary_functor.cuh
@@ -131,6 +131,13 @@ struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kCast, Src, half> {
   OF_DEVICE_FUNC half operator()(Src src) const { return __float2half(static_cast<float>(src)); }
 };
 
+template<>
+struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kCast, half, half> {
+  OF_DEVICE_FUNC UnaryFunctor(Scalar attr0, Scalar attr1) {}
+
+  OF_DEVICE_FUNC half operator()(half src) const { return src; }
+};
+
 /*********nv_bfloat16 dtype support*********/
 #if CUDA_VERSION >= 11000
 template<>
@@ -168,6 +175,14 @@ struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kCast, nv_bfloat16, half> {
     return __float2half(__bfloat162float(src));
   }
 };
+
+template<>
+struct UnaryFunctor<DeviceType::kCUDA, UnaryOp::kCast, nv_bfloat16, nv_bfloat16> {
+  OF_DEVICE_FUNC UnaryFunctor(Scalar attr0, Scalar attr1) {}
+
+  OF_DEVICE_FUNC nv_bfloat16 operator()(nv_bfloat16 src) const { return src; }
+};
+
 #endif  // CUDA_VERSION >= 11000
 
 #define SPECIALIZATION_PSEUDO_HALF_UNARY_FUNCTOR(op)                                         \
