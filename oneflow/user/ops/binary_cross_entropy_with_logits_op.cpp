df3d82e28285c9c92bd299d6cfcbf3aca919e866
BBuf <1182563586@qq.com>
2022-08-02 11:00:13 +0800

    refine

--- oneflow/user/ops/binary_cross_entropy_with_logits_op.cpp
+++ oneflow/user/ops/binary_cross_entropy_with_logits_op.cpp
@@ -34,7 +34,7 @@ Maybe<void> InferTensorDescFn(user_op::InferContext* ctx) {
     const auto& pos_weight_desc = ctx->InputTensorDesc("pos_weight", 0);
     CHECK_EQ_OR_RETURN(pos_weight_desc.is_dynamic(), input_desc.is_dynamic());
     CHECK_EQ_OR_RETURN(pos_weight_desc.shape(),
-                  Shape({input_desc.shape().At(input_desc.shape().NumAxes() - 1)}));
+                       Shape({input_desc.shape().At(input_desc.shape().NumAxes() - 1)}));
   }
   user_op::TensorDesc* out_desc = ctx->MutOutputTensorDesc("out", 0);
   *out_desc->mut_is_dynamic() = input_desc.is_dynamic();
