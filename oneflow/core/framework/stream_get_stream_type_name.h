dad9f88603801ab450fda5e5798642f32ee680dd
Yu OuYang <xuanjiuye@gmail.com>
2022-08-02 13:36:28 +0800

    Rename StreamRole to StreamType (#8816)
    * Rename StreamRole to StreamType
    
    * rm stream_role.h
    
    * refine define
    
    * refine
    
    Co-authored-by: binbinHan <han_binbin@163.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- oneflow/core/framework/stream_get_stream_type_name.h
+++ oneflow/core/framework/stream_get_stream_type_name.h
@@ -13,18 +13,18 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#ifndef ONEFLOW_CORE_FRAMEWORK_STREAM_GET_STREAM_ROLE_NAME_H_
-#define ONEFLOW_CORE_FRAMEWORK_STREAM_GET_STREAM_ROLE_NAME_H_
+#ifndef ONEFLOW_CORE_FRAMEWORK_STREAM_GET_STREAM_TYPE_NAME_H_
+#define ONEFLOW_CORE_FRAMEWORK_STREAM_GET_STREAM_TYPE_NAME_H_
 
 #include <glog/logging.h>
 #include <string>
-#include "oneflow/core/common/stream_role.h"
+#include "oneflow/core/common/stream_type.h"
 #include "oneflow/core/common/device_type.h"
 #include "oneflow/core/framework/to_string.h"
 
 namespace oneflow {
 
-struct GetStreamRoleName : public StreamRoleVisitor<GetStreamRoleName> {
+struct GetStreamTypeName : public StreamTypeVisitor<GetStreamTypeName> {
   static const char* VisitCompute() { return "compute"; }
   static const char* VisitHost2Device() { return "h2d"; }
   static const char* VisitDevice2Host() { return "d2h"; }
@@ -40,4 +40,4 @@ struct GetStreamRoleName : public StreamRoleVisitor<GetStreamRoleName> {
 
 }  // namespace oneflow
 
-#endif  // ONEFLOW_CORE_FRAMEWORK_STREAM_GET_STREAM_ROLE_NAME_H_
+#endif  // ONEFLOW_CORE_FRAMEWORK_STREAM_GET_STREAM_TYPE_NAME_H_
