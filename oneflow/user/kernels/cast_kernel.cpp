9f28472395874df4eed9b57e26ecb2cf30b0a339
Flowingsun007 <flowingsun007@163.com>
2022-08-04 11:09:30 +0800

    handle scalar tensor

--- oneflow/user/kernels/cast_kernel.cpp
+++ oneflow/user/kernels/cast_kernel.cpp
@@ -17,6 +17,7 @@ limitations under the License.
 #include "oneflow/core/common/tensor_meta.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/cuda_graph_support.h"
+#include "oneflow/core/ep/include/primitive/cast.h"
 #include "oneflow/core/ep/include/primitive/broadcast_elementwise_unary.h"
 #include "oneflow/user/kernels/op_kernel_wrapper.h"
 
@@ -25,6 +26,14 @@ namespace oneflow {
 namespace user_op {
 
 namespace {
+
+template<typename Context>
+std::unique_ptr<ep::primitive::Cast> NewCastPrimitive(Context* ctx) {
+  const DataType in_data_type = ctx->TensorDesc4ArgNameAndIndex("in", 0)->data_type();
+  const DataType out_data_type = ctx->TensorDesc4ArgNameAndIndex("out", 0)->data_type();
+  return ep::primitive::NewPrimitive<ep::primitive::CastFactory>(ctx->device_type(), in_data_type,
+                                                                 out_data_type);
+}
 
 template<typename Context>
 std::unique_ptr<ep::primitive::BroadcastElementwiseUnary> NewBroadcastPrimitive(Context* ctx) {
@@ -52,16 +61,29 @@ class CastKernel final : public OpKernel, public user_op::CudaGraphSupport {
         << "The number of cast op's input and output elements should be equal.";
     if (input->data_type() == output->data_type() && input->dptr() == output->dptr()) { return; }
     const size_t ndim = input->shape_view().NumAxes();
-    auto primitive = NewBroadcastPrimitive(ctx);
-    CHECK(primitive);
-    primitive->Launch(ctx->stream(), ndim, input->shape_view().data(), input->stride().data(),
-                      input->dptr(), ndim, output->shape_view().data(), output->stride().data(),
-                      output->mut_dptr());
+    if (ndim == 0 && elem_cnt == 1) {
+      // TODO: use BroadcastElementwiseUnary primitive when it support 0-dim(scalar) tensor
+      auto cast_primitive = NewCastPrimitive(ctx);
+      CHECK(cast_primitive);
+      cast_primitive->Launch(ctx->stream(), input->dptr(), output->mut_dptr(), elem_cnt);
+    } else {
+      auto broadcast_primitive = NewBroadcastPrimitive(ctx);
+      CHECK(broadcast_primitive);
+      broadcast_primitive->Launch(
+          ctx->stream(), ndim, input->shape_view().data(), input->stride().data(), input->dptr(),
+          ndim, output->shape_view().data(), output->stride().data(), output->mut_dptr());
+    }
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
 auto CastPrimitiveExists() {
+  return hob::make_custom("CastPrimitiveExists", [](const user_op::KernelRegContext& ctx) -> bool {
+    return NewCastPrimitive(&ctx).operator bool();
+  });
+}
+
+auto BroadcastPrimitiveExists() {
   return hob::make_custom("BroadcastElementwiseUnaryPrimitiveExists",
                           [](const user_op::KernelRegContext& ctx) -> bool {
                             return NewBroadcastPrimitive(&ctx).operator bool();
@@ -70,7 +92,7 @@ auto CastPrimitiveExists() {
 
 REGISTER_USER_KERNEL("cast")
     .SetCreateFn<CastKernel>()
-    .SetIsMatchedHob(CastPrimitiveExists() == true)
+    .SetIsMatchedHob(CastPrimitiveExists() == true && BroadcastPrimitiveExists() == true)
     .SetInplaceProposalFn([](const user_op::InferContext& ctx,
                              const user_op::AddInplaceArgPair& AddInplaceArgPairFn) -> Maybe<void> {
       if (ctx.InputDType("in", 0) == ctx.Attr<DataType>("dtype")) {
