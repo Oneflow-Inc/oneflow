978cec3d507f0ce8721c3f337eb8f23a23e31dd4
BBuf <1182563586@qq.com>
2022-08-08 20:10:08 +0800

    add dropout1d/2d/3d module

--- python/oneflow/nn/__init__.py
+++ python/oneflow/nn/__init__.py
@@ -95,6 +95,9 @@ from oneflow.nn.modules.dataset import (
 )
 
 from oneflow.nn.modules.dropout import Dropout
+from oneflow.nn.modules.dropout import Dropout as Dropout1d
+from oneflow.nn.modules.dropout import Dropout as Dropout2d
+from oneflow.nn.modules.dropout import Dropout as Dropout3d
 from oneflow.nn.modules.flatten import Flatten
 from oneflow.nn.modules.instancenorm import (
     InstanceNorm1d,
