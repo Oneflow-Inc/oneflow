fbdd70b81b5ab792acf78c431d3f2f7fbb21ee19
jackalcooper <jackalcooper@gmail.com>
2022-08-09 11:49:19 +0800

    fix string concat

--- oneflow/core/graph/copy_task_node.cpp
+++ oneflow/core/graph/copy_task_node.cpp
@@ -88,7 +88,7 @@ OperatorConf CopyHdTaskNode::NewCopyOpConf() {
   } else {
     LOG(FATAL) << "unknow copy type: " << copy_type_;
   }
-  conf.set_name(copy_type_name + "_" + NewUniqueId());
+  conf.set_name(std::string(copy_type_name) + "_" + NewUniqueId());
   *conf.mutable_user_conf()->mutable_op_type_name() = copy_type_name;
   auto in_regst = GetSoleConsumedRegst("copy_in");
   CHECK_EQ(in_regst->NumOfLbi(), 1);
