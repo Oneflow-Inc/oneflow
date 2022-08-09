49dd66d326fd42000aa14c97198ec532a80a6771
Yinggang Wang <wyg19970408@gmail.com>
2022-08-09 21:34:21 +0800

    Support slice update kernel with stride (#8810)
    * feat(SliceUpdateKernel): support slice update kernel with stride
    
    * Perf slice update stride kernel and add test (#8822)
    
    * modify slice_util.cpp and add test
    
    * modify test_slice.py
    
    * modify test_slice.py
    
    * remove superfluous file
    
    * modify test_slice.py
    
    * test(SliceUpdate): refine test case with non-contiguous
    
    * test(SliceUpdate): test all device
    
    * format code
    
    * add comment
    
    Co-authored-by: wyg1997 <wangyinggang@foxmail.com>
    
    * fix(SliceUpdateTest): fix cpu test bug because tensor shared memory with
                          numpy array
    
    * fix(SliceUpdate): fix a fatal bug for cuda kernel
    
    * fix(SliceUpdate): fix bug when slice step more than 1
    
    * fix push bug
    
    * modify nd_index_offset_helper_test.cpp
    
    * modify nd_index_offset_helper_test.cpp
    
    * auto format by CI
    
    * modify nd_index_offset_helper_test.cpp
    
    * modify nd_index_offset_helper_test.cpp
    
    * modify slice_util.cpp
    
    * fix stride infer
    
    * fix slice kernel input stride empty bug when lazy mode
    
    Co-authored-by: Zhimin Yang <76760002+small1945@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: small1945 <2387491899@qq.com>
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

--- oneflow/user/kernels/slice_kernel.cpp
+++ oneflow/user/kernels/slice_kernel.cpp
@@ -249,8 +249,20 @@ void WriteSlice(user_op::KernelComputeContext* ctx, const user_op::Tensor* src,
     positive_stop_vec[i] = RegulateSliceStop(stop_attr[i], logical_dims[i]);
   }
 
+  // BUG(wyg): Input's stride is empty in lazy mode, so we need initialize it for kernel
+  // temporarily. Remove this and copy stride straightly after fix lazy stride infer.
+  auto CopyStrideOrCreate = [](const user_op::Tensor* tensor, SliceParams* slice_params) {
+    if (tensor->shape_view().NumAxes() != tensor->stride().size()) {
+      const Stride& stride = Stride(Shape(tensor->shape_view()));
+      std::copy(stride.begin(), stride.end(), slice_params->stride);
+    } else {
+      std::copy(tensor->stride().begin(), tensor->stride().end(), slice_params->stride);
+    }
+  };
   SliceParams large_slice_param;
+  CopyStrideOrCreate(large, &large_slice_param);
   SliceParams small_slice_param;
+  CopyStrideOrCreate(small, &small_slice_param);
   ConstructSliceParamsLarge(slice_ctx, positive_start_vec, positive_stop_vec, step_attr,
                             large->shape_view(), &large_slice_param);
   ConstructSliceParamsSmall(slice_ctx, positive_start_vec, positive_stop_vec, step_attr,
