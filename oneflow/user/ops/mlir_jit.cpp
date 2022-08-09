9aef7f747342f05bdf2315e492f8358037f555d7
yuhao <1171760467@qq.com>
2022-08-09 11:13:06 +0800

    mlir assembly infer done

--- oneflow/user/ops/mlir_jit.cpp
+++ oneflow/user/ops/mlir_jit.cpp
@@ -11,24 +11,83 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include "oneflow/core/common/data_type.pb.h"
+#include "oneflow/core/common/device_type.pb.h"
+#include "oneflow/core/common/shape.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/user/ops/nn_util.h"
 #include "oneflow/core/framework/op_generated.h"
 #include "oneflow/core/operator/operator.h"
+#include <regex>
 
 namespace oneflow {
 
 namespace {
 
 Maybe<void> InferTensorDesc(user_op::InferContext* ctx) {
-      // TODO: infer shape by extracting Ops from mlir_assembly
-      CHECK_EQ(ctx->inputs().size(), 2);
-      CHECK_EQ(ctx->outputs().size(), 1);
-      const Shape& in_shape = ctx->InputShape("in", 0);
-      Shape* out_shape = ctx->MutOutputShape("out", 0);
-      *out_shape = in_shape;
-      *ctx->MutOutputDType("out", 0) = ctx->InputDType("in", 1);
-      return Maybe<void>::Ok();
+  auto parse_mlir =
+      [](const std::string& mlir) -> std::pair<std::pair<std::string, std::string>, std::string> {
+    std::regex function_type_pattern("function_type = \\((.+?), (.+?)\\) -> (.+?),");
+    std::smatch result;
+    if (std::regex_search(mlir, result, function_type_pattern)) {
+      return {{result.str(1), result.str(2)}, result.str(3)};
+    }
+    LOG(ERROR) << "Parse mlir assembly failed";
+    return {};
+  };
+
+  auto parse_type = [](const std::string& type) -> DataType {
+    std::string full_type;
+    if (type.at(0) == 'i' || type.at(0) == 'u') {
+      full_type = type.at(0) == 'i' ? "kInt" : "kUInt";
+      full_type += type.substr(1);
+      DataType val;
+      if (DataType_Parse(full_type, &val)) { return val; }
+    } else if (type.at(0) == 'f') {
+      if ("f16" == type) return DataType::kFloat16;
+      else if ("f32" == type) return DataType::kFloat;
+      else if ("f64" == type) return DataType::kDouble;
+    }
+    LOG(ERROR) << "Parse type in mlir assembly failed";
+    return {};
+  };
+
+  auto parse_tensor = [&](const std::string& tensor) -> std::pair<Shape, DataType> {
+    std::regex tensor_type_pattern("tensor<(.*)x(.+?)>");
+    std::regex num_pattern("(\\d+)");
+    std::smatch result;
+    DimVector res;
+    if (regex_match(tensor, result, tensor_type_pattern)) {
+      auto nums = result.str(1);
+      for (std::sregex_iterator it(nums.begin(), nums.end(), num_pattern), end_it; it != end_it;
+           ++it) {
+        res.push_back(std::stoi(it->str(1)));
+      }
+      return {Shape(res), parse_type(result.str(2))};
+    }
+    LOG(ERROR) << "Parse tensor in mlir assembly failed";
+    return {};
+  };
+
+  auto mlir_assembly = ctx->Attr<std::string>("mlir_assembly");
+  auto tensors = parse_mlir(mlir_assembly);
+  auto in0_info = parse_tensor(tensors.first.first);
+  auto in1_info = parse_tensor(tensors.first.second);
+  auto out0_info = parse_tensor(tensors.second);
+
+  CHECK_EQ(in0_info.first, out0_info.first);
+  CHECK_EQ(in0_info.first, ctx->InputShape("in", 0));
+
+  CHECK_EQ(in1_info.second, out0_info.second);
+  CHECK_EQ(in1_info.second, ctx->InputDType("in", 1));
+
+  CHECK_EQ(ctx->inputs().size(), 2);
+  CHECK_EQ(ctx->outputs().size(), 1);
+  const Shape& in_shape = ctx->InputShape("in", 0);
+  Shape* out_shape = ctx->MutOutputShape("out", 0);
+  *out_shape = in_shape;
+  *ctx->MutOutputDType("out", 0) = ctx->InputDType("in", 1);
+  return Maybe<void>::Ok();
 }
 
 Maybe<void> GetSbpFn(user_op::SbpContext* ctx) {
@@ -37,8 +96,6 @@ Maybe<void> GetSbpFn(user_op::SbpContext* ctx) {
 }
 
 Maybe<void> InferDataTypeFn(user_op::InferContext* ctx) {
-  *ctx->MutOutputDType("out", 0) = ctx->InputDType("in", 0);
-  return Maybe<void>::Ok();
   *ctx->MutOutputDType("out", 0) = ctx->InputDType("in", 0);
   return Maybe<void>::Ok();
 }
