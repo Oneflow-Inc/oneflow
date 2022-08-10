a7674ec047be61466ccc19fb0c25f58768afdef7
yuhao <1171760467@qq.com>
2022-08-10 09:10:25 +0800

    refine

--- oneflow/ir/oneflow-extension/mlir_jit_op.cpp
+++ oneflow/ir/oneflow-extension/mlir_jit_op.cpp
@@ -13,20 +13,17 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-/*
-Copyright 2020 The OneFlow Authors. All rights reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 
 #include "OneFlow/OneFlowDialect.h"
+#include "oneflow/core/common/data_type.pb.h"
+#include "oneflow/core/common/device_type.pb.h"
+#include "oneflow/core/common/shape.h"
+#include "oneflow/core/framework/dtype.h"
+#include "oneflow/core/framework/framework.h"
+#include "oneflow/core/framework/op_generated.h"
+#include "oneflow/core/operator/operator.h"
+#include "oneflow/user/ops/nn_util.h"
+
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Block.h"
 #include "mlir/IR/BuiltinTypes.h"
@@ -34,14 +31,6 @@ limitations under the License.
 #include "mlir/IR/Types.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/Parser/Parser.h"
-#include "oneflow/core/common/data_type.pb.h"
-#include "oneflow/core/common/device_type.pb.h"
-#include "oneflow/core/common/shape.h"
-#include "oneflow/core/framework/dtype.h"
-#include "oneflow/core/framework/framework.h"
-#include "oneflow/user/ops/nn_util.h"
-#include "oneflow/core/framework/op_generated.h"
-#include "oneflow/core/operator/operator.h"
 
 namespace oneflow {
 
