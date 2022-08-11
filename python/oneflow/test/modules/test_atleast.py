04ce7979443e0bb65e952ebc8bf1736e4b85ebf9
WangYi <buaawangyi03@gmail.com>
2022-08-11 12:00:34 +0800

    add unittest for single input

--- python/oneflow/test/modules/test_atleast.py
+++ python/oneflow/test/modules/test_atleast.py
@@ -25,7 +25,7 @@ from oneflow.test_utils.automated_test_util import *
 @flow.unittest.skip_unless_1n1d()
 class TestAtLeast(flow.unittest.TestCase):
     @autotest(n=5)
-    def test_atleast_1d_with_random_data(test_case):
+    def test_atleast_1d_with_list_random_data(test_case):
         device = random_device()
         x = random_tensor(ndim=0).to(device)
         y = random_tensor(ndim=2).to(device)
@@ -33,7 +33,14 @@ class TestAtLeast(flow.unittest.TestCase):
         return out
 
     @autotest(n=5)
-    def test_atleast_2d_with_random_data(test_case):
+    def test_atleast_1d_with_random_data(test_case):
+        device = random_device()
+        x = random_tensor(ndim=random(low=0, high=3).to(int)).to(device)
+        out = torch.atleast_1d(x)
+        return out
+
+    @autotest(n=5)
+    def test_atleast_2d_with_list_random_data(test_case):
         device = random_device()
         x = random_tensor(ndim=0).to(device)
         y = random_tensor(ndim=1).to(device)
@@ -42,13 +49,27 @@ class TestAtLeast(flow.unittest.TestCase):
         return out
 
     @autotest(n=5)
-    def test_atleast_3d_with_random_data(test_case):
+    def test_atleast_2d_with_random_data(test_case):
+        device = random_device()
+        x = random_tensor(ndim=random(low=0, high=4).to(int)).to(device)
+        out = torch.atleast_2d(x)
+        return out
+
+    @autotest(n=5)
+    def test_atleast_3d_with_list_random_data(test_case):
         device = random_device()
         x = random_tensor(ndim=0).to(device)
         y = random_tensor(ndim=1).to(device)
         z = random_tensor(ndim=2).to(device)
         p = random_tensor(ndim=4).to(device)
         out = torch.atleast_3d([x, y, z, p])
+        return out
+
+    @autotest(n=5)
+    def test_atleast_3d_with_random_data(test_case):
+        device = random_device()
+        x = random_tensor(ndim=random(low=0, high=5).to(int)).to(device)
+        out = torch.atleast_3d(x)
         return out
 
 
