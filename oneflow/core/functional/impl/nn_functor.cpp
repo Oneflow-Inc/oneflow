af351b857848022b5c968ad5ca4b1e4ec1c623d8
WangYi <buaawangyi03@gmail.com>
2022-08-12 17:04:10 +0800

    refine code

--- oneflow/core/functional/impl/nn_functor.cpp
+++ oneflow/core/functional/impl/nn_functor.cpp
@@ -2639,7 +2639,7 @@ class PairwiseDistanceFunctor {
     const int64_t xdim = x->ndim();
     const int64_t ydim = y->ndim();
     const int64_t output_dim = xdim > ydim ? xdim : ydim;
-    auto sub = JUST(ScalarAdd(JUST(Sub(x, y, 1, false)), eps, 1, false));
+    const auto& sub = JUST(ScalarAdd(JUST(Sub(x, y, 1, false)), eps, 1, false));
     return ScalarNorm(sub, p, output_dim - 1, keepdim, NullOpt);
   }
 };
