94818cd2990b44c9d1c79c169edf7a592450e8b1
yuhao <1171760467@qq.com>
2022-08-16 21:46:32 +0800

    rename perms to dims

--- oneflow/ir/lib/OneFlow/Conversion/OneFlowToTosa.cpp
+++ oneflow/ir/lib/OneFlow/Conversion/OneFlowToTosa.cpp
@@ -100,21 +100,21 @@ bool allSignless(FunctionType funcType) {
 }
 
 Value CreateTransposeValue(Location& loc, ConversionPatternRewriter& rewriter, Value input,
-                      ArrayRef<int32_t> perms) {
-  int perms_size = perms.size();
+                      ArrayRef<int32_t> dims) {
+  int perms_size = dims.size();
   auto transpose_perms = rewriter.create<tosa::ConstOp>(
       loc, RankedTensorType::get({perms_size}, rewriter.getI32Type()),
-      rewriter.getI32TensorAttr(perms));
+      rewriter.getI32TensorAttr(dims));
   const auto shape_type = input.getType().cast<ShapedType>();
   std::vector<int64_t> ranked_type;
-  for (const auto& index : perms) ranked_type.push_back(shape_type.getDimSize(index));
+  for (const auto& index : dims) ranked_type.push_back(shape_type.getDimSize(index));
   return rewriter.create<tosa::TransposeOp>(
       loc, RankedTensorType::get(ranked_type, shape_type.getElementType()), input, transpose_perms);
 };
 
-RankedTensorType CreateTransposeType(ShapedType output, ArrayRef<int32_t> perms) {
+RankedTensorType CreateTransposeType(ShapedType output, ArrayRef<int32_t> dims) {
   std::vector<int64_t> ranked_type;
-  for (auto index : perms) ranked_type.push_back(output.getDimSize(index));
+  for (auto index : dims) ranked_type.push_back(output.getDimSize(index));
   return RankedTensorType::get(ranked_type, output.getElementType());
 };
 
