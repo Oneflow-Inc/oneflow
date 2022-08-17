12ad74b1d9e1cb51e836f28efb0ba0a53de8612d
Yipeng Li <jamesonli1313@gmail.com>
2022-08-17 18:40:08 +0800

    Add the mising sbp for optional input UserSourceOpTickInput

--- oneflow/user/ops/coco_reader_op.cpp
+++ oneflow/user/ops/coco_reader_op.cpp
@@ -77,7 +77,7 @@ namespace oneflow {
 }
 
 /* static */ Maybe<void> COCOReaderOp::GetSbp(user_op::SbpContext* ctx) {
-  ctx->NewBuilder().Split(ctx->outputs(), 0).Build();
+  ctx->NewBuilder().Broadcast(ctx->inputs()).Split(ctx->outputs(), 0).Build();
   return Maybe<void>::Ok();
 }
 
