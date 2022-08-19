1fb018a040928c32a6d14064e0dd8cd37450c56d
MARD1NO <359521840@qq.com>
2022-08-19 11:05:01 +0800

    refine

--- oneflow/user/ops/math_unary_elementwise_op.cpp
+++ oneflow/user/ops/math_unary_elementwise_op.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "oneflow/core/common/preprocessor.h"
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/user/ops/math_unary_elementwise_seq.h"
 #include "oneflow/core/framework/op_generated.h"
