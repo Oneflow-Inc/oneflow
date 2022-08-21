2725d71843ee18fdd3e74a3e1e432ddcddaa38cd
WangYi <buaawangyi03@gmail.com>
2022-08-21 20:20:33 +0800

    refine test

--- python/oneflow/test/modules/test_ctc_loss.py
+++ python/oneflow/test/modules/test_ctc_loss.py
@@ -19,11 +19,12 @@ from collections import OrderedDict
 
 import torch
 import numpy as np
-from oneflow.test_utils.automated_test_util.generators import random_device
-from oneflow.test_utils.automated_test_util.torch_flow_dual_object import random_tensor
-from oneflow.test_utils.automated_test_util import autotest
+from oneflow.test_utils.automated_test_util import *
+# from oneflow.test_utils.automated_test_util.generators import random_device
+# from oneflow.test_utils.automated_test_util.torch_flow_dual_object import random_tensor
+# from oneflow.test_utils.automated_test_util import autotest
 from oneflow.test_utils.test_util import GenArgList
-from oneflow.test_utils.automated_test_util import oneof
+# from oneflow.test_utils.automated_test_util import oneof
 
 import oneflow as flow
 import oneflow.unittest
@@ -310,24 +311,25 @@ def _test_ctc_loss(test_case, log_prob, target, input_lengths, target_lengths, z
 
 @flow.unittest.skip_unless_1n1d()
 class TestCTCLoss1n1d(flow.unittest.TestCase):
-    def test_ctc_loss(test_case):
-        for arg in gen_arg_list():
-            compare_with_np(*arg)
+    # def test_ctc_loss(test_case):
+    #     for arg in gen_arg_list():
+    #         compare_with_np(*arg)
     
     @autotest(n=5)
-    def test_ctc_loss_torch(test_case):
+    def test_mean(test_case):
         batch_size = 4
         num_classes = 5
         max_input_length = 20
         max_target_length = 10
         device = random_device()
-        ctc_loss = torch.nn.CTCLoss()
         log_probs = random_tensor(ndim=3, dim0=max_input_length, dim1=batch_size, dim2=num_classes).to(device)
-        log_probs.oneflow
+        # log_probs = random_tensor(ndim=1).to(device)
+        # log_probs.oneflow
         targets = random_tensor(ndim=2, dim0=batch_size, dim1=max_target_length, low=1, high=num_classes).to(device)
         input_lengths = random_tensor(low=max_input_length / 2, high=max_input_length, ndim=1, dim0=batch_size).to(device)
         target_lengths = random_tensor(low=max_target_length / 2, high=max_target_length, ndim=1, dim0=batch_size).to(device)
-        return ctc_loss(log_probs, targets, input_lengths, target_lengths)
+        # return torch.mean(log_probs)
+        return torch.nn.functional.ctc_loss(log_probs, targets, input_lengths, target_lengths)
 
 
 if __name__ == "__main__":
