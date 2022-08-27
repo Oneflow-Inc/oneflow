a9d6f7624ba00cfade5a14ffd3ebc5a27c3114ad
ZZK <359521840@qq.com>
2022-08-27 10:09:43 +0800

    Restruct Math Elementwise Kernel (#8936)
    * init
    
    * under debug
    
    * fix bug
    
    * fix gradient func
    
    * add more functor
    
    * try to use seq add acos
    
    * add fully support for dyx math unary operation
    
    * support dy y
    
    * remove sigmoid v2 and support backward with fill math op
    
    * support negative and remove redundant op and kernel
    
    * fix format
    
    * refine
    
    * refine format and remove redundant lazy grad register and seq
    
    * Refine list
    
    * refine
    
    * fix comment
    
    * use zeroslike instead of fill
    
    * add habs specialization
    
    * Add more test
    
    * fix compile error
    
    * remove higher order negative grad
    
    * Fix higher derivative and add annotaion
    
    * fix docs and sign bug
    
    * auto format by CI
    
    * fix format
    
    * fix
    
    * fix
    
    * auto format by CI
    
    * Revert sparse softmax crossentropy docstr
    
    * fix space
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

--- oneflow/core/autograd/higher_order_gradient_funcs/math_unary_op.cpp
+++ oneflow/core/autograd/higher_order_gradient_funcs/math_unary_op.cpp
@@ -94,34 +94,8 @@ class CosGradGrad : public OpExprGradFunction<UnaryGradGradState> {
   }
 };
 
-class NegativeGradGrad : public OpExprGradFunction<UnaryGradGradState> {
-  // neg_grad = -1 * grad
-  // So: out_grad_grad = -1 * gradgrad
-  //     x_grad_grad = 0 * gradgrad = 0
- public:
-  Maybe<void> Init(const OpExpr& op) override { return Maybe<void>::Ok(); }
-
-  Maybe<void> Capture(UnaryGradGradState* ctx, const TensorTuple& inputs,
-                      const TensorTuple& outputs, const AttrMap& attrs) const override {
-    CHECK_EQ_OR_RETURN(inputs.size(), 2);   // NOLINT(maybe-need-error-msg)
-    CHECK_EQ_OR_RETURN(outputs.size(), 1);  // NOLINT(maybe-need-error-msg)
-    ctx->x_requires_grad = inputs.at(0)->requires_grad();
-    ctx->grad_requires_grad = inputs.at(1)->requires_grad();
-    return Maybe<void>::Ok();
-  }
-
-  Maybe<void> Apply(const UnaryGradGradState* ctx, const TensorTuple& out_grads,
-                    TensorTuple* in_grads) const override {
-    in_grads->resize(2);
-    if (ctx->x_requires_grad) { in_grads->at(0) = JUST(functional::ZerosLike(out_grads.at(0))); }
-    if (ctx->grad_requires_grad) { in_grads->at(1) = JUST(functional::Negative(out_grads.at(0))); }
-    return Maybe<void>::Ok();
-  }
-};
-
 REGISTER_OP_EXPR_GRAD_FUNCTION("sin_grad", SinGradGrad);
 REGISTER_OP_EXPR_GRAD_FUNCTION("cos_grad", CosGradGrad);
-REGISTER_OP_EXPR_GRAD_FUNCTION("negative_grad", NegativeGradGrad);
 
 }  // namespace one
 }  // namespace oneflow
