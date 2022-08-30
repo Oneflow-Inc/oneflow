04e8fcbb6a898b74cc4a6496c64039cbb8a73e3d
luyang <flowingsun007@163.com>
2022-08-30 05:57:32 +0000

    skip cuda test

--- python/oneflow/test/modules/test_activation.py
+++ python/oneflow/test/modules/test_activation.py
@@ -277,6 +277,7 @@ class TestGelu(flow.unittest.TestCase):
 
 @flow.unittest.skip_unless_1n1d()
 class TestSigmoidModule(flow.unittest.TestCase):
+    @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
     @autotest(n=5, atol=1e-3)
     def test_sigmoid_flow_with_half_data(test_case):
         device = gpu_device()
