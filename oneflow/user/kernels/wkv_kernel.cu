e7caa56bd7978f2a81cf49ea700240c085f06caf
clackhan <han_binbin@163.com>
2022-09-01 12:41:49 +0800

    use fast math op in wkv kernel

--- oneflow/user/kernels/wkv_kernel.cu
+++ oneflow/user/kernels/wkv_kernel.cu
@@ -28,6 +28,50 @@ namespace {
 
 #define MIN_VALUE (-1e38)
 
+template<typename T>
+__inline__ __device__ T Exp(T x);
+
+template<>
+__inline__ __device__ float Exp<float>(float x) {
+#ifndef OF_WKV_USE_FAST_MATH
+  return __expf(x);
+#else
+  return exp(x);
+#endif
+}
+
+template<>
+__inline__ __device__ double Exp<double>(double x) {
+  return exp(x);
+}
+
+template<typename T>
+__inline__ __device__ T Div(T a, T b);
+
+template<>
+__inline__ __device__ float Div<float>(float a, float b) {
+#ifndef OF_WKV_USE_FAST_MATH
+  return __fdividef(a, b);
+#else
+  return a / b;
+#endif
+}
+
+template<>
+__inline__ __device__ double Div<double>(double a, double b) {
+  return a / b;
+}
+
+template<>
+__inline__ __device__ nv_bfloat16 Div<nv_bfloat16>(nv_bfloat16 a, nv_bfloat16 b) {
+  return a / b;
+}
+
+template<>
+__inline__ __device__ half Div<half>(half a, half b) {
+  return a / b;
+}
+
 template<typename F>
 __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C,
                                const F* __restrict__ const _w, const F* __restrict__ const _u,
@@ -39,7 +83,7 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
   const int _offset = _b * T * C + _c;
 
   F u = _u[_c];
-  const F w = -1 * exp(_w[_c]);
+  const F w = -1 * Exp(_w[_c]);
   const F* __restrict__ const k = _k + _offset;
   const F* __restrict__ const v = _v + _offset;
   F* __restrict__ const y = _y + _offset;
@@ -50,13 +94,13 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
     const int ii = i * C;
 
     F no = max(o, u + k[ii]);
-    F A = exp(o - no);
-    F B = exp(u + k[ii] - no);
-    y[ii] = (A * p + B * v[ii]) / (A * q + B);
+    F A = Exp(o - no);
+    F B = Exp(u + k[ii] - no);
+    y[ii] = Div((A * p + B * v[ii]), (A * q + B));
 
     no = max(w + o, k[ii]);
-    A = exp(w + o - no);
-    B = exp(k[ii] - no);
+    A = Exp(w + o - no);
+    B = Exp(k[ii] - no);
     p = A * p + B * v[ii];
     q = A * q + B;
     o = no;
@@ -74,7 +118,7 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
   const int _offset = _b * T * C + _c;
 
   half u = _u[_c];
-  const half w = static_cast<half>(-1.0) * static_cast<half>(exp(static_cast<float>(_w[_c])));
+  const half w = static_cast<half>(-1.0) * static_cast<half>(Exp(static_cast<float>(_w[_c])));
   const half* __restrict__ const k = _k + _offset;
   const half* __restrict__ const v = _v + _offset;
   half* __restrict__ const y = _y + _offset;
@@ -92,9 +136,9 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
     } else {
       no = u + k[ii];
     }
-    AA = static_cast<half>(exp(static_cast<float>(o - no)));
-    BB = static_cast<half>(exp(static_cast<float>(u + k[ii] - no)));
-    y[ii] = (AA * p + BB * v[ii]) / (AA * q + BB);
+    AA = static_cast<half>(Exp(static_cast<float>(o - no)));
+    BB = static_cast<half>(Exp(static_cast<float>(u + k[ii] - no)));
+    y[ii] = Div((AA * p + BB * v[ii]), (AA * q + BB));
 
     // no = max(w + o, k[ii]);
     // no = w + o > k[ii] ? w + o : k[ii];
@@ -103,8 +147,8 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
     } else {
       no = k[ii];
     }
-    AA = static_cast<half>(exp(static_cast<float>(w + o - no)));
-    BB = static_cast<half>(exp(static_cast<float>(k[ii] - no)));
+    AA = static_cast<half>(Exp(static_cast<float>(w + o - no)));
+    BB = static_cast<half>(Exp(static_cast<float>(k[ii] - no)));
     p = AA * p + BB * v[ii];
     q = AA * q + BB;
     o = no;
@@ -126,7 +170,7 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
 
   nv_bfloat16 u = _u[_c];
   const nv_bfloat16 w =
-      static_cast<nv_bfloat16>(-1.0) * static_cast<nv_bfloat16>(exp(static_cast<float>(_w[_c])));
+      static_cast<nv_bfloat16>(-1.0) * static_cast<nv_bfloat16>(Exp(static_cast<float>(_w[_c])));
   const nv_bfloat16* __restrict__ const k = _k + _offset;
   const nv_bfloat16* __restrict__ const v = _v + _offset;
   nv_bfloat16* __restrict__ const y = _y + _offset;
@@ -145,9 +189,9 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
     } else {
       no = u + k[ii];
     }
-    AA = static_cast<nv_bfloat16>(exp(static_cast<float>(o - no)));
-    BB = static_cast<nv_bfloat16>(exp(static_cast<float>(u + k[ii] - no)));
-    y[ii] = (AA * p + BB * v[ii]) / (AA * q + BB);
+    AA = static_cast<nv_bfloat16>(Exp(static_cast<float>(o - no)));
+    BB = static_cast<nv_bfloat16>(Exp(static_cast<float>(u + k[ii] - no)));
+    y[ii] = Div((AA * p + BB * v[ii]), (AA * q + BB));
 
     // no = max(w + o, k[ii]);
     // no = w + o > k[ii] ? w + o : k[ii];
@@ -156,8 +200,8 @@ __global__ void kernel_forward(const int64_t B, const int64_t T, const int64_t C
     } else {
       no = k[ii];
     }
-    AA = static_cast<nv_bfloat16>(exp(static_cast<float>(w + o - no)));
-    BB = static_cast<nv_bfloat16>(exp(static_cast<float>(k[ii] - no)));
+    AA = static_cast<nv_bfloat16>(Exp(static_cast<float>(w + o - no)));
+    BB = static_cast<nv_bfloat16>(Exp(static_cast<float>(k[ii] - no)));
     p = AA * p + BB * v[ii];
     q = AA * q + BB;
     o = no;
@@ -195,11 +239,11 @@ __global__ void kernel_backward(const int64_t B, const int64_t T, const int64_t 
   for (int i = 0; i < T; i++) {
     const int ii = i * C;
     F no = max(o, k[ii] + u);
-    F A = exp(o - no);
-    F B = exp(k[ii] + u - no);
+    F A = Exp(o - no);
+    F B = Exp(k[ii] + u - no);
 
     F num = A * p + B * v[ii];
-    F iden = 1 / (A * q + B);
+    F iden = Div(static_cast<F>(1), (A * q + B));
 
     y[i] = num * iden;
     z[i] = iden;
@@ -209,8 +253,8 @@ __global__ void kernel_backward(const int64_t B, const int64_t T, const int64_t 
     gu += gy[ii] * (v[ii] - y[i]) * B * iden;
 
     no = max(w + o, k[ii]);
-    A = exp(w + o - no);
-    B = exp(k[ii] - no);
+    A = Exp(w + o - no);
+    B = Exp(k[ii] - no);
     dpdw = A * (p + dpdw);
     dqdw = A * (q + dqdw);
     p = A * p + B * v[ii];
@@ -222,14 +266,14 @@ __global__ void kernel_backward(const int64_t B, const int64_t T, const int64_t 
   o = MIN_VALUE;
   for (int i = T - 1; i >= 0; i--) {
     const int ii = i * C;
-    F A = gy[ii] * z[i] * exp(zexp[i]);
-    F B = exp(k[ii] + o);
+    F A = gy[ii] * z[i] * Exp(zexp[i]);
+    F B = Exp(k[ii] + o);
     gk[ii] = A * (v[ii] - y[i]) + B * (gp * v[ii] + gq);
     gv[ii] = A + B * gp;
 
     F no = max(w + o, zexp[i] - k[ii] - u);
-    A = exp(w + o - no);
-    B = gy[ii] * z[i] * exp(zexp[i] - k[ii] - u - no);
+    A = Exp(w + o - no);
+    B = gy[ii] * z[i] * Exp(zexp[i] - k[ii] - u - no);
     gp = A * gp + B;
     gq = A * gq - B * y[i];
     o = no;
@@ -280,12 +324,12 @@ __global__ void kernel_backward(
     } else {
       no = k[ii] + u;
     }
-    nv_bfloat16 A = static_cast<nv_bfloat16>(exp(static_cast<float>(o - no)));
-    nv_bfloat16 B = static_cast<nv_bfloat16>(exp(static_cast<float>(k[ii] + u - no)));
+    nv_bfloat16 A = static_cast<nv_bfloat16>(Exp(static_cast<float>(o - no)));
+    nv_bfloat16 B = static_cast<nv_bfloat16>(Exp(static_cast<float>(k[ii] + u - no)));
 
     nv_bfloat16 num = A * p + B * v[ii];
     // half iden = 1 / (A * q + B);
-    nv_bfloat16 iden = static_cast<nv_bfloat16>(1.0) / (A * q + B);
+    nv_bfloat16 iden = Div(static_cast<nv_bfloat16>(1.0), static_cast<nv_bfloat16>(A * q + B));
 
     y[i] = num * iden;
     z[i] = iden;
@@ -301,8 +345,8 @@ __global__ void kernel_backward(
     } else {
       no = k[ii];
     }
-    A = static_cast<nv_bfloat16>(exp(static_cast<float>(w + o - no)));
-    B = static_cast<nv_bfloat16>(exp(static_cast<float>(k[ii] - no)));
+    A = static_cast<nv_bfloat16>(Exp(static_cast<float>(w + o - no)));
+    B = static_cast<nv_bfloat16>(Exp(static_cast<float>(k[ii] - no)));
     dpdw = A * (p + dpdw);
     dqdw = A * (q + dqdw);
     p = A * p + B * v[ii];
@@ -314,8 +358,8 @@ __global__ void kernel_backward(
   o = static_cast<nv_bfloat16>(MIN_VALUE);
   for (int i = T - 1; i >= 0; i--) {
     const int ii = i * C;
-    nv_bfloat16 A = gy[ii] * z[i] * static_cast<nv_bfloat16>(exp(static_cast<float>(zexp[i])));
-    nv_bfloat16 B = static_cast<nv_bfloat16>(exp(static_cast<float>(k[ii] + o)));
+    nv_bfloat16 A = gy[ii] * z[i] * static_cast<nv_bfloat16>(Exp(static_cast<float>(zexp[i])));
+    nv_bfloat16 B = static_cast<nv_bfloat16>(Exp(static_cast<float>(k[ii] + o)));
     gk[ii] = A * (v[ii] - y[i]) + B * (gp * v[ii] + gq);
     gv[ii] = A + B * gp;
 
@@ -326,9 +370,9 @@ __global__ void kernel_backward(
     } else {
       no = zexp[i] - k[ii] - u;
     }
-    A = static_cast<nv_bfloat16>(exp(static_cast<float>(w + o - no)));
+    A = static_cast<nv_bfloat16>(Exp(static_cast<float>(w + o - no)));
     // B = gy[ii] * z[i] * exp(zexp[i] - k[ii] - u - no);
-    B = gy[ii] * z[i] * static_cast<nv_bfloat16>(exp(static_cast<float>(zexp[i] - k[ii] - u - no)));
+    B = gy[ii] * z[i] * static_cast<nv_bfloat16>(Exp(static_cast<float>(zexp[i] - k[ii] - u - no)));
     gp = A * gp + B;
     gq = A * gq - B * y[i];
     o = no;
