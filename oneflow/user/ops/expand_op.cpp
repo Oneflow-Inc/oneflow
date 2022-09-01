85d7b430f9b5616983a2daf5895814723c9745fe
leaves-zwx <kunta0932@gmail.com>
2022-09-01 17:14:43 +0000

    fix conflict

--- oneflow/user/ops/expand_op.cpp
+++ oneflow/user/ops/expand_op.cpp
@@ -21,29 +21,25 @@ namespace oneflow {
 
 namespace {
 
-Maybe<void> InferExpandOutputShapeAndStride(const Shape& input_shape, const Stride& input_stride,
-                                            const Shape& expand_shape, Shape* output_shape,
-                                            Stride* output_stride) {
+Maybe<void> InferExpandOutputStride(const Shape& input_shape, const Stride& input_stride,
+                                    const Shape& expand_shape, Stride* output_stride) {
   CHECK_EQ_OR_RETURN(input_shape.size(), input_stride.size());  // NOLINT(maybe-need-error-msg)
   size_t lpad = expand_shape.size() - input_shape.size();
   CHECK_GE_OR_RETURN(lpad, 0);  // NOLINT(maybe-need-error-msg)
 
-  Stride expand_stride(expand_shape.size(), 0);
+  output_stride->resize(expand_shape.size(), 0);
   for (int i = expand_shape.size() - 1; i >= 0; --i) {
     int64_t dim = i < lpad ? 1 : input_shape[i - lpad];
     if (dim == expand_shape[i]) {
       if (i >= lpad) {
-        expand_stride[i] = input_stride[i - lpad];
+        output_stride->at(i) = input_stride[i - lpad];
       } else if (i < expand_shape.size() - 1) {
-        expand_stride[i] = expand_stride[i + 1] * expand_shape[i + 1];
+        output_stride->at(i) = output_stride->at(i + 1) * expand_shape[i + 1];
       }
     } else {
       CHECK_EQ_OR_RETURN(dim, 1);  // NOLINT(maybe-need-error-msg)
     }
   }
-
-  *output_shape = expand_shape;
-  *output_stride = expand_stride;
 
   return Maybe<void>::Ok();
 }
@@ -53,18 +49,19 @@ Maybe<void> InferExpandOutputShapeAndStride(const Shape& input_shape, const Stri
 /* static */ Maybe<void> ExpandOp::InferLogicalTensorDesc(user_op::InferContext* ctx) {
   const Shape& input_shape = ctx->InputShape("in", 0);
   const Stride& input_stride = ctx->InputStride("in", 0);
-  Shape* output_shape = ctx->MutOutputShape("out", 0);
-  Stride* output_stride = ctx->MutOutputStride("out", 0);
   const Shape& expand_shape = ctx->Attr<Shape>("expand_shape");
-  return InferExpandOutputShapeAndStride(input_shape, input_stride, expand_shape, output_shape,
-                                         output_stride);
+
+  ctx->SetOutputShape("out", 0, expand_shape);
+
+  Stride output_stride;
+  JUST(InferExpandOutputStride(input_shape, input_stride, expand_shape, &output_stride));
+  ctx->SetOutputStride("out", 0, output_stride);
+  return Maybe<void>::Ok();
 }
 
 /*static*/ Maybe<void> ExpandOp::InferPhysicalTensorDesc(user_op::InferContext* ctx) {
   const Shape& input_shape = ctx->InputShape("in", 0);
   const Stride& input_stride = ctx->InputStride("in", 0);
-  Shape* output_shape = ctx->MutOutputShape("out", 0);
-  Stride* output_stride = ctx->MutOutputStride("out", 0);
 
   const auto& global_expand_shape = ctx->Attr<Shape>("expand_shape");
   const auto& output_sbp = ctx->NdSbp4ArgNameAndIndex("out", 0);
@@ -73,9 +70,12 @@ Maybe<void> InferExpandOutputShapeAndStride(const Shape& input_shape, const Stri
   const auto local_view =
       GetTensorSliceView4ParallelId(device_mesh, output_sbp, global_expand_shape, rank);
   const auto& local_expand_shape = local_view.shape();
+  ctx->SetOutputShape("out", 0, local_expand_shape);
 
-  return InferExpandOutputShapeAndStride(input_shape, input_stride, local_expand_shape,
-                                         output_shape, output_stride);
+  Stride output_stride;
+  JUST(InferExpandOutputStride(input_shape, input_stride, local_expand_shape, &output_stride));
+  ctx->SetOutputStride("out", 0, output_stride);
+  return Maybe<void>::Ok();
 }
 
 /* static */ Maybe<void> ExpandOp::GetSbp(user_op::SbpContext* ctx) {
@@ -102,7 +102,7 @@ Maybe<void> InferExpandOutputShapeAndStride(const Shape& input_shape, const Stri
 }
 
 /* static */ Maybe<void> ExpandOp::InferDataType(user_op::InferContext* ctx) {
-  *ctx->MutOutputDType("out", 0) = ctx->InputDType("in", 0);
+  ctx->SetOutputDType("out", 0, ctx->InputDType("in", 0));
   return Maybe<void>::Ok();
 }
 
