2d3b4c63e39445eb3fc10ac9979b33f0e8938a50
hjchen2 <chenhoujiangcug@gmail.com>
2022-09-03 05:03:38 +0000

    add bfloat16 requires grad datatype

--- oneflow/core/common/data_type.cpp
+++ oneflow/core/common/data_type.cpp
@@ -78,6 +78,7 @@ bool IsSupportRequireGradDataType(DataType data_type) {
 #define REQUIRE_GRAD_CASE(type_cpp, type_proto) \
   case type_proto: return true;
     OF_PP_FOR_EACH_TUPLE(REQUIRE_GRAD_CASE, FLOATING_DATA_TYPE_SEQ FLOAT16_DATA_TYPE_SEQ)
+    case kBFloat16: return true;
     default: return false;
   }
 #undef REQUIRE_GRAD_CASE
