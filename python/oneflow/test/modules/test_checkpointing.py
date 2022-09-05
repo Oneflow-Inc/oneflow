d4a8d88b32f8daed601eeec5ad777e54d0c7ebac
daquexian <daquexian566@gmail.com>
2022-09-05 12:36:16 +0800

    refine test
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- python/oneflow/test/modules/test_checkpointing.py
+++ python/oneflow/test/modules/test_checkpointing.py
@@ -13,7 +13,6 @@ class TestCheckpointing(flow.unittest.TestCase):
         relu_forward_num = 0
         relu_backward_num = 0
 
-
         class MyReLU(flow.autograd.Function):
             @staticmethod
             def forward(ctx, x):
@@ -30,8 +29,7 @@ class TestCheckpointing(flow.unittest.TestCase):
                 y = ctx.saved_tensors[0]
                 return dy * (y > 0)
 
-
-        class ModuleWithoutCheckpointing(flow.nn.Module):
+        class M(flow.nn.Module):
             def __init__(self):
                 super().__init__()
                 self.conv1 = flow.nn.Conv2d(3, 3, 3)
@@ -39,20 +37,10 @@ class TestCheckpointing(flow.unittest.TestCase):
 
             def forward(self, x):
                 x = self.conv1(x)
-                x = MyReLU.apply(x)
-                x = self.conv2(x)
-                return x
-
-
-        class ModuleWithCheckpointing(flow.nn.Module):
-            def __init__(self):
-                super().__init__()
-                self.conv1 = flow.nn.Conv2d(3, 3, 3)
-                self.conv2 = flow.nn.Conv2d(3, 3, 3)
-
-            def forward(self, x):
-                x = self.conv1(x)
-                x = flow.utils.checkpoint.checkpoint(MyReLU.apply, x)
+                if checkpointing:
+                    x = flow.utils.checkpoint.checkpoint(MyReLU.apply, x)
+                else:
+                    x = MyReLU.apply(x)
                 x = self.conv2(x)
                 return x
 
@@ -60,12 +48,14 @@ class TestCheckpointing(flow.unittest.TestCase):
         x1 = flow.randn(1, 3, 8, 16).requires_grad_()
         x2 = x1.detach().clone().requires_grad_()
 
-        model_without_checkpointing = ModuleWithoutCheckpointing()
-        y1 = model_without_checkpointing(x1)
+        m = M()
+
+        checkpointing = True
+        y1 = m(x1)
         y1.sum().backward()
 
-        model_with_checkpointing = ModuleWithCheckpointing()
-        y2 = model_with_checkpointing(x2)
+        checkpointing = False
+        y2 = m(x2)
         y2.sum().backward()
 
         test_case.assertTrue(np.array_equal(y1, y2))
