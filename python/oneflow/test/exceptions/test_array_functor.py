2b13e2f0cc51df6aaa2d303cea653a32f6e2ed2d
Zhu, Ping <pingzhuu@outlook.com>
2022-09-13 10:33:26 +0800

    add testcase

--- python/oneflow/test/exceptions/test_array_functor.py
+++ python/oneflow/test/exceptions/test_array_functor.py
@@ -33,6 +33,16 @@ class TestArrayError(flow.unittest.TestCase):
         test_case.assertTrue(
             "The expanded size of the tensor" in str(context.exception)
         )
+    
+    def test_broadcast_like_numaxes_runtime_error(test_case):
+        with test_case.assertRaises(Exception) as context:
+            x = flow.ones((2, 2, 2), dtype=flow.float32, requires_grad=True)
+            like = flow.ones((2, 2), dtype=flow.float32, requires_grad=True)
+            y = flow._C.broadcast_like(x, like)
+        print(str(context.exception))
+        test_case.assertTrue(
+            "The number of sizes provided" in str(context.exception)
+        )
 
     def test_concat_index_error(test_case):
         with test_case.assertRaises(Exception) as context:
