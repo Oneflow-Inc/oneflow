df836909cebeabd3c7e34b597aee96ef21039194
songyicheng <int.rejoice@gmail.com>
2022-09-20 07:12:58 +0000

    add profiler for sgd foreach test

--- python/oneflow/test/modules/test_optim_sgd_foreach.py
+++ python/oneflow/test/modules/test_optim_sgd_foreach.py
@@ -21,12 +21,14 @@ import time
 import pdb
 
 import numpy as np
+from oneflow import optim
 from oneflow.test_utils.test_util import GenArgDict
 from optimizer_test_util import clip_grad_norm_np
 
 import oneflow as flow
 from oneflow.nn.parameter import Parameter
 import oneflow.profiler as profiler
+from oneflow.profiler.events import CustomEvent, KernelEvent
 
 def compare_with_sgd_foreach(
     test_case,
@@ -107,13 +109,51 @@ def compare_with_sgd_foreach(
                 sgd.load_state_dict(state_dict)
         return x
 
-    @get_run_time
-    def train_not_foreach():
-        return _train_with_sgd(False)
+    def get_event(events, name: str, input_shapes: str = "-"):
+        for item in events:
+            if isinstance(item, CustomEvent):
+                print(item.name)
+                if item.name == name:
+                    return item
+            if isinstance(item, KernelEvent):
+                print(item.name, item.input_shapes)
+                if item.name == name and item.input_shapes == input_shapes:
+                    return item
+        return None
 
-    @get_run_time
+    def train_with_profiler(foreach):
+        activities = [profiler.ProfilerActivity.CPU]
+        if device == 'cuda':
+            activities.append(profiler.ProfilerActivity.CUDA)
+
+        with profiler.profile(
+            activities=activities,
+            record_shapes=True,
+            record_bandwidth_for_cuda=True,
+        ) as prof:
+            with profiler.record_function("foreach:{}".format(foreach)) as f:
+                ret =  _train_with_sgd(foreach)
+
+        events = prof.key_averages(group_by_input_shape=True)
+
+        optim_event = get_event(
+            events, "conv2d", "[(2,3,32,32), (6,3,5,5)]"
+        )
+        return ret
+        test_case.assertIsNotNone(optim_event)
+        print('cpu time:{}, cpu totol time:{}, cuda time:{}, cuda total time:{}, band width:{}'.format(
+            optim_event.cpu_time, optim_event.cpu_time_total, optim_event.cuda_time,
+            optim_event.cuda_time_total, optim_event.bandwidth
+        ))
+        print('-' * 100)
+
+        return ret
+
+    def train_not_foreach():
+        return train_with_profiler(False)
+
     def train_foreach():
-        return _train_with_sgd(True)
+        return train_with_profiler(True)
 
     a = train_not_foreach()
     b = train_foreach()
@@ -124,8 +164,6 @@ def compare_with_sgd_foreach(
                 a[i].numpy().flatten(), b[i].numpy().flatten(), rtol=0.0001, atol=0.0001
             )
         )
-
-    print('-' * 100)
     
 @flow.unittest.skip_unless_1n1d()
 class TestOptimizers(flow.unittest.TestCase):
@@ -133,17 +171,17 @@ class TestOptimizers(flow.unittest.TestCase):
         arg_dict = OrderedDict()
         #arg_dict["device"] = ["cpu", "cuda"]
         arg_dict["device"] = ["cuda"]
-        arg_dict["x_shape"] = [(10,), (100,), (1000,), (10000,)]
-        arg_dict["tensor_num"] = [10, 100]
+        arg_dict["x_shape"] = [(10,)]
+        arg_dict["tensor_num"] = [10]
         # arg_dict["momentum"] = [0.0, 0.9]
         # arg_dict["dampening"] = [0.0, 0.9]
         # arg_dict["nesterov"] = [True, False]
         # arg_dict["maximize"] = [True, False]
-        arg_dict["weight_decay"] = [0.9, 0.0]
-        arg_dict["learning_rate"] = [1.0, 0.1, 1e-3]
+        arg_dict["weight_decay"] = [0.9]
+        arg_dict["learning_rate"] = [0.1]
         arg_dict["train_iters"] = [10, 100]
         arg_dict["reload_state_step"] = [5]  # save and load optim state
-        arg_dict["save_load_by_pickle"] = [False, True]
+        arg_dict["save_load_by_pickle"] = [True]
         for arg in GenArgDict(arg_dict):
             compare_with_sgd_foreach(test_case, **arg)
 
