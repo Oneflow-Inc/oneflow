ed038cf14064b25c2d506cd60ee8ad74794df881
BBuf <1182563586@qq.com>
2022-09-20 07:10:47 +0000

    add oneflow hub docs

--- python/oneflow/hub.py
+++ python/oneflow/hub.py
@@ -373,7 +373,7 @@ def _load_entry_from_hubconf(m, model):
 
 
 def get_dir():
-    r"""
+    """
     Get the OneFlow Hub cache directory used for storing downloaded models & weights.
     If :func:`~oneflow.hub.set_dir` is not called, default path is ``$ONEFLOW_HOME/hub`` where
     environment variable ``$ONEFLOW_HOME`` defaults to ``$XDG_CACHE_HOME/oneflow``.
@@ -391,8 +391,9 @@ def get_dir():
 
 
 def set_dir(d):
-    r"""
+    """
     Optionally set the Torch Hub directory used to save downloaded models & weights.
+
     Args:
         d (str): path to a local folder to save downloaded models & weights.
     """
@@ -401,13 +402,13 @@ def set_dir(d):
 
 
 def list(github, force_reload=False, skip_validation=False, trust_repo=None):
-    r"""
+    """
     List all callable entrypoints available in the repo specified by ``github``.
+
     Args:
         github (str): a string with format "repo_owner/repo_name[:ref]" with an optional
             ref (tag or branch). If ``ref`` is not specified, the default branch is assumed to be ``main`` if
-            it exists, and otherwise ``master``.
-            Example: ' Oneflow-Inc/vision:0.2.0'
+            it exists, and otherwise ``master``. Example: ' Oneflow-Inc/vision:0.2.0'
         force_reload (bool, optional): whether to discard the existing cache and force a fresh download.
             Default is ``False``.
         skip_validation (bool, optional): if ``False``, oneflowhub will check that the branch or commit
@@ -417,22 +418,29 @@ def list(github, force_reload=False, skip_validation=False, trust_repo=None):
         trust_repo (bool, str or None): ``"check"``, ``True``, ``False`` or ``None``.
             This parameter was introduced in v1.12 and helps ensuring that users
             only run code from repos that they trust.
-            - If ``False``, a prompt will ask the user whether the repo should
-              be trusted.
-            - If ``True``, the repo will be added to the trusted list and loaded
-              without requiring explicit confirmation.
+            - If ``False``, a prompt will ask the user whether the repo should be trusted.
+
+            - If ``True``, the repo will be added to the trusted list and loaded without 
+              requiring explicit confirmation.
+
             - If ``"check"``, the repo will be checked against the list of
               trusted repos in the cache. If it is not present in that list, the
               behaviour will fall back onto the ``trust_repo=False`` option.
-            - If ``None``: this will raise a warning, inviting the user to set
+
+            - If ``None``, this will raise a warning, inviting the user to set
               ``trust_repo`` to either ``False``, ``True`` or ``"check"``. This
               is only present for backward compatibility and will be removed in
               v1.14.
+        
             Default is ``None`` and will eventually change to ``"check"`` in v1.14.
+    
     Returns:
         list: The available callables entrypoint
-    Example:
+    
+    For example:
+
         >>> entrypoints = oneflow.hub.list('Oneflow-Inc/vision', force_reload=True)
+    
     """
     repo_dir = _get_cache_or_reload(
         github,
@@ -461,8 +469,9 @@ def list(github, force_reload=False, skip_validation=False, trust_repo=None):
 
 
 def help(github, model, force_reload=False, skip_validation=False, trust_repo=None):
-    r"""
+    """
     Show the docstring of entrypoint ``model``.
+
     Args:
         github (str): a string with format <repo_owner/repo_name[:ref]> with an optional
             ref (a tag or a branch). If ``ref`` is not specified, the default branch is assumed
@@ -478,19 +487,25 @@ def help(github, model, force_reload=False, skip_validation=False, trust_repo=No
         trust_repo (bool, str or None): ``"check"``, ``True``, ``False`` or ``None``.
             This parameter was introduced in v1.12 and helps ensuring that users
             only run code from repos that they trust.
+            
             - If ``False``, a prompt will ask the user whether the repo should
               be trusted.
+            
             - If ``True``, the repo will be added to the trusted list and loaded
               without requiring explicit confirmation.
+            
             - If ``"check"``, the repo will be checked against the list of
               trusted repos in the cache. If it is not present in that list, the
               behaviour will fall back onto the ``trust_repo=False`` option.
+            
             - If ``None``: this will raise a warning, inviting the user to set
               ``trust_repo`` to either ``False``, ``True`` or ``"check"``. This
               is only present for backward compatibility and will be removed in
               v1.14.
+            
             Default is ``None`` and will eventually change to ``"check"`` in v1.14.
-    Example:
+    
+    For example:
         >>> print(oneflow.hub.help('Oneflow-Inc/vision', 'resnet18', force_reload=True))
     """
     repo_dir = _get_cache_or_reload(
@@ -525,7 +540,7 @@ def load(
     skip_validation=False,
     **kwargs,
 ):
-    r"""
+    """
     Load a model from a github repo or a local directory.
     Note: Loading a model is the typical use case, but this can also be used to
     for loading other objects such as tokenizers, loss functions, etc.
@@ -534,6 +549,7 @@ def load(
     ref (a tag or a branch).
     If ``source`` is 'local', ``repo_or_dir`` is expected to be a
     path to a local directory.
+    
     Args:
         repo_or_dir (str): If ``source`` is 'github',
             this should correspond to a github repo with format ``repo_owner/repo_name[:ref]`` with
@@ -548,17 +564,22 @@ def load(
         trust_repo (bool, str or None): ``"check"``, ``True``, ``False`` or ``None``.
             This parameter was introduced in v1.12 and helps ensuring that users
             only run code from repos that they trust.
+            
             - If ``False``, a prompt will ask the user whether the repo should
               be trusted.
+            
             - If ``True``, the repo will be added to the trusted list and loaded
               without requiring explicit confirmation.
+            
             - If ``"check"``, the repo will be checked against the list of
               trusted repos in the cache. If it is not present in that list, the
               behaviour will fall back onto the ``trust_repo=False`` option.
+            
             - If ``None``: this will raise a warning, inviting the user to set
               ``trust_repo`` to either ``False``, ``True`` or ``"check"``. This
               is only present for backward compatibility and will be removed in
               v1.14.
+            
             Default is ``None`` and will eventually change to ``"check"`` in v1.14.
         force_reload (bool, optional): whether to force a fresh download of
             the github repo unconditionally. Does not have any effect if
@@ -572,10 +593,12 @@ def load(
             requests to the GitHub API; you can specify a non-default GitHub token by setting the
             ``GITHUB_TOKEN`` environment variable. Default is ``False``.
         **kwargs (optional): the corresponding kwargs for callable ``model``.
+    
     Returns:
         The output of the ``model`` callable when called with the given
         ``*args`` and ``**kwargs``.
-    Example:
+    
+    For example:
         >>> # from a github repo
         >>> repo = 'Oneflow-Inc/vision'
         >>> model = oneflow.hub.load(repo, 'resnet50', weights='ResNet50_Weights.IMAGENET1K_V1')
@@ -606,8 +629,9 @@ def load(
 
 
 def _load_local(hubconf_dir, model, *args, **kwargs):
-    r"""
+    """
     Load a model from a local directory with a ``hubconf.py``.
+    
     Args:
         hubconf_dir (str): path to a local directory that contains a
             ``hubconf.py``.
@@ -615,9 +639,11 @@ def _load_local(hubconf_dir, model, *args, **kwargs):
             ``hubconf.py``.
         *args (optional): the corresponding args for callable ``model``.
         **kwargs (optional): the corresponding kwargs for callable ``model``.
+    
     Returns:
         a single model with corresponding pretrained weights.
-    Example:
+    
+    For example:
         >>> # xdoctest: +SKIP("stub local path")
         >>> path = '/some/local/path/oneflow/vision'
         >>> model = _load_local(path, 'resnet50', weights='ResNet50_Weights.IMAGENET1K_V1')
@@ -636,7 +662,8 @@ def _load_local(hubconf_dir, model, *args, **kwargs):
 
 
 def download_url_to_file(url, dst, hash_prefix=None, progress=True):
-    r"""Download object at the given URL to a local path.
+    """Download object at the given URL to a local path.
+    
     Args:
         url (str): URL of the object to download
         dst (str): Full path where object will be saved, e.g. ``/tmp/temporary_file``
@@ -644,7 +671,8 @@ def download_url_to_file(url, dst, hash_prefix=None, progress=True):
             Default: None
         progress (bool, optional): whether or not to display a progress bar to stderr
             Default: True
-    Example:
+    
+    For example:
         >>> # xdoctest: +REQUIRES(POSIX)
         >>> oneflow.hub.download_url_to_file('https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth', '/tmp/temporary_file')
     """
@@ -738,13 +766,14 @@ def load_state_dict_from_url(
     check_hash: bool = False,
     file_name: Optional[str] = None,
 ) -> Dict[str, Any]:
-    r"""Loads the Torch serialized object at the given URL.
+    """Loads the Torch serialized object at the given URL.
     If downloaded file is a zip file, it will be automatically
     decompressed.
     If the object is already present in `model_dir`, it's deserialized and
     returned.
     The default value of ``model_dir`` is ``<hub_dir>/checkpoints`` where
     ``hub_dir`` is the directory returned by :func:`~oneflow.hub.get_dir`.
+    
     Args:
         url (str): URL of the object to download
         model_dir (str, optional): directory in which to save the object
@@ -757,7 +786,8 @@ def load_state_dict_from_url(
             ensure unique names and to verify the contents of the file.
             Default: False
         file_name (str, optional): name for the downloaded file. Filename from ``url`` will be used if not set.
-    Example:
+    
+    For example:
         >>> state_dict = oneflow.hub.load_state_dict_from_url('https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth')
     """
     # Issue warning to move data if old env is set
