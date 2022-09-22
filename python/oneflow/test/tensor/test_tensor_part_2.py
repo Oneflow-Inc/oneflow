3d7966a9ac152175c3199de8f71fd0d98e3f19be
clackhan <han_binbin@163.com>
2022-09-22 18:42:35 +0800

    fix_global_tensor_detach_bug

--- python/oneflow/test/tensor/test_tensor_part_2.py
+++ python/oneflow/test/tensor/test_tensor_part_2.py
@@ -1055,6 +1055,16 @@ class TestTensorNumpy(flow.unittest.TestCase):
         z = x.repeat_interleave(y, 1, output_size=2)
         return z
 
+    @flow.unittest.skip_unless_1n2d()
+    @autotest(n=5)
+    def test_global_tensor_detach(test_case):
+        device = random_device()
+        placement = flow.placement(device, [0, 1])
+        a = flow.ones(2, 3).to_global(placement, flow.sbp.broadcast)
+        test_case.assertTrue(a.is_leaf)
+        b = a.float().clone().detach()
+        test_case.assertTrue(b.is_leaf)
+
 
 if __name__ == "__main__":
     unittest.main()
