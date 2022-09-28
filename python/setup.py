4273391a4e5e92b12a0c4153e783e027cfbf44e7
farmerzhang1 <zhang2000dc@gmail.com>
2022-09-28 03:31:05 +0000

    use `console_scripts` to install oneflow-torch to PATH

--- python/setup.py
+++ python/setup.py
@@ -98,4 +98,7 @@ setup(
     zip_safe=False,
     distclass=BinaryDistribution,
     cmdclass={"install": InstallPlatlib},
+    entry_points={
+        'console_scripts': ['oneflow-torch=oneflow_torch:main']
+    }
 )
