bcee2848fc060f867ef05285c20741c9b69522b6
farmerzhang1 <zhang2000dc@gmail.com>
2022-09-30 12:44:29 +0000

    use positional argument with 2 choices

--- python/mock_torch.py
+++ python/mock_torch.py
@@ -4,17 +4,16 @@ import oneflow
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--enable", default=False, action="store_true", required=False)
-parser.add_argument("--disable", default=False, action="store_true", required=False)
+parser.add_argument('mock', choices=['enable', 'disable'], help='enable/disable mocking \'import torch\'') 
 args = parser.parse_args()
 
 torch_env = Path(oneflow.__path__[0], 'mock_torch')
 
 
 def main():
-    if args.enable:
+    if args.mock == 'enable':
         print('export PYTHONPATH=' + str(torch_env) + ':$PYTHONPATH')
-    elif args.disable:
+    elif args.mock == 'disable':
         paths = os.environ['PYTHONPATH'].rstrip(':').split(':')
         paths = [x for x in paths if x != str(torch_env)]
         path = ':'.join(paths)
