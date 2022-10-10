4664ba90b6c0abe165c9c5817b1596d789cb7501
farmerzhang1 <zhang2000dc@gmail.com>
2022-10-10 04:08:02 +0000

    don't run setup.py

--- ci/test/test_mock.sh
+++ ci/test/test_mock.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-ONEFLOW_PY_DIR="$PWD/python"
-MOCK_TORCH=$ONEFLOW_PY_DIR/oneflow/test/misc/test_mock_torch.py
+MOCK_TORCH=$PWD/python/oneflow/test/misc/test_mock_torch.py
 
-cd $ONEFLOW_PY_DIR
-python3 setup.py install --user
 eval $(oneflow-mock-torch) # default argument is enable
 if [[ "$(python3 $MOCK_TORCH)" != *"True"* ]]; then
     exit 1
