c118d823932c668fd5cd4471c0aac29453bbd572
farmerzhang1 <zhang2000dc@gmail.com>
2022-10-10 03:47:44 +0000

    refactor test script

--- ci/test/test_mock.sh
+++ ci/test/test_mock.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-oneflow_py_dir="$PWD/python"
-simple_torch_file=$oneflow_py_dir/oneflow/test/misc/test_mock_torch.py
-python3 -m pip install torch
+ONEFLOW_PY_DIR="$PWD/python"
+MOCK_TORCH=$ONEFLOW_PY_DIR/oneflow/test/misc/test_mock_torch.py
 
-cd $oneflow_py_dir
+cd $ONEFLOW_PY_DIR
 python3 setup.py install --user
-eval $(oneflow-mock-torch enable)
-if [[ "$(python3 $simple_torch_file)" != *"True"* ]]; then
-exit 1
+eval $(oneflow-mock-torch) # default argument is enable
+if [[ "$(python3 $MOCK_TORCH)" != *"True"* ]]; then
+    exit 1
 fi
 eval $(oneflow-mock-torch disable)
-if [[ "$(python3 $simple_torch_file)" != *"False"* ]]; then
-exit 1
+if [[ "$(python3 $MOCK_TORCH)" != *"False"* ]]; then
+    exit 1
 fi
+eval $(oneflow-mock-torch enable)
+if [[ "$(python3 $MOCK_TORCH)" != *"True"* ]]; then
+    exit 1
+fi
+eval $(oneflow-mock-torch disable) # recover
