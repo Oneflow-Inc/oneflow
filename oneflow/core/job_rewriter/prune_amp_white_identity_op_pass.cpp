c2594568178fb0dd08152ed1908c59ef0dcb9f1d
leaves-zwx <kunta0932@gmail.com>
2022-10-20 16:35:41 +0000

    ref algorithm

--- oneflow/core/job_rewriter/prune_amp_white_identity_op_pass.cpp
+++ oneflow/core/job_rewriter/prune_amp_white_identity_op_pass.cpp
@@ -41,9 +41,10 @@ class PruneAmpWhiteIdentityOpPass final : public JobPass {
 Maybe<void> PruneAmpWhiteIdentityOpPass::Apply(Job* job, JobPassCtx* ctx) const {
   if (!ctx->job_desc().prune_amp_white_identity_ops()) { return Maybe<void>::Ok(); }
   if (!NeedDoPass(job)) { return Maybe<void>::Ok(); }
+
   const OpGraph op_graph(*job);
-  JobBuilder job_builder(job);
-  HashMap<std::string, OperatorConf> op_name2op_conf;
+  using ArgPair = std::pair<const OpNode*, std::string>;  // OpNode and ibn
+  HashMap<ArgPair, std::string> to_update_args;
   HashSet<std::string> del_op_names;
 
   HashSet<std::string> ctrl_in_op_names;
@@ -54,41 +55,61 @@ Maybe<void> PruneAmpWhiteIdentityOpPass::Apply(Job* job, JobPassCtx* ctx) const 
   });
 
   op_graph.ForEachNode([&](const OpNode* op_node) {
-    const std::string op_name = op_node->op().op_name();
+    const std::string& op_name = op_node->op().op_name();
     const OperatorConf& op_conf = op_node->op().op_conf();
+    // not amp identity op
     if (!IsAmpIdentityOp(op_conf)) { return; }
+    // has ctrl in
     if (!op_conf.ctrl_in_op_name().empty()) { return; }
+    // is ctrl in of another op
     if (ctrl_in_op_names.find(op_name) != ctrl_in_op_names.end()) { return; }
+    // not sole in
     if (op_node->in_edges().size() != 1) { return; }
-    if (del_op_names.find(op_name) != del_op_names.end()) { return; }
-    del_op_names.insert(op_conf.name());
 
-    const OpNode* last_amp_id_op = op_node;
-    const OpNode* consumer = last_amp_id_op->SoleOutEdge()->dst_node();
-    while (IsAmpIdentityOp(consumer->op().op_conf())) {
-      if (del_op_names.insert(consumer->op().op_name()).second) { last_amp_id_op = consumer; }
-      consumer = consumer->SoleOutEdge()->dst_node();
-    }
-    const auto& new_in_lbi = op_node->op().BnInOp2Lbi(op_node->op().SoleIbn());
-    const auto& old_in_lbi = last_amp_id_op->op().BnInOp2Lbi(last_amp_id_op->op().SoleObn());
+    // add amp identity to del list
+    CHECK(del_op_names.insert(op_name).second);
 
-    const auto& consumer_op_name = consumer->op().op_name();
-    auto iter = op_name2op_conf.find(consumer_op_name);
-    if (iter == op_name2op_conf.end()) {
-      iter = op_name2op_conf.emplace(consumer->op().op_name(), consumer->op().op_conf()).first;
-    }
-    OperatorConf& consumer_op_conf = iter->second;
-    for (const std::string& ibn : consumer->op().input_bns()) {
-      if (consumer->op().BnInOp2Lbi(ibn) == old_in_lbi) {
-        const auto& old_val = ReplaceInputLbnInOpCustomizedConf(&consumer_op_conf, ibn,
-                                                                GenLogicalBlobName(new_in_lbi));
-        CHECK_EQ(GenLogicalBlobName(old_in_lbi), old_val);
+    std::string in_lbn = GenLogicalBlobName(op_node->op().BnInOp2Lbi(op_node->op().SoleIbn()));
+    std::string out_lbn = GenLogicalBlobName(op_node->op().BnInOp2Lbi(op_node->op().SoleObn()));
+    // consume a deleted amp identity, find it's preceding lbn
+    auto iter = to_update_args.find(ArgPair{op_node, op_node->op().SoleIbn()});
+    if (iter != to_update_args.end()) { in_lbn = iter->second; }
+
+    for (const OpEdge* out_edge : op_node->out_edges()) {
+      const OpNode* consumer = out_edge->dst_node();
+      if (del_op_names.find(consumer->op().op_name()) == del_op_names.end()) {
+        for (const std::string& ibn : consumer->op().input_bns()) {
+          if (GenLogicalBlobName(consumer->op().BnInOp2Lbi(ibn)) == out_lbn) {
+            CHECK(to_update_args.emplace(ArgPair{consumer, ibn}, in_lbn).second);
+          }
+        }
+      } else {
+        // comsumer is an already deleted amp identity
+        // modify it's succeeding op's input arg lbn
+        bool find_arg = false;
+        for (auto& arg : to_update_args) {
+          if (arg.second == out_lbn) {
+            arg.second = in_lbn;
+            find_arg = true;
+          }
+        }
+        CHECK(find_arg);
       }
     }
   });
 
-  for (const auto& pair : op_name2op_conf) { job_builder.MutOpsOnlyOnce({pair.second}); }
-
+  JobBuilder job_builder(job);
+  std::vector<OperatorConf> to_update_op_confs;
+  for (const auto& arg_lbn : to_update_args) {
+    const Operator& op = arg_lbn.first.first->op();
+    OperatorConf op_conf = op.op_conf();
+    const auto& ibn = arg_lbn.first.second;
+    auto old_lbn = GenLogicalBlobName(op.BnInOp2Lbi(ibn));
+    const auto& old_val = ReplaceInputLbnInOpCustomizedConf(&op_conf, ibn, arg_lbn.second);
+    CHECK_EQ(old_lbn, old_val);
+    to_update_op_confs.emplace_back(std::move(op_conf));
+  }
+  job_builder.MutOpsOnlyOnce(to_update_op_confs);
   job_builder.DelOps(std::vector<std::string>{del_op_names.begin(), del_op_names.end()});
 
   return Maybe<void>::Ok();
