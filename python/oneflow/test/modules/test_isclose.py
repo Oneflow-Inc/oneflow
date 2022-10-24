2c8e49a25aa0a65aa391bb2d1a95624dbbf701ee
farmerzhang1 <zhang2000dc@gmail.com>
2022-10-24 07:05:15 +0000

    generate one random data, the other is perturbation

--- python/oneflow/test/modules/test_isclose.py
+++ python/oneflow/test/modules/test_isclose.py
@@ -4,96 +4,60 @@ from collections import OrderedDict
 import numpy as np
 
 from oneflow.test_utils.automated_test_util import *
-from oneflow.test_utils.test_util import GenArgList
 
 import oneflow as flow
 import oneflow.unittest
 
-
-def _test_less_normal(test_case, device):
-    input1 = flow.tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device)
-    )
-    input2 = flow.tensor(
-        np.random.randn(2, 6, 5, 3), dtype=flow.float32, device=flow.device(device)
-    )
-    of_out = flow.isclose(input1, input2)
-    np_out = np.isclose(input1.numpy(), input2.numpy())
-    test_case.assertTrue(np.array_equal(of_out.numpy(), np_out))
+rtol = 1e-3
 
 
-def _test_less_symbol(test_case, device):
-    input1 = flow.tensor(
-        np.array([1, 1, 4]).astype(np.float32),
-        dtype=flow.float32,
-        device=flow.device(device),
+def perturbate(device, shape, x):
+    diff = (
+        random_tensor(len(shape), *shape, low=-1, high=1, requires_grad=False).to(
+            device
+        )
+        * rtol
+        * 2
     )
-    input2 = flow.tensor(
-        np.array([1, 2, 3]).astype(np.float32),
-        dtype=flow.float32,
-        device=flow.device(device),
-    )
-    of_out = flow.isclose(input1, input2)
-    np_out = np.isclose(input1.numpy(), input2.numpy())
-    test_case.assertTrue(np.array_equal(of_out.numpy(), np_out))
+    return x + diff
 
 
 @flow.unittest.skip_unless_1n1d()
-class TestLess(flow.unittest.TestCase):
-    def test_less(test_case):
-        arg_dict = OrderedDict()
-        arg_dict["test_fun"] = [_test_less_normal, _test_less_symbol]
-        arg_dict["device"] = ["cpu", "cuda"]
-        for arg in GenArgList(arg_dict):
-            arg[0](test_case, *arg[1:])
-
-    @autotest(n=10, auto_backward=False, check_graph=True)
-    def test_less_with_random_data(test_case):
+class TestIsClose(flow.unittest.TestCase):
+    @autotest(n=10, auto_backward=False, check_graph=False)
+    def test_isclose_with_random_data(test_case):
         device = random_device()
         shape = random_tensor().oneflow.shape
         x1 = random_tensor(len(shape), *shape, requires_grad=False).to(device)
-        x2 = random_tensor(len(shape), *shape, requires_grad=False).to(device)
-        y = torch.isclose(x1, oneof(x2, random().to(int).to(float)))
+        x2 = perturbate(device, shape, x1)
+        y = torch.isclose(x1, x2, rtol=rtol)
         return y
 
-    @autotest(n=10, auto_backward=False, check_graph=True)
-    def test_less_with_0dim_data(test_case):
+    @autotest(n=10, auto_backward=False, check_graph=False)
+    def test_isclose_with_0dim_data(test_case):
         device = random_device()
         shape = random_tensor().oneflow.shape
-        x1 = random_tensor(ndim=0).to(device)
-        x2 = random_tensor(ndim=0).to(device)
-        y = torch.isclose(x1, oneof(x2, random().to(int).to(float)))
+        x1 = random_tensor(ndim=0, requires_grad=False).to(device)
+        x2 = perturbate(device, shape, x1)
+        y = torch.isclose(x1, x2, rtol=rtol)
         return y
 
-    @autotest(n=10, auto_backward=False, check_graph=True)
-    def test_tensor_less_with_random_data(test_case):
+    @autotest(n=10, auto_backward=False, check_graph=False)
+    def test_tensor_isclose_with_random_data(test_case):
         device = random_device()
         shape = random_tensor().oneflow.shape
         x1 = random_tensor(len(shape), *shape, requires_grad=False).to(device)
-        x2 = random_tensor(len(shape), *shape, requires_grad=False).to(device)
-        y = x1.isclose(oneof(x2, random().to(int), random().to(float)))
+        x2 = perturbate(device, shape, x1)
+        y = x1.isclose(x2, rtol=rtol)
         return y
 
-    @autotest(n=10, auto_backward=False, check_graph=True)
-    def test_less_bool_with_random_data(test_case):
+    @autotest(n=10, auto_backward=False, check_graph=False)
+    def test_isclose_broadcast(test_case):
         device = random_device()
-        shape = random_tensor().oneflow.shape
-        x1 = random_tensor(len(shape), *shape, requires_grad=False).to(
-            device=device, dtype=torch.bool
-        )
-        x2 = random_tensor(len(shape), *shape, requires_grad=False).to(
-            device=device, dtype=torch.bool
-        )
-        y = torch.isclose(x1, oneof(x2, random().to(int).to(float)))
-        return y
-
-    @autotest(n=10, auto_backward=False, check_graph=True)
-    def test_tensor_less_with_0dim_data(test_case):
-        device = random_device()
-        shape = random_tensor().oneflow.shape
-        x1 = random_tensor(ndim=0).to(device)
-        x2 = random_tensor(ndim=0).to(device)
-        y = x1.isclose(oneof(x2, random().to(int), random().to(float)))
+        shape = random_tensor(2, 2, 4).oneflow.shape
+        x1 = random_tensor(len(shape), *shape, requires_grad=False).to(device)
+        x2 = random_tensor(len(shape), 2, 1, requires_grad=False).to(device)
+        y = torch.isclose(x1, x2, rtol=rtol)
         return y
 
 
