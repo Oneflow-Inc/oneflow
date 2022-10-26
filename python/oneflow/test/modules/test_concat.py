658d404f2eb947d8c1fa308819382f0b2c368ac9
small1945 <2387491899@qq.com>
2022-10-26 17:11:58 +0800

    remove blank line

--- python/oneflow/test/modules/test_concat.py
+++ python/oneflow/test/modules/test_concat.py
@@ -162,7 +162,6 @@ def _test_concat_single_input_type(test_case, device):
     flow_cat_list = flow.cat(flow_list)
     test_case.assertTrue(flow_cat_list.dtype is oneflow.int64)
 
-
 @flow.unittest.skip_unless_1n1d()
 class TestModule(flow.unittest.TestCase):
     def test_concat(test_case):
