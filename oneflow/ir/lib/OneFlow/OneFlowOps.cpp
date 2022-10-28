1d8282186ae7ae4d0371d86f13ba45895ba8c231
jackalcooper <jackalcooper@gmail.com>
2022-10-28 18:20:34 +0800

    rm hack

--- oneflow/ir/lib/OneFlow/OneFlowOps.cpp
+++ oneflow/ir/lib/OneFlow/OneFlowOps.cpp
@@ -514,28 +514,6 @@ llvm::SmallVector<Value, 4> NormalizationOp::NchwToNhwc(llvm::SmallVector<Value,
   return results;
 }
 
-bool GroupNormOp::IsNCHW() { return false; }
-
-llvm::DenseSet<Value> GroupNormOp::OperandsToTranspose() { return {this->x()}; }
-
-llvm::DenseSet<Value> GroupNormOp::ResultsToTranspose() { return {this->y()}; }
-
-llvm::SmallVector<Value, 4> GroupNormOp::NchwToNhwc(llvm::SmallVector<Value, 4> value,
-                                                    PatternRewriter& rewriter) {
-  auto normalization_op = *this;
-  SmallVector<Value, 4> operands;
-  operands.push_back(value[0]);
-  NamedAttrList attributes = normalization_op->getAttrs();
-  attributes.erase("affine");
-  attributes.erase("num_groups");
-  attributes.erase("epsilon");
-  auto created = rewriter.create<oneflow::ReluOp>(normalization_op.getLoc(), getNHWCType(this->y()),
-                                                  operands, attributes);
-  llvm::SmallVector<Value, 4> results;
-  results.push_back(created.y());
-  return results;
-}
-
 bool MaxPool2DOp::IsNCHW() { return this->data_format().str() == "channels_first"; }
 
 llvm::DenseSet<Value> MaxPool2DOp::OperandsToTranspose() { return {this->x()}; }
