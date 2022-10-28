102743053834a3cedf0119eb8bd0caa2a7d0e496
yuhao <1171760467@qq.com>
2022-10-28 15:16:59 +0800

    fix

--- oneflow/ir/include/OneFlow/OKL/Conversion/SplitIntoFuncs.h
+++ oneflow/ir/include/OneFlow/OKL/Conversion/SplitIntoFuncs.h
@@ -13,6 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#ifndef ONEFLOW_IR_INCLUDE_ONEFLOW_OKL_CONVERSION_SPLITINTOFUNCS_H_
+#define ONEFLOW_IR_INCLUDE_ONEFLOW_OKL_CONVERSION_SPLITINTOFUNCS_H_
 
 #include "mlir/Pass/Pass.h"
 
