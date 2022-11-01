10a2f60d51f78cf000a92d34a2e1882ab4c6ae35
farmerzhang1 <zhang2000dc@gmail.com>
2022-11-01 05:36:42 +0000

    remove include

--- oneflow/core/ep/cuda/primitive/binary_functor.cuh
+++ oneflow/core/ep/cuda/primitive/binary_functor.cuh
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include <cstdint>
 #include "oneflow/core/ep/common/primitive/binary_functor.h"
 
 namespace oneflow {
