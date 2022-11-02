f91f6d06b4df2e159104f903a736293d564e1da2
strint <xiaoyulink@gmail.com>
2022-11-02 23:05:53 +0800

    fix typo of auto_parallel_mainstream_algo

--- python/oneflow/nn/graph/graph_config.py
+++ python/oneflow/nn/graph/graph_config.py
@@ -353,11 +353,11 @@ class GraphConfig(object):
         """
         self.proto.auto_parallel_transfer_cost = cost
 
-    def enable_auto_parallel_mainstem_algo(self, mode: bool = True):
+    def enable_auto_parallel_mainstream_algo(self, mode: bool = True):
         """
-        Find the mainstem of the SBP graph, then reduce the wait time for tributaries.
+        Find the mainstream of the SBP graph, then reduce the wait time for tributaries.
         """
-        self.proto.enable_auto_parallel_mainstem_algo = mode
+        self.proto.enable_auto_parallel_mainstream_algo = mode
 
     def enable_auto_parallel_sbp_collector(self, mode: bool = True):
         """
