027718a4069b4993f4d0f57570af4ef957db6791
farmerzhang1 <zhang2000dc@gmail.com>
2022-11-07 08:53:14 +0000

    remove redundant imports; add comment

--- python/oneflow/test/misc/test_mock_scope.py
+++ python/oneflow/test/misc/test_mock_scope.py
@@ -3,43 +3,51 @@ import oneflow as flow
 import oneflow.unittest
 from oneflow.mock_torch import Mock
 
+import torch
+import torch.nn
+import torch.version
+
+"""
+Mock saves a module dict (like sys.modules) for real torch modules and oneflow modules respectively
+when using mock.enable/mock.disable,
+torch-related k-v pairs in sys.modules and global scope are replaced with the cache in `mock`
+"""
 mock = Mock({})  # default enable mocking
+
+import torch
+import torch.nn
+import torch.version
 
 
 class TestMock(flow.unittest.TestCase):
-    def test_complex(test_case):
-        import torch
-
+    def test_simple(test_case):
+        mock.enable(globals())
         test_case.assertTrue(torch.__package__ == "oneflow")
-        import torch.nn
-
         test_case.assertTrue(torch.nn.__package__ == "oneflow.nn")
-        import torch.version
-
         test_case.assertTrue(torch.version.__version__ == flow.__version__)
+
         mock.disable(globals())
-        import torch
 
         test_case.assertTrue(torch.__package__ == "torch")
-        import torch.nn
-
         test_case.assertTrue(torch.nn.__package__ == "torch.nn")
-        import torch.version
-
         test_case.assertTrue(torch.version.__version__ == torch.__version__)
 
+    def test_import_from(test_case):
         mock.enable(globals())
         from torch import nn
         from torch.version import __version__
 
         test_case.assertTrue(nn.__package__ == "oneflow.nn")
         test_case.assertTrue(__version__ == flow.__version__)
+
         mock.disable(globals())
         from torch import nn
         from torch.version import __version__
 
         test_case.assertTrue(nn.__package__ == "torch.nn")
         test_case.assertTrue(__version__ == torch.__version__)
+
+    def test_error(test_case):
         mock.enable(globals())
         with test_case.assertRaises(Exception) as context:
             from torch import noexist
@@ -55,18 +63,10 @@ class TestMock(flow.unittest.TestCase):
 
     def test_with(test_case):
         with mock.enable_with(globals()):
-            import torch
-            import torch.nn
-            import torch.version
-
             test_case.assertTrue(torch.__package__ == "oneflow")
             test_case.assertTrue(torch.nn.__package__ == "oneflow.nn")
             test_case.assertTrue(torch.version.__version__ == flow.__version__)
         with mock.disable_with(globals()):
-            import torch
-            import torch.nn
-            import torch.version
-
             test_case.assertTrue(torch.__package__ == "torch")
             test_case.assertTrue(torch.nn.__package__ == "torch.nn")
             test_case.assertTrue(torch.version.__version__ == torch.__version__)
