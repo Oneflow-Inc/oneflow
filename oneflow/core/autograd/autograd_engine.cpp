b73676b145483e19fbb944b2df36030b95a14f0e
wyg1997 <wangyinggang@foxmail.com>
2022-11-09 19:25:43 +0800

    fix(Autograd): fix autogra.grad need_execute calculate bug

--- oneflow/core/autograd/autograd_engine.cpp
+++ oneflow/core/autograd/autograd_engine.cpp
@@ -334,11 +334,13 @@ Maybe<void> GraphTask::ComputeDependenciesAndPruneNode(const TensorTuple& inputs
         continue;  // recurse
       }
     } else {
-      grad_fn2exec_info_[frame.node_].need_execute =
-          std::any_of(frame.node_->next_functions().begin(), frame.node_->next_functions().end(),
-                      [&](const std::shared_ptr<FunctionNode>& fn) {
-                        return grad_fn2exec_info_[fn.get()].need_execute;
-                      });
+      if (!frame.node_->next_functions().empty()) {
+        grad_fn2exec_info_[frame.node_].need_execute =
+            std::any_of(frame.node_->next_functions().begin(), frame.node_->next_functions().end(),
+                        [&](const std::shared_ptr<FunctionNode>& fn) {
+                          return grad_fn2exec_info_[fn.get()].need_execute;
+                        });
+      }
       seen.insert(frame.node_);
       stack.pop();
     }
