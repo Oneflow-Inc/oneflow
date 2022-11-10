f1eac8ea61157ee223fcb6ae2b427df5ba770216
small1945 <2387491899@qq.com>
2022-11-10 18:01:03 +0800

    refactor rand,randint,randn about global functor

--- oneflow/core/functional/impl/random_functor.cpp
+++ oneflow/core/functional/impl/random_functor.cpp
@@ -157,18 +157,28 @@ class GlobalRandFunctor {
       }
     }
 
-    const auto gen = generator.value_or(JUST(one::DefaultAutoGenerator()));
-    const auto& distribution_state = std::make_shared<DistributionKernelState>(gen);
+    auto gen = generator.value_or(JUST(one::DefaultAutoGenerator()));
+    uint64_t init_seed = JUST(gen->Get<CPUGeneratorImpl>(0))->engine()();
+
     const auto& nd_sbp = JUST(GetNdSbp(sbp_tuple));
 
     auto& attrs = THREAD_CACHED_MUTABLE_ATTR_MAP("from", "to", "shape", "dtype", "seed", "nd_sbp");
     if (LazyMode::is_enabled()) {
       attrs.SetAllAttrs(static_cast<double>(0), static_cast<double>(1), shape, dtype_val,
-                        static_cast<int64_t>(gen->current_seed()), *JUST(GetNdSbpStrList(nd_sbp)));
+                        static_cast<int64_t>(init_seed), *JUST(GetNdSbpStrList(nd_sbp)));
     } else {
+      uint64_t rank_seed = 0;
+      {
+        JUST(BroadcastSeedToAllRanks(&init_seed, /*root=*/0));
+        rank_seed =
+            JUST(GetRandomSeedForRank(*placement, *nd_sbp, init_seed, GlobalProcessCtx::Rank()));
+      }
       attrs.SetAllAttrs(static_cast<double>(0), static_cast<double>(1), shape, dtype_val,
-                        static_cast<int64_t>(gen->current_seed()), NullOpt);
+                        static_cast<int64_t>(rank_seed), NullOpt);
+      gen = JUST(MakeGenerator(placement->device_type()));
+      gen->set_current_seed(rank_seed);
     }
+    const auto& distribution_state = std::make_shared<DistributionKernelState>(gen);
 
     auto result = JUST(OpInterpUtil::Dispatch<Tensor>(
         *op_, {}, OpExprInterpContext(attrs, placement, nd_sbp, distribution_state)));
@@ -179,76 +189,35 @@ class GlobalRandFunctor {
  private:
   std::shared_ptr<OpExpr> op_;
 };
-
 class RandNFunctor {
  public:
-  RandNFunctor() { op_ = CHECK_JUST(one::OpBuilder("normal").Output("out").Build()); }
   Maybe<Tensor> operator()(const Shape& shape, const Optional<Symbol<DType>>& dtype,
                            const Optional<Symbol<Device>>& device,
                            const Optional<one::Generator>& generator,
                            const bool& requires_grad) const {
-    DataType dtype_val = GetDefaultDType()->data_type();
-    if (dtype) { dtype_val = JUST(dtype)->data_type(); }
-    if (dtype_val != DataType::kFloat && dtype_val != DataType::kDouble
-        && dtype_val != DataType::kFloat16) {
+    if (dtype.has_value() && !JUST(dtype)->is_floating_point()) {
       OF_UNIMPLEMENTED() << "Only support floating dtype in randn().";
     }
-
-    const auto gen = generator.value_or(JUST(one::DefaultAutoGenerator()));
-    auto& attrs = THREAD_CACHED_MUTABLE_ATTR_MAP("mean", "std", "shape", "dtype", "seed");
-    attrs.SetAllAttrs(static_cast<double>(0), static_cast<double>(1), shape, dtype_val,
-                      static_cast<int64_t>(gen->current_seed()));
-
-    const auto& distribution_state = std::make_shared<DistributionKernelState>(gen);
-
-    OpExprInterpContext ctx(attrs, distribution_state);
-    ctx.device = device;
-    auto result = JUST(OpInterpUtil::Dispatch<Tensor>(*op_, {}, ctx));
-    JUST(result->set_requires_grad(requires_grad));
-    return result;
+    const auto& out = Optional<one::Tensor>();
+    return Normal(static_cast<double>(0), static_cast<double>(1), shape, out, dtype, device,
+                  generator, requires_grad);
   }
-
- private:
-  std::shared_ptr<OpExpr> op_;
 };
 
 class GlobalRandNFunctor {
  public:
-  GlobalRandNFunctor() { op_ = CHECK_JUST(one::OpBuilder("normal").Output("out").Build()); }
   Maybe<Tensor> operator()(const Shape& shape, const Symbol<ParallelDesc>& placement,
                            const std::vector<Symbol<SbpParallel>>& sbp_tuple,
                            const Optional<Symbol<DType>>& dtype,
                            const Optional<one::Generator>& generator,
                            const bool& requires_grad) const {
-    JUST(CheckDeviceIdsIsValid(placement));
-    DataType dtype_val = GetDefaultDType()->data_type();
-    if (dtype) { dtype_val = JUST(dtype)->data_type(); }
-    if (dtype_val != DataType::kFloat && dtype_val != DataType::kDouble
-        && dtype_val != DataType::kFloat16) {
+    if (dtype.has_value() && !JUST(dtype)->is_floating_point()) {
       OF_UNIMPLEMENTED() << "Only support floating dtype in randn().";
     }
-
-    const auto gen = generator.value_or(JUST(one::DefaultAutoGenerator()));
-    const auto& distribution_state = std::make_shared<DistributionKernelState>(gen);
-    const auto& nd_sbp = JUST(GetNdSbp(sbp_tuple));
-
-    auto& attrs = THREAD_CACHED_MUTABLE_ATTR_MAP("mean", "std", "shape", "dtype", "seed", "nd_sbp");
-    if (LazyMode::is_enabled()) {
-      attrs.SetAllAttrs(static_cast<double>(0), static_cast<double>(1), shape, dtype_val,
-                        static_cast<int64_t>(gen->current_seed()), *JUST(GetNdSbpStrList(nd_sbp)));
-    } else {
-      attrs.SetAllAttrs(static_cast<double>(0), static_cast<double>(1), shape, dtype_val,
-                        static_cast<int64_t>(gen->current_seed()), NullOpt);
-    }
-
-    auto result = JUST(OpInterpUtil::Dispatch<Tensor>(
-        *op_, {}, OpExprInterpContext(attrs, placement, nd_sbp, distribution_state)));
-    JUST(result->set_requires_grad(requires_grad));
-    return result;
+    const auto& out = Optional<one::Tensor>();
+    return GlobalNormal(static_cast<double>(0), static_cast<double>(1), shape, out, placement,
+                        sbp_tuple, dtype, generator, requires_grad);
   }
-
- private:
-  std::shared_ptr<OpExpr> op_;
 };
 
 class NormalFunctor {
@@ -517,18 +486,26 @@ class GlobalRandIntFunctor {
     DataType dtype_val = DataType::kInt64;
     if (dtype) { dtype_val = JUST(dtype)->data_type(); }
 
-    const auto gen = generator.value_or(JUST(one::DefaultAutoGenerator()));
-    const auto& distribution_state = std::make_shared<DistributionKernelState>(gen);
+    auto gen = generator.value_or(JUST(one::DefaultAutoGenerator()));
     const auto& nd_sbp = JUST(GetNdSbp(sbp));
+    uint64_t init_seed = JUST(gen->Get<CPUGeneratorImpl>(0))->engine()();
 
     auto& attrs = THREAD_CACHED_MUTABLE_ATTR_MAP("shape", "from", "to", "dtype", "seed", "nd_sbp");
     if (LazyMode::is_enabled()) {
-      attrs.SetAllAttrs(shape, low, high, dtype_val, static_cast<int64_t>(gen->current_seed()),
+      attrs.SetAllAttrs(shape, low, high, dtype_val, static_cast<int64_t>(init_seed),
                         *JUST(GetNdSbpStrList(nd_sbp)));
     } else {
-      attrs.SetAllAttrs(shape, low, high, dtype_val, static_cast<int64_t>(gen->current_seed()),
-                        NullOpt);
+      uint64_t rank_seed = 0;
+      {
+        JUST(BroadcastSeedToAllRanks(&init_seed, /*root=*/0));
+        rank_seed =
+            JUST(GetRandomSeedForRank(*placement, *nd_sbp, init_seed, GlobalProcessCtx::Rank()));
+      }
+      attrs.SetAllAttrs(shape, low, high, dtype_val, static_cast<int64_t>(rank_seed), NullOpt);
+      gen = JUST(MakeGenerator(placement->device_type()));
+      gen->set_current_seed(rank_seed);
     }
+    const auto& distribution_state = std::make_shared<DistributionKernelState>(gen);
 
     auto result = JUST(OpInterpUtil::Dispatch<Tensor>(
         *op_, {}, OpExprInterpContext(attrs, placement, nd_sbp, distribution_state)));
