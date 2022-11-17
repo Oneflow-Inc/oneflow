512f096237cea1ea530a6d0b9cebfd6f2132a732
yuang-chen <yuang.chen.berlin@gmail.com>
2022-11-17 12:33:47 +0000

    remove unnessary files

--- gcn/read_embedding.py
+++
@@ -1,9 +0,0 @@
-import os
-from oneflow.one_embedding import make_persistent_table_reader
-import numpy as np
-import oneflow as flow
-
-
-ids = np.random.randint(0, 1000, (100, 3), dtype=np.int64)
-ids_tensor = flow.tensor(ids, requires_grad=False).to("cuda")
-print(ids_tensor.size())
