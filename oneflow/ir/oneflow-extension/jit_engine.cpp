9e087ecccadac0e3ee1a9680aa27bc6b07bd1235
yuhao <1171760467@qq.com>
2022-11-24 10:23:04 +0800

    fix

--- oneflow/ir/oneflow-extension/jit_engine.cpp
+++ oneflow/ir/oneflow-extension/jit_engine.cpp
@@ -40,8 +40,6 @@ void launch(void* run_ctx, void* kernel) {
   oneflow::okl::RunContext* compute_ctx_ =
       static_cast<typename std::tuple_element_t<0, oneflow::okl::LaunchArgs>>(run_ctx);
   engine->Compute(compute_ctx_, compute_ctx_->FetchState(), compute_ctx_->FetchCache());
-  // OF_CUDA_CHECK(cudaDeviceSynchronize());
-  // OF_CUDA_CHECK(cudaGetLastError());
 }
 }  // extern "C"
 
