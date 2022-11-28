01ceaf001e73b0fada4e25868029174e1480ffc4
jackalcooper <jackalcooper@gmail.com>
2022-11-28 15:59:58 +0800

    refine

--- oneflow/ir/test/OneFlow/with_cuda/test_fuse_bn_add_relu.py
+++ oneflow/ir/test/OneFlow/with_cuda/test_fuse_bn_add_relu.py
@@ -22,9 +22,6 @@ import numpy as np
 import os
 
 os.environ["ONEFLOW_MLIR_ENABLE_ROUND_TRIP"] = "1"
-os.environ["ONEFLOW_MLIR_PREFER_NHWC"] = "1"
-os.environ["ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION"] = "1"
-os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
 os.environ["ONEFLOW_MLIR_FUSE_NORMALIZATION_OPS"] = "1"
 os.environ["ONEFLOW_MLIR_PRINT_STATS"] = "1"
 
