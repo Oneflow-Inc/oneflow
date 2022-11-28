ec772368db29589987ae7e303a5164d3c693f9e9
jackalcooper <jackalcooper@gmail.com>
2022-11-28 15:52:40 +0800

    fix typo

--- oneflow/ir/test/OneFlow/with_cuda/test_fuse_bn_add_relu.py
+++ oneflow/ir/test/OneFlow/with_cuda/test_fuse_bn_add_relu.py
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 # RUN: python3 %s | FileCheck %s
-# CHECK-NOT: "oneflow.normalization_add_relu"
+# CHECK-NOT: "oneflow.normalization"
 
 import unittest
 import numpy as np
