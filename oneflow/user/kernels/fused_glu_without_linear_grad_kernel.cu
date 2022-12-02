b07e7ead4ee89275edc3c6b609bc0cc63fccc1d4
zobinHuang <zobin1999@gmail.com>
2022-12-02 16:35:00 +0800

    init branch

--- oneflow/user/kernels/fused_glu_without_linear_grad_kernel.cu
+++ oneflow/user/kernels/fused_glu_without_linear_grad_kernel.cu
@@ -17,14 +17,69 @@ namespace oneflow {
 
 namespace {
 
-template<typename T, typename IndexType, ep::primitive::UnaryOp act_type>
+// current: pack_size default to be 1, pack_num default to be number of elements
+template<typename T, typename IndexType, typename FUNCTOR, ep::primitive::UnaryOp act_type, int32_t pack_size>
 __global__ void FusedGluWithoutLinearGradGpu(
-    const IndexType m, const IndexType n, const IndexType output_stride,
+    const IndexType m, const IndexType packed_n, const IndexType pack_num, 
+    const IndexType input_stride, const IndexType output_stride,
+    FUNCTOR act_grad_functor,
     ep::primitive::UnaryFunctor<DeviceType::kCUDA, act_type, T, T> act,
     const T* dy, const T* matmul_wx, const T* matmul_vx, 
     T* d_matmul_wx, T* d_matmul_vx
 ){
-    
+    // obtain global thread index
+    IndexType global_thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // workload of current thread
+    for(IndexType pack_index = global_thread_id, step = gridDim.x * blockDim.x;
+      pack_index < pack_num; pack_index += step){
+        // TODO
+    }
+}
+
+template<typename T, ep::primitive::UnaryOp act_type>
+void DispatchAlignment(ep::Stream* stream, const int64_t m, const int64_t n, 
+  const int64_t input_stride, const int64_t output_stride,
+  const T* dy, const T* matmul_wx, const T* matmul_vx, 
+    T* d_matmul_wx, T* d_matmul_vx
+) {
+    const auto IsAligned = [&](const size_t alignment) {
+    const uintptr_t dy_ptr = reinterpret_cast<uintptr_t>(dy);
+    const uintptr_t matmul_wx_ptr = reinterpret_cast<uintptr_t>(matmul_wx);
+    const uintptr_t matmul_vx_ptr = reinterpret_cast<uintptr_t>(matmul_vx);
+    const uintptr_t d_matmul_wx_ptr = reinterpret_cast<uintptr_t>(d_matmul_wx);
+    const uintptr_t d_matmul_vx_ptr = reinterpret_cast<uintptr_t>(d_matmul_vx);
+
+    return (/* memory address alignment */
+            dy_ptr % alignment == 0 && matmul_vx_ptr % alignment == 0
+            && matmul_wx_ptr % alignment == 0 && d_matmul_wx_ptr % alignment == 0
+            && d_matmul_vx_ptr % alignment == 0
+            /* #element per row alignment */
+            && n % (alignment / sizeof(T)) == 0);
+    };
+}
+
+template<typename T>
+void DispatchActivationType(ep::Stream* stream, const int64_t m, const int64_t n, 
+  const int64_t input_stride, const int64_t output_stride,
+  const T* dy, const T* matmul_wx, const T* matmul_vx, 
+    T* d_matmul_wx, T* d_matmul_vx
+) {
+  if (activation == "none") {
+    DispatchAlignment<T, ep::primitive::UnaryOp::kIdentity>(stream, m, n, input_stride, output_stride, dy, matmul_wx, matmul_vx, d_matmul_wx, d_matmul_vx);
+  } else if (activation == "sigmoid") {
+    DispatchAlignment<T, ep::primitive::UnaryOp::kSigmoid>(stream, m, n, input_stride, output_stride, dy, matmul_wx, matmul_vx, d_matmul_wx, d_matmul_vx);
+  } else if (activation == "relu") {
+    DispatchAlignment<T, ep::primitive::UnaryOp::kRelu>(stream, m, n, input_stride, output_stride, dy, matmul_wx, matmul_vx, d_matmul_wx, d_matmul_vx);
+  } else if (activation == "gelu") {
+    DispatchAlignment<T, ep::primitive::UnaryOp::kGelu>(stream, m, n, input_stride, output_stride, dy, matmul_wx, matmul_vx, d_matmul_wx, d_matmul_vx);
+  } else if (activation == "fast_gelu") {
+    DispatchAlignment<T, ep::primitive::UnaryOp::kFastGelu>(stream, m, n, input_stride, output_stride, dy, matmul_wx, matmul_vx, d_matmul_wx, d_matmul_vx);
+  } else if (activation == "silu") {
+    DispatchAlignment<T, ep::primitive::UnaryOp::kSilu>(stream, m, n, input_stride, output_stride, dy, matmul_wx, matmul_vx, d_matmul_wx, d_matmul_vx);
+  } else {
+    UNIMPLEMENTED();
+  }
 }
 
 template<typename T>
