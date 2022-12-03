94eef3a08e0b364c1f86a29f9bb05303f36aca53
BBuf <1182563586@qq.com>
2022-12-03 06:06:22 +0000

    delete useless headfile

--- oneflow/core/functional/impl/array_functor.cpp
+++ oneflow/core/functional/impl/array_functor.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <cstdint>
 #include "oneflow/core/common/container_util.h"
 #include "oneflow/core/common/maybe.h"
 #include "oneflow/core/framework/mutable_attr_map.h"
