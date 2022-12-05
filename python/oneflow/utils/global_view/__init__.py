a7502eaa12d37ec1547b63415f85fc162046db24
strint <xiaoyulink@gmail.com>
2022-12-05 15:34:18 +0800

    address review

--- python/oneflow/utils/global_view/__init__.py
+++ python/oneflow/utils/global_view/__init__.py
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import oneflow
 from oneflow.utils.global_view.to_global import to_global
 from oneflow.utils.global_view.to_local import to_local
 from oneflow._oneflow_internal.global_view import global_mode, current_global_mode
