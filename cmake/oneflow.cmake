da497a82242c947cd99abae0f01740c5285a1246
liujuncheng <liujuncheng1022@gmail.com>
2022-12-08 22:32:55 +0800

    Add cutlass impl

--- cmake/oneflow.cmake
+++ cmake/oneflow.cmake
@@ -319,13 +319,6 @@ elseif(WIN32)
 endif()
 
 if(BUILD_CUDA)
-  foreach(CUDA_ARCH ${CMAKE_CUDA_ARCHITECTURES})
-    if(CUDA_ARCH MATCHES "^([0-9]+)\\-real$")
-      list(APPEND CUDA_REAL_ARCHS_LIST ${CMAKE_MATCH_1})
-    elseif(CUDA_ARCH MATCHES "^([0-9]+)$")
-      list(APPEND CUDA_REAL_ARCHS_LIST ${CMAKE_MATCH_1})
-    endif()
-  endforeach()
   string(JOIN "," CUDA_REAL_ARCHS ${CUDA_REAL_ARCHS_LIST})
   set_source_files_properties(${PROJECT_SOURCE_DIR}/oneflow/core/hardware/cuda_device_descriptor.cpp
                               PROPERTIES COMPILE_FLAGS "-DCUDA_REAL_ARCHS=\"${CUDA_REAL_ARCHS}\"")
@@ -336,14 +329,12 @@ if(BUILD_CUDA)
     add_definitions(-DCUTLASS_ENABLE_TENSOR_CORE_MMA=1)
   endif()
 
-  get_target_property(CUTLASS_FMHA_INCLUDE_DIR cutlass_fmha_headers INTERFACE_INCLUDE_DIRECTORIES)
   set_property(
     SOURCE ${PROJECT_SOURCE_DIR}/oneflow/user/kernels/fused_multi_head_attention_inference_kernel.cu
-    APPEND PROPERTY INCLUDE_DIRECTORIES ${CUTLASS_FMHA_INCLUDE_DIR})
-  get_target_property(CUTLASS_DUAL_GEMM_INCLUDE_DIR cutlass_dual_gemm_headers
-                      INTERFACE_INCLUDE_DIRECTORIES)
+    APPEND PROPERTY INCLUDE_DIRECTORIES
+                    ${CUTLASS_INSTALL_DIR}/examples/41_fused_multi_head_attention)
   set_property(SOURCE ${PROJECT_SOURCE_DIR}/oneflow/user/kernels/fused_glu_kernel.cu APPEND
-               PROPERTY INCLUDE_DIRECTORIES ${CUTLASS_DUAL_GEMM_INCLUDE_DIR})
+               PROPERTY INCLUDE_DIRECTORIES ${CUTLASS_INSTALL_DIR}/examples/45_dual_gemm)
   if("${CMAKE_CUDA_COMPILER_ID}" STREQUAL "NVIDIA")
     set_property(
       SOURCE
