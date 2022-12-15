8c9207ab4c2188d74694cc3ae4746efe266581cf
youxiudeshouyeren <1929724847@qq.com>
2022-12-15 08:53:58 +0000

    fix docstr of oneflow.median

--- python/oneflow/test/modules/test_median.py
+++ python/oneflow/test/modules/test_median.py
@@ -56,7 +56,7 @@ class TestMedianModule(flow.unittest.TestCase):
         x = random_tensor(ndim=3, dim1=0, requires_grad=False).to(device)
         return torch.median(x, 0)
 
-    @autotest(n=5, auto_backward=False, check_graph=False)
+    @autotest(n=5, auto_backward=False)
     def test_median_return_type(test_case):
         x = random_tensor(3, 4)
         result = x.median(1)
