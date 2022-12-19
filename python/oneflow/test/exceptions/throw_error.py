03703a95edffbc13365c2840a0213c05cd960721
daquexian <daquexian566@gmail.com>
2022-12-19 15:41:21 +0800

    add comments about throw_error.py
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- python/oneflow/test/exceptions/throw_error.py
+++ python/oneflow/test/exceptions/throw_error.py
@@ -13,6 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+# This file is intended to be run in
+# python/oneflow/test/exceptions/test_error_reported_in_thread.py
+
 import oneflow as flow
 
 
