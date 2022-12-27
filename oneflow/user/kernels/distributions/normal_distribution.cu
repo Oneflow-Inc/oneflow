c6681ac1bdb7d5547ca66e46be327617b919ae00
Ldpe2G <liangdepeng@gmail.com>
2022-12-27 03:14:26 +0000

    refine normal kernel

--- oneflow/user/kernels/distributions/normal_distribution.cu
+++ oneflow/user/kernels/distributions/normal_distribution.cu
@@ -19,29 +19,11 @@ limitations under the License.
 #include "oneflow/core/ep/include/device.h"
 #include "oneflow/core/ep/cuda/cuda_stream.h"
 #include "oneflow/user/kernels/fused_rnn_cell_kernel_util.h"
+#include "oneflow/core/cuda/layer_norm.cuh"
 
 namespace oneflow {
 
 namespace {
-
-template<typename T>
-struct AccumulateType {};
-template<>
-struct AccumulateType<float> {
-  using type = float;
-};
-template<>
-struct AccumulateType<double> {
-  using type = double;
-};
-
-template <>
-struct AccumulateType<half> {
-  using type = float;
-};
-
-template<typename T>
-using acc_type = typename AccumulateType<T>::type;
 
 // launch bounds used for kernels
 const uint32_t block_size_bound = 256;
@@ -68,47 +50,11 @@ std::tuple<uint64_t, dim3, dim3> calc_execution_policy(int64_t total_elements,
   return std::make_tuple(counter_offset, grid, dim_block);
 }
 
-
-template<typename T>
-__device__ T GenNormal(curandState* state, const T mean, const T std);
-
-template<>
-__device__ float GenNormal<float>(curandState* state, const float mean, const float std) {
-  return curand_normal(state) * std + mean;
-}
-
-template<>
-__device__ double GenNormal<double>(curandState* state, const double mean, const double std) {
-  return curand_normal_double(state) * std + mean;
-}
-
-template<typename T>
-__global__ void GenerateGpu(curandState* state, const int64_t elem_cnt, T* dptr, const T mean,
-                            const T std) {
-  const int id = blockIdx.x * blockDim.x + threadIdx.x;
-  curandState localState = state[id];
-  CUDA_1D_KERNEL_LOOP(i, elem_cnt) { dptr[i] = GenNormal<T>(&localState, mean, std); }
-  state[id] = localState;
-}
-
-// specialization for half
-template<>
-__global__ void GenerateGpu(curandState* state, const int64_t elem_cnt, half* dptr, const half mean,
-                            const half std) {
-  const int id = blockIdx.x * blockDim.x + threadIdx.x;
-  curandState localState = state[id];
-  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
-    dptr[i] = static_cast<half>(GenNormal<float>(&localState, mean, std));
-  }
-  state[id] = localState;
-}
-
-
-template<typename T, typename ACC_T>
+template<typename T, typename ComputeType>
 OF_LAUNCH_BOUNDS_2(block_size_bound, grid_size_bound)
 __global__ void distribution_elementwise_grid_stride_kernel_double(int32_t numel, uint64_t seed,
-                                                                   uint64_t offset, ACC_T mean,
-                                                                   ACC_T std, T* out_ptr) {
+                                                                   uint64_t offset, ComputeType mean,
+                                                                   ComputeType std, T* out_ptr) {
   int32_t unroll_factor = 2;
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   curandStatePhilox4_32_10_t state;
@@ -123,18 +69,18 @@ __global__ void distribution_elementwise_grid_stride_kernel_double(int32_t numel
     for (int ii = 0; ii < unroll_factor; ii++) {
       int li = linear_index + blockDim.x * gridDim.x * ii;
       if (li < numel) {
-        out_ptr[li] = static_cast<T>(static_cast<ACC_T>((&rand.x)[ii]) * std + mean);
+        out_ptr[li] = static_cast<T>(static_cast<ComputeType>((&rand.x)[ii]) * std + mean);
       }
     }
     __syncthreads();
   }
 }
 
-template<typename T, typename ACC_T>
+template<typename T, typename ComputeType>
 OF_LAUNCH_BOUNDS_2(block_size_bound, grid_size_bound)
 __global__ void distribution_elementwise_grid_stride_kernel_float(int32_t numel, uint64_t seed,
-                                                                  uint64_t offset,  ACC_T mean,
-                                                                  ACC_T std, T* out_ptr) {
+                                                                  uint64_t offset,  ComputeType mean,
+                                                                  ComputeType std, T* out_ptr) {
   int32_t unroll_factor = 4;
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   curandStatePhilox4_32_10_t state;
@@ -149,7 +95,7 @@ __global__ void distribution_elementwise_grid_stride_kernel_float(int32_t numel,
     for (int ii = 0; ii < unroll_factor; ii++) {
       int li = linear_index + blockDim.x * gridDim.x * ii;
       if (li < numel) {
-        out_ptr[li] = static_cast<T>(static_cast<ACC_T>((&rand.x)[ii]) * std + mean);
+        out_ptr[li] = static_cast<T>(static_cast<ComputeType>((&rand.x)[ii]) * std + mean);
       }
     }
     __syncthreads();
@@ -181,22 +127,16 @@ void NormalDistribution<DeviceType::kCUDA, T>::operator()(
     offset = gen->get_philox_offset(counter_offset);
   }
 
-  using ACC_T = acc_type<T>;
+  using ComputeType = typename cuda::layer_norm::DefaultComputeType<T>::type;
   if (std::is_same<T, double>::value) {
-    distribution_elementwise_grid_stride_kernel_double<T, ACC_T><<<
+    distribution_elementwise_grid_stride_kernel_double<T, ComputeType><<<
       grid, block, 0, stream->As<ep::CudaStream>()->cuda_stream()>>>(
-        elem_cnt, seed, offset, static_cast<ACC_T>(mean_), static_cast<ACC_T>(std_), dptr);
+        elem_cnt, seed, offset, static_cast<ComputeType>(mean_), static_cast<ComputeType>(std_), dptr);
   } else {
-    distribution_elementwise_grid_stride_kernel_float<T, ACC_T><<<
+    distribution_elementwise_grid_stride_kernel_float<T, ComputeType><<<
       grid, block, 0, stream->As<ep::CudaStream>()->cuda_stream()>>>(
-        elem_cnt, seed, offset, static_cast<ACC_T>(mean_), static_cast<ACC_T>(std_), dptr);
+        elem_cnt, seed, offset, static_cast<ComputeType>(mean_), static_cast<ComputeType>(std_), dptr);
   }
-
-  // int32_t block_num = gen->max_block_num();
-  // int32_t thread_num = gen->max_thread_num();
-  // auto* curand_states = gen->curand_states();
-  // GenerateGpu<T><<<block_num, thread_num, 0, stream->As<ep::CudaStream>()->cuda_stream()>>>(
-  //     curand_states, elem_cnt, dptr, mean_, std_);
 }
 
 #define INITIATE_CUDA_NORMAL_DISTRIBUTION(T, typeproto)               \
