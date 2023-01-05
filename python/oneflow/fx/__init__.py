b344808d25d40380af9914a5707e6ad9d19d70ed
daquexian <daquexian566@gmail.com>
2023-01-05 12:50:42 +0800

    add fake fx.Proxy for transformers
    Signed-off-by: daquexian <daquexian566@gmail.com>

---
+++ python/oneflow/fx/__init__.py
@@ -0,0 +1,5 @@
+class Proxy:
+    def __init__(self):
+        raise NotImplementedError(
+            "oneflow.fx.Proxy is only for compatibility with PyTorch and is not actually implemented."
+        )
