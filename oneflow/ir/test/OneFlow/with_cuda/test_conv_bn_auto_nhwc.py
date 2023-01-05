ef65f5a71fa17c6233eaf5f39632db0f3c27dbfd
JasonChen9 <995975102@qq.com>
2023-01-05 05:05:06 +0000

    refine

--- oneflow/ir/test/OneFlow/with_cuda/test_conv_bn_auto_nhwc.py
+++ oneflow/ir/test/OneFlow/with_cuda/test_conv_bn_auto_nhwc.py
@@ -29,10 +29,7 @@ os.environ["ONEFLOW_MLIR_PREFER_NHWC"] = "1"
 os.environ["ONEFLOW_MLIR_ENABLE_INFERENCE_OPTIMIZATION"] = "1"
 os.environ["ONEFLOW_MLIR_FUSE_FORWARD_OPS"] = "1"
 os.environ["ONEFLOW_MLIR_FUSE_NORMALIZATION_OPS"] = "1"
-# os.environ["ONEFLOW_MLIR_PRINT_STATS"] = "1"
-# os.environ["ONEFLOW_MLIR_STDOUT"] = "1"
-os.environ["ONEFLOW_MLIR_ENABLE_TIMING"] = "1"
-# os.environ["ONEFLOW_MLIR_ENABLE_IR_PRINTING"] = "1"
+os.environ["ONEFLOW_MLIR_PRINT_STATS"] = "1"
 
 
 def _test_fuse_conv_bn(test_case, with_cuda):
