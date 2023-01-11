022a3029d90b68d6a7949d5613007e8727abc873
songyicheng <int.rejoice@gmail.com>
2023-01-11 04:31:55 +0000

    fix float16 EagerLocalTensorZeros bug temporarily

--- oneflow/core/common/data_type.h
+++ oneflow/core/common/data_type.h
@@ -44,13 +44,23 @@ using float16 = half_float::half;
 
 // Type Trait: extened IsScalarType
 
-template<typename T>
-struct IsScalarType<typename std::remove_cv<T>> final {
-  static const bool value = std::is_same<float16, T>::value
-#ifdef WITH_CUDA
-                            || std::is_same<half, T>::value
-#endif  // WITH_CUDA
-                            || std::is_same<bfloat16, T>::value;
+// template<typename T>
+// struct IsScalarType<typename std::remove_cv<T>> final {
+//   static const bool value = std::is_same<float16, T>::value
+// #ifdef WITH_CUDA
+//                             || std::is_same<half, T>::value
+// #endif  // WITH_CUDA
+//                             || std::is_same<bfloat16, T>::value;
+// };
+
+template<>
+struct IsScalarType<bfloat16> final {
+  static const bool value = true;
+};
+
+template<>
+struct IsScalarType<float16> final {
+  static const bool value = true;
 };
 
 // Type Trait: IsFloat16
