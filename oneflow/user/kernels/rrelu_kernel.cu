1927e4d137a51604afb8e27aa00d2820d360642d
youxiudeshouyeren <1929724847@qq.com>
2023-01-16 11:33:23 +0000

    fix rrelu cuda kernel

--- oneflow/user/kernels/rrelu_kernel.cu
+++ oneflow/user/kernels/rrelu_kernel.cu
@@ -15,23 +15,40 @@ limitations under the License.
 */
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
-#include "oneflow/core/kernel/random_generator.h"
-
+#include "oneflow/user/kernels/distributions/distribution_template_util.cuh"
+#include "oneflow/user/kernels/distributions/common.h"
 namespace oneflow {
 namespace {
 
-template<typename T>
+template<typename T, int unroll_factor, typename F>
 __global__ void compute_rrelu(const T* in, T* out, T* noise_data, int64_t elem_cnt, const T lower,
-                              const T upper, const T range) {
-  CUDA_1D_KERNEL_LOOP(i, elem_cnt) {
-    if (in[i] <= static_cast<T>(0.0)) {
-      T random_float = noise_data[i] * range + lower;
-      noise_data[i] = random_float;
-      out[i] = in[i] * random_float;
-    } else {
-      noise_data[i] = static_cast<T>(1.0);
-      out[i] = in[i];
+                              const T upper, uint64_t seed, uint64_t offset, const F& random_func) {
+  const int id = blockIdx.x * blockDim.x + threadIdx.x;
+  curandStatePhilox4_32_10_t state;
+  curand_init(seed, id, offset, &state);
+  int grid_stride = blockDim.x * gridDim.x * unroll_factor;
+  int rounded_size = ((elem_cnt - 1) / grid_stride + 1) * grid_stride;
+  T range = upper - lower;
+  for (int linear_index = id; linear_index < rounded_size; linear_index += grid_stride) {
+    auto rand = random_func(&state);
+
+    // ensure that (&rand.x)[ii] is safe
+    static_assert(sizeof(rand) / sizeof(rand.x) == unroll_factor, "");
+#pragma unroll
+    for (int ii = 0; ii < unroll_factor; ii++) {
+      int li = linear_index + blockDim.x * gridDim.x * ii;
+      if (li >= elem_cnt) { continue; }
+      T r = static_cast<T>((&rand.x)[ii]);
+      r = r * range + lower;
+      if (in[li] <= static_cast<T>(0)) {
+        out[li] = in[li] * r;
+        noise_data[li] = r;
+      } else {
+        out[li] = in[li];
+        noise_data[li] = static_cast<T>(1);
+      }
     }
+    __syncthreads();
   }
 }
 }  // namespace
@@ -42,13 +59,20 @@ class CudaRReluKernel final : public user_op::OpKernel {
   CudaRReluKernel() = default;
   ~CudaRReluKernel() = default;
 
+  std::shared_ptr<user_op::OpKernelState> CreateOpKernelState(
+      user_op::KernelInitContext* ctx) const override {
+    const auto& generator = CHECK_JUST(one::MakeGenerator(DeviceType::kCUDA));
+    return std::make_shared<DistributionKernelState>(generator);
+  }
+
  private:
   using user_op::OpKernel::Compute;
-  void Compute(user_op::KernelComputeContext* ctx) const override {
+  void Compute(user_op::KernelComputeContext* ctx, user_op::OpKernelState* state,
+               const user_op::OpKernelCache* cache) const override {
     const user_op::Tensor* in = ctx->Tensor4ArgNameAndIndex("in", 0);
     const int64_t size = in->shape_view().elem_cnt();
     if (size == 0) return;
-    
+
     user_op::Tensor* out = ctx->Tensor4ArgNameAndIndex("output", 0);
     user_op::Tensor* noise_data = ctx->Tensor4ArgNameAndIndex("noise_data", 0);
     const T& lower = ctx->Attr<float>("lower");
@@ -57,13 +81,34 @@ class CudaRReluKernel final : public user_op::OpKernel {
     T* out_ptr = out->mut_dptr<T>();
     T* noise_ptr = noise_data->mut_dptr<T>();
     const T* in_ptr = in->dptr<T>();
-    
-    RandomGenerator<DeviceType::kCUDA> gen(NewRandomSeed(), ctx->stream());
-    gen.Uniform<T>(size, noise_ptr);
-    T range = upper - lower;
-    
-    RUN_CUDA_KERNEL(compute_rrelu<T>, ctx->stream(), size, in_ptr, out_ptr, noise_ptr, size, lower,
-                    upper, range);
+
+    auto* distribution_state = dynamic_cast<DistributionKernelState*>(state);
+    CHECK_NOTNULL(distribution_state);
+    const auto& generator = distribution_state->generator();
+    CHECK_NOTNULL(generator);
+    ep::CudaStream* cuda_stream = ctx->stream()->As<ep::CudaStream>();
+    const auto device_index = ctx->stream()->device()->device_index();
+    std::shared_ptr<one::CUDAGeneratorImpl> cuda_gen =
+        CHECK_JUST(generator->Get<one::CUDAGeneratorImpl>(device_index));
+    auto execution_policy = cuda_gen->CalcExecutionPolicy(size, cuda_stream);
+    auto counter_offset = std::get<0>(execution_policy);
+    uint64_t seed = cuda_gen->current_seed();
+    uint64_t offset = cuda_gen->get_philox_offset(counter_offset);
+
+    auto grid = std::get<1>(execution_policy);
+    auto block = std::get<2>(execution_policy);
+
+    if (std::is_same<T, double>::value) {
+      DistributionFunctor<DistributionOp::kUniform2Double> dist_functor;
+      compute_rrelu<T, 2,decltype(dist_functor)><<<grid, block, 0, cuda_stream->cuda_stream()>>>(
+          in_ptr, out_ptr, noise_ptr, size, lower, upper, seed, offset, dist_functor);
+
+    } else {
+      // float
+       DistributionFunctor<DistributionOp::kUniform4> dist_functor;
+      compute_rrelu<T, 4,decltype(dist_functor)><<<grid, block, 0, cuda_stream->cuda_stream()>>>(
+          in_ptr, out_ptr, noise_ptr, size, lower, upper, seed, offset, dist_functor);
+    }
   }
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
