78039f1826d520b26dd32dde00bd83c502eec038
Yipeng Li <jamesonli1313@gmail.com>
2023-01-19 16:53:43 +0800

    Add some ctrl edges back and remove some useless code

--- oneflow/core/job_rewriter/insert_nccl_logical_op_pass.cpp
+++ oneflow/core/job_rewriter/insert_nccl_logical_op_pass.cpp
@@ -527,6 +527,15 @@ void InsertNcclLogicalOpsAsCloseAsPossibleToDstNode(
             CHECK(old_lbn == GenLogicalBlobName(lbi));
           }
 
+          // add necessary ctrl edge for strict order
+          if (nccl_op_confs->size() >= 1) {
+            // NOTE(chengcheng): MUST add ctrl edge between nccl ops for 1 dst node insert
+            //  multi-nccl
+            const std::string& pre_nccl_op_name =
+                nccl_op_confs->at(nccl_op_confs->size() - 1).name();
+            nccl_op.add_ctrl_in_op_name(pre_nccl_op_name);
+          }
+
           // NOTE(chengcheng): dst_node Maybe not the first node in subgraph, try find the
           //   Immediately previous op of dst_node.
           std::string pre_op_name = "";
@@ -541,6 +550,7 @@ void InsertNcclLogicalOpsAsCloseAsPossibleToDstNode(
             if (src_op_name != pre_op_name) {
               // NOTE(chengcheng): MUST add ctrl edge for strict exec order
               CHECK(!pre_op_name.empty());
+              nccl_op.add_ctrl_in_op_name(pre_op_name);
             }
           } else {
             pre_op_name = src_op_name;
@@ -705,25 +715,13 @@ void InsertNcclLogicalOpsAfterAcc(const OpGraph& op_graph,
     op2subgraph_order.emplace(ordered_after_acc_subgraph.at(i), i);
   }
 
-  for (int64_t i = 1; i < ordered_after_acc_subgraph.size(); ++i) {
-    const OpNode* this_node = ordered_after_acc_subgraph.at(i);
-    const OpNode* pre_node = ordered_after_acc_subgraph.at(i - 1);
-    const std::string& this_op_name = this_node->op().op_name();
-    const std::string& pre_op_name = pre_node->op().op_name();
-    // build ctrl edge if need.
-    if (!IsReachable(pre_op_name, this_op_name)) {
-      auto it = mut_consumer_name2op->find(this_op_name);
-      if (it == mut_consumer_name2op->end()) {
-        auto ret_pair = mut_consumer_name2op->emplace(this_op_name, this_node->op().op_conf());
-        CHECK(ret_pair.second);
-        it = ret_pair.first;
-      }
-    }
-  }
-
   for (int64_t i = 0; i < nccl_op_infos.size(); ++i) {
     auto& info = nccl_op_infos.at(i);
-    if (i == 0) { info.nccl_op_conf.add_ctrl_in_op_name(bw_sink_tick_op_name); }
+    if (i == 0) {
+      info.nccl_op_conf.add_ctrl_in_op_name(bw_sink_tick_op_name);
+    } else {
+      info.nccl_op_conf.add_ctrl_in_op_name(nccl_op_infos.at(i - 1).nccl_op_conf.name());
+    }
 
     nccl_op_confs->emplace_back(info.nccl_op_conf);
     nccl_op_parallel_confs->emplace_back(info.nccl_parallel_conf);
@@ -742,6 +740,8 @@ void InsertNcclLogicalOpsAfterAcc(const OpGraph& op_graph,
           if (mut_consumer_name2op->find(next_op_name) == mut_consumer_name2op->end()) {
             CHECK(mut_consumer_name2op->emplace(next_op_name, next_op->op().op_conf()).second);
           }
+          // NOTE(chengcheng): MUST add ctrl edge for strict exec order
+          mut_consumer_name2op->at(next_op_name).add_ctrl_in_op_name(info.nccl_op_conf.name());
         }
       }
     }
@@ -808,17 +808,6 @@ void InsertNcclLogicalOpsInSubGraph(
   const OpNode* first_node = subgraph_order.at(0);
   HashMap<std::string, OperatorConf> subgraph_op_name2conf;
   subgraph_op_name2conf.emplace(first_node->op().op_name(), first_node->op().op_conf());
-
-  // add ctrl for strict order.
-  for (int64_t i = 1; i < subgraph_order.size(); ++i) {
-    const OpNode* this_node = subgraph_order.at(i);
-    const OpNode* pre_node = subgraph_order.at(i - 1);
-    const std::string& this_op_name = this_node->op().op_name();
-    const std::string& pre_op_name = pre_node->op().op_name();
-    CHECK(subgraph_op_name2conf.emplace(this_op_name, this_node->op().op_conf()).second);
-    // build ctrl edge if need.
-    if (!IsReachable(pre_op_name, this_op_name)) { mut_op_names.insert(this_op_name); }
-  }
 
   std::vector<OperatorConf> nccl_op_confs;
   std::vector<ParallelConf> nccl_op_parallel_confs;
