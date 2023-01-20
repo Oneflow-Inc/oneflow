c4dee14819ecfe7b230e3e540b26bd6719cb8032
jinjuncan <2301165479@qq.com>
2023-01-20 03:37:27 +0000

    add swa_utils

--- python/oneflow/nn/modules/batchnorm.py
+++ python/oneflow/nn/modules/batchnorm.py
@@ -126,6 +126,11 @@ class _BatchNorm(_NormBase):
         if self.training and self.track_running_stats:
             if self.num_batches_tracked is not None:
                 self.num_batches_tracked.add_(1)
+                # add cumulative moving average to match pytorch
+                if self.momentum is None:
+                    exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+                else:  # use exponential moving average
+                    exponential_average_factor = self.momentum
         if self.training:
             is_training = True
         else:
@@ -139,7 +144,7 @@ class _BatchNorm(_NormBase):
             self.bias,
             axis=self.channel_axis,
             epsilon=self.eps,
-            momentum=self.momentum,
+            momentum=exponential_average_factor,
             is_training=is_training,
         )
 
