fc3895ea07c323cccd21d865b75b01b7d476e965
liujuncheng <liujuncheng1022@gmail.com>
2023-01-29 21:03:51 +0800

    revert protobuf grpc

--- cmake/third_party/absl.cmake
+++ cmake/third_party/absl.cmake
@@ -2,8 +2,7 @@ include(ExternalProject)
 include(GNUInstallDirs)
 
 set(ABSL_PROJECT absl)
-set(ABSL_TAR_URL
-    https://github.com/abseil/abseil-cpp/archive/78be63686ba732b25052be15f8d6dee891c05749.tar.gz)
+set(ABSL_TAR_URL https://github.com/Oneflow-Inc/abseil-cpp/archive/ac258e3e0cb11aacb7cf75429989a0cbe0b71dc9.tar.gz)
 use_mirror(VARIABLE ABSL_TAR_URL URL ${ABSL_TAR_URL})
 set(ABSL_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/absl/src/absl)
 set(ABSL_INSTALL ${THIRD_PARTY_DIR}/absl)
@@ -28,6 +27,7 @@ else()
   set(ABSL_BUILD_LIBRARY_DIR ${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR})
   set(ABSL_LIBRARY_NAMES
       libabsl_spinlock_wait.a
+      libabsl_dynamic_annotations.a
       libabsl_malloc_internal.a
       libabsl_throw_delegate.a
       libabsl_int128.a
@@ -35,39 +35,7 @@ else()
       libabsl_str_format_internal.a
       libabsl_time.a
       libabsl_bad_optional_access.a
-      libabsl_base.a
-      libabsl_status.a
-      libabsl_synchronization.a
-      libabsl_cord.a
-      libabsl_cord_internal.a
-      libabsl_cordz_handle.a
-      libabsl_cordz_functions.a
-      libabsl_cordz_info.a
-      libabsl_raw_logging_internal.a
-      libabsl_strings_internal.a
-      libabsl_crc32c.a
-      libabsl_crc_cord_state.a
-      libabsl_crc_internal.a
-      libabsl_random_internal_pool_urbg.a
-      libabsl_random_internal_seed_material.a
-      libabsl_random_internal_platform.a
-      libabsl_random_internal_randen.a
-      libabsl_random_internal_randen_hwaes.a
-      libabsl_random_internal_randen_hwaes_impl.a
-      libabsl_random_internal_randen_slow.a
-      libabsl_random_seed_gen_exception.a
-      libabsl_bad_variant_access.a
-      libabsl_time_zone.a
-      libabsl_exponential_biased.a
-      libabsl_statusor.a
-      libabsl_strerror.a
-      libabsl_hash.a
-      libabsl_low_level_hash.a
-      libabsl_city.a
-      libabsl_symbolize.a
-      libabsl_debugging_internal.a
-      libabsl_demangle_internal.a
-      libabsl_stacktrace.a)
+      libabsl_base.a)
 endif()
 
 foreach(LIBRARY_NAME ${ABSL_LIBRARY_NAMES})
@@ -80,7 +48,7 @@ if(THIRD_PARTY)
     ${ABSL_PROJECT}
     PREFIX absl
     URL ${ABSL_TAR_URL}
-    URL_MD5 3db4fcf154b69ff52b2f43e583d89ded
+    URL_MD5 8a51092e32aeae60dc45980ea50b955e
     UPDATE_COMMAND ""
     BUILD_BYPRODUCTS ${ABSL_STATIC_LIBRARIES}
     CMAKE_CACHE_ARGS
@@ -90,6 +58,5 @@ if(THIRD_PARTY)
       -DCMAKE_INSTALL_LIBDIR:PATH=${ABSL_INSTALL}/${CMAKE_INSTALL_LIBDIR}
       -DCMAKE_INSTALL_MESSAGE:STRING=${CMAKE_INSTALL_MESSAGE}
       -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
-      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-      -DCMAKE_CXX_STANDARD:STRING=${CMAKE_CXX_STANDARD})
+      -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE})
 endif(THIRD_PARTY)
