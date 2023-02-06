fbe5f3bdaac174eec42fa58dc28776eb1a7d5aa5
rejoicesyc <int.rejoice@gmail.com>
2023-02-06 03:05:12 +0000

    add ContiguousParamsGroup

---
+++ python/oneflow/nn/utils/parameters_grouping.py
@@ -0,0 +1,206 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import collections
+from typing import List
+
+import oneflow as flow
+from oneflow.framework.tensor import Tensor
+
+
+def numel_in_bucket(tensor: Tensor):
+    assert flow.is_floating_point(
+        tensor
+    ), "params should be float tensor while grouping."
+
+    def align(x: int, unit_size: int):
+        return (x + (unit_size - 1)) // unit_size * unit_size
+
+    # tensor memory should be align to 512 bytes for cuda operations,
+    # align size depends on floating type
+    return align(
+        tensor.numel(),
+        flow._oneflow_internal.max_alignment_size()
+        // (flow.finfo(tensor.dtype).bits // 8),
+    )
+
+
+# recording all grouped params in use
+_all_grouped_params = set()
+
+
+class ContiguousParamsGroup(object):
+    def __init__(self, params_group_list: List[List[Tensor]]):
+        """
+        The ContiguousParamsGroup is created by 2D List of Tensors,
+        which indicates the Tensors in the same 1D List should be 
+        grouped into the same Tensor buffer.
+        """
+        assert not any(
+            [any([p.is_global for p in params]) for params in params_group_list]
+        ), "All parameters must be local tensor for params grouping."
+
+        self.params_group_list = params_group_list
+        self.grouped_tensors = []
+        self.grouped_tensors_offset = {} 
+        self.modified_buf = set()
+        last_extra_params = self._remapping_groups()
+        self._parameters_grouping(last_extra_params)
+
+    def _remapping_groups(self):
+        global _all_grouped_params
+
+        current_buffer_tensor_mapping = collections.defaultdict(set)
+        current_buffer_tensor_mapping[None] = set()
+        params_requires_grad_group_list = []
+        last_extra_params = _all_grouped_params.copy()
+
+        for params in self.params_group_list:
+            params_set = set()
+            for p in params:
+                if p.requires_grad:
+                    current_buffer_tensor_mapping[p._ref_tensor].add(p)
+                    self.grouped_tensors_offset[p._ref_tensor] = 0
+                    params_set.add(p)
+
+                    if p._ref_tensor is not None:
+                        self.modified_buf.add(p._ref_tensor)
+                        
+            params_requires_grad_group_list.append(params_set)
+
+            # handling parameters used last time but not this time
+            last_extra_params -= params_set
+
+            # updating all grouped parameters set
+            _all_grouped_params |= params_set
+
+        self.params_group_list = params_requires_grad_group_list
+
+        # clone last_extra_params data
+        new_last_extra_params = set()
+        for tensor in last_extra_params:
+            # since we do memcpy the tensors on buf, so we record the copy of data here
+            new_last_extra_params.add((tensor, tensor.detach().clone()))
+        last_extra_params = new_last_extra_params
+
+        new_params_group_list = []
+        for tensors_set in current_buffer_tensor_mapping.values():
+            for new_tensors_set in self.params_group_list:
+                # handling the new groups that satisfy both conditions
+                tensors_intersection = tensors_set & new_tensors_set
+
+                actual_tensors_group = collections.defaultdict(list)
+                for tensor in tensors_intersection:
+                    tensor_key = (tensor.dtype, tensor.device)
+                    actual_tensors_group[tensor_key].append((tensor, tensor.detach().clone()))
+
+                for actual_tensors in actual_tensors_group.values():
+                    if len(actual_tensors) > 0:
+                        new_params_group_list.append(actual_tensors)
+
+        self.params_group_list = new_params_group_list
+        return last_extra_params
+
+    def _parameters_grouping(self, last_extra_params):
+        # handling the params used last time but not this time
+        for (p, p_data) in last_extra_params:
+            param_buf = p._ref_tensor
+
+            if param_buf in self.modified_buf:
+                param_grad_buf = param_buf.grad
+                index = self.grouped_tensors_offset[param_buf]
+
+                size = p.numel()
+                shape = p.data.shape
+
+                param_buf[index : index + size] = p_data.data.view(-1)
+                p.data = param_buf[index : index + size].view(shape)
+                p.grad = param_grad_buf[index : index + size].view(shape)
+
+                index += numel_in_bucket(p)
+                self.grouped_tensors_offset[param_buf] = index
+
+        # handling the params used this time
+        for params in self.params_group_list:
+            physical_param_buf = params[0][0]._ref_tensor
+            logical_bufsize = sum([numel_in_bucket(p) for (p, _) in params])
+
+            if physical_param_buf == None:
+                dtype = params[0][0].dtype
+                device = params[0][0].device
+
+                physical_param_buf = flow.zeros(logical_bufsize, dtype=dtype, device=device)
+                physical_param_grad_buf = flow.zeros(logical_bufsize, dtype=dtype, device=device)
+                physical_param_buf.grad = physical_param_grad_buf
+
+                self.grouped_tensors_offset[physical_param_buf] = 0
+            else:
+                # reuse the previous allocated param_buf
+                physical_param_grad_buf = physical_param_buf.grad
+
+            index = self.grouped_tensors_offset[physical_param_buf]
+            physical_index_start = index
+
+            for (p, p_data) in params:
+                size = p.numel()
+                shape = p.data.shape
+                
+                physical_param_buf[index : index + size] = p_data.data.view(-1)
+                p.data = physical_param_buf[index : index + size].view(shape)
+                p._ref_tensor = physical_param_buf
+
+                """
+                Assigning p.grad makes p.grad_fn = <accumulate_grad>, which makes oneflow
+                and pytorch behave differently as following, but won't affect this function yet.
+
+                Oneflow:
+                >>> a = oneflow.ones(1)
+                >>> b = oneflow.ones(1, requires_grad=True)
+                >>> b
+                tensor([1.], dtype=oneflow.float32, requires_grad=True)
+                >>> b.grad = a[:1]
+                >>> b
+                tensor([1.], dtype=oneflow.float32, grad_fn=<accumulate_grad>)
+
+                Pytorch:
+                >>> a = torch.ones(1)
+                >>> b = torch.ones(1, requires_grad=True)
+                >>> b
+                tensor([1.], requires_grad=True)
+                >>> b.grad = a[:1]
+                >>> b
+                tensor([1.], requires_grad=True)
+                """
+                p.grad = physical_param_grad_buf[index : index + size].view(shape)
+
+                index += numel_in_bucket(p)
+
+            self.grouped_tensors_offset[physical_param_buf] = index
+
+            # construct the logical param_buf for new usage
+            logical_param_buf = (
+                physical_param_buf[physical_index_start : index].view(logical_bufsize)
+            )
+            logical_param_grad_buf = (
+                physical_param_grad_buf[physical_index_start : index].view(logical_bufsize)
+            )
+            logical_param_buf.grad = logical_param_grad_buf
+            self.grouped_tensors.append(logical_param_buf)
+
+        flow.cuda.empty_cache()
+
+    @property
+    def grouped_parameters(self):
+        return self.grouped_tensors
---
+++ python/oneflow/test/modules/test_parameters_grouping.py
@@ -0,0 +1,99 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import unittest
+import numpy as np
+from collections import OrderedDict
+
+import oneflow as flow
+from oneflow.test_utils.test_util import GenArgDict
+from oneflow.nn.utils.parameters_grouping import ContiguousParamsGroup as CPG
+from oneflow.nn.parameter import Parameter
+import oneflow.unittest
+
+
+def np_allclose_with_shape(a, b, *args, **kwargs):
+    return a.shape == b.shape and np.allclose(a, b, *args, **kwargs)
+
+
+def test_cpg_grouping(test_case, device):
+    init_seq = []
+
+    for i in range(7):
+        init_seq.append(np.random.uniform(size=(i + 1,)).astype(np.float32))
+
+    def _test_cpg_grad_helper():
+        a = Parameter(flow.Tensor(init_seq[0], device=flow.device(device)))
+        b = Parameter(flow.Tensor(init_seq[1], device=flow.device(device)))
+        c = Parameter(flow.Tensor(init_seq[2], device=flow.device(device)))
+        d = Parameter(flow.Tensor(init_seq[3], device=flow.device(device)))
+        e = Parameter(flow.Tensor(init_seq[4], device=flow.device(device)))
+        g = Parameter(flow.Tensor(init_seq[6], device=flow.device(device)))
+
+        loss = a.sum() + b.sum() + c.sum() + d.sum() + e.sum() + g.sum()
+        loss.backward()
+        return [a.grad.numpy(), b.grad.numpy(), c.grad.numpy(), d.grad.numpy(), e.grad.numpy(), g.grad.numpy()]
+
+    def _test_cpg_regrouping():
+        # groups0 = [[a, b], [c, d, e, f]]
+        # groups1 = [[e, d, g], [a], [b, c]]
+        # -> [[d, e], [g], [a], [b], [c]]
+
+        a = Parameter(flow.Tensor(init_seq[0], device=flow.device(device)))
+        b = Parameter(flow.Tensor(init_seq[1], device=flow.device(device)))
+        c = Parameter(flow.Tensor(init_seq[2], device=flow.device(device)))
+        d = Parameter(flow.Tensor(init_seq[3], device=flow.device(device)))
+        e = Parameter(flow.Tensor(init_seq[4], device=flow.device(device)))
+        f = Parameter(flow.Tensor(init_seq[5], device=flow.device(device)))
+        g = Parameter(flow.Tensor(init_seq[6], device=flow.device(device)))
+
+        groups0 = [[a, b], [c, d, e, f]]
+        cpg = CPG(groups0)
+
+        test_case.assertTrue(len(cpg.grouped_parameters) == 2)
+        test_case.assertTrue(len(set([p._ref_tensor for p in [a, b, c, d, e, f]])) == 2)
+        test_case.assertTrue(len(set([p._ref_tensor for p in [a, b]])) == 1)
+        test_case.assertTrue(len(set([p._ref_tensor for p in [c, d, e, f]])) == 1)
+
+        groups1 = [[e, d, g], [a], [b, c]]
+        cpg = CPG(groups1)
+
+        test_case.assertTrue(len(cpg.grouped_parameters) == 5)
+        test_case.assertTrue(len(set([p._ref_tensor for p in [a, b, c, d, e, g]])) == 3)
+        test_case.assertTrue(len(set([p._ref_tensor for p in [d, e]])) == 1)
+
+        loss = a.sum() + b.sum() + c.sum() + d.sum() + e.sum() + g.sum()
+        loss.backward()
+
+        return [a.grad.numpy(), b.grad.numpy(), c.grad.numpy(), d.grad.numpy(), e.grad.numpy(), g.grad.numpy()]
+
+    grouped_grad = _test_cpg_regrouping()
+    grad = _test_cpg_grad_helper()
+
+    for (x, y) in zip(grouped_grad, grad):
+        test_case.assertTrue(np_allclose_with_shape(x, y))
+
+
+@flow.unittest.skip_unless_1n1d()
+class TestCPG(flow.unittest.TestCase):
+    def test_cpg(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["device"] = ["cuda", "cpu"]
+        for arg in GenArgDict(arg_dict):
+            test_cpg_grouping(test_case, **arg)
+
+
+if __name__ == "__main__":
+    unittest.main()
