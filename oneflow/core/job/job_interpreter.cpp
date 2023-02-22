f5272934f69cce90bc990a36f24ad73b5e273007
daquexian <daquexian566@gmail.com>
2023-02-22 20:57:32 +0800

    refine
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- oneflow/core/job/job_interpreter.cpp
+++ oneflow/core/job/job_interpreter.cpp
@@ -10,10 +10,10 @@
 namespace oneflow {
 namespace one {
 
-Maybe<one::TensorTuple> InterpretJob(const one::TensorTuple& graph_inputs,
-                                     const std::shared_ptr<NNGraph>& graph) {
-  const auto& job = graph->job();
-  std::map<std::string, std::shared_ptr<Tensor>> env;
+using Env = std::map<std::string, std::shared_ptr<Tensor>>;
+
+Maybe<Env> InitEnv(const one::TensorTuple& graph_inputs, const std::shared_ptr<NNGraph>& graph) {
+  Env env;
   for (const auto& [name, tensor] : graph->variable_op_name2tensor()) {
     env.emplace(name + "/out", tensor);
   }
@@ -21,111 +21,148 @@ Maybe<one::TensorTuple> InterpretJob(const one::TensorTuple& graph_inputs,
     const auto& name = graph->inputs_op_names()[i];
     env.emplace(name + "/out", JUST(VectorAt(graph_inputs, i)));
   }
+  return env;
+}
 
-  // tensors in dead_tensors[i] will not be accessed any more after i-th op
-  // so they can be released after i-th op's execution
+Maybe<UserOpExpr> OpConfToUserOpExpr(const OperatorConf& op_conf) {
+  CHECK_OR_RETURN(op_conf.has_user_conf());
+  const auto& user_conf = op_conf.user_conf();
+  auto builder = OpBuilder(user_conf.op_type_name());
+  for (const auto& pair : user_conf.attr()) { builder.Attr(pair.first, pair.second); }
+  for (const auto& pair : user_conf.input()) {
+    // ignore "UserSourceOpTickInput"
+    if (pair.first == "UserSourceOpTickInput") { continue; }
+    builder.Input(pair.first, pair.second.s_size());
+  }
+  for (const auto& pair : user_conf.output()) { builder.Output(pair.first, pair.second.s_size()); }
+  return JUST(builder.Build());
+}
+
+template<typename Func>
+Maybe<TensorTuple> GetInputTensors(const UserOpConf& user_conf, const Env& env,
+                                   const Func& preprocess) {
+  TensorTuple inputs;
+  for (const auto& pair : user_conf.input()) {
+    if (pair.first == "UserSourceOpTickInput") { continue; }
+    for (const auto& name : pair.second.s()) {
+      inputs.emplace_back(preprocess(JUST(MapAt(env, name))));
+    }
+  }
+  return inputs;
+}
+
+OpArgsVector<std::string> GetOutputNamesOfOp(const UserOpConf& user_conf) {
+  OpArgsVector<std::string> output_names;
+  for (const auto& pair : user_conf.output()) {
+    for (const auto& name : pair.second.s()) { output_names.emplace_back(name); }
+  }
+  return output_names;
+}
+
+// Only support a limited subset of view ops for now
+bool IsViewOp(const std::shared_ptr<UserOpExpr>& op) {
+  return op->op_type_name() == "reshape" || op->op_type_name() == "expand_dims";
+}
+
+Maybe<void> RunViewOp(const std::shared_ptr<UserOpExpr>& op, Env& env, const TensorTuple& inputs,
+                      const OpArgsVector<std::string>& output_names) {
+  // eliminate the memcpy of view ops
+  CHECK_OR_RETURN(IsViewOp(op));
+  const std::shared_ptr<const LocalTensorInferResult> result =
+      JUST([&]() -> Maybe<const LocalTensorInferResult> {
+        LocalTensorMetaInferArgs infer_args;
+        JUST(infer_args.Init(op->base_attrs(), JUST(inputs[0]->device()), inputs));
+        return JUST(op->mut_local_tensor_infer_cache()->GetOrInfer(infer_args));
+      }());
+  const auto& output_shape = result->output_tensor_metas()[0]->shape();
+  const auto output =
+      JUST(view::BasicView(inputs[0], output_shape, JUST(inputs[0]->storage_offset())));
+  env.emplace(output_names[0], output);
+  return Maybe<void>::Ok();
+}
+
+Maybe<void> RunNormalOp(const std::shared_ptr<UserOpExpr>& op, Env& env, const TensorTuple& inputs,
+                        const OpArgsVector<std::string>& output_names) {
+  TensorTuple outputs(output_names.size());
+  static EagerLocalInterpreter it;
+  static AttrMap empty_attr_map;
+  JUST(it.Apply(*op, inputs, &outputs, empty_attr_map));
+  for (size_t i = 0; i < output_names.size(); ++i) {
+    env.emplace(output_names[i], JUST(VectorAt(outputs, i)));
+  }
+  return Maybe<void>::Ok();
+}
+
+// tensors in dead_tensors[i] will not be accessed any more after i-th op
+// so they can be released once i-th op's execution finishes.
+std::vector<std::vector<std::string>> GetDeadTensorVector(const Job& job) {
   std::vector<std::vector<std::string>> dead_tensors(job.net().op_size());
-  {
-    std::set<std::string> visited;
-    for (int i = job.net().op_size() - 1; i >= 0; --i) {
-      const auto& op_conf = job.net().op(i);
-      // do not release the graph output tensors
-      if (op_conf.has_output_conf()) {
-        const auto& output_conf = op_conf.output_conf();
-        visited.insert(output_conf.in());
-      } else if (op_conf.has_user_conf()) {
-        const auto& user_conf = op_conf.user_conf();
-        for (const auto& pair : user_conf.input()) {
-          if (pair.first == "UserSourceOpTickInput") { continue; }
-          for (const auto& name : pair.second.s()) {
-            if (visited.find(name) == visited.end()) {
-              dead_tensors[i].push_back(name);
-              visited.insert(name);
-            }
+  std::set<std::string> visited;
+  for (int i = job.net().op_size() - 1; i >= 0; --i) {
+    const auto& op_conf = job.net().op(i);
+    // do not release the graph output tensors
+    if (op_conf.has_output_conf()) {
+      const auto& output_conf = op_conf.output_conf();
+      visited.insert(output_conf.in());
+    } else if (op_conf.has_user_conf()) {
+      const auto& user_conf = op_conf.user_conf();
+      for (const auto& pair : user_conf.input()) {
+        if (pair.first == "UserSourceOpTickInput") { continue; }
+        for (const auto& name : pair.second.s()) {
+          if (visited.find(name) == visited.end()) {
+            dead_tensors[i].push_back(name);
+            visited.insert(name);
           }
         }
       }
     }
   }
+  return dead_tensors;
+}
 
-  auto ToDevice = [](const std::shared_ptr<Tensor>& tensor, const std::string& device_tag) {
-    if (CHECK_JUST(tensor->device())->type() == device_tag) { return tensor; }
-    return CHECK_JUST(functional::Copy(tensor, device_tag, 0, false));
-  };
-  if (graph->cached_op_exprs.empty()) {
-    for (int i = 0; i < job.net().op_size(); i++) {
-      const auto& op_conf = job.net().op(i);
-      if (op_conf.has_user_conf()) {
-        const auto& user_conf = op_conf.user_conf();
-        auto builder = OpBuilder(user_conf.op_type_name());
-        for (const auto& pair : user_conf.attr()) { builder.Attr(pair.first, pair.second); }
-        for (const auto& pair : user_conf.input()) {
-          // ignore "UserSourceOpTickInput"
-          if (pair.first == "UserSourceOpTickInput") { continue; }
-          builder.Input(pair.first, pair.second.s_size());
-        }
-        for (const auto& pair : user_conf.output()) {
-          builder.Output(pair.first, pair.second.s_size());
-        }
-        auto op = JUST(builder.Build());
-        graph->cached_op_exprs.push_back(op);
-      } else {
-        graph->cached_op_exprs.push_back(nullptr);
-      }
+Maybe<void> InitOpExprs(const std::shared_ptr<NNGraph>& graph) {
+  CHECK_OR_RETURN(graph->cached_op_exprs.empty());
+
+  const auto& job = graph->job();
+  for (int i = 0; i < job.net().op_size(); i++) {
+    const auto& op_conf = job.net().op(i);
+    if (op_conf.has_user_conf()) {
+      const auto op_expr = JUST(OpConfToUserOpExpr(op_conf));
+      graph->cached_op_exprs.push_back(op_expr);
+    } else {
+      graph->cached_op_exprs.push_back(nullptr);
     }
   }
+  return Maybe<void>::Ok();
+}
+
+Maybe<one::TensorTuple> InterpretJob(const one::TensorTuple& graph_inputs,
+                                     const std::shared_ptr<NNGraph>& graph) {
+  if (graph->cached_op_exprs.empty()) { JUST(InitOpExprs(graph)); }
+
+  const auto& job = graph->job();
+  auto env = *JUST(InitEnv(graph_inputs, graph));
+
+  const auto dead_tensors = GetDeadTensorVector(job);
 
   one::TensorTuple graph_outputs;
-  EagerLocalInterpreter it;
   for (int i = 0; i < job.net().op_size(); i++) {
     const auto& op_conf = job.net().op(i);
     if (op_conf.has_user_conf()) {
       auto op = CHECK_NOTNULL(graph->cached_op_exprs[i]);
       const auto& user_conf = op_conf.user_conf();
       OF_PROFILER_RANGE_GUARD(user_conf.op_type_name());
-      const auto& op_type_name = user_conf.op_type_name();
-      // eliminate the memcpy of view ops
-      // TODO: use a more elegant way
-      if (op_type_name == "reshape" || op_type_name == "expand_dims") {
-        const auto& input_name = user_conf.input().begin()->second.s(0);
-        const auto& output_name = user_conf.output().begin()->second.s(0);
-        if (op_type_name == "reshape") {
-          env.emplace(output_name,
-                      JUST(view::Reshape(JUST(MapAt(env, input_name)),
-                                         Shape(user_conf.attr().at("shape").at_shape()))));
-        } else if (op_type_name == "expand_dims") {
-          env.emplace(output_name, JUST(view::Unsqueeze(JUST(MapAt(env, input_name)),
-                                                        user_conf.attr().at("axis").at_int32())));
-        }
+      TensorTuple inputs =
+          *JUST(GetInputTensors(user_conf, env, [&op_conf](const std::shared_ptr<Tensor>& tensor) {
+            return CHECK_JUST(functional::To(tensor, op_conf.device_tag()));
+          }));
+      OpArgsVector<std::string> output_names = GetOutputNamesOfOp(user_conf);
+      if (IsViewOp(op)) {
+        JUST(RunViewOp(op, env, inputs, output_names));
       } else {
-        TensorTuple inputs;
-        for (const auto& pair : user_conf.input()) {
-          if (pair.first == "UserSourceOpTickInput") { continue; }
-          for (const auto& name : pair.second.s()) {
-            inputs.emplace_back(ToDevice(JUST(MapAt(env, name)), op_conf.device_tag()));
-          }
-        }
-        small_vector<std::string, 4> output_names;
-        TensorTuple outputs;
-        for (const auto& pair : user_conf.output()) {
-          for (const auto& name : pair.second.s()) {
-            output_names.emplace_back(name);
-            if (env.find(name) == env.end()) {
-              outputs.emplace_back();
-            } else {
-              outputs.emplace_back(env[name]);
-            }
-          }
-        }
-        static AttrMap empty_attr_map;
-        JUST(it.Apply(*op, inputs, &outputs, empty_attr_map));
-        for (size_t i = 0; i < output_names.size(); ++i) {
-          env.emplace(output_names[i], JUST(VectorAt(outputs, i)));
-        }
+        JUST(RunNormalOp(op, env, inputs, output_names));
       }
-      for (const auto& name : dead_tensors[i]) {
-        CHECK_EQ_OR_RETURN(env.erase(name), 1);
-      }
+      for (const auto& name : dead_tensors[i]) { CHECK_EQ_OR_RETURN(env.erase(name), 1); }
     } else if (op_conf.has_output_conf()) {
       const auto& output_conf = op_conf.output_conf();
       graph_outputs.emplace_back(JUST(MapAt(env, output_conf.in())));
