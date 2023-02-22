0f479f621dd01f555e903526f6e99e482bcaa732
yuhao <1171760467@qq.com>
2023-02-22 07:27:55 +0000

    fix

--- oneflow/ir/test/OneFlow/kernel_launch/test_resnet.py
+++ oneflow/ir/test/OneFlow/kernel_launch/test_resnet.py
@@ -1,9 +1,12 @@
 """
 Copyright 2020 The OneFlow Authors. All rights reserved.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
+
     http://www.apache.org/licenses/LICENSE-2.0
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -12,22 +15,21 @@ limitations under the License.
 """
 # RUN: python3 -m oneflow.test_utils.throttle --with-cuda=%with_cuda python3 %s
 
+from networks.resnet50 import resnet50
+import oneflow.unittest
+import oneflow as flow
+import numpy as np
+import unittest
 import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(os.path.abspath(os.path.dirname(__file__)) + "/..")
 
+# TODO(peihong): extract MLIR ir env variables into a single module to control.
 os.environ["ONEFLOW_MLIR_ENABLE_ROUND_TRIP"] = "1"
 os.environ["ONEFLOW_MLIR_FUSE_KERNEL_LAUNCH"] = "1"
 os.environ["ONEFLOW_KERNEL_ENABLE_CUDA_GRAPH"] = "1"
-os.environ["ONEFLOW_MLIR_ENABLE_IR_PRINTING"] = "1"
-
-import unittest
-import numpy as np
-import oneflow as flow
-import oneflow.unittest
-from networks.resnet50 import resnet50
 
 
 def _test_okl_resnet(test_case):
