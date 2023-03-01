39395a37474e24e549ac6c1debbf3d78d38fd0c6
lixiang007666 <88304454@qq.com>
2023-03-01 08:26:22 +0000

    Revert

--- python/oneflow/test/tensor/test_tensor_part_1.py
+++ python/oneflow/test/tensor/test_tensor_part_1.py
@@ -839,7 +839,7 @@ class TestTensor(flow.unittest.TestCase):
         y = x.argmax(dim=random(0, ndim).to(int), keepdim=random().to(bool))
         return y
 
-    @autotest(auto_backward=False, check_graph=True)
+    @autotest(auto_backward=False, check_graph=False)
     def test_max_bool_input_with_random_data(test_case):
         device = random_device()
         dim = random(1, 4).to(int)
@@ -848,7 +848,7 @@ class TestTensor(flow.unittest.TestCase):
         )
         return x.max(dim)
 
-    @autotest(auto_backward=False, check_graph=True)
+    @autotest(auto_backward=False, check_graph=False)
     def test_min_bool_input_with_random_data(test_case):
         device = random_device()
         dim = random(1, 4).to(int)
