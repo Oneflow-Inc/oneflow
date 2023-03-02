8a3718380f4ea8cfe67bb8fcb40b9cd81c43bd4b
lixiang007666 <88304454@qq.com>
2023-03-02 11:43:31 +0000

    Refine type

--- python/oneflow/test_utils/automated_test_util/torch_flow_dual_object.py
+++ python/oneflow/test_utils/automated_test_util/torch_flow_dual_object.py
@@ -426,7 +426,7 @@ def get_functional_graph_res(
             test_g_res = oneflow_res
         # For some ops whose input parameters is int, 'int' object has no attribute 'placement'.
         elif (
-            is_global() and len(oneflow_args) != 0 and isinstance(oneflow_args[0], int)
+            is_global() and len(oneflow_args) != 0 and isinstance(oneflow_args[0], (int, float))
         ):
             test_g_res = oneflow_res
         # When doing the global op test, get_global_test_device() will be executed, and temporarily skipping the graph autotest on cpu device.
