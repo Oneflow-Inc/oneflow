22f9f5d543a42667403456d5337db28c0e9e2811
hhhfccz <hjk1938927583@163.com>
2023-03-03 08:11:11 +0000

    add: docs

--- python/oneflow/nn/modules/module.py
+++ python/oneflow/nn/modules/module.py
@@ -1093,6 +1093,36 @@ class Module(object):
     def register_full_backward_hook(
         self, hook: Callable[["Module", _grad_t, _grad_t], Union[None, Tensor]],
     ):
+        r"""Registers a backward hook on the module.
+
+        The hook will be called every time the gradients with respect to module
+        inputs are computed. The hook should have the following signature::
+
+            hook(module, grad_input, grad_output) -> TensorTuple or None
+
+        The :attr:`grad_input` and :attr:`grad_output` are :class:`oneflow.TensorTuple` that contain the gradients
+        with respect to the inputs and outputs respectively. The hook should
+        not modify its arguments, but it can optionally return a new gradient with
+        respect to the input that will be used in place of :attr:`grad_input` in
+        subsequent computations. :attr:`grad_input` will only correspond to the inputs given
+        as positional arguments and all kwarg arguments are ignored. Entries
+        in :attr:`grad_input` and :attr:`grad_output` will be ``None`` for all non-Tensor
+        arguments.
+
+        For technical reasons, when this hook is applied to a Module, its forward function will
+        receive a view of each Tensor passed to the Module. Similarly the caller will receive a view
+        of each Tensor returned by the Module's forward function.
+
+        .. warning ::
+            Modifying inputs or outputs inplace is not allowed when using backward hooks and
+            will raise an error.
+
+        Returns:
+            :class:`oneflow.utils.hooks.RemovableHandle`:
+                a handle that can be used to remove the added hook by calling
+                ``handle.remove()``
+
+        """
         if self._is_full_backward_hook is False:
             raise RuntimeError(
                 "Cannot use both regular backward hooks and full backward hooks on a "
