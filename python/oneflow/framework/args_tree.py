7ee77d572e2232d470b6b18901fa3d37ed8760cb
lixiang007666 <88304454@qq.com>
2023-03-05 13:45:48 +0000

    Modify iter tuple nodes

--- python/oneflow/framework/args_tree.py
+++ python/oneflow/framework/args_tree.py
@@ -173,20 +173,23 @@ class ArgsTree(object):
         Only iterate through nodes of type tuple or list.
         """
         assert not self._gen_name, "Only use this if gen_name is not set!"
-        args_to_iter = self._io_args
 
-        stack = []
-        stack.append(iter(args_to_iter))
-        while len(stack) > 0:
-            curr = next(stack[-1], None)
-            if curr is None:
-                stack.pop()
-                continue
+        for node in self._io_args:
+            yield node
+        # args_to_iter = self._io_args
 
-            if _is_raw_type(curr, list) or _is_raw_type(curr, tuple):
-                stack.append(iter(curr))
-            else:
-                yield curr
+        # stack = []
+        # stack.append(iter(args_to_iter))
+        # while len(stack) > 0:
+        #     curr = next(stack[-1], None)
+        #     if curr is None:
+        #         stack.pop()
+        #         continue
+
+        #     if _is_raw_type(curr, list) or _is_raw_type(curr, tuple):
+        #         stack.append(iter(curr))
+        #     else:
+        #         yield curr
 
     def iter_named_nodes(self):
         assert self._gen_name, "Only use this if gen_name is set!"
