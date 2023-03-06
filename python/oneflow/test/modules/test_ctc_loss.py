6dc6e7d45050647f133192b4f832bd77aaa716b6
brandonliu2 <brandonliulc@gmail.com>
2023-03-06 07:35:23 +0000

    add TODO comment

--- python/oneflow/test/modules/test_ctc_loss.py
+++ python/oneflow/test/modules/test_ctc_loss.py
@@ -288,6 +288,7 @@ def gen_arg_list():
 
 @flow.unittest.skip_unless_1n1d()
 class TestCTCLoss1n1d(flow.unittest.TestCase):
+    #TODO TEMPORARILY removed. ci test sometimes fail here, need further investigation
     # def test_ctc_loss(test_case):
     #     for arg in gen_arg_list():
     #         compare_with_np(*arg)
