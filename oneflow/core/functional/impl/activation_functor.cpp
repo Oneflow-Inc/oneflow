a85c347721589b6bfbf9ea5bebcfd23239a2e8d2
ccsuu <ccsuwen@gmail.com>
2023-03-16 03:23:10 +0000

    fix add_frac_op

--- oneflow/core/functional/impl/activation_functor.cpp
+++ oneflow/core/functional/impl/activation_functor.cpp
@@ -13,8 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <iostream>
-#include <vector>
 #include "oneflow/core/common/container_util.h"
 #include "oneflow/core/common/error.h"
 #include "oneflow/core/common/scalar.h"
