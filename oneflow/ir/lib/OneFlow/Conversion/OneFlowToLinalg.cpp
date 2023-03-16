7d2b23aee29024e582fe9b97e700a52aa611c926
jackalcooper <jackalcooper@gmail.com>
2023-03-16 17:19:42 +0800

    add add pat for softmax

--- oneflow/ir/lib/OneFlow/Conversion/OneFlowToLinalg.cpp
+++ oneflow/ir/lib/OneFlow/Conversion/OneFlowToLinalg.cpp
@@ -1,15 +1,39 @@
-
-
+#include "OneFlow/OneFlowOps.h"
 #include "OneFlow/Passes.h"
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Transforms/DialectConversion.h"
+
 namespace mlir {
 
 namespace oneflow {
 
 namespace {
 
+struct SoftmaxOpLowering final : public OpConversionPattern<SoftmaxOp> {
+ public:
+  using OpConversionPattern<SoftmaxOp>::OpConversionPattern;
+  LogicalResult matchAndRewrite(SoftmaxOp op, OpAdaptor adaptor,
+                                ConversionPatternRewriter& rewriter) const override {
+    return success();
+  }
+};
+
 struct OneFlowLoweringToLinalgPass
     : public LowerOneFlowToLinalgPassBase<OneFlowLoweringToLinalgPass> {
-  void runOnOperation() {}
+  void runOnOperation() {
+    MLIRContext* context = &getContext();
+    ConversionTarget target(*context);
+    target
+        .addLegalDialect<memref::MemRefDialect, mlir::func::FuncDialect, tosa::TosaDialect,
+                         linalg::LinalgDialect, tensor::TensorDialect, arith::ArithmeticDialect>();
+    RewritePatternSet patterns(context);
+    patterns.add<SoftmaxOpLowering>(context);
+    if (failed(applyPartialConversion(getOperation(), target, std::move(patterns)))) {
+      signalPassFailure();
+    }
+  }
 };
 
 }  // namespace
