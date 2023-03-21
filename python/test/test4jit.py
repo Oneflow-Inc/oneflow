d513a5e2e336634f4edc79e4bbb9d811438f6777
ShiYao <60888751+AtomicReactor-lab@users.noreply.github.com>
2023-03-21 21:41:51 +0800

    Update test4jit.py

--- python/test/test4jit.py
+++ python/test/test4jit.py
@@ -5,15 +5,16 @@ mock.enable()
 import torch
 from torch import nn
 
-model = nn.Sequential(
+@autotest()
+def testcase4module():
+    model = nn.Sequential(
     nn.Linear(5, 3),
     nn.Linear(3, 1)
-)
-
-if isinstance(model, torch.jit.ScriptModule):
-    print(True)
-else:
-    print(False)
+    )
+    if isinstance(model, torch.jit.ScriptModule):
+        print(True)
+    else:
+        print(False)
 #原先报错为ModuleNotFoundError: No module named 'oneflow.jit.__ScriptModule'
 #修改后打印信息为false，修改完毕
 
