304e4673d8b0088b4d718829d3e6cc953f503d8b
ShiYao <60888751+AtomicReactor-lab@users.noreply.github.com>
2023-03-21 22:57:49 +0800

    Update test4jit.py

--- python/test/test4jit.py
+++ python/test/test4jit.py
@@ -1,4 +1,20 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import oneflow
+from oneflow.test_utils.automated_test_util.torch_flow_dual_object import autotest
 
 @autotest()
 def testcase4module():
@@ -7,9 +23,9 @@ def testcase4module():
     oneflow.nn.Linear(3, 1)
     )
     if isinstance(model, oneflow.jit.ScriptModule):
-        print(True)
+        return True
     else:
-        print(False)
-#原先报错为ModuleNotFoundError: No module named 'oneflow.jit.__ScriptModule'
-#修改后打印信息为false，修改完毕
+        return False
 
+if __name__ == "__main__":
+    testcase4module()
