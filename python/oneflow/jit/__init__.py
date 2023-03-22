b993d228d7abf8f0d0e360f451bcaad553786435
ShiYao <60888751+AtomicReactor-lab@users.noreply.github.com>
2023-03-22 20:17:25 +0800

    Update __init__.py
    无关文件

--- python/oneflow/jit/__init__.py
+++ python/oneflow/jit/__init__.py
@@ -71,14 +71,3 @@ def is_scripting():
 def is_tracing():
     return False
 
-def ScriptModule(
-    obj,
-    optimize=None,
-    _frames_up=0,
-    _rcb=None,
-    example_inputs: Union[List[Tuple], Dict[Callable, List[Tuple]], None] = None,
-):
-    warnings.warn(
-        "The oneflow.jit.ScriptModule interface is just to align the torch.jit.script interface and has no practical significance."
-    )
-    return obj
