3435a70d3dcc02028485dae10db8f6028b111b9e
Luyang <flowingsun007@163.com>
2023-03-23 11:25:55 +0800

    Dev support mlu normalization training (#56)
    * refine batchnorm inference
    
    * impl of mlu batchnorm training kernel
    
    * refine
    
    * refine
    
    * refine
    
    * refine

--- oneflow/cambricon/kernels/normalization_kernel.cpp
+++ oneflow/cambricon/kernels/normalization_kernel.cpp
@@ -25,16 +25,35 @@ limitations under the License.
 
 namespace oneflow {
 
+namespace{
+
 template<typename Context>
 std::unique_ptr<ep::primitive::Permute> NewPermutePrimitive(Context* ctx, const int& num_dims) {
   return ep::primitive::NewPrimitive<ep::primitive::PermuteFactory>(ctx->device_type(), num_dims);
 }
 
+void infer_channel_sizes(const ShapeView& shape, int* n, int* c, int* h, int* w){
+  if (shape.NumAxes() == 2) {
+    *n = shape.At(0);
+    *h = 1;
+    *w = 1;
+    *c = shape.At(1);
+  } else {
+    *n = shape.At(0);
+    *c = shape.At(1);
+    *h = shape.At(2);
+    *w = shape.At(3);
+  }
+}
+
+} // namespace
+
+
 template<typename T>
-class MluNormalizationKernel final : public user_op::OpKernel {
+class MluNormalizationInferenceKernel final : public user_op::OpKernel {
  public:
-  MluNormalizationKernel() = default;
-  ~MluNormalizationKernel() = default;
+  MluNormalizationInferenceKernel() = default;
+  ~MluNormalizationInferenceKernel() = default;
 
  private:
   using user_op::OpKernel::Compute;
@@ -54,18 +73,8 @@ class MluNormalizationKernel final : public user_op::OpKernel {
     const auto epsilon = ctx->Attr<float>("epsilon");
 
     int n = 0, c = 0, h = 0, w = 0;
-    if (x->shape_view().NumAxes() == 2) {
-      n = x->shape_view().At(0);
-      h = 1;
-      w = 1;
-      c = x->shape_view().At(1);
-    } else {
-      n = x->shape_view().At(0);
-      c = x->shape_view().At(1);
-      h = x->shape_view().At(2);
-      w = x->shape_view().At(3);
-    }
-
+    infer_channel_sizes(x->shape_view(), &n, &c, &h, &w);
+    
     size_t tmp_in_size = x->shape_view().elem_cnt() * GetSizeOfDataType(x->data_type());
     size_t tmp_out_size = y->shape_view().elem_cnt() * GetSizeOfDataType(y->data_type());
     CnnlWorkspace tmp_in_workspace(ctx->stream()->As<ep::MluStream>(), tmp_in_size);
@@ -88,6 +97,8 @@ class MluNormalizationKernel final : public user_op::OpKernel {
     output_desc.set(4, shape_nhwc, dtype, CNNL_LAYOUT_NHWC);
     int dim[1] = {c};
     weight_bias_mean_var_desc.set(1, dim, dtype, CNNL_LAYOUT_ARRAY);
+    // api reference:
+    // https://www.cambricon.com/docs/sdk_1.10.0/cambricon_cnnl_1.15.2/developer_guide/cnnl_api/api/batchnorm.html#cnnlbatchnormforwardinference
     // inference
     OF_CNNL_CHECK(cnnlBatchNormForwardInference(
         ctx->stream()->As<ep::MluStream>()->cnnl_handle(), nullptr, nullptr, input_desc.desc(),
@@ -103,15 +114,116 @@ class MluNormalizationKernel final : public user_op::OpKernel {
   bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
 };
 
-#define REGISTER_BN_IINFERENCE_MLU_KERNEL(dtype)                      \
-  REGISTER_USER_KERNEL("normalization")                               \
-      .SetCreateFn<MluNormalizationKernel<dtype>>()                   \
-      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU) \
-                       && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value));
+#define REGISTER_BN_INFERENCE_MLU_KERNEL(dtype)                                       \
+  REGISTER_USER_KERNEL("normalization")                                               \
+      .SetCreateFn<MluNormalizationInferenceKernel<dtype>>()                          \
+      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU)                 \
+                       && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value) \
+                       && (user_op::HobAttr<bool>("training") == false));
 
-REGISTER_BN_IINFERENCE_MLU_KERNEL(float)
-REGISTER_BN_IINFERENCE_MLU_KERNEL(float16)
+REGISTER_BN_INFERENCE_MLU_KERNEL(float)
+REGISTER_BN_INFERENCE_MLU_KERNEL(float16)
 
-#undef REGISTER_BN_IINFERENCE_MLU_KERNEL
+#undef REGISTER_BN_INFERENCE_MLU_KERNEL
+
+template<typename T>
+class MluNormalizationTrainingKernel final : public user_op::OpKernel {
+ public:
+  MluNormalizationTrainingKernel() = default;
+  ~MluNormalizationTrainingKernel() = default;
+
+ private:
+  using user_op::OpKernel::Compute;
+
+  void Compute(user_op::KernelComputeContext* ctx) const override {
+    const bool training = ctx->Attr<bool>("training");
+    CHECK(training);
+    const user_op::Tensor* x = ctx->Tensor4ArgNameAndIndex("x", 0);
+    user_op::Tensor* y = ctx->Tensor4ArgNameAndIndex("y", 0);
+
+    const auto axis = ctx->Attr<int32_t>("axis");
+    const auto epsilon = ctx->Attr<float>("epsilon");
+    const auto momentum = ctx->Attr<float>("momentum");
+
+    const DataType data_type = x->data_type();
+    CHECK_EQ(x->shape_view(), y->shape_view());
+    CHECK_EQ(y->data_type(), data_type);
+    CHECK_LT(axis, x->shape_view().NumAxes());
+    // make sure input tensor's format NCHW, so channel axis must be 1
+    CHECK_EQ(axis, 1);
+
+    const auto* gamma = ctx->Tensor4ArgNameAndIndex("gamma", 0);
+    const auto* beta = ctx->Tensor4ArgNameAndIndex("beta", 0);
+    auto* mean = ctx->Tensor4ArgNameAndIndex("mean", 0);
+    auto* inv_variance = ctx->Tensor4ArgNameAndIndex("inv_variance", 0);
+
+    user_op::Tensor* moving_mean = nullptr;
+    user_op::Tensor* moving_variance = nullptr;
+    if (ctx->has_input("moving_mean", 0)) {
+      CHECK(ctx->has_input("moving_variance", 0));
+      moving_mean = ctx->Tensor4ArgNameAndIndex("moving_mean", 0);
+      moving_variance = ctx->Tensor4ArgNameAndIndex("moving_variance", 0);
+    }
+    void* moving_mean_ptr = nullptr;
+    void* moving_variance_ptr = nullptr;
+    if (moving_mean != nullptr && moving_variance != nullptr) {
+      moving_mean_ptr = (void*)moving_mean->mut_dptr();
+      moving_variance_ptr = (void*)moving_variance->mut_dptr();
+    }
+
+    int n = 0, c = 0, h = 0, w = 0;
+    infer_channel_sizes(x->shape_view(), &n, &c, &h, &w);
+
+    size_t tmp_in_size = x->shape_view().elem_cnt() * GetSizeOfDataType(x->data_type());
+    size_t tmp_out_size = y->shape_view().elem_cnt() * GetSizeOfDataType(y->data_type());
+    CnnlWorkspace tmp_in_workspace(ctx->stream()->As<ep::MluStream>(), tmp_in_size);
+    CnnlWorkspace tmp_out_workspace(ctx->stream()->As<ep::MluStream>(), tmp_out_size);
+    void* tmp_in_dptr = tmp_in_workspace.dptr();
+    void* tmp_out_dptr = tmp_out_workspace.dptr();
+
+    auto transpose = NewPermutePrimitive(ctx, x->shape_view().NumAxes());
+    CHECK(transpose);
+    int permute_nhwc[4] = {0, 2, 3, 1};
+    // transpose input NCHW -> NHWC
+    transpose->Launch(ctx->stream(), x->data_type(), x->shape_view().NumAxes(),
+                      x->shape_view().data(), x->dptr<T>(), permute_nhwc, tmp_in_dptr);
+
+    int64_t shape_nhwc[4] = {n, h, w, c};
+    CnnlTensorDescriptor input_desc, output_desc, weight_bias_mean_var_desc;
+    auto dtype = ConvertToCnnlDataType(x->data_type());
+    input_desc.set(4, shape_nhwc, dtype, CNNL_LAYOUT_NHWC);
+    output_desc.set(4, shape_nhwc, dtype, CNNL_LAYOUT_NHWC);
+    int dim[1] = {c};
+    weight_bias_mean_var_desc.set(1, dim, dtype, CNNL_LAYOUT_ARRAY);
+
+    // api reference:
+    // https://www.cambricon.com/docs/sdk_1.10.0/cambricon_cnnl_1.15.2/developer_guide/cnnl_api/api/batchnorm.html#cnnlbatchnormforwardtraining
+    // training
+    OF_CNNL_CHECK(cnnlBatchNormForwardTraining(
+        ctx->stream()->As<ep::MluStream>()->cnnl_handle(), nullptr, nullptr, input_desc.desc(),
+        tmp_in_dptr, weight_bias_mean_var_desc.desc(), gamma->dptr(), beta->dptr(), moving_mean_ptr,
+        moving_variance_ptr, epsilon, momentum, output_desc.desc(), tmp_out_dptr, mean->mut_dptr(),
+        inv_variance->mut_dptr()));
+
+    // transpose output NHWC -> NCHW
+    int permute_nchw[4] = {0, 3, 1, 2};
+    transpose->Launch(ctx->stream(), y->data_type(), y->shape_view().NumAxes(), shape_nhwc,
+                      tmp_out_dptr, permute_nchw, y->mut_dptr());
+  }
+
+  bool AlwaysComputeWhenAllOutputsEmpty() const override { return false; }
+};
+
+#define REGISTER_BN_TRAINING_MLU_KERNEL(dtype)                                        \
+  REGISTER_USER_KERNEL("normalization")                                               \
+      .SetCreateFn<MluNormalizationTrainingKernel<dtype>>()                           \
+      .SetIsMatchedHob((user_op::HobDeviceType() == DeviceType::kMLU)                 \
+                       && (user_op::HobDataType("x", 0) == GetDataType<dtype>::value) \
+                       && (user_op::HobAttr<bool>("training") == true));
+
+REGISTER_BN_TRAINING_MLU_KERNEL(float)
+REGISTER_BN_TRAINING_MLU_KERNEL(float16)
+
+#undef REGISTER_BN_TRAINING_MLU_KERNEL
 
 }  // namespace oneflow
--- python/oneflow/test/cambricon/test_batchnorm.py
+++ python/oneflow/test/cambricon/test_batchnorm.py
@@ -24,15 +24,16 @@ import oneflow as flow
 import oneflow.unittest
 
 
-def _test_batchnorm2d_forward(test_case, shape, affine, device, dtype):
+def _test_batchnorm2d_infer(
+    test_case, shape, track_running_stats, affine, device, dtype
+):
     arr = np.random.randn(*shape) * 3
-    # NOTE: mlu batchnorm only support NHWC format tensor as input, so need permutation.
     x1 = flow.tensor(arr, device=flow.device(device), dtype=dtype)
     x2 = flow.tensor(arr, device="cpu", dtype=dtype)
     m1 = (
         flow.nn.BatchNorm2d(
             num_features=int(x1.shape[1]),
-            track_running_stats=True,
+            track_running_stats=track_running_stats,
             affine=affine,
             eps=1e-05,
             momentum=0.1,
@@ -44,7 +45,7 @@ def _test_batchnorm2d_forward(test_case, shape, affine, device, dtype):
     m2 = (
         flow.nn.BatchNorm2d(
             num_features=int(x2.shape[1]),
-            track_running_stats=True,
+            track_running_stats=track_running_stats,
             affine=affine,
             eps=1e-05,
             momentum=0.1,
@@ -59,15 +60,49 @@ def _test_batchnorm2d_forward(test_case, shape, affine, device, dtype):
     test_case.assertTrue(np.allclose(mlu_out.numpy(), cpu_out.numpy(), 0.001, 0.001))
 
 
+def _test_batchnorm2d_train(
+    test_case, shape, track_running_stats, affine, device, dtype
+):
+    arr = np.random.randn(*shape)
+    x1 = flow.tensor(arr, device=flow.device(device), dtype=dtype, requires_grad=True)
+    x2 = flow.tensor(arr, device="cpu", dtype=dtype, requires_grad=True)
+    m1 = (
+        flow.nn.BatchNorm2d(
+            num_features=int(x1.shape[1]),
+            track_running_stats=track_running_stats,
+            affine=affine,
+        )
+        .train()
+        .to(flow.device(device))
+    )
+
+    m2 = (
+        flow.nn.BatchNorm2d(
+            num_features=int(x2.shape[1]),
+            track_running_stats=track_running_stats,
+            affine=affine,
+        )
+        .train()
+        .to("cpu")
+    )
+
+    mlu_out = m1(x1)
+    cpu_out = m2(x2)
+    # forward
+    test_case.assertTrue(np.allclose(mlu_out.numpy(), cpu_out.numpy(), 0.001, 0.001))
+
+
 @flow.unittest.skip_unless_1n1d()
 class TestBatchNormCambriconModule(flow.unittest.TestCase):
     def test_batchnorm2d(test_case):
         arg_dict = OrderedDict()
         arg_dict["test_fun"] = [
-            _test_batchnorm2d_forward,
+            _test_batchnorm2d_infer,
+            _test_batchnorm2d_train,
         ]
         arg_dict["shape"] = [(2, 3, 4, 5), (1, 2, 3, 4), (5, 6, 7, 8)]
-        arg_dict["affine"] = [True]
+        arg_dict["track_running_stats"] = [True, False]
+        arg_dict["affine"] = [True, False]
         arg_dict["device"] = ["mlu"]
         arg_dict["dtype"] = [flow.float32]
         for arg in GenArgList(arg_dict):
