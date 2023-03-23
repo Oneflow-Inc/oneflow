ac3c660811a6e8cabe7e4c929f1ccf4905559fff
lunzhongwang <1787648106@qq.com>
2023-03-23 02:10:57 +0000

    reformat_extra

--- python/oneflow/nn/parameter.py
+++ python/oneflow/nn/parameter.py
@@ -16,12 +16,3 @@ limitations under the License.
 import oneflow as flow
 
 Parameter = flow._oneflow_internal.nn.Parameter
-
-
-class UninitializedParameter(Parameter):
-    cls_to_become = Parameter
-
-    def __new__(cls, required_grad=True, device=None, dtype=None) -> None:
-        factory_kwargs = {"device": device, "dtype": dtype}
-        data = flow.empty(0, **factory_kwargs)
-        return flow.Tensor._make_subclass(cls, data, required_grad)
