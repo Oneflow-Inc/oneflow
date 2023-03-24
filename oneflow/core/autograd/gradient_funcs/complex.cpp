af420cd6441011d2a18a9e79920cddcf51e25978
levi131 <limaolin@oneflow.org>
2023-03-24 10:12:02 +0000

    support autograd

---
+++ oneflow/core/autograd/gradient_funcs/complex.cpp
@@ -0,0 +1,89 @@
+#include "oneflow/core/framework/op_expr_grad_function.h"
+#include "oneflow/core/functional/functional.h"
+
+namespace oneflow {
+namespace one {
+
+struct BaseComplexCaptureState : public AutoGradCaptureState {
+  bool requires_grad;
+};
+
+// TODO(lml): redesign these Apply method to support high order autograd.
+class RealGrad : public OpExprGradFunction<BaseComplexCaptureState> {
+ public:
+  Maybe<void> Init(const OpExpr& op) override { return Maybe<void>::Ok(); }
+
+  Maybe<void> Capture(BaseComplexCaptureState* ctx, const TensorTuple& inputs, const TensorTuple& outputs,
+                      const AttrMap& attrs) const override {
+    CHECK_EQ_OR_RETURN(inputs.size(), 1);
+    CHECK_EQ_OR_RETURN(outputs.size(), 1);
+    ctx->requires_grad = inputs.at(0)->requires_grad();
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> Apply(const BaseComplexCaptureState* ctx, const TensorTuple& out_grads,
+                    TensorTuple* in_grads) const override {
+    CHECK_EQ_OR_RETURN(out_grads.size(), 1);
+    in_grads->resize(1);
+    if (ctx->requires_grad) {
+      const auto& results = JUST(functional::RealGrad(out_grads.at(0)));
+      in_grads->at(0) = results;
+    }
+    return Maybe<void>::Ok();
+  }
+};
+
+class ImagGrad : public OpExprGradFunction<BaseComplexCaptureState> {
+ public:
+  Maybe<void> Init(const OpExpr& op) override { return Maybe<void>::Ok(); }
+
+  Maybe<void> Capture(BaseComplexCaptureState* ctx, const TensorTuple& inputs, const TensorTuple& outputs,
+                      const AttrMap& attrs) const override {
+    CHECK_EQ_OR_RETURN(inputs.size(), 1);
+    CHECK_EQ_OR_RETURN(outputs.size(), 1);
+    ctx->requires_grad = inputs.at(0)->requires_grad();
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> Apply(const BaseComplexCaptureState* ctx, const TensorTuple& out_grads,
+                    TensorTuple* in_grads) const override {
+    CHECK_EQ_OR_RETURN(out_grads.size(), 1);
+    in_grads->resize(1);
+    if (ctx->requires_grad) {
+      const auto& results = JUST(functional::ImagGrad(out_grads.at(0)));
+      in_grads->at(0) = results;
+    }
+    return Maybe<void>::Ok();
+  }
+};
+
+class ConjPhysicalGrad : public OpExprGradFunction<BaseComplexCaptureState> {
+ public:
+  Maybe<void> Init(const OpExpr& op) override { return Maybe<void>::Ok(); }
+
+  Maybe<void> Capture(BaseComplexCaptureState* ctx, const TensorTuple& inputs, const TensorTuple& outputs,
+                      const AttrMap& attrs) const override {
+    CHECK_EQ_OR_RETURN(inputs.size(), 1);
+    CHECK_EQ_OR_RETURN(outputs.size(), 1);
+    ctx->requires_grad = inputs.at(0)->requires_grad();
+    return Maybe<void>::Ok();
+  }
+
+  Maybe<void> Apply(const BaseComplexCaptureState* ctx, const TensorTuple& out_grads,
+                    TensorTuple* in_grads) const override {
+    CHECK_EQ_OR_RETURN(out_grads.size(), 1);
+    in_grads->resize(1);
+    if (ctx->requires_grad) {
+      const auto& results = JUST(functional::ConjPhysicalGrad(out_grads.at(0)));
+      in_grads->at(0) = results;
+    }
+    return Maybe<void>::Ok();
+  }
+};
+
+REGISTER_OP_EXPR_GRAD_FUNCTION("real", RealGrad);
+REGISTER_OP_EXPR_GRAD_FUNCTION("imag", ImagGrad);
+REGISTER_OP_EXPR_GRAD_FUNCTION("conj_physical", ConjPhysicalGrad);
+
+}  // namespace one
+}  // namespace oneflow
