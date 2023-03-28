17af82901e1ed72fe0bc9331bfe0aac31cff4ce9
nono-Sang <1908865287@qq.com>
2023-03-28 07:04:42 +0000

    Add skip init test

--- python/oneflow/test/tensor/test_meta_tensor.py
+++ python/oneflow/test/tensor/test_meta_tensor.py
@@ -18,6 +18,26 @@ import os
 import unittest
 import oneflow as flow
 import oneflow.unittest
+from oneflow import nn
+
+class CustomModule(nn.Module):
+  def __init__(self, foo, bar, device=None):
+    super().__init__()
+    # ==== Case 1: Module creates parameters directly. ====
+    self.param1 = nn.Parameter(flow.empty((foo, bar), device=device))
+    self.register_parameter('param2', nn.Parameter(flow.empty(bar, device=device)))
+    with flow.no_grad():
+      nn.init.kaiming_uniform_(self.param1)
+      nn.init.uniform_(self.param2)
+    # ==== Case 2: Module creates submodules. ====
+    self.fc = nn.Linear(bar, 5, device=device)
+    self.linears = nn.Sequential(
+      nn.Linear(5, 5, device=device),
+      nn.Linear(5, 1, device=device)
+    )
+    # ==== Case 3: Module creates buffers. ====
+    self.register_buffer('some_buffer', flow.ones(7, device=device))
+
 
 @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
 class TestMetaTensor(flow.unittest.TestCase):
@@ -69,6 +89,32 @@ class TestMetaTensor(flow.unittest.TestCase):
     test_case.assertEqual(x.weight.shape, y.weight.shape)
     test_case.assertEqual(x.weight.requires_grad, y.weight.requires_grad)
     test_case.assertEqual(x.weight.device, flow.device("meta"))
+  
+  @flow.unittest.skip_unless_1n1d()
+  def test_skip_init_function(test_case):
+    x = flow.nn.utils.skip_init(flow.nn.Linear, 4, 3)
+    y = flow.nn.Linear(4, 3, device="cpu")
+    test_case.assertEqual(x.weight.dtype, y.weight.dtype)
+    test_case.assertEqual(x.weight.shape, y.weight.shape)
+    test_case.assertEqual(x.weight.requires_grad, y.weight.requires_grad)
+    test_case.assertEqual(x.weight.device, flow.device("cpu"))
+  
+  @flow.unittest.skip_unless_1n1d()
+  def test_skip_init_function_custom_module(test_case):
+    x = flow.nn.utils.skip_init(CustomModule, 4, 3)
+    y = CustomModule(4, 3, device="cpu")
+    test_case.assertEqual(x.param1.dtype, y.param1.dtype)
+    test_case.assertEqual(x.param1.shape, y.param1.shape)
+    test_case.assertEqual(x.param1.requires_grad, y.param1.requires_grad)
+    test_case.assertEqual(x.param1.device, flow.device("cpu"))
+    test_case.assertEqual(x.param2.dtype, y.param2.dtype)
+    test_case.assertEqual(x.param2.shape, y.param2.shape)
+    test_case.assertEqual(x.param2.requires_grad, y.param2.requires_grad)
+    test_case.assertEqual(x.param2.device, flow.device("cpu"))
+    test_case.assertEqual(x.fc.weight.dtype, y.fc.weight.dtype)
+    test_case.assertEqual(x.fc.weight.shape, y.fc.weight.shape)
+    test_case.assertEqual(x.fc.weight.requires_grad, y.fc.weight.requires_grad)
+    test_case.assertEqual(x.fc.weight.device, flow.device("cpu"))
 
 
 if __name__ == "__main__":
