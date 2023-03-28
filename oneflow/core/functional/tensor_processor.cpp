b5d0b63fa75964f6f94676e3636a7ce7c8552152
brandonliu2 <brandonliulc@gmail.com>
2023-03-28 13:51:30 +0000

    add non blocking api in yaml

--- oneflow/core/functional/tensor_processor.cpp
+++ oneflow/core/functional/tensor_processor.cpp
@@ -220,7 +220,7 @@ Maybe<void> TensorAutoCastProcessor::Apply() {
     for (int i = 0; i < inputs_.size(); ++i) {
       if (args_eligible[i] && JUST(IsDeviceType(inputs_[i], autocast_device_type))
           && inputs_[i]->dtype()->is_floating_point() && inputs_[i]->dtype() != autocast_dtype) {
-        autocast_inputs_[i] = JUST(functional::To(inputs_[i], autocast_dtype, /*copy*/ false));
+        autocast_inputs_[i] = JUST(functional::To(inputs_[i], autocast_dtype, false, false));
       } else {
         autocast_inputs_[i] = inputs_[i];
       }
@@ -233,7 +233,7 @@ Maybe<void> TensorAutoCastProcessor::Apply() {
     for (int i = 0; i < inputs_.size(); ++i) {
       if (JUST(IsDeviceType(inputs_[i], autocast_device_type))
           && inputs_[i]->dtype()->is_floating_point() && inputs_[i]->dtype() != promote_dtype) {
-        autocast_inputs_[i] = JUST(functional::To(inputs_[i], promote_dtype, /*copy*/ false));
+        autocast_inputs_[i] = JUST(functional::To(inputs_[i], promote_dtype, false));
       } else {
         autocast_inputs_[i] = inputs_[i];
       }
