2b7c4f81f07ec85f758162c7adfd999832dec695
levi131 <limaolin@oneflow.org>
2023-03-28 06:58:59 +0000

    support add for complex on cpu

--- test_complex.py
+++ test_complex.py
@@ -5,18 +5,22 @@ a = flow.tensor([1.0 + 2j, 2.0 - 3j, 1j], dtype=flow.cfloat)
 a.requires_grad = True
 print("a: ", a)
 
-b = flow.real(a)
+b = flow.conj(a)
 
 print("b: ", b)
 
-c = flow.imag(a)
+c = a + b
 
 print("c: ", c)
 
-d = flow.conj(a)
+d = flow.real(c)
 
 print("d: ", d)
 
-loss = flow.sum(b+c)
+e = flow.imag(c)
+
+print("e: ", e)
+
+loss = flow.sum(d+e)
 
 loss.backward()
