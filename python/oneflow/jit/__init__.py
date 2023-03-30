4e891f6541ab1a544514ac2d6cce047e78efbc8c
shiyao <1143409869@qq.com>
2023-03-30 01:28:19 +0000

    sync

--- python/oneflow/jit/__init__.py
+++ python/oneflow/jit/__init__.py
@@ -15,11 +15,8 @@ limitations under the License.
 """
 import warnings
 from typing import Any, Dict, List, Set, Tuple, Union, Callable
-<<<<<<< HEAD
-import oneflow.nn
-=======
 import oneflow
->>>>>>> 2de0f9472880fdeb009c8b30a600d118dc2d3495
+
 
 def script(
     obj,
@@ -76,24 +73,6 @@ def is_scripting():
 def is_tracing():
     return False
 
-<<<<<<< HEAD
-# def ScriptModule(
-#     obj,
-#     optimize=None,
-#     _frames_up=0,
-#     _rcb=None,
-#     example_inputs: Union[List[Tuple], Dict[Callable, List[Tuple]], None] = None,
-# ):
-#     warnings.warn(
-#         "The oneflow.jit.ScriptModule interface is just to align the torch.jit.script interface and has no practical significance."
-#     )
-#     return obj
-
 class ScriptModule(oneflow.nn.Module):
     def __init__(self, arg=None):
         super().__init__()
-=======
-class ScriptModule(oneflow.nn.Module):
-    def __init__(self, arg=None):
-        super().__init__()
->>>>>>> 2de0f9472880fdeb009c8b30a600d118dc2d3495
