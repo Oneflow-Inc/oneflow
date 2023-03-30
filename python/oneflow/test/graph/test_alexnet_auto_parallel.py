a4428695c9b0b8ff33498696fdb098fd16f61239
Xiaoyu Xu <xiaoyulink@gmail.com>
2023-03-30 09:26:35 +0800

    Update test_alexnet_auto_parallel.py

--- python/oneflow/test/graph/test_alexnet_auto_parallel.py
+++ python/oneflow/test/graph/test_alexnet_auto_parallel.py
@@ -179,7 +179,7 @@ def _test_alexnet_graph(test_case, args, placement, sbp):
                             epoch, i, len(train_iter), l, end_t - start_t
                         )
                     )
-                # Stop after 10 iters to save time
+                # Stop after 20 iters to save time
                 break
         if flow.env.get_rank() == 0:
             print("epoch %d train done, start validation" % epoch)
