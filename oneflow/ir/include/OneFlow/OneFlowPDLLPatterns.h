d1d5972edfe375bdaf59848acfc315b1b7dde12d
jackalcooper <jackalcooper@gmail.com>
2023-03-31 15:38:20 +0800

    remove legacy code

--- oneflow/ir/include/OneFlow/OneFlowPDLLPatterns.h
+++ oneflow/ir/include/OneFlow/OneFlowPDLLPatterns.h
@@ -23,7 +23,6 @@ namespace oneflow {
 
 void populateForwardOpPatterns(RewritePatternSet& patterns);
 void populateNormalizationOpPatterns(RewritePatternSet& patterns);
-void populateElementwiseFusionPatterns(RewritePatternSet& patterns);
 void populateFuseConv2DBatchNormPattern(RewritePatternSet& patterns);
 void populateFuseOpsWithBackwardImplPattern(RewritePatternSet& patterns);
 
