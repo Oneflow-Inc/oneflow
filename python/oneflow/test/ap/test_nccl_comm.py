fe9ec03e7fd12878479661925358b782a894ab4e
strint <xiaoyulink@gmail.com>
2023-04-03 05:07:00 +0000

    s2b

--- python/oneflow/test/ap/test_nccl_comm.py
+++ python/oneflow/test/ap/test_nccl_comm.py
@@ -25,6 +25,11 @@ from oneflow.test_utils.test_util import GenArgList
 import time
 import os
 
+def _get_specific_global_tensor_mem(placement, sbp, tensor):
+    size_tensor = tensor.clone().detach().to_local()
+    cnt_size = size_tensor.element_size() * flow.numel(size_tensor)
+    return cnt_size / 1024 / 1024
+
 def _test_nccl_comm_1d(test_case, src_nd_sbp, dst_nd_sbp):
     # can not process p in dst
     if flow.sbp.partial_sum() in dst_nd_sbp:
@@ -36,12 +41,9 @@ def _test_nccl_comm_1d(test_case, src_nd_sbp, dst_nd_sbp):
 
     # input
     placement = flow.placement("cuda", ranks=[0, 1])
-    local_np = np.arange(2 * 2 * 2).reshape(2, 2, 2)
+    local_np = np.arange(1024 * 1024 * 20).reshape(1024, 1024, 20)
     x = flow.tensor(local_np, sbp=src_nd_sbp, placement=placement)
-
-    # check eager boxing
-    eager_out = x.to_global(sbp=dst_nd_sbp, placement=placement)
-    test_case.assertTrue(np.array_equal(eager_out.numpy(), x.numpy()))
+    print("input tensor size ", _get_specific_global_tensor_mem(placement, src_nd_sbp, x), "MB")
 
     # check graph boxing
     flow.boxing.nccl.enable_use_compute_stream(True)
@@ -51,32 +53,34 @@ def _test_nccl_comm_1d(test_case, src_nd_sbp, dst_nd_sbp):
             super().__init__()
 
         def build(self, x):
+            x = x + 2
             y = x.to_global(sbp=dst_nd_sbp, placement=placement)
+            y = y - 2
             return y
 
     graph = TestNcclLogicalComm1DGraph()
     # graph.debug(0)
-    y = graph(x)
+    for i in range(10):
+        y = graph(x)
     out_np = y.numpy()
     in_np = x.numpy()
-    # if flow.env.get_rank() == 0:
-    #    print("src sbp ", src_nd_sbp, ", dst sbp ", dst_nd_sbp)
-    #    print(graph)
-    #    equal = np.array_equal(out_np, in_np)
-    #    if not equal:
-    #        print("in ", in_np)
-    #        print("out ", out_np)
-    #    print("====================")
-    test_case.assertTrue(np.array_equal(out_np, in_np))
+    if flow.env.get_rank() == 0:
+       print("src sbp ", src_nd_sbp, ", dst sbp ", dst_nd_sbp)
+       print(graph)
+       equal = np.array_equal(out_np, in_np)
+       if not equal:
+           print("in ", in_np)
+           print("out ", out_np)
+       print("====================")
 
 
-def gen_1d_sbp():
+def gen_src_1d_sbp():
     sbp_list = [
-        flow.sbp.partial_sum(),
-        flow.sbp.broadcast(),
+        #flow.sbp.partial_sum(),
+        #flow.sbp.broadcast(),
         flow.sbp.split(0),
-        flow.sbp.split(1),
-        flow.sbp.split(2),
+        #flow.sbp.split(1),
+        #flow.sbp.split(2),
     ]
     nd_sbp_list = []
     for sbp0 in sbp_list:
@@ -85,6 +89,20 @@ def gen_1d_sbp():
         )
     return nd_sbp_list
 
+def gen_dst_1d_sbp():
+    sbp_list = [
+        #flow.sbp.partial_sum(),
+        flow.sbp.broadcast(),
+        #flow.sbp.split(0),
+        #flow.sbp.split(1),
+        #flow.sbp.split(2),
+    ]
+    nd_sbp_list = []
+    for sbp0 in sbp_list:
+        nd_sbp_list.append(
+            [sbp0,]
+        )
+    return nd_sbp_list
 
 @flow.unittest.skip_unless_1n2d()
 @unittest.skipIf(os.getenv("ONEFLOW_TEST_CPU_ONLY"), "only test cpu cases")
@@ -92,9 +110,11 @@ class TestNcclComm1D(flow.unittest.TestCase):
     def test_nccl_comm_1d(test_case):
         os.environ["ONEFLOW_BOXING_DISABLE_MIDDLE_NODE_AND_CHECK"] = "1"
         arg_dict = OrderedDict()
-        arg_dict["src_nd_sbp"] = gen_1d_sbp()
-        arg_dict["dst_nd_sbp"] = gen_1d_sbp()
+        arg_dict["src_nd_sbp"] = gen_src_1d_sbp()
+        arg_dict["dst_nd_sbp"] = gen_dst_1d_sbp()
+        print("=====")
         for arg in GenArgList(arg_dict):
+            print("------", arg)
             _test_nccl_comm_1d(test_case, *arg)
 
 
