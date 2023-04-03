b7f3cb592fba78e6b137029aaf40919ef0240fb1
lixiang007666 <88304454@qq.com>
2023-04-03 07:39:56 +0000

    deal with bincount

--- python/oneflow/test/modules/test_global_bincount.py
+++ python/oneflow/test/modules/test_global_bincount.py
@@ -19,7 +19,7 @@ import oneflow.unittest
 from oneflow.test_utils.automated_test_util import *
 
 
-@autotest(n=1, check_graph=False, auto_backward=False)
+@autotest(n=1, check_graph="ValidatedFalse", auto_backward=False)
 def _test_bincount(test_case, placement, sbp):
     x = random_tensor(1, 64, low=0, dtype=int).to_global(placement=placement, sbp=sbp)
     weight = random_tensor(1, 64).to_global(placement=placement, sbp=sbp)
