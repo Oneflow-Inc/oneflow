7a21b6b92484297eb0aad1e7a4be8321b41bc5cc
luyang <flowingsun007@163.com>
2023-04-06 04:24:21 +0000

    refine

---
+++ python/oneflow/test/modules/test_auto_to_global.py
@@ -0,0 +1,62 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import unittest
+from collections import OrderedDict
+
+import numpy as np
+import oneflow as flow
+import os
+
+import oneflow.unittest
+from oneflow.test_utils.test_util import GenArgList
+
+os.environ["ONEFLOW_ENABLE_PIPELINE_PARALLELISM_AUTO_TO_GLOBAL"] = "true"
+
+def _test_auto_to_global(
+    test_case, device
+):
+    sbp=[flow.sbp.broadcast, flow.sbp.broadcast], 
+    x = flow.ones(
+        (2,2), 
+        sbp=sbp, 
+        placement=flow.placement(device, ranks=[[0], [1]])
+    )
+    y = flow.zeros(
+            (2,2), 
+            sbp=sbp, 
+            placement=flow.placement(device, ranks=[[2], [3]])
+        )
+    z = x + y
+    test_case.assertTrue(
+        np.array_equal(
+            x.to_local().numpy(),
+            z.to_local().numpy()
+        )
+    )
+
+@flow.unittest.skip_unless_1n4d()
+class TestAutoToGlobal(flow.unittest.TestCase):
+    def test_auto_to_global(test_case):
+        arg_dict = OrderedDict()
+        arg_dict["device"] = ["cpu", "cuda"]
+        for arg in GenArgList(arg_dict):
+            _test_auto_to_global(test_case, *arg)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
