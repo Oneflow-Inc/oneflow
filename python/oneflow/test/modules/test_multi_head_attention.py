97d0273b97d36541f17612e66d3d6377aa295ffb
zhao di <54237695+Alokia@users.noreply.github.com>
2023-04-11 15:11:26 +0800

    rewrite test case

--- python/oneflow/test/modules/test_multi_head_attention.py
+++ python/oneflow/test/modules/test_multi_head_attention.py
@@ -24,30 +24,28 @@ import oneflow as flow
 import oneflow.unittest
 from oneflow.test_utils.automated_test_util import *
 import torch
+from itertools import product
 
 
-def _multi_head_attention_torch(test_case, device, query):
-    device = torch.device(device)
-    embed_dim = 16
-    num_heads = 4
-    query = torch.from_numpy(query).to(device).to(torch.float32)
+def _multi_head_attention(m, query, device, add_zero_attn, embed_dim, num_heads):
+    device = m.device(device)
+    query = m.from_numpy(query).to(device).to(m.float32)
     key = query
     value = query
 
-    in_proj_weight = torch.nn.Parameter(torch.ones((3 * embed_dim, embed_dim))).to(
+    in_proj_weight = m.nn.Parameter(m.ones((3 * embed_dim, embed_dim))).to(
         device
     )
-    in_proj_bias = torch.nn.Parameter(torch.ones(3 * embed_dim)).to(device)
-    bias_k = torch.nn.Parameter(torch.ones((1, 1, embed_dim))).to(device)
-    bias_v = torch.nn.Parameter(torch.ones((1, 1, embed_dim))).to(device)
-    add_zero_attn = True#random().to(bool)
-    dropout_p = 0
-    out_proj_weight = torch.nn.Parameter(torch.ones((embed_dim, embed_dim))).to(
+    in_proj_bias = m.nn.Parameter(m.ones(3 * embed_dim)).to(device)
+    bias_k = m.nn.Parameter(m.ones((1, 1, embed_dim))).to(device)
+    bias_v = m.nn.Parameter(m.ones((1, 1, embed_dim))).to(device)
+    dropout_p = 0 # must be 0
+    out_proj_weight = m.nn.Parameter(m.ones((embed_dim, embed_dim))).to(
         device
     )
-    out_proj_bias = torch.nn.Parameter(torch.ones((embed_dim))).to(device)
+    out_proj_bias = m.nn.Parameter(m.ones((embed_dim))).to(device)
 
-    y, _ = torch.nn.functional.multi_head_attention_forward(
+    y1, y2 = m.nn.functional.multi_head_attention_forward(
         query,
         key,
         value,
@@ -62,46 +60,7 @@ def _multi_head_attention_torch(test_case, device, query):
         out_proj_weight,
         out_proj_bias,
     )
-    return y
-
-
-def _multi_head_attention_flow(test_case, device, query):
-    device = flow.device(device)
-    embed_dim = 16
-    num_heads = 4
-    query = flow.from_numpy(query).to(device).to(flow.float32)
-    key = query
-    value = query
-
-    in_proj_weight = flow.nn.Parameter(flow.ones((3 * embed_dim, embed_dim))).to(
-        device
-    )
-    in_proj_bias = flow.nn.Parameter(flow.ones(3 * embed_dim)).to(device)
-    bias_k = flow.nn.Parameter(flow.ones((1, 1, embed_dim))).to(device)
-    bias_v = flow.nn.Parameter(flow.ones((embed_dim))).to(device)
-    add_zero_attn = True#random().to(bool)
-    dropout_p = 0
-    out_proj_weight = flow.nn.Parameter(flow.ones((embed_dim, embed_dim))).to(
-        device
-    )
-    out_proj_bias = flow.nn.Parameter(flow.ones((embed_dim))).to(device)
-
-    y, _ = flow.nn.functional.multi_head_attention_forward(
-        query,
-        key,
-        value,
-        embed_dim,
-        num_heads,
-        in_proj_weight,
-        in_proj_bias,
-        bias_k,
-        bias_v,
-        add_zero_attn,
-        dropout_p,
-        out_proj_weight,
-        out_proj_bias,
-    )
-    return y
+    return y1, y2
 
 
 @flow.unittest.skip_unless_1n1d()
@@ -109,15 +68,32 @@ class TestModule(flow.unittest.TestCase):
     @flow.unittest.skip_unless_1n1d()
     def test_multi_head_attention_with_torch(test_case):
         device_list = ["cpu", "cuda"]
-        for device in device_list:
-            query = np.random.rand(4, 8, 16)
-            y_torch = _multi_head_attention_torch(test_case, device, query.copy())
-            y_flow = _multi_head_attention_flow(test_case, device, query.copy())
+        add_zero_attn_list = [True, False]
+
+        for device, aza  in product(device_list, add_zero_attn_list):
+            param = {
+                "device": device,
+                "add_zero_attn": aza,
+                "embed_dim": 16,
+                "num_heads": 4
+            }
+            query = np.random.rand(4, 8, param["embed_dim"])
+            y1_torch, y2_torch = _multi_head_attention(torch, query.copy(), **param)
+            y1_flow, y2_flow = _multi_head_attention(flow, query.copy(), **param)
+
             test_case.assertTrue(
                 np.allclose(
-                    y_torch.detach().cpu().numpy(), y_flow.detach().cpu().numpy(), rtol=1e-4, atol=1e-3
+                    y1_torch.detach().cpu().numpy(), y1_flow.detach().cpu().numpy(), rtol=1e-4, atol=1e-3
                 )
             )
+            if y2_torch is None:
+                test_case.assertTrue(y2_flow is None)
+            else:
+                test_case.assertTrue(
+                    np.allclose(
+                        y2_torch.detach().cpu().numpy(), y2_flow.detach().cpu().numpy(), rtol=1e-4, atol=1e-3
+                    )
+                )
 
 
 if __name__ == "__main__":
