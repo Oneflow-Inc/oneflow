c023346bf1ccb6120fe8e331fa1c8fcca901a8da
zhao di <54237695+Alokia@users.noreply.github.com>
2023-04-11 14:24:06 +0800

    fix test

--- python/oneflow/test/modules/test_multi_head_attention.py
+++ python/oneflow/test/modules/test_multi_head_attention.py
@@ -23,49 +23,103 @@ from oneflow.test_utils.test_util import GenArgList
 import oneflow as flow
 import oneflow.unittest
 from oneflow.test_utils.automated_test_util import *
+import torch
+
+
+def _multi_head_attention_torch(test_case, device, query):
+    device = torch.device(device)
+    embed_dim = 16
+    num_heads = 4
+    query = torch.from_numpy(query).to(device).to(torch.float32)
+    key = query
+    value = query
+
+    in_proj_weight = torch.nn.Parameter(torch.ones((3 * embed_dim, embed_dim))).to(
+        device
+    )
+    in_proj_bias = torch.nn.Parameter(torch.ones(3 * embed_dim)).to(device)
+    bias_k = torch.nn.Parameter(torch.ones((1, 1, embed_dim))).to(device)
+    bias_v = torch.nn.Parameter(torch.ones((1, 1, embed_dim))).to(device)
+    add_zero_attn = True#random().to(bool)
+    dropout_p = 0
+    out_proj_weight = torch.nn.Parameter(torch.ones((embed_dim, embed_dim))).to(
+        device
+    )
+    out_proj_bias = torch.nn.Parameter(torch.ones((embed_dim))).to(device)
+
+    y, _ = torch.nn.functional.multi_head_attention_forward(
+        query,
+        key,
+        value,
+        embed_dim,
+        num_heads,
+        in_proj_weight,
+        in_proj_bias,
+        bias_k,
+        bias_v,
+        add_zero_attn,
+        dropout_p,
+        out_proj_weight,
+        out_proj_bias,
+    )
+    return y
+
+
+def _multi_head_attention_flow(test_case, device, query):
+    device = flow.device(device)
+    embed_dim = 16
+    num_heads = 4
+    query = flow.from_numpy(query).to(device).to(flow.float32)
+    key = query
+    value = query
+
+    in_proj_weight = flow.nn.Parameter(flow.ones((3 * embed_dim, embed_dim))).to(
+        device
+    )
+    in_proj_bias = flow.nn.Parameter(flow.ones(3 * embed_dim)).to(device)
+    bias_k = flow.nn.Parameter(flow.ones((1, 1, embed_dim))).to(device)
+    bias_v = flow.nn.Parameter(flow.ones((embed_dim))).to(device)
+    add_zero_attn = True#random().to(bool)
+    dropout_p = 0
+    out_proj_weight = flow.nn.Parameter(flow.ones((embed_dim, embed_dim))).to(
+        device
+    )
+    out_proj_bias = flow.nn.Parameter(flow.ones((embed_dim))).to(device)
+
+    y, _ = flow.nn.functional.multi_head_attention_forward(
+        query,
+        key,
+        value,
+        embed_dim,
+        num_heads,
+        in_proj_weight,
+        in_proj_bias,
+        bias_k,
+        bias_v,
+        add_zero_attn,
+        dropout_p,
+        out_proj_weight,
+        out_proj_bias,
+    )
+    return y
 
 
 @flow.unittest.skip_unless_1n1d()
 class TestModule(flow.unittest.TestCase):
-    @autotest(n=3, check_graph=False, rtol=1e-4, atol=1e-3)
+    @flow.unittest.skip_unless_1n1d()
     def test_multi_head_attention_with_torch(test_case):
-        device = random_device()
-        embed_dim = 16
-        num_heads = 4
-        query =  random_tensor(ndim=3, dim0=4, dim1=8, dim2=embed_dim).to(device)
-        key = query
-        value = query
-
-        in_proj_weight = torch.nn.Parameter(torch.ones((3 * embed_dim, embed_dim))).to(
-            device
-        )
-        in_proj_bias = torch.nn.Parameter(torch.ones(3 * embed_dim)).to(device)
-        bias_k = torch.nn.Parameter(torch.ones((1, 1, embed_dim))).to(device)
-        bias_v = torch.nn.Parameter(torch.ones((1, 1, embed_dim))).to(device)
-        add_zero_attn = True#random().to(bool)
-        dropout_p = 0.1
-        out_proj_weight = torch.nn.Parameter(torch.ones((embed_dim, embed_dim))).to(
-            device
-        )
-        out_proj_bias = torch.nn.Parameter(torch.ones((1, 1, embed_dim))).to(device)
-
-        y, _ = torch.nn.functional.multi_head_attention_forward(
-            query,
-            key,
-            value,
-            embed_dim,
-            num_heads,
-            in_proj_weight,
-            in_proj_bias,
-            bias_k,
-            bias_v,
-            add_zero_attn,
-            dropout_p,
-            out_proj_weight,
-            out_proj_bias,
-        )
-        return y
+        device_list = ["cpu", "cuda"]
+        for device in device_list:
+            query = np.random.rand(4, 8, 16)
+            y_torch = _multi_head_attention_torch(test_case, device, query.copy())
+            y_flow = _multi_head_attention_flow(test_case, device, query.copy())
+            test_case.assertTrue(
+                np.allclose(
+                    y_torch.detach().cpu().numpy(), y_flow.detach().cpu().numpy(), rtol=1e-4, atol=1e-3
+                )
+            )
 
 
 if __name__ == "__main__":
     unittest.main()
+    
