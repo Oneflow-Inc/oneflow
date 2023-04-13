42e42e2460908b83eeaba5c378409b4c4d060160
strint <xiaoyulink@gmail.com>
2023-04-13 08:32:26 +0000

    rm unrelated

--- oneflow/core/framework/instructions_builder.cpp
+++ oneflow/core/framework/instructions_builder.cpp
@@ -207,7 +207,7 @@ Maybe<void> InstructionsBuilder::SoftSyncNNGraphBuffers(
 namespace {
 
 int64_t NewSymbolId() {
-  static std::atomic<int64_t> cnt(1);
+  static std::atomic<int64_t> cnt(0);
   return cnt.fetch_add(1, std::memory_order_relaxed);
 }
 
@@ -223,6 +223,10 @@ Maybe<ParallelDesc> InstructionsBuilder::GetParallelDescSymbol(const ParallelCon
 
 Maybe<Scope> InstructionsBuilder::GetScopeSymbol(const ScopeProto& scope_proto) {
   return Singleton<symbol::Storage<Scope>>::Get()->FindOrCreate(scope_proto, &NewSymbolId);
+}
+
+Maybe<OperatorConfSymbol> InstructionsBuilder::GetOpConfSymbol(const OperatorConf& op_conf) {
+  return Singleton<symbol::Storage<OperatorConfSymbol>>::Get()->FindOrCreate(op_conf, &NewSymbolId);
 }
 
 Maybe<Scope> InstructionsBuilder::BuildInitialScope(
--- oneflow/core/graph/task_graph.cpp
+++ oneflow/core/graph/task_graph.cpp
@@ -14,11 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/graph/task_graph.h"
-#include <cstddef>
-#include "oneflow/core/common/just.h"
-#include "oneflow/core/common/maybe.h"
 #include "oneflow/core/common/util.h"
-#include "oneflow/core/common/decorator.h"
 #include "oneflow/core/common/container_util.h"
 #include "oneflow/core/common/env_var/debug_mode.h"
 #include "oneflow/core/graph/inplace_lbi_graph.h"
