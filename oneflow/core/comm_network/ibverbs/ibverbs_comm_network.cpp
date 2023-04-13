eb3df2502e17fed4833526a12e6ac9950f91b07e
Shiyuan Shangguan <shiyuan@oneflow.org>
2023-04-13 15:44:00 +0000

    return value

--- oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
+++ oneflow/core/comm_network/ibverbs/ibverbs_comm_network.cpp
@@ -213,7 +213,7 @@ void IBVerbsCommNet::PollCQ() {
           qp->RecvDone(wr_id);
           break;
         }
-        default: OF_UNIMPLEMENTED() << wc.opcode;
+        default: UNIMPLEMENTED() << wc.opcode;
       }
     }
   }
