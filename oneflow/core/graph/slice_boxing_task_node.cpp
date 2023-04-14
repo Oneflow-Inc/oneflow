e8b30531e709b97116b8217926594db64ac55588
strint <xiaoyulink@gmail.com>
2023-04-14 16:39:16 +0000

    rm useless

--- oneflow/core/graph/slice_boxing_task_node.cpp
+++ oneflow/core/graph/slice_boxing_task_node.cpp
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #include "oneflow/core/graph/slice_boxing_task_node.h"
+
 #include "oneflow/core/framework/to_string.h"
 #include "oneflow/core/graph/task_graph_rebuild_ctx.h"
-#include "oneflow/core/graph/boxing_task_graph.pb.h"
 
 namespace oneflow {
 
