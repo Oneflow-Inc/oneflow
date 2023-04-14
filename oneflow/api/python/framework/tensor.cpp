55e656cde016f2fafa68a9cacf5d04501c7b0923
daquexian <daquexian566@gmail.com>
2023-04-14 18:52:08 +0800

    support data_ptr() on global tensors
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- oneflow/api/python/framework/tensor.cpp
+++ oneflow/api/python/framework/tensor.cpp
@@ -390,8 +390,10 @@ static PyObject* PyTensorObject_check_meta_consistency(PyObject* self, PyObject*
 static PyObject* PyTensorObject_data_ptr(PyObject* self, PyObject* unused) {
   HANDLE_ERRORS
   const auto& t = PyTensor_Unpack(self);
+  const std::shared_ptr<LocalTensor> local_tensor =
+      t->is_local() ? ASSERT_PTR(t->AsLocalTensor()) : ASSERT_PTR(t->cur_rank_phy_tensor());
   return functional::CastToPyObject(
-      reinterpret_cast<int64_t>(ASSERT(GetTensorDataPtr(ASSERT_PTR(t->AsLocalTensor())))));
+      reinterpret_cast<int64_t>(ASSERT(GetTensorDataPtr(local_tensor))));
   END_HANDLE_ERRORS
 }
 
