3f239a0923aef8856a28a1faca7911e5e114eda5
Houjiang Chen <chenhoujiangcug@gmail.com>
2023-04-18 13:37:14 +0800

    fix where (#141)

--- oneflow/cambricon/ep/primitive/where.cpp
+++ oneflow/cambricon/ep/primitive/where.cpp
@@ -13,11 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 #include "oneflow/core/ep/include/primitive/where.h"
-#include <cstdint>
-#include <type_traits>
-#include "cnnl.h"
 #include "oneflow/cambricon/cnnl/cnnl_tensor_descriptor.h"
 #include "oneflow/cambricon/cnnl/cnnl_workspace.h"
 #include "oneflow/core/common/constant.h"
@@ -36,6 +32,15 @@ namespace primitive {
 
 namespace {
 
+void Expand(Stream* stream, cnnlDataType_t data_type, size_t src_ndim, const int64_t* src_dims,
+            const void* src, size_t dst_ndim, const int64_t* dst_dims, void* dst) {
+  CnnlTensorDescriptor src_desc, dst_desc;
+  src_desc.set(src_ndim, src_dims, data_type);
+  dst_desc.set(dst_ndim, dst_dims, data_type);
+  OF_CNNL_CHECK(cnnlExpand(stream->As<ep::MluStream>()->cnnl_handle(), src_desc.desc(), src,
+                           dst_desc.desc(), dst));
+}
+
 template<typename T, typename CondT>
 class WhereImpl : public Where {
  public:
@@ -46,36 +51,66 @@ class WhereImpl : public Where {
   void Launch(Stream* stream, size_t num_cond_dims, const int64_t* cond_dims, const void* cond,
               size_t num_x_dims, const int64_t* x_dims, const void* x, size_t num_y_dims,
               const int64_t* y_dims, const void* y, void* z) override {
+    DataType cond_data_type = GetDataType<CondT>::value;
+    CHECK_OR_THROW(cond_data_type == DataType::kBool || cond_data_type == DataType::kInt8)
+        << "condition data type should be oneflow.bool or oneflow.int8.";
     cnnlDataType_t cnnl_cond_data_type, cnnl_xy_data_type;
-    if (std::is_same_v<CondT, bool>) {
-      cnnl_cond_data_type = ConvertToCnnlDataType(kBool);
-      switch (sizeof(T)) {
-        case 1: cnnl_xy_data_type = ConvertToCnnlDataType(kInt8); break;
-        case 2: cnnl_xy_data_type = ConvertToCnnlDataType(kInt16); break;
-        case 4: cnnl_xy_data_type = ConvertToCnnlDataType(kInt32); break;
-        case 8: cnnl_xy_data_type = ConvertToCnnlDataType(kInt64); break;
-        default: UNIMPLEMENTED_THEN_THROW();
-      }
-    } else {
-      UNIMPLEMENTED_THEN_THROW();
+    cnnl_cond_data_type = ConvertToCnnlDataType(cond_data_type);
+    switch (sizeof(T)) {
+      case 1: cnnl_xy_data_type = ConvertToCnnlDataType(kInt8); break;
+      case 2: cnnl_xy_data_type = ConvertToCnnlDataType(kInt16); break;
+      case 4: cnnl_xy_data_type = ConvertToCnnlDataType(kInt32); break;
+      case 8: cnnl_xy_data_type = ConvertToCnnlDataType(kInt64); break;
+      default: UNIMPLEMENTED_THEN_THROW();
     }
-
-    CnnlTensorDescriptor condition_desc, x_desc, y_desc, z_desc;
-    condition_desc.set(num_cond_dims, cond_dims, cnnl_cond_data_type);
-    x_desc.set(num_x_dims, x_dims, cnnl_xy_data_type);
-    y_desc.set(num_y_dims, y_dims, cnnl_xy_data_type);
     // get z_desc
+    int64_t max_element_count = 1;
     int64_t z_dims[kMaxNumDims];
     size_t max_dims = std::max(num_x_dims, num_y_dims);
-    for (int i = max_dims - 1; i >= 0; i--) {
-      if (num_x_dims > i && num_y_dims > i) {
-        z_dims[i] = std::max(x_dims[i], y_dims[i]);
-      } else if (num_x_dims > i) {
-        z_dims[i] = x_dims[i];
-      } else {
-        z_dims[i] = y_dims[i];
-      }
+    max_dims = std::max(max_dims, num_cond_dims);
+    for (size_t i = 0; i < max_dims; ++i) {
+      size_t cond_lpad = max_dims - num_cond_dims;
+      size_t x_lpad = max_dims - num_x_dims;
+      size_t y_lpad = max_dims - num_y_dims;
+      int64_t cond_dim = (i < cond_lpad) ? 1 : cond_dims[i - cond_lpad];
+      int64_t x_dim = (i < x_lpad) ? 1 : x_dims[i - x_lpad];
+      int64_t y_dim = (i < y_lpad) ? 1 : y_dims[i - y_lpad];
+      int64_t max_dim = std::max(x_dim, y_dim);
+      max_dim = std::max(max_dim, cond_dim);
+      z_dims[i] = max_dim;
+      max_element_count *= max_dim;
     }
+
+    // expand inputs to same shape since cnnlSelectV2 broadcasting may lead to incorrect result when
+    // the calculation scale is large
+    Shape z_shape(DimVector(z_dims, z_dims + max_dims));
+    Shape cond_shape(DimVector(cond_dims, cond_dims + num_cond_dims));
+    Shape x_shape(DimVector(x_dims, x_dims + num_x_dims));
+    Shape y_shape(DimVector(y_dims, y_dims + num_y_dims));
+    auto* mlu_stream = stream->As<ep::MluStream>();
+    CnnlWorkspace workspace_cond(mlu_stream), workspace_x(mlu_stream), workspace_y(mlu_stream);
+    if (cond_shape != z_shape) {
+      workspace_cond.resize(max_element_count * GetSizeOfDataType(cond_data_type));
+      Expand(stream, cnnl_cond_data_type, num_cond_dims, cond_dims, cond, max_dims, z_dims,
+             workspace_cond.dptr());
+      cond = workspace_cond.dptr();
+    }
+    if (x_shape != z_shape) {
+      workspace_x.resize(max_element_count * sizeof(T));
+      Expand(stream, cnnl_xy_data_type, num_x_dims, x_dims, x, max_dims, z_dims,
+             workspace_x.dptr());
+      x = workspace_x.dptr();
+    }
+    if (y_shape != z_shape) {
+      workspace_y.resize(max_element_count * sizeof(T));
+      Expand(stream, cnnl_xy_data_type, num_y_dims, y_dims, y, max_dims, z_dims,
+             workspace_y.dptr());
+      y = workspace_y.dptr();
+    }
+    CnnlTensorDescriptor condition_desc, x_desc, y_desc, z_desc;
+    condition_desc.set(max_dims, z_dims, cnnl_cond_data_type);
+    x_desc.set(max_dims, z_dims, cnnl_xy_data_type);
+    y_desc.set(max_dims, z_dims, cnnl_xy_data_type);
     z_desc.set(max_dims, z_dims, cnnl_xy_data_type);
     size_t workspace_size = 0;
     OF_CNNL_CHECK(cnnlGetSelectV2WorkspaceSize(stream->As<ep::MluStream>()->cnnl_handle(),
