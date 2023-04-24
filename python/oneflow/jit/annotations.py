25cf4e51116f79c1445e265bb73978ea41c13e86
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2023-04-24 09:57:24 +0800

    add oneflow.utils.model_zoo api (#10183)
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- python/oneflow/jit/annotations.py
+++ python/oneflow/jit/annotations.py
@@ -13,9 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from oneflow.framework.config_util import api_load_library as load_library
-from oneflow.utils import tensor
-from oneflow.utils import global_view
-from oneflow.utils import model_zoo
-from . import checkpoint
-from . import hooks
+from typing import Tuple
+
+BroadcastingList2 = Tuple
