004ea1f9923ffdf0a18e3f7b69ae89c518db3b49
daquexian <daquexian566@gmail.com>
2023-04-25 20:08:30 +0800

    VLOG -> VLOG_REMAT
    Signed-off-by: daquexian <daquexian566@gmail.com>

--- oneflow/core/vm/remat/util.cpp
+++ oneflow/core/vm/remat/util.cpp
@@ -22,10 +22,12 @@ limitations under the License.
 #include "oneflow/core/common/env_var/remat.h"
 #include "oneflow/core/common/env_var/vm.h"
 #include "oneflow/core/eager/tensor_storage.h"
+#include "oneflow/core/framework/compute_complexity_fn_context.h"
 #include "oneflow/core/vm/op_call_instruction_policy.h"
 #include "oneflow/core/vm/remat/env.h"
 #include "oneflow/core/vm/remat/disjoint_set.h"
 #include "oneflow/user/kernels/stateful_opkernel.h"
+#include "oneflow/core/framework/user_op_registry_manager.h"
 
 namespace oneflow {
 
@@ -135,19 +137,111 @@ static Maybe<double> GetEstimatedComputeTime(const vm::OpCallInstructionPolicy& 
   const auto& outputs = operand.outputs();
   size_t estimated_compute_time = 0;
   for (const auto& input : inputs) {
-    estimated_compute_time += input->tensor_storage()->blob_bytes();
+    estimated_compute_time += input->shape().elem_cnt();
   }
   for (const auto& output : outputs) {
-    estimated_compute_time += output->tensor_storage()->blob_bytes();
+    estimated_compute_time += output->shape().elem_cnt();
   }
   return estimated_compute_time;
+}
+
+int32_t TryGetTensorTupleIndex(const std::unordered_map<std::string, std::vector<int32_t>>&
+                                   arg_name2bn_index2tensor_tuple_index,
+                               const std::string& arg_name, const int32_t arg_index) {
+  auto it = arg_name2bn_index2tensor_tuple_index.find(arg_name);
+  if (it != arg_name2bn_index2tensor_tuple_index.end()) { return it->second.at(arg_index); }
+  return -1;
+}
+
+class SingleDeviceOpComputeComplexityFnContext : public user_op::ComputeComplexityFnContext {
+ public:
+  using ArgVec = std::vector<std::pair<std::string, int32_t>>;
+
+  SingleDeviceOpComputeComplexityFnContext(const OperatorConf& op_conf,
+                                           const vm::EagerBlobObjectList& inputs,
+                                           const vm::EagerBlobObjectList& outputs,
+                                           const ArgTuple* input_arg_tuple,
+                                           const ArgTuple* output_arg_tuple)
+      : user_op::ComputeComplexityFnContext(user_op::UserOpConfWrapper(op_conf)),
+        input_tensors_(inputs),
+        output_tensors_(outputs),
+        input_arg_tuple_(input_arg_tuple),
+        output_arg_tuple_(output_arg_tuple) {}
+  ~SingleDeviceOpComputeComplexityFnContext() override = default;
+
+#define RETURN_IF_FOUND(inputs, outputs, post_action)                                             \
+  int32_t i = TryGetTensorTupleIndex(input_arg_tuple_->arg_name2bn_index2tensor_tuple_index(),    \
+                                     arg_name, index);                                            \
+  if (i >= 0) { return (inputs).at(i) post_action; }                                              \
+  i = TryGetTensorTupleIndex(output_arg_tuple_->arg_name2bn_index2tensor_tuple_index(), arg_name, \
+                             index);                                                              \
+  if (i >= 0) { return (outputs).at(i) post_action; }
+
+  const user_op::TensorDesc* TensorDesc4ArgNameAndIndex(const std::string& arg_name,
+                                                        int32_t index) override {
+    RETURN_IF_FOUND(input_tensors_, output_tensors_, ->mut_tensor_meta().get())
+    return nullptr;
+  }
+  const Shape& Shape4ArgNameAndIndex(const std::string& arg_name, int32_t index) const override {
+    RETURN_IF_FOUND(input_tensors_, output_tensors_, ->shape())
+    UNIMPLEMENTED_THEN_THROW();
+  }
+  DataType Dtype4ArgNameAndIndex(const std::string& arg_name, int32_t index) const override {
+    RETURN_IF_FOUND(input_tensors_, output_tensors_, ->data_type())
+    UNIMPLEMENTED_THEN_THROW();
+  }
+  bool IsDynamic4ArgNameAndIndex(const std::string& arg_name, int32_t index) const override {
+    return false;
+  }
+
+  const NdSbp NdSbp4ArgNameAndIndex(const std::string& arg_name, int32_t index) const override {
+    static NdSbp nd_sbp = []() {
+      NdSbp nd_sbp;
+      nd_sbp.add_sbp_parallel()->broadcast_parallel();
+      return nd_sbp;
+    }();
+    return nd_sbp;
+  }
+
+  const ArgVec& inputs() const override { return inputs_; }
+  const ArgVec& outputs() const override { return outputs_; }
+  const ParallelDesc& parallel_desc() const override {
+    auto shape = std::make_shared<Shape>();
+    shape->push_back(1);
+    static ParallelDesc parallel_desc = *CHECK_JUST(ParallelDesc::New("cpu", {"0:0-0"}, shape));
+    return parallel_desc;
+  }
+  const NdSbpSignature* GetNdSbpSignature() const override { UNIMPLEMENTED_THEN_THROW(); }
+
+ private:
+  ArgVec inputs_;
+  ArgVec outputs_;
+  const vm::EagerBlobObjectList& input_tensors_;
+  const vm::EagerBlobObjectList& output_tensors_;
+  const ArgTuple* input_arg_tuple_;
+  const ArgTuple* output_arg_tuple_;
+};
+
+Maybe<double> GetFLOPs(const vm::OpCallInstructionPolicy& operand) {
+  const auto& op_conf = operand.opkernel().op_conf();
+  auto registry =
+      user_op::UserOpRegistryMgr::Get().GetOpRegistryResult(op_conf.user_conf().op_type_name());
+
+  if (registry->compute_complexity_fn) {
+    SingleDeviceOpComputeComplexityFnContext ctx(op_conf, operand.inputs(), operand.outputs(),
+                                                 operand.opkernel().input_arg_tuple(),
+                                                 operand.opkernel().output_arg_tuple());
+    return registry->compute_complexity_fn(&ctx);
+  } else {
+    return GetEstimatedComputeTime(operand);
+  }
 }
 }  // namespace
 
 Maybe<double> GetComputeTime(const vm::OpCallInstructionPolicy& operand) {
   const static json time_dataset = LoadTimeDataset();
   if (!time_dataset.empty()) { return GetDatasetComputeTime(time_dataset, operand); }
-  return GetEstimatedComputeTime(operand);
+  return GetFLOPs(operand);
 }
 
 }  // namespace remat
@@ -178,7 +272,7 @@ RematHelper::RematHelper(const OpCallInstructionPolicy& op_call_instruction_poli
     }
     if (!inputs_rematable) {
       for (auto& storage : output_storages_) {
-        VLOG(1) << "set storage " << storage->id() << " unevictable" << std::endl;
+        VLOG_REMAT(1) << "set storage " << storage->id() << " unevictable" << std::endl;
         storage->set_eviction_disabled(true);
       }
     }
@@ -187,11 +281,11 @@ RematHelper::RematHelper(const OpCallInstructionPolicy& op_call_instruction_poli
 
 Maybe<void> RematHelper::_IncReferenceNumOfRecomputedTensor(
     int& pinned_num, std::set<const DtrOpCallInstructionPolicy*>& visited_ops) {
-  VLOG(1) << "op is " << op_call_instruction_policy_.opkernel().op_type_name();
+  VLOG_REMAT(1) << "op is " << op_call_instruction_policy_.opkernel().op_type_name();
   for (int i = 0; i < input_storages_.size(); i++) {
     auto& storage = input_storages_[i];
     storage->Pin();
-    VLOG(1) << "No." << i << " input is in memory? " << storage->is_in_memory();
+    VLOG_REMAT(1) << "No." << i << " input is in memory? " << storage->is_in_memory();
     if (!storage->is_in_memory()) {
       OpCallInstructionPolicy tmp_op = storage->compute_op();
       if (!storage->is_needed_by_backward()) {
@@ -207,7 +301,7 @@ Maybe<void> RematHelper::_IncReferenceNumOfRecomputedTensor(
       pinned_num++;
     }
   }
-  VLOG(1) << "op " << op_call_instruction_policy_.opkernel().op_type_name() << " end";
+  VLOG_REMAT(1) << "op " << op_call_instruction_policy_.opkernel().op_type_name() << " end";
   return Maybe<void>::Ok();
 }
 
@@ -224,16 +318,16 @@ Maybe<void> RematHelper::RematInputs(
   CHECK_OR_RETURN(!ThreadLocalEnvBool<ONEFLOW_VM_MULTI_THREAD>());
   Singleton<remat::Env>::Get()->current_op_type_name =
       op_call_instruction_policy_.opkernel().op_type_name();
-  VLOG(2) << "set current op type name to " << Singleton<remat::Env>::Get()->current_op_type_name
+  VLOG_REMAT(2) << "set current op type name to " << Singleton<remat::Env>::Get()->current_op_type_name
           << std::endl;
   if (first) { JUST(IncReferenceNumOfRecomputedTensor()); }
-  VLOG(1) << "compute " << op_call_instruction_policy_.opkernel().op_type_name() << std::endl;
-  VLOG(1) << "input num " << op_call_instruction_policy_.inputs().size() << std::endl;
+  VLOG_REMAT(1) << "compute " << op_call_instruction_policy_.opkernel().op_type_name() << std::endl;
+  VLOG_REMAT(1) << "input num " << op_call_instruction_policy_.inputs().size() << std::endl;
 
   for (int i = 0; i < input_storages_.size(); i++) {
     auto& storage = input_storages_[i];
     if (!storage->is_in_memory()) {
-      VLOG(1) << "recompute No." << i << " input by " << storage->compute_op_type_name()
+      VLOG_REMAT(1) << "recompute No." << i << " input by " << storage->compute_op_type_name()
               << ". Storage id: " << storage->id();
       OpCallInstructionPolicy tmp_op = storage->compute_op();
       JUST(compute_fn(&tmp_op, vm_stream));
@@ -254,7 +348,7 @@ Maybe<void> RematHelper::EagerlyEvictRemattedTensors(bool first) {
   if (first) {
     if (!need_eager_eviction_storages.empty()) {
       for (const auto& storage : need_eager_eviction_storages) {
-        VLOG(1) << "not empty, storage id: " << storage->id();
+        VLOG_REMAT(1) << "not empty, storage id: " << storage->id();
       }
     }
     CHECK_OR_RETURN(need_eager_eviction_storages.empty());
@@ -269,10 +363,10 @@ Maybe<void> RematHelper::UpdateRematInfo(bool first, bool recompute, bool includ
       auto compute_op = std::make_unique<OpCallInstructionPolicy>(op_call_instruction_policy_);
       for (int i = 0; i < output_storages_.size(); i++) {
         const auto& storage = output_storages_[i];
-        VLOG(1) << "output " << i << " storage id: " << storage->id();
+        VLOG_REMAT(1) << "output " << i << " storage id: " << storage->id();
         if (storage->is_eviction_disabled()) { continue; }
         if (storage_is_initialized_[i] && !recompute) {
-          VLOG(1) << "storage->is_initialized(), op is " << storage->compute_op_type_name()
+          VLOG_REMAT(1) << "storage->is_initialized(), op is " << storage->compute_op_type_name()
                   << std::endl;
           compute_op = std::make_unique<OpCallInstructionPolicy>(
               Singleton<remat::Env>::Get()->update_tensor_with_storage(
@@ -304,10 +398,12 @@ Maybe<void> RematHelper::UpdateRematInfo(bool first, bool recompute, bool includ
 
   if (recompute) { Singleton<remat::Env>::Get()->add_recomputation_num(); }
   Singleton<remat::Env>::Get()->add_time(JUST(remat::GetComputeTime(op_call_instruction_policy_)));
-  VLOG(1) << "end compute " << op_call_instruction_policy_.opkernel().op_type_name() << std::endl;
+  VLOG_REMAT(1) << "end compute " << op_call_instruction_policy_.opkernel().op_type_name() << std::endl;
   Singleton<remat::Env>::Get()->current_op_type_name = "None";
   return Maybe<void>::Ok();
 }
+
+namespace {}
 }  // namespace vm
 
 }  // namespace oneflow
