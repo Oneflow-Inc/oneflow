3a52d51c77d5bd6eba8e58d1c404ca2b89a17f0d
strint <xiaoyulink@gmail.com>
2023-04-26 23:51:55 +0800

    rm unused

--- python/oneflow/test/graph/test_graph_interplay.py
+++ python/oneflow/test/graph/test_graph_interplay.py
@@ -19,45 +19,6 @@ import numpy as np
 
 import oneflow.unittest
 
-
-def __test_linear(test_case, device):
-    import oneflow as flow
-    linear = flow.nn.Linear(3, 8, False)
-    linear = linear.to(device)
-    input_arr = np.array(
-        [
-            [-0.94630778, -0.83378579, -0.87060891],
-            [2.0289922, -0.28708987, -2.18369248],
-            [0.35217619, -0.67095644, -1.58943879],
-            [0.08086036, -1.81075924, 1.20752494],
-            [0.8901075, -0.49976737, -1.07153746],
-            [-0.44872912, -1.07275683, 0.06256855],
-            [-0.22556897, 0.74798368, 0.90416439],
-            [0.48339456, -2.32742195, -0.59321527],
-        ],
-        dtype=np.float32,
-    )
-    np_weight = np.ones((3, 8)).astype(np.float32)
-    np_weight.fill(2.3)
-    x = flow.tensor(input_arr, device=device)
-    flow.nn.init.constant_(linear.weight, 2.3)
-    of_eager_out = linear(x)
-    np_out = np.matmul(input_arr, np_weight)
-    test_case.assertTrue(np.allclose(of_eager_out.numpy(), np_out, 1e-05, 1e-05))
-
-    class LinearGraph(flow.nn.Graph):
-        def __init__(self):
-            super().__init__()
-            self.my_linear = linear
-
-        def build(self, x):
-            return self.my_linear(x)
-
-    linear_g = LinearGraph()
-    linear_g.debug(0)
-    of_lazy_out = linear_g(x)
-    test_case.assertTrue(np.array_equal(of_lazy_out.numpy(), of_eager_out.numpy()))
-
 def _test_linear(test_case, device):
     from typing import List
     import torch
