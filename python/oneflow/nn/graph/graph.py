ea20eff8b9707c3f2446a10fb22f96a302510466
Peiyuan Liu <hank010626@163.com>
2023-04-28 07:54:41 +0000

    update

--- python/oneflow/nn/graph/graph.py
+++ python/oneflow/nn/graph/graph.py
@@ -1151,7 +1151,7 @@ class Graph(object):
                 tensor_list.append(tensor_of_item.to(device_of_item))
             return (name_list, convert_to_tensor_tuple(tensor_list))
 
-        def _load_input_from_state_dict(state_dict):
+        def __load_input_from_runtime_state_dict(state_dict):
             tensor_list = []
             for _, item in state_dict.items():
                 tensor_of_item, device_of_item = item
@@ -1236,7 +1236,7 @@ class Graph(object):
         self._c_nn_graph.align_states_after_logical_graph_compile()
         self._c_nn_graph.init_runtime()
         self._is_compiled = True
-        self.__run(_load_input_from_state_dict(state_dict["inputs"]))  
+        self.__run(__load_input_from_runtime_state_dict(state_dict["inputs"]))  
         build_graph_end = time.perf_counter()
         self.__print(
             0,
