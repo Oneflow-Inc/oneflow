149fb27e3e4dba52ee4225bc8a3d6877dafdb58f
BBuf <1182563586@qq.com>
2023-04-28 05:48:56 +0000

    fix bug

--- python/oneflow/utils/backend/torch_fx_to_oneflow.py
+++ python/oneflow/utils/backend/torch_fx_to_oneflow.py
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,12 +13,16 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import re
+import inspect
+import operator
 import oneflow
 import torch
 from torch import fx
 
 mapping_dict = {}
+
+for _, func in inspect.getmembers(operator, inspect.isbuiltin):
+    mapping_dict[func] = func
 
 internal_oneflow_funcs = [
     "FunctionConfig",
@@ -36,19 +40,17 @@ internal_oneflow_funcs = [
     "framework",
 ]
 
-
 oneflow_funcs = dir(oneflow)
 for funcs_name in oneflow_funcs:
     if not funcs_name.startswith("_") and funcs_name not in internal_oneflow_funcs:
         if hasattr(torch, funcs_name):
-            mapping_dict[eval("torch." + funcs_name)] = "oneflow." + funcs_name
+            mapping_dict[eval("torch." + funcs_name)] = eval("oneflow." + funcs_name)
 
 oneflow_nn_functional_funcs = dir(oneflow.nn.functional)
 for funcs_name in oneflow_nn_functional_funcs:
     if not funcs_name.startswith("_") and funcs_name not in internal_oneflow_funcs:
         if hasattr(torch.nn.functional, funcs_name):
-            mapping_dict[eval("torch.nn.functional." + funcs_name)] = "oneflow.nn.functional." + funcs_name
-
+            mapping_dict[eval("torch.nn.functional." + funcs_name)] = eval("oneflow.nn.functional." + funcs_name)
 
 def to_of_transform(
     gm: torch.fx.GraphModule, tracer_class: type = fx.Tracer
@@ -57,10 +59,8 @@ def to_of_transform(
         # Checks if we're calling a function (i.e:
         # torch.add)
         if node.op == "call_function":
-            # The target attribute is the function
-            # that call_function calls.
-            node.target = eval(mapping_dict[node.target])
-        elif node.op == "call_method":
+            node.target = mapping_dict[node.target]
+        elif node.op == "call_method": 
             if hasattr(torch.Tensor, node.target):
                 pass
             else:
