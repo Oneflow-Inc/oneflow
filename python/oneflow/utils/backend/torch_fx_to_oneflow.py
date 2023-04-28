882f4219402998e66ab8220ada18fdbc859dc1fe
BBuf <1182563586@qq.com>
2023-04-28 06:12:17 +0000

    simpify code

--- python/oneflow/utils/backend/torch_fx_to_oneflow.py
+++ python/oneflow/utils/backend/torch_fx_to_oneflow.py
@@ -5,21 +5,6 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-"""
-Copyright 2020 The OneFlow Authors. All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE2.0
 
 Unless required by applicable law or agreed to in writing, software
@@ -68,7 +53,7 @@ for funcs_name in oneflow_nn_functional_funcs:
             mapping_dict[eval("torch.nn.functional." + funcs_name)] = eval("oneflow.nn.functional." + funcs_name)
 
 def to_of_transform(
-    gm: torch.fx.GraphModule, tracer_class: type = fx.Tracer
+    gm: torch.fx.GraphModule
 ) -> torch.fx.GraphModule:
     for node in gm.graph.nodes:
         if node.op == "call_function":
