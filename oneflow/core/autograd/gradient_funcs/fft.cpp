197f829b8691a6b4c8445752fd8594c22c555266
MarioLulab <3180101734@zju.edu.cn>
2023-05-08 11:12:33 +0800

    remove default attr value of fft ops

--- oneflow/core/autograd/gradient_funcs/fft.cpp
+++ oneflow/core/autograd/gradient_funcs/fft.cpp
@@ -34,56 +34,59 @@ struct FftR2CCaptureState : public AutoGradCaptureState {
 
 class FftR2C : public OpExprGradFunction<FftR2CCaptureState> {
  public:
-  Maybe<void> Init(const OpExpr& op) override {
-    const auto* fw_op_expr = dynamic_cast<const UserOpExpr*>(&op);
-    CHECK_NOTNULL_OR_RETURN(fw_op_expr);
-    return Maybe<void>::Ok();
-  }
+  Maybe<void> Init(const OpExpr& op) override { return Maybe<void>::Ok(); }
 
   Maybe<void> Capture(FftR2CCaptureState* ctx, const TensorTuple& inputs,
                       const TensorTuple& outputs, const AttrMap& attrs) const override {
-    CHECK_EQ_OR_RETURN(inputs.size(), 1);
-    ctx->requires_grad = inputs.at(0)->requires_grad();
+    CHECK_EQ_OR_RETURN(inputs.size(), 1) << Error::RuntimeError();
+    ctx->requires_grad = JUST(oneflow::VectorAt(inputs, 0))->requires_grad();
+    if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
+
     ctx->onesided = JUST(attrs.GetAttr<bool>("onesided"));
     ctx->dims = JUST(attrs.GetAttr<std::vector<int64_t>>("dims"));
     ctx->norm_mode = JUST(attrs.GetAttr<int32_t>("norm_mode"));
-    ctx->input_shape_vec = inputs.at(0)->shape()->dim_vec();
+    ctx->input_shape_vec = JUST(oneflow::VectorAt(inputs, 0))->shape()->dim_vec();
 
     return Maybe<void>::Ok();
   }
 
   Maybe<void> Apply(const FftR2CCaptureState* ctx, const TensorTuple& out_grads,
                     TensorTuple* in_grads) const override {
-    CHECK_EQ_OR_RETURN(out_grads.size(), 1);
+    CHECK_EQ_OR_RETURN(out_grads.size(), 1) << Error::RuntimeError();
+    if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
+
     in_grads->resize(1);
     if (!ctx->onesided) {
-      std::cout << "ctx->norm_mode = " << ctx->norm_mode << std::endl; 
-      std::cout << "ctx has no attrs of name \" forward \"" << std::endl; 
-      auto complex_grad =
-          JUST(functional::FftC2C(out_grads.at(0), ctx->dims, ctx->norm_mode,
-                                  /*forward*/ false));
-      in_grads->at(0) = JUST(functional::Real(complex_grad));
+      auto complex_grad = JUST(functional::FftC2C(JUST(oneflow::VectorAt(out_grads, 0)), NullOpt,
+                                                  ctx->dims, ctx->norm_mode,
+                                                  /*forward=*/false, /*normalized=*/false));
+      JUST(oneflow::VectorAt(*in_grads, 0)) = JUST(functional::Real(complex_grad));
     } else {
-      std::cout << "=========== [FftR2C Op Backward] ctx->onesided ===========" << std::endl;
-      Shape input_shape(ctx->input_shape_vec);
       std::vector<int64_t> fft_dims = ctx->dims;
       std::vector<int64_t> fft_shapes(fft_dims.size(), 0);
-      FOR_RANGE(size_t, i, 0, fft_dims.size()) { fft_shapes[i] = input_shape[fft_dims[i]]; }
+      FOR_RANGE(size_t, i, 0, fft_dims.size()) {
+        fft_shapes[i] = ctx->input_shape_vec[fft_dims[i]];
+      }
+
       // fill the last dim
       bool must_copy = false;
-      auto x_sizes = out_grads.at(0)->shape()->dim_vec();
+      auto x_sizes = JUST(oneflow::VectorAt(out_grads, 0))->shape()->dim_vec();
       std::vector<int64_t> pad_amount(x_sizes.size() * 2, 0);
       int64_t last_dim = ctx->dims.back();
-      if (x_sizes[last_dim] < ctx->input_shape_vec[last_dim]){
+      if (x_sizes[last_dim] < ctx->input_shape_vec[last_dim]) {
         must_copy = true;
         auto pad_idx = pad_amount.size() - 2 * last_dim - 1;
         pad_amount[pad_idx] = ctx->input_shape_vec[last_dim] - x_sizes[last_dim];
       }
-      auto complex_full_grad = must_copy ? JUST(functional::ConstantPad(out_grads.at(0), pad_amount, 0)) : out_grads.at(0);
-      complex_full_grad = JUST(functional::FftC2C(complex_full_grad, ctx->dims, ctx->norm_mode,
-                                  /*forward*/ false));
+      auto complex_full_grad =
+          must_copy
+              ? JUST(functional::ConstantPad(JUST(oneflow::VectorAt(out_grads, 0)), pad_amount, 0))
+              : JUST(oneflow::VectorAt(out_grads, 0));
+      complex_full_grad =
+          JUST(functional::FftC2C(complex_full_grad, NullOpt, ctx->dims, ctx->norm_mode,
+                                  /*forward=*/false, /*normalized=*/false));
 
-      in_grads->at(0) = JUST(functional::Real(complex_full_grad));
+      JUST(oneflow::VectorAt(*in_grads, 0)) = JUST(functional::Real(complex_full_grad));
     }
 
     return Maybe<void>::Ok();
@@ -99,17 +102,14 @@ struct FftC2CCaptureState : public AutoGradCaptureState {
 
 class FftC2C : public OpExprGradFunction<FftC2CCaptureState> {
  public:
-  Maybe<void> Init(const OpExpr& op) override {
-    const auto* fw_op_expr = dynamic_cast<const UserOpExpr*>(&op);
-    CHECK_NOTNULL_OR_RETURN(fw_op_expr);
-    return Maybe<void>::Ok();
-  }
+  Maybe<void> Init(const OpExpr& op) override { return Maybe<void>::Ok(); }
 
   Maybe<void> Capture(FftC2CCaptureState* ctx, const TensorTuple& inputs,
                       const TensorTuple& outputs, const AttrMap& attrs) const override {
-    CHECK_EQ_OR_RETURN(inputs.size(), 1);
+    CHECK_EQ_OR_RETURN(inputs.size(), 1) << Error::RuntimeError();
 
-    ctx->requires_grad = inputs.at(0)->requires_grad();
+    ctx->requires_grad = JUST(oneflow::VectorAt(inputs, 0))->requires_grad();
+    if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
 
     ctx->forward = JUST(attrs.GetAttr<bool>("forward"));
     ctx->dims = JUST(attrs.GetAttr<std::vector<int64_t>>("dims"));
@@ -120,10 +120,13 @@ class FftC2C : public OpExprGradFunction<FftC2CCaptureState> {
 
   Maybe<void> Apply(const FftC2CCaptureState* ctx, const TensorTuple& out_grads,
                     TensorTuple* in_grads) const override {
-    CHECK_EQ_OR_RETURN(out_grads.size(), 1);
+    CHECK_EQ_OR_RETURN(out_grads.size(), 1) << Error::RuntimeError();
+    if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
+
     in_grads->resize(1);
-    in_grads->at(0) = JUST(functional::FftC2C(out_grads.at(0), ctx->dims, ctx->norm_mode,
-                                              /*forward*/ !(ctx->forward)));
+    JUST(oneflow::VectorAt(*in_grads, 0)) = JUST(functional::FftC2C(
+        JUST(oneflow::VectorAt(out_grads, 0)), NullOpt, ctx->dims, ctx->norm_mode,
+        /*forward=*/!(ctx->forward), /*normalized=*/false));
     return Maybe<void>::Ok();
   }
 };
@@ -138,35 +141,40 @@ struct FftC2RCaptureState : public AutoGradCaptureState {
 
 class FftC2R : public OpExprGradFunction<FftC2RCaptureState> {
  public:
-  Maybe<void> Init(const OpExpr& op) override {
-    const auto* fw_op_expr = dynamic_cast<const UserOpExpr*>(&op);
-    CHECK_NOTNULL_OR_RETURN(fw_op_expr);
-    return Maybe<void>::Ok();
-  }
+  Maybe<void> Init(const OpExpr& op) override { return Maybe<void>::Ok(); }
 
   Maybe<void> Capture(FftC2RCaptureState* ctx, const TensorTuple& inputs,
                       const TensorTuple& outputs, const AttrMap& attrs) const override {
-    CHECK_EQ_OR_RETURN(inputs.size(), 1);
-    ctx->requires_grad = inputs.at(0)->requires_grad();
+    CHECK_EQ_OR_RETURN(inputs.size(), 1) << Error::RuntimeError();
+    ctx->requires_grad = JUST(oneflow::VectorAt(inputs, 0))->requires_grad();
+    if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
+
     ctx->dims = JUST(attrs.GetAttr<std::vector<int64_t>>("dims"));
     ctx->norm_mode = JUST(attrs.GetAttr<int32_t>("norm_mode"));
     ctx->last_dim_size = JUST(attrs.GetAttr<int64_t>("last_dim_size"));
-    ctx->input_shape_vec = inputs.at(0)->shape()->dim_vec();
+    ctx->input_shape_vec = JUST(oneflow::VectorAt(inputs, 0))->shape()->dim_vec();
 
     return Maybe<void>::Ok();
   }
 
   Maybe<void> Apply(const FftC2RCaptureState* ctx, const TensorTuple& out_grads,
                     TensorTuple* in_grads) const override {
-    CHECK_EQ_OR_RETURN(out_grads.size(), 1);
+    CHECK_EQ_OR_RETURN(out_grads.size(), 1) << Error::RuntimeError();
+    if (!ctx->requires_grad) { return Maybe<void>::Ok(); }
+
     in_grads->resize(1);
-    auto complex_grad = JUST(functional::FftR2C(out_grads.at(0), ctx->dims, ctx->norm_mode, /*onesided=*/true)); // no need conj
+
+    // NOTE: set `forward` True to prevent conjugating result
+    auto complex_grad = JUST(functional::FftR2C(
+        JUST(oneflow::VectorAt(out_grads, 0)), NullOpt, ctx->dims, ctx->norm_mode,
+        /*onesided=*/true, /*forward=*/true, /*normalized=*/false));  // no need conj
     Shape input_shape(ctx->input_shape_vec);
     int64_t last_dim = ctx->dims.back();
-    auto double_length = out_grads.at(0)->dim(last_dim) - complex_grad->dim(last_dim);
+    auto double_length =
+        JUST(oneflow::VectorAt(out_grads, 0))->dim(last_dim) - complex_grad->dim(last_dim);
     auto in_grad = complex_grad;
 
-    // mul by 2, and slice
+    // Mul by 2, and slice
     if (double_length > 0) {
       in_grad = JUST(functional::Narrow(complex_grad, last_dim, 1,
                                         double_length));  // will change shape of in_grad
@@ -179,7 +187,7 @@ class FftC2R : public OpExprGradFunction<FftC2RCaptureState> {
     auto sliced_tensor =
         JUST(functional::Slice(complex_grad, slice_st, slice_end, slice_step, false));
 
-    in_grads->at(0) = sliced_tensor;
+    JUST(oneflow::VectorAt(*in_grads, 0)) = sliced_tensor;
     return Maybe<void>::Ok();
   }
 };
