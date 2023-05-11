4fe35fd7791b243fe95cb689b6e19f8e392a4947
yuhao <1171760467@qq.com>
2023-05-11 16:01:28 +0800

    fix

--- oneflow/ir/lib/Transform/TransformDialectExtension.cpp
+++ oneflow/ir/lib/Transform/TransformDialectExtension.cpp
@@ -1,31 +1,3 @@
-/*
-Copyright 2020 The OneFlow Authors. All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-//===- TransformDialectExtension.cpp ----------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This file defines an extension of the MLIR Transform dialect for testing
-// purposes.
-//
-//===----------------------------------------------------------------------===//
-
 #include "Transform/TransformDialectExtension.h"
 #include "Transform/TransformStateExtension.h"
 #include "mlir/Dialect/PDL/IR/PDL.h"
--- oneflow/ir/lib/Transform/TransformDialectInterpreter.cpp
+++ oneflow/ir/lib/Transform/TransformDialectInterpreter.cpp
@@ -1,31 +1,3 @@
-/*
-Copyright 2020 The OneFlow Authors. All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-//===- TransformDialectInterpreter.cpp --------------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This file defines a test pass that interprets Transform dialect operations in
-// the module.
-//
-//===----------------------------------------------------------------------===//
-
 #include "mlir/Dialect/Transform/IR/TransformInterfaces.h"
 #include "mlir/Dialect/Transform/Transforms/TransformInterpreterPassBase.h"
 #include "mlir/IR/Builders.h"
