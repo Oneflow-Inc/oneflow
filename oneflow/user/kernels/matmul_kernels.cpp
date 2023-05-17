78b599da2953ffa11906254de93b86c026591cef
lucky <17835344407@163.com>
2023-05-17 06:49:50 +0000

    [fix]: delete useless code

--- oneflow/user/kernels/matmul_kernels.cpp
+++ oneflow/user/kernels/matmul_kernels.cpp
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include <iostream>
 #include "oneflow/core/framework/framework.h"
 #include "oneflow/core/kernel/new_kernel_util.h"
 #include "oneflow/core/framework/config_def.h"
