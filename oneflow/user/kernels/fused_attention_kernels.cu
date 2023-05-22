795702d24cf19cb85b86ceeb36451b78c654929f
clackhan <han_binbin@163.com>
2023-05-22 18:27:01 +0800

    fuse rms and add

--- oneflow/user/kernels/fused_attention_kernels.cu
+++ oneflow/user/kernels/fused_attention_kernels.cu
@@ -32,9 +32,98 @@ limitations under the License.
 #include "oneflow/user/kernels/collective_communication/include/all_reduce.h"
 #include "oneflow/core/ep/include/primitive/unary_op.h"
 #include "oneflow/core/ep/cuda/primitive/unary_functor.cuh"
+#include "oneflow/core/rpc/include/global_process_ctx.h"
+#include "oneflow/core/cuda/rms_norm_output_norm_arg.cuh"
 
 namespace oneflow {
 namespace {
+
+template<typename SRC, typename DST>
+struct SkipLoad {
+  using LoadType = DST;
+  SkipLoad(const SRC* src, const SRC* skip, int64_t row_size)
+      : src(src), skip(skip), row_size(row_size) {}
+  template<int N>
+  __device__ void load(DST* dst, int64_t row, int64_t col) const {
+    cuda::layer_norm::Pack<SRC, N> src_pack;
+    cuda::layer_norm::Pack<SRC, N> skip_pack;
+    const int64_t offset = (row * row_size + col) / N;
+    src_pack.storage = *(reinterpret_cast<const cuda::layer_norm::PackType<SRC, N>*>(src) + offset);
+    if (skip) {
+      skip_pack.storage = *(reinterpret_cast<const cuda::layer_norm::PackType<SRC, N>*>(skip) + offset);
+    } else {
+#pragma unroll
+      for (int i = 0; i < N; ++i) { skip_pack.elem[i] = static_cast<SRC>(0.f); }
+    }
+#pragma unroll
+    for (int i = 0; i < N; ++i) {
+      dst[i] = static_cast<DST>(src_pack.elem[i] + skip_pack.elem[i]);
+    }
+  }
+  const SRC* src;
+  const SRC* skip;
+  int64_t row_size;
+};
+
+template<typename SRC, typename DST, bool affine>
+struct AffineStore {
+  AffineStore(DST* dst, const DST* weight, int32_t row_size)
+      : dst(dst), weight(weight), row_size(row_size) {}
+
+  template<int N>
+  __device__ void store(const SRC* src, int32_t row, int32_t col) {
+    cuda::layer_norm::Pack<DST, N> dst_pack;
+    cuda::layer_norm::Pack<DST, N> weight_pack;
+    const int32_t offset = (row * row_size + col) / N;
+    const int32_t weight_offset = col / N;
+    if (affine) {
+      weight_pack.storage =
+          *(reinterpret_cast<const cuda::layer_norm::PackType<DST, N>*>(weight) + weight_offset);
+    }
+#pragma unroll
+    for (int i = 0; i < N; ++i) {
+      if (affine) {
+        dst_pack.elem[i] = static_cast<DST>(src[i]) * weight_pack.elem[i];
+      } else {
+        dst_pack.elem[i] = static_cast<DST>(src[i]);
+      }
+    }
+    *(reinterpret_cast<cuda::layer_norm::PackType<DST, N>*>(dst) + offset) = dst_pack.storage;
+  }
+
+  DST* dst;
+  const DST* weight;
+  int32_t row_size;
+};
+
+template<typename T, typename ComputeType, bool affine>
+void DispatchSkipRmsNormOutputNormArgForwardAffine(ep::Stream* stream, const int64_t nrow, const int64_t ncol,
+                                      const double eps, const T* x_dptr,
+                                      const T* w_dptr, const T* skip_dptr,
+                                      T* norm_arg_dptr, T* y_dptr, ComputeType* inv_rms) {
+  constexpr int32_t block_size = 128;
+  unsigned int nb_element = nrow * ncol;
+  unsigned int grid_size = (nb_element + block_size - 1) / block_size;
+  SkipLoad<T, ComputeType> load(x_dptr, skip_dptr, ncol);
+  cuda::layer_norm::DirectStore<ComputeType, T> norm_arg_store(norm_arg_dptr, ncol);
+  AffineStore<ComputeType, T, affine> output_store(y_dptr, w_dptr, ncol);
+  OF_CUDA_CHECK((cuda::rms_norm_output_norm_arg::LaunchRmsNormOutputNormArg<decltype(load),  decltype(norm_arg_store), decltype(output_store), ComputeType>(
+      stream->As<ep::CudaStream>()->cuda_stream(), load, norm_arg_store, output_store, nrow, ncol, eps, inv_rms)));
+}
+
+template<typename T, typename ComputeType>
+void SkipRmsNormOutputNormArgForward(ep::Stream* stream, const int64_t nrow, const int64_t ncol,
+                        const double eps, const T* x_dptr, const T* w_dptr,
+                        const T* skip_dptr,  T* norm_arg_dptr, T* y_dptr, ComputeType* inv_rms) {
+  if (w_dptr) {
+    DispatchSkipRmsNormOutputNormArgForwardAffine<T, ComputeType, true>(
+        stream, nrow, ncol, eps, x_dptr, w_dptr, skip_dptr, norm_arg_dptr, y_dptr, inv_rms);
+  } else {
+    DispatchSkipRmsNormOutputNormArgForwardAffine<T, ComputeType, false>(
+        stream, nrow, ncol, eps, x_dptr, w_dptr, skip_dptr, norm_arg_dptr, y_dptr, inv_rms);
+  }
+}
+
 union CublasScalarParameter {
   double d;
   float s;
@@ -1768,16 +1857,13 @@ class LlamaDecoderLayerForwardKernel final : public user_op::OpKernel {
       all_reduce->Launch(ctx->stream(), tmp_buffer->dptr(), tmp_buffer->mut_dptr(),
                          input_num_elements, kernel_cache->communication_ctx());
       // step 8, residual add: out = out + hidden_states
-      cuda::elementwise::Binary(Add<T>(), input_num_elements, output->mut_dptr<T>(),
-                                tmp_buffer->dptr<T>(), hidden_states->dptr<T>(),
-                                cuda_stream->cuda_stream());
-
       // step 9, post rms-norm
       h = output->shape_view().At(2) / k;
-      cuda::rms_norm::RmsNormForward(
-          cuda_stream, bm, h * k, 1e-6, output->dptr<T>(), post_norm_weight->dptr<T>(),
-          tmp_buffer->mut_dptr<T>(),
-          reinterpret_cast<RmsNormComputeType*>(tmp_buffer->mut_dptr<char>() + activation_size));
+      SkipRmsNormOutputNormArgForward<T>(cuda_stream, bm, h * k, 1e-6, hidden_states->dptr<T>(),
+                                      post_norm_weight->dptr<T>(), tmp_buffer->dptr<T>(),
+                                      output->mut_dptr<T>(), tmp_buffer->mut_dptr<T>(),
+                                      reinterpret_cast<RmsNormComputeType*>(tmp_buffer->mut_dptr<char>() + activation_size));
+
       // step 10, fused_glu, + silu
       auto glu_matmul_n = mlp_gate_weight->shape_view().At(0);
       T* gate_ptr = tmp_buffer->mut_dptr<T>() + input_num_elements;
