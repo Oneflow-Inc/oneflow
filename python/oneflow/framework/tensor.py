3e63f9d2b99b65c051032794512b4281034be15d
Ldpe2G <liangdepeng@gmail.com>
2023-08-08 03:46:01 +0000

    tensor add storage method

--- python/oneflow/framework/tensor.py
+++ python/oneflow/framework/tensor.py
@@ -532,6 +532,8 @@ def _conj(self):
 def _conj_physical(self):
     return flow._C.conj_physical(self)
 
+def _storage(self):
+    return self
 
 @property
 def _layout(self):
@@ -610,6 +612,7 @@ def RegisterMethods():
     Tensor.conj = _conj
     Tensor.conj_physical = _conj_physical
     Tensor.layout = _layout
+    Tensor.storage = _storage
 
 
 def register_tensor_op(op_name):
