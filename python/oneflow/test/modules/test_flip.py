2d24fe08be1b1bedcc22fb409c5d688924ce89fc
Wang Yi <53533850+marigoold@users.noreply.github.com>
2023-08-15 12:37:39 +0800

    flip memory coalescing for last dim case (#10310)
    针对 dim = -1 时候访存无法合并的情况做了优化。
    实现方式是先 flip 后写到 shared memory，然后统一从 shm 中顺序写到 global memory 中，此时可以合并访存。
    对比：
    ```
    import oneflow as flow
    x = flow.randn(32, 2048).cuda()
    
    flow._oneflow_internal.profiler.RangePush("flip_prof")
    for i in range(100):
        y = flow.flip(x, [-1])
    flow._oneflow_internal.profiler.RangePop()
    
    import torch
    x = torch.randn(32, 2048).cuda()
    
    torch.cuda.nvtx.range_push("flip_prof")
    for i in range(100):
        y = torch.flip(x, [-1])
    torch.cuda.nvtx.range_pop()
    ```
    
    - nsys 结果对比
    
    torch kernel：
    
    ![image](https://github.com/Oneflow-Inc/oneflow/assets/53533850/77344b9b-3485-4f36-971f-76bce1cca9b7)
    
    oneflow kernel（优化后）：
    
    ![image](https://github.com/Oneflow-Inc/oneflow/assets/53533850/b244f779-7478-48dd-b237-0f8153323ef9)
    
    oneflow kernel（优化前）：
    
    ![image](https://github.com/Oneflow-Inc/oneflow/assets/53533850/72a180da-a23d-4b93-a208-beb943a11e48)
    
    ---------
    
    Co-authored-by: oneflow-ci-bot <ci-bot@oneflow.org>

--- python/oneflow/test/modules/test_flip.py
+++ python/oneflow/test/modules/test_flip.py
@@ -55,9 +55,18 @@ class TestFlip(flow.unittest.TestCase):
         y = torch.flip(x, constant((0, 1, 2)))
         return y
 
+    def test_flow_flip_list_lastdim_with_random_data(test_case):
+        device = random_device()
+        x = random_tensor(
+            ndim=4, dim1=random().to(int), dim2=random().to(int), dim3=random().to(int)
+        ).to(device)
+        y = torch.flip(x, [-1,])
+        return y
+
     @profile(torch.flip)
     def profile_flip(test_case):
         torch.flip(torch.ones(100, 100, 100), [0, 1])
+        torch.flip(torch.ones(1, 100000), [-1,])
 
 
 if __name__ == "__main__":
