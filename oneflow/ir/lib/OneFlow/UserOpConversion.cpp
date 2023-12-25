9381871707eaad1f22a311c8603901abeff34ee2
lyon <flowingsun007@163.com>
2023-12-24 21:23:51 -0500

    support huawei ascend910b chip

--- oneflow/ir/lib/OneFlow/UserOpConversion.cpp
+++ oneflow/ir/lib/OneFlow/UserOpConversion.cpp
@@ -486,6 +486,8 @@ LogicalResult ConvertUserOpInputs(llvm::StringRef op_type_name, ValueRange opera
     return ::oneflow::DeviceType::kCUDA;
   } else if (device_tag.str() == "mlu") {
     return ::oneflow::DeviceType::kMLU;
+  } else if (device_tag.str() == "npu") {
+    return ::oneflow::DeviceType::kMLU;
   } else {
     LOG(FATAL) << "unsupported device tag: " << device_tag.str();
     return ::oneflow::DeviceType::kInvalidDevice;
