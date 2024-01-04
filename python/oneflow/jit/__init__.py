028e05f66c862f8ab757e2024bc47d473afb40b1
WangYi <buaawangyi03@gmail.com>
2024-01-04 06:08:17 +0000

    add dummy interfaces for import timm

--- python/oneflow/jit/__init__.py
+++ python/oneflow/jit/__init__.py
@@ -16,6 +16,10 @@ limitations under the License.
 import warnings
 from typing import Any, Dict, List, Set, Tuple, Union, Callable
 
+warnings.warn(
+    "The oneflow.jit interface is just to align the torch.jit interface and has no practical significance."
+)
+
 
 def script(
     obj,
@@ -71,3 +75,69 @@ def is_scripting():
 
 def is_tracing():
     return False
+
+
+class _Final:
+    """Mixin to prohibit subclassing"""
+
+    __slots__ = ("__weakref__",)
+
+    def __init_subclass__(self, /, *args, **kwds):
+        if "_root" not in kwds:
+            raise TypeError("Cannot subclass special typing classes")
+
+
+class _SpecialForm(_Final, _root=True):
+    __slots__ = ("_name", "__doc__", "_getitem")
+
+    def __init__(self, getitem):
+        self._getitem = getitem
+        self._name = getitem.__name__
+        self.__doc__ = getitem.__doc__
+
+    def __getattr__(self, item):
+        if item in {"__name__", "__qualname__"}:
+            return self._name
+
+        raise AttributeError(item)
+
+    def __mro_entries__(self, bases):
+        raise TypeError(f"Cannot subclass {self!r}")
+
+    def __repr__(self):
+        return "typing." + self._name
+
+    def __reduce__(self):
+        return self._name
+
+    def __call__(self, *args, **kwds):
+        raise TypeError(f"Cannot instantiate {self!r}")
+
+    def __or__(self, other):
+        return Union[self, other]
+
+    def __ror__(self, other):
+        return Union[other, self]
+
+    def __instancecheck__(self, obj):
+        raise TypeError(f"{self} cannot be used with isinstance()")
+
+    def __subclasscheck__(self, cls):
+        raise TypeError(f"{self} cannot be used with issubclass()")
+
+    def __getitem__(self, parameters):
+        return self._getitem(self, parameters)
+
+
+@_SpecialForm
+def Final(*args, **kwargs):
+    warnings.warn(
+        "The oneflow.jit.Final interface is just to align the torch.jit.Final interface and has no practical significance."
+    )
+
+
+def interface(fn):
+    warnings.warn(
+        "The oneflow.jit.interface interface is just to align the torch.jit.interface interface and has no practical significance."
+    )
+    return fn
