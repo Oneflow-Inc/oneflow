edce8d1c2d37539d1c59725ac47e7897e6aeef72
lihuizhao <1075452679@qq.com>
2024-01-23 06:25:57 +0000

    remove test_jacobian

--- python/oneflow/test/misc/test_autograd_functional.py
+++ python/oneflow/test/misc/test_autograd_functional.py
@@ -110,6 +110,7 @@ class TestAutogradFunctional(flow.unittest.TestCase):
         )
         result_tensors = torch.autograd.functional.hvp(_func_multi_scalar, inputs, v)
 
+    """
     @autotest(n=1, check_graph=False)
     def test_jacobian(test_case):
         inputs = random_tensor(ndim=2, dim0=5, dim1=5)
@@ -124,6 +125,7 @@ class TestAutogradFunctional(flow.unittest.TestCase):
         result_tensors = torch.autograd.functional.jacobian(
             _func_multi_scalar, inputs, vectorize=False, strategy="reverse-mode"
         )
+    """
 
     """
     @autotest(n=1, check_graph=False)
