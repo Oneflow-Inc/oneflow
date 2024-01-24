28e58a4b27e31339714ba5c88bde381ef708dd4a
lihuizhao <1075452679@qq.com>
2024-01-24 12:29:01 +0000

    move layer_norm code from modules to functional

--- python/oneflow/nn/functional/__init__.py
+++ python/oneflow/nn/functional/__init__.py
@@ -65,7 +65,7 @@ from oneflow._C import tanh
 from oneflow._C import threshold
 from oneflow._C import silu
 from oneflow._C import mish
-from oneflow.nn.modules.normalization import layer_norm
+from .normalization import layer_norm
 from oneflow._C import dropout, dropout1d, dropout2d, dropout3d
 from oneflow._C import smooth_l1_loss
 from .pad import pad
--- python/oneflow/nn/modules/normalization.py
+++ python/oneflow/nn/modules/normalization.py
@@ -20,6 +20,7 @@ import oneflow as flow
 from oneflow.framework.tensor import Tensor
 from oneflow.nn import init
 from oneflow.nn.modules.module import Module
+import oneflow.nn.functional as F
 
 _shape_t = Union[int, Tuple[int], flow._oneflow_internal.Size]
 
@@ -141,69 +142,6 @@ class GroupNorm(Module):
         )
 
 
-def layer_norm(input, normalized_shape, weight=None, bias=None, eps=1e-05):
-    assert len(input.shape) > len(
-        normalized_shape
-    ), "Input tensor dim must greater than normalized dim!"
-    begin_norm_axis = len(input.shape) - len(normalized_shape)
-    begin_params_axis = len(input.shape) - len(normalized_shape)
-
-    elementwise_affine = True if (weight is not None and bias is not None) else False
-
-    for i in range(0, len(normalized_shape)):
-        if input.shape[i + begin_params_axis] != normalized_shape[i]:
-            raise RuntimeError(
-                f"Given normalized_shape={normalized_shape}, expected input with shape [*, {str(normalized_shape)[1:-1]}], but got input of size {input.shape}"
-            )
-
-    if input.is_cpu:
-        reduce_axis = []
-        for dim in range(len(input.shape)):
-            if dim >= begin_norm_axis:
-                reduce_axis.append(dim)
-        mean = input.mean(dim=reduce_axis, keepdim=True)
-        variance = input.var(dim=reduce_axis, unbiased=False, keepdim=True)
-        params_shape = input.shape[begin_params_axis:]
-        if len(mean.shape) == 1:
-            nd_params_shape = [1] * len(input.shape)
-            nd_params_shape[begin_norm_axis] = params_shape[0]
-            mean = flow.reshape(mean, shape=nd_params_shape)
-            variance = flow.reshape(variance, nd_params_shape)
-            if weight is not None and params_shape[0] == weight.nelement():
-                weight = flow.reshape(weight, shape=nd_params_shape)
-            if bias is not None and params_shape[0] == bias.nelement():
-                bias = flow.reshape(bias, shape=nd_params_shape)
-        elif len(mean.shape) == len(input.shape):
-            pass
-        else:
-            raise ValueError(
-                "shape of mean and variance should be 1D or has number of axes and x's"
-            )
-        variance += eps
-        normalized = (input - mean) * variance.rsqrt()
-        if elementwise_affine:
-            normalized = normalized * weight + bias
-        return normalized
-    else:
-        if elementwise_affine:
-            res = flow._C.layer_norm_affine(
-                input,
-                weight,
-                bias,
-                begin_norm_axis=begin_norm_axis,
-                begin_params_axis=begin_params_axis,
-                epsilon=eps,
-            )
-        else:
-            res = flow._C.layer_norm(
-                input,
-                begin_norm_axis=begin_norm_axis,
-                begin_params_axis=begin_params_axis,
-                epsilon=eps,
-            )
-        return res
-
-
 class LayerNorm(Module):
     """Applies Layer Normalization over a mini-batch of inputs as described in
     the paper `Layer Normalization <https://arxiv.org/abs/1607.06450>`__
@@ -317,7 +255,7 @@ class LayerNorm(Module):
             init.zeros_(self.bias)
 
     def forward(self, x):
-        return layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
 
     def extra_repr(self) -> str:
         return "{normalized_shape}, eps={eps}, elementwise_affine={elementwise_affine}".format(
