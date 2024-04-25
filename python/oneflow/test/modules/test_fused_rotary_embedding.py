9fe84e16190d825ebbf82826a9cc60d50da5e574
shw <shw20010329@163.com>
2024-04-25 20:19:58 +0800

    add test_fused_rope

--- python/oneflow/test/modules/test_fused_rotary_embedding.py
+++ python/oneflow/test/modules/test_fused_rotary_embedding.py
@@ -22,7 +22,115 @@ import oneflow as flow
 import numpy as np
 import math
 
+# tensor version:
+def plane_shuffle_tensor(x):
+    x1, x2 = x[..., :x.shape[-1] // 2], x[..., x.shape[-1] // 2:]
+    return flow.cat((-x2, x1), dim=-1)
 
+
+def shuffle_adjacent_two_elem_tensor(x):
+    y = x.clone()  # 使用.clone()以确保我们不会在原始输入上进行操作
+    for i in range(x.shape[-1] // 2):
+        y[..., 2*i] = -x[..., 2*i + 1]
+        y[..., 2*i + 1] = x[..., 2*i]
+    return y
+
+
+def parseDims_tensor(dims, x_layout):
+    B, M, H, K = 1, 1, 1, 1  # 初始化维度
+    merged_dims = dims
+    if x_layout == "BHMK":
+        B, H, M, K = dims
+    elif x_layout == "BMHK":
+        B, M, H, K = dims
+    elif x_layout == "MBHK":
+        M, B, H, K = dims
+    elif x_layout == "BM(HK)":
+        B, M, H, K = dims
+        merged_dims = [dims[0], dims[1], dims[2] * dims[3]]  # merge H and K
+    elif x_layout == "MB(HK)":
+        M, B, H, K = dims
+        merged_dims = [dims[0], dims[1], dims[2] * dims[3]]
+    elif x_layout == "BM(H3K)":
+        B, M, H, K = dims
+        merged_dims = [dims[0], dims[1], 3 * dims[2] * dims[3]]  # merge and scale
+    elif x_layout == "MB(H3K)":
+        M, B, H, K = dims
+        merged_dims = [dims[0], dims[1], 3 * dims[2] * dims[3]]
+
+    return B, M, H, K, merged_dims
+
+
+def naive_embedding_tensor(x, cos, sin, x_layout, B, M, H, K, dims, merged_dims, rotary_size, rotary_ndims, mode):
+    naive_out = None
+    if mode == "plane":
+        if rotary_ndims == 2:
+            y1 = plane_shuffle_tensor(x[..., : rotary_size // 2])
+            y2 = plane_shuffle_tensor(x[..., rotary_size // 2 : rotary_size])
+            y3 = x[..., rotary_size:]
+            y = flow.cat((y1, y2, y3), dim=-1)
+        else:
+            y1 = plane_shuffle_tensor(x[..., :rotary_size])
+            y2 = x[..., rotary_size:]
+            y = flow.cat((y1, y2), dim=-1)
+    else:
+        y = shuffle_adjacent_two_elem_tensor(x)
+
+    if x_layout == "BHMK":
+        naive_out = x * cos + y * sin
+    elif x_layout == "BMHK":
+        naive_out = x.reshape(dims) * cos.reshape([B, M, 1, K]) + y.reshape(
+            dims
+        ) * sin.reshape(
+            [B, M, 1, K]
+        )  # un-merge
+    elif x_layout == "MBHK" or x_layout == "MB(HK)":
+        naive_out = x.reshape(dims) * cos.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        ) + y.reshape(dims) * sin.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        )  # un-merge
+    elif x_layout == "BM(HK)":
+        naive_out = x.reshape(dims) * cos.reshape([B, M, 1, K]) + y.reshape(
+            dims
+        ) * sin.reshape(
+            [B, M, 1, K]
+        )  # un-merge
+    elif x_layout == "BM(H3K)":
+        out0 = x[..., 0, :].reshape(dims) * cos.reshape([B, M, 1, K]) + y[
+            ..., 0, :
+        ].reshape(dims) * sin.reshape([B, M, 1, K])
+        out1 = x[..., 1, :].reshape(dims) * cos.reshape([B, M, 1, K]) + y[
+            ..., 1, :
+        ].reshape(dims) * sin.reshape([B, M, 1, K])
+        out2 = x[..., 2, :].reshape(dims) * cos.reshape([B, M, 1, K]) + y[
+            ..., 2, :
+        ].reshape(dims) * sin.reshape([B, M, 1, K])
+
+        naive_out = flow.cat((out0, out1, out2), axis=-1)
+    elif x_layout == "MB(H3K)":
+        out0 = x[..., 0, :].reshape(dims) * cos.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        ) + y[..., 0, :].reshape(dims) * sin.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        )
+        out1 = x[..., 1, :].reshape(dims) * cos.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        ) + y[..., 1, :].reshape(dims) * sin.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        )
+        out2 = x[..., 2, :].reshape(dims) * cos.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        ) + y[..., 2, :].reshape(dims) * sin.transpose([2, 0, 1, 3]).reshape(
+            [M, B, 1, K]
+        )
+
+        naive_out = flow.cat((out0, out1, out2), axis=-1)
+
+    return naive_out
+
+
+# numpy version:
 def plane_shuffle(x):
     x1, x2 = x[..., : x.shape[-1] // 2], x[..., x.shape[-1] // 2 :]
     return np.concatenate((-x2, x1), axis=-1)
@@ -571,7 +679,7 @@ def _test_with_position_sinuous(
         naive_x = x.reshape([B, M, H, -1, K])
     elif x_layout == "MB(HK)" or x_layout == "MB(H2K)" or x_layout == "MB(H3K)":
         naive_x = x.reshape([M, B, H, -1, K])
-
+    
     naive_out = naive_embedding(
         naive_x,
         naive_cos,
@@ -587,6 +695,41 @@ def _test_with_position_sinuous(
         rotary_ndims,
         mode,
     )
+
+    naive_x_tensor = flow.tensor(naive_x, dtype=dtype, device=device, requires_grad=True)
+    naive_cos_tensor = flow.tensor(naive_cos, dtype=dtype, device=device)  # 不用grad
+    naive_sin_tensor = flow.tensor(naive_sin, dtype=dtype, device=device)
+    naive_out_tensor = naive_embedding_tensor(
+        naive_x_tensor,
+        naive_cos_tensor,
+        naive_sin_tensor,
+        x_layout,
+        B,
+        M,
+        H,
+        K,
+        dims,
+        merged_dims,
+        rotary_size,
+        rotary_ndims,
+        mode,
+    )
+    #print('naive_out_tensor')
+   #print(naive_out_tensor)   # 验证这里的naive_out_tensor与naive_out是否有精度误差;
+    # get grad
+    test_case.assertTrue(
+        np.allclose(
+            naive_out.reshape(merged_dims),
+            naive_out_tensor.numpy().reshape(merged_dims),
+            atol=5e-2,
+            rtol=5e-3,
+        )
+    )
+
+    naive_out_backward = naive_out_tensor.sum()
+    naive_out_backward.backward()
+    naive_out_grad = naive_x_tensor.grad
+    #print(naive_out_grad)
 
     fused_cos = np.array(
         [
@@ -673,7 +816,8 @@ def _test_with_position_sinuous(
             tensor_index=2,
         )
 
-        fused_out = np.concatenate((out0, out1, out2), axis=-1)
+        #fused_out = np.concatenate((out0, out1, out2), axis=-1)
+        fused_out = flow.cat((out0, out1, out2), dim = -1)
     else:
         fused_out = flow._C.fused_apply_rotary_emb(
             fused_x,
@@ -685,12 +829,27 @@ def _test_with_position_sinuous(
             base=base,
             rotary_size=rotary_size,
             mode=mode,
-        ).numpy()
+        )
 
+    fused_out_backward = fused_out.sum()
+    fused_out_backward.backward()
+    fused_out_grad = fused_out_backward.grad
+    #print(fused_out_grad)
+    # test forward
     test_case.assertTrue(
         np.allclose(
             naive_out.reshape(merged_dims),
-            fused_out.reshape(merged_dims),
+            fused_out.numpy().reshape(merged_dims),
+            atol=5e-2,
+            rtol=5e-3,
+        )
+    )
+
+    # test backward
+    test_case.assertTrue(
+        np.allclose(
+            naive_out_grad.numpy().reshape(merged_dims),
+            fused_out_grad.numpy().reshape(merged_dims),
             atol=5e-2,
             rtol=5e-3,
         )
