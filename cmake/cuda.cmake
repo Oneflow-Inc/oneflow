b949aeb524564b77d8e91b9167a9e7024a5e695c
ShawnXuan <xiexuanx2@gmail.com>
2025-02-28 03:42:27 +0000

    add WITH_NPU

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -19,6 +19,8 @@ if(BUILD_CUDA)
     message(FATAL_ERROR "CUDA_NVCC_GENCODES is deprecated, use CMAKE_CUDA_ARCHITECTURES instead")
   endif()
   add_definitions(-DWITH_CUDA)
+  add_definitions(-DWITH_NPU)
+
   # NOTE: For some unknown reason, CUDAToolkit_VERSION may become empty when running cmake again
   set(CUDA_VERSION ${CUDAToolkit_VERSION} CACHE STRING "")
   if(NOT CUDA_VERSION)
