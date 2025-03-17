efd7b1c9717c94fcd4ce614ef5379641496b1240
luyang <Flowingsun007@163.com>
2025-03-17 07:50:45 +0000

    refine nccl_use_compute_stream

--- oneflow/core/job/resource_desc.cpp
+++ oneflow/core/job/resource_desc.cpp
@@ -73,12 +73,7 @@ CollectiveBoxingConf ResourceDesc::collective_boxing_conf() const {
 bool ResourceDesc::nccl_use_compute_stream() const {
 #if defined(WITH_CUDA) && NCCL_VERSION_CODE > 2700
   return resource_.nccl_use_compute_stream();
-#else
-  return false;
-#endif
-
-// TODO: find a batter way for multi devices
-#if defined(WITH_NPU)
+#elif defined(WITH_NPU)  // TODO: find a batter way for multi devices
   return true;
 #else
   return false;
